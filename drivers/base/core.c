7fa11c2139c708ef05483e65938d81afa42f54be
Greg Kroah-Hartman <gregkh@suse.de>
2002-04-09 12:14:34 -0700

    kobject: put kobject_actions in kobject.h
    This prevents the extern declaration in the driver core.
    
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "base.h"
 #include "power/power.h"
 
-extern const char *kobject_actions[];
-
 int (*platform_notify)(struct device * dev) = NULL;
 int (*platform_notify_remove)(struct device * dev) = NULL;
 
