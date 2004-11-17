b6e363059766ea8e974a89cde19ec7a2ed81d399
Anton Altaparmakov <aia21@cantab.net>
2004-11-17 15:45:08 +0000

    NTFS: Use i_size_read() in fs/ntfs/file.c::ntfs_file_open().
    Signed-off-by: Anton Altaparmakov <aia21@cantab.net>

--- fs/ntfs/file.c
+++ fs/ntfs/file.c
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static int ntfs_file_open(struct inode *vi, struct file *filp)
 {
 	if (sizeof(unsigned long) < 8) {
-		if (vi->i_size > MAX_LFS_FILESIZE)
+		if (i_size_read(vi) > MAX_LFS_FILESIZE)
 			return -EFBIG;
 	}
 	return generic_file_open(vi, filp);
