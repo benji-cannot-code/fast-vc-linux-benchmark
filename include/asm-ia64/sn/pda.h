89aa9d3552297c324d2108be19b409099a80043f
Dean Nelson <dcn@sgi.com>
2005-03-22 16:00:00 -0700

    [IA64-SGI] move nodepda pointer out of pda
    Remove the p_nodepda and p_subnodepda pointers from the pda_s structure.
    And then define a new per-cpu pointer to the nodepda and export it so
    that it can be accessed by kernel modules.
    
    Signed-off-by: Dean Nelson <dcn@sgi.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/asm-ia64/sn/pda.h
+++ include/asm-ia64/sn/pda.h
@@ -25,14 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef struct pda_s {
 
-	/* Having a pointer in the begining of PDA tends to increase
-	 * the chance of having this pointer in cache. (Yes something
-	 * else gets pushed out). Doing this reduces the number of memory
-	 * access to all nodepda variables to be one
-	 */
-	struct nodepda_s *p_nodepda;		/* Pointer to Per node PDA */
-	struct subnodepda_s *p_subnodepda;	/* Pointer to CPU  subnode PDA */
-
 	/*
 	 * Support for SN LEDs
 	 */
