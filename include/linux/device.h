de2941d22c2acf1414c78d2cdc9354c24239f3f2
mochel@digitalimplant.org <mochel@digitalimplant.org>
2005-03-24 19:08:30 -0800

    [PATCH] Use a klist for device child lists.
    - Use klist iterator in device_for_each_child(), making it safe to use for
      removing devices.
    - Remove unused list_to_dev() function.
    - Kills all usage of devices_subsys.rwsem.
    
    Signed-off-by: Patrick Mochel <mochel@digitalimplant.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/device.h
+++ include/linux/device.h
@@ -263,8 +263,8 @@ extern void class_device_destroy(struct class *cls, dev_t devt);
 
 
 struct device {
-	struct list_head node;		/* node in sibling list */
-	struct list_head children;
+	struct klist		klist_children;
+	struct klist_node	knode_parent;		/* node in sibling list */
 	struct klist_node	knode_driver;
 	struct klist_node	knode_bus;
 	struct device 	* parent;
@@ -298,12 +298,6 @@ struct device {
 
 	void	(*release)(struct device * dev);
 };
-
-static inline struct device *
-list_to_dev(struct list_head *node)
-{
-	return list_entry(node, struct device, node);
-}
 
 static inline void *
 dev_get_drvdata (struct device *dev)
