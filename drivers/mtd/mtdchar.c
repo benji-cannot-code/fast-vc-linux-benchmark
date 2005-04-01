15326e6ec6c8a6fa3179c69138d9c446c4f63969
Nicolas Pitre <nico@cam.org>
2005-04-01 16:36:15 +0100

    [MTD] Reset file position when switching OTP mode
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- drivers/mtd/mtdchar.c
+++ drivers/mtd/mtdchar.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: mtdchar.c,v 1.68 2005/02/08 19:12:50 nico Exp $
+ * $Id: mtdchar.c,v 1.70 2005/04/01 15:36:11 nico Exp $
  *
  * Character-device access to raw MTD devices.
  *
@@ -584,6 +584,7 @@ static int mtd_ioctl(struct inode *inode, struct file *file,
 		case MTD_OTP_OFF:
 			break;
 		}
+		file->f_pos = 0;
 		break;
 	}
 
