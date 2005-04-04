87020fef6d6e5db9a4e186e9438d87aa89af0012
Jesper Juhl <juhl-lkml@dif.dk>
2005-04-04 14:59:56 +0100

    NTFS: Remove checks for NULL before calling kfree() since kfree() does the       checking itself.  (Jesper Juhl)
    Signed-off-by: Jesper Juhl <juhl-lkml@dif.dk>
    Signed-off-by: Anton Altaparmakov <aia21@cantab.net>

--- fs/ntfs/dir.c
+++ fs/ntfs/dir.c
@@ -184,8 +184,7 @@ found_it:
 				name->len = 0;
 				*res = name;
 			} else {
-				if (name)
-					kfree(name);
+				kfree(name);
 				*res = NULL;
 			}
 			mref = le64_to_cpu(ie->data.dir.indexed_file);
@@ -445,8 +444,7 @@ found_it2:
 				name->len = 0;
 				*res = name;
 			} else {
-				if (name)
-					kfree(name);
+				kfree(name);
 				*res = NULL;
 			}
 			mref = le64_to_cpu(ie->data.dir.indexed_file);
@@ -1463,10 +1461,8 @@ err_out:
 		unlock_page(ia_page);
 		ntfs_unmap_page(ia_page);
 	}
-	if (ir)
-		kfree(ir);
-	if (name)
-		kfree(name);
+	kfree(ir);
+	kfree(name);
 	if (ctx)
 		ntfs_attr_put_search_ctx(ctx);
 	if (m)
