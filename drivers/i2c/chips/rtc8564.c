d27c850d727d991be42dbec4ae859aacdef8211f
Clemens Koller <clemens.koller@anagramm.de>
2005-04-11 11:49:14 +0200

    [PATCH] I2C: rtc8564.c remove duplicate include
    [PATCH] I2C rtc8564.c remove duplicate include
    
    Trivial fix: removes duplicate include line.
    
    Signed-off-by: Clemens Koller <clemens.koller@anagramm.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/i2c/chips/rtc8564.c
+++ drivers/i2c/chips/rtc8564.c
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/rtc.h>		/* get the user-level API */
 #include <linux/init.h>
-#include <linux/init.h>
 
 #include "rtc8564.h"
 
