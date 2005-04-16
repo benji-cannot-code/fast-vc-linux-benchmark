e02f9c80b09f77c2cf5edd22fdec83d4b071d2f7
Benoit Boissinot <benoit.boissinot@ens-lyon.org>
2005-04-16 15:24:29 -0700

    [PATCH] ppc32: fix compilation error in arch/ppc/syslib/open_pic_defs.h
    make defconfig give the following error on ppc (gcc-4):
    
    arch/ppc/syslib/open_pic.c:36: error: static declaration of ‘OpenPIC’ follows non-static declaration
    arch/ppc/syslib/open_pic_defs.h:175: error: previous declaration of ‘OpenPIC’ was here
    
    Signed-Off-By: Benoit Boissinot <benoit.boissinot@ens-lyon.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc/syslib/open_pic_defs.h
+++ arch/ppc/syslib/open_pic_defs.h
@@ -173,9 +173,6 @@ struct OpenPIC {
     OpenPIC_Processor Processor[OPENPIC_MAX_PROCESSORS];
 };
 
-extern volatile struct OpenPIC __iomem *OpenPIC;
-
-
     /*
      *  Current Task Priority Register
      */
