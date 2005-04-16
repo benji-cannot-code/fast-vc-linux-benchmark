596aa5e528233a0cedf19eb32e94e99280b5bd3c
Pavel Machek <pavel@ucw.cz>
2005-04-16 15:25:25 -0700

    [PATCH] u32 vs. pm_message_t fixes for drivers/net
    This fixes remaining u32s in drivers/ net.
    
    Signed-off-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/net/wireless/airo.c
+++ drivers/net/wireless/airo.c
@@ -62,7 +62,7 @@ MODULE_DEVICE_TABLE(pci, card_ids);
 
 static int airo_pci_probe(struct pci_dev *, const struct pci_device_id *);
 static void airo_pci_remove(struct pci_dev *);
-static int airo_pci_suspend(struct pci_dev *pdev, u32 state);
+static int airo_pci_suspend(struct pci_dev *pdev, pm_message_t state);
 static int airo_pci_resume(struct pci_dev *pdev);
 
 static struct pci_driver airo_driver = {
@@ -5465,7 +5465,7 @@ static void __devexit airo_pci_remove(struct pci_dev *pdev)
 {
 }
 
-static int airo_pci_suspend(struct pci_dev *pdev, u32 state)
+static int airo_pci_suspend(struct pci_dev *pdev, pm_message_t state)
 {
 	struct net_device *dev = pci_get_drvdata(pdev);
 	struct airo_info *ai = dev->priv;
