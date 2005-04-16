4e40ae2af47510fba41da5c00f03e2562a96fe4b
Benoit Boissinot <benoit.boissinot@ens-lyon.org>
2005-04-16 15:25:59 -0700

    [PATCH] cpuset: remove function attribute const
    gcc-4 warns with
    include/linux/cpuset.h:21: warning: type qualifiers ignored on function
    return type
    
    cpuset_cpus_allowed is declared with const
    extern const cpumask_t cpuset_cpus_allowed(const struct task_struct *p);
    
    First const should be __attribute__((const)), but the gcc manual
    explains that:
    
    "Note that a function that has pointer arguments and examines the data
    pointed to must not be declared const. Likewise, a function that calls a
    non-const function usually must not be const. It does not make sense for
    a const function to return void."
    
    The following patch remove const from the function declaration.
    
    Signed-off-by: Benoit Boissinot <benoit.boissinot@ens-lyon.org>
    Acked-by: Paul Jackson <pj@sgi.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1433,7 +1433,7 @@ void cpuset_exit(struct task_struct *tsk)
  * tasks cpuset.
  **/
 
-const cpumask_t cpuset_cpus_allowed(const struct task_struct *tsk)
+cpumask_t cpuset_cpus_allowed(const struct task_struct *tsk)
 {
 	cpumask_t mask;
 
