3ba14b58bdcf5ad110aff5014fa5f4ca07417371
James Bottomley <James.Bottomley@SteelEye.com>
2005-04-16 15:25:54 -0700

    [PATCH] add Big Endian variants of ioread/iowrite
    In the new io infrastructure, all of our operators are expecting the
    underlying device to be little endian (because the PCI bus, their main
    consumer, is LE).
    
    However, there are a fair few devices and busses in the world that are
    actually Big Endian.  There's even evidence that some of these BE bus and
    chip types are attached to LE systems.  Thus, there's a need for a BE
    equivalent of our io{read,write}{16,32} operations.
    
    The attached patch adds this as io{read,write}{16,32}be.  When it's in,
    I'll add the first consume (the 53c700 SCSI chip driver).
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-generic/iomap.h
+++ include/asm-generic/iomap.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __GENERIC_IO_H
 
 #include <linux/linkage.h>
+#include <asm/byteorder.h>
 
 /*
  * These are the "generic" interfaces for doing new-style
@@ -27,11 +28,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 extern unsigned int fastcall ioread8(void __iomem *);
 extern unsigned int fastcall ioread16(void __iomem *);
+extern unsigned int fastcall ioread16be(void __iomem *);
 extern unsigned int fastcall ioread32(void __iomem *);
+extern unsigned int fastcall ioread32be(void __iomem *);
 
 extern void fastcall iowrite8(u8, void __iomem *);
 extern void fastcall iowrite16(u16, void __iomem *);
+extern void fastcall iowrite16be(u16, void __iomem *);
 extern void fastcall iowrite32(u32, void __iomem *);
+extern void fastcall iowrite32be(u32, void __iomem *);
 
 /*
  * "string" versions of the above. Note that they
