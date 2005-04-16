d9b14831c56af46d3adafc51b0a07feca302f0a9
Yoichi Yuasa <yuasa@hh.iij4u.or.jp>
2005-04-16 15:24:41 -0700

    [PATCH] mips: remove #include <linux/audit.h> two times
    This patch removes #include <linux/audit.h>.  Because it includes it two
    times.
    
    Signed-off-by: Yoichi Yuasa <yuasa@hh.iij4u.or.jp>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/mips/kernel/ptrace.c
+++ arch/mips/kernel/ptrace.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp_lock.h>
 #include <linux/user.h>
 #include <linux/security.h>
-#include <linux/audit.h>
 
 #include <asm/cpu.h>
 #include <asm/fpu.h>
