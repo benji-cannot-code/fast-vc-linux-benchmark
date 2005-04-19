07101535c6342d6e1c29c75a3c362962a15ef5d4
Hugh Dickins <hugh@veritas.com>
2005-04-19 13:29:22 -0700

    [PATCH] freepgt: arm26 FIRST_USER_ADDRESS PAGE_SIZE
    ARM26 define FIRST_USER_ADDRESS as PAGE_SIZE (beyond the machine vectors when
    they are mapped low), and use that definition in place of locally defined
    MIN_MAP_ADDR.  Previously, ARM26 permitted user mappings at 0 if the machine
    vectors were mapped high; but that's inconsistent with ARM, and
    FIRST_USER_ADDRESS would then have to be determined at runtime.  Let's fix it
    at PAGE_SIZE throughout the architecture.
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-arm26/pgtable.h
+++ include/asm-arm26/pgtable.h
@@ -63,6 +63,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PTRS_PER_PMD            1
 #define PTRS_PER_PTE            32
 
+/*
+ * This is the lowest virtual address we can permit any user space
+ * mapping to be mapped at.  This is particularly important for
+ * non-high vector CPUs.
+ */
+#define FIRST_USER_ADDRESS	PAGE_SIZE
+
 #define FIRST_USER_PGD_NR       1
 #define USER_PTRS_PER_PGD       ((TASK_SIZE/PGD_SIZE) - FIRST_USER_PGD_NR)
 
