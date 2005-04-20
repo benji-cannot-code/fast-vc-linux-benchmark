a4e11a0458f7f84bf49342016e5eda8422f4a800
Herbert Xu <herbert@gondor.apana.org.au>
2005-04-19 20:46:37 -0700

    [NET]: Shave sizeof(ptr) bytes off dst_entry
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/dst.h
+++ include/net/dst.h
@@ -43,7 +43,8 @@ struct dst_entry
 	int			__use;
 	struct dst_entry	*child;
 	struct net_device       *dev;
-	int			obsolete;
+	short			error;
+	short			obsolete;
 	int			flags;
 #define DST_HOST		1
 #define DST_NOXFRM		2
@@ -61,8 +62,6 @@ struct dst_entry
 
 	unsigned long		rate_last;	/* rate limiting for ICMP */
 	unsigned long		rate_tokens;
-
-	int			error;
 
 	struct neighbour	*neighbour;
 	struct hh_cache		*hh;
