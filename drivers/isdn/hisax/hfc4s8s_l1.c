765cc49523f20d06bd44667b2f69753700e6a410
Al Viro <viro@www.linux.org.uk>
2005-04-24 12:28:35 -0700

    [PATCH] missing include in hisax
    Missing include, breaks at least on arm.
    
    Signed-off-by: Al Viro <viro@parcelfarce.linux.theplanet.co.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/isdn/hisax/hfc4s8s_l1.c
+++ drivers/isdn/hisax/hfc4s8s_l1.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/skbuff.h>
 #include <linux/wait.h>
+#include <asm/io.h>
 #include "hisax_if.h"
 #include "hfc4s8s_l1.h"
 
