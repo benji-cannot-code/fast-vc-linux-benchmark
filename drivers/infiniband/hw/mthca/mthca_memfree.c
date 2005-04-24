5db09ea66010b961c81d96879afd3c93923ac35c
Al Viro <viro@www.linux.org.uk>
2005-04-24 12:28:36 -0700

    [PATCH] missing include in mthca
    Missing include - usual portability problems...
    
    Signed-off-by: Al Viro <viro@parcelfarce.linux.theplanet.co.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/infiniband/hw/mthca/mthca_memfree.c
+++ drivers/infiniband/hw/mthca/mthca_memfree.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "mthca_memfree.h"
 #include "mthca_dev.h"
 #include "mthca_cmd.h"
+#include <linux/mm.h>
 
 /*
  * We allocate in as big chunks as we can, up to a maximum of 256 KB
