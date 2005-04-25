c12c231d1a1762a7149e08c0aa6f0964221f6ee0
Lucas Correia Villa Real <lucasvr@org.rmk.(none)>
2005-04-25 23:12:50 +0100

    [PATCH] ARM: 2644/1: Adds S3C2400 support to uncompress.h
    Patch from Lucas Correia Villa Real
    
    The S3C2400 doesn't have a cpuid information stored anywhere. This patch adds
    support to the S3C2400 at include/asm-arm/arch-s3c2400/uncompress.h by
    initializing the cpuid variable to the S3C2410, as they share the same
    routine. The GSTATUS1 pin is then used only if not compiling for the S3C2400.
    
    Signed-off-by: Lucas Correia Villa Real
    Signed-off-by: Ben Dooks
    Signed-off-by: Russell King

--- include/asm-arm/arch-s3c2410/uncompress.h
+++ include/asm-arm/arch-s3c2410/uncompress.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  12-Oct-2004 BJD  Take account of debug uart configuration
  *  15-Nov-2004 BJD  Fixed uart configuration
  *  22-Feb-2005 BJD  Added watchdog to uncompress
+ *  04-Apr-2005 LCVR Added support to S3C2400 (no cpuid at GSTATUS1)
 */
 
 #ifndef __ASM_ARCH_UNCOMPRESS_H
@@ -70,9 +71,12 @@ uart_rd(unsigned int reg)
 static void
 putc(char ch)
 {
-	int cpuid = *((volatile unsigned int *)S3C2410_GSTATUS1);
+	int cpuid = S3C2410_GSTATUS1_2410;
 
+#ifndef CONFIG_CPU_S3C2400
+	cpuid = *((volatile unsigned int *)S3C2410_GSTATUS1);
 	cpuid &= S3C2410_GSTATUS1_IDMASK;
+#endif
 
 	if (ch == '\n')
 		putc('\r');    /* expand newline to \r\n */
