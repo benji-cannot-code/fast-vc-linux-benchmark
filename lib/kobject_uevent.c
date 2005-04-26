fe6734957d64c7ed3e18e75e68ba8d7b6fcefd1d
Dmitry Torokhov <dtor_core@ameritech.net>
2005-04-26 02:32:00 -0500

    [PATCH] Make kobject's name be const char *
    kobject: make kobject's name const char * since users should not
    	 attempt to change it (except by calling kobject_rename).
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -198,7 +198,7 @@ void kobject_hotplug(struct kobject *kobj, enum kobject_action action)
 	int i = 0;
 	int retval;
 	char *kobj_path = NULL;
-	char *name = NULL;
+	const char *name = NULL;
 	char *action_string;
 	u64 seq;
 	struct kobject *top_kobj = kobj;
@@ -250,7 +250,7 @@ void kobject_hotplug(struct kobject *kobj, enum kobject_action action)
 		name = kobject_name(&kset->kobj);
 
 	argv [0] = hotplug_path;
-	argv [1] = name;
+	argv [1] = (char *)name; /* won't be changed but 'const' has to go */
 	argv [2] = NULL;
 
 	/* minimal command environment */
