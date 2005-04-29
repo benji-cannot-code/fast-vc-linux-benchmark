bce556321cede6edeac1a107a99fc91f61f1cf00
Dmitry Torokhov <dtor_core@ameritech.net>
2005-04-29 01:26:06 -0500

    [PATCH] sysfs: (driver/block) if show/store is missing return -EIO
    sysfs: fix drivers/block so if an attribute doesn't implement
           show or store method read/write will return -EIO
           instead of 0 or -EINVAL.
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/block/ll_rw_blk.c
+++ drivers/block/ll_rw_blk.c
@@ -3575,7 +3575,7 @@ queue_attr_show(struct kobject *kobj, struct attribute *attr, char *page)
 
 	q = container_of(kobj, struct request_queue, kobj);
 	if (!entry->show)
-		return 0;
+		return -EIO;
 
 	return entry->show(q, page);
 }
@@ -3589,7 +3589,7 @@ queue_attr_store(struct kobject *kobj, struct attribute *attr,
 
 	q = container_of(kobj, struct request_queue, kobj);
 	if (!entry->store)
-		return -EINVAL;
+		return -EIO;
 
 	return entry->store(q, page, length);
 }
