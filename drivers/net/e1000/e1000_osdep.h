71431ac4719f8a9e1daad5429e378c219aa5599f
Malli Chilakala <mallikarjuna.chilakala@intel.com>
2005-04-28 19:38:43 -0700

    [PATCH] e1000: Fix msec-delay definition to use msleep
    Fix msec-delay definition in e1000_osdep.h to use msleep
    
    Signed-off-by: Mallikarjuna R Chilakala <mallikarjuna.chilakala@intel.com>
    Signed-off-by: Ganesh Venkatesan <ganesh.venkatesan@intel.com>
    Signed-off-by: John Ronciak <john.ronciak@intel.com>
    diff -up net-drivers-2.6/drivers/net/e1000/e1000_osdep.h net-drivers-2.6/drivers/net/e1000.new/e1000_osdep.h

--- drivers/net/e1000/e1000_osdep.h
+++ drivers/net/e1000/e1000_osdep.h
@@ -43,7 +43,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 
 #ifndef msec_delay
-#define msec_delay(x) msleep(x)
+#define msec_delay(x)	do { if(in_interrupt()) { \
+				/* Don't mdelay in interrupt context! */ \
+	                	BUG(); \
+			} else { \
+				msleep(x); \
+			} } while(0)
 
 /* Some workarounds require millisecond delays and are run during interrupt
  * context.  Most notably, when establishing link, the phy may need tweaking
