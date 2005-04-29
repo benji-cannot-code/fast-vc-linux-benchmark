6901303fc957a0b99de007e6013f3c2bbdfe2db6
Steve French <smfrench@austin.rr.com>
2005-04-28 22:41:05 -0700

    [PATCH] cifs: Add new mount parm mapchars
    For handling seven special characters that shells use for filenames.
    
    This first parts implements conversions from Unicode. 
    
    Signed-off-by: Steve French
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/cifs/cifs_fs_sb.h
+++ fs/cifs/cifs_fs_sb.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CIFS_MOUNT_SERVER_INUM  4 /* inode numbers from uniqueid from server */
 #define CIFS_MOUNT_DIRECT_IO    8 /* do not write nor read through page cache */
 #define CIFS_MOUNT_NO_XATTR  0x10 /* if set - disable xattr support */
+#define CIFS_MOUNT_MAP_SPECIAL_CHR 0x20 /* remap illegal chars in filenames */
 
 struct cifs_sb_info {
 	struct cifsTconInfo *tcon;	/* primary mount */
