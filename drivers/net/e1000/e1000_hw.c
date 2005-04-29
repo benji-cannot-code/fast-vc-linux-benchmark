54cc2722c6ac0492975f120f95a54aba7bf62539
Malli Chilakala <mallikarjuna.chilakala@intel.com>
2005-04-28 19:44:46 -0700

    [PATCH] e1000:Driver version,white space,comments,device id
    Driver version, white space, comments, device id & other
    
    Signed-off-by: Mallikarjuna R Chilakala <mallikarjuna.chilakala@intel.com>
    Signed-off-by: Ganesh Venkatesan <ganesh.venkatesan@intel.com>
    Signed-off-by: John Ronciak <john.ronciak@intel.com>
    diff -up net-drivers-2.6/drivers/net/e1000/e1000_ethtool.c net-drivers-2.6/drivers/net/e1000.new/e1000_ethtool.c

--- drivers/net/e1000/e1000_hw.c
+++ drivers/net/e1000/e1000_hw.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*******************************************************************************
 
   
-  Copyright(c) 1999 - 2004 Intel Corporation. All rights reserved.
+  Copyright(c) 1999 - 2005 Intel Corporation. All rights reserved.
   
   This program is free software; you can redistribute it and/or modify it 
   under the terms of the GNU General Public License as published by the Free 
@@ -144,7 +144,6 @@ e1000_phy_init_script(struct e1000_hw *hw)
     uint16_t phy_saved_data;
 
     DEBUGFUNC("e1000_phy_init_script");
-
 
     if(hw->phy_init_script) {
         msec_delay(20);
