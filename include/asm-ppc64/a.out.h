67e42d4b1afa9cd5543704a972045e416f79f6ed
Anton Blanchard <anton@samba.org>
2005-05-01 08:58:46 -0700

    [PATCH] ppc64: remove unnecessary include
    We no longer use any ppcdebug stuff in a.out.h, so remove the define.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ppc64/a.out.h
+++ include/asm-ppc64/a.out.h
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __PPC64_A_OUT_H__
 #define __PPC64_A_OUT_H__
 
-#include <asm/ppcdebug.h>
-
 /*
  * c 2001 PPC 64 Team, IBM Corp
  *
