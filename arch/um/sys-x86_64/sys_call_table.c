0d14a523fa180714928277ac371b3197fa7223aa
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2005-05-01 08:58:55 -0700

    [PATCH] uml: fix syscall table by including $(SUBARCH)'s one, for i386
    Split the i386 entry.S files into entry.S and syscall_table.S which is
    included in the previous one (so actually there is no difference between them)
    and use the syscall_table.S in the UML build, instead of tracking by hand the
    syscall table changes (which is inherently error-prone).
    
    We must only insert the right #defines to inject the changes we need from the
    i386 syscall table (for instance some different function names); also, we
    don't implement some i386 syscalls, as ioperm(), nor some TLS-related ones
    (yet to provide).
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/sys-x86_64/sys_call_table.c
+++ arch/um/sys-x86_64/sys_call_table.c
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* 
+/*
  * Copyright (C) 2000 Jeff Dike (jdike@karaya.com)
  * Copyright 2003 PathScale, Inc.
  * Licensed under the GPL
@@ -14,6 +14,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "asm/signal.h"
 #include "sysdep/syscalls.h"
 #include "kern_util.h"
+
+#ifdef CONFIG_NFSD
+#define NFSSERVCTL sys_nfsservctl
+#else
+#define NFSSERVCTL sys_ni_syscall
+#endif
 
 #define LAST_GENERIC_SYSCALL __NR_keyctl
 
@@ -184,7 +190,7 @@ syscall_handler_t *sys_call_table[] = {
 	[ __NR_getresuid ] = (syscall_handler_t *) sys_getresuid16,
 	[ __NR_query_module ] = (syscall_handler_t *) sys_ni_syscall,
 	[ __NR_poll ] = (syscall_handler_t *) sys_poll,
-	[ __NR_nfsservctl ] = (syscall_handler_t *) sys_nfsservctl,
+	[ __NR_nfsservctl ] = (syscall_handler_t *) NFSSERVCTL,
 	[ __NR_setresgid ] = (syscall_handler_t *) sys_setresgid16,
 	[ __NR_getresgid ] = (syscall_handler_t *) sys_getresgid16,
 	[ __NR_prctl ] = (syscall_handler_t *) sys_prctl,
@@ -266,6 +272,6 @@ syscall_handler_t *sys_call_table[] = {
 	[ __NR_keyctl ] = (syscall_handler_t *) sys_keyctl,
 
 	ARCH_SYSCALLS
-	[ LAST_SYSCALL + 1 ... NR_syscalls ] = 
+	[ LAST_SYSCALL + 1 ... NR_syscalls ] =
 		(syscall_handler_t *) sys_ni_syscall
 };
