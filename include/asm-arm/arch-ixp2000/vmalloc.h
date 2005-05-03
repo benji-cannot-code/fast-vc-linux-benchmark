c00b2a340475d3ba3b462d971cf99d7216759569
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-05-03 12:20:29 +0100

    [PATCH] ARM: cleanup vmalloc start/offset macros
    VMALLOC_START and VMALLOC_OFFSET are common between all ARM
    machine classes.  Move them into include/asm-arm/pgtable.h,
    but allow a machine class to override them if required.
    
    Signed-off-by: Russell King <rmk@arm.linux.org.uk>

--- include/asm-arm/arch-ixp2000/vmalloc.h
+++ include/asm-arm/arch-ixp2000/vmalloc.h
@@ -18,7 +18,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The vmalloc() routines leaves a hole of 4kB between each vmalloced
  * area for the same reason. ;)
  */
-#define VMALLOC_OFFSET	    (8*1024*1024)
-#define VMALLOC_START	    (((unsigned long)high_memory + VMALLOC_OFFSET) & ~(VMALLOC_OFFSET-1))
-#define VMALLOC_VMADDR(x)   ((unsigned long)(x))
 #define VMALLOC_END	    0xfaffefff
