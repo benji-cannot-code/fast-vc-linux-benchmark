b83adc5532e9f08a8434d032caac9c7c224243c0
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2005-05-05 16:15:15 -0700

    [PATCH] uml: obvious compile fixes for x86-64 Subarch and x86 regression fixes
    This patch does some totally trivial compilation fixes.  It also restores the
    debugregs manipulation, which was commented out simply because it doesn't
    compile on x86_64 (we haven't yet implemented there debugregs handling).
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/sys-x86_64/syscalls.c
+++ arch/um/sys-x86_64/syscalls.c
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "linux/linkage.h"
 #include "linux/slab.h"
 #include "linux/shm.h"
+#include "linux/utsname.h"
+#include "linux/personality.h"
 #include "asm/uaccess.h"
 #define __FRAME_OFFSETS
 #include "asm/ptrace.h"
