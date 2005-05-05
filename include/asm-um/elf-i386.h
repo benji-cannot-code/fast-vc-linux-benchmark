e0543c7875ac78ae8215b138a75adabaec3330c1
Bodo Stroesser <bstroesser@fujitsu-siemens.com>
2005-05-05 16:15:35 -0700

    [PATCH] uml: s390 preparation, elf.h
    This patch make elh.h a symlink to the new arch-specific include files of the
    form elf-<subarch>.h, as in the same way already is done for some other
    includes.  Also moves Elf-stuff from archparam-<subarch>.h and elf.h to the
    new elf-<subarch>.h files.
    
    Signed-off-by: Bodo Stroesser <bstroesser@fujitsu-siemens.com>
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-um/elf-i386.h
+++ include/asm-um/elf-i386.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "user.h"
 
-#if defined(CONFIG_UML_X86) && !defined(CONFIG_64BIT)
-
 #define R_386_NONE	0
 #define R_386_32	1
 #define R_386_PC32	2
@@ -22,31 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define R_386_GOTOFF	9
 #define R_386_GOTPC	10
 #define R_386_NUM	11
-
-#elif defined(CONFIG_UML_X86) && defined(CONFIG_64BIT)
-
-/* x86-64 relocation types */
-#define R_X86_64_NONE		0	/* No reloc */
-#define R_X86_64_64		1	/* Direct 64 bit  */
-#define R_X86_64_PC32		2	/* PC relative 32 bit signed */
-#define R_X86_64_GOT32		3	/* 32 bit GOT entry */
-#define R_X86_64_PLT32		4	/* 32 bit PLT address */
-#define R_X86_64_COPY		5	/* Copy symbol at runtime */
-#define R_X86_64_GLOB_DAT	6	/* Create GOT entry */
-#define R_X86_64_JUMP_SLOT	7	/* Create PLT entry */
-#define R_X86_64_RELATIVE	8	/* Adjust by program base */
-#define R_X86_64_GOTPCREL	9	/* 32 bit signed pc relative
-					   offset to GOT */
-#define R_X86_64_32		10	/* Direct 32 bit zero extended */
-#define R_X86_64_32S		11	/* Direct 32 bit sign extended */
-#define R_X86_64_16		12	/* Direct 16 bit zero extended */
-#define R_X86_64_PC16		13	/* 16 bit sign extended pc relative */
-#define R_X86_64_8		14	/* Direct 8 bit sign extended  */
-#define R_X86_64_PC8		15	/* 8 bit sign extended pc relative */
-
-#define R_X86_64_NUM		16
-
-#endif
 
 typedef unsigned long elf_greg_t;
 
