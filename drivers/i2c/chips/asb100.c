47ebcb5cb675654fb2efe9720fc4f6bbbbc3e2e5
Dominik Hackl <dominik@hackl.dhs.org>
2005-05-16 18:12:18 +0200

    [PATCH] I2C: include of jiffies.h for some i2c drivers
    This patch includes jiffies.h in two i2c drivers.
    (jiffies.h is needed for the time_after function.)
    
    Signed-off-by: Dominik Hackl <dominik@hackl.dhs.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/i2c/chips/asb100.c
+++ drivers/i2c/chips/asb100.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c-sensor.h>
 #include <linux/i2c-vid.h>
 #include <linux/init.h>
+#include <linux/jiffies.h>
 #include "lm75.h"
 
 /*
