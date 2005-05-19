d77230736c9cac27b9ac8df359ccc97ff98ab3a0
Thomas Gleixner <tglx@linutronix.de>
2005-05-19 17:10:26 +0100

    [MTD] NAND: Honour autoplacement schemes supplied by the caller
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/mtd/mtd-abi.h
+++ include/mtd/mtd-abi.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: mtd-abi.h,v 1.10 2005/02/09 09:17:42 pavlov Exp $
+ * $Id: mtd-abi.h,v 1.11 2005/05/19 16:08:58 gleixner Exp $
  *
  * Portions of MTD ABI definition which are shared by kernel and user space 
  */
@@ -62,6 +62,7 @@ struct mtd_oob_buf {
 #define MTD_NANDECC_PLACE	1	// Use the given placement in the structure (YAFFS1 legacy mode)
 #define MTD_NANDECC_AUTOPLACE	2	// Use the default placement scheme
 #define MTD_NANDECC_PLACEONLY	3	// Use the given placement in the structure (Do not store ecc result on read)
+#define MTD_NANDECC_AUTOPL_USR 	4	// Use the given autoplacement scheme rather than using the default
 
 /* OTP mode selection */
 #define MTD_OTP_OFF		0
