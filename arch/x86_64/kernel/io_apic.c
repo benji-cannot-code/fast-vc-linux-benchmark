300e0b733821b4568028f77468e2fd07e936567d
Andi Kleen <ak@suse.de>
2005-05-20 14:27:59 -0700

    [PATCH] x86_64: Add option to disable timer check
    This works around the too fast timer seen on some ATI boards.
    
    I don't feel confident enough about it yet to enable it by default, but give
    users the option.
    
    Patch and debugging from Christopher Allen Wing <wingc@engin.umich.edu>, with
    minor tweaks (renamed the option and documented it)
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/io_apic.c
+++ arch/x86_64/kernel/io_apic.c
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int sis_apic_bug; /* not actually supported, dummy for compile */
 
+static int no_timer_check;
+
 static DEFINE_SPINLOCK(ioapic_lock);
 
 /*
@@ -1602,7 +1604,7 @@ static inline void check_timer(void)
 		 * Ok, does IRQ0 through the IOAPIC work?
 		 */
 		unmask_IO_APIC_irq(0);
-		if (timer_irq_works()) {
+		if (!no_timer_check && timer_irq_works()) {
 			nmi_watchdog_default();
 			if (nmi_watchdog == NMI_IO_APIC) {
 				disable_8259A_irq(0);
@@ -1671,6 +1673,13 @@ static inline void check_timer(void)
 	apic_printk(APIC_VERBOSE," failed :(.\n");
 	panic("IO-APIC + timer doesn't work! Try using the 'noapic' kernel parameter\n");
 }
+
+static int __init notimercheck(char *s)
+{
+	no_timer_check = 1;
+	return 1;
+}
+__setup("no_timer_check", notimercheck);
 
 /*
  *
