26220b612efae12676392577ac51c02691a92569
Takashi Iwai <tiwai@suse.de>
2005-05-24 13:16:56 +0200

    [ALSA] Add Shuttle SN25P support
    ICE1712 driver
    Added the support for Shuttle SN25P.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ice1712/vt1720_mobo.h
+++ sound/pci/ice1712/vt1720_mobo.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VT1720_SUBDEVICE_ZNF3_150	0x0f2741f6
 #define VT1720_SUBDEVICE_ZNF3_250	0x0f2745f6
 #define VT1720_SUBDEVICE_9CJS		0x0f272327
+#define VT1720_SUBDEVICE_SN25P		0x97123650
 
 extern struct snd_ice1712_card_info  snd_vt1720_mobo_cards[];
 
