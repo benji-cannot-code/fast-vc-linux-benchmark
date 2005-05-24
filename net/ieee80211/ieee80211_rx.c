7d07abee750dc41d8e67ea9aa22c7bf09b9a01cb
Jiri Benc <jbenc@suse.cz>
2005-05-24 15:10:18 +0200

    [PATCH] ieee80211: cleanup
    Cleanup of unused and duplicated constants and structures in the ieee80211
    header.
    
    Signed-off-by: Jiri Benc <jbenc@suse.cz>
    Signed-off-by: Jirka Bohac <jbohac@suse.cz>

--- net/ieee80211/ieee80211_rx.c
+++ net/ieee80211/ieee80211_rx.c
@@ -476,7 +476,7 @@ int ieee80211_rx(struct ieee80211_device *ieee, struct sk_buff *skb,
 #endif
 
 	/* Data frame - extract src/dst addresses */
-	if (skb->len < IEEE80211_DATA_HDR3_LEN)
+	if (skb->len < IEEE80211_3ADDR_LEN)
 		goto rx_dropped;
 
 	switch (fc & (IEEE80211_FCTL_FROMDS | IEEE80211_FCTL_TODS)) {
@@ -489,7 +489,7 @@ int ieee80211_rx(struct ieee80211_device *ieee, struct sk_buff *skb,
 		memcpy(src, hdr->addr2, ETH_ALEN);
 		break;
 	case IEEE80211_FCTL_FROMDS | IEEE80211_FCTL_TODS:
-		if (skb->len < IEEE80211_DATA_HDR4_LEN)
+		if (skb->len < IEEE80211_4ADDR_LEN)
 			goto rx_dropped;
 		memcpy(dst, hdr->addr3, ETH_ALEN);
 		memcpy(src, hdr->addr4, ETH_ALEN);
