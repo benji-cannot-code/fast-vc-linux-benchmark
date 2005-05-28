6954e415d9c0129839c9e937dd545b02f82c1ef9
Jeff Garzik <jgarzik@pobox.com>
2005-05-28 07:47:39 -0400

    [SCSI] Remove unnecessary locking around completion function calls
    The SCSI ->done() hook should not be called from inside a spinlock.
    
    Drivers that do this are mostly cut-n-paste from 2.2.x-era.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/ieee1394/sbp2.c
+++ drivers/ieee1394/sbp2.c
@@ -2454,8 +2454,6 @@ static void sbp2scsi_complete_command(struct scsi_id_instance_data *scsi_id,
 				      u32 scsi_status, struct scsi_cmnd *SCpnt,
 				      void (*done)(struct scsi_cmnd *))
 {
-	unsigned long flags;
-
 	SBP2_DEBUG("sbp2scsi_complete_command");
 
 	/*
@@ -2554,11 +2552,7 @@ static void sbp2scsi_complete_command(struct scsi_id_instance_data *scsi_id,
 	/*
 	 * Tell scsi stack that we're done with this command
 	 */
-	spin_lock_irqsave(scsi_id->scsi_host->host_lock,flags);
 	done (SCpnt);
-	spin_unlock_irqrestore(scsi_id->scsi_host->host_lock,flags);
-
-	return;
 }
 
 
