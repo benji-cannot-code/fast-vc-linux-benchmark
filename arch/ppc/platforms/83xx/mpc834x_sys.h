222709326b96e9a7ab2b0379841eefe17eba4a83
Kumar Gala <galak@freescale.com>
2005-05-28 15:52:15 -0700

    [PATCH] ppc32: Add soft reset to MPC834x
    This change allows mpc83xx_restart to issue a software reset.
    
    Signed-off-by: Randy Vinson <rvinson@mvista.com>
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc/platforms/83xx/mpc834x_sys.h
+++ arch/ppc/platforms/83xx/mpc834x_sys.h
@@ -29,6 +29,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCSR_PHYS_ADDR		((uint)0xf8000000)
 #define BCSR_SIZE		((uint)(32 * 1024))
 
+#define BCSR_MISC_REG2_OFF	0x07
+#define BCSR_MISC_REG2_PORESET	0x01
+
+#define BCSR_MISC_REG3_OFF	0x08
+#define BCSR_MISC_REG3_CNFLOCK	0x80
+
 #ifdef CONFIG_PCI
 /* PCI interrupt controller */
 #define PIRQA        MPC83xx_IRQ_IRQ4
