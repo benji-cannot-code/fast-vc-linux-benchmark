cd4732fe560f9ffe1d7e0a04cb05f556e4c88841
Thomas Graf <tgraf@suug.ch>
2005-05-31 15:17:28 -0700

    [PKT_SCHED]: Disable dsmark debugging messages by default
    Signed-off-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_dsmark.c
+++ net/sched/sch_dsmark.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/byteorder.h>
 
 
-#if 1 /* control */
+#if 0 /* control */
 #define DPRINTK(format,args...) printk(KERN_DEBUG format,##args)
 #else
 #define DPRINTK(format,args...)
