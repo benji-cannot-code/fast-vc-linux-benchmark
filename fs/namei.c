cdd0333eef35d6d04359d2b12f57445becfcbf95
Al Viro <viro@www.linux.org.uk>
2005-06-06 13:36:01 -0700

    [PATCH] namei fixes (5/19)
    fix for too early mntput() in open_namei() - we pin path.mnt down for the
    duration of __do_follow_link().  Otherwise we could get the fs where our
    symlink lived unmounted while we were in __do_follow_link().  That would end
    up with dentry of symlink staying pinned down through the fs shutdown.
    
    Signed-off-by: Al Viro <viro@parcelfarce.linux.theplanet.co.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/namei.c
+++ fs/namei.c
@@ -1525,8 +1525,10 @@ do_link:
 	error = security_inode_follow_link(path.dentry, nd);
 	if (error)
 		goto exit_dput;
+	mntget(path.mnt);
 	error = __do_follow_link(path.dentry, nd);
 	dput(path.dentry);
+	mntput(path.mnt);
 	path.mnt = nd->mnt;
 	if (error)
 		return error;
