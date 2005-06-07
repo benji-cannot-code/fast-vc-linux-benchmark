d1f9ce32b63fbeaeb94f0cc65b89e0d24958040a
Yann Droneaud <ydroneaud@mandriva.com>
2005-06-07 16:54:01 +0200

    [ACPI] check acpi_disabled in IPMI
    Signed-off-by: Yann Droneaud <ydroneaud@mandriva.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/char/ipmi/ipmi_si_intf.c
+++ drivers/char/ipmi/ipmi_si_intf.c
@@ -1485,6 +1485,9 @@ static int try_init_acpi(int intf_num, struct smi_info **new_info)
 	char             *io_type;
 	u8 		 addr_space;
 
+	if (acpi_disabled)
+		return -ENODEV;
+
 	if (acpi_failure)
 		return -ENODEV;
 
