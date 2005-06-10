4b1133a6e6ff315dd29e0d9e0ee5b57a4af90c1e
Evgeniy Polyakov <johnpol@2ka.mipt.ru>
2005-06-10 14:53:22 +0400

    [PATCH] w1: fix compiler warnings
    Signed-off-by: Evgeniy Polyakov <johnpol@2ka.mipt.ru>

--- drivers/w1/w1_family.c
+++ drivers/w1/w1_family.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 DEFINE_SPINLOCK(w1_flock);
 static LIST_HEAD(w1_families);
+extern void w1_reconnect_slaves(struct w1_family *f);
 
 static int w1_check_family(struct w1_family *f)
 {
