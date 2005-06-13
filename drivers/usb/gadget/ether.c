0e9343828f2545acf56121e0d5ba09d45fc24d0f
David Brownell <david-b@pacbell.net>
2005-06-13 06:55:03 -0700

    [PATCH] USB: resolve ethernet gadget build glitch on pxa
    This fixes a build error on pxa25x processes with pxa2xx_udc and
    
            CONFIG_USB_ETH=m
            # CONFIG_USB_ETH_RNDIS is not set
    
    The error is because on that CPU there's no status transfer support
    except with RNDIS.  Workaround, enable the RNDIS support too.
    
    Signed-off-by: Ian Campbell <icampbell@arcom.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/ether.c
+++ drivers/usb/gadget/ether.c
@@ -2429,6 +2429,7 @@ autoconf_fail:
 	dev->req->complete = eth_setup_complete;
 
 	/* ... and maybe likewise for status transfer */
+#ifdef DEV_CONFIG_CDC
 	if (dev->status_ep) {
 		dev->stat_req = eth_req_alloc (dev->status_ep,
 					STATUS_BYTECOUNT, GFP_KERNEL);
@@ -2438,6 +2439,7 @@ autoconf_fail:
 		}
 		dev->stat_req->context = NULL;
 	}
+#endif
 
 	/* finish hookup to lower layer ... */
 	dev->gadget = gadget;
