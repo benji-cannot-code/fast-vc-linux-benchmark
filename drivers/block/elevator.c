a6c8aa9c171af2521d1dd7c6fc8e67c10b5b0b14
Tejun Heo <htejun@gmail.com>
2005-06-16 12:57:31 +0200

    This patch fixes q->unplug_thresh condition check in __elv_add_request().  rq.count[READ] + rq.count[WRITE] can increase more than one if another thread has allocated a request after the current request is allocated or in_flight could have changed resulting in larger-than-one change of nrq, thus breaking the threshold mechanism.
    Signed-off-by: Jens Axboe <axboe@suse.de>
    Signed-off-by: Tejun Heo <htejun@gmail.com>

--- drivers/block/elevator.c
+++ drivers/block/elevator.c
@@ -318,7 +318,7 @@ void __elv_add_request(request_queue_t *q, struct request *rq, int where,
 			int nrq = q->rq.count[READ] + q->rq.count[WRITE]
 				  - q->in_flight;
 
-			if (nrq == q->unplug_thresh)
+			if (nrq >= q->unplug_thresh)
 				__generic_unplug_device(q);
 		}
 	} else
