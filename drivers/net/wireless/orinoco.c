a254744e3f1610cfd9c5ba476d621d4f92643d8b
Christoph Hellwig <hch@lst.de>
2005-06-19 01:27:28 +0200

    [PATCH] orinoco: include <net/ieee80211.h>
    We need constants from this header in the next patches.

--- drivers/net/wireless/orinoco.c
+++ drivers/net/wireless/orinoco.c
@@ -464,6 +464,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_arp.h>
 #include <linux/etherdevice.h>
 #include <linux/wireless.h>
+#include <net/ieee80211.h>
 
 #include <asm/uaccess.h>
 #include <asm/io.h>
