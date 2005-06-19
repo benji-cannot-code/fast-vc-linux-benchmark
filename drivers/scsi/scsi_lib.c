7477461f4d6eef9fcad2292fa29a95424e3f5ff0
Christoph Hellwig <hch@lst.de>
2005-06-19 13:40:08 +0200

    [SCSI] remove scsi_cmnd->abort_reason
    Never used for anything but printing it out in debug routines.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -302,7 +302,6 @@ static int scsi_init_cmd_errh(struct scsi_cmnd *cmd)
 {
 	cmd->owner = SCSI_OWNER_MIDLEVEL;
 	cmd->serial_number = 0;
-	cmd->abort_reason = 0;
 
 	memset(cmd->sense_buffer, 0, sizeof cmd->sense_buffer);
 
@@ -323,7 +322,6 @@ static int scsi_init_cmd_errh(struct scsi_cmnd *cmd)
 	memcpy(cmd->data_cmnd, cmd->cmnd, sizeof(cmd->cmnd));
 	cmd->buffer = cmd->request_buffer;
 	cmd->bufflen = cmd->request_bufflen;
-	cmd->abort_reason = 0;
 
 	return 1;
 }
