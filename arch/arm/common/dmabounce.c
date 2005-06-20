faa944b203ba1de452359ec8de01619ebc5e4d5f
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-06-20 16:56:08 +0100

    [PATCH] ARM: Add missed include for dmabounce.c
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/common/dmabounce.c
+++ arch/arm/common/dmabounce.c
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmapool.h>
 #include <linux/list.h>
 
+#include <asm/cacheflush.h>
+
 #undef DEBUG
 
 #undef STATS
