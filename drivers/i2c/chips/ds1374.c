fff30deed60f7c481e883f0d8b5e18f155b7e744
Andrew Morton <akpm@osdl.org>
2005-06-20 14:25:45 -0700

    [PATCH] I2C: fix ds1374 build
    Not all architectures implement asm/rtc.h
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/i2c/chips/ds1374.c
+++ drivers/i2c/chips/ds1374.c
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rtc.h>
 #include <linux/bcd.h>
 
-#include <asm/rtc.h>
-
 #define DS1374_REG_TOD0		0x00
 #define DS1374_REG_TOD1		0x01
 #define DS1374_REG_TOD2		0x02
