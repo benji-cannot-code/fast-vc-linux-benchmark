d84595945347f6ce0d486a93d8d34f253f99d24b
Greg Kroah-Hartman <gregkh@suse.de>
2005-06-20 21:15:16 -0700

    [PATCH] devfs: Last little devfs cleanups throughout the kernel tree.
    Just removes a few unused #defines and fixes some comments due to
    devfs now being gone.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/cdrom/viocd.c
+++ drivers/cdrom/viocd.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/iseries/vio.h>
 
 #define VIOCD_DEVICE			"iseries/vcd"
-#define VIOCD_DEVICE_DEVFS		"iseries/vcd"
 
 #define VIOCD_VERS "1.06"
 
