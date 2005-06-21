5bfcf7e8c5266fdd135fb890278a78f61401b945
Greg Kroah-Hartman <gregkh@suse.de>
2005-06-20 21:15:16 -0700

    [PATCH] USB Serial: move name to driver structure
    This fixes up a lot of problems in sysfs with some of the usb serial
    drivers, they had incorrect driver names.  Also saves a tiny ammount
    of memory.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/hp4x.c
+++ drivers/usb/serial/hp4x.c
@@ -39,7 +39,7 @@ MODULE_DEVICE_TABLE(usb, id_table);
 
 static struct usb_driver hp49gp_driver = {
 	.owner =	THIS_MODULE,
-	.name =		"HP4X",
+	.name =		"hp4X",
 	.probe =	usb_serial_probe,
 	.disconnect =	usb_serial_disconnect,
 	.id_table =	id_table,
@@ -48,8 +48,8 @@ static struct usb_driver hp49gp_driver = {
 static struct usb_serial_driver hp49gp_device = {
 	.driver = {
 		.owner =	THIS_MODULE,
+		.name =		"hp4X",
 	},
-	.name =			"HP4X",
 	.id_table =		id_table,
 	.num_interrupt_in =	NUM_DONT_CARE,
 	.num_bulk_in =		NUM_DONT_CARE,
