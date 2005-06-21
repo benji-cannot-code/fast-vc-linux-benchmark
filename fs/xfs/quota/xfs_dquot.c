444e6d5e00a0ea8961551e78043a56b39b17f4b1
Nathan Scott <nathans@sgi.com>
2005-06-21 15:57:57 +1000

    [XFS] Remove some debugging code from quota syscalls.
    SGI-PV: 932952
    SGI-Modid: xfs-linux-melb:xfs-kern:22929a
    
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/quota/xfs_dquot.c
+++ fs/xfs/quota/xfs_dquot.c
@@ -912,7 +912,6 @@ xfs_qm_dqget(
 	if ((! XFS_IS_UQUOTA_ON(mp) && type == XFS_DQ_USER) ||
 	    (! XFS_IS_PQUOTA_ON(mp) && type == XFS_DQ_PROJ) ||
 	    (! XFS_IS_GQUOTA_ON(mp) && type == XFS_DQ_GROUP)) {
-printk("XQM: ESRCH1\n");
 		return (ESRCH);
 	}
 	h = XFS_DQ_HASH(mp, id, type);
@@ -990,7 +989,6 @@ printk("XQM: ESRCH1\n");
 				  &dqp))) {
 		if (ip)
 			xfs_ilock(ip, XFS_ILOCK_EXCL);
-if (error == ESRCH) printk("XQM: ESRCH2\n");
 		return (error);
 	}
 
@@ -1016,7 +1014,6 @@ if (error == ESRCH) printk("XQM: ESRCH2\n");
 		if (! XFS_IS_DQTYPE_ON(mp, type)) {
 			/* inode stays locked on return */
 			xfs_qm_dqdestroy(dqp);
-printk("XQM: ESRCH3\n");
 			return XFS_ERROR(ESRCH);
 		}
 		/*
