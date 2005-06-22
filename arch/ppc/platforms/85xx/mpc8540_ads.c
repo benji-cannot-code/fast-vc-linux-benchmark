ee79caf5d034c9df9030a8d611a6ef0353406583
Kumar Gala <galak@freescale.com>
2005-06-21 17:15:28 -0700

    [PATCH] ppc32: remove some unnecessary includes of prom.h
    Fight the Good Fight: Limit prom.h header creep.
    
    Signed-off-by: Jon Loeliger <jdl@freescale.com>
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc/platforms/85xx/mpc8540_ads.c
+++ arch/ppc/platforms/85xx/mpc8540_ads.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/time.h>
 #include <asm/io.h>
 #include <asm/machdep.h>
-#include <asm/prom.h>
 #include <asm/open_pic.h>
 #include <asm/bootinfo.h>
 #include <asm/pci-bridge.h>
