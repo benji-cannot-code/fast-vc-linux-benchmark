66eee77891cb823e5b0a1e3f84fc8bd64eac19ff
Heiko Carstens <heiko.carstens@de.ibm.com>
2005-06-21 17:16:29 -0700

    [PATCH] s390: cmm sender parameter visibility
    Make cmm module parameter "sender" visible in sysfs.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/s390/mm/cmm.c
+++ arch/s390/mm/cmm.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 
 static char *sender = "VMRMSVM";
-module_param(sender, charp, 0);
+module_param(sender, charp, 0400);
 MODULE_PARM_DESC(sender,
 		 "Guest name that may send SMSG messages (default VMRMSVM)");
 
