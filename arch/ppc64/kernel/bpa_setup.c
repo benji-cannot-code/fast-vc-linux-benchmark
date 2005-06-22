7f6f70d982cfaeff42a7c0d39cd0eace01245274
Arnd Bergmann <arnd@arndb.de>
2005-06-23 09:43:43 +1000

    [PATCH] ppc64: Add driver for BPA interrupt controllers
    Add support for the integrated interrupt controller on BPA
    CPUs. There is one of those for each SMT thread.
    
    The mapping of interrupt numbers to HW interrupt sources
    is described in arch/ppc64/kernel/bpa_iic.h.
    
    This version hardcodes the 'Spider' chip as the secondary
    interrupt controller. That is not really generic for the
    architecture, but at the moment it is the only secondary
    PIC that exists.
    
    A little more work will be needed on this as soon as
    we have boards with multiple external interrupt controllers.
    
    Signed-off-by: Arnd Bergmann <arndb@de.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc64/kernel/bpa_setup.c
+++ arch/ppc64/kernel/bpa_setup.c
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cputable.h>
 
 #include "pci.h"
+#include "bpa_iic.h"
 
 #ifdef DEBUG
 #define DBG(fmt...) udbg_printf(fmt)
@@ -72,6 +73,9 @@ static void bpa_progress(char *s, unsigned short hex)
 
 static void __init bpa_setup_arch(void)
 {
+	ppc_md.init_IRQ       = iic_init_IRQ;
+	ppc_md.get_irq        = iic_get_irq;
+
 #ifdef CONFIG_SMP
 	smp_init_pSeries();
 #endif
@@ -87,7 +91,7 @@ static void __init bpa_setup_arch(void)
 	/* Find and initialize PCI host bridges */
 	init_pci_config_tokens();
 	find_and_init_phbs();
-
+	spider_init_IRQ();
 #ifdef CONFIG_DUMMY_CONSOLE
 	conswitchp = &dummy_con;
 #endif
