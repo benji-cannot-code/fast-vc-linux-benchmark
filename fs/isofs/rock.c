efb5720052379f6a571c0d3fa7380c3fdf14960c
Andrew Morton <akpm@osdl.org>
2005-06-21 17:16:45 -0700

    [PATCH] rock: remove CHECK_CE
    Remove the CHECK_CE macro
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/isofs/rock.c
+++ fs/isofs/rock.c
@@ -38,11 +38,6 @@ static int check_sp(struct rock_ridge *rr, struct inode *inode)
 	return 0;
 }
 
-#define CHECK_CE	       			\
-      {cont_extent = isonum_733(rr->u.CE.extent); \
-      cont_offset = isonum_733(rr->u.CE.offset); \
-      cont_size = isonum_733(rr->u.CE.size);}
-
 #define SETUP_ROCK_RIDGE(DE,CHR,LEN)	      		      	\
   {LEN= sizeof(struct iso_directory_record) + DE->name_len[0];	\
   if(LEN & 1) LEN++;						\
@@ -127,7 +122,9 @@ repeat:
 				goto out;
 			break;
 		case SIG('C', 'E'):
-			CHECK_CE;
+			cont_extent = isonum_733(rr->u.CE.extent);
+			cont_offset = isonum_733(rr->u.CE.offset);
+			cont_size = isonum_733(rr->u.CE.size);
 			break;
 		case SIG('N', 'M'):
 			if (truncate)
@@ -224,7 +221,9 @@ repeat:
 				goto out;
 			break;
 		case SIG('C', 'E'):
-			CHECK_CE;
+			cont_extent = isonum_733(rr->u.CE.extent);
+			cont_offset = isonum_733(rr->u.CE.offset);
+			cont_size = isonum_733(rr->u.CE.size);
 			break;
 		case SIG('E', 'R'):
 			ISOFS_SB(inode->i_sb)->s_rock = 1;
@@ -593,7 +592,9 @@ repeat:
 			break;
 		case SIG('C', 'E'):
 			/* This tells is if there is a continuation record */
-			CHECK_CE;
+			cont_extent = isonum_733(rr->u.CE.extent);
+			cont_offset = isonum_733(rr->u.CE.offset);
+			cont_size = isonum_733(rr->u.CE.size);
 		default:
 			break;
 		}
