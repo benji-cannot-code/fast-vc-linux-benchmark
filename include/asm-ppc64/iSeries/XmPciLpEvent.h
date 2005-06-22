8e10879d285a0ed011ed9713f8439ebbd710975d
Stephen Rothwell <sfr@canb.auug.org.au>
2005-06-21 17:15:34 -0700

    [PATCH] ppc64 iSeries: more header file white space cleanups
    This patch just contains white space and comment cleanups in the iSeries
    headers files.  There are no semantic changes.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ppc64/iSeries/XmPciLpEvent.h
+++ include/asm-ppc64/iSeries/XmPciLpEvent.h
@@ -1,8 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-
 #ifndef	__XMPCILPEVENT_H__
 #define	__XMPCILPEVENT_H__
-
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,7 +8,6 @@ extern "C" {
 
 int XmPciLpEvent_init(void);
 void ppc_irq_dispatch_handler(struct pt_regs *regs, int irq);
-
 
 #ifdef __cplusplus
 }
