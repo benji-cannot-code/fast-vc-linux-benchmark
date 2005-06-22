7465a16ad33daa40f1fa20f3c4b2ed3ce09caf71
Stephen Rothwell <sfr@canb.auug.org.au>
2005-06-21 17:15:49 -0700

    [PATCH] ppc64 iSeries: irq simple cleanups
    This patch is just simple cleanups to the iSeries irq code.
    	- whitespace and comments
    	- rearrange some functions to avoid forward declarations
    	- remove XmPciLpEvent.h as its functions were declared elsewhere
    	- remove decaration of function that no longer exists
    No semantic changes.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ppc64/iSeries/XmPciLpEvent.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef	__XMPCILPEVENT_H__
-#define	__XMPCILPEVENT_H__
-
-extern int XmPciLpEvent_init(void);
-extern void ppc_irq_dispatch_handler(struct pt_regs *regs, int irq);
-
-#endif /* __XMPCILPEVENT_H__ */
