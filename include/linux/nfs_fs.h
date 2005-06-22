9e60f393c2f48184bd1a6182abf2eaf5286b7066
Trond Myklebust <Trond.Myklebust@netapp.com>
2005-06-22 17:16:21 +0000

    [PATCH] NFS: Remove unused NFS inode field readdir_timestamp.
     Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- include/linux/nfs_fs.h
+++ include/linux/nfs_fs.h
@@ -129,7 +129,6 @@ struct nfs_inode {
 	 *
 	 *	mtime != read_cache_mtime
 	 */
-	unsigned long		readdir_timestamp;
 	unsigned long		read_cache_jiffies;
 	unsigned long		attrtimeo;
 	unsigned long		attrtimeo_timestamp;
