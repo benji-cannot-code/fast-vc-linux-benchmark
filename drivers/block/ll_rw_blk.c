832dd0ad3e94f61a817f3e6e072e15034f4022ae
Nick Piggin <nickpiggin@yahoo.com.au>
2005-06-23 00:08:54 -0700

    [PATCH] blk: unplug later
    get_request_wait needn't unplug the device immediately.
    
    Signed-off-by: Nick Piggin <nickpiggin@yahoo.com.au>
    Cc: Jens Axboe <axboe@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/block/ll_rw_blk.c
+++ drivers/block/ll_rw_blk.c
@@ -1970,7 +1970,6 @@ static struct request *get_request_wait(request_queue_t *q, int rw)
 	DEFINE_WAIT(wait);
 	struct request *rq;
 
-	generic_unplug_device(q);
 	do {
 		struct request_list *rl = &q->rq;
 
@@ -1982,6 +1981,7 @@ static struct request *get_request_wait(request_queue_t *q, int rw)
 		if (!rq) {
 			struct io_context *ioc;
 
+			generic_unplug_device(q);
 			io_schedule();
 
 			/*
