e6121e57527e690ddc969a19a32670b9f40651b7
Adrian Bunk <bunk@stusta.de>
2005-06-23 11:36:56 +0200

    [PATCH] better USB_MON dependencies
    This makes the USB_MON less confusing.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/usb/core/hcd.c
+++ drivers/usb/core/hcd.c
@@ -1795,7 +1795,7 @@ EXPORT_SYMBOL (usb_remove_hcd);
 
 /*-------------------------------------------------------------------------*/
 
-#if defined(CONFIG_USB_MON) || defined(CONFIG_USB_MON_MODULE)
+#if defined(CONFIG_USB_MON)
 
 struct usb_mon_operations *mon_ops;
 
