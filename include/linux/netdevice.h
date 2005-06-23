e68d7097190ff5630534dac3238aee197f2b68e6
Jeff Moyer <jmoyer@redhat.com>
2005-06-22 22:05:31 -0700

    [NETPOLL]: Introduce a netpoll_info struct
    This patch introduces a netpoll_info structure, which the struct net_device
    will now point to instead of pointing to a struct netpoll.  The reason for
    this is two-fold: 1) fields such as the rx_flags, poll_owner, and poll_lock
    should be maintained per net_device, not per netpoll;  and 2) this is a first
    step in providing support for multiple netpoll clients to register against the
    same net_device.
    
    The struct netpoll is now pointed to by the netpoll_info structure.  As
    such, the previous behaviour of the code is preserved.
    
    Signed-off-by: Jeff Moyer <jmoyer@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct divert_blk;
 struct vlan_group;
 struct ethtool_ops;
-struct netpoll;
+struct netpoll_info;
 					/* source back-compat hooks */
 #define SET_ETHTOOL_OPS(netdev,ops) \
 	( (netdev)->ethtool_ops = (ops) )
@@ -469,7 +469,7 @@ struct net_device
 						     unsigned char *haddr);
 	int			(*neigh_setup)(struct net_device *dev, struct neigh_parms *);
 #ifdef CONFIG_NETPOLL
-	struct netpoll		*np;
+	struct netpoll_info	*npinfo;
 #endif
 #ifdef CONFIG_NET_POLL_CONTROLLER
 	void                    (*poll_controller)(struct net_device *dev);
