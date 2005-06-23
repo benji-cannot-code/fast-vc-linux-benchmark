5b525c426e413d709d82c0ca134e9a42d72f52fa
Stephen Hemminger <shemminger@osdl.org>
2005-06-23 12:21:28 -0700

    [TCP]: Report congestion control algorithm in tcp_diag.
    Enhancement to the tcp_diag interface used by the iproute2 ss command
    to report the tcp congestion control being used by a socket.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/tcp_diag.h
+++ include/linux/tcp_diag.h
@@ -100,9 +100,10 @@ enum
 	TCPDIAG_MEMINFO,
 	TCPDIAG_INFO,
 	TCPDIAG_VEGASINFO,
+	TCPDIAG_CONG,
 };
 
-#define TCPDIAG_MAX TCPDIAG_VEGASINFO
+#define TCPDIAG_MAX TCPDIAG_CONG
 
 
 /* TCPDIAG_MEM */
@@ -123,6 +124,5 @@ struct tcpvegas_info {
 	__u32	tcpv_rtt;
 	__u32	tcpv_minrtt;
 };
-
 
 #endif /* _TCP_DIAG_H_ */
