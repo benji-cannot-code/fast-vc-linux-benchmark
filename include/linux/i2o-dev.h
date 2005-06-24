ea5ccc7fd808ac4b0f431e37b9fefec5fbfece99
Markus Lidel <Markus.Lidel@shadowconnect.com>
2005-06-23 22:02:16 -0700

    [PATCH] I2O: new sysfs attributes and Adaptec specific block device access and 64-bit DMA support
    Changes:
     - Added Bus-OSM which could be used by user space programs to reset a
       channel on the controller
     - Make ioctl's in Config-OSM obsolete in prefer for sysfs attributes and
       move those to its own file
     - Added sysfs attribute for firmware read and write access for I2O
       controllers
     - Added special handling of firmware read and write access for Adaptec
       controllers
     - Added vendor id and product id as sysfs-attribute to Executive classes
     - Added automatic notification of LCT change handling to Exec-OSM
     - Added flushing function to Block-OSM for later barrier implementation
     - Use PRIVATE messages for Block access on Adaptec controllers, which are
       faster then BLOCK class access
     - Cleaned up support for Promise controller
     - New messages are now detected using the IRQ status register as
       suggested by the I2O spec
     - Added i2o_dma_high() and i2o_dma_low() functions
     - Added facility for SG tablesize calculation when using 32-bit and
       64-bit DMA addresses
     - Added i2o_dma_map_single() and i2o_dma_map_sg() which could build the
       SG list for 32-bit as well as 64-bit DMA addresses
    
    Signed-off-by: Markus Lidel <Markus.Lidel@shadowconnect.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/i2o-dev.h
+++ include/linux/i2o-dev.h
@@ -33,6 +33,10 @@ typedef unsigned int u32;
 
 #endif				/* __KERNEL__ */
 
+/*
+ *	Vendors
+ */
+#define I2O_VENDOR_DPT				0x001b
 
 /*
  * I2O Control IOCTLs and structures
@@ -334,7 +338,7 @@ typedef struct _i2o_status_block {
 #define I2O_CLASS_ATE_PERIPHERAL		0x061
 #define I2O_CLASS_FLOPPY_CONTROLLER		0x070
 #define I2O_CLASS_FLOPPY_DEVICE 		0x071
-#define I2O_CLASS_BUS_ADAPTER_PORT		0x080
+#define I2O_CLASS_BUS_ADAPTER			0x080
 #define I2O_CLASS_PEER_TRANSPORT_AGENT		0x090
 #define I2O_CLASS_PEER_TRANSPORT		0x091
 #define	I2O_CLASS_END				0xfff
