d55080b220e9a4acb0e1c77b7856c3cc363fd098
Nick Piggin <nickpiggin@yahoo.com.au>
2005-06-25 14:57:19 -0700

    [PATCH] sched: balance on fork
    Reimplement the balance on exec balancing to be sched-domains aware.  Use this
    to also do balance on fork balancing.  Make x86_64 do balance on fork over the
    NUMA domain.
    
    The problem that the non sched domains aware blancing became apparent on dual
    core, multi socket opterons.  What we want is for the new tasks to be sent to
    a different socket, but more often than not, we would first load up our
    sibling core, or fill two cores of a single remote socket before selecting a
    new one.
    
    This gives large improvements to STREAM on such systems.
    
    Signed-off-by: Nick Piggin <nickpiggin@yahoo.com.au>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -94,6 +94,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	.idle_idx		= 0,			\
 	.newidle_idx		= 0,			\
 	.wake_idx		= 0,			\
+	.forkexec_idx		= 0,			\
 	.flags			= SD_LOAD_BALANCE	\
 				| SD_BALANCE_NEWIDLE	\
 				| SD_BALANCE_EXEC	\
@@ -124,6 +125,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	.idle_idx		= 0,			\
 	.newidle_idx		= 1,			\
 	.wake_idx		= 1,			\
+	.forkexec_idx		= 0,			\
 	.flags			= SD_LOAD_BALANCE	\
 				| SD_BALANCE_NEWIDLE	\
 				| SD_BALANCE_EXEC	\
