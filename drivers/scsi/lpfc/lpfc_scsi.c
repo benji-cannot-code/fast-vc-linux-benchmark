f6a01d5592486ebda26180be9d94034739a9cc2a
James.Smart@Emulex.Com <James.Smart@Emulex.Com>
2005-06-25 10:34:17 -0400

    [SCSI] lpfc: Set max_sectors in host template
    Add max_sectors to the driver host template and initialize it with
    0xFFFF since the driver has no limitations on the size a transfer
    contained by a scsi command and that fits within the sg_tablesize
    provisioned by the driver.  This fixes a performance issue seen in
    some configurations.
    
    Signed-off-by: James Smart <James.Smart@emulex.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/lpfc/lpfc_scsi.c
+++ drivers/scsi/lpfc/lpfc_scsi.c
@@ -1274,4 +1274,5 @@ struct scsi_host_template lpfc_template = {
 	.cmd_per_lun		= LPFC_CMD_PER_LUN,
 	.use_clustering		= ENABLE_CLUSTERING,
 	.shost_attrs		= lpfc_host_attrs,
+	.max_sectors		= 0xFFFF,
 };
