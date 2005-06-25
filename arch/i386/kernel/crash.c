828987e75f54c418ab0d2b1124e0e3d11b4ad86d
Eric W. Biederman <ebiederm@xmission.com>
2005-06-25 14:58:00 -0700

    [PATCH] kexec: x86 shutdown APICs during crash_shutdown
    In the case of a crash/panic an architecture specific function
    machine_crash_shutdown is called.  This patch adds to the x86 machine_crash
    function the standard kernel code for shutting down apics.
    
    Every line of code added to that function increases the risk that we will call
    code after a kernel panic that is not safe.
    
    This patch should not make it to the stable kernel without a being reviewed a
    lot more.  It is unclear how much a hardned kernel can take when it comes to
    misconfigured apics.  So since a normal kernel has problems this patch does a
    clean shutdown.
    
    It is my expectation this patch will be dropped from future generations of the
    kexec work.  But for the moment it is a crutch to keep from breaking
    everything.
    
    Signed-off-by: Eric Biederman <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/kernel/crash.c
+++ arch/i386/kernel/crash.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/hardirq.h>
 #include <asm/nmi.h>
 #include <asm/hw_irq.h>
+#include <asm/apic.h>
 #include <mach_ipi.h>
 
 #define MAX_NOTE_BYTES 1024
@@ -116,6 +117,7 @@ static int crash_nmi_callback(struct pt_regs *regs, int cpu)
 {
 	local_irq_disable();
 	crash_save_this_cpu(regs, cpu);
+	disable_local_APIC();
 	atomic_dec(&waiting_for_crash_ipi);
 	/* Assume hlt works */
 	__asm__("hlt");
@@ -154,6 +156,7 @@ static void nmi_shootdown_cpus(void)
 	}
 
 	/* Leave the nmi callback set */
+	disable_local_APIC();
 }
 #else
 static void nmi_shootdown_cpus(void)
@@ -175,5 +178,9 @@ void machine_crash_shutdown(void)
 	/* The kernel is broken so disable interrupts */
 	local_irq_disable();
 	nmi_shootdown_cpus();
+	lapic_shutdown();
+#if defined(CONFIG_X86_IO_APIC)
+	disable_IO_APIC();
+#endif
 	crash_save_self();
 }
