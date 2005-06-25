99180f441d83547f2f6edb5740390f7eebf28c77
Eric W. Biederman <ebiederm@xmission.com>
2005-06-25 14:58:04 -0700

    [PATCH] crashdump: x86_64: crashkernel option
    This is the x86_64 implementation of the crashkernel option.  It reserves
    a window of memory very early in the bootup process, so we never use
    it for anything but the kernel to switch to when the running
    kernel panics.
    
    In addition to reserving this memory a resource structure is registered
    so looking at /proc/iomem it is clear what happened to that memory.
    
    ISSUES:
    Is it possible to implement this in a architecture generic way?
    What should be done with architectures that always use an iommu and
    thus don't report their RAM memory resources in /proc/iomem?
    
    Signed-off-by: Eric Biederman <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/e820.c
+++ arch/x86_64/kernel/e820.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bootmem.h>
 #include <linux/ioport.h>
 #include <linux/string.h>
+#include <linux/kexec.h>
 #include <asm/page.h>
 #include <asm/e820.h>
 #include <asm/proto.h>
@@ -211,6 +212,9 @@ void __init e820_reserve_resources(void)
 			 */
 			request_resource(res, &code_resource);
 			request_resource(res, &data_resource);
+#ifdef CONFIG_KEXEC
+			request_resource(res, &crashk_res);
+#endif
 		}
 	}
 }
