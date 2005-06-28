17a120272635c76d9fc0a09c5d0c48caf7b2acf1
Patrick McHardy <kaber@trash.net>
2005-06-28 12:56:45 -0700

    [NETLINK]: Missing padding fields in dumped structures
    Plug holes with padding fields and initialized them to zero.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/rtnetlink.h
+++ include/linux/rtnetlink.h
@@ -364,6 +364,8 @@ enum
 struct rta_session
 {
 	__u8	proto;
+	__u8	pad1;
+	__u16	pad2;
 
 	union {
 		struct {
@@ -636,10 +638,13 @@ struct ifinfomsg
 struct prefixmsg
 {
 	unsigned char	prefix_family;
+	unsigned char	prefix_pad1;
+	unsigned short	prefix_pad2;
 	int		prefix_ifindex;
 	unsigned char	prefix_type;
 	unsigned char	prefix_len;
 	unsigned char	prefix_flags;
+	unsigned char	prefix_pad3;
 };
 
 enum 
