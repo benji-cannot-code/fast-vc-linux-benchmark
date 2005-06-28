376b38e98c3a101f3975e4f56df46a1a7dd2ebc2
Arnd Bergmann <arndb@de.ibm.com>
2005-06-28 15:58:50 -0700

    [NET]: Add missing include to linux/netdevice.h
    linux/etherdevice.h can't be included standalone at the moment, which
    is required in order to sort the header files in the recommended
    alphabetic order. This patch fixes that and is needed to build spider_net.
    
    Signed-off-by: Arnd Bergmann <arndb@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/etherdevice.h
+++ include/linux/etherdevice.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_ETHERDEVICE_H
 
 #include <linux/if_ether.h>
+#include <linux/netdevice.h>
 #include <linux/random.h>
 
 #ifdef __KERNEL__
