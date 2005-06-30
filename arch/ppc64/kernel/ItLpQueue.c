7c53bad2726da098884e3579fe2aeeabd8286864
Michael Ellerman <michael@ellerman.id.au>
2005-06-30 15:08:56 +1000

    [PATCH] ppc64: Make two ItLpQueue related functions static
    External parties don't need to use ItLpQueue_getNextLpEvent() or
    ItLpQueue_clearValid(), they're internal to ItLpQueue.c
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Acked-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc64/kernel/ItLpQueue.c
+++ arch/ppc64/kernel/ItLpQueue.c
@@ -63,7 +63,7 @@ static __inline__ void clear_inUse(void)
 extern LpEventHandler lpEventHandler[HvLpEvent_Type_NumTypes];
 unsigned long ItLpQueueInProcess = 0;
 
-struct HvLpEvent * ItLpQueue_getNextLpEvent(void)
+static struct HvLpEvent * ItLpQueue_getNextLpEvent(void)
 {
 	struct HvLpEvent * nextLpEvent = 
 		(struct HvLpEvent *)xItLpQueue.xSlicCurEventPtr;
@@ -98,7 +98,7 @@ int ItLpQueue_isLpIntPending(void)
 	return next_event->xFlags.xValid | xItLpQueue.xPlicOverflowIntPending;
 }
 
-void ItLpQueue_clearValid( struct HvLpEvent * event )
+static void ItLpQueue_clearValid( struct HvLpEvent * event )
 {
 	/* Clear the valid bit of the event
 	 * Also clear bits within this event that might
