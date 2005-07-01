7749c4b4c59294b74c267ca200fbf11cd647d687
Todd Poynor <tpoynor@mvista.com>
2005-07-01 11:27:05 +0100

    [PATCH] ARM: 2781/2: PXA27x Standby mode take 2
    Patch from Todd Poynor
    
    Add support for PXA27x Standby mode, a low-power mode that retains CPU
    and some peripheral state (the existing "sleep" mode is a power-power
    mode that retains less state). Activated via:
    echo -n standby > /sys/power/state
    From: David Burrage and Todd Poynor
    
    Signed-off-by: Todd Poynor <tpoynor@mvista.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-pxa/pxa-regs.h
+++ include/asm-arm/arch-pxa/pxa-regs.h
@@ -1506,6 +1506,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PSSR_OTGPH	(1 << 6)	/* OTG Peripheral control Hold */
 #define PSSR_RDH	(1 << 5)	/* Read Disable Hold */
 #define PSSR_PH		(1 << 4)	/* Peripheral Control Hold */
+#define PSSR_STS	(1 << 3)	/* Standby Mode Status */
 #define PSSR_VFS	(1 << 2)	/* VDD Fault Status */
 #define PSSR_BFS	(1 << 1)	/* Battery Fault Status */
 #define PSSR_SSS	(1 << 0)	/* Software Sleep Status */
