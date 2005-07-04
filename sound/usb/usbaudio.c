aca9ed047df7d5088cc25e5869aaab625c538397
Clemens Ladisch <clemens@ladisch.de>
2005-07-04 09:18:46 +0200

    [ALSA] usb-audio - enable high speed transfers with Audiy 2 NX
    USB generic driver
    This patch enables the boot commands to activate high speed mode (and
    associated sample formats like 8 channels with 24 bits at 96 kHz) on the
    SB Audigy 2 NX.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>

--- sound/usb/usbaudio.c
+++ sound/usb/usbaudio.c
@@ -2939,8 +2939,6 @@ static int snd_usb_extigy_boot_quirk(struct usb_device *dev, struct usb_interfac
 
 static int snd_usb_audigy2nx_boot_quirk(struct usb_device *dev)
 {
-#if 0
-	/* TODO: enable this when high speed synchronization actually works */
 	u8 buf = 1;
 
 	snd_usb_ctl_msg(dev, usb_rcvctrlpipe(dev, 0), 0x2a,
@@ -2952,7 +2950,6 @@ static int snd_usb_audigy2nx_boot_quirk(struct usb_device *dev)
 				1, 2000, NULL, 0, 1000);
 		return -ENODEV;
 	}
-#endif
 	return 0;
 }
 
