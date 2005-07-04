9a05bbea5289eeb59840f3e18f20bf3da9a9d25f
Christoph Hellwig <hch@lst.de>
2005-07-04 17:48:41 +0200

    [SCSI] qla1280: remove SG_SEGMENTS
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Thiemo Seufer <ths@linux-mips.org>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla1280.h
+++ drivers/scsi/qla1280.h
@@ -95,9 +95,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define REQUEST_ENTRY_CNT		256 /* Number of request entries. */
 #define RESPONSE_ENTRY_CNT		16  /* Number of response entries. */
 
-/* Number of segments 1 - 65535 */
-#define SG_SEGMENTS			32  /* Cmd entry + 6 continuations */
-
 /*
  * SCSI Request Block structure  (sp)  that is placed
  * on cmd->SCp location of every I/O
