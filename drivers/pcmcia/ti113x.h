1a1887cbee77aa5f338d3a7e43ed43cc15b4e7a5
Dominik Brodowski <linux@dominikbrodowski.net>
2005-07-07 17:59:09 -0700

    [PATCH] yenta: don't depend on CardBus
    As a follow-up, we can allow the yenta-driver to be limited to PCMCIA
    operation.
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/pcmcia/ti113x.h
+++ drivers/pcmcia/ti113x.h
@@ -155,8 +155,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ENE_TEST_C9			0xc9	/* 8bit */
 #define ENE_TEST_C9_TLTENABLE		0x02
 
-#ifdef CONFIG_CARDBUS
-
 /*
  * Texas Instruments CardBus controller overrides.
  */
@@ -843,8 +841,6 @@ static int ti1250_override(struct yenta_socket *socket)
 
 	return ti12xx_override(socket);
 }
-
-#endif /* CONFIG_CARDBUS */
 
 #endif /* _LINUX_TI113X_H */
 
