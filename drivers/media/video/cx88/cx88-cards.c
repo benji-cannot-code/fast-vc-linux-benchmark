c8f47fa6e8ad58984c1731bddc986bd7239b9aa2
Michael Krufky <mkrufky@m1k.net>
2005-07-07 17:58:40 -0700

    [PATCH] v4l: add TerraTec Cinergy 1400 DVB-T
    Add support for TerraTec Cinergy 1400 DVB-T.
    
    Signed-off-by: Uli Luckas <luckas@musoft.de>
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/cx88/cx88-cards.c
+++ drivers/media/video/cx88/cx88-cards.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: cx88-cards.c,v 1.82 2005/06/28 04:33:53 mkrufky Exp $
+ * $Id: cx88-cards.c,v 1.84 2005/07/02 19:42:09 mkrufky Exp $
  *
  * device driver for Conexant 2388x based TV cards
  * card-specific stuff.
@@ -744,6 +744,15 @@ struct cx88_board cx88_boards[] = {
                 }},
 		.dvb            = 1,
 	},
+	[CX88_BOARD_TERRATEC_CINERGY_1400_DVB_T1] = {
+		.name           = "TerraTec Cinergy 1400 DVB-T",
+		.tuner_type     = TUNER_ABSENT,
+		.input          = {{
+			.type   = CX88_VMUX_DVB,
+			.vmux   = 0,
+		}},
+		.dvb            = 1,
+	},
 };
 const unsigned int cx88_bcount = ARRAY_SIZE(cx88_boards);
 
@@ -867,6 +876,10 @@ struct cx88_subid cx88_subids[] = {
 		.subvendor = 0x1421,
 		.subdevice = 0x0334,
 		.card      = CX88_BOARD_ADSTECH_DVB_T_PCI,
+ 	},{
+		.subvendor = 0x153b,
+		.subdevice = 0x1166,
+		.card      = CX88_BOARD_TERRATEC_CINERGY_1400_DVB_T1,
 	},
 };
 const unsigned int cx88_idcount = ARRAY_SIZE(cx88_subids);
--- drivers/media/video/cx88/cx88-dvb.c
+++ drivers/media/video/cx88/cx88-dvb.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: cx88-dvb.c,v 1.37 2005/06/28 23:41:47 mkrufky Exp $
+ * $Id: cx88-dvb.c,v 1.39 2005/07/02 20:00:46 mkrufky Exp $
  *
  * device driver for Conexant 2388x based TV cards
  * MPEG Transport Stream (DVB) routines
@@ -236,6 +236,7 @@ static int dvb_register(struct cx8802_dev *dev)
 		dev->dvb.frontend = cx22702_attach(&hauppauge_novat_config,
 						   &dev->core->i2c_adap);
 		break;
+	case CX88_BOARD_TERRATEC_CINERGY_1400_DVB_T1:
 	case CX88_BOARD_CONEXANT_DVB_T1:
 		dev->dvb.frontend = cx22702_attach(&connexant_refboard_config,
 						   &dev->core->i2c_adap);
