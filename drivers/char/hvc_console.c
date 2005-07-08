f48a3698da46ee224267edf02eeee63d5c2bc93d
Milton Miller <miltonm@bga.com>
2005-07-07 17:56:25 -0700

    [PATCH] hvc_console: Register ops when setting up hvc_console
    When registering the hvc console port, register a list of ops (read and write)
    to go with it, instead of calling fixed function names.
    
    This allows different ports to encode the data differently.
    
    Signed-off-by: Milton Miller <miltonm@bga.com>
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/hvc_console.c
+++ drivers/char/hvc_console.c
@@ -86,6 +86,7 @@ struct hvc_struct {
 	char outbuf[N_OUTBUF] __ALIGNED__;
 	int n_outbuf;
 	uint32_t vtermno;
+	struct hv_ops *ops;
 	int irq_requested;
 	int irq;
 	struct list_head next;
@@ -143,6 +144,7 @@ struct hvc_struct *hvc_get_by_index(int index)
  * console interfaces but can still be used as a tty device.  This has to be
  * static because kmalloc will not work during early console init.
  */
+static struct hv_ops *cons_ops[MAX_NR_HVC_CONSOLES];
 static uint32_t vtermnos[MAX_NR_HVC_CONSOLES] =
 	{[0 ... MAX_NR_HVC_CONSOLES - 1] = -1};
 
@@ -155,14 +157,14 @@ void hvc_console_print(struct console *co, const char *b, unsigned count)
 {
 	char c[16] __ALIGNED__;
 	unsigned i = 0, n = 0;
-	int r, donecr = 0;
+	int r, donecr = 0, index = co->index;
 
 	/* Console access attempt outside of acceptable console range. */
-	if (co->index >= MAX_NR_HVC_CONSOLES)
+	if (index >= MAX_NR_HVC_CONSOLES)
 		return;
 
 	/* This console adapter was removed so it is not useable. */
-	if (vtermnos[co->index] < 0)
+	if (vtermnos[index] < 0)
 		return;
 
 	while (count > 0 || i > 0) {
@@ -176,7 +178,7 @@ void hvc_console_print(struct console *co, const char *b, unsigned count)
 				--count;
 			}
 		} else {
-			r = hvc_put_chars(vtermnos[co->index], c, i);
+			r = cons_ops[index]->put_chars(vtermnos[index], c, i);
 			if (r < 0) {
 				/* throw away chars on error */
 				i = 0;
@@ -246,7 +248,7 @@ console_initcall(hvc_console_init);
  * vty adapters do NOT get an hvc_instantiate() callback since they
  * appear after early console init.
  */
-int hvc_instantiate(uint32_t vtermno, int index)
+int hvc_instantiate(uint32_t vtermno, int index, struct hv_ops *ops)
 {
 	struct hvc_struct *hp;
 
@@ -264,6 +266,7 @@ int hvc_instantiate(uint32_t vtermno, int index)
 	}
 
 	vtermnos[index] = vtermno;
+	cons_ops[index] = ops;
 
 	/* reserve all indices upto and including this index */
 	if (last_hvc < index)
@@ -467,7 +470,7 @@ static void hvc_push(struct hvc_struct *hp)
 {
 	int n;
 
-	n = hvc_put_chars(hp->vtermno, hp->outbuf, hp->n_outbuf);
+	n = hp->ops->put_chars(hp->vtermno, hp->outbuf, hp->n_outbuf);
 	if (n <= 0) {
 		if (n == 0)
 			return;
@@ -605,7 +608,7 @@ static int hvc_poll(struct hvc_struct *hp)
 			break;
 		}
 
-		n = hvc_get_chars(hp->vtermno, buf, count);
+		n = hp->ops->get_chars(hp->vtermno, buf, count);
 		if (n <= 0) {
 			/* Hangup the tty when disconnected from host */
 			if (n == -EPIPE) {
@@ -738,7 +741,8 @@ static struct kobj_type hvc_kobj_type = {
 	.release = destroy_hvc_struct,
 };
 
-struct hvc_struct __devinit *hvc_alloc(uint32_t vtermno, int irq)
+struct hvc_struct __devinit *hvc_alloc(uint32_t vtermno, int irq,
+					struct hv_ops *ops)
 {
 	struct hvc_struct *hp;
 	int i;
@@ -751,6 +755,7 @@ struct hvc_struct __devinit *hvc_alloc(uint32_t vtermno, int irq)
 
 	hp->vtermno = vtermno;
 	hp->irq = irq;
+	hp->ops = ops;
 
 	kobject_init(&hp->kobj);
 	hp->kobj.ktype = &hvc_kobj_type;
