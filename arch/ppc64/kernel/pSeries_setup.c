cbc530daa93d32a4f689a5642874f9e83d1ccc1b
Olaf Hering <olh@suse.de>
2005-07-10 19:35:15 +0000

    [PATCH] remove linux/version.h include from arch/ppc64
    Changing CONFIG_LOCALVERSION rebuilds too much, for no apparent reason.
    
    Use system_utsname for progress and debug header.
    
    Signed-off-by: Olaf Hering <olh@suse.de>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc64/kernel/pSeries_setup.c
+++ arch/ppc64/kernel/pSeries_setup.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ioport.h>
 #include <linux/console.h>
 #include <linux/pci.h>
-#include <linux/version.h>
+#include <linux/utsname.h>
 #include <linux/adb.h>
 #include <linux/module.h>
 #include <linux/delay.h>
@@ -273,7 +273,7 @@ static int __init pSeries_init_panel(void)
 {
 	/* Manually leave the kernel version on the panel. */
 	ppc_md.progress("Linux ppc64\n", 0);
-	ppc_md.progress(UTS_RELEASE, 0);
+	ppc_md.progress(system_utsname.version, 0);
 
 	return 0;
 }
