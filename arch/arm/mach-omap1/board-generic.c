3f7049ce312385de62f8903886ac6103bfea9619
Tony Lindgren <tony@atomide.com>
2005-07-10 19:58:11 +0100

    [PATCH] ARM: 2806/1: OMAP update 5/11: Move board files into mach-omap1 directory
    Patch from Tony Lindgren
    
    This patch by Paul Mundt and other OMAP developers
    moves OMAP1 board files into mach-omap1 directory.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap1/board-generic.c
+++ arch/arm/mach-omap1/board-generic.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/arch/arm/mach-omap/board-generic.c
+ * linux/arch/arm/mach-omap1/board-generic.c
  *
  * Modified from board-innovator1510.c
  *
--- arch/arm/mach-omap1/board-h3.c
+++ arch/arm/mach-omap1/board-h3.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/arch/arm/mach-omap/board-h3.c
+ * linux/arch/arm/mach-omap1/board-h3.c
  *
  * This file contains OMAP1710 H3 specific code.
  *
