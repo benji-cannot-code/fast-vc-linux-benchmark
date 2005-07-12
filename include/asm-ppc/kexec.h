375fa9362cb9a34fd0b95d318bfaca1bf3188581
Albert Herranz <albert_herranz@yahoo.es>
2005-07-12 13:58:34 -0700

    [PATCH] kexec-ppc: fix for ksysfs crash_notes
    The following patch prevents the crash dump helper code found within kexec
    from breaking ppc which still lacks crash dump functionality.
    
    ksysfs crash_notes attribute handling was left under CONFIG_KEXEC for
    simplicity although it is not strictly kexec related.
    
    We provide here a dummy definition for crash_notes on ppc.
    
    Signed-off-by: Albert Herranz <albert_herranz@yahoo.es>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ppc/kexec.h
+++ include/asm-ppc/kexec.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+extern void *crash_notes;
+
 struct kimage;
 
 extern void machine_kexec_simple(struct kimage *image);
