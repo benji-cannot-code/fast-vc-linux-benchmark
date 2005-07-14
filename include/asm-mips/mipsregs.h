89b5b9c0ccec726e2f83ae9aadbccf62a567c900
Ralf Baechle <ralf@linux-mips.org>
2005-07-14 07:30:27 +0000

    Macros to access the register of processors using the new MIPS Multithreading ASE, also know as MT ASE.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    
    diff --git a/include/asm-mips/mipsmtregs.h b/include/asm-mips/mipsmtregs.h
    new file mode 100644

--- include/asm-mips/mipsregs.h
+++ include/asm-mips/mipsregs.h
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ULCAST_ (unsigned long)
 #endif
 
+#include <asm/mipsmtregs.h>
+
 /*
  * Coprocessor 0 register names
  */
