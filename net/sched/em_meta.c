ee8ad5d2c09458db0f06603ad4d1e908ce66e87b
David S. Miller <davem@davemloft.net>
2005-07-18 17:10:55 -0700

    [EMATCH]: Kill TCF_META_ID_TCCLASSID reference from meta ematch as well.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/em_meta.c
+++ net/sched/em_meta.c
@@ -251,11 +251,6 @@ META_COLLECTOR(int_tcverd)
 {
 	dst->value = skb->tc_verd;
 }
-
-META_COLLECTOR(int_tcclassid)
-{
-	dst->value = skb->tc_classid;
-}
 #endif
 
 /**************************************************************************
@@ -530,7 +525,6 @@ static struct meta_ops __meta_ops[TCF_META_TYPE_MAX+1][TCF_META_ID_MAX+1] = {
 		[META_ID(TCINDEX)]		= META_FUNC(int_tcindex),
 #ifdef CONFIG_NET_CLS_ACT
 		[META_ID(TCVERDICT)]		= META_FUNC(int_tcverd),
-		[META_ID(TCCLASSID)]		= META_FUNC(int_tcclassid),
 #endif
 #ifdef CONFIG_NET_CLS_ROUTE
 		[META_ID(RTCLASSID)]		= META_FUNC(int_rtclassid),
