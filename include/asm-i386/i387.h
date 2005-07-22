8d94d183fb0686d2d2e2dbacea6cf9a58b642e59
Linus Torvalds <torvalds@evo.osdl.org>
2005-07-22 16:06:16 -0400

    x86: make restore_fpu() use alternative assembler instructions
    It's really just a single instruction, conditional on whether the CPU
    supports FXSR or not, so implement it as such instead of making it a
    function that queries FXSR dynamically.
    
    This means that the instruction just gets automatically rewritten to the
    correct one at boot-time.

--- include/asm-i386/i387.h
+++ include/asm-i386/i387.h
@@ -20,10 +20,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void mxcsr_feature_mask_init(void);
 extern void init_fpu(struct task_struct *);
+
 /*
  * FPU lazy state save handling...
  */
-extern void restore_fpu( struct task_struct *tsk );
+
+/*
+ * The "nop" is needed to make the instructions the same
+ * length.
+ */
+#define restore_fpu(tsk)			\
+	alternative_input(			\
+		"nop ; frstor %1",		\
+		"fxrstor %1",			\
+		X86_FEATURE_FXSR,		\
+		"m" ((tsk)->thread.i387.fsave))
 
 extern void kernel_fpu_begin(void);
 #define kernel_fpu_end() do { stts(); preempt_enable(); } while(0)
