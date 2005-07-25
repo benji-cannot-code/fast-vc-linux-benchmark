62f6856044b219c2770c1a07f45b0a84b690ce2d
David S. Miller <davem@davemloft.net>
2005-07-24 19:35:08 -0700

    [SPARC]: Fix __KERNEL_SYSCALLS__ defining in envctrl.c and bbc_envctrl.c
    It needs to happen before any header includes because nowadays
    some things implicitly include asm/unistd.h which ends up being
    before the __KERNEL_SYSCALLS__ define gets done.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/sbus/char/envctrl.c
+++ drivers/sbus/char/envctrl.c
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *              Daniele Bellucci <bellucda@tiscali.it>
  */
 
+#define __KERNEL_SYSCALLS__
+
 #include <linux/config.h>
 #include <linux/module.h>
 #include <linux/sched.h>
@@ -36,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/envctrl.h>
 
-#define __KERNEL_SYSCALLS__
 static int errno;
 #include <asm/unistd.h>
 
