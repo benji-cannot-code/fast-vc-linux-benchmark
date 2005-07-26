4d455547f402bbe69a12cedc7421953f163587a9
Eric W. Biederman <ebiederm@xmission.com>
2005-07-26 11:36:01 -0600

    [PATCH] Don't export machine_restart, machine_halt, or machine_power_off.
    machine_restart, machine_halt and machine_power_off are machine
    specific hooks deep into the reboot logic, that modules
    have no business messing with.  Usually code should be calling
    kernel_restart, kernel_halt, kernel_power_off, or
    emergency_restart. So don't export machine_restart,
    machine_halt, and machine_power_off so we can catch buggy users.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/sparc64/kernel/process.c
+++ arch/sparc64/kernel/process.c
@@ -125,8 +125,6 @@ void machine_halt(void)
 	panic("Halt failed!");
 }
 
-EXPORT_SYMBOL(machine_halt);
-
 void machine_alt_power_off(void)
 {
 	if (!serial_console && prom_palette)
@@ -154,8 +152,6 @@ void machine_restart(char * cmd)
 	prom_reboot("");
 	panic("Reboot failed!");
 }
-
-EXPORT_SYMBOL(machine_restart);
 
 static void show_regwindow32(struct pt_regs *regs)
 {
--- arch/x86_64/kernel/reboot.c
+++ arch/x86_64/kernel/reboot.c
@@ -151,13 +151,9 @@ void machine_restart(char * __unused)
 	}      
 }
 
-EXPORT_SYMBOL(machine_restart);
-
 void machine_halt(void)
 {
 }
-
-EXPORT_SYMBOL(machine_halt);
 
 void machine_power_off(void)
 {
@@ -165,4 +161,3 @@ void machine_power_off(void)
 		pm_power_off();
 }
 
-EXPORT_SYMBOL(machine_power_off);
