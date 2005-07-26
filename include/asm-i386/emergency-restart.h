2baaed4c8ca49e8a0c0cc3e43948ac0874abe439
Eric W. Biederman <ebiederm@xmission.com>
2005-07-26 11:41:26 -0600

    [PATCH] i386: Implement machine_emergency_reboot
    set_cpus_allowed is not safe in interrupt context
    and disabling apics is complicated code so don't
    call machine_shutdown on i386 from emergency_restart().
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/emergency-restart.h
+++ include/asm-i386/emergency-restart.h
@@ -2,6 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_EMERGENCY_RESTART_H
 #define _ASM_EMERGENCY_RESTART_H
 
-#include <asm-generic/emergency-restart.h>
+extern void machine_emergency_restart(void);
 
 #endif /* _ASM_EMERGENCY_RESTART_H */
