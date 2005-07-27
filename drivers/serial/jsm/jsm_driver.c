5ab7e2e86f3a8d7f3a870eef222494b742ea65df
V. ANANDA KRISHNAN <mansarov@us.ibm.com>
2005-07-27 11:43:49 -0700

    [PATCH] jsm: warning fixes
    - updates the version
    
    - fix mixing of declarations and code.  The mixing of declarations and
      code displays warnings when used against RedHat RHEL4.0 distro (compiler
      version is 3.4.3-22.1) and hence I separated them out.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/serial/jsm/jsm_driver.c
+++ drivers/serial/jsm/jsm_driver.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Scott H Kilau <Scott_Kilau@digi.com>
  * Wendy Xiong   <wendyx@us.ltcfwd.linux.ibm.com>
  *
+ *
  ***********************************************************************/
 #include <linux/moduleparam.h>
 #include <linux/pci.h>
