83eeffea825a6656a1e7eedc4ff5e8e89936319e
Michael Krufky <mkrufky@m1k.net>
2005-07-27 11:45:55 -0700

    [PATCH] dvb/4vl: RF input selection fir
    Select the RF input connector based upon the type of demodulation selected.
     ANT RF connector is selected for 8-VSB and CABLE RF connector is selected
    for QAM64/QAM256.  This only affects the cards that use the Microtune 4042
    tuner.
    
    Signed-off-by: Mac Michaels <wmichaels1@earthlink.net>
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/dvb/frontends/lgdt3302.c
+++ drivers/media/dvb/frontends/lgdt3302.c
@@ -215,6 +215,10 @@ static int lgdt3302_set_parameters(struct dvb_frontend* fe,
 
 			/* Select VSB mode and serial MPEG interface */
 			top_ctrl_cfg[1] = 0x07;
+
+			/* Select ANT connector if supported by card */
+			if (state->config->pll_rf_set)
+				state->config->pll_rf_set(fe, 1);
 			break;
 
 		case QAM_64:
@@ -222,6 +226,10 @@ static int lgdt3302_set_parameters(struct dvb_frontend* fe,
 
 			/* Select QAM_64 mode and serial MPEG interface */
 			top_ctrl_cfg[1] = 0x04;
+
+			/* Select CABLE connector if supported by card */
+			if (state->config->pll_rf_set)
+				state->config->pll_rf_set(fe, 0);
 			break;
 
 		case QAM_256:
@@ -229,6 +237,10 @@ static int lgdt3302_set_parameters(struct dvb_frontend* fe,
 
 			/* Select QAM_256 mode and serial MPEG interface */
 			top_ctrl_cfg[1] = 0x05;
+
+			/* Select CABLE connector if supported by card */
+			if (state->config->pll_rf_set)
+				state->config->pll_rf_set(fe, 0);
 			break;
 		default:
 			printk(KERN_WARNING "lgdt3302: %s: Modulation type(%d) UNSUPPORTED\n", __FUNCTION__, param->u.vsb.modulation);
