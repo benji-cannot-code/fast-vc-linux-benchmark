f4bba56b8f562a5aee11e639d33548e3288a6898
Michael Krufky <mkrufky@m1k.net>
2005-07-27 11:45:54 -0700

    [PATCH] dvb/v4l: lgdt3302: isolate tuner
    Remove the dvb_pll_desc from the frontend and replace with a
    pll_set-callback to isolate the tuner programming from the frontend.
    
    Signed-off-by: Mac Michaels <wmichaels1@earthlink.net>
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/dvb/frontends/lgdt3302.h
+++ drivers/media/dvb/frontends/lgdt3302.h
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: lgdt3302.h,v 1.2 2005/06/28 23:50:48 mkrufky Exp $
- *
  *    Support for LGDT3302 (DViCO FustionHDTV 3 Gold) - VSB/QAM
  *
  *    Copyright (C) 2005 Wilson Michaels <wilsonmichaels@earthlink.net>
@@ -31,8 +29,9 @@ struct lgdt3302_config
 {
 	/* The demodulator's i2c address */
 	u8 demod_address;
-	u8 pll_address;
-	struct dvb_pll_desc *pll_desc;
+
+	/* PLL interface */
+	int (*pll_set)(struct dvb_frontend* fe, struct dvb_frontend_parameters* params, u8* pll_address);
 
 	/* Need to set device param for start_dma */
 	int (*set_ts_params)(struct dvb_frontend* fe, int is_punctured);
