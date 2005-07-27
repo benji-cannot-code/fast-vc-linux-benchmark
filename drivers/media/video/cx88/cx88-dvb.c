94cb4a30868ecc88fefcab33716e96d6e5111673
Michael Krufky <mkrufky@m1k.net>
2005-07-27 11:45:58 -0700

    [PATCH] v4l: hybrid dvb: move #defines to Makefile
    This patch moves #define from cx88-dvb.c and saa7134-dvb.c into Makefile as
    CFLAGS, allowing code compatability with video4linux cvs.
    
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/cx88/cx88-dvb.c
+++ drivers/media/video/cx88/cx88-dvb.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: cx88-dvb.c,v 1.49 2005/07/20 05:38:09 mkrufky Exp $
+ * $Id: cx88-dvb.c,v 1.50 2005/07/23 10:08:00 mkrufky Exp $
  *
  * device driver for Conexant 2388x based TV cards
  * MPEG Transport Stream (DVB) routines
@@ -30,11 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/file.h>
 #include <linux/suspend.h>
+#include <linux/config.h>
 
-#define CONFIG_DVB_MT352 1
-#define CONFIG_DVB_CX22702 1
-#define CONFIG_DVB_OR51132 1
-#define CONFIG_DVB_LGDT3302 1
 
 #include "cx88.h"
 #include "dvb-pll.h"
