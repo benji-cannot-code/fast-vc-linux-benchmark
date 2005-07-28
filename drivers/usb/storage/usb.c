085fcb42032387eff0777d8e16e7c9dce16eab43
Matthew Dharm <mdharm-usb@one-eyed-alien.net>
2005-07-28 14:49:01 -0700

    [PATCH] USB Storage: add support for Maxtor One-Touch button
    This patch is originally from Nick Sillik, and has been rediffed against
    the latest tree.
    
    This patch adds usability to the OneTouch Button on Maxtor External USB
    Hard Drives. Using an unusual device entry it declares an extra init
    function which claims the interrupt endpoint associated with this
    button.  The button is connected to the input system.
    
    Signed-off-by: Nick Sillik <n.sillik@temple.edu>
    Signed-off-by: Matthew Dharm <mdharm-usb@one-eyed-alien.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/storage/usb.c
+++ drivers/usb/storage/usb.c
@@ -91,7 +91,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_USB_STORAGE_JUMPSHOT
 #include "jumpshot.h"
 #endif
-
+#ifdef CONFIG_USB_STORAGE_ONETOUCH
+#include "onetouch.h"
+#endif
 
 /* Some informational data */
 MODULE_AUTHOR("Matthew Dharm <mdharm-usb@one-eyed-alien.net>");
