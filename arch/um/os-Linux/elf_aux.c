76df8522f849d0b6def7d730774fd93410039b57
Jeff Dike <jdike@addtoit.com>
2005-07-29 14:03:33 -0700

    [PATCH] uml: fix vsyscall brokenness
    The #if/#ifdef cleanup exposed a bug in UML's ELF header processing.  With
    this bug fixed, UML recognizes the vsyscall info coming from the host.  On
    FC4, there is a vsyscall page low in the address space, which UML doesn't
    provide.  This causes an infinite page fault loop and a hang on boot.
    
    This patch works around that by making this look like a no-vsyscall system.
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/os-Linux/elf_aux.c
+++ arch/um/os-Linux/elf_aux.c
@@ -10,9 +10,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <elf.h>
 #include <stddef.h>
+#include <asm/elf.h>
 #include "init.h"
 #include "elf_user.h"
-#include <asm/elf.h>
+#include "mem_user.h"
 
 #if ELF_CLASS == ELFCLASS32
 typedef Elf32_auxv_t elf_auxv_t;
@@ -42,6 +43,9 @@ __init void scan_elf_aux( char **envp)
 				break;
 			case AT_SYSINFO_EHDR:
 				vsyscall_ehdr = auxv->a_un.a_val;
+				/* See if the page is under TASK_SIZE */
+				if (vsyscall_ehdr < (unsigned long) envp)
+					vsyscall_ehdr = 0;
 				break;
 			case AT_HWCAP:
 				elf_aux_hwcap = auxv->a_un.a_val;
