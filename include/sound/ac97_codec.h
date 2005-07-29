ee6c79782acb0dc6a6cbd5c40b196737b263f950
Liam Girdwood <liam.girdwood@wolfsonmicro.com>
2005-07-29 11:41:55 +0200

    [ALSA] WM97xx AC97 codec controls
    AC97 Codec
    o Enhanced current WM97xx support to provide additional controls and
      use the kcontrol suffix naming convention.
    o Added AC97_HAS_NO_MIC, AC97_HAS_NO_TONE and AC97_HAS_NO_STD_PCM.
    o Cleaned up WM97xx related comments.
    o Removed some wm9713 double mono controls and replaced with stereo
      controls.
    
    Signed-off-by: Liam Girdwood <liam.girdwood@wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/ac97_codec.h
+++ include/sound/ac97_codec.h
@@ -375,6 +375,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define AC97_HAS_NO_PC_BEEP	(1<<12) /* no PC Beep volume */
 #define AC97_HAS_NO_VIDEO	(1<<13) /* no Video volume */
 #define AC97_HAS_NO_CD		(1<<14) /* no CD volume */
+#define AC97_HAS_NO_MIC	(1<<15) /* no MIC volume */
+#define AC97_HAS_NO_TONE	(1<<16) /* no Tone volume */
+#define AC97_HAS_NO_STD_PCM	(1<<17)	/* no standard AC97 PCM volume and mute */
 
 /* rates indexes */
 #define AC97_RATES_FRONT_DAC	0
