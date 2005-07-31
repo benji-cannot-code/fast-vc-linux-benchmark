ab32275b2f1546a50e1ce423ed733e142779c2d2
Jean Delvare <khali@linux-fr.org>
2005-07-31 21:45:27 +0200

    [PATCH] hwmon: hwmon vs i2c, second round (05/11)
    The i2c_detect function has no more user, delete it.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/i2c-sensor.h
+++ include/linux/i2c-sensor.h
@@ -201,11 +201,4 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 				      NULL }; \
   SENSORS_INSMOD
 
-/* Detect function. It iterates over all possible addresses itself. For
-   SMBus addresses, it will only call found_proc if some client is connected
-   to the SMBus (unless a 'force' matched). */
-extern int i2c_detect(struct i2c_adapter *adapter,
-		      struct i2c_client_address_data *address_data,
-		      int (*found_proc) (struct i2c_adapter *, int, int));
-
 #endif				/* def _LINUX_I2C_SENSOR_H */
