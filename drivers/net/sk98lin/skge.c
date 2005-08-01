5cc3b81f0d633c8be7c46d5c4c8cd25ad5af4f44
Andrew Morton <akpm@osdl.org>
2005-07-31 22:34:40 -0700

    [PATCH] skge build fix
    Make it compile with CONFIG_PM=n
    
    Cc: "Rafael J. Wysocki" <rjw@sisk.pl>
    Cc: Jeff Garzik <jgarzik@pobox.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/net/sk98lin/skge.c
+++ drivers/net/sk98lin/skge.c
@@ -5207,6 +5207,9 @@ static int skge_resume(struct pci_dev *pdev)
 
 	return 0;
 }
+#else
+#define skge_suspend NULL
+#define skge_resume NULL
 #endif
 
 static struct pci_device_id skge_pci_tbl[] = {
