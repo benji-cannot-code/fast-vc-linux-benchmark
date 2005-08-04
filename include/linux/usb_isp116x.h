896c63b9b82ccf993113b672ecffb4f17bf81e45
Olav Kongas <ok@artecdesign.ee>
2005-08-04 16:54:08 +0300

    [PATCH] USB: isp116x-hcd: per-port overcurrent reporting
    This patch sets the isp116x to report overcurrent always per-port.
    
    Signed-off-by: Olav Kongas <ok@artecdesign.ee>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/usb_isp116x.h
+++ include/linux/usb_isp116x.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct isp116x_platform_data {
 	/* Enable internal resistors on downstream ports */
 	unsigned sel15Kres:1;
-	/* On-chip overcurrent protection */
+	/* On-chip overcurrent detection */
 	unsigned oc_enable:1;
 	/* INT output polarity */
 	unsigned int_act_high:1;
