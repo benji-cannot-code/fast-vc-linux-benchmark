0ba84cc65c70e4b77ebb67109cc1b163f85b651b
John McCutchan <ttb@tentacle.dhs.org>
2005-08-04 13:07:08 -0700

    [PATCH] inotify delete race fix
    The included patch fixes a problem where a inotify client would receive a
    delete event before the file was actually deleted.  The bug affects both
    dnotify & inotify.
    
    Signed-off-by: John McCutchan <ttb@tentacle.dhs.org>
    Signed-off-by: Robert Love <rml@novell.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/namei.c
+++ fs/namei.c
@@ -1802,8 +1802,8 @@ int vfs_rmdir(struct inode *dir, struct dentry *dentry)
 	}
 	up(&dentry->d_inode->i_sem);
 	if (!error) {
-		fsnotify_rmdir(dentry, dentry->d_inode, dir);
 		d_delete(dentry);
+		fsnotify_rmdir(dentry, dentry->d_inode, dir);
 	}
 	dput(dentry);
 
@@ -1875,8 +1875,14 @@ int vfs_unlink(struct inode *dir, struct dentry *dentry)
 
 	/* We don't d_delete() NFS sillyrenamed files--they still exist. */
 	if (!error && !(dentry->d_flags & DCACHE_NFSFS_RENAMED)) {
-		fsnotify_unlink(dentry, dir);
+#if defined(CONFIG_INOTIFY) || defined(CONFIG_DNOTIFY)
+		dget(dentry);
 		d_delete(dentry);
+		fsnotify_unlink(dentry, dir);
+		dput(dentry);
+#else
+		d_delete(dentry);
+#endif
 	}
 
 	return error;
