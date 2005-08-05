7823e1f25b5423eceab4f58dd481cc76deacbcbd
David Gibson <david@gibson.dropbear.id.au>
2005-08-05 19:39:06 +1000

    [PATCH] Four level pagetables for ppc64
    Implement 4-level pagetables for ppc64
    
    This patch implements full four-level page tables for ppc64, thereby
    extending the usable user address range to 44 bits (16T).
    
    The patch uses a full page for the tables at the bottom and top level,
    and a quarter page for the intermediate levels.  It uses full 64-bit
    pointers at every level, thus also increasing the addressable range of
    physical memory.  This patch also tweaks the VSID allocation to allow
    matching range for user addresses (this halves the number of available
    contexts) and adds some #if and BUILD_BUG sanity checks.
    
    Signed-off-by: David Gibson <dwg@au1.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-ppc64/imalloc.h
+++ include/asm-ppc64/imalloc.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define PHBS_IO_BASE  	  VMALLOC_END
 #define IMALLOC_BASE      (PHBS_IO_BASE + 0x80000000ul)	/* Reserve 2 gigs for PHBs */
-#define IMALLOC_END       (VMALLOC_START + EADDR_MASK)
+#define IMALLOC_END       (VMALLOC_START + PGTABLE_RANGE)
 
 
 /* imalloc region types */
