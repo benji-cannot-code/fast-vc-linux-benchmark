270fee0538313012e946008226422ee8808cd758
Arnaldo Carvalho de Melo <acme@mandriva.com>
2005-08-10 05:54:28 -0300

    [TCPDIAG]: Implement cheapest way of supporting DCCPDIAG_GETSOCK
    With ugly ifdefs, etc, but this actually:
    
    1. keeps the existing ABI, i.e. no need to recompile the iproute2
       utilities if not interested in DCCP.
    
    2. Provides all the tcp_diag functionality in DCCP, with just a
       small patch that makes iproute2 support DCCP.
    
    Of course I'll get this cleaned-up in time, but for now I think its
    OK to be this way to quickly get this functionality.
    
    iproute2-ss050808 patch at:
    
    http://vger.kernel.org/~acme/iproute2-ss050808.dccp.patch
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@mandriva.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/tcp_diag.h
+++ include/linux/tcp_diag.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Just some random number */
 #define TCPDIAG_GETSOCK 18
+#define DCCPDIAG_GETSOCK 19
 
 /* Socket identity */
 struct tcpdiag_sockid
