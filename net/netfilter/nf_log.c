4279aad81f2437fd1418dde62bf4f5c90e5a53ff
Harald Welte <laforge@netfilter.org>
2005-08-09 20:23:36 -0700

    [NETFILTER]: more verbose return codes from nf_{log,queue}
    This adds EEXIST to distinguish between the following return values:
    0: 	nobody was registered, registration successful
    EEXIST:	the exact same handler was already registered, no registration
    	required
    EBUSY:	somebody else is registered, registration unsuccessful.
    
    Signed-off-by: Harald Welte <laforge@netfilter.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/nf_log.c
+++ net/netfilter/nf_log.c
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct nf_logger *nf_logging[NPROTO]; /* = NULL */
 static DEFINE_SPINLOCK(nf_log_lock);
 
+/* return EBUSY if somebody else is registered, EEXIST if the same logger
+ * is registred, 0 on success. */
 int nf_log_register(int pf, struct nf_logger *logger)
 {
 	int ret = -EBUSY;
@@ -29,7 +31,9 @@ int nf_log_register(int pf, struct nf_logger *logger)
 	if (!nf_logging[pf]) {
 		rcu_assign_pointer(nf_logging[pf], logger);
 		ret = 0;
-	}
+	} else if (nf_logging[pf] == logger)
+		ret = -EEXIST;
+
 	spin_unlock(&nf_log_lock);
 	return ret;
 }		
