310e02ae626e7608c6c3d581da8fdf79b06a735b
Malli Chilakala <mallikarjuna.chilakala@intel.com>
2005-08-11 13:59:59 -0700

    [PATCH] ixgb: Driver version, white space, comments
    Driver version, white space, comments & added Module_version Patch from linville
    
    Signed-off-by: Mallikarjuna R Chilakala <mallikarjuna.chilakala@intel.com>
    Signed-off-by: Ganesh Venkatesan <ganesh.venkatesan@intel.com>
    Signed-off-by: John Ronciak <john.ronciak@intel.com>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/ixgb/ixgb_main.c
+++ drivers/net/ixgb/ixgb_main.c
@@ -30,6 +30,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ixgb.h"
 
 /* Change Log
+ * 1.0.96 04/19/05
+ * - Make needlessly global code static -- bunk@stusta.de
+ * - ethtool cleanup -- shemminger@osdl.org
+ * - Support for MODULE_VERSION -- linville@tuxdriver.com
+ * - add skb_header_cloned check to the tso path -- herbert@apana.org.au
  * 1.0.88 01/05/05
  * - include fix to the condition that determines when to quit NAPI - Robert Olsson
  * - use netif_poll_{disable/enable} to synchronize between NAPI and i/f up/down
@@ -48,10 +53,9 @@ char ixgb_driver_string[] = "Intel(R) PRO/10GbE Network Driver";
 #else
 #define DRIVERNAPI "-NAPI"
 #endif
-
-#define DRV_VERSION "1.0.95-k2"DRIVERNAPI
+#define DRV_VERSION		"1.0.100-k2"DRIVERNAPI
 char ixgb_driver_version[] = DRV_VERSION;
-char ixgb_copyright[] = "Copyright (c) 1999-2005 Intel Corporation.";
+static char ixgb_copyright[] = "Copyright (c) 1999-2005 Intel Corporation.";
 
 /* ixgb_pci_tbl - PCI Device ID Table
  *
