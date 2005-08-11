adfb10a2226ac6f044b0951c45a64a0e50c0be13
Evgeniy Polyakov <johnpol@2ka.mipt.ru>
2005-08-11 17:27:50 +0400

    [PATCH] w1: Detouching bug fixed.
    Signed-off-by: Evgeniy Polyakov <johnpol@2ka.mipt.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/w1/w1.h
+++ drivers/w1/w1.h
@@ -77,7 +77,7 @@ struct w1_slave
 	struct w1_master	*master;
 	struct w1_family	*family;
 	struct device		dev;
-	struct completion	dev_released;
+	struct completion	released;
 };
 
 typedef void (* w1_slave_found_callback)(unsigned long, u64);
@@ -177,7 +177,6 @@ struct w1_master
 
 	struct device_driver	*driver;
 	struct device		dev;
-	struct completion	dev_released;
 	struct completion	dev_exited;
 
 	struct w1_bus_master	*bus_master;
