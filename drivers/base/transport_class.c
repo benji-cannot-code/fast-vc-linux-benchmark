bae0cda682f6e4bdfb3ffca9fd7ed73066de63ea
James Bottomley <James.Bottomley@steeleye.com>
2005-08-15 16:13:19 -0500

    [SCSI] fix transport class corner case after rework
    If your transport class sets the ATTRIBUTE_CONTAINER_NO_CLASSDEVS flag,
    then its configure method never gets called.  This patch fixes that so
    that the configure method is called with a NULL classdev.
    
    Also remove a spurious inverted comma in the transport_class comments.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/base/transport_class.c
+++ drivers/base/transport_class.c
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file is licensed under GPLv2
  *
  * The basic idea here is to allow any "device controller" (which
- * would most often be a Host Bus Adapter" to use the services of one
+ * would most often be a Host Bus Adapter to use the services of one
  * or more tranport classes for performing transport specific
  * services.  Transport specific services are things that the generic
  * command layer doesn't want to know about (speed settings, line
