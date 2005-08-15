9273ba7371fc4dad2c1c5c0faca4da8a7493e99b
Jean Delvare <khali@linux-fr.org>
2005-08-15 19:57:04 +0200

    [PATCH] I2C: Drop I2C_DEVNAME and i2c_clientname
    I2C_DEVNAME and i2c_clientname were introduced in 2.5.68 [1] to help
    media/video driver authors who wanted their code to be compatible with
    both Linux 2.4 and 2.6. The cause of the incompatibility has gone since
    [2], so I think we can get rid of them, as they tend to make the code
    harder to read and longer to preprocess/compile for no more benefit.
    
    I'd hope nobody seriously attempts to keep media/video driver compatible
    across Linux trees anymore, BTW.
    
    [1] http://marc.theaimsgroup.com/?l=linux-kernel&m=104930186524598&w=2
    [2] http://www.linuxhq.com/kernel/v2.6/0-test3/include/linux/i2c.h
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/media/video/bttv-i2c.c
+++ drivers/media/video/bttv-i2c.c
@@ -110,7 +110,7 @@ static struct i2c_adapter bttv_i2c_adap_sw_template = {
 #ifdef I2C_CLASS_TV_ANALOG
 	.class             = I2C_CLASS_TV_ANALOG,
 #endif
-	I2C_DEVNAME("bt848"),
+	.name              = "bt848",
 	.id                = I2C_HW_B_BT848,
 	.client_register   = attach_inform,
 };
@@ -281,7 +281,7 @@ static struct i2c_adapter bttv_i2c_adap_hw_template = {
 #ifdef I2C_CLASS_TV_ANALOG
 	.class         = I2C_CLASS_TV_ANALOG,
 #endif
-	I2C_DEVNAME("bt878"),
+	.name          = "bt878",
 	.id            = I2C_HW_B_BT848 /* FIXME */,
 	.algo          = &bttv_algo,
 	.client_register = attach_inform,
@@ -297,7 +297,7 @@ static int attach_inform(struct i2c_client *client)
 	if (bttv_debug)
 		printk(KERN_DEBUG "bttv%d: %s i2c attach [addr=0x%x,client=%s]\n",
 			btv->c.nr,client->driver->name,client->addr,
-			i2c_clientname(client));
+			client->name);
 	if (!client->driver->command)
 		return 0;
 
@@ -325,7 +325,7 @@ void bttv_call_i2c_clients(struct bttv *btv, unsigned int cmd, void *arg)
 }
 
 static struct i2c_client bttv_i2c_client_template = {
-	I2C_DEVNAME("bttv internal"),
+	.name	= "bttv internal",
 };
 
 
--- drivers/media/video/msp3400.c
+++ drivers/media/video/msp3400.c
@@ -1438,7 +1438,7 @@ static struct i2c_driver driver = {
 
 static struct i2c_client client_template =
 {
-	I2C_DEVNAME("(unset)"),
+	.name      = "(unset)",
 	.flags     = I2C_CLIENT_ALLOW_USE,
         .driver    = &driver,
 };
@@ -1510,7 +1510,7 @@ static int msp_attach(struct i2c_adapter *adap, int addr, int kind)
 	}
 
 	/* hello world :-) */
-	printk(KERN_INFO "msp34xx: init: chip=%s",i2c_clientname(c));
+	printk(KERN_INFO "msp34xx: init: chip=%s", c->name);
 	if (HAVE_NICAM(msp))
 		printk(" +nicam");
 	if (HAVE_SIMPLE(msp))
