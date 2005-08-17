3798ab7e16aa4592c96c6eb22bfcfc91519bf2d5
David Woodhouse <dwmw2@shinybook.infradead.org>
2005-08-17 14:45:55 +0100

    AUDIT: Fix task refcount leak in audit_filter_syscall()
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- kernel/auditsc.c
+++ kernel/auditsc.c
@@ -1008,7 +1008,7 @@ void audit_syscall_exit(struct task_struct *tsk, int valid, long return_code)
 	/* Not having a context here is ok, since the parent may have
 	 * called __put_task_struct. */
 	if (likely(!context))
-		return;
+		goto out;
 
 	if (context->in_syscall && context->auditable)
 		audit_log_exit(context, GFP_KERNEL);
@@ -1027,6 +1027,7 @@ void audit_syscall_exit(struct task_struct *tsk, int valid, long return_code)
 		audit_zero_context(context, context->state);
 		tsk->audit_context = context;
 	}
+ out:
 	put_task_struct(tsk);
 }
 
