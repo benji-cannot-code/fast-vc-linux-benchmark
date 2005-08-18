d5388824ae05ea9b328fbc93f8fadec95a9a7462
Robert Love <rlove@rlove.org>
2005-08-18 11:24:14 -0700

    [PATCH] SH64: inotify and ioprio syscalls
    Add inotify and ioprio syscall stubs to SH64.
    
    Signed-off-by: Robert Love <rml@novell.com>
    Acked-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-sh64/unistd.h
+++ include/asm-sh64/unistd.h
@@ -339,8 +339,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_add_key		313
 #define __NR_request_key	314
 #define __NR_keyctl		315
+#define __NR_ioprio_set		316
+#define __NR_ioprio_get		317
+#define __NR_inotify_init	318
+#define __NR_inotify_add_watch	319
+#define __NR_inotify_rm_watch	320
 
-#define NR_syscalls 316
+#define NR_syscalls 321
 
 /* user-visible error numbers are in the range -1 - -125: see <asm-sh64/errno.h> */
 
