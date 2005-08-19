cc09e1fa3ed9305d2bea7e56650547d3f7c99cf6
Roland Dreier <roland@eddore.topspincom.com>
2005-08-19 10:59:31 -0700

    [PATCH] IB/mthca: Add SRQ implementation
    Add mthca support for shared receive queues (SRQs),
    including userspace SRQs.
    
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/mthca/mthca_profile.h
+++ drivers/infiniband/hw/mthca/mthca_profile.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct mthca_profile {
 	int num_qp;
 	int rdb_per_qp;
+	int num_srq;
 	int num_cq;
 	int num_mcg;
 	int num_mpt;
