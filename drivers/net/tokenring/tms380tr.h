d2f034f59e00051cc7d07e76226cc162998d6c06
Jochen Friedrich <jochen@scram.de>
2005-08-19 21:05:56 -0400

    [netdrvr] Convert madgemc to new MCA API.
    Now that all tms380 devices have a valid
    struct device with dma_mask, remove dmalimit from tmsdev_init().
    
    Kconfig: depend tms380tr and madgemc on MCA.
    abyss.c, proteon.c, skisa.c, tmspci.c, tms380tr.h:
      remove dmalimit parameter from tmsdev_init().
    tms380tr.c: use device->dma_mask instead of dmalimit.
    madgemc.c: move to new MCA API using struct device.
    
    Signed-off-by: Jochen Friedrich <jochen@scram.de>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/tokenring/tms380tr.h
+++ drivers/net/tokenring/tms380tr.h
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int tms380tr_open(struct net_device *dev);
 int tms380tr_close(struct net_device *dev);
 irqreturn_t tms380tr_interrupt(int irq, void *dev_id, struct pt_regs *regs);
-int tmsdev_init(struct net_device *dev, unsigned long dmalimit,
-		struct device *pdev);
+int tmsdev_init(struct net_device *dev, struct device *pdev);
 void tmsdev_term(struct net_device *dev);
 void tms380tr_wait(unsigned long time);
 
