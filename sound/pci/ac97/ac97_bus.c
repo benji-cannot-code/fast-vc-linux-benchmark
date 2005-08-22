290f921864d74f2a2d58bca6896415f9f077ef00
Jaroslav Kysela <perex@suse.cz>
2005-08-22 11:30:37 +0200

    [ALSA] ac97_bus - replace <linux/module.h> with <sound/driver.h> for consistency
    AC97 Codec
    
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/ac97/ac97_bus.c
+++ sound/pci/ac97/ac97_bus.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * (at your option) any later version.
  */
 
-#include <linux/module.h>
+#include <sound/driver.h>
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/string.h>
