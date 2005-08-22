0696fce2659ae2beb9a710dc15925dc12f0501d1
Greg Edwards <edwardsg@sgi.com>
2005-08-22 09:57:00 -0700

    [IA64] clean up sn2 region definitions
    Clean up some duplicate region definitions in sn2 code.
    
    Signed-off-by: Greg Edwards <edwardsg@sgi.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/asm-ia64/page.h
+++ include/asm-ia64/page.h
@@ -18,9 +18,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Different regions are assigned to different purposes.
  */
 #define RGN_SHIFT	(61)
-#define RGN_BASE(r)  (__IA64_UL_CONST(r)<<RGN_SHIFT)
+#define RGN_BASE(r)	(__IA64_UL_CONST(r)<<RGN_SHIFT)
+#define RGN_BITS	(RGN_BASE(-1))
 
-#define KHIGH -1	/* high three bits of Kernel virtual address */
 #define RGN_KERNEL	7	/* Identity mapped region */
 #define RGN_UNCACHED    6	/* Identity mapped I/O region */
 #define RGN_GATE	5	/* Gate page, Kernel text, etc */
