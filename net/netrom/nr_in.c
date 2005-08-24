8301554f8eaeaa4d6fd51054e3a8b1628fbf0745
Ralf Baechle <ralf@linux-mips.org>
2005-08-24 11:37:45 -0700

    [AX25/NETROM/ROSE]: Kill net/ip.h inclusion
    All these are claiming to include <net/ip.h> to get ip_rcv() but in
    fact don't need the header at all, so away with the inclusion.
    
    Signed-off-by: Ralf Baechle DL5RB <ralf@linux-mips.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netrom/nr_in.c
+++ net/netrom/nr_in.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/skbuff.h>
 #include <net/sock.h>
 #include <net/tcp_states.h>
-#include <net/ip.h>			/* For ip_rcv */
 #include <asm/uaccess.h>
 #include <asm/system.h>
 #include <linux/fcntl.h>
--- net/rose/rose_in.c
+++ net/rose/rose_in.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/netdevice.h>
 #include <linux/skbuff.h>
 #include <net/sock.h>
-#include <net/ip.h>			/* For ip_rcv */
 #include <net/tcp_states.h>
 #include <asm/system.h>
 #include <linux/fcntl.h>
