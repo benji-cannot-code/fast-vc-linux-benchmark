ebf372f9bd9ff203238efc4fb0cb80f5cad94b0c
Steve French <sfrench@us.ibm.com>
2005-08-24 20:25:21 -0700

    [CIFS] Use file instead of dentry in cifs dir_notify struct
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -403,7 +403,7 @@ struct dir_notify_req {
        __u16 netfid;
        __u32 filter; /* CompletionFilter (for multishot) */
        int multishot;
-       struct file * dentry;
+       struct file * pfile;
 };
 
 #define   MID_FREE 0
