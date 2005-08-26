98621ed4be1142d2f6448c58c068fa3dd05c483a
Mike Kershaw <dragorn@kismetwireless.net>
2005-08-26 00:41:54 -0500

    Adds radiotap support to ipw2200 in monitor mode..
    Signed-off-by: Mike Kershaw <dragorn@kismetwireless.net>
    Signed-off-by: James Ketrenos <jketreno@linux.intel.com>

--- drivers/net/wireless/ipw2200.h
+++ drivers/net/wireless/ipw2200.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 
 #include <net/ieee80211.h>
+#include <net/ieee80211_radiotap.h>
 
 #define DRV_NAME	"ipw2200"
 
