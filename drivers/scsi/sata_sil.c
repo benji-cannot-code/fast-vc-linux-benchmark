d6d4bd38c5f236a6e3b6f671e03aa0322158903d
Jeff Garzik <jgarzik@pobox.com>
2005-08-26 19:46:24 -0400

    [libata sata_sil] list documentation URL, since its public

--- drivers/scsi/sata_sil.c
+++ drivers/scsi/sata_sil.c
@@ -25,6 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  If you do not delete the provisions above, a recipient may use your
  *  version of this file under either the OSL or the GPL.
  *
+ *  Documentation for SiI 3112:
+ *  http://gkernel.sourceforge.net/specs/sii/3112A_SiI-DS-0095-B2.pdf.bz2
+ *
+ *  Other errata and documentation available under NDA.
+ *
  */
 
 #include <linux/kernel.h>
