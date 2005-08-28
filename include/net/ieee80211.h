ba44249f4bc409bd1912bcdcb92159b374075007
Jouni Malinen <jkmaline@cc.hut.fi>
2005-08-28 10:51:32 -0700

    [PATCH] ieee80211: Remove WIRELESS_EXT < 17 support
    No need to maintain support for WIRELESS_EXT < 17 since this kernel
    tree is already using WIRELESS_EXT 18.
    
    Signed-off-by: Jouni Malinen <jkmaline@cc.hut.fi>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- include/net/ieee80211.h
+++ include/net/ieee80211.h
@@ -25,15 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>   /* ARRAY_SIZE */
 #include <linux/wireless.h>
 
-#if WIRELESS_EXT < 17
-#define IW_QUAL_QUAL_INVALID   0x10
-#define IW_QUAL_LEVEL_INVALID  0x20
-#define IW_QUAL_NOISE_INVALID  0x40
-#define IW_QUAL_QUAL_UPDATED   0x1
-#define IW_QUAL_LEVEL_UPDATED  0x2
-#define IW_QUAL_NOISE_UPDATED  0x4
-#endif
-
 #define IEEE80211_DATA_LEN		2304
 /* Maximum size for the MA-UNITDATA primitive, 802.11 standard section
    6.2.1.1.2.
