102415a6e3aac74144d84910c5984866fee72e21
Jouni Malinen <jkmaline@cc.hut.fi>
2005-08-28 10:51:35 -0700

    [PATCH] hostap: Update version
    Version 0.4.4 of Host AP driver was released, so let's sync the version
    number in netdev-2.6 tree.
    
    Signed-off-by: Jouni Malinen <jkmaline@cc.hut.fi>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/wireless/hostap/hostap_config.h
+++ drivers/net/wireless/hostap/hostap_config.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef HOSTAP_CONFIG_H
 #define HOSTAP_CONFIG_H
 
-#define PRISM2_VERSION "0.4.1-kernel"
+#define PRISM2_VERSION "0.4.4-kernel"
 
 /* In the previous versions of Host AP driver, support for user space version
  * of IEEE 802.11 management (hostapd) used to be disabled in the default
