97a9213b156b69ae0b4f56f6380d34258935515f
David Brownell <david-b@pacbell.net>
2005-08-31 09:54:36 -0700

    [PATCH] USB: usbnet (8/9) module for RNDIS devices
    This adds host-side RNDIS support to the "usbnet" driver, so Linux can talk
    to various devices (often based on WinCE) that otherwise only Windows could
    talk to.
    
    Tested with little-endian Linux talking to a Linux-USB Ethernet/RNDIS based
    peripheral.  This also includes updates from Eddie C. Dost <ecd@brainaid.de>
    for big-endian SPARC Linux talking to a Nokia 9500 Communicator.
    
    It's still marked as EXPERIMENTAL because this code is so young.  This
    ought to let Linux to work with various cable modems that previously
    would have been "Windows Only".
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/net/usbnet.c
+++ drivers/usb/net/usbnet.c
@@ -104,6 +104,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 		disconnect; other cleanups. (db)  Flush net1080 fifos
  * 		after several sequential framing errors. (Johannes Erdfelt)
  * 22-aug-2003	AX8817X support (Dave Hollis).
+ *
  * 14-jun-2004  Trivial patch for AX8817X based Buffalo LUA-U2-KTX in Japan
  *		(Neil Bortnak)
  * 03-nov-2004	Trivial patch for KC2190 (KC-190) chip. (Jonathan McDowell)
@@ -1374,14 +1375,6 @@ static const struct usb_device_id	products [] = {
 }, {
 	USB_DEVICE (0x067b, 0x0001),	// PL-2302
 	.driver_info =	(unsigned long) &prolific_info,
-},
-#endif
-
-#ifdef	CONFIG_USB_RNDIS
-{
-	/* RNDIS is MSFT's un-official variant of CDC ACM */
-	USB_INTERFACE_INFO (USB_CLASS_COMM, 2 /* ACM */, 0x0ff),
-	.driver_info = (unsigned long) &rndis_info,
 },
 #endif
 	{ },		// END
