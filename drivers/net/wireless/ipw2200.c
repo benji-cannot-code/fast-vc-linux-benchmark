636de3c92fa2022daa1e82d9c382e370598d74f5
Hong Liu <hong.liu@intel.com>
2005-08-31 18:14:27 +0800

    Card with WEP enabled and using shared-key auth will have firmware error when it tries to auth to a WPA ap. The patch filters out WPA networks if the card is not wpa enabled when selecting network to associate to.
    Signed-off-by: Hong Liu <hong.liu@intel.com>

--- drivers/net/wireless/ipw2200.c
+++ drivers/net/wireless/ipw2200.c
@@ -5511,6 +5511,15 @@ static int ipw_best_network(struct ipw_priv *priv,
 		return 0;
 	}
 
+	if (!priv->ieee->wpa_enabled && (network->wpa_ie_len > 0 ||
+					 network->rsn_ie_len > 0)) {
+		IPW_DEBUG_ASSOC("Network '%s (" MAC_FMT ")' excluded "
+				"because of WPA capability mismatch.\n",
+				escape_essid(network->ssid, network->ssid_len),
+				MAC_ARG(network->bssid));
+		return 0;
+	}
+
 	if ((priv->config & CFG_STATIC_BSSID) &&
 	    memcmp(network->bssid, priv->bssid, ETH_ALEN)) {
 		IPW_DEBUG_ASSOC("Network '%s (" MAC_FMT ")' excluded "
@@ -6229,6 +6238,7 @@ static int ipw_wpa_enable(struct ipw_priv *priv, int value)
 {
 	/* This is called when wpa_supplicant loads and closes the driver
 	 * interface. */
+	priv->ieee->wpa_enabled = value;
 	return 0;
 }
 
