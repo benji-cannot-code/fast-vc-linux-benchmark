885f8d9150f0687d7f8b5f5104a6e864b0634b05
Thiemo Seufer <ths@networkno.de>
2005-09-01 09:02:08 +0000

    Define some more common ip22 CPU features.
    Signed-off-by: Thiemo Seufer <ths@networkno.de>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-ip22/cpu-feature-overrides.h
+++ include/asm-mips/mach-ip22/cpu-feature-overrides.h
@@ -12,6 +12,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * IP22 with a variety of processors so we can't use defaults for everything.
  */
+#define cpu_has_tlb		1
+#define cpu_has_4kex		1
+#define cpu_has_4ktlb		1
+#define cpu_has_fpu		1
+#define cpu_has_32fpr		1
+#define cpu_has_counter		1
 #define cpu_has_mips16		0
 #define cpu_has_divec		0
 #define cpu_has_cache_cdex_p	1
