775eb5380be79d6263578a7c583211f3f9e024ed
Christoph Hellwig <hch@sgi.com>
2005-09-02 16:56:14 +1000

    [XFS] remove unessecary vnode flags
    SGI-PV: 934766
    SGI-Modid: xfs-linux:xfs-kern:196852a
    
    Signed-off-by: Christoph Hellwig <hch@sgi.com>
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/linux-2.6/xfs_vnode.c
+++ fs/xfs/linux-2.6/xfs_vnode.c
@@ -79,10 +79,6 @@ vn_reclaim(
 	}
 	ASSERT(vp->v_fbhv == NULL);
 
-	VN_LOCK(vp);
-	vp->v_flag &= (VRECLM|VWAIT);
-	VN_UNLOCK(vp, 0);
-
 	vp->v_fbhv = NULL;
 
 #ifdef XFS_VNODE_TRACE
@@ -90,31 +86,6 @@ vn_reclaim(
 	vp->v_trace = NULL;
 #endif
 
-	return 0;
-}
-
-STATIC void
-vn_wakeup(
-	struct vnode	*vp)
-{
-	VN_LOCK(vp);
-	if (vp->v_flag & VWAIT)
-		sv_broadcast(vptosync(vp));
-	vp->v_flag &= ~(VRECLM|VWAIT|VMODIFIED);
-	VN_UNLOCK(vp, 0);
-}
-
-int
-vn_wait(
-	struct vnode	*vp)
-{
-	VN_LOCK(vp);
-	if (vp->v_flag & (VINACT | VRECLM)) {
-		vp->v_flag |= VWAIT;
-		sv_wait(vptosync(vp), PINOD, &vp->v_lock, 0);
-		return 1;
-	}
-	VN_UNLOCK(vp, 0);
 	return 0;
 }
 
@@ -222,7 +193,6 @@ vn_purge(
 {
 	vn_trace_entry(vp, "vn_purge", (inst_t *)__return_address);
 
-again:
 	/*
 	 * Check whether vp has already been reclaimed since our caller
 	 * sampled its version while holding a filesystem cache lock that
@@ -235,19 +205,6 @@ again:
 	}
 
 	/*
-	 * If vp is being reclaimed or inactivated, wait until it is inert,
-	 * then proceed.  Can't assume that vnode is actually reclaimed
-	 * just because the reclaimed flag is asserted -- a vn_alloc
-	 * reclaim can fail.
-	 */
-	if (vp->v_flag & (VINACT | VRECLM)) {
-		ASSERT(vn_count(vp) == 0);
-		vp->v_flag |= VWAIT;
-		sv_wait(vptosync(vp), PINOD, &vp->v_lock, 0);
-		goto again;
-	}
-
-	/*
 	 * Another process could have raced in and gotten this vnode...
 	 */
 	if (vn_count(vp) > 0) {
@@ -256,7 +213,6 @@ again:
 	}
 
 	XFS_STATS_DEC(vn_active);
-	vp->v_flag |= VRECLM;
 	VN_UNLOCK(vp, 0);
 
 	/*
@@ -267,11 +223,6 @@ again:
 	 */
 	if (vn_reclaim(vp) != 0)
 		panic("vn_purge: cannot reclaim");
-
-	/*
-	 * Wakeup anyone waiting for vp to be reclaimed.
-	 */
-	vn_wakeup(vp);
 }
 
 /*
@@ -316,11 +267,6 @@ vn_rele(
 	 * return.
 	 */
 	if (!vcnt) {
-		/*
-		 * As soon as we turn this on, noone can find us in vn_get
-		 * until we turn off VINACT or VRECLM
-		 */
-		vp->v_flag |= VINACT;
 		VN_UNLOCK(vp, 0);
 
 		/*
@@ -331,10 +277,7 @@ vn_rele(
 			VOP_INACTIVE(vp, NULL, cache);
 
 		VN_LOCK(vp);
-		if (vp->v_flag & VWAIT)
-			sv_broadcast(vptosync(vp));
-
-		vp->v_flag &= ~(VINACT|VWAIT|VRECLM|VMODIFIED);
+		vp->v_flag &= ~VMODIFIED;
 	}
 
 	VN_UNLOCK(vp, 0);
