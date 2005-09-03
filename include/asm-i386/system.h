7ac35a07bd68fb8c084d5f400bf9e683e82a5a29
Zachary Amsden <zach@vmware.com>
2005-09-03 15:56:44 -0700

    [PATCH] x86: make IOPL explicit
    The pushf/popf in switch_to are ONLY used to switch IOPL.  Making this
    explicit in C code is more clear.  This pushf/popf pair was added as a
    bugfix for leaking IOPL to unprivileged processes when using
    sysenter/sysexit based system calls (sysexit does not restore flags).
    
    When requesting an IOPL change in sys_iopl(), it is just as easy to change
    the current flags and the flags in the stack image (in case an IRET is
    required), but there is no reason to force an IRET if we came in from the
    SYSENTER path.
    
    This change is the minimal solution for supporting a paravirtualized Linux
    kernel that allows user processes to run with I/O privilege.  Other
    solutions require radical rewrites of part of the low level fault / system
    call handling code, or do not fully support sysenter based system calls.
    
    Unfortunately, this added one field to the thread_struct.  But as a bonus,
    on P4, the fastest time measured for switch_to() went from 312 to 260
    cycles, a win of about 17% in the fast case through this performance
    critical path.
    
    Signed-off-by: Zachary Amsden <zach@vmware.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/system.h
+++ include/asm-i386/system.h
@@ -15,8 +15,7 @@ extern struct task_struct * FASTCALL(__switch_to(struct task_struct *prev, struc
 
 #define switch_to(prev,next,last) do {					\
 	unsigned long esi,edi;						\
-	asm volatile("pushfl\n\t"					\
-		     "pushl %%ebp\n\t"					\
+	asm volatile("pushl %%ebp\n\t"					\
 		     "movl %%esp,%0\n\t"	/* save ESP */		\
 		     "movl %5,%%esp\n\t"	/* restore ESP */	\
 		     "movl $1f,%1\n\t"		/* save EIP */		\
@@ -24,7 +23,6 @@ extern struct task_struct * FASTCALL(__switch_to(struct task_struct *prev, struc
 		     "jmp __switch_to\n"				\
 		     "1:\t"						\
 		     "popl %%ebp\n\t"					\
-		     "popfl"						\
 		     :"=m" (prev->thread.esp),"=m" (prev->thread.eip),	\
 		      "=a" (last),"=S" (esi),"=D" (edi)			\
 		     :"m" (next->thread.esp),"m" (next->thread.eip),	\
