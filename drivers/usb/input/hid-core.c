806d890e7a092c85ea2fd980738ad3a83617a5a4
Vojtech Pavlik <vojtech@suse.cz>
2005-09-05 00:13:03 -0500

    Input: HID - fix URB success status handling
    Add a missing break; statement to the URB status handling
    in hid-core.c, avoiding flushing the request queue on success.
    
    Signed-off-by: Vojtech Pavlik <vojtech@suse.cz>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/usb/input/hid-core.c
+++ drivers/usb/input/hid-core.c
@@ -1098,6 +1098,7 @@ static void hid_irq_out(struct urb *urb, struct pt_regs *regs)
 
 	switch (urb->status) {
 		case 0:			/* success */
+			break;
 		case -ESHUTDOWN:	/* unplug */
 		case -EILSEQ:		/* unplug timeout on uhci */
 			unplug = 1;
@@ -1145,6 +1146,7 @@ static void hid_ctrl(struct urb *urb, struct pt_regs *regs)
 		case 0:			/* success */
 			if (hid->ctrl[hid->ctrltail].dir == USB_DIR_IN)
 				hid_input_report(hid->ctrl[hid->ctrltail].report->type, urb, 0, regs);
+			break;
 		case -ESHUTDOWN:	/* unplug */
 		case -EILSEQ:		/* unplug timectrl on uhci */
 			unplug = 1;
