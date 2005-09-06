0281e6600d9b5684239d4f7a0f0c17e1dcf10c4d
Milton Miller <miltonm@bga.com>
2005-09-06 11:50:55 +1000

    [PATCH] ppc64: Add missing include
    inline pmac_call_feature references ppc_md so include asm/machdep.h
    in asm/pmac_feature.h
    
    Signed-off-by: Milton Miller <miltonm@bga.com>
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-ppc/pmac_feature.h
+++ include/asm-ppc/pmac_feature.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __PPC_ASM_PMAC_FEATURE_H
 
 #include <asm/macio.h>
+#include <asm/machdep.h>
 
 /*
  * Known Mac motherboard models
