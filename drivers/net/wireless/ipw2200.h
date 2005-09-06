9378015af69d0f29da1c3408eb696893a3df3a59
David S. Miller <davem@sunset.davemloft.net>
2005-09-05 23:08:01 -0700

    [IPW2200]: ipw2200.h needs linux/dma-mapping.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/wireless/ipw2200.h
+++ drivers/net/wireless/ipw2200.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/firmware.h>
 #include <linux/wireless.h>
+#include <linux/dma-mapping.h>
 #include <asm/io.h>
 
 #include <net/ieee80211.h>
