ffe8ba6448996ffa9ae3aee7071cf1584ee1edca
Corey Minyard <minyard@acm.org>
2005-09-06 15:18:40 -0700

    [PATCH] ipmi: allow userland to include ipmi.h
    The IPMI driver include file needs to include compiler.h so it has definitions
    for __user and such.
    
    Signed-off-by: Corey Minyard <minyard@acm.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/ipmi.h
+++ include/linux/ipmi.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __LINUX_IPMI_H
 
 #include <linux/ipmi_msgdefs.h>
+#include <linux/compiler.h>
 
 /*
  * This file describes an interface to an IPMI driver.  You have to
