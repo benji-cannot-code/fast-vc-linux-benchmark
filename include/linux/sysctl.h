a8d4b70ac96a7bd0f07ffb4d0e52772f238aa652
Corey Minyard <minyard@acm.org>
2005-09-06 15:18:46 -0700

    [PATCH] ipmi poweroff: fix chassis control
    The IPMI power control function proc_write_chassctrl was badly written, it
    directly used userspace pointers, it assumed that strings were NULL
    terminated, and it used the evil sscanf function.  This converts over to
    using the sysctl interface for this data and changes the semantics to be a
    little more logical.
    
    Signed-off-by: Corey Minyard <minyard@acm.org>
    Cc: <viro@parcelfarce.linux.theplanet.co.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -712,6 +712,7 @@ enum {
 	DEV_RAID=4,
 	DEV_MAC_HID=5,
 	DEV_SCSI=6,
+	DEV_IPMI=7,
 };
 
 /* /proc/sys/dev/cdrom */
@@ -775,6 +776,11 @@ enum {
 /* /proc/sys/dev/scsi */
 enum {
 	DEV_SCSI_LOGGING_LEVEL=1,
+};
+
+/* /proc/sys/dev/ipmi */
+enum {
+	DEV_IPMI_POWEROFF_POWERCYCLE=1,
 };
 
 /* /proc/sys/abi */
