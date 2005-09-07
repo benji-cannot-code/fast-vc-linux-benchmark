8e6b45e7ba2125e1ca5d8a281b140df727d42b29
Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
2005-09-07 14:06:25 +0900

    [IA64] Minor cleanups - remove CONFIG_ACPI_DEALLOCATE_IRQ
    The config option 'CONFIG_ACPI_DEALLOCATE_IRQ' is no longer
    needed. This patch removes it.
    
    Signed-off-by: Kenji Kaneshige <kaneshige.kenji@jp.fujitsu.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/iosapic.c
+++ arch/ia64/kernel/iosapic.c
@@ -777,7 +777,6 @@ again:
 	return vector;
 }
 
-#ifdef CONFIG_ACPI_DEALLOCATE_IRQ
 void
 iosapic_unregister_intr (unsigned int gsi)
 {
@@ -860,7 +859,6 @@ iosapic_unregister_intr (unsigned int gsi)
 	spin_unlock(&iosapic_lock);
 	spin_unlock_irqrestore(&idesc->lock, flags);
 }
-#endif /* CONFIG_ACPI_DEALLOCATE_IRQ */
 
 /*
  * ACPI calls this when it finds an entry for a platform interrupt.
