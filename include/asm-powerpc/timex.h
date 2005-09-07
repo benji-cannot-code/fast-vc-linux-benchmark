1c23c43ddec2d64f94a647c97881cee73e5e8ebb
jdl@freescale.com <jdl@freescale.com>
2005-09-07 15:59:48 -0500

    [PATCH] powerpc: Merge a few more include files
    Merge a few asm-ppc and asm-ppc64 header files.
    Note: the merge of setup.h intentionally does not carry
    forward the m68k cruft.  That means this patch continues
    to break the already broken amiga on the ppc32.
    
    Signed-off-by: Jon Loeliger <jdl@freescale.com>
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/timex.h
+++ include/asm-powerpc/timex.h
@@ -1,12 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*
- * include/asm-ppc/timex.h
- *
- * ppc architecture timex specifications
- */
+#ifndef _ASM_POWERPC_TIMEX_H
+#define _ASM_POWERPC_TIMEX_H
+
 #ifdef __KERNEL__
-#ifndef _ASMppc_TIMEX_H
-#define _ASMppc_TIMEX_H
+
+/*
+ * PowerPC architecture timex specifications
+ */
 
 #include <linux/config.h>
 #include <asm/cputable.h>
@@ -15,14 +15,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 typedef unsigned long cycles_t;
 
-/*
- * For the "cycle" counter we use the timebase lower half.
- * Currently only used on SMP.
- */
-
 static inline cycles_t get_cycles(void)
 {
-	cycles_t ret = 0;
+	cycles_t ret;
+
+#ifdef __powerpc64__
+
+	__asm__ __volatile__("mftb %0" : "=r" (ret) : );
+
+#else
+	/*
+	 * For the "cycle" counter we use the timebase lower half.
+	 * Currently only used on SMP.
+	 */
+
+	ret = 0;
 
 	__asm__ __volatile__(
 		"98:	mftb %0\n"
@@ -34,8 +41,10 @@ static inline cycles_t get_cycles(void)
 		"	.long 99b\n"
 		".previous"
 		: "=r" (ret) : "i" (CPU_FTR_601));
+#endif
+
 	return ret;
 }
 
-#endif
-#endif /* __KERNEL__ */
+#endif	/* __KERNEL__ */
+#endif	/* _ASM_POWERPC_TIMEX_H */
