7e6c8d5392c25536a58f24ce36d4dee8e0c87fef
Patrick McHardy <kaber@trash.net>
2005-09-06 19:47:50 -0700

    [NET]: proto_unregister: fix sleeping while atomic
    proto_unregister holds a lock while calling kmem_cache_destroy, which
    can sleep.
    
    Noticed by Daniele Orlandi <daniele@orlandi.com>.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1530,6 +1530,8 @@ EXPORT_SYMBOL(proto_register);
 void proto_unregister(struct proto *prot)
 {
 	write_lock(&proto_list_lock);
+	list_del(&prot->node);
+	write_unlock(&proto_list_lock);
 
 	if (prot->slab != NULL) {
 		kmem_cache_destroy(prot->slab);
@@ -1551,9 +1553,6 @@ void proto_unregister(struct proto *prot)
 		kfree(name);
 		prot->twsk_slab = NULL;
 	}
-
-	list_del(&prot->node);
-	write_unlock(&proto_list_lock);
 }
 
 EXPORT_SYMBOL(proto_unregister);
