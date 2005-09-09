f9d0131e6fbea3715c8f7df1de7fe5c94f814e90
Mauro Carvalho Chehab <mchehab@brturbo.com.br>
2005-09-09 13:04:05 -0700

    [PATCH] v4l: tveeprom improved to support newer Hauppage cards
    - tveeprom improved and updated to reflect newer Hauppage cards.
    - CodingStyle fixes.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/media/tveeprom.h
+++ include/media/tveeprom.h
@@ -4,15 +4,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct tveeprom {
 	u32 has_radio;
+	u32 has_ir;     /* 0: no IR, 1: IR present, 2: unknown */
 
 	u32 tuner_type;
 	u32 tuner_formats;
+
+	u32 tuner2_type;
+	u32 tuner2_formats;
 
 	u32 digitizer;
 	u32 digitizer_formats;
 
 	u32 audio_processor;
-	/* a_p_fmts? */
+	u32 decoder_processor;
 
 	u32 model;
 	u32 revision;
@@ -20,7 +24,7 @@ struct tveeprom {
 	char rev_str[5];
 };
 
-void tveeprom_hauppauge_analog(struct tveeprom *tvee,
+void tveeprom_hauppauge_analog(struct i2c_client *c, struct tveeprom *tvee,
 			       unsigned char *eeprom_data);
 
 int tveeprom_read(struct i2c_client *c, unsigned char *eedata, int len);
