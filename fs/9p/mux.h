1a166e662dae5882da2e8aab5a7cbf460baeebae
Eric Van Hensbergen <ericvh@gmail.com>
2005-09-09 13:04:28 -0700

    [PATCH] v9fs: fix handling of malformed 9P messages
    This patch attempts to do a better job of cleaning up after detecting errors
    on the transport.  This should also improve error reporting on broken
    connections to servers.
    
    Signed-off-by: Latchesar Ionkov <lucho@ionkov.net>
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/9p/mux.h
+++ fs/9p/mux.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct v9fs_rpcreq {
 	struct v9fs_fcall *tcall;
 	struct v9fs_fcall *rcall;
+	int err;	/* error code if response failed */
 
 	/* XXX - could we put scatter/gather buffers here? */
 
