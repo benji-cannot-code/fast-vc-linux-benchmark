31325277407756d62af71d3c0958bddc04713c75
Tony Luck <tony.luck@intel.com>
2005-09-09 11:41:12 -0700

    [IA64] Need to include <asm/sn/io.h> in a few more places.
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/sn/pci/pcibr/pcibr_reg.c
+++ arch/ia64/sn/pci/pcibr/pcibr_reg.c
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/interrupt.h>
 #include <linux/types.h>
+#include <asm/sn/io.h>
 #include <asm/sn/pcibr_provider.h>
 #include <asm/sn/pcibus_provider_defs.h>
 #include <asm/sn/pcidev.h>
