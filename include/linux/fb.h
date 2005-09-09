25e6a710799db0177cd0c471614f91d31e514b3e
Thomas Winischhofer <thomas@winischhofer.net>
2005-09-09 13:04:45 -0700

    [PATCH] sisfb update
    This lifts sisfb from version 1.7.17 to version 1.8.9. Changes include:
    
    - Added support for XGI V3XT, V5, V8, Z7 chipsets, including POSTing of
      all of these chipsets.
    
    - Added support for latest SiS chipsets (761).
    
    - Added support for SiS76x memory "hybrid" mode.
    
    - Added support for new LCD resolutions (eg 1280x854, 856x480).
    
    - Fixed support for 320x240 STN panels (for embedded devices).
    
    - Fixed many HDTV modes (525p, 750p, 1080i).
    
    - Fixed PCI config register reading/writing to use proper kernel
      functions for this purpose.
    
    - Fixed PCI ROM handling to use the kernel's proper functions.
    
    - Removed lots of "typedef"s.
    
    - Removed lots of code which was for X.org/XFree86 only.
    
    - Fixed coding style in many places.
    
    - Removed lots of 2.4 cruft.
    
    - Reduced stack size by unifying two previously separate structs into
      one.
    
    - Added new hooks for memory allocation (for DRM).  Now the driver can
      truly handle multiple cards, including memory management.
    
    - Fixed numerous minor bugs.
    
    Signed-off-by: Thomas Winischhofer <thomas@winischhofer.net>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -108,6 +108,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FB_ACCEL_NV_20          44      /* nVidia Arch 20               */
 #define FB_ACCEL_NV_30          45      /* nVidia Arch 30               */
 #define FB_ACCEL_NV_40          46      /* nVidia Arch 40               */
+#define FB_ACCEL_XGI_VOLARI_V	47	/* XGI Volari V3XT, V5, V8      */
+#define FB_ACCEL_XGI_VOLARI_Z	48	/* XGI Volari Z7                */
 #define FB_ACCEL_NEOMAGIC_NM2070 90	/* NeoMagic NM2070              */
 #define FB_ACCEL_NEOMAGIC_NM2090 91	/* NeoMagic NM2090              */
 #define FB_ACCEL_NEOMAGIC_NM2093 92	/* NeoMagic NM2093              */
