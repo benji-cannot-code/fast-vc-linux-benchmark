f549cc7def5b6b8f7d27144b134b1a43354f29f6
Miklos Szeredi <miklos@szeredi.hu>
2005-09-09 13:10:35 -0700

    [PATCH] FUSE - direct I/O
    This patch adds support for the "direct_io" mount option of FUSE.
    
    When this mount option is specified, the page cache is bypassed for
    read and write operations.  This is useful for example, if the
    filesystem doesn't know the size of files before reading them, or when
    any kind of caching is harmful.
    
    Signed-off-by: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -35,6 +35,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     be flushed on open */
 #define FUSE_KERNEL_CACHE        (1 << 2)
 
+/** Bypass the page cache for read and write operations  */
+#define FUSE_DIRECT_IO           (1 << 3)
+
 /** FUSE inode */
 struct fuse_inode {
 	/** Inode data */
@@ -207,6 +210,9 @@ struct fuse_conn {
 
 	/** Maximum read size */
 	unsigned max_read;
+
+	/** Maximum write size */
+	unsigned max_write;
 
 	/** Readers of the connection are waiting on this */
 	wait_queue_head_t waitq;
