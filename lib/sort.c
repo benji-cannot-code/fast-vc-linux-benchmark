31d29caa5fae375ae6881cbb75ddb2caf95c0e57
Adrian Bunk <bunk@stusta.de>
2005-09-10 00:26:59 -0700

    [PATCH] lib/sort.c: small cleanups
    This patch contains the following small cleanups:
    - make two needlessly global functions static
    - every file should #include the header files containing the prototypes
      of it's global functions
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- lib/sort.c
+++ lib/sort.c
@@ -7,15 +7,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/sort.h>
 
-void u32_swap(void *a, void *b, int size)
+static void u32_swap(void *a, void *b, int size)
 {
 	u32 t = *(u32 *)a;
 	*(u32 *)a = *(u32 *)b;
 	*(u32 *)b = t;
 }
 
-void generic_swap(void *a, void *b, int size)
+static void generic_swap(void *a, void *b, int size)
 {
 	char t;
 
