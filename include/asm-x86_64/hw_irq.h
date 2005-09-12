5b63b2f73c7d1385888e1647c769f35c6b189522
Jan Beulich <jbeulich@novell.com>
2005-09-12 18:49:24 +0200

    [PATCH] x86-64: Declare NMI_VECTOR and handle it in the IPI sending code.
    Signed-off-by: Jan Beulich <jbeulich@novell.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/hw_irq.h
+++ include/asm-x86_64/hw_irq.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct hw_interrupt_type;
 #endif
 
+#define NMI_VECTOR		0x02
 /*
  * IDT vectors usable for external interrupt sources start
  * at 0x20:
--- include/asm-x86_64/ipi.h
+++ include/asm-x86_64/ipi.h
@@ -32,9 +32,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline unsigned int __prepare_ICR (unsigned int shortcut, int vector, unsigned int dest)
 {
-	unsigned int icr =  APIC_DM_FIXED | shortcut | vector | dest;
-	if (vector == KDB_VECTOR)
-		icr = (icr & (~APIC_VECTOR_MASK)) | APIC_DM_NMI;
+	unsigned int icr = shortcut | dest;
+
+	switch (vector) {
+	default:
+		icr |= APIC_DM_FIXED | vector;
+		break;
+	case NMI_VECTOR:
+		/*
+		 * Setup KDB IPI to be delivered as an NMI
+		 */
+	case KDB_VECTOR:
+		icr |= APIC_DM_NMI;
+		break;
+	}
 	return icr;
 }
 
