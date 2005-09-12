df9403bc8a43e5e827bda601857ba110495d260d
Nakul Saraiya <saraiya@fabric7.com>
2005-09-12 18:49:24 +0200

    [PATCH] Increase nodemap hash.
    Needed for some newer Opteron systems with E stepping and memory
    relocation enabled. The node addresses are different in lower
    bits now so the nodemap hash function needs to be enlarged.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/mmzone.h
+++ include/asm-x86_64/mmzone.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/smp.h>
 
-#define NODEMAPSIZE 0xff
+#define NODEMAPSIZE 0xfff
 
 /* Simple perfect hash to map physical addresses to node numbers */
 extern int memnode_shift; 
