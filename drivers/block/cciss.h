fc49e9eef00b5402bf8161aa26df53212b938270
Mike Miller <mike.miller@hp.com>
2005-09-13 01:25:22 -0700

    [PATCH] cciss: direct lookup for command completions
    This patch changes the way we complete commands.  In the old method when we
    got a completion we searched our command list from the top until we find it.
    
    This method uses a tag associated with each command (not SCSI command tagging)
    to index us directly to the completed command.  This helps performance.
    
    Signed-off-by: Don Brace <dab@hp.com>
    Signed-off-by: Mike Miller <mike.miller@hp.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/block/cciss.h
+++ drivers/block/cciss.h
@@ -102,6 +102,7 @@ struct ctlr_info
 #ifdef CONFIG_CISS_SCSI_TAPE
 	void *scsi_ctlr; /* ptr to structure containing scsi related stuff */
 #endif
+	unsigned char alive;
 };
 
 /*  Defining the diffent access_menthods */
