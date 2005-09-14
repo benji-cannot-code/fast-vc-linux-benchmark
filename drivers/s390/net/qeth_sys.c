5b817b1becc7ade854c1ec1407b794751a516c0a
Frank Pavlic <pavlic@de.ibm.com>
2005-09-14 18:05:31 +0200

    [PATCH] s390: qeth driver fixes
    [patch 4/4] s390: qeth driver fixes .
    
    From: Frank Pavlic <pavlic@de.ibm.com>
    	- Clear read channel first prior to using ccw_device_set_offline.
    	- use QETH_DBF_TEXT instead of QETH_DBF_SPRINTF
    	- invoke qeth_halt_channel and qeth_clear_channel for all channels,
    	  even if halt/clear for one of the channel fails.
    	- enable qeth_arp_query function for GuestLAN devices
    
    Signed-off-by: Frank Pavlic <pavlic@de.ibm.com>
    
    diffstat:
     qeth.h      |    2 -
     qeth_main.c |  106 +++++++++++++++++++++++++-----------------------------------
     qeth_sys.c  |   11 +++---
     3 files changed, 53 insertions(+), 66 deletions(-)
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/s390/net/qeth_sys.c
+++ drivers/s390/net/qeth_sys.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *
- * linux/drivers/s390/net/qeth_sys.c ($Revision: 1.53 $)
+ * linux/drivers/s390/net/qeth_sys.c ($Revision: 1.54 $)
  *
  * Linux on zSeries OSA Express and HiperSockets support
  * This file contains code related to sysfs.
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "qeth_mpc.h"
 #include "qeth_fs.h"
 
-const char *VERSION_QETH_SYS_C = "$Revision: 1.53 $";
+const char *VERSION_QETH_SYS_C = "$Revision: 1.54 $";
 
 /*****************************************************************************/
 /*                                                                           */
@@ -723,10 +723,13 @@ qeth_dev_layer2_store(struct device *dev, struct device_attribute *attr, const c
 
 	if (!card)
 		return -EINVAL;
+	if (card->info.type == QETH_CARD_TYPE_IQD) {
+                PRINT_WARN("Layer2 on Hipersockets is not supported! \n");
+                return -EPERM;
+        }
 
 	if (((card->state != CARD_STATE_DOWN) &&
-	     (card->state != CARD_STATE_RECOVER)) ||
-	    (card->info.type != QETH_CARD_TYPE_OSAE))
+	     (card->state != CARD_STATE_RECOVER)))
 		return -EPERM;
 
 	i = simple_strtoul(buf, &tmp, 16);
