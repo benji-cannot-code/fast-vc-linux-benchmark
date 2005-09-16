0deb709b2c5e73bae7cf30fbfc87579036bc3197
Nicolas Pitre <nico@cam.org>
2005-09-16 18:46:36 +0200

    [ALSA] remove bogus match method for ac97_bus
    AC97 Codec
    The bus_id is initialized with a generic identifier string which is not
    really useful for proper driver matching.  Let the driver decide what it
    needs via its probe method instead.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/ac97/ac97_bus.c
+++ sound/pci/ac97/ac97_bus.c
@@ -18,12 +18,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 
 /*
- * Codec families have names seperated by commas, so we search for an
- * individual codec name within the family string. 
+ * Let drivers decide whether they want to support given codec from their
+ * probe method.  Drivers have direct access to the ac97_t structure and may
+ * decide based on the id field amongst other things.
  */
 static int ac97_bus_match(struct device *dev, struct device_driver *drv)
 {
-	return (strstr(dev->bus_id, drv->name) != NULL);
+	return 1;
 }
 
 static int ac97_bus_suspend(struct device *dev, pm_message_t state)
