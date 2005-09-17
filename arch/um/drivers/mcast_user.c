1793f6ad9d6f108552e3e1bdeb07240d22ce8034
Jeff Dike <jdike@addtoit.com>
2005-09-16 19:27:47 -0700

    [PATCH] uml: Remove a useless include
    linux/inet.h isn't needed, and on my system, is empty.
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Cc: Paolo Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/drivers/mcast_user.c
+++ arch/um/drivers/mcast_user.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <errno.h>
 #include <unistd.h>
-#include <linux/inet.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/time.h>
