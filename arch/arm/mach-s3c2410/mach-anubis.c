53099d784173887207c4e78e8cecd47cc638f38c
Ben Dooks <ben-linux@fluff.org>
2005-09-20 17:24:33 +0100

    [ARM] 2928/1: S3C2410 - make machine init code static
    Patch from Ben Dooks
    
    This code is not being exported, declare it static
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c2410/mach-anubis.c
+++ arch/arm/mach-s3c2410/mach-anubis.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Modifications:
  *	02-May-2005 BJD  Copied from mach-bast.c
+ *	20-Sep-2005 BJD  Added static to non-exported items
 */
 
 #include <linux/kernel.h>
@@ -233,7 +234,7 @@ static struct s3c24xx_board anubis_board __initdata = {
 	.clocks_count  = ARRAY_SIZE(anubis_clocks)
 };
 
-void __init anubis_map_io(void)
+static void __init anubis_map_io(void)
 {
 	/* initialise the clocks */
 
