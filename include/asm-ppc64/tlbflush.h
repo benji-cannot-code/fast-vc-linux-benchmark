994d273f0c1e2b9332fac06bdd718b48247d309d
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2005-09-20 13:52:50 +1000

    [PATCH] ppc64: Store virtual address in TLB flush batches
    This patch slightly change the TLB flush batch mecanism so that we
    store the full vaddr (including vsid) when adding an entry to the
    batch so that the flush part doesn't have to get to the context.
    
    This cleans it a bit, and paves the way to future updates like
    dynamic vsids.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-ppc64/tlbflush.h
+++ include/asm-ppc64/tlbflush.h
@@ -21,10 +21,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct mm_struct;
 struct ppc64_tlb_batch {
 	unsigned long index;
-	unsigned long context;
 	struct mm_struct *mm;
 	pte_t pte[PPC64_TLB_BATCH_NR];
-	unsigned long addr[PPC64_TLB_BATCH_NR];
 	unsigned long vaddr[PPC64_TLB_BATCH_NR];
 };
 DECLARE_PER_CPU(struct ppc64_tlb_batch, ppc64_tlb_batch);
@@ -48,8 +46,7 @@ static inline void flush_tlb_pending(void)
 #define flush_tlb_kernel_range(start, end)	flush_tlb_pending()
 #define flush_tlb_pgtables(mm, start, end)	do { } while (0)
 
-extern void flush_hash_page(unsigned long context, unsigned long ea, pte_t pte,
-			    int local);
-void flush_hash_range(unsigned long context, unsigned long number, int local);
+extern void flush_hash_page(unsigned long va, pte_t pte, int local);
+void flush_hash_range(unsigned long number, int local);
 
 #endif /* _PPC64_TLBFLUSH_H */
