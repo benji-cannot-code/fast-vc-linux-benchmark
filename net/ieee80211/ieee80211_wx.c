91fc00dde37799cfe28f67c173896daae47f2d55
James Ketrenos <jketreno@linux.intel.com>
2005-09-21 11:53:43 -0500

    [PATCH] ieee80211: Fixed a kernel oops on module unload
    tree 367069f24fc38b4aa910e86ff40094d2078d8aa7
    parent a33a1982012e9070736e3717231714dc9892303b
    author James Ketrenos <jketreno@linux.intel.com> 1124430800 -0500
    committer James Ketrenos <jketreno@linux.intel.com> 1127310571 -0500
    
    Fixed a kernel oops on module unload by adding spin lock protection to
    ieee80211's crypt handlers (thanks to Zhu Yi)
    
    Modified scan result logic to report WPA and RSN IEs if set (vs.being
    based on wpa_enabled)
    
    Added ieee80211_device as the first parameter to the crypt init()
    method.  TKIP modified to use that structure for determining whether to
    countermeasures are active.
    
    Signed-off-by: James Ketrenos <jketreno@linux.intel.com>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- net/ieee80211/ieee80211_wx.c
+++ net/ieee80211/ieee80211_wx.c
@@ -183,7 +183,7 @@ static inline char *ipw2100_translate_scan(struct ieee80211_device *ieee,
 	if (iwe.u.data.length)
 		start = iwe_stream_add_point(start, stop, &iwe, custom);
 
-	if (ieee->wpa_enabled && network->wpa_ie_len) {
+	if (network->wpa_ie_len) {
 		char buf[MAX_WPA_IE_LEN * 2 + 30];
 
 		u8 *p = buf;
@@ -198,7 +198,7 @@ static inline char *ipw2100_translate_scan(struct ieee80211_device *ieee,
 		start = iwe_stream_add_point(start, stop, &iwe, buf);
 	}
 
-	if (ieee->wpa_enabled && network->rsn_ie_len) {
+	if (network->rsn_ie_len) {
 		char buf[MAX_WPA_IE_LEN * 2 + 30];
 
 		u8 *p = buf;
@@ -352,7 +352,7 @@ int ieee80211_wx_set_encode(struct ieee80211_device *ieee,
 		}
 
 		if (new_crypt->ops && try_module_get(new_crypt->ops->owner))
-			new_crypt->priv = new_crypt->ops->init(key);
+			new_crypt->priv = new_crypt->ops->init(ieee, key);
 
 		if (!new_crypt->ops || !new_crypt->priv) {
 			kfree(new_crypt);
