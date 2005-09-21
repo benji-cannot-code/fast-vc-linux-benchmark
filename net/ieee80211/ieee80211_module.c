32e4fc98e236299f05343eab37d9f08551cbbcb6
James Ketrenos <jketreno@linux.intel.com>
2005-09-21 11:54:15 -0500

    [PATCH] ieee80211: Fix kernel Oops when module unload
    tree b69e983266840983183a00f5ac02c66d5270ca47
    parent cdd6372949b76694622ed74fe36e1dd17a92eb71
    author Zhu Yi <jketreno@io.(none)> 1124435425 -0500
    committer James Ketrenos <jketreno@linux.intel.com> 1127312421 -0500
    
    Fix kernel Oops when module unload.
    
    Export a new function ieee80211_crypt_quiescing from ieee80211. Device
    drivers call it to make the host crypto stack enter the quiescence
    state, which means "process existing requests, but don't accept new
    ones". This is usually called during a driver's host crypto data
    structure free (module unload) path.
    
    Signed-off-by: James Ketrenos <jketreno@linux.intel.com>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- net/ieee80211/ieee80211_module.c
+++ net/ieee80211/ieee80211_module.c
@@ -139,6 +139,7 @@ struct net_device *alloc_ieee80211(int sizeof_priv)
 	init_timer(&ieee->crypt_deinit_timer);
 	ieee->crypt_deinit_timer.data = (unsigned long)ieee;
 	ieee->crypt_deinit_timer.function = ieee80211_crypt_deinit_handler;
+	ieee->crypt_quiesced = 0;
 
 	spin_lock_init(&ieee->lock);
 
@@ -162,6 +163,7 @@ void free_ieee80211(struct net_device *dev)
 
 	int i;
 
+	ieee80211_crypt_quiescing(ieee);
 	del_timer_sync(&ieee->crypt_deinit_timer);
 	ieee80211_crypt_deinit_entries(ieee, 1);
 
