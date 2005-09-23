8babb4df9efa6e5a7ecc17f1414b5c5ec8ea2e1c
Pavel Roskin <proski@gnu.org>
2005-09-23 04:18:06 -0400

    [PATCH] orinoco: Bump version to 0.15rc3.
    Signed-off-by: Pavel Roskin <proski@gnu.org>
    
    Bump version to 0.15rc3.
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/wireless/orinoco.h
+++ drivers/net/wireless/orinoco.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ORINOCO_H
 #define _ORINOCO_H
 
-#define DRIVER_VERSION "0.15rc2"
+#define DRIVER_VERSION "0.15rc3"
 
 #include <linux/netdevice.h>
 #include <linux/wireless.h>
