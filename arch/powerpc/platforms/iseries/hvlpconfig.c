6e3190d86f98e17f08bfc20a30d47bab152c61d2
Stephen Rothwell <sfr@canb.auug.org.au>
2005-09-23 13:23:58 +1000

    powerpc: Move HvLpConfig.c to powerpc arch
    Also rename it to hvlpconfig.c
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/platforms/iseries/hvlpconfig.c
+++ arch/powerpc/platforms/iseries/hvlpconfig.c
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * HvLpConfig.c
  * Copyright (C) 2001  Kyle A. Lucke, IBM Corporation
  * 
  * This program is free software; you can redistribute it and/or modify
