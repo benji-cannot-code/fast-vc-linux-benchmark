3c6c9aaad3c9b995b2745d029df7cc24a00b6558
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2005-09-22 21:44:20 -0700

    [PATCH] uml: avoid fixing faults while atomic
    Following i386, we should maybe refuse trying to fault in pages when we're
    doing atomic operations, because to handle the fault we could need to take
    already taken spinlocks.
    
    Also, if we're doing an atomic operation (in the sense of in_atomic()) we're
    surely in kernel mode and we're surely going to handle adequately the failed
    fault, so it's safe to behave this way.
    
    Currently, on UML SMP is rarely used, and we don't support PREEMPT, so this is
    unlikely to create problems right now, but it might in the future.
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Cc: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/kernel/trap_kern.c
+++ arch/um/kernel/trap_kern.c
@@ -41,6 +41,12 @@ int handle_page_fault(unsigned long address, unsigned long ip,
 	int err = -EFAULT;
 
 	*code_out = SEGV_MAPERR;
+
+	/* If the fault was during atomic operation, don't take the fault, just
+	 * fail. */
+	if (in_atomic())
+		goto out_nosemaphore;
+
 	down_read(&mm->mmap_sem);
 	vma = find_vma(mm, address);
 	if(!vma) 
@@ -91,6 +97,7 @@ survive:
 	flush_tlb_page(vma, address);
 out:
 	up_read(&mm->mmap_sem);
+out_nosemaphore:
 	return(err);
 
 /*
