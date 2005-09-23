0f66c4b527be30244204082005b492df7c1d13f0
David S. Miller <davem@sunset.davemloft.net>
2005-09-23 11:59:44 -0700

    [SPARC64]: Mark functions called by paging_init() as __init.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/mm/init.c
+++ arch/sparc64/mm/init.c
@@ -411,7 +411,7 @@ unsigned long prom_virt_to_phys(unsigned long promva, int *error)
  * HI_OBP_ADDRESS range are handled in entry.S and do not use the vpte
  * scheme (also, see rant in inherit_locked_prom_mappings()).
  */
-static void build_obp_range(unsigned long start, unsigned long end, unsigned long data)
+static void __init build_obp_range(unsigned long start, unsigned long end, unsigned long data)
 {
 	unsigned long vaddr;
 
@@ -455,7 +455,7 @@ static inline int in_obp_range(unsigned long vaddr)
 }
 
 #define OBP_PMD_SIZE 2048
-static void build_obp_pgtable(int prom_trans_ents)
+static void __init build_obp_pgtable(int prom_trans_ents)
 {
 	unsigned long i;
 
@@ -481,7 +481,7 @@ static void build_obp_pgtable(int prom_trans_ents)
 /* Read OBP translations property into 'prom_trans[]'.
  * Return the number of entries.
  */
-static int read_obp_translations(void)
+static int __init read_obp_translations(void)
 {
 	int n, node;
 
@@ -506,7 +506,7 @@ static int read_obp_translations(void)
 	return n;
 }
 
-static void remap_kernel(void)
+static void __init remap_kernel(void)
 {
 	unsigned long phys_page, tte_vaddr, tte_data;
 	int tlb_ent = sparc64_highest_locked_tlbent();
@@ -532,7 +532,7 @@ static void remap_kernel(void)
 	}
 }
 
-static void inherit_prom_mappings(void)
+static void __init inherit_prom_mappings(void)
 {
 	int n;
 
@@ -1605,7 +1605,7 @@ void __init mem_init(void)
 		cheetah_ecache_flush_init();
 }
 
-void free_initmem (void)
+void free_initmem(void)
 {
 	unsigned long addr, initend;
 
