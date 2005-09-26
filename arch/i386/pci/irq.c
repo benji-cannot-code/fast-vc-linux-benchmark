a7aa27f07935f4cb0642465697544f64a76f3675
Al Viro <viro@ftp.linux.org.uk>
2005-09-26 05:49:44 +0100

    [PATCH] useless includes of linux/irq.h in arch/i386
    	Most of these guys are simply not needed (pulled by other stuff
    via asm-i386/hardirq.h).  One that is not entirely useless is hilarious -
    arch/i386/oprofile/nmi_timer_int.c includes linux/irq.h... as a way to
    get linux/errno.h
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/pci/irq.c
+++ arch/i386/pci/irq.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
-#include <linux/irq.h>
 #include <linux/dmi.h>
 #include <asm/io.h>
 #include <asm/smp.h>
