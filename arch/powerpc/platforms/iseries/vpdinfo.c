74ac8d5a79866c48768e95ecfc5e4ef1b90cd380
Stephen Rothwell <sfr@canb.auug.org.au>
2005-09-28 02:59:19 +1000

    powerpc: Move iSeries_VpdInfo.c to powerpc/platforms/iseries
    And rename it to vpdinfo.c.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/platforms/iseries/vpdinfo.c
+++ arch/powerpc/platforms/iseries/vpdinfo.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * File iSeries_vpdInfo.c created by Allan Trautman on Fri Feb  2 2001.
- *
  * This code gets the card location of the hardware
  * Copyright (C) 2001  <Allan H Trautman> <IBM Corp>
  * Copyright (C) 2005  Stephen Rothwel, IBM Corp
