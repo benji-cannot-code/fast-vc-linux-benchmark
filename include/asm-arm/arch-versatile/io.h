53ab5614991713ace821c3abfc599b373c431a5f
Al Viro <viro@ftp.linux.org.uk>
2005-09-29 00:09:02 +0100

    [PATCH] arm/versatile iomem annotations
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-arm/arch-versatile/io.h
+++ include/asm-arm/arch-versatile/io.h
@@ -23,7 +23,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define IO_SPACE_LIMIT 0xffffffff
 
-#define __io(a)			((void __iomem *)(a))
+static inline void __iomem *__io(unsigned long addr)
+{
+	return (void __iomem *)addr;
+}
+#define __io(a)	__io(a)
 #define __mem_pci(a)		(a)
 #define __mem_isa(a)		(a)
 
