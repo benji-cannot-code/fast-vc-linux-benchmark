03ea25db911f4a62773cb6f0de7f0fe1c61138f6
Dominik Brodowski <linux@dominikbrodowski.net>
2005-09-28 19:28:37 +0200

    [PATCH] pcmcia: allow one port excludes
    Allow for excluding only one port in /etc/pcmcia/config.otps
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/rsrc_nonstatic.c
+++ drivers/pcmcia/rsrc_nonstatic.c
@@ -692,7 +692,7 @@ static int adjust_memory(struct pcmcia_socket *s, unsigned int action, unsigned 
 	unsigned long size = end - start + 1;
 	int ret = 0;
 
-	if (end <= start)
+	if (end < start)
 		return -EINVAL;
 
 	down(&rsrc_sem);
@@ -725,7 +725,7 @@ static int adjust_io(struct pcmcia_socket *s, unsigned int action, unsigned long
 	unsigned long size = end - start + 1;
 	int ret = 0;
 
-	if (end <= start)
+	if (end < start)
 		return -EINVAL;
 
 	if (end > IO_SPACE_LIMIT)
@@ -926,7 +926,7 @@ static ssize_t store_io_db(struct class_device *class_dev, const char *buf, size
 				return -EINVAL;
 		}
 	}
-	if (end_addr <= start_addr)
+	if (end_addr < start_addr)
 		return -EINVAL;
 
 	ret = adjust_io(s, add, start_addr, end_addr);
@@ -978,7 +978,7 @@ static ssize_t store_mem_db(struct class_device *class_dev, const char *buf, siz
 				return -EINVAL;
 		}
 	}
-	if (end_addr <= start_addr)
+	if (end_addr < start_addr)
 		return -EINVAL;
 
 	ret = adjust_memory(s, add, start_addr, end_addr);
