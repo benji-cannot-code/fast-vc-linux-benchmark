9f259368056e08761cd30d8f1fe0437b42bac0d4
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-09-29 11:15:51 +0100

    [ARM] Don't include mach-types.h unnecessarily
    It's pointless to include mach-types.h if you're not going to use
    anything from it.  These references were removed as a result of:
    
    grep -lr 'asm/mach-types\.h' . | xargs grep -L 'machine_is_\|MACH_TYPE_\|MACHINE_START\|machine_type'
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/serial/s3c2410.c
+++ drivers/serial/s3c2410.c
@@ -83,8 +83,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/regs-serial.h>
 #include <asm/arch/regs-gpio.h>
 
-#include <asm/mach-types.h>
-
 /* structures */
 
 struct s3c24xx_uart_info {
