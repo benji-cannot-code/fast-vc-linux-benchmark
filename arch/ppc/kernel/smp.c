3cc65622a1e87246c75a0bfe8355da1f725bc299
Paul Mackerras <paulus@samba.org>
2005-10-04 14:30:07 +1000

    ppc32: remove obsolete klock_info definition
    klock_info isn't used or referenced anywhere else in the kernel.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc/kernel/smp.c
+++ arch/ppc/kernel/smp.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 volatile int smp_commenced;
 int smp_tb_synchronized;
 struct cpuinfo_PPC cpu_data[NR_CPUS];
-struct klock_info_struct klock_info = { KLOCK_CLEAR, 0 };
 atomic_t ipi_recv;
 atomic_t ipi_sent;
 cpumask_t cpu_online_map;
