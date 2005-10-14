1dd3e303ebcb693efa7511c4b990eee2f85e1c72
Stephen Rothwell <sfr@canb.auug.org.au>
2005-10-14 16:49:58 +1000

    powerpc: eliminate DsaAddr from pci_dn
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/platforms/iseries/pci.h
+++ arch/powerpc/platforms/iseries/pci.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * End Change Activity
  */
 
+#include <asm/pci-bridge.h>
+
 struct pci_dev;				/* For Forward Reference */
 
 /*
@@ -45,6 +47,17 @@ struct pci_dev;				/* For Forward Reference */
 
 #define ISERIES_GET_DEVICE_FROM_SUBBUS(subbus)		((subbus >> 5) & 0x7)
 #define ISERIES_GET_FUNCTION_FROM_SUBBUS(subbus)	((subbus >> 2) & 0x7)
+
+/*
+ * Generate a Direct Select Address for the Hypervisor
+ */
+static inline u64 iseries_ds_addr(struct device_node *node)
+{
+	struct pci_dn *pdn = PCI_DN(node);
+
+	return ((u64)pdn->busno << 48) + ((u64)pdn->bussubno << 40)
+			+ ((u64)0x10 << 32);
+}
 
 extern void	iSeries_Device_Information(struct pci_dev*, int);
 
