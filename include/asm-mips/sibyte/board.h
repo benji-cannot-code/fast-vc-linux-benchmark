993f5fa0de587449f975c48cdf48f291c9eef2f4
Andrew Isaacson <adi@broadcom.com>
2005-10-19 23:57:11 -0700

    Support for BigSur board.
    Signed-Off-By: Andy Isaacson <adi@broadcom.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/sibyte/board.h
+++ include/asm-mips/sibyte/board.h
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sibyte/carmel.h>
 #endif
 
+#ifdef CONFIG_SIBYTE_BIGSUR
+#include <asm/sibyte/bigsur.h>
+#endif
+
 #ifdef __ASSEMBLY__
 
 #ifdef LEDS_PHYS
