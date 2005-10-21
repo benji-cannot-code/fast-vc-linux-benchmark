cd12d90681e018edbb2e0479a1d452e5dc804f1b
David Gibson <david@gibson.dropbear.id.au>
2005-10-21 15:45:50 +1000

    [PATCH] powerpc: Merge thread_info.h
    Merge ppc32 and ppc64 versions of thread_info.h.  They were pretty
    similar already, the chief changes are:
    
    	- Instead of inline asm to implement current_thread_info(),
    which needs to be different for ppc32 and ppc64, we use C with an
    asm("r1") register variable.  gcc turns it into the same asm as we
    used to have for both platforms.
    	- We replace ppc32's 'local_flags' with the ppc64
    'syscall_noerror' field.  The noerror flag was in fact the only thing
    in the local_flags field anyway, so the ppc64 approach is simpler, and
    means we only need a load-immediate/store instead of load/mask/store
    when clearing the flag.
    	- In readiness for 64k pages, when THREAD_SIZE will be less
    than a page, ppc64 used kmalloc() rather than get_free_pages() to
    allocate the kernel stack.  With this patch we do the same for ppc32,
    since there's no strong reason not to.
    	- For ppc64, we no longer export THREAD_SHIFT and THREAD_SIZE
    via asm-offsets, thread_info.h can now be safely included in asm, as
    on ppc32.
    
    Built and booted on G4 Powerbook (ARCH=ppc and ARCH=powerpc) and
    Power5 (ARCH=ppc64 and ARCH=powerpc).
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc64/kernel/asm-offsets.c
+++ arch/ppc64/kernel/asm-offsets.c
@@ -47,8 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 int main(void)
 {
 	/* thread struct on stack */
-	DEFINE(THREAD_SHIFT, THREAD_SHIFT);
-	DEFINE(THREAD_SIZE, THREAD_SIZE);
 	DEFINE(TI_FLAGS, offsetof(struct thread_info, flags));
 	DEFINE(TI_PREEMPT, offsetof(struct thread_info, preempt_count));
 	DEFINE(TI_SC_NOERR, offsetof(struct thread_info, syscall_noerror));
