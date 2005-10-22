7270802e915b08b8569556ff0c57de8982d7ea96
James Courtier-Dutton <James@superbug.co.uk>
2005-10-22 11:49:51 +0200

    [ALSA] ca0106: Added copyright messages.
    Modules: CA0106 driver
    
    Signed-off-by: James Courtier-Dutton <James@superbug.co.uk>

--- sound/pci/ca0106/ca_midi.c
+++ sound/pci/ca0106/ca_midi.c
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Changelog:
  *    Implementation is based on mpu401 and emu10k1x and
  *    tested with ca0106.
+ *    mpu401: Copyright (c) by Jaroslav Kysela <perex@suse.cz>
+ *    emu10k1x: Copyright (c) by Francisco Moraes <fmoraes@nc.rr.com>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
