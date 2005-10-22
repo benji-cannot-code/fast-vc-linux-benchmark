be136ccb58822366b90df3bbeec75f961b7f9b73
Grant Grundler <grundler@parisc-linux.org>
2005-10-21 22:56:35 -0400

    [PARISC] Specify level to fix binutils level promotion bug
    fixup.S needs to specify .level and use correct LDREG macro.
    New binutils has a bug where it doesn't "promote" from PA1.0 to PA1.1
    correctly when using ",s" completer.
    
    remove use of __LP64__ in assembly.h and add some white space.
    
    Signed-off-by: Grant Grundler <grundler@parisc-linux.org>
    
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- include/asm-parisc/assembly.h
+++ include/asm-parisc/assembly.h
@@ -23,7 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _PARISC_ASSEMBLY_H
 
 #define CALLEE_FLOAT_FRAME_SIZE	80
-#ifdef __LP64__
+
+#ifdef CONFIG_64BIT
 #define LDREG	ldd
 #define STREG	std
 #define LDREGX  ldd,s
@@ -33,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RP_OFFSET	16
 #define FRAME_SIZE	128
 #define CALLEE_REG_FRAME_SIZE	144
-#else
+#else	/* CONFIG_64BIT */
 #define LDREG	ldw
 #define STREG	stw
 #define LDREGX  ldwx,s
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FRAME_SIZE	64
 #define CALLEE_REG_FRAME_SIZE	128
 #endif
+
 #define CALLEE_SAVE_FRAME_SIZE (CALLEE_REG_FRAME_SIZE + CALLEE_FLOAT_FRAME_SIZE)
 
 #ifdef CONFIG_PA20
