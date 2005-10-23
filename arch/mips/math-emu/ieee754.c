232e98a4d4ad796f4cbb9c455298e5e58c092f8a
Ralf Baechle <ralf@linux-mips.org>
2005-10-23 13:46:25 +0100

    Include for prototypes.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/math-emu/ieee754.c
+++ arch/mips/math-emu/ieee754.c
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #include "ieee754int.h"
+#include "ieee754sp.h"
+#include "ieee754dp.h"
 
 #define DP_EBIAS	1023
 #define DP_EMIN		(-1022)
