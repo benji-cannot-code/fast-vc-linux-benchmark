fa5b23c93320a39af71b3d1ff96837a4afade9c0
Clemens Ladisch <clemens@ladisch.de>
2005-10-24 17:02:46 +0200

    [ALSA] usb-audio: remove superfluous include
    Modules: USB generic driver
    
    Remove a superfluous include of the sound/minors.h header.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>

--- sound/usb/usbmidi.c
+++ sound/usb/usbmidi.c
@@ -48,7 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/usb.h>
 #include <sound/core.h>
-#include <sound/minors.h>
 #include <sound/rawmidi.h>
 #include "usbaudio.h"
 
