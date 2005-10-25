4e0497d1a9ac997d3c09e82b668b3ff4662813a4
Andrew Morton <akpm@osdl.org>
2005-10-24 23:05:58 -0700

    [PATCH] alpha: atomic dependency fix
    My alpha build is exploding because asm/atomic.h now needs smb_mb(), which is
    over in the (not included) system.h.
    
    I fear what will happen if I include system.h into atomic.h, so let's put the
    barriers into their own header file.
    
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-alpha/atomic.h
+++ include/asm-alpha/atomic.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ALPHA_ATOMIC_H
 #define _ALPHA_ATOMIC_H
 
+#include <asm/barrier.h>
+
 /*
  * Atomic operations that C can't guarantee us.  Useful for
  * resource counting etc...
