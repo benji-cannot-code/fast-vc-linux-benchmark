11532c4875e3e5b5dd18aca18bcb6513014d6968
Nicolas Pitre <nico@cam.org>
2005-10-28 16:39:33 +0100

    [ARM] 2897/2: PXA2xx IRDA support
    Patch from Nicolas Pitre
    
    This is the PXA2xx common IRDA driver, plus platform support
    for Lubbock and Mainstone.
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Acked-by: Jean Tourrilhes <jt@hpl.hp.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/generic.c
+++ arch/arm/mach-pxa/generic.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/udc.h>
 #include <asm/arch/pxafb.h>
 #include <asm/arch/mmc.h>
+#include <asm/arch/irda.h>
 #include <asm/arch/i2c.h>
 
 #include "generic.h"
@@ -302,6 +303,22 @@ static struct platform_device i2s_device = {
 	.num_resources	= ARRAY_SIZE(i2s_resources),
 };
 
+static u64 pxaficp_dmamask = ~(u32)0;
+
+static struct platform_device pxaficp_device = {
+	.name		= "pxa2xx-ir",
+	.id		= -1,
+	.dev		= {
+		.dma_mask = &pxaficp_dmamask,
+		.coherent_dma_mask = 0xffffffff,
+	},
+};
+
+void __init pxa_set_ficp_info(struct pxaficp_platform_data *info)
+{
+	pxaficp_device.dev.platform_data = info;
+}
+
 static struct platform_device *devices[] __initdata = {
 	&pxamci_device,
 	&udc_device,
@@ -309,6 +326,7 @@ static struct platform_device *devices[] __initdata = {
 	&ffuart_device,
 	&btuart_device,
 	&stuart_device,
+	&pxaficp_device,
 	&i2c_device,
 	&i2s_device,
 };
