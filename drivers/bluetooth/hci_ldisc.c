f33e2a6f6d1ad198e421a6101153c41201259418
Marcel Holtmann <marcel@holtmann.org>
2005-10-28 19:20:40 +0200

    [Bluetooth] Remove TXCRC compile option for BCSP driver
    The TXCRC compile option is not really useful and thus change it
    into a module parameter.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_ldisc.c
+++ drivers/bluetooth/hci_ldisc.c
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * $Id: hci_ldisc.c,v 1.5 2002/10/02 18:37:20 maxk Exp $    
  */
-#define VERSION "2.1"
+#define VERSION "2.2"
 
 #include <linux/config.h>
 #include <linux/module.h>
