e387cdc6974e1eb71e43a70dc3a3f9d4e3da908d
Al Viro <viro@ftp.linux.org.uk>
2005-10-29 06:46:03 +0100

    [PATCH] missing include in infiniband
    use of IS_ERR/PTR_ERR in infiniband/core/agent.c, without a portable
    chain of includes pulling err.h (breaks on a bunch of platforms).
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Acked-by: Roland Dreier <rolandd@cisco.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/infiniband/core/agent.h
+++ drivers/infiniband/core/agent.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __AGENT_H_
 #define __AGENT_H_
 
+#include <linux/err.h>
 #include <rdma/ib_mad.h>
 
 extern int ib_agent_port_open(struct ib_device *device, int port_num);
