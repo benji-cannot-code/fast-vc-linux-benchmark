loongson-community/linux-loongson-community

Support the MIPS32 / MIPS64 DSP ASE.

arch/mips/kernel/setup.c
@@ -569,3 +569,12 @@ int __init fpu_disable(char *s)
 }
 
 __setup("nofpu", fpu_disable);
+
+int __init dsp_disable(char *s)
+{
+	cpu_data[0].options &= ~MIPS_ASE_DSP;
+
+	return 1;
+}
+
+__setup("nodsp", dsp_disable);

benji-cannot-code/fast-vc-linux-benchmark

Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

arch/mips/kernel/setup.c
@@ -550,3 +550,12 @@ int __init fpu_disable(char *s)
 }
 
 __setup("nofpu", fpu_disable);
+
+int __init dsp_disable(char *s)
+{
+	cpu_data[0].ases &= ~MIPS_ASE_DSP;
+
+	return 1;
+}
+
+__setup("nodsp", dsp_disable);