62b432a040261f79487f37075407552bc0315c05
Eugene Surovegin <ebs@ebshome.net>
2005-10-29 12:45:31 -0700

    [PATCH] PPC 44x EMAC driver: add 440GR support
    Add PowerPC 440GR support
    
    Signed-off-by: Stefan Roese <sr@denx.de>
    Signed-off-by: Eugene Surovegin <ebs@ebshome.net>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/ibm_emac/ibm_emac_mal.h
+++ drivers/net/ibm_emac/ibm_emac_mal.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * reflect the fact that 40x and 44x have slightly different MALs. --ebs
  */
 #if defined(CONFIG_405GP) || defined(CONFIG_405GPR) || defined(CONFIG_405EP) || \
-    defined(CONFIG_440EP) || defined(CONFIG_NP405H)
+    defined(CONFIG_440EP) || defined(CONFIG_440GR) || defined(CONFIG_NP405H)
 #define MAL_VERSION		1
 #elif defined(CONFIG_440GP) || defined(CONFIG_440GX) || defined(CONFIG_440SP) || \
       defined(CONFIG_440SPE)
