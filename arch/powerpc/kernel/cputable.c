892c155274c7fd301607f1dc72e79d6833b59d87
Paul Mackerras <paulus@samba.org>
2005-10-29 22:08:55 +1000

    powerpc: 32-bit needs cur_cpu_spec exported too
    Somehow we ended up with an #ifdef CONFIG_PPC64 around the export
    of cur_cpu_spec, but raid6 as a module needs it on ppc32 as well
    as ppc64.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/cputable.c
+++ arch/powerpc/kernel/cputable.c
@@ -22,9 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cputable.h>
 
 struct cpu_spec* cur_cpu_spec = NULL;
-#ifdef CONFIG_PPC64
 EXPORT_SYMBOL(cur_cpu_spec);
-#endif
 
 /* NOTE:
  * Unlike ppc32, ppc64 will only call this once for the boot CPU, it's
