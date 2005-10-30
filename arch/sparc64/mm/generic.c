f0937fe22a8303b4d6b01d65b9aba1e25a752384
Hugh Dickins <hugh@veritas.com>
2005-10-29 18:16:24 -0700

    [PATCH] mm: arches skip ptlock
    Convert those few architectures which are calling pud_alloc, pmd_alloc,
    pte_alloc_map on a user mm, not to take the page_table_lock first, nor drop it
    after.  Each of these can continue to use pte_alloc_map, no need to change
    over to pte_alloc_map_lock, they're neither racy nor swappable.
    
    In the sparc64 io_remap_pfn_range, flush_tlb_range then falls outside of the
    page_table_lock: that's okay, on sparc64 it's like flush_tlb_mm, and that has
    always been called from outside of page_table_lock in dup_mmap.
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/sparc64/mm/generic.c
+++ arch/sparc64/mm/generic.c
@@ -136,9 +136,8 @@ int io_remap_pfn_range(struct vm_area_struct *vma, unsigned long from,
 	dir = pgd_offset(mm, from);
 	flush_cache_range(vma, beg, end);
 
-	spin_lock(&mm->page_table_lock);
 	while (from < end) {
-		pud_t *pud = pud_alloc(current->mm, dir, from);
+		pud_t *pud = pud_alloc(mm, dir, from);
 		error = -ENOMEM;
 		if (!pud)
 			break;
@@ -148,8 +147,7 @@ int io_remap_pfn_range(struct vm_area_struct *vma, unsigned long from,
 		from = (from + PGDIR_SIZE) & PGDIR_MASK;
 		dir++;
 	}
-	flush_tlb_range(vma, beg, end);
-	spin_unlock(&mm->page_table_lock);
 
+	flush_tlb_range(vma, beg, end);
 	return error;
 }
