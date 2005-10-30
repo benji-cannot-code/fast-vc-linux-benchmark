49997b44b3f5a71edf166e7a0485f8c6de5b2985
Clemens Ladisch <clemens@ladisch.de>
2005-10-30 15:03:33 -0800

    [PATCH] hpet: allow non-power-of-two frequencies
    It was only the RTC hardware that restricted interrupt frequencies to a power
    of two.  There is no reason to take over this restriction into the HPET
    driver, so remove the offending check.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Acked-by: Bob Picco <bob.picco@hp.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/hpet.c
+++ drivers/char/hpet.c
@@ -520,7 +520,7 @@ hpet_ioctl_common(struct hpet_dev *devp, int cmd, unsigned long arg, int kernel)
 			break;
 		}
 
-		if (!arg || (arg & (arg - 1))) {
+		if (!arg) {
 			err = -EINVAL;
 			break;
 		}
