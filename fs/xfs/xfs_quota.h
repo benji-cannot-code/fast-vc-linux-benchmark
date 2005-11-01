90150b76d014521c54ccf341c44c3e011ede671f
Nathan Scott <nathans@sgi.com>
2005-11-02 10:31:41 +1100

    [XFS] Fix signedness issues in dquot ID handling, allowing uids/gids above MAXINT
    SGI-PV: 942528
    SGI-Modid: xfs-linux:xfs-kern:23828a
    
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/xfs_quota.h
+++ fs/xfs/xfs_quota.h
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * uid_t and gid_t are hard-coded to 32 bits in the inode.
  * Hence, an 'id' in a dquot is 32 bits..
  */
-typedef __int32_t	xfs_dqid_t;
+typedef __uint32_t	xfs_dqid_t;
 
 /*
  * Eventhough users may not have quota limits occupying all 64-bits,
