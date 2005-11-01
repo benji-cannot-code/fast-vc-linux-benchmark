00241752ab66497d52c7661cb8b4a272c85455cb
Arnd Bergmann <arndb@de.ibm.com>
2005-10-31 20:08:39 -0500

    [PATCH] powerpc: move rtas_fw.c out of platforms/pseries
    Cell uses the same code as pSeries for flashing the firmware
    through rtas, so the implementation should not be part of
    platforms/pseries.
    
    Put it into arch/powerpc/kernel instead.
    
    Signed-off-by: Arnd Bergmann <arndb@de.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/rtas_fw.c
+++ arch/powerpc/kernel/rtas_fw.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *
- * Procedures for firmware flash updates on pSeries systems.
+ * Procedures for firmware flash updates.
  *
  * Peter Bergner, IBM	March 2001.
  * Copyright (C) 2001 IBM.
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/delay.h>
 #include <asm/uaccess.h>
 #include <asm/systemcfg.h>
-
-#include "rtas-fw.h"
 
 struct flash_block_list_header rtas_firmware_flash_list = {0, NULL};
 
