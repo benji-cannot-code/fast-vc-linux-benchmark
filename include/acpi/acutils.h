d3dc7adc1c7e14b512664e55f978b53f43bbe94d
Bob Moore <robert.moore@intel.com>
2005-11-02 00:00:00 -0500

    [ACPI] ACPICA 20051102
    Modified the subsystem initialization sequence to improve
    GPE support. The GPE initialization has been split into
    two parts in order to defer execution of the _PRW methods
    (Power Resources for Wake) until after the hardware is
    fully initialized and the SCI handler is installed. This
    allows the _PRW methods to access fields protected by the
    Global Lock. This will fix systems where a NO_GLOBAL_LOCK
    exception has been seen during initialization.
    
    Fixed a regression with the ConcatenateResTemplate()
    ASL operator introduced in the 20051021 release.
    
    Implemented support for "local" internal ACPI object
    types within the debugger "Object" command and the
    acpi_walk_namespace() external interfaces. These local
    types include RegionFields, BankFields, IndexFields, Alias,
    and reference objects.
    
    Moved common AML resource handling code into a new file,
    "utresrc.c". This code is shared by both the Resource
    Manager and the AML Debugger.
    
    Signed-off-by: Bob Moore <robert.moore@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acutils.h
+++ include/acpi/acutils.h
@@ -45,6 +45,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ACUTILS_H
 #define _ACUTILS_H
 
+extern const u8 acpi_gbl_resource_aml_sizes[];
+
+/* Types for Resource descriptor entries */
+
+#define ACPI_INVALID_RESOURCE           0
+#define ACPI_FIXED_LENGTH               1
+#define ACPI_VARIABLE_LENGTH            2
+#define ACPI_SMALL_VARIABLE_LENGTH      3
+
 typedef
 acpi_status(*acpi_pkg_callback) (u8 object_type,
 				 union acpi_operand_object * source_object,
@@ -419,13 +428,19 @@ acpi_ut_strtoul64(char *string, u32 base, acpi_integer * ret_integer);
 
 #define ACPI_ANY_BASE        0
 
+acpi_status acpi_ut_validate_resource(void *aml, u8 * return_index);
+
 u32 acpi_ut_get_descriptor_length(void *aml);
 
 u16 acpi_ut_get_resource_length(void *aml);
 
+u8 acpi_ut_get_resource_header_length(void *aml);
+
 u8 acpi_ut_get_resource_type(void *aml);
 
-u8 *acpi_ut_get_resource_end_tag(union acpi_operand_object *obj_desc);
+acpi_status
+acpi_ut_get_resource_end_tag(union acpi_operand_object *obj_desc,
+			     u8 ** end_tag);
 
 u8 acpi_ut_generate_checksum(u8 * buffer, u32 length);
 
