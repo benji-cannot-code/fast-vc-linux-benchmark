95713fc9ddc0f83b0b84861498c5b602c1b77c57
Kelly Daly <kelly@au.ibm.com>
2005-11-02 13:48:25 +1100

    merge filename and modify references to iseries/it_exp_vpd_panel.h
    Signed-off-by: Kelly Daly <kelly@au.ibm.com>

--- arch/powerpc/platforms/iseries/lpardata.c
+++ arch/powerpc/platforms/iseries/lpardata.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/iSeries/ItLpRegSave.h>
 #include <asm/paca.h>
 #include <asm/iSeries/LparMap.h>
-#include <asm/iSeries/ItExtVpdPanel.h>
+#include <asm/iseries/it_exp_vpd_panel.h>
 #include <asm/iSeries/ItLpQueue.h>
 
 #include "vpd_areas.h"
