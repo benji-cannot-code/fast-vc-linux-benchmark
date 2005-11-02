bc3a4de87ef8f0a61ca4f5b11869e123c2a88e4e
Kelly Daly <kelly@au.ibm.com>
2005-11-02 15:22:37 +1100

    fix incorrect dir
    Signed-off-by: Kelly Daly <kelly@au.ibm.com>

--- include/asm-powerpc/iseries/mf.h
+++ include/asm-powerpc/iseries/mf.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 #include <asm/iseries/hv_types.h>
-#include <asm/iSeries/iseries/hv_call_event.h>
+#include <asm/iseries/hv_call_event.h>
 
 struct rtc_time;
 
