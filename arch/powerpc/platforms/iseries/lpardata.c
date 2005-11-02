6052e6b3e6ba3e3bfde05cc3bf2ffec5bcc6f7ec
Kelly Daly <kelly@au.ibm.com>
2005-11-02 14:49:47 +1100

    merge filename and modify references to iseries/it_lp_reg_save.h
    Signed-off-by: Kelly Daly <kelly@au.ibm.com>

--- arch/powerpc/platforms/iseries/lpardata.c
+++ arch/powerpc/platforms/iseries/lpardata.c
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/abs_addr.h>
 #include <asm/iseries/it_lp_naca.h>
 #include <asm/lppaca.h>
-#include <asm/iSeries/ItLpRegSave.h>
+#include <asm/iseries/it_lp_reg_save.h>
 #include <asm/paca.h>
 #include <asm/iSeries/LparMap.h>
 #include <asm/iseries/it_exp_vpd_panel.h>
