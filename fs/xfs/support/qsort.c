d94b51d9896aa5ab16d25747869cce13684b17e6
Nathan Scott <nathans@sgi.com>
2005-11-02 14:58:39 +1100

    [XFS] Update license/copyright notices to match the prefered SGI boilerplate.
    SGI-PV: 913862
    SGI-Modid: xfs-linux:xfs-kern:23903a
    
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/support/qsort.c
+++ fs/xfs/support/qsort.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
 #include <linux/kernel.h>
 #include <linux/string.h>
 
