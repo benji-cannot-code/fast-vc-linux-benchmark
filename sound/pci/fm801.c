ce545424564c2c08163e7b78dc4be69ef9573f68
Greg Kroah-Hartman <gregkh@suse.de>
2005-11-04 18:56:13 -0800

    [PATCH] PCI: removed unneeded .owner field from struct pci_driver
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- sound/pci/fm801.c
+++ sound/pci/fm801.c
@@ -1460,7 +1460,6 @@ static void __devexit snd_card_fm801_remove(struct pci_dev *pci)
 
 static struct pci_driver driver = {
 	.name = "FM801",
-	.owner = THIS_MODULE,
 	.id_table = snd_fm801_ids,
 	.probe = snd_card_fm801_probe,
 	.remove = __devexit_p(snd_card_fm801_remove),
