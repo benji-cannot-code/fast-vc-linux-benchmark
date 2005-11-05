d7a5f4435c9dd6d1363f15027f1b8a941df8419e
Ashok Raj <ashok.raj@intel.com>
2005-11-05 17:25:53 +0100

    [PATCH] x86_64: Remove duplicate __cpuinit define
    Remove duplicate __cpuinit in smp.c. Already defined in init.h which is
    already included.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/smp.c
+++ arch/x86_64/kernel/smp.c
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/proto.h>
 #include <asm/apicdef.h>
 
-#define __cpuinit __init
-
 /*
  *	Smarter SMP flushing macros. 
  *		c/o Linus Torvalds.
