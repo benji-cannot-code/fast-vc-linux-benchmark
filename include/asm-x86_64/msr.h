2cd85adca3958e1c74491f3733ef467227e993c5
Andi Kleen <ak@suse.de>
2005-11-05 17:25:54 +0100

    [PATCH] x86_64: Formatting fixes for arch/x86_64/kernel/process.c
    No functional changes.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/msr.h
+++ include/asm-x86_64/msr.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 			    : "=a" (a__), "=d" (b__) \
 			    : "c" (msr)); \
        val = a__ | (b__<<32); \
-} while(0); 
+} while(0)
 
 #define wrmsr(msr,val1,val2) \
      __asm__ __volatile__("wrmsr" \
