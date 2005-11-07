12f9c68894a0633d8d61d3de0e04fa642e687108
Jean Delvare <khali@linux-fr.org>
2005-11-07 01:00:34 -0800

    [PATCH] radeonfb: prevent spurious recompilations
    Prevent spurious recompilations of the radeonfb driver when I2C/DDC support
    is not included and i2c header files are modified.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/aty/radeon_base.c
+++ drivers/video/aty/radeon_base.c
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/vmalloc.h>
 #include <linux/device.h>
-#include <linux/i2c.h>
 
 #include <asm/io.h>
 #include <asm/uaccess.h>
