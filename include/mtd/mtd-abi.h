0505cf4c2d285dd2fb043eb0a7aad580ea4da516
Thomas Gleixner <tglx@linutronix.de>
2005-11-07 11:15:37 +0000

    [MTD] user-abi: Clean up trailing white spaces
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/mtd/mtd-abi.h
+++ include/mtd/mtd-abi.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: mtd-abi.h,v 1.12 2005/08/06 04:40:43 nico Exp $
+ * $Id: mtd-abi.h,v 1.13 2005/11/07 11:14:56 gleixner Exp $
  *
- * Portions of MTD ABI definition which are shared by kernel and user space 
+ * Portions of MTD ABI definition which are shared by kernel and user space
  */
 
 #ifndef __MTD_ABI_H__
@@ -82,7 +82,7 @@ struct mtd_info_user {
 };
 
 struct region_info_user {
-	uint32_t offset;		/* At which this region starts, 
+	uint32_t offset;		/* At which this region starts,
 					 * from the beginning of the MTD */
 	uint32_t erasesize;		/* For this region */
 	uint32_t numblocks;		/* Number of blocks in this region */
