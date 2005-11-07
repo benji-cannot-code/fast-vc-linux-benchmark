32d7326a933da111cdb5a101c385b17e4867b031
Anton Blanchard <anton@samba.org>
2005-11-07 19:05:31 +1100

    [PATCH] ppc64: remove some direct xmon calls
    Even though we can enable and disable xmon at runtime now, there are a
    few places in the merge tree that call xmon and xmon_printf directly.
    
    In the case below we call die() which will call xmon if it is enabled.
    
    Also remove an unnecessary include of xmon.h in smp.c.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/smp.c
+++ arch/powerpc/kernel/smp.c
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 #include <asm/smp.h>
 #include <asm/time.h>
-#include <asm/xmon.h>
 #include <asm/machdep.h>
 #include <asm/cputable.h>
 #include <asm/system.h>
