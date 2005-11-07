df129ba87fcff768ba544e6ee538a94bdbda6f98
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-11-07 10:30:16 +0000

    [ARM] realview core.h uses leds_event_t, so include asm/leds.h
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-realview/core.h
+++ arch/arm/mach-realview/core.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_REALVIEW_H
 
 #include <asm/hardware/amba.h>
+#include <asm/leds.h>
 #include <asm/io.h>
 
 #define __io_address(n)		__io(IO_ADDRESS(n))
