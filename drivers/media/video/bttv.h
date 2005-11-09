a2b3c2c5744a474fb2ba6eea1207a7173c578e82
nshmyrev@yandex.ru <nshmyrev@yandex.ru>
2005-11-08 21:37:38 -0800

    [PATCH] v4l: 794: added asound skyeye bttv card
    - Added Asound Skyeye bttv card.
    
    Signed-off-by: Nickolay V. Shmyrev <<nshmyrev@yandex.ru>>
    Signed-off-by: Nickolay V. Shmyrev <nshmyrev@yandex.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/bttv.h
+++ drivers/media/video/bttv.h
@@ -162,6 +162,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BTTV_BOARD_PV_BT878P_2E            0x8a
 #define BTTV_BOARD_PV_M4900                0x8b
 #define BTTV_BOARD_OSPREY440               0x8c
+#define BTTV_BOARD_ASOUND_SKYEYE	   0x8d
 
 /* i2c address list */
 #define I2C_TSA5522        0xc2
