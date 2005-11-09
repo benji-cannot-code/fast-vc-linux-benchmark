d4d70675d403d0457136e9406e91623032087f12
Antonino A. Daplas <adaplas@gmail.com>
2005-11-08 21:39:14 -0800

    [PATCH] fbcon: Console Rotation - Add support for 270-degree rotation
    Add support for 270-degree (counterclockwise) rotation of the console.  To
    activate, boot with:
    
    fbcon=rotate:3
    
    Signed-off-by: Antonino Daplas <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/console/fbcon_rotate.h
+++ drivers/video/console/fbcon_rotate.h
@@ -102,4 +102,5 @@ static inline void rotate_ccw(const char *in, char *out, u32 width, u32 height)
 
 extern void fbcon_rotate_cw(struct fbcon_ops *ops);
 extern void fbcon_rotate_ud(struct fbcon_ops *ops);
+extern void fbcon_rotate_ccw(struct fbcon_ops *ops);
 #endif
