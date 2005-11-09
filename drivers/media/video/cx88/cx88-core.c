cf4eaeee9d58da4e46f497afa057c7ddaa18ccef
Mauro Carvalho Chehab <mchehab@brturbo.com.br>
2005-11-08 21:37:17 -0800

    [PATCH] v4l: 739: created make changelog to make easier to generate patches
    - Created make changelog to make easier to generate patches.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/cx88/cx88-core.c
+++ drivers/media/video/cx88/cx88-core.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/pci.h>
 #include <linux/delay.h>
-#include <linux/videodev.h>
+#include <linux/videodev2.h>
 
 #include "cx88.h"
 
