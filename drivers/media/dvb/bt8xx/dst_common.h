177ec4a412dc8fea0708f044a0d7d16011dbfa6c
Manu Abraham <manu@linuxtv.org>
2005-11-08 21:35:36 -0800

    [PATCH] dvb: dst: protect the read/write commands with a mutex
    We need to protect the read/write commands with a mutex.
    
    Bug reported by Henrik Sjoberg <henke@epact.se>
    
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Cc: Johannes Stezenbach <js@linuxtv.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/dvb/bt8xx/dst_common.h
+++ drivers/media/dvb/bt8xx/dst_common.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef DST_COMMON_H
 #define DST_COMMON_H
 
+#include <linux/smp_lock.h>
 #include <linux/dvb/frontend.h>
 #include <linux/device.h>
 #include "bt878.h"
@@ -120,6 +121,8 @@ struct dst_state {
 	u8 card_info[8];
 	u8 vendor[8];
 	u8 board_info[8];
+
+	struct semaphore dst_mutex;
 };
 
 struct dst_types {
