47b525fd39bc8a47096db662019197531708233d
David Woodhouse <dwmw2@infradead.org>
2005-11-08 21:36:46 -0800

    [PATCH] v4l: 687: fix source charset make symbols utf 8
    - Fix source charset. Make symbols UTF-8.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>.
    Signed-off-by: Nickolay V. Shmyrev <nshmyrev@yandex.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/bttv-cards.c
+++ drivers/media/video/bttv-cards.c
@@ -1064,7 +1064,7 @@ struct tvcard bttv_tvcards[] = {
 },{
 
 /* ---- card 0x34 ---------------------------------- */
-	/* David Härdeman <david@2gen.com> */
+	/* David HÃ¤rdeman <david@2gen.com> */
 	.name           = "Pinnacle PCTV Studio Pro",
 	.video_inputs   = 4,
 	.audio_inputs   = 1,
@@ -3371,7 +3371,7 @@ void bttv_tda9880_setnorm(struct bttv *btv, int norm)
 
 /*
  * reset/enable the MSP on some Hauppauge cards
- * Thanks to Kyösti Mälkki (kmalkki@cc.hut.fi)!
+ * Thanks to KyÃ¶sti MÃ¤lkki (kmalkki@cc.hut.fi)!
  *
  * Hauppauge:  pin  5
  * Voodoo:     pin 20
