4fc9516fe762bb57081c4e3d558287b50d6afa6a
Pierre Ossman <drzeus@drzeus.cx>
2005-11-09 23:22:27 +0000

    [MMC] wbsd version 1.5
    New kernel, new patches, new version.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/wbsd.c
+++ drivers/mmc/wbsd.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "wbsd.h"
 
 #define DRIVER_NAME "wbsd"
-#define DRIVER_VERSION "1.4"
+#define DRIVER_VERSION "1.5"
 
 #ifdef CONFIG_MMC_DEBUG
 #define DBG(x...) \
