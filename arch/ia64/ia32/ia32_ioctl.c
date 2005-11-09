5169f6ffbef3b10b32fd1bf3c66bc7f0f88b90df
Christoph Hellwig <hch@lst.de>
2005-11-08 21:35:07 -0800

    [PATCH] remove ioctl32_handler_t
    Some architectures define and use this type in their compat_ioctl code, but
    all of them can easily use the identical ioctl_trans_handler_t type that is
    defined in common code.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Cc: Andi Kleen <ak@muc.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ia64/ia32/ia32_ioctl.c
+++ arch/ia64/ia32/ia32_ioctl.c
@@ -30,10 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CODE
 #include "compat_ioctl.c"
 
-typedef int (* ioctl32_handler_t)(unsigned int, unsigned int, unsigned long, struct file *);
-
 #define COMPATIBLE_IOCTL(cmd)		HANDLE_IOCTL((cmd),sys_ioctl)
-#define HANDLE_IOCTL(cmd,handler)	{ (cmd), (ioctl32_handler_t)(handler), NULL },
+#define HANDLE_IOCTL(cmd,handler)	{ (cmd), (ioctl_trans_handler_t)(handler), NULL },
 #define IOCTL_TABLE_START \
 	struct ioctl_trans ioctl_start[] = {
 #define IOCTL_TABLE_END \
