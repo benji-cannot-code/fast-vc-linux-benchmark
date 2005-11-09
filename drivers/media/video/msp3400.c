f17194461656cecf902e7136857b715c5284bc73
Hans Verkuil <hverkuil@xs4all.nl>
2005-11-08 21:37:34 -0800

    [PATCH] v4l: 788: log message
    - This fixes dual language detection on the msp3400.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/msp3400.c
+++ drivers/media/video/msp3400.c
@@ -1789,6 +1789,10 @@ static int msp_command(struct i2c_client *client, unsigned int cmd, void *arg)
 		dprintk("msp34xx: AUDC_SET_RADIO\n");
 		msp->norm = VIDEO_MODE_RADIO;
 		dprintk("msp34xx: switching to radio mode\n");
+		if (IS_MSP34XX_G(msp)) {
+			msp34xxg_reset(client);
+			break;
+		}
 		msp->watch_stereo = 0;
 		switch (msp->opmode) {
 		case OPMODE_MANUAL:
@@ -1903,6 +1907,10 @@ static int msp_command(struct i2c_client *client, unsigned int cmd, void *arg)
 
 		dprintk("msp34xx: VIDIOCSCHAN (norm=%d)\n",vc->norm);
 		msp->norm = vc->norm;
+		if (IS_MSP34XX_G(msp)) {
+			msp34xxg_reset(client);
+			break;
+		}
 		msp_wake_thread(client);
 		break;
 	}
@@ -1912,6 +1920,10 @@ static int msp_command(struct i2c_client *client, unsigned int cmd, void *arg)
 	{
 		/* new channel -- kick audio carrier scan */
 		dprintk("msp34xx: VIDIOCSFREQ\n");
+		if (IS_MSP34XX_G(msp)) {
+			msp34xxg_reset(client);
+			break;
+		}
 		msp_wake_thread(client);
 		break;
 	}
