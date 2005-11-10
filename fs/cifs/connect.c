bddf78adc1b45784925fed47c4a08b7f9588086f
Steve French <sfrench@us.ibm.com>
2005-11-10 15:33:38 -0800

    [CIFS] Reserve upcall IDX value for CIFS with connector header and add Kconfig option for CIFS upcall.
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ntlmssp.h"
 #include "nterr.h"
 #include "rfc1002pdu.h"
+#include "cn_cifs.h"
 
 #define CIFS_PORT 445
 #define RFC1001_PORT 139
