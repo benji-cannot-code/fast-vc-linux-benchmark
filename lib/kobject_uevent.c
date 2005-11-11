3b44f4bd60422ee5fbdb8ba9fbbf2e192df7da66
Kay Sievers <kay.sievers@suse.de>
2005-11-11 06:09:55 +0100

    [PATCH] remove mount/umount uevents from superblock handling
    The names of these events have been confusing from the beginning
    on, as they have been more like claim/release events. We needed these
    events for noticing HAL if storage devices have been mounted.
    
    Thanks to Al, we have the proper solution now and can poll()
    /proc/mounts instead to get notfied about mount tree changes.
    
    Signed-off-by: Kay Sievers <kay.sievers@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -40,10 +40,6 @@ static char *action_to_string(enum kobject_action action)
 		return "remove";
 	case KOBJ_CHANGE:
 		return "change";
-	case KOBJ_MOUNT:
-		return "mount";
-	case KOBJ_UMOUNT:
-		return "umount";
 	case KOBJ_OFFLINE:
 		return "offline";
 	case KOBJ_ONLINE:
