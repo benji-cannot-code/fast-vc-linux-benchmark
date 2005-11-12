3ff8189de370600fc754239ebedc32f0222b4891
Dave Airlie <airlied@starflyer.(none)>
2005-11-12 21:52:46 +1100

    drm: update VIA driver to 2.7.2
    Add PCI DMA blitengine to VIA DRM
    Add portability code for porting VIA to FreeBSD.
    Sync via_drm.h with 3d driver
    
    From: Thomas Hellstrom <unichrome@shipmail.org>, Eric Anholt <anholt@freebsd.org>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_os_linux.h
+++ drivers/char/drm/drm_os_linux.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRM_ERR(d)			-(d)
 /** Current process ID */
 #define DRM_CURRENTPID			current->pid
+#define DRM_SUSER(p)			capable(CAP_SYS_ADMIN)
 #define DRM_UDELAY(d)			udelay(d)
 /** Read a byte from a MMIO region */
 #define DRM_READ8(map, offset)		readb(((void __iomem *)(map)->handle) + (offset))
