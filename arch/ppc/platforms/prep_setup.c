8a50caa8ad9691ccef59a546adb843ed964a136e
Adrian Bunk <bunk@stusta.de>
2005-11-13 02:26:09 +0100

    [PATCH] PPC_PREP: remove unneeded exports
    This patch removes the EXPORT_SYMBOL'ed but completely unused variable
    ucSystemType and removes the unneeded EXPORT_SYMBOL(_prep_type).
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Acked-by: Tom Rini <trini@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc/platforms/prep_setup.c
+++ arch/ppc/platforms/prep_setup.c
@@ -73,7 +73,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 TODC_ALLOC();
 
-unsigned char ucSystemType;
 unsigned char ucBoardRev;
 unsigned char ucBoardRevMaj, ucBoardRevMin;
 
