6275c440e88f0972b7beaebeec2ab456a039d2a2
Paul Mackerras <paulus@samba.org>
2005-11-14 17:32:50 +1100

    powerpc: Fix 32-bit compile: PPC_MEMSTART was undeclared
    This defines PPC_MEMSTART as 0 because it is still used in a couple
    of places in the 32-bit code.
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/page_32.h
+++ include/asm-powerpc/page_32.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VM_DATA_DEFAULT_FLAGS	VM_DATA_DEFAULT_FLAGS32
 
+#define PPC_MEMSTART	0
+
 #ifndef __ASSEMBLY__
 /*
  * The basic type of a PTE - 64 bits for those CPUs with > 32 bit
