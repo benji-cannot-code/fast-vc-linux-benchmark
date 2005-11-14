3bc496f60b0329e4c7dc68ac6ab31ed8ab218a1c
Nickolay V. Shmyrev <nshmyrev@yandex.ru>
2005-11-13 16:07:51 -0800

    [PATCH] v4l: (937) Included missing interrupt.h at saa7134-alsa.c
    Included missing interrupt.h at saa7134-alsa.c
    
    Signed-off-by: Nickolay V. Shmyrev <nshmyrev@yandex.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/media/video/saa7134/saa7134-alsa.c
+++ drivers/media/video/saa7134/saa7134-alsa.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/control.h>
 #include <sound/pcm.h>
 #include <sound/initval.h>
+#include <linux/interrupt.h>
 
 #include "saa7134.h"
 #include "saa7134-reg.h"
