b7b61659ab6176febfcf69717f1825d106004808
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-11-16 14:51:20 +0000

    [ARM] Include asm/hardware.h instead of asm/arch/hardware.h
    Rationalise hardware.h include.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-epxa10db/uncompress.h
+++ include/asm-arm/arch-epxa10db/uncompress.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include "asm/arch/platform.h"
-#include "asm/arch/hardware.h"
+#include "asm/hardware.h"
 #define UART00_TYPE (volatile unsigned int*)
 #include "asm/arch/uart00.h"
 
