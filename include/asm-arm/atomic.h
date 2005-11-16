0ab6ac9c78e04ea1a219700d50f8fd0592636a6b
Russell King <rmk@dyn-67.arm.linux.org.uk>
2005-11-16 17:23:57 +0000

    [ARM] Add linux/compiler.h includes where required
    atomic.h, bitops.h and mmu_context.h are using likely/unlikely.
    thread_info.h uses __attribute_const__.  Hence these files require
    linux/compiler.h to be included.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/atomic.h
+++ include/asm-arm/atomic.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARM_ATOMIC_H
 
 #include <linux/config.h>
+#include <linux/compiler.h>
 
 typedef struct { volatile int counter; } atomic_t;
 
