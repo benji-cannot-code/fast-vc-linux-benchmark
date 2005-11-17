20f4a0e3c1d1cd3b410914cb81173a5844af8182
Takashi Iwai <tiwai@suse.de>
2005-11-17 10:22:32 +0100

    [ALSA] Remove tea6330t struct definition from public header
    Modules: I2C tea6330t
    
    Remove tea6330t struct definition from public header.
    It's anyway unaccessible from outside.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/i2c/tea6330t.c
+++ sound/i2c/tea6330t.c
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <sound/core.h>
+#include <sound/control.h>
 #include <sound/tea6330t.h>
 
 MODULE_AUTHOR("Jaroslav Kysela <perex@suse.cz>");
@@ -43,6 +44,17 @@ MODULE_LICENSE("GPL");
 #define TEA6330T_SADDR_AUDIO_SWITCH	0x05	/* audio switch */
 #define   TEA6330T_GMU			0x80	/* mute control, general mute */
 #define   TEA6330T_EQN			0x40	/* equalizer switchover (0=equalizer-on) */
+
+typedef struct {
+	snd_i2c_device_t *device;
+	snd_i2c_bus_t *bus;
+	int equalizer;
+	int fader;
+	unsigned char regs[8];
+	unsigned char mleft, mright;
+	unsigned char bass, treble;
+	unsigned char max_bass, max_treble;
+} tea6330t_t;
 
 int snd_tea6330t_detect(snd_i2c_bus_t *bus, int equalizer)
 {
