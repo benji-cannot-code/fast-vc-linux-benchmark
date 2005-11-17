5b672cdbe54dd824b0521bafc680a275540b11ce
Kyle McMartin <kyle@parisc-linux.org>
2005-11-17 16:27:44 -0500

    [PARISC] Fix uniprocessor build by dummying smp_send_all_nop()
    Since irq.c uses smp_send_all_nop, we must define it for UP builds
    as well. Make it a static inline so it gets optimized away. This forces
    irq.c to include <asm/smp.h> though.
    
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- arch/parisc/kernel/irq.c
+++ arch/parisc/kernel/irq.c
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/types.h>
 
+#include <asm/smp.h>
+
 #undef PARISC_IRQ_CR16_COUNTS
 
 extern irqreturn_t timer_interrupt(int, void *, struct pt_regs *);
