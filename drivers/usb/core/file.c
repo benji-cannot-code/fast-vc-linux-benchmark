90b2f4cb029bcde7b32e9d15e1dc4195b78ad847
Greg Kroah-Hartman <gregkh@suse.de>
2005-11-17 09:48:09 -0800

    [PATCH] USB: move CONFIG_USB_DEBUG checks into the Makefile
    This lets us remove a lot of code in the drivers that were all checking
    the same thing.  It also found some bugs in a few of the drivers, which
    has been fixed up.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/usb/core/file.c
+++ drivers/usb/core/file.c
@@ -20,12 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/spinlock.h>
 #include <linux/errno.h>
-
-#ifdef CONFIG_USB_DEBUG
-	#define DEBUG
-#else
-	#undef DEBUG
-#endif
 #include <linux/usb.h>
 
 #include "usb.h"
