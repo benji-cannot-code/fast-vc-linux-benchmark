3363c3149dd8012f43544477029d1af954e426f0
Paul Mackerras <paulus@samba.org>
2005-11-18 13:16:43 +1100

    powerpc: Update __NR_syscalls to account for SPU syscalls
    A previous patch ended up not increasing __NR_syscalls to account
    for the new SPU syscalls (probably my fault).
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/unistd.h
+++ include/asm-powerpc/unistd.h
@@ -300,7 +300,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_spu_run		278
 #define __NR_spu_create		279
 
-#define __NR_syscalls		278
+#define __NR_syscalls		280
 
 #ifdef __KERNEL__
 #define __NR__exit __NR_exit
