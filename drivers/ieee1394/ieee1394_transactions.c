dcc2d230c73e82b3ef9f9a0316e335d0c6fdbe48
Adrian Bunk <bunk@stusta.de>
2005-11-19 21:23:48 -0500

    Every file should #include the headers containing the prototypes for it's global functions.
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Jody McIntyre <scjody@modernduck.com>

--- drivers/ieee1394/ieee1394_transactions.c
+++ drivers/ieee1394/ieee1394_transactions.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ieee1394_core.h"
 #include "highlevel.h"
 #include "nodemgr.h"
+#include "ieee1394_transactions.h"
 
 
 #define PREP_ASYNC_HEAD_ADDRESS(tc) \
