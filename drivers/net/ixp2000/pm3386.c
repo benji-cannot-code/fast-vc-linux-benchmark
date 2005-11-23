c5dc0d7c63147d19e6277fe53f45f3f8cb92ff3d
Lennert Buytenhek <buytenh@wantstofly.org>
2005-11-23 12:49:21 +0100

    [PATCH] caleb/pm3386: include proper header files
    Make caleb.c and pm3386.c include their own header files, to catch
    incorrect prototype definitions.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/ixp2000/pm3386.c
+++ drivers/net/ixp2000/pm3386.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/netdevice.h>
 #include <asm/io.h>
+#include "pm3386.h"
 
 /*
  * Read from register 'reg' of PM3386 device 'pm'.
