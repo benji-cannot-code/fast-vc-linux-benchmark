3c872562755bb03d62f169319636e22efb60d901
Jean Delvare <khali@linux-fr.org>
2005-11-26 21:00:54 +0100

    [PATCH] i2c: Rework client usage count, 2 of 3
    Make I2C_CLIENT_ALLOW_USE the default for all i2c clients. It doesn't
    hurt if the usage count is actually never used for any given driver,
    and allows for nice code simplifications in i2c-core.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/media/video/saa7134/saa6752hs.c
+++ drivers/media/video/saa7134/saa6752hs.c
@@ -609,7 +609,6 @@ static struct i2c_driver driver = {
 static struct i2c_client client_template =
 {
 	.name       = "saa6752hs",
-	.flags      = I2C_CLIENT_ALLOW_USE,
 	.driver     = &driver,
 };
 
