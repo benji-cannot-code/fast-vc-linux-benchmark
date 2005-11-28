78288697df1cc5de6265cd5b708774c0661b57cf
NeilBrown <neilb@suse.de>
2005-11-28 13:44:10 -0800

    [PATCH] md: fix locking problem in r5/r6
    bitmap_unplug actually writes data (bits) to storage, so we shouldn't be
    holding a spinlock...
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/raid6main.c
+++ drivers/md/raid6main.c
@@ -1785,7 +1785,9 @@ static void raid6d (mddev_t *mddev)
 
 		if (conf->seq_flush - conf->seq_write > 0) {
 			int seq = conf->seq_flush;
+			spin_unlock_irq(&conf->device_lock);
 			bitmap_unplug(mddev->bitmap);
+			spin_lock_irq(&conf->device_lock);
 			conf->seq_write = seq;
 			activate_bit_delay(conf);
 		}
