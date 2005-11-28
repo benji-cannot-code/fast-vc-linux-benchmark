8f14b6418df0a6ea58ab587b8efe3d3fce2bea36
Jeff Garzik <jgarzik@pobox.com>
2005-11-28 13:54:22 -0500

    [bonding] Remove superfluous changelog.
    No need to record this information in source code, its all in the git
    repository, and kernel archives.

--- drivers/net/bonding/bond_alb.h
+++ drivers/net/bonding/bond_alb.h
@@ -19,15 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The full GNU General Public License is included in this distribution in the
  * file called LICENSE.
  *
- *
- * Changes:
- *
- * 2003/08/06 - Amir Noam <amir.noam at intel dot com>
- *	- Add support for setting bond's MAC address with special
- *	  handling required for ALB/TLB.
- *
- * 2003/12/01 - Shmulik Hen <shmulik.hen at intel dot com>
- *	- Code cleanup and style changes
  */
 
 #ifndef __BOND_ALB_H__
