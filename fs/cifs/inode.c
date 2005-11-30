dc4228b140543abe9e29e473ac50ca578caa5d02
Steve French <sfrench@us.ibm.com>
2005-11-29 21:22:19 -0800

    [CIFS] Missing parenthesis and typo in previous fix
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/inode.c
+++ fs/cifs/inode.c
@@ -779,6 +779,7 @@ int cifs_mkdir(struct inode *inode, struct dentry *direntry, int mode)
 						current->fsgid;
 				}
 			}
+		}
 	}
 	kfree(full_path);
 	FreeXid(xid);
@@ -1125,7 +1126,7 @@ int cifs_setattr(struct dentry *direntry, struct iattr *attrs)
 	cifs_sb = CIFS_SB(direntry->d_inode->i_sb);
 	pTcon = cifs_sb->tcon;
 
-	if (cifs_sb->mnt_cifs_flags & CIFS_MOUNT_NO_PERM == 0) {
+	if ((cifs_sb->mnt_cifs_flags & CIFS_MOUNT_NO_PERM) == 0) {
 		/* check if we have permission to change attrs */
 		rc = inode_change_ok(direntry->d_inode, attrs);
 		if(rc < 0) {
