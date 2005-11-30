9fc9d21d745e53df29942bc44428bb275c57833f
Takashi Iwai <tiwai@suse.de>
2005-11-30 10:12:26 +0100

    [ALSA] ad1848 - Fix compilation without CONFIG_PM
    Modules: AD1848 driver
    
    Fix compilation without CONFIG_PM.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/isa/ad1848/ad1848_lib.c
+++ sound/isa/ad1848/ad1848_lib.c
@@ -933,8 +933,10 @@ int snd_ad1848_create(struct snd_card *card,
 		return err;
 	}
 
+#ifdef CONFIG_PM
 	chip->suspend = snd_ad1848_suspend;
 	chip->resume = snd_ad1848_resume;
+#endif
 
 	*rchip = chip;
 	return 0;
