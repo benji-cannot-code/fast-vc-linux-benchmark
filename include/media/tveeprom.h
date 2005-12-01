caacc603b0b4f952f3501504ef64e071b76b0832
Steven Toth <stoth@hauppauge.com>
2005-12-01 00:51:27 -0800

    [PATCH] V4L: tveeprom MAC address parsing/cleanup
    - Added a mac address field to the tveeprom structure.
    
    - allow callers to query the MAC address.
    
    - removed some redundant eeprom parsing code in cx88-cards.c (specific to
      Hauppauge DVB products) Instead, placed calls directly to the single eeprom
      parsing function in tveeprom.c
    
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/media/tveeprom.h
+++ include/media/tveeprom.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct tveeprom {
 	u32 has_radio;
 	u32 has_ir;     /* 0: no IR, 1: IR present, 2: unknown */
+	u32 has_MAC_address; /* 0: no MAC, 1: MAC present, 2: unknown */
 
 	u32 tuner_type;
 	u32 tuner_formats;
@@ -22,6 +23,7 @@ struct tveeprom {
 	u32 revision;
 	u32 serial_number;
 	char rev_str[5];
+	u8 MAC_address[6];
 };
 
 void tveeprom_hauppauge_analog(struct i2c_client *c, struct tveeprom *tvee,
