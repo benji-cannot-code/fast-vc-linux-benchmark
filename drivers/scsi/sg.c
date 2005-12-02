6e09daad81997895447c260555245694d828e827
Hugh Dickins <hugh@veritas.com>
2005-12-02 15:59:59 +0000

    [SCSI] sg and st unmap_user_pages allow PageReserved
    2.6.15-rc1 made sg's st_unmap_user_pages and st's sgl_unmap_user_pages
    BUG on a PageReserved page.  But that's wrong: they could be unmapping
    the ZERO_PAGE, which is marked PG_reserved; and perhaps others (while
    get_user_pages is still permitted on VM_PFNMAP areas - that may change).
    
    More change is needed here: sg claims to dirty even pages written from,
    and st claims not to dirty even pages read into; and SetPageDirty is not
    adequate for this nowadays.  Fixes to those follow in a later patch: for
    the moment just fix the 2.6.15 regression.
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Acked-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/sg.c
+++ drivers/scsi/sg.c
@@ -1879,8 +1879,6 @@ st_unmap_user_pages(struct scatterlist *sgl, const unsigned int nr_pages,
 	for (i=0; i < nr_pages; i++) {
 		struct page *page = sgl[i].page;
 
-		/* XXX: just for debug. Remove when PageReserved is removed */
-		BUG_ON(PageReserved(page));
 		if (dirtied)
 			SetPageDirty(page);
 		/* unlock_page(page); */
