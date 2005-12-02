c79c43f7aa52f4222fc77f80faf4d467fedeeb4f
Steve French <sfrench@us.ibm.com>
2005-12-01 22:32:42 -0800

    [CIFS] Kerberos and CIFS ACL support part 1
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifspdu.h
+++ fs/cifs/cifspdu.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *   fs/cifs/cifspdu.h
  *
- *   Copyright (c) International Business Machines  Corp., 2002
+ *   Copyright (c) International Business Machines  Corp., 2002,2005
  *   Author(s): Steve French (sfrench@us.ibm.com)
  *
  *   This library is free software; you can redistribute it and/or modify
