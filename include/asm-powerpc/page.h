0f8074535e0bb262f9bf682fd97159190d68098e
Michael Ellerman <michael@ellerman.id.au>
2005-12-04 18:39:23 +1100

    [PATCH] powerpc: Add CONFIG_CRASH_DUMP
    This patch adds a Kconfig variable, CONFIG_CRASH_DUMP, which configures the
    built kernel for use as a Kdump kernel.
    
    Currently "all" this involves is changing the value of KERNELBASE to 32 MB.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/page.h
+++ include/asm-powerpc/page.h
@@ -52,8 +52,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * If you want to test if something's a kernel address, use is_kernel_addr().
  */
+
+#ifdef CONFIG_CRASH_DUMP
+/* Kdump kernel runs at 32 MB, change at your peril. */
+#define PHYSICAL_START	0x2000000
+#else
+#define PHYSICAL_START	0x0
+#endif
+
 #define PAGE_OFFSET     ASM_CONST(CONFIG_KERNEL_START)
-#define KERNELBASE      PAGE_OFFSET
+#define KERNELBASE      (PAGE_OFFSET + PHYSICAL_START)
 
 #ifdef CONFIG_DISCONTIGMEM
 #define page_to_pfn(page)	discontigmem_page_to_pfn(page)
