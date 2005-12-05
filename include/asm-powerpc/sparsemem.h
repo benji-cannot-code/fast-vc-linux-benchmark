527c7e8ab686c653a3795f80144e895136369330
Mike Kravetz <kravetz@us.ibm.com>
2005-12-05 12:06:42 -0800

    [PATCH] powerpc: numa placement for dynamically added memory
    This places dynamically added memory within the appropriate
    numa node.  A new routine hot_add_scn_to_nid() replicates most of
    the memory scanning code in parse_numa_properties().
    
    Signed-off-by: Mike Kravetz <kravetz@us.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/sparsemem.h
+++ include/asm-powerpc/sparsemem.h
@@ -14,6 +14,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_MEMORY_HOTPLUG
 extern void create_section_mapping(unsigned long start, unsigned long end);
+#ifdef CONFIG_NUMA
+extern int hot_add_scn_to_nid(unsigned long scn_addr);
+#else
+static inline int hot_add_scn_to_nid(unsigned long scn_addr)
+{
+	return 0;
+}
+#endif /* CONFIG_NUMA */
 #endif /* CONFIG_MEMORY_HOTPLUG */
 
 #endif /* CONFIG_SPARSEMEM */
