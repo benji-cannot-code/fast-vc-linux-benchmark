9fc382279523a669a314081e66b620ca616517ae
Arnd Bergmann <arnd@arndb.de>
2005-12-05 22:52:22 -0500

    [PATCH] spufs: fix local store page refcounting
    With the new rules for reserved pages, the spufs now
    needs working page reference counting.
    
    I should probably look into converting to vm_insert_page,
    but for now this patch makes spufs work again.
    
    Signed-off-by: Arnd Bergmann <arndb@de.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/cell/spufs/file.c
+++ arch/powerpc/platforms/cell/spufs/file.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <linux/ioctl.h>
 #include <linux/module.h>
+#include <linux/pagemap.h>
 #include <linux/poll.h>
 
 #include <asm/io.h>
@@ -107,6 +108,7 @@ spufs_mem_mmap_nopage(struct vm_area_struct *vma,
 	if (type)
 		*type = VM_FAULT_MINOR;
 
+	page_cache_get(page);
 	return page;
 }
 
@@ -121,7 +123,6 @@ spufs_mem_mmap(struct file *file, struct vm_area_struct *vma)
 		return -EINVAL;
 
 	/* FIXME: */
-	vma->vm_flags |= VM_RESERVED;
 	vma->vm_page_prot = __pgprot(pgprot_val(vma->vm_page_prot)
 				     | _PAGE_NO_CACHE);
 
