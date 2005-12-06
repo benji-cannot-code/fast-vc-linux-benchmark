c21ecd85a652ecaf9b5423658ee413ceb4cda773
Jeff Garzik <jgarzik@pobox.com>
2005-12-06 05:01:27 -0500

    [libata] irq-pio: fix breakage related to err_mask merge

--- drivers/scsi/libata-core.c
+++ drivers/scsi/libata-core.c
@@ -4337,7 +4337,8 @@ fsm_start:
 		       ap->id, status, host_stat);
 
 		ap->hsm_task_state = HSM_ST_IDLE;
-		ata_qc_complete(qc, status | ATA_ERR);
+		qc->err_mask |= __ac_err_mask(status);
+		ata_qc_complete(qc);
 		break;
 	default:
 		goto idle_irq;
