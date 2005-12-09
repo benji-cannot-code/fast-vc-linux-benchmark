fca1c7c94a66d426bde81a64a1bb1e5dc2213efa
Stephen Hemminger <shemminger@osdl.org>
2005-12-09 11:35:05 -0800

    [PATCH] sky2: interrupt/poll optimization
    Optimize poll routine by not clearing interrupt till after
    processing, and checking for more work before re-enable.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -1737,7 +1737,6 @@ static int sky2_poll(struct net_device *dev0, int *budget)
 	u16 hwidx;
 	u16 tx_done[2] = { TX_NO_STATUS, TX_NO_STATUS };
 
-	sky2_write32(hw, STAT_CTRL, SC_STAT_CLR_IRQ);
 	hwidx = sky2_read16(hw, STAT_PUT_IDX);
 	BUG_ON(hwidx >= STATUS_RING_SIZE);
 	rmb();
@@ -1754,8 +1753,6 @@ static int sky2_poll(struct net_device *dev0, int *budget)
 		le = hw->st_le + hw->st_idx;
 		hw->st_idx = (hw->st_idx + 1) % STATUS_RING_SIZE;
 		prefetch(hw->st_le + hw->st_idx);
-
-		BUG_ON(le->link >= hw->ports || !hw->dev[le->link]);
 
 		BUG_ON(le->link >= 2);
 		dev = hw->dev[le->link];
@@ -1822,12 +1819,13 @@ static int sky2_poll(struct net_device *dev0, int *budget)
 	}
 
 exit_loop:
+	sky2_write32(hw, STAT_CTRL, SC_STAT_CLR_IRQ);
 	mmiowb();
 
 	sky2_tx_check(hw, 0, tx_done[0]);
 	sky2_tx_check(hw, 1, tx_done[1]);
 
-	if (work_done < to_do) {
+	if (sky2_read16(hw, STAT_PUT_IDX) == hw->st_idx) {
 		/* need to restart TX timer */
 		if (is_ec_a1(hw)) {
 			sky2_write8(hw, STAT_TX_TIMER_CTRL, TIM_STOP);
