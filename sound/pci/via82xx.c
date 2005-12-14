079cf54b108de4f3f6a8ac7df883c739f3d6acfb
Takashi Iwai <tiwai@suse.de>
2005-12-14 12:00:46 +0100

    [ALSA] via82xx - Add a quirk for Targa Traveller 811
    Modules: VIA82xx driver
    
    Add an ac97 quirk entry for Targa Traveller 811.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/via82xx.c
+++ sound/pci/via82xx.c
@@ -1764,6 +1764,12 @@ static struct ac97_quirk ac97_quirks[] = {
 		.name = "Arima Notebook",
 		.type = AC97_TUNE_HP_ONLY,
 	},
+	{
+		.subvendor = 0x161f,
+		.subdevice = 0x2032,
+		.name = "Targa Traveller 811",
+		.type = AC97_TUNE_HP_ONLY,
+	},
 	{ } /* terminator */
 };
 
