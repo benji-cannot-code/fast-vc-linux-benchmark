3a2ab1f244bdadd7dc4e934a6f3f19133fd95d94
Antonino A. Daplas <adaplas@gmail.com>
2005-12-19 15:11:40 +0800

    [PATCH] intelfb: Fix freeing of nonexistent resource
    Fix intelfb trying to free a non-existent resource in its error path.
    
    Signed-off-by: Antonino Daplas <adaplas@pol.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/intelfb/intelfbdrv.c
+++ drivers/video/intelfb/intelfbdrv.c
@@ -474,9 +474,9 @@ cleanup(struct intelfb_info *dinfo)
 	if (dinfo->aperture.virtual)
 		iounmap((void __iomem *)dinfo->aperture.virtual);
 
-	if (dinfo->mmio_base_phys)
+	if (dinfo->flag & INTELFB_MMIO_ACQUIRED)
 		release_mem_region(dinfo->mmio_base_phys, INTEL_REG_SIZE);
-	if (dinfo->aperture.physical)
+	if (dinfo->flag & INTELFB_FB_ACQUIRED)
 		release_mem_region(dinfo->aperture.physical,
 				   dinfo->aperture.size);
 	framebuffer_release(dinfo->info);
@@ -573,6 +573,9 @@ intelfb_pci_register(struct pci_dev *pdev, const struct pci_device_id *ent)
 		cleanup(dinfo);
 		return -ENODEV;
 	}
+
+	dinfo->flag |= INTELFB_FB_ACQUIRED;
+
 	if (!request_mem_region(dinfo->mmio_base_phys,
 				INTEL_REG_SIZE,
 				INTELFB_MODULE_NAME)) {
@@ -580,6 +583,8 @@ intelfb_pci_register(struct pci_dev *pdev, const struct pci_device_id *ent)
 		cleanup(dinfo);
 		return -ENODEV;
 	}
+
+	dinfo->flag |= INTELFB_MMIO_ACQUIRED;
 
 	/* Get the chipset info. */
 	dinfo->pci_chipset = pdev->device;
