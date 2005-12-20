e42c6e43c26d187bd63172b8a55036e24947b172
Stephen Hemminger <shemminger@osdl.org>
2005-12-20 15:08:09 -0800

    [PATCH] sky2: handle hardware packet overrun
    It is possible for hardware to get confused when an oversized frame
    is received. In that case, just drop the packet and increment a counter.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -1695,6 +1695,9 @@ static struct sk_buff *sky2_receive(struct sky2_port *sky2,
 	if (!(status & GMR_FS_RX_OK))
 		goto resubmit;
 
+	if ((status >> 16) != length || length > sky2->rx_bufsize)
+		goto oversize;
+
 	if (length < copybreak) {
 		skb = alloc_skb(length + 2, GFP_ATOMIC);
 		if (!skb)
@@ -1736,7 +1739,13 @@ resubmit:
 
 	return skb;
 
+oversize:
+	++sky2->net_stats.rx_over_errors;
+	goto resubmit;
+
 error:
+	++sky2->net_stats.rx_errors;
+
 	if (netif_msg_rx_err(sky2))
 		printk(KERN_INFO PFX "%s: rx error, status 0x%x length %d\n",
 		       sky2->netdev->name, status, length);
