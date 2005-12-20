b3cf8c2d5c8ad6ceeff2688a3831196ca6aa0c7b
Paul Mackerras <paulus@samba.org>
2005-12-20 18:43:52 +1100

    powerpc: Fix some #ifndef __KERNEL__ that should be #ifdef
    Grrr....
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/heathrow.h
+++ include/asm-powerpc/heathrow.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_POWERPC_HEATHROW_H
 #define _ASM_POWERPC_HEATHROW_H
-#ifndef __KERNEL__
+#ifdef __KERNEL__
 /*
  * heathrow.h: definitions for using the "Heathrow" I/O controller chip.
  *
