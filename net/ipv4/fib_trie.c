7e46cb61ddaaf7bf5ec7e486fe5856b8a2e39695
Stephen Hemminger <shemminger@osdl.org>
2006-01-03 14:38:34 -0800

    [IPV4] fib_trie: build fix
    Need this to fix build of fib_trie in net-2.6.16 (rebased) tree.
    The code needs the new inet_make_mask inline.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/fib_trie.c
+++ net/ipv4/fib_trie.c
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/in.h>
 #include <linux/inet.h>
+#include <linux/inetdevice.h>
 #include <linux/netdevice.h>
 #include <linux/if_arp.h>
 #include <linux/proc_fs.h>
