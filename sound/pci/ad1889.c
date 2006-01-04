b2cceaa66cc8e8d2953b32dec66dd4109a2186b6
Jaroslav Kysela <perex@suse.cz>
2006-01-04 10:26:16 +0100

    [ALSA] ad1889 - remove CVS ID from the driver identification
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/ad1889.c
+++ sound/pci/ad1889.c
@@ -51,7 +51,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ad1889.h"
 #include "ac97/ac97_id.h"
 
-#define	AD1889_DRVVER	"$Revision: 1.4 $"
+#define	AD1889_DRVVER	"Version: 1.7"
 
 MODULE_AUTHOR("Kyle McMartin <kyle@parisc-linux.org>, Thibaut Varene <t-bone@parisc-linux.org>");
 MODULE_DESCRIPTION("Analog Devices AD1889 ALSA sound driver");
