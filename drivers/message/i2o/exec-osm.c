b1fe841692837f669c1d533597c9bc2897d4a7d1
Markus Lidel <Markus.Lidel@shadowconnect.com>
2006-01-06 00:19:34 -0800

    [PATCH] I2O: Lindent run
    Signed-off-by: Markus Lidel <Markus.Lidel@shadowconnect.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/message/i2o/exec-osm.c
+++ drivers/message/i2o/exec-osm.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 #include <linux/string.h>
 #include <linux/slab.h>
-#include <linux/sched.h>   /* wait_event_interruptible_timeout() needs this */
+#include <linux/sched.h>	/* wait_event_interruptible_timeout() needs this */
 #include <asm/param.h>		/* HZ */
 #include "core.h"
 
