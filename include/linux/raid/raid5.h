daceb4e35651bdb53085c98e9ad653896e3facdd
NeilBrown <neilb@suse.de>
2006-01-06 00:20:17 -0800

    [PATCH] md: fix raid6 resync check/repair code
    raid6 currently does not check the P/Q syndromes when doing a resync, it just
    calculates the correct value and writes it.  Doing the check can reduce writes
    (often to 0) for a resync, and it is needed to properly implement the
    
      echo check > sync_action
    
    operation.
    
    This patch implements the appropriate checks and tidies up some related code.
    
    It also allows raid6 user-requested resync to bypass the intent bitmap.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/raid/raid5.h
+++ include/linux/raid/raid5.h
@@ -229,6 +229,8 @@ struct raid5_private_data {
 					    * Cleared when a sync completes.
 					    */
 
+	struct page 		*spare_page; /* Used when checking P/Q in raid6 */
+
 	/*
 	 * Free stripes pool
 	 */
