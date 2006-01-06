42df6575a0d773300a902b91fe22a057d700692b
NeilBrown <neilb@suse.de>
2006-01-06 00:20:12 -0800

    [PATCH] md: improve raid1 "IO Barrier" concept
    raid1 needs to put up a barrier to new requests while it does resync or other
    background recovery.  The code for this is currently open-coded, slighty
    obscure by its use of two waitqueues, and not documented.
    
    This patch gathers all the related code into 4 functions, and includes a
    comment which (hopefully) explains what is happening.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/raid/raid1.h
+++ include/linux/raid/raid1.h
@@ -46,6 +46,7 @@ struct r1_private_data_s {
 
 	spinlock_t		resync_lock;
 	int			nr_pending;
+	int			nr_waiting;
 	int			barrier;
 	sector_t		next_resync;
 	int			fullsync;  /* set to 1 if a full sync is needed,
@@ -53,8 +54,7 @@ struct r1_private_data_s {
 					    * Cleared when a sync completes.
 					    */
 
-	wait_queue_head_t	wait_idle;
-	wait_queue_head_t	wait_resume;
+	wait_queue_head_t	wait_barrier;
 
 	struct pool_info	*poolinfo;
 
