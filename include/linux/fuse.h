4ff19b99e1eb3604d10a9e0401533d1feeabbaa5
Miklos Szeredi <miklos@szeredi.hu>
2006-01-06 00:19:36 -0800

    [PATCH] fuse: bump interface version
    Change interface version to 7.4.
    
    Following changes will need backward compatibility support, so store the minor
    version returned by userspace.
    
    Signed-off-by: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/fuse.h
+++ include/linux/fuse.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FUSE_KERNEL_VERSION 7
 
 /** Minor version number of this interface */
-#define FUSE_KERNEL_MINOR_VERSION 3
+#define FUSE_KERNEL_MINOR_VERSION 4
 
 /** The node ID of the root inode */
 #define FUSE_ROOT_ID 1
