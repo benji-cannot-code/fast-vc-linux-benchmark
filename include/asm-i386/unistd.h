38059d5de904d6b25389fbcd024216f51bf646eb
Andrew Morton <akpm@osdl.org>
2006-01-06 00:11:09 -0800

    [PATCH] kill last zone_reclaim() bits
    Remove the last bits of Martin's ill-fated sys_set_zone_reclaim().
    
    Cc: Martin Hicks <mort@wildopensource.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/unistd.h
+++ include/asm-i386/unistd.h
@@ -257,7 +257,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_io_submit		248
 #define __NR_io_cancel		249
 #define __NR_fadvise64		250
-#define __NR_set_zone_reclaim	251
+/* 251 is available for reuse (was briefly sys_set_zone_reclaim) */
 #define __NR_exit_group		252
 #define __NR_lookup_dcookie	253
 #define __NR_epoll_create	254
