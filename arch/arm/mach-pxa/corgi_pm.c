2cca58c7713ea0a2e3eebc3dce1e483ea2464f85
Richard Purdie <rpurdie@rpsys.net>
2006-01-07 11:12:26 +0000

    [ARM] 3235/1: SharpSL PM: Fix a gcc4 build error
    Patch from Richard Purdie
    
    Fix a gcc4 build error (incomplete element type) in the pxa SharpSL
    PM code.
    
    Signed-off-by: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/corgi_pm.c
+++ arch/arm/mach-pxa/corgi_pm.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 #include <asm/hardware.h>
 #include <asm/hardware/scoop.h>
-#include <asm/hardware/sharpsl_pm.h>
 
 #include <asm/arch/sharpsl.h>
 #include <asm/arch/corgi.h>
--- arch/arm/mach-pxa/sharpsl_pm.c
+++ arch/arm/mach-pxa/sharpsl_pm.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/pm.h>
 #include <asm/arch/pxa-regs.h>
 #include <asm/arch/sharpsl.h>
-#include <asm/hardware/sharpsl_pm.h>
 #include "sharpsl.h"
 
 struct battery_thresh spitz_battery_levels_acin[] = {
