a39c43b3c216f70af53c6a9aa107d3d788b05c9f
David S. Miller <davem@davemloft.net>
2006-01-06 22:55:39 -0800

    [DCCP]: ipv6.c needs net/ip6_checksum.c
    Reported by Dave Jones.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ipv6.c
+++ net/dccp/ipv6.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ipv6.h>
 #include <net/protocol.h>
 #include <net/transp_v6.h>
+#include <net/ip6_checksum.h>
 #include <net/xfrm.h>
 
 #include "dccp.h"
