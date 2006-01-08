f8d9b02b4203e46f2319735586621af648ed60e6
Matt Mackall <mpm@selenic.com>
2006-01-08 01:05:26 -0800

    [PATCH] Make vm86 support optional
    This adds an option to remove vm86 support under CONFIG_EMBEDDED.  Saves
    about 5k.
    
    This version eliminates most of the #ifdefs of the previous version and
    instead uses function stubs in vm86.h.  Also, release_vm86_irqs is moved
    from asm-i386/irq.h to a more appropriate home in vm86.h so that the stubs
    can live together.
    
    $ size vmlinux-baseline vmlinux-novm86
       text    data     bss     dec     hex filename
    2920821  523232  190652 3634705  377611 vmlinux-baseline
    2916268  523100  190492 3629860  376324 vmlinux-novm86
    
    Signed-off-by: Matt Mackall <mpm@selenic.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/kernel/process.c
+++ arch/i386/kernel/process.c
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 #include <asm/i387.h>
 #include <asm/desc.h>
+#include <asm/vm86.h>
 #ifdef CONFIG_MATH_EMULATION
 #include <asm/math_emu.h>
 #endif
