e92413b3e41e029304fb9d53861330136dd6dd2c
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2006-01-08 01:04:49 -0800

    [PATCH] PTRACE_SYSEMU is only for i386 and clashes with other ptrace codes of other archs
    PTRACE_SYSEMU{,_SINGLESTEP} is actually arch specific, for now, and the
    current allocated number clashes with a ptrace code of frv, i.e.
    PTRACE_GETFDPIC.  I should have submitted this much earlier, anyway we get no
    breakage for this.
    
    CC: Daniel Jacobowitz <dan@debian.org>
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/ptrace.h
+++ include/linux/ptrace.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PTRACE_DETACH		0x11
 
 #define PTRACE_SYSCALL		  24
-#define PTRACE_SYSEMU		  31
-#define PTRACE_SYSEMU_SINGLESTEP  32
 
 /* 0x4200-0x4300 are reserved for architecture-independent additions.  */
 #define PTRACE_SETOPTIONS	0x4200
