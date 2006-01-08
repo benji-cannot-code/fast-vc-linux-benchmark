28162b64c15f5da6fc1e571d9708fce304eec43f
Woody Suwalski <woodys@xandros.com>
2006-01-08 01:01:29 -0800

    [PATCH] ARM: Netwinder ds1620 driver needs an export to be built as module
    ds1620 module is using gpio_read symbol, so works only if "built-in" symbol
    needs to be exported from the kernel image
    
    Signed-off-by: Woody Suwalski <woodys@xandros.com>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/arm/mach-footbridge/netwinder-hw.c
+++ arch/arm/mach-footbridge/netwinder-hw.c
@@ -602,6 +602,7 @@ EXPORT_SYMBOL(gpio_lock);
 EXPORT_SYMBOL(gpio_modify_op);
 EXPORT_SYMBOL(gpio_modify_io);
 EXPORT_SYMBOL(cpld_modify);
+EXPORT_SYMBOL(gpio_read);
 
 /*
  * Initialise any other hardware after we've got the PCI bus
