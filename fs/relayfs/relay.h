07798a5e520ce8ee9b332d6bbab07ed11ff3c428
Tom Zanussi <zanussi@us.ibm.com>
2006-01-08 01:02:23 -0800

    [PATCH] relayfs: decouple buffer creation from inode creation
    The patch series implementa or fixes 3 things that were specifically requested
    or suggested by relayfs users:
    
    - support for non-relay files (patches 1-6)
    
    Currently, the relayfs API only supports the creation of directories
    (relayfs_create_dir()) and relay files (relay_open()).  These patches adds
    support for non-relay files (relayfs_create_file()).  This is so relayfs
    applications can create 'control files' in relayfs itself rather than in /proc
    or via a netlink channel, as is currently done in the relay-app examples.
    Basically what this amounts to is exporting relayfs_create_file() with an
    additional file_ops param that clients can use to supply file operations for
    their own special-purpose files in relayfs.
    
    - make exported relay file ops useful (patches 7-8)
    
    The relayfs relay_file_operations have always been exported, the intent being
    to make it possible to create relay files in other filesystems such as
    debugfs.  The problem, though, is that currently the file operations are too
    tightly coupled to relayfs to actually be used for this purpose.  This patch
    fixes that by adding a couple of callback functions that allow a client to
    hook into relay_open()/close() and supply the files that will be used to
    represent the channel buffers; the default implementation if no callbacks are
    defined is to create the files in relayfs.
    
    - add an option to create global relay buffer (patches 9-10) The file creation
    callback also supplies an optional param, is_global, that can be used by
    clients to create a single global relayfs buffer instead of the default
    per-cpu buffers.  This was suggested as being useful for certain debugging
    applications where it's more convenient to be able to get all the data from a
    single channel without having to go to the bother of dealing with per-cpu
    files.
    
    - cleanup, some renaming and Documentation updates (patches 11-12)
    
    There were several comments that the use of netlink in the example code was
    non-intuitive and in fact the whole relay-app business was needlessly
    confusing.  Based on that feedback, the example code has been completely
    converted over to relayfs control files as supported by this patch, and have
    also been made completely self-contained.
    
    The converted examples along with a couple of new examples that demonstrate
    using exported relay files can be found in relay-apps tarball:
    http://prdownloads.sourceforge.net/relayfs/relay-apps-0.9.tar.gz?download
    
    This patch:
    
    Separate buffer create/destroy from inode create/destroy.  We want to be able
    to associate other data and not just relay buffers with inodes.  Buffer
    create/destroy is moved out of inode.c and into relayfs core code.
    
    Signed-off-by: Tom Zanussi <zanussi@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/relayfs/relay.h
+++ fs/relayfs/relay.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct dentry *relayfs_create_file(const char *name,
 				   struct dentry *parent,
 				   int mode,
-				   struct rchan *chan);
+				   void *data);
 extern int relayfs_remove(struct dentry *dentry);
 extern int relay_buf_empty(struct rchan_buf *buf);
 extern void relay_destroy_channel(struct kref *kref);
