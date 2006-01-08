718ef45c4ef64337532dc8e2973baa1a8be1ed8c
Tom Zanussi <zanussi@us.ibm.com>
2006-01-08 01:02:24 -0800

    [PATCH] relayfs: export relayfs_create_file() with fileops param
    This patch adds a mandatory fileops param to relayfs_create_file() and exports
    that function so that clients can use it to create files defined by their own
    set of file operations, in relayfs.  The purpose is to allow relayfs
    applications to create their own set of 'control' files alongside their relay
    files in relayfs rather than having to create them in /proc or debugfs for
    instance.  relayfs_create_file() is also used by relay_open_buf() to create
    the relay files for a channel.  In this case, a pointer to
    relayfs_file_operations is passed in, along with a pointer to the buffer
    associated with the file.
    
    Signed-off-by: Tom Zanussi <zanussi@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/relayfs/relay.h
+++ fs/relayfs/relay.h
@@ -2,10 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _RELAY_H
 #define _RELAY_H
 
-struct dentry *relayfs_create_file(const char *name,
-				   struct dentry *parent,
-				   int mode,
-				   void *data);
 extern int relayfs_remove(struct dentry *dentry);
 extern int relay_buf_empty(struct rchan_buf *buf);
 extern void relay_destroy_channel(struct kref *kref);
