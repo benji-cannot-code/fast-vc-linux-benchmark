e85d8fd024b515ae8974121fd9b3a08599336e92
Michael H. Schimek <mschimek@gmx.at>
2006-01-09 15:25:27 -0200

    V4L/DVB (3178): bttv VBI fixes
    - V4L2_(G|S|TRY)_FMT returned incorrect VBI start lines for PAL-M,
    NTSC-JP, and PAL-60. They also returned an inaccurate VBI offset.
    - V4L2_(G|S)_FMT and V4L2_TRY_FMT disagreed about the start of VBI
    capturing in PAL and SECAM second field. Note the start line fixes
    may break applications using VIDIOCSVBIFMT because this ioctl fails
    when the driver does not support exactly the requested parameters.
    - V4L2_TRY_FMT did not clear the reserved field in struct
    v4l2_vbi_format.
    - V4L2_(S|TRY)_FMT did not expect very large or small VBI start or
    count values, returning wrong (but safe) counts due to an overflow.
    - VIDIOCGVBIFMT confused V4L and V4L2 VBI flags. However this had no
    effect because the flags have the same value and bttv never sets
    them.
    - In v4l_compat_translate_ioctl() the VIDIOC(G|S)VBIFMT code did not
    expect V4L2 drivers supporting VBI formats besides V4L2_PIX_FMT_GREY.
    
    Signed-off-by: Michael H. Schimek <mschimek@gmx.at>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>

--- drivers/media/video/bttvp.h
+++ drivers/media/video/bttvp.h
@@ -74,6 +74,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define UNSET (-1U)
 
+#define clamp(x, low, high) min (max (low, x), high)
+
 /* ---------------------------------------------------------- */
 
 struct bttv_tvnorm {
@@ -89,6 +91,9 @@ struct bttv_tvnorm {
 	u8    vbipack;
 	u16   vtotal;
 	int   sram;
+	/* ITU-R frame line number of the first VBI line we can
+	   capture, of the first and second field. */
+	u16   vbistart[2];
 };
 extern const struct bttv_tvnorm bttv_tvnorms[];
 
