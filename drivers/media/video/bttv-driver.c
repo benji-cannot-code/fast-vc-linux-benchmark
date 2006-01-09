9f86e875c2b466048b221becdef9664a51489e2a
Hans Verkuil <hverkuil@xs4all.nl>
2006-01-09 15:25:16 -0200

    V4L/DVB (3105): Remove AUDC_CONFIG_PINNACLE horror, fix mt20xx radio support.
    - Remove AUDC_CONFIG_PINNACLE horror. This also fixes radio support
    for mt20xx tuners.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>

--- drivers/media/video/bttv-driver.c
+++ drivers/media/video/bttv-driver.c
@@ -3948,7 +3948,6 @@ static int __devinit bttv_probe(struct pci_dev *dev,
 
 	btv->i2c_rc = -1;
 	btv->tuner_type  = UNSET;
-	btv->pinnacle_id = UNSET;
 	btv->new_input   = UNSET;
 	btv->has_radio=radio[btv->c.nr];
 
