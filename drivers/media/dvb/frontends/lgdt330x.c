6d723bfc6d0611d3f3be24b52f57ff1e97242f3c
Michael Krufky <mkrufky@m1k.net>
2006-01-09 15:32:42 -0200

    V4L/DVB (3287): Add DViCO Bluebird firmware to dvb_get_firmware script
    - Add DViCO Bluebird firmware to dvb_get_firmware script,
    for FusionHDTV USB devices.
    - Use usb alt setting 0 for EP4 transfer (dvb-t),
    - Use usb alt setting 7 for EP2 transfer (atsc)
    - Added comment to lgdt330x.c to indicate support for
    DViCO FusionHDTV5 USB Gold.
    Thanks to: Jeff Lee <JeffLee@dvico.com>
    
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@brturbo.com.br>

--- drivers/media/dvb/frontends/lgdt330x.c
+++ drivers/media/dvb/frontends/lgdt330x.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *   DViCO FusionHDTV 3 Gold-T
  *   DViCO FusionHDTV 5 Gold
  *   DViCO FusionHDTV 5 Lite
+ *   DViCO FusionHDTV 5 USB Gold
  *   Air2PC/AirStar 2 ATSC 3rd generation (HD5000)
  *
  * TODO:
