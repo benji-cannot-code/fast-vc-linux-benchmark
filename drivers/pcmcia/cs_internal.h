f39f038d31b4f1608f254e7dabc260d1d7aa3c87
Dominik Brodowski <linux@dominikbrodowski.net>
2006-01-10 21:35:49 +0100

    [PATCH] pcmcia: remove include of config.h
    Remove the inclusion of include/config.h as it isn't needed any longer.
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/cs_internal.h
+++ drivers/pcmcia/cs_internal.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_CS_INTERNAL_H
 #define _LINUX_CS_INTERNAL_H
 
-#include <linux/config.h>
 #include <linux/kref.h>
 
 /* Flags in client state */
