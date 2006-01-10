dc06aa465f74173e7aa1ccddfc60ea499a32e7da
Adrian Bunk <bunk@stusta.de>
2006-01-09 20:54:06 -0800

    [PATCH] fs/ext3/: small cleanups
    This patch contains the following cleanups:
    - there's no need for ext3_count_free() #ifndef EXT3FS_DEBUG
    - having prototypes for ext3_count_free() in two different headers is
      nonsense
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/ext3/bitmap.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/*  linux/fs/ext3/bitmap.c
- *
- * Copyright (C) 2005 Simtec Electronics
- *	Ben Dooks <ben@simtec.co.uk>
- *
-*/
-
-extern unsigned long ext3_count_free (struct buffer_head *, unsigned int );
