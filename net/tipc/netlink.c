4217a4d38e66d58436ba11bedf7a841e4d1d3bb3
Jon Maloy <jon.maloy@ericsson.com>
2006-01-10 18:54:24 +0000

    [TIPC} Fixed bug in disc_timeout()
    Signed-off-by: Jon Maloy <jon.maloy@ericsson.com>

--- net/tipc/netlink.c
+++ net/tipc/netlink.c
@@ -81,6 +81,8 @@ static int family_registered = 0;
 
 int netlink_start(void)
 {
+
+
 	if (genl_register_family(&family))
 		goto err;
 
@@ -89,7 +91,7 @@ int netlink_start(void)
 	if (genl_register_ops(&family, &ops))
 		goto err_unregister;
 
-	return 0;
+        return 0;
 
  err_unregister:
 	genl_unregister_family(&family);
