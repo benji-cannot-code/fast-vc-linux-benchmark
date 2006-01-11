f63f34bf7fec1a51e1fc18f0d9a4590c7af64442
Chuck Ebbert <76306.1226@compuserve.com>
2006-01-11 22:46:03 +0100

    [PATCH] x86_64: allow setting RF in EFLAGS
    Setting RF (resume flag) allows a debugger to resume execution after a code
    breakpoint without tripping the breakpoint again.  It is reset by the CPU
    after executing one instruction.
    
    Signed-off-by: Chuck Ebbert <76306.1226@compuserve.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/ptrace.c
+++ arch/x86_64/kernel/ptrace.c
@@ -37,9 +37,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * in exit.c or in signal.c.
  */
 
-/* determines which flags the user has access to. */
-/* 1 = access 0 = no access */
-#define FLAG_MASK 0x44dd5UL
+/*
+ * Determines which flags the user has access to [1 = access, 0 = no access].
+ * Prohibits changing ID(21), VIP(20), VIF(19), VM(17), IOPL(12-13), IF(9).
+ * Also masks reserved bits (63-22, 15, 5, 3, 1).
+ */
+#define FLAG_MASK 0x54dd5UL
 
 /* set's the trap flag. */
 #define TRAP_FLAG 0x100UL
