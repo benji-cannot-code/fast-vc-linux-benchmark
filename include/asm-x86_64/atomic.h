9c506a5ddd282e06388a95d1858c8aec87fbe940
Andi Kleen <ak@suse.de>
2006-01-11 22:46:12 +0100

    [PATCH] x86_64: Make sure BITS_PER_ATOMIC is defined in asm-generic/atomic.h
    Fixes
    
      CC      fs/nfsctl.o
    In file included from include2/asm/atomic.h:427,
                     from /home/lsrc/quilt/linux/include/linux/file.h:8,
                     from /home/lsrc/quilt/linux/fs/nfsctl.c:8:
    /home/lsrc/quilt/linux/include/asm-generic/atomic.h:20:5: warning: "BITS_PER_LONG" is not defined
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/atomic.h
+++ include/asm-x86_64/atomic.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_X86_64_ATOMIC__
 
 #include <linux/config.h>
+#include <asm/types.h>
 
 /* atomic_t should be 32 bit signed type */
 
