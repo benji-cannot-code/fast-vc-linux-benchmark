015f44bd222fc16c9b77034103555712dc17f99b
Per Liden <per.liden@nospam.ericsson.com>
2006-01-11 18:40:41 +0100

    [TIPC] Update of file headers
    The copyright statements from different parts of Ericsson
    have been merged into one.
    
    Signed-off-by: Per Liden <per.liden@nospam.ericsson.com>

--- net/tipc/netlink.c
+++ net/tipc/netlink.c
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * net/tipc/netlink.c: TIPC configuration handling
  * 
  * Copyright (c) 2005, Wind River Systems
- * Copyright (c) 2005-2006, Ericsson AB
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
