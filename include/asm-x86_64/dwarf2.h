96f87d8f567aab1650b4f24546dbde1d5e0feafa
Jan Beulich <jbeulich@novell.com>
2006-01-11 22:42:05 +0100

    [PATCH] x86_64: Separate CONFIG_UNWIND_INFO from CONFIG_DEBUG_INFO
    As a follow-up to the introduction of CONFIG_UNWIND_INFO, this
    separates the generation of frame unwind information for x86-64 from
    that of full debug information.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/dwarf2.h
+++ include/asm-x86_64/dwarf2.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    away for older version. 
  */
 
-#ifdef CONFIG_DEBUG_INFO
+#ifdef CONFIG_UNWIND_INFO
 
 #define CFI_STARTPROC .cfi_startproc
 #define CFI_ENDPROC .cfi_endproc
