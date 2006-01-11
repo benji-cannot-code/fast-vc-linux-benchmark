0d8c70b2842da890e80e4fc7c24872cc45e303c3
Per Liden <per.liden@nospam.ericsson.com>
2006-01-11 12:28:47 +0100

    [TIPC] Moved configuration interface into tipc_config.h
    Restored the old tipc_config.h to get a cleaner division between the
    interfaces used by normal TIPC users and TIPC administration utilities.
    
    Signed-off-by: Per Liden <per.liden@nospam.ericsson.com>

--- net/tipc/socket.c
+++ net/tipc/socket.c
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 
 #include <linux/tipc.h>
+#include <linux/tipc_config.h>
 #include <net/tipc/tipc_msg.h>
 #include <net/tipc/tipc_port.h>
 
