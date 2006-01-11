b867574bbf4b71af71b83dc736d8700dd29b3725
Andi Kleen <ak@suse.de>
2006-01-11 22:46:36 +0100

    [PATCH] x86_64: Remove unused AMD K8 C stepping flag
    X86_FEATURE_K8_C was a synthetic Linux CPUID flag that was used for some
    code optimizations in Opteron C stepping or later. But support for pre C
    stepping optimizations has been removed, so this isn't needed anymore.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/setup.c
+++ arch/x86_64/kernel/setup.c
@@ -880,7 +880,6 @@ static void __init amd_detect_cmp(struct cpuinfo_x86 *c)
 static int __init init_amd(struct cpuinfo_x86 *c)
 {
 	int r;
-	int level;
 
 #ifdef CONFIG_SMP
 	unsigned long value;
@@ -903,11 +902,6 @@ static int __init init_amd(struct cpuinfo_x86 *c)
 	   3DNow is IDd by bit 31 in extended CPUID (1*32+31) anyway */
 	clear_bit(0*32+31, &c->x86_capability);
 	
-	/* C-stepping K8? */
-	level = cpuid_eax(1);
-	if ((level >= 0x0f48 && level < 0x0f50) || level >= 0x0f58)
-		set_bit(X86_FEATURE_K8_C, &c->x86_capability);
-
 	r = get_model_name(c);
 	if (!r) { 
 		switch (c->x86) { 
