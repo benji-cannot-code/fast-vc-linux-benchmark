553cba03299595923c80fbf6e0b8da32a1461ef4
Nicolas Kaiser <nikai@nikai.net>
2006-01-11 02:04:35 +0100

    netfilter: headers included twice
    Headers included twice.
    
    Signed-off-by: Nicolas Kaiser <nikai@nikai.net>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- net/ipv4/netfilter/ip_conntrack_proto_tcp.c
+++ net/ipv4/netfilter/ip_conntrack_proto_tcp.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/tcp.h>
 
-#include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 #include <linux/netfilter_ipv4/ip_conntrack.h>
 #include <linux/netfilter_ipv4/ip_conntrack_protocol.h>
--- net/ipv4/netfilter/ip_conntrack_proto_udp.c
+++ net/ipv4/netfilter/ip_conntrack_proto_udp.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/udp.h>
 #include <linux/seq_file.h>
 #include <net/checksum.h>
-#include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 #include <linux/netfilter_ipv4/ip_conntrack_protocol.h>
 
