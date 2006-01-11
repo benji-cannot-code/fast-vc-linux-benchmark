15ccd52d545a32196477d0a90aa80646bf2779b4
Paul Fulghum <paulkf@microgate.com>
2006-01-11 17:51:48 -0600

    [PATCH] new tty buffering access fix
    Fix typos in new tty buffering that incorrectly
    access and update buffers in pending queue.
    
    Signed-off-by: Paul Fulghum <paulkf@microgate.com>
    Acked-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/tty_io.c
+++ drivers/char/tty_io.c
@@ -313,7 +313,7 @@ static struct tty_buffer *tty_buffer_find(struct tty_struct *tty, size_t size)
 
 int tty_buffer_request_room(struct tty_struct *tty, size_t size)
 {
-	struct tty_buffer *b = tty->buf.head, *n;
+	struct tty_buffer *b = tty->buf.tail, *n;
 	int left = 0;
 
 	/* OPTIMISATION: We could keep a per tty "zero" sized buffer to
@@ -327,7 +327,6 @@ int tty_buffer_request_room(struct tty_struct *tty, size_t size)
 	n = tty_buffer_find(tty, size);
 	if(n == NULL)
 		return left;
-	n->next = b;
 	if(b != NULL)
 		b->next = n;
 	else
@@ -2752,6 +2751,8 @@ static void flush_to_ldisc(void *private_)
 	spin_lock_irqsave(&tty->read_lock, flags);
 	while((tbuf = tty->buf.head) != NULL) {
 		tty->buf.head = tbuf->next;
+		if (tty->buf.head == NULL)
+			tty->buf.tail = NULL;
 		spin_unlock_irqrestore(&tty->read_lock, flags);
 		/* printk("Process buffer %p for %d\n", tbuf, tbuf->used); */
 		disc->receive_buf(tty, tbuf->char_buf_ptr,
@@ -2760,7 +2761,6 @@ static void flush_to_ldisc(void *private_)
 		spin_lock_irqsave(&tty->read_lock, flags);
 		tty_buffer_free(tty, tbuf);
 	}
-	tty->buf.tail = NULL;
 	spin_unlock_irqrestore(&tty->read_lock, flags);
 out:
 	tty_ldisc_deref(disc);
