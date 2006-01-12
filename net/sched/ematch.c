833f12bc09b3abf79729eef243f5b2413262a4e6
Evgeniy Polyakov <johnpol@2ka.mipt.ru>
2006-01-11 16:13:46 -0800

    [PKT_SCHED] ematch: Remove bogus include.
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/ematch.c
+++ net/sched/ematch.c
@@ -93,7 +93,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rtnetlink.h>
 #include <linux/skbuff.h>
 #include <net/pkt_cls.h>
-#include <config/net/ematch/stack.h>
 
 static LIST_HEAD(ematch_ops);
 static DEFINE_RWLOCK(ematch_mod_lock);
