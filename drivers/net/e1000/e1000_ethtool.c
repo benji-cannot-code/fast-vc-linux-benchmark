9082a0b924d7afdaae7e347255bd75e2a6c7cbc2
Jeff Kirsher <jeffrey.t.kirsher@intel.com>
2006-01-12 16:51:01 -0800

    [PATCH] e1000: Fix desc. clean up
    These were two separate community submitted patches.
    
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Signed-off-by: John Ronciak <john.ronciak@intel.com>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/e1000/e1000_ethtool.c
+++ drivers/net/e1000/e1000_ethtool.c
@@ -981,11 +981,11 @@ e1000_free_desc_rings(struct e1000_adapter *adapter)
 		}
 	}
 
-	if(txdr->desc) {
+	if (txdr->desc) {
 		pci_free_consistent(pdev, txdr->size, txdr->desc, txdr->dma);
 		txdr->desc = NULL;
 	}
-	if(rxdr->desc) {
+	if (rxdr->desc) {
 		pci_free_consistent(pdev, rxdr->size, rxdr->desc, rxdr->dma);
 		rxdr->desc = NULL;
 	}
@@ -994,6 +994,7 @@ e1000_free_desc_rings(struct e1000_adapter *adapter)
 	txdr->buffer_info = NULL;
 	kfree(rxdr->buffer_info);
 	rxdr->buffer_info = NULL;
+
 	return;
 }
 
