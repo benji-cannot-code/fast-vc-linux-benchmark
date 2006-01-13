bc5eac0a0be810bb0a843c5b53f0a78b98771dca
Michael Krufky <mkrufky@m1k.net>
2006-01-13 14:10:25 -0200

    V4L/DVB (3359): Redesign tuners struct for maximum flexibility
    - Tunertype struct redefined to allow one or more tuner_params structs
      per tuner definition, one for each video standard.
    - Each tuner_params struct has an element containing an arbitrary
      amount of tuner_ranges.
      (this is needed for dvb tuners - to be handled later)
    - A tuner_range may be referenced by multiple tuner_params structs.
      There are many duplicates in here. Reusing tuner_range structs,
      rather than defining new ones for each tuner, will cut down on
      memory usage, and is preferred when possible.
    - tunertype struct contains an element, has_tda988x.
      We must set this for all tunertypes that contain a tda988x
      chip, and then we can remove this setting from the various
      card structs.
    - Improves tuners array memory usage efficiency.
    - Right now, all tuners are using the first tuner_params[] array element
      for analog mode. In the future, we will be merging similar tuner
      definitions together, such that each tuner definition will have a
      tuner_params struct for each available video standard. At that point,
      the tuner_params[] array element will be chosen based on the video
      standard in use.
    
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _TUNER_H
 
 #include <linux/videodev2.h>
+#include <media/tuner-types.h>
 
 #define ADDR_UNSET (255)
 
