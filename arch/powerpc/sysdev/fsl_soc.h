3bf515edba47d7ebe8ee498fa10a5f8a6cdc6790
Kumar Gala <galak@gate.crashing.org>
2006-01-13 11:19:13 -0600

    [PATCH] powerpc: Add FSL SOC library and setup code
    Parse the flat device tree for devices on Freescale SOC's that we know
    about (gianfar, gianfar_mdio, i2c, mpc83xx_wdt).  We need to setup
    platform devices and platform data for these devices to match arch/ppc
    usage.
    
    Also add a helper function (get_immrbase) that reports the base
    address of the MMIO registers on the SOC.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

---
+++ arch/powerpc/sysdev/fsl_soc.h
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __PPC_FSL_SOC_H
+#define __PPC_FSL_SOC_H
+#ifdef __KERNEL__
+
+extern phys_addr_t get_immrbase(void);
+
+#endif
+#endif
