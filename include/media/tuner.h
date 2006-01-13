b843a74355f14f855cc2cab125de98ea886755a4
Hans Verkuil <hverkuil@xs4all.nl>
2006-01-13 14:10:25 -0200

    V4L/DVB (3363): Add support for Samsung tuner TCPN 2121P30A
    - Add support for Samsung tuner TCPN 2121P30A, used in
      Hauppauge PVR-500 cards.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -116,6 +116,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TUNER_PHILIPS_TUV1236D		68	/* ATI HDTV Wonder */
 #define TUNER_TNF_5335MF                69	/* Sabrent Bt848   */
+#define TUNER_SAMSUNG_TCPN_2121P30A     70 	/* Hauppauge PVR-500MCE NTSC */
 
 /* tv card specific */
 #define TDA9887_PRESENT 		(1<<0)
