4c828657c9af23e1bb69d5afae461e602d538027
Kyle McMartin <kyle@parisc-linux.org>
2006-01-13 22:05:21 -0700

    [PARISC] Remove obsolete _hlt cruft
    Remove a bunch of obsolete code from process.c, these routines were
    likely imported from the i386 version of process.c when the port
    started. The routines are only used in floppy.c, which I seriously
    doubt will ever work on parisc, due to architectural assumptions.
    
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- arch/parisc/kernel/process.c
+++ arch/parisc/kernel/process.c
@@ -55,27 +55,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/unwind.h>
 
-static int hlt_counter __read_mostly;
-
 /*
  * Power off function, if any
  */ 
 void (*pm_power_off)(void);
 EXPORT_SYMBOL(pm_power_off);
-
-void disable_hlt(void)
-{
-	hlt_counter++;
-}
-
-EXPORT_SYMBOL(disable_hlt);
-
-void enable_hlt(void)
-{
-	hlt_counter--;
-}
-
-EXPORT_SYMBOL(enable_hlt);
 
 void default_idle(void)
 {
