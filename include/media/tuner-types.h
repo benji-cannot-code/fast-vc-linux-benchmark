d1edf7688f78e34808925360c278b093ec6d5e2f
Michael Krufky <mkrufky@m1k.net>
2006-01-15 12:46:18 -0200

    V4L/DVB (3383): Move tda988x options into tuner_params struct.
    - Tda988x parameters should be defined per tuner_param_type,
    for each tuner_params array member.
    
    Signed-off-by: Michael Krufky <mkrufky@m1k.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/tuner-types.h
+++ include/media/tuner-types.h
@@ -20,6 +20,7 @@ struct tuner_range {
 
 struct tuner_params {
 	enum param_type type;
+	unsigned int tda988x;
 	unsigned char config; /* to be moved into struct tuner_range for dvb-pll merge */
 
 	unsigned int count;
@@ -28,7 +29,6 @@ struct tuner_params {
 
 struct tunertype {
 	char *name;
-	unsigned int has_tda988x:1;
 	struct tuner_params *params;
 };
 
