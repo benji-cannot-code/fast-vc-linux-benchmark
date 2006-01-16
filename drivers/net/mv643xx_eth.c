be741999db4901baf9dcf4a0fa467c0308f5ea15
Olaf Hering <olh@suse.de>
2006-01-16 16:47:00 -0700

    [PATCH] mv643xx_eth: 2.6.16 needs ip.h and in.h
    Signed-off-by: Olaf Hering <olh@suse.de>
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>
    
     mv643xx_eth.c |    2 ++
     1 file changed, 2 insertions(+)
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/mv643xx_eth.c
+++ drivers/net/mv643xx_eth.c
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tcp.h>
 #include <linux/udp.h>
 #include <linux/etherdevice.h>
+#include <linux/in.h>
+#include <linux/ip.h>
 
 #include <linux/bitops.h>
 #include <linux/delay.h>
