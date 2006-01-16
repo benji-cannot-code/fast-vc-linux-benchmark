d960d7e74e67b82e8ab724cd08d1aeb4ec58410a
Per Liden <per.liden@ericsson.com>
2006-01-16 12:42:35 +0100

    [TIPC] Remove unused #includes
    Signed-off-by: Per Liden <per.liden@ericsson.com>

--- net/tipc/eth_media.c
+++ net/tipc/eth_media.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/tipc/tipc_bearer.h>
 #include <net/tipc/tipc_msg.h>
 #include <linux/netdevice.h>
-#include <linux/version.h>
 
 #define MAX_ETH_BEARERS		2
 #define ETH_LINK_PRIORITY	TIPC_DEF_LINK_PRI
