a378a23b7dbf2e31559bf85f8eb49d0cdb0bc073
Andi Kleen <ak@suse.de>
2006-01-16 01:56:39 +0100

    [PATCH] x86_64: Allow nesting of int3 by default for kprobes
    This unbreaks recursive kprobes which didn't work anymore
    due to an earlier patch which converted the debug entry point
    to use an IST.
    
    This also allows nesting of the debug entry point too.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/asm-offsets.c
+++ arch/x86_64/kernel/asm-offsets.c
@@ -44,6 +44,7 @@ int main(void)
 	ENTRY(irqcount);
 	ENTRY(cpunumber);
 	ENTRY(irqstackptr);
+	ENTRY(data_offset);
 	BLANK();
 #undef ENTRY
 #ifdef CONFIG_IA32_EMULATION
@@ -67,8 +68,6 @@ int main(void)
 	DEFINE(pbe_orig_address, offsetof(struct pbe, orig_address));
 	DEFINE(pbe_next, offsetof(struct pbe, next));
 	BLANK();
-#if DEBUG_STKSZ > EXCEPTION_STKSZ
-	DEFINE(DEBUG_IST, DEBUG_STACK);
-#endif
+	DEFINE(TSS_ist, offsetof(struct tss_struct, ist));
 	return 0;
 }
