75829674fef1fd2e2082149d10860b13576d0bd7
Len Brown <len.brown@intel.com>
2006-01-16 15:22:45 -0500

    Revert "[ACPI] fix pnpacpi regression resulting from ACPICA 20051117"
    This reverts ed349a8a0a780ed27e2a765f16cee54d9b63bfee commit.

--- drivers/acpi/resources/rsxface.c
+++ drivers/acpi/resources/rsxface.c
@@ -287,12 +287,6 @@ acpi_walk_resources(acpi_handle device_handle,
 			break;
 		}
 
-		/* end_tag indicates end-of-list */
-
-		if (resource->type == ACPI_RESOURCE_TYPE_END_TAG) {
-			break;
-		}
-
 		/* Invoke the user function, abort on any error returned */
 
 		status = user_function(resource, context);
@@ -302,6 +296,12 @@ acpi_walk_resources(acpi_handle device_handle,
 
 				status = AE_OK;
 			}
+			break;
+		}
+
+		/* end_tag indicates end-of-list */
+
+		if (resource->type == ACPI_RESOURCE_TYPE_END_TAG) {
 			break;
 		}
 
