66a1f4d163df238c5cb45eed13b87ab344373a1c
Miklos Szeredi <miklos@szeredi.hu>
2006-01-16 22:14:35 -0800

    [PATCH] fuse: make fuse connection a kobject
    Kobjectify fuse_conn, and make it visible under /sys/fs/fuse/connections.
    
    Lacking any natural naming, connections are numbered.
    
    This patch doesn't add any attributes, just the infrastructure.
    
    Signed-off-by: Miklos Szeredi <miklos@szeredi.hu>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -197,9 +197,6 @@ struct fuse_req {
  * unmounted.
  */
 struct fuse_conn {
-	/** Reference count */
-	int count;
-
 	/** The user id for this mount */
 	uid_t user_id;
 
@@ -289,6 +286,9 @@ struct fuse_conn {
 
 	/** Backing dev info */
 	struct backing_dev_info bdi;
+
+	/** kobject */
+	struct kobject kobj;
 };
 
 static inline struct fuse_conn *get_fuse_conn_super(struct super_block *sb)
@@ -299,6 +299,11 @@ static inline struct fuse_conn *get_fuse_conn_super(struct super_block *sb)
 static inline struct fuse_conn *get_fuse_conn(struct inode *inode)
 {
 	return get_fuse_conn_super(inode->i_sb);
+}
+
+static inline struct fuse_conn *get_fuse_conn_kobj(struct kobject *obj)
+{
+	return container_of(obj, struct fuse_conn, kobj);
 }
 
 static inline struct fuse_inode *get_fuse_inode(struct inode *inode)
@@ -399,12 +404,6 @@ void fuse_init_symlink(struct inode *inode);
  * Change attributes of an inode
  */
 void fuse_change_attributes(struct inode *inode, struct fuse_attr *attr);
-
-/**
- * Check if the connection can be released, and if yes, then free the
- * connection structure
- */
-void fuse_release_conn(struct fuse_conn *fc);
 
 /**
  * Initialize the client device
