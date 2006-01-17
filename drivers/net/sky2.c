99860796196eca82d73aafe309bb0f2123880f5e
Stephen Hemminger <shemminger@osdl.org>
2006-01-17 13:43:12 -0800

    [PATCH] sky2: version 0.12
    Version update.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sky2.h"
 
 #define DRV_NAME		"sky2"
-#define DRV_VERSION		"0.11"
+#define DRV_VERSION		"0.12"
 #define PFX			DRV_NAME " "
 
 /*
