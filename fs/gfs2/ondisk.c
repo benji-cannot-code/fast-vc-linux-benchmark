0e14101df9267a88c2fc2edbb95967b46d0b8a4f
Steven Whitehouse <steve@chygwyn.com>
2006-01-18 10:57:10 +0000

    [GFS2] Update init_dinode() to reduce stack usage
    We no longer allocate a dinode on the stack in init_dinode()
    and we no longer use gfs2_dinode_out (eliminating one copy) and
    gfs2_meta_header_in (eliminating another copy). The meta_header_in
    fucntion is now no longer referenced from outside gfs2_ondisk.c, so
    make it static.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/ondisk.c
+++ fs/gfs2/ondisk.c
@@ -78,7 +78,7 @@ void gfs2_inum_print(struct gfs2_inum *no)
 	pv(no, no_addr, "%llu");
 }
 
-void gfs2_meta_header_in(struct gfs2_meta_header *mh, char *buf)
+static void gfs2_meta_header_in(struct gfs2_meta_header *mh, char *buf)
 {
 	struct gfs2_meta_header *str = (struct gfs2_meta_header *)buf;
 
@@ -87,7 +87,7 @@ void gfs2_meta_header_in(struct gfs2_meta_header *mh, char *buf)
 	mh->mh_format = be16_to_cpu(str->mh_format);
 }
 
-void gfs2_meta_header_out(struct gfs2_meta_header *mh, char *buf)
+static void gfs2_meta_header_out(struct gfs2_meta_header *mh, char *buf)
 {
 	struct gfs2_meta_header *str = (struct gfs2_meta_header *)buf;
 
