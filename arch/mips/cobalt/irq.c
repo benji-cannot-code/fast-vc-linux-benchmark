6c4e4af788e7681a3d65b370448f74dc592bd66e
Ralf Baechle <ralf@linux-mips.org>
2006-01-18 23:26:43 +0000

    [MIPS] Rename include/asm-mips/cobalt to include/asm-mips/mach-cobalt.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/cobalt/irq.c
+++ arch/mips/cobalt/irq.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/gt64120.h>
 #include <asm/ptrace.h>
 
-#include <asm/cobalt/cobalt.h>
+#include <asm/mach-cobalt/cobalt.h>
 
 extern void cobalt_handle_int(void);
 
