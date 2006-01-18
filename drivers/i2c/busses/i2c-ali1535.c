1ff7f8d29adf6b03133d7596cf4caa504caa7aa1
Jean Delvare <khali@linux-fr.org>
2006-01-18 23:17:01 +0100

    [PATCH] i2c: Semaphore to mutex conversions, part 3
    Cleanup after the semaphores to mutexes conversions in the i2c
    subsystem.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/i2c/busses/i2c-ali1535.c
+++ drivers/i2c/busses/i2c-ali1535.c
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/mutex.h>
 #include <asm/io.h>
-#include <asm/semaphore.h>
 
 
 /* ALI1535 SMBus address offsets */
--- drivers/i2c/busses/scx200_acb.c
+++ drivers/i2c/busses/scx200_acb.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp_lock.h>
 #include <linux/pci.h>
 #include <linux/delay.h>
+#include <linux/mutex.h>
 #include <asm/io.h>
 #include <asm/msr.h>
 
@@ -75,7 +76,7 @@ struct scx200_acb_iface {
 	struct scx200_acb_iface *next;
 	struct i2c_adapter adapter;
 	unsigned base;
-	struct semaphore sem;
+	struct mutex mutex;
 
 	/* State machine data */
 	enum scx200_acb_state state;
@@ -315,7 +316,7 @@ static s32 scx200_acb_smbus_xfer(struct i2c_adapter *adapter,
 		return -EINVAL;
 	}
 
-	down(&iface->sem);
+	mutex_lock(&iface->mutex);
 
 	iface->address_byte = (address << 1) | rw;
 	iface->command = command;
@@ -339,7 +340,7 @@ static s32 scx200_acb_smbus_xfer(struct i2c_adapter *adapter,
 
 	rc = iface->result;
 
-	up(&iface->sem);
+	mutex_unlock(&iface->mutex);
 
 	if (rc == 0 && size == I2C_SMBUS_WORD_DATA && rw == I2C_SMBUS_READ)
 		data->word = le16_to_cpu(cur_word);
@@ -432,7 +433,7 @@ static int  __init scx200_acb_create(const char *text, int base, int index)
 	adapter->algo = &scx200_acb_algorithm;
 	adapter->class = I2C_CLASS_HWMON;
 
-	init_MUTEX(&iface->sem);
+	mutex_init(&iface->mutex);
 
 	snprintf(description, sizeof(description), "%s ACCESS.bus [%s]",
 		 text, adapter->name);
