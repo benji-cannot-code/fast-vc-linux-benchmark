5d8d7fc529f040a52c3f3f9a1e1d892edba7fa1b
Alan Cox <alan@lxorguk.ukuu.org.uk>
2006-01-18 17:44:07 -0800

    [PATCH] EDAC: atomic scrub operations
    EDAC requires a way to scrub memory if an ECC error is found and the chipset
    does not do the work automatically.  That means rewriting memory locations
    atomically with respect to all CPUs _and_ bus masters.  That means we can't
    use atomic_add(foo, 0) as it gets optimised for non-SMP
    
    This adds a function to include/asm-foo/atomic.h for the platforms currently
    supported which implements a scrub of a mapped block.
    
    It also adjusts a few other files include order where atomic.h is included
    before types.h as this now causes an error as atomic_scrub uses u32.
    
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- net/ipv4/raw.c
+++ net/ipv4/raw.c
@@ -41,12 +41,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
  
 #include <linux/config.h> 
+#include <linux/types.h>
 #include <asm/atomic.h>
 #include <asm/byteorder.h>
 #include <asm/current.h>
 #include <asm/uaccess.h>
 #include <asm/ioctls.h>
-#include <linux/types.h>
 #include <linux/stddef.h>
 #include <linux/slab.h>
 #include <linux/errno.h>
