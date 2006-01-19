a56115604058d68f4c02ad19d9cb3144a67749b4
Grant C. Likely <grant.likely@secretlab.ca>
2006-01-19 01:12:32 -0700

    [PATCH] powerpc: Move xparameters.h into xilinx virtex device specific path
    xparameters should not be needed by anything but virtex platform code.
    Move it from include/asm-ppc/ to platforms/4xx/xparameters/
    
    This is preparing for work to remove xparameters from the dependancy tree
    for most c files.  xparam changes should not cause a recompile of the world.
    Instead, drivers should get device info from the platform bus (populated
    by the boot code)
    
    Signed-off-by: Grant C. Likely <grant.likely@secretlab.ca>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc/platforms/4xx/virtex-ii_pro.h
+++ arch/ppc/platforms/4xx/virtex-ii_pro.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_VIRTEXIIPRO_H__
 
 #include <linux/config.h>
-#include <asm/xparameters.h>
+#include <platforms/4xx/xparameters/xparameters.h>
 
 /* serial defines */
 
