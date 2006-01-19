daba2c037ce0fb6d1c7904bc20182c528e72cc9e
Denis MONTERRAT <denis.monterrat@sagem.com>
2006-01-19 14:52:38 +0100

    [PATCH] USB: add new pl2303 device ids
    Signed-off-by: FALIPOU F Developer <fred.falipou@free.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/pl2303.h
+++ drivers/usb/serial/pl2303.h
@@ -69,3 +69,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* CA-42 CLONE Cable www.ca-42.com chipset: Prolific Technology Inc */
 #define CA_42_CA42_VENDOR_ID	0x10b5
 #define CA_42_CA42_PRODUCT_ID	0xac70
+
+#define SAGEM_VENDOR_ID		0x079b
+#define SAGEM_PRODUCT_ID	0x0027
