4cfa57398de02767d5c6ed4c13e292ec3756d117
David Brownell <david-b@pacbell.net>
2006-01-20 13:55:14 -0800

    [PATCH] USB: EHCI and NF2 quirk
    This teaches the EHCI driver about a quirk seen in older NForce2 chips,
    adding a workaround to ignore selective suspend requests.  Bus-wide
    (so-called "global") suspend still works, as does USB wakeup of a
    root hub that's globally suspended.
    
    There's still a hole in this support though.  Strictly speaking, this
    should _fail_ selective suspend requests, rather than ignoring them,
    since doing it this way means that devices which should be able to issue
    remote wakeup are not going to be able to do that.  For now, we'll just
    live with that problem ... since usbcore expects to do selective suspend
    on the way towards a full bus suspend, and usbcore needs to be able to
    do full bus suspend.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -90,6 +90,8 @@ struct ehci_hcd {			/* one per controller */
 	u32			command;
 
 	unsigned		is_tdi_rh_tt:1;	/* TDI roothub with TT */
+	unsigned		no_selective_suspend:1;
+	u8			sbrn;		/* packed release number */
 
 	/* irq statistics */
 #ifdef EHCI_STATS
@@ -98,7 +100,6 @@ struct ehci_hcd {			/* one per controller */
 #else
 #	define COUNT(x) do {} while (0)
 #endif
-	u8			sbrn;		/* packed release number */
 };
 
 /* convert between an HCD pointer and the corresponding EHCI_HCD */ 
