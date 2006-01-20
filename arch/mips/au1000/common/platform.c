ad542ff28b6ed102c7c13c1bba9d4fa49fa3ba1d
Jordan Crouse <jordan.crouse@amd.com>
2006-01-20 14:09:54 -0800

    [PATCH] USB: OHCI for AU1200
    ALCHEMY:  Add OHCI support for AU1200
    
    Updated by moving the OHCI support out of the EHCI patch.
    
    Signed-off-by: Jordan Crouse <jordan.crouse@amd.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/mips/au1000/common/platform.c
+++ arch/mips/au1000/common/platform.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static struct resource au1xxx_usb_ohci_resources[] = {
 	[0] = {
 		.start		= USB_OHCI_BASE,
-		.end		= USB_OHCI_BASE + USB_OHCI_LEN,
+		.end		= USB_OHCI_BASE + USB_OHCI_LEN - 1,
 		.flags		= IORESOURCE_MEM,
 	},
 	[1] = {
