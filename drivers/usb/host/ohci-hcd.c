f3ff46b7d420fdbdc27cac4f0b2080ecf663bf9c
Andrew Victor <andrew@sanpeople.com>
2006-01-22 10:32:13 -0800

    [PATCH] USB: add support for OCHI on AT91rm9200
    This adds support for OHCI on AT91rm9200 based boards.
    
    Possibly of interest here is the way this uses <linux/clk.h> to
    gate clocks on/off during system pm state transitions.  That's
    typical for non-PCI systems.  Some can go further; Mini-A host
    side connectors enable ID-pin sensing.
    
    From: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/host/ohci-hcd.c
+++ drivers/usb/host/ohci-hcd.c
@@ -906,6 +906,10 @@ MODULE_LICENSE ("GPL");
 #include "ohci-ppc-soc.c"
 #endif
 
+#ifdef CONFIG_ARCH_AT91RM9200
+#include "ohci-at91.c"
+#endif
+
 #if !(defined(CONFIG_PCI) \
       || defined(CONFIG_SA1111) \
       || defined(CONFIG_ARCH_S3C2410) \
@@ -914,6 +918,7 @@ MODULE_LICENSE ("GPL");
       || defined (CONFIG_PXA27x) \
       || defined (CONFIG_SOC_AU1X00) \
       || defined (CONFIG_USB_OHCI_HCD_PPC_SOC) \
+      || defined (CONFIG_ARCH_AT91RM9200) \
 	)
 #error "missing bus glue for ohci-hcd"
 #endif
