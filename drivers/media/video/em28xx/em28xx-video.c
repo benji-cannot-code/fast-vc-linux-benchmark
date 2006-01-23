232d9a3e80e095864fbfd5adb282dfb0100fb1f3
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-01-23 17:06:19 -0200

    V4L/DVB (3406): Added credits for em28xx-video.c
    - Added credits for sn9c102 kernel module and his author as
    some parts of em28xx-video were based.
    
    Acked-by: Luca Risolia <luca.risolia@studio.unibo.it>
    Acked-by: Markus Rechberger <mrechberger@gmail.com>
    Acked-by: Ludovico Cavedon <cavedon@sssup.it>
    Acked-by: Sascha Sommer <saschasommer@freenet.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx-video.c
+++ drivers/media/video/em28xx/em28xx-video.c
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 		      Mauro Carvalho Chehab <mchehab@brturbo.com.br>
 		      Sascha Sommer <saschasommer@freenet.de>
 
+	Some parts based on SN9C10x PC Camera Controllers GPL driver made
+		by Luca Risolia <luca.risolia@studio.unibo.it>
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
