a37434260034212053b3a98cbadc8c4b3d9fc2e3
Zhu Yi <yi.zhu@intel.com>
2006-01-24 16:37:05 +0800

    [PATCH] ipw2200: use jiffies_to_msec() wherever possible
    This substitutes Linux jiffies_to_msec() wherever there is a
    computation for determining milliseconds from jiffies,
    following lead from ieee80211 code. And it does a little cleanup.
    "it's" == "it is" ... "its" == possessive "it". Indulge me. ;-)
    
    Signed-off-by: Cahill, Ben M <ben.m.cahill@intel.com>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ipw2200.h
+++ drivers/net/wireless/ipw2200.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 #include <linux/wireless.h>
 #include <linux/dma-mapping.h>
+#include <linux/jiffies.h>
 #include <asm/io.h>
 
 #include <net/ieee80211.h>
