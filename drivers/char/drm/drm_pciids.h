021d21dabf804c94121e6b6a5afc37a4aee8ae4e
Dave Airlie <airlied@starflyer.(none)>
2006-01-25 14:52:43 +1100

    drm: add X600 PCI IDs
    From: Brice Goglin <Brice.Goglin@ens-lyon.org>
    
    Now that Xorg 6.9/7.0 has been released, DRI is supported on more Radeon
    cards without ATI proprietary drivers.  I got my X300 to work without
    problem.  But, another Radeon X600 required to add its PCI ids to the
    Radeon driver.  Patch is attached.
    
    I can't be sure about the "CHIP_RV350", I copied it from the X300 entry
    (from http://dri.freedesktop.org/wiki/ATIRadeon, X600 is a rv380 chip while
    X300 is a rv370).  But, at least it works now.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@ens-lyon.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_pciids.h
+++ drivers/char/drm/drm_pciids.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    Please contact dri-devel@lists.sf.net to add new cards to this list
 */
 #define radeon_PCI_IDS \
+	{0x1002, 0x3150, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_RV350},\
 	{0x1002, 0x4136, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_RS100|CHIP_IS_IGP}, \
 	{0x1002, 0x4137, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_RS200|CHIP_IS_IGP}, \
 	{0x1002, 0x4144, PCI_ANY_ID, PCI_ANY_ID, 0, 0, CHIP_R300}, \
