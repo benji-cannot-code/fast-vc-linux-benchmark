0c3ef029a183f48a7cd40108ab7addde1dd56720
Prarit Bhargava <prarit@sgi.com>
2006-01-25 18:51:14 -0500

    [IA64-SGI] Add PROM feature set for device flush list
    Introduce PRF_DEVICE_FLUSH_LIST flag for older PROMs.
    
    Signed-off-by: Prarit Bhargava <prarit@sgi.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/asm-ia64/sn/sn_feature_sets.h
+++ include/asm-ia64/sn/sn_feature_sets.h
@@ -32,7 +32,8 @@ extern int sn_prom_feature_available(int id);
  * 			...
  */
 
-#define PRF_PAL_CACHE_FLUSH_SAFE		0
+#define PRF_PAL_CACHE_FLUSH_SAFE	0
+#define PRF_DEVICE_FLUSH_LIST		1
 
 
 
@@ -49,7 +50,7 @@ extern int sn_prom_feature_available(int id);
  *
  * By default, features are disabled unless explicitly enabled.
  */
-#define  OSF_MCA_SLV_TO_OS_INIT_SLV		0
-#define  OSF_FEAT_LOG_SBES			1
+#define  OSF_MCA_SLV_TO_OS_INIT_SLV	0
+#define  OSF_FEAT_LOG_SBES		1
 
 #endif /* _ASM_IA64_SN_FEATURE_SETS_H */
