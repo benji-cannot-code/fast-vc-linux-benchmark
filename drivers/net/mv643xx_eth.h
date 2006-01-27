8d01c287d78b08acdfe254e42e288d1e3d114191
Dale Farnsworth <dale@farnsworth.org>
2006-01-27 01:07:48 -0700

    [PATCH] mv643xx_eth: Merge unicast and multicast address filtering code
    Remove duplicated code by having unicast and multicast code use
    a common filter table function: eth_port_set_filter_table_entry().
    
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>
    Signed-off-by: Jeff Garzik <jgarzik@pobox.com>

--- drivers/net/mv643xx_eth.h
+++ drivers/net/mv643xx_eth.h
@@ -90,10 +90,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-/* MAC accepet/reject macros */
-#define ACCEPT_MAC_ADDR				0
-#define REJECT_MAC_ADDR				1
-
 /* Buffer offset from buffer pointer */
 #define RX_BUF_OFFSET				0x2
 
