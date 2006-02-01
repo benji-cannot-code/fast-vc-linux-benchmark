98affab8f8b9615e77ae37c3bd3d64c8f970e48b
Thomas Renninger <trenn@suse.de>
2006-02-01 11:38:37 +0100

    [CPUFREQ] Check for not initialized freq on cpufreq changes
    Test for old_freq equals 0 to insure not to divide by 0:
    ______________________________________________
    
    Check for not initialized freq on cpufreq changes
    
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- arch/i386/kernel/timers/timer_tsc.c
+++ arch/i386/kernel/timers/timer_tsc.c
@@ -273,6 +273,10 @@ time_cpufreq_notifier(struct notifier_block *nb, unsigned long val,
 	if (val != CPUFREQ_RESUMECHANGE)
 		write_seqlock_irq(&xtime_lock);
 	if (!ref_freq) {
+		if (!freq->old){
+			ref_freq = freq->new;
+			goto end;
+		}
 		ref_freq = freq->old;
 		loops_per_jiffy_ref = cpu_data[freq->cpu].loops_per_jiffy;
 #ifndef CONFIG_SMP
@@ -298,6 +302,7 @@ time_cpufreq_notifier(struct notifier_block *nb, unsigned long val,
 #endif
 	}
 
+end:
 	if (val != CPUFREQ_RESUMECHANGE)
 		write_sequnlock_irq(&xtime_lock);
 
