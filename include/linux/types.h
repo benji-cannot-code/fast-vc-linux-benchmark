22b349969562bd22241cc4c65ffb9189e5b1e5ed
Bryan O'Sullivan <bos@pathscale.com>
2006-02-01 03:05:15 -0800

    [PATCH] Define BITS_PER_BYTE
    This can make the intent behind some arithmetic expressions clearer.
    
    Signed-off-by: Bryan O'Sullivan <bos@pathscale.com>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/types.h
+++ include/linux/types.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	(((bits)+BITS_PER_LONG-1)/BITS_PER_LONG)
 #define DECLARE_BITMAP(name,bits) \
 	unsigned long name[BITS_TO_LONGS(bits)]
+
+#define BITS_PER_BYTE 8
 #endif
 
 #include <linux/posix_types.h>
