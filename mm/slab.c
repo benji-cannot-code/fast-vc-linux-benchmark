458dbda976df94236c0465894a8ccd38e5320fd1
Steven Rostedt <rostedt@goodmis.org>
2006-02-01 03:05:44 -0800

    [PATCH] slab: have index_of bug at compile time
    I noticed the code for index_of is a creative way of finding the cache
    index using the compiler to optimize to a single hard coded number.  But
    I couldn't help noticing that it uses two methods to let you know that
    someone used it wrong.  One is at compile time (the correct way), and
    the other is at run time (not good).
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Acked-by: Manfred Spraul <manfred@colorfullife.com>
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/slab.c
+++ mm/slab.c
@@ -317,6 +317,8 @@ struct kmem_list3 __initdata initkmem_list3[NUM_INIT_LISTS];
  */
 static __always_inline int index_of(const size_t size)
 {
+	extern void __bad_size(void);
+
 	if (__builtin_constant_p(size)) {
 		int i = 0;
 
@@ -327,12 +329,9 @@ static __always_inline int index_of(const size_t size)
 		i++;
 #include "linux/kmalloc_sizes.h"
 #undef CACHE
-		{
-			extern void __bad_size(void);
-			__bad_size();
-		}
+		__bad_size();
 	} else
-		BUG();
+		__bad_size();
 	return 0;
 }
 
