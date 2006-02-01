5bd770a0facae40969e7fa427c1e2a7657e765a3
Alexey Dobriyan <adobriyan@gmail.com>
2006-02-01 03:06:16 -0800

    [PATCH] arm26: fix warnings about NR_IRQS being not defined
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Acked-by: Ian Molton <spyro@f2s.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-arm26/hardirq.h
+++ include/asm-arm26/hardirq.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/config.h>
 #include <linux/cache.h>
 #include <linux/threads.h>
+#include <asm/irq.h>
 
 typedef struct {
 	unsigned int __softirq_pending;
