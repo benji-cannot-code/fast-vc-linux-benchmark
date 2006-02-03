aec8a2cbec10985adbdaabb34130a915dc22a524
Jens Axboe <axboe@suse.de>
2006-02-03 08:37:08 +0100

    [PATCH] blk: Fix SG_IO ioctl failure retry looping
    When issuing an SG_IO ioctl through sd that resulted in an unrecoverable
    error, a nearly infinite retry loop was discovered. This is due to the
    fact that the block layer SG_IO code is not setting up rq->retries. This
    patch also fixes up the sg_scsi_ioctl path.
    
    Signed-off-by: Brian King <brking@us.ibm.com>
    Signed-off-by: Jens Axboe <axboe@suse.de>

--- block/scsi_ioctl.c
+++ block/scsi_ioctl.c
@@ -311,6 +311,8 @@ static int sg_io(struct file *file, request_queue_t *q,
 	if (!rq->timeout)
 		rq->timeout = BLK_DEFAULT_TIMEOUT;
 
+	rq->retries = 0;
+
 	start_time = jiffies;
 
 	/* ignore return value. All information is passed back to caller
@@ -428,6 +430,7 @@ static int sg_scsi_ioctl(struct file *file, request_queue_t *q,
 	rq->data = buffer;
 	rq->data_len = bytes;
 	rq->flags |= REQ_BLOCK_PC;
+	rq->retries = 0;
 
 	blk_execute_rq(q, bd_disk, rq, 0);
 	err = rq->errors & 0xff;	/* only 8 bit SCSI status */
