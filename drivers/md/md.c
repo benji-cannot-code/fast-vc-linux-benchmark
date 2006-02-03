19c51178b4a9a06813d6fae90bbf19bf340c27f2
NeilBrown <neilb@suse.de>
2006-02-03 03:03:40 -0800

    [PATCH] md: Handle overflow of mdu_array_info_t->size better
    mdu_array_info_t->size is 'int', which isn't big enough for the size (in KB of
    each component in) some arrays.
    
    So rather than a random overflow, set size to -1 when it cannot be set
    correctly.
    
    To update aspect on an array, userspace will sometimes:
      get_array_info
      change one field
      set_array_info
    
    in this case, we don't want the '-1' in 'size' to change to size, or look like
    a size change at all.  So test for that in update_array_info.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -2943,6 +2943,8 @@ static int get_array_info(mddev_t * mddev, void __user * arg)
 	info.ctime         = mddev->ctime;
 	info.level         = mddev->level;
 	info.size          = mddev->size;
+	if (info.size != mddev->size) /* overflow */
+		info.size = -1;
 	info.nr_disks      = nr;
 	info.raid_disks    = mddev->raid_disks;
 	info.md_minor      = mddev->md_minor;
@@ -3524,7 +3526,7 @@ static int update_array_info(mddev_t *mddev, mdu_array_info_t *info)
 		)
 		return -EINVAL;
 	/* Check there is only one change */
-	if (mddev->size != info->size) cnt++;
+	if (info->size >= 0 && mddev->size != info->size) cnt++;
 	if (mddev->raid_disks != info->raid_disks) cnt++;
 	if (mddev->layout != info->layout) cnt++;
 	if ((state ^ info->state) & (1<<MD_SB_BITMAP_PRESENT)) cnt++;
@@ -3541,7 +3543,7 @@ static int update_array_info(mddev_t *mddev, mdu_array_info_t *info)
 		else
 			return mddev->pers->reconfig(mddev, info->layout, -1);
 	}
-	if (mddev->size != info->size)
+	if (info->size >= 0 && mddev->size != info->size)
 		rv = update_size(mddev, info->size);
 
 	if (mddev->raid_disks    != info->raid_disks)
