80d5cd30b4f58190ca38f43f5c96dbb2502bb22e
Janak Desai <janak@us.ibm.com>
2006-02-08 15:43:38 -0800

    [IA64] unshare system call registration for ia64
    Registers system call for the ia64 architecture.
    
    Reserves space for ppoll and pselect, and adds unshare at system
    call number 1296.
    
    Signed-off-by: Janak Desai <janak@us.ibm.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/asm-ia64/unistd.h
+++ include/asm-ia64/unistd.h
@@ -284,12 +284,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_readlinkat			1291
 #define __NR_fchmodat			1292
 #define __NR_faccessat			1293
+/* 1294, 1295 reserved for pselect/ppoll */
+#define __NR_unshare			1296
 
 #ifdef __KERNEL__
 
 #include <linux/config.h>
 
-#define NR_syscalls			270 /* length of syscall table */
+#define NR_syscalls			273 /* length of syscall table */
 
 #define __ARCH_WANT_SYS_RT_SIGACTION
 
