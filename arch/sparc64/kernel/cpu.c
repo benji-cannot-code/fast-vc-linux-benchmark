0575f4dd54e8e1eb07f860676802e571fdc3fb5e
David S. Miller <davem@sunset.davemloft.net>
2006-02-09 03:00:00 -0800

    [SPARC64]: kernel/cpu.c needs asm/spitfire.h
    For 'tlb_type'.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/cpu.c
+++ arch/sparc64/kernel/cpu.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/system.h>
 #include <asm/fpumacro.h>
 #include <asm/cpudata.h>
+#include <asm/spitfire.h>
 
 DEFINE_PER_CPU(cpuinfo_sparc, __cpu_data) = { 0 };
 
