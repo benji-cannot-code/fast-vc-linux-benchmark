f70c669f2010e13fdcc7f92484d4c429f07b8b05
Petr Vandrovec <vandrove@vc.cvut.cz>
2006-02-10 02:04:00 -0800

    [PATCH] pcmcia: Add support for Possio GCC AKA PCMCIA Siemens MC45
    This ugly hack add support for Siemens MC45 PCMCIA GPRS card (which is
    identical to Possio GCC, and which is offered by one of our local GPRS
    providers).  Card has unfortunate feature that after poweron oxcf950 chip
    is fully powered and works, but attached MC45 modem is powered down :-(
    There is a special sequence (which takes 1 sec :-( ) to poweron MC45 (and
    after MC45 powers on, it takes more than 2 secs until firmware fully
    boots...) which needs to be executed after all powerons.
    
    I'm really not familiar with PCMCIA subsystem, so I have no idea whether I
    should issue request_region() on rest of oxcf950 address range (0-7 is
    UART, 8-F are special configuration registers), or how this should be
    better integrated with PM system and so on - I just put it in same place
    where another hack already lived...
    
    Card uses 18.432MHz XTAL, so to get it to work you must add lines below to
    the /etc/pcmcia/serial.opts.
    
    case "$MANFID-$FUNCID-$PRODID_1-$PRODID_2-$PRODID_3-$PRODID_4" in
    '030c,0003-2-GPRS-CARD--')
        SERIAL_OPTS="baud_base 1152000"
        ;;
    esac
    
    Cc: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- include/pcmcia/ciscode.h
+++ include/pcmcia/ciscode.h
@@ -123,4 +123,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MANFID_XIRCOM			0x0105
 
+#define MANFID_POSSIO			0x030c
+#define PRODID_POSSIO_GCC		0x0003
+
 #endif /* _LINUX_CISCODE_H */
