37e6a47525476494c1020aa73af8c09a27b55b66
Heiko Carstens <heiko.carstens@de.ibm.com>
2006-02-11 17:56:02 -0800

    [PATCH] s390: add support for unshare system call
    Add support for unshare system call.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-s390/unistd.h
+++ include/asm-s390/unistd.h
@@ -296,8 +296,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_faccessat		300
 #define __NR_pselect6		301
 #define __NR_ppoll		302
+#define __NR_unshare		303
 
-#define NR_syscalls 303
+#define NR_syscalls 304
 
 /* 
  * There are some system calls that are not present on 64 bit, some
