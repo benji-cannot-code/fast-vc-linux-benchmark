d916c1825c04519974a3e4c98f5215d515d2dee6
Michael Buesch <mbuesch@freenet.de>
2006-02-13 00:11:07 +0100

    [PATCH] bcm43xx: Code cleanups. This removes various "inline" statements and reduces codesize.
    Signed-off-by: Michael Buesch <mbuesch@freenet.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/bcm43xx/bcm43xx.h
+++ drivers/net/wireless/bcm43xx/bcm43xx.h
@@ -20,16 +20,17 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PFX				KBUILD_MODNAME ": "
 
-#define BCM43xx_SWITCH_CORE_MAX_RETRIES	10
+#define BCM43xx_SWITCH_CORE_MAX_RETRIES	50
 #define BCM43xx_IRQWAIT_MAX_RETRIES	50
 
 #define BCM43xx_IO_SIZE			8192
-#define BCM43xx_REG_ACTIVE_CORE		0x80
 
-/* Interrupt Control PCI Configuration Register. (Only on PCI cores with rev >= 6) */
-#define BCM43xx_PCICFG_ICR		0x94
+/* Active Core PCI Configuration Register. */
+#define BCM43xx_PCICFG_ACTIVE_CORE	0x80
 /* SPROM control register. */
 #define BCM43xx_PCICFG_SPROMCTL		0x88
+/* Interrupt Control PCI Configuration Register. (Only on PCI cores with rev >= 6) */
+#define BCM43xx_PCICFG_ICR		0x94
 
 /* MMIO offsets */
 #define BCM43xx_MMIO_DMA1_REASON	0x20
