fd93b4046fbb7c5b840c6501f46ed4867c64f59f
David S. Miller <davem@sunset.davemloft.net>
2006-02-12 22:29:36 -0800

    [SPARC64]: Missing 'return' statement in sun4v_pci_init().
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/pci_sun4v.c
+++ arch/sparc64/kernel/pci_sun4v.c
@@ -917,8 +917,10 @@ void sun4v_pci_init(int node, char *model_name)
 		       &p->pbm_A :
 		       &p->pbm_B);
 
-		if (pbm->devhandle == (devhandle ^ 0x40))
+		if (pbm->devhandle == (devhandle ^ 0x40)) {
 			pci_sun4v_pbm_init(p, node, devhandle);
+			return;
+		}
 	}
 
 	p = kmalloc(sizeof(struct pci_controller_info), GFP_ATOMIC);
@@ -962,7 +964,4 @@ void sun4v_pci_init(int node, char *model_name)
 	pci_memspace_mask = 0x7fffffffUL;
 
 	pci_sun4v_pbm_init(p, node, devhandle);
-
-	prom_printf("sun4v_pci_init: Implement me.\n");
-	prom_halt();
 }
