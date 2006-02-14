bc7ff7316e21010a57af994b34888042770e1398
David S. Miller <davem@sunset.davemloft.net>
2006-02-13 21:28:40 -0800

    [SPARC64] sunhv: Bug fixes.
    Add udelay to polling console write loop, and increment
    the loop limit.
    
    Name the device "ttyHV" and pass that to add_preferred_console()
    when we're using hypervisor console.
    
    Kill sunhv_console_setup(), it's empty.
    
    Handle the case where we don't want to use hypervisor console.
    (ie. we have a head attached to a sun4v machine)
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/setup.c
+++ arch/sparc64/kernel/setup.c
@@ -411,6 +411,7 @@ static int __init set_preferred_console(void)
 		 * value anyways...
 		 */
 		serial_console = 4;
+		return add_preferred_console("ttyHV", 0, NULL);
 	} else {
 		prom_printf("Inconsistent console: "
 			    "input %d, output %d\n",
