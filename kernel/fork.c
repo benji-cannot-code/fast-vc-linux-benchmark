ae3caf9060d34a515189be99eae7c78b351a3a3b
Oleg Nesterov <oleg@tv-sign.ru>
2006-02-15 22:13:24 +0300

    [PATCH] fix kill_proc_info() vs CLONE_THREAD race
    There is a window after copy_process() unlocks ->sighand.siglock
    and before it adds the new thread to the thread list.
    
    In that window __group_complete_signal(SIGKILL) will not see the
    new thread yet, so this thread will start running while the whole
    thread group was supposed to exit.
    
    I beleive we have another good reason to place attach_pid(PID/TGID)
    under ->sighand.siglock. We can do the same for
    
    	release_task()->__unhash_process()
    
    	de_thread()->switch_exec_pids()
    
    After that we don't need tasklist_lock to iterate over the thread
    list, and we can simplify things, see for example do_sigaction()
    or sys_times().
    
    Signed-off-by: Oleg Nesterov <oleg@tv-sign.ru>
    Cc: Roland McGrath <roland@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1124,8 +1124,8 @@ static task_t *copy_process(unsigned long clone_flags,
 		p->real_parent = current;
 	p->parent = p->real_parent;
 
+	spin_lock(&current->sighand->siglock);
 	if (clone_flags & CLONE_THREAD) {
-		spin_lock(&current->sighand->siglock);
 		/*
 		 * Important: if an exit-all has been started then
 		 * do not create this new thread - the whole thread
@@ -1163,8 +1163,6 @@ static task_t *copy_process(unsigned long clone_flags,
 			 */
 			p->it_prof_expires = jiffies_to_cputime(1);
 		}
-
-		spin_unlock(&current->sighand->siglock);
 	}
 
 	/*
@@ -1190,6 +1188,7 @@ static task_t *copy_process(unsigned long clone_flags,
 
 	nr_threads++;
 	total_forks++;
+	spin_unlock(&current->sighand->siglock);
 	write_unlock_irq(&tasklist_lock);
 	proc_fork_connector(p);
 	return p;
