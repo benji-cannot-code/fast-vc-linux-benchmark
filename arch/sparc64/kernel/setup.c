08189adb4ed492a89679475bd726096ff10dc430
David S. Miller <davem@sunset.davemloft.net>
2006-02-17 08:38:06 -0800

    [SPARC64]: Fix uniprocessor IRQ targetting on SUN4V.
    We need to use the real hardware processor ID when
    targetting interrupts, not the "define to 0" thing
    the uniprocessor build gives us.
    
    Also, fill in the Node-ID and Agent-ID fields properly
    on sun4u/Safari.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/setup.c
+++ arch/sparc64/kernel/setup.c
@@ -223,7 +223,6 @@ static struct pt_regs fake_swapper_regs = { { 0, }, 0, 0, 0, 0 };
 
 static void __init per_cpu_patch(void)
 {
-#ifdef CONFIG_SMP
 	struct cpuid_patch_entry *p;
 	unsigned long ver;
 	int is_jbus;
@@ -234,8 +233,8 @@ static void __init per_cpu_patch(void)
 	is_jbus = 0;
 	if (tlb_type != hypervisor) {
 		__asm__ ("rdpr %%ver, %0" : "=r" (ver));
-		is_jbus = ((ver >> 32) == __JALAPENO_ID ||
-			   (ver >> 32) == __SERRANO_ID);
+		is_jbus = ((ver >> 32UL) == __JALAPENO_ID ||
+			   (ver >> 32UL) == __SERRANO_ID);
 	}
 
 	p = &__cpuid_patch;
@@ -280,7 +279,6 @@ static void __init per_cpu_patch(void)
 
 		p++;
 	}
-#endif
 }
 
 static void __init sun4v_patch(void)
