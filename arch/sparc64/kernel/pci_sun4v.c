0022c27c383e0257fcb8d5d870a90d3d70cda437
David S. Miller <davem@davemloft.net>
2006-02-20 01:42:51 -0800

    [SPARC64]: Handle zero-length map requests in pci_sun4v.c
    By simply changing the do-while loop into a plain
    while loop.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/pci_sun4v.c
+++ arch/sparc64/kernel/pci_sun4v.c
@@ -58,7 +58,7 @@ static long pci_iommu_batch_flush(struct pci_iommu_batch *p)
 	u64 *pglist = p->pglist;
 	unsigned long npages = p->npages;
 
-	do {
+	while (npages != 0) {
 		long num;
 
 		num = pci_sun4v_iommu_map(devhandle, HV_PCI_TSBID(0, entry),
@@ -76,7 +76,7 @@ static long pci_iommu_batch_flush(struct pci_iommu_batch *p)
 		entry += num;
 		npages -= num;
 		pglist += num;
-	} while (npages != 0);
+	}
 
 	p->entry = entry;
 	p->npages = 0;
