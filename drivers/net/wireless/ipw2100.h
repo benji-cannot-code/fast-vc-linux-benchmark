54542d76a53b2ef417c114b0ada9d02bc099fecf
Stefan Rompf <stefan@loplof.de>
2006-02-21 18:36:17 +0800

    [PATCH] ipw2100: add radiotap headers to packtes captured in monitor mode
    Signed-off-by: Stefan Rompf <stefan@loplof.de>
    Signed-off-by: Andrea Merello <andreamrl at tiscali it>
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ipw2100.h
+++ drivers/net/wireless/ipw2100.h
@@ -42,6 +42,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/ieee80211.h>
 
+#ifdef CONFIG_IPW2100_MONITOR
+#include <net/ieee80211_radiotap.h>
+#endif
+
 #include <linux/workqueue.h>
 
 struct ipw2100_priv;
