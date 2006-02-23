4cb456cd9aa0ea22e4ddbe304ce33e64c88c0b64
Michael Buesch <mbuesch@freenet.de>
2006-02-23 21:15:39 +0100

    [PATCH] bcm43xx: Move TX/RX related functions to its own file. Add basic RTS/CTS code.
    Signed-off-by: Michael Buesch <mbuesch@freenet.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/bcm43xx/bcm43xx_debugfs.c
+++ drivers/net/wireless/bcm43xx/bcm43xx_debugfs.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bcm43xx_debugfs.h"
 #include "bcm43xx_dma.h"
 #include "bcm43xx_pio.h"
+#include "bcm43xx_xmit.h"
 
 #define REALLY_BIG_BUFFER_SIZE	(1024*256)
 
--- drivers/net/wireless/bcm43xx/bcm43xx_dma.c
+++ drivers/net/wireless/bcm43xx/bcm43xx_dma.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bcm43xx_main.h"
 #include "bcm43xx_debugfs.h"
 #include "bcm43xx_power.h"
+#include "bcm43xx_xmit.h"
 
 #include <linux/dmapool.h>
 #include <linux/pci.h>
