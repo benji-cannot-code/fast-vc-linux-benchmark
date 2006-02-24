fbfaa3adb5490f334d6d247e3f6f7a414b33b5ca
Russell King <rmk@dyn-67.arm.linux.org.uk>
2006-02-24 21:44:56 +0000

    [ARM] Fix typo in tlbflush.h
    s/CONFIG_MMY/CONFIG_MMU/
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/tlbflush.h
+++ include/asm-arm/tlbflush.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define tlb_flush(tlb)	((void) tlb)
 
-#else /* CONFIG_MMY */
+#else /* CONFIG_MMU */
 
 #include <asm/glue.h>
 
