4cf530914257f506a56c07e5ebee0648f412beec
James Bottomley <James.Bottomley@SteelEye.com>
2006-02-26 17:07:45 -0600

    [PATCH] fix voyager after topology.c move
    Commit 9c869edac591977314323a4eaad5f7633fca684f broke voyager again
    rather subtly because it already had its own topology exporting
    functions, so now each CPU gets registered twice.
    
    I think we can actually use the generic ones, so I don't propose
    reverting it.  The attached should eliminate the voyager topology
    functions in favour of the generic ones.
    
    I also added a define to ensure voyager is never hotplug CPU (we don't
    have the support in the SMP harness).
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/mach-voyager/voyager_basic.c
+++ arch/i386/mach-voyager/voyager_basic.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysrq.h>
 #include <linux/smp.h>
 #include <linux/nodemask.h>
-#include <asm/cpu.h>
 #include <asm/io.h>
 #include <asm/voyager.h>
 #include <asm/vic.h>
@@ -332,16 +331,3 @@ void machine_power_off(void)
 	if (pm_power_off)
 		pm_power_off();
 }
-
-static struct i386_cpu cpu_devices[NR_CPUS];
-
-static int __init topology_init(void)
-{
-	int i;
-
-	for_each_present_cpu(i)
-		register_cpu(&cpu_devices[i].cpu, i, NULL);
-	return 0;
-}
-
-subsys_initcall(topology_init);
