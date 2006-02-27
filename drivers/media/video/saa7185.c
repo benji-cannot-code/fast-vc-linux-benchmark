636e3e196e5ff5c32f25011873ad72dfb36e3b0e
Jean Delvare <khali@linux-fr.org>
2006-02-27 23:13:53 +0100

    [PATCH] I2C: Drop unneeded i2c-dev.h includes
    Several media/video and sound drivers include i2c-dev.h while they
    don't need it at all. Clean it up.
    
    This header file is really only needed by i2c-dev.c and
    compat_ioctl.c, other drivers should never need it.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/media/video/saa7185.c
+++ drivers/media/video/saa7185.c
@@ -50,7 +50,6 @@ MODULE_AUTHOR("Dave Perks");
 MODULE_LICENSE("GPL");
 
 #include <linux/i2c.h>
-#include <linux/i2c-dev.h>
 
 #define I2C_NAME(s) (s)->name
 
--- sound/oss/dmasound/dmasound_awacs.c
+++ sound/oss/dmasound/dmasound_awacs.c
@@ -89,8 +89,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pmu.h>
 #endif
 
-#include <linux/i2c-dev.h>
-
 #include <asm/uaccess.h>
 #include <asm/prom.h>
 #include <asm/machdep.h>
