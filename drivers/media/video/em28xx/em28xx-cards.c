5ad85c6afc4ec53a6b11fb4225c5ba141e156223
Markus Rechberger <mrechberger@gmail.com>
2006-02-27 00:07:34 -0300

    V4L/DVB (3293): Fixed amux hauppauge hvr900/terratec hybrid xs
    Fixed amux hauppauge hvr900/terratec hybrid xs
    
    Signed-off-by: Markus Rechberger <mrechberger@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx-cards.c
+++ drivers/media/video/em28xx/em28xx-cards.c
@@ -102,7 +102,7 @@ struct em28xx_board em28xx_boards[] = {
 		.input          = {{
 			.type     = EM28XX_VMUX_TELEVISION,
 			.vmux     = 2,
-			.amux     = 0,
+			.amux     = 1,
 		},{
 			.type     = EM28XX_VMUX_COMPOSITE1,
 			.vmux     = 0,
@@ -166,11 +166,11 @@ struct em28xx_board em28xx_boards[] = {
 		.input          = {{
 			.type     = EM28XX_VMUX_COMPOSITE1,
 			.vmux     = 2,
-			.amux     = 0,
+			.amux     = 1,
 		},{
 			.type     = EM28XX_VMUX_TELEVISION,
 			.vmux     = 0,
-			.amux     = 1,
+			.amux     = 0,
 		},{
 			.type     = EM28XX_VMUX_SVIDEO,
 			.vmux     = 9,
@@ -186,12 +186,12 @@ struct em28xx_board em28xx_boards[] = {
 		.tuner_type   = TUNER_XCEIVE_XC3028,
 		.decoder      = EM28XX_TVP5150,
 		.input          = {{
-			.type     = EM28XX_VMUX_COMPOSITE1,
-			.vmux     = 2,
-			.amux     = 0,
-		},{
 			.type     = EM28XX_VMUX_TELEVISION,
 			.vmux     = 0,
+			.amux     = 0,
+		},{
+			.type     = EM28XX_VMUX_COMPOSITE1,
+			.vmux     = 2,
 			.amux     = 1,
 		},{
 			.type     = EM28XX_VMUX_SVIDEO,
