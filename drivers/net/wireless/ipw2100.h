ebfda2f048a5fad299e0fd3d0ef2697891f23de3
Zhu Yi <yi.zhu@intel.com>
2006-02-28 08:38:07 +0800

    [PATCH] ipw2100: move mutex.h include from ipw2100.c to ipw2100.h
    Signed-off-by: Zhu Yi <yi.zhu@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ipw2100.h
+++ drivers/net/wireless/ipw2100.h
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #include <linux/workqueue.h>
+#include <linux/mutex.h>
 
 struct ipw2100_priv;
 struct ipw2100_tx_packet;
