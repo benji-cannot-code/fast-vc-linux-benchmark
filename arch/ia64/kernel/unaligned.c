e377223fa24fbe2686440b8cbd0731b395ca4ae8
Tony Luck <tony.luck@intel.com>
2006-02-27 16:18:58 -0800

    [IA64] die_if_kernel() can return
    arch/ia64/kernel/unaligned.c erroneously marked die_if_kernel()
    with a "noreturn" attribute ... which is silly (it returns whenever
    the argument regs say that the fault happened in user mode, as one
    might expect given the "if_kernel" part of its name!).  Thanks to
    Alan and Gareth for pointing this out.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/unaligned.c
+++ arch/ia64/kernel/unaligned.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/unaligned.h>
 
-extern void die_if_kernel(char *str, struct pt_regs *regs, long err) __attribute__ ((noreturn));
+extern void die_if_kernel(char *str, struct pt_regs *regs, long err);
 
 #undef DEBUG_UNALIGNED_TRAP
 
