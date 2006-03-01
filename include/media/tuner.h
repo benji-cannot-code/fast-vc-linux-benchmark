216eb69fc6d98594a65649a56614bbec5010b474
Michael Krufky <mkrufky@linuxtv.org>
2006-03-01 00:04:42 -0300

    V4L/DVB (3411): FE6600 is a Thomson tuner
    - The tuner used in DViCO FusionHDTV DVB-T hybrid is made by Thomson
    - renamed tuner and dvb_pll structs accordingly
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -119,7 +119,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TUNER_SAMSUNG_TCPN_2121P30A     70 	/* Hauppauge PVR-500MCE NTSC */
 #define TUNER_XCEIVE_XC3028		71
 
-#define TUNER_FE6600			72	/* DViCO FusionHDTV DVB-T Hybrid */
+#define TUNER_THOMSON_FE6600		72	/* DViCO FusionHDTV DVB-T Hybrid */
 
 /* tv card specific */
 #define TDA9887_PRESENT 		(1<<0)
