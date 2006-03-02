ba643a9416f19cffe45ff677e8f101adcf347454
Steve French <sfrench@us.ibm.com>
2006-03-02 03:24:57 +0000

    [CIFS] Make POSIX CIFS Extensions SetFSInfo match exactly what we want not just the posix path feature.
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -650,7 +650,9 @@ int cifs_lock(struct file *file, int cmd, struct file_lock *pfLock)
 	wire */
 	if (IS_GETLK(cmd)) {
 		if(experimEnabled && 
-		   (cifs_sb->tcon->ses->capabilities & CAP_UNIX)) {
+		   (cifs_sb->tcon->ses->capabilities & CAP_UNIX) &&
+		   (CIFS_UNIX_FCNTL_CAP & 
+			le64_to_cpu(cifs_sb->tcon->fsUnixInfo.Capability))) {
 			int posix_lock_type;
 			if(lockType & LOCKING_ANDX_SHARED_LOCK)
 				posix_lock_type = CIFS_RDLCK;
@@ -687,7 +689,9 @@ int cifs_lock(struct file *file, int cmd, struct file_lock *pfLock)
 		return rc;
 	}
 	if (experimEnabled &&
-		(cifs_sb->tcon->ses->capabilities & CAP_UNIX)) {
+		(cifs_sb->tcon->ses->capabilities & CAP_UNIX) &&
+		(CIFS_UNIX_FCNTL_CAP &
+			 le64_to_cpu(cifs_sb->tcon->fsUnixInfo.Capability))) {
 		int posix_lock_type;
 		if(lockType & LOCKING_ANDX_SHARED_LOCK)
 			posix_lock_type = CIFS_RDLCK;
