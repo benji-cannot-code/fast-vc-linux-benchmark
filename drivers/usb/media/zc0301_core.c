c8a8a2bb3808d72f0a1bf40fe08d41a6a48dc1d8
Luca Risolia <luca.risolia@studio.unibo.it>
2006-03-03 09:58:39 +0000

    [PATCH] USB: ZC0301 driver bugfix
    ZC0301 driver bugfix.
    
    Use correct PID/VID USB entries.
    
    Signed-off-by: Luca Risolia <luca.risolia@studio.unibo.it>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/media/zc0301_core.c
+++ drivers/usb/media/zc0301_core.c
@@ -53,8 +53,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ZC0301_MODULE_AUTHOR  "(C) 2006 Luca Risolia"
 #define ZC0301_AUTHOR_EMAIL   "<luca.risolia@studio.unibo.it>"
 #define ZC0301_MODULE_LICENSE "GPL"
-#define ZC0301_MODULE_VERSION "1:1.02"
-#define ZC0301_MODULE_VERSION_CODE  KERNEL_VERSION(1, 0, 2)
+#define ZC0301_MODULE_VERSION "1:1.03"
+#define ZC0301_MODULE_VERSION_CODE  KERNEL_VERSION(1, 0, 3)
 
 /*****************************************************************************/
 
@@ -638,7 +638,6 @@ static void zc0301_release_resources(struct zc0301_device* cam)
 	DBG(2, "V4L2 device /dev/video%d deregistered", cam->v4ldev->minor);
 	video_set_drvdata(cam->v4ldev, NULL);
 	video_unregister_device(cam->v4ldev);
-	usb_put_dev(cam->usbdev);
 	kfree(cam->control_buffer);
 }
 
@@ -728,6 +727,7 @@ static int zc0301_release(struct inode* inode, struct file* filp)
 
 	if (cam->state & DEV_DISCONNECTED) {
 		zc0301_release_resources(cam);
+		usb_put_dev(cam->usbdev);
 		mutex_unlock(&cam->dev_mutex);
 		kfree(cam);
 		return 0;
