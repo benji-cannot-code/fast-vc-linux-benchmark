5d7027a52caf5f223cc34e7c6b746e4f40d9f610
Dominik Brodowski <linux@dominikbrodowski.net>
2006-03-05 11:04:33 +0100

    [PATCH] pcmcia: convert DEV_OK to pcmcia_dev_present
    Instead of the DEV_OK macro, drivers should use pcmcia_dev_present().
    
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/net/pcmcia/xirc2ps_cs.c
+++ drivers/net/pcmcia/xirc2ps_cs.c
@@ -1526,7 +1526,7 @@ do_open(struct net_device *dev)
 
     /* Check that the PCMCIA card is still here. */
     /* Physical device present signature. */
-    if (!DEV_OK(link))
+    if (!pcmcia_dev_present(link))
 	return -ENODEV;
 
     /* okay */
