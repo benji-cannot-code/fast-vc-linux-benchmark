985086abadd0a02bae2579d69a1d1b04536f2bc7
Jeff Garzik <jeff@garzik.org>
2006-03-04 21:52:42 -0500

    [PATCH] PCI: fix pci_request_region[s] arg
        Add missing 'const' to pci_request_region[s] 'res_name' arg,
        since we pass it directly to __request_region(), whose 'name' arg
        is also const.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -640,7 +640,7 @@ void pci_release_region(struct pci_dev *pdev, int bar)
  *	Returns 0 on success, or %EBUSY on error.  A warning
  *	message is also printed on failure.
  */
-int pci_request_region(struct pci_dev *pdev, int bar, char *res_name)
+int pci_request_region(struct pci_dev *pdev, int bar, const char *res_name)
 {
 	if (pci_resource_len(pdev, bar) == 0)
 		return 0;
@@ -698,7 +698,7 @@ void pci_release_regions(struct pci_dev *pdev)
  *	Returns 0 on success, or %EBUSY on error.  A warning
  *	message is also printed on failure.
  */
-int pci_request_regions(struct pci_dev *pdev, char *res_name)
+int pci_request_regions(struct pci_dev *pdev, const char *res_name)
 {
 	int i;
 	
