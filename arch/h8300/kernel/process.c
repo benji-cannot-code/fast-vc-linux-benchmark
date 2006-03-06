e4f3a6a874e7145bd2ca6eda7d35ce371b15d68a
Adrian Bunk <bunk@stusta.de>
2006-03-06 15:42:47 -0800

    [PATCH] add missing pm_power_off's
    Add the missing pm_power_off's for the h8300, v850 and xtensa
    architectures.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
    Cc: Miles Bader <uclinux-v850@lsi.nec.co.jp>
    Cc: Chris Zankel <chris@zankel.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/h8300/kernel/process.c
+++ arch/h8300/kernel/process.c
@@ -46,6 +46,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/setup.h>
 #include <asm/pgtable.h>
 
+void (*pm_power_off)(void) = NULL;
+EXPORT_SYMBOL(pm_power_off);
+
 asmlinkage void ret_from_fork(void);
 
 /*
