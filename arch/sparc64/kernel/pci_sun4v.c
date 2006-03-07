65a87ea7ac148329f4259f7e65c336616876bbe1
David S. Miller <davem@sunset.davemloft.net>
2006-03-06 22:50:44 -0800

    [SPARC64]: Fix new context version SMP handling.
    Don't piggy back the SMP receive signal code to do the
    context version change handling.
    
    Instead allocate another fixed PIL number for this
    asynchronous cross-call.  We can't use smp_call_function()
    because this thing is invoked with interrupts disabled
    and a few spinlocks held.
    
    Also, fix smp_call_function_mask() to count "cpus" correctly.
    There is no guarentee that the local cpu is in the mask
    yet that is exactly what this code was assuming.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/pci_sun4v.c
+++ arch/sparc64/kernel/pci_sun4v.c
@@ -736,11 +736,11 @@ static unsigned int pci_sun4v_irq_build(struct pci_pbm_info *pbm,
 	u32 devhandle = pbm->devhandle;
 	int pil;
 
-	pil = 4;
+	pil = 5;
 	if (pdev) {
 		switch ((pdev->class >> 16) & 0xff) {
 		case PCI_BASE_CLASS_STORAGE:
-			pil = 4;
+			pil = 5;
 			break;
 
 		case PCI_BASE_CLASS_NETWORK:
@@ -759,7 +759,7 @@ static unsigned int pci_sun4v_irq_build(struct pci_pbm_info *pbm,
 			break;
 
 		default:
-			pil = 4;
+			pil = 5;
 			break;
 		};
 	}
