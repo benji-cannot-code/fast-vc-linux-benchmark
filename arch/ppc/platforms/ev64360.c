df22f417f8068072ac181b49f4574e2dd7db66da
Adrian Cox <adrian@humboldt.co.uk>
2006-03-08 22:10:20 +0000

    [PATCH] ppc: Fix platform_notify functions marked __init
    While adding USB support to an MV64360 based board this week, I
    discovered that all MV64x60 boards in the kernel have platform_notify
    functions marked with __init. This causes an oops if a device is added
    after boot.
    
    The patch below removes the __init markers. I do not have all these
    boards to test on, but the change seems very unlikely to break anything
    else.
    
    Signed-off-by: Adrian Cox <adrian@humboldt.co.uk>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc/platforms/ev64360.c
+++ arch/ppc/platforms/ev64360.c
@@ -299,7 +299,7 @@ ev64360_fixup_eth_pdata(struct platform_device *pdev)
 }
 #endif
 
-static int __init
+static int
 ev64360_platform_notify(struct device *dev)
 {
 	static struct {
--- arch/ppc/platforms/katana.c
+++ arch/ppc/platforms/katana.c
@@ -597,7 +597,7 @@ katana_fixup_mv64xxx_pdata(struct platform_device *pdev)
 }
 #endif
 
-static int __init
+static int
 katana_platform_notify(struct device *dev)
 {
 	static struct {
