a960ebd0e26045d13d69f05e0c19e6182df2ad9b
David S. Miller <davem@sunset.davemloft.net>
2006-03-08 16:09:19 -0800

    [SPARC64]: Add SMT scheduling support for Niagara.
    The mapping is a simple "(cpuid >> 2) == core" for now.
    Later we'll add more sophisticated code that will walk
    the sun4v machine description and figure this out from
    there.
    
    We should also add core mappings for jaguar and panther
    processors.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/smp.h
+++ include/asm-sparc64/smp.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern cpumask_t phys_cpu_present_map;
 #define cpu_possible_map phys_cpu_present_map
 
+extern cpumask_t cpu_sibling_map[NR_CPUS];
+
 /*
  *	General functions that each host system must provide.
  */
