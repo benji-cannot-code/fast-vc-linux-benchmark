c1328dd0a2a751083698f3b4a753c6b77425d253
Grant Likely <grant.likely@secretlab.ca>
2006-03-09 23:36:40 -0700

    [PATCH] Fix compile error for ML300/403
    Needed due to changes in ppc_sys.c.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc/platforms/4xx/virtex.h
+++ arch/ppc/platforms/4xx/virtex.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Device type enumeration for platform bus definitions */
 #ifndef __ASSEMBLY__
 enum ppc_sys_devices {
-	VIRTEX_UART,
+	VIRTEX_UART, NUM_PPC_SYS_DEVS,
 };
 #endif
   
