814944908c55fa2ab6918732f607cefc9ea22dc7
David Chinner <dgc@sgi.com>
2006-03-14 13:23:52 +1100

    [XFS] Add support for hotplug CPUs to the per-CPU superblock counters by registering a notifier callback that listens to CPU up/down events to modify the counters appropriately.
    SGI-PV: 949726
    SGI-Modid: xfs-linux-melb:xfs-kern:25214a
    
    Signed-off-by: David Chinner <dgc@sgi.com>
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/linux-2.6/xfs_linux.h
+++ fs/xfs/linux-2.6/xfs_linux.h
@@ -74,6 +74,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/proc_fs.h>
 #include <linux/sort.h>
+#include <linux/cpu.h>
+#include <linux/notifier.h>
 
 #include <asm/page.h>
 #include <asm/div64.h>
