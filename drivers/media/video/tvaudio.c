8b565a3d81e79af832f7ef2f72064ae0679a282e
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-03-18 08:08:14 -0300

    V4L/DVB (3547): Tvaudio.h are just i2c addresses. Merged into i2c-addr.h
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tvaudio.c
+++ drivers/media/video/tvaudio.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/audiochip.h>
 #include <media/v4l2-common.h>
 
-#include "tvaudio.h"
+#include <media/i2c-addr.h>
 
 /* ---------------------------------------------------------------------- */
 /* insmod args                                                            */
