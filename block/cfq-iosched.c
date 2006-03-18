fdb2f289bb7d3ebebbab93bc9fd318ae88ccd6ca
Al Viro <viro@zeniv.linux.org.uk>
2006-03-18 13:26:44 -0500

    [PATCH] fix the exclusion for ioprio_set()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -48,6 +48,8 @@ static int cfq_slice_idle = HZ / 100;
  */
 static const int cfq_max_depth = 2;
 
+static DEFINE_RWLOCK(cfq_exit_lock);
+
 /*
  * for the hash of cfqq inside the cfqd
  */
@@ -1355,12 +1357,18 @@ static inline void changed_ioprio(struct cfq_io_context *cic)
  */
 static int cfq_ioc_set_ioprio(struct io_context *ioc, unsigned int ioprio)
 {
-	struct cfq_io_context *cic = ioc->cic;
+	struct cfq_io_context *cic;
+
+	write_lock(&cfq_exit_lock);
+
+	cic = ioc->cic;
 
 	changed_ioprio(cic);
 
 	list_for_each_entry(cic, &cic->list, list)
 		changed_ioprio(cic);
+
+	write_unlock(&cfq_exit_lock);
 
 	return 0;
 }
@@ -1451,8 +1459,10 @@ cfq_get_io_context(struct cfq_data *cfqd, pid_t pid, gfp_t gfp_mask)
 		 */
 		cic->ioc = ioc;
 		cic->key = cfqd;
+		read_lock(&cfq_exit_lock);
 		ioc->set_ioprio = cfq_ioc_set_ioprio;
 		ioc->cic = cic;
+		read_unlock(&cfq_exit_lock);
 	} else {
 		struct cfq_io_context *__cic;
 
@@ -1488,7 +1498,9 @@ cfq_get_io_context(struct cfq_data *cfqd, pid_t pid, gfp_t gfp_mask)
 
 		__cic->ioc = ioc;
 		__cic->key = cfqd;
+		read_lock(&cfq_exit_lock);
 		list_add(&__cic->list, &cic->list);
+		read_unlock(&cfq_exit_lock);
 		cic = __cic;
 	}
 
