0188ffdadd67df54a32da3d3eb8346d675ec812b
Lennert Buytenhek <buytenh@wantstofly.org>
2006-03-20 17:10:17 +0000

    [ARM] 3373/1: move uengine loader to arch/arm/common
    Patch from Lennert Buytenhek
    
    Move the uengine loader from arch/arm/mach-ixp2000 to arch/arm/common
    so that ixp23xx can use it too.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/common/uengine.c
+++ arch/arm/common/uengine.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <asm/hardware.h>
 #include <asm/arch/ixp2000-regs.h>
-#include <asm/arch/uengine.h>
+#include <asm/hardware/uengine.h>
 #include <asm/io.h>
 
 #define USTORE_ADDRESS			0x000
