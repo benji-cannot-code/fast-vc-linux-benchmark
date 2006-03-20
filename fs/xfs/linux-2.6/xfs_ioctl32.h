92e00bc10a8d426de9955f7fd080595d95f4b379
Nathan Scott <nathans@sgi.com>
2006-03-20 13:25:48 +1100

    [XFS] Fix compiler warning from xfs_file_compat_invis_ioctl prototype.
    SGI-PV: 904196
    SGI-Modid: xfs-linux-melb:xfs-kern:25509a
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Nathan Scott <nathans@sgi.com>

--- fs/xfs/linux-2.6/xfs_ioctl32.h
+++ fs/xfs/linux-2.6/xfs_ioctl32.h
@@ -20,6 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __XFS_IOCTL32_H__
 
 extern long xfs_file_compat_ioctl(struct file *, unsigned, unsigned long);
-extern long xfs_file_compat_invis_ioctl(struct file *, unsigned, unsigned);
+extern long xfs_file_compat_invis_ioctl(struct file *, unsigned, unsigned long);
 
 #endif /* __XFS_IOCTL32_H__ */
