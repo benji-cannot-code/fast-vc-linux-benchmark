9786ac5f426422298fc4c6bc3c2446dcf0a999c1
Lennert Buytenhek <buytenh@wantstofly.org>
2006-03-20 21:02:37 +0000

    [ARM] 3374/1: ep93xx: gpio interrupt support
    Patch from Lennert Buytenhek
    
    Add GPIO interrupt support for the first 16 GPIO lines (port A
    and B.)
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-ep93xx/irqs.h
+++ include/asm-arm/arch-ep93xx/irqs.h
@@ -68,7 +68,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IRQ_EP93XX_SAI			60
 #define EP93XX_VIC2_VALID_IRQ_MASK	0x1fffffff
 
-#define NR_EP93XX_IRQS			64
+#define IRQ_EP93XX_GPIO(x)		(64 + (x))
+
+#define NR_EP93XX_IRQS			IRQ_EP93XX_GPIO(16)
 
 #define EP93XX_BOARD_IRQ(x)		(NR_EP93XX_IRQS + (x))
 #define EP93XX_BOARD_IRQS		32
