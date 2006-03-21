b4509c77e24848de50c036dc47ab8270d5c3d802
David S. Miller <davem@davemloft.net>
2006-03-20 22:52:32 -0800

    [INET]: Fix typo in Arnaldo's connection sock compat fixups.
    "struct inet_csk" --> "struct inet_connection_sock" :-)
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/inet_connection_sock.c
+++ net/ipv4/inet_connection_sock.c
@@ -673,7 +673,7 @@ EXPORT_SYMBOL_GPL(inet_csk_ctl_sock_create);
 int inet_csk_compat_getsockopt(struct sock *sk, int level, int optname,
 			       char __user *optval, int __user *optlen)
 {
-	const struct inet_csk *icsk = inet_csk(sk);
+	const struct inet_connection_sock *icsk = inet_csk(sk);
 
 	if (icsk->icsk_af_ops->compat_getsockopt != NULL)
 		return icsk->icsk_af_ops->compat_getsockopt(sk, level, optname,
@@ -687,7 +687,7 @@ EXPORT_SYMBOL_GPL(inet_csk_compat_getsockopt);
 int inet_csk_compat_setsockopt(struct sock *sk, int level, int optname,
 			       char __user *optval, int optlen)
 {
-	const struct inet_csk *icsk = inet_csk(sk);
+	const struct inet_connection_sock *icsk = inet_csk(sk);
 
 	if (icsk->icsk_af_ops->compat_setsockopt != NULL)
 		return icsk->icsk_af_ops->compat_setsockopt(sk, level, optname,
