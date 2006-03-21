5499fa7eac47f48dbc8d61880dad8016a41cc9c4
Michael Chan <mchan@broadcom.com>
2006-03-20 17:55:25 -0800

    [BNX2]: include <linux/vmalloc.h>
    Include <linux/vmalloc.h> so that it compiles properly on all archs.
    
    Update version to 1.4.38.
    
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_MODULE_NAME		"bnx2"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"1.4.37"
-#define DRV_MODULE_RELDATE	"February 1, 2006"
+#define DRV_MODULE_VERSION	"1.4.38"
+#define DRV_MODULE_RELDATE	"February 10, 2006"
 
 #define RUN_AT(x) (jiffies + (x))
 
