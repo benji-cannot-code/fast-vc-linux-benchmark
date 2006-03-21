2c40551c4c7a4e5e6ea4c94d6995a8cff9108a07
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2006-03-20 17:04:53 -0800

    [IPV6]: ROUTE: Add support for Router Preference (RFC4191).
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/ipv6_route.h
+++ include/linux/ipv6_route.h
@@ -28,7 +28,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RTF_FLOW	0x02000000	/* flow significant route	*/
 #define RTF_POLICY	0x04000000	/* policy route			*/
 
+#define RTF_PREF(pref)	((pref) << 27)
+#define RTF_PREF_MASK	0x18000000
+
 #define RTF_LOCAL	0x80000000
+
+#ifdef __KERNEL__
+#define IPV6_EXTRACT_PREF(flag)	(((flag) & RTF_PREF_MASK) >> 27)
+#define IPV6_DECODE_PREF(pref)	((pref) ^ 2)	/* 1:low,2:med,3:high */
+#endif
 
 struct in6_rtmsg {
 	struct in6_addr		rtmsg_dst;
