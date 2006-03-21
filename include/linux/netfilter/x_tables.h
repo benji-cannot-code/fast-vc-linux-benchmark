3e7b693692e3d6ab78853e15b4ed61e4fa30fc9c
Patrick McHardy <kaber@trash.net>
2006-03-20 18:03:40 -0800

    [NETFILTER]: x_tables: replace IPv4/IPv6 policy match by address family independant version
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netfilter/x_tables.h
+++ include/linux/netfilter/x_tables.h
@@ -127,6 +127,8 @@ struct xt_match
 	unsigned int matchsize;
 	unsigned int hooks;
 	unsigned short proto;
+
+	unsigned short family;
 	u_int8_t revision;
 };
 
@@ -170,6 +172,8 @@ struct xt_target
 	unsigned int targetsize;
 	unsigned int hooks;
 	unsigned short proto;
+
+	unsigned short family;
 	u_int8_t revision;
 };
 
