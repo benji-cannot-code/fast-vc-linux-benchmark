a787e2f498585803bf3c2eb699b3917200da66b5
Michael Chan <mchan@broadcom.com>
2006-03-20 17:50:08 -0800

    [BNX2]: Update version
    Update version to 1.4.37.
    
    Add missing flush_scheduled_work() in bnx2_suspend as noted by Jeff
    Garzik.
    
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_MODULE_NAME		"bnx2"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"1.4.31"
-#define DRV_MODULE_RELDATE	"January 19, 2006"
+#define DRV_MODULE_VERSION	"1.4.37"
+#define DRV_MODULE_RELDATE	"February 1, 2006"
 
 #define RUN_AT(x) (jiffies + (x))
 
@@ -5766,6 +5766,7 @@ bnx2_suspend(struct pci_dev *pdev, pm_message_t state)
 	if (!netif_running(dev))
 		return 0;
 
+	flush_scheduled_work();
 	bnx2_netif_stop(bp);
 	netif_device_detach(dev);
 	del_timer_sync(&bp->timer);
