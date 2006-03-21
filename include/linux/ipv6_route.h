600c2fbe40be000c833ac8ec2a2e607adcf7c0f2
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2006-03-20 17:06:24 -0800

    [IPV6]: ROUTE: Add experimental support for Route Information Option in RA (RFC4191).
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/ipv6_route.h
+++ include/linux/ipv6_route.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RTF_NONEXTHOP	0x00200000	/* route with no nexthop	*/
 #define RTF_EXPIRES	0x00400000
 
+#define RTF_ROUTEINFO	0x00800000	/* route information - RA	*/
+
 #define RTF_CACHE	0x01000000	/* cache entry			*/
 #define RTF_FLOW	0x02000000	/* flow significant route	*/
 #define RTF_POLICY	0x04000000	/* policy route			*/
