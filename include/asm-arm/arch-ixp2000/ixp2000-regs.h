e401b20244300799cbeb622a1107a47a40eba29c
Lennert Buytenhek <buytenh@wantstofly.org>
2006-03-22 20:14:11 +0000

    [ARM] 3381/1: ixp2000: fix slowport write timing control register fields
    Patch from Lennert Buytenhek
    
    The original version of the chip docs had the PW and SU fields in
    the slowport write timing control register accidentally reversed.
    This is mentioned in the errata (documentation change #4) and fixed
    in newer docs.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-ixp2000/ixp2000-regs.h
+++ include/asm-arm/arch-ixp2000/ixp2000-regs.h
@@ -354,8 +354,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Masks and shifts for various fields in the WTC and RTC registers.
  */
 #define	SLOWPORT_WRTC_MASK_HD		0x0003
-#define	SLOWPORT_WRTC_MASK_SU		0x003c
-#define	SLOWPORT_WRTC_MASK_PW		0x03c0
+#define	SLOWPORT_WRTC_MASK_PW		0x003c
+#define	SLOWPORT_WRTC_MASK_SU		0x03c0
 
 #define	SLOWPORT_WRTC_SHIFT_HD		0x00
 #define	SLOWPORT_WRTC_SHIFT_SU		0x02
