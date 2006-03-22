1c945394c565c895e6487aa3e7264865166dd5c5
Shaun Pereira <spereira@tusc.com.au>
2006-03-22 00:00:40 -0800

    [X25]: fix kernel error message 64 bit kernel
    Fixes the following error from kernel
    T2 kernel: schedule_timeout:
    wrong timeout value ffffffffffffffff from ffffffff88164796
    
    Signed-off-by: Shaun Pereira <spereira@tusc.com.au>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/x25/af_x25.c
+++ net/x25/af_x25.c
@@ -745,7 +745,7 @@ out:
 	return rc;
 }
 
-static int x25_wait_for_data(struct sock *sk, int timeout)
+static int x25_wait_for_data(struct sock *sk, long timeout)
 {
 	DECLARE_WAITQUEUE(wait, current);
 	int rc = 0;
