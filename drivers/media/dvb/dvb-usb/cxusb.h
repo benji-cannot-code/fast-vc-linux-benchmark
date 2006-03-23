b5d706d1b36cc90e70070b54e21b5c74220f3656
Michael Krufky <mkrufky@linuxtv.org>
2006-03-23 01:11:18 -0300

    V4L/DVB (3575): Cxusb: fix i2c debug messages for bluebird devices
    Only the Medion boxes return 0x08 after an i2c read/write.
    The bluebird devices do not return anything at all.
    This patch conditionalizes the test for the 0x08 return code
    to produce a warning message when using the Medion box, only.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/cxusb.h
+++ drivers/media/dvb/dvb-usb/cxusb.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int dvb_usb_cxusb_debug;
 #define deb_info(args...)   dprintk(dvb_usb_cxusb_debug,0x01,args)
+#define deb_i2c(args...)    if (d->udev->descriptor.idVendor == USB_VID_MEDION) \
+				dprintk(dvb_usb_cxusb_debug,0x01,args)
 
 /* usb commands - some of it are guesses, don't have a reference yet */
 #define CMD_I2C_WRITE     0x08
