fe5e9d34e94958a300f908d55d046258dc2696dc
Alexey Dobriyan <adobriyan@gmail.com>
2006-03-23 03:01:01 -0800

    [PATCH] fs/*/file.c: drop insane header dependencies
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/qnx4/file.c
+++ fs/qnx4/file.c
@@ -13,10 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 27-06-1998 by Frank Denis : file overwriting.
  */
 
-#include <linux/config.h>
-#include <linux/types.h>
 #include <linux/fs.h>
-#include <linux/time.h>
 #include <linux/qnx4_fs.h>
 
 /*
