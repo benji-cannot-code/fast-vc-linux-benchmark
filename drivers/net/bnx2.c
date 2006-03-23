abd7017b75bf1f67d9535382a1e31f9df197602f
Michael Chan <mchan@broadcom.com>
2006-03-23 01:13:43 -0800

    [BNX2]: Separate tx producer and consumer fields
    Put the tx producer and consumer fields in separate cache lines in
    the device structure, similar to the VJ net channel queue structure.
    
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/workqueue.h>
 #include <linux/crc32.h>
 #include <linux/prefetch.h>
+#include <linux/cache.h>
 
 #include "bnx2.h"
 #include "bnx2_fw.h"
