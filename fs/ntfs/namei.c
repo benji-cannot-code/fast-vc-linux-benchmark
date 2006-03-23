55f4c366ecd4c7ef5030bbdbebb8779bbe062bd0
Anton Altaparmakov <aia21@cantab.net>
2006-03-23 14:57:43 +0000

    NTFS: Fix an (innocent) off-by-one error in the runlist code.
    Signed-off-by: Anton Altaparmakov <aia21@cantab.net>

--- fs/ntfs/namei.c
+++ fs/ntfs/namei.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * namei.c - NTFS kernel directory inode operations. Part of the Linux-NTFS
  *	     project.
  *
- * Copyright (c) 2001-2004 Anton Altaparmakov
+ * Copyright (c) 2001-2006 Anton Altaparmakov
  *
  * This program/include file is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
