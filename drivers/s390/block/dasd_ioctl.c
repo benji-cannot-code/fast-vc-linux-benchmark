7316a0bfcc2009310adfa02b2db9a1e6d6b4a1d0
Horst Hummel <horst.hummel@de.ibm.com>
2006-03-24 03:15:24 -0800

    [PATCH] s390: random values in result of BIODASDINFO2
    Use kzalloc to get a zeroed buffer for the structure returned to user space by
    the BIODASDINFO2 ioctl.  Not all fields are set up, e.g.  the read_devno is
    missing.
    
    Signed-off-by: Horst Hummel <horst.hummel@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/s390/block/dasd_ioctl.c
+++ drivers/s390/block/dasd_ioctl.c
@@ -261,7 +261,7 @@ dasd_ioctl_information(struct dasd_device *device,
 	if (!device->discipline->fill_info)
 		return -EINVAL;
 
-	dasd_info = kmalloc(sizeof(struct dasd_information2_t), GFP_KERNEL);
+	dasd_info = kzalloc(sizeof(struct dasd_information2_t), GFP_KERNEL);
 	if (dasd_info == NULL)
 		return -ENOMEM;
 
@@ -304,8 +304,7 @@ dasd_ioctl_information(struct dasd_device *device,
 		memcpy(dasd_info->type, device->discipline->name, 4);
 	else
 		memcpy(dasd_info->type, "none", 4);
-	dasd_info->req_queue_len = 0;
-	dasd_info->chanq_len = 0;
+
 	if (device->request_queue->request_fn) {
 		struct list_head *l;
 #ifdef DASD_EXTENDED_PROFILING
