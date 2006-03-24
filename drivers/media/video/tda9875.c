c153b44e4a126ed63dfc7885ff7e5d4bea070f7f
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-03-23 21:45:24 -0300

    V4L/DVB (3546): Fix Compilation after moving bttv code
    - Missing a Makefile for bt8xx
    - rds.h were at wrong directory, since it is a global header for an internal
      interface
    - tda7432 and tda9875 were dependent from bttv.h
    - bttv.h were holding i2c addresses
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tda9875.c
+++ drivers/media/video/tda9875.c
@@ -31,13 +31,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c-algo-bit.h>
 #include <linux/init.h>
 
-#include "bttv.h"
 #include <media/audiochip.h>
+
+#include <media/i2c-addr.h>
 
 static int debug; /* insmod parameter */
 module_param(debug, int, S_IRUGO | S_IWUSR);
 MODULE_LICENSE("GPL");
-
 
 /* Addresses to scan */
 static unsigned short normal_i2c[] =  {
