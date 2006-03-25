1847e474a786d7e02626e889a9f7295a83a2b4e5
Albert Lee <albertcc@tw.ibm.com>
2006-03-25 17:39:34 +0800

    [PATCH] libata-dev: Fix merge problem with upstream
    Fix merge problem with upstream.
    Changes:
    1. add missing dev->cdb_len = 16 for ATA devices
    2. use ata_pio_task instead of atapi_packet_task
    
    Signed-off-by: Albert Lee <albertcc@tw.ibm.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/scsi/libata-core.c
+++ drivers/scsi/libata-core.c
@@ -1300,6 +1300,7 @@ static int ata_dev_configure(struct ata_port *ap, struct ata_device *dev,
 				ap->id, device, dev->multi_count);
 		}
 
+		dev->cdb_len = 16;
 	}
 
 	/* ATAPI-specific feature tests */
@@ -4289,7 +4290,7 @@ unsigned int ata_qc_issue_prot(struct ata_queued_cmd *qc)
 		/* send cdb by polling if no cdb interrupt */
 		if ((!(qc->dev->flags & ATA_DFLAG_CDB_INTR)) ||
 		    (qc->tf.flags & ATA_TFLAG_POLLING))
-			ata_port_queue_task(ap, atapi_packet_task, ap, 0);
+			ata_port_queue_task(ap, ata_pio_task, ap, 0);
 		break;
 
 	case ATA_PROT_ATAPI_DMA:
@@ -4301,7 +4302,7 @@ unsigned int ata_qc_issue_prot(struct ata_queued_cmd *qc)
 
 		/* send cdb by polling if no cdb interrupt */
 		if (!(qc->dev->flags & ATA_DFLAG_CDB_INTR))
-			ata_port_queue_task(ap, atapi_packet_task, ap, 0);
+			ata_port_queue_task(ap, ata_pio_task, ap, 0);
 		break;
 
 	default:
