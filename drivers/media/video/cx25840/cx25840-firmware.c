a7f0443ceb794bb5944ef2b9d6e722b478ae00c6
Hans Verkuil <hverkuil@xs4all.nl>
2006-03-25 10:26:09 -0300

    V4L/DVB (3610): Added the new routing commands to cx25840.
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx25840/cx25840-firmware.c
+++ drivers/media/video/cx25840/cx25840-firmware.c
@@ -21,8 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c-algo-bit.h>
 #include <linux/firmware.h>
 #include <media/v4l2-common.h>
+#include <media/cx25840.h>
 
-#include "cx25840.h"
+#include "cx25840-core.h"
 
 #define FWFILE "v4l-cx25840.fw"
 #define FWSEND 1024
--- drivers/media/video/cx25840/cx25840-vbi.c
+++ drivers/media/video/cx25840/cx25840-vbi.c
@@ -20,8 +20,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 #include <linux/i2c.h>
 #include <media/v4l2-common.h>
+#include <media/cx25840.h>
 
-#include "cx25840.h"
+#include "cx25840-core.h"
 
 static int odd_parity(u8 c)
 {
