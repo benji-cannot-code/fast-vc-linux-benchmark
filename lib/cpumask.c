16904f1b0fc5d4fe0ce48ba28c756de10d97844c
Andrew Morton <akpm@osdl.org>
2006-03-25 03:08:08 -0800

    [PATCH] cpumask: uninline first_cpu()
               text    data     bss     dec     hex filename
    before: 3490577 1322408  360000 5172985  4eeef9 vmlinux
    after:  3488027 1322496  360128 5170651  4ee5db vmlinux
    
    Cc: Paul Jackson <pj@sgi.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ lib/cpumask.c
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <linux/kernel.h>
+#include <linux/bitops.h>
+#include <linux/cpumask.h>
+#include <linux/module.h>
+
+int __first_cpu(const cpumask_t *srcp)
+{
+	return min_t(int, NR_CPUS, find_first_bit(srcp->bits, NR_CPUS));
+}
+EXPORT_SYMBOL(__first_cpu);
+
