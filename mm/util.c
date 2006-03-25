178be5288816bf494711362fcf0ef3cf784b0ac1
Pekka Enberg <penberg@cs.helsinki.fi>
2006-03-25 03:06:43 -0800

    [PATCH] slab: optimize constant-size kzalloc calls
    As suggested by Eric Dumazet, optimize kzalloc() calls that pass a
    compile-time constant size.  Please note that the patch increases kernel
    text slightly (~200 bytes for defconfig on x86).
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/util.c
+++ mm/util.c
@@ -6,18 +6,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 
 /**
- * kzalloc - allocate memory. The memory is set to zero.
+ * __kzalloc - allocate memory. The memory is set to zero.
  * @size: how many bytes of memory are required.
  * @flags: the type of memory to allocate.
  */
-void *kzalloc(size_t size, gfp_t flags)
+void *__kzalloc(size_t size, gfp_t flags)
 {
 	void *ret = ____kmalloc(size, flags);
 	if (ret)
 		memset(ret, 0, size);
 	return ret;
 }
-EXPORT_SYMBOL(kzalloc);
+EXPORT_SYMBOL(__kzalloc);
 
 /*
  * kstrdup - allocate space for and copy an existing string
