58a19ffab2c546eb45e9c146f2a816036660f2c9
Badari Pulavarty <pbadari@us.ibm.com>
2006-03-26 01:38:01 -0800

    [PATCH] map multiple blocks for mpage_readpages()
    This patch changes mpage_readpages() and get_block() to get the disk mapping
    information for multiple blocks at the same time.
    
    b_size represents the amount of disk mapping that needs to mapped.  On the
    successful get_block() b_size indicates the amount of disk mapping thats
    actually mapped.  Only the filesystems who care to use this information and
    provide multiple disk blocks at a time can choose to do so.
    
    No changes are needed for the filesystems who wants to ignore this.
    
    [akpm@osdl.org: cleanups]
    Signed-off-by: Badari Pulavarty <pbadari@us.ibm.com>
    Cc: Mingming Cao <cmm@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/xfs/linux-2.6/xfs_aops.c
+++ fs/xfs/linux-2.6/xfs_aops.c
@@ -1311,8 +1311,9 @@ xfs_get_block(
 	struct buffer_head	*bh_result,
 	int			create)
 {
-	return __xfs_get_block(inode, iblock, 0, bh_result,
-					create, 0, BMAPI_WRITE);
+	return __xfs_get_block(inode, iblock,
+				bh_result->b_size >> inode->i_blkbits,
+				bh_result, create, 0, BMAPI_WRITE);
 }
 
 STATIC int
