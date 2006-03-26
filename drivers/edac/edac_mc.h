e758da33102be69b68a82601029b5e2044ba983c
Dave Peterson <dsp@llnl.gov>
2006-03-26 01:38:49 -0800

    [PATCH] EDAC: kobject/sysfs fixes
    - After we unregister a kobject, wait for our kobject release method
      to call complete().  This causes us to wait until the kobject
      reference count reaches 0.  Otherwise, a task accessing the EDAC
      sysfs interface can hold the reference count above 0 until after the
      EDAC module has been unloaded.  When the reference count finally
      drops to 0, this will result in an attempt to call our release
      method inside the EDAC module after the module has already been
      unloaded.
    
      This isn't the best fix, since a process can get stuck sleeping forever
      uninterruptibly if the user does the following:
    
          rmmod my_module < /sys/my_sysfs/file
    
      I'll go back and implement a better fix later.  However this should
      be ok for now.
    
    - Call edac_remove_sysfs_mci_device() from edac_mc_del_mc() rather
      than from edac_mc_free().  Since edac_mc_add_mc() calls
      edac_create_sysfs_mci_device(), edac_mc_del_mc() should call
      edac_remove_sysfs_mci_device().
    
    Signed-off-by: David S. Peterson <dsp@llnl.gov>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/edac/edac_mc.h
+++ drivers/edac/edac_mc.h
@@ -186,11 +186,6 @@ enum scrub_type {
 #define SCRUB_FLAG_HW_PROG_SRC	BIT(SCRUB_HW_PROG_SRC_CORR)
 #define SCRUB_FLAG_HW_TUN	BIT(SCRUB_HW_TUNABLE)
 
-enum mci_sysfs_status {
-	MCI_SYSFS_INACTIVE = 0,	/* sysfs entries NOT registered */
-	MCI_SYSFS_ACTIVE	/* sysfs entries ARE registered */
-};
-
 /* FIXME - should have notify capabilities: NMI, LOG, PROC, etc */
 
 /*
@@ -300,6 +295,7 @@ struct csrow_info {
 	struct mem_ctl_info *mci;	/* the parent */
 
 	struct kobject kobj;	/* sysfs kobject for this csrow */
+	struct completion kobj_complete;
 
 	/* FIXME the number of CHANNELs might need to become dynamic */
 	u32 nr_channels;
@@ -320,8 +316,6 @@ struct mem_ctl_info {
 				   edac_cap would not have that capability. */
 	unsigned long scrub_cap;	/* chipset scrub capabilities */
 	enum scrub_type scrub_mode;	/* current scrub mode */
-
-	enum mci_sysfs_status sysfs_active;	/* status of sysfs */
 
 	/* pointer to edac checking routine */
 	void (*edac_check) (struct mem_ctl_info * mci);
@@ -360,6 +354,7 @@ struct mem_ctl_info {
 
 	/* edac sysfs device control */
 	struct kobject edac_mci_kobj;
+	struct completion kobj_complete;
 };
 
 
