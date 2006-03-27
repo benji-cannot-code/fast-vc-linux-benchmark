83adf19ea68719e016673d3bba27a69577102330
Tobias Klauser <tklauser@nuerscht.ch>
2006-03-27 01:17:39 -0800

    [PATCH] drivers/video: Use ARRAY_SIZE macro
    Use ARRAY_SIZE macro instead of sizeof(x)/sizeof(x[0]) and remove
    duplicates of ARRAY_SIZE.  Some coding style and trailing whitespaces are
    also fixed.
    
    Compile-tested where possible (some are other arch or BROKEN)
    
    Signed-off-by: Tobias Klauser <tklauser@nuerscht.ch>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/modedb.c
+++ drivers/video/modedb.c
@@ -501,7 +501,7 @@ int fb_find_mode(struct fb_var_screeninfo *var,
     /* Set up defaults */
     if (!db) {
 	db = modedb;
-	dbsize = sizeof(modedb)/sizeof(*modedb);
+	dbsize = ARRAY_SIZE(modedb);
     }
     if (!default_mode)
 	default_mode = &modedb[DEFAULT_MODEDB_INDEX];
