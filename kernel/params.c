377e4f9129584daf727b1ecb7db2b883127ceb06
Eric Sesterhenn <snakebyte@gmx.de>
2006-03-28 01:56:24 -0800

    [PATCH] Change dash2underscore() return value to char
    Since dash2underscore() just operates and returns chars, I guess its safe
    to change the return value to a char.  With my .config, this reduces its
    size by 5 bytes.
    
       text    data     bss     dec     hex filename
       4155     152       0    4307    10d3 params.o.orig
       4150     152       0    4302    10ce params.o
    
    Signed-off-by: Eric Sesterhenn <snakebyte@gmx.de>
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/params.c
+++ kernel/params.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DEBUGP(fmt, a...)
 #endif
 
-static inline int dash2underscore(char c)
+static inline char dash2underscore(char c)
 {
 	if (c == '-')
 		return '_';
