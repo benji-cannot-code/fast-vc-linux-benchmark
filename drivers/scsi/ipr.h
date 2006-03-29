0ff6e516339c353bc25ef050daae5ef86f7a172b
Brian King <brking@us.ibm.com>
2006-03-29 09:37:49 -0600

    [SCSI] ipr: Bump version
    Bump ipr driver version to 2.1.3
    
    Signed-off-by: Brian King <brking@us.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/ipr.h
+++ drivers/scsi/ipr.h
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Literals
  */
-#define IPR_DRIVER_VERSION "2.1.2"
-#define IPR_DRIVER_DATE "(February 8, 2006)"
+#define IPR_DRIVER_VERSION "2.1.3"
+#define IPR_DRIVER_DATE "(March 29, 2006)"
 
 /*
  * IPR_MAX_CMD_PER_LUN: This defines the maximum number of outstanding
