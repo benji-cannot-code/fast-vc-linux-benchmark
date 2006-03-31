8485e5642a0480da5c50fb510af19f9045663a75
Tejun Heo <htejun@gmail.com>
2006-03-31 20:36:47 +0900

    [PATCH] libata: fix ata_qc_issue failure path
    On sg_err failure path, ata_qc_issue() doesn't mark the qc active
    before returning.  This triggers WARN_ON() in __ata_qc_complete() when
    the qc gets completed.  This patch moves ap->active_tag and
    QCFLAG_ACTIVE setting to the top of the function.
    
    Signed-off-by: Tejun Heo <htejun@gmail.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/scsi/libata-core.c
+++ drivers/scsi/libata-core.c
@@ -4007,6 +4007,9 @@ unsigned int ata_qc_issue(struct ata_queued_cmd *qc)
 {
 	struct ata_port *ap = qc->ap;
 
+	qc->ap->active_tag = qc->tag;
+	qc->flags |= ATA_QCFLAG_ACTIVE;
+
 	if (ata_should_dma_map(qc)) {
 		if (qc->flags & ATA_QCFLAG_SG) {
 			if (ata_sg_setup(qc))
@@ -4020,9 +4023,6 @@ unsigned int ata_qc_issue(struct ata_queued_cmd *qc)
 	}
 
 	ap->ops->qc_prep(qc);
-
-	qc->ap->active_tag = qc->tag;
-	qc->flags |= ATA_QCFLAG_ACTIVE;
 
 	return ap->ops->qc_issue(qc);
 
