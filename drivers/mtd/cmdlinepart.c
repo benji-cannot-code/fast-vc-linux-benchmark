4d48d6499ee6614b78086c82a462ab84721a4a06
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2006-03-31 02:29:46 -0800

    [PATCH] mtd cmdlinepart: allow zero offset value
    Current cmdlinepart.c uses offset value 0 to specify a continuous
    partition.  This prevents creating a second partition starting at 0.
    
    For example, I can split 4MB device using "mtdparts=id:2M,2M", but I can
    not do "mtdparts=id:2M@2M,2M@0" to swap mtd0 and mtd1.
    
    This patch introduces special OFFSET_CONTINUOUS value for a continuous
    partition and allows 0 for offset value.
    
    Also this patch replaces 0xffffffff with UINT_MAX for SIZE_REMAINING.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/mtd/cmdlinepart.c
+++ drivers/mtd/cmdlinepart.c
@@ -43,7 +43,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /* special size referring to all the remaining space in a partition */
-#define SIZE_REMAINING 0xffffffff
+#define SIZE_REMAINING UINT_MAX
+#define OFFSET_CONTINUOUS UINT_MAX
 
 struct cmdline_mtd_partition {
 	struct cmdline_mtd_partition *next;
@@ -76,7 +77,7 @@ static struct mtd_partition * newpart(char *s,
 {
 	struct mtd_partition *parts;
 	unsigned long size;
-	unsigned long offset = 0;
+	unsigned long offset = OFFSET_CONTINUOUS;
 	char *name;
 	int name_len;
 	unsigned char *extra_mem;
@@ -315,7 +316,7 @@ static int parse_cmdline_partitions(struct mtd_info *master,
 		{
 			for(i = 0, offset = 0; i < part->num_parts; i++)
 			{
-				if (!part->parts[i].offset)
+				if (part->parts[i].offset == OFFSET_CONTINUOUS)
 				  part->parts[i].offset = offset;
 				else
 				  offset = part->parts[i].offset;
