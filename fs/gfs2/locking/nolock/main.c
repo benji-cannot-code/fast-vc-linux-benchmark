de1352e85fbe8537def020b6aa8f5820ded0b966
Steven Whitehouse <swhiteho@redhat.com>
2006-03-31 16:48:41 -0500

    [GFS2] Update GFS2 for the recent pull from Linus
    Some interfaces have changed. In particular one of the posix
    locking functions has changed prototype, along with the
    address space operation invalidatepage and the block getting
    callback to the direct IO function.
    
    In addition add the splice file operations. These will need to
    be updated to support AOP_TRUNCATED_PAGE before they will be
    of much use to us.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/locking/nolock/main.c
+++ fs/gfs2/locking/nolock/main.c
@@ -177,14 +177,13 @@ static void nolock_sync_lvb(lm_lock_t *lock, char *lvb)
 static int nolock_plock_get(lm_lockspace_t *lockspace, struct lm_lockname *name,
 			    struct file *file, struct file_lock *fl)
 {
-	struct file_lock *tmp;
+	struct file_lock tmp;
+	int ret;
 
-	lock_kernel();
-	tmp = posix_test_lock(file, fl);
+	ret = posix_test_lock(file, fl, &tmp);
 	fl->fl_type = F_UNLCK;
-	if (tmp)
-		memcpy(fl, tmp, sizeof(struct file_lock));
-	unlock_kernel();
+	if (ret)
+		memcpy(fl, &tmp, sizeof(struct file_lock));
 
 	return 0;
 }
@@ -193,9 +192,7 @@ static int nolock_plock(lm_lockspace_t *lockspace, struct lm_lockname *name,
 			struct file *file, int cmd, struct file_lock *fl)
 {
 	int error;
-	lock_kernel();
 	error = posix_lock_file_wait(file, fl);
-	unlock_kernel();
 	return error;
 }
 
@@ -203,9 +200,7 @@ static int nolock_punlock(lm_lockspace_t *lockspace, struct lm_lockname *name,
 			  struct file *file, struct file_lock *fl)
 {
 	int error;
-	lock_kernel();
 	error = posix_lock_file_wait(file, fl);
-	unlock_kernel();
 	return error;
 }
 
