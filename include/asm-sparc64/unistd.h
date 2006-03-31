a0cb4c2704bf2877f99382781402fbb561deedfc
David S. Miller <davem@sunset.davemloft.net>
2006-03-31 02:04:15 -0800

    [SPARC]: Wire up sys_splice() into the syscall tables.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/unistd.h
+++ include/asm-sparc64/unistd.h
@@ -251,7 +251,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 #define __NR_time		231 /* Linux sparc32                               */
 #endif
-/* #define __NR_oldstat         232    Linux Specific                              */
+#define __NR_sys_splice         232 /* Linux Specific                              */
 #define __NR_stime              233 /* Linux Specific                              */
 #define __NR_statfs64           234 /* Linux Specific                              */
 #define __NR_fstatfs64          235 /* Linux Specific                              */
