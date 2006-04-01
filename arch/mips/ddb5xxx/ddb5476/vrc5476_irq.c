2fa791b055cc3fcff659515bff405d47caf379b9
Ralf Baechle <ralf@linux-mips.org>
2006-04-01 21:17:45 +0100

    [MIPS] Rewrite spurious_interrupt from assembler to C.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ddb5xxx/ddb5476/vrc5476_irq.c
+++ arch/mips/ddb5xxx/ddb5476/vrc5476_irq.c
@@ -81,8 +81,6 @@ vrc5476_irq_init(u32 base)
 asmlinkage void
 vrc5476_irq_dispatch(struct pt_regs *regs)
 {
-	extern void spurious_interrupt(void);
-
 	u32 mask;
 	int nile4_irq;
 
@@ -108,5 +106,5 @@ vrc5476_irq_dispatch(struct pt_regs *regs)
 			return;
 		}
 	}
-	spurious_interrupt();
+	spurious_interrupt(regs);
 }
