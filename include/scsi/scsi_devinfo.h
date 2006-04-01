86c04100e8fdf06884f0e1233f29aff34a83516b
James Bottomley <James.Bottomley@steeleye.com>
2006-03-31 20:07:45 -0600

    [SCSI] add SCSI_UNKNOWN and LUN transfer limit restrictions
    Original From: Ingo Flaschberger <if@xip.at>
    
    To support the RA4100 array from Compaq.
    
    This patch now correctly handles SCSI_UNKNOWN types with regard to
    BLIST_REPORTLUNS2 (allow it) and cdb[1] LUN inclusion (don't).
    
    It also allows a BLIST_MAX_512 flag to restrict the maximum transfer
    length to 512 blocks (apparently this is an RA4100 problem).
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- include/scsi/scsi_devinfo.h
+++ include/scsi/scsi_devinfo.h
@@ -29,4 +29,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BLIST_NO_ULD_ATTACH	0x100000 /* device is actually for RAID config */
 #define BLIST_SELECT_NO_ATN	0x200000 /* select without ATN */
 #define BLIST_RETRY_HWERROR	0x400000 /* retry HARDWARE_ERROR */
+#define BLIST_MAX_512		0x800000 /* maximum 512 sector cdb length */
 #endif
