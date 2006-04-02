7ca3fb4c1c62b58173401b6ae364a168495897fc
Jens Axboe <axboe@suse.de>
2006-04-02 23:11:04 +0200

    [PATCH] splice: fix page stealing LRU handling.
    Originally from Nick Piggin, just adapted to the newer branch.
    
    You can't check PageLRU without holding zone->lru_lock.  The page
    release code can get away with it only because the page refcount is 0 at
    that point. Also, you can't reliably remove pages from the LRU unless
    the refcount is 0. Ever.
    
    Signed-off-by: Nick Piggin <nickpiggin@yahoo.com.au>
    Signed-off-by: Jens Axboe <axboe@suse.de>

--- include/linux/pipe_fs_i.h
+++ include/linux/pipe_fs_i.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define PIPE_BUFFERS (16)
 
+#define PIPE_BUF_FLAG_STOLEN	0x01
+#define PIPE_BUF_FLAG_LRU	0x02
+
 struct pipe_buffer {
 	struct page *page;
 	unsigned int offset, len;
 	struct pipe_buf_operations *ops;
+	unsigned int flags;
 };
 
 struct pipe_buf_operations {
