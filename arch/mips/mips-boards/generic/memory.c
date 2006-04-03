acdea98d65a1f6d85648cb92e5d1c02b832ecc4c
Ralf Baechle <ralf@linux-mips.org>
2006-04-03 10:17:21 +0100

    [MIPS] Replace redundant declarations of _end by <asm/sections.h>.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/generic/memory.c
+++ arch/mips/mips-boards/generic/memory.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/bootinfo.h>
 #include <asm/page.h>
+#include <asm/sections.h>
 
 #include <asm/mips-boards/prom.h>
 
@@ -46,9 +47,6 @@ static char *mtypes[3] = {
 	"Free memmory",
 };
 #endif
-
-/* References to section boundaries */
-extern char _end;
 
 struct prom_pmemblock * __init prom_getmdesc(void)
 {
@@ -107,7 +105,7 @@ struct prom_pmemblock * __init prom_getmdesc(void)
 
 	mdesc[3].type = yamon_dontuse;
 	mdesc[3].base = 0x00100000;
-	mdesc[3].size = CPHYSADDR(PAGE_ALIGN(&_end)) - mdesc[3].base;
+	mdesc[3].size = CPHYSADDR(PAGE_ALIGN((unsigned long)&_end)) - mdesc[3].base;
 
 	mdesc[4].type = yamon_free;
 	mdesc[4].base = CPHYSADDR(PAGE_ALIGN(&_end));
--- arch/mips/mips-boards/sim/sim_mem.c
+++ arch/mips/mips-boards/sim/sim_mem.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/bootinfo.h>
 #include <asm/page.h>
+#include <asm/sections.h>
 
 #include <asm/mips-boards/prom.h>
 
@@ -39,9 +40,6 @@ static char *mtypes[3] = {
 	"SIM free memory",
 };
 #endif
-
-/* References to section boundaries */
-extern char _end;
 
 struct prom_pmemblock * __init prom_getmdesc(void)
 {
