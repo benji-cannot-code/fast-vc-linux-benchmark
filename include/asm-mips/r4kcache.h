55a72c9c1318df9bb67960e529a1433e2eed9fc9
Ralf Baechle <ralf@linux-mips.org>
2006-04-03 23:32:39 +0100

    [MIPS] Use "R" constraint for cache_op.
    Gcc might emit an absolute address for the the "m" constraint which
    gas unfortunately does not permit.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/r4kcache.h
+++ include/asm-mips/r4kcache.h
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	"	cache	%0, %1					\n"	\
 	"	.set	pop					\n"	\
 	:								\
-	: "i" (op), "m" (*(unsigned char *)(addr)))
+	: "i" (op), "R" (*(unsigned char *)(addr)))
 
 static inline void flush_icache_line_indexed(unsigned long addr)
 {
