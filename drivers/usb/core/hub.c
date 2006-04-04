13ba9dbf99e203744a4e612b0f5497407756ea59
Adrian Bunk <bunk@stusta.de>
2006-04-04 09:56:04 +0200

    [PATCH] USB: drivers/usb/core/: remove unused exports
    This patch removes the following unused EXPORT_SYMBOL's:
    - hub.c: usb_set_device_state
    - usb.c: usb_alloc_dev
    - usb.c: usb_disconnect
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1023,7 +1023,6 @@ void usb_set_device_state(struct usb_device *udev,
 		recursively_mark_NOTATTACHED(udev);
 	spin_unlock_irqrestore(&device_state_lock, flags);
 }
-EXPORT_SYMBOL(usb_set_device_state);
 
 
 #ifdef CONFIG_PM
