435ea6ef7a8b6fdaab9c39b1b9c1b0058d4156aa
Brian Haley <brian.haley@hp.com>
2006-04-07 15:00:06 -0700

    [NETFILTER]: Fix build with CONFIG_NETFILTER=y/m on IA64
    Can't build with CONFIG_NETFILTER=y/m on IA64, there's a missing
    #include in net/ipv6/netfilter.c
    
    net/ipv6/netfilter.c: In function `nf_ip6_checksum':
    net/ipv6/netfilter.c:92: warning: implicit declaration of function
    `csum_ipv6_magic'
    
    Signed-off-by: Brian Haley <brian.haley@hp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/netfilter.c
+++ net/ipv6/netfilter.c
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ipv6.h>
 #include <net/ip6_route.h>
 #include <net/xfrm.h>
+#include <net/ip6_checksum.h>
 
 int ip6_route_me_harder(struct sk_buff *skb)
 {
