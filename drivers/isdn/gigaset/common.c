b955ab528d0cde1a11f754de73f707942c8b6946
Adrian Bunk <bunk@stusta.de>
2006-04-10 22:55:19 -0700

    [PATCH] isdn/gigaset/common.c: fix a memory leak
    Fix a memory leak spotted by the Coverity checker if
    (!try_module_get(owner)).
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Acked-by: Tilman Schmidt <tilman@imap.cc>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/isdn/gigaset/common.c
+++ drivers/isdn/gigaset/common.c
@@ -1111,8 +1111,9 @@ struct gigaset_driver *gigaset_initdriver(unsigned minor, unsigned minors,
 	drv = kmalloc(sizeof *drv, GFP_KERNEL);
 	if (!drv)
 		return NULL;
+
 	if (!try_module_get(owner))
-		return NULL;
+		goto out1;
 
 	drv->cs = NULL;
 	drv->have_tty = 0;
@@ -1126,10 +1127,11 @@ struct gigaset_driver *gigaset_initdriver(unsigned minor, unsigned minors,
 
 	drv->cs = kmalloc(minors * sizeof *drv->cs, GFP_KERNEL);
 	if (!drv->cs)
-		goto out1;
+		goto out2;
+
 	drv->flags = kmalloc(minors * sizeof *drv->flags, GFP_KERNEL);
 	if (!drv->flags)
-		goto out2;
+		goto out3;
 
 	for (i = 0; i < minors; ++i) {
 		drv->flags[i] = 0;
@@ -1146,11 +1148,12 @@ struct gigaset_driver *gigaset_initdriver(unsigned minor, unsigned minors,
 
 	return drv;
 
-out2:
+out3:
 	kfree(drv->cs);
+out2:
+	module_put(owner);
 out1:
 	kfree(drv);
-	module_put(owner);
 	return NULL;
 }
 EXPORT_SYMBOL_GPL(gigaset_initdriver);
