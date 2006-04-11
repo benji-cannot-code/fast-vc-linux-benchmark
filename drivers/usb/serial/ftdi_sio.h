51c37f56e55af688333cd8a97f7f977be96c1d63
Luiz Fernando N. Capitulino <lcapitulino@mandriva.com.br>
2006-04-11 15:52:41 -0300

    [PATCH] USB: ftdi_sio: Adds support for iPlus device.
    Adds support in ftdi_sio usbserial driver for USB modems sold by
    Plus GSM Company in Poland.
    
    Signed-off-by: Luiz Fernando Capitulino <lcapitulino@mandriva.com.br>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -40,6 +40,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* www.thoughttechnology.com/ TT-USB provide with procomp use ftdi_sio */
 #define FTDI_TTUSB_PID 0xFF20 /* Product Id */
 
+/* iPlus device */
+#define FTDI_IPLUS_PID 0xD070 /* Product Id */
+
 /* www.crystalfontz.com devices - thanx for providing free devices for evaluation ! */
 /* they use the ftdi chipset for the USB interface and the vendor id is the same */
 #define FTDI_XF_632_PID 0xFC08	/* 632: 16x2 Character Display */
