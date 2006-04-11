35af39b0e8b3c443f46302bf224a4546ab617cb3
NeilBrown <neilb@suse.de>
2006-04-10 22:55:24 -0700

    [PATCH] knfsd: nfsd4: Wrong error handling in nfs4acl
    this fixes coverity id #3.  Coverity detected dead code, since the == -1
    comparison only returns 0 or 1 to error.  Therefore the if ( error < 0 )
    statement was always false.  Seems that this was an if( error = nfs4...  )
    statement some time ago, which got broken during cleanup.
    
    Signed-off-by: Eric Sesterhenn <snakebyte@gmx.de>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/nfsd/nfs4acl.c
+++ fs/nfsd/nfs4acl.c
@@ -791,7 +791,7 @@ nfs4_acl_split(struct nfs4_acl *acl, struct nfs4_acl *dacl)
 			continue;
 
 		error = nfs4_acl_add_ace(dacl, ace->type, ace->flag,
-				ace->access_mask, ace->whotype, ace->who) == -1;
+				ace->access_mask, ace->whotype, ace->who);
 		if (error < 0)
 			goto out;
 
