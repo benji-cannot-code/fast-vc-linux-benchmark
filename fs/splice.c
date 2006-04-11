e115217fe563ba21bcbaf9bde8e59e0ae9e41ffd
Jens Axboe <axboe@suse.de>
2006-04-11 13:56:34 +0200

    [PATCH] splice: add Ingo as addition copyright holder
    The comment is also somewhat out of date, correct that as well.
    
    Signed-off-by: Jens Axboe <axboe@suse.de>

--- fs/splice.c
+++ fs/splice.c
@@ -10,11 +10,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * that transfers data buffers to or from a pipe buffer.
  *
  * Named by Larry McVoy, original implementation from Linus, extended by
- * Jens to support splicing to files and fixing the initial implementation
- * bugs.
+ * Jens to support splicing to files, network, direct splicing, etc and
+ * fixing lots of bugs.
  *
- * Copyright (C) 2005 Jens Axboe <axboe@suse.de>
- * Copyright (C) 2005 Linus Torvalds <torvalds@osdl.org>
+ * Copyright (C) 2005-2006 Jens Axboe <axboe@suse.de>
+ * Copyright (C) 2005-2006 Linus Torvalds <torvalds@osdl.org>
+ * Copyright (C) 2006 Ingo Molnar <mingo@elte.hu>
  *
  */
 #include <linux/fs.h>
