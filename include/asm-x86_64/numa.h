03f5dc8e2671cb39607c50afd0f54be0efd4ec4b
Yasunori Goto <y-goto@jp.fujitsu.com>
2006-04-10 22:53:53 -0700

    [PATCH] Configurable NODES_SHIFT
    Current implementations define NODES_SHIFT in include/asm-xxx/numnodes.h for
    each arch.  Its definition is sometimes configurable.  Indeed, ia64 defines 5
    NODES_SHIFT values in the current git tree.  But it looks a bit messy.
    
    SGI-SN2(ia64) system requires 1024 nodes, and the number of nodes already has
    been changeable by config.  Suitable node's number may be changed in the
    future even if it is other architecture.  So, I wrote configurable node's
    number.
    
    This patch set defines just default value for each arch which needs multi
    nodes except ia64.  But, it is easy to change to configurable if necessary.
    
    On ia64 the number of nodes can be already configured in generic ia64 and SN2
    config.  But, NODES_SHIFT is defined for DIG64 and HP'S machine too.  So, I
    changed it so that all platforms can be configured via CONFIG_NODES_SHIFT.  It
    would be simpler.
    
    See also: http://marc.theaimsgroup.com/?l=linux-kernel&m=114358010523896&w=2
    
    Signed-off-by: Yasunori Goto <y-goto@jp.fujitsu.com>
    Cc: Hirokazu Takata <takata@linux-m32r.org>
    Cc: "Luck, Tony" <tony.luck@intel.com>
    Cc: Andi Kleen <ak@muc.de>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Kyle McMartin <kyle@mcmartin.ca>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Jack Steiner <steiner@sgi.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/numa.h
+++ include/asm-x86_64/numa.h
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X8664_NUMA_H 1
 
 #include <linux/nodemask.h>
-#include <asm/numnodes.h>
 
 struct bootnode {
 	u64 start,end; 
