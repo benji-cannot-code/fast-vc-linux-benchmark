250a0a6b55ed16ba4493a014da23175c09ee1a2c
Jean Delvare <khali@linux-fr.org>
2006-04-18 22:22:09 -0700

    [PATCH] fb: Fix section mismatch in savagefb
    Fix the following section mismatch:
    
    WARNING: drivers/video/savage/savagefb.o - Section mismatch: reference to .init.data: from .text.savagefb_probe after 'savagefb_probe' (at offset 0x5e2)
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/savage/savagefb_driver.c
+++ drivers/video/savage/savagefb_driver.c
@@ -74,7 +74,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* --------------------------------------------------------------------- */
 
 
-static char *mode_option __initdata = NULL;
+static char *mode_option __devinitdata = NULL;
 
 #ifdef MODULE
 
