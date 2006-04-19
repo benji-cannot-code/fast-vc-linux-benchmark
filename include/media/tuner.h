1e53d0a857ae87b6b0e4d4702d06080a85f8afe9
Hans Verkuil <hverkuil@xs4all.nl>
2006-04-19 18:50:35 -0300

    V4L/DVB (3814): Add support for Samsung TCPG 6121P30A PAL tuner.
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -120,6 +120,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TUNER_XCEIVE_XC3028		71
 
 #define TUNER_THOMSON_FE6600		72	/* DViCO FusionHDTV DVB-T Hybrid */
+#define TUNER_SAMSUNG_TCPG_6121P30A     73 	/* Hauppauge PVR-500 PAL */
 
 /* tv card specific */
 #define TDA9887_PRESENT 		(1<<0)
