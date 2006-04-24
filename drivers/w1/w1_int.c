17165d05b52c55a2c91a450c44b8e5ea9bfd945c
Evgeniy Polyakov <johnpol@2ka.mipt.ru>
2006-04-24 10:46:14 +0400

    [PATCH] W1: possible cleanups
    This patch contains the following possible cleanups:
    - the following file did't #include the header with the prototypes for
      it's global functions:
      - w1_int.c
    - #if 0 the following unused global function:
      - w1_family.c: w1_family_get()
    - make the following needlessly global functions static:
      - w1_family.c: __w1_family_put()
      - w1_io.c: w1_delay()
      - w1_io.c: w1_touch_bit()
      - w1_io.c: w1_read_8()
    - remove the following unused EXPORT_SYMBOL's:
      - w1_family.c: w1_family_put
      - w1_family.c: w1_family_registered
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Evgeniy Polyakov <johnpol@2ka.mipt.ru>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/w1/w1_int.c
+++ drivers/w1/w1_int.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "w1.h"
 #include "w1_log.h"
 #include "w1_netlink.h"
+#include "w1_int.h"
 
 static u32 w1_ids = 1;
 
