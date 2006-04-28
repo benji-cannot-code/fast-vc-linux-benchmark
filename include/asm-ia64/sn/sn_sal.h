46e5827da220eb896541eba63f60fb911658d2e8
Jack Steiner <steiner@sgi.com>
2006-04-27 20:01:05 -0500

    [IA64-SGI] - Pass OS logical cpu number to the SN prom (bios)
    Pass the OS logical cpu number to the PROM. This allows PROM
    to log the OS logical cpu number in error records viewed thru POD.
    
    Signed-off-by: Jack Steiner <steiner@sgi.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/asm-ia64/sn/sn_sal.h
+++ include/asm-ia64/sn/sn_sal.h
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  SN_SAL_GET_PROM_FEATURE_SET		   0x02000065
 #define  SN_SAL_SET_OS_FEATURE_SET		   0x02000066
 #define  SN_SAL_INJECT_ERROR			   0x02000067
+#define  SN_SAL_SET_CPU_NUMBER			   0x02000068
 
 /*
  * Service-specific constants
@@ -1150,5 +1151,14 @@ sn_inject_error(u64 paddr, u64 *data, u64 *ecc)
 				(u64)ecc, 0, 0, 0, 0);
 	local_irq_restore(irq_flags);
 	return ret_stuff.status;
+}
+
+static inline int
+ia64_sn_set_cpu_number(int cpu)
+{
+	struct ia64_sal_retval rv;
+
+	SAL_CALL_NOLOCK(rv, SN_SAL_SET_CPU_NUMBER, cpu, 0, 0, 0, 0, 0, 0);
+	return rv.status;
 }
 #endif /* _ASM_IA64_SN_SN_SAL_H */
