839bed664b42ba12564283610ab00a003602845c
Takashi Iwai <tiwai@suse.de>
2006-04-28 15:13:39 +0200

    [ALSA] Clean up EXPORT_SYMBOL()s in snd-seq module
    Move EXPORT_SYMBOL()s to places adjacent to functions/variables.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/core/seq/seq_lock.c
+++ sound/core/seq/seq_lock.c
@@ -45,4 +45,6 @@ void snd_use_lock_sync_helper(snd_use_lock_t *lockp, const char *file, int line)
 	}
 }
 
+EXPORT_SYMBOL(snd_use_lock_sync_helper);
+
 #endif
--- sound/core/seq/seq_ports.c
+++ sound/core/seq/seq_ports.c
@@ -678,6 +678,7 @@ int snd_seq_event_port_attach(int client,
 	return ret;
 }
 
+EXPORT_SYMBOL(snd_seq_event_port_attach);
 
 /*
  * Detach the driver from a port.
@@ -697,3 +698,5 @@ int snd_seq_event_port_detach(int client, int port)
 
 	return err;
 }
+
+EXPORT_SYMBOL(snd_seq_event_port_detach);
