0f3b6b1163919d3b7a3b05db3db3d1e7e0096cb2
Vitaly Bordug <vbordug@ru.mvista.com>
2006-04-29 22:32:44 +0400

    [PATCH] ppc32 CPM_UART: Fixed break send on SCC
    SCC uart sends a break sequence each time it is stopped with the
    CPM_CR_STOP_TX command. That means that each time an application closes the
    serial device, a break is transmitted. To fix this, graceful tx stop is
    issued for SCC.
    
    Signed-off-by: David Jander <david.jander@protonic.nl>
    Signed-off-by: Vitaly Bordug <vbordug@ru.mvista.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-ppc/commproc.h
+++ include/asm-ppc/commproc.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CPM_CR_INIT_TX		((ushort)0x0002)
 #define CPM_CR_HUNT_MODE	((ushort)0x0003)
 #define CPM_CR_STOP_TX		((ushort)0x0004)
+#define CPM_CR_GRA_STOP_TX	((ushort)0x0005)
 #define CPM_CR_RESTART_TX	((ushort)0x0006)
 #define CPM_CR_CLOSE_RX_BD	((ushort)0x0007)
 #define CPM_CR_SET_GADDR	((ushort)0x0008)
