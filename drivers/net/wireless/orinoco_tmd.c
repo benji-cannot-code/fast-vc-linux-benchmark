9f67b34046903f0b5e24cba47ee7774254f23757
Pavel Roskin <proski@gnu.org>
2006-05-01 02:13:33 -0400

    [PATCH] orinoco: don't put PCI resource data to the network device
    The resource data in the network device is intended for ISA and other
    older busses, but not for PCI.  Don't put PCI data there.  Don't (ab)use
    the network device for keeping the IRQ number.
    
    Retire orinoco_pci_setup_netdev(), and print some minimal information to
    the kernel log instead, identifying the network device and the driver
    mostly to identify problems at startup.  Scripts should rely on sysfs.
    
    Signed-off-by: Pavel Roskin <proski@gnu.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/orinoco_tmd.c
+++ drivers/net/wireless/orinoco_tmd.c
@@ -148,7 +148,6 @@ static int orinoco_tmd_init_one(struct pci_dev *pdev,
 		err = -EBUSY;
 		goto fail_irq;
 	}
-	orinoco_pci_setup_netdev(dev, pdev, 2);
 
 	err = orinoco_tmd_cor_reset(priv);
 	if (err) {
@@ -163,6 +162,8 @@ static int orinoco_tmd_init_one(struct pci_dev *pdev,
 	}
 
 	pci_set_drvdata(pdev, dev);
+	printk(KERN_DEBUG "%s: " DRIVER_NAME " at %s\n", dev->name,
+	       pci_name(pdev));
 
 	return 0;
 
@@ -195,7 +196,7 @@ static void __devexit orinoco_tmd_remove_one(struct pci_dev *pdev)
 	struct orinoco_pci_card *card = priv->card;
 
 	unregister_netdev(dev);
-	free_irq(dev->irq, dev);
+	free_irq(pdev->irq, dev);
 	pci_set_drvdata(pdev, NULL);
 	free_orinocodev(dev);
 	pci_iounmap(pdev, priv->hw.iobase);
