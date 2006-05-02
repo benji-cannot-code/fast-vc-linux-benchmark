ca1862faa6698553c0ae8a9d9220ce5567c71bd5
Chris Dearman <chris@mips.com>
2006-05-02 14:08:46 +0100

    [MIPS] Fix detection and handling of the 74K processor.
    Nothing exciting; Linux just didn't know it yet so this is most adding
    a value to a case statement.
        
    Signed-off-by: Chris Dearman <chris@mips.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/cpu.h
+++ include/asm-mips/cpu.h
@@ -88,6 +88,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PRID_IMP_24K		0x9300
 #define PRID_IMP_34K		0x9500
 #define PRID_IMP_24KE		0x9600
+#define PRID_IMP_74K		0x9700
 
 /*
  * These are the PRID's for when 23:16 == PRID_COMP_SIBYTE
@@ -197,7 +198,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CPU_34K			60
 #define CPU_PR4450		61
 #define CPU_SB1A		62
-#define CPU_LAST		62
+#define CPU_74K			63
+#define CPU_LAST		63
 
 /*
  * ISA Level encodings
