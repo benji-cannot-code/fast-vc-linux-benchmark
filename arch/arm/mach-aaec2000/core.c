687309ac642b463da25ef4b5c76c28b860253620
Bellido Nicolas <ml@acolin.be>
2006-05-07 22:49:21 +0100

    [ARM] 3504/1: Fix clcd includes for aaec2000
    Patch from Bellido Nicolas
    
    Since this patch:
     [ARM] 3366/1: Allow the 16bpp mode configuration in the CLCD control register
    
    linux/amba/bus.h needs to be included before linux/amba/clcd.h
    
    Signed-off-by: Nicolas Bellido <ml@acolin.be>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-aaec2000/core.c
+++ arch/arm/mach-aaec2000/core.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/timex.h>
 #include <linux/signal.h>
-#include <linux/amba/bus.h>
 
 #include <asm/hardware.h>
 #include <asm/irq.h>
