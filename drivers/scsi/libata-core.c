0cd63e861c05180e08c0b7e2de1a2082e930d4b1
Tejun Heo <htejun@gmail.com>
2006-05-15 21:03:45 +0900

    [PATCH] libata-ncq: implement NCQ command translation and exclusion
    This patch implements NCQ command translation and exclusion.  Note
    that NCQ commands don't use ata_rwcmd_protocol() to choose ATA
    command.  This is because, unlike non-NCQ RW commands, NCQ commands
    can only be used for NCQ protocol and FUA handling is done with a flag
    rather than separate command.
    
    NCQ enabled device will have queue depth larger than one but no two
    non-NCQ commands can be issued simultaneously, neither can a non-NCQ
    command and NCQ commands.  This patch makes ata_scsi_translate()
    return SCSI_MLQUEUE_DEVICE_BUSY if such exclusion is necessary.  SCSI
    midlayer will retry the command later.
    
    As SCSI midlayer always retries once a command completes, this doesn't
    incur unnecessary delays and as most commands will be NCQ ones for NCQ
    device, so the overhead should be negligible.
    
    Initial implementation is from Jens Axboe and using
    SCSI_MLQUEUE_DEVICE_BUSY for exclusion is suggested by Jeff Garzik.
    
    Signed-off-by: Tejun Heo <htejun@gmail.com>

--- drivers/scsi/libata-core.c
+++ drivers/scsi/libata-core.c
@@ -4417,6 +4417,7 @@ static inline int ata_should_dma_map(struct ata_queued_cmd *qc)
 	struct ata_port *ap = qc->ap;
 
 	switch (qc->tf.protocol) {
+	case ATA_PROT_NCQ:
 	case ATA_PROT_DMA:
 	case ATA_PROT_ATAPI_DMA:
 		return 1;
