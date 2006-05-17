4782a38d481c5baf517590d7daca53a6d4985cf2
Andrew Vasquez <andrew.vasquez@qlogic.com>
2006-05-17 15:09:34 -0700

    [SCSI] qla2xxx: Remove obsolete firmware-loader-module support.
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla2xxx/qla_gbl.h
+++ drivers/scsi/qla2xxx/qla_gbl.h
@@ -10,9 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/interrupt.h>
 
-extern void qla2x00_remove_one(struct pci_dev *);
-extern int qla2x00_probe_one(struct pci_dev *, struct qla_board_info *);
-
 /*
  * Global Function Prototypes in qla_init.c source file.
  */
