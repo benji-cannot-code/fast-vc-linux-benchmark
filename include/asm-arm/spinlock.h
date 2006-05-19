089b49c43cfc77ba9a03a11784daae21764b4c3e
Catalin Marinas <catalin.marinas@arm.com>
2006-05-19 21:55:35 +0100

    [ARM] 3533/1: Implement the __raw_(read|write)_can_lock functions on ARM
    Patch from Catalin Marinas
    
    Recent patches introduced the write_can_lock() call in the kernel/ptrace.c
    file. Implement the __raw_* variants on ARM (SMP) as well.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/spinlock.h
+++ include/asm-arm/spinlock.h
@@ -143,6 +143,9 @@ static inline void __raw_write_unlock(raw_rwlock_t *rw)
 	: "cc");
 }
 
+/* write_can_lock - would write_trylock() succeed? */
+#define __raw_write_can_lock(x)		((x)->lock == 0x80000000)
+
 /*
  * Read locks are a bit more hairy:
  *  - Exclusively load the lock value.
@@ -198,5 +201,8 @@ static inline void __raw_read_unlock(raw_rwlock_t *rw)
 }
 
 #define __raw_read_trylock(lock) generic__raw_read_trylock(lock)
+
+/* read_can_lock - would read_trylock() succeed? */
+#define __raw_read_can_lock(x)		((x)->lock < 0x80000000)
 
 #endif /* __ASM_SPINLOCK_H */
