e7ec66d4b0218f3fe72223c086ffef9547d1e8e4
Tejun Heo <htejun@gmail.com>
2006-05-19 21:07:05 +0900

    [PATCH] SCSI: make scsi_implement_eh() generic API for SCSI transports
    libata implemented a feature to schedule EH without an associated EH
    by manipulating shost->host_eh_scheduled in ata_scsi_schedule_eh()
    directly.  Move this function to scsi_error.c and rename it to
    scsi_schedule_eh().  It is now an exported API for SCSI transports and
    exported via new header file drivers/scsi/scsi_transport_api.h
    
    This patch also de-export scsi_eh_wakeup() which was exported
    specifically for ata_scsi_schedule_eh().
    
    Signed-off-by: Tejun Heo <htejun@gmail.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/scsi/libata-eh.c
+++ drivers/scsi/libata-eh.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_eh.h>
 #include <scsi/scsi_device.h>
 #include <scsi/scsi_cmnd.h>
+#include "scsi_transport_api.h"
 
 #include <linux/libata.h>
 
@@ -433,7 +434,7 @@ void ata_port_schedule_eh(struct ata_port *ap)
 	WARN_ON(!ap->ops->error_handler);
 
 	ap->flags |= ATA_FLAG_EH_PENDING;
-	ata_schedule_scsi_eh(ap->host);
+	scsi_schedule_eh(ap->host);
 
 	DPRINTK("port EH scheduled\n");
 }
