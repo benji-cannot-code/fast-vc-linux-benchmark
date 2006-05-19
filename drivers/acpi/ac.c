3e1085ae43f122d303e70a21b677788dbf2b1edc
Patrick Mochel <mochel@linux.intel.com>
2006-05-19 16:54:49 -0400

    ACPI: ac: Remove unneeded acpi_handle from driver.
    Signed-off-by: Patrick Mochel <mochel@linux.intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/ac.c
+++ drivers/acpi/ac.c
@@ -66,7 +66,6 @@ static struct acpi_driver acpi_ac_driver = {
 };
 
 struct acpi_ac {
-	acpi_handle handle;
 	struct acpi_device * device;
 	unsigned long state;
 };
@@ -223,7 +222,6 @@ static int acpi_ac_add(struct acpi_device *device)
 		return -ENOMEM;
 	memset(ac, 0, sizeof(struct acpi_ac));
 
-	ac->handle = device->handle;
 	ac->device = device;
 	strcpy(acpi_device_name(device), ACPI_AC_DEVICE_NAME);
 	strcpy(acpi_device_class(device), ACPI_AC_CLASS);
