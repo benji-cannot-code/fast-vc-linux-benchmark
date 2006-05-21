3b5af7adf0fa7126ef775997c5faff46ee709ebe
dmitry pervushin <dpervushin@ru.mvista.com>
2006-05-21 14:53:06 +0400

    [MIPS] Support for the R5500-based NEC EMMA2RH Mark-eins board
    Signed-off-by: dmitry pervushin  <dpervushin@ru.mvista.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/bootinfo.h
+++ include/asm-mips/bootinfo.h
@@ -219,6 +219,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MACH_GROUP_TITAN       22	/* PMC-Sierra Titan		*/
 #define  MACH_TITAN_YOSEMITE	1	/* PMC-Sierra Yosemite		*/
 
+/*
+ * Valid machtype for group NEC EMMA2RH
+ */
+#define MACH_GROUP_NEC_EMMA2RH 25	/* NEC EMMA2RH (was 23)		*/
+#define  MACH_NEC_MARKEINS	0	/* NEC EMMA2RH Mark-eins	*/
+
 #define CL_SIZE			COMMAND_LINE_SIZE
 
 const char *get_system_type(void);
