232e3025b3376c911dd1653eaa9a669ae7ce8f27
Takashi Iwai <tiwai@suse.de>
2006-05-22 14:31:57 +0200

    [ALSA] hdsp - Fix compilation with hdsp driver built in kernel
    Fixed the compilation with hdsp driver built in kernel.
    The traditional hwdep loader is used in this case.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/rme9652/hdsp.c
+++ sound/pci/rme9652/hdsp.c
@@ -390,7 +390,7 @@ MODULE_SUPPORTED_DEVICE("{{RME Hammerfall-DSP},"
 
 /* use hotplug firmeare loader? */
 #if defined(CONFIG_FW_LOADER) || defined(CONFIG_FW_LOADER_MODULE)
-#ifndef HDSP_USE_HWDEP_LOADER
+#if !defined(HDSP_USE_HWDEP_LOADER) && !defined(CONFIG_SND_HDSP)
 #define HDSP_FW_LOADER
 #endif
 #endif
