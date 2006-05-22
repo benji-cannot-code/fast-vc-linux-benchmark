078c3020fc126fbc1da9367fd9b5711e1e1dfb21
Michael Schimek <mschimek@gmx.at>
2006-05-22 10:32:11 -0300

    V4L/DVB (4001): Cx88 NTSC VBI fixes
    VBI (Closed Caption, CC) capturing never worked in NTSC mode with the
    cx88 driver.
    They were tested with libzvbi and a patched version of tvtime.
    
    Signed-off-by: Michael H. Schimek <mschimek@gmx.at>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx88/cx88.h
+++ drivers/media/video/cx88/cx88.h
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/version.h>
 #include <linux/mutex.h>
-#define CX88_VERSION_CODE KERNEL_VERSION(0,0,5)
+#define CX88_VERSION_CODE KERNEL_VERSION(0,0,6)
 
 #ifndef TRUE
 # define TRUE (1==1)
