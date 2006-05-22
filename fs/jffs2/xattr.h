65bebf611c7b89bcbada7aa5e0e6ba6f883e8ba7
David Woodhouse <dwmw2@infradead.org>
2006-05-22 16:32:05 +0100

    [JFFS2] Put list of nodes in common part of ic/x_ref/x_datum structure
    We'll be using a proper list of nodes in the jffs2_xattr_datum and
    jffs2_xattr_ref structures, because the existing code to overwrite
    them is just broken. Put it in the common part at the front of the
    structure which is shared with the jffs2_inode_cache, so that the
    jffs2_link_node_ref() function can do the right thing.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- fs/jffs2/xattr.h
+++ fs/jffs2/xattr.h
@@ -21,11 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct jffs2_xattr_datum
 {
 	void *always_null;
-	u8 class;
-	u8 flags;
-	u16 xprefix;			/* see JFFS2_XATTR_PREFIX_* */
-
 	struct jffs2_raw_node_ref *node;
+	uint8_t class;
+	uint8_t flags;
+	uint16_t xprefix;			/* see JFFS2_XATTR_PREFIX_* */
+
 	struct list_head xindex;	/* chained from c->xattrindex[n] */
 	uint32_t refcnt;		/* # of xattr_ref refers this */
 	uint32_t xid;
@@ -43,11 +43,11 @@ struct jffs2_inode_cache;
 struct jffs2_xattr_ref
 {
 	void *always_null;
-	u8 class;
-	u8 flags;		/* Currently unused */
+	struct jffs2_raw_node_ref *node;
+	uint8_t class;
+	uint8_t flags;		/* Currently unused */
 	u16 unused;
 
-	struct jffs2_raw_node_ref *node;
 	union {
 		struct jffs2_inode_cache *ic;	/* reference to jffs2_inode_cache */
 		uint32_t ino;			/* only used in scanning/building  */
