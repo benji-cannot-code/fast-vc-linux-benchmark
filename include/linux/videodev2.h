3df934d6b819c3ca670cdbe2d3c62bbc6fa426a9
David Mosberger-Tang <David.Mosberger@acm.org>
2006-05-26 10:28:13 -0300

    V4L/DVB (4046): Trivial videodev2.h patch
    linux/videodev2.h uses types such as __u8 but it fails to include
    <linux/types.h>.  Within the kernel, that's not a problem because
    <linux/time.h> already includes <linux/types.h>.  However, there are
    user apps that try to include videodev2.h (e.g., ekiga) and at least
    on ia64, it causes compilation failures since <linux/types.h> doesn't
    get included for any other reason, leaving __u8 etc. undefined.  The
    attached patch fixes the problem for me.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/linux/videodev2.h
+++ include/linux/videodev2.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/mutex.h>
 #endif
+#include <linux/types.h>
 #include <linux/compiler.h> /* need __user */
 
 
