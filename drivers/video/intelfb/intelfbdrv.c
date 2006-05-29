3bba6fa11e1ca7f3df38dcd80e381240ce5c77d2
Dennis Munsie <dmunsie@cecropia.com>
2006-05-29 18:58:09 +1000

    intelfb: use regular modedb table instead of VESA
    intelfb driver -- use the regular modedb table instead of the VESA modedb
    table.  Ideally, the 9xx stride patch should be applied first, since there
    are modes in the VESA table that won't work without that patch.
    
    Signed-off-by: Dennis Munsie <dmunsie@cecropia.com>
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/video/intelfb/intelfbdrv.c
+++ drivers/video/intelfb/intelfbdrv.c
@@ -1060,8 +1060,7 @@ intelfb_init_var(struct intelfb_info *dinfo)
 				       "intelfb: looking for mode in global "
 				       "database ");
 				msrc = fb_find_mode(var, dinfo->info, mode,
-						    vesa_modes,
-						    VESA_MODEDB_SIZE, NULL, 0);
+						    NULL, 0, NULL, 0);
 
 				if (msrc)
 					msrc |= 8;
@@ -1071,8 +1070,7 @@ intelfb_init_var(struct intelfb_info *dinfo)
 
 		if (!msrc) {
 			msrc = fb_find_mode(var, dinfo->info, PREFERRED_MODE,
-					    vesa_modes, VESA_MODEDB_SIZE,
-					    NULL, 0);
+					    NULL, 0, NULL, 0);
 		}
 	}
 
