522ace2d53f34d43acae38296b00489290076cce
Dave Jones <davej@redhat.com>
2006-05-30 17:25:14 -0400

    [CPUFREQ] Typo in powernow-k8
    (Also fix some horked indentation)
    
    Signed-off-by: Dave Jones <davej@redhat.com>

--- arch/i386/kernel/cpu/cpufreq/powernow-k8.c
+++ arch/i386/kernel/cpu/cpufreq/powernow-k8.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  of the "BIOS and Kernel Developer's Guide for the AMD Athlon 64 and AMD
  *  Opteron Processors" available for download from www.amd.com
  *
- *  Tables for specific CPUs can be infrerred from
+ *  Tables for specific CPUs can be inferred from
  *     http://www.amd.com/us-en/assets/content_type/white_papers_and_tech_docs/30430.pdf
  */
 
@@ -897,7 +897,7 @@ static int transition_frequency(struct powernow_k8_data *data, unsigned int inde
 	for_each_cpu_mask(i, cpu_core_map[data->cpu]) {
 		freqs.cpu = i;
 		cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
-        }
+	}
 	return res;
 }
 
