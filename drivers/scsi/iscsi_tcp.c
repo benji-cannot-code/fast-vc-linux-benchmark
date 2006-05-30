79142f7b0d0d5f7de48913bd35ecbbd99be090ef
Mike Christie <michaelc@cs.wisc.edu>
2006-05-30 01:04:51 -0500

    [SCSI] iscsi: update version to 1.0-595
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/iscsi_tcp.c
+++ drivers/scsi/iscsi_tcp.c
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "iscsi_tcp.h"
 
-#define ISCSI_TCP_VERSION "1.0-574"
+#define ISCSI_TCP_VERSION "1.0-595"
 
 MODULE_AUTHOR("Dmitry Yusupov <dmitry_yus@yahoo.com>, "
 	      "Alex Aizman <itn780@yahoo.com>");
