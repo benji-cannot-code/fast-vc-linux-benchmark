3124b5ead9053ead5f397f988747fc7ee8d4dfca
Steve French <sfrench@us.ibm.com>
2006-06-01 05:09:10 +0000

    [CIFS] Missing include shows up on some architectures
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/sess.c
+++ fs/cifs/sess.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ntlmssp.h"
 #include "nterr.h"
 #include <linux/ctype.h>
+#include <linux/utsname.h>
 
 extern void SMBencrypt(unsigned char *passwd, unsigned char *c8,
                        unsigned char *p24);
