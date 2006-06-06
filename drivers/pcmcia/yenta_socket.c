f06eaa777a4475818ff0e009c5caf120008b3b7d
Randy Dunlap <rdunlap@xenotime.net>
2006-06-06 12:06:41 -0700

    [PATCH] pcmcia: fix kernel-doc function name
    Fix kernel-doc function name spello.
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/yenta_socket.c
+++ drivers/pcmcia/yenta_socket.c
@@ -1007,7 +1007,7 @@ static void yenta_config_init(struct yenta_socket *socket)
 }
 
 /**
- * yenta_fixup_parent_subordinate - Fix subordinate bus# of the parent bridge
+ * yenta_fixup_parent_bridge - Fix subordinate bus# of the parent bridge
  * @cardbus_bridge: The PCI bus which the CardBus bridge bridges to
  *
  * Checks if devices on the bus which the CardBus bridge bridges to would be
