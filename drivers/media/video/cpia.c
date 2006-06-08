a44a4ec0bfd091b5b76665a9a9a17eede2e34fe4
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-06-08 17:16:46 -0300

    V4L/DVB (4086): There were a cross-reference on cpia and cpia_pp/cpia_usb
    Those cross-references were generating an error:
    *** Warning: "cpia_pp_init" [/home/v4l/master/v4l/cpia.ko] undefined!
    *** Warning: "cpia_usb/_init" [/home/v4l/master/v4l/cpia.ko] undefined!
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cpia.c
+++ drivers/media/video/cpia.c
@@ -48,13 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "cpia.h"
 
-#ifdef CONFIG_VIDEO_CPIA_PP
-extern int cpia_pp_init(void);
-#endif
-#ifdef CONFIG_VIDEO_CPIA_USB
-extern int cpia_usb_init(void);
-#endif
-
 static int video_nr = -1;
 
 #ifdef MODULE
@@ -4046,13 +4039,6 @@ static int __init cpia_init(void)
 
 #ifdef CONFIG_PROC_FS
 	proc_cpia_create();
-#endif
-
-#ifdef CONFIG_VIDEO_CPIA_PP
-	cpia_pp_init();
-#endif
-#ifdef CONFIG_VIDEO_CPIA_USB
-	cpia_usb_init();
 #endif
 
 	return 0;
