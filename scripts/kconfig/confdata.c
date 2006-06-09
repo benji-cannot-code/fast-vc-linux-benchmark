8b3839ac6fdfc2c3f6fb55043ce7925ec402d845
Roman Zippel <zippel@linux-m68k.org>
2006-06-08 22:12:38 -0700

    kconfig: improve config load/save output
    During loading special case the first common case (.config), be silent about
    it and otherwise mark it as a change that requires saving.  Instead output
    that the file has been changed.  IOW if conf does nothing (special), it's
    silent.
    
    Signed-off-by: Roman Zippel <zippel@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -99,20 +99,28 @@ int conf_read_simple(const char *name)
 		in = zconf_fopen(name);
 	} else {
 		const char **names = conf_confnames;
+		name = *names++;
+		if (!name)
+			return 1;
+		in = zconf_fopen(name);
+		if (in)
+			goto load;
+		sym_change_count++;
 		while ((name = *names++)) {
 			name = conf_expand_value(name);
 			in = zconf_fopen(name);
 			if (in) {
 				printf(_("#\n"
-				         "# using defaults found in %s\n"
-				         "#\n"), name);
-				break;
+					 "# using defaults found in %s\n"
+					 "#\n"), name);
+				goto load;
 			}
 		}
 	}
 	if (!in)
 		return 1;
 
+load:
 	conf_filename = name;
 	conf_lineno = 0;
 	conf_warnings = 0;
@@ -276,6 +284,8 @@ int conf_read(const char *name)
 	struct expr *e;
 	int i;
 
+	sym_change_count = 0;
+
 	if (conf_read_simple(name))
 		return 1;
 
@@ -326,7 +336,7 @@ int conf_read(const char *name)
 				sym->flags |= e->right.sym->flags & SYMBOL_NEW;
 	}
 
-	sym_change_count = conf_warnings || conf_unsaved;
+	sym_change_count += conf_warnings || conf_unsaved;
 
 	return 0;
 }
@@ -524,6 +534,10 @@ int conf_write(const char *name)
 	sprintf(tmpname, "%s%s", dirname, basename);
 	if (rename(newname, tmpname))
 		return 1;
+
+	printf(_("#\n"
+		 "# configuration written to %s\n"
+		 "#\n"), tmpname);
 
 	sym_change_count = 0;
 
