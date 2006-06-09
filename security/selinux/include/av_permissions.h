25bacc2b43c84632c279d2132c1acb4c044d9f89
James Morris <jmorris@namei.org>
2006-06-09 00:27:28 -0700

    [SECMARK]: Add new flask definitions to SELinux
    Secmark implements a new scheme for adding security markings to
    packets via iptables, as well as changes to SELinux to use these
    markings for security policy enforcement.  The rationale for this
    scheme is explained and discussed in detail in the original threads:
    
     http://thread.gmane.org/gmane.linux.network/34927/
     http://thread.gmane.org/gmane.linux.network/35244/
    
    Examples of policy and rulesets, as well as a full archive of patches
    for iptables and SELinux userland, may be found at:
    
    http://people.redhat.com/jmorris/selinux/secmark/
    
    The code has been tested with various compilation options and in
    several scenarios, including with 'complicated' protocols such as FTP
    and also with the new generic conntrack code with IPv6 connection
    tracking.
    
    This patch:
    
    Add support for a new object class ('packet'), and associated
    permissions ('send', 'recv', 'relabelto').  These are used to enforce
    security policy for network packets labeled with SECMARK, and for
    adding labeling rules.
    
    Signed-off-by: James Morris <jmorris@namei.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- security/selinux/include/av_permissions.h
+++ security/selinux/include/av_permissions.h
@@ -957,3 +957,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define APPLETALK_SOCKET__SEND_MSG                0x00100000UL
 #define APPLETALK_SOCKET__NAME_BIND               0x00200000UL
 
+#define PACKET__SEND                              0x00000001UL
+#define PACKET__RECV                              0x00000002UL
+#define PACKET__RELABELTO                         0x00000004UL
--- security/selinux/include/flask.h
+++ security/selinux/include/flask.h
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SECCLASS_ASSOCIATION                             54
 #define SECCLASS_NETLINK_KOBJECT_UEVENT_SOCKET           55
 #define SECCLASS_APPLETALK_SOCKET                        56
+#define SECCLASS_PACKET                                  57
 
 /*
  * Security identifier indices for initial entities
