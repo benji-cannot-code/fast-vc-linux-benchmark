28b6f198a03185f9ccef6f8748673f3124338f04
Anton Blanchard <anton@samba.org>
2006-06-10 23:14:51 +1000

    [POWERPC] Fix HV bit handling on non partitioned machines
    On non partitioned machines we currently set the HV bit in kernel space
    only. It turns out we are supposed to maintain the HV bit in both user
    and kernel space.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/reg.h
+++ include/asm-powerpc/reg.h
@@ -94,8 +94,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MSR_LE		__MASK(MSR_LE_LG)	/* Little Endian */
 
 #ifdef CONFIG_PPC64
-#define MSR_		MSR_ME | MSR_RI | MSR_IR | MSR_DR | MSR_ISF
-#define MSR_KERNEL      MSR_ | MSR_SF | MSR_HV
+#define MSR_		MSR_ME | MSR_RI | MSR_IR | MSR_DR | MSR_ISF |MSR_HV
+#define MSR_KERNEL      MSR_ | MSR_SF
 
 #define MSR_USER32	MSR_ | MSR_PR | MSR_EE
 #define MSR_USER64	MSR_USER32 | MSR_SF
