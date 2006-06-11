ad4a029a86437a221b18c7aa212e696acf77528d
Laurent Riffard <laurent.riffard@free.fr>
2006-06-11 08:02:06 +0200

    kbuild: fix module.symvers parsing in modpost
    read_dump didn't split lines between module name and export type.
    
    Signed-off-by: Laurent Riffard <laurent.riffard@free.fr>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1290,7 +1290,7 @@ static void read_dump(const char *fname, unsigned int kernel)
 		if (!(modname = strchr(symname, '\t')))
 			goto fail;
 		*modname++ = '\0';
-		if (!(export = strchr(modname, '\t')))
+		if ((export = strchr(modname, '\t')) != NULL)
 			*export++ = '\0';
 
 		crc = strtoul(line, &d, 16);
