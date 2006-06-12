6a61505dec7fb0eadc361ad15a3d5e9139268d0c
Ralf Baechle <ralf@linux-mips.org>
2006-06-12 09:13:56 +0100

    [MIPS] Consolidate definitions of pfn_valid in one file.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mmzone.h
+++ include/asm-mips/mmzone.h
@@ -15,14 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define kvaddr_to_nid(kvaddr)	pa_to_nid(__pa(kvaddr))
 #define pfn_to_nid(pfn)		pa_to_nid((pfn) << PAGE_SHIFT)
 
-#define pfn_valid(pfn)						\
-({								\
- 	unsigned long __pfn = (pfn);				\
- 	int __n = pfn_to_nid(__pfn);				\
-	((__n >= 0) ? (__pfn < NODE_DATA(__n)->node_start_pfn +	\
-		       NODE_DATA(__n)->node_spanned_pages) : 0);\
-})
-
 /* XXX: FIXME -- wli */
 #define kern_addr_valid(addr)	(0)
 
