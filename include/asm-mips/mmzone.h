5a7024a042302edf1a9349d9753bfbc109b5be02
Ralf Baechle <ralf@linux-mips.org>
2006-06-12 12:20:09 +0100

    [MIPS] Drop 0 definition for kern_addr_valid
    kern_addr_valid is currently only being used in kmem_ptr_validate which
    is making some vague attempt at verfying the validity of an address.
    Only IA-64, PARISC and x86-64 actually make some actual effort to verify
    the validity of the pointer.  Most architecture definitions of
    kern_addr_valid() just define it as 1; the Alpha and CONFIG_DISCONTIGMEM
    on i386 and MIPS even as 0; the 0-definition will result in
    kmem_ptr_validate always failing which in turn will cause d_validate to
    always fail.  d_validate's only two users are smbfs and ncpfs, so the
    0 definition ended breaking those ...
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mmzone.h
+++ include/asm-mips/mmzone.h
@@ -15,9 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define kvaddr_to_nid(kvaddr)	pa_to_nid(__pa(kvaddr))
 #define pfn_to_nid(pfn)		pa_to_nid((pfn) << PAGE_SHIFT)
 
-/* XXX: FIXME -- wli */
-#define kern_addr_valid(addr)	(0)
-
 #endif /* CONFIG_DISCONTIGMEM */
 
 #endif /* _ASM_MMZONE_H_ */
