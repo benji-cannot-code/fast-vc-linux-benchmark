ab549ec1c7e58cdbf1a0a9795edaffcbce58ed19
Takashi Iwai <tiwai@suse.de>
2006-06-13 11:57:22 +0200

    [ALSA] hda-codec - Add SPDIF support to Thinkpad T/X/Z60
    Added IEC958 (SPDIF) output support to Thinkpad T/X/Z60 with
    AD1981HD codec.  The spdif jack is on docking station.
    Also, renamed 'IEC958 Playback Route' to 'IEC958 Playback Source'
    to avoid the mixer name confliction with IEC958 switch.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/hda/patch_analog.c
+++ sound/pci/hda/patch_analog.c
@@ -964,7 +964,7 @@ static struct snd_kcontrol_new ad1983_mixers[] = {
 	},
 	{
 		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-		.name = SNDRV_CTL_NAME_IEC958("",PLAYBACK,NONE) "Route",
+		.name = SNDRV_CTL_NAME_IEC958("",PLAYBACK,NONE) "Source",
 		.info = ad1983_spdif_route_info,
 		.get = ad1983_spdif_route_get,
 		.put = ad1983_spdif_route_put,
@@ -1104,7 +1104,7 @@ static struct snd_kcontrol_new ad1981_mixers[] = {
 	/* identical with AD1983 */
 	{
 		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-		.name = SNDRV_CTL_NAME_IEC958("",PLAYBACK,NONE) "Route",
+		.name = SNDRV_CTL_NAME_IEC958("",PLAYBACK,NONE) "Source",
 		.info = ad1983_spdif_route_info,
 		.get = ad1983_spdif_route_get,
 		.put = ad1983_spdif_route_put,
@@ -1350,6 +1350,14 @@ static struct snd_kcontrol_new ad1981_thinkpad_mixers[] = {
 		.get = ad198x_mux_enum_get,
 		.put = ad198x_mux_enum_put,
 	},
+	/* identical with AD1983 */
+	{
+		.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
+		.name = SNDRV_CTL_NAME_IEC958("",PLAYBACK,NONE) "Source",
+		.info = ad1983_spdif_route_info,
+		.get = ad1983_spdif_route_get,
+		.put = ad1983_spdif_route_put,
+	},
 	{ } /* end */
 };
 
@@ -1423,7 +1431,6 @@ static int patch_ad1981(struct hda_codec *codec)
 		break;
 	case AD1981_THINKPAD:
 		spec->mixers[0] = ad1981_thinkpad_mixers;
-		spec->multiout.dig_out_nid = 0;
 		spec->input_mux = &ad1981_thinkpad_capture_source;
 		break;
 	}
