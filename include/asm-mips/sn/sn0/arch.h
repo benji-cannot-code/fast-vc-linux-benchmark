6c8189ec795018530778788b7608f3e11d457fde
Ralf Baechle <ralf@linux-mips.org>
2006-06-14 08:10:35 +0100

    [MIPS] IP27: Cleanup N/M mode configuration.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/sn/sn0/arch.h
+++ include/asm-mips/sn/sn0/arch.h
@@ -60,9 +60,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifdef CONFIG_SGI_SN_N_MODE
 #define MAX_MEM_SLOTS   16                      /* max slots per node */
-#else /* !CONFIG_SGI_SN_N_MODE, assume M_MODE */
+#else /* !CONFIG_SGI_SN_N_MODE, assume CONFIG_SGI_SN_M_MODE */
 #define MAX_MEM_SLOTS   32                      /* max slots per node */
-#endif /* defined(N_MODE) */
+#endif /* CONFIG_SGI_SN_M_MODE */
 
 #define SLOT_SHIFT      	(27)
 #define SLOT_MIN_MEM_SIZE	(32*1024*1024)
