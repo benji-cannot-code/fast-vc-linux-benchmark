6c1f074d858c8ef98f52f3a00c6b9dd53928fb97
Roland Dreier <rolandd@cisco.com>
2006-06-17 20:37:40 -0700

    IB/uverbs: Don't decrement usecnt on error paths
    In error paths when destroying an object, uverbs should not decrement
    associated objects' usecnt, since ib_dereg_mr(), ib_destroy_qp(),
    etc. already do that.
    
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -493,7 +493,6 @@ err_idr:
 
 err_unreg:
 	ib_dereg_mr(mr);
-	atomic_dec(&pd->usecnt);
 
 err_up:
 	mutex_unlock(&ib_uverbs_idr_mutex);
@@ -987,11 +986,6 @@ err_idr:
 
 err_destroy:
 	ib_destroy_qp(qp);
-	atomic_dec(&pd->usecnt);
-	atomic_dec(&attr.send_cq->usecnt);
-	atomic_dec(&attr.recv_cq->usecnt);
-	if (attr.srq)
-		atomic_dec(&attr.srq->usecnt);
 
 err_up:
 	mutex_unlock(&ib_uverbs_idr_mutex);
@@ -1889,7 +1883,6 @@ err_idr:
 
 err_destroy:
 	ib_destroy_srq(srq);
-	atomic_dec(&pd->usecnt);
 
 err_up:
 	mutex_unlock(&ib_uverbs_idr_mutex);
