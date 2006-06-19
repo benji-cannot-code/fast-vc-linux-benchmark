6afbb0509762f9a7d0dc48f9287f3f2e53e75daf
Andrew Victor <andrew@sanpeople.com>
2006-06-19 15:26:51 +0100

    [ARM] 3581/1: AT91RM9200 Internal SRAM
    Patch from Andrew Victor
    
    This patch maps the AT91RM9200's internal SRAM into the virtual memory
    address space - just below the internal peripheral registers.
    
    Signed-off-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-at91rm9200/common.c
+++ arch/arm/mach-at91rm9200/common.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 
 #include <asm/arch/hardware.h>
+#include "generic.h"
 
 static struct map_desc at91rm9200_io_desc[] __initdata = {
 	{
@@ -94,6 +95,11 @@ static struct map_desc at91rm9200_io_desc[] __initdata = {
 		.virtual	= AT91_VA_BASE_TCB0,
 		.pfn		= __phys_to_pfn(AT91_BASE_TCB0),
 		.length		= SZ_16K,
+		.type		= MT_DEVICE,
+	}, {
+		.virtual	= AT91_SRAM_VIRT_BASE,
+		.pfn		= __phys_to_pfn(AT91_SRAM_BASE),
+		.length		= AT91_SRAM_SIZE,
 		.type		= MT_DEVICE,
 	},
 };
