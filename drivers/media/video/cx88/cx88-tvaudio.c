5a438ade99e517ca49c3ec8dd9dd54ca29117959
Michael Krufky <mkrufky@linuxtv.org>
2006-06-19 09:54:00 -0300

    V4L/DVB (4135): Cx88-tvaudio.c must #include <linux/config.h>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx88/cx88-tvaudio.c
+++ drivers/media/video/cx88/cx88-tvaudio.c
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/smp_lock.h>
 #include <linux/delay.h>
+#include <linux/config.h>
 #include <linux/kthread.h>
 
 #include "cx88.h"
