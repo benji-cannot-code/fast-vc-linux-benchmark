3961c196db46899845770e8acb7a4921bd1cfaaf
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2006-06-20 00:19:13 +0900

    [MIPS] Make timer interrupt frequency configurable from kconfig.
    Make HZ configurable.  DECSTATION can select 128/256/1024 HZ, JAZZ can
    only select 100 HZ, others can select 100/128/250/256/1000/1024 HZ if
    not explicitly specified).  Also remove all mach-xxx/param.h files and
    update all defconfigs according to current HZ value.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/param.h
+++ include/asm-mips/param.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 
-# include <param.h>			/* Internal kernel timer frequency */
+# define HZ		CONFIG_HZ	/* Internal kernel timer frequency */
 # define USER_HZ	100		/* .. some user interfaces are in "ticks" */
 # define CLOCKS_PER_SEC	(USER_HZ)	/* like times() */
 #endif
