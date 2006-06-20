0c87af520fbccf2d0cac109367a65f3c183023d2
Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
2006-06-20 23:17:18 +0900

    [MIPS] Remove unused system type name for DDB5074 and DDB5476.
    This patch removes unused system type name.
    DDB5074 and DDB5476 were already removed.
    
    Signed-off-by: Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/ddb5xxx/common/prom.c
+++ arch/mips/ddb5xxx/common/prom.c
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 const char *get_system_type(void)
 {
 	switch (mips_machtype) {
-	case MACH_NEC_DDB5074:		return "NEC DDB Vrc-5074";
-	case MACH_NEC_DDB5476:		return "NEC DDB Vrc-5476";
 	case MACH_NEC_DDB5477:		return "NEC DDB Vrc-5477";
 	case MACH_NEC_ROCKHOPPER:	return "NEC Rockhopper";
 	case MACH_NEC_ROCKHOPPERII:     return "NEC RockhopperII";
