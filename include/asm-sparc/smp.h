64c8c232fa98c17b19c652330d56dc41e1eefccb
Bob Breuer <breuerr@mc.net>
2006-06-20 00:30:31 -0700

    [SPARC]: Setup cpu_possible_map
    Setup cpu_possible_map so the secondary cpus will get started.
    
    Signed-off-by: Bob Breuer <breuerr@mc.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc/smp.h
+++ include/asm-sparc/smp.h
@@ -147,6 +147,8 @@ static inline int hard_smp_processor_id(void)
 #define prof_multiplier(__cpu)		cpu_data(__cpu).multiplier
 #define prof_counter(__cpu)		cpu_data(__cpu).counter
 
+void smp_setup_cpu_possible_map(void);
+
 #endif /* !(__ASSEMBLY__) */
 
 /* Sparc specific messages. */
@@ -163,7 +165,11 @@ static inline int hard_smp_processor_id(void)
 #define MBOX_IDLECPU2         0xFD
 #define MBOX_STOPCPU2         0xFE
 
-#endif /* SMP */
+#else /* SMP */
+
+#define smp_setup_cpu_possible_map() do { } while (0)
+
+#endif /* !(SMP) */
 
 #define NO_PROC_ID            0xFF
 
