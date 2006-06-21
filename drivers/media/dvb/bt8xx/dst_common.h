910617aff073e8371bac1940de498b133df175c6
Manu Abraham <abraham.manu@gmail.com>
2006-06-21 10:28:05 -0300

    V4L/DVB (4168): ATSC tuner doesn't have variable length field
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/bt8xx/dst_common.h
+++ drivers/media/dvb/bt8xx/dst_common.h
@@ -53,6 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DST_TYPE_HAS_OBS_REGS	128
 #define DST_TYPE_HAS_INC_COUNT	256
 #define DST_TYPE_HAS_MULTI_FE	512
+#define DST_TYPE_HAS_NEWTUNE_2	1024
 
 /*	Card capability list	*/
 
