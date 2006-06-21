7c6d9d79d594b2033afcff8dd801d66ac666e5d4
Manu Abraham <abraham.manu@gmail.com>
2006-06-21 10:41:45 -0300

    V4L/DVB (4179): VLF fixes for DCT
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/bt8xx/dst_common.h
+++ drivers/media/dvb/bt8xx/dst_common.h
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DST_TYPE_HAS_MULTI_FE	512
 #define DST_TYPE_HAS_NEWTUNE_2	1024
 #define DST_TYPE_HAS_DBOARD	2048
+#define DST_TYPE_HAS_VLF	4096
 
 /*	Card capability list	*/
 
