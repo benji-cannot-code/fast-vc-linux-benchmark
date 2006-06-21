f52dc4c9d6bf84c7f087e3bf532ff888b6bd90d8
Steven Whitehouse <swhiteho@redhat.com>
2006-06-21 11:54:43 -0400

    [GFS2] Make file_read_actor export _GPL
    Make file_read_actor export a _GPL export.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1045,7 +1045,7 @@ success:
 	desc->arg.buf += size;
 	return size;
 }
-EXPORT_SYMBOL(file_read_actor);
+EXPORT_SYMBOL_GPL(file_read_actor);
 
 /*
  * This is the "read()" routine for all filesystems
