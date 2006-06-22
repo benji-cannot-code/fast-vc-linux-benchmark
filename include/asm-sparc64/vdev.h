0607cb44249ceeaa3c03714e6f3145eade07ef08
David S. Miller <davem@sunset.davemloft.net>
2006-06-21 22:56:20 -0700

    [SPARC64]: Convert sun4v virtual-device layer to in-kernel PROM device tree.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/vdev.h
+++ include/asm-sparc64/vdev.h
@@ -8,10 +8,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _SPARC64_VDEV_H
 
 #include <linux/types.h>
+#include <asm/prom.h>
 
 extern u32 sun4v_vdev_devhandle;
-extern int sun4v_vdev_root;
+extern struct device_node *sun4v_vdev_root;
 
-extern unsigned int sun4v_vdev_device_interrupt(unsigned int);
+extern unsigned int sun4v_vdev_device_interrupt(struct device_node *dev_node);
 
 #endif /* !(_SPARC64_VDEV_H) */
