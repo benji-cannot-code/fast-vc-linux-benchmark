20ff64e6bcda3152b3710ef0c44d9110b9ce025c
Jon Loeliger <jdl@freescale.com>
2006-06-22 13:02:58 -0500

    [POWERPC] Remove obsolete #include <linux/config.h>.
    Signed-off-by: Jon Loeliger <jdl@freescale.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/mpc86xx.h
+++ include/asm-powerpc/mpc86xx.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_POWERPC_MPC86xx_H__
 #define __ASM_POWERPC_MPC86xx_H__
 
-#include <linux/config.h>
 #include <asm/mmu.h>
 
 #ifdef CONFIG_PPC_86xx
