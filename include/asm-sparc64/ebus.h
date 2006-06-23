ed124be6d3fcb45968e16df821135bdb510ad7ff
David S. Miller <davem@sunset.davemloft.net>
2006-06-23 01:44:10 -0700

    [SPARC64]: Add of_device layer and make ebus/isa use it.
    Sparcspkr and power drivers are converted, to make sure it works.
    Eventually the SBUS device layer will use this as a sub-class.
    
    I really cannot cut loose on that bit until sparc32 is given the
    same infrastructure.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/ebus.h
+++ include/asm-sparc64/ebus.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pbm.h>
 #include <asm/oplib.h>
 #include <asm/prom.h>
+#include <asm/of_device.h>
 
 struct linux_ebus_child {
 	struct linux_ebus_child		*next;
@@ -25,6 +26,7 @@ struct linux_ebus_child {
 };
 
 struct linux_ebus_device {
+	struct of_device		ofdev;
 	struct linux_ebus_device	*next;
 	struct linux_ebus_child		*children;
 	struct linux_ebus		*bus;
@@ -34,8 +36,10 @@ struct linux_ebus_device {
 	unsigned int			 irqs[PROMINTR_MAX];
 	int				 num_irqs;
 };
+#define to_ebus_device(d) container_of(d, struct linux_ebus_device, ofdev.dev)
 
 struct linux_ebus {
+	struct of_device		ofdev;
 	struct linux_ebus		*next;
 	struct linux_ebus_device	*devices;
 	struct pci_pbm_info		*parent;
@@ -44,6 +48,7 @@ struct linux_ebus {
 	int				 is_rio;
 	struct device_node		*prom_node;
 };
+#define to_ebus(d) container_of(d, struct linux_ebus, ofdev.dev)
 
 struct ebus_dma_info {
 	spinlock_t	lock;
