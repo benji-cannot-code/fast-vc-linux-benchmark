eefee812e02760337e0ee317463cd3a225398e22
Matt Helsley <matthltc@us.ibm.com>
2006-06-23 02:05:42 -0700

    [PATCH] Process Events - Header Cleanup
    Move connector header include to precisely where it's needed.
    
    Remove unused time.h header file as well.  This was leftover from previous
    iterations of the process events patches.
    
    Signed-off-by: Matt Helsley <matthltc@us.ibm.com>
    Cc: Guillaume Thouvenin <guillaume.thouvenin@bull.net>
    Cc: Nguyen Anh Quynh <aquynh@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/cn_proc.h
+++ include/linux/cn_proc.h
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CN_PROC_H
 
 #include <linux/types.h>
-#include <linux/time.h>
-#include <linux/connector.h>
 
 /*
  * Userspace sends this enum to register with the kernel that it is listening
