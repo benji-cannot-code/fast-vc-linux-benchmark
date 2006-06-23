e1257df5b419d6c1fc0733077646df3a142bf899
Nick Piggin <npiggin@suse.de>
2006-06-23 02:03:22 -0700

    [PATCH] radix-tree: small
    Reduce radix tree node memory usage by about a factor of 4 for small files
    (< 64K).  There are pointer traversal and memory usage costs for large
    files with dense pagecache.
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- lib/radix-tree.c
+++ lib/radix-tree.c
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #ifdef __KERNEL__
-#define RADIX_TREE_MAP_SHIFT	6
+#define RADIX_TREE_MAP_SHIFT	(CONFIG_BASE_SMALL ? 4 : 6)
 #else
 #define RADIX_TREE_MAP_SHIFT	3	/* For more stressful testing */
 #endif
