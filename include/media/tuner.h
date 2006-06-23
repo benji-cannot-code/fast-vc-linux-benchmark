42bf2dd50ab7177cdc902c8ff93faf1df6e86ee2
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-06-23 16:13:56 -0300

    V4L/DVB (4205): Merge tda9887 module into tuner.
    Most uses a tda988[5/6/7] IF demodulator as part of the device.
    Having this as a separate stuff makes harder to configure it, since there
    are some tda9887 options that are tuner-dependent and should be bound into
    tuner-types structures.
    This patch merges tda9887 module into tuner. More work is required to make
    tuner-types to properly use it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 #include <media/tuner-types.h>
 
+extern int tuner_debug;
+
 #define ADDR_UNSET (255)
 
 #define TUNER_TEMIC_PAL			0        /* 4002 FH5 (3X 7756, 9483) */
@@ -121,6 +123,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TUNER_THOMSON_FE6600		72	/* DViCO FusionHDTV DVB-T Hybrid */
 #define TUNER_SAMSUNG_TCPG_6121P30A     73 	/* Hauppauge PVR-500 PAL */
+#define TUNER_TDA9887                   74      /* This tuner should be used only internally */
 
 /* tv card specific */
 #define TDA9887_PRESENT 		(1<<0)
@@ -192,6 +195,10 @@ struct tuner {
 
 	int          using_v4l2;
 
+	/* used by tda9887 */
+	unsigned int       tda9887_config;
+	unsigned char 	   tda9887_data[4];
+
 	/* used by MT2032 */
 	unsigned int xogc;
 	unsigned int radio_if2;
@@ -208,6 +215,8 @@ struct tuner {
 	void (*set_radio_freq)(struct i2c_client *c, unsigned int freq);
 	int  (*has_signal)(struct i2c_client *c);
 	int  (*is_stereo)(struct i2c_client *c);
+	int  (*get_afc)(struct i2c_client *c);
+	void (*tuner_status)(struct i2c_client *c);
 	void (*standby)(struct i2c_client *c);
 };
 
@@ -220,6 +229,7 @@ extern int tda8290_probe(struct i2c_client *c);
 extern int tea5767_tuner_init(struct i2c_client *c);
 extern int default_tuner_init(struct i2c_client *c);
 extern int tea5767_autodetection(struct i2c_client *c);
+extern int tda9887_tuner_init(struct i2c_client *c);
 
 #define tuner_warn(fmt, arg...) do {\
 	printk(KERN_WARNING "%s %d-%04x: " fmt, t->i2c.driver->driver.name, \
