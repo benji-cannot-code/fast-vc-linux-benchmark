0fa8fe9791cde53698045dc0a085c1b1d4b24b1d
David S. Miller <davem@sunset.davemloft.net>
2006-06-23 15:55:17 -0700

    [SPARC]: Port of_device layer and make ebus use it.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc/ebus.h
+++ include/asm-sparc/ebus.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 #include <asm/oplib.h>
 #include <asm/prom.h>
+#include <asm/of_device.h>
 
 struct linux_ebus_child {
 	struct linux_ebus_child		*next;
@@ -28,6 +29,7 @@ struct linux_ebus_child {
 };
 
 struct linux_ebus_device {
+	struct of_device		ofdev;
 	struct linux_ebus_device	*next;
 	struct linux_ebus_child		*children;
 	struct linux_ebus		*bus;
@@ -37,14 +39,17 @@ struct linux_ebus_device {
 	unsigned int			 irqs[PROMINTR_MAX];
 	int				 num_irqs;
 };
+#define to_ebus_device(d) container_of(d, struct linux_ebus_device, ofdev.dev)
 
 struct linux_ebus {
+	struct of_device		ofdev;
 	struct linux_ebus		*next;
 	struct linux_ebus_device	*devices;
 	struct linux_pbm_info		*parent;
 	struct pci_dev			*self;
 	struct device_node		*prom_node;
 };
+#define to_ebus(d) container_of(d, struct linux_ebus, ofdev.dev)
 
 struct linux_ebus_dma {
 	unsigned int dcsr;
--- include/asm-sparc/of_device.h
+++ include/asm-sparc/of_device.h
@@ -1,13 +1,12 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _ASM_SPARC64_OF_DEVICE_H
-#define _ASM_SPARC64_OF_DEVICE_H
+#ifndef _ASM_SPARC_OF_DEVICE_H
+#define _ASM_SPARC_OF_DEVICE_H
 #ifdef __KERNEL__
 
 #include <linux/device.h>
 #include <linux/mod_devicetable.h>
 #include <asm/prom.h>
 
-extern struct bus_type isa_bus_type;
 extern struct bus_type ebus_bus_type;
 extern struct bus_type sbus_bus_type;
 
@@ -62,4 +61,4 @@ extern struct of_device *of_platform_device_create(struct device_node *np,
 extern void of_release_dev(struct device *dev);
 
 #endif /* __KERNEL__ */
-#endif /* _ASM_SPARC64_OF_DEVICE_H */
+#endif /* _ASM_SPARC_OF_DEVICE_H */
