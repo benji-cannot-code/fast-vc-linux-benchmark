54d918295123bf4ef88273cf458889a89a674ab0
Kirill Smelkov <kirr@mns.spb.ru>
2006-06-23 02:04:33 -0700

    [PATCH] x86: compile fix for asm-i386/alternatives.h
    compile fix:  <asm-i386/alternative.h>  needs  <asm/types.h> for 'u8' --
    just look at struct alt_instr.
    
    My module includes <asm/bitops.h> as the first header, and as of 2.6.17 this
    leads to compilation errors.
    
    Signed-off-by: Kirill Smelkov <kirr@mns.spb.ru>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/alternative.h
+++ include/asm-i386/alternative.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 
+#include <asm/types.h>
+
 struct alt_instr {
 	u8 *instr; 		/* original instruction */
 	u8 *replacement;
