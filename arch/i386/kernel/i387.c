fd98b48a702cdf6d4a0cf8cb6311c63d0fb8aad2
Andreas Mohr <andi@rhlx01.fht-esslingen.de>
2006-06-23 02:04:26 -0700

    [PATCH] x86: make i387 mxcsr_feature_mask __read_mostly
    Signed-off-by: Andreas Mohr <andi@lisas.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/kernel/i387.c
+++ arch/i386/kernel/i387.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define HAVE_HWFP 1
 #endif
 
-static unsigned long mxcsr_feature_mask = 0xffffffff;
+static unsigned long mxcsr_feature_mask __read_mostly = 0xffffffff;
 
 void mxcsr_feature_mask_init(void)
 {
