bce1f6f3dcc05dc43213d484dd7800a2922c7328
KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
2006-06-23 02:03:13 -0700

    [PATCH] support for panic at OOM
    This patch adds panic_on_oom sysctl under sys.vm.
    
    When sysctl vm.panic_on_oom = 1, the kernel panics intead of killing rogue
    processes.  And if vm.panic_on_oom is 0 the kernel will do oom_kill() in
    the same way as it does today.  Of course, the default value is 0 and only
    root can modifies it.
    
    In general, oom_killer works well and kill rogue processes.  So the whole
    system can survive.  But there are environments where panic is preferable
    rather than kill some processes.
    
    Signed-off-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/jiffies.h>
 #include <linux/cpuset.h>
 
+int sysctl_panic_on_oom;
 /* #define DEBUG */
 
 /**
@@ -345,6 +346,8 @@ void out_of_memory(struct zonelist *zonelist, gfp_t gfp_mask, int order)
 		break;
 
 	case CONSTRAINT_NONE:
+		if (sysctl_panic_on_oom)
+			panic("out of memory. panic_on_oom is selected\n");
 retry:
 		/*
 		 * Rambo mode: Shoot down a process and hope it solves whatever
