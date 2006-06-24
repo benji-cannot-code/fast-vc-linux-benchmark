1016864b0b3bc744fa98541598e8d2a6ecfcdaf0
Tejun Heo <htejun@gmail.com>
2006-06-24 20:30:19 +0900

    [PATCH] libata: make two functions global
    Make ata_do_simple_cmd() and ata_flush_cache() global.  These will be
    used from libata-eh.c.
    
    Signed-off-by: Tejun Heo <htejun@gmail.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/scsi/libata-core.c
+++ drivers/scsi/libata-core.c
@@ -1146,7 +1146,7 @@ unsigned ata_exec_internal(struct ata_device *dev,
  *	RETURNS:
  *	Zero on success, AC_ERR_* mask on failure
  */
-static unsigned int ata_do_simple_cmd(struct ata_device *dev, u8 cmd)
+unsigned int ata_do_simple_cmd(struct ata_device *dev, u8 cmd)
 {
 	struct ata_taskfile tf;
 
@@ -4974,7 +4974,7 @@ int ata_port_offline(struct ata_port *ap)
 	return 0;
 }
 
-static int ata_flush_cache(struct ata_device *dev)
+int ata_flush_cache(struct ata_device *dev)
 {
 	unsigned int err_mask;
 	u8 cmd;
