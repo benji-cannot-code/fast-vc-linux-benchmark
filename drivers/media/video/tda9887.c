fbba048c1463d230f4ce66d681f2f22b3c1b0e66
Hans Verkuil <hverkuil@xs4all.nl>
2006-06-25 11:02:02 -0300

    V4L/DVB (4252): Remove duplicate 'tda9887' in info messages.
    Remove the duplicate '(tda9887)' in these messages:
    tda9887 8-0043 (tda9887): tda988[5/6/7] found @ 0x43 (tuner)
    The same string is already printed as the prefix in this line.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tda9887.c
+++ drivers/media/video/tda9887.c
@@ -23,11 +23,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 #define tda9887_info(fmt, arg...) do {\
-	printk(KERN_INFO "%s %d-%04x (tda9887): " fmt, t->i2c.name, \
+	printk(KERN_INFO "%s %d-%04x: " fmt, t->i2c.name, \
 			i2c_adapter_id(t->i2c.adapter), t->i2c.addr , ##arg); } while (0)
 #define tda9887_dbg(fmt, arg...) do {\
 	if (tuner_debug) \
-		printk(KERN_INFO "%s %d-%04x (tda9887): " fmt, t->i2c.name, \
+		printk(KERN_INFO "%s %d-%04x: " fmt, t->i2c.name, \
 			i2c_adapter_id(t->i2c.adapter), t->i2c.addr , ##arg); } while (0)
 
 
