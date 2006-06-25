075ebde7aab9e3da6c36099ed54f641f5a70efbe
Jan Engelhardt <jengelh@linux01.gwdg.de>
2006-06-25 05:48:49 -0700

    [PATCH] fuse: use MISC_MAJOR
    The following patches add POSIX file locking to the fuse interface.
    
    Additional changes ralated to this are:
    
      - asynchronous interrupt of requests by SIGKILL no longer supported
    
      - separate control filesystem, instead of using sysfs objects
    
      - add support for synchronously interrupting requests
    
    Details are documented in Documentation/filesystems/fuse.txt throughout the
    patches.
    
    This patch:
    
    Have fuse.h use MISC_MAJOR rather than a hardcoded '10'.
    
    Signed-off-by: Jan Engelhardt <jengelh@gmx.de>
    Signed-off-by: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/fuse.h
+++ include/linux/fuse.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* This file defines the kernel interface of FUSE */
 
 #include <asm/types.h>
+#include <linux/major.h>
 
 /** Version number of this interface */
 #define FUSE_KERNEL_VERSION 7
@@ -21,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FUSE_ROOT_ID 1
 
 /** The major number of the fuse character device */
-#define FUSE_MAJOR 10
+#define FUSE_MAJOR MISC_MAJOR
 
 /** The minor number of the fuse character device */
 #define FUSE_MINOR 229
