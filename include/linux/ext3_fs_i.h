f85d2083d31a34976a4c662b0bb83a14104307ee
Mingming Cao <cmm@us.ibm.com>
2006-06-25 05:48:06 -0700

    [PATCH] ext3_fsblk_t: filesystem, group blocks and bug fixes
    Some of the in-kernel ext3 block variable type are treated as signed 4 bytes
    int type, thus limited ext3 filesystem to 8TB (4kblock size based).  While
    trying to fix them, it seems quite confusing in the ext3 code where some
    blocks are filesystem-wide blocks, some are group relative offsets that need
    to be signed value (as -1 has special meaning).  So it seem saner to define
    two types of physical blocks: one is filesystem wide blocks, another is
    group-relative blocks.  The following patches clarify these two types of
    blocks in the ext3 code, and fix the type bugs which limit current 32 bit ext3
    filesystem limit to 8TB.
    
    With this series of patches and the percpu counter data type changes in the mm
    tree, we are able to extend exts filesystem limit to 16TB.
    
    This work is also a pre-request for the recent >32 bit ext3 work, and makes
    the kernel to able to address 48 bit ext3 block a lot easier: Simply redefine
    ext3_fsblk_t from unsigned long to sector_t and redefine the format string for
    ext3 filesystem block corresponding.
    
    Two RFC with a series patches have been posted to ext2-devel list and have
    been reviewed and discussed:
    http://marc.theaimsgroup.com/?l=ext2-devel&m=114722190816690&w=2
    
    http://marc.theaimsgroup.com/?l=ext2-devel&m=114784919525942&w=2
    
    Patches are tested on both 32 bit machine and 64 bit machine, <8TB ext3 and
    >8TB ext3 filesystem(with the latest to be released e2fsprogs-1.39).  Tests
    includes overnight fsx, tiobench, dbench and fsstress.
    
    This patch:
    
    Defines ext3_fsblk_t and ext3_grpblk_t, and the printk format string for
    filesystem wide blocks.
    
    This patch classifies all block group relative blocks, and ext3_fsblk_t blocks
    occurs in the same function where used to be confusing before.  Also include
    kernel bug fixes for filesystem wide in-kernel block variables.  There are
    some fileystem wide blocks are treated as int/unsigned int type in the kernel
    currently, especially in ext3 block allocation and reservation code.  This
    patch fixed those bugs by converting those variables to ext3_fsblk_t(unsigned
    long) type.
    
    Signed-off-by: Mingming Cao <cmm@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/ext3_fs_i.h
+++ include/linux/ext3_fs_i.h
@@ -22,6 +22,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seqlock.h>
 #include <linux/mutex.h>
 
+/* data type for block offset of block group */
+typedef int ext3_grpblk_t;
+
+/* data type for filesystem-wide blocks number */
+typedef unsigned long ext3_fsblk_t;
+
+#define E3FSBLK "%lu"
+
 struct ext3_reserve_window {
 	__u32			_rsv_start;	/* First byte reserved */
 	__u32			_rsv_end;	/* Last byte reserved or 0 */
