4f69a01c0b193703d3882472a0830c865b2b98ba
Russell King <rmk@dyn-67.arm.linux.org.uk>
2006-06-25 12:01:48 +0100

    [ARM] Remove MODE_(SVC|IRQ|FIQ|USR) and DEFAULT_FIQ
    DEFAULT_FIQ was entirely unused.  MODE_* are just redefinitions
    of *_MODE.  Use *_MODE instead.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/assembler.h
+++ include/asm-arm/assembler.h
@@ -56,13 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PLD(code...)
 #endif
 
-#define MODE_USR	USR_MODE
-#define MODE_FIQ	FIQ_MODE
-#define MODE_IRQ	IRQ_MODE
-#define MODE_SVC	SVC_MODE
-
-#define DEFAULT_FIQ	MODE_FIQ
-
 /*
  * Enable and disable interrupts
  */
