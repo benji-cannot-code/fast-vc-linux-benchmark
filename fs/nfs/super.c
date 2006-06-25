a8c0bc0b14a7f195b0637b2c8d273a09b84d06d2
Andrew Morton <akpm@osdl.org>
2006-06-25 02:41:27 -0700

    nfs-build-fix-99
    fs/built-in.o:(__param+0x20): undefined reference to `nfs_idmap_cache_timeout'
    fs/built-in.o:(__param+0x48): undefined reference to `nfs_callback_set_tcpport'
    
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: Andreas Gruenbacher <agruen@suse.de>
    Cc: Andy Adamson <andros@citi.umich.edu>
    Cc: Chuck Lever <cel@netapp.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: J. Bruce Fields <bfields@fieldses.org>
    Cc: Manoj Naik <manoj@almaden.ibm.com>
    Cc: Marc Eshel <eshel@almaden.ibm.com>
    Cc: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -188,6 +188,7 @@ static struct super_operations nfs4_sops = {
 };
 #endif
 
+#ifdef CONFIG_NFS_V4
 static const int nfs_set_port_min = 0;
 static const int nfs_set_port_max = 65535;
 
@@ -203,7 +204,9 @@ static int param_set_port(const char *val, struct kernel_param *kp)
 
 module_param_call(callback_tcpport, param_set_port, param_get_int,
 		 &nfs_callback_set_tcpport, 0644);
+#endif
 
+#ifdef CONFIG_NFS_V4
 static int param_set_idmap_timeout(const char *val, struct kernel_param *kp)
 {
 	char *endp;
@@ -217,6 +220,7 @@ static int param_set_idmap_timeout(const char *val, struct kernel_param *kp)
 
 module_param_call(idmap_cache_timeout, param_set_idmap_timeout, param_get_int,
 		 &nfs_idmap_cache_timeout, 0644);
+#endif
 
 /*
  * Register the NFS filesystems
