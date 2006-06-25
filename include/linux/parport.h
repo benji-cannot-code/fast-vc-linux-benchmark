caab52349e317f34d55b54491400e5ed9b98ac3b
Ben Dooks <ben-linux@fluff.org>
2006-06-25 05:48:03 -0700

    [PATCH] AX88796 parallel port driver
    Driver for the simple parallel port interface on the Asix AX88796 chip on
    an platform_bus.
    
    [akpm@osdl.org: x86_64 build fix]
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/parport.h
+++ include/linux/parport.h
@@ -128,6 +128,10 @@ struct amiga_parport_state {
        unsigned char statusdir;/* ciab.ddrb & 7 */
 };
 
+struct ax88796_parport_state {
+	unsigned char cpr;
+};
+
 struct ip32_parport_state {
 	unsigned int dcr;
 	unsigned int ecr;
@@ -139,6 +143,7 @@ struct parport_state {
 		/* ARC has no state. */
 		struct ax_parport_state ax;
 		struct amiga_parport_state amiga;
+		struct ax88796_parport_state ax88796;
 		/* Atari has not state. */
 		struct ip32_parport_state ip32;
 		void *misc; 
