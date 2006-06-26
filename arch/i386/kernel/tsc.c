b1df68f7bb666929aa616524f308f9d4cc80d7e0
john stultz <johnstul@us.ibm.com>
2006-06-26 00:25:11 -0700

    [PATCH] Time: i386 Conversion - part 3: Enable Generic Timekeeping
    This converts the i386 arch to use the generic timeofday subsystem.  It
    enabled the GENERIC_TIME option, disables the timer_opts code and other arch
    specific timekeeping code and reworks the delay code.
    
    While this patch enables the generic timekeeping, please note that this patch
    does not provide any i386 clocksource.  Thus only the jiffies clocksource will
    be available.  To get full replacements for the code being disabled here, the
    timeofday-clocks-i386 patch will needed.
    
    Signed-off-by: John Stultz <johnstul@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/kernel/tsc.c
+++ arch/i386/kernel/tsc.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 
 #include <asm/tsc.h>
+#include <asm/delay.h>
 #include <asm/io.h>
 
 #include "mach_timer.h"
@@ -45,7 +46,6 @@ static int __init tsc_setup(char *str)
 #endif
 
 __setup("notsc", tsc_setup);
-
 
 /*
  * code to mark and check if the TSC is unstable
@@ -206,6 +206,7 @@ void tsc_init(void)
 				(unsigned long)cpu_khz % 1000);
 
 	set_cyc2ns_scale(cpu_khz);
+	use_tsc_delay();
 }
 
 #ifdef CONFIG_CPU_FREQ
