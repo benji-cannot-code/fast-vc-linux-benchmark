fe012acea7263d1dc3442aef5aaa6a6cd65f0bdc
Andi Kleen <ak@suse.de>
2006-06-26 13:56:49 +0200

    [PATCH] x86_64: Use -ENODEV in IOMMU initialization
    Fix
    
    initcall at 0xffffffff806c5b89: pci_iommu_init+0x0/0x53c(): returned with error code -1
    
    Return -ENODEV instead when the IOMMU is not used.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/pci-gart.c
+++ arch/x86_64/kernel/pci-gart.c
@@ -582,7 +582,7 @@ static int __init pci_iommu_init(void)
 
 	if (cache_k8_northbridges() < 0 || num_k8_northbridges == 0) {
 		printk(KERN_INFO "PCI-GART: No AMD northbridge found.\n");
-		return -1;
+		return -ENODEV;
 	}
 
 #ifndef CONFIG_AGP_AMD64
@@ -596,7 +596,7 @@ static int __init pci_iommu_init(void)
 #endif	
 
 	if (swiotlb)
-		return -1; 
+		return -ENODEV;
 
 	if (no_iommu ||
 	    (!force_iommu && end_pfn <= MAX_DMA32_PFN) ||
@@ -608,7 +608,7 @@ static int __init pci_iommu_init(void)
 					"but IOMMU not available.\n"
 			       KERN_ERR "WARNING 32bit PCI may malfunction.\n");
 		}
-		return -1;
+		return -ENODEV;
 	}
 
 	printk(KERN_INFO "PCI-DMA: using GART IOMMU.\n");
