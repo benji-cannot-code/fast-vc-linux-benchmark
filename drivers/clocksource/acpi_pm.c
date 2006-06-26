ecb0829f3785942970fac9d220efc1c2d49fd915
Andreas Mohr <andi@rhlx01.fht-esslingen.de>
2006-06-26 00:25:14 -0700

    [PATCH] make pmtmr_ioport __read_mostly
    - written on init only, accessed for every timer read --> __read_mostly
    - fix broken sentence
    
    Signed-off-by: Andreas Mohr <andi@lisas.de>
    Cc: john stultz <johnstul@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/clocksource/acpi_pm.c
+++ drivers/clocksource/acpi_pm.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * The location is detected during setup_arch(),
  * in arch/i386/acpi/boot.c
  */
-u32 pmtmr_ioport;
+u32 pmtmr_ioport __read_mostly;
 
 #define ACPI_PM_MASK 0xFFFFFF /* limit it to 24 bits */
 
@@ -48,7 +48,7 @@ static cycle_t acpi_pm_read_verified(void)
 	/*
 	 * It has been reported that because of various broken
 	 * chipsets (ICH4, PIIX4 and PIIX4E) where the ACPI PM clock
-	 * source is not latched, so you must read it multiple
+	 * source is not latched, you must read it multiple
 	 * times to ensure a safe value is read:
 	 */
 	do {
