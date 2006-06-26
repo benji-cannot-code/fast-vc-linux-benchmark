a42f1eca467f9c15e18e25f50b808d8da14e8d47
NeilBrown <neilb@suse.de>
2006-06-26 00:27:36 -0700

    [PATCH] md: remove arbitrary limit on chunk size
    The largest chunk size the code can support without substantial surgery is
    2^30 bytes, so make that the limit instead of an arbitrary 4Meg.  Some day,
    the 'chunksize' should change to a sector-shift instead of a byte-count.  Then
    no limit would be needed.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/raid/md_k.h
+++ include/linux/raid/md_k.h
@@ -41,7 +41,8 @@ typedef struct mdk_rdev_s mdk_rdev_t;
  * options passed in raidrun:
  */
 
-#define MAX_CHUNK_SIZE (4096*1024)
+/* Currently this must fix in an 'int' */
+#define MAX_CHUNK_SIZE (1<<30)
 
 /*
  * MD's 'extended' device
