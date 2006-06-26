23abcfa19d2407d5a56f2f5086e4a082612861d6
Antonino A. Daplas <adaplas@gmail.com>
2006-06-26 00:26:40 -0700

    [PATCH] vesafb: Fix return code of vesafb_setcolreg
    If the hardware palette cannot be accessed, make vesafb_setcolreg return a
    nonzero value.
    
    Signed-off-by: Antonino Daplas <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/vesafb.c
+++ drivers/video/vesafb.c
@@ -81,10 +81,11 @@ static int vesafb_pan_display(struct fb_var_screeninfo *var,
 	return 0;
 }
 
-static void vesa_setpalette(int regno, unsigned red, unsigned green,
+static int vesa_setpalette(int regno, unsigned red, unsigned green,
 			    unsigned blue)
 {
 	int shift = 16 - depth;
+	int err = -EINVAL;
 
 #ifdef __i386__
 	struct { u_char blue, green, red, pad; } entry;
@@ -103,7 +104,7 @@ static void vesa_setpalette(int regno, unsigned red, unsigned green,
                   "d" (regno),          /* EDX */
                   "D" (&entry),         /* EDI */
                   "S" (&pmi_pal));      /* ESI */
-		return;
+		err = 0;
 	}
 #endif
 
@@ -111,18 +112,23 @@ static void vesa_setpalette(int regno, unsigned red, unsigned green,
  * without protected mode interface and if VGA compatible,
  * try VGA registers...
  */
-	if (vga_compat) {
+	if (err && vga_compat) {
 		outb_p(regno,       dac_reg);
 		outb_p(red   >> shift, dac_val);
 		outb_p(green >> shift, dac_val);
 		outb_p(blue  >> shift, dac_val);
+		err = 0;
 	}
+
+	return err;
 }
 
 static int vesafb_setcolreg(unsigned regno, unsigned red, unsigned green,
 			    unsigned blue, unsigned transp,
 			    struct fb_info *info)
 {
+	int err = 0;
+
 	/*
 	 *  Set a single color register. The values supplied are
 	 *  already rounded down to the hardware's capabilities
@@ -134,7 +140,7 @@ static int vesafb_setcolreg(unsigned regno, unsigned red, unsigned green,
 		return 1;
 
 	if (info->var.bits_per_pixel == 8)
-		vesa_setpalette(regno,red,green,blue);
+		err = vesa_setpalette(regno,red,green,blue);
 	else if (regno < 16) {
 		switch (info->var.bits_per_pixel) {
 		case 16:
@@ -165,7 +171,7 @@ static int vesafb_setcolreg(unsigned regno, unsigned red, unsigned green,
 		}
 	}
 
-	return 0;
+	return err;
 }
 
 static struct fb_ops vesafb_ops = {
