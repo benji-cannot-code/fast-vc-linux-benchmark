36b90c0ee159bdacbe891ea5d93661ca46b2971d
Eric W. Biederman <ebiederm@xmission.com>
2006-06-26 00:25:51 -0700

    [PATCH] proc: Remove tasklist_lock from proc_pid_readdir
    We don't need the tasklist_lock to safely iterate through processes
    anymore.
    
    This depends on my previous to task patches that make get_task_struct rcu
    safe, and that make next_task() rcu safe.  I haven't gotten
    first_tid/next_tid yet only because next_thread is missing an
    rcu_dereference.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2044,7 +2044,7 @@ out:
 static struct task_struct *first_tgid(int tgid, int nr)
 {
 	struct task_struct *pos = NULL;
-	read_lock(&tasklist_lock);
+	rcu_read_lock();
 	if (tgid && nr) {
 		pos = find_task_by_pid(tgid);
 		if (pos && !thread_group_leader(pos))
@@ -2070,7 +2070,7 @@ static struct task_struct *first_tgid(int tgid, int nr)
 	}
 	pos = NULL;
 done:
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
 	return pos;
 }
 
@@ -2083,7 +2083,7 @@ done:
 static struct task_struct *next_tgid(struct task_struct *start)
 {
 	struct task_struct *pos;
-	read_lock(&tasklist_lock);
+	rcu_read_lock();
 	pos = start;
 	if (pid_alive(start))
 		pos = next_task(start);
@@ -2093,7 +2093,7 @@ static struct task_struct *next_tgid(struct task_struct *start)
 	}
 	pos = NULL;
 done:
-	read_unlock(&tasklist_lock);
+	rcu_read_unlock();
 	put_task_struct(start);
 	return pos;
 }
