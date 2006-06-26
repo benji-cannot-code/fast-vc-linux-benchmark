964a612f079fc0d4094dcaad66255a08e64ac9ae
Adrian Bunk <bunk@stusta.de>
2006-06-26 00:26:45 -0700

    [PATCH] fbdev: Remove unused exports
    This patch removes the following unused EXPORT_SYMBOL's:
    - fbcvt.c: fb_find_mode_cvt
    - fbmem.c: fb_con_duit
    - fbmem.c: fb_new_modelist
    - macmodes.c: mac_var_to_vmode
    - modedb.c: fb_delete_videomode
    - modedb.c: fb_destroy_modelist
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Antonino Daplas <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/fbmem.c
+++ drivers/video/fbmem.c
@@ -1510,7 +1510,6 @@ int fb_con_duit(struct fb_info *info, int event, void *data)
 
 	return blocking_notifier_call_chain(&fb_notifier_list, event, &evnt);
 }
-EXPORT_SYMBOL(fb_con_duit);
 
 static char *video_options[FB_MAX];
 static int ofonly;
@@ -1621,6 +1620,5 @@ EXPORT_SYMBOL(fb_set_suspend);
 EXPORT_SYMBOL(fb_register_client);
 EXPORT_SYMBOL(fb_unregister_client);
 EXPORT_SYMBOL(fb_get_options);
-EXPORT_SYMBOL(fb_new_modelist);
 
 MODULE_LICENSE("GPL");
