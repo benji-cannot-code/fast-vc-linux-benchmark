cd2a7fa3622cc080084747777613491d39de8942
Jeff Mahoney <jeffm@suse.com>
2006-06-26 00:27:25 -0700

    [PATCH] dm: add module ref counting
    The reference counting on dm-mod is zero if no mapped devices are open.  This
    is incorrect, and can lead to an oops if the module is unloaded while mapped
    devices exist.
    
    This patch claims a reference to the module whenever a device is created, and
    drops it again when the device is freed.
    
    Devices must be removed before dm-mod is unloaded.
    
    [akpm: too late for 2.6.17 - suitable for 2.6.17.x after it has settled]
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -853,6 +853,9 @@ static struct mapped_device *alloc_dev(unsigned int minor, int persistent)
 		return NULL;
 	}
 
+	if (!try_module_get(THIS_MODULE))
+		goto bad0;
+
 	/* get a minor number for the dev */
 	r = persistent ? specific_minor(md, minor) : next_free_minor(md, &minor);
 	if (r < 0)
@@ -919,6 +922,8 @@ static struct mapped_device *alloc_dev(unsigned int minor, int persistent)
 	blk_cleanup_queue(md->queue);
 	free_minor(minor);
  bad1:
+	module_put(THIS_MODULE);
+ bad0:
 	kfree(md);
 	return NULL;
 }
@@ -942,6 +947,7 @@ static void free_dev(struct mapped_device *md)
 
 	put_disk(md->disk);
 	blk_cleanup_queue(md->queue);
+	module_put(THIS_MODULE);
 	kfree(md);
 }
 
