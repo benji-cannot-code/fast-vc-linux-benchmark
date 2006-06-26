baaa5da1dee69b3eba6e59291d3e33feb9d785a5
Ingo Molnar <mingo@elte.hu>
2006-06-26 00:24:31 -0700

    [PATCH] work around ppc64 bootup bug by making mutex-debugging save/restore irqs
    It seems ppc64 wants to lock mutexes in early bootup code, with interrupts
    disabled, and they expect interrupts to stay disabled, else they crash.
    
    Work around this bug by making mutex debugging variants save/restore irq
    flags.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/mutex-debug.c
+++ kernel/mutex-debug.c
@@ -154,13 +154,13 @@ next:
 			continue;
 		count++;
 		cursor = curr->next;
-		debug_spin_lock_restore(&debug_mutex_lock, flags);
+		debug_spin_unlock_restore(&debug_mutex_lock, flags);
 
 		printk("\n#%03d:            ", count);
 		printk_lock(lock, filter ? 0 : 1);
 		goto next;
 	}
-	debug_spin_lock_restore(&debug_mutex_lock, flags);
+	debug_spin_unlock_restore(&debug_mutex_lock, flags);
 	printk("\n");
 }
 
@@ -317,7 +317,7 @@ void mutex_debug_check_no_locks_held(struct task_struct *task)
 			continue;
 		list_del_init(curr);
 		DEBUG_OFF();
-		debug_spin_lock_restore(&debug_mutex_lock, flags);
+		debug_spin_unlock_restore(&debug_mutex_lock, flags);
 
 		printk("BUG: %s/%d, lock held at task exit time!\n",
 			task->comm, task->pid);
@@ -326,7 +326,7 @@ void mutex_debug_check_no_locks_held(struct task_struct *task)
 			printk("exiting task is not even the owner??\n");
 		return;
 	}
-	debug_spin_lock_restore(&debug_mutex_lock, flags);
+	debug_spin_unlock_restore(&debug_mutex_lock, flags);
 }
 
 /*
@@ -353,7 +353,7 @@ void mutex_debug_check_no_locks_freed(const void *from, unsigned long len)
 			continue;
 		list_del_init(curr);
 		DEBUG_OFF();
-		debug_spin_lock_restore(&debug_mutex_lock, flags);
+		debug_spin_unlock_restore(&debug_mutex_lock, flags);
 
 		printk("BUG: %s/%d, active lock [%p(%p-%p)] freed!\n",
 			current->comm, current->pid, lock, from, to);
@@ -363,7 +363,7 @@ void mutex_debug_check_no_locks_freed(const void *from, unsigned long len)
 			printk("freeing task is not even the owner??\n");
 		return;
 	}
-	debug_spin_lock_restore(&debug_mutex_lock, flags);
+	debug_spin_unlock_restore(&debug_mutex_lock, flags);
 }
 
 /*
