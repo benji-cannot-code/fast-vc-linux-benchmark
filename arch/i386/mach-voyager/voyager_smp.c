1055d949bce43ee72c62411dc9b86d07a8204cd3
James Bottomley <James.Bottomley@HansenPartnership.com>
2006-06-26 21:33:09 -0500

    [PATCH] voyager: add cpu_present_map
    Voyager stopped booting some time in the 2.6.16-2.6.17 timeframe;
    the reason was that it doesn't have a cpu_present_map, so add
    one.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/i386/mach-voyager/voyager_smp.c
+++ arch/i386/mach-voyager/voyager_smp.c
@@ -662,6 +662,7 @@ do_boot_cpu(__u8 cpu)
 		print_cpu_info(&cpu_data[cpu]);
 		wmb();
 		cpu_set(cpu, cpu_callout_map);
+		cpu_set(cpu, cpu_present_map);
 	}
 	else {
 		printk("CPU%d FAILED TO BOOT: ", cpu);
@@ -1913,6 +1914,7 @@ void __devinit smp_prepare_boot_cpu(void)
 	cpu_set(smp_processor_id(), cpu_online_map);
 	cpu_set(smp_processor_id(), cpu_callout_map);
 	cpu_set(smp_processor_id(), cpu_possible_map);
+	cpu_set(smp_processor_id(), cpu_present_map);
 }
 
 int __devinit
