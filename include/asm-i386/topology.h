2caefe598ff7bbeed2d3a1b7f14c96c50dd7c538
Rohit Seth <rohitseth@google.com>
2006-06-27 02:53:46 -0700

    [PATCH] i386: move phys_proc_id and cpu_core_id to cpuinfo_x86
    Move the phys_core_id and cpu_core_id to cpuinfo_x86 structure.  Similar
    patch for x86_64 is already accepted by Andi earlier this week.
    
    [akpm@osdl.org: fix warning]
    Signed-off-by: Rohit Seth <rohitseth@google.com>
    Cc: Andi Kleen <ak@muc.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/topology.h
+++ include/asm-i386/topology.h
@@ -29,10 +29,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_I386_TOPOLOGY_H
 
 #ifdef CONFIG_X86_HT
-#define topology_physical_package_id(cpu)				\
-	(phys_proc_id[cpu] == BAD_APICID ? -1 : phys_proc_id[cpu])
-#define topology_core_id(cpu)						\
-	(cpu_core_id[cpu] == BAD_APICID ? 0 : cpu_core_id[cpu])
+#define topology_physical_package_id(cpu)	(cpu_data[cpu].phys_proc_id)
+#define topology_core_id(cpu)			(cpu_data[cpu].cpu_core_id)
 #define topology_core_siblings(cpu)		(cpu_core_map[cpu])
 #define topology_thread_siblings(cpu)		(cpu_sibling_map[cpu])
 #endif
