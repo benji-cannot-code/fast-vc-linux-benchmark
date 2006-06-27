31dcaaa9dd668d1a426800956d32db1ee73bdf97
Randy Dunlap <rdunlap@xenotime.net>
2006-06-27 02:53:53 -0700

    [PATCH] update two drivers for poison.h
    Update two drivers to use poison.h.
    
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/poison.h
+++ include/linux/poison.h
@@ -43,4 +43,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define	POOL_POISON_FREED	0xa7	/* !inuse */
 #define	POOL_POISON_ALLOCATED	0xa9	/* !initted */
 
+/********** drivers/atm/ **********/
+#define ATM_POISON_FREE		0x12
+
+/********** sound/oss/ **********/
+#define OSS_POISON_FREE		0xAB
+
 #endif
