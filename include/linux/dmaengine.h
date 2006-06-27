f8f462d9b8a671aeedbff4a157463632d0709132
David Woodhouse <dwmw2@infradead.org>
2006-06-27 02:53:56 -0700

    [PATCH] Remove gratuitous inclusion of <linux/config.h> from <linux/dmaengine.h>
    We include config.h on the compiler command line. There's no need for it
    to be included again.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/dmaengine.h
+++ include/linux/dmaengine.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifndef DMAENGINE_H
 #define DMAENGINE_H
-#include <linux/config.h>
+
 #ifdef CONFIG_DMA_ENGINE
 
 #include <linux/device.h>
