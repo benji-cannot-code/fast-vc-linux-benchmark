16a832abd5fea8ebb6b3441fdd0688e5adfeadb9
Len Brown <len.brown@intel.com>
2006-06-26 23:04:31 -0400

    ACPI: un-export ACPI_WARNING() -- use printk(KERN_WARNING...)
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/pci_irq.c
+++ drivers/acpi/pci_irq.c
@@ -270,8 +270,8 @@ acpi_pci_allocate_irq(struct acpi_prt_entry *entry,
 						 entry->link.index, triggering,
 						 polarity, link);
 		if (irq < 0) {
-			ACPI_WARNING((AE_INFO,
-				      "Invalid IRQ link routing entry"));
+			printk(KERN_WARNING PREFIX
+				      "Invalid IRQ link routing entry\n");
 			return_VALUE(-1);
 		}
 	} else {
@@ -380,8 +380,8 @@ acpi_pci_irq_derive(struct pci_dev *dev,
 	}
 
 	if (irq < 0) {
-		ACPI_WARNING((AE_INFO, "Unable to derive IRQ for device %s",
-			      pci_name(dev)));
+		printk(KERN_WARNING PREFIX "Unable to derive IRQ for device %s\n",
+			      pci_name(dev));
 		return_VALUE(-1);
 	}
 
