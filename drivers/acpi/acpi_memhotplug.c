e3b681488643f646197af4448d74dafdcf3afcd1
Yasunori Goto <y-goto@jp.fujitsu.com>
2006-06-27 02:53:28 -0700

    [PATCH] Catch notification of memory add event of ACPI via container driver. (register start func for memory device)
    This is a patch to call add_memroy() when notify reaches for new node's add
    event.
    
    When new node is added, notify of ACPI reaches container device which means
    the node.
    
    Container device driver calls acpi_bus_scan() to find and add belonging
    devices (which means cpu, memory and so on).  Its function calls add and
    start function of belonging devices's driver.
    
    Howevever, current memory hotplug driver just register add function to
    create sysfs file for its memory.  But, acpi_memory_enable_device() is not
    called because it is considered just the case that notify reaches memory
    device directly.  So, if notify reaches container device nothing can call
    add_memory().
    
    This is a patch to create start function which calls add_memory().
    add_memory() can be called by this when notify reaches container device.
    
    [akpm@osdl.org: coding cleanups]
    Signed-off-by: Yasunori Goto <y-goto@jp.fujitsu.com>
    Cc: "Brown, Len" <len.brown@intel.com>
    Cc: Dave Hansen <haveblue@us.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/acpi/acpi_memhotplug.c
+++ drivers/acpi/acpi_memhotplug.c
@@ -58,6 +58,7 @@ MODULE_LICENSE("GPL");
 
 static int acpi_memory_device_add(struct acpi_device *device);
 static int acpi_memory_device_remove(struct acpi_device *device, int type);
+static int acpi_memory_device_start(struct acpi_device *device);
 
 static struct acpi_driver acpi_memory_device_driver = {
 	.name = ACPI_MEMORY_DEVICE_DRIVER_NAME,
@@ -66,6 +67,7 @@ static struct acpi_driver acpi_memory_device_driver = {
 	.ops = {
 		.add = acpi_memory_device_add,
 		.remove = acpi_memory_device_remove,
+		.start = acpi_memory_device_start,
 		},
 };
 
@@ -432,6 +434,25 @@ static int acpi_memory_device_remove(struct acpi_device *device, int type)
 	kfree(mem_device);
 
 	return_VALUE(0);
+}
+
+static int acpi_memory_device_start (struct acpi_device *device)
+{
+	struct acpi_memory_device *mem_device;
+	int result = 0;
+
+	ACPI_FUNCTION_TRACE("acpi_memory_device_start");
+
+	mem_device = acpi_driver_data(device);
+
+	if (!acpi_memory_check_device(mem_device)) {
+		/* call add_memory func */
+		result = acpi_memory_enable_device(mem_device);
+		if (result)
+			ACPI_DEBUG_PRINT((ACPI_DB_ERROR,
+				"Error in acpi_memory_enable_device\n"));
+	}
+	return_VALUE(result);
 }
 
 /*
