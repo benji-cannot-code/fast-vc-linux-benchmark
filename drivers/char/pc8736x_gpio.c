6d11a0f21b674a8959f291fac115f2c9c7a67524
Jim Cromie <jim.cromie@gmail.com>
2006-06-27 02:54:25 -0700

    [PATCH] gpio-patchset-fixups: include linux/io.h
    Hmm.  Im somewhat ambivalent about this patch, since with it, driver wont
    build for vanilla 17 or older.
    
    Its also only 1/2 of your suggestion - when I tried it, I was building against
    vanilla 17, and asm/uaccess.h cause compilation failure.  Looking back, Im
    perplexed as to why linux/io.h didnt cause same failure ?!?
    
    use linux/io.h rather than asm/io.h
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/pc8736x_gpio.c
+++ drivers/char/pc8736x_gpio.c
@@ -16,12 +16,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/io.h>
 #include <linux/ioport.h>
 #include <linux/mutex.h>
 #include <linux/nsc_gpio.h>
 #include <linux/platform_device.h>
 #include <asm/uaccess.h>
-#include <asm/io.h>
 
 #define DEVNAME "pc8736x_gpio"
 
