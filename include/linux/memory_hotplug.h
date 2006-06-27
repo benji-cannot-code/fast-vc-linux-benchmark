caee282a6bd8d712b0786da14d87252dab302063
Yasunori Goto <y-goto@jp.fujitsu.com>
2006-06-27 02:53:33 -0700

    [PATCH] pgdat allocation for new node add (refresh node_data[])
    Refresh NODE_DATA() for generic archs.  In this case, NODE_DATA(nid) ==
    node_data[nid].  node_data[] is array of address of pgdat.  So, refresh is
    quite simple.
    
    Signed-off-by: Yasunori Goto <y-goto@jp.fujitsu.com>
    Signed-off-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Dave Hansen <haveblue@us.ibm.com>
    Cc: "Brown, Len" <len.brown@intel.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/memory_hotplug.h
+++ include/linux/memory_hotplug.h
@@ -92,6 +92,9 @@ static inline pg_data_t *arch_alloc_nodedata(int nid)
 static inline void arch_free_nodedata(pg_data_t *pgdat)
 {
 }
+static inline void arch_refresh_nodedata(int nid, pg_data_t *pgdat)
+{
+}
 
 #else /* CONFIG_HAVE_ARCH_NODEDATA_EXTENSION */
 
@@ -115,6 +118,12 @@ static inline void arch_free_nodedata(pg_data_t *pgdat)
  */
 #define generic_free_nodedata(pgdat)	kfree(pgdat)
 
+extern pg_data_t *node_data[];
+static inline void arch_refresh_nodedata(int nid, pg_data_t *pgdat)
+{
+	node_data[nid] = pgdat;
+}
+
 #else /* !CONFIG_NUMA */
 
 /* never called */
@@ -124,6 +133,9 @@ static inline pg_data_t *generic_alloc_nodedata(int nid)
 	return NULL;
 }
 static inline void generic_free_nodedata(pg_data_t *pgdat)
+{
+}
+static inline void arch_refresh_nodedata(int nid, pg_data_t *pgdat)
 {
 }
 #endif /* CONFIG_NUMA */
