ad71b800bdb519e4d3df66f1bda820e573db00d4
Mike Christie <michaelc@cs.wisc.edu>
2006-06-28 12:00:27 -0500

    [SCSI] iscsi: fix session refcouting
    iscsi_tcp and iser cannot be rmmod from the kernel when sessions
    are running because session removal is driven from userspace. For
    those modules we get a module reference when a session is
    created then drop it when the session is removed.
    
    For qla4xxx, they can jsut remove the sessions from the pci remove
    function like normal HW drivers, so this patch moves the module
    reference from the transport class functions shared by all
    drivers to the libiscsi functions only used be software iscsi
    modules.
    
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/scsi_transport_iscsi.c
+++ drivers/scsi/scsi_transport_iscsi.c
@@ -229,13 +229,11 @@ static struct iscsi_cls_conn *iscsi_conn_lookup(uint32_t sid, uint32_t cid)
 static void iscsi_session_release(struct device *dev)
 {
 	struct iscsi_cls_session *session = iscsi_dev_to_session(dev);
-	struct iscsi_transport *transport = session->transport;
 	struct Scsi_Host *shost;
 
 	shost = iscsi_session_to_shost(session);
 	scsi_host_put(shost);
 	kfree(session);
-	module_put(transport->owner);
 }
 
 static int iscsi_is_session_dev(const struct device *dev)
@@ -306,13 +304,11 @@ iscsi_create_session(struct Scsi_Host *shost,
 	struct iscsi_cls_session *session;
 	int err;
 
-	if (!try_module_get(transport->owner))
-		return NULL;
-
 	session = kzalloc(sizeof(*session) + transport->sessiondata_size,
 			  GFP_KERNEL);
 	if (!session)
-		goto module_put;
+		return NULL;
+
 	session->transport = transport;
 	session->recovery_tmo = 120;
 	INIT_WORK(&session->recovery_work, session_recovery_timedout, session);
@@ -350,8 +346,6 @@ iscsi_create_session(struct Scsi_Host *shost,
 
 free_session:
 	kfree(session);
-module_put:
-	module_put(transport->owner);
 	return NULL;
 }
 
