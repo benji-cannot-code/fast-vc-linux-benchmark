e2370f9dc3d32f7ee0b136ce949732d266507194
Milan Svoboda <msvoboda@ra.rockwell.com>
2006-06-29 12:40:00 -0700

    [PATCH] usb gadget: fixup pxa2xx_udc to include asm/arch/udc.h again
    This fixes pxa2xx_udc.c to include asm/arch/udc.h again to fix current
    build breakage.
    
    Signed-off-by: Milan Svoboda <msvoboda@ra.rockwell.com>
    [ forwarded by David Brownell <david-b@pacbell.net> ]
    [ fixed to apply properly by Linus ]
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/usb/gadget/pxa2xx_udc.c
+++ drivers/usb/gadget/pxa2xx_udc.c
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb_ch9.h>
 #include <linux/usb_gadget.h>
 
-#include <asm/arch/hardware/intel_udc.h>
+#include <asm/arch/udc.h>
 
 
 /*
