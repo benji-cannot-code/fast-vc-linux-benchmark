20f9351c8b549681b1e57c65b4742400979bf917
Trent Piepho <xyzzy@speakeasy.org>
2006-06-29 13:16:04 -0300

    V4L/DVB (4275): The FE_SET_FRONTEND_TUNE_MODE ioctl always returns EOPNOTSUPP
    When someone added the front-end ioctl FE_SET_FRONTEND_TUNE_MODE, they
    forgot to set the return value to 0.  It always returns EOPNOTSUPP,
    causing problems for programmers who actually check for error conditions.
    
    Signed-off-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Andrew de Quincey <adq_dvb@lidskialf.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-core/dvb_frontend.c
+++ drivers/media/dvb/dvb-core/dvb_frontend.c
@@ -976,6 +976,7 @@ static int dvb_frontend_ioctl(struct inode *inode, struct file *file,
 
 	case FE_SET_FRONTEND_TUNE_MODE:
 		fepriv->tune_mode_flags = (unsigned long) parg;
+		err = 0;
 		break;
 	};
 
