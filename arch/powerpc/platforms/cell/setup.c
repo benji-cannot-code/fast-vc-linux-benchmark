37dcdba16c2bb20490b7bb3d635b74d4e42975b0
Dave Jones <davej@redhat.com>
2006-06-29 16:52:53 -0400

    [POWERPC] fix implicit declaration on cell.
    (Only fails with -Werror-implicit-function-declaration, but
     it should still be fixed).
    
    arch/powerpc/platforms/cell/setup.c:145: error: implicit declaration of function 'udbg_init_rtas_console'
    
    Signed-off-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/cell/setup.c
+++ arch/powerpc/platforms/cell/setup.c
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <asm/spu.h>
 #include <asm/spu_priv1.h>
+#include <asm/udbg.h>
 
 #include "interrupt.h"
 #include "iommu.h"
