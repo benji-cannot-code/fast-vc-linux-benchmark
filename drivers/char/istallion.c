846a51da7ee0686fcf518fc30e91902257114df5
Adrian Bunk <bunk@stusta.de>
2006-06-30 01:55:30 -0700

    [PATCH] fix ISTALLION=y
    drivers/char/istallion.c: In function âstli_initbrdsâ:
    drivers/char/istallion.c:4150: error: implicit declaration of function âstli_parsebrdâ
    drivers/char/istallion.c:4150: error: âstli_brdspâ undeclared (first use in this function)
    drivers/char/istallion.c:4150: error: (Each undeclared identifier is reported only once
    drivers/char/istallion.c:4150: error: for each function it appears in.)
    drivers/char/istallion.c:4164: error: implicit declaration of function âstli_argbrdsâ
    
    While I was at it, I also removed the #ifdef MODULE around the initialation
    code to allow it to perhaps work when built into the kernel and made a
    needlessly global function static.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/istallion.c
+++ drivers/char/istallion.c
@@ -283,7 +283,6 @@ static char	*stli_brdnames[] = {
 
 /*****************************************************************************/
 
-#ifdef MODULE
 /*
  *	Define some string labels for arguments passed from the module
  *	load line. These allow for easy board definitions, and easy
@@ -381,8 +380,6 @@ module_param_array(board2, charp, NULL, 0);
 MODULE_PARM_DESC(board2, "Board 2 config -> name[,ioaddr[,memaddr]");
 module_param_array(board3, charp, NULL, 0);
 MODULE_PARM_DESC(board3, "Board 3 config -> name[,ioaddr[,memaddr]");
-
-#endif
 
 /*
  *	Set up a default memory address table for EISA board probing.
@@ -644,14 +641,8 @@ static unsigned int	stli_baudrates[] = {
  *	Prototype all functions in this driver!
  */
 
-#ifdef MODULE
-static void	stli_argbrds(void);
 static int	stli_parsebrd(stlconf_t *confp, char **argp);
-
-static unsigned long	stli_atol(char *str);
-#endif
-
-int		stli_init(void);
+static int	stli_init(void);
 static int	stli_open(struct tty_struct *tty, struct file *filp);
 static void	stli_close(struct tty_struct *tty, struct file *filp);
 static int	stli_write(struct tty_struct *tty, const unsigned char *buf, int count);
@@ -786,8 +777,6 @@ static int	stli_timeron;
 /*****************************************************************************/
 
 static struct class *istallion_class;
-
-#ifdef MODULE
 
 /*
  *	Loadable module initialization stuff.
@@ -954,8 +943,6 @@ static int stli_parsebrd(stlconf_t *confp, char **argp)
 		confp->memaddr = stli_atol(argp[2]);
 	return(1);
 }
-
-#endif
 
 /*****************************************************************************/
 
@@ -4695,7 +4682,7 @@ static struct tty_operations stli_ops = {
 
 /*****************************************************************************/
 
-int __init stli_init(void)
+static int __init stli_init(void)
 {
 	int i;
 	printk(KERN_INFO "%s: version %s\n", stli_drvtitle, stli_drvversion);
