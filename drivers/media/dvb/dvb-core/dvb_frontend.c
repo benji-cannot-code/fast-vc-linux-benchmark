4f61008ad728e2bed91f05ce038511ca2c934122
Manu Abraham <abraham.manu@gmail.com>
2006-06-29 22:05:23 -0300

    V4L/DVB (4282): Fix: use swzigzag for swalgo
    Signed-off-by: Manu Abraham <manu@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-core/dvb_frontend.c
+++ drivers/media/dvb/dvb-core/dvb_frontend.c
@@ -571,7 +571,8 @@ static int dvb_frontend_thread(void *data)
 					dvb_frontend_add_event(fe, s);
 					fepriv->status = s;
 				}
-			}
+			} else
+				dvb_frontend_swzigzag(fe);
 		} else
 			dvb_frontend_swzigzag(fe);
 	}
