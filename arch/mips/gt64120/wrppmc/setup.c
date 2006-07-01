bbe8fc114caab3f436236da5862c5481af80b73f
Ralf Baechle <ralf@linux-mips.org>
2006-07-01 22:07:23 +0100

    [MIPS] Don't include obsolete <linux/config.h>.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/gt64120/wrppmc/setup.c
+++ arch/mips/gt64120/wrppmc/setup.c
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Copyright (C) 1996, 1997, 2004 by Ralf Baechle (ralf@linux-mips.org)
  * Copyright (C) 2006, Wind River System Inc. Rongkai.zhan <rongkai.zhan@windriver.com>
  */
-#include <linux/config.h>
 #include <linux/init.h>
 #include <linux/string.h>
 #include <linux/kernel.h>
