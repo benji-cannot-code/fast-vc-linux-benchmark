9618a23a0dc49d888a533d21136458cda9b1f07a
Lennert Buytenhek <buytenh@wantstofly.org>
2006-07-01 19:58:20 +0100

    [ARM] 3705/1: add supersection support to ioremap()
    Patch from Lennert Buytenhek
    
    Analogous to the previous patch that allows ioremap() to use section
    mappings, this patch allows ioremap() to use supersection mappings.
    Original patch by Deepak Saxena.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/memory.h
+++ include/asm-arm/memory.h
@@ -70,9 +70,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define XIP_VIRT_ADDR(physaddr)  (MODULE_START + ((physaddr) & 0x000fffff))
 
 /*
- * Allow 2MB-aligned ioremap pages
+ * Allow 16MB-aligned ioremap pages
  */
-#define IOREMAP_MAX_ORDER	21
+#define IOREMAP_MAX_ORDER	24
 
 #else /* CONFIG_MMU */
 
