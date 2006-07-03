ea5b180a261e44023d56e4486189acaaf1120f5e
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2006-07-03 17:22:05 +1000

    [POWERPC] Fix booting on Momentum "Apache" board (a Maple derivative)
    This extends the maple device-tree workarounds to work on the
    Apache board as well, and extends the maple platform probing code
    to recognize the Apache board.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/maple/setup.c
+++ arch/powerpc/platforms/maple/setup.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
-#define DEBUG
+#undef DEBUG
 
 #include <linux/init.h>
 #include <linux/errno.h>
@@ -257,7 +257,9 @@ static void __init maple_progress(char *s, unsigned short hex)
 static int __init maple_probe(void)
 {
 	unsigned long root = of_get_flat_dt_root();
-	if (!of_flat_dt_is_compatible(root, "Momentum,Maple"))
+
+	if (!of_flat_dt_is_compatible(root, "Momentum,Maple") &&
+	    !of_flat_dt_is_compatible(root, "Momentum,Apache"))
 		return 0;
 	/*
 	 * On U3, the DART (iommu) must be allocated now since it
