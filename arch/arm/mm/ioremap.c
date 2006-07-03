ee44faca44c9a5499da2c2a805a529e39b9e4f08
Lennert Buytenhek <buytenh@wantstofly.org>
2006-07-03 12:26:02 +0100

    [ARM] 3708/2: fix SMP build after section ioremap changes
    Patch from Lennert Buytenhek
    
    Commit ff0daca525dde796382b9ccd563f169df2571211 broke the SMP build,
    this patch fixes it up again.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/ioremap.c
+++ arch/arm/mm/ioremap.c
@@ -370,6 +370,7 @@ void __iounmap(void __iomem *addr)
 
 	addr = (void __iomem *)(PAGE_MASK & (unsigned long)addr);
 
+#ifndef CONFIG_SMP
 	/*
 	 * If this is a section based mapping we need to handle it
 	 * specially as the VM subysystem does not know how to handle
@@ -391,6 +392,7 @@ void __iounmap(void __iomem *addr)
 		}
 	}
 	write_unlock(&vmlist_lock);
+#endif
 
 	if (!section_mapping)
 		vunmap(addr);
