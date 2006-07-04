099f7c8f11509278079d437313e73820ed47e539
Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
2006-07-04 22:59:41 +0900

    [MIPS] VR41xx: Set VR41_CONF_BP only for PrId 0x0c80.
    Signed-off-by: Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mipsregs.h
+++ include/asm-mips/mipsregs.h
@@ -471,6 +471,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Bits specific to the VR41xx.  */
 #define VR41_CONF_CS		(_ULCAST_(1) << 12)
+#define VR41_CONF_BP		(_ULCAST_(1) << 16)
 #define VR41_CONF_M16		(_ULCAST_(1) << 20)
 #define VR41_CONF_AD		(_ULCAST_(1) << 23)
 
