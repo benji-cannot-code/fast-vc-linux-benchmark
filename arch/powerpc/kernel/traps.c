0ca3f3dd66d9523926358af37df80e91b0fcef6c
Michael Ellerman <michael@ellerman.id.au>
2006-07-05 14:39:43 +1000

    [POWERPC] Make crash.c work on 32-bit and 64-bit
    To compile kexec on 32-bit we need a few more bits and pieces. Rather
    than add empty definitions, we can make crash.c work on 32-bit, with
    only a couple of kludges.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/traps.c
+++ arch/powerpc/kernel/traps.c
@@ -56,9 +56,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef CONFIG_PPC64	/* XXX */
 #define _IO_BASE	pci_io_base
-#ifdef CONFIG_KEXEC
-cpumask_t cpus_in_sr = CPU_MASK_NONE;
-#endif
 #endif
 
 #ifdef CONFIG_DEBUGGER
