d8f1b9d8929048e16d67de86addf984b99e3565c
Ralf Baechle <ralf@linux-mips.org>
2006-07-08 11:25:38 +0100

    [MIPS] MIPSsim: Delete redeclaration of ll_local_timer_interrupt.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/sim/sim_time.c
+++ arch/mips/mips-boards/sim/sim_time.c
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 unsigned long cpu_khz;
 
-extern asmlinkage void ll_local_timer_interrupt(int irq, struct pt_regs *regs);
-
 irqreturn_t sim_timer_interrupt(int irq, void *dev_id, struct pt_regs *regs)
 {
 #ifdef CONFIG_SMP
