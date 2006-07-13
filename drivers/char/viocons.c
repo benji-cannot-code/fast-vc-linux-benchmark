7e5bc8742302c71fa5649e9ebcf645c8bcd4df6e
Stephen Rothwell <sfr@canb.auug.org.au>
2006-07-13 18:51:22 +1000

    [POWERPC] iseries: A new iSeries console
    This driver uses the hvc_console.c infrastructure that is used by the
    pSeries virtual and RTAS consoles.  This will allow us to make viocons.c
    obsolete and is another step along the way to a combined kernel (as
    viocons could not coexist with CONFIG_VT).
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- drivers/char/viocons.c
+++ drivers/char/viocons.c
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysrq.h>
 
 #include <asm/iseries/vio.h>
-
 #include <asm/iseries/hv_lp_event.h>
 #include <asm/iseries/hv_call_event.h>
 #include <asm/iseries/hv_lp_config.h>
@@ -67,35 +66,6 @@ static DEFINE_SPINLOCK(consoleloglock);
 static int vio_sysrq_pressed;
 extern int sysrq_enabled;
 #endif
-
-/*
- * The structure of the events that flow between us and OS/400.  You can't
- * mess with this unless the OS/400 side changes too
- */
-struct viocharlpevent {
-	struct HvLpEvent event;
-	u32 reserved;
-	u16 version;
-	u16 subtype_result_code;
-	u8 virtual_device;
-	u8 len;
-	u8 data[VIOCHAR_MAX_DATA];
-};
-
-#define VIOCHAR_WINDOW		10
-#define VIOCHAR_HIGHWATERMARK	3
-
-enum viocharsubtype {
-	viocharopen = 0x0001,
-	viocharclose = 0x0002,
-	viochardata = 0x0003,
-	viocharack = 0x0004,
-	viocharconfig = 0x0005
-};
-
-enum viochar_rc {
-	viochar_rc_ebusy = 1
-};
 
 #define VIOCHAR_NUM_BUF		16
 
@@ -1184,6 +1154,7 @@ static int __init viocons_init(void)
 		port_info[i].magic = VIOTTY_MAGIC;
 	}
 	HvCall_setLogBufferFormatAndCodepage(HvCall_LogBuffer_ASCII, 437);
+	add_preferred_console("viocons", 0, NULL);
 	register_console(&viocons_early);
 	return 0;
 }
