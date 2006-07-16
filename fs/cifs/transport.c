4e646b6035de9299b90211f07a2a10cc3943edbb
Steve French <sfrench@us.ibm.com>
2006-07-16 21:33:15 +0000

    [CIFS] Make midState usage more consistent
    Although harmless, we were sometimes treating midState like it contained
    flags but they are exclusive states, and this makes that more clear.
    
    Signed-off-by: Jeremy Allison <jra@samba.org>
    Signed-off-by: Steve French <sfrench@us.ibm.com>
    (cherry picked from 586c057c3a68dd6ae0f3ba94fbf76798b1558074 commit)

--- fs/cifs/transport.c
+++ fs/cifs/transport.c
@@ -449,13 +449,13 @@ SendReceive2(const unsigned int xid, struct cifsSesInfo *ses,
 		for (;;) {
 			curr_timeout = timeout + jiffies;
 			wait_event(ses->server->response_q,
-				(!(midQ->midState & MID_REQUEST_SUBMITTED)) || 
+				(!(midQ->midState == MID_REQUEST_SUBMITTED)) || 
 				time_after(jiffies, curr_timeout) || 
 				((ses->server->tcpStatus != CifsGood) &&
 				 (ses->server->tcpStatus != CifsNew)));
 
 			if (time_after(jiffies, curr_timeout) &&
-				(midQ->midState & MID_REQUEST_SUBMITTED) &&
+				(midQ->midState == MID_REQUEST_SUBMITTED) &&
 				((ses->server->tcpStatus == CifsGood) ||
 				 (ses->server->tcpStatus == CifsNew))) {
 
@@ -486,7 +486,7 @@ SendReceive2(const unsigned int xid, struct cifsSesInfo *ses,
 		}
 	} else {
 		wait_event(ses->server->response_q,
-			(!(midQ->midState & MID_REQUEST_SUBMITTED)) || 
+			(!(midQ->midState == MID_REQUEST_SUBMITTED)) || 
 			((ses->server->tcpStatus != CifsGood) &&
 			 (ses->server->tcpStatus != CifsNew)));
 	}
@@ -750,13 +750,13 @@ SendReceive(const unsigned int xid, struct cifsSesInfo *ses,
 		for (;;) {
 			curr_timeout = timeout + jiffies;
 			wait_event(ses->server->response_q,
-				(!(midQ->midState & MID_REQUEST_SUBMITTED)) || 
+				(!(midQ->midState == MID_REQUEST_SUBMITTED)) || 
 				time_after(jiffies, curr_timeout) || 
 				((ses->server->tcpStatus != CifsGood) &&
 				 (ses->server->tcpStatus != CifsNew)));
 
 			if (time_after(jiffies, curr_timeout) &&
-				(midQ->midState & MID_REQUEST_SUBMITTED) &&
+				(midQ->midState == MID_REQUEST_SUBMITTED) &&
 				((ses->server->tcpStatus == CifsGood) ||
 				 (ses->server->tcpStatus == CifsNew))) {
 
@@ -781,7 +781,7 @@ SendReceive(const unsigned int xid, struct cifsSesInfo *ses,
 		}
 	} else {
 		wait_event(ses->server->response_q,
-			(!(midQ->midState & MID_REQUEST_SUBMITTED)) || 
+			(!(midQ->midState == MID_REQUEST_SUBMITTED)) || 
 			((ses->server->tcpStatus != CifsGood) &&
 			 (ses->server->tcpStatus != CifsNew)));
 	}
