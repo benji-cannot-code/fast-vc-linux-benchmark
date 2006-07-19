18fa9367ce8f161183f435a50d7b54e1f40f1343
Wim Van Sebroeck <wim@iguana.be>
2006-07-19 22:39:13 +0200

    [WATCHDOG] iTCO_wdt.c - pci_dev_put fix
    for_each_pci_dev calls pci_get_device (and thus
    it calls pci_dev_get). So we need to do a pci_dev_put
    to keep the refcounting correct.
    (Thanks to Jiri Slaby <jirislaby@gmail.com>)
    
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/char/watchdog/iTCO_wdt.c
+++ drivers/char/watchdog/iTCO_wdt.c
@@ -46,7 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Module and version information */
 #define DRV_NAME        "iTCO_wdt"
 #define DRV_VERSION     "1.00"
-#define DRV_RELDATE     "18-Jun-2006"
+#define DRV_RELDATE     "19-Jul-2006"
 #define PFX		DRV_NAME ": "
 
 /* Includes */
@@ -559,6 +559,7 @@ static int iTCO_wdt_init(struct pci_dev *pdev, const struct pci_device_id *ent, 
 	if (base_address == 0x00000000) {
 		/* Something's wrong here, ACPIBASE has to be set */
 		printk(KERN_ERR PFX "failed to get TCOBASE address\n");
+		pci_dev_put(pdev);
 		return -ENODEV;
 	}
 	iTCO_wdt_private.iTCO_version = iTCO_chipset_info[ent->driver_data].iTCO_version;
@@ -650,6 +651,7 @@ out:
 	if (iTCO_wdt_private.iTCO_version == 2)
 		iounmap(iTCO_wdt_private.gcs);
 	iTCO_wdt_private.ACPIBASE = 0;
+	pci_dev_put(iTCO_wdt_private.pdev);
 	return ret;
 }
 
@@ -665,6 +667,7 @@ static void iTCO_wdt_cleanup(void)
 	release_region(TCOBASE, 0x20);
 	if (iTCO_wdt_private.iTCO_version == 2)
 		iounmap(iTCO_wdt_private.gcs);
+	pci_dev_put(iTCO_wdt_private.pdev);
 }
 
 static int iTCO_wdt_probe(struct platform_device *dev)
