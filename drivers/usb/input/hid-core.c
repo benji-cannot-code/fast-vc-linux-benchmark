4c95ce71ef3247e273908b96ad4aa8d1114a4b2a
Anssi Hannula <anssi.hannula@gmail.com>
2006-07-19 01:40:55 -0400

    Input: use new FF interface in the HID force feedback drivers
    Signed-off-by: Anssi Hannula <anssi.hannula@gmail.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/usb/input/hid-core.c
+++ drivers/usb/input/hid-core.c
@@ -544,8 +544,6 @@ static void hid_free_device(struct hid_device *device)
 {
 	unsigned i,j;
 
-	hid_ff_exit(device);
-
 	for (i = 0; i < HID_REPORT_TYPES; i++) {
 		struct hid_report_enum *report_enum = device->report_enum + i;
 
