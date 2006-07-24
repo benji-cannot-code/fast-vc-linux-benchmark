4f29a11642fbd49d491d8019815cee1b43887e8d
Christoph Hellwig <hch@lst.de>
2006-07-24 15:31:14 -0700

    [NET]: Correct dev_alloc_skb kerneldoc
    dev_alloc_skb is designated for RX descriptors, not TX.  (Some drivers
    use it for the latter anyway, but that's a different story)
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1068,7 +1068,7 @@ static inline void __skb_queue_purge(struct sk_buff_head *list)
 }
 
 /**
- *	__dev_alloc_skb - allocate an skbuff for sending
+ *	__dev_alloc_skb - allocate an skbuff for receiving
  *	@length: length to allocate
  *	@gfp_mask: get_free_pages mask, passed to alloc_skb
  *
@@ -1089,7 +1089,7 @@ static inline struct sk_buff *__dev_alloc_skb(unsigned int length,
 }
 
 /**
- *	dev_alloc_skb - allocate an skbuff for sending
+ *	dev_alloc_skb - allocate an skbuff for receiving
  *	@length: length to allocate
  *
  *	Allocate a new &sk_buff and assign it a usage count of one. The
