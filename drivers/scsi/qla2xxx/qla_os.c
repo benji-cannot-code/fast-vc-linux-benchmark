717c0f93f5e7ca6e94668a0ec9806306e57c6f1e
Vladislav Bolkhovitin <vst@vlnb.net>
2006-08-01 13:48:15 -0700

    [SCSI] qla2xxx: Fix to allow to reset devices using sg interface (sg_reset).
    Currently it is impossible to reset provided by Qlogic QLA2xxx driver
    SCSI devices externally using corresponding sg devices, particularly via
    sg_reset utility, because qla2xxx driver in qla2xxx_eh_device_reset()
    function checks if the input scsi_cmnd has its private data (CMD_SP())
    attached. Then the found pointer isn't used anywhere inside of
    qla2xxx_eh_device_reset(). If the RESET request comes from sg device, it
    doesn't have such private data.
    
    The attached patch removes check for non-NULL CMD_SP() from
    qla2xxx_eh_device_reset(), hence allows to reset QLA2xxx's devices using
    corresponding sg devices.
    
    AV: change applies to bus/host reset handlers as well.
    
    Signed-off-by: Vladislav Bolkhovitin <vst@vlnb.net>
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -745,7 +745,6 @@ qla2xxx_eh_device_reset(struct scsi_cmnd *cmd)
 {
 	scsi_qla_host_t *ha = to_qla_host(cmd->device->host);
 	fc_port_t *fcport = (struct fc_port *) cmd->device->hostdata;
-	srb_t *sp;
 	int ret;
 	unsigned int id, lun;
 	unsigned long serial;
@@ -756,8 +755,7 @@ qla2xxx_eh_device_reset(struct scsi_cmnd *cmd)
 	lun = cmd->device->lun;
 	serial = cmd->serial_number;
 
-	sp = (srb_t *) CMD_SP(cmd);
-	if (!sp || !fcport)
+	if (!fcport)
 		return ret;
 
 	qla_printk(KERN_INFO, ha,
@@ -876,7 +874,6 @@ qla2xxx_eh_bus_reset(struct scsi_cmnd *cmd)
 {
 	scsi_qla_host_t *ha = to_qla_host(cmd->device->host);
 	fc_port_t *fcport = (struct fc_port *) cmd->device->hostdata;
-	srb_t *sp;
 	int ret;
 	unsigned int id, lun;
 	unsigned long serial;
@@ -887,8 +884,7 @@ qla2xxx_eh_bus_reset(struct scsi_cmnd *cmd)
 	lun = cmd->device->lun;
 	serial = cmd->serial_number;
 
-	sp = (srb_t *) CMD_SP(cmd);
-	if (!sp || !fcport)
+	if (!fcport)
 		return ret;
 
 	qla_printk(KERN_INFO, ha,
@@ -937,7 +933,6 @@ qla2xxx_eh_host_reset(struct scsi_cmnd *cmd)
 {
 	scsi_qla_host_t *ha = to_qla_host(cmd->device->host);
 	fc_port_t *fcport = (struct fc_port *) cmd->device->hostdata;
-	srb_t *sp;
 	int ret;
 	unsigned int id, lun;
 	unsigned long serial;
@@ -948,8 +943,7 @@ qla2xxx_eh_host_reset(struct scsi_cmnd *cmd)
 	lun = cmd->device->lun;
 	serial = cmd->serial_number;
 
-	sp = (srb_t *) CMD_SP(cmd);
-	if (!sp || !fcport)
+	if (!fcport)
 		return ret;
 
 	qla_printk(KERN_INFO, ha,
