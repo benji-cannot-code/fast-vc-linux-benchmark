a11ad764d6b03d9818ed5604e20b8bb86721509d
Ulrich Kunitz <kune@deine-taler.de>
2006-08-01 23:43:35 +0200

    [PATCH] zd1211rw: Packet filter fix for managed (STA) mode
    I had problems with my AVM Fritz!Box access point. It appeared
    that the AP deauthorized me and the softmac didn't reconnect me.
    This patch handles the problem.
    
    Signed-off-by: Ulrich Kunitz <kune@deine-taler.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/zd1211rw/zd_chip.h
+++ drivers/net/wireless/zd1211rw/zd_chip.h
@@ -467,6 +467,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RX_FILTER_BEACON		0x0100
 #define RX_FILTER_DISASSOC		0x0400
 #define RX_FILTER_AUTH			0x0800
+#define AP_RX_FILTER			0x0400feff
+#define STA_RX_FILTER			0x0000ffff
+
 /* Monitor mode sets filter to 0xfffff */
 
 #define CR_ACK_TIMEOUT_EXT		CTL_REG(0x0690)
@@ -548,9 +551,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CR_ZD1211B_AIFS_CTL2		CTL_REG(0x0b14)
 #define CR_ZD1211B_TXOP			CTL_REG(0x0b20)
 #define CR_ZD1211B_RETRY_MAX		CTL_REG(0x0b28)
-
-#define AP_RX_FILTER			0x0400feff
-#define STA_RX_FILTER			0x0000ffff
 
 #define CWIN_SIZE			0x007f043f
 
