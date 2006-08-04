aacb087b495b550981fe655e5fefcd71119bb9b5
David Teigland <teigland@redhat.com>
2006-08-04 13:14:50 -0500

    [GFS2] lockproto api prefix
    Use the gfs2_ prefix on the register/unregister functions for the lock
    modules.  The gfs_ prefix was left from an old idea on how to share these
    with gfs1.
    
    Signed-off-by: David Teigland <teigland@redhat.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/locking/dlm/main.c
+++ fs/gfs2/locking/dlm/main.c
@@ -21,7 +21,7 @@ static int __init init_lock_dlm(void)
 {
 	int error;
 
-	error = gfs_register_lockproto(&gdlm_ops);
+	error = gfs2_register_lockproto(&gdlm_ops);
 	if (error) {
 		printk(KERN_WARNING "lock_dlm:  can't register protocol: %d\n",
 		       error);
@@ -30,14 +30,14 @@ static int __init init_lock_dlm(void)
 
 	error = gdlm_sysfs_init();
 	if (error) {
-		gfs_unregister_lockproto(&gdlm_ops);
+		gfs2_unregister_lockproto(&gdlm_ops);
 		return error;
 	}
 
 	error = gdlm_plock_init();
 	if (error) {
 		gdlm_sysfs_exit();
-		gfs_unregister_lockproto(&gdlm_ops);
+		gfs2_unregister_lockproto(&gdlm_ops);
 		return error;
 	}
 
@@ -53,7 +53,7 @@ static void __exit exit_lock_dlm(void)
 {
 	gdlm_plock_exit();
 	gdlm_sysfs_exit();
-	gfs_unregister_lockproto(&gdlm_ops);
+	gfs2_unregister_lockproto(&gdlm_ops);
 }
 
 module_init(init_lock_dlm);
