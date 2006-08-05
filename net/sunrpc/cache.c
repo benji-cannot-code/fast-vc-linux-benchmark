10747e9f461e1afed5b2dff530e123410327a759
Neil Brown <neilb@suse.de>
2006-08-05 12:14:29 -0700

    [PATCH] knfsd: fix race related problem when adding items to and svcrpc auth cache
    If we don't find the item we are lookng for, we allocate a new one, and
    then grab the lock again and search to see if it has been added while we
    did the alloc.  If it had been added we need to 'cache_put' the newly
    created item that we are never going to use.  But as it hasn't been
    initialised properly, putting it can cause an oops.
    
    So move the ->init call earlier to that it will always be fully initilised
    if we have to put it.
    
    Thanks to Philipp Matthias Hahn <pmhahn@svs.Informatik.Uni-Oldenburg.de>
    for reporting the problem.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- net/sunrpc/cache.c
+++ net/sunrpc/cache.c
@@ -72,7 +72,12 @@ struct cache_head *sunrpc_cache_lookup(struct cache_detail *detail,
 	new = detail->alloc();
 	if (!new)
 		return NULL;
+	/* must fully initialise 'new', else
+	 * we might get lose if we need to
+	 * cache_put it soon.
+	 */
 	cache_init(new);
+	detail->init(new, key);
 
 	write_lock(&detail->hash_lock);
 
@@ -86,7 +91,6 @@ struct cache_head *sunrpc_cache_lookup(struct cache_detail *detail,
 			return tmp;
 		}
 	}
-	detail->init(new, key);
 	new->next = *head;
 	*head = new;
 	detail->entries++;
