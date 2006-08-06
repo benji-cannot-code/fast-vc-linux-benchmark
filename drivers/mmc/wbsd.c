869ec70ae24ab7551e07c2dc921c9bc7f24dda69
Pierre Ossman <drzeus@drzeus.cx>
2006-08-07 01:40:04 +0200

    [MMC] Another stray 'io' reference
    Another misuse of the global 'io' variable instead of the local 'base'.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/mmc/wbsd.c
+++ drivers/mmc/wbsd.c
@@ -1774,7 +1774,7 @@ static int __devinit wbsd_init(struct device *dev, int base, int irq, int dma,
 	/*
 	 * Request resources.
 	 */
-	ret = wbsd_request_resources(host, io, irq, dma);
+	ret = wbsd_request_resources(host, base, irq, dma);
 	if (ret) {
 		wbsd_release_resources(host);
 		wbsd_free_mmc(dev);
@@ -1862,6 +1862,7 @@ static void __devexit wbsd_shutdown(struct device *dev, int pnp)
 
 static int __devinit wbsd_probe(struct platform_device *dev)
 {
+	/* Use the module parameters for resources */
 	return wbsd_init(&dev->dev, io, irq, dma, 0);
 }
 
