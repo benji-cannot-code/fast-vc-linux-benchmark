7c4f4953c78352bd32ad8f7518a3eae0c977642c
Herbert Xu <herbert@gondor.apana.org.au>
2006-08-06 23:12:59 +1000

    [CRYPTO] api: Added asynchronous flag
    This patch adds the asynchronous flag and changes all existing users to
    only look up algorithms that are synchronous.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CRYPTO_ALG_LARVAL		0x00000010
 #define CRYPTO_ALG_DEAD			0x00000020
 #define CRYPTO_ALG_DYING		0x00000040
+#define CRYPTO_ALG_ASYNC		0x00000080
 
 /*
  * Transform masks and values (for crt_flags).
