2568ffdaa66403136d88e1ce7a5da56fa730da45
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-08-08 09:10:01 -0300

    V4L/DVB (4348): Fix: compile for radio aimslab and aztech with V4L2 only
    All radio devices use an obsolete mode of opening/release driver.
    Since this is not V4L1 core, better to keep the method available for more
    time than to rewrite open/release without a radio device to test, since the
    newer method is much more complex than the previous one (although providing
    support for multiple opens and multiple devices).
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/radio/radio-aimslab.c
+++ drivers/media/radio/radio-aimslab.c
@@ -373,7 +373,7 @@ static struct video_device rtrack_radio=
 	.owner		= THIS_MODULE,
 	.name		= "RadioTrack radio",
 	.type		= VID_TYPE_TUNER,
-	.hardware	= VID_HARDWARE_RTRACK,
+	.hardware	= 0,
 	.fops           = &rtrack_fops,
 };
 
