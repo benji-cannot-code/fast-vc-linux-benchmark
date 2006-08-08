6f87f8d05514e74ee2a1bba7ec47b7ea8d0a1bef
Olivier DANET <odanet@caramail.com>
2006-08-08 15:48:09 -0300

    V4L/DVB: Adding support for MT2060 and thus for some DVB-USB-devices based on it
    - MT2060 tuner driver
    - Added support for some USB DVB-T devices based on Dib3000P
    
    Signed-off-by: Olivier DANET <odanet@caramail.com>
    Signed-off-by: Patrick Boettcher <pb@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dibusb.h
+++ drivers/media/dvb/dvb-usb/dibusb.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb-usb.h"
 
 #include "dib3000.h"
+#include "mt2060.h"
 
 /*
  * protocol of all dibusb related devices
@@ -95,8 +96,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DIBUSB_IOCTL_CMD_ENABLE_STREAM	0x01
 #define DIBUSB_IOCTL_CMD_DISABLE_STREAM	0x02
 
+/* Tuner types.
+   These constants shall correspond to the proper AGC values in the array
+   dib3000mc_agc_tuner[][12] in dib3000mc_priv.h */
+#define DIBUSB_TUNER_DEFAULT 1 // a.k.a Panasonic
+#define DIBUSB_TUNER_MT2060  4
+
+
 struct dibusb_state {
 	struct dib_fe_xfer_ops ops;
+	struct mt2060_state mt2060;
 
 	/* for RC5 remote control */
 	int old_toggle;
--- drivers/media/dvb/frontends/dib3000mc.c
+++ drivers/media/dvb/frontends/dib3000mc.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * see Documentation/dvb/README.dibusb for more information
  *
  */
+#include <linux/config.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -462,9 +463,8 @@ static int dib3000mc_set_frontend(struct dvb_frontend* fe,
 	int search_state,auto_val;
 	u16 val;
 
-	if (tuner && fe->ops.tuner_ops.set_params) { /* initial call from dvb */
-		fe->ops.tuner_ops.set_params(fe, fep);
-		if (fe->ops.i2c_gate_ctrl) fe->ops.i2c_gate_ctrl(fe, 0);
+	if (tuner && state->config.pll_set) { /* initial call from dvb */
+		state->config.pll_set(fe,fep);
 
 		state->last_tuned_freq = fep->frequency;
 	//	if (!scanboost) {
@@ -559,6 +559,7 @@ static int dib3000mc_set_frontend(struct dvb_frontend* fe,
 static int dib3000mc_fe_init(struct dvb_frontend* fe, int mobile_mode)
 {
 	struct dib3000_state *state = fe->demodulator_priv;
+	int AGCtuner=(int)fe->misc_priv;
 	deb_info("init start\n");
 
 	state->timing_offset = 0;
@@ -584,10 +585,11 @@ static int dib3000mc_fe_init(struct dvb_frontend* fe, int mobile_mode)
 	/* mobile mode - portable reception */
 	wr_foreach(dib3000mc_reg_mobile_mode,dib3000mc_mobile_mode[1]);
 
-/* TUNER_PANASONIC_ENV57H12D5: */
+/* TUNER_PANASONIC_ENV57H12D5 or TUNER_MICROTUNE_MT2060. Sets agc_tuner accordingly */
 	wr_foreach(dib3000mc_reg_agc_bandwidth,dib3000mc_agc_bandwidth);
 	wr_foreach(dib3000mc_reg_agc_bandwidth_general,dib3000mc_agc_bandwidth_general);
-	wr_foreach(dib3000mc_reg_agc,dib3000mc_agc_tuner[1]);
+	if (AGCtuner<0 || AGCtuner>=DIB3000MC_AGC_TUNER_COUNT) AGCtuner=1;
+	wr_foreach(dib3000mc_reg_agc,dib3000mc_agc_tuner[AGCtuner]);
 
 	wr(DIB3000MC_REG_UNK_110,DIB3000MC_UNK_110);
 	wr(26,0x6680);
@@ -642,6 +644,9 @@ static int dib3000mc_fe_init(struct dvb_frontend* fe, int mobile_mode)
 	set_and(DIB3000MC_REG_DIVERSITY3,DIB3000MC_DIVERSITY3_IN_OFF);
 
 	set_or(DIB3000MC_REG_CLK_CFG_7,DIB3000MC_CLK_CFG_7_DIV_IN_OFF);
+
+	if (state->config.pll_init)
+		state->config.pll_init(fe);
 
 	deb_info("init end\n");
 	return 0;
@@ -837,6 +842,7 @@ struct dvb_frontend* dib3000mc_attach(const struct dib3000_config* config,
 	/* setup the state */
 	state->i2c = i2c;
 	memcpy(&state->config,config,sizeof(struct dib3000_config));
+	memcpy(&state->ops, &dib3000mc_ops, sizeof(struct dvb_frontend_ops));
 
 	/* check for the correct demod */
 	if (rd(DIB3000_REG_MANUFACTOR_ID) != DIB3000_I2C_ID_DIBCOM)
@@ -856,7 +862,7 @@ struct dvb_frontend* dib3000mc_attach(const struct dib3000_config* config,
 	}
 
 	/* create dvb_frontend */
-	memcpy(&state->frontend.ops, &dib3000mc_ops, sizeof(struct dvb_frontend_ops));
+	state->frontend.ops = &state->ops;
 	state->frontend.demodulator_priv = state;
 
 	/* set the xfer operations */
@@ -873,7 +879,6 @@ error:
 	kfree(state);
 	return NULL;
 }
-EXPORT_SYMBOL(dib3000mc_attach);
 
 static struct dvb_frontend_ops dib3000mc_ops = {
 
@@ -912,3 +917,5 @@ static struct dvb_frontend_ops dib3000mc_ops = {
 MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 MODULE_LICENSE("GPL");
+
+EXPORT_SYMBOL(dib3000mc_attach);
