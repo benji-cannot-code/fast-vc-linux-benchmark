67ad40ed013ca6ec2065d345a8e0258eb59f9a3a
Andrew de Quincey <adq_dvb@lidskialf.net>
2006-08-08 09:10:08 -0300

    V4L/DVB (4383): Convert SEC drivers to new frontend API
    Convert SEC drivers to new frontend API
    
    Signed-off-by: Andrew de Quincey <adq_dvb@lidskialf.net>
    Acked-by: Michael Krufky <mkrufky@linuxtv.org>
    Acked-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/lnbp21.h
+++ drivers/media/dvb/frontends/lnbp21.h
@@ -41,6 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dvb/frontend.h>
 
 /* override_set and override_clear control which system register bits (above) to always set & clear */
-extern int lnbp21_attach(struct dvb_frontend *fe, struct i2c_adapter *i2c, u8 override_set, u8 override_clear);
+extern struct dvb_frontend *lnbp21_attach(struct dvb_frontend *fe, struct i2c_adapter *i2c, u8 override_set, u8 override_clear);
 
 #endif
