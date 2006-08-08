64b662a6ac912db0ea6ceb2f9c315d4ea3cc0128
Andrew Morton <akpm@osdl.org>
2006-08-08 10:52:22 -0300

    V4L/DVB (4484): Git-dvb: cadet build fix
    drivers/media/radio/radio-cadet.c: In function 'cadet_do_ioctl':
    drivers/media/radio/radio-cadet.c:362: warning: implicit declaration of function 'KERNEL_VERSION'
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/radio/radio-cadet.c
+++ drivers/media/radio/radio-cadet.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *		Changed API to V4L2
  */
 
+#include <linux/version.h>
 #include <linux/module.h>	/* Modules 			*/
 #include <linux/init.h>		/* Initdata			*/
 #include <linux/ioport.h>	/* request_region		*/
