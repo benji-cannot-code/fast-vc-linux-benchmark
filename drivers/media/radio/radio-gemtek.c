8938f433f6f341b07e483f5ff60ccc206839dfb3
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-08-08 09:10:00 -0300

    V4L/DVB (4339): Removed the remaining config.h stuff
    Since kernel include autoconf.h via command line, those config.h inclusion
    can be removed.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/radio/radio-gemtek.c
+++ drivers/media/radio/radio-gemtek.c
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>	/* copy to/from user		*/
 #include <linux/videodev.h>	/* kernel radio structs		*/
 #include <media/v4l2-common.h>
-#include <linux/config.h>	/* CONFIG_RADIO_GEMTEK_PORT 	*/
 #include <linux/spinlock.h>
 
 #ifndef CONFIG_RADIO_GEMTEK_PORT
