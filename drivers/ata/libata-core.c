225300af3a3673e4a029bdf0377a6bb6e2ad652c
Jeff Garzik <jeff@garzik.org>
2006-08-10 07:31:37 -0400

    Move libata to drivers/ata.

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/jiffies.h>
 #include <linux/scatterlist.h>
 #include <scsi/scsi.h>
-#include "scsi_priv.h"
 #include <scsi/scsi_cmnd.h>
 #include <scsi/scsi_host.h>
 #include <linux/libata.h>
--- drivers/ata/libata-eh.c
+++ drivers/ata/libata-eh.c
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_eh.h>
 #include <scsi/scsi_device.h>
 #include <scsi/scsi_cmnd.h>
-#include "scsi_transport_api.h"
+#include "../scsi/scsi_transport_api.h"
 
 #include <linux/libata.h>
 
