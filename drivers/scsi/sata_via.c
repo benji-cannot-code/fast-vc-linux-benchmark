6384f00c7d5bbefc44d5f14ed59535f5fa82becc
Pavel Roskin <proski@gnu.org>
2006-08-10 18:13:18 +0900

    [PATCH] libata: replace pci_module_init() with pci_register_driver()
    Replace pci_module_init() with pci_register_driver().
    
    Signed-off-by: Pavel Roskin <proski@gnu.org>
    Signed-off-by: Tejun Heo <htejun@gmail.com>

--- drivers/scsi/sata_via.c
+++ drivers/scsi/sata_via.c
@@ -382,7 +382,7 @@ err_out:
 
 static int __init svia_init(void)
 {
-	return pci_module_init(&svia_pci_driver);
+	return pci_register_driver(&svia_pci_driver);
 }
 
 static void __exit svia_exit(void)
