46ef6b0030795f235d625d570c2ed56fa4ec2c73
Alan Stern <stern@rowland.harvard.edu>
2006-08-11 16:52:39 -0400

    usbcore: khubd and busy-port handling
    We don't want khubd to start interfering in the device-resume process
    merely because the PORT_STATUS_C_SUSPEND feature happens to be set.
    Ports need to be marked as busy while a resume is taking place.
    
    In addition, so long as ports are marked as busy, khubd won't be able to
    clear their various status-change features.  On an interrupt-driven root
    hub this could lead to an interrupt storm.  Root hub IRQs should not be
    re-enabled until the busy_bits value is equal to 0.
    
    This patch (as765) fixes these two potential problems.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1659,6 +1659,8 @@ hub_port_resume(struct usb_hub *hub, int port1, struct usb_device *udev)
 
 	// dev_dbg(hub->intfdev, "resume port %d\n", port1);
 
+	set_bit(port1, hub->busy_bits);
+
 	/* see 7.1.7.7; affects power usage, but not budgeting */
 	status = clear_port_feature(hub->hdev,
 			port1, USB_PORT_FEAT_SUSPEND);
@@ -1707,6 +1709,10 @@ hub_port_resume(struct usb_hub *hub, int port1, struct usb_device *udev)
 	}
 	if (status < 0)
 		hub_port_logical_disconnect(hub, port1);
+
+	clear_bit(port1, hub->busy_bits);
+	if (!hub->hdev->parent && !hub->busy_bits[0])
+		usb_enable_root_hub_irq(hub->hdev->bus);
 
 	return status;
 }
@@ -2691,7 +2697,7 @@ static void hub_events(void)
 
 		/* If this is a root hub, tell the HCD it's okay to
 		 * re-enable port-change interrupts now. */
-		if (!hdev->parent)
+		if (!hdev->parent && !hub->busy_bits[0])
 			usb_enable_root_hub_irq(hdev->bus);
 
 loop:
@@ -2866,6 +2872,9 @@ int usb_reset_device(struct usb_device *udev)
 			break;
 	}
 	clear_bit(port1, parent_hub->busy_bits);
+	if (!parent_hdev->parent && !parent_hub->busy_bits[0])
+		usb_enable_root_hub_irq(parent_hdev->bus);
+
 	if (ret < 0)
 		goto re_enumerate;
  
