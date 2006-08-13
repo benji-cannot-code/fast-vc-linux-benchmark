089df50a9bfe93d35a3a6ba9659ec3154b825122
Alexey Dobriyan <adobriyan@gmail.com>
2006-08-12 22:01:27 -0300

    V4L/DVB (4499): CONFIG_PM=n slim: drivers/media/video/*
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/bt8xx/bttv-driver.c
+++ drivers/media/video/bt8xx/bttv-driver.c
@@ -4195,6 +4195,7 @@ static void __devexit bttv_remove(struct pci_dev *pci_dev)
 	return;
 }
 
+#ifdef CONFIG_PM
 static int bttv_suspend(struct pci_dev *pci_dev, pm_message_t state)
 {
 	struct bttv *btv = pci_get_drvdata(pci_dev);
@@ -4275,6 +4276,7 @@ static int bttv_resume(struct pci_dev *pci_dev)
 	spin_unlock_irqrestore(&btv->s_lock,flags);
 	return 0;
 }
+#endif
 
 static struct pci_device_id bttv_pci_tbl[] = {
 	{PCI_VENDOR_ID_BROOKTREE, PCI_DEVICE_ID_BT848,
@@ -4295,8 +4297,10 @@ static struct pci_driver bttv_pci_driver = {
 	.id_table = bttv_pci_tbl,
 	.probe    = bttv_probe,
 	.remove   = __devexit_p(bttv_remove),
+#ifdef CONFIG_PM
 	.suspend  = bttv_suspend,
 	.resume   = bttv_resume,
+#endif
 };
 
 static int bttv_init_module(void)
