35549d476cc5d95d7b41a289ff2842ee9b62e0a5
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-08-12 21:59:19 -0300

    V4L/DVB (4498): Offset parameter permission were 0x666, instead of 0x664
    Although not dangerous, it may allow a normal user with access to a machine
    to untune a TV channel.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tuner-simple.c
+++ drivers/media/video/tuner-simple.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/v4l2-common.h>
 
 static int offset = 0;
-module_param(offset, int, 0666);
+module_param(offset, int, 0664);
 MODULE_PARM_DESC(offset,"Allows to specify an offset for tuner");
 
 /* ---------------------------------------------------------------------- */
