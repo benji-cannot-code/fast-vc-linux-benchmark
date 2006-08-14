2611b9b8cc4b0c48abbb8c80370b8c9cf39783ea
James Smart <James.Smart@Emulex.Com>
2006-08-14 08:20:25 -0400

    [SCSI] fc transport: convert fc_host symbolic_name attribute to a dynamic attribute
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- include/scsi/scsi_transport_fc.h
+++ include/scsi/scsi_transport_fc.h
@@ -410,6 +410,7 @@ struct fc_function_template {
 	void	(*get_host_active_fc4s)(struct Scsi_Host *);
 	void	(*get_host_speed)(struct Scsi_Host *);
 	void	(*get_host_fabric_name)(struct Scsi_Host *);
+	void	(*get_host_symbolic_name)(struct Scsi_Host *);
 
 	struct fc_host_statistics * (*get_fc_host_stats)(struct Scsi_Host *);
 	void	(*reset_fc_host_stats)(struct Scsi_Host *);
@@ -446,7 +447,6 @@ struct fc_function_template {
 	unsigned long	show_host_permanent_port_name:1;
 	unsigned long	show_host_supported_classes:1;
 	unsigned long	show_host_supported_fc4s:1;
-	unsigned long	show_host_symbolic_name:1;
 	unsigned long	show_host_supported_speeds:1;
 	unsigned long	show_host_maxframe_size:1;
 	unsigned long	show_host_serial_number:1;
@@ -457,6 +457,7 @@ struct fc_function_template {
 	unsigned long	show_host_active_fc4s:1;
 	unsigned long	show_host_speed:1;
 	unsigned long	show_host_fabric_name:1;
+	unsigned long	show_host_symbolic_name:1;
 };
 
 
