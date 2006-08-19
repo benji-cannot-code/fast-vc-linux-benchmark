e1d946315b5e7d791ddf35e1b316e419bda205ec
Herbert Xu <herbert@gondor.apana.org.au>
2006-08-19 22:24:23 +1000

    [CRYPTO] digest: Added user API for new hash type
    The existing digest user interface is inadequate for support asynchronous
    operations.  For one it doesn't return a value to indicate success or
    failure, nor does it take a per-operation descriptor which is essential
    for the issuing of requests while other requests are still outstanding.
    
    This patch is the first in a series of steps to remodel the interface
    for asynchronous operations.
    
    For the ease of transition the new interface will be known as "hash"
    while the old one will remain as "digest".
    
    This patch also changes sg_next to allow chaining.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/scatterwalk.h
+++ crypto/scatterwalk.h
@@ -21,11 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "internal.h"
 
-/* Define sg_next is an inline routine now in case we want to change
-   scatterlist to a linked list later. */
 static inline struct scatterlist *sg_next(struct scatterlist *sg)
 {
-	return sg + 1;
+	return (++sg)->length ? sg : (void *)sg->page;
 }
 
 static inline unsigned long scatterwalk_samebuf(struct scatter_walk *walk_in,
