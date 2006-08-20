c6519b6a40c8cb86142bb00de5969b00ecd4bf1c
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2006-08-20 22:55:52 +0900

    [MIPS] Fix build errors related to wbflush.h on tx4927/tx4938.
    TX49 CPUs have a SYNC instruction so that CONFIG_CPU_HAS_WB is no
    longer needed.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/tx4938/common/irq.c
+++ arch/mips/tx4938/common/irq.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <asm/mipsregs.h>
 #include <asm/system.h>
+#include <asm/wbflush.h>
 #include <asm/tx4938/rbtx4938.h>
 
 /**********************************************************************************/
