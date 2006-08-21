a202296a8f291500647c14c4de0c73bb32b43705
Russell King <rmk@dyn-67.arm.linux.org.uk>
2006-08-21 19:23:38 +0100

    [ARM] Move mmu.c out of the way
    Rename mmu.c to context.c - it's the ARMv6 ASID context handling
    code rather than generic "mmu" handling code.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/context.c
+++ arch/arm/mm/context.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *  linux/arch/arm/mm/mmu.c
+ *  linux/arch/arm/mm/context.c
  *
  *  Copyright (C) 2002-2003 Deep Blue Solutions Ltd, all rights reserved.
  *
