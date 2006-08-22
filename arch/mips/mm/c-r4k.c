a979d4f1f3ce8938745bc550d8f61e3d52be5bbf
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2006-08-22 21:15:47 +0900

    [MIPS] Remove redundant r4k_blast_icache() calls
    r4k_flush_cache_all() and r4k_flush_cache_mm() case: these are noop if
    the CPU did not have dc_aliases.  It would mean we do not need to care
    about icache here.
    
    r4k_flush_cache_range case: if r4k_flush_cache_mm() did not need to
    care about icache, it would be same for this function.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mm/c-r4k.c
+++ arch/mips/mm/c-r4k.c
@@ -324,7 +324,6 @@ static void __init r4k_blast_scache_setup(void)
 static inline void local_r4k_flush_cache_all(void * args)
 {
 	r4k_blast_dcache();
-	r4k_blast_icache();
 }
 
 static void r4k_flush_cache_all(void)
@@ -360,21 +359,19 @@ static void r4k___flush_cache_all(void)
 static inline void local_r4k_flush_cache_range(void * args)
 {
 	struct vm_area_struct *vma = args;
-	int exec;
 
 	if (!(cpu_context(smp_processor_id(), vma->vm_mm)))
 		return;
 
-	exec = vma->vm_flags & VM_EXEC;
-	if (cpu_has_dc_aliases || exec)
-		r4k_blast_dcache();
-	if (exec)
-		r4k_blast_icache();
+	r4k_blast_dcache();
 }
 
 static void r4k_flush_cache_range(struct vm_area_struct *vma,
 	unsigned long start, unsigned long end)
 {
+	if (!cpu_has_dc_aliases)
+		return;
+
 	r4k_on_each_cpu(local_r4k_flush_cache_range, vma, 1, 1);
 }
 
@@ -386,7 +383,6 @@ static inline void local_r4k_flush_cache_mm(void * args)
 		return;
 
 	r4k_blast_dcache();
-	r4k_blast_icache();
 
 	/*
 	 * Kludge alert.  For obscure reasons R4000SC and R4400SC go nuts if we
