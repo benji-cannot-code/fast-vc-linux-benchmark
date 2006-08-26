5168423757015de4aa75c02955f3d1d91f9b7137
Herbert Xu <herbert@gondor.apana.org.au>
2006-08-26 18:34:10 +1000

    [CRYPTO] padlock: Convert padlock-sha to use crypto_hash
    This patch converts padlock-sha to use crypto_hash for its fallback.
    It also changes the fallback selection to use selection by type instead
    of name.  This is done through the new CRYPTO_ALG_NEED_FALLBACK bit,
    which is set if and only if an algorithm needs a fallback of the same
    type.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -44,6 +44,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CRYPTO_ALG_ASYNC		0x00000080
 
 /*
+ * Set this bit if and only if the algorithm requires another algorithm of
+ * the same type to handle corner cases.
+ */
+#define CRYPTO_ALG_NEED_FALLBACK	0x00000100
+
+/*
  * Transform masks and values (for crt_flags).
  */
 #define CRYPTO_TFM_MODE_MASK		0x000000ff
