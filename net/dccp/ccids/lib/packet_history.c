bb82d57902b629820447b23fb5c1b030bb50ff2a
Ian McDonald <ian.mcdonald@jandi.co.nz>
2006-08-26 19:01:03 -0700

    [DCCP]: Fix typo
    This fixes a small typo in net/dccp/libs/packet_history.c
    
    Signed off by: Ian McDonald <ian.mcdonald@jandi.co.nz>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ccids/lib/packet_history.c
+++ net/dccp/ccids/lib/packet_history.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *  net/dccp/packet_history.h
+ *  net/dccp/packet_history.c
  *
  *  Copyright (c) 2005 The University of Waikato, Hamilton, New Zealand.
  *
