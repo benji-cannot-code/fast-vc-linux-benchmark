9ffca6e3010287e3c66bd91b18114853f9005f26
Greg Kroah-Hartman <gregkh@suse.de>
2006-08-28 11:43:25 -0700

    PCI: fix __must_check warnings
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pci/pcie/portdrv_core.c
+++ drivers/pci/pcie/portdrv_core.c
@@ -341,8 +341,7 @@ static int suspend_iter(struct device *dev, void *data)
 
 int pcie_port_device_suspend(struct pci_dev *dev, pm_message_t state)
 {
-	device_for_each_child(&dev->dev, &state, suspend_iter);
-	return 0;
+	return device_for_each_child(&dev->dev, &state, suspend_iter);
 }
 
 static int resume_iter(struct device *dev, void *data)
@@ -360,8 +359,7 @@ static int resume_iter(struct device *dev, void *data)
 
 int pcie_port_device_resume(struct pci_dev *dev)
 {
-	device_for_each_child(&dev->dev, NULL, resume_iter);
-	return 0;
+	return device_for_each_child(&dev->dev, NULL, resume_iter);
 }
 #endif
 
