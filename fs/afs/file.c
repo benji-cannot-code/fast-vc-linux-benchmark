228fb4d9c4a9df9401f7232542dd0655d36f862b
David Howells <dhowells@redhat.com>
2006-08-29 19:06:29 +0100

    [PATCH] BLOCK: Remove no-longer necessary linux/buffer_head.h inclusions [try #6]
    Remove inclusions of linux/buffer_head.h that are no longer necessary due to the
    transfer of a number of things out of there.
    
    Signed-Off-By: David Howells <dhowells@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- fs/afs/file.c
+++ fs/afs/file.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/fs.h>
 #include <linux/pagemap.h>
-#include <linux/buffer_head.h>
 #include "volume.h"
 #include "vnode.h"
 #include <rxrpc/call.h>
