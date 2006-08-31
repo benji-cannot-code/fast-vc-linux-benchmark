4de0832947946e8928aaf639fc3562447b952485
Auke Kok <auke\-jan.h.kok@intel.com>
2006-08-31 14:27:49 -0700

    e100: increment version to 3.5.16-k2
    Increment the version of e100 to 3.5.16-k2, increment dates.
    
    Signed-off-by: Auke Kok <auke-jan.h.kok@intel.com>

--- drivers/net/e100.c
+++ drivers/net/e100.c
@@ -160,7 +160,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME		"e100"
 #define DRV_EXT			"-NAPI"
-#define DRV_VERSION		"3.5.10-k4"DRV_EXT
+#define DRV_VERSION		"3.5.16-k2"DRV_EXT
 #define DRV_DESCRIPTION		"Intel(R) PRO/100 Network Driver"
 #define DRV_COPYRIGHT		"Copyright(c) 1999-2006 Intel Corporation"
 #define PFX			DRV_NAME ": "
