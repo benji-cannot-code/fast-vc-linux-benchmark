293771cfe6f8eee61d9844d1442206d21d0b0b3d
Al Viro <viro@zeniv.linux.org.uk>
2006-08-31 19:02:42 -0400

    [PATCH] syscall classes hookup for ppc and s390
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

---
+++ arch/powerpc/kernel/compat_audit.c
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#undef __powerpc64__
+#include <asm/unistd.h>
+
+unsigned ppc32_dir_class[] = {
+#include <asm-generic/audit_dir_write.h>
+~0U
+};
+
+unsigned ppc32_chattr_class[] = {
+#include <asm-generic/audit_change_attr.h>
+~0U
+};
