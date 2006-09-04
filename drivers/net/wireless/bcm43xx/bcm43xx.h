3c05d8ec877431d7ede79717e8faf3b3f137fb05
Larry Finger <Larry.Finger@lwfinger.net>
2006-09-04 17:13:57 -0500

    [PATCH] bcm43xx: Add firmware version printout
    This patch prints microcode revision, patchlevel, date and time to
    KERN_INFO. Also, version 4.xx microcodes (rev>0x128) will be rejected
    by the driver, because they still do not work.
    
    Signed-off-by: Martin Langer <martin-langer@gmx.de>
    Acked-by: Michael Buesch <mb@bu3sch.de>
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/bcm43xx/bcm43xx.h
+++ drivers/net/wireless/bcm43xx/bcm43xx.h
@@ -307,6 +307,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCM43xx_SBF_TIME_UPDATE		0x10000000
 #define BCM43xx_SBF_80000000		0x80000000 /*FIXME: fix name*/
 
+/* Microcode */
+#define BCM43xx_UCODE_REVISION          0x0000
+#define BCM43xx_UCODE_PATCHLEVEL        0x0002
+#define BCM43xx_UCODE_DATE              0x0004
+#define BCM43xx_UCODE_TIME              0x0006
+
 /* MicrocodeFlagsBitfield (addr + lo-word values?)*/
 #define BCM43xx_UCODEFLAGS_OFFSET	0x005E
 
