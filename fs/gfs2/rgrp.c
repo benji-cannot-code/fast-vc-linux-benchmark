efd47009c73f448686522d40f6e511a2103b449e
Steven Whitehouse <swhiteho@redhat.com>
2006-09-05 15:41:57 -0400

    [GFS2] More style fixes
    As per Jan Engelhardt's follow up emails, here are a few small
    fixes which were missed earlier.
    
    Cc: Jan Engelhardt <jengelh@linux01.gwdg.de>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/rgrp.c
+++ fs/gfs2/rgrp.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ops_file.h"
 #include "util.h"
 
-#define BFITNOENT (u32)~0
+#define BFITNOENT ((u32)~0)
 
 /*
  * These routines are used by the resource group routines (rgrp.c)
@@ -692,17 +692,6 @@ struct gfs2_alloc *gfs2_alloc_get(struct gfs2_inode *ip)
 	/* FIXME: Should assert that the correct locks are held here... */
 	memset(al, 0, sizeof(*al));
 	return al;
-}
-
-/**
- * gfs2_alloc_put - throw away the struct gfs2_alloc for an inode
- * @ip: the inode
- *
- */
-
-void gfs2_alloc_put(struct gfs2_inode *ip)
-{
-	return;
 }
 
 /**
