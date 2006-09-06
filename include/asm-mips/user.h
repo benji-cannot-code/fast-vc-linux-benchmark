2736c4a91bad8de6219450a30f1147fa68476550
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2006-09-07 01:00:22 +0900

    [MIPS] Fix errors detected by "make headers_check"
    * export asm/sgidefs.h
    * include asm/isadep.h only if in kernel
    * do not export contents of asm/timex.h and asm/user.h
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/user.h
+++ include/asm-mips/user.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_USER_H
 #define _ASM_USER_H
 
+#ifdef __KERNEL__
+
 #include <asm/page.h>
 #include <asm/reg.h>
 
@@ -55,5 +57,7 @@ struct user {
 #define HOST_TEXT_START_ADDR	(u.start_code)
 #define HOST_DATA_START_ADDR	(u.start_data)
 #define HOST_STACK_END_ADDR	(u.start_stack + u.u_ssize * NBPG)
+
+#endif /* __KERNEL__ */
 
 #endif /* _ASM_USER_H */
