7c84497d471688664db6a9887303b848f8641b0e
Olof Johansson <olof@lixom.net>
2006-09-06 14:35:57 -0500

    [POWERPC] powerpc: PA6T cputable entry, PVR value
    Introduce PWRficient PA6T cputable entries and feature bits.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/reg.h
+++ include/asm-powerpc/reg.h
@@ -593,6 +593,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PV_630p	0x0041
 #define PV_970MP	0x0044
 #define PV_BE		0x0070
+#define PV_PA6T		0x0090
 
 /*
  * Number of entries in the SLB. If this ever changes we should handle
