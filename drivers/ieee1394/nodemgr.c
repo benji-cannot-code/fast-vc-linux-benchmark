161afa7da1f0c872df6d7a0f3feb742947489aa0
Stefan Richter <stefanr@s5r6.in-berlin.de>
2006-09-06 19:04:00 +0200

    ieee1394: nodemgr: fix rwsem recursion
    nodemgr_update_pdrv grabbed an rw semaphore (as reader) which was
    already taken by its caller's caller, nodemgr_probe_ne (as reader too).
    Reported by Miles Lane, call path pointed out by Arjan van de Ven.
    
    FIXME:
    Shouldn't we rather use class->sem there, not class->subsys.rwsem?
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/nodemgr.c
+++ drivers/ieee1394/nodemgr.c
@@ -1317,6 +1317,7 @@ static void nodemgr_node_scan(struct host_info *hi, int generation)
 }
 
 
+/* Caller needs to hold nodemgr_ud_class.subsys.rwsem as reader. */
 static void nodemgr_suspend_ne(struct node_entry *ne)
 {
 	struct class_device *cdev;
@@ -1369,15 +1370,14 @@ static void nodemgr_resume_ne(struct node_entry *ne)
 }
 
 
+/* Caller needs to hold nodemgr_ud_class.subsys.rwsem as reader. */
 static void nodemgr_update_pdrv(struct node_entry *ne)
 {
 	struct unit_directory *ud;
 	struct hpsb_protocol_driver *pdrv;
-	struct class *class = &nodemgr_ud_class;
 	struct class_device *cdev;
 
-	down_read(&class->subsys.rwsem);
-	list_for_each_entry(cdev, &class->children, node) {
+	list_for_each_entry(cdev, &nodemgr_ud_class.children, node) {
 		ud = container_of(cdev, struct unit_directory, class_dev);
 		if (ud->ne != ne || !ud->device.driver)
 			continue;
@@ -1390,7 +1390,6 @@ static void nodemgr_update_pdrv(struct node_entry *ne)
 			up_write(&ud->device.bus->subsys.rwsem);
 		}
 	}
-	up_read(&class->subsys.rwsem);
 }
 
 
@@ -1421,6 +1420,8 @@ static void nodemgr_irm_write_bc(struct node_entry *ne, int generation)
 }
 
 
+/* Caller needs to hold nodemgr_ud_class.subsys.rwsem as reader because the
+ * calls to nodemgr_update_pdrv() and nodemgr_suspend_ne() here require it. */
 static void nodemgr_probe_ne(struct host_info *hi, struct node_entry *ne, int generation)
 {
 	struct device *dev;
