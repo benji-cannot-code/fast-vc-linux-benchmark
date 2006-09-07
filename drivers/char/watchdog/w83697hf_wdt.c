62d12e399de015026f3e20a5eb5607bce61fb5dd
Samuel Tardieu <sam@rfc1149.net>
2006-09-07 11:57:00 +0200

    [WATCHDOG] w83697hf/hg WDT driver - patch 2
    This is patch 2 in the series of patches that converts
    Marcus Junker's w83697hf watchdog driver to Samuel Tardieau's
    w83697hf/hg watchdog driver.
    
    This patch contains following changes:
     - wdt_io is 2 bytes long. We should do a
       request_region for 2 bytes instead of 1.
    
    Signed-off-by: Samuel Tardieu <sam@rfc1149.net>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/char/watchdog/w83697hf_wdt.c
+++ drivers/char/watchdog/w83697hf_wdt.c
@@ -322,7 +322,7 @@ wdt_init(void)
 			WATCHDOG_TIMEOUT);
 	}
 
-	if (!request_region(wdt_io, 1, WATCHDOG_NAME)) {
+	if (!request_region(wdt_io, 2, WATCHDOG_NAME)) {
 		printk (KERN_ERR PFX "I/O address 0x%04x already in use\n",
 			wdt_io);
 		ret = -EIO;
@@ -353,7 +353,7 @@ out:
 unreg_reboot:
 	unregister_reboot_notifier(&wdt_notifier);
 unreg_regions:
-	release_region(wdt_io, 1);
+	release_region(wdt_io, 2);
 	goto out;
 }
 
@@ -362,7 +362,7 @@ wdt_exit(void)
 {
 	misc_deregister(&wdt_miscdev);
 	unregister_reboot_notifier(&wdt_notifier);
-	release_region(wdt_io,1);
+	release_region(wdt_io, 2);
 }
 
 module_init(wdt_init);
