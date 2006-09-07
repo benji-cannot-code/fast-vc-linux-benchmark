8301a2104b03cfc1c606b0f82108bbd4be515619
Steven Whitehouse <swhiteho@redhat.com>
2006-09-07 14:40:21 -0400

    [GFS2] Move glock hash table out of superblock
    There are several reasons why we want to do this:
     - Firstly its large and thus we'll scale better with multiple
       GFS2 fs mounted at the same time
     - Secondly its easier to scale its size as required (thats a plan
       for later patches)
     - Thirdly, we can use kzalloc rather than vmalloc when allocating
       the superblock (its now only 4888 bytes)
     - Fourth its all part of my plan to eventually be able to use RCU
       with the glock hash.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/main.c
+++ fs/gfs2/main.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "ops_fstype.h"
 #include "sys.h"
 #include "util.h"
+#include "glock.h"
 
 static void gfs2_init_inode_once(void *foo, kmem_cache_t *cachep, unsigned long flags)
 {
@@ -70,8 +71,11 @@ static int __init init_gfs2_fs(void)
 	if (error)
 		return error;
 
-	error = -ENOMEM;
+	error = gfs2_glock_init();
+	if (error)
+		goto fail;
 
+	error = -ENOMEM;
 	gfs2_glock_cachep = kmem_cache_create("gfs2_glock",
 					      sizeof(struct gfs2_glock),
 					      0, 0, 
