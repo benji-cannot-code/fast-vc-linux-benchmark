a81927e571ee35e28509b2f8b40cea3104a7aab8
Alan Cox <alan@lxorguk.ukuu.org.uk>
2006-09-12 17:15:12 +0100

    [PATCH] Update SiS PATA
    New chipset identifiers
    
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/pata_sis.c
+++ drivers/ata/pata_sis.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ata.h>
 
 #define DRV_NAME	"pata_sis"
-#define DRV_VERSION	"0.4.2"
+#define DRV_VERSION	"0.4.3"
 
 struct sis_chipset {
 	u16 device;			/* PCI host ID */
@@ -858,6 +858,10 @@ static int sis_init_one (struct pci_dev *pdev, const struct pci_device_id *ent)
 	struct sis_chipset *chipset = NULL;
 
 	static struct sis_chipset sis_chipsets[] = {
+	
+		{ 0x0968, &sis_info133 },
+		{ 0x0966, &sis_info133 },
+		{ 0x0965, &sis_info133 },
 		{ 0x0745, &sis_info100 },
 		{ 0x0735, &sis_info100 },
 		{ 0x0733, &sis_info100 },
