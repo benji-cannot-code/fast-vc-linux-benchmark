19efc93510ab5a653275d8024b76efb151f9cd01
Olof Johansson <olof@lixom.net>
2006-09-13 13:32:39 -0500

    [POWERPC] Fix non-smp build
    This fixes a compile error that only surfaces on CONFIG_SMP=n builds;
    <asm/hvcall.h> seems to get pulled in through another header file for
    SMP builds.  This problem was introduced by the hvcall stats patch.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/asm-offsets.c
+++ arch/powerpc/kernel/asm-offsets.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cache.h>
 #include <asm/compat.h>
 #include <asm/mmu.h>
+#include <asm/hvcall.h>
 #endif
 
 #define DEFINE(sym, val) \
