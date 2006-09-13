cfb5aa0ea9937dfbcd10dfc2d11216ae1b3f6b4e
Alexey Dobriyan <adobriyan@gmail.com>
2006-09-12 20:36:01 -0700

    [PATCH] headers_check: clarify error message
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- scripts/hdrcheck.sh
+++ scripts/hdrcheck.sh
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 for FILE in `grep '^[ \t]*#[ \t]*include[ \t]*<' $2 | cut -f2 -d\< | cut -f1 -d\> | egrep ^linux\|^asm` ; do
     if [ ! -r $1/$FILE ]; then
-	echo $2 requires $FILE, which does not exist
+	echo $2 requires $FILE, which does not exist in exported headers
 	exit 1
     fi
 done
