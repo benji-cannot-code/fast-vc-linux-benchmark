7bea6c3f8b6e807ea122658e591913ff407af59b
Steven Whitehouse <swhiteho@redhat.com>
2006-09-14 13:57:38 -0400

    [GFS2] Fix glock hash clearing
    A one liner bug fix to prevent the return value being
    wrong when more than one superblock is mounted.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -1893,7 +1893,6 @@ static int examine_bucket(glock_examiner examiner, struct gfs2_sbd *sdp,
 	/* Can't use hlist_for_each_entry - don't want prefetch here */
 	if (hlist_empty(head))
 		goto out;
-	has_entries = 1;
 	gl = list_entry(head->first, struct gfs2_glock, gl_list);
 	while(1) {
 		if (gl->gl_sbd == sdp) {
@@ -1903,6 +1902,7 @@ static int examine_bucket(glock_examiner examiner, struct gfs2_sbd *sdp,
 				gfs2_glock_put(prev);
 			prev = gl;
 			examiner(gl);
+			has_entries = 1;
 			read_lock(gl_lock_addr(hash));
 		}
 		if (gl->gl_list.next == NULL)
