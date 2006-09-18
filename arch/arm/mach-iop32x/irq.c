91c7545d4c0006467f3320520ebd41e73280ded5
Lennert Buytenhek <buytenh@wantstofly.org>
2006-09-18 23:22:24 +0100

    [ARM] 3826/1: iop3xx: remove IOP3??_IRQ_OFS irq offset
    Get rid of the unused IOP3??_IRQ_OFS irq offset define, start IRQ
    numbering from zero.
    
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-iop32x/irq.c
+++ arch/arm/mach-iop32x/irq.c
@@ -44,7 +44,7 @@ static void
 iop321_irq_mask (unsigned int irq)
 {
 
-	iop321_mask &= ~(1 << (irq - IOP321_IRQ_OFS));
+	iop321_mask &= ~(1 << irq);
 
 	intctl_write(iop321_mask);
 }
@@ -52,7 +52,7 @@ iop321_irq_mask (unsigned int irq)
 static void
 iop321_irq_unmask (unsigned int irq)
 {
-	iop321_mask |= (1 << (irq - IOP321_IRQ_OFS));
+	iop321_mask |= (1 << irq);
 
 	intctl_write(iop321_mask);
 }
@@ -74,7 +74,7 @@ void __init iop321_init_irq(void)
 	   machine_is_iq31244()) 	// all interrupts are inputs to chip
 		*IOP3XX_PCIIRSR = 0x0f;
 
-	for(i = IOP321_IRQ_OFS; i < NR_IRQS; i++)
+	for(i = 0; i < NR_IRQS; i++)
 	{
 		set_irq_chip(i, &ext_chip);
 		set_irq_handler(i, do_level_IRQ);
