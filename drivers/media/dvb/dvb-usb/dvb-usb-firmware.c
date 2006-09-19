ace0fc3d5eaedcfb9b067c8d2543ef8702f235a8
Patrick Boettcher <pb@linuxtv.org>
2006-09-19 12:51:33 -0300

    V4L/DVB (4646): Misc. changes, DiB3000MC, MT2060
    Changed the attach-function of the dib3000mc-driver to return only one
    frontend. In case of multiple dib3000-chips on one board, one has to call the
    i2c-enumeration manually before.
    Added a field to Microtune 2060 config to output the clock to other
    tuners/device on a board.
    
    Signed-off-by: Patrick Boettcher <pb@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dvb-usb-firmware.c
+++ drivers/media/dvb/dvb-usb/dvb-usb-firmware.c
@@ -25,9 +25,6 @@ static struct usb_cypress_controller cypress[] = {
 	{ .id = CYPRESS_FX2,     .name = "Cypress FX2",     .cpu_cs_register = 0xe600 },
 };
 
-static int dvb_usb_get_hexline(const struct firmware *fw, struct hexline *hx,
-			       int *pos);
-
 /*
  * load a firmware packet to the device
  */
@@ -116,7 +113,7 @@ int dvb_usb_download_firmware(struct usb_device *udev, struct dvb_usb_device_pro
 	return ret;
 }
 
-static int dvb_usb_get_hexline(const struct firmware *fw, struct hexline *hx,
+int dvb_usb_get_hexline(const struct firmware *fw, struct hexline *hx,
 			       int *pos)
 {
 	u8 *b = (u8 *) &fw->data[*pos];
@@ -147,3 +144,4 @@ static int dvb_usb_get_hexline(const struct firmware *fw, struct hexline *hx,
 
 	return *pos;
 }
+EXPORT_SYMBOL(dvb_usb_get_hexline);
