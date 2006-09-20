71d628cc3dc1ea7cda2fb33865842e791b31b2b7
Christian Borntraeger <cborntra@de.ibm.com>
2006-09-20 15:59:24 +0200

    [S390] fix typo in vmcp.
    Fix comment typo in vmcp, it is z/VM and not v/VM.
    
    Signed-off-by: Christian Borntraeger <cborntra@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/char/vmcp.h
+++ drivers/s390/char/vmcp.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (C) 2004, 2005 IBM Corporation
- * Interface implementation for communication with the v/VM control program
+ * Interface implementation for communication with the z/VM control program
  * Version 1.0
  * Author(s): Christian Borntraeger <cborntra@de.ibm.com>
  *
