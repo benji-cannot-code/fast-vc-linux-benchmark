7d69fae4ce0f779ab3af72a7fb746aa9e9db92c9
Peter Oberparleiter <peter.oberparleiter@de.ibm.com>
2006-09-20 15:59:54 +0200

    [S390] cio: subchannels in no-path state.
    Subchannel may incorrectly remain in state no-path after channel paths
    have reappeared. Currently the scan for subchannels which are using a
    channel path ends at the first occurrence if a full link address was
    provided by the channel subsystem. The scan needs to continue over
    all subchannels.
    
    Signed-off-by: Peter Oberparleiter <peter.oberparleiter@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/cio/chsc.c
+++ drivers/s390/cio/chsc.c
@@ -379,6 +379,7 @@ __s390_process_res_acc(struct subchannel_id schid, void *data)
 
 	if (chp_mask == 0) {
 		spin_unlock_irq(&sch->lock);
+		put_device(&sch->dev);
 		return 0;
 	}
 	old_lpm = sch->lpm;
@@ -393,7 +394,7 @@ __s390_process_res_acc(struct subchannel_id schid, void *data)
 
 	spin_unlock_irq(&sch->lock);
 	put_device(&sch->dev);
-	return (res_data->fla_mask == 0xffff) ? -ENODEV : 0;
+	return 0;
 }
 
 
