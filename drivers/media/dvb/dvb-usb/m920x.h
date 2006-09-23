c404964ffbdabcc5f04478cc5870bf19b6b0a4ca
Michael Krufky <mkrufky@linuxtv.org>
2006-09-23 20:01:29 -0300

    V4L/DVB (5126): M920x: cleanups after rename from megasky.[ch]
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/m920x.h
+++ drivers/media/dvb/dvb-usb/m920x.h
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _DVB_USB_MEGASKY_H_
-#define _DVB_USB_MEGASKY_H_
+#ifndef _DVB_USB_M920X_H_
+#define _DVB_USB_M920X_H_
 
-#define DVB_USB_LOG_PREFIX "megasky"
+#define DVB_USB_LOG_PREFIX "m920x"
 #include "dvb-usb.h"
 
-extern int dvb_usb_megasky_debug;
-#define deb_rc(args...)   dprintk(dvb_usb_megasky_debug,0x01,args)
+extern int dvb_usb_m920x_debug;
+#define deb_rc(args...)   dprintk(dvb_usb_m920x_debug,0x01,args)
 
 #endif
