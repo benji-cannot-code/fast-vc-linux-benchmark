cf7addb89cd83b079752bec1cff83b8296695962
Stephen Rothwell <sfr@canb.auug.org.au>
2006-09-25 13:27:17 +1000

    [POWERPC] iSeries: set FW_FEATURE_ISERIES earlier
    This will help in the combined kernel as some really early things depend
    on being able to check this.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- arch/powerpc/platforms/iseries/setup.c
+++ arch/powerpc/platforms/iseries/setup.c
@@ -656,9 +656,6 @@ static int __init iseries_probe(void)
 	if (!of_flat_dt_is_compatible(root, "IBM,iSeries"))
 		return 0;
 
-	powerpc_firmware_features |= FW_FEATURE_ISERIES;
-	powerpc_firmware_features |= FW_FEATURE_LPAR;
-
 	hpte_init_iSeries();
 
 	return 1;
@@ -687,6 +684,9 @@ define_machine(iseries) {
 void * __init iSeries_early_setup(void)
 {
 	unsigned long phys_mem_size;
+
+	powerpc_firmware_features |= FW_FEATURE_ISERIES;
+	powerpc_firmware_features |= FW_FEATURE_LPAR;
 
 	iSeries_fixup_klimit();
 
