fe23807952e02bba8e443ee01dc46f60d5455dc4
Ralf Baechle <ralf@linux-mips.org>
2006-09-25 15:49:49 +0100

    [MIPS] s/__ASSEMBLER__/__ASSEMBLY__/ for clarity sake.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/hazards.h
+++ include/asm-mips/hazards.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_HAZARDS_H
 
 
-#ifdef __ASSEMBLER__
+#ifdef __ASSEMBLY__
 #define ASMMACRO(name, code...) .macro name; code; .endm
 #else
 
