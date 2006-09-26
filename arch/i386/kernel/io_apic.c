6844b8106e787ae1dc981025c5852c4202ae983a
Andi Kleen <ak@suse.de>
2006-09-26 10:52:26 +0200

    [PATCH] i386: Allow to use GENERICARCH for UP kernels
    There are some machines around (large xSeries or Unisys ES7000) that
    need physical IO-APIC destination mode to access all of their IO
    devices. This currently doesn't work in UP kernels as used in
    distribution installers.
    
    This patch allows to compile even UP kernels as GENERICARCH which
    allows to use physical or clustered APIC mode.
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/i386/kernel/io_apic.c
+++ arch/i386/kernel/io_apic.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/nmi.h>
 
 #include <mach_apic.h>
+#include <mach_apicdef.h>
 
 #include "io_ports.h"
 
--- arch/i386/kernel/mpparse.c
+++ arch/i386/kernel/mpparse.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io_apic.h>
 
 #include <mach_apic.h>
+#include <mach_apicdef.h>
 #include <mach_mpparse.h>
 #include <bios_ebda.h>
 
