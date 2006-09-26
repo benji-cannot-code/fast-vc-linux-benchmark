850903f6d4fd496e52e5fb0da87f7befcdff0927
Stephen Smalley <sds@tycho.nsa.gov>
2006-09-25 23:32:03 -0700

    [PATCH] selinux: fix tty locking
    Take tty_mutex when accessing ->signal->tty in selinux code.  Noted by Alan
    Cox.  Longer term, we are looking at refactoring the code to provide better
    encapsulation of the tty layer, but this is a simple fix that addresses the
    immediate bug.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    Acked-by: Alan Cox <alan@redhat.com>
    Acked-by: James Morris <jmorris@namei.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1690,10 +1690,12 @@ static inline void flush_unauthorized_files(struct files_struct * files)
 {
 	struct avc_audit_data ad;
 	struct file *file, *devnull = NULL;
-	struct tty_struct *tty = current->signal->tty;
+	struct tty_struct *tty;
 	struct fdtable *fdt;
 	long j = -1;
 
+	mutex_lock(&tty_mutex);
+	tty = current->signal->tty;
 	if (tty) {
 		file_list_lock();
 		file = list_entry(tty->tty_files.next, typeof(*file), f_u.fu_list);
@@ -1713,6 +1715,7 @@ static inline void flush_unauthorized_files(struct files_struct * files)
 		}
 		file_list_unlock();
 	}
+	mutex_unlock(&tty_mutex);
 
 	/* Revalidate access to inherited open files. */
 
