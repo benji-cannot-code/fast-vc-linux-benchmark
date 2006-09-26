8ec2d6efc6844d8f4a2388e4c8bfc42f0d446190
Andi Kleen <ak@suse.de>
2006-09-26 10:52:32 +0200

    [PATCH] i386: Clean up spin/rwlocks
    - Inline spinlock strings into their inline functions
    - Convert macros to typesafe inlines
    - Replace some leftover __asm__ __volatile__s with asm volatile
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-i386/rwlock.h
+++ include/asm-i386/rwlock.h
@@ -21,18 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RW_LOCK_BIAS		 0x01000000
 #define RW_LOCK_BIAS_STR	"0x01000000"
 
-#define __build_read_lock(rw, helper)   \
-	asm volatile(LOCK_PREFIX " subl $1,(%0)\n\t" \
-			"jns 1f\n" \
-			"call " helper "\n\t" \
-			"1:\n" \
-			::"a" (rw) : "memory")
-
-#define __build_write_lock(rw, helper) \
-	asm volatile(LOCK_PREFIX " subl $" RW_LOCK_BIAS_STR ",(%0)\n\t" \
-			"jz 1f\n" \
-			"call " helper "\n\t" \
-			"1:\n" \
-			::"a" (rw) : "memory")
+/* Code is in asm-i386/spinlock.h */
 
 #endif
