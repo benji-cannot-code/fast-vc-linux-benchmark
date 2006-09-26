e53f34e38933d73ca5ff3e7b70598aae23166b2f
Andi Kleen <ak@suse.de>
2006-09-26 10:52:29 +0200

    [PATCH] Add some comments what tce.c actually does
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/tce.c
+++ arch/x86_64/kernel/tce.c
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
+ * This file manages the translation entries for the IBM Calgary IOMMU.
+ *
  * Derived from arch/powerpc/platforms/pseries/iommu.c
  *
  * Copyright (C) IBM Corporation, 2006
