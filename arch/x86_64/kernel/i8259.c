45407d466825fa33012ec5d8ca3653c58eb26b21
Matthew Garrett <mjg59@srcf.ucam.org>
2006-09-26 10:52:41 +0200

    [PATCH] x86: - restore i8259A eoi status on resume
    Got it. i8259A_resume calls init_8259A(0) unconditionally, even if
    auto_eoi has been set. Keep track of the current status and restore that
    on resume. This fixes it for AMD64 and i386.
    
    Signed-off-by: Matthew Garrett <mjg59@srcf.ucam.org>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/i8259.c
+++ arch/x86_64/kernel/i8259.c
@@ -124,6 +124,8 @@ void (*interrupt[NR_IRQS])(void) = {
 
 DEFINE_SPINLOCK(i8259A_lock);
 
+static int i8259A_auto_eoi;
+
 static void end_8259A_irq (unsigned int irq)
 {
 	if (irq > 256) { 
@@ -337,6 +339,8 @@ void init_8259A(int auto_eoi)
 {
 	unsigned long flags;
 
+	i8259A_auto_eoi = auto_eoi;
+
 	spin_lock_irqsave(&i8259A_lock, flags);
 
 	outb(0xff, 0x21);	/* mask all of 8259A-1 */
@@ -395,7 +399,7 @@ static void save_ELCR(char *trigger)
 
 static int i8259A_resume(struct sys_device *dev)
 {
-	init_8259A(0);
+	init_8259A(i8259A_auto_eoi);
 	restore_ELCR(irq_trigger);
 	return 0;
 }
