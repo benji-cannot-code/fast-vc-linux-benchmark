981b5e7a991bc7edb6e2a21ef8501264deda596e
Dave Jones <davej@redhat.com>
2006-09-26 10:52:36 +0200

    [PATCH] x86: remove config.h includes from asm-i386 & asm-x86_64
    This is now automatically included by kbuild.
    
    Signed-off-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-x86_64/calgary.h
+++ include/asm-x86_64/calgary.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_X86_64_CALGARY_H
 #define _ASM_X86_64_CALGARY_H
 
-#include <linux/config.h>
 #include <linux/spinlock.h>
 #include <linux/device.h>
 #include <linux/dma-mapping.h>
