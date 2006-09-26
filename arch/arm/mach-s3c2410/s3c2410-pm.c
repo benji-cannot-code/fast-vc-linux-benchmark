c8471e3806d6a28a85cb268bbeff1594860ec1a6
David Anders <danders@amltd.com>
2006-09-26 17:46:00 +0100

    [ARM] 3862/2: S3C2410 - add basic power management support for AML M5900 series
    this patch registers the wakeup irq, sets a gpio pin to indicate the
    status of system for suspend/resume operations, and adds the machine to
    the supported machines for use with the simtec-pm
    
    Signed-off-by: David Anders <danders@amltd.com>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c2410/s3c2410-pm.c
+++ arch/arm/mach-s3c2410/s3c2410-pm.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/hardware.h>
 #include <asm/io.h>
 
+#include <asm/mach-types.h>
+
 #include <asm/arch/regs-gpio.h>
 
 #include "cpu.h"
@@ -50,6 +52,10 @@ static void s3c2410_pm_prepare(void)
 
 	DBG("GSTATUS3 0x%08x\n", __raw_readl(S3C2410_GSTATUS3));
 	DBG("GSTATUS4 0x%08x\n", __raw_readl(S3C2410_GSTATUS4));
+
+	if ( machine_is_aml_m5900() )
+		s3c2410_gpio_setpin(S3C2410_GPF2, 1);
+
 }
 
 int s3c2410_pm_resume(struct sys_device *dev)
@@ -61,6 +67,9 @@ int s3c2410_pm_resume(struct sys_device *dev)
 	tmp = __raw_readl(S3C2410_GSTATUS2);
 	tmp &= S3C2410_GSTATUS2_OFFRESET;
 	__raw_writel(tmp, S3C2410_GSTATUS2);
+
+	if ( machine_is_aml_m5900() )
+		s3c2410_gpio_setpin(S3C2410_GPF2, 0);
 
 	return 0;
 }
