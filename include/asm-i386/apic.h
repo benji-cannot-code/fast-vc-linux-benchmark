489d79cc8bd3e311485f83db3433386418e3ff7f
Adrian Bunk <bunk@stusta.de>
2006-09-26 10:52:35 +0200

    [PATCH] i386: Make enable_local_apic static
    enable_local_apic can now become static.
    
    Cc: len.brown@intel.com
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-i386/apic.h
+++ include/asm-i386/apic.h
@@ -17,19 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define APIC_VERBOSE 1
 #define APIC_DEBUG   2
 
-extern int enable_local_apic;
 extern int apic_verbosity;
-
-static inline void lapic_disable(void)
-{
-	enable_local_apic = -1;
-	clear_bit(X86_FEATURE_APIC, boot_cpu_data.x86_capability);
-}
-
-static inline void lapic_enable(void)
-{
-	enable_local_apic = 1;
-}
 
 /*
  * Define the default level of output to be very little
