0f86689a4d81e04403d82f3160bf62e95a5b6748
David Howells <dhowells@redhat.com>
2006-09-25 23:32:04 -0700

    [PATCH] FRV: Use the generic IRQ stuff
    Make the FRV arch use the generic IRQ code rather than having its own
    routines for doing so.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/frv/kernel/time.c
+++ arch/frv/kernel/time.c
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/timer-regs.h>
 #include <asm/mb-regs.h>
 #include <asm/mb86943a.h>
-#include <asm/irq-routing.h>
 
 #include <linux/timex.h>
 
