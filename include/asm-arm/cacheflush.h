5035a8c69392f359681103eb7a75e2b4d478a391
Hyok S. Choi <hyok.choi@samsung.com>
2006-09-26 17:38:32 +0900

    [ARM] nommu: add ARM946E-S core support
    This patch adds ARM946E-S core support which has typically 8KB I&D cache.
    It has a MPU and supports ARMv5TE instruction set.
    
    Because the ARM946E-S core can be synthesizable with various cache size,
    CONFIG_CPU_DCACHE_SIZE is defined for vendor specific configurations.
    
    Signed-off-by: Hyok S. Choi <hyok.choi@samsung.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/cacheflush.h
+++ include/asm-arm/cacheflush.h
@@ -65,6 +65,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # endif
 #endif
 
+#if defined(CONFIG_CPU_ARM946E)
+# ifdef _CACHE
+#  define MULTI_CACHE 1
+# else
+#  define _CACHE arm946
+# endif
+#endif
+
 #if defined(CONFIG_CPU_SA110) || defined(CONFIG_CPU_SA1100)
 # ifdef _CACHE
 #  define MULTI_CACHE 1
