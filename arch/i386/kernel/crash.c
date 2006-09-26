caf24df46fb342dc4099a37919ba1750b2105105
Vivek Goyal <vgoyal@in.ibm.com>
2006-09-26 10:52:27 +0200

    [PATCH] i386: Kdump i386 nmi event notification fix
    After a crash we should wait for NMI IPI event and not for external NMI or
    NMI watchdog tick.
    
    Signed-off-by: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Cc: Don Zickus <dzickus@redhat.com>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>

--- arch/i386/kernel/crash.c
+++ arch/i386/kernel/crash.c
@@ -103,7 +103,7 @@ static int crash_nmi_callback(struct notifier_block *self,
 	struct pt_regs fixed_regs;
 	int cpu;
 
-	if (val != DIE_NMI)
+	if (val != DIE_NMI_IPI)
 		return NOTIFY_OK;
 
 	regs = ((struct die_args *)data)->regs;
@@ -114,7 +114,7 @@ static int crash_nmi_callback(struct notifier_block *self,
 	 * an NMI if system was initially booted with nmi_watchdog parameter.
 	 */
 	if (cpu == crashing_cpu)
-		return 1;
+		return NOTIFY_STOP;
 	local_irq_disable();
 
 	if (!user_mode_vm(regs)) {
