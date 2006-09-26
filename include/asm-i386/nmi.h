a8f2e42b82ef730293ae0c6fa969b0f7f9eac93c
Don Zickus <dzickus@redhat.com>
2006-09-26 10:52:27 +0200

    [PATCH] x86: Add abilty to enable/disable nmi watchdog with sysctl
    Adds a new /proc/sys/kernel/nmi call that will enable/disable the nmi
    watchdog.
    
    Signed-off-by:  Don Zickus <dzickus@redhat.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-i386/nmi.h
+++ include/asm-i386/nmi.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 int do_nmi_callback(struct pt_regs *regs, int cpu);
 
+extern int nmi_watchdog_enabled;
 extern int avail_to_resrv_perfctr_nmi_bit(unsigned int);
 extern int avail_to_resrv_perfctr_nmi(unsigned int);
 extern int reserve_perfctr_nmi(unsigned int);
