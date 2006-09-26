664eec0f4c56dade5f940ec47b4598219ed8b8d3
Dmitriy Zavin <dmitriyz@google.com>
2006-09-26 10:52:42 +0200

    [PATCH] x86: Refactor thermal throttle processing
    Refactor the event processing (syslog messaging and rate limiting)
    into separate file therm_throt.c. This allows consistent reporting
    of CPU thermal throttle events.
    
    After ACK'ing the interrupt, if the event is current, the user
    (p4.c/mce_intel.c) calls therm_throt_process to log (and rate limit)
    the event. If that function returns 1, the user has the option to log
    things further (such as to mce_log in x86_64).
    
    AK: minor cleanup
    
    Signed-off-by: Dmitriy Zavin <dmitriyz@google.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/i386/kernel/cpu/mcheck/p4.c
+++ arch/i386/kernel/cpu/mcheck/p4.c
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/msr.h>
 #include <asm/apic.h>
 
+#include <asm/therm_throt.h>
+
 #include "mce.h"
 
 /* as supported by the P4/Xeon family */
@@ -45,25 +47,12 @@ static void unexpected_thermal_interrupt(struct pt_regs *regs)
 /* P4/Xeon Thermal transition interrupt handler */
 static void intel_thermal_interrupt(struct pt_regs *regs)
 {
-	u32 l, h;
-	unsigned int cpu = smp_processor_id();
-	static unsigned long next[NR_CPUS];
+	__u64 msr_val;
 
 	ack_APIC_irq();
 
-	if (time_after(next[cpu], jiffies))
-		return;
-
-	next[cpu] = jiffies + HZ*5;
-	rdmsr(MSR_IA32_THERM_STATUS, l, h);
-	if (l & 0x1) {
-		printk(KERN_EMERG "CPU%d: Temperature above threshold\n", cpu);
-		printk(KERN_EMERG "CPU%d: Running in modulated clock mode\n",
-				cpu);
-		add_taint(TAINT_MACHINE_CHECK);
-	} else {
-		printk(KERN_INFO "CPU%d: Temperature/speed normal\n", cpu);
-	}
+	rdmsrl(MSR_IA32_THERM_STATUS, msr_val);
+	therm_throt_process(msr_val & 0x1);
 }
 
 /* Thermal interrupt handler for this CPU setup */
@@ -123,7 +112,7 @@ static void intel_init_thermal(struct cpuinfo_x86 *c)
 	
 	rdmsr (MSR_IA32_MISC_ENABLE, l, h);
 	wrmsr (MSR_IA32_MISC_ENABLE, l | (1<<3), h);
-	
+
 	l = apic_read (APIC_LVTTHMR);
 	apic_write_around (APIC_LVTTHMR, l & ~APIC_LVT_MASKED);
 	printk (KERN_INFO "CPU%d: Thermal monitoring enabled\n", cpu);
