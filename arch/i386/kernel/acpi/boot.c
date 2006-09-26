83b630bfad5dd69d9175eda3013fbd05eee0e013
Adrian Bunk <bunk@stusta.de>
2006-09-26 10:52:35 +0200

    [PATCH] i386: Make acpi_force static
    acpi_force can become static.
    
    Cc: len.brown@intel.com
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/i386/kernel/acpi/boot.c
+++ arch/i386/kernel/acpi/boot.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/mpspec.h>
 
-int __initdata acpi_force = 0;
+static int __initdata acpi_force = 0;
 
 #ifdef	CONFIG_ACPI
 int acpi_disabled = 0;
