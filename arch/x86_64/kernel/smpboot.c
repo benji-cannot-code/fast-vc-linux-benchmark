de03e143c63ed6a05a20f7aa445def09ff70b298
Andi Kleen <ak@suse.de>
2006-09-26 10:52:30 +0200

    [PATCH] Remove obsolete PIC mode
    PIC mode is an outdated way to drive the APICs that was used on
    some early MP boards. It is not supported in the ACPI model.
    
    It is unlikely to be ever configured by any x86-64 system
    
    Remove it thus.
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/smpboot.c
+++ arch/x86_64/kernel/smpboot.c
@@ -1091,7 +1091,6 @@ void __init smp_prepare_cpus(unsigned int max_cpus)
 	/*
 	 * Switch from PIC to APIC mode.
 	 */
-	connect_bsp_APIC();
 	setup_local_APIC();
 
 	if (GET_APIC_ID(apic_read(APIC_ID)) != boot_cpu_id) {
