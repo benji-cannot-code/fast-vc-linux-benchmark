95b68a24cf6c7aa9b2fac3a78b6d25500d1ce7cc
Andi Kleen <ak@suse.de>
2006-09-26 10:52:27 +0200

    [PATCH] i386: Enable NMI watchdog by default
    I've had good experiences with having this on by default on x86-64.
    It turns nasty hangs into easier to debug oopses.
    
    Enable the local APIC wdog by default for systems newer than 2004.
    
    This comes from a strange compromise: according to arjan the reason
    it was off by default was some old IBM systems that corrupted
    registered when NMI happened in SMI. Can't remember more specific,
    but >= 2004 should avoid these. It's probably overly broad
    because most older systems should be ok (and the really old systems
    won't be supported by the local apic watchdog anyways)
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/i386/kernel/nmi.c
+++ arch/i386/kernel/nmi.c
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sysdev.h>
 #include <linux/sysctl.h>
 #include <linux/percpu.h>
+#include <linux/dmi.h>
 
 #include <asm/smp.h>
 #include <asm/nmi.h>
@@ -204,6 +205,14 @@ static int __init check_nmi_watchdog(void)
 	volatile int endflag = 0;
 	unsigned int *prev_nmi_count;
 	int cpu;
+
+	/* Enable NMI watchdog for newer systems.
+           Actually it should be safe for most systems before 2004 too except
+	   for some IBM systems that corrupt registers when NMI happens
+	   during SMM. Unfortunately we don't have more exact information
+ 	   on these and use this coarse check. */
+	if (nmi_watchdog == NMI_DEFAULT && dmi_get_year(DMI_BIOS_DATE) >= 2004)
+		nmi_watchdog = NMI_LOCAL_APIC;
 
 	if ((nmi_watchdog == NMI_NONE) || (nmi_watchdog == NMI_DEFAULT))
 		return 0;
