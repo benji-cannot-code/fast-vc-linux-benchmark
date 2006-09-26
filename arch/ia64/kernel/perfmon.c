aceb21f1308d66d73d25fcb5a2e591eabfbba1e4
Andrew Morton <akpm@osdl.org>
2006-09-25 23:32:33 -0700

    [PATCH] smp_call_function_single() cleanup
    If we're going to implement smp_call_function_single() on three architecture
    with the same prototype then it should have a declaration in a
    non-arch-specific header file.
    
    Move it into <linux/smp.h>.
    
    Cc: Stephane Eranian <eranian@hpl.hp.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ia64/kernel/perfmon.c
+++ arch/ia64/kernel/perfmon.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/file.h>
 #include <linux/poll.h>
 #include <linux/vfs.h>
+#include <linux/smp.h>
 #include <linux/pagemap.h>
 #include <linux/mount.h>
 #include <linux/bitops.h>
