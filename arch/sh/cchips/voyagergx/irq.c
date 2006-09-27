f3df66595e46c69ea867e9e06dd23d77c0f69a7f
Paul Mundt <lethal@linux-sh.org>
2006-09-27 16:48:46 +0900

    sh: VoyagerGX cleanups and 8250 UART support.
    This adds the VoyagerGX UART to the RTS7751R2D setup
    code, and cleans up a few build issues.
    
    Signed-off-by: Manuel Lauss <mano@roarinelk.homelinux.net>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/cchips/voyagergx/irq.c
+++ arch/sh/cchips/voyagergx/irq.c
@@ -33,8 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/io.h>
 #include <asm/irq.h>
-#include <asm/rts7751r2d/rts7751r2d.h>
-#include <asm/rts7751r2d/voyagergx_reg.h>
+#include <asm/mach/voyagergx_reg.h>
 
 static void disable_voyagergx_irq(unsigned int irq)
 {
