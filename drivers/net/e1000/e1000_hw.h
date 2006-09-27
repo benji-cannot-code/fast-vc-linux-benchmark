ab545a0606e0320e9cc249c46a0d1b42d0831891
Jeff Kirsher <jeffrey.t.kirsher@intel.com>
2006-09-27 12:53:51 -0700

    e1000: gather hardware bit tweaks.
    Several hardware bits were set all over the driver and have been
    consolidated into a single function.
    
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Auke Kok <auke-jan.h.kok@intel.com>

--- drivers/net/e1000/e1000_hw.h
+++ drivers/net/e1000/e1000_hw.h
@@ -1441,6 +1441,7 @@ struct e1000_hw {
     boolean_t tbi_compatibility_on;
     boolean_t laa_is_present;
     boolean_t phy_reset_disable;
+    boolean_t initialize_hw_bits_disable;
     boolean_t fc_send_xon;
     boolean_t fc_strict_ieee;
     boolean_t report_tx_early;
