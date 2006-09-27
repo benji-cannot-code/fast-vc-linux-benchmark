f26a66d1b42fe9827a38cd5c6971da6dac33aa63
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2006-09-27 01:50:31 -0700

    [PATCH] uml: fix proc-vs-interrupt context spinlock deadlock
    This spinlock can be taken on interrupt too, so spin_lock_irq[save] must be
    used.
    
    However, Documentation/networking/netdevices.txt explains we are called with
    rtnl_lock() held - so we don't need to care about other concurrent opens.
    Verified also in LDD3 and by direct checking.  Also verified that the network
    layer (through a state machine) guarantees us that nobody will close the
    interface while it's being used.  Please correct me if I'm wrong.
    
    Also, we must check we don't sleep with irqs disabled!!!  But anyway, this is
    not news - we already can't sleep while holding a spinlock.  Who says this is
    guaranted really by the present code?
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Cc: Jeff Dike <jdike@addtoit.com>
    Cc: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/drivers/net_kern.c
+++ arch/um/drivers/net_kern.c
@@ -115,8 +115,6 @@ static int uml_net_open(struct net_device *dev)
 	struct uml_net_private *lp = dev->priv;
 	int err;
 
-	spin_lock(&lp->lock);
-
 	if(lp->fd >= 0){
 		err = -ENXIO;
 		goto out;
@@ -150,8 +148,6 @@ static int uml_net_open(struct net_device *dev)
 	 */
 	while((err = uml_net_rx(dev)) > 0) ;
 
-	spin_unlock(&lp->lock);
-
 	spin_lock(&opened_lock);
 	list_add(&lp->list, &opened);
 	spin_unlock(&opened_lock);
@@ -161,7 +157,6 @@ out_close:
 	if(lp->close != NULL) (*lp->close)(lp->fd, &lp->user);
 	lp->fd = -1;
 out:
-	spin_unlock(&lp->lock);
 	return err;
 }
 
@@ -170,14 +165,11 @@ static int uml_net_close(struct net_device *dev)
 	struct uml_net_private *lp = dev->priv;
 	
 	netif_stop_queue(dev);
-	spin_lock(&lp->lock);
 
 	free_irq(dev->irq, dev);
 	if(lp->close != NULL)
 		(*lp->close)(lp->fd, &lp->user);
 	lp->fd = -1;
-
-	spin_unlock(&lp->lock);
 
 	spin_lock(&opened_lock);
 	list_del(&lp->list);
@@ -247,9 +239,9 @@ static int uml_net_set_mac(struct net_device *dev, void *addr)
 	struct uml_net_private *lp = dev->priv;
 	struct sockaddr *hwaddr = addr;
 
-	spin_lock(&lp->lock);
+	spin_lock_irq(&lp->lock);
 	set_ether_mac(dev, hwaddr->sa_data);
-	spin_unlock(&lp->lock);
+	spin_unlock_irq(&lp->lock);
 
 	return(0);
 }
@@ -259,7 +251,7 @@ static int uml_net_change_mtu(struct net_device *dev, int new_mtu)
 	struct uml_net_private *lp = dev->priv;
 	int err = 0;
 
-	spin_lock(&lp->lock);
+	spin_lock_irq(&lp->lock);
 
 	new_mtu = (*lp->set_mtu)(new_mtu, &lp->user);
 	if(new_mtu < 0){
@@ -270,7 +262,7 @@ static int uml_net_change_mtu(struct net_device *dev, int new_mtu)
 	dev->mtu = new_mtu;
 
  out:
-	spin_unlock(&lp->lock);
+	spin_unlock_irq(&lp->lock);
 	return err;
 }
 
