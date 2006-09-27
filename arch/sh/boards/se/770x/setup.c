a756444efc03837c4c1aac386db8df7146fac1b7
Paul Mundt <lethal@linux-sh.org>
2006-09-27 11:16:20 +0900

    sh: Move smc37c93x.h for SystemH board use.
    SystemH needs this header as well, not just 770x SE.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/se/770x/setup.c
+++ arch/sh/boards/se/770x/setup.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ide.h>
 #include <asm/io.h>
 #include <asm/se/se.h>
-#include <asm/se/smc37c93x.h>
+#include <asm/smc37c93x.h>
 
 /*
  * Configure the Super I/O chip
