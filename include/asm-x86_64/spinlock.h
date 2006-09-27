32b3050154d1f7d6fd706666f7dde0247de36e80
Andrew Morton <akpm@osdl.org>
2006-09-27 01:49:42 -0700

    [PATCH] fix x86_64-mm-spinlock-cleanup
    We need processor.h for cpu_relax().
    
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/spinlock.h
+++ include/asm-x86_64/spinlock.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atomic.h>
 #include <asm/rwlock.h>
 #include <asm/page.h>
+#include <asm/processor.h>
 
 /*
  * Your basic SMP spinlocks, allowing only a single CPU anywhere
