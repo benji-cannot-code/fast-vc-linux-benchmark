cdbe374d8e735d357df83619c77dbd5acb2f3808
Erez Zilber <erezz@voltaire.com>
2006-09-27 15:27:10 +0300

    IB/iser: Have iSER data transaction object point to iSER conn
    iSER uses a data transaction object (struct iser_dto) as part
    of its IB data descriptors (struct iser_desc) management.
    It also uses a hierarchy of connection structures pointing to
    each other. A DTO may exist even after the iscsi_iser connection
    pointed by it is destroyed (eg one that is bound to a post
    receive buffer which was flushed by the IB HW). Hence DTOs need
    point to the lowest connection, which is struct iser_conn.
    
    Signed-off-by: Erez Zilber <erezz@voltaire.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/ulp/iser/iscsi_iser.h
+++ drivers/infiniband/ulp/iser/iscsi_iser.h
@@ -193,7 +193,7 @@ struct iser_regd_buf {
 
 struct iser_dto {
 	struct iscsi_iser_cmd_task *ctask;
-	struct iscsi_iser_conn     *conn;
+	struct iser_conn *ib_conn;
 	int                        notify_enable;
 
 	/* vector of registered buffers */
