22029457edb712a42b63b662f0dd298366743fd3
Christoph Lameter <clameter@sgi.com>
2006-09-27 01:50:06 -0700

    [PATCH] Add NUMA_BUILD definition in kernel.h to avoid #ifdef CONFIG_NUMA
    The NUMA_BUILD constant is always available and will be set to 1 on
    NUMA_BUILDs.  That way checks valid only under CONFIG_NUMA can easily be done
    without #ifdef CONFIG_NUMA
    
    F.e.
    
    if (NUMA_BUILD && <numa_condition>) {
    ...
    }
    
    [akpm: not a thing we'd normally do, but CONFIG_NUMA is special: it is
     causing ifdef explosion in core kernel, so let's see if this is a comfortable
     way in whcih to control that]
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -943,7 +943,7 @@ get_page_from_freelist(gfp_t gfp_mask, unsigned int order,
 	 */
 	do {
 		zone = *z;
-		if (unlikely((gfp_mask & __GFP_THISNODE) &&
+		if (unlikely(NUMA_BUILD && (gfp_mask & __GFP_THISNODE) &&
 			zone->zone_pgdat != zonelist->zones[0]->zone_pgdat))
 				break;
 		if ((alloc_flags & ALLOC_CPUSET) &&
@@ -1257,14 +1257,12 @@ unsigned int nr_free_pagecache_pages(void)
 {
 	return nr_free_zone_pages(gfp_zone(GFP_HIGHUSER));
 }
-#ifdef CONFIG_NUMA
-static void show_node(struct zone *zone)
+
+static inline void show_node(struct zone *zone)
 {
-	printk("Node %ld ", zone_to_nid(zone));
+	if (NUMA_BUILD)
+		printk("Node %ld ", zone_to_nid(zone));
 }
-#else
-#define show_node(zone)	do { } while (0)
-#endif
 
 void si_meminfo(struct sysinfo *val)
 {
