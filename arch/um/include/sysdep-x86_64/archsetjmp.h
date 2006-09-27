9a3d7ea6fae86dfaccf1fff28363becb8cef211a
Jeff Dike <jdike@addtoit.com>
2006-09-27 01:50:40 -0700

    [PATCH] uml: thread creation tidying
    fork on UML has always somewhat subtle.  The underlying cause has been the
    need to initialize a stack for the new process.  The only portable way to
    initialize a new stack is to set it as the alternate signal stack and take a
    signal.  The signal handler does whatever initialization is needed and jumps
    back to the original stack, where the fork processing is finished.  The basic
    context switching mechanism is a jmp_buf for each process.  You switch to a
    new process by longjmping to its jmp_buf.
    
    Now that UML has its own implementation of setjmp and longjmp, and I can poke
    around inside a jmp_buf without fear that libc will change the structure, a
    much simpler mechanism is possible.  The jmpbuf can simply be initialized by
    hand.
    
    This eliminates -
    	the need to set up and remove the alternate signal stack
    	sending and handling a signal
    	the signal blocking needed around the stack switching, since
    there is no stack switching
    	setting up the jmp_buf needed to jump back to the original
    stack after the new one is set up
    
    In addition, since jmp_buf is now defined by UML, and not by libc, it can be
    embedded in the thread struct.  This makes it unnecessary to have it exist on
    the stack, where it used to be.  It also simplifies interfaces, since the
    switch jmp_buf used to be a void * inside the thread struct, and functions
    which took it as an argument needed to define a jmp_buf variable and assign it
    from the void *.
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Cc: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/include/sysdep-x86_64/archsetjmp.h
+++ arch/um/include/sysdep-x86_64/archsetjmp.h
@@ -19,4 +19,7 @@ struct __jmp_buf {
 
 typedef struct __jmp_buf jmp_buf[1];
 
+#define JB_IP __rip
+#define JB_SP __rsp
+
 #endif				/* _SETJMP_H */
--- include/asm-um/processor-generic.h
+++ include/asm-um/processor-generic.h
@@ -14,6 +14,7 @@ struct task_struct;
 #include "asm/ptrace.h"
 #include "choose-mode.h"
 #include "registers.h"
+#include "sysdep/archsetjmp.h"
 
 struct mm_struct;
 
@@ -44,8 +45,7 @@ struct thread_struct {
 #endif
 #ifdef CONFIG_MODE_SKAS
 		struct {
-			void *switch_buf;
-			void *fork_buf;
+			jmp_buf switch_buf;
 			int mm_count;
 		} skas;
 #endif
@@ -139,7 +139,7 @@ extern struct cpuinfo_um cpu_data[];
 
 #ifdef CONFIG_MODE_SKAS
 #define KSTK_REG(tsk, reg) \
-	get_thread_reg(reg, tsk->thread.mode.skas.switch_buf)
+	get_thread_reg(reg, &tsk->thread.mode.skas.switch_buf)
 #else
 #define KSTK_REG(tsk, reg) (0xbadbabe)
 #endif
