5dd7b6b5b7706202f78e149ef97bf0dc0f25a807
Paul Mundt <lethal@linux-sh.org>
2006-09-27 17:11:32 +0900

    sh: Rename rtc_get/set_time() to avoid RTC_CLASS conflict.
    We have a clash with RTC_CLASS over these names, so we
    change them..
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/rtc.h
+++ include/asm-sh/rtc.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void sh_rtc_gettimeofday(struct timespec *ts);
 extern int sh_rtc_settimeofday(const time_t secs);
 extern void (*board_time_init)(void);
-extern void (*rtc_get_time)(struct timespec *);
-extern int (*rtc_set_time)(const time_t);
+extern void (*rtc_sh_get_time)(struct timespec *);
+extern int (*rtc_sh_set_time)(const time_t);
 
 /* RCR1 Bits */
 #define RCR1_CF		0x80	/* Carry Flag             */
