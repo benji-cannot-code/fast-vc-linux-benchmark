238a66784590502adc73fc05b620723f201bbd93
Paul Mundt <lethal@linux-sh.org>
2006-09-27 14:02:09 +0900

    sh: RTS7751R2D board updates.
    More of the same, trivial cleanups, and moving options to their
    own board-specific Kconfig.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/renesas/rts7751r2d/mach.c
+++ arch/sh/boards/renesas/rts7751r2d/mach.c
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/machvec.h>
 #include <asm/rtc.h>
 #include <asm/irq.h>
-#include <asm/rts7751r2d/io.h>
+#include <asm/mach/rts7751r2d.h>
 
 extern void heartbeat_rts7751r2d(void);
 extern void init_rts7751r2d_IRQ(void);
@@ -55,7 +55,6 @@ struct sh_machine_vector mv_rts7751r2d __initmv = {
 	.mv_outsl		= rts7751r2d_outsl,
 
 	.mv_ioremap		= rts7751r2d_ioremap,
-	.mv_isa_port2addr	= rts7751r2d_isa_port2addr,
 	.mv_init_irq		= init_rts7751r2d_IRQ,
 #ifdef CONFIG_HEARTBEAT
 	.mv_heartbeat		= heartbeat_rts7751r2d,
