8a20e27909dbdd5d5312701b1382b3a92b09d9cf
Paul Mundt <lethal@linux-sh.org>
2006-09-27 13:42:57 +0900

    sh: Move hd64461.h to a more sensible location.
    With the I/O rework for hd64461 we're down to a single header,
    so move it by itself and get rid of the directory.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/cchips/hd6446x/hd64461/setup.c
+++ arch/sh/cchips/hd6446x/hd64461/setup.c
@@ -15,8 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/io.h>
 #include <asm/irq.h>
-
-#include <asm/hd64461/hd64461.h>
+#include <asm/hd64461.h>
 
 static void disable_hd64461_irq(unsigned int irq)
 {
--- include/asm-sh/hp6xx/io.h
+++ include/asm-sh/hp6xx/io.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Nothing special here.. just use the generic cchip io routines.
  */
-#include <asm/hd64461/io.h>
+#include <asm/hd64461.h>
 
 #endif /* __ASM_SH_HP6XX_IO_H */
 
