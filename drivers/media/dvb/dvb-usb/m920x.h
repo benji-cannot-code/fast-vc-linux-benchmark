92632413187203ab9e37f9dd56966cb240c43618
Michael Krufky <mkrufky@linuxtv.org>
2006-09-28 01:46:49 -0300

    V4L/DVB (5131): M920x: more cleanups
    Some cleanups and suggestions from Patrick Boettcher.
    Dropped the mutex in m9206_rc_query using #if 0, because
    M9206_CORE, M9206_I2C, M9206_FILTER and M9206_FW can be accessed
    concurrently.
    Thanks to both Aapo Tahkola and Patrick Boettcher.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/m920x.h
+++ drivers/media/dvb/dvb-usb/m920x.h
@@ -5,15 +5,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_USB_LOG_PREFIX "m920x"
 #include "dvb-usb.h"
 
-extern int dvb_usb_m920x_debug;
 #define deb_rc(args...)   dprintk(dvb_usb_m920x_debug,0x01,args)
 
 #define M9206_CORE	0x22
-#define M9206_RC_STATE		0xff51
-#define M9206_RC_KEY		0xff52
-#define M9206_RC_INIT1		0xff54
-#define M9206_RC_INIT2		0xff55
-#define M9206_FW_GO		0xff69
+#define M9206_RC_STATE	0xff51
+#define M9206_RC_KEY	0xff52
+#define M9206_RC_INIT1	0xff54
+#define M9206_RC_INIT2	0xff55
+#define M9206_FW_GO	0xff69
 
 #define M9206_I2C	0x23
 #define M9206_FILTER	0x25
@@ -25,5 +24,4 @@ struct m9206_state {
 	int filtering_enabled;
 	int rep_count;
 };
-
 #endif
