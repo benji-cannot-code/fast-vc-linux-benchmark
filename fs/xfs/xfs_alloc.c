60a189f3539873275cec41eb9a84c609e1ae2e20
Nathan Scott <nathans@sgi.com>
2006-09-28 11:03:44 +1000

    [XFS] Minor code rearranging and cleanup to prevent some coverity false positives.
    SGI-PV: 955502
    SGI-Modid: xfs-linux-melb:xfs-kern:26805a
    
    Signed-off-by: Nathan Scott <nathans@sgi.com>
    Signed-off-by: Tim Shimmin <tes@sgi.com>

--- fs/xfs/xfs_alloc.c
+++ fs/xfs/xfs_alloc.c
@@ -1478,8 +1478,10 @@ xfs_alloc_ag_vextent_small(
 	/*
 	 * Can't allocate from the freelist for some reason.
 	 */
-	else
+	else {
+		fbno = NULLAGBLOCK;
 		flen = 0;
+	}
 	/*
 	 * Can't do the allocation, give up.
 	 */
