3fa446158002fe5008717dce5f4cb7d732584a0f
Al Viro <viro@zeniv.linux.org.uk>
2006-09-27 18:48:18 -0700

    [XFRM]: xrfm_replay_check() annotations
    seq argument is net-endian
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/xfrm6_input.c
+++ net/ipv6/xfrm6_input.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ipv6.h>
 #include <net/xfrm.h>
 
-int xfrm6_rcv_spi(struct sk_buff *skb, u32 spi)
+int xfrm6_rcv_spi(struct sk_buff *skb, __be32 spi)
 {
 	int err;
 	__be32 seq;
