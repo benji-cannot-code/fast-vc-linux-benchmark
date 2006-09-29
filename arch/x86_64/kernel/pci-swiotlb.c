1a9c3aa01a47c2eadcb4f1548a4e9b008b09adb8
Rolf Eike Beer <eike-kernel@sf-tec.de>
2006-09-29 01:59:48 -0700

    [PATCH] Move valid_dma_direction() from x86_64 to generic code
    As suggested by Muli Ben-Yehuda this function is moved to generic code as
    may be useful for all archs.
    
    [akpm@osdl.org: fix]
    Signed-off-by: Rolf Eike Beer <eike-kernel@sf-tec.de>
    Cc: Muli Ben-Yehuda <muli@il.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/x86_64/kernel/pci-swiotlb.c
+++ arch/x86_64/kernel/pci-swiotlb.c
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/cache.h>
 #include <linux/module.h>
-#include <asm/dma-mapping.h>
+#include <linux/dma-mapping.h>
+
 #include <asm/proto.h>
 #include <asm/swiotlb.h>
 #include <asm/dma.h>
