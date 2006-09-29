c5fa7cc281239e22821a24cc79061c47998db90b
Johannes Berg <johannes@sipsolutions.net>
2006-09-29 02:00:14 -0700

    [PATCH] make leds.h include relevant headers
    Make it possible to include linux/leds.h without first including list.h and
    spinlock.h.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Acked-by: Richard Purdie <rpurdie@rpsys.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/leds.h
+++ include/linux/leds.h
@@ -13,6 +13,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_LEDS_H_INCLUDED
 #define __LINUX_LEDS_H_INCLUDED
 
+#include <linux/list.h>
+#include <linux/spinlock.h>
+
 struct device;
 struct class_device;
 /*
