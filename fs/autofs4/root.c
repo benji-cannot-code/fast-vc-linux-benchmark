213c504bf92f5555e6769e5902527a0245888e5c
Ian Kent <raven@themaw.net>
2006-09-29 02:00:54 -0700

    [PATCH] autofs4: pending flag not cleared on mount fail
    During testing I've found that the mount pending flag can be left set at
    exit from autofs4_lookup after a failed mount request.  This shouldn't be
    allowed to happen and causes incorrect error returns.
    
    Signed-off-by: Ian Kent <raven@themaw.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/autofs4/root.c
+++ fs/autofs4/root.c
@@ -282,9 +282,6 @@ static int try_to_fill_dentry(struct dentry *dentry, int flags)
 
 		DPRINTK("mount done status=%d", status);
 
-		if (status && dentry->d_inode)
-			return status; /* Try to get the kernel to invalidate this dentry */
-
 		/* Turn this into a real negative dentry? */
 		if (status == -ENOENT) {
 			spin_lock(&dentry->d_lock);
@@ -541,6 +538,9 @@ static struct dentry *autofs4_lookup(struct inode *dir, struct dentry *dentry, s
 			    return ERR_PTR(-ERESTARTNOINTR);
 			}
 		}
+		spin_lock(&dentry->d_lock);
+		dentry->d_flags &= ~DCACHE_AUTOFS_PENDING;
+		spin_unlock(&dentry->d_lock);
 	}
 
 	/*
