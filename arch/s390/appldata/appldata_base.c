8a049ef04c751ff046e1cdd898e1e47de0c1afa8
Heiko Carstens <heiko.carstens@de.ibm.com>
2006-09-29 01:58:41 -0700

    [PATCH] convert s390 page handling macros to functions
    Convert s390 page handling macros to functions.  In particular this fixes a
    problem with s390's SetPageUptodate macro which uses its input parameter
    twice which again can cause subtle bugs.
    
    [akpm@osdl.org: build fix]
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/s390/appldata/appldata_base.c
+++ arch/s390/appldata/appldata_base.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/interrupt.h>
 #include <linux/proc_fs.h>
-#include <linux/page-flags.h>
+#include <linux/mm.h>
 #include <linux/swap.h>
 #include <linux/pagemap.h>
 #include <linux/sysctl.h>
