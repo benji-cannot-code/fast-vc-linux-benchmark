e2974a6fee25997f98d0bd89b6bca7a693badd13
Alexey Dobriyan <adobriyan@gmail.com>
2006-09-29 02:00:18 -0700

    [PATCH] CONFIG_PM=n slim: sound/oss/cs46xx.c
    Remove some code which is unneeded if CONFIG_PM=n.
    
    * Make suspend/resume registration look like the rest of drivers:
      #ifdef CONFIG_PM in struct pci_driver, prototypes, actual hooks.
    * Drop CS46XX_ACPI_SUPPORT. It logically duplicated CONFIG_PM. It was
      hardcoded to 1 approx forever (ALSA merge just moved driver to
      sound/oss/).
    * After previous point, sound/oss/cs46xxpm-24.h removed as being useless.
    * As side effect selling (unused) static inline functions as suspend/resume
      hooks funkiness removed too.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- sound/oss/cs46xx.c
+++ sound/oss/cs46xx.c
@@ -97,7 +97,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/dma.h>
 #include <asm/uaccess.h>
 
-#include "cs46xxpm-24.h"
+#include "cs46xxpm.h"
 #include "cs46xx_wrapper-24.h"
 #include "cs461x.h"
 
@@ -390,8 +390,10 @@ static int cs_hardware_init(struct cs_card *card);
 static int cs46xx_powerup(struct cs_card *card, unsigned int type);
 static int cs461x_powerdown(struct cs_card *card, unsigned int type, int suspendflag);
 static void cs461x_clear_serial_FIFOs(struct cs_card *card, int type);
+#ifdef CONFIG_PM
 static int cs46xx_suspend_tbl(struct pci_dev *pcidev, pm_message_t state);
 static int cs46xx_resume_tbl(struct pci_dev *pcidev);
+#endif
 
 #if CSDEBUG
 
@@ -5390,8 +5392,10 @@ static struct pci_driver cs46xx_pci_driver = {
 	.id_table = cs46xx_pci_tbl,
 	.probe	  = cs46xx_probe,
 	.remove	  = __devexit_p(cs46xx_remove),
-	.suspend  = CS46XX_SUSPEND_TBL,
-	.resume	  = CS46XX_RESUME_TBL,
+#ifdef CONFIG_PM
+	.suspend  = cs46xx_suspend_tbl,
+	.resume	  = cs46xx_resume_tbl,
+#endif
 };
 
 static int __init cs46xx_init_module(void)
@@ -5421,7 +5425,7 @@ static void __exit cs46xx_cleanup_module(void)
 module_init(cs46xx_init_module);
 module_exit(cs46xx_cleanup_module);
 
-#if CS46XX_ACPI_SUPPORT
+#ifdef CONFIG_PM
 static int cs46xx_suspend_tbl(struct pci_dev *pcidev, pm_message_t state)
 {
 	struct cs_card *s = PCI_GET_DRIVER_DATA(pcidev);
