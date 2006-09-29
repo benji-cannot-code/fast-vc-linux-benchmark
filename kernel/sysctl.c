e733d870670c229152151bd45bf5eb1f99e81da0
Andi Kleen <ak@suse.de>
2006-09-30 01:47:55 +0200

    [PATCH] x86: Clean up x86 NMI sysctls
    Use prototypes in headers
    Don't define panic_on_unrecovered_nmi for all architectures
    
    Cc: dzickus@redhat.com
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -53,6 +53,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern int proc_nr_files(ctl_table *table, int write, struct file *filp,
                      void __user *buffer, size_t *lenp, loff_t *ppos);
 
+#ifdef CONFIG_X86
+#include <asm/nmi.h>
+#endif
+
 #if defined(CONFIG_SYSCTL)
 
 /* External variables not in a header file. */
@@ -74,13 +78,6 @@ extern int pid_max_min, pid_max_max;
 extern int sysctl_drop_caches;
 extern int percpu_pagelist_fraction;
 extern int compat_log;
-
-#if defined(CONFIG_X86_LOCAL_APIC) && defined(CONFIG_X86)
-int unknown_nmi_panic;
-int nmi_watchdog_enabled;
-extern int proc_nmi_enabled(struct ctl_table *, int , struct file *,
-			void __user *, size_t *, loff_t *);
-#endif
 
 /* this is needed for the proc_dointvec_minmax for [fs_]overflow UID and GID */
 static int maxolduid = 65535;
