f3d5678e36cad7a80f681a61c33e05161baa4117
Jens Axboe <axboe@kernel.dk>
2006-09-30 20:29:12 +0200

    [PATCH] Remove ->waiting member from struct request
    As the comments indicates in blkdev.h, we can fold it into ->end_io_data
    usage as that is really what ->waiting is. Fixup the users of
    blk_end_sync_rq().
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -267,7 +267,6 @@ struct request {
 	request_queue_t *q;
 	struct request_list *rl;
 
-	struct completion *waiting;
 	void *special;
 	char *buffer;
 
@@ -286,7 +285,7 @@ struct request {
 	int retries;
 
 	/*
-	 * completion callback. end_io_data should be folded in with waiting
+	 * completion callback.
 	 */
 	rq_end_io_fn *end_io;
 	void *end_io_data;
