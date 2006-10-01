70557edaf5fc669f68d23e3bf1e53d92345b9519
Sam Ravnborg <sam@neptun.ravnborg.org>
2006-10-01 11:48:53 +0200

    kconfig: fix saving alternate kconfig file in parent dir
    This fixes bugzilla entry: 7182
    http://bugzilla.kernel.org/show_bug.cgi?id=7182
    
    With this patch we no longer append the directory part twice
    before saving the config file.
    This patch has been sent to Roman Zippel for review with no feedback.
    It is so obviously simple that this should be OK to apply it anyway.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- scripts/kconfig/confdata.c
+++ scripts/kconfig/confdata.c
@@ -518,7 +518,7 @@ int conf_write(const char *name)
 	fclose(out);
 
 	if (*tmpname) {
-		strcat(dirname, name ? name : conf_get_configname());
+		strcat(dirname, basename);
 		strcat(dirname, ".old");
 		rename(newname, dirname);
 		if (rename(tmpname, newname))
