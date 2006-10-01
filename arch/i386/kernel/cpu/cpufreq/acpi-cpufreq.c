9542dc34497fcc94ff97cda4acbf2a0f916f07e0
Dave Jones <davej@redhat.com>
2006-09-30 23:19:38 -0400

    [CPUFREQ] Make acpi-cpufreq unsticky again.
    This caused suspend/resume regressions.
    
    Signed-off-by: Dave Jones <davej@redhat.com>

--- arch/i386/kernel/cpu/cpufreq/acpi-cpufreq.c
+++ arch/i386/kernel/cpu/cpufreq/acpi-cpufreq.c
@@ -598,7 +598,6 @@ static struct cpufreq_driver acpi_cpufreq_driver = {
 	.name	= "acpi-cpufreq",
 	.owner	= THIS_MODULE,
 	.attr	= acpi_cpufreq_attr,
-	.flags	= CPUFREQ_STICKY,
 };
 
 
@@ -609,7 +608,7 @@ acpi_cpufreq_init (void)
 
 	acpi_cpufreq_early_init_acpi();
 
- 	return cpufreq_register_driver(&acpi_cpufreq_driver);
+	return cpufreq_register_driver(&acpi_cpufreq_driver);
 }
 
 
