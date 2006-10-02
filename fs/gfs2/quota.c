a0229fec1c8c8ae75466c3643db23de8059af4d6
Steven Whitehouse <swhiteho@redhat.com>
2006-10-02 11:38:25 -0400

    [GFS2] Mark metadata reads for blktrace
    Mark the metadata reads so that blktrace knows what they are.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/quota.c
+++ fs/gfs2/quota.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/buffer_head.h>
 #include <linux/sort.h>
 #include <linux/fs.h>
+#include <linux/bio.h>
 #include <linux/gfs2_ondisk.h>
 #include <linux/lm_interface.h>
 
@@ -610,7 +611,7 @@ static int gfs2_adjust_quota(struct gfs2_inode *ip, loff_t loc,
 		set_buffer_uptodate(bh);
 
 	if (!buffer_uptodate(bh)) {
-		ll_rw_block(READ, 1, &bh);
+		ll_rw_block(READ_META, 1, &bh);
 		wait_on_buffer(bh);
 		if (!buffer_uptodate(bh))
 			goto unlock;
