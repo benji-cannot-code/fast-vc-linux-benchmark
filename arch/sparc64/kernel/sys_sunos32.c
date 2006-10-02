4e2f6b8febc0e22afd35ea75b54718d99ac0bb47
David S. Miller <davem@sunset.davemloft.net>
2006-10-02 14:17:57 -0700

    [SPARC64]: Move signal compat bits to new header file.
    Create asm-sparc64/compat_signal.h and stuff things there.
    
    This avoids the "linux/compat.h includes asm/signal.h but
    asm/signal.h needs compat_sigset_t which isn't defined yet"
    problems introduced recently.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/sys_sunos32.c
+++ arch/sparc64/kernel/sys_sunos32.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/idprom.h> /* for gethostid() */
 #include <asm/unistd.h>
 #include <asm/system.h>
+#include <asm/compat_signal.h>
 
 /* For the nfs mount emulation */
 #include <linux/socket.h>
