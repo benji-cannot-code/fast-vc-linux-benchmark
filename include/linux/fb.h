1f447b75c7321634f5cbab6fe522dc90caaf6e99
Dennis Munsie <dmunsie@cecropia.com>
2006-10-03 01:14:42 -0700

    [PATCH] fbdev: Add generic ddc read functionality
    Adds functionality to read the EDID information over the DDC bus in a generic
    way.  This code is based on the DDC implementation in the radeon driver.
    
    [adaplas]
    - separate from fbmon.c and place in new file fb_ddc.c
    - remove dependency to CONFIG_I2C and CONFIG_I2C_ALGOBIT, otherwise, feature
      will not compile if i2c support is compiled as a module
    - feature is selectable only by drivers needing it. It must have a
      'select FB_DDC if xxx' in Kconfig
    - change printk's to dev_*, the i2c people prefers it
    
    Signed-off-by: Dennis Munsie <dmunsie@cecropia.com>
    Signed-off-by: Antonino Daplas <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_FB_H
 
 #include <asm/types.h>
+#include <linux/i2c.h>
 
 /* Definitions of frame buffers						*/
 
@@ -941,6 +942,7 @@ extern void fb_edid_to_monspecs(unsigned char *edid,
 				struct fb_monspecs *specs);
 extern void fb_destroy_modedb(struct fb_videomode *modedb);
 extern int fb_find_mode_cvt(struct fb_videomode *mode, int margins, int rb);
+extern unsigned char *fb_ddc_read(struct i2c_adapter *adapter);
 
 /* drivers/video/modedb.c */
 #define VESA_MODEDB_SIZE 34
