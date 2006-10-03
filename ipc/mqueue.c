97fc17a968bd680802e297bfcba4c0a89e579e5e
Michal Wronski <michal.wronski@gmail.com>
2006-10-03 23:23:27 +0200

    Michal Wronski: update contact info
    My email has changed.
    
    Signed-Off-By: Michal Wronski <michal.wronski@gmail.com>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- ipc/mqueue.c
+++ ipc/mqueue.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * POSIX message queues filesystem for Linux.
  *
  * Copyright (C) 2003,2004  Krzysztof Benedyczak    (golbi@mat.uni.torun.pl)
- *                          Michal Wronski          (Michal.Wronski@motorola.com)
+ *                          Michal Wronski          (michal.wronski@gmail.com)
  *
  * Spinlocks:               Mohamed Abbas           (abbas.mohamed@intel.com)
  * Lockless receive & send, fd based notify:
