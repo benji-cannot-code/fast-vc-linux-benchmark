4b12795d164381a9628a688878657333f70323f2
Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
2006-10-03 01:14:54 -0700

    [PATCH] drivers/video/sis/init301.h: removal of old code
    Signed-off-by: Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Cc: Thomas Winischhofer <thomas@winischhofer.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/sis/init301.h
+++ drivers/video/sis/init301.h
@@ -72,15 +72,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <asm/io.h>
 #include <linux/fb.h>
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0)
-#include <video/fbcon.h>
-#endif
 #include "sis.h"
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,5,0)
-#include <linux/sisfb.h>
-#else
 #include <video/sisfb.h>
-#endif
 #endif
 
 static const unsigned char SiS_YPbPrTable[3][64] = {
