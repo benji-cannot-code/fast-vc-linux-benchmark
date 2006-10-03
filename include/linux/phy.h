a0204bc41cb3be04b509ccb01ee5eccb104d9bef
Maciej W. Rozycki <macro@linux-mips.org>
2006-10-03 16:18:28 +0100

    [PATCH] 2.6.18: sb1250-mac: Missing inclusions from <linux/phy.h>
     The <linux/phy.h> uses some types and macros defined in
    <linux/ethtool.h>, <linux/mii.h>, <linux/timer.h> and <linux/workqueue.h>,
    but fails to include these headers.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    
    patch-mips-2.6.18-20060920-include-phy-16
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/spinlock.h>
 #include <linux/device.h>
+#include <linux/ethtool.h>
+#include <linux/mii.h>
+#include <linux/timer.h>
+#include <linux/workqueue.h>
 
 #define PHY_BASIC_FEATURES	(SUPPORTED_10baseT_Half | \
 				 SUPPORTED_10baseT_Full | \
