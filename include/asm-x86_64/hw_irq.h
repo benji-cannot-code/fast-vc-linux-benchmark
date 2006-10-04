5cd11497dc4a471a33042e3e1532a0cb0fa7ddc5
Ingo Molnar <mingo@elte.hu>
2006-10-04 02:16:25 -0700

    [PATCH] genirq: convert the x86_64 architecture to irq-chips
    This patch converts all the x86_64 PIC controllers layers to the new and
    simpler irq-chip interrupt handling layer.
    
    [mingo@elte.hu: The patch also enables the fasteoi handler for x86_64]
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Roland Dreier <rolandd@cisco.com>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/hw_irq.h
+++ include/asm-x86_64/hw_irq.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <linux/profile.h>
 #include <linux/smp.h>
-
-struct hw_interrupt_type;
 #endif
 
 #define NMI_VECTOR		0x02
