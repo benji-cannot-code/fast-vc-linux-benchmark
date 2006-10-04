4a66d51874be6eb76e12f806a62379ee3c87e97b
Josh Triplett <josht@us.ibm.com>
2006-10-04 02:17:21 -0700

    [PATCH] RCU: CREDITS and MAINTAINERS
    Add MAINTAINERS entry for Read-Copy Update (RCU), listing Dipankar Sarma as
    maintainer, and giving the URL for Paul McKenney's RCU site.  Add
    MAINTAINERS entry for rcutorture, listing myself as maintainer.  Add
    CREDITS entries for developers of RCU, RCU variants, and rcutorture.  Use
    Paul McKenney's preferred email address in include/linux/rcupdate.h .
    
    Signed-off-by: Josh Triplett <josh@freedesktop.org>
    Cc: Paul McKenney <paulmck@us.ibm.com>
    Cc: Dipankar Sarma <dipankar@in.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Author: Dipankar Sarma <dipankar@in.ibm.com>
  * 
- * Based on the original work by Paul McKenney <paul.mckenney@us.ibm.com>
+ * Based on the original work by Paul McKenney <paulmck@us.ibm.com>
  * and inputs from Rusty Russell, Andrea Arcangeli and Andi Kleen.
  * Papers:
  * http://www.rdrop.com/users/paulmck/paper/rclockpdcsproof.pdf
