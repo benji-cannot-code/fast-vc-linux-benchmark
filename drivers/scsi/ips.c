22c146962496e0434d03279105059be5d0eadcdf
Henne <henne@nachtwindheim.de>
2006-10-04 10:22:09 +0200

    [SCSI] scsi: remove hosts.h
    Remove the obsolete hosts.h file under drivers/scsi.
    
    Signed-off-by: Henrik Kretzschmar <henne@nachtwindheim.de>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/ips.c
+++ drivers/scsi/ips.c
@@ -183,14 +183,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 
 #include <scsi/sg.h>
-
 #include "scsi.h"
-
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,5,0)
-#include "hosts.h"
-#else
 #include <scsi/scsi_host.h>
-#endif
 
 #include "ips.h"
 
