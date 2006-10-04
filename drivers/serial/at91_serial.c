cd692abe9b42703c8a717624c395ce225cfe4a93
Haavard Skinnemoen <hskinnemoen@atmel.com>
2006-10-04 16:02:00 +0200

    [PATCH] at91_serial -> atmel_serial: at91rm9200_usart.h
    Move include/asm/arch/at91rm9200_usart.h into drivers/serial and rename
    it atmel_usart.h. Also delete AVR32's version of this file.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Acked-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/serial/at91_serial.c
+++ drivers/serial/at91_serial.c
@@ -37,12 +37,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/io.h>
 
-#include <asm/arch/at91rm9200_usart.h>
 #include <asm/arch/at91rm9200_pdc.h>
 #include <asm/mach/serial_at91.h>
 #include <asm/arch/board.h>
 #include <asm/arch/system.h>
 #include <asm/arch/gpio.h>
+
+#include "atmel_serial.h"
 
 #if defined(CONFIG_SERIAL_AT91_CONSOLE) && defined(CONFIG_MAGIC_SYSRQ)
 #define SUPPORT_SYSRQ
--- drivers/serial/atmel_serial.h
+++ drivers/serial/atmel_serial.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * include/asm-arm/arch-at91rm9200/at91rm9200_usart.h
+ * drivers/serial/atmel_serial.h
  *
  * Copyright (C) 2005 Ivan Kokshaysky
  * Copyright (C) SAN People
