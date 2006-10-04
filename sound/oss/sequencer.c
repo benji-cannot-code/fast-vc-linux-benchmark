93d9f86388c30805830201889a1e2fc92add4fdc
Adrian Bunk <bunk@stusta.de>
2006-10-04 02:17:22 -0700

    [PATCH] The scheduled removal of some OSS drivers
    This patch contains the scheduled removal of OSS drivers that:
    - have ALSA drivers for the same hardware without known regressions and
    - whose Kconfig options have been removed in 2.6.17.
    
    [michal.k.k.piotrowski@gmail.com: build fix]
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- sound/oss/sequencer.c
+++ sound/oss/sequencer.c
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/kmod.h>
 #include <linux/spinlock.h>
-#define SEQUENCER_C
 #include "sound_config.h"
 
 #include "midi_ctrl.h"
