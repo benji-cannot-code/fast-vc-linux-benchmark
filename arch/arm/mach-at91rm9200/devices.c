3cea76d0b06ab30542f767b6847bf90696b0974e
Haavard Skinnemoen <hskinnemoen@atmel.com>
2006-10-04 16:02:03 +0200

    [PATCH] at91_serial -> atmel_serial: Platform device name
    Rename the "at91_usart" platform driver "atmel_usart" and update
    platform devices accordingly.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Acked-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/arm/mach-at91rm9200/devices.c
+++ arch/arm/mach-at91rm9200/devices.c
@@ -565,7 +565,7 @@ static struct at91_uart_data dbgu_data = {
 };
 
 static struct platform_device at91rm9200_dbgu_device = {
-	.name		= "at91_usart",
+	.name		= "atmel_usart",
 	.id		= 0,
 	.dev		= {
 				.platform_data	= &dbgu_data,
@@ -600,7 +600,7 @@ static struct at91_uart_data uart0_data = {
 };
 
 static struct platform_device at91rm9200_uart0_device = {
-	.name		= "at91_usart",
+	.name		= "atmel_usart",
 	.id		= 1,
 	.dev		= {
 				.platform_data	= &uart0_data,
@@ -642,7 +642,7 @@ static struct at91_uart_data uart1_data = {
 };
 
 static struct platform_device at91rm9200_uart1_device = {
-	.name		= "at91_usart",
+	.name		= "atmel_usart",
 	.id		= 2,
 	.dev		= {
 				.platform_data	= &uart1_data,
@@ -683,7 +683,7 @@ static struct at91_uart_data uart2_data = {
 };
 
 static struct platform_device at91rm9200_uart2_device = {
-	.name		= "at91_usart",
+	.name		= "atmel_usart",
 	.id		= 3,
 	.dev		= {
 				.platform_data	= &uart2_data,
@@ -718,7 +718,7 @@ static struct at91_uart_data uart3_data = {
 };
 
 static struct platform_device at91rm9200_uart3_device = {
-	.name		= "at91_usart",
+	.name		= "atmel_usart",
 	.id		= 4,
 	.dev		= {
 				.platform_data	= &uart3_data,
