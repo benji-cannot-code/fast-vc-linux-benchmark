9070552e2963d697975b3ba7072934888c6ce6f0
Jean Delvare <khali@linux-fr.org>
2006-10-04 18:38:16 +0200

    [ALSA] sound: Don't include i2c-dev.h
    Don't include <linux/i2c-dev.h> as it's not needed.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/aoa/codecs/snd-aoa-codec-onyx.h
+++ sound/aoa/codecs/snd-aoa-codec-onyx.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __SND_AOA_CODEC_ONYX_H
 #include <stddef.h>
 #include <linux/i2c.h>
-#include <linux/i2c-dev.h>
 #include <asm/pmac_low_i2c.h>
 #include <asm/prom.h>
 
