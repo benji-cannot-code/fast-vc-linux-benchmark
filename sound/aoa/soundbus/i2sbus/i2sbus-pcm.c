46fc36c5bfbaa7790bea5fbbe71bcd943e401d2c
Johannes Berg <johannes@sipsolutions.net>
2006-10-05 15:07:23 +0200

    [ALSA] aoa: set device pointer in pcms
    This patch makes a few whitespace cleanups and makes i2sbus assign the new
    struct device pointer in struct snd_pcm so that the proper device symlink
    shows up in sysfs.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/aoa/soundbus/i2sbus/i2sbus-pcm.c
+++ sound/aoa/soundbus/i2sbus/i2sbus-pcm.c
@@ -902,11 +902,7 @@ i2sbus_attach_codec(struct soundbus_dev *dev, struct snd_card *card,
 	}
 
 	if (!dev->pcm) {
-		err = snd_pcm_new(card,
-				  dev->pcmname,
-				  dev->pcmid,
-				  0,
-				  0,
+		err = snd_pcm_new(card, dev->pcmname, dev->pcmid, 0, 0,
 				  &dev->pcm);
 		if (err) {
 			printk(KERN_DEBUG "i2sbus: failed to create pcm\n");
@@ -916,6 +912,7 @@ i2sbus_attach_codec(struct soundbus_dev *dev, struct snd_card *card,
 			module_put(THIS_MODULE);
 			return err;
 		}
+		dev->pcm->dev = &dev->ofdev.dev;
 	}
 
 	/* ALSA yet again sucks.
