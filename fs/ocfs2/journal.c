742789ca917f8258adaf45deda56a4f90b209633
Mark Fasheh <mark.fasheh@oracle.com>
2006-10-05 13:54:39 -0700

    ocfs2: remove unused ocfs2_journal_handle field
    max_buffs was just being set and not actually used.
    
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/journal.c
+++ fs/ocfs2/journal.c
@@ -125,7 +125,6 @@ struct ocfs2_journal_handle *ocfs2_alloc_handle(struct ocfs2_super *osb)
 		return NULL;
 	}
 
-	retval->max_buffs = 0;
 	retval->num_locks = 0;
 	retval->k_handle = NULL;
 
@@ -172,8 +171,6 @@ struct ocfs2_journal_handle *ocfs2_start_trans(struct ocfs2_super *osb,
 		     "handle!\n");
 		goto done_free;
 	}
-
-	handle->max_buffs = max_buffs;
 
 	down_read(&osb->journal->j_trans_barrier);
 
@@ -354,9 +351,7 @@ int ocfs2_extend_trans(struct ocfs2_journal_handle *handle,
 			mlog_errno(status);
 			goto bail;
 		}
-		handle->max_buffs = nblocks;
-	} else
-		handle->max_buffs += nblocks;
+	}
 
 	status = 0;
 bail:
