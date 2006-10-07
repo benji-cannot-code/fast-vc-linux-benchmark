151fa242d1cece48ff20d9d2a609d6406725f93f
Matthew Wilcox <matthew@wil.cx>
2006-10-06 20:47:23 -0600

    Build fixes for struct pt_regs removal
    Signed-off-by: Matthew Wilcox <matthew@wil.cx>

--- arch/parisc/kernel/irq.c
+++ arch/parisc/kernel/irq.c
@@ -36,8 +36,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #undef PARISC_IRQ_CR16_COUNTS
 
-extern irqreturn_t timer_interrupt(int, void *, struct pt_regs *);
-extern irqreturn_t ipi_interrupt(int, void *, struct pt_regs *);
+extern irqreturn_t timer_interrupt(int, void *);
+extern irqreturn_t ipi_interrupt(int, void *);
 
 #define EIEM_MASK(irq)       (1UL<<(CPU_IRQ_MAX - irq))
 
@@ -376,7 +376,7 @@ void do_cpu_irq_mask(struct pt_regs *regs)
 		goto set_out;
 	}
 #endif
-	__do_IRQ(irq, regs);
+	__do_IRQ(irq);
 
  out:
 	irq_exit();
