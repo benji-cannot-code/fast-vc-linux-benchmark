f7f83df9ce08de395d56b3334045dfd34ec20c0b
Adrian Bunk <bunk@stusta.de>
2006-10-09 01:16:24 +0200

    USB: ftdi-elan.c: remove dead code
    The Coverity checker spotted this obviously dead code.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/misc/ftdi-elan.c
+++ drivers/usb/misc/ftdi-elan.c
@@ -514,8 +514,6 @@ static void ftdi_elan_respond_work(void *data)
                         ftdi->disconnected += 1;
                 } else if (retval == -ENODEV) {
                         ftdi->disconnected += 1;
-                } else if (retval == -ENODEV) {
-                        ftdi->disconnected += 1;
                 } else if (retval == -EILSEQ) {
                         ftdi->disconnected += 1;
                 } else {
