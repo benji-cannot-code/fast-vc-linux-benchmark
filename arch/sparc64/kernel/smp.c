82ef33cfedb59f7f1fb462efe1eb0f864f4ff681
Al Viro <viro@zeniv.linux.org.uk>
2006-10-08 08:23:28 -0400

    [PATCH] sparc64 pt_regs fixes
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/sparc64/kernel/smp.c
+++ arch/sparc64/kernel/smp.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/cpudata.h>
 
 #include <asm/irq.h>
+#include <asm/irq_regs.h>
 #include <asm/page.h>
 #include <asm/pgtable.h>
 #include <asm/oplib.h>
@@ -1188,6 +1189,7 @@ void smp_percpu_timer_interrupt(struct pt_regs *regs)
 	unsigned long compare, tick, pstate;
 	int cpu = smp_processor_id();
 	int user = user_mode(regs);
+	struct pt_regs *old_regs;
 
 	/*
 	 * Check for level 14 softint.
@@ -1204,8 +1206,9 @@ void smp_percpu_timer_interrupt(struct pt_regs *regs)
 		clear_softint(tick_mask);
 	}
 
+	old_regs = set_irq_regs(regs);
 	do {
-		profile_tick(CPU_PROFILING, regs);
+		profile_tick(CPU_PROFILING);
 		if (!--prof_counter(cpu)) {
 			irq_enter();
 
@@ -1237,6 +1240,7 @@ void smp_percpu_timer_interrupt(struct pt_regs *regs)
 				     : /* no outputs */
 				     : "r" (pstate));
 	} while (time_after_eq(tick, compare));
+	set_irq_regs(old_regs);
 }
 
 static void __init smp_setup_percpu_timer(void)
