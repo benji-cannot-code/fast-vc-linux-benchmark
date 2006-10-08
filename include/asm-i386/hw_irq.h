f3641800d391a3dffba1b31cb5f9195c49895f10
Eric W. Biederman <ebiederm@xmission.com>
2006-10-08 07:43:46 -0600

    [PATCH] i386/x86_64: Remove global IO_APIC_VECTOR
    Which vector an irq is assigned to now varies dynamically and is
    not needed outside of io_apic.c.  So remove the possibility
    of accessing the information outside of io_apic.c and remove
    the silly macro that makes looking for users of irq_vector
    difficult.
    
    The fact this compiles ensures there aren't any more pieces
    of the old CONFIG_PCI_MSI weirdness that I failed to remove.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/hw_irq.h
+++ include/asm-i386/hw_irq.h
@@ -27,9 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Interrupt entry/exit code at both C and assembly level
  */
 
-extern u8 irq_vector[NR_IRQ_VECTORS];
-#define IO_APIC_VECTOR(irq)	(irq_vector[irq])
-
 extern void (*interrupt[NR_IRQS])(void);
 
 #ifdef CONFIG_SMP
