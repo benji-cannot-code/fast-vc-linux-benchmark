65c41380a3d45561ba28a66972b3b253041a0da6
Al Viro <viro@ftp.linux.org.uk>
2006-10-09 02:11:47 +0100

    [PATCH] m68k/kernel/dma.c assumes !MMU_SUN3
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-m68k/dma-mapping.h
+++ include/asm-m68k/dma-mapping.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct scatterlist;
 
+#ifndef CONFIG_MMU_SUN3
 static inline int dma_supported(struct device *dev, u64 mask)
 {
 	return 1;
@@ -88,5 +89,9 @@ static inline int dma_mapping_error(dma_addr_t handle)
 {
 	return 0;
 }
+
+#else
+#include <asm-generic/dma-mapping-broken.h>
+#endif
 
 #endif  /* _M68K_DMA_MAPPING_H */
