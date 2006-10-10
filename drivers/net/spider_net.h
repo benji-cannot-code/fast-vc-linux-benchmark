f6750abc28367db696368932a3e3bdebd93bbe02
Linas Vepstas <linas@austin.ibm.com>
2006-10-10 15:56:04 -0500

    [PATCH] powerpc/cell spidernet ethtool -i version number info.
    This patch adds version information as reported by
    ethtool -i to the Spidernet driver.
    
    From: James K Lewis <jklewis@us.ibm.com>
    Signed-off-by: James K Lewis <jklewis@us.ibm.com>
    Signed-off-by: Linas Vepstas <linas@austin.ibm.com>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/spider_net.h
+++ drivers/net/spider_net.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SPIDER_NET_H
 #define _SPIDER_NET_H
 
+#define VERSION "1.1 A"
+
 #include "sungem_phy.h"
 
 extern int spider_net_stop(struct net_device *netdev);
