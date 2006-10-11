8f714b747d1314a6884b1dbf498eaab6dac4bc61
Luca Tettamanti <kronos.it@gmail.com>
2006-10-11 01:20:41 -0700

    [PATCH] Fix menuconfig build failure due to missing stdbool.h
    scripts/kconfig/lxdialog/util.c fails to build because it uses
    true/false without including stdbool.h:
    
    kronos:~/src/linux-2.6$ make O=../linux-build-git menuconfig
      GEN     /home/kronos/src/linux-build/Makefile
      HOSTCC  scripts/kconfig/lxdialog/util.o
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c: In function 'set_classic_theme':
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:68: error: 'true' undeclared (first use in this function)
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:68: error: (Each undeclared identifier is reported only once
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:68: error: for each function it appears in.)
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:70: error: 'false' undeclared (first use in this function)
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c: In function 'set_blackbg_theme':
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:101: error: 'true' undeclared (first use in this function)
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:102: error: 'false' undeclared (first use in this function)
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c: In function 'set_bluetitle_theme':
    /home/kronos/src/linux-2.6/scripts/kconfig/lxdialog/util.c:144: error: 'true' undeclared (first use in this function)
    make[2]: *** [scripts/kconfig/lxdialog/util.o] Error 1
    make[1]: *** [menuconfig] Error 2
    make: *** [menuconfig] Error 2
    
    Add <stdbool.h> to dialog.h to fix the breakage.
    
    Signed-off-by: Luca Tettamanti <kronos.it@gmail.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- scripts/kconfig/lxdialog/dialog.h
+++ scripts/kconfig/lxdialog/dialog.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #ifdef __sun__
 #define CURS_MACROS
