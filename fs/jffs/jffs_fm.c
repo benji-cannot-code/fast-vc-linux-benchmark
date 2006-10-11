59c4230e9e40da05e0943453be06b60cb4b89829
Artem Bityutskiy <dedekind@infradead.org>
2006-10-11 14:52:47 +0300

    [MTD] return error code from get_mtd_device()
    get_mtd_device() returns NULL in case of any failure. Teach it to return an
    error code instead. Fix all users as well.
    
    Signed-off-by: Artem Bityutskiy <dedekind@infradead.org>

--- fs/jffs/jffs_fm.c
+++ fs/jffs/jffs_fm.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 #include <linux/slab.h>
+#include <linux/err.h>
 #include <linux/blkdev.h>
 #include <linux/jffs.h>
 #include "jffs_fm.h"
@@ -105,7 +106,7 @@ jffs_build_begin(struct jffs_control *c, int unit)
 
 	mtd = get_mtd_device(NULL, unit);
 
-	if (!mtd) {
+	if (IS_ERR(mtd)) {
 		kfree(fmc);
 		DJM(no_jffs_fmcontrol--);
 		return NULL;
