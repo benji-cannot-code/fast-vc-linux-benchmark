be91654e48a562f56a32c31fc54ddd103eb0b296
Al Viro <viro@ftp.linux.org.uk>
2006-10-12 19:00:35 +0100

    [PATCH] fixing includes in alpha_ksyms.c
    kernel_execve() fallout
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/alpha/kernel/alpha_ksyms.c
+++ arch/alpha/kernel/alpha_ksyms.c
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/fpu.h>
 #include <asm/machvec.h>
 
-#include <asm/unistd.h>
+#include <linux/syscalls.h>
 
 /* these are C runtime functions with special calling conventions: */
 extern void __divl (void);
