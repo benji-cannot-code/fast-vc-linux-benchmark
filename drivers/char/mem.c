6288e7126f5fb491843884c0f23db1ab7b1c4aa0
Linus Torvalds <torvalds@g5.osdl.org>
2006-10-13 08:42:10 -0700

    Include proper header file for PFN_DOWN()
    The recent commit (99a10a60ba9bedcf5d70ef81414d3e03816afa3f) to fix up
    mmap_kmem() broke compiles because it used PFN_DOWN() without including
    <linux/pfn.h>.
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/backing-dev.h>
 #include <linux/bootmem.h>
 #include <linux/pipe_fs_i.h>
+#include <linux/pfn.h>
 
 #include <asm/uaccess.h>
 #include <asm/io.h>
