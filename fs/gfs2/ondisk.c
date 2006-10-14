35d59b16c5017ca0798728395f83db0b07fd8d67
Al Viro <viro@zeniv.linux.org.uk>
2006-10-13 21:07:22 -0400

    [GFS2] split and annotate gfs2_rgrp
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/ondisk.c
+++ fs/gfs2/ondisk.c
@@ -121,7 +121,7 @@ void gfs2_rindex_print(const struct gfs2_rindex *ri)
 	pv(ri, ri_bitbytes, "%u");
 }
 
-void gfs2_rgrp_in(struct gfs2_rgrp *rg, const void *buf)
+void gfs2_rgrp_in(struct gfs2_rgrp_host *rg, const void *buf)
 {
 	const struct gfs2_rgrp *str = buf;
 
@@ -132,7 +132,7 @@ void gfs2_rgrp_in(struct gfs2_rgrp *rg, const void *buf)
 	rg->rg_igeneration = be64_to_cpu(str->rg_igeneration);
 }
 
-void gfs2_rgrp_out(const struct gfs2_rgrp *rg, void *buf)
+void gfs2_rgrp_out(const struct gfs2_rgrp_host *rg, void *buf)
 {
 	struct gfs2_rgrp *str = buf;
 
