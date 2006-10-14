d8ebcbb81afcccc3a8fdb9ff61ea986c4417b98d
Michael Krufky <mkrufky@linuxtv.org>
2006-10-13 22:18:01 -0300

    V4L/DVB (5798): Dvb-pll: add support for Philips fcv1236d
    This patch adds support to the dvb-pll library for the
    Philips fcv1236d tuner, based on the FCV1236D datasheet.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/dvb-pll.h
+++ drivers/media/dvb/frontends/dvb-pll.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_PHILIPS_TD1316         19
 #define DVB_PLL_THOMSON_FE6600         20
 #define DVB_PLL_OPERA1                 21
+#define DVB_PLL_FCV1236D               22
 
 /**
  * Attach a dvb-pll to the supplied frontend structure.
