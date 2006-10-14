8fd0de7800a0799d228466bccb31ce22484ef517
Al Viro <viro@ftp.linux.org.uk>
2006-10-14 16:53:38 +0100

    [PATCH] sun3_ioremap() prototype
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-m68k/sun3mmu.h
+++ include/asm-m68k/sun3mmu.h
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __SUN3_MMU_H__
 #define __SUN3_MMU_H__
 
+#include <linux/types.h>
 #include <asm/movs.h>
 #include <asm/sun3-head.h>
 
@@ -161,7 +162,7 @@ static inline void sun3_put_context(unsigned char c)
 	return;
 }
 
-extern void *sun3_ioremap(unsigned long phys, unsigned long size,
+extern void __iomem *sun3_ioremap(unsigned long phys, unsigned long size,
 			  unsigned long type);
 
 extern int sun3_map_test(unsigned long addr, char *val);
