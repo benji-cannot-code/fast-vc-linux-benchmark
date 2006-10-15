7a9d781f19c57ab5358778e5abebaf1852b6046f
Ulrich Drepper <drepper@redhat.com>
2006-10-15 15:03:48 -0400

    [PATCH] make UML compile (FC6/x86-64)
    I need this patch to get a UML kernel to compile.  This is with the
    kernel headers in FC6 which are automatically generated from the kernel
    tree.  Some headers are missing but those files don't need them.  At
    least it appears so since the resuling kernel works fine.
    
    Tested on x86-64.
    
    Signed-off-by: Ulrich Drepper <drepper@redhat.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/include/kern_util.h
+++ arch/um/include/kern_util.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __KERN_UTIL_H__
 #define __KERN_UTIL_H__
 
-#include "linux/threads.h"
 #include "sysdep/ptrace.h"
 #include "sysdep/faultinfo.h"
 
