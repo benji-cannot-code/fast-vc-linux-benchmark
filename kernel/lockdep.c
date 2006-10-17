3ed91b59385129bb9eb589092eb85b518d5e5a14
Ingo Molnar <mingo@elte.hu>
2006-10-17 00:09:28 -0700

    [PATCH] lockdep: increase max allowed recursion depth
    In general, lockdep warnings are intended to be non-fatal, so I have put in
    various practical limits on internal data structure failure modes.  We haven't
    had a /single/ lockdep-internal crash ever since lockdep went upstream [the
    unwinder crashes are outside of lockdep], and that's largely due to the good
    internal checks it does.
    
    Recursion within the dependency graph is currently limited to 20, that's
    probably not enough on some many-CPU boxes - this patch doubles it to 40.  I
    have written the lockdep functions to have as small stackframes as possible,
    so 40 should be OK too.  (The practical recursion limit should be somewhere
    between 100 and 200 entries.  If we hit that then I'll change the algorithm to
    be iteration-based.  Graph walking logic is so easy to program via recursion,
    so i'd like to keep recursion as long as possible.)
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -576,6 +576,8 @@ static noinline int print_circular_bug_tail(void)
 	return 0;
 }
 
+#define RECURSION_LIMIT 40
+
 static int noinline print_infinite_recursion_bug(void)
 {
 	__raw_spin_unlock(&hash_lock);
@@ -596,7 +598,7 @@ check_noncircular(struct lock_class *source, unsigned int depth)
 	debug_atomic_inc(&nr_cyclic_check_recursions);
 	if (depth > max_recursion_depth)
 		max_recursion_depth = depth;
-	if (depth >= 20)
+	if (depth >= RECURSION_LIMIT)
 		return print_infinite_recursion_bug();
 	/*
 	 * Check this lock's dependency list:
@@ -646,7 +648,7 @@ find_usage_forwards(struct lock_class *source, unsigned int depth)
 
 	if (depth > max_recursion_depth)
 		max_recursion_depth = depth;
-	if (depth >= 20)
+	if (depth >= RECURSION_LIMIT)
 		return print_infinite_recursion_bug();
 
 	debug_atomic_inc(&nr_find_usage_forwards_checks);
@@ -685,7 +687,7 @@ find_usage_backwards(struct lock_class *source, unsigned int depth)
 
 	if (depth > max_recursion_depth)
 		max_recursion_depth = depth;
-	if (depth >= 20)
+	if (depth >= RECURSION_LIMIT)
 		return print_infinite_recursion_bug();
 
 	debug_atomic_inc(&nr_find_usage_backwards_checks);
