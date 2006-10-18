1aa569b17556272c8f9dad3ba1eb9349ce583d70
Al Viro <viro@zeniv.linux.org.uk>
2006-10-18 01:47:25 -0400

    [PATCH] severing module.h->sched.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- arch/i386/kernel/cpu/mcheck/therm_throt.c
+++ arch/i386/kernel/cpu/mcheck/therm_throt.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpu.h>
 #include <asm/cpu.h>
 #include <linux/notifier.h>
+#include <linux/jiffies.h>
 #include <asm/therm_throt.h>
 
 /* How long to wait between reporting thermal events */
