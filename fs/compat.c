32ff0e61adb28430a3b32f70af6ca4d05407a8e1
Al Viro <viro@zeniv.linux.org.uk>
2006-10-19 17:23:57 -0400

    [PATCH] severing skbuff.h -> poll.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/compat.c
+++ fs/compat.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rwsem.h>
 #include <linux/tsacct_kern.h>
 #include <linux/highmem.h>
+#include <linux/poll.h>
 #include <linux/mm.h>
 
 #include <net/sock.h>		/* siocdevprivate_ioctl */
--- include/net/udp.h
+++ include/net/udp.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/ip.h>
 #include <linux/ipv6.h>
 #include <linux/seq_file.h>
+#include <linux/poll.h>
 
 /**
  *	struct udp_skb_cb  -  UDP(-Lite) private variables
