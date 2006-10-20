7e17cc8aa08225d68b514946b80ae913ecb4d02f
Artem Bityutskiy <dedekind@infradead.org>
2006-10-20 11:23:56 +0300

    [MTD] NAND: nandsim: support subpage write
    As flash cannot do 0->1 bit transitions when programming, do not do this in
    the simulator too. This makes nandsim able to accept subpage writes.
    
    Signed-off-by: Artem Bityutskiy <dedekind@infradead.org>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- drivers/mtd/nand/nandsim.c
+++ drivers/mtd/nand/nandsim.c
@@ -853,6 +853,7 @@ static void erase_sector(struct nandsim *ns)
  */
 static int prog_page(struct nandsim *ns, int num)
 {
+	int i;
 	union ns_mem *mypage;
 	u_char *pg_off;
 
@@ -868,7 +869,8 @@ static int prog_page(struct nandsim *ns, int num)
 	}
 
 	pg_off = NS_PAGE_BYTE_OFF(ns);
-	memcpy(pg_off, ns->buf.byte, num);
+	for (i = 0; i < num; i++)
+		pg_off[i] &= ns->buf.byte[i];
 
 	return 0;
 }
