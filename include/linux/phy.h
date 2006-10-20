a87870dce249caf3fc5bb1eac2923ad4702b59cd
Andy Fleming <afleming@freescale.com>
2006-10-19 19:52:26 -0500

    [PATCH] Slight refactor of interrupt mapping for FSL parts
    * Cleaned up interrupt mapping a little by adding a helper
      function which parses the irq out of the device-tree, and puts
      it into a resource.
    * Changed the arch/ppc platform files to specify PHY_POLL, instead of -1
    * Changed the fixed phy to use PHY_IGNORE_INTERRUPT
    * Added ethtool.h and mii.h to phy.h includes
    
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/spinlock.h>
 #include <linux/device.h>
+#include <linux/ethtool.h>
+#include <linux/mii.h>
+#include <asm/irq.h>
 
 #define PHY_BASIC_FEATURES	(SUPPORTED_10baseT_Half | \
 				 SUPPORTED_10baseT_Full | \
