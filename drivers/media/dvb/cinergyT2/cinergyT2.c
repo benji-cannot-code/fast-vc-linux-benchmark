42d6fc7792174fe5be432f4867e485c6e535081d
Al Viro <viro@zeniv.linux.org.uk>
2006-10-20 15:17:02 -0400

    [PATCH] severing poll.h -> mm.h
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- drivers/media/dvb/cinergyT2/cinergyT2.c
+++ drivers/media/dvb/cinergyT2/cinergyT2.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/dvb/frontend.h>
 #include <linux/mutex.h>
+#include <linux/mm.h>
 
 #include "dmxdev.h"
 #include "dvb_demux.h"
--- sound/oss/cs46xx.c
+++ sound/oss/cs46xx.c
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/poll.h>
 #include <linux/ac97_codec.h>
 #include <linux/mutex.h>
+#include <linux/mm.h>
 
 #include <asm/io.h>
 #include <asm/dma.h>
