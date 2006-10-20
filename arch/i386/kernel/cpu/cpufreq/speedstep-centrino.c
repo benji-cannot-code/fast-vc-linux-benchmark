a758124ee221f9bb7bd82380cb944708fb5e2fef
Andrew Morton <akpm@osdl.org>
2006-10-20 14:31:01 -0700

    [CPUFREQ] speedstep-centrino: remove dead code
    arch/i386/kernel/cpu/cpufreq/speedstep-centrino.c:396: warning: 'sw_any_bug_dmi_table' defined but not used
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- arch/i386/kernel/cpu/cpufreq/speedstep-centrino.c
+++ arch/i386/kernel/cpu/cpufreq/speedstep-centrino.c
@@ -393,7 +393,7 @@ static int sw_any_bug_found(struct dmi_system_id *d)
 	return 0;
 }
 
-
+#ifdef CONFIG_SMP
 static struct dmi_system_id sw_any_bug_dmi_table[] = {
 	{
 		.callback = sw_any_bug_found,
@@ -406,7 +406,7 @@ static struct dmi_system_id sw_any_bug_dmi_table[] = {
 	},
 	{ }
 };
-
+#endif
 
 /*
  * centrino_cpu_init_acpi - register with ACPI P-States library
