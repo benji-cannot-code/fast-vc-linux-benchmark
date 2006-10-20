d0a2c2d487d775b37cd9bbd605b3cf0e4e2e74ed
Paul Mundt <lethal@linux-sh.org>
2006-10-20 15:30:55 +0900

    sh: Convert INTC2 to IRQ table registration.
    Currently the INTC2 code contains a fixed IRQ table that it
    iterates through to set the handler type, we move this in to
    the CPU subtype setup code instead and allow for submitting
    the table that way.
    
    This drops the ST40 tables, as nothing has been happening
    with those processors, while converting the only existing
    users to use the new table directly (SH7760 and SH7780).
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/r7780rp.h
+++ include/asm-sh/r7780rp.h
@@ -82,7 +82,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IRQ_PCISLOT2    66              /* PCI Slot #2 IRQ */
 #define IRQ_PCISLOT3    67              /* PCI Slot #3 IRQ */
 #define IRQ_PCISLOT4    68              /* PCI Slot #4 IRQ */
-#define IRQ_CFCARD      1               /* CF Card IRQ */
 // #define IRQ_CFINST   0               /* CF Card Insert IRQ */
 #define IRQ_TP          2               /* Touch Panel IRQ */
 #define IRQ_SCI1        3               /* SCI1 IRQ */
@@ -154,7 +153,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define IRQ_PCISLOT2	1		/* PCI Slot #2 IRQ */
 #define IRQ_PCISLOT3	2		/* PCI Slot #3 IRQ */
 #define IRQ_PCISLOT4	3		/* PCI Slot #4 IRQ */
-#define IRQ_CFCARD	4		/* CF Card IRQ */
 #define IRQ_CFINST	5		/* CF Card Insert IRQ */
 #define IRQ_M66596	6		/* M66596 IRQ */
 #define IRQ_SDCARD	7		/* SD Card IRQ */
