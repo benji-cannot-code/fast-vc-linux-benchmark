c2ef9834cf8006f551771d7e3dbe5902ac9ba004
Pierre Ossman <drzeus@drzeus.cx>
2006-10-22 22:13:10 +0200

    mmc: Fix mmc_delay() function
    Several fixes for mmc_delay():
    
     * Repair if-clause that was supposed to detect sub-hz delays.
     * Change yield() to cond_resched() as yield() no longer has the
       semantics we desire.
     * mmc_delay() is used to guarantee protocol delays, so we cannot
       return early (i.e. use _interruptable).
    
    Based on patch by Amol Lad.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -455,11 +455,11 @@ static void mmc_deselect_cards(struct mmc_host *host)
 
 static inline void mmc_delay(unsigned int ms)
 {
-	if (ms < HZ / 1000) {
-		yield();
+	if (ms < 1000 / HZ) {
+		cond_resched();
 		mdelay(ms);
 	} else {
-		msleep_interruptible (ms);
+		msleep(ms);
 	}
 }
 
