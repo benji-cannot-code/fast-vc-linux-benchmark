3b298b26cd5f6bc430765be265478c56891d8141
Stephen Hemminger <shemminger@osdl.org>
2006-10-26 15:46:55 -0700

    netpoll queue cleanup
    The beast had a long and not very happy history. At one
    point, a friend (netdump) had asked that he open up a little.
    Well, the friend was long gone now, and the beast had
    this dangling piece hanging (netpoll_queue).
    
    It wasn't hard to stitch the netpoll_queue back in
    where it belonged and make everything tidy.
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>

--- drivers/net/netconsole.c
+++ drivers/net/netconsole.c
@@ -61,7 +61,6 @@ static struct netpoll np = {
 	.local_port = 6665,
 	.remote_port = 6666,
 	.remote_mac = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
-	.drop = netpoll_queue,
 };
 static int configured = 0;
 
