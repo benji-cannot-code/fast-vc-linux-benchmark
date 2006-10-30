3a822c80f672697bac6a8aad0777280cb40e9ba9
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2006-10-29 22:46:41 -0800

    [PATCH] Fix "Remove the use of _syscallX macros in UML"
    Fix commit 5f4c6bc1f369f20807a8e753c2308d1629478c61: it spits out warnings
    about missing syscall prototype (it is in <unistd.h>) and it does not
    recognize that two uses of _syscallX are to be resolved against kernel
    headers in the source tree, not against _syscallX; they in fact do not
    compile and would not work anyway.
    
    If _syscallX macros will be removed from the kernel tree altogether, the
    only reasonable solution for that piece of code is switching to open-coded
    inline assembly (it's remapping the whole executable from memory, except
    the page containing this code).
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Cc: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/os-Linux/tls.c
+++ arch/um/os-Linux/tls.c
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <errno.h>
 #include <sys/ptrace.h>
 #include <sys/syscall.h>
+#include <unistd.h>
 #include <asm/ldt.h>
 #include "sysdep/tls.h"
 #include "uml-config.h"
