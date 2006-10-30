7d0589b3c0e814059b8f3e6df94435e2c0632686
Darrick J. Wong <djwong@us.ibm.com>
2006-10-30 15:18:50 -0800

    [SCSI] libsas: add sas_abort_task
    This patch adds an external function, sas_abort_task, to enable LLDDs
    to abort sas_tasks.  It also adds a work_struct so that the actual
    work of aborting a task can be shifted from tasklet context (in the
    LLDD) onto the scsi_host's workqueue.
    
    Signed-off-by: Darrick J. Wong <djwong@us.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- include/scsi/libsas.h
+++ include/scsi/libsas.h
@@ -529,6 +529,8 @@ struct sas_task {
 
 	void   *lldd_task;	  /* for use by LLDDs */
 	void   *uldd_task;
+
+	struct work_struct abort_work;
 };
 
 
@@ -627,5 +629,7 @@ int  sas_discover_end_dev(struct domain_device *);
 void sas_unregister_dev(struct domain_device *);
 
 void sas_init_dev(struct domain_device *);
+
+void sas_task_abort(struct sas_task *task);
 
 #endif /* _SASLIB_H_ */
