24100ea95eb691dd0368bf05ae6c0a5509829d0e
Oleg Nesterov <oleg@tv-sign.ru>
2006-10-30 22:07:15 -0800

    [PATCH] taskstats: fix sub-threads accounting
    If there are no listeners, taskstats_exit_send() just returns because
    taskstats_exit_alloc() didn't allocate *tidstats.  This is wrong, each
    sub-thread should do fill_tgid_exit() on exit, otherwise its ->delays is
    not recorded in ->signal->stats and lost.
    
    Q: We don't send TASKSTATS_TYPE_AGGR_TGID when single-threaded process
    exits.  Is it good?  How can the listener figure out that it was actually a
    process exit, not sub-thread?
    
    Signed-off-by: Oleg Nesterov <oleg@tv-sign.ru>
    Cc: Balbir Singh <balbir@in.ibm.com>
    Acked-by: Shailabh Nagar <nagar@watson.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/taskstats.c
+++ kernel/taskstats.c
@@ -456,10 +456,9 @@ void taskstats_exit_send(struct task_struct *tsk, struct taskstats *tidstats,
 	int is_thread_group;
 	struct nlattr *na;
 
-	if (!family_registered || !tidstats)
+	if (!family_registered)
 		return;
 
-	rc = 0;
 	/*
 	 * Size includes space for nested attributes
 	 */
@@ -467,8 +466,15 @@ void taskstats_exit_send(struct task_struct *tsk, struct taskstats *tidstats,
 		nla_total_size(sizeof(struct taskstats)) + nla_total_size(0);
 
 	is_thread_group = (tsk->signal->stats != NULL);
-	if (is_thread_group)
-		size = 2 * size;	/* PID + STATS + TGID + STATS */
+	if (is_thread_group) {
+		/* PID + STATS + TGID + STATS */
+		size = 2 * size;
+		/* fill the tsk->signal->stats structure */
+		fill_tgid_exit(tsk);
+	}
+
+	if (!tidstats)
+		return;
 
 	rc = prepare_reply(NULL, TASKSTATS_CMD_NEW, &rep_skb, &reply, size);
 	if (rc < 0)
@@ -488,11 +494,8 @@ void taskstats_exit_send(struct task_struct *tsk, struct taskstats *tidstats,
 		goto send;
 
 	/*
-	 * tsk has/had a thread group so fill the tsk->signal->stats structure
 	 * Doesn't matter if tsk is the leader or the last group member leaving
 	 */
-
-	fill_tgid_exit(tsk);
 	if (!group_dead)
 		goto send;
 
