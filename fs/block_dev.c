6e7da5e2c9e19ab7f801c708d7fef726e0e53d4d
Andrew Morton <akpm@osdl.org>
2006-10-30 22:07:03 -0800

    [PATCH] find_bd_holder() fix
    fs/block_dev.c: In function 'find_bd_holder':
    fs/block_dev.c:666: warning: return makes integer from pointer without a cast
    fs/block_dev.c:669: warning: return makes integer from pointer without a cast
    fs/block_dev.c: In function 'add_bd_holder':
    fs/block_dev.c:685: warning: unused variable 'tmp'
    fs/block_dev.c: In function 'bd_claim_by_kobject':
    fs/block_dev.c:773: warning: assignment makes pointer from integer without a cast
    
    Acked-by: Jun'ichi Nomura <j-nomura@ce.jp.nec.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/block_dev.c
+++ fs/block_dev.c
@@ -652,7 +652,8 @@ static void free_bd_holder(struct bd_holder *bo)
  * If found, increment the reference count and return the pointer.
  * If not found, returns NULL.
  */
-static int find_bd_holder(struct block_device *bdev, struct bd_holder *bo)
+static struct bd_holder *find_bd_holder(struct block_device *bdev,
+					struct bd_holder *bo)
 {
 	struct bd_holder *tmp;
 
@@ -678,7 +679,6 @@ static int find_bd_holder(struct block_device *bdev, struct bd_holder *bo)
  */
 static int add_bd_holder(struct block_device *bdev, struct bd_holder *bo)
 {
-	struct bd_holder *tmp;
 	int ret;
 
 	if (!bo)
