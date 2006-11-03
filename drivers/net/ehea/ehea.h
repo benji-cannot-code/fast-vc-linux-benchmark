f097167e7007458a7ffbda6b8776729d140d5a4c
Thomas Klein <osstklei@de.ibm.com>
2006-11-03 17:47:52 +0100

    [PATCH] ehea: Removed redundant define
    Removed define H_CB_ALIGNMENT which is already defined in include/asm-powerpc/hvcall.h
    
    Signed-off-by: Thomas Klein <tklein@de.ibm.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/ehea/ehea.h
+++ drivers/net/ehea/ehea.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 
 #define DRV_NAME	"ehea"
-#define DRV_VERSION	"EHEA_0034"
+#define DRV_VERSION	"EHEA_0043"
 
 #define EHEA_MSG_DEFAULT (NETIF_MSG_LINK | NETIF_MSG_TIMER \
 	| NETIF_MSG_RX_ERR | NETIF_MSG_TX_ERR)
@@ -105,9 +105,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EHEA_BCMC_TAGGED	0x00
 #define EHEA_BCMC_VLANID_ALL	0x01
 #define EHEA_BCMC_VLANID_SINGLE	0x00
-
-/* Use this define to kmallocate pHYP control blocks */
-#define H_CB_ALIGNMENT		4096
 
 #define EHEA_CACHE_LINE          128
 
