61b7f7ef14929a62b63f08786e921879199f102d
Ryan Bradetich <rbrad@parisc-linux.org>
2006-11-03 05:38:39 +0000

    [PARISC] [MUX] Mux driver bug fix
    This patch addresses the problems identified by Russell King in the
    following email:
    
    http://lists.parisc-linux.org/pipermail/parisc-linux/2005-December/027912.html
    
    Signed-off-by: Ryan Bradetich <rbrad@parisc-linux.org>
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- drivers/serial/mux.c
+++ drivers/serial/mux.c
@@ -52,7 +52,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MUX_NR 256
 static unsigned int port_cnt __read_mostly;
-static struct uart_port mux_ports[MUX_NR];
+struct mux_port {
+	struct uart_port port;
+	int enabled;
+};
+static struct mux_port mux_ports[MUX_NR];
 
 static struct uart_driver mux_driver = {
 	.owner = THIS_MODULE,
@@ -251,7 +255,7 @@ static void mux_read(struct uart_port *port)
  */
 static int mux_startup(struct uart_port *port)
 {
-	mod_timer(&mux_timer, jiffies + MUX_POLL_DELAY);
+	mux_ports[port->line].enabled = 1;
 	return 0;
 }
 
@@ -263,6 +267,7 @@ static int mux_startup(struct uart_port *port)
  */
 static void mux_shutdown(struct uart_port *port)
 {
+	mux_ports[port->line].enabled = 0;
 }
 
 /**
@@ -320,7 +325,7 @@ static int mux_request_port(struct uart_port *port)
  * @port: Ptr to the uart_port.
  * @type: Bitmask of required configurations.
  *
- * Perform any autoconfiguration steps for the port.  This functino is
+ * Perform any autoconfiguration steps for the port.  This function is
  * called if the UPF_BOOT_AUTOCONF flag is specified for the port.
  * [Note: This is required for now because of a bug in the Serial core.
  *  rmk has already submitted a patch to linus, should be available for
@@ -358,11 +363,11 @@ static void mux_poll(unsigned long unused)
 	int i;
 
 	for(i = 0; i < port_cnt; ++i) {
-		if(!mux_ports[i].info)
+		if(!mux_ports[i].enabled)
 			continue;
 
-		mux_read(&mux_ports[i]);
-		mux_write(&mux_ports[i]);
+		mux_read(&mux_ports[i].port);
+		mux_write(&mux_ports[i].port);
 	}
 
 	mod_timer(&mux_timer, jiffies + MUX_POLL_DELAY);
@@ -457,7 +462,7 @@ static int __init mux_probe(struct parisc_device *dev)
 	}
 
 	for(i = 0; i < ports; ++i, ++port_cnt) {
-		port = &mux_ports[port_cnt];
+		port = &mux_ports[port_cnt].port;
 		port->iobase	= 0;
 		port->mapbase	= dev->hpa.start + MUX_OFFSET +
 						(i * MUX_LINE_OFFSET);
@@ -485,6 +490,8 @@ static int __init mux_probe(struct parisc_device *dev)
 #ifdef CONFIG_SERIAL_MUX_CONSOLE
         register_console(&mux_console);
 #endif
+	mod_timer(&mux_timer, jiffies + MUX_POLL_DELAY);
+
 	return 0;
 }
 
@@ -521,9 +528,9 @@ static void __exit mux_exit(void)
 	int i;
 
 	for (i = 0; i < port_cnt; i++) {
-		uart_remove_one_port(&mux_driver, &mux_ports[i]);
-		if (mux_ports[i].membase)
-			iounmap(mux_ports[i].membase);
+		uart_remove_one_port(&mux_driver, &mux_ports[i].port);
+		if (mux_ports[i].port.membase)
+			iounmap(mux_ports[i].port.membase);
 	}
 
 	uart_unregister_driver(&mux_driver);
