5377d4002af91f9fb7dff5a9254e65d2cf839000
Takashi Iwai <tiwai@suse.de>
2006-11-06 15:38:55 +0100

    [ALSA] hdspm - Fix printk warnings
    sound/pci/rme9652/hdspm.c: In function 'snd_hdspm_hw_params':
    sound/pci/rme9652/hdspm.c:3681: warning: format '%08X' expects type 'unsigned int', but argument 4 has type 'unsigned char *'
    sound/pci/rme9652/hdspm.c:3692: warning: format '%08X' expects type 'unsigned int', but argument 4 has type 'unsigned char *'
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/rme9652/hdspm.c
+++ sound/pci/rme9652/hdspm.c
@@ -3679,7 +3679,7 @@ static int snd_hdspm_hw_params(struct snd_pcm_substream *substream,
 
 		hdspm->playback_buffer =
 		    (unsigned char *) substream->runtime->dma_area;
-		snd_printdd("Allocated sample buffer for playback at 0x%08X\n",
+		snd_printdd("Allocated sample buffer for playback at %p\n",
 				hdspm->playback_buffer);
 	} else {
 		hdspm_set_sgbuf(hdspm, sgbuf, HDSPM_pageAddressBufferIn,
@@ -3690,7 +3690,7 @@ static int snd_hdspm_hw_params(struct snd_pcm_substream *substream,
 
 		hdspm->capture_buffer =
 		    (unsigned char *) substream->runtime->dma_area;
-		snd_printdd("Allocated sample buffer for capture at 0x%08X\n",
+		snd_printdd("Allocated sample buffer for capture at %p\n",
 				hdspm->capture_buffer);
 	}
 	/*
