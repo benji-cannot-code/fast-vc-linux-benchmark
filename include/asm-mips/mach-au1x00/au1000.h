fc0ddadb01ccf2999ccc313bb0c827921e29be4f
Ralf Baechle <ralf@linux-mips.org>
2006-11-07 09:58:30 +0000

    [MIPS] Alchemy: Fix bunch of warnings
      CC      arch/mips/au1000/common/pci.o
    arch/mips/au1000/common/pci.c:42: warning: large integer implicitly truncated to unsigned type
    arch/mips/au1000/common/pci.c:43: warning: large integer implicitly truncated to unsigned type
    arch/mips/au1000/common/pci.c:49: warning: large integer implicitly truncated to unsigned type
    arch/mips/au1000/common/pci.c:50: warning: large integer implicitly truncated to unsigned type
    arch/mips/au1000/common/pci.c: In function ‘au1x_pci_setup’:
    arch/mips/au1000/common/pci.c:82: warning: ISO C90 forbids mixed declarations and code
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-au1x00/au1000.h
+++ include/asm-mips/mach-au1x00/au1000.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LANGUAGE_ASSEMBLY
 
 #include <linux/delay.h>
+#include <linux/types.h>
 #include <asm/io.h>
 
 /* cpu pipeline flush */
@@ -1665,12 +1666,12 @@ extern au1xxx_irq_map_t au1xxx_irq_map[];
  * addresses.  For PCI IO, it's simpler because we get to do the ioremap
  * ourselves and then adjust the device's resources.
  */
-#define Au1500_EXT_CFG            0x600000000ULL
-#define Au1500_EXT_CFG_TYPE1      0x680000000ULL
-#define Au1500_PCI_IO_START       0x500000000ULL
-#define Au1500_PCI_IO_END         0x5000FFFFFULL
-#define Au1500_PCI_MEM_START      0x440000000ULL
-#define Au1500_PCI_MEM_END        0x44FFFFFFFULL
+#define Au1500_EXT_CFG            ((resource_size_t) 0x600000000ULL)
+#define Au1500_EXT_CFG_TYPE1      ((resource_size_t) 0x680000000ULL)
+#define Au1500_PCI_IO_START       ((resource_size_t) 0x500000000ULL)
+#define Au1500_PCI_IO_END         ((resource_size_t) 0x5000FFFFFULL)
+#define Au1500_PCI_MEM_START      ((resource_size_t) 0x440000000ULL)
+#define Au1500_PCI_MEM_END        ((resource_size_t) 0x44FFFFFFFULL)
 
 #define PCI_IO_START    (Au1500_PCI_IO_START + 0x1000)
 #define PCI_IO_END      (Au1500_PCI_IO_END)
