7c2dc8eb2f59715550b7501bd2d9087f1fcae389
Michael Krufky <mkrufky@linuxtv.org>
2006-11-08 16:47:32 -0300

    V4L/DVB (4805): Dib0700: Add support for Leadtek Winfast DTV Dongle (STK7700P based)
    This patch adds support for the new, STK7700-based revision
    of the Leadtek Winfast DTV Dongle.
    
    Signed-off-by: Michal CIJOML Semler <cijoml@volny.cz>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dib0700_devices.c
+++ drivers/media/dvb/dvb-usb/dib0700_devices.c
@@ -278,6 +278,7 @@ struct usb_device_id dib0700_usb_id_table[] = {
 		{ USB_DEVICE(USB_VID_AVERMEDIA, USB_PID_AVERMEDIA_VOLAR) },
 		{ USB_DEVICE(USB_VID_COMPRO,    USB_PID_COMPRO_VIDEOMATE_U500) },
 		{ USB_DEVICE(USB_VID_UNIWILL,   USB_PID_UNIWILL_STK7700P) },
+		{ USB_DEVICE(USB_VID_LEADTEK,   USB_PID_WINFAST_DTV_DONGLE_STK7700P) },
 		{ }		/* Terminating entry */
 };
 MODULE_DEVICE_TABLE(usb, dib0700_usb_id_table);
@@ -319,7 +320,7 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 			},
 		},
 
-		.num_device_descs = 5,
+		.num_device_descs = 6,
 		.devices = {
 			{   "DiBcom STK7700P reference design",
 				{ &dib0700_usb_id_table[0], &dib0700_usb_id_table[1] },
@@ -339,6 +340,10 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 			},
 			{   "Uniwill STK7700P based (Hama and others)",
 				{ &dib0700_usb_id_table[7], NULL },
+				{ NULL },
+			},
+			{   "Leadtek Winfast DTV Dongle (STK7700P based)",
+				{ &dib0700_usb_id_table[8], NULL },
 				{ NULL },
 			}
 		}
