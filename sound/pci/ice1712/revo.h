d36f95920e50480a03819b5376067a937d0e9e8d
Takashi Iwai <tiwai@suse.de>
2006-11-09 16:47:26 +0100

    [ALSA] ice1724 - Add support of M-Audio Audiophile 192
    Added the (experimental) support of M-Audio Audiophile 192 board.
    Currently, the analog and the digital playbacks seem working fine.
    The inputs seem not working as far as I've tested yet.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/ice1712/revo.h
+++ sound/pci/ice1712/revo.h
@@ -27,10 +27,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define REVO_DEVICE_DESC \
 		"{MidiMan M Audio,Revolution 7.1},"\
-		"{MidiMan M Audio,Revolution 5.1},"
+		"{MidiMan M Audio,Revolution 5.1},"\
+		"{MidiMan M Audio,Audiophile 192},"
 
 #define VT1724_SUBDEVICE_REVOLUTION71	0x12143036
 #define VT1724_SUBDEVICE_REVOLUTION51	0x12143136
+#define VT1724_SUBDEVICE_AUDIOPHILE192	0x12143236
 
 /* entry point */
 extern struct snd_ice1712_card_info snd_vt1724_revo_cards[];
@@ -48,6 +50,7 @@ extern struct snd_ice1712_card_info snd_vt1724_revo_cards[];
 #define VT1724_REVO_CS2		0x40	/* surround AKM4355 CS (revo71) */
 #define VT1724_REVO_I2C_DATA    0x40    /* I2C: PT 2258 SDA (on revo51) */
 #define VT1724_REVO_I2C_CLOCK   0x80    /* I2C: PT 2258 SCL (on revo51) */
+#define VT1724_REVO_CS3		0x80	/* AK4114 for AP192 */
 #define VT1724_REVO_MUTE	(1<<22)	/* 0 = all mute, 1 = normal operation */
 
 #endif /* __SOUND_REVO_H */
