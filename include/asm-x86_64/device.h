80fef0aadbe9f8d8550ed59622af6d3ea9049712
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2006-11-11 17:18:42 +1100

    ACPI: Change ACPI to use dev_archdata instead of firmware_data
    Change ACPI to use dev_archdata instead of firmware_data
    
    This patch changes ACPI to use the new dev_archdata on i386, x86_64
    and ia64 (is there any other arch using ACPI ?) to store it's
    acpi_handle.
    
    It also removes the firmware_data field from struct device as this
    was the only user.
    
    Only build-tested on x86
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Len Brown <lenb@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/asm-x86_64/device.h
+++ include/asm-x86_64/device.h
@@ -4,5 +4,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * This file is released under the GPLv2
  */
-#include <asm-generic/device.h>
+#ifndef _ASM_X86_64_DEVICE_H
+#define _ASM_X86_64_DEVICE_H
 
+struct dev_archdata {
+#ifdef CONFIG_ACPI
+	void	*acpi_handle;
+#endif
+};
+
+#endif /* _ASM_X86_64_DEVICE_H */
