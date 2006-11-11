8a6be8ff987d1a72a875db475409446df6a9cf77
Sylvain Munaut <tnt@246tNt.com>
2006-11-11 10:53:19 +0100

    [PATCH] ppc: Fix io.h for config with CONFIG_PCI not set
    When CONFIG_PCI option is not set, the variables
    pci_dram_offset, isa_io_base and isa_mem_base are not defined.
    
    Currently, the test is handled in each platform header. This
    patch moves the test in io.h once and for all.
    
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-ppc/io.h
+++ include/asm-ppc/io.h
@@ -27,17 +27,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #if defined(CONFIG_4xx)
 #include <asm/ibm4xx.h>
-#elif defined(CONFIG_PPC_MPC52xx)
-#include <asm/mpc52xx.h>
 #elif defined(CONFIG_8xx)
 #include <asm/mpc8xx.h>
 #elif defined(CONFIG_8260)
 #include <asm/mpc8260.h>
-#elif defined(CONFIG_83xx)
-#include <asm/mpc83xx.h>
-#elif defined(CONFIG_85xx)
-#include <asm/mpc85xx.h>
-#elif defined(CONFIG_APUS)
+#elif defined(CONFIG_APUS) || !defined(CONFIG_PCI)
 #define _IO_BASE	0
 #define _ISA_MEM_BASE	0
 #define PCI_DRAM_OFFSET 0
