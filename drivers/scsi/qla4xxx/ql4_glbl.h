75360180c24f6f47dcb00846dd686dcfb83d099b
David C Somayajulu <david.somayajulu@qlogic.com>
2006-11-15 16:41:09 -0800

    [PATCH] qla4xxx: bug fix: driver hardware semaphore needs to be grabbed before soft reset
    On qla4xxx, the driver needs to grab the drvr semaphore provided by
    the hardware, prior to issuing a reset. This patches takes care of a
    couple of places where it was not being done. In addition there is
    minor clean up.
    
    Signed-off-by: David Somayajulu <david.somayajulu@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla4xxx/ql4_glbl.h
+++ drivers/scsi/qla4xxx/ql4_glbl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __QLA4x_GBL_H
 #define	__QLA4x_GBL_H
 
+int ql4xxx_lock_drvr_wait(struct scsi_qla_host *a);
 int qla4xxx_send_tgts(struct scsi_qla_host *ha, char *ip, uint16_t port);
 int qla4xxx_send_command_to_isp(struct scsi_qla_host *ha, struct srb * srb);
 int qla4xxx_initialize_adapter(struct scsi_qla_host * ha,
