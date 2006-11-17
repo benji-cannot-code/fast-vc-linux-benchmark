f419011fcff2c9ed3e79366e5ad636dc08523955
Wim Van Sebroeck <wim@iguana.be>
2006-11-17 21:51:35 +0100

    [WATCHDOG] MIPS RM9000 on-chip watchdog device - patch 1
    Locate parameter descriptions close to parameter definition -
    not in bottom of file.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/char/watchdog/rm9k_wdt.c
+++ drivers/char/watchdog/rm9k_wdt.c
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CPGIG1ER               0x0054
 
 
-
 /* Function prototypes */
 static int __init wdt_gpi_probe(struct device *);
 static int __exit wdt_gpi_remove(struct device *);
@@ -60,13 +59,10 @@ static int wdt_gpi_notify(struct notifier_block *, unsigned long, void *);
 static irqreturn_t wdt_gpi_irqhdl(int, void *, struct pt_regs *);
 
 
-
-
 static const char wdt_gpi_name[] = "wdt_gpi";
 static atomic_t opencnt;
 static int expect_close;
 static int locked = 0;
-
 
 
 /* These are set from device resources */
@@ -74,19 +70,26 @@ static void __iomem * wd_regs;
 static unsigned int wd_irq, wd_ctr;
 
 
-
 /* Module arguments */
 static int timeout = MAX_TIMEOUT_SECONDS;
 module_param(timeout, int, 0444);
+MODULE_PARM_DESC(timeout, "Watchdog timeout in seconds");
+
 static unsigned long resetaddr = 0xbffdc200;
 module_param(resetaddr, ulong, 0444);
+MODULE_PARM_DESC(resetaddr, "Address to write to to force a reset");
+
 static unsigned long flagaddr = 0xbffdc104;
 module_param(flagaddr, ulong, 0444);
+MODULE_PARM_DESC(flagaddr, "Address to write to boot flags to");
+
 static int powercycle = 0;
 module_param(powercycle, bool, 0444);
+MODULE_PARM_DESC(powercycle, "Cycle power if watchdog expires");
 
 static int nowayout = WATCHDOG_NOWAYOUT;
 module_param(nowayout, bool, 0444);
+MODULE_PARM_DESC(nowayout, "Watchdog cannot be disabled once started");
 
 
 
@@ -425,8 +428,4 @@ MODULE_DESCRIPTION("Basler eXcite watchdog driver for gpi devices");
 MODULE_VERSION("0.1");
 MODULE_LICENSE("GPL");
 MODULE_ALIAS_MISCDEV(WATCHDOG_MINOR);
-MODULE_PARM_DESC(timeout, "Watchdog timeout in seconds");
-MODULE_PARM_DESC(resetaddr, "Address to write to to force a reset");
-MODULE_PARM_DESC(flagaddr, "Address to write to boot flags to");
-MODULE_PARM_DESC(nowayout, "Watchdog cannot be disabled once started");
-MODULE_PARM_DESC(powercycle, "Cycle power if watchdog expires");
+
