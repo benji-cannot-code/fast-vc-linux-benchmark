6d40a8d55544852b9597f600c41d4738a1ec192f
Linus Torvalds <torvalds@woody.osdl.org>
2006-11-16 22:18:28 -0800

    Fix generic fb_ddc i2c edid probe msg
    Benh points out that the msgs[0].flags entry never got initialized, and
    since it's an automatic stack allocation, it could have any random
    value, which is bad.
    
    Rewrite the initializer to explicitly initialize all fields of the small
    i2c_msg structure array we generate.  Just to keep it all obvious, let's
    handle msgs[1].buf in the same initializer while we're at it, instead of
    initializing that one separately later.
    
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/fb_ddc.c
+++ drivers/video/fb_ddc.c
@@ -21,26 +21,26 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static unsigned char *fb_do_probe_ddc_edid(struct i2c_adapter *adapter)
 {
 	unsigned char start = 0x0;
+	unsigned char *buf = kmalloc(EDID_LENGTH, GFP_KERNEL);
 	struct i2c_msg msgs[] = {
 		{
 			.addr	= DDC_ADDR,
+			.flags	= 0,
 			.len	= 1,
 			.buf	= &start,
 		}, {
 			.addr	= DDC_ADDR,
 			.flags	= I2C_M_RD,
 			.len	= EDID_LENGTH,
+			.buf	= buf,
 		}
 	};
-	unsigned char *buf;
 
-	buf = kmalloc(EDID_LENGTH, GFP_KERNEL);
 	if (!buf) {
 		dev_warn(&adapter->dev, "unable to allocate memory for EDID "
 			 "block.\n");
 		return NULL;
 	}
-	msgs[1].buf = buf;
 
 	if (i2c_transfer(adapter, msgs, 2) == 2)
 		return buf;
