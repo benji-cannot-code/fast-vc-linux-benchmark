cfd02a6c9bc7af0e88bcab56e73dbddcd550d734
Nicolas DET <nd@bplan-gmbh.de>
2006-11-17 16:03:20 +0100

    [POWERPC] Fix compile issue for Efika platform
    This patch fixes a compile issue for the Efika platform recently
    introduced by API changes.
    
    Signed-off-by: Nicolas DET <nd@bplan-gmbh.de>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/efika/setup.c
+++ arch/powerpc/platforms/efika/setup.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/machdep.h>
 #include <asm/rtas.h>
 #include <asm/of_device.h>
+#include <asm/of_platform.h>
 #include <asm/mpc52xx.h>
 
 #include "efika.h"
