fe7786af804c36bee88ee5204ce374ffa2ea165d
Paul Mundt <lethal@linux-sh.org>
2006-11-20 13:55:34 +0900

    sh: Drop CPU subtype IRQ headers.
    This drops the various IRQ headers that were floating around
    and primarily providing hardcoded IRQ definitions for the
    various CPU subtypes. This quickly got to be an unmaintainable
    mess, made even more evident by the subtle breakage introduced
    by the SH-2 and SH-2A changes.
    
    Now that subtypes are able to register IRQ maps directly, just
    rip all of the headers out.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/irq/ipr.c
+++ arch/sh/kernel/cpu/irq/ipr.c
@@ -20,10 +20,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/irq.h>
 #include <linux/module.h>
-#include <asm/system.h>
-#include <asm/io.h>
-#include <asm/machvec.h>
-
+#include <linux/io.h>
+#include <linux/interrupt.h>
 
 static void disable_ipr_irq(unsigned int irq)
 {
