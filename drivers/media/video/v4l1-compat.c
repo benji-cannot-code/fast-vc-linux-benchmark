a0c82aad31f8345c36b4d6b9d7da97662dff9d74
audetto@tiscali.it <audetto@tiscali.it>
2006-11-20 18:27:44 -0300

    V4L/DVB (4862): Fixes uninitialized variables passed to VIDIOC_G_FBUF.
    Signed-off-by: Andrea A Odetti <audetto@tiscali.it>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/v4l1-compat.c
+++ drivers/media/video/v4l1-compat.c
@@ -351,6 +351,7 @@ v4l_compat_translate_ioctl(struct inode         *inode,
 		struct video_buffer	*buffer = arg;
 
 		memset(buffer, 0, sizeof(*buffer));
+		memset(&fbuf2, 0, sizeof(fbuf2));
 
 		err = drv(inode, file, VIDIOC_G_FBUF, &fbuf2);
 		if (err < 0) {
@@ -617,6 +618,7 @@ v4l_compat_translate_ioctl(struct inode         *inode,
 	case VIDIOCSPICT: /*  set tone controls & partial capture format  */
 	{
 		struct video_picture	*pict = arg;
+		memset(&fbuf2, 0, sizeof(fbuf2));
 
 		set_v4l_control(inode, file,
 				V4L2_CID_BRIGHTNESS, pict->brightness, drv);
