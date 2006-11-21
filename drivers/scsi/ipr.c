8026f30e8116b7012f96af6c9206a316af82c671
Brian King <brking@us.ibm.com>
2006-11-21 10:28:29 -0600

    [SCSI] ipr: Remove debug trace points from dump code
    Remove some debug trace points that clutter up the log
    when ipr debugging is turned on.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/ipr.c
+++ drivers/scsi/ipr.c
@@ -2970,7 +2970,6 @@ static int ipr_alloc_dump(struct ipr_ioa_cfg *ioa_cfg)
 	struct ipr_dump *dump;
 	unsigned long lock_flags = 0;
 
-	ENTER;
 	dump = kzalloc(sizeof(struct ipr_dump), GFP_KERNEL);
 
 	if (!dump) {
@@ -2997,7 +2996,6 @@ static int ipr_alloc_dump(struct ipr_ioa_cfg *ioa_cfg)
 	}
 	spin_unlock_irqrestore(ioa_cfg->host->host_lock, lock_flags);
 
-	LEAVE;
 	return 0;
 }
 
