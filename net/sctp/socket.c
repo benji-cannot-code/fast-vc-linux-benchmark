f23ae11672c96590430ace1e804a0a15472809ed
Al Viro <viro@zeniv.linux.org.uk>
2006-11-20 17:24:53 -0800

    [SCTP]: Annotate ->inaddr_any().
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -5056,7 +5056,7 @@ static int sctp_autobind(struct sock *sk)
 {
 	union sctp_addr autoaddr;
 	struct sctp_af *af;
-	unsigned short port;
+	__be16 port;
 
 	/* Initialize a local sockaddr structure to INADDR_ANY. */
 	af = sctp_sk(sk)->pf->af;
