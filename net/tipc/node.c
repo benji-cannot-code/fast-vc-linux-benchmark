4de2a06a232163501a9753e94da423d0abe065fd
Arnaldo Carvalho de Melo <acme@mandriva.com>
2006-11-21 01:22:12 -0200

    [TIPC]: Use kzalloc where appropriate
    Signed-off-by: Arnaldo Carvalho de Melo <acme@mandriva.com>

--- net/tipc/node.c
+++ net/tipc/node.c
@@ -61,7 +61,7 @@ struct node *tipc_node_create(u32 addr)
 	struct node *n_ptr;
         struct node **curr_node;
 
-	n_ptr = kmalloc(sizeof(*n_ptr),GFP_ATOMIC);
+	n_ptr = kzalloc(sizeof(*n_ptr),GFP_ATOMIC);
 	if (!n_ptr) {
 		warn("Node creation failed, no memory\n");
 		return NULL;
@@ -76,7 +76,6 @@ struct node *tipc_node_create(u32 addr)
 		return NULL;
 	}
 		
-	memset(n_ptr, 0, sizeof(*n_ptr));
 	n_ptr->addr = addr;
                 spin_lock_init(&n_ptr->lock);
 	INIT_LIST_HEAD(&n_ptr->nsub);
