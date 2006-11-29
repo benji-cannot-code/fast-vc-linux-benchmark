2e76428fea824bf91526df595d2edfedd305ebde
Patrick McHardy <kaber@trash.net>
2006-11-29 02:35:22 +0100

    [NETFILTER]: nf_conntrack: /proc compatibility with old connection tracking
    This patch adds /proc/net/ip_conntrack, /proc/net/ip_conntrack_expect and
    /proc/net/stat/ip_conntrack files to keep old programs using them working.
    
    The /proc/net/ip_conntrack and /proc/net/ip_conntrack_expect files show only
    IPv4 entries, the /proc/net/stat/ip_conntrack shows global statistics.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/netfilter/nf_conntrack_l3proto_generic.c
+++ net/netfilter/nf_conntrack_l3proto_generic.c
@@ -93,3 +93,4 @@ struct nf_conntrack_l3proto nf_conntrack_l3proto_generic = {
 	.prepare	 = generic_prepare,
 	.get_features	 = generic_get_features,
 };
+EXPORT_SYMBOL_GPL(nf_conntrack_l3proto_generic);
