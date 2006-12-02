5194271f7755bc3e0388aecd8e098aa2cd72bc4c
Patrick Boettcher <pboettcher@dibcom.fr>
2006-12-02 21:15:51 -0200

    V4L/DVB (4524): Initial commit for the DiB7000M-demod
    Initial commit for the driver for the DiB7000M COFDM demodulator.
    
    Signed-off-by: Francois KANOUNNIKOFF <fkanounnikoff@dibcom.fr>
    Signed-off-by: Patrick Boettcher <pboettcher@dibcom.fr>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dib0700_devices.c
+++ drivers/media/dvb/dvb-usb/dib0700_devices.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dib0700.h"
 
 #include "dib3000mc.h"
+#include "dib7000m.h"
 #include "mt2060.h"
 
 static int force_lna_activation;
