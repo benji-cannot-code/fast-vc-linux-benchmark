70c469fa43b9a0c729930459dfbcbca00036fee5
Amit S. Kale <amitkale@netxen.com>
2006-12-04 09:18:00 -0800

    [PATCH] NetXen: whitespace cleaup and more cleanup fixes
    Signed-off-by: Amit S. Kale <amitkale@netxen.com>
    
     netxen_nic.h         |   56 ++++++++++++++++++++--------------------------
     netxen_nic_ethtool.c |   53 +++++++++++++++++++++-----------------------
     netxen_nic_hdr.h     |    6 ++---
     netxen_nic_hw.c      |   54 +++++++++++++++++++++------------------------
     netxen_nic_hw.h      |   10 ++++----
     netxen_nic_init.c    |   61 +++++++++++++++++++++++++--------------------------
     netxen_nic_ioctl.h   |    6 ++---
     netxen_nic_isr.c     |   48 +++++++++++++++++-----------------------
     netxen_nic_main.c    |   54 +++++++++++++++++----------------------------
     netxen_nic_niu.c     |   10 ++++----
     10 files changed, 165 insertions(+), 193 deletions(-)
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/netxen/netxen_nic_niu.c
+++ drivers/net/netxen/netxen_nic_niu.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Copyright (C) 2003 - 2006 NetXen, Inc.
  * All rights reserved.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -17,10 +17,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston,
  * MA  02111-1307, USA.
- * 
+ *
  * The full GNU General Public License is included in this distribution
  * in the file called LICENSE.
- * 
+ *
  * Contact Information:
  *    info@netxen.com
  * NetXen,
@@ -400,8 +400,8 @@ int netxen_niu_gbe_init_port(struct netxen_adapter *adapter, int port)
 {
 	int result = 0;
 	__le32 status;
-	if (adapter->ops->disable_phy_interrupts)
-		adapter->ops->disable_phy_interrupts(adapter, port);
+	if (adapter->disable_phy_interrupts)
+		adapter->disable_phy_interrupts(adapter, port);
 	mdelay(2);
 
 	if (0 ==
