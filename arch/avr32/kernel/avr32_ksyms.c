80ccea77ceb927ba16fc3561564fff01fffe8137
Haavard Skinnemoen <hskinnemoen@atmel.com>
2006-12-04 14:17:39 +0100

    [AVR32] Don't include <asm/delay.h>
    Include <linux/delay.h> instead of <asm/delay.h> from a few places.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>

--- arch/avr32/kernel/avr32_ksyms.c
+++ arch/avr32/kernel/avr32_ksyms.c
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+#include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/module.h>
 
 #include <asm/checksum.h>
 #include <asm/uaccess.h>
-#include <asm/delay.h>
 
 /*
  * GCC functions
