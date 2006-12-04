94dcf1b1200ed33a4549ac68b7c911c238471161
Al Viro <viro@hera.kernel.org>
2006-12-04 20:41:19 +0000

    [PATCH] fix fallout from header dependency trimming
    OK, that seems to be enough to deal with the mess.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/net.h>
 #include <linux/textsearch.h>
 #include <net/checksum.h>
+#include <linux/rcupdate.h>
 #include <linux/dmaengine.h>
 
 #define HAVE_ALLOC_SKB		/* For the drivers to know */
