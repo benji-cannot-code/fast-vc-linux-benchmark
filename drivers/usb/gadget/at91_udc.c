1332cf014ffd51536e072d8f3028af2886603439
David Brownell <david-b@pacbell.net>
2006-12-05 03:15:33 -0800

    USB: gadget driver unbind() is optional; section fixes; misc
    Allow gadget drivers to omit the unbind() method.  When they're
    statically linked, that's an appropriate memory saving tweak.
    
    Similarly, provide consistent/simpler handling for a should-not-happen
    error case:  removing a peripheral controller driver when a gadget
    driver is still loaded.  Such code dates back to early versions of the
    first implementation of the gadget API, and has never been triggered.
    
    Includes relevant section annotation fixs for gmidi.c, file_storage.c,
    and serial.c; we don't yet have an "init or exit" annotation.  Also
    some whitespace fixes in gmidi.c (space at EOL, before tabs, etc).
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/at91_udc.c
+++ drivers/usb/gadget/at91_udc.c
@@ -1575,7 +1575,6 @@ int usb_gadget_register_driver (struct usb_gadget_driver *driver)
 	if (!driver
 			|| driver->speed != USB_SPEED_FULL
 			|| !driver->bind
-			|| !driver->unbind
 			|| !driver->setup) {
 		DBG("bad parameter.\n");
 		return -EINVAL;
@@ -1612,7 +1611,7 @@ int usb_gadget_unregister_driver (struct usb_gadget_driver *driver)
 {
 	struct at91_udc *udc = &controller;
 
-	if (!driver || driver != udc->driver)
+	if (!driver || driver != udc->driver || !driver->unbind)
 		return -EINVAL;
 
 	local_irq_disable();
@@ -1732,10 +1731,10 @@ static int __devexit at91udc_remove(struct platform_device *pdev)
 
 	DBG("remove\n");
 
-	pullup(udc, 0);
+	if (udc->driver)
+		return -EBUSY;
 
-	if (udc->driver != 0)
-		usb_gadget_unregister_driver(udc->driver);
+	pullup(udc, 0);
 
 	device_init_wakeup(&pdev->dev, 0);
 	remove_debug_file(udc);
