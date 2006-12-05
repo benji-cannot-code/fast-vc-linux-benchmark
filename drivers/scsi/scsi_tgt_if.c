264a75e3cc9bc92eb4fbcf3604195f998251c368
akpm@osdl.org <akpm@osdl.org>
2006-12-04 23:06:48 -0800

    [SCSI] tgt: fix undefined flush_dcache_page() problem
    drivers/scsi/scsi_tgt_if.c: In function 'tgt_uspace_send_event':
    drivers/scsi/scsi_tgt_if.c:88: warning: implicit declaration of function 'flush_dcache_page'
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/scsi_tgt_if.c
+++ drivers/scsi/scsi_tgt_if.c
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_tgt.h>
 #include <scsi/scsi_tgt_if.h>
 
+#include <asm/cacheflush.h>
+
 #include "scsi_tgt_priv.h"
 
 struct tgt_ring {
