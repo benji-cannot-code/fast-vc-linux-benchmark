f144894d2b25dcc70621e2952760f759ff639f6c
Paul Mundt <lethal@linux-sh.org>
2006-12-06 11:24:48 +0900

    sh: compile fixes for header cleanup.
    Since some header inclusion paths were cleaned up, compilation
    broke. Add in the headers we need directly to build again.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/time.c
+++ arch/sh/kernel/time.c
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/profile.h>
+#include <linux/timex.h>
+#include <linux/sched.h>
 #include <asm/clock.h>
 #include <asm/rtc.h>
 #include <asm/timer.h>
