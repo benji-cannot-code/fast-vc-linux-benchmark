c52726e4628df5c8b9b7e2ed31b142ca6fa4c402
Maciej W. Rozycki <macro@linux-mips.org>
2006-12-06 11:50:23 +0000

    [MIPS] *-berr: Header inclusions for DEC bus error handlers
     A fixup to add missing header inclusions for bus error handlers for
    DECstation system after the recent switch to get_irq_regs().
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/dec/kn01-berr.c
+++ arch/mips/dec/kn01-berr.c
@@ -21,8 +21,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 
 #include <asm/inst.h>
+#include <asm/irq_regs.h>
 #include <asm/mipsregs.h>
 #include <asm/page.h>
+#include <asm/ptrace.h>
 #include <asm/system.h>
 #include <asm/traps.h>
 #include <asm/uaccess.h>
