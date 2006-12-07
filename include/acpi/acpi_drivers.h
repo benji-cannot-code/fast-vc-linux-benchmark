eab17a423d7eb544f8411be185d9d46d7b7679a6
Zhang Rui <rui.zhang@intel.com>
2006-12-07 20:57:10 +0800

    ACPI: Set fake hid for non-PNPID ACPI devices
    We do this mainly because:
    1.	hid is used to match ACPI devices and drivers.
    	.match method which is incompatible to driver model
    	can be deleted from acpi_driver.ops then.
    2.	As the .uevent method mark ACPI drivers by PNPID,
    	fake hid is set to non-PNPID devices so that udev script
    	can load the right ACPI driver by looking for
    	"HWID = " or "COMPTID = ".
    
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acpi_drivers.h
+++ include/acpi/acpi_drivers.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ACPI_BUTTON_HID_POWERF		"ACPI_FPB"
 #define ACPI_BUTTON_HID_SLEEPF		"ACPI_FSB"
 
+#define ACPI_VIDEO_HID			"ACPI_VID"
+#define ACPI_PCI_BRIDGE_HID		"ACPI_PCI"
 /* --------------------------------------------------------------------------
                                        PCI
    -------------------------------------------------------------------------- */
