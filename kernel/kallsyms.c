0d1db8282329d9909b8f59146ca45136f35cc7b7
Adam B. Jerome <abj@novell.com>
2006-12-06 20:35:30 -0800

    [PATCH] /proc/kallsyms reports lower-case types for some non-exported symbols
    This patch addresses incorrect symbol type information reported through
    /proc/kallsyms.  A lowercase character should designate the symbol as local
    (or non-exported).  An uppercase character should designate the symbol as
    global (or external).
    
    Without this patch, some non-exported symbols are incorrectly assigned an
    upper-case designation in /proc/kallsyms.  This patch corrects this
    condition by converting non-exported symbols types to lower case when
    appropriate and eliminates the superfluous upcase_if_global function
    
    Signed-off-by: Adam B. Jerome <abj@novell.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/proc_fs.h>
 #include <linux/sched.h>	/* for cond_resched */
 #include <linux/mm.h>
+#include <linux/ctype.h>
 
 #include <asm/sections.h>
 
@@ -302,13 +303,6 @@ struct kallsym_iter
 	char name[KSYM_NAME_LEN+1];
 };
 
-/* Only label it "global" if it is exported. */
-static void upcase_if_global(struct kallsym_iter *iter)
-{
-	if (is_exported(iter->name, iter->owner))
-		iter->type += 'A' - 'a';
-}
-
 static int get_ksymbol_mod(struct kallsym_iter *iter)
 {
 	iter->owner = module_get_kallsym(iter->pos - kallsyms_num_syms,
@@ -317,7 +311,10 @@ static int get_ksymbol_mod(struct kallsym_iter *iter)
 	if (iter->owner == NULL)
 		return 0;
 
-	upcase_if_global(iter);
+	/* Label it "global" if it is exported, "local" if not exported. */
+	iter->type = is_exported(iter->name, iter->owner)
+		? toupper(iter->type) : tolower(iter->type);
+
 	return 1;
 }
 
