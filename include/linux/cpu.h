428f6118e36361d25cb317165bd77bda5d7b75ca
Siddha, Suresh B <suresh.b.siddha@intel.com>
2006-12-07 02:14:10 +0100

    [PATCH] i386: change the 'no_control' field to 'hotpluggable' in the struct cpu
    Change the 'no_control' field in the cpu struct to a more positive
    and better term 'hotpluggable'. And change(/cleanup) the logic accordingly.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Cc: Andi Kleen <ak@suse.de>
    Cc: "Li, Shaohua" <shaohua.li@intel.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>

--- include/linux/cpu.h
+++ include/linux/cpu.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct cpu {
 	int node_id;		/* The node which contains the CPU */
-	int no_control;		/* Should the sysfs control file be created? */
+	int hotpluggable;	/* creates sysfs control file if hotpluggable */
 	struct sys_device sysdev;
 };
 
