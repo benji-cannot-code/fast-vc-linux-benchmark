96802b43aaf5d52f299e246d34327c2fcbf329d4
Adrian Bunk <bunk@stusta.de>
2006-12-06 20:32:43 -0800

    [PATCH] make mm/thrash.c:global_faults static
    This patch makes the needlessly global "global_faults" static.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- mm/thrash.c
+++ mm/thrash.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static DEFINE_SPINLOCK(swap_token_lock);
 struct mm_struct *swap_token_mm;
-unsigned int global_faults;
+static unsigned int global_faults;
 
 void grab_swap_token(void)
 {
