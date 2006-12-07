a5c62a7e3dc4a2ed6839e1f32cb301bf6f969979
Andi Kleen <ak@suse.de>
2006-12-07 02:14:06 +0100

    [PATCH] x86-64: Use probe_kernel_address in arch/x86_64/*
    Instead of open coded __get_user
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/traps.c
+++ arch/x86_64/kernel/traps.c
@@ -31,9 +31,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kprobes.h>
 #include <linux/kexec.h>
 #include <linux/unwind.h>
+#include <linux/uaccess.h>
 
 #include <asm/system.h>
-#include <asm/uaccess.h>
 #include <asm/io.h>
 #include <asm/atomic.h>
 #include <asm/debugreg.h>
