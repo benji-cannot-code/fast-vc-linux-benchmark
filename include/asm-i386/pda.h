394bf1756ae91c2258515d5ea97c33f7ff3006c2
Jeremy Fitzhardinge <jeremy@goop.org>
2006-12-07 02:14:03 +0100

    [PATCH] i386: Implement smp_processor_id() with the PDA
    Use the cpu_number in the PDA to implement raw_smp_processor_id.  This is a
    little simpler than using thread_info, though the cpu field in thread_info
    cannot be removed since it is used for things other than getting the current
    CPU in common code.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Cc: Chuck Ebbert <76306.1226@compuserve.com>
    Cc: Zachary Amsden <zach@vmware.com>
    Cc: Jan Beulich <jbeulich@novell.com>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>

--- include/asm-i386/pda.h
+++ include/asm-i386/pda.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct i386_pda
 {
 	struct i386_pda *_pda;		/* pointer to self */
+
+	int cpu_number;
 };
 
 extern struct i386_pda *_cpu_pda[];
