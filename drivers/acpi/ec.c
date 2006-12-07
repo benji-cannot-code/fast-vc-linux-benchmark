a3b40031a04bc2c4a54e0b777dbe7aac04586bbe
Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>
2006-12-07 18:42:16 +0300

    ACPI: ec: Change semaphore to mutex.
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/ec.c
+++ drivers/acpi/ec.c
@@ -104,7 +104,7 @@ struct acpi_ec {
 	unsigned long command_addr;
 	unsigned long data_addr;
 	unsigned long global_lock;
-	struct semaphore sem;
+	struct mutex lock;
 	atomic_t query_pending;
 	atomic_t leaving_burst;	/* 0 : No, 1 : Yes, 2: abort */
 	wait_queue_head_t wait;
@@ -295,7 +295,7 @@ static int acpi_ec_transaction(struct acpi_ec *ec, u8 command,
 		if (ACPI_FAILURE(status))
 			return -ENODEV;
 	}
-	down(&ec->sem);
+	mutex_lock(&ec->lock);
 
 	/* Make sure GPE is enabled before doing transaction */
 	acpi_enable_gpe(NULL, ec->gpe_bit, ACPI_NOT_ISR);
@@ -311,7 +311,7 @@ static int acpi_ec_transaction(struct acpi_ec *ec, u8 command,
                                               rdata, rdata_len);
 
 end:
-	up(&ec->sem);
+	mutex_unlock(&ec->lock);
 
 	if (ec->global_lock)
 		acpi_release_global_lock(glk);
@@ -648,7 +648,7 @@ static int acpi_ec_add(struct acpi_device *device)
 
 	ec->handle = device->handle;
 	ec->uid = -1;
-	init_MUTEX(&ec->sem);
+	mutex_init(&ec->lock);
 	atomic_set(&ec->query_pending, 0);
 	if (acpi_ec_mode == EC_INTR) {
 		atomic_set(&ec->leaving_burst, 1);
@@ -831,7 +831,7 @@ acpi_fake_ecdt_callback(acpi_handle handle,
 {
 	acpi_status status;
 
-	init_MUTEX(&ec_ecdt->sem);
+	mutex_init(&ec_ecdt->lock);
 	if (acpi_ec_mode == EC_INTR) {
 		init_waitqueue_head(&ec_ecdt->wait);
 	}
@@ -916,7 +916,7 @@ static int __init acpi_ec_get_real_ecdt(void)
 		return -ENOMEM;
 	memset(ec_ecdt, 0, sizeof(struct acpi_ec));
 
-	init_MUTEX(&ec_ecdt->sem);
+	mutex_init(&ec_ecdt->lock);
 	if (acpi_ec_mode == EC_INTR) {
 		init_waitqueue_head(&ec_ecdt->wait);
 	}
