0299cfa63c46fd51c2f3e6d02f32af54e2c38e7d
Andrzej Zaborowski <balrog@zabor.org>
2006-12-06 17:13:51 -0800

    ARM: OMAP: Palm Tungsten E board update
    General update of the board file for Palm Tungsten E. Registers the
    platform devices contained in the PDA (ROM chip, keypad, infra-red)
    and updates the configuration for USB and MMC, whose config values
    were previously guessed in most cases due to lack of documentation
    (and now are confirmed by a number of users). Macros for GPIO pins are
    moved to a file in include/asm-arm/arch-omap.
    
    Signed-off-by: Andrzej Zaborowski <balrog@zabor.org>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/asm-arm/arch-omap/hardware.h
+++ include/asm-arm/arch-omap/hardware.h
@@ -331,6 +331,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-voiceblue.h"
 #endif
 
+#ifdef CONFIG_MACH_OMAP_PALMTE
+#include "board-palmte.h"
+#endif
+
 #endif /* !__ASSEMBLER__ */
 
 #endif	/* __ASM_ARCH_OMAP_HARDWARE_H */
