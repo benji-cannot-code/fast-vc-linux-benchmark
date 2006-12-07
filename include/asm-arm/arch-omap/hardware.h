99b8327f6c23d2946613c7d3cb6bc5b71c67da44
Marek Vasut <marek.vasut@gmail.com>
2006-12-06 17:13:57 -0800

    ARM: OMAP: PalmZ71 support
    Palmz71 specific things - board file.
    
    Signed-off-by: Marek Vasut <marek.vasut@gmail.com>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/asm-arm/arch-omap/hardware.h
+++ include/asm-arm/arch-omap/hardware.h
@@ -335,6 +335,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-palmte.h"
 #endif
 
+#ifdef CONFIG_MACH_OMAP_PALMZ71
+#include "board-palmz71.h"
+#endif
+
 #endif /* !__ASSEMBLER__ */
 
 #endif	/* __ASM_ARCH_OMAP_HARDWARE_H */
