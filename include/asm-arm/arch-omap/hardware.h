502ed49ef40bc261a7ed93ee51ef022e8c258fef
Vladimir Ananiev <vovan888@gmail.com>
2006-12-06 17:14:08 -0800

    ARM: OMAP: Basic support for siemens sx1
    This adds basic support for Siemens SX1. More patches are available,
    with video driver, mixer, and serial ports working. That is enough to
    do gsm calls with right userland.
    
    Signed-off-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- include/asm-arm/arch-omap/hardware.h
+++ include/asm-arm/arch-omap/hardware.h
@@ -343,6 +343,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "board-palmtt.h"
 #endif
 
+#ifdef CONFIG_MACH_SX1
+#include "board-sx1.h"
+#endif
+
 #endif /* !__ASSEMBLER__ */
 
 #endif	/* __ASM_ARCH_OMAP_HARDWARE_H */
