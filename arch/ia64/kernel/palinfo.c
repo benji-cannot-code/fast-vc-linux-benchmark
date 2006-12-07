4d4f02b5dac2c3bd997a19eac7a9423a1799fc2e
Russ Anderson <rja@sgi.com>
2006-12-07 11:06:35 -0800

    [IA64] Update processor_info features
    Add the printing of additional processor features to proc_features.
    
    Based on Rev 2.2 of Volume 2 of "Intel Itanium Architecture Software
    Developer's Manual" (January 2006) fields (pages 2:430-2:432).
    This patch gets the features back in sync with the spec.
    
    Sample output before:
    --------------------------------------------------------------
    cobra:~ # cat /proc/pal/cpu0/processor_info
    XIP,XPSR,XFS implemented                 : On NoCtrl
    XR1-XR3 implemented                      : On NoCtrl
    Disable dynamic predicate prediction     : NotImpl
    Disable processor physical number        : NotImpl
    Disable dynamic data cache prefetch      : NotImpl
    Disable dynamic inst cache prefetch      : NotImpl
    Disable dynamic branch prediction        : NotImpl
    Disable BINIT on processor time-out      : On Ctrl
    Disable dynamic power management (DPM)   : NotImpl
    Disable coherency                        : NotImpl
    Disable cache                            : NotImpl
    Enable CMCI promotion                    : Off Ctrl
    Enable MCA to BINIT promotion            : Off Ctrl
    Enable MCA promotion                     : NotImpl
    Enable BERR promotion                    : NotImpl
    cobra:~ #
    --------------------------------------------------------------
    
    Sample output after:
    --------------------------------------------------------------
    cobra:~ # cat /proc/pal/cpu0/processor_info
    Unimplemented instruction address fault  : NotImpl
    INIT, PMI, and LINT pins                 : NotImpl
    Simple unimplimented instr addresses     : On NoCtrl
    Variable P-state performance             : NotImpl
    Virtual machine features implemeted      : On NoCtrl
    XIP,XPSR,XFS implemented                 : On NoCtrl
    XR1-XR3 implemented                      : On NoCtrl
    Disable dynamic predicate prediction     : NotImpl
    Disable processor physical number        : NotImpl
    Disable dynamic data cache prefetch      : NotImpl
    Disable dynamic inst cache prefetch      : NotImpl
    Disable dynamic branch prediction        : NotImpl
    Disable P-states                         : Off Ctrl
    Enable MCA on Data Poisoning             : Off Ctrl
    Enable vmsw instruction                  : On Ctrl
    Enable extern environmental notification : NotImpl
    Disable BINIT on processor time-out      : On Ctrl
    Disable dynamic power management (DPM)   : NotImpl
    Disable coherency                        : NotImpl
    Disable cache                            : NotImpl
    Enable CMCI promotion                    : Off Ctrl
    Enable MCA to BINIT promotion            : Off Ctrl
    Enable MCA promotion                     : NotImpl
    Enable BERR promotion                    : NotImpl
    cobra:~ #
    --------------------------------------------------------------
    
    Signed-off-by: Russ Anderson (rja@sgi.com)
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/palinfo.c
+++ arch/ia64/kernel/palinfo.c
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * 02/05/2001   S.Eranian	fixed module support
  * 10/23/2001	S.Eranian	updated pal_perf_mon_info bug fixes
  * 03/24/2004	Ashok Raj	updated to work with CPU Hotplug
+ * 10/26/2006   Russ Anderson	updated processor features to rev 2.2 spec
  */
 #include <linux/types.h>
 #include <linux/errno.h>
@@ -468,7 +469,11 @@ static const char *proc_features[]={
 	NULL,NULL,NULL,NULL,NULL,NULL,NULL, NULL,NULL,
 	NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,
 	NULL,NULL,NULL,NULL,NULL, NULL,NULL,NULL,NULL,
-	NULL,NULL,NULL,NULL,NULL,
+	"Unimplemented instruction address fault",
+	"INIT, PMI, and LINT pins",
+	"Simple unimplemented instr addresses",
+	"Variable P-state performance",
+	"Virtual machine features implemented",
 	"XIP,XPSR,XFS implemented",
 	"XR1-XR3 implemented",
 	"Disable dynamic predicate prediction",
@@ -476,7 +481,11 @@ static const char *proc_features[]={
 	"Disable dynamic data cache prefetch",
 	"Disable dynamic inst cache prefetch",
 	"Disable dynamic branch prediction",
-	NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL,
+	"Disable P-states",
+	"Enable MCA on Data Poisoning",
+	"Enable vmsw instruction",
+	"Enable extern environmental notification",
 	"Disable BINIT on processor time-out",
 	"Disable dynamic power management (DPM)",
 	"Disable coherency",
