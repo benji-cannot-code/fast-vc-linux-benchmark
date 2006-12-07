ec3d6ae1448d42e4e16634c31ece4e19ab52524b
Stephane Eranian <eranian@hpl.hp.com>
2006-12-07 02:14:00 +0100

    [PATCH] x86-64: x86_64 rename X86_FEATURE_DTES to X86_FEATURE_DS
    Here is a patch (used by perfmon2) that renamed X86_FEATURE_DTES
    to X86_FEATURE_DS to match Intel's documentation for the Debug Store
    save area. The patch also adds cpu_has_ds.
    
    changelog:
    	- rename X86_FEATURE_DTES to X86_FEATURE_DS to match documentation
    	- adds cpu_has_ds to test for X86_FEATURE_DS
    
    Signed-off-by: stephane eranian <eranian@hpl.hp.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-x86_64/cpufeature.h
+++ include/asm-x86_64/cpufeature.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define X86_FEATURE_PSE36	(0*32+17) /* 36-bit PSEs */
 #define X86_FEATURE_PN		(0*32+18) /* Processor serial number */
 #define X86_FEATURE_CLFLSH	(0*32+19) /* Supports the CLFLUSH instruction */
-#define X86_FEATURE_DTES	(0*32+21) /* Debug Trace Store */
+#define X86_FEATURE_DS		(0*32+21) /* Debug Store */
 #define X86_FEATURE_ACPI	(0*32+22) /* ACPI via MSR */
 #define X86_FEATURE_MMX		(0*32+23) /* Multimedia Extensions */
 #define X86_FEATURE_FXSR	(0*32+24) /* FXSAVE and FXRSTOR instructions (fast save and restore */
@@ -113,5 +113,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define cpu_has_cyrix_arr      0
 #define cpu_has_centaur_mcr    0
 #define cpu_has_clflush	       boot_cpu_has(X86_FEATURE_CLFLSH)
+#define cpu_has_ds 	       boot_cpu_has(X86_FEATURE_DS)
 
 #endif /* __ASM_X8664_CPUFEATURE_H */
