d0795387653f8f9031c7f0f6b0e338de5acfd890
Jiri Slaby <jirislaby@gmail.com>
2006-12-08 02:39:20 -0800

    [PATCH] Char: istallion, ifdef eisa code
    Disable compiling eisa stuff if STLI_EISAPROBE == 0.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/istallion.c
+++ drivers/char/istallion.c
@@ -356,6 +356,7 @@ MODULE_PARM_DESC(board2, "Board 2 config -> name[,ioaddr[,memaddr]");
 module_param_array(board3, charp, NULL, 0);
 MODULE_PARM_DESC(board3, "Board 3 config -> name[,ioaddr[,memaddr]");
 
+#if STLI_EISAPROBE != 0
 /*
  *	Set up a default memory address table for EISA board probing.
  *	The default addresses are all bellow 1Mbyte, which has to be the
@@ -373,6 +374,7 @@ static unsigned long	stli_eisamemprobeaddrs[] = {
 };
 
 static int	stli_eisamempsize = ARRAY_SIZE(stli_eisamemprobeaddrs);
+#endif
 
 /*
  *	Define the Stallion PCI vendor and device IDs.
@@ -685,7 +687,9 @@ static struct stliport *stli_getport(unsigned int brdnr, unsigned int panelnr, u
 
 static int	stli_initecp(struct stlibrd *brdp);
 static int	stli_initonb(struct stlibrd *brdp);
+#if STLI_EISAPROBE != 0
 static int	stli_eisamemprobe(struct stlibrd *brdp);
+#endif
 static int	stli_initports(struct stlibrd *brdp);
 
 /*****************************************************************************/
@@ -3712,6 +3716,7 @@ static int __devinit stli_brdinit(struct stlibrd *brdp)
 	return 0;
 }
 
+#if STLI_EISAPROBE != 0
 /*****************************************************************************/
 
 /*
@@ -3805,6 +3810,7 @@ static int stli_eisamemprobe(struct stlibrd *brdp)
 	}
 	return 0;
 }
+#endif
 
 static int stli_getbrdnr(void)
 {
@@ -3820,6 +3826,7 @@ static int stli_getbrdnr(void)
 	return -1;
 }
 
+#if STLI_EISAPROBE != 0
 /*****************************************************************************/
 
 /*
@@ -3895,6 +3902,9 @@ static int stli_findeisabrds(void)
 
 	return 0;
 }
+#else
+static inline int stli_findeisabrds(void) { return 0; }
+#endif
 
 /*****************************************************************************/
 
@@ -4020,8 +4030,7 @@ static int stli_initbrds(void)
 		stli_brdinit(brdp);
 	}
 
-	if (STLI_EISAPROBE)
-		stli_findeisabrds();
+	stli_findeisabrds();
 
 	retval = pci_register_driver(&stli_pcidriver);
 	/* TODO: check retval and do something */
