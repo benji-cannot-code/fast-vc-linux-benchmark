96d36c127b0ff6871a39996a54521173882c924a
Sukadev Bhattiprolu <sukadev@us.ibm.com>
2006-12-08 02:37:58 -0800

    [PATCH] rename struct pspace to struct pid_namespace
    Rename struct pspace to struct pid_namespace for consistency with other
    namespaces (uts_namespace and ipc_namespace).  Also rename
    include/linux/pspace.h to include/linux/pid_namespace.h and variables from
    pspace to pid_ns.
    
    Signed-off-by: Sukadev Bhattiprolu <sukadev@us.ibm.com>
    Signed-off-by: Cedric Le Goater <clg@fr.ibm.com>
    Cc: Kirill Korotaev <dev@openvz.org>
    Cc: Eric W. Biederman <ebiederm@xmission.com>
    Cc: Herbert Poetzl <herbert@13thfloor.at>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/pid_namespace.h
+++ include/linux/pid_namespace.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _LINUX_PSPACE_H
-#define _LINUX_PSPACE_H
+#ifndef _LINUX_PID_NS_H
+#define _LINUX_PID_NS_H
 
 #include <linux/sched.h>
 #include <linux/mm.h>
@@ -14,11 +14,11 @@ struct pidmap {
 
 #define PIDMAP_ENTRIES         ((PID_MAX_LIMIT + 8*PAGE_SIZE - 1)/PAGE_SIZE/8)
 
-struct pspace {
+struct pid_namespace {
        struct pidmap pidmap[PIDMAP_ENTRIES];
        int last_pid;
 };
 
-extern struct pspace init_pspace;
+extern struct pid_namespace init_pid_ns;
 
-#endif /* _LINUX_PSPACE_H */
+#endif /* _LINUX_PID_NS_H */
