e22c5e7a5fad7c5d326132720c681dd2b1199485
Herbert Poetzl <herbert@13thfloor.at>
2006-12-08 02:36:00 -0800

    [PATCH] Fix linux banner utsname information
    utsname information is shown in the linux banner, which also is used for
    /proc/version (which can have different utsname values inside a uts
    namespaces).  this patch makes the varying data arguments and changes the
    string to a format string, using those arguments.
    
    Signed-off-by: Herbert Poetzl <herbert@13thfloor.at>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/proc/proc_misc.c
+++ fs/proc/proc_misc.c
@@ -253,8 +253,8 @@ static int version_read_proc(char *page, char **start, off_t off,
 {
 	int len;
 
-	strcpy(page, linux_banner);
-	len = strlen(page);
+	len = sprintf(page, linux_banner,
+		utsname()->release, utsname()->version);
 	return proc_calc_metrics(page, start, off, count, eof, len);
 }
 
