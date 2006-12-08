637d926332d8cd62a04c4aea13803323ed93b5a5
Dmitry Torokhov <dtor@insightbb.com>
2006-12-08 01:37:03 -0500

    Input: ucb1400 - fix compile error
    try_to_freeze() was moved into include/linux/freezer.h
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/touchscreen/ucb1400_ts.c
+++ drivers/input/touchscreen/ucb1400_ts.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
-#include <linux/sched.h>
 #include <linux/completion.h>
 #include <linux/delay.h>
 #include <linux/input.h>
@@ -27,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/suspend.h>
 #include <linux/slab.h>
 #include <linux/kthread.h>
+#include <linux/freezer.h>
 
 #include <sound/driver.h>
 #include <sound/core.h>
