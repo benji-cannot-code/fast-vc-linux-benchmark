46711cd6912af66b4da06d564667035ee10c7c38
Jiri Slaby <jirislaby@gmail.com>
2006-12-08 02:38:12 -0800

    [PATCH] Char: mxser_new, correct include file
    include mxser_new.h instead of original mxser.h
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/mxser_new.c
+++ drivers/char/mxser_new.c
@@ -65,7 +65,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/bitops.h>
 #include <asm/uaccess.h>
 
-#include "mxser.h"
+#include "mxser_new.h"
 
 #define	MXSER_VERSION	"1.8"
 #define	MXSERMAJOR	 174
