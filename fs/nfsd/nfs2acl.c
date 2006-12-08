35328d428c7a80a40974d76c00330420b46d9406
Jesper Juhl <jesper.juhl@gmail.com>
2006-12-08 02:39:39 -0800

    [PATCH] NFS2: Calculate 'w' a bit later in nfsaclsvc_encode_getaclres()
    NFS2: Calculate 'w' a bit later in nfsaclsvc_encode_getaclres()
          This is a small performance optimization since we can return before
          needing 'w'. It also saves a few bytes of .text :
          Before:
               text    data     bss     dec     hex filename
               2406     212       0    2618     a3a fs/nfsd/nfs2acl.o
          After:
               text    data     bss     dec     hex filename
               2400     212       0    2612     a34 fs/nfsd/nfs2acl.o
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Cc: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/nfsd/nfs2acl.c
+++ fs/nfsd/nfs2acl.c
@@ -223,12 +223,10 @@ static int nfsaclsvc_encode_getaclres(struct svc_rqst *rqstp, __be32 *p,
 {
 	struct dentry *dentry = resp->fh.fh_dentry;
 	struct inode *inode = dentry->d_inode;
-	int w = nfsacl_size(
-		(resp->mask & NFS_ACL)   ? resp->acl_access  : NULL,
-		(resp->mask & NFS_DFACL) ? resp->acl_default : NULL);
 	struct kvec *head = rqstp->rq_res.head;
 	unsigned int base;
 	int n;
+	int w;
 
 	if (dentry == NULL || dentry->d_inode == NULL)
 		return 0;
@@ -240,7 +238,9 @@ static int nfsaclsvc_encode_getaclres(struct svc_rqst *rqstp, __be32 *p,
 		return 0;
 	base = (char *)p - (char *)head->iov_base;
 
-	rqstp->rq_res.page_len = w;
+	rqstp->rq_res.page_len = w = nfsacl_size(
+		(resp->mask & NFS_ACL)   ? resp->acl_access  : NULL,
+		(resp->mask & NFS_DFACL) ? resp->acl_default : NULL);
 	while (w > 0) {
 		if (!rqstp->rq_respages[rqstp->rq_resused++])
 			return 0;
