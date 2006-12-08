079467473c2560fbf2912c0aa2f61a920d1a6532
Haavard Skinnemoen <hskinnemoen@atmel.com>
2006-12-08 14:38:30 +0100

    [PATCH] MACB: Use __raw register access
    Since macb is a chip-internal device, use __raw_readl and
    __raw_writel instead of readl/writel. This will perform native-endian
    accesses, which is the right thing to do on both AVR32 and ARM devices.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/macb.h
+++ drivers/net/macb.h
@@ -251,9 +251,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Register access macros */
 #define macb_readl(port,reg)				\
-	readl((port)->regs + MACB_##reg)
+	__raw_readl((port)->regs + MACB_##reg)
 #define macb_writel(port,reg,value)			\
-	writel((value), (port)->regs + MACB_##reg)
+	__raw_writel((value), (port)->regs + MACB_##reg)
 
 struct dma_desc {
 	u32	addr;
