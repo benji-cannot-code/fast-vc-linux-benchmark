6af75d74e8b61832e66c19969528b451bcd74dc7
Amol Lad <amol@verismonetworks.com>
2006-12-08 02:40:12 -0800

    [PATCH] ioremap balanced with iounmap for drivers/video/ffb
    ioremap must be balanced by an iounmap and failing to do so can result in a
    memory leak.
    
    Signed-off-by: Amol Lad <amol@verismonetworks.com>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Cc: David S. Miller <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/ffb.c
+++ drivers/video/ffb.c
@@ -969,6 +969,8 @@ static int ffb_init_one(struct of_device *op)
 
 	if (fb_alloc_cmap(&all->info.cmap, 256, 0)) {
 		printk(KERN_ERR "ffb: Could not allocate color map.\n");
+		of_iounmap(all->par.fbc, sizeof(struct ffb_fbc));
+		of_iounmap(all->par.dac, sizeof(struct ffb_dac));
 		kfree(all);
 		return -ENOMEM;
 	}
@@ -979,6 +981,8 @@ static int ffb_init_one(struct of_device *op)
 	if (err < 0) {
 		printk(KERN_ERR "ffb: Could not register framebuffer.\n");
 		fb_dealloc_cmap(&all->info.cmap);
+		of_iounmap(all->par.fbc, sizeof(struct ffb_fbc));
+		of_iounmap(all->par.dac, sizeof(struct ffb_dac));
 		kfree(all);
 		return err;
 	}
