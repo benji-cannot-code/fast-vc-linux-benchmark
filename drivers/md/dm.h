0de52c905bd47d425951fb77383f3b12e6af18df
Kiyoshi Ueda <k-ueda@ct.jp.nec.com>
2006-12-08 02:41:07 -0800

    [PATCH] dm: ioctl: add noflush suspend
    Provide a dm ioctl option to request noflush suspending.  (See next patch for
    what this is for.) As the interface is extended, the version number is
    incremented.
    
    Other than accepting the new option through the interface, There is no change
    to existing behaviour.
    
    Test results:
    Confirmed the option is given from user-space correctly.
    
    Signed-off-by: Kiyoshi Ueda <k-ueda@ct.jp.nec.com>
    Signed-off-by: Jun'ichi Nomura <j-nomura@ce.jp.nec.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>
    Cc: dm-devel@redhat.com
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/md/dm.h
+++ drivers/md/dm.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Suspend feature flags
  */
 #define DM_SUSPEND_LOCKFS_FLAG		(1 << 0)
+#define DM_SUSPEND_NOFLUSH_FLAG		(1 << 1)
 
 /*
  * List of devices that a metadevice uses and should open/close.
