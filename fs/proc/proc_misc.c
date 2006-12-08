8eacc5cca9dfbe1f7bb088744e79e5582e599474
Andrew Morton <akpm@osdl.org>
2006-12-08 02:41:14 -0800

    [PATCH] proc_misc build fix
    fs/proc/proc_misc.c: In function `version_read_proc':
    fs/proc/proc_misc.c:256: warning: implicit declaration of function `utsname'
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/proc/proc_misc.c
+++ fs/proc/proc_misc.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/times.h>
 #include <linux/profile.h>
+#include <linux/utsname.h>
 #include <linux/blkdev.h>
 #include <linux/hugetlb.h>
 #include <linux/jiffies.h>
