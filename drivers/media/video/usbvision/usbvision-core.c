d8a86bd5c17fe17b11aedf1a21904875513f5148
Mauro Carvalho Chehab <mchehab@infradead.org>
2006-12-09 11:43:31 -0300

    V4L/DVB (4942): Whitespace cleanups
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/usbvision/usbvision-core.c
+++ drivers/media/video/usbvision/usbvision-core.c
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * usbvision-core.c - driver for NT100x USB video capture devices
- * 
+ *
  *
  * Copyright (c) 1999-2005 Joerg Heckenbach <joerg@heckenbach-aw.de>
  *                         Dwaine Garden <dwainegarden@rogers.com>
