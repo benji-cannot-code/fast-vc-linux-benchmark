2f66b3ebb4bad9db2295ba66ded6949253f426e4
Avi Kivity <avi@qumranet.com>
2006-12-10 02:21:36 -0800

    [PATCH] kvm: userspace interface
    web site: http://kvm.sourceforge.net
    
    mailing list: kvm-devel@lists.sourceforge.net
      (http://lists.sourceforge.net/lists/listinfo/kvm-devel)
    
    The following patchset adds a driver for Intel's hardware virtualization
    extensions to the x86 architecture.  The driver adds a character device
    (/dev/kvm) that exposes the virtualization capabilities to userspace.  Using
    this driver, a process can run a virtual machine (a "guest") in a fully
    virtualized PC containing its own virtual hard disks, network adapters, and
    display.
    
    Using this driver, one can start multiple virtual machines on a host.
    
    Each virtual machine is a process on the host; a virtual cpu is a thread in
    that process.  kill(1), nice(1), top(1) work as expected.  In effect, the
    driver adds a third execution mode to the existing two: we now have kernel
    mode, user mode, and guest mode.  Guest mode has its own address space mapping
    guest physical memory (which is accessible to user mode by mmap()ing
    /dev/kvm).  Guest mode has no access to any I/O devices; any such access is
    intercepted and directed to user mode for emulation.
    
    The driver supports i386 and x86_64 hosts and guests.  All combinations are
    allowed except x86_64 guest on i386 host.  For i386 guests and hosts, both pae
    and non-pae paging modes are supported.
    
    SMP hosts and UP guests are supported.  At the moment only Intel
    hardware is supported, but AMD virtualization support is being worked on.
    
    Performance currently is non-stellar due to the naive implementation of the
    mmu virtualization, which throws away most of the shadow page table entries
    every context switch.  We plan to address this in two ways:
    
    - cache shadow page tables across tlb flushes
    - wait until AMD and Intel release processors with nested page tables
    
    Currently a virtual desktop is responsive but consumes a lot of CPU.  Under
    Windows I tried playing pinball and watching a few flash movies; with a recent
    CPU one can hardly feel the virtualization.  Linux/X is slower, probably due
    to X being in a separate process.
    
    In addition to the driver, you need a slightly modified qemu to provide I/O
    device emulation and the BIOS.
    
    Caveats (akpm: might no longer be true):
    
    - The Windows install currently bluescreens due to a problem with the
      virtual APIC.  We are working on a fix.  A temporary workaround is to
      use an existing image or install through qemu
    - Windows 64-bit does not work.  That's also true for qemu, so it's
      probably a problem with the device model.
    
    [bero@arklinux.org: build fix]
    [simon.kagstrom@bth.se: build fix, other fixes]
    [uril@qumranet.com: KVM: Expose interrupt bitmap]
    [akpm@osdl.org: i386 build fix]
    [mingo@elte.hu: i386 fixes]
    [rdreier@cisco.com: add log levels to all printks]
    [randy.dunlap@oracle.com: Fix sparse NULL and C99 struct init warnings]
    [anthony@codemonkey.ws: KVM: AMD SVM: 32-bit host support]
    Signed-off-by: Yaniv Kamay <yaniv@qumranet.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>
    Cc: Simon Kagstrom <simon.kagstrom@bth.se>
    Cc: Bernhard Rosenkraenzer <bero@arklinux.org>
    Signed-off-by: Uri Lublin <uril@qumranet.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Roland Dreier <rolandd@cisco.com>
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Anthony Liguori <anthony@codemonkey.ws>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ drivers/kvm/segment_descriptor.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+struct segment_descriptor {
+	u16 limit_low;
+	u16 base_low;
+	u8  base_mid;
+	u8  type : 4;
+	u8  system : 1;
+	u8  dpl : 2;
+	u8  present : 1;
+	u8  limit_high : 4;
+	u8  avl : 1;
+	u8  long_mode : 1;
+	u8  default_op : 1;
+	u8  granularity : 1;
+	u8  base_high;
+} __attribute__((packed));
+
+
