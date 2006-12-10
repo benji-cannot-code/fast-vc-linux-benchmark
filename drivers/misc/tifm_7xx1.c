7bd233ce6014d03d7150735f1da2f9c7c9c09720
Alex Dubov <oakad@yahoo.com>
2006-12-11 01:55:32 +1100

    tifm_7xx1: simplify eject function
    Eject function can take advantage of the socket_id field instead of explicit
    pointer comparison.
    
    Signed-off-by: Alex Dubov <oakad@yahoo.com>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/misc/tifm_7xx1.c
+++ drivers/misc/tifm_7xx1.c
@@ -18,18 +18,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static void tifm_7xx1_eject(struct tifm_adapter *fm, struct tifm_dev *sock)
 {
-	int cnt;
 	unsigned long flags;
 
 	spin_lock_irqsave(&fm->lock, flags);
 	if (!fm->inhibit_new_cards) {
-		for (cnt = 0; cnt < fm->max_sockets; cnt++) {
-			if (fm->sockets[cnt] == sock) {
-				fm->remove_mask |= (1 << cnt);
-				queue_work(fm->wq, &fm->media_remover);
-				break;
-			}
-		}
+		fm->remove_mask |= 1 << sock->socket_id;
+		queue_work(fm->wq, &fm->media_remover);
 	}
 	spin_unlock_irqrestore(&fm->lock, flags);
 }
