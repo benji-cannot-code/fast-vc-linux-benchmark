dad7bfef4da08b73aaad1be4a78eef1cbb63ecf6
Randy Dunlap <randy.dunlap@oracle.com>
2006-12-10 02:18:58 -0800

    [PATCH] freezer.h uses task_struct fields
    freezer.h uses task_struct fields so it should include sched.h.
    
      CC [M]  fs/jfs/jfs_txnmgr.o
    In file included from fs/jfs/jfs_txnmgr.c:49:
    include/linux/freezer.h: In function 'frozen':
    include/linux/freezer.h:9: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:9: error: 'PF_FROZEN' undeclared (first use in this function)
    include/linux/freezer.h:9: error: (Each undeclared identifier is reported only once
    include/linux/freezer.h:9: error: for each function it appears in.)
    include/linux/freezer.h: In function 'freezing':
    include/linux/freezer.h:17: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:17: error: 'PF_FREEZE' undeclared (first use in this function)
    include/linux/freezer.h: In function 'freeze':
    include/linux/freezer.h:26: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:26: error: 'PF_FREEZE' undeclared (first use in this function)
    include/linux/freezer.h: In function 'do_not_freeze':
    include/linux/freezer.h:34: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:34: error: 'PF_FREEZE' undeclared (first use in this function)
    include/linux/freezer.h: In function 'thaw_process':
    include/linux/freezer.h:43: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:43: error: 'PF_FROZEN' undeclared (first use in this function)
    include/linux/freezer.h:44: warning: implicit declaration of function 'wake_up_process'
    include/linux/freezer.h: In function 'frozen_process':
    include/linux/freezer.h:55: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:55: error: dereferencing pointer to incomplete type
    include/linux/freezer.h:55: error: 'PF_FREEZE' undeclared (first use in this function)
    include/linux/freezer.h:55: error: 'PF_FROZEN' undeclared (first use in this function)
    fs/jfs/jfs_txnmgr.c: In function 'freezing':
    include/linux/freezer.h:18: warning: control reaches end of non-void function
    make[2]: *** [fs/jfs/jfs_txnmgr.o] Error 1
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Acked-by: Dave Kleikamp <shaggy@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Freezer declarations */
 
+#include <linux/sched.h>
+
 #ifdef CONFIG_PM
 /*
  * Check if a process has been frozen
