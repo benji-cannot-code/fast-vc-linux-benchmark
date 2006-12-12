adbe2d40a3a002bba9d9ce906422a3411c1e8b94
Daniel Drake <dsd@gentoo.org>
2006-12-12 01:26:11 +0000

    [PATCH] zd1211rw: Remove addressing abstraction
    Instead of passing our own custom 32-bit addresses around and
    translating them, this patch makes all our register address constants
    absolute and removes the translation.
    
    There are two ugly parts:
     - fw_reg_addr() is needed to compute addresses of firmware registers, as this
       is dynamic based upon firmware
     - inc_addr() needs a small hack to handle byte vs word addressing
    
    However, both of those are only small, and we don't use fw_regs a whole
    lot anyway.
    
    The bonuses here include simplicity and improved driver readability. Also, the
    fact that registers are now referenced by 16-bit absolute addresses (as
    opposed to 32-bit pseudo addresses) means that over 2kb compiled code size has
    been shaved off.
    
    Includes some touchups and sparse fixes from Ulrich Kunitz.
    
    Signed-off-by: Daniel Drake <dsd@gentoo.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/zd1211rw/zd_usb.h
+++ drivers/net/wireless/zd1211rw/zd_usb.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/usb.h>
 
 #include "zd_def.h"
-#include "zd_types.h"
 
 enum devicetype {
 	DEVICE_ZD1211  = 0,
@@ -182,15 +181,14 @@ struct zd_usb_tx {
 	spinlock_t lock;
 };
 
-/* Contains the usb parts. The structure doesn't require a lock, because intf
- * and fw_base_offset, will not be changed after initialization.
+/* Contains the usb parts. The structure doesn't require a lock because intf
+ * will not be changed after initialization.
  */
 struct zd_usb {
 	struct zd_usb_interrupt intr;
 	struct zd_usb_rx rx;
 	struct zd_usb_tx tx;
 	struct usb_interface *intf;
-	u16 fw_base_offset;
 };
 
 #define zd_usb_dev(usb) (&usb->intf->dev)
