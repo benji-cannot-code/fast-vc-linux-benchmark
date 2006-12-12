37551fbd68262d374517677c41849d2cca2768c8
Yasuyuki Kozakai <yasuyuki.kozakai@toshiba.co.jp>
2006-12-12 00:29:02 -0800

    [NETFILTER]: x_tables: add missing try to load conntrack from match/targets
    CLUSTERIP, CONNMARK, CONNSECMARK, and connbytes need ip_conntrack or
    layer 3 protocol module of nf_conntrack.
    
    Signed-off-by: Yasuyuki Kozakai <yasuyuki.kozakai@toshiba.co.jp>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/xt_connbytes.c
+++ net/netfilter/xt_connbytes.c
@@ -140,7 +140,19 @@ static int check(const char *tablename,
 	    sinfo->direction != XT_CONNBYTES_DIR_BOTH)
 		return 0;
 
+	if (nf_ct_l3proto_try_module_get(match->family) < 0) {
+		printk(KERN_WARNING "can't load conntrack support for "
+				    "proto=%d\n", match->family);
+		return 0;
+	}
+
 	return 1;
+}
+
+static void
+destroy(const struct xt_match *match, void *matchinfo)
+{
+	nf_ct_l3proto_module_put(match->family);
 }
 
 static struct xt_match xt_connbytes_match[] = {
@@ -149,6 +161,7 @@ static struct xt_match xt_connbytes_match[] = {
 		.family		= AF_INET,
 		.checkentry	= check,
 		.match		= match,
+		.destroy	= destroy,
 		.matchsize	= sizeof(struct xt_connbytes_info),
 		.me		= THIS_MODULE
 	},
@@ -157,6 +170,7 @@ static struct xt_match xt_connbytes_match[] = {
 		.family		= AF_INET6,
 		.checkentry	= check,
 		.match		= match,
+		.destroy	= destroy,
 		.matchsize	= sizeof(struct xt_connbytes_info),
 		.me		= THIS_MODULE
 	},
