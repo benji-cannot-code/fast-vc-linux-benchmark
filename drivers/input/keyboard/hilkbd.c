2396b71e2a5c754f627f4acc9389efdcccd3a418
Al Viro <viro@ftp.linux.org.uk>
2006-12-13 00:35:01 -0800

    [PATCH] missing includes in hilkbd
    Now that it's built on m68k too...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/input/keyboard/hilkbd.c
+++ drivers/input/keyboard/hilkbd.c
@@ -24,7 +24,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/hil.h>
+#include <linux/io.h>
 #include <linux/spinlock.h>
+#include <asm/irq.h>
+#ifdef CONFIG_HP300
+#include <asm/hwtest.h>
+#endif
 
 
 MODULE_AUTHOR("Philip Blundell, Matthew Wilcox, Helge Deller");
