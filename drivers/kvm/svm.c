49d70f992aa120ad8e04d189519507957e0c6b4b
Avi Kivity <avi@qumranet.com>
2006-12-13 00:34:14 -0800

    [PATCH] KVM: Disallow the kvm-amd module on intel hardware, and vice versa
    They're not on speaking terms.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/kvm/svm.c
+++ drivers/kvm/svm.c
@@ -378,6 +378,7 @@ static __init int svm_hardware_setup(void)
 	void *msrpm_va;
 	int r;
 
+	kvm_emulator_want_group7_invlpg();
 
 	iopm_pages = alloc_pages(GFP_KERNEL, IOPM_ALLOC_ORDER);
 
@@ -1629,9 +1630,7 @@ static struct kvm_arch_ops svm_arch_ops = {
 
 static int __init svm_init(void)
 {
-	kvm_emulator_want_group7_invlpg();
-	kvm_init_arch(&svm_arch_ops, THIS_MODULE);
-	return 0;
+	return kvm_init_arch(&svm_arch_ops, THIS_MODULE);
 }
 
 static void __exit svm_exit(void)
