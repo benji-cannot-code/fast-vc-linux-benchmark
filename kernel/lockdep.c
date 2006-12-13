9200bc6f7e94656ca23ccf8e897492e425e5a57e
Ingo Molnar <mingo@elte.hu>
2006-12-13 00:34:43 -0800

    [PATCH] lockdep: print irq-trace info on asserts
    When we print an assert due to scheduling-in-atomic bugs, and if lockdep
    is enabled, then the IRQ tracing information of lockdep can be printed
    to pinpoint the code location that disabled interrupts. This saved me
    quite a bit of debugging time in cases where the backtrace did not
    identify the irq-disabling site well enough.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -1448,7 +1448,7 @@ check_usage_backwards(struct task_struct *curr, struct held_lock *this,
 	return print_irq_inversion_bug(curr, backwards_match, this, 0, irqclass);
 }
 
-static inline void print_irqtrace_events(struct task_struct *curr)
+void print_irqtrace_events(struct task_struct *curr)
 {
 	printk("irq event stamp: %u\n", curr->irq_events);
 	printk("hardirqs last  enabled at (%u): ", curr->hardirq_enable_event);
@@ -1461,10 +1461,6 @@ static inline void print_irqtrace_events(struct task_struct *curr)
 	print_ip_sym(curr->softirq_disable_ip);
 }
 
-#else
-static inline void print_irqtrace_events(struct task_struct *curr)
-{
-}
 #endif
 
 static int
