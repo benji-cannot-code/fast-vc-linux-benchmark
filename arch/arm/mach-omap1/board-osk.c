0ba4ec587f206ddbdedd5028209c4d6609299ba8
David Brownell <david-b@pacbell.net>
2006-12-13 00:33:49 -0800

    [PATCH] another build fix, header rearrangements (OSK)
    Some of the header file rearrangements broke the build for board-osk.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: <tony@atomide.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/arm/mach-omap1/board-osk.c
+++ arch/arm/mach-omap1/board-osk.c
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/irq.h>
+#include <linux/interrupt.h>
 
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
