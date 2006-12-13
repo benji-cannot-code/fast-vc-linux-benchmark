4bfafd155abd0954c47627c0b01e04c796320b68
Linas Vepstas <linas@austin.ibm.com>
2006-12-13 15:08:25 -0600

    Spidernet add net_ratelimit to suppress long output
    This patch adds net_ratelimit to many of the printks in order to
    limit extraneous warning messages (created in response to Bug 28554).
    This patch supercedes all previous ratelimit patches.
    This has been tested, please apply.
    
    From: James K Lewis <jklewis@us.ibm.com>
    Signed-off-by: James K Lewis <jklewis@us.ibm.com>
    Signed-off-by: Linas Vepstas <jlinas@austin.ibm.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/spider_net.h
+++ drivers/net/spider_net.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SPIDER_NET_H
 #define _SPIDER_NET_H
 
-#define VERSION "1.6 A"
+#define VERSION "1.6 B"
 
 #include "sungem_phy.h"
 
