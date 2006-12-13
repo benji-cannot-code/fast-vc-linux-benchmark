0183c471e46e4dacb81037e15fba00e7f0c6cc5b
Mike Miller <mike.miller@hp.com>
2006-12-13 00:34:22 -0800

    [PATCH] cciss: remove calls to pci_disable_device
    Remove calls to pci_disable_device except in fail_all_cmds.  The
    pci_disable_device function does something nasty to Smart Array controllers
    that pci_enable_device does not undo.  So if the driver is unloaded it
    cannot be reloaded.
    
    Also, customers can disable any pci device via the ROM Based Setup Utility
    (RBSU).  If the customer has disabled the controller we should not try to
    blindly enable the card from the driver.  Please consider this for
    inclusion.
    
    Signed-off-by: Mike Miller <mike.miller@hp.com>
    Acked-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/block/cciss.c
+++ drivers/block/cciss.c
@@ -2838,7 +2838,7 @@ static int cciss_pci_init(ctlr_info_t *c, struct pci_dev *pdev)
 	if (err) {
 		printk(KERN_ERR "cciss: Cannot obtain PCI resources, "
 		       "aborting\n");
-		goto err_out_disable_pdev;
+		return err;
 	}
 
 	subsystem_vendor_id = pdev->subsystem_vendor;
@@ -3006,10 +3006,11 @@ static int cciss_pci_init(ctlr_info_t *c, struct pci_dev *pdev)
 	return 0;
 
       err_out_free_res:
+	/*
+	 * Deliberately omit pci_disable_device(): it does something nasty to
+	 * Smart Array controllers that pci_enable_device does not undo
+	 */
 	pci_release_regions(pdev);
-
-      err_out_disable_pdev:
-	pci_disable_device(pdev);
 	return err;
 }
 
@@ -3383,8 +3384,11 @@ static int __devinit cciss_init_one(struct pci_dev *pdev,
 		if (drv->queue)
 			blk_cleanup_queue(drv->queue);
 	}
+	/*
+	 * Deliberately omit pci_disable_device(): it does something nasty to
+	 * Smart Array controllers that pci_enable_device does not undo
+	 */
 	pci_release_regions(pdev);
-	pci_disable_device(pdev);
 	pci_set_drvdata(pdev, NULL);
 	free_hba(i);
 	return -1;
@@ -3453,8 +3457,11 @@ static void __devexit cciss_remove_one(struct pci_dev *pdev)
 #ifdef CONFIG_CISS_SCSI_TAPE
 	kfree(hba[i]->scsi_rejects.complete);
 #endif
+	/*
+	 * Deliberately omit pci_disable_device(): it does something nasty to
+	 * Smart Array controllers that pci_enable_device does not undo
+	 */
 	pci_release_regions(pdev);
-	pci_disable_device(pdev);
 	pci_set_drvdata(pdev, NULL);
 	free_hba(i);
 }
