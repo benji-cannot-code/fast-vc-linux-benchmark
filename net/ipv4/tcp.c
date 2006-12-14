e12ea2ecb9fa7b56430c8eaedab685f5849131c6
David S. Miller <davem@sunset.davemloft.net>
2006-12-13 16:25:44 -0800

    [TCP]: Fix oops caused by __tcp_put_md5sig_pool()
    It should call tcp_free_md5sig_pool() not __tcp_free_md5sig_pool()
    so that it does proper refcounting.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -2365,8 +2365,9 @@ struct tcp_md5sig_pool *__tcp_get_md5sig_pool(int cpu)
 
 EXPORT_SYMBOL(__tcp_get_md5sig_pool);
 
-void __tcp_put_md5sig_pool(void) {
-	__tcp_free_md5sig_pool(tcp_md5sig_pool);
+void __tcp_put_md5sig_pool(void)
+{
+	tcp_free_md5sig_pool();
 }
 
 EXPORT_SYMBOL(__tcp_put_md5sig_pool);
