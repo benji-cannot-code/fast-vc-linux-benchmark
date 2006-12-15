3962105362c51698ffcf4a6b8badf357b14c6cdb
Matthew Wilcox <matthew@wil.cx>
2006-12-15 09:34:36 -0700

    [PARISC] Remove sched.h from uaccess.h on parisc
    Al Viro did this for x86-64 and reduced the number of dependencies on
    sched.h significantly.  We had a couple of files which were relying on
    uaccess.h pulling in sched.h, so they need explicit dependencies added.
    
    Signed-off-by: Matthew Wilcox <matthew@wil.cx>
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- arch/parisc/kernel/unaligned.c
+++ arch/parisc/kernel/unaligned.c
@@ -21,8 +21,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  */
 
+#include <linux/jiffies.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/signal.h>
 #include <asm/uaccess.h>
 
 /* #define DEBUG_UNALIGNED 1 */
--- include/asm-parisc/uaccess.h
+++ include/asm-parisc/uaccess.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * User space memory access functions
  */
-#include <linux/sched.h>
 #include <asm/page.h>
 #include <asm/system.h>
 #include <asm/cache.h>
