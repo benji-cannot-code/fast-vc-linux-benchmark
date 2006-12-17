6752a2b8ff8934612189711a477fb364d0e7590c
Ben Dooks <ben-linux@fluff.org>
2006-12-17 23:32:21 +0100

    [ARM] 4051/1: S3C24XX: clean includes in S3C2440 and S3C2442 support
    Clean the includes in arch/arm/mach-s3c2410/s3c2440.c
    and arch/arm/mach-s3c2410/s3c2442.c which should have
    been pruned when these where split and updated.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c2410/s3c2440.c
+++ arch/arm/mach-s3c2410/s3c2440.c
@@ -30,17 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/irq.h>
 
-#include <asm/arch/regs-clock.h>
-#include <asm/arch/regs-serial.h>
-#include <asm/arch/regs-gpio.h>
-#include <asm/arch/regs-gpioj.h>
-#include <asm/arch/regs-dsc.h>
-
 #include "s3c2440.h"
-#include "clock.h"
 #include "devs.h"
 #include "cpu.h"
-#include "pm.h"
 
 static struct sys_device s3c2440_sysdev = {
 	.cls		= &s3c2440_sysclass,
