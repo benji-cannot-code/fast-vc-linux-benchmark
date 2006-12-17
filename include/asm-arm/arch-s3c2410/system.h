910796ad7071455980768628eaf984cf8dd2ed9f
Ben Dooks <ben-linux@fluff.org>
2006-12-17 19:59:20 +0100

    [ARM] 4038/1: S3C24XX: Fix copyrights in include/asm-arm/arch-s3c2410 (core)
    Fix copyright notices in include/asm-arm/arch-s3c2410
    to actually have `Copyright` in the line. This patch
    deals with all the core files.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-s3c2410/system.h
+++ include/asm-arm/arch-s3c2410/system.h
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* linux/include/asm-arm/arch-s3c2410/system.h
  *
- * (c) 2003 Simtec Electronics
- *  Ben Dooks <ben@simtec.co.uk>
+ * Copyright (c) 2003 Simtec Electronics
+ *	Ben Dooks <ben@simtec.co.uk>
  *
  * S3C2410 - System function defines and includes
  *
