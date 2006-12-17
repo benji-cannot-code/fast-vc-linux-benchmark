ab5c9455ef9b3030ee1cbf7120551d151995271c
Ben Dooks <ben-linux@fluff.org>
2006-12-17 20:18:40 +0100

    [ARM] 4042/1: H1940: Fix sparse errors from VA addresses
    Fix address-space conversion errors from passing addresses
    generated from include/asm-arm/arch-s3c2410/map.h by adding
    an __force argument to the `void __iomem *` for all the
    virtual addresses.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-s3c2410/h1940-latch.h
+++ include/asm-arm/arch-s3c2410/h1940-latch.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #ifndef __ASSEMBLY__
-#define H1940_LATCH		((void __iomem *)0xF8000000)
+#define H1940_LATCH		((void __force __iomem *)0xF8000000)
 #else
 #define H1940_LATCH		0xF8000000
 #endif
