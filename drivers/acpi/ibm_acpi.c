1d841a7f942b318802a2b7df08876240f4ccf980
Alexey Starikovskiy <alexey_y_starikovskiy@linux.intel.com>
2006-12-18 14:53:33 -0300

    ACPI: ibm_acpi: allow clean removal
    Allow clean removal by setting notify_installed in the right place.
    
    Signed-off-by: Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/ibm_acpi.c
+++ drivers/acpi/ibm_acpi.c
@@ -2504,7 +2504,7 @@ static int __init setup_notify(struct ibm_struct *ibm)
 		       ibm->name, status);
 		return -ENODEV;
 	}
-
+	ibm->notify_installed = 1;
 	return 0;
 }
 
@@ -2581,7 +2581,6 @@ static int __init ibm_init(struct ibm_struct *ibm)
 		ret = setup_notify(ibm);
 		if (ret < 0)
 			return ret;
-		ibm->notify_installed = 1;
 	}
 
 	return 0;
