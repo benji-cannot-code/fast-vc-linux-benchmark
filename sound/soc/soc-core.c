2e1bf70a7b9dd70924af89cc81aefbef5a4a4e3f
Liam Girdwood <lg@opensource.wolfsonmicro.com>
2006-12-18 14:39:02 +0100

    [ALSA] Additional credits to soc-core
    This patch adds copyright and credit for my good friend Richard Purdie
    from OpenedHand for his help and code contribution throughout the
    development of the core code. Many thanks Richard (I guess we overlooked
    this in trying to get everything working well).
    It also adds some extra comments wrt to DAI clock matching.
    
    Signed-off-by: Liam Girdwood <lg@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -3,8 +3,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * soc-core.c  --  ALSA SoC Audio Layer
  *
  * Copyright 2005 Wolfson Microelectronics PLC.
+ * Copyright 2005 Openedhand Ltd.
+ *
  * Author: Liam Girdwood
  *         liam.girdwood@wolfsonmicro.com or linux@wolfsonmicro.com
+ *         with code, comments and ideas from :-
+ *         Richard Purdie <richard@openedhand.com>
  *
  *  This program is free software; you can redistribute  it and/or modify it
  *  under  the terms of  the GNU General  Public License as published by the
@@ -518,7 +522,8 @@ found:
 	 * Check we have matching bitclocks. If we don't then it means the
 	 * sysclock returned by either the codec or cpu DAI (selected by the
 	 * machine sysclock function) is wrong compared with the supported DAI
-	 * modes for the codec or cpu DAI.
+	 * modes for the codec or cpu DAI. Check  your codec or CPU DAI
+	 * config_sysclock() functions.
 	 */
 	if (cpu_bclk != codec_bclk && cpu_bclk){
 		printk(KERN_ERR
