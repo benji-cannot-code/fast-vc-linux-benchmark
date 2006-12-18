b5b4842db902dddb17e6e74e78d0d0a8ee74fe22
Amit S. Kale <amitkale@netxen.com>
2006-12-18 05:52:39 -0800

    NetXen: Multiple adapter fix.
    Signed-off-by: Amit S. Kale <amitkale@netxen.com>
    
     netxen_nic.h      |    3 +--
     netxen_nic_main.c |   12 ------------
     2 files changed, 1 insertion(+), 14 deletions(-)
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/netxen/netxen_nic.h
+++ drivers/net/netxen/netxen_nic.h
@@ -64,7 +64,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "netxen_nic_hw.h"
 
-#define NETXEN_NIC_BUILD_NO     "1"
+#define NETXEN_NIC_BUILD_NO     "2"
 #define _NETXEN_NIC_LINUX_MAJOR 3
 #define _NETXEN_NIC_LINUX_MINOR 3
 #define _NETXEN_NIC_LINUX_SUBVERSION 2
@@ -888,7 +888,6 @@ struct netxen_adapter {
 	struct netxen_recv_context recv_ctx[MAX_RCV_CTX];
 
 	int is_up;
-	int number;
 	struct netxen_dummy_dma dummy_dma;
 
 	/* Context interface shared between card and host */
