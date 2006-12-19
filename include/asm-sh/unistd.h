44f95e7318694a512996a0596cedcb48268de89c
SUGIOKA Toshinobu <sugioka@itonet.co.jp>
2006-12-19 12:13:55 +0900

    sh: syscall 300 should be __NR_fstatat64.
    syscall number 300 fails while testing with latest LTP
    (ltp-full-20061121.tgz) on sh.
    
    sys_fstatat64 is called on syscall 300 (see arch/sh/kernel/syscalls.S),
    and __ARCH_WANT_STAT64 is defined in include/asm-sh/unistd.h, so
    following patch seems correct.
    
    Signed-off-by: SUGIOKA Toshinobu <sugioka@itonet.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/unistd.h
+++ include/asm-sh/unistd.h
@@ -308,7 +308,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_mknodat		297
 #define __NR_fchownat		298
 #define __NR_futimesat		299
-#define __NR_newfstatat		300
+#define __NR_fstatat64		300
 #define __NR_unlinkat		301
 #define __NR_renameat		302
 #define __NR_linkat		303
