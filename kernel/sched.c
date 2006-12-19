814fb284363c433b19cf3a08375cda28917bb250
Peter Williams <pwil3058@bigpond.net.au>
2006-12-19 12:48:50 +1000

    [PATCH] sched: improve efficiency of sched_fork()
    Problem:
      sched_fork() has always called scheduler_tick() in some (unlikely)
      circumstances in order to update the current task in light of those
      circumstances.  It has always been the case that the work done by
      scheduler_tick() was more than was required to handle the problem in
      hand but no harm was done except for the waste of a few CPU cycles.
    
      However, the splitting of scheduler_tick() into two procedures in
      2.6.20-rc1 enables the wasted cycles to be saved as the new procedure
      task_running_tick() does all the work that is required to rectify the
      problem being handled.
    
    Solution:
      Replace the call to scheduler_tick() in sched_fork() with a call to
      task_running_tick().
    
    Signed-off-by: Peter Williams <pwil3058@bigpond.com.au>
    Acked-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -1568,6 +1568,7 @@ int fastcall wake_up_state(struct task_struct *p, unsigned int state)
 	return try_to_wake_up(p, state, 0);
 }
 
+static void task_running_tick(struct rq *rq, struct task_struct *p);
 /*
  * Perform scheduler related setup for a newly forked process p.
  * p is forked by current.
@@ -1628,7 +1629,7 @@ void fastcall sched_fork(struct task_struct *p, int clone_flags)
 		 * runqueue lock is not a problem.
 		 */
 		current->time_slice = 1;
-		scheduler_tick();
+		task_running_tick(cpu_rq(cpu), current);
 	}
 	local_irq_enable();
 	put_cpu();
