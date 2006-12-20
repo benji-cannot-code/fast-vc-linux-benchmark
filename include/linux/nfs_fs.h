4ce6e429306b5b7113e0df5c8df917d707aec0f6
Benny Halevy <bhalevy@panasas.com>
2006-12-20 22:29:46 +0200

    NFS: move NFS_DEBUG definition
    Trond, looks like the changes to include/linux/nfs_fs.h in 2.6.18
    that moved the #include's of sunrpc header files into the #ifdef __KERNEL__ block
    disabled nfs debugging for all nfs c file not including any sunrpc header.
    
    The following patch moves the definition down, right before its use
    for defining ifdebug.
    
    Signed-off-by: Benny Halevy <bhalevy@panasas.com>
    (Moved definition further down into the __KERNEL__ section: Trond)
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- include/linux/nfs_fs.h
+++ include/linux/nfs_fs.h
@@ -12,14 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/magic.h>
 
-/*
- * Enable debugging support for nfs client.
- * Requires RPC_DEBUG.
- */
-#ifdef RPC_DEBUG
-# define NFS_DEBUG
-#endif
-
 /* Default timeout values */
 #define NFS_MAX_UDP_TIMEOUT	(60*HZ)
 #define NFS_MAX_TCP_TIMEOUT	(600*HZ)
@@ -568,6 +560,15 @@ extern void * nfs_root_data(void);
 #define NFSDBG_ALL		0xFFFF
 
 #ifdef __KERNEL__
+
+/*
+ * Enable debugging support for nfs client.
+ * Requires RPC_DEBUG.
+ */
+#ifdef RPC_DEBUG
+# define NFS_DEBUG
+#endif
+
 # undef ifdebug
 # ifdef NFS_DEBUG
 #  define ifdebug(fac)		if (unlikely(nfs_debug & NFSDBG_##fac))
