e50effb26edcddcff2c764126d66d7e2a91818f2
Thomas Hellstrom <thomas@tungstengraphics.com>
2006-12-20 16:33:41 +0100

    [AGPGART] Remove unnecessary flushes when inserting and removing pages.
    This patch is to speed up flipping of pages in and out of the AGP aperture as
    needed by the new drm memory manager.
    
    A number of global cache flushes are removed as well as some PCI posting flushes.
    The following guidelines have been used:
    
    1) Memory that is only mapped uncached and that has been subject to a global
    cache flush after the mapping was changed to uncached does not need any more
    cache flushes. Neither before binding to the aperture nor after unbinding.
    
    2) Only do one PCI posting flush after a sequence of writes modifying page
    entries in the GATT.
    
    Signed-off-by: Thomas Hellstrom <thomas@tungstengraphics.com>
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/char/agp/generic.c
+++ drivers/char/agp/generic.c
@@ -966,6 +966,9 @@ int agp_generic_insert_memory(struct agp_memory * mem, off_t pg_start, int type)
 	if (!bridge)
 		return -EINVAL;
 
+	if (mem->page_count == 0)
+		return 0;
+
 	temp = bridge->current_size;
 
 	switch (bridge->driver->size_type) {
@@ -1017,8 +1020,8 @@ int agp_generic_insert_memory(struct agp_memory * mem, off_t pg_start, int type)
 
 	for (i = 0, j = pg_start; i < mem->page_count; i++, j++) {
 		writel(bridge->driver->mask_memory(bridge, mem->memory[i], mem->type), bridge->gatt_table+j);
-		readl(bridge->gatt_table+j);	/* PCI Posting. */
 	}
+	readl(bridge->gatt_table+j-1);	/* PCI Posting. */
 
 	bridge->driver->tlb_flush(mem);
 	return 0;
@@ -1035,6 +1038,9 @@ int agp_generic_remove_memory(struct agp_memory *mem, off_t pg_start, int type)
 	if (!bridge)
 		return -EINVAL;
 
+	if (mem->page_count == 0)
+		return 0;
+
 	if (type != 0 || mem->type != 0) {
 		/* The generic routines know nothing of memory types */
 		return -EINVAL;
@@ -1043,10 +1049,9 @@ int agp_generic_remove_memory(struct agp_memory *mem, off_t pg_start, int type)
 	/* AK: bogus, should encode addresses > 4GB */
 	for (i = pg_start; i < (mem->page_count + pg_start); i++) {
 		writel(bridge->scratch_page, bridge->gatt_table+i);
-		readl(bridge->gatt_table+i);	/* PCI Posting. */
 	}
+	readl(bridge->gatt_table+i-1);	/* PCI Posting. */
 
-	global_cache_flush();
 	bridge->driver->tlb_flush(mem);
 	return 0;
 }
