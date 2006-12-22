0bb0227d4bc83cb5ce6d13cce41ecdf233dea71d
Michael Riepe <michael@mr511.de>
2006-12-22 01:05:53 -0800

    [PATCH] KVM: Handle p5 mce msrs
    This allows plan9 to get a little further booting.
    
    Signed-off-by: Michael Riepe <michael@mr511.de>
    Signed-off-by: Avi Kivity <avi@qumranet.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/kvm/svm.c
+++ drivers/kvm/svm.c
@@ -1074,6 +1074,8 @@ static int emulate_on_interception(struct kvm_vcpu *vcpu, struct kvm_run *kvm_ru
 static int svm_get_msr(struct kvm_vcpu *vcpu, unsigned ecx, u64 *data)
 {
 	switch (ecx) {
+	case MSR_IA32_P5_MC_ADDR:
+	case MSR_IA32_P5_MC_TYPE:
 	case MSR_IA32_MC0_CTL:
 	case MSR_IA32_MCG_STATUS:
 	case MSR_IA32_MCG_CAP:
