7fda44270f38251e1367d0460a6d4282418c93fa
Andrew Morton <akpm@osdl.org>
2006-12-27 14:36:37 -0800

    firewire: build fix
    alpha:
    
    drivers/firewire/fw-ohci.c: In function 'ar_context_tasklet':
    drivers/firewire/fw-ohci.c:266: warning: implicit declaration of function 'dma_sync_single_for_device'
    drivers/firewire/fw-ohci.c:267: error: 'DMA_TO_DEVICE' undeclared (first use in this function)
    drivers/firewire/fw-ohci.c:267: error: (Each undeclared identifier is reported only once
    drivers/firewire/fw-ohci.c:267: error: for each function it appears in.)
    drivers/firewire/fw-ohci.c: In function 'ar_context_init':
    drivers/firewire/fw-ohci.c:282: warning: implicit declaration of function 'dma_map_single'
    drivers/firewire/fw-ohci.c:283: error: 'DMA_TO_DEVICE' undeclared (first use in this function)
    drivers/firewire/fw-ohci.c:293: error: 'DMA_FROM_DEVICE' undeclared (first use in this function)
    etc.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Kristian Høgsberg <krh@redhat.com>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-ohci.c
+++ drivers/firewire/fw-ohci.c
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/delay.h>
 #include <linux/poll.h>
+#include <linux/dma-mapping.h>
+
 #include <asm/uaccess.h>
 #include <asm/semaphore.h>
 
