0a9de8a5710d07222a57b74a6a328e289b43cb0f
Mike Isely <isely@pobox.com>
2006-12-27 23:30:13 -0300

    V4L/DVB (5051): Pvrusb2: Better radio versus tv frequency handling
    Separate track radio versus tv frequency so that when we switch modes
    we can also switch to a sane frequency appropriate for the mode.  Also
    implement logic to automate mode switching in certain cases.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-hdw-internal.h
+++ drivers/media/video/pvrusb2/pvrusb2-hdw-internal.h
@@ -215,7 +215,6 @@ struct pvr2_hdw {
 	/* Frequency table */
 	unsigned int freqTable[FREQTABLE_SIZE];
 	unsigned int freqProgSlot;
-	unsigned int freqSlot;
 
 	/* Stuff for handling low level control interaction with device */
 	struct mutex ctl_lock_mutex;
@@ -261,7 +260,11 @@ struct pvr2_hdw {
 	/* Tuner / frequency control stuff */
 	unsigned int tuner_type;
 	int tuner_updated;
-	unsigned int freqVal;
+	unsigned int freqValTelevision;  /* Current freq for tv mode */
+	unsigned int freqValRadio;       /* Current freq for radio mode */
+	unsigned int freqSlotTelevision; /* Current slot for tv mode */
+	unsigned int freqSlotRadio;      /* Current slot for radio mode */
+	unsigned int freqSelector;       /* 0=radio 1=television */
 	int freqDirty;
 
 	/* Video standard handling */
@@ -324,6 +327,7 @@ struct pvr2_hdw {
 	VCREATE_DATA(res_hor);
 	VCREATE_DATA(res_ver);
 	VCREATE_DATA(srate);
+	VCREATE_DATA(automodeswitch);
 #undef VCREATE_DATA
 
 	struct pvr2_ctld_info *mpeg_ctrl_info;
@@ -331,6 +335,9 @@ struct pvr2_hdw {
 	struct pvr2_ctrl *controls;
 	unsigned int control_cnt;
 };
+
+/* This function gets the current frequency */
+unsigned long pvr2_hdw_get_cur_freq(struct pvr2_hdw *);
 
 #endif /* __PVRUSB2_HDW_INTERNAL_H */
 
