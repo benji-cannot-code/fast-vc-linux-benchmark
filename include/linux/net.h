2ca0b99d0495079f14de6431317ec01511fcfbc8
David Woodhouse <dwmw2@infradead.org>
2006-12-28 21:21:55 -0800

    [NET]: Don't export linux/random.h outside __KERNEL__.
    Don't add it there please; add it lower down inside the existing #ifdef
    __KERNEL__. You just made the _userspace_ net.h include random.h, which
    then fails to compile unless <asm/types.h> was already included.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/net.h
+++ include/linux/net.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_NET_H
 
 #include <linux/wait.h>
-#include <linux/random.h>
 #include <asm/socket.h>
 
 struct poll_table_struct;
@@ -58,6 +57,7 @@ typedef enum {
 
 #ifdef __KERNEL__
 #include <linux/stringify.h>
+#include <linux/random.h>
 
 #define SOCK_ASYNC_NOSPACE	0
 #define SOCK_ASYNC_WAITDATA	1
