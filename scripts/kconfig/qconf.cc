62be92f94a498c05089ff6cb029fd62fcf7c22da
Cyrill V. Gorcunov <gorcunov@gmail.com>
2007-01-05 16:37:02 -0800

    [PATCH] qconf: fix SIGSEGV on empty menu items
    qconf may cause SIGSEGV by trying to show debug information on empty menu
    items
    
    Signed-off-by: Cyrill V. Gorcunov <gorcunov@gmail.com>
    Cc: Roman Zippel <zippel@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- scripts/kconfig/qconf.cc
+++ scripts/kconfig/qconf.cc
@@ -926,6 +926,8 @@ ConfigInfoView::ConfigInfoView(QWidget* parent, const char *name)
 		configSettings->endGroup();
 		connect(configApp, SIGNAL(aboutToQuit()), SLOT(saveSettings()));
 	}
+
+	has_dbg_info = 0;
 }
 
 void ConfigInfoView::saveSettings(void)
@@ -954,10 +956,13 @@ void ConfigInfoView::setInfo(struct menu *m)
 	if (menu == m)
 		return;
 	menu = m;
-	if (!menu)
+	if (!menu) {
+		has_dbg_info = 0;
 		clear();
-	else
+	} else {
+		has_dbg_info = 1;
 		menuInfo();
+       }
 }
 
 void ConfigInfoView::setSource(const QString& name)
@@ -991,6 +996,9 @@ void ConfigInfoView::setSource(const QString& name)
 void ConfigInfoView::symbolInfo(void)
 {
 	QString str;
+
+	if (!has_dbg_info)
+		return;
 
 	str += "<big>Symbol: <b>";
 	str += print_filter(sym->name);
