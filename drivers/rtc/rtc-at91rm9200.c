19f405c7b39e0f55ee68ce9dd733d44a82ba3bc4
David Brownell <david-b@pacbell.net>
2007-01-05 16:36:25 -0800

    [PATCH] rtc-at91rm9200 build fix
    The at91rm9200 RTC driver needs some assistance to build, because of recent
    header file rearrangement.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Cc: Alessandro Zummo <alessandro.zummo@towertech.it>
    Cc: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/rtc/rtc-at91rm9200.c
+++ drivers/rtc/rtc-at91rm9200.c
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mach/time.h>
 
+#include <asm/arch/at91_rtc.h>
+
 
 #define AT91_RTC_FREQ		1
 #define AT91_RTC_EPOCH		1900UL	/* just like arch/arm/common/rtctime.c */
