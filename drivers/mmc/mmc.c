419444c4f374fc366a30974da3079ff9657111e4
Pierre Ossman <drzeus@drzeus.cx>
2007-01-07 16:59:06 +0100

    mmc: Graceful fallback for fancy features
    MMC high-speed, wide bus support and SD high-speed
    are functions that aren't critical for correct
    operation of the card. As such, they shouldn't mark
    the card as bad or dead when there is a failure
    activating these features.
    
    This is needed in particular on some really stupid
    hardware (e.g. Winbond's) where not all data transfer
    commands are supported.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -1093,7 +1093,8 @@ static void mmc_process_ext_csds(struct mmc_host *host)
 		mmc_wait_for_req(host, &mrq);
 
 		if (cmd.error != MMC_ERR_NONE || data.error != MMC_ERR_NONE) {
-			mmc_card_set_dead(card);
+			printk("%s: unable to read EXT_CSD, performance "
+				"might suffer.\n", mmc_hostname(card->host));
 			continue;
 		}
 
@@ -1109,7 +1110,6 @@ static void mmc_process_ext_csds(struct mmc_host *host)
 			printk("%s: card is mmc v4 but doesn't support "
 			       "any high-speed modes.\n",
 				mmc_hostname(card->host));
-			mmc_card_set_bad(card);
 			continue;
 		}
 
@@ -1290,7 +1290,9 @@ static void mmc_read_switch_caps(struct mmc_host *host)
 		mmc_wait_for_req(host, &mrq);
 
 		if (cmd.error != MMC_ERR_NONE || data.error != MMC_ERR_NONE) {
-			mmc_card_set_dead(card);
+			printk("%s: unable to read switch capabilities, "
+				"performance might suffer.\n",
+				mmc_hostname(card->host));
 			continue;
 		}
 
@@ -1322,12 +1324,8 @@ static void mmc_read_switch_caps(struct mmc_host *host)
 
 		mmc_wait_for_req(host, &mrq);
 
-		if (cmd.error != MMC_ERR_NONE || data.error != MMC_ERR_NONE) {
-			mmc_card_set_dead(card);
-			continue;
-		}
-
-		if ((status[16] & 0xF) != 1) {
+		if (cmd.error != MMC_ERR_NONE || data.error != MMC_ERR_NONE ||
+			(status[16] & 0xF) != 1) {
 			printk(KERN_WARNING "%s: Problem switching card "
 				"into high-speed mode!\n",
 				mmc_hostname(host));
