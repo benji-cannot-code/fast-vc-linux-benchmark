82d3d7176f79b626e278a19ca4ebd99ad1d67bec
Stefan Richter <stefanr@s5r6.in-berlin.de>
2007-01-07 21:49:27 +0100

    ieee1394: restore config ROM when resuming
    After PM suspend + resume, the local configuration ROM was not restored.
    This prevented remote nodes from recognizing the resuming machine.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/ieee1394_core.c
+++ drivers/ieee1394/ieee1394_core.c
@@ -1179,6 +1179,7 @@ module_exit(ieee1394_cleanup);
 /** hosts.c **/
 EXPORT_SYMBOL(hpsb_alloc_host);
 EXPORT_SYMBOL(hpsb_add_host);
+EXPORT_SYMBOL(hpsb_resume_host);
 EXPORT_SYMBOL(hpsb_remove_host);
 EXPORT_SYMBOL(hpsb_update_config_rom_image);
 
