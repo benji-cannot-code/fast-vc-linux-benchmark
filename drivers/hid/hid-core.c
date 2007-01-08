4c380d9c942ad0885b616fd323354d07fbbdd33b
Jiri Kosina <jkosina@suse.cz>
2007-01-08 13:42:43 +0100

    HID: mousepoll parameter makes no sense for generic HID
    mousepoll parameter makes no sense for generic HID code. It
    belongs to (and is implemented by) usbhid. This is also where
    all users are expecting it.
    
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -45,14 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DRIVER_LICENSE "GPL"
 
 /*
- * Module parameters.
- */
-
-static unsigned int hid_mousepoll_interval;
-module_param_named(mousepoll, hid_mousepoll_interval, uint, 0644);
-MODULE_PARM_DESC(mousepoll, "Polling interval of mice");
-
-/*
  * Register a new report for a device.
  */
 
