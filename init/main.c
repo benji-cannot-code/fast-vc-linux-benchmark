8ba974900e68d9c4935d047e5498a69fb9e0aaf7
Vivek Goyal <vgoyal@in.ibm.com>
2007-01-11 01:52:44 +0100

    [PATCH] i386: Convert some functions to __init to avoid MODPOST warnings
    o Some functions which should have been in init sections as they are called
      only once. Put them in init sections. Otherwise MODPOST generates warning
      as these functions are placed in .text and they end up accessing something
      in init sections.
    
    WARNING: vmlinux - Section mismatch: reference to .init.text:migration_init
    from .text between 'do_pre_smp_initcalls' (at offset 0xc01000d1) and
    'run_init_process'
    
    Signed-off-by: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- init/main.c
+++ init/main.c
@@ -696,7 +696,7 @@ static void __init do_basic_setup(void)
 	do_initcalls();
 }
 
-static void do_pre_smp_initcalls(void)
+static void __init do_pre_smp_initcalls(void)
 {
 	extern int spawn_ksoftirqd(void);
 #ifdef CONFIG_SMP
