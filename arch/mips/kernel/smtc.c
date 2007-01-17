76856ede66abe3af9cb5c394a75c1c7c86d88b21
Ralf Baechle <ralf@linux-mips.org>
2007-01-17 18:58:44 +0000

    [MIPS] SMTC: Fix cp0 hazard.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/smtc.c
+++ arch/mips/kernel/smtc.c
@@ -262,6 +262,7 @@ void smtc_configure_tlb(void)
 		    }
 		}
 		write_c0_mvpcontrol(read_c0_mvpcontrol() | MVPCONTROL_STLB);
+		ehb();
 
 		/*
 		 * Setup kernel data structures to use software total,
