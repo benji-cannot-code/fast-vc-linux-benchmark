5f141b3cd6508b415c3f8e74a764be751678bf1a
David Brownell <david-b@pacbell.net>
2007-01-17 22:11:27 +0100

    [ARM] 4098/1: ARM: rtc_lock only used with rtc_cmos
    Fix build glitches on ARM ... the only user of "rtc_lock" today is the
    optional PC-style "CMOS" RTC driver, the legacy SA1100 RTC driver is
    not even in the tree any more.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/time.c
+++ arch/arm/kernel/time.c
@@ -41,12 +41,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct sys_timer *system_timer;
 
+#if defined(CONFIG_RTC_DRV_CMOS) || defined(CONFIG_RTC_DRV_CMOS_MODULE)
 /* this needs a better home */
 DEFINE_SPINLOCK(rtc_lock);
 
-#ifdef CONFIG_SA1100_RTC_MODULE
+#ifdef CONFIG_RTC_DRV_CMOS_MODULE
 EXPORT_SYMBOL(rtc_lock);
 #endif
+#endif	/* pc-style 'CMOS' RTC support */
 
 /* change this if you have some constant time drift */
 #define USECS_PER_JIFFY	(1000000/HZ)
