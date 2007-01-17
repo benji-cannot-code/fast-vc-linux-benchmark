05a27926139f76ef7c149c8c50b4915ec70751d6
Kurt Hackel <kurt.hackel@oracle.com>
2007-01-17 14:54:39 -0800

    ocfs2_dlm: Drop inflight refmap even if no locks found on the lockres
    Signed-off-by: Kurt Hackel <kurt.hackel@oracle.com>
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/dlm/dlmrecovery.c
+++ fs/ocfs2/dlm/dlmrecovery.c
@@ -1909,11 +1909,9 @@ skip_lvb:
 
 leave:
 	/* balance the ref taken when the work was queued */
-	if (added > 0) {
-		spin_lock(&res->spinlock);
-		dlm_lockres_drop_inflight_ref(dlm, res);
-		spin_unlock(&res->spinlock);
-	}
+	spin_lock(&res->spinlock);
+	dlm_lockres_drop_inflight_ref(dlm, res);
+	spin_unlock(&res->spinlock);
 
 	if (ret < 0) {
 		mlog_errno(ret);
