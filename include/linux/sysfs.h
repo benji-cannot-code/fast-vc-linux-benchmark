3240f92cecf7e8791823a62f34a394866a980730
Frank Haverkamp <haver@vnet.ibm.com>
2007-01-17 17:51:18 +0100

    SYSFS: Fix missing include of list.h in sysfs.h
    Sysfs.h uses definitions (e.g. struct list_head s_sibling) from list.h
    but does not include it.
    
    Signed-off-by: Frank Haverkamp <haver@vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _SYSFS_H_
 
 #include <linux/compiler.h>
+#include <linux/list.h>
 #include <asm/atomic.h>
 
 struct kobject;
