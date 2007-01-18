d8bf5c764222e005c45dca876f9a1dd64d614e63
Kyungmin Park <kyungmin.park@samsung.com>
2007-01-18 11:10:57 +0900

    [MTD] OneNAND: Update copyrights and code cleanup
    Update copyrights and code cleanup
    
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>

--- include/linux/mtd/onenand.h
+++ include/linux/mtd/onenand.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *  linux/include/linux/mtd/onenand.h
  *
- *  Copyright (C) 2005-2006 Samsung Electronics
+ *  Copyright (C) 2005-2007 Samsung Electronics
  *  Kyungmin Park <kyungmin.park@samsung.com>
  *
  * This program is free software; you can redistribute it and/or modify
--- include/linux/mtd/onenand_regs.h
+++ include/linux/mtd/onenand_regs.h
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *  OneNAND Register header file
  *
- *  Copyright (C) 2005-2006 Samsung Electronics
+ *  Copyright (C) 2005-2007 Samsung Electronics
+ *  Kyungmin Park <kyungmin.park@samsung.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
