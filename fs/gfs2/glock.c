3f3bc9e0d362f7aad5b8530df8e184e8d09b71b4
Steven Whitehouse <swhiteho@redhat.com>
2007-01-19 13:57:36 -0500

    [GFS2] Remove unused go_callback operation
    This is never used, so we might as well remove it.
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -1569,8 +1569,6 @@ static void blocking_cb(struct gfs2_sbd *sdp, struct lm_lockname *name,
 	if (!gl)
 		return;
 
-	if (gl->gl_ops->go_callback)
-		gl->gl_ops->go_callback(gl, state);
 	handle_callback(gl, state);
 
 	spin_lock(&gl->gl_spin);
