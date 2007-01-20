0900a14c9a6ac1b9396928b3c0d5650683f310a9
Mike Isely <isely@pobox.com>
2007-01-20 00:30:23 -0300

    V4L/DVB (5089): Pvrusb2: V4L EXT_CTRLS fixup
    Attempts to enumerate or operate on a group of EXT_CTRLS where the
    group size is zero is OK; don't fail on such operations.  At least one
    application uses this to probe for the existence of this API so let it
    succeed.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-v4l2.c
+++ drivers/media/video/pvrusb2/pvrusb2-v4l2.c
@@ -637,6 +637,7 @@ static int pvr2_v4l2_do_ioctl(struct inode *inode, struct file *file,
 		struct v4l2_ext_control *ctrl;
 		unsigned int idx;
 		int val;
+		ret = 0;
 		for (idx = 0; idx < ctls->count; idx++) {
 			ctrl = ctls->controls + idx;
 			ret = pvr2_ctrl_get_value(
@@ -659,6 +660,7 @@ static int pvr2_v4l2_do_ioctl(struct inode *inode, struct file *file,
 			(struct v4l2_ext_controls *)arg;
 		struct v4l2_ext_control *ctrl;
 		unsigned int idx;
+		ret = 0;
 		for (idx = 0; idx < ctls->count; idx++) {
 			ctrl = ctls->controls + idx;
 			ret = pvr2_ctrl_set_value(
@@ -681,6 +683,7 @@ static int pvr2_v4l2_do_ioctl(struct inode *inode, struct file *file,
 		unsigned int idx;
 		/* For the moment just validate that the requested control
 		   actually exists. */
+		ret = 0;
 		for (idx = 0; idx < ctls->count; idx++) {
 			ctrl = ctls->controls + idx;
 			pctl = pvr2_hdw_get_ctrl_v4l(hdw,ctrl->id);
