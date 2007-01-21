d6901bac751260b6957a6c3946d4579bc85af342
Aapo Tahkola <aet@rasterburn.org>
2007-01-21 15:57:20 -0300

    V4L/DVB (5139): M920x: Fix tuner identification bug with qt1010 module
    Fixes qt1010 identification bug with megasky caused by
    the Quantek QT1010 tuner module patch.
    
    Signed-off-by: Aapo Tahkola <aet@rasterburn.org>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/m920x.h
+++ drivers/media/dvb/dvb-usb/m920x.h
@@ -19,9 +19,18 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define M9206_FW	0x30
 
 #define M9206_MAX_FILTERS 8
+
+#define M9206_I2C_TUNER	0
+#define M9206_I2C_DEMOD	1
+#define M9206_I2C_MAX	2
+
 struct m9206_state {
 	u16 filters[M9206_MAX_FILTERS];
 	int filtering_enabled;
 	int rep_count;
+	struct {
+		unsigned char addr;
+		unsigned char magic;
+	}i2c_r[M9206_I2C_MAX];
 };
 #endif
