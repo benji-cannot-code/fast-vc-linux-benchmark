30c9e12fcfb1ec5d5a47982336157b666e3bafce
Adrian Bunk <bunk@stusta.de>
2007-01-22 19:17:37 +0100

    firewire: cleanups
    This patch contains the following cleanups:
    - "extern inline" -> "static inline"
    - fw-topology.c: make struct fw_node_create static
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-topology.h
+++ drivers/firewire/fw-topology.h
@@ -58,13 +58,13 @@ struct fw_node {
         struct fw_port ports[0];
 };
 
-extern inline struct fw_node *
+static inline struct fw_node *
 fw_node(struct list_head *l)
 {
         return list_entry (l, struct fw_node, link);
 }
 
-extern inline struct fw_node *
+static inline struct fw_node *
 fw_node_get(struct fw_node *node)
 {
 	atomic_inc(&node->ref_count);
@@ -72,7 +72,7 @@ fw_node_get(struct fw_node *node)
 	return node;
 }
 
-extern inline void
+static inline void
 fw_node_put(struct fw_node *node)
 {
 	if (atomic_dec_and_test(&node->ref_count))
