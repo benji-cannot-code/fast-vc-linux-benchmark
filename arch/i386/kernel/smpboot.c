e19c93993105446003dd70022dbf7f53f11a30ff
James Bottomley <James.Bottomley@SteelEye.com>
2007-01-22 09:18:31 -0600

    [PATCH] x86: fix PDA variables to work during boot
    The current PDA code, which went in in post 2.6.19 has a flaw in that it
    doesn't correctly cycle the GDT and %GS segment through the boot PDA,
    the CPU PDA and finally the per-cpu PDA.
    
    The bug generally doesn't show up if the boot CPU id is zero, but
    everything falls apart for a non zero boot CPU id.  The basically kills
    voyager which is perfectly capable of doing non zero CPU id boots, so
    voyager currently won't boot without this.
    
    The fix is to be careful and actually do the GDT setups correctly.
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>
    Cc: Andi Kleen <ak@suse.de>
    Cc: Jeremy Fitzhardinge <jeremy@goop.org>
    Cc: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/i386/kernel/smpboot.c
+++ arch/i386/kernel/smpboot.c
@@ -597,6 +597,12 @@ static void __cpuinit start_secondary(void *unused)
 void __devinit initialize_secondary(void)
 {
 	/*
+	 * switch to the per CPU GDT we already set up
+	 * in do_boot_cpu()
+	 */
+	cpu_set_gdt(current_thread_info()->cpu);
+
+	/*
 	 * We don't actually need to load the full TSS,
 	 * basically just the stack pointer and the eip.
 	 */
@@ -972,9 +978,6 @@ static int __cpuinit do_boot_cpu(int apicid, int cpu)
 	printk("Booting processor %d/%d eip %lx\n", cpu, apicid, start_eip);
 	/* Stack for startup_32 can be just as for start_secondary onwards */
 	stack_start.esp = (void *) idle->thread.esp;
-
-	start_pda = cpu_pda(cpu);
-	cpu_gdt_descr = per_cpu(cpu_gdt_descr, cpu);
 
 	irq_ctx_init(cpu);
 
