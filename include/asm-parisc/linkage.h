453dc22a7b5912058006fb0cb0131c2cb0df418d
Helge Deller <deller@gmx.de>
2007-01-23 23:32:10 +0100

    [PARISC] fix ENTRY() and ENDPROC() for 64bit-parisc
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- include/asm-parisc/linkage.h
+++ include/asm-parisc/linkage.h
@@ -8,12 +8,22 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 /*
- * In parisc assembly a semicolon marks a comment.
- * Because of that we use an exclamation mark to seperate independend lines.
+ * In parisc assembly a semicolon marks a comment while a
+ * exclamation mark is used to seperate independend lines.
  */
 #define ENTRY(name) \
-	.globl name !\
+	.export name !\
 	ALIGN !\
 name:
+
+#ifdef CONFIG_64BIT
+#define ENDPROC(name) \
+	END(name)
+#else
+#define ENDPROC(name) \
+	.type name, @function !\
+	END(name)
+#endif
+
 
 #endif  /* __ASM_PARISC_LINKAGE_H */
