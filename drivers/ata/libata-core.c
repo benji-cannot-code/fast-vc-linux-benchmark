853707bb89da14e6178151d68bd95963a39e798f
Jeff Garzik <jeff@garzik.org>
2007-01-31 07:43:15 -0500

    [libata] Shuffle DRV_xxx in core and SiS drivers, to kill warnings
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -60,6 +60,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "libata.h"
 
+#define DRV_VERSION	"2.10"	/* must be exactly four chars */
+
+
 /* debounce timing parameters in msecs { interval, duration, timeout } */
 const unsigned long sata_deb_timing_normal[]		= {   5,  100, 2000 };
 const unsigned long sata_deb_timing_hotplug[]		= {  25,  500, 2000 };
--- drivers/ata/pata_sis.c
+++ drivers/ata/pata_sis.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ata.h>
 #include "libata.h"
 
+#undef DRV_NAME		/* already defined in libata.h, for libata-core */
 #define DRV_NAME	"pata_sis"
 #define DRV_VERSION	"0.4.5"
 
