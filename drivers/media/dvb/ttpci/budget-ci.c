d392c4a3593da405212305378a58dcc545e44315
Trent Piepho <xyzzy@speakeasy.org>
2007-01-30 23:25:46 -0300

    V4L/DVB (5164): Compat: Handle input_register_device() change and some others
    input_register_device() was changed to return an error code instead of
    being void in 2.6.15.  Handle it with a macro wrapper in config.h.  For
    this to work, linux/input.h must be included before config.h.  This
    required some trivial header re-ordering in budget-ci.c and ttusb_dec.c.
    In kernel 2.6.15-rc1 a helper function called setup_timer() was added to
    linux/timer.h.  Add to compat.h, but require that linux/timer.h be
    included first to give the definition of struct timer_list.
    A new 4GB DMA zone, __GFP_DMA32, was added in 2.6.15-rc2.  Alias it to
    __GFP_DMA on older kernels.
    Handle another 2.6.15 "input_dev->dev to input_dev->cdev.dev" change for
    some recently added code in cinergyT2.c.
    
    Signed-off-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/ttpci/budget-ci.c
+++ drivers/media/dvb/ttpci/budget-ci.c
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * the project's page is at http://www.linuxtv.org/dvb/
  */
 
-#include "budget.h"
-
 #include <linux/module.h>
 #include <linux/errno.h>
 #include <linux/slab.h>
@@ -39,6 +37,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/input.h>
 #include <linux/spinlock.h>
 #include <media/ir-common.h>
+
+#include "budget.h"
 
 #include "dvb_ca_en50221.h"
 #include "stv0299.h"
