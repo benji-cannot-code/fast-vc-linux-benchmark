ef5652448d4b4ffff41dd8070d21c7b4ac0c0df6
Ralf Baechle <ralf@linux-mips.org>
2007-01-31 14:09:29 -0500

    s2io: De-typedef driver.
    Removed namespace collisions due to usage of nic_t as per Ralf's patch
    
    Signed-off-by: Sivakumar Subramani <sivakumar.subramani@neterion.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/s2io-regs.h
+++ drivers/net/s2io-regs.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define TBD 0
 
-typedef struct _XENA_dev_config {
+struct XENA_dev_config {
 /* Convention: mHAL_XXX is mask, vHAL_XXX is value */
 
 /* General Control-Status Registers */
@@ -853,9 +853,9 @@ typedef struct _XENA_dev_config {
 #define SPI_CONTROL_DONE		BIT(6)
 	u64 spi_data;
 #define SPI_DATA_WRITE(data,len)	vBIT(data,0,len)
-} XENA_dev_config_t;
+};
 
-#define XENA_REG_SPACE	sizeof(XENA_dev_config_t)
+#define XENA_REG_SPACE	sizeof(struct XENA_dev_config)
 #define	XENA_EEPROM_SPACE (0x01 << 11)
 
 #endif				/* _REGS_H */
