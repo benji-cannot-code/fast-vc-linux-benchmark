c702b616455875f836999621a3f5aa2be4ea0717
Divy Le Ray <divy@chelsio.com>
2007-01-30 19:44:35 -0800

    cxgb3 - Add dual licensing
    Dual licensing, needed for OFED 1.2
    
    Signed-off-by: Divy Le Ray <divy@chelsio.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/cxgb3/cxgb3_defs.h
+++ drivers/net/cxgb3/cxgb3_defs.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Copyright (c) 2006 Chelsio, Inc. All rights reserved.
- * Copyright (c) 2006 Open Grid Computing, Inc. All rights reserved.
+ * Copyright (c) 2006-2007 Chelsio, Inc. All rights reserved.
+ * Copyright (c) 2006-2007 Open Grid Computing, Inc. All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
