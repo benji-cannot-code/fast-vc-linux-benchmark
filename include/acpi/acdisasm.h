1b428783fe629af00eec6501ccd57ce1f18d24d1
Bob Moore <robert.moore@intel.com>
2007-02-02 19:48:21 +0300

    ACPICA: Add full table name to disassembler output
    Signed-off-by: Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acdisasm.h
+++ include/acpi/acdisasm.h
@@ -102,6 +102,7 @@ typedef const struct acpi_dmtable_info {
 #define ACPI_DMT_MADT                   24
 #define ACPI_DMT_SRAT                   25
 #define ACPI_DMT_EXIT                   26
+#define ACPI_DMT_SIG                    27
 
 typedef
 void (*ACPI_TABLE_HANDLER) (struct acpi_table_header * table);
@@ -110,6 +111,7 @@ struct acpi_dmtable_data {
 	char *signature;
 	struct acpi_dmtable_info *table_info;
 	ACPI_TABLE_HANDLER table_handler;
+	char *name;
 };
 
 struct acpi_op_walk_info {
