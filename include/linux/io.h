68c5e9c8b1ddfc6f8e19e24ce21ec025f01ab7ab
Andrew Morton <akpm@linux-foundation.org>
2007-02-02 18:07:15 -0800

    git-libata-all: forward declare struct device
    In file included from drivers/infiniband/hw/ipath/ipath_diag.c:44:
    include/linux/io.h:35: warning: 'struct device' declared inside parameter list
    include/linux/io.h:35: warning: its scope is only this definition or declaration
    
    Cc: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- include/linux/io.h
+++ include/linux/io.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/page.h>
 
+struct device;
+
 void __iowrite32_copy(void __iomem *to, const void *from, size_t count);
 void __iowrite64_copy(void __iomem *to, const void *from, size_t count);
 
