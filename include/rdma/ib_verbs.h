54d6812b5e0ea0cb887a6837cf2ad3b51db2ebd2
Michael S. Tsirkin <mst@mellanox.co.il>
2007-02-04 14:11:55 -0800

    IB: Include <linux/kref.h> explicitly in <rdma/ib_verbs.h>
    <rdma/ib_verbs.h> uses struct kref, so it should include <linux/kref.h>
    explicitly to avoid hidden include dependencies.
    
    Signed-off-by: Michael S. Tsirkin <mst@mellanox.co.il>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/mm.h>
 #include <linux/dma-mapping.h>
+#include <linux/kref.h>
 
 #include <asm/atomic.h>
 #include <asm/scatterlist.h>
