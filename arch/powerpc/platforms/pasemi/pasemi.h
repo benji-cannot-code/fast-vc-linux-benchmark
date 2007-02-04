a4d0fe3e09b3fe1783abb62fa5d9edcdaf2a9195
Olof Johansson <olof@lixom.net>
2007-02-04 16:36:54 -0600

    [POWERPC] pasemi: Configure DMA controller interrupts
    The DMA controller on PWRficient is somewhat special -- has a PCI header
    so it looks like it's on the root PCI (-Express) root bus, but it uses
    more than the default number of interrupts (and they are hardwired).
    
    We need to wire up all interrupts for the DMA controller. The generic
    IRQ code will only map the primary interrupt from the PCI header (128),
    so add 129->211 by hand.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/pasemi/pasemi.h
+++ arch/powerpc/platforms/pasemi/pasemi.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern unsigned long pas_get_boot_time(void);
 extern void pas_pci_init(void);
+extern void __devinit pas_pci_irq_fixup(struct pci_dev *dev);
 
 extern void __init pasemi_idle_init(void);
 
