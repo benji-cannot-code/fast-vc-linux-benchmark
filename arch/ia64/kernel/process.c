9eabb3d8a2a9bdc8eed63b2e227ffdc37c028038
Horms <horms@verge.net.au>
2007-02-05 13:49:10 -0800

    [IA64] kexec: Move machine_shutdown from machine_kexec.c to process.c
    This moves the ia64 implementation of machine_shutdown() from
    machine_kexec.c to process.c, which is in keeping with the implelmentation
    on other architectures, and seems like a much more appropriate home for it.
    
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/process.c
+++ arch/ia64/kernel/process.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ia32.h>
 #include <asm/irq.h>
 #include <asm/kdebug.h>
+#include <asm/kexec.h>
 #include <asm/pgalloc.h>
 #include <asm/processor.h>
 #include <asm/sal.h>
@@ -802,6 +803,21 @@ cpu_halt (void)
 
 	while (1)
 		ia64_pal_halt(min_power_state);
+}
+
+void machine_shutdown(void)
+{
+#ifdef CONFIG_HOTPLUG_CPU
+	int cpu;
+
+	for_each_online_cpu(cpu) {
+		if (cpu != smp_processor_id())
+			cpu_down(cpu);
+	}
+#endif
+#ifdef CONFIG_KEXEC
+	kexec_disable_iosapic();
+#endif
 }
 
 void
