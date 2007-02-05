b7bd7a94b0864aec83c7ce8dcfd7867741d23fb9
Horst Hummel <horst.hummel@de.ibm.com>
2007-02-05 21:17:24 +0100

    [S390] dasd: fix unconditional reserve handling.
    The reserve/release IOCTLs sometimes do not work. If second system
    does a 'steal lock' the pending unit check (Format 3 Msg F) is
    delivered. Since ERP is disabled for reserve/release, the IOCTL call
    fails. We have to allow basic ERP (retries) for reserve/release IOCTLs.
    
    Signed-off-by: Horst Hummel <horst.hummel@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/block/dasd_eckd.c
+++ drivers/s390/block/dasd_eckd.c
@@ -1381,7 +1381,7 @@ dasd_eckd_release(struct dasd_device *device)
 	cqr->device = device;
 	clear_bit(DASD_CQR_FLAGS_USE_ERP, &cqr->flags);
 	set_bit(DASD_CQR_FLAGS_FAILFAST, &cqr->flags);
-	cqr->retries = 0;
+	cqr->retries = 2;	/* set retry counter to enable basic ERP */
 	cqr->expires = 2 * HZ;
 	cqr->buildclk = get_clock();
 	cqr->status = DASD_CQR_FILLED;
@@ -1421,7 +1421,7 @@ dasd_eckd_reserve(struct dasd_device *device)
 	cqr->device = device;
 	clear_bit(DASD_CQR_FLAGS_USE_ERP, &cqr->flags);
 	set_bit(DASD_CQR_FLAGS_FAILFAST, &cqr->flags);
-	cqr->retries = 0;
+	cqr->retries = 2;	/* set retry counter to enable basic ERP */
 	cqr->expires = 2 * HZ;
 	cqr->buildclk = get_clock();
 	cqr->status = DASD_CQR_FILLED;
@@ -1460,7 +1460,7 @@ dasd_eckd_steal_lock(struct dasd_device *device)
 	cqr->device = device;
 	clear_bit(DASD_CQR_FLAGS_USE_ERP, &cqr->flags);
 	set_bit(DASD_CQR_FLAGS_FAILFAST, &cqr->flags);
-	cqr->retries = 0;
+	cqr->retries = 2;	/* set retry counter to enable basic ERP */
 	cqr->expires = 2 * HZ;
 	cqr->buildclk = get_clock();
 	cqr->status = DASD_CQR_FILLED;
