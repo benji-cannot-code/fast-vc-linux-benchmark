922d984e44c47cce5a6e4e1f34e2ce4bb08587ee
Heiko Carstens <heiko.carstens@de.ibm.com>
2007-02-05 21:17:16 +0100

    [S390] sclp: don't call local_bh_disable/_local_bh_enable if in_interrupt()
    local_bh_disable/_local_bh_enable must not be called if in_irq() is
    true. Besides that if in_interrupt() is true bottom halves are
    disabled anyway.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/char/sclp.c
+++ drivers/s390/char/sclp.c
@@ -403,6 +403,7 @@ sclp_sync_wait(void)
 	unsigned long flags;
 	unsigned long cr0, cr0_sync;
 	u64 timeout;
+	int irq_context;
 
 	/* We'll be disabling timer interrupts, so we need a custom timeout
 	 * mechanism */
@@ -415,7 +416,9 @@ sclp_sync_wait(void)
 	}
 	local_irq_save(flags);
 	/* Prevent bottom half from executing once we force interrupts open */
-	local_bh_disable();
+	irq_context = in_interrupt();
+	if (!irq_context)
+		local_bh_disable();
 	/* Enable service-signal interruption, disable timer interrupts */
 	trace_hardirqs_on();
 	__ctl_store(cr0, 0, 0);
@@ -436,7 +439,8 @@ sclp_sync_wait(void)
 	}
 	local_irq_disable();
 	__ctl_load(cr0, 0, 0);
-	_local_bh_enable();
+	if (!irq_context)
+		_local_bh_enable();
 	local_irq_restore(flags);
 }
 
