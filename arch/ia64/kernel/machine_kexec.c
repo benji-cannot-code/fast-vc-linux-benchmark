377d6225f194d88c6c112d5c781731fa969973a5
Horms <horms@verge.net.au>
2007-02-05 10:16:20 +0900

    [IA64] kexec: Remove inline declaration of efi_get_pal_addr()
    Remove the Remove inline declaration of efi_get_pal_addr() as it is
    declared in linux/efi.h.
    
    Signed-Off-By: Simon Horman <horms@verge.net.au>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/machine_kexec.c
+++ arch/ia64/kernel/machine_kexec.c
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kexec.h>
 #include <linux/cpu.h>
 #include <linux/irq.h>
+#include <linux/efi.h>
 #include <asm/mmu_context.h>
 #include <asm/setup.h>
 #include <asm/delay.h>
@@ -86,7 +87,6 @@ void machine_shutdown(void)
  * Do not allocate memory (or fail in any way) in machine_kexec().
  * We are past the point of no return, committed to rebooting now.
  */
-extern void *efi_get_pal_addr(void);
 static void ia64_machine_kexec(struct unw_frame_info *info, void *arg)
 {
 	struct kimage *image = arg;
