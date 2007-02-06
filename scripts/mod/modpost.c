d67415dad9ad90ac7758832943dd704278788c6b
Matthew Wilcox <matthew@wil.cx>
2007-02-05 16:34:00 -0800

    [PATCH] PA-RISC: Fix bogus warnings from modpost
    parisc and parisc64 seem to name sections a little differently from other
    targets.  parisc64 gives spurious warnings like:
    
    WARNING: drivers/net/dummy.o - Section mismatch: reference to .init.text:dummy_setup from .data.rel.ro between '.LC1' (at offset 0x0) and '.LC6'
    
    and parisc gives spurious warnings like:
    
    WARNING: drivers/net/dummy.o - Section mismatch: reference to .init.text:dummy_setup from .rodata.cst4 between '.LC1' (at offset 0x0) and '.LC6'
    
    Given the other comments in modpost.c, it seems that the best solution is
    to move rodata down to the 'match at start of name' section and add
    .data.rel.ro to the 'match entire name' section.
    
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Kyle McMartin <kyle@mcmartin.ca>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -911,7 +911,7 @@ static int init_section_ref_ok(const char *name)
 		".opd",   /* see comment [OPD] at exit_section_ref_ok() */
 		".toc1",  /* used by ppc64 */
 		".stab",
-		".rodata",
+		".data.rel.ro", /* used by parisc64 */
 		".parainstructions",
 		".text.lock",
 		"__bug_table", /* used by powerpc for BUG() */
@@ -934,6 +934,7 @@ static int init_section_ref_ok(const char *name)
 		".eh_frame",
 		".debug",
 		".parainstructions",
+		".rodata",
 		NULL
 	};
 	/* part of section name */
