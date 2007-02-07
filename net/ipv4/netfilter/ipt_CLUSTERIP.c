cfc19d763f2a816e7af4ebb9a0b671ea1306b94c
Patrick McHardy <kaber@trash.net>
2007-02-07 15:07:43 -0800

    [NET]: Add UDPLITE support in a few missing spots
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/netfilter/ipt_CLUSTERIP.c
+++ net/ipv4/netfilter/ipt_CLUSTERIP.c
@@ -248,6 +248,7 @@ clusterip_hashfn(struct sk_buff *skb, struct clusterip_config *config)
 	switch (iph->protocol) {
 	case IPPROTO_TCP:
 	case IPPROTO_UDP:
+	case IPPROTO_UDPLITE:
 	case IPPROTO_SCTP:
 	case IPPROTO_DCCP:
 	case IPPROTO_ICMP:
