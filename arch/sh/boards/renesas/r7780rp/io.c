7b48c2841205190faef264c715264928df6c18e3
Paul Mundt <lethal@linux-sh.org>
2007-02-07 18:35:39 +0900

    sh: dcache write-back for R7780RP PIO.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/renesas/r7780rp/io.c
+++ arch/sh/boards/renesas/r7780rp/io.c
@@ -215,6 +215,8 @@ void r7780rp_insw(unsigned long port, void *dst, unsigned long count)
 
 	while (count--)
 		*buf++ = *p;
+
+	flush_dcache_all();
 }
 
 void r7780rp_insl(unsigned long port, void *dst, unsigned long count)
@@ -272,6 +274,8 @@ void r7780rp_outsw(unsigned long port, const void *src, unsigned long count)
 
 	while (count--)
 		*p = *buf++;
+
+	flush_dcache_all();
 }
 
 void r7780rp_outsl(unsigned long port, const void *src, unsigned long count)
