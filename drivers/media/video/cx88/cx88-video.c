0bca3a96a381bb2d88c6d5b6222c045b90d3b57e
Adrian Bunk <bunk@stusta.de>
2007-02-06 21:53:04 -0300

    V4L/DVB (5199): Cx88-video.c: remove struct radionorms
    This patch removes the unused struct radionorms.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx88/cx88-video.c
+++ drivers/media/video/cx88/cx88-video.c
@@ -87,8 +87,6 @@ static LIST_HEAD(cx8800_devlist);
 /* ------------------------------------------------------------------- */
 /* static data                                                         */
 
-v4l2_std_id radionorms[] = { 0 };
-
 static struct cx8800_fmt formats[] = {
 	{
 		.name     = "8 bpp, gray",
