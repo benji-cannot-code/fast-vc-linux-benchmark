a16e5b3db567cb40b05680a0f9d582ea6c2a79ef
Jan Engelhardt <jengelh@gmx.de>
2007-02-07 15:06:43 -0800

    [NETFILTER]: Remove useless comparisons before assignments
    Remove unnecessary if() constructs before assignment.
    
    Signed-off-by: Jan Engelhardt <jengelh@gmx.de>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/xt_CONNMARK.c
+++ net/netfilter/xt_CONNMARK.c
@@ -62,7 +62,7 @@ target(struct sk_buff **pskb,
 #else
 				nf_conntrack_event_cache(IPCT_MARK, *pskb);
 #endif
-		}
+			}
 			break;
 		case XT_CONNMARK_SAVE:
 			newmark = (*ctmark & ~markinfo->mask) |
@@ -79,8 +79,7 @@ target(struct sk_buff **pskb,
 		case XT_CONNMARK_RESTORE:
 			mark = (*pskb)->mark;
 			diff = (*ctmark ^ mark) & markinfo->mask;
-			if (diff != 0)
-				(*pskb)->mark = mark ^ diff;
+			(*pskb)->mark = mark ^ diff;
 			break;
 		}
 	}
