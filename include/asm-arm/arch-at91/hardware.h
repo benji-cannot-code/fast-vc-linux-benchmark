e8e0d51e6dbd9b19bf023e29302279a59877f4a3
Andrew Victor <andrew@sanpeople.com>
2007-02-08 09:42:40 +0100

    [ARM] 4145/2: AT91: Add support for AT91SAM9263 processor
    Add support for the Atmel AT91SAM9263 processor.  It is similar to the
    AT91SAM9260 but with more integrated peripherals, 5 GPIO banks, etc.
    
    Original patch from Nicolas Ferre.
    
    Signed-off-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-at91/hardware.h
+++ include/asm-arm/arch-at91/hardware.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/at91sam9260.h>
 #elif defined(CONFIG_ARCH_AT91SAM9261)
 #include <asm/arch/at91sam9261.h>
+#elif defined(CONFIG_ARCH_AT91SAM9263)
+#include <asm/arch/at91sam9263.h>
 #else
 #error "Unsupported AT91 processor"
 #endif
