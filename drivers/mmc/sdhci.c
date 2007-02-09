474f1234c16c2f2be69078a27011aa279defc1f8
Pierre Ossman <drzeus@drzeus.cx>
2007-02-09 08:29:19 +0100

    mmc: sdhci: Stop asking for mail
    We get enough error reports without having to ask for it.
    Remove notices about mailing the development list.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/sdhci.c
+++ drivers/mmc/sdhci.c
@@ -24,8 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRIVER_NAME "sdhci"
 
-#define BUGMAIL "<sdhci-devel@list.drzeus.cx>"
-
 #define DBG(f, x...) \
 	pr_debug(DRIVER_NAME " [%s()]: " f, __func__,## x)
 
@@ -154,8 +152,7 @@ static void sdhci_reset(struct sdhci_host *host, u8 mask)
 	/* hw clears the bit when it's done */
 	while (readb(host->ioaddr + SDHCI_SOFTWARE_RESET) & mask) {
 		if (timeout == 0) {
-			printk(KERN_ERR "%s: Reset 0x%x never completed. "
-				"Please report this to " BUGMAIL ".\n",
+			printk(KERN_ERR "%s: Reset 0x%x never completed.\n",
 				mmc_hostname(host->mmc), (int)mask);
 			sdhci_dumpregs(host);
 			return;
@@ -474,12 +471,11 @@ static void sdhci_finish_data(struct sdhci_host *host)
 
 	if ((data->error == MMC_ERR_NONE) && blocks) {
 		printk(KERN_ERR "%s: Controller signalled completion even "
-			"though there were blocks left. Please report this "
-			"to " BUGMAIL ".\n", mmc_hostname(host->mmc));
+			"though there were blocks left.\n",
+			mmc_hostname(host->mmc));
 		data->error = MMC_ERR_FAILED;
 	} else if (host->size != 0) {
-		printk(KERN_ERR "%s: %d bytes were left untransferred. "
-			"Please report this to " BUGMAIL ".\n",
+		printk(KERN_ERR "%s: %d bytes were left untransferred.\n",
 			mmc_hostname(host->mmc), host->size);
 		data->error = MMC_ERR_FAILED;
 	}
@@ -526,8 +522,7 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	while (readl(host->ioaddr + SDHCI_PRESENT_STATE) & mask) {
 		if (timeout == 0) {
 			printk(KERN_ERR "%s: Controller never released "
-				"inhibit bit(s). Please report this to "
-				BUGMAIL ".\n", mmc_hostname(host->mmc));
+				"inhibit bit(s).\n", mmc_hostname(host->mmc));
 			sdhci_dumpregs(host);
 			cmd->error = MMC_ERR_FAILED;
 			tasklet_schedule(&host->finish_tasklet);
@@ -548,8 +543,7 @@ static void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	sdhci_set_transfer_mode(host, cmd->data);
 
 	if ((cmd->flags & MMC_RSP_136) && (cmd->flags & MMC_RSP_BUSY)) {
-		printk(KERN_ERR "%s: Unsupported response type! "
-			"Please report this to " BUGMAIL ".\n",
+		printk(KERN_ERR "%s: Unsupported response type!\n",
 			mmc_hostname(host->mmc));
 		cmd->error = MMC_ERR_INVALID;
 		tasklet_schedule(&host->finish_tasklet);
@@ -647,9 +641,8 @@ static void sdhci_set_clock(struct sdhci_host *host, unsigned int clock)
 	while (!((clk = readw(host->ioaddr + SDHCI_CLOCK_CONTROL))
 		& SDHCI_CLOCK_INT_STABLE)) {
 		if (timeout == 0) {
-			printk(KERN_ERR "%s: Internal clock never stabilised. "
-				"Please report this to " BUGMAIL ".\n",
-				mmc_hostname(host->mmc));
+			printk(KERN_ERR "%s: Internal clock never "
+				"stabilised.\n", mmc_hostname(host->mmc));
 			sdhci_dumpregs(host);
 			return;
 		}
@@ -899,9 +892,8 @@ static void sdhci_timeout_timer(unsigned long data)
 	spin_lock_irqsave(&host->lock, flags);
 
 	if (host->mrq) {
-		printk(KERN_ERR "%s: Timeout waiting for hardware interrupt. "
-			"Please report this to " BUGMAIL ".\n",
-			mmc_hostname(host->mmc));
+		printk(KERN_ERR "%s: Timeout waiting for hardware "
+			"interrupt.\n", mmc_hostname(host->mmc));
 		sdhci_dumpregs(host);
 
 		if (host->data) {
@@ -934,8 +926,6 @@ static void sdhci_cmd_irq(struct sdhci_host *host, u32 intmask)
 	if (!host->cmd) {
 		printk(KERN_ERR "%s: Got command interrupt even though no "
 			"command operation was in progress.\n",
-			mmc_hostname(host->mmc));
-		printk(KERN_ERR "%s: Please report this to " BUGMAIL ".\n",
 			mmc_hostname(host->mmc));
 		sdhci_dumpregs(host);
 		return;
@@ -971,8 +961,6 @@ static void sdhci_data_irq(struct sdhci_host *host, u32 intmask)
 
 		printk(KERN_ERR "%s: Got data interrupt even though no "
 			"data operation was in progress.\n",
-			mmc_hostname(host->mmc));
-		printk(KERN_ERR "%s: Please report this to " BUGMAIL ".\n",
 			mmc_hostname(host->mmc));
 		sdhci_dumpregs(host);
 
@@ -1045,8 +1033,7 @@ static irqreturn_t sdhci_irq(int irq, void *dev_id)
 	intmask &= SDHCI_INT_BUS_POWER;
 
 	if (intmask) {
-		printk(KERN_ERR "%s: Unexpected interrupt 0x%08x. Please "
-			"report this to " BUGMAIL ".\n",
+		printk(KERN_ERR "%s: Unexpected interrupt 0x%08x.\n",
 			mmc_hostname(host->mmc), intmask);
 		sdhci_dumpregs(host);
 
