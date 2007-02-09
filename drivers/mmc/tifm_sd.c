fd25f57d2fbe809fe51406d5c78bcaf41fa62253
Pierre Ossman <drzeus@drzeus.cx>
2007-02-09 22:49:31 +0100

    mmc: Move OCR bit defines
    All host drivers were #include:ing mmc/protocol.h just to
    get access to the OCR bit defines. Move these to host.h instead.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/tifm_sd.c
+++ drivers/mmc/tifm_sd.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #include <linux/tifm.h>
-#include <linux/mmc/protocol.h>
 #include <linux/mmc/host.h>
 #include <linux/highmem.h>
 #include <linux/scatterlist.h>
--- drivers/mmc/wbsd.c
+++ drivers/mmc/wbsd.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pnp.h>
 #include <linux/highmem.h>
 #include <linux/mmc/host.h>
-#include <linux/mmc/protocol.h>
 
 #include <asm/io.h>
 #include <asm/dma.h>
