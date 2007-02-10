bb2cf862070813d900350f4b4345ca243a30ba25
Randy Dunlap <randy.dunlap@oracle.com>
2007-02-10 14:41:56 -0800

    PCI/sysfs/kobject kernel-doc fixes
    Fix kernel-doc warnings in PCI, sysfs, and kobject files.
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- lib/kobject.c
+++ lib/kobject.c
@@ -327,6 +327,7 @@ int kobject_rename(struct kobject * kobj, const char *new_name)
 /**
  *	kobject_rename - change the name of an object
  *	@kobj:	object in question.
+ *	@new_parent: object's new parent
  *	@new_name: object's new name
  */
 
