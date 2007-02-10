46da310e74228e1d55d6407cacfc58ae43ad34e3
Richard Purdie <rpurdie@rpsys.net>
2007-02-10 15:04:08 +0000

    backlight: Clean up pmac_backlight handling
    Move the setting/unsetting of pmac_backlight into the
    backlight core instead of doing it in each driver.
    
    Signed-off-by: Richard Purdie <rpurdie@rpsys.net>

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/err.h>
 #include <linux/fb.h>
 
+#ifdef CONFIG_PMAC_BACKLIGHT
+#include <asm/backlight.h>
+#endif
 
 #if defined(CONFIG_FB) || (defined(CONFIG_FB_MODULE) && \
 			   defined(CONFIG_BACKLIGHT_CLASS_DEVICE_MODULE))
@@ -263,6 +266,13 @@ struct backlight_device *backlight_device_register(const char *name,
 		}
 	}
 
+#ifdef CONFIG_PMAC_BACKLIGHT
+	mutex_lock(&pmac_backlight_mutex);
+	if (!pmac_backlight)
+		pmac_backlight = new_bd;
+	mutex_unlock(&pmac_backlight_mutex);
+#endif
+
 	return new_bd;
 }
 EXPORT_SYMBOL(backlight_device_register);
@@ -281,6 +291,13 @@ void backlight_device_unregister(struct backlight_device *bd)
 		return;
 
 	pr_debug("backlight_device_unregister: name=%s\n", bd->class_dev.class_id);
+
+#ifdef CONFIG_PMAC_BACKLIGHT
+	mutex_lock(&pmac_backlight_mutex);
+	if (pmac_backlight == bd)
+		pmac_backlight = NULL;
+	mutex_unlock(&pmac_backlight_mutex);
+#endif
 
 	for (i = 0; i < ARRAY_SIZE(bl_class_device_attributes); i++)
 		class_device_remove_file(&bd->class_dev,
