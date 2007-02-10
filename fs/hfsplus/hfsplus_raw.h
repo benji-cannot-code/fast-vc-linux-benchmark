fdb9e4d38de8d3ecaed6f2081f21204cc30d909b
Robert P. J. Day <rpjday@mindspring.com>
2007-02-10 01:46:20 -0800

    [PATCH] extend the set of "__attribute__" shortcut macros
    Extend the set of "__attribute__" shortcut macros, and remove identical
    (and now superfluous) definitions from a couple of source files.
    
    based on a page at robert love's blog:
    
    	http://rlove.org/log/2005102601
    
    extend the set of shortcut macros defined in compiler-gcc.h with the
    following:
    
    #define __packed                       __attribute__((packed))
    #define __weak                         __attribute__((weak))
    #define __naked                        __attribute__((naked))
    #define __noreturn                     __attribute__((noreturn))
    #define __pure                         __attribute__((pure))
    #define __aligned(x)                   __attribute__((aligned(x)))
    #define __printf(a,b)                  __attribute__((format(printf,a,b)))
    
    Once these are in place, it's up to subsystem maintainers to decide if they
    want to take advantage of them.  there is already a strong precedent for
    using shortcuts like this in the source tree.
    
    The ones that might give people pause are "__aligned" and "__printf", but
    shortcuts for both of those are already in use, and in some ways very
    confusingly.  note the two very different definitions for a macro named
    "ALIGNED":
    
      drivers/net/sgiseeq.c:#define ALIGNED(x) ((((unsigned long)(x)) + 0xf) & ~(0xf))
      drivers/scsi/ultrastor.c:#define ALIGNED(x) __attribute__((aligned(x)))
    
    also:
    
      include/acpi/platform/acgcc.h:
        #define ACPI_PRINTF_LIKE(c) __attribute__ ((__format__ (__printf__, c, c+1)))
    
    Given the precedent, then, it seems logical to at least standardize on a
    consistent set of these macros.
    
    Signed-off-by: Robert P. J. Day <rpjday@mindspring.com>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/hfsplus/hfsplus_raw.h
+++ fs/hfsplus/hfsplus_raw.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/types.h>
 
-#define __packed __attribute__ ((packed))
-
 /* Some constants */
 #define HFSPLUS_SECTOR_SIZE        512
 #define HFSPLUS_SECTOR_SHIFT         9
