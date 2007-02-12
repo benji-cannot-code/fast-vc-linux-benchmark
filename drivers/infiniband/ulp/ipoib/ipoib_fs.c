fee7bb7b905a4a59e66b57cf291ff430e33619be
Arjan van de Ven <arjan@linux.intel.com>
2007-02-12 00:55:32 -0800

    [PATCH] mark struct file_operations const 3
    Many struct file_operations in the kernel can be "const".  Marking them const
    moves these to the .rodata section, which avoids false sharing with potential
    dirty data.  In addition it'll catch accidental writes at compile time to
    these shared resources.
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/infiniband/ulp/ipoib/ipoib_fs.c
+++ drivers/infiniband/ulp/ipoib/ipoib_fs.c
@@ -147,7 +147,7 @@ static int ipoib_mcg_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static struct file_operations ipoib_mcg_fops = {
+static const struct file_operations ipoib_mcg_fops = {
 	.owner   = THIS_MODULE,
 	.open    = ipoib_mcg_open,
 	.read    = seq_read,
@@ -253,7 +253,7 @@ static int ipoib_path_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static struct file_operations ipoib_path_fops = {
+static const struct file_operations ipoib_path_fops = {
 	.owner   = THIS_MODULE,
 	.open    = ipoib_path_open,
 	.read    = seq_read,
