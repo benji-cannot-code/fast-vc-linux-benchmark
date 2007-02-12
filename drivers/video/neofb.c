53f7ebcf677ca5f27fa55b019564949ee96fc03b
Adrian Bunk <bunk@stusta.de>
2007-02-12 00:55:01 -0800

    [PATCH] proper prototype for tosh_smm()
    Add a proper prototype for tosh_smm() to include/linux/toshiba.h
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Cc: James Simmons <jsimmons@infradead.org>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/neofb.c
+++ drivers/video/neofb.c
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #ifdef CONFIG_TOSHIBA
 #include <linux/toshiba.h>
-extern int tosh_smm(SMMRegisters *regs);
 #endif
 
 #include <asm/io.h>
