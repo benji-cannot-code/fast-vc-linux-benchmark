767f8ca84e779f118e7385470bb4930eef124e94
Heiko Carstens <heiko.carstens@de.ibm.com>
2007-02-12 00:52:20 -0800

    [PATCH] lockdep: forward declare struct task_struct
    3117df0453828bd045c16244e6f50e5714667a8a causes this:
    
    In file included from arch/s390/kernel/early.c:13:
    include/linux/lockdep.h:300: warning:
    		"struct task_struct" declared inside parameter list
    include/linux/lockdep.h:300:
    		warning: its scope is only this definition or
    		declaration, which is probably not what you want
    
    Acked-by: Ingo Molnar <mingo@elte.hu>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: <stable@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/lockdep.h
+++ include/linux/lockdep.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __LINUX_LOCKDEP_H
 #define __LINUX_LOCKDEP_H
 
+struct task_struct;
+
 #ifdef CONFIG_LOCKDEP
 
 #include <linux/linkage.h>
