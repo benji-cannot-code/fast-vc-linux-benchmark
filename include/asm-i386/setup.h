9bd9f3ce28643b76e49bdb71f3b6b03afbdfa023
Alon Bar-Lev <alon.barlev@gmail.com>
2007-02-12 00:54:27 -0800

    [PATCH] i386: 2048-byte command line
    Current implementation allows the kernel to receive up to 255 characters from
    the bootloader.  While the boot protocol allows greater buffers to be sent.
    
    In current environment, the command-line is used in order to specify many
    values, including suspend/resume, module arguments, splash, initramfs and
    more.
    
    255 characters are not enough anymore.
    
    After edd issue was fixed, and dynammic kernel command-line patch was
    accepted, we can extend the COMMAND_LINE_SIZE without runtime memory
    requirements.
    
    Signed-off-by: Alon Bar-Lev <alon.barlev@gmail.com>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-i386/setup.h
+++ include/asm-i386/setup.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _i386_SETUP_H
 #define _i386_SETUP_H
 
-#define COMMAND_LINE_SIZE 256
+#define COMMAND_LINE_SIZE 2048
 
 #ifdef __KERNEL__
 #include <linux/pfn.h>
