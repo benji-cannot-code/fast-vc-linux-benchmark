bc9bb80bfc13328c229301f01e099f93410c4865
Ralf Baechle <ralf@linux-mips.org>
2007-02-13 01:28:09 +0000

    [MIPS] signal: Move {restore,setup}_sigcontext prototypes to their user
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/signal_n32.c
+++ arch/mips/kernel/signal_n32.c
@@ -50,6 +50,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define _BLOCKABLE (~(sigmask(SIGKILL) | sigmask(SIGSTOP)))
 
+extern int setup_sigcontext(struct pt_regs *, struct sigcontext __user *);
+extern int restore_sigcontext(struct pt_regs *, struct sigcontext __user *);
+
+
 /* IRIX compatible stack_t  */
 typedef struct sigaltstack32 {
 	s32 ss_sp;
