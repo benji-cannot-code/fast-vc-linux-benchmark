2758dcc29e34cba9f929e97c488f93fc8dddce80
Evgeniy Polyakov <johnpol@2ka.mipt.ru>
2007-02-13 13:26:25 +0100

    [PATCH] x86-64: Minor patch for compilation warning in x86_64 signal code
    If DEBUG_SIG is enbaled in source code, ia32_signal.c compiles with warning
    due to wrong format string.  Attached patch fixes that.  It is quite minor
    update, since by default DEBUG_SIG is not enabled and can not be turned on
    without code modification.
    
    Signed-off-by: Evgeniy Polyakov <johnpol@2ka.mipt.ru>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86_64/ia32/ia32_signal.c
+++ arch/x86_64/ia32/ia32_signal.c
@@ -496,7 +496,7 @@ int ia32_setup_frame(int sig, struct k_sigaction *ka,
 		ptrace_notify(SIGTRAP);
 
 #if DEBUG_SIG
-	printk("SIG deliver (%s:%d): sp=%p pc=%p ra=%p\n",
+	printk("SIG deliver (%s:%d): sp=%p pc=%lx ra=%u\n",
 		current->comm, current->pid, frame, regs->rip, frame->pretcode);
 #endif
 
@@ -602,7 +602,7 @@ int ia32_setup_rt_frame(int sig, struct k_sigaction *ka, siginfo_t *info,
 		ptrace_notify(SIGTRAP);
 
 #if DEBUG_SIG
-	printk("SIG deliver (%s:%d): sp=%p pc=%p ra=%p\n",
+	printk("SIG deliver (%s:%d): sp=%p pc=%lx ra=%u\n",
 		current->comm, current->pid, frame, regs->rip, frame->pretcode);
 #endif
 
