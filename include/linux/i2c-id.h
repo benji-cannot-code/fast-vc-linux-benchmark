d403c2f2f8dc294caf9e66c8ba3570d303689c78
Jean Delvare <khali@linux-fr.org>
2007-02-13 22:09:01 +0100

    i2c: Update the list of bus IDs
    * The Voodoo3 has no SMBus, it has two bit-banged busses which
      already have an ID assigned (I2C_HW_B_VOO).
    * The i2c-ipmi bus driver was a non-sense, it'll never be ported
      to Linux 2.6.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Acked-by: Yani Ioannou <yani.ioannou@gmail.com>

--- include/linux/i2c-id.h
+++ include/linux/i2c-id.h
@@ -233,7 +233,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define I2C_HW_SMBUS_PIIX4	0x040000
 #define I2C_HW_SMBUS_ALI15X3	0x040001
 #define I2C_HW_SMBUS_VIA2	0x040002
-#define I2C_HW_SMBUS_VOODOO3	0x040003
 #define I2C_HW_SMBUS_I801	0x040004
 #define I2C_HW_SMBUS_AMD756	0x040005
 #define I2C_HW_SMBUS_SIS5595	0x040006
@@ -252,9 +251,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* --- ISA pseudo-adapter						*/
 #define I2C_HW_ISA		0x050000
-
-/* --- IPMI pseudo-adapter						*/
-#define I2C_HW_IPMI		0x0b0000
 
 /* --- IPMB adapter						*/
 #define I2C_HW_IPMB		0x0c0000
