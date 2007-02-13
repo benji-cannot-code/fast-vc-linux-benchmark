90e4c839760369d6aa6e7b45c592a886e550caff
Dan Williams <dan.j.williams@intel.com>
2007-02-13 17:13:34 +0100

    [ARM] 4187/1: iop: unify time implementation across iop32x, iop33x, and iop13xx
    * architecture specific details are handled in asm/arch/time.h
    * ARCH_IOP13XX now selects PLAT_IOP
    * as suggested by Lennert use ifdef CONFIG_XSCALE to skip the cp_wait on
    XSC3
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-iop32x/n2100.c
+++ arch/arm/mach-iop32x/n2100.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 #include <asm/page.h>
 #include <asm/pgtable.h>
+#include <asm/arch/time.h>
 
 /*
  * N2100 timer tick configuration.
@@ -45,12 +46,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 static void __init n2100_timer_init(void)
 {
 	/* 33.000 MHz crystal.  */
-	iop3xx_init_time(198000000);
+	iop_init_time(198000000);
 }
 
 static struct sys_timer n2100_timer = {
 	.init		= n2100_timer_init,
-	.offset		= iop3xx_gettimeoffset,
+	.offset		= iop_gettimeoffset,
 };
 
 
