f3bebe2ab837b69c9b54d285623d98167e1b6030
OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
2007-02-13 13:26:20 +0100

    [PATCH] mmconfig: remove #define MMCONFIG_APER_XXX
    MMCONFIG_APER_XXX is unneeded in arch/x86_64/pci/mmconfig.c.
    
    Signed-off-by: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/pci/mmconfig.c
+++ arch/x86_64/pci/mmconfig.c
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "pci.h"
 
-/* aperture is up to 256MB but BIOS may reserve less */
-#define MMCONFIG_APER_MIN	(2 * 1024*1024)
-#define MMCONFIG_APER_MAX	(256 * 1024*1024)
-
 /* Verify the first 16 busses. We assume that systems with more busses
    get MCFG right. */
 #define PCI_MMCFG_MAX_CHECK_BUS 16
