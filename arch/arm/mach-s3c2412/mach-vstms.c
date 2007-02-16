bd30b5ee10790dd1a059ef8f34e7bb3365ebbc6e
Ben Dooks <ben-linux@fluff.org>
2007-02-16 12:54:15 +0100

    [ARM] 4209/1: S3C24XX: remove unnecessary includes of iomd.h
    Machines in the S3C24XX architectures should not
    be including <asm/hardware/iomd.h> as this is not
    needed.
    
    Also remove commented out includes
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c2412/mach-vstms.c
+++ arch/arm/mach-s3c2412/mach-vstms.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/irq.h>
 
 #include <asm/hardware.h>
-#include <asm/hardware/iomd.h>
 #include <asm/setup.h>
 #include <asm/io.h>
 #include <asm/irq.h>
