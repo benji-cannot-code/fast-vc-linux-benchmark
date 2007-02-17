c2d9292138fbdf4590df8c9b626c55b52ee3fba9
Robert P. J. Day <rpjday@mindspring.com>
2007-02-17 19:13:42 +0100

    Replace remaining references to "driverfs" with "sysfs".
    Globally, s/driverfs/sysfs/g.
    
    Signed-off-by: Robert P. J. Day <rpjday@mindspring.com>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- arch/parisc/kernel/topology.c
+++ arch/parisc/kernel/topology.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * arch/parisc/kernel/topology.c - Populate driverfs with topology information
+ * arch/parisc/kernel/topology.c - Populate sysfs with topology information
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
