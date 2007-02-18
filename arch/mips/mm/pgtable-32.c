05b9fd98e39ea9fe553392916ff0f4a7408242c5
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2007-02-19 01:27:34 +0900

    [MIPS] Add external declaration of pagetable_init() to pgalloc.h
    This fixes some sparse warnings.
    
    pgtable-32.c:15:6: warning: symbol 'pgd_init' was not declared. Should it be static?
    pgtable-32.c:32:13: warning: symbol 'pagetable_init' was not declared. Should it be static?
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mm/pgtable-32.c
+++ arch/mips/mm/pgtable-32.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/highmem.h>
 #include <asm/fixmap.h>
 #include <asm/pgtable.h>
+#include <asm/pgalloc.h>
 
 void pgd_init(unsigned long page)
 {
