735425005be1d50e10bf95a34675a4435337f6be
Christoph Lameter <clameter@sgi.com>
2007-02-20 13:57:52 -0800

    [PATCH] slab: reduce size of alien cache to cover only possible nodes
    The alien cache is a per cpu per node array allocated for every slab on the
    system.  Currently we size this array for all nodes that the kernel does
    support.  For IA64 this is 1024 nodes.  So we allocate an array with 1024
    objects even if we only boot a system with 4 nodes.
    
    This patch uses "nr_node_ids" to determine the number of possible nodes
    supported by a hardware configuration and only allocates an alien cache
    sized for possible nodes.
    
    The initialization of nr_node_ids occurred too late relative to the bootstrap
    of the slab allocator and so I moved the setup_nr_node_ids() into
    free_area_init_nodes().
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/slab.c
+++ mm/slab.c
@@ -1043,7 +1043,7 @@ static void *alternate_node_alloc(struct kmem_cache *, gfp_t);
 static struct array_cache **alloc_alien_cache(int node, int limit)
 {
 	struct array_cache **ac_ptr;
-	int memsize = sizeof(void *) * MAX_NUMNODES;
+	int memsize = sizeof(void *) * nr_node_ids;
 	int i;
 
 	if (limit > 1)
