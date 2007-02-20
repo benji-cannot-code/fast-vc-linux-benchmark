444813e0e6be52436f98d514fbe7a70f63991166
Linas Vepstas <linas@austin.ibm.com>
2007-02-20 16:42:59 -0600

    spidernet: janitorial, typos
    Janitorial patch. Undo long lines, fix typo in err msg.
    
    Signed-off-by: Linas Vepstas <linas@austin.ibm.com>
    Cc: Jens Osterkamp <Jens.Osterkamp@de.ibm.com>
    Cc: Kou Ishizaki <kou.ishizaki@toshiba.co.jp>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/spider_net.h
+++ drivers/net/spider_net.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _SPIDER_NET_H
 #define _SPIDER_NET_H
 
-#define VERSION "1.6 C"
+#define VERSION "2.0 A"
 
 #include "sungem_phy.h"
 
