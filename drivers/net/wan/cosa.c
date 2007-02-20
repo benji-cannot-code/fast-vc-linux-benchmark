cec14bbca8bb84692418391f0e2d8e66bad9bdfe
Alan <alan@lxorguk.ukuu.org.uk>
2007-02-20 18:08:57 +0000

    net: remove a collection of unneeded #undef REALLY_SLOW_IO stuff
    Signed-off-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/wan/cosa.c
+++ drivers/net/wan/cosa.c
@@ -95,7 +95,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 
 #undef COSA_SLOW_IO	/* for testing purposes only */
-#undef REALLY_SLOW_IO
 
 #include <asm/io.h>
 #include <asm/dma.h>
