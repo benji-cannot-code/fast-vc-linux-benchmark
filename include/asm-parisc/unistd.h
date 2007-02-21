59602efc9a9535e6b1c8b3291dd8e6d677b21b93
Guy Martin <gmsoft@tuxicoman.be>
2007-02-21 15:32:57 +0000

    [PARISC] Add missing statfs64 and fstatfs64 syscalls
    Signed-off-by: Guy Martin <gmsoft@tuxicoman.be>

--- include/asm-parisc/unistd.h
+++ include/asm-parisc/unistd.h
@@ -791,8 +791,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_move_pages		(__NR_Linux + 295)
 #define __NR_getcpu		(__NR_Linux + 296)
 #define __NR_epoll_pwait	(__NR_Linux + 297)
+#define __NR_statfs64		(__NR_Linux + 298)
+#define __NR_fstatfs64		(__NR_Linux + 299)
 
-#define __NR_Linux_syscalls     298
+#define __NR_Linux_syscalls     300
 
 #define HPUX_GATEWAY_ADDR       0xC0000004
 #define LINUX_GATEWAY_ADDR      0x100
