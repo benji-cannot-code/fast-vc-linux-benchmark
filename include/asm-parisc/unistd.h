816d71fa419810798dab00776e82c860354a9487
Kyle McMartin <kyle@hera.kernel.org>
2007-02-21 15:36:05 +0000

    [PARISC] Use symbolic last syscall in __NR_Linux_syscalls
    Hopefully will prevent people from forgetting to update this...
    
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- include/asm-parisc/unistd.h
+++ include/asm-parisc/unistd.h
@@ -794,7 +794,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_statfs64		(__NR_Linux + 298)
 #define __NR_fstatfs64		(__NR_Linux + 299)
 
-#define __NR_Linux_syscalls     300
+#define __NR_Linux_syscalls     (__NR_fstatfs64 + 1)
 
 #define HPUX_GATEWAY_ADDR       0xC0000004
 #define LINUX_GATEWAY_ADDR      0x100
