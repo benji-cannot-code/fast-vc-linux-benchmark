caa62e6861e880b86755138e9d49adc6240e001d
Mike Galbraith <efault@gmx.de>
2007-02-21 12:45:35 -0800

    driver core: refcounting fix
    Fix a reference counting bug exposed by commit
    725522b5453dd680412f2b6463a988e4fd148757.  If driver.mod_name exists, we
    take a reference in module_add_driver(), and never release it.  Undo that
    reference in module_remove_driver().
    
    Signed-off-by: Mike Galbraith <efault@gmx.de>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- kernel/module.c
+++ kernel/module.c
@@ -2426,6 +2426,12 @@ void module_remove_driver(struct device_driver *drv)
 			kfree(driver_name);
 		}
 	}
+	/*
+	 * Undo the additional reference we added in module_add_driver()
+	 * via kset_find_obj()
+	 */
+	if (drv->mod_name)
+		kobject_put(&drv->kobj);
 }
 EXPORT_SYMBOL(module_remove_driver);
 #endif
