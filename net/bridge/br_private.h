99d31b991ea420a1fe0343775baa5d43085c4686
Stephen Hemminger <shemminger@linux-foundation.org>
2007-02-22 01:07:53 -0800

    [BRIDGE]: get rid of miscdevice include
    The bridge hasn't used miscdevice for a long long time.
    
    Signed-off-by: Stephen Hemminger <shemminger@linux-foundation.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/br_private.h
+++ net/bridge/br_private.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _BR_PRIVATE_H
 
 #include <linux/netdevice.h>
-#include <linux/miscdevice.h>
 #include <linux/if_bridge.h>
 
 #define BR_HASH_BITS 8
