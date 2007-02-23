6c669a00e1538053df4af7c33046ed06e6e57d0b
Eric W. Biederman <ebiederm@xmission.com>
2007-02-23 04:20:59 -0700

    [PATCH] x86_64 irq: Simplfiy the set_affinity logic.
    For some reason the code has been picking TARGET_CPUS when asked to
    set the affinity to an empty set of cpus.  That is just silly it's
    extra work.  Instead if there are no cpus to set the affinity to we
    should just give up immediately.  That is simpler and a little more
    intuitive.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/x86_64/kernel/io_apic.c
+++ arch/x86_64/kernel/io_apic.c
@@ -245,9 +245,7 @@ static void set_ioapic_affinity_irq(unsigned int irq, cpumask_t mask)
 
 	cpus_and(tmp, mask, cpu_online_map);
 	if (cpus_empty(tmp))
-		tmp = TARGET_CPUS;
-
-	cpus_and(mask, tmp, CPU_MASK_ALL);
+		return;
 
 	vector = assign_irq_vector(irq, mask, &tmp);
 	if (vector < 0)
@@ -1891,9 +1889,7 @@ static void set_msi_irq_affinity(unsigned int irq, cpumask_t mask)
 
 	cpus_and(tmp, mask, cpu_online_map);
 	if (cpus_empty(tmp))
-		tmp = TARGET_CPUS;
-
-	cpus_and(mask, tmp, CPU_MASK_ALL);
+		return;
 
 	vector = assign_irq_vector(irq, mask, &tmp);
 	if (vector < 0)
@@ -1986,9 +1982,7 @@ static void set_ht_irq_affinity(unsigned int irq, cpumask_t mask)
 
 	cpus_and(tmp, mask, cpu_online_map);
 	if (cpus_empty(tmp))
-		tmp = TARGET_CPUS;
-
-	cpus_and(mask, tmp, CPU_MASK_ALL);
+		return;
 
 	vector = assign_irq_vector(irq, mask, &tmp);
 	if (vector < 0)
