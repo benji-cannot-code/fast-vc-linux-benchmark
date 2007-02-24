49f8e182148294603bc5c2af0204020f23f5e3be
Sivakumar Subramani <Sivakumar.Subramani@neterion.com>
2007-02-24 01:57:32 -0500

    S2IO: Optimized the delay to wait for command completion
    - Optimized delay to wait for command completion so as to reduce the
      initialization wait time.
    - Disable differentiated services steering. By default RMAC is configured to
      steer traffic with certain DS codes to other queues. Driver must initialize
      the DS memory to 0 to make sure that DS steering will not be used by default.
    
    Signed-off-by: Sivakumar Subramani <sivakumar.subramani@neterion.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/s2io.h
+++ drivers/net/s2io.h
@@ -33,7 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FAILURE -1
 #define S2IO_MINUS_ONE 0xFFFFFFFFFFFFFFFFULL
 #define S2IO_MAX_PCI_CONFIG_SPACE_REINIT 100
-
+#define S2IO_BIT_RESET 1
+#define S2IO_BIT_SET 2
 #define CHECKBIT(value, nbit) (value & (1 << nbit))
 
 /* Maximum time to flicker LED when asked to identify NIC using ethtool */
@@ -1006,7 +1007,8 @@ static int s2io_set_swapper(struct s2io_nic * sp);
 static void s2io_card_down(struct s2io_nic *nic);
 static int s2io_card_up(struct s2io_nic *nic);
 static int get_xena_rev_id(struct pci_dev *pdev);
-static int wait_for_cmd_complete(void __iomem *addr, u64 busy_bit);
+static int wait_for_cmd_complete(void __iomem *addr, u64 busy_bit,
+					int bit_state);
 static int s2io_add_isr(struct s2io_nic * sp);
 static void s2io_rem_isr(struct s2io_nic * sp);
 
@@ -1020,6 +1022,7 @@ static void queue_rx_frame(struct sk_buff *skb);
 static void update_L3L4_header(struct s2io_nic *sp, struct lro *lro);
 static void lro_append_pkt(struct s2io_nic *sp, struct lro *lro,
 			   struct sk_buff *skb, u32 tcp_len);
+static int rts_ds_steer(struct s2io_nic *nic, u8 ds_codepoint, u8 ring);
 
 #define s2io_tcp_mss(skb) skb_shinfo(skb)->gso_size
 #define s2io_udp_mss(skb) skb_shinfo(skb)->gso_size
