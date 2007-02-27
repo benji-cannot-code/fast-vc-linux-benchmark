9df12a5d06fcec93407b25674011840083298ac9
Michael Olberg <michael.olberg@chalmers.se>
2007-02-27 12:57:12 +0100

    USB: add QL355P power supply ids to fdti_sio

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -340,6 +340,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FTDI_SUUNTO_SPORTS_PID	0xF680	/* Suunto Sports instrument */
 
 /*
+ * TTi (Thurlby Thandar Instruments)
+ */
+#define TTI_VID			0x103E	/* Vendor Id */
+#define TTI_QL355P_PID		0x03E8	/* TTi QL355P power supply */
+
+/*
  * Definitions for B&B Electronics products.
  */
 #define BANDB_VID		0x0856	/* B&B Electronics Vendor ID */
