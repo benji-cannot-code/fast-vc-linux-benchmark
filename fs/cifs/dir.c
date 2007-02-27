8d0ed67f0dead783e8feb85b063ca5c03a8d7366
Steve French <sfrench@us.ibm.com>
2007-02-27 05:35:17 +0000

    [CIFS] Remove some unused functions/declarations
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/dir.c
+++ fs/cifs/dir.c
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "cifs_debug.h"
 #include "cifs_fs_sb.h"
 
-void
+static void
 renew_parental_timestamps(struct dentry *direntry)
 {
 	/* BB check if there is a way to get the kernel to do this or if we really need this */
