66d3caa74244f6652c63bd30bd907f20f67d92ab
Paul Moore <paul.moore@hp.com>
2007-02-28 15:14:23 -0500

    SELinux: rename selinux_netlabel.h to netlabel.h
    In the beginning I named the file selinux_netlabel.h to avoid potential
    namespace colisions.  However, over time I have realized that there are several
    other similar cases of multiple header files with the same name so I'm changing
    the name to something which better fits with existing naming conventions.
    
    Signed-off-by: Paul Moore <paul.moore@hp.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "objsec.h"
 #include "netif.h"
 #include "xfrm.h"
-#include "selinux_netlabel.h"
+#include "netlabel.h"
 
 #define XATTR_SELINUX_SUFFIX "selinux"
 #define XATTR_NAME_SELINUX XATTR_SECURITY_PREFIX XATTR_SELINUX_SUFFIX
