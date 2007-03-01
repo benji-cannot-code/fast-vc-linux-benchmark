72c57c44faabcc0df5ed0c39c28d0ef2adedc2c9
David Howells <dhowells@redhat.com>
2007-02-28 20:13:26 -0800

    [PATCH] FRV: Missing error defs
    linux/irq.h uses EINVAL but does not #include linux/errno.h.  This results in
    the compiler spitting out errors on some files.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spinlock.h>
 #include <linux/cpumask.h>
 #include <linux/irqreturn.h>
+#include <linux/errno.h>
 
 #include <asm/irq.h>
 #include <asm/ptrace.h>
