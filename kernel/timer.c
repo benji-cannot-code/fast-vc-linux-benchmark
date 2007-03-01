bf0b40b29bb5fd6fb3ff0586b4fd8c7a63fb2e33
Daniel Walker <dwalker@mvista.com>
2007-02-28 20:12:07 -0800

    [PATCH] update timekeeping_is_continuous comment
    Signed-off-by: Daniel Walker <dwalker@mvista.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/timer.c
+++ kernel/timer.c
@@ -909,7 +909,7 @@ static inline void change_clocksource(void) { }
 #endif
 
 /**
- * timeofday_is_continuous - check to see if timekeeping is free running
+ * timekeeping_is_continuous - check to see if timekeeping is free running
  */
 int timekeeping_is_continuous(void)
 {
