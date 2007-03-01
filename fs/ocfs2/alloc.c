009421e7415ad4c2248f3ddaeabc5c5d8552e86e
Mark Fasheh <mark.fasheh@oracle.com>
2007-03-01 11:22:19 -0800

    ocfs2: Use do_sync_mapping_range() in ocfs2_zero_tail_for_truncate()
    Do this instead of filemap_fdatawrite() - this way we sync only the
    range between i_size and the cluster boundary.
    
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/alloc.c
+++ fs/ocfs2/alloc.c
@@ -3518,6 +3518,7 @@ int ocfs2_zero_tail_for_truncate(struct inode *inode, handle_t *handle,
 				 u64 new_i_size)
 {
 	int ret, numpages;
+	loff_t endbyte;
 	struct page **pages = NULL;
 	u64 phys;
 
@@ -3556,7 +3557,9 @@ int ocfs2_zero_tail_for_truncate(struct inode *inode, handle_t *handle,
 	 * wait on them - the truncate_inode_pages() call later will
 	 * do that for us.
 	 */
-	ret = filemap_fdatawrite(inode->i_mapping);
+	endbyte = ocfs2_align_bytes_to_clusters(inode->i_sb, new_i_size);
+	ret = do_sync_mapping_range(inode->i_mapping, new_i_size,
+				    endbyte - 1, SYNC_FILE_RANGE_WRITE);
 	if (ret)
 		mlog_errno(ret);
 
