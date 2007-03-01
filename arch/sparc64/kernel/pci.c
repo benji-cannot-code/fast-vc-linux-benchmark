47185fa9f1ae71746b5f859d6727b534fb3e2941
David S. Miller <davem@sunset.davemloft.net>
2007-02-28 23:38:38 -0800

    [SPARC64]: Internalize pci_memspace_mask.
    The only user was bus_dvma_to_mem() which is no longer used
    by any driver, so kill that, and the export of pci_memspace_mask.
    
    The only user now is the PCI mmap support code.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/pci.c
+++ arch/sparc64/kernel/pci.c
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/isa.h>
 #include <asm/prom.h>
 
+#include "pci_impl.h"
+
 unsigned long pci_memspace_mask = 0xffffffffUL;
 
 #ifndef CONFIG_PCI
--- arch/sparc64/kernel/pci_impl.h
+++ arch/sparc64/kernel/pci_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 
 extern struct pci_controller_info *pci_controller_root;
+extern unsigned long pci_memspace_mask;
 
 extern int pci_num_controllers;
 
