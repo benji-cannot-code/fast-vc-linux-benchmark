fa1f2a6f42b5cd3bd8262a2498fab3cfd8157e0d
Tilman Schmidt <tilman@imap.cc>
2007-02-28 20:13:01 -0800

    [PATCH] drivers/isdn/gigaset: build asyncdata.o into the gigaset module (fix)
    a) Remove #define acrobatics that have become unnecessary by the move of
       asyncdata.o into the common part.
    
    b) Correct the rule for building the common part into the kernel when
       some or all hardware specific parts are built as modules.
    
    Signed-off-by: Tilman Schmidt <tilman@imap.cc>
    Cc: Adrian Bunk <bunk@stusta.de>
    Cc: Karsten Keil <kkeil@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/isdn/gigaset/asyncdata.c
+++ drivers/isdn/gigaset/asyncdata.c
@@ -14,11 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * =====================================================================
  */
 
-/* not set by Kbuild when building both ser_gigaset and usb_gigaset */
-#ifndef KBUILD_MODNAME
-#define KBUILD_MODNAME "asy_gigaset"
-#endif
-
 #include "gigaset.h"
 #include <linux/crc-ccitt.h>
 #include <linux/bitrev.h>
