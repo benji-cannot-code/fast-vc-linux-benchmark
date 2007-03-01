dd672d29a2f110dfffa14403aad2919014df9d5a
Oliver Neukum <oneukum@suse.de>
2007-03-01 22:31:24 +0100

    USB: kill dead code from hub.c
    this kills residual obsoletet code from hub.c
    
    Signed-off-by: Oliver Neukum <oneukum@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1282,12 +1282,6 @@ int usb_new_device(struct usb_device *udev)
 {
 	int err;
 
-	/* Lock ourself into memory in order to keep a probe sequence
-	 * sleeping in a new thread from allowing us to be unloaded.
-	 */
-	if (!try_module_get(THIS_MODULE))
-		return -EINVAL;
-
 	/* Determine quirks */
 	usb_detect_quirks(udev);
 
@@ -1391,7 +1385,6 @@ int usb_new_device(struct usb_device *udev)
 		usb_autoresume_device(udev->parent);
 
 exit:
-	module_put(THIS_MODULE);
 	return err;
 
 fail:
