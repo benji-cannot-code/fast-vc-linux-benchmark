1915728f7add618c334710480e2b10333b33a603
Florian Fainelli <florian.fainelli@int-evry.fr>
2007-03-02 22:07:48 +0100

    [MIPS] MTX1: add idsel cardbus ressources
    Adds cardbus ressources for MTX1 boards which have a PCMCIA controller.
    
    Signed-off-by: Florian Fainelli <florian.fainelli@int-evry.fr>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/au1000/mtx-1/irqmap.c
+++ arch/mips/au1000/mtx-1/irqmap.c
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-au1x00/au1000.h>
 
 char irq_tab_alchemy[][5] __initdata = {
- [0] = { -1, INTA, INTB, INTX, INTX},   /* IDSEL 00 - AdapterA-Slot0 (top)    */
+ [0] = { -1, INTA, INTA, INTX, INTX},   /* IDSEL 00 - AdapterA-Slot0 (top)    */
  [1] = { -1, INTB, INTA, INTX, INTX},   /* IDSEL 01 - AdapterA-Slot1 (bottom) */
  [2] = { -1, INTC, INTD, INTX, INTX},   /* IDSEL 02 - AdapterB-Slot0 (top)    */
  [3] = { -1, INTD, INTC, INTX, INTX},   /* IDSEL 03 - AdapterB-Slot1 (bottom) */
