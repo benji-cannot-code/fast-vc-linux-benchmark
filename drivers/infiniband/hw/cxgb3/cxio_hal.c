33af0edd8c5315bff0f924ad0a417355c03088e4
David Miller <davem@davemloft.net>
2007-03-05 15:21:29 -0800

    RDMA/cxgb3: Fix build on sparc64
    cxgb3 uses dma_alloc_coherent() et al. thus needs linux/dma-mapping.h
    include in order to build reliably.
    
    Noticed on sparc64.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Acked-by: Steve Wise <swise@opengridcomputing.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/cxgb3/cxio_hal.c
+++ drivers/infiniband/hw/cxgb3/cxio_hal.c
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/spinlock.h>
 #include <linux/pci.h>
+#include <linux/dma-mapping.h>
 
 #include "cxio_resource.h"
 #include "cxio_hal.h"
