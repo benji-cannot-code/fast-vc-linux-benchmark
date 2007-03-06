1cbc0bb5ef45375d16e608a8354bfcd8e41c6a84
akpm@linux-foundation.org <akpm@linux-foundation.org>
2007-03-05 23:10:39 -0800

    [GFS2] build fix
    fs/gfs2/glock.c:2198: error: 'THIS_MODULE' undeclared here (not in a function)
    
    Cc: Steven Whitehouse <swhiteho@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/list.h>
 #include <linux/lm_interface.h>
 #include <linux/wait.h>
+#include <linux/module.h>
 #include <linux/rwsem.h>
 #include <asm/uaccess.h>
 
