099f600f4ed76d0417d78a35eb76b25d1fc6286f
Jeff Dike <jdike@addtoit.com>
2007-03-06 01:42:17 -0800

    [PATCH] linux/audit.h needs linux/types.h
    Include linux/types.h here because we need a definition of __u32.  This file
    appears not be exported verbatim by libc, so I think this doesn't have any
    userspace consequences.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Cc: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/audit.h
+++ include/linux/audit.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_AUDIT_H_
 #define _LINUX_AUDIT_H_
 
+#include <linux/types.h>
 #include <linux/elf-em.h>
 
 /* The netlink messages for the audit system is divided into blocks:
