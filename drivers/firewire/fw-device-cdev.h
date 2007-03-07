ba7320adcb3deccff3af3e6f38d2dc658e67a3b8
Kristian Høgsberg <krh@redhat.com>
2007-03-07 12:12:54 -0500

    firewire: Export juju specific rcodes to user space.
    Signed-off-by: Kristian Høgsberg <krh@redhat.com>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-device-cdev.h
+++ drivers/firewire/fw-device-cdev.h
@@ -52,6 +52,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RCODE_TYPE_ERROR		0x6
 #define RCODE_ADDRESS_ERROR		0x7
 
+#define RCODE_SEND_ERROR		0x10
+#define RCODE_CANCELLED			0x11
+#define RCODE_BUSY			0x12
+#define RCODE_GENERATION		0x13
+#define RCODE_NO_ACK			0x14
+
 #define SCODE_100			0x0
 #define SCODE_200			0x1
 #define SCODE_400			0x2
