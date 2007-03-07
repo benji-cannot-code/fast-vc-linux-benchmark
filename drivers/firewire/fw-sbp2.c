5249dde4b9cb1151be56b1d55d046e28264d3e6f
Kristian Høgsberg <krh@redhat.com>
2007-03-07 12:12:40 -0500

    firewire: Only use INIT_DELAYED_WORK for first initialization.
    Use PREPARE_DELAYED_WORK to just change the function pointer.
    
    Signed-off-by: Kristian Høgsberg <krh@redhat.com>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-sbp2.c
+++ drivers/firewire/fw-sbp2.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*						-*- c-basic-offset: 8 -*-
- * fw-sbp2.c -- SBP2 driver (SCSI over IEEE1394)
+ * fw-spb2.c -- SBP2 driver (SCSI over IEEE1394)
  *
  * Copyright (C) 2005-2007  Kristian Hoegsberg <krh@bitplanet.net>
  *
@@ -578,7 +578,7 @@ static void sbp2_login(struct work_struct *work)
 	sbp2_set_busy_timeout(scsi_id);
 #endif
 
-	INIT_DELAYED_WORK(&sd->work, sbp2_reconnect);
+	PREPARE_DELAYED_WORK(&sd->work, sbp2_reconnect);
 	sbp2_agent_reset(unit);
 
 	retval = add_scsi_devices(unit);
@@ -588,7 +588,7 @@ static void sbp2_login(struct work_struct *work)
 					 NULL);
 		/* Set this back to sbp2_login so we fall back and
 		 * retry login on bus reset. */
-		INIT_DELAYED_WORK(&sd->work, sbp2_login);
+		PREPARE_DELAYED_WORK(&sd->work, sbp2_login);
 	}
 }
 
@@ -715,7 +715,7 @@ static void sbp2_reconnect(struct work_struct *work)
 				 unit->device.bus_id);
 			/* Fall back and try to log in again. */
 			sd->retries = 0;
-			INIT_DELAYED_WORK(&sd->work, sbp2_login);
+			PREPARE_DELAYED_WORK(&sd->work, sbp2_login);
 		}
 		schedule_delayed_work(&sd->work, DIV_ROUND_UP(HZ, 5));
 		return;
