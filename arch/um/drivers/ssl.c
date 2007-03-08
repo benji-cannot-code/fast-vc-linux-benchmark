2296717c31cf7767d6b312bfbacc86b8b1123775
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2007-03-07 20:41:11 -0800

    [PATCH] um: mark both consoles as CON_ANYTIME
    Since both UML consoles do not use percpu variables, they may be called when
    the cpu is still offline, and they may be marked CON_ANYTIME (this is
    documented in kernel/printk.c, grep for CON_ANYTIME to find mentions of this).
    
    Works well in testing done with lock debug enabled, should be safe but is not
    needed for next release.
    
    This would probably help also stderr_console.c, but this is yet to test.
    
    Signed-off-by: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Acked-by: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/drivers/ssl.c
+++ arch/um/drivers/ssl.c
@@ -180,7 +180,7 @@ static struct console ssl_cons = {
 	.write		= ssl_console_write,
 	.device		= ssl_console_device,
 	.setup		= ssl_console_setup,
-	.flags		= CON_PRINTBUFFER,
+	.flags		= CON_PRINTBUFFER|CON_ANYTIME,
 	.index		= -1,
 };
 
