7831d008325c7a1cd50accefacc6639e34972ba7
Zang Roy-r61911 <tie-fei.zang@freescale.com>
2007-03-09 13:27:28 +0800

    [POWERPC] 86xx/85xx: Unify Freescale PCI Express memory map registers structure
    Unify PCI Express memory map registers structure define
    to arch/pwoerpc/sysdev/fsl_pcie.h for Freescale 85xx/86xx
    processor family.
    
    Signed-off-by: Roy Zang <tie-fei.zang@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/86xx/pci.c
+++ arch/powerpc/platforms/86xx/pci.c
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atomic.h>
 #include <asm/io.h>
 #include <asm/prom.h>
-#include <asm/immap_86xx.h>
 #include <asm/pci-bridge.h>
 #include <sysdev/fsl_soc.h>
+#include <sysdev/fsl_pcie.h>
 
 #include "mpc86xx.h"
 
