97e26996143f10171902a92a98a9f4916604aec3
Stephen Rothwell <sfr@canb.auug.org.au>
2007-03-09 11:54:57 +1100

    [POWERPC] Allocate syscall number for sys_getcpu
    I forgot to do this when wiring up the syscall.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/unistd.h
+++ include/asm-powerpc/unistd.h
@@ -325,10 +325,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NR_get_robust_list	299
 #define __NR_set_robust_list	300
 #define __NR_move_pages		301
+#define __NR_getcpu		302
 
 #ifdef __KERNEL__
 
-#define __NR_syscalls		302
+#define __NR_syscalls		303
 
 #define __NR__exit __NR_exit
 #define NR_syscalls	__NR_syscalls
