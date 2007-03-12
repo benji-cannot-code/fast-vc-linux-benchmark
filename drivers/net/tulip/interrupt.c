56dc3dced4e0e0ade2a573a8591f094f464a4dc3
Valerie Henson <val_henson@linux.intel.com>
2007-03-12 02:31:33 -0700

    Fix tulip SytemError typo
    Fix an annoying typo - SytemError -> SystemError
    
    Signed-off-by: Valerie Henson <val_henson@linux.intel.com>
    Cc: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/tulip/interrupt.c
+++ drivers/net/tulip/interrupt.c
@@ -674,7 +674,7 @@ irqreturn_t tulip_interrupt(int irq, void *dev_instance)
 				if (tp->link_change)
 					(tp->link_change)(dev, csr5);
 			}
-			if (csr5 & SytemError) {
+			if (csr5 & SystemError) {
 				int error = (csr5 >> 23) & 7;
 				/* oops, we hit a PCI error.  The code produced corresponds
 				 * to the reason:
@@ -744,7 +744,7 @@ irqreturn_t tulip_interrupt(int irq, void *dev_instance)
 			  TxFIFOUnderflow |
 			  TxJabber |
 			  TPLnkFail |
-			  SytemError )) != 0);
+			  SystemError )) != 0);
 #else
 	} while ((csr5 & (NormalIntr|AbnormalIntr)) != 0);
 
