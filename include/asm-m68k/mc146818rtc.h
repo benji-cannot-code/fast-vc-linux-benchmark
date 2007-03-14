3faec5821897e3fed258bdc433e5f5bb9b3d371a
Al Viro <viro@ftp.linux.org.uk>
2007-03-14 09:16:54 +0000

    [PATCH] rtc-cmos needs RTC_ALWAYS_BCD known
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-m68k/mc146818rtc.h
+++ include/asm-m68k/mc146818rtc.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atarihw.h>
 
 #define RTC_PORT(x)	(TT_RTC_BAS + 2*(x))
+#define RTC_ALWAYS_BCD	0
 
 #define CMOS_READ(addr) ({ \
 atari_outb_p((addr),RTC_PORT(0)); \
