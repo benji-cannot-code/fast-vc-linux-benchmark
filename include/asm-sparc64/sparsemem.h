e4af63276d10ee1de9a12c205fdef28088c5c446
David S. Miller <davem@sunset.davemloft.net>
2007-03-15 00:21:45 -0700

    [SPARC64]: MAX_PHYSADDR_BITS et al. really need to be 42 bits not 41.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/sparsemem.h
+++ include/asm-sparc64/sparsemem.h
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 
 #define SECTION_SIZE_BITS       31
-#define MAX_PHYSADDR_BITS       41
-#define MAX_PHYSMEM_BITS        41
+#define MAX_PHYSADDR_BITS       42
+#define MAX_PHYSMEM_BITS        42
 
 #endif /* !(__KERNEL__) */
 
