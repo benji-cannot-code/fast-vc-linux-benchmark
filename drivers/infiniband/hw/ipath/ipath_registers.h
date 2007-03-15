5a68a8fd9a27d8e297a833c3248e55bd6eaf7ffc
Bryan O'Sullivan <bos@pathscale.com>
2007-03-15 14:44:55 -0700

    IB/ipath: Change packet problems vs chip errors handling and reporting
    Some types of packet errors are moderately common with longer IB
    cables and large clusters, and are not reported with prints by other
    IB HCA drivers.  This suppresses those messages unless the new
    __IPATH_ERRPKTDBG bit is set in ipath_debug.  Reporting of temporarily
    disabled frequent error interrupts was also made clearer
    
    We also distinguish between chip errors, and bad packets sent or
    received in the wording of the messages.
    
    Signed-off-by: Dave Olson <dave.olson@qlogic.com>
    Signed-off-by: Bryan O'Sullivan <bryan.osullivan@qlogic.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/ipath/ipath_registers.h
+++ drivers/infiniband/hw/ipath/ipath_registers.h
@@ -127,6 +127,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define INFINIPATH_E_RESET           0x0004000000000000ULL
 #define INFINIPATH_E_HARDWARE        0x0008000000000000ULL
 
+/*
+ * this is used to print "common" packet errors only when the
+ * __IPATH_ERRPKTDBG bit is set in ipath_debug.
+ */
+#define INFINIPATH_E_PKTERRS ( INFINIPATH_E_SPKTLEN \
+		| INFINIPATH_E_SDROPPEDDATAPKT | INFINIPATH_E_RVCRC \
+		| INFINIPATH_E_RICRC | INFINIPATH_E_RSHORTPKTLEN \
+		| INFINIPATH_E_REBP )
+
 /* kr_hwerrclear, kr_hwerrmask, kr_hwerrstatus, bits */
 /* TXEMEMPARITYERR bit 0: PIObuf, 1: PIOpbc, 2: launchfifo
  * RXEMEMPARITYERR bit 0: rcvbuf, 1: lookupq, 2:  expTID, 3: eagerTID
