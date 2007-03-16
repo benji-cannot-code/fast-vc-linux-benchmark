c924a24627e4332d7c491dab283acd96aa0e3284
Andrew Johnson <ajohnson@intrinsyc.com>
2007-03-16 13:38:24 -0800

    [PATCH] swsusp: fix suspend when console is in VT_AUTO+KD_GRAPHICS mode
    When the console is in VT_AUTO+KD_GRAPHICS mode, switching to the
    SUSPEND_CONSOLE fails, resulting in vt_waitactive() waiting indefinitely or
    until the task is interrupted.  This patch tests if a console switch can
    occur in set_console() and returns early if a console switch is not
    possible.
    
    [akpm@linux-foundation.org: cleanup]
    Signed-off-by: Andrew Johnson <ajohnson@intrinsyc.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Cc: "Rafael J. Wysocki" <rjw@sisk.pl>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/vt_ioctl.c
+++ drivers/char/vt_ioctl.c
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kbd_diacr.h>
 #include <linux/selection.h>
 
-static char vt_dont_switch;
+char vt_dont_switch;
 extern struct tty_driver *console_driver;
 
 #define VT_IS_IN_USE(i)	(console_driver->ttys[i] && console_driver->ttys[i]->count)
