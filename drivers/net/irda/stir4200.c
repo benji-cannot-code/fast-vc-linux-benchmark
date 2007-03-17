07137f2d88a4c2094b0dce5e4096a1d95e6ca4e2
Samuel Ortiz <samuel@sortiz.org>
2007-03-16 20:35:25 -0700

    [IrDA]: removing stir4200 useless include
    stir4200 doesn't need to include irlap.h
    
    Signed-off-by: Samuel Ortiz <samuel@sortiz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/irda/stir4200.c
+++ drivers/net/irda/stir4200.c
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kthread.h>
 #include <linux/freezer.h>
 #include <net/irda/irda.h>
-#include <net/irda/irlap.h>
 #include <net/irda/irda_device.h>
 #include <net/irda/wrapper.h>
 #include <net/irda/crc.h>
