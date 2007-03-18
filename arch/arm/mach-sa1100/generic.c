41ca857411e721794f55ec40e1a86b3d4b9cb7cf
David Brownell <david-b@pacbell.net>
2007-03-18 01:26:13 -0800

    [PATCH] gpio_direction_output-needs-an-initial-value fix
    Build fix:  sa1100/generic.c should already have included <asm/gpio.h>,
    but it didn't ... causing a build problem with a recent patch.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/arm/mach-sa1100/generic.c
+++ arch/arm/mach-sa1100/generic.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/map.h>
 #include <asm/mach/flash.h>
 #include <asm/irq.h>
+#include <asm/gpio.h>
 
 #include "generic.h"
 
