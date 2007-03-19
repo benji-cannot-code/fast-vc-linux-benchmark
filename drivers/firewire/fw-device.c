c8bbac3636b476bcf9d2a4f77582158e47951fcd
Stefan Richter <stefanr@s5r6.in-berlin.de>
2007-03-19 11:37:16 -0400

    firewire: add includes for sem and rw_sem
    as a precaution.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-device.c
+++ drivers/firewire/fw-device.c
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/delay.h>
 #include <linux/idr.h>
+#include <linux/rwsem.h>
+#include <asm/semaphore.h>
 #include "fw-transaction.h"
 #include "fw-topology.h"
 #include "fw-device.h"
