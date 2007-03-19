1b43a1de23486df8299fa8280c46648cef43decc
Steven Toth <stoth@hauppauge.com>
2007-03-19 18:01:07 -0300

    V4L/DVB (6155): Cleanup/remove code to access the sram memory maps
    The cx23885 and cx23887 family use two different memory maps which govern
    how the internal SRAM is configured. This patch streamlines the access to those
    structures.
    
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx23885/cx23885.h
+++ drivers/media/video/cx23885/cx23885.h
@@ -45,8 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define CX23885_MAXBOARDS 8
 
-#define SRAM 0
-
 /* Max number of inputs by card */
 #define MAX_CX23885_INPUT 8
 
