829766bd86eb651a6ff5699dd303f54ca4438520
Michael Krufky <mkrufky@linuxtv.org>
2007-03-20 21:32:12 -0300

    V4L/DVB (6161): cx23885-dvb.c doesnt need to include dvb-pll.h
    The dvb-pll module is not being used by this driver.
    Remove the unneeded #include.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx23885/cx23885-dvb.c
+++ drivers/media/video/cx23885/cx23885-dvb.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/suspend.h>
 
 #include "cx23885.h"
-#include "dvb-pll.h"
 #include <media/v4l2-common.h>
 
 #include "s5h1409.h"
