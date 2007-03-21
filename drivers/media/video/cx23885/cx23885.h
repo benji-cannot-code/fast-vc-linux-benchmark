01bd9acf57d58248c6d1408a5f14ef2402d003bc
Michael Krufky <mkrufky@linuxtv.org>
2007-03-20 23:03:52 -0300

    V4L/DVB (6163): cx23885: remove old comments
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx23885/cx23885.h
+++ drivers/media/video/cx23885/cx23885.h
@@ -48,8 +48,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Max number of inputs by card */
 #define MAX_CX23885_INPUT 8
 
-//#define SHADOW_MAX 3
-
 #define BUFFER_TIMEOUT     (HZ)  /* 0.5 seconds */
 
 #define CX23885_BOARD_NOAUTO               UNSET
@@ -181,7 +179,6 @@ struct cx23885_dev {
 	int                        pci_bus, pci_slot;
 	u32                        __iomem *lmmio;
 	u8                         __iomem *bmmio;
-	//u32                        shadow[SHADOW_MAX];
 	int                        pci_irqmask;
 
 	/* I2C adapters: Master 1 & 2 (External) & Master 3 (Internal only) */
