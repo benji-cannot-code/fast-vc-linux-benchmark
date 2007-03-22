80f0b633658e5d9250027969915b56e6227a59d7
Olaf Hering <olaf@aepfle.de>
2007-03-22 23:14:07 +0100

    [POWERPC] Fix link errors when EEH is disabled
    Fix link errors with CONFIG_EEH=n:
    
    arch/powerpc/platforms/built-in.o: In function `.pcibios_fixup_new_pci_devices':
    (.text+0x41c8): undefined reference to `.eeh_add_device_tree_late'
    arch/powerpc/platforms/built-in.o: In function `.init_phb_dynamic':
    (.text+0x4280): undefined reference to `.eeh_add_device_tree_early'
    arch/powerpc/platforms/built-in.o: In function `.pcibios_remove_pci_devices':
    (.text+0x42fc): undefined reference to `.eeh_remove_bus_device'
    arch/powerpc/platforms/built-in.o: In function `.pcibios_add_pci_devices':
    (.text+0x43c0): undefined reference to `.eeh_add_device_tree_early'
    arch/powerpc/platforms/built-in.o: In function `.pSeries_final_fixup':
    (.init.text+0xb4): undefined reference to `.pci_addr_cache_build'
    make[1]: *** [.tmp_vmlinux1] Error 1
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/pseries/pci_dlpar.c
+++ arch/powerpc/platforms/pseries/pci_dlpar.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pci-bridge.h>
 #include <asm/ppc-pci.h>
 #include <asm/firmware.h>
+#include <asm/eeh.h>
 
 static struct pci_bus *
 find_bus_among_children(struct pci_bus *bus,
