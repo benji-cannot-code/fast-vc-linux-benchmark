1721a3dac3034016ac828624e5d95f7697a61bfa
Thomas Graf <tgraf@suug.ch>
2007-03-22 23:29:10 -0700

    [NETLINK]: Ignore control messages directly in netlink_run_queue()
    Changes netlink_rcv_skb() to skip netlink controll messages and don't
    pass them on to the message handler.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netlink/genetlink.c
+++ net/netlink/genetlink.c
@@ -305,9 +305,6 @@ static int genl_rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh,
 	struct genlmsghdr *hdr = nlmsg_data(nlh);
 	int hdrlen, err = -EINVAL;
 
-	if (nlh->nlmsg_type < NLMSG_MIN_TYPE)
-		goto ignore;
-
 	family = genl_family_find_byid(nlh->nlmsg_type);
 	if (family == NULL) {
 		err = -ENOENT;
@@ -364,9 +361,6 @@ static int genl_rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh,
 
 	*errp = err = ops->doit(skb, &info);
 	return err;
-
-ignore:
-	return 0;
 
 errout:
 	*errp = err;
