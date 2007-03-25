5b779f47a50b927974a304440116b5ba08224ed5
Stefan Richter <stefanr@s5r6.in-berlin.de>
2007-03-25 22:22:40 +0200

    ieee1394: remove usage of skb_queue as packet queue
    This considerably reduces the memory requirements for a packet and
    eliminates ieee1394's dependency on CONFIG_NET.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/ieee1394/hosts.h
+++ drivers/ieee1394/hosts.h
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/device.h>
 #include <linux/list.h>
-#include <linux/skbuff.h>
 #include <linux/timer.h>
 #include <linux/types.h>
 #include <linux/workqueue.h>
@@ -26,8 +25,7 @@ struct hpsb_host {
 
 	atomic_t generation;
 
-	struct sk_buff_head pending_packet_queue;
-
+	struct list_head pending_packets;
 	struct timer_list timeout;
 	unsigned long timeout_interval;
 
