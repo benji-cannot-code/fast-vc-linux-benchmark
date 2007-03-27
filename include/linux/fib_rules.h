03f3071ce55114e1619fd41b10376172897e3847
Thomas Graf <tgraf@suug.ch>
2007-03-26 17:37:59 -0700

    [NET] fib_rules: Mark rules detached from the device
    Rules which match against device names in their selector can
    remain while the device itself disappears, in fact the device
    doesn't have to present when the rule is added in the first
    place. The device name is resolved by trying when the rule is
    added and later by listening to NETDEV_REGISTER/UNREGISTER
    notifications.
    
    This patch adds the flag FIB_RULE_DEV_DETACHED which is set
    towards userspace when a rule contains a device match which
    is unresolved at the moment. This eases spotting the reason
    why certain rules seem not to function properly.
    
    Signed-off-by: Thomas Graf <tgraf@suug.ch>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/fib_rules.h
+++ include/linux/fib_rules.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define FIB_RULE_PERMANENT	1
 #define FIB_RULE_INVERT		2
 #define FIB_RULE_UNRESOLVED	4
+#define FIB_RULE_DEV_DETACHED	8
 
 struct fib_rule_hdr
 {
