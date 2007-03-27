5c39e3cabf5941366080e57478f1cdd7d55a1ea0
Takashi Iwai <tiwai@suse.de>
2007-03-27 15:54:27 +0200

    [ALSA] ak4114 - Fix a typo in DIF2 bit definition
    Fixed a typo in AK4114_DIF2 bit definition.  This may fix some
    problems for Audiophile 192 and Juli boards.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- include/sound/ak4114.h
+++ include/sound/ak4114.h
@@ -74,7 +74,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* AK4114_REQ_FORMAT bits */
 #define AK4114_MONO		(1<<7)	/* Double Sampling Frequency Mode: 0 = stereo, 1 = mono */
-#define AK4114_DIF2		(1<<5)	/* Audio Data Control */
+#define AK4114_DIF2		(1<<6)	/* Audio Data Control */
 #define AK4114_DIF1		(1<<5)	/* Audio Data Control */
 #define AK4114_DIF0		(1<<4)	/* Audio Data Control */
 #define AK4114_DIF_16R		(0)				/* STDO: 16-bit, right justified */
