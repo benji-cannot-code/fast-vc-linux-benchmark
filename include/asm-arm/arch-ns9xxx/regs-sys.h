ad8af6e158c8b4b11fdc00fc861ed58f393ea4c6
Uwe Kleine-König <ukleinek@informatik.uni-freiburg.de>
2007-03-28 17:54:22 +0100

    [ARM] 4293/1: ns9xxx: Add bit fields FS and ND to the definition of 	SYS_PLL register
    Signed-off-by: Uwe Kleine-König <ukleinek@informatik.uni-freiburg.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-ns9xxx/regs-sys.h
+++ include/asm-arm/arch-ns9xxx/regs-sys.h
@@ -49,6 +49,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* PLL Configuration register */
 #define SYS_PLL		__REG(0xa0900188)
 
+/* PLL FS status */
+#define SYS_PLL_FS		__REGBITS(24, 23)
+
+/* PLL ND status */
+#define SYS_PLL_ND		__REGBITS(20, 16)
+
 /* PLL Configuration register: PLL SW change */
 #define SYS_PLL_SWC		__REGBIT(15)
 #define SYS_PLL_SWC_NO			__REGVAL(SYS_PLL_SWC, 0)
