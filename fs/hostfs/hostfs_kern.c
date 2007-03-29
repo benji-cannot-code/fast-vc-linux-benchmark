8ed734121c84471413fd7b078d888842b7a65cd0
Jeff Dike <jdike@addtoit.com>
2007-03-29 01:20:32 -0700

    [PATCH] uml: fix compilation problems
    Fix a few miscellaneous compilation problems -
    	an assignment with mismatched types in ldt.c
    	a missing include in mconsole.h which needs a definition of uml_pt_regs
    	I missed removing an include of user_util.h in hostfs
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Cc: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/hostfs/hostfs_kern.c
+++ fs/hostfs/hostfs_kern.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "hostfs.h"
 #include "kern_util.h"
 #include "kern.h"
-#include "user_util.h"
 #include "init.h"
 
 struct hostfs_inode_info {
