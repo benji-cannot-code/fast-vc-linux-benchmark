72a7287ee890b1869da94994e6b39a09e3074062
Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
2007-03-31 11:55:19 -0300

    [SK_BUFF]: Introduce skb_copy_to_linear_data{_offset}
    To clearly state the intent of copying to linear sk_buffs, _offset being a
    overly long variant but interesting for the sake of saving some bytes.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>

--- drivers/net/defxx.c
+++ drivers/net/defxx.c
@@ -3092,7 +3092,9 @@ static void dfx_rcv_queue_process(
 					{
 						/* Receive buffer allocated, pass receive packet up */
 
-						memcpy(skb->data, p_buff + RCV_BUFF_K_PADDING, pkt_len+3);
+						skb_copy_to_linear_data(skb,
+							       p_buff + RCV_BUFF_K_PADDING,
+							       pkt_len + 3);
 					}
 
 					skb_reserve(skb,3);		/* adjust data field so that it points to FC byte */
