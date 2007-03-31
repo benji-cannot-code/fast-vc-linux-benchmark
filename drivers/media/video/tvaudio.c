a588584e5cb1d8a2d8b949c6364e87fa5efba88d
Jean Delvare <khali@linux-fr.org>
2007-03-31 10:35:24 -0300

    V4L/DVB (5492): Remove useless includes of i2c-algo-bit.h
    The tda7432, tda9875 and tvaudio media drivers don't need to include
    the linux/i2c-algo-bit.h header file.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tvaudio.c
+++ drivers/media/video/tvaudio.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/videodev.h>
 #include <linux/i2c.h>
-#include <linux/i2c-algo-bit.h>
 #include <linux/init.h>
 #include <linux/smp_lock.h>
 #include <linux/kthread.h>
