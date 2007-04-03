218b5c80851740af69cab8bd08b3a5293f27a43e
Stephen Rothwell <sfr@canb.auug.org.au>
2007-04-03 10:52:17 +1000

    [POWERPC] get_property returns const
    This just tidies up some of the remains.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- drivers/scsi/ibmvscsi/ibmvstgt.c
+++ drivers/scsi/ibmvscsi/ibmvstgt.c
@@ -898,7 +898,7 @@ static int get_system_info(void)
 {
 	struct device_node *rootdn;
 	const char *id, *model, *name;
-	unsigned int *num;
+	const unsigned int *num;
 
 	rootdn = find_path_device("/");
 	if (!rootdn)
@@ -913,7 +913,7 @@ static int get_system_info(void)
 	if (name)
 		strncpy(partition_name, name, sizeof(partition_name));
 
-	num = (unsigned int *) get_property(rootdn, "ibm,partition-no", NULL);
+	num = get_property(rootdn, "ibm,partition-no", NULL);
 	if (num)
 		partition_number = *num;
 
