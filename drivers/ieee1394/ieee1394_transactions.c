9091c02b4c4c382e6e3c046e6d067a72e345e8b2
Torsten Kaiser <just.for.lkml@googlemail.com>
2007-04-09 21:03:15 +0200

    ieee1394: ieee1394_transactions needs sched.h
    drivers/ieee1394/ieee1394_transactions.c fails for me if CONFIG_SMP=n
    
    gcc complains:
      CC      drivers/ieee1394/ieee1394_transactions.o
    drivers/ieee1394/ieee1394_transactions.c: In function 'hpsb_get_tlabel':
    drivers/ieee1394/ieee1394_transactions.c:183: error:
    'TASK_INTERRUPTIBLE' undeclared (first use in this function)
    drivers/ieee1394/ieee1394_transactions.c:183: error: (Each undeclared
    identifier is reported only once
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de> (added comment)

--- drivers/ieee1394/ieee1394_transactions.c
+++ drivers/ieee1394/ieee1394_transactions.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bitops.h>
 #include <linux/hardirq.h>
 #include <linux/spinlock.h>
+#include <linux/sched.h>  /* because linux/wait.h is broken if CONFIG_SMP=n */
 #include <linux/wait.h>
 
 #include <asm/bug.h>
