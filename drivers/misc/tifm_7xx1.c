d6a07732062eef88bcbab9eaf420384fed1f2556
Alex Dubov <oakad@yahoo.com>
2007-04-12 17:05:26 +1000

    tifm: layout fixes, small changes to comments and printfs
    Cosmetic changes to the code.
    
    Signed-off-by: Alex Dubov <oakad@yahoo.com>
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/misc/tifm_7xx1.c
+++ drivers/misc/tifm_7xx1.c
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/tifm.h>
-#include <linux/dma-mapping.h>
-#include <linux/freezer.h>
 
 #define DRIVER_NAME "tifm_7xx1"
 #define DRIVER_VERSION "0.8"
@@ -42,8 +40,7 @@ static irqreturn_t tifm_7xx1_isr(int irq, void *dev_id)
 {
 	struct tifm_adapter *fm = dev_id;
 	struct tifm_dev *sock;
-	unsigned int irq_status;
-	unsigned int cnt;
+	unsigned int irq_status, cnt;
 
 	spin_lock(&fm->lock);
 	irq_status = readl(fm->addr + FM_INTERRUPT_STATUS);
@@ -135,11 +132,10 @@ static void tifm_7xx1_switch_media(struct work_struct *work)
 {
 	struct tifm_adapter *fm = container_of(work, struct tifm_adapter,
 					       media_switcher);
+	struct tifm_dev *sock;
 	unsigned long flags;
 	unsigned char media_id;
-	int cnt;
-	struct tifm_dev *sock;
-	unsigned int socket_change_set;
+	unsigned int socket_change_set, cnt;
 
 	spin_lock_irqsave(&fm->lock, flags);
 	socket_change_set = fm->socket_change_set;
