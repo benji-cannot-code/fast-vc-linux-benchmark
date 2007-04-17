c76b7e918471430dbe55ce31798965439ca5aa3d
Graeme Gregory <gg@opensource.wolfsonmicro.com>
2007-04-17 12:35:18 +0200

    [ALSA] ASoC Samsung S3c24xx updates - audio DMA cleanup
    This patch cleans up the audio DMA for the Samsung S3C24xx platform.
    
    Signed-off-by: Graeme Gregory <gg@opensource.wolfsonmicro.com>
    Signed-off-by: Liam Girdwood <lg@opensource.wolfsonmicro.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/soc/s3c24xx/s3c24xx-pcm.h
+++ sound/soc/s3c24xx/s3c24xx-pcm.h
@@ -17,14 +17,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ST_OPENED		(1<<1)
 
 struct s3c24xx_pcm_dma_params {
-	struct s3c2410_dma_client *client;			/* stream identifier */
-	int channel;						/* Channel ID */
+	struct s3c2410_dma_client *client;	/* stream identifier */
+	int channel;				/* Channel ID */
 	dma_addr_t dma_addr;
+	int dma_size;			/* Size of the DMA transfer */
 };
 
 #define S3C24XX_DAI_I2S			0
-
-extern struct snd_soc_cpu_dai s3c24xx_i2s_dai;
 
 /* platform data */
 extern struct snd_soc_platform s3c24xx_soc_platform;
