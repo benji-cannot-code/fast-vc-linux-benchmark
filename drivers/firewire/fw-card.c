c54f229361920425c7e414293b63c772c7d1dfa1
Kristian Høgsberg <krh@redhat.com>
2007-04-17 11:51:57 -0400

    firewire: Don't use subsystem rwsem, it's going away.
    Signed-off-by: Kristian Høgsberg <krh@redhat.com>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-card.c
+++ drivers/firewire/fw-card.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/errno.h>
 #include <linux/device.h>
+#include <linux/rwsem.h>
 #include "fw-transaction.h"
 #include "fw-topology.h"
 #include "fw-device.h"
@@ -50,6 +51,7 @@ crc16_itu_t(const u32 *buffer, size_t length)
 	return crc;
 }
 
+static DECLARE_RWSEM(card_rwsem);
 static LIST_HEAD(card_list);
 
 static LIST_HEAD(descriptor_list);
@@ -163,7 +165,7 @@ fw_core_add_descriptor (struct fw_descriptor *desc)
 	if (i != desc->length)
 		return -EINVAL;
 
-	down_write(&fw_bus_type.subsys.rwsem);
+	down_write(&card_rwsem);
 
 	list_add_tail (&desc->link, &descriptor_list);
 	descriptor_count++;
@@ -171,7 +173,7 @@ fw_core_add_descriptor (struct fw_descriptor *desc)
 		descriptor_count++;
 	update_config_roms();
 
-	up_write(&fw_bus_type.subsys.rwsem);
+	up_write(&card_rwsem);
 
 	return 0;
 }
@@ -180,7 +182,7 @@ EXPORT_SYMBOL(fw_core_add_descriptor);
 void
 fw_core_remove_descriptor (struct fw_descriptor *desc)
 {
-	down_write(&fw_bus_type.subsys.rwsem);
+	down_write(&card_rwsem);
 
 	list_del(&desc->link);
 	descriptor_count--;
@@ -188,7 +190,7 @@ fw_core_remove_descriptor (struct fw_descriptor *desc)
 		descriptor_count--;
 	update_config_roms();
 
-	up_write(&fw_bus_type.subsys.rwsem);
+	up_write(&card_rwsem);
 }
 EXPORT_SYMBOL(fw_core_remove_descriptor);
 
@@ -405,10 +407,10 @@ fw_card_add(struct fw_card *card,
 	 * drops it when the driver calls fw_core_remove_card. */
 	fw_card_get(card);
 
-	down_write(&fw_bus_type.subsys.rwsem);
+	down_write(&card_rwsem);
 	config_rom = generate_config_rom (card, &length);
 	list_add_tail(&card->link, &card_list);
-	up_write(&fw_bus_type.subsys.rwsem);
+	up_write(&card_rwsem);
 
 	return card->driver->enable(card, config_rom, length);
 }
@@ -488,9 +490,9 @@ fw_core_remove_card(struct fw_card *card)
 				     PHY_LINK_ACTIVE | PHY_CONTENDER, 0);
 	fw_core_initiate_bus_reset(card, 1);
 
-	down_write(&fw_bus_type.subsys.rwsem);
+	down_write(&card_rwsem);
 	list_del(&card->link);
-	up_write(&fw_bus_type.subsys.rwsem);
+	up_write(&card_rwsem);
 
 	/* Set up the dummy driver. */
 	card->driver = &dummy_driver;
