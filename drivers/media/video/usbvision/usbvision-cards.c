8a435fc0c87ba6d494ac31712bb8e04b187c09ed
Thierry MERLE <thierry.merle@free.fr>
2007-04-17 02:28:32 -0300

    V4L/DVB (5530): Usbvision: remove CustomDevice facility
    usbvision has a module parameter that ables the user to add a new USB 
    entry at driver load. This functionality is useless by experience 
    (adding statically the entry is easy). 
    
    Furthermore, the USB_DEVICE(0xfff0, 0xfff0) USB entry caused 
    usbvision_probe to be called for all unclaimed devices. 
    
    Signed-off-by: Thierry MERLE <thierry.merle@free.fr>
    Acked-by: Dwaine Garden <DwaineGarden@rogers.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/usbvision/usbvision-cards.c
+++ drivers/media/video/usbvision/usbvision-cards.c
@@ -33,20 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Supported Devices: A table for usbvision.c*/
 struct usbvision_device_data_st  usbvision_device_data[] = {
-	[DUMMY_DEVICE] = {
-		.Interface     = -1,
-		.Codec         = CODEC_SAA7111,
-		.VideoChannels = 3,
-		.VideoNorm     = V4L2_STD_NTSC,
-		.AudioChannels = 1,
-		.Radio         = 1,
-		.vbi           = 1,
-		.Tuner         = 1,
-		.TunerType     = TUNER_PHILIPS_NTSC_M,
-		.X_Offset      = -1,
-		.Y_Offset      = -1,
-		.ModelString   = "Custom Dummy USBVision Device",
-	},
 	[XANBOO] = {
 		.Interface     = -1,
 		.Codec         = CODEC_SAA7113,
@@ -1033,7 +1019,6 @@ const int usbvision_device_data_size=ARRAY_SIZE(usbvision_device_data);
 /* Supported Devices */
 
 struct usb_device_id usbvision_table [] = {
-	{ USB_DEVICE(0xfff0, 0xfff0), .driver_info=DUMMY_DEVICE },
 	{ USB_DEVICE(0x0a6f, 0x0400), .driver_info=XANBOO },
 	{ USB_DEVICE(0x050d, 0x0106), .driver_info=BELKIN_VIDEOBUS_II },
 	{ USB_DEVICE(0x050d, 0x0207), .driver_info=BELKIN_VIDEOBUS },
