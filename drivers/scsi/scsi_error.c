5696bac7af5f300acbeba3589d2a27af8e18697a
Brian King <brking@linux.vnet.ibm.com>
2007-04-19 13:59:59 -0500

    [SCSI] use sysfs configured timeout for EH Start Unit timeout
    Use the sysfs configurable timeout when issuing a START_UNIT
    command from the scsi error handler. This is needed for devices which
    take longer than thirty seconds to respond to the start
    unit. The problem was observed when sending a start unit
    to a disk array device in an ipr RAID adapter, which results
    in the adapter firmware sending potentially multiple commands
    to physical devices as a result of this command, which ended
    up timing out sometimes. This patch does not change the default
    value used for this command.
    
    Signed-off-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/scsi_error.c
+++ drivers/scsi/scsi_error.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "scsi_logging.h"
 
 #define SENSE_TIMEOUT		(10*HZ)
-#define START_UNIT_TIMEOUT	(30*HZ)
 
 /*
  * These should *probably* be handled by the host itself.
@@ -937,7 +936,7 @@ static int scsi_eh_try_stu(struct scsi_cmnd *scmd)
 
 		for (i = 0; rtn == NEEDS_RETRY && i < 2; i++)
 			rtn = scsi_send_eh_cmnd(scmd, stu_command, 6,
-						START_UNIT_TIMEOUT, 0);
+						scmd->device->timeout, 0);
 
 		if (rtn == SUCCESS)
 			return 0;
