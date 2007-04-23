ac42577eb34dbd7a4f595248efcc7b44a8ceaba5
Luc Saillard <luc@saillard.org>
2007-04-22 23:54:36 -0300

    V4L/DVB (5547): Add ENUM_FRAMESIZES and ENUM_FRAMEINTERVALS ioctls
    This patch add support for the VIDIOC_ENUM_FRAMESIZES and
    VIDIOC_ENUM_FRAMEINTERVALS ioctl.
    * check if the maximum native framesize for raw mode is correct
    * raw mode framerates for all three chipset types
    
    Signed-off-by: Gregor Jasny <gjasny@web.de>
    Signed-off-by: Luc Saillard <luc@saillard.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pwc/pwc-kiara.h
+++ drivers/media/video/pwc/pwc-kiara.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <media/pwc-ioctl.h>
 
+#define PWC_FPS_MAX_KIARA 6
+
 struct Kiara_table_entry
 {
 	char alternate;			/* USB alternate interface */
@@ -38,8 +40,9 @@ struct Kiara_table_entry
 	unsigned char mode[12];		/* precomputed mode settings for cam */
 };
 
-extern const struct Kiara_table_entry Kiara_table[PSZ_MAX][6][4];
+extern const struct Kiara_table_entry Kiara_table[PSZ_MAX][PWC_FPS_MAX_KIARA][4];
 extern const unsigned int KiaraRomTable[8][2][16][8];
+extern const unsigned int Kiara_fps_vector[PWC_FPS_MAX_KIARA];
 
 #endif
 
