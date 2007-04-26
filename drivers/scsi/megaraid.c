dc3e50f4ac5247644eb8aa634921204e2cf21b7e
Amol Lad <amol@verismonetworks.com>
2007-04-26 00:35:13 -0700

    [SCSI] megaraid: replace yield() with cond_resched()
    For this driver cond_resched() seems to be a better alternative
    
    Signed-off-by: Amol Lad <amol@verismonetworks.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/megaraid.c
+++ drivers/scsi/megaraid.c
@@ -1755,7 +1755,8 @@ __mega_busywait_mbox (adapter_t *adapter)
 	for (counter = 0; counter < 10000; counter++) {
 		if (!mbox->m_in.busy)
 			return 0;
-		udelay(100); yield();
+		udelay(100);
+		cond_resched();
 	}
 	return -1;		/* give up after 1 second */
 }
