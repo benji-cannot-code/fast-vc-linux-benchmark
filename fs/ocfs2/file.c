7591c4466751aa8f3128913cdda129c6466d2db5
Mark Fasheh <mark.fasheh@oracle.com>
2007-04-26 11:43:43 -0700

    ocfs2: filter more error prints
    We don't want to print anything at all in ocfs2_lookup() when getting an
    error from ocfs2_iget() - it could be something as innocuous as a signal
    being detected in the dlm.
    
    ocfs2_permission() should filter on -ENOENT which ocfs2_meta_lock() can
    return if the inode was deleted on another node.
    
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/file.c
+++ fs/ocfs2/file.c
@@ -973,7 +973,8 @@ int ocfs2_permission(struct inode *inode, int mask, struct nameidata *nd)
 
 	ret = ocfs2_meta_lock(inode, NULL, 0);
 	if (ret) {
-		mlog_errno(ret);
+		if (ret != -ENOENT)
+			mlog_errno(ret);
 		goto out;
 	}
 
