b4751c8b86a1161b07bca46a42235dce8c2fe5ce
Ralf Baechle <ralf@linux-mips.org>
2007-04-26 15:46:25 +0100

    [MIPS] Malta: Delete unused prototype of mips_timer_interrupt.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/malta/malta_int.c
+++ arch/mips/mips-boards/malta/malta_int.c
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mips-boards/msc01_pci.h>
 #include <asm/msc01_ic.h>
 
-extern void mips_timer_interrupt(void);
-
 static DEFINE_SPINLOCK(mips_irq_lock);
 
 static inline int mips_pcibios_iack(void)
