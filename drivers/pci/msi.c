68364a06055085765205a9b9b03c764268324ac7
Dan Williams <dan.j.williams@intel.com>
2007-04-26 18:21:38 -0700

    msi: fix ARM compile
    In file included from drivers/pci/msi.c:22:
    include/asm/smp.h:17:26: asm/arch/smp.h: No such file or directory
    include/asm/smp.h:20:3: #error "<asm-arm/smp.h> included in non-SMP build"
    include/asm/smp.h:23:1: warning: "raw_smp_processor_id" redefined
    In file included from include/linux/sched.h:65,
                     from include/linux/mm.h:4,
                     from drivers/pci/msi.c:10:
    include/linux/smp.h:85:1: warning: this is the location of the previous
    definition
    
    Tested on powerpc, i386, and x86_64.
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Acked-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -16,10 +16,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/proc_fs.h>
 #include <linux/msi.h>
+#include <linux/smp.h>
 
 #include <asm/errno.h>
 #include <asm/io.h>
-#include <asm/smp.h>
 
 #include "pci.h"
 #include "msi.h"
