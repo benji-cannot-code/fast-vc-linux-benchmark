aac6944ebea9ffcab4ef4cd1fd5d83c74585babf
Johannes Berg <johannes@sipsolutions.net>
2007-04-30 21:37:15 +1000

    [POWERPC] Fix suspend states again
    In commit 0fba3a1f39f8b0a50b56c8b068fa52131cbc84c2 (a very long time ago,
    May 2006), I fixed a bug that caused powermacs to crash when you tried
    entering standby/mem suspend states.
    
    As I'm now getting more familiar with the suspend code I notice a few
    more things:
     1. we previously misunderstood what pm_ops is for, it isn't supposed to be
        for doing platform dependent suspend/resume stuff that needs to be done
        for suspend to disk (as we currently try to use it!), it is instead for
        entering platform dependent suspend states ("standby", "mem").
     2. due to the first point, we never properly save FPU and altivec states
        when suspending to disk. It probably hasn't hurt yet because the process
        that writes the "disk" to /sys/power/state uses neither and its context
        is used.
    
    This patch addresses these points as follows:
     1. remove all pm_ops from powermac, powermac suspend to ram isn't currently
        usable via /sys/power/state but is done via the PMU instead.
     2. move the code responsible for storing FPU/altivec state into
        save_processor_state and the set_context() call to restore_processor_state.
     3. add a call to kernel_enable_spe()
    
    It may look like there is some code removal missing but that is
    actually because the new suspend.h file overrides the ppc/suspend.h
    one which was previously used.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

---
+++ include/asm-powerpc/suspend.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_POWERPC_SUSPEND_H
+#define __ASM_POWERPC_SUSPEND_H
+
+static inline int arch_prepare_suspend(void) { return 0; }
+
+void save_processor_state(void);
+void restore_processor_state(void);
+
+#endif /* __ASM_POWERPC_SUSPEND_H */
