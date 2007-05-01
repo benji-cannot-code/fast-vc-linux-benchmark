ddd13a957c07e10bc509fb8db40cfc51199d11dc
Jean Delvare <khali@linux-fr.org>
2007-05-01 23:26:28 +0200

    i2c: i2c_adapter devices need no driver
    Kill i2c_adapter_driver as it doesn't make sense and it prevents
    further i2c-core cleanups. i2c_adapter devices are virtual devices
    (ex-class devices) and as such they don't need a driver.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* --- For i2c-isa ---------------------------------------------------- */
 
 extern void i2c_adapter_dev_release(struct device *dev);
-extern struct device_driver i2c_adapter_driver;
 extern struct class i2c_adapter_class;
 extern struct bus_type i2c_bus_type;
 
