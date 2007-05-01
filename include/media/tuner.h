df92498dfedc0b8dd8ee9cb377dd79ca1dc90deb
Jean Delvare <khali@linux-fr.org>
2007-05-01 23:26:29 +0200

    i2c: Cleanup the includes of <linux/i2c.h>
    Clean up the includes of <linux/i2c.h>. Only include this header file
    when we actually need it.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>

--- include/media/tuner.h
+++ include/media/tuner.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _TUNER_H
 
 #include <linux/videodev2.h>
+#include <linux/i2c.h>
 #include <media/tuner-types.h>
 
 extern int tuner_debug;
