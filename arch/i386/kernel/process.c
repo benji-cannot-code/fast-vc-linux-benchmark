8e280deca1a8fb3474e884f1aff57b9f611e7d85
Rusty Russell <rusty@rustcorp.com.au>
2007-05-02 19:27:13 +0200

    [PATCH] i386: i386 separate hardware-defined TSS from Linux additions
    On Thu, 2007-03-29 at 13:16 +0200, Andi Kleen wrote:
    > Please clean it up properly with two structs.
    
    Not sure about this, now I've done it.  Running it here.
    
    If you like it, I can do x86-64 as well.
    
    ==
    lguest defines its own TSS struct because the "struct tss_struct"
    contains linux-specific additions.  Andi asked me to split the struct
    in processor.h.
    
    Unfortunately it makes usage a little awkward.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/i386/kernel/process.c
+++ arch/i386/kernel/process.c
@@ -376,7 +376,7 @@ void exit_thread(void)
 		t->io_bitmap_max = 0;
 		tss->io_bitmap_owner = NULL;
 		tss->io_bitmap_max = 0;
-		tss->io_bitmap_base = INVALID_IO_BITMAP_OFFSET;
+		tss->x86_tss.io_bitmap_base = INVALID_IO_BITMAP_OFFSET;
 		put_cpu();
 	}
 }
@@ -555,7 +555,7 @@ static noinline void __switch_to_xtra(struct task_struct *next_p,
 		 * Disable the bitmap via an invalid offset. We still cache
 		 * the previous bitmap owner and the IO bitmap contents:
 		 */
-		tss->io_bitmap_base = INVALID_IO_BITMAP_OFFSET;
+		tss->x86_tss.io_bitmap_base = INVALID_IO_BITMAP_OFFSET;
 		return;
 	}
 
@@ -565,7 +565,7 @@ static noinline void __switch_to_xtra(struct task_struct *next_p,
 		 * matches the next task, we dont have to do anything but
 		 * to set a valid offset in the TSS:
 		 */
-		tss->io_bitmap_base = IO_BITMAP_OFFSET;
+		tss->x86_tss.io_bitmap_base = IO_BITMAP_OFFSET;
 		return;
 	}
 	/*
@@ -577,7 +577,7 @@ static noinline void __switch_to_xtra(struct task_struct *next_p,
 	 * redundant copies when the currently switched task does not
 	 * perform any I/O during its timeslice.
 	 */
-	tss->io_bitmap_base = INVALID_IO_BITMAP_OFFSET_LAZY;
+	tss->x86_tss.io_bitmap_base = INVALID_IO_BITMAP_OFFSET_LAZY;
 }
 
 /*
