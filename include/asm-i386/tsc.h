5ffcf5fd0d3d6590cf38d7ce244d1bc03735356e
Andi Kleen <ak@suse.de>
2007-05-02 19:27:20 +0200

    [PATCH] i386: Implement X86_FEATURE_SYNC_RDTSC on i386
    Syncs up with x86-64.
    
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-i386/tsc.h
+++ include/asm-i386/tsc.h
@@ -36,7 +36,6 @@ static inline cycles_t get_cycles(void)
 static __always_inline cycles_t get_cycles_sync(void)
 {
 	unsigned long long ret;
-#ifdef X86_FEATURE_SYNC_RDTSC
 	unsigned eax;
 
 	/*
@@ -45,9 +44,6 @@ static __always_inline cycles_t get_cycles_sync(void)
 	 */
 	alternative_io("cpuid", ASM_NOP2, X86_FEATURE_SYNC_RDTSC,
 			  "=a" (eax), "0" (1) : "ebx","ecx","edx","memory");
-#else
-	sync_core();
-#endif
 	rdtscll(ret);
 
 	return ret;
