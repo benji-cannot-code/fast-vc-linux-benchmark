97640fe9f6441387c4f900e12c506f3586806774
Jeremy Fitzhardinge <jeremy@goop.org>
2007-05-02 19:27:16 +0200

    [PATCH] x86: update for i386 and x86-64 check_bugs
    Remove spurious comments, headers and keywords from x86-64 bugs.[ch].
    
    Use identify_boot_cpu()
    
    AK: merged with other patch
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/bugs.c
+++ arch/x86_64/kernel/bugs.c
@@ -4,19 +4,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *  Copyright (C) 1994  Linus Torvalds
  *  Copyright (C) 2000  SuSE
- *
- * This is included by init/main.c to check for architecture-dependent bugs.
- *
- * Needs:
- *	void check_bugs(void);
  */
 
 #include <linux/kernel.h>
+#include <linux/init.h>
 #include <asm/alternative.h>
 #include <asm/processor.h>
-#include <asm/i387.h>
-#include <asm/msr.h>
-#include <asm/pda.h>
 
 void __init check_bugs(void)
 {
--- include/asm-i386/bugs.h
+++ include/asm-i386/bugs.h
@@ -8,6 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_I386_BUG_H
 #define _ASM_I386_BUG_H
 
-extern void __init check_bugs(void);
+void check_bugs(void);
 
 #endif	/* _ASM_I386_BUG_H */
