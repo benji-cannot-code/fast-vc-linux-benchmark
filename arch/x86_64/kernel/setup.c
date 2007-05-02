910ea672cffdd97643f3476581008913b5c88d8e
Vivek Goyal <vgoyal@in.ibm.com>
2007-05-02 19:27:07 +0200

    [PATCH] x86-64: Remove the identity mapping as early as possible
    With the rewrite of the SMP trampoline and the early page
    allocator there is nothing that needs identity mapped pages,
    once we start executing C code.
    
    So add zap_identity_mappings into head64.c and remove
    zap_low_mappings() from much later in the code.  The functions
     are subtly different thus the name change.
    
    This also kills boot_level4_pgt which was from an earlier
    attempt to move the identity mappings as early as possible,
    and is now no longer needed.  Essentially I have replaced
    boot_level4_pgt with trampoline_level4_pgt in trampoline.S
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/setup.c
+++ arch/x86_64/kernel/setup.c
@@ -275,8 +275,6 @@ void __init setup_arch(char **cmdline_p)
 
 	dmi_scan_machine();
 
-	zap_low_mappings(0);
-
 #ifdef CONFIG_ACPI
 	/*
 	 * Initialize the ACPI boot-time table parser (gets the RSDP and SDT).
