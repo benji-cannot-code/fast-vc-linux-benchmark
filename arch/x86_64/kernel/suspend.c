98454dd9ab2c89b3d3037fc0be3decf9ed59c755
Vivek Goyal <vgoyal@in.ibm.com>
2007-05-02 19:27:07 +0200

    [PATCH] x86: Move swsusp __pa() dependent code to arch portion
    o __pa() should be used only on kernel linearly mapped virtual addresses
      and not on kernel text and data addresses.
    
    o Hibernation code needs to determine the physical address associated
      with kernel symbol to mark a section boundary which contains pages which
      don't have to be saved and restored during hibernate/resume operation.
    
    o Move this piece of code in arch dependent section. So that architectures
      which don't have kernel text/data mapped into kernel linearly mapped
      region can come up with their own ways of determining physical addresses
      associated with a kernel text.
    
    Signed-off-by: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- arch/x86_64/kernel/suspend.c
+++ arch/x86_64/kernel/suspend.c
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/page.h>
 #include <asm/pgtable.h>
 
+/* References to section boundaries */
+extern const void __nosave_begin, __nosave_end;
+
 struct saved_context saved_context;
 
 unsigned long saved_context_eax, saved_context_ebx, saved_context_ecx, saved_context_edx;
@@ -220,5 +223,16 @@ int swsusp_arch_resume(void)
 		return error;
 	restore_image();
 	return 0;
+}
+
+/*
+ *	pfn_is_nosave - check if given pfn is in the 'nosave' section
+ */
+
+int pfn_is_nosave(unsigned long pfn)
+{
+	unsigned long nosave_begin_pfn = __pa_symbol(&__nosave_begin) >> PAGE_SHIFT;
+	unsigned long nosave_end_pfn = PAGE_ALIGN(__pa_symbol(&__nosave_end)) >> PAGE_SHIFT;
+	return (pfn >= nosave_begin_pfn) && (pfn < nosave_end_pfn);
 }
 #endif /* CONFIG_SOFTWARE_SUSPEND */
