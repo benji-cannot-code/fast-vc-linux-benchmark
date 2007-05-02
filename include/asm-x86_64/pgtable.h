9aa3561b032a1883d8e63397574fa1faf31e1639
Vivek Goyal <vgoyal@in.ibm.com>
2007-05-02 19:27:06 +0200

    [PATCH] x86-64: Clean up the early boot page table
    - Merge physmem_pgt and ident_pgt, removing physmem_pgt.  The merge
      is broken as soon as mm/init.c:init_memory_mapping is run.
    - As physmem_pgt is gone don't export it in pgtable.h.
    - Use defines from pgtable.h for page permissions.
    - Fix the physical memory identity mapping so it is at the correct
      address.
    - Remove the physical memory mapping from wakeup_level4_pgt it
      is at the wrong address so we can't possibly be usinging it.
    - Simply NEXT_PAGE the work to calculate the phys_ alias
      of the labels was very cool.  Unfortuantely it was a brittle
      special purpose hack that makes maitenance more difficult.
      Instead just use label - __START_KERNEL_map like we do
      everywhere else in assembly.
    
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    Signed-off-by: Vivek Goyal <vgoyal@in.ibm.com>
    Signed-off-by: Andi Kleen <ak@suse.de>

--- include/asm-x86_64/pgtable.h
+++ include/asm-x86_64/pgtable.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pda.h>
 
 extern pud_t level3_kernel_pgt[512];
-extern pud_t level3_physmem_pgt[512];
 extern pud_t level3_ident_pgt[512];
 extern pmd_t level2_kernel_pgt[512];
 extern pgd_t init_level4_pgt[];
