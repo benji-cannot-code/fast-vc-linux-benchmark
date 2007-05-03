99c6b539c66cd91e7be1fd751cac3030d20e31d5
Stephen Rothwell <sfr@canb.auug.org.au>
2007-05-03 15:19:05 +1000

    [POWERPC] iSeries: suppress build warning in lparmap.c
    lparmap.c: Assembler messages:
    lparmap.c:51: Warning: ignoring changed section attributes for .text
    
    Idea from Segher Boessenkool.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/lparmap.c
+++ arch/powerpc/kernel/lparmap.c
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/iseries/lpar_map.h>
 
-const struct LparMap __attribute__((__section__(".text"))) xLparMap = {
+/* The # is to stop gcc trying to make .text nonexecutable */
+const struct LparMap __attribute__((__section__(".text #"))) xLparMap = {
 	.xNumberEsids = HvEsidsToMap,
 	.xNumberRanges = HvRangesToMap,
 	.xSegmentTableOffs = STAB0_PAGE,
