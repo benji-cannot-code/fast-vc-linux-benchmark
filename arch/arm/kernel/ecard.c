2ddc479b8a0b929701a3174f6ab97d4008729325
Russell King <rmk@dyn-67.arm.linux.org.uk>
2007-05-03 10:20:47 +0100

    [ARM] ecard: Move private ecard junk out of asm/ecard.h
    Move ecard.c private junk from asm/ecard.h to a local header file.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/ecard.c
+++ arch/arm/kernel/ecard.c
@@ -52,6 +52,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/irq.h>
 #include <asm/tlbflush.h>
 
+#include "ecard.h"
+
 #ifndef CONFIG_ARCH_RPC
 #define HAVE_EXPMASK
 #endif
