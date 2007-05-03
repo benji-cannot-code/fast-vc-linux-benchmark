989013845b4b1634ba69260d141cc445f059d580
Stephen Rothwell <sfr@canb.auug.org.au>
2007-05-03 17:26:52 +1000

    [POWERPC] Rename device_is_compatible to of_device_is_compatible
    for consistency with other Open Firmware interfaces (and Sparc).
    
    This is just a straight replacement.
    
    This leaves the compatibility define in place.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/maple/setup.c
+++ arch/powerpc/platforms/maple/setup.c
@@ -232,7 +232,7 @@ static void __init maple_init_IRQ(void)
 	 */
 
 	for_each_node_by_type(np, "interrupt-controller")
-		if (device_is_compatible(np, "open-pic")) {
+		if (of_device_is_compatible(np, "open-pic")) {
 			mpic_node = np;
 			break;
 		}
