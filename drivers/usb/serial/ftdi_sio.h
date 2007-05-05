4645693df554d6e2879258201e406a26189020ee
Tony Lindgren <tony@atomide.com>
2007-05-04 18:23:24 -0700

    USB: Add support for Olimex arm-usb-ocd JTAG interface serial port
    This patch adds support for the serial port on Olimex arm-usb-ocd
    JTAG interface.
    
    The device appears as two serial ports, but the first one is reserved
    for the JTAG interface. The JTAG interface can be used with OpenOCD
    from userspace. For more information, please see:
    
    http://openocd.berlios.de/web/
    http://www.olimex.com/dev/arm-usb-ocd.html
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.h
+++ drivers/usb/serial/ftdi_sio.h
@@ -527,6 +527,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define FTDI_MAXSTREAM_PID	0xEE18	/* Xbee PKG-U Module */
 
+/* Olimex */
+#define OLIMEX_VID			0x15BA
+#define OLIMEX_ARM_USB_OCD_PID		0x0003
+
 /* Commands */
 #define FTDI_SIO_RESET 		0 /* Reset the port */
 #define FTDI_SIO_MODEM_CTRL 	1 /* Set the modem control register */
