b007c3296117920936fe73c5d2b90220976e7e62
Arnaud Patard <arnaud.patard@rtp-net.org>
2007-05-05 15:12:17 +0100

    [ARM] 4358/1: S3C24XX: mach-qt2410.c: remove linux/mmc/protocol.h header
    linux/mmc/protocol.h header is gone, thus breaking the build of the
    mach-qt2410.c file. As this header is not used, I'm removing it. The
    right headers may still be added later if needed.
    
    Signed-off-by: Arnaud Patard <arnaud.patard@rtp-net.org>
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-s3c2410/mach-qt2410.c
+++ arch/arm/mach-s3c2410/mach-qt2410.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/serial_core.h>
-#include <linux/mmc/protocol.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_bitbang.h>
 
