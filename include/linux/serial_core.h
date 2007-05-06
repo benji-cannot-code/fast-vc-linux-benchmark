78aece01853db53c7e480df2dd9d7580991ec1f2
Bryan Wu <bryan.wu@analog.com>
2007-05-06 14:50:30 -0700

    blackfin: serial driver
    This patch implements the driver necessary use the Analog Devices Blackfin
    processor's Serial Port.
    
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Cc: Russell King <rmk+lkml@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -137,6 +137,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Xilinx uartlite */
 #define PORT_UARTLITE	74
 
+/* Blackfin bf5xx */
+#define PORT_BFIN	75
+
 #ifdef __KERNEL__
 
 #include <linux/compiler.h>
