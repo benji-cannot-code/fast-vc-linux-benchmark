066a908f04e7d493b05485d617866f50c7a3973f
Jeff Dike <jdike@addtoit.com>
2007-05-06 14:50:57 -0700

    uml: delete unused code
    Get rid of a bunch of unused stuff -
    	cpu_feature had no users
    	linux_prog is little-used, so its declaration is moved to the
    user for easy deletion when the whole file goes away
    	a long-unused debugging aid in helper.c is gone
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Cc: Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/kernel/tt/gdb.c
+++ arch/um/kernel/tt/gdb.c
@@ -116,6 +116,8 @@ struct gdb_data {
 	int err;
 };
 
+extern char *linux_prog;
+
 static void config_gdb_cb(void *arg)
 {
 	struct gdb_data *data = arg;
--- arch/um/os-Linux/main.c
+++ arch/um/os-Linux/main.c
@@ -26,12 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "os.h"
 #include "um_malloc.h"
 
-/* Set in set_stklim, which is called from main and __wrap_malloc.
- * __wrap_malloc only calls it if main hasn't started.
- */
-unsigned long stacksizelim;
-
-/* Set in main */
+/* Set in main, unchanged thereafter */
 char *linux_prog;
 
 #define PGD_BOUND (4 * 1024 * 1024)
@@ -53,7 +48,6 @@ static void set_stklim(void)
 			exit(1);
 		}
 	}
-	stacksizelim = (lim.rlim_cur + PGD_BOUND - 1) & ~(PGD_BOUND - 1);
 }
 
 static __init void do_uml_initcalls(void)
