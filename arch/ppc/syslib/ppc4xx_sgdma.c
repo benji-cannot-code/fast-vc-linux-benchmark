89f958f15d7e94bd93adff12636c087b72c80dc1
David Gibson <david@gibson.dropbear.id.au>
2007-05-07 12:13:36 +1000

    [POWERPC] Fix build problem in ppc4xx_sgdma.c
    ppc4xx_sgdma.c is #including asm/dma-mapping.h directly, which should
    only ever be included via linux/dma-mapping.h.  asm/dma-mapping.h
    relies on an enum defined in linux/dma-mapping.h before its own
    include.  This fixes the problem.
    
    Signed-off-by: David Gibson <dwg@au1.ibm.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/ppc/syslib/ppc4xx_sgdma.c
+++ arch/ppc/syslib/ppc4xx_sgdma.c
@@ -24,11 +24,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/dma-mapping.h>
 #include <linux/pci.h>
 
 #include <asm/system.h>
 #include <asm/io.h>
-#include <asm/dma-mapping.h>
 #include <asm/ppc4xx_dma.h>
 
 void
