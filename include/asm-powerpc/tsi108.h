6e05b86e8a4a65aab73b78678a4eb6f25f6b36a7
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2007-05-08 07:27:15 +1000

    [POWERPC] Generalize tsi108 PCI setup
    Generalize tsi108_setup_pci to take the config space physical address and
    primary bus designator as a parameter.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>
    Acked-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/tsi108.h
+++ include/asm-powerpc/tsi108.h
@@ -69,7 +69,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define TSI108_PB_ERRCS_ES		(1 << 1)
 #define TSI108_PB_ISR_PBS_RD_ERR	(1 << 8)
 
-#define TSI108_PCI_CFG_BASE_PHYS	(0xfb000000)
 #define TSI108_PCI_CFG_SIZE		(0x01000000)
 
 /*
