d13d65a28abefb29878ac85f5d35072d16b8e760
Franck Bui-Huu <fbuihuu@gmail.com>
2007-05-07 18:01:52 +0200

    [MIPS] Remove Momenco Jaguar ATX support
    It has some hackish code and it odd DMA results in the need to support
    old features in kernel code.
    
    Signed-off-by: Franck Bui-Huu <fbuihuu@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/bootinfo.h
+++ include/asm-mips/bootinfo.h
@@ -122,7 +122,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define  MACH_MOMENCO_OCELOT	0
 #define  MACH_MOMENCO_OCELOT_G	1	/* no more supported (may 2007) */
 #define  MACH_MOMENCO_OCELOT_C	2
-#define  MACH_MOMENCO_JAGUAR_ATX 3
+#define  MACH_MOMENCO_JAGUAR_ATX 3	/* no more supported (may 2007) */
 #define  MACH_MOMENCO_OCELOT_3	4
 
 /*
