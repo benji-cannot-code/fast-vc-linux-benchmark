17a313300be1b14078d9e97bbd9d7268b730aeb0
David S. Miller <davem@sunset.davemloft.net>
2007-05-07 21:51:41 -0700

    [SPARC64] PCI: Use common routine to fetch PBM properties.
    Namely bus-range and ino-bitmap.
    
    This allows us also to eliminate pci_controller_info's
    pci_{first,last}_busno fields as only the pbm ones are
    used now.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/pci_common.c
+++ arch/sparc64/kernel/pci_common.c
@@ -16,6 +16,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "pci_impl.h"
 
+void pci_get_pbm_props(struct pci_pbm_info *pbm)
+{
+	const u32 *val = of_get_property(pbm->prom_node, "bus-range", NULL);
+
+	pbm->pci_first_busno = val[0];
+	pbm->pci_last_busno = val[1];
+
+	val = of_get_property(pbm->prom_node, "ino-bitmap", NULL);
+	if (val) {
+		pbm->ino_bitmap = (((u64)val[1] << 32UL) |
+				   ((u64)val[0] <<  0UL));
+	}
+}
+
 static void pci_register_legacy_regions(struct resource *io_res,
 					struct resource *mem_res)
 {
