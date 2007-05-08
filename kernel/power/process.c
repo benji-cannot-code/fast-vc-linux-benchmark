c4631ea456bc1b082966d889c73cb1d7b9a2f5a5
Oleg Nesterov <oleg@tv-sign.ru>
2007-05-08 00:24:01 -0700

    freezer: task->exit_state should be treated as bolean
    Except for BUG_ON() checks, we should not use EXIT_XXXX defines outside of
    exit/wait paths.
    
    Signed-off-by: Oleg Nesterov <oleg@tv-sign.ru>
    Cc: "Rafael J. Wysocki" <rjw@sisk.pl>
    Cc: Pavel Machek <pavel@ucw.cz>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Nick Piggin <nickpiggin@yahoo.com.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -26,10 +26,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static inline int freezeable(struct task_struct * p)
 {
-	if ((p == current) || 
+	if ((p == current) ||
 	    (p->flags & PF_NOFREEZE) ||
-	    (p->exit_state == EXIT_ZOMBIE) ||
-	    (p->exit_state == EXIT_DEAD))
+	    (p->exit_state != 0))
 		return 0;
 	return 1;
 }
