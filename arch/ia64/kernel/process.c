c31d3a7c440de4a42a509f791fe5138066a1dadd
Siddha, Suresh B <suresh.b.siddha@intel.com>
2007-05-08 09:22:47 -0700

    [IA64] save and restore cpus_allowed in cpu_idle_wait
    Save and Restore the task's cpus_allowed mask, across the set_cpus_allowed()
    in cpu_idle_wait(). Without this, we will endup corrupting task's cpu affinity.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/process.c
+++ arch/ia64/kernel/process.c
@@ -238,6 +238,7 @@ void cpu_idle_wait(void)
 {
 	unsigned int cpu, this_cpu = get_cpu();
 	cpumask_t map;
+	cpumask_t tmp = current->cpus_allowed;
 
 	set_cpus_allowed(current, cpumask_of_cpu(this_cpu));
 	put_cpu();
@@ -259,6 +260,7 @@ void cpu_idle_wait(void)
 		}
 		cpus_and(map, map, cpu_online_map);
 	} while (!cpus_empty(map));
+	set_cpus_allowed(current, tmp);
 }
 EXPORT_SYMBOL_GPL(cpu_idle_wait);
 
