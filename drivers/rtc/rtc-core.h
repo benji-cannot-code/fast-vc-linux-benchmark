8f2c387d598e5447d7e3c543b66b07ceb38b4592
David Brownell <david-b@pacbell.net>
2007-05-08 00:33:27 -0700

    rtc: remove /sys/class/rtc-dev/*
    This simplifies the /dev support by removing a superfluous class_device (the
    /sys/class/rtc-dev stuff) and the class_interface that hooks it into the rtc
    core.  Accordingly, if it's configured then /dev support is now part of the
    RTC core, and is never a separate module.
    
    It's another step towards being able to remove "struct class_device".
    
    [bunk@stusta.de: drivers/rtc/rtc-dev.c should #include "rtc-core.h"]
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Acked-by: Greg Kroah-Hartman <gregkh@suse.de>
    Acked-By: Alessandro Zummo <a.zummo@towertech.it>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ drivers/rtc/rtc-core.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifdef CONFIG_RTC_INTF_DEV
+
+extern void __init rtc_dev_init(void);
+extern void __exit rtc_dev_exit(void);
+extern void rtc_dev_add_device(struct rtc_device *rtc);
+extern void rtc_dev_del_device(struct rtc_device *rtc);
+
+#else
+
+#define rtc_dev_init()		do{}while(0)
+#define rtc_dev_exit()		do{}while(0)
+#define rtc_dev_add_device(r)	do{}while(0)
+#define rtc_dev_del_device(r)	do{}while(0)
+
+#endif
