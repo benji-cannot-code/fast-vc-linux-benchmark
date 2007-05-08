05869aac503b483c6734fc10edf836ca4164e967
Adrian Bunk <bunk@stusta.de>
2007-05-08 00:24:47 -0700

    init/do_mounts.c: proper prepare_namespace() prototype
    Add a proper protype for prepare_namespace() in include/linux/init.h.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/mount.h>
 #include <linux/device.h>
+#include <linux/init.h>
 
 #include <linux/nfs_fs.h>
 #include <linux/nfs_fs_sb.h>
