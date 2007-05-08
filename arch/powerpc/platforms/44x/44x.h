2665d15f2c2b59a712f949af3c4b614674f6d36c
David Gibson <david@gibson.dropbear.id.au>
2007-05-08 12:59:30 +1000

    [POWERPC] Add powerpc/platforms/44x, disable platforms/4xx for now
    This prepares for Ebony/440 support by creating an
    arch/powerpc/platforms/44x directory.  It is populated with a single
    misc_44x.S file, into which is moved the 44x specific reset code from
    head_44x.S (on the grounds that we should really stop clogging up the
    head_* files with random asm helper routines).
    
    At the same time, we disable the (empty save Kconfig and Makefile)
    arch/powerpc/platforms/4xx directory from the arch/powerpc/platforms
    Makefile.  Contrary to the comment in
    arch/powerpc/platforms/4xx/Makefile, attempting to build such an empty
    Makefile will fail, thus breaking compile for the 44x platforms we're
    about to add.  It can go back in once we start porting some of the 40x
    platforms (and thus it becomes non-empty).
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

---
+++ arch/powerpc/platforms/44x/44x.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __POWERPC_PLATFORMS_44X_44X_H
+#define __POWERPC_PLATFORMS_44X_44X_H
+
+extern void ppc44x_reset_system(char *cmd);
+
+#endif /* __POWERPC_PLATFORMS_44X_44X_H */
