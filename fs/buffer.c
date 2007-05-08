44581b40e2cd6bd49f36a78f04c9e951f2b535e8
Andrew Morton <akpm@osdl.org>
2007-05-08 00:23:27 -0700

    block_write_full_page(): report ENOSPC
    block_write_full_page() forgot to propagate ENPSOC into the address_space.
    
    Cc: Guillaume Chazarain <guichaz@yahoo.fr>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/buffer.c
+++ fs/buffer.c
@@ -1728,6 +1728,7 @@ recover:
 	} while ((bh = bh->b_this_page) != head);
 	SetPageError(page);
 	BUG_ON(PageWriteback(page));
+	mapping_set_error(page->mapping, err);
 	set_page_writeback(page);
 	do {
 		struct buffer_head *next = bh->b_this_page;
