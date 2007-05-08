aaad0cfd414e57ffff564edcfd3b1db3d4b70ec4
Kristian Høgsberg <krh@redhat.com>
2007-05-07 20:33:36 -0400

    firewire: Use linux/*.h instead of asm/*.h header files.
    Signed-off-by: Kristian Hoegsberg <krh@redhat.com>
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- include/linux/firewire-cdev.h
+++ include/linux/firewire-cdev.h
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __fw_cdev_h
 #define __fw_cdev_h
 
-#include <asm/ioctl.h>
-#include <asm/types.h>
+#include <linux/ioctl.h>
+#include <linux/types.h>
 
 #define TCODE_WRITE_QUADLET_REQUEST	0
 #define TCODE_WRITE_BLOCK_REQUEST	1
