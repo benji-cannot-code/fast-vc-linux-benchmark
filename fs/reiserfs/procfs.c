84b40eaa9518942619b6e6b61967cb8f19b29426
Robert P. J. Day <rpjday@mindspring.com>
2007-05-08 00:25:01 -0700

    reiserfs: correct misspelled "REISERFS_PROC_INFO" to "CONFIG_REISERFS_PROC_INFO"
    Correct the misspelling of the preprocessor check of a Kconfig option to refer
    to CONFIG_REISERFS_PROC_INFO and not just the incorrect REISERFS_PROC_INFO.
    
    Signed-off-by: Robert P. J. Day <rpjday@mindspring.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/reiserfs/procfs.c
+++ fs/reiserfs/procfs.c
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 
-#if defined( REISERFS_PROC_INFO )
+#ifdef CONFIG_REISERFS_PROC_INFO
 
 /*
  * LOCKING:
