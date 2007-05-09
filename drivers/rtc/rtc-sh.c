a6cb873cd6dd07a119340bf7237ca7eec979b2c1
Paul Mundt <lethal@linux-sh.org>
2007-05-09 12:37:53 +0900

    rtc: rtc-sh: Fix rtc_dev pointer for rtc_update_irq().
    When the rtc_update_irq() callsites stopped passing in the
    class_dev, the rtc_dev references weren't fixed. Fix it up,
    so we pass in the proper pointer.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- drivers/rtc/rtc-sh.c
+++ drivers/rtc/rtc-sh.c
@@ -105,7 +105,7 @@ static irqreturn_t sh_rtc_interrupt(int irq, void *dev_id)
 
 	writeb(tmp, rtc->regbase + RCR1);
 
-	rtc_update_irq(&rtc->rtc_dev, 1, events);
+	rtc_update_irq(rtc->rtc_dev, 1, events);
 
 	spin_unlock(&rtc->lock);
 
@@ -140,7 +140,7 @@ static irqreturn_t sh_rtc_alarm(int irq, void *dev_id)
 
 		rtc->rearm_aie = 1;
 
-		rtc_update_irq(&rtc->rtc_dev, 1, events);
+		rtc_update_irq(rtc->rtc_dev, 1, events);
 	}
 
 	spin_unlock(&rtc->lock);
@@ -154,7 +154,7 @@ static irqreturn_t sh_rtc_periodic(int irq, void *dev_id)
 
 	spin_lock(&rtc->lock);
 
-	rtc_update_irq(&rtc->rtc_dev, 1, RTC_PF | RTC_IRQF);
+	rtc_update_irq(rtc->rtc_dev, 1, RTC_PF | RTC_IRQF);
 
 	spin_unlock(&rtc->lock);
 
