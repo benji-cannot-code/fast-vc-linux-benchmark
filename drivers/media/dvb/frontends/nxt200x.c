587a771ca69d998e074980511fa4d810c7d4cba0
Michael Krufky <mkrufky@linuxtv.org>
2007-05-09 11:49:24 -0300

    V4L/DVB (5648): Dvb/frontends: remove unnecessary #include's of "dvb-pll.h"
    These sources do not need to #include "dvb-pll.h"
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/nxt200x.c
+++ drivers/media/dvb/frontends/nxt200x.c
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 
 #include "dvb_frontend.h"
-#include "dvb-pll.h"
 #include "nxt200x.h"
 
 struct nxt200x_state {
