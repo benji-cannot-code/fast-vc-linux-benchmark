abd63ed8c7f1ef2d43aeafb1e6a961e95b442de5
David S. Miller <davem@sunset.davemloft.net>
2007-05-08 18:25:14 -0700

    [SPARC64]: Optimize fault kprobe handling just like powerpc.
    And eliminate DIE_GPF while we're at it.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/kdebug.h
+++ include/asm-sparc64/kdebug.h
@@ -8,8 +8,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct pt_regs;
 
-extern int register_page_fault_notifier(struct notifier_block *);
-extern int unregister_page_fault_notifier(struct notifier_block *);
+/*
+ * These are only here because kprobes.c wants them to implement a
+ * blatant layering violation.  Will hopefully go away soon once all
+ * architectures are updated.
+ */
+static inline int register_page_fault_notifier(struct notifier_block *nb)
+{
+	return 0;
+}
+static inline int unregister_page_fault_notifier(struct notifier_block *nb)
+{
+	return 0;
+}
 
 extern void bad_trap(struct pt_regs *, long);
 
@@ -21,7 +32,6 @@ enum die_val {
 	DIE_DIE,
 	DIE_TRAP,
 	DIE_TRAP_TL1,
-	DIE_GPF,
 	DIE_CALL,
 	DIE_PAGE_FAULT,
 };
