1b9cff305e9e9a9e016c1890e52c0fbc3c5fb096
Stefan Richter <stefanr@s5r6.in-berlin.de>
2007-05-11 23:04:08 +0200

    firewire: fw-sbp2: remove unused struct member
    cleanup after support of single-buffer requests was dropped
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>
    Signed-off-by: Kristian Hoegsberg <krh@redhat.com>

--- drivers/firewire/fw-sbp2.c
+++ drivers/firewire/fw-sbp2.c
@@ -208,7 +208,6 @@ struct sbp2_command_orb {
 
 	struct sbp2_pointer page_table[SG_ALL];
 	dma_addr_t page_table_bus;
-	dma_addr_t request_buffer_bus;
 };
 
 /*
@@ -882,11 +881,6 @@ complete_command_orb(struct sbp2_orb *base_orb, struct sbp2_status *status)
 	if (orb->page_table_bus != 0)
 		dma_unmap_single(device->card->device, orb->page_table_bus,
 				 sizeof(orb->page_table_bus), DMA_TO_DEVICE);
-
-	if (orb->request_buffer_bus != 0)
-		dma_unmap_single(device->card->device, orb->request_buffer_bus,
-				 sizeof(orb->request_buffer_bus),
-				 DMA_FROM_DEVICE);
 
 	orb->cmd->result = result;
 	orb->done(orb->cmd);
