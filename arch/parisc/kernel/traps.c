c02c8bd1d1a05403865c11f2d38198e40b5a1256
Simon Arlott <simon@fire.lp0.eu>
2007-05-11 20:42:34 +0100

    [PARISC] spelling fixes: arch/parisc/
    Spelling fixes in arch/parisc/.
    
    Signed-off-by: Simon Arlott <simon@fire.lp0.eu>
    Acked-by: Grant Grundler <grundler@parisc-linux.org>
    Signed-off-by: Kyle McMartin <kyle@parisc-linux.org>

--- arch/parisc/kernel/traps.c
+++ arch/parisc/kernel/traps.c
@@ -616,7 +616,7 @@ void handle_interruption(int code, struct pt_regs *regs)
 		
 	case 13:
 		/* Conditional Trap
-		   The condition succees in an instruction which traps 
+		   The condition succeeds in an instruction which traps
 		   on condition  */
 		if(user_mode(regs)){
 			si.si_signo = SIGFPE;
