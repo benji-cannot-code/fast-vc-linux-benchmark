a5eed35affc23125f115412b8136e9fdb6ea023a
kogiidena <kogiidena@eggplant.ddo.jp>
2007-05-10 22:22:54 -0700

    rtc-rs5c313.c: error and warning are fixed
    Correct a compile error and warning.
    
    Signed-off-by: kogiidena <kogiidena@eggplant.ddo.jp>
    Cc: Alessandro Zummo <a.zummo@towertech.it>
    Cc: David Brownell <david-b@pacbell.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-rs5c313.c
+++ drivers/rtc/rtc-rs5c313.c
@@ -127,7 +127,7 @@ static void rs5c313_write_data(unsigned char data)
 static unsigned char rs5c313_read_data(void)
 {
 	int i;
-	unsigned char data;
+	unsigned char data = 0;
 
 	for (i = 0; i < 8; i++) {
 		ndelay(700);
@@ -195,7 +195,7 @@ static void rs5c313_write_reg(unsigned char addr, unsigned char data)
 	return;
 }
 
-static inline unsigned char rs5c313_read_cntreg(unsigned char addr)
+static inline unsigned char rs5c313_read_cntreg(void)
 {
 	return rs5c313_read_reg(RS5C313_ADDR_CNTREG);
 }
@@ -357,7 +357,7 @@ static int rs5c313_rtc_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, rtc);
 
-	return err;
+	return 0;
 }
 
 static int __devexit rs5c313_rtc_remove(struct platform_device *pdev)
