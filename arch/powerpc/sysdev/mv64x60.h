a363cf2e7699c547fc619944859fb5042fca6b81
Dale Farnsworth <dale@farnsworth.org>
2007-05-12 10:57:12 +1000

    [POWERPC] Add Marvell mv64x60 PCI bridge support
    This patch adds PCI bridge support for the Marvell mv64x60 chip.
    We also provide the ability to read/write the mv64x60 hotswap
    register via sysfs if the hs_reg_valid property is set in the
    device tree.
    
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/sysdev/mv64x60.h
+++ arch/powerpc/sysdev/mv64x60.h
@@ -7,4 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void __init mv64x60_init_irq(void);
 extern unsigned int mv64x60_get_irq(void);
 
+extern void __init mv64x60_pci_init(void);
+
 #endif /* __MV64X60_H__ */
