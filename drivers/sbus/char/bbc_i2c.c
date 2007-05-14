57c202116d1f97c37ab0b94ec22cb4f09d92e921
David S. Miller <davem@sunset.davemloft.net>
2007-05-13 22:22:13 -0700

    [SPARC] SBUS: bbc_i2c.c needs asm/io.h
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/sbus/char/bbc_i2c.c
+++ drivers/sbus/char/bbc_i2c.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ebus.h>
 #include <asm/spitfire.h>
 #include <asm/bbc.h>
+#include <asm/io.h>
 
 #include "bbc_i2c.h"
 
