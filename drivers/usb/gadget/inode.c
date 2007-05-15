1daad86f11c777482e50168b5ad1fac1ee073fa5
David Brownell <david-b@pacbell.net>
2007-05-14 19:36:41 -0700

    USB: export <linux/usb_gadgetfs> as <linux/usb/gadgetfs.h>
    Make sure gadgetfs userspace interface is properly exported:
    
     - Move <linux/usb_gadgetfs.h> to <linux/usb/gadgetfs.h>;
     - Export it using Kbuild;
     - Add an #include guard;
     - Correct some internal documentation;
     - Update struct layout so it's the same on 32/64 bit kernels.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/inode.c
+++ drivers/usb/gadget/inode.c
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/moduleparam.h>
 
-#include <linux/usb_gadgetfs.h>
+#include <linux/usb/gadgetfs.h>
 #include <linux/usb_gadget.h>
 
 
