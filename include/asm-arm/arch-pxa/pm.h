df178ebc4d39d68877917de4508bede8dd9989e2
Russell King <rmk@dyn-67.arm.linux.org.uk>
2007-05-15 11:03:24 +0100

    [ARM] pxa: remove useless pxa_pm_finish() function
    pxa_pm_finish() does nothing but return zero.  The core code
    does nothing with this return value, and will not try to call
    the finish method in the pm_ops structure if it is NULL.
    
    Therefore, we can remove this useless function.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-pxa/pm.h
+++ include/asm-arm/arch-pxa/pm.h
@@ -10,4 +10,3 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern int pxa_pm_prepare(suspend_state_t state);
 extern int pxa_pm_enter(suspend_state_t state);
-extern int pxa_pm_finish(suspend_state_t state);
