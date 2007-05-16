966e7d499ccca6a0d2e04ea25f624c0487e5270f
Linus Torvalds <torvalds@woody.linux-foundation.org>
2007-05-15 18:45:49 -0700

    x86: Fix discontigmem + non-HIGHMEM compile
    It's not necessarily a very sane configuration, but people running "make
    randconfig" noticed it wouldn't compile.  This fixes some obvious
    problems in discontig.c to allow a clean compile.
    
    Acked-by: andrew hendry <andrew.hendry@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/i386/mm/discontig.c
+++ arch/i386/mm/discontig.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/kexec.h>
 #include <linux/pfn.h>
+#include <linux/swap.h>
 
 #include <asm/e820.h>
 #include <asm/setup.h>
@@ -98,14 +99,8 @@ unsigned long node_memmap_size_bytes(int nid, unsigned long start_pfn,
 #endif
 
 extern unsigned long find_max_low_pfn(void);
-extern void find_max_pfn(void);
 extern void add_one_highpage_init(struct page *, int, int);
-
-extern struct e820map e820;
 extern unsigned long highend_pfn, highstart_pfn;
-extern unsigned long max_low_pfn;
-extern unsigned long totalram_pages;
-extern unsigned long totalhigh_pages;
 
 #define LARGE_PAGE_BYTES (PTRS_PER_PTE * PAGE_SIZE)
 
@@ -361,7 +356,9 @@ void __init zone_sizes_init(void)
 	max_zone_pfns[ZONE_DMA] =
 		virt_to_phys((char *)MAX_DMA_ADDRESS) >> PAGE_SHIFT;
 	max_zone_pfns[ZONE_NORMAL] = max_low_pfn;
+#ifdef CONFIG_HIGHMEM
 	max_zone_pfns[ZONE_HIGHMEM] = highend_pfn;
+#endif
 
 	/* If SRAT has not registered memory, register it now */
 	if (find_max_pfn_with_active_regions() == 0) {
