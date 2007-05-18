a6d159ab5a3b77f48a6dc7d6135d6265d177590b
Greg Ungerer <gerg@snapgear.com>
2007-05-18 06:28:01 +0100

    [ARM] 4391/1: make at91 debug unit support optional
    The AT91x40 family doesn't have the debug unit like its bigger brothers.
    But it does have the ID and extension registers (with the bit meanings
    the same). Reorganize at91_dbgu.h to cater for this.
    
    This also affects the load uncompressor, since it outputs to the
    debug port.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Acked-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-at91/uncompress.h
+++ include/asm-arm/arch-at91/uncompress.h
@@ -34,20 +34,24 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 static void putc(int c)
 {
+#ifdef AT91_DBGU
 	void __iomem *sys = (void __iomem *) AT91_BASE_SYS;	/* physical address */
 
 	while (!(__raw_readl(sys + AT91_DBGU_SR) & AT91_DBGU_TXRDY))
 		barrier();
 	__raw_writel(c, sys + AT91_DBGU_THR);
+#endif
 }
 
 static inline void flush(void)
 {
+#ifdef AT91_DBGU
 	void __iomem *sys = (void __iomem *) AT91_BASE_SYS;	/* physical address */
 
 	/* wait for transmission to complete */
 	while (!(__raw_readl(sys + AT91_DBGU_SR) & AT91_DBGU_TXEMPTY))
 		barrier();
+#endif
 }
 
 #define arch_decomp_setup()
