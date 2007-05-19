0a96230520da543a590eca36c2e43445fb48f6bc
Pierre Ossman <drzeus@drzeus.cx>
2007-05-19 14:32:22 +0200

    mmc: move layer init and workqueue to core file
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/core/sysfs.c
+++ drivers/mmc/core/sysfs.c
@@ -11,17 +11,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *  MMC sysfs/driver model support.
  */
-#include <linux/module.h>
-#include <linux/init.h>
 #include <linux/device.h>
-#include <linux/idr.h>
-#include <linux/workqueue.h>
 
 #include <linux/mmc/card.h>
-#include <linux/mmc/host.h>
 
-#include "bus.h"
-#include "host.h"
 #include "sysfs.h"
 
 int mmc_add_attrs(struct mmc_card *card, struct device_attribute *attrs)
@@ -49,47 +42,3 @@ void mmc_remove_attrs(struct mmc_card *card, struct device_attribute *attrs)
 		device_remove_file(&card->dev, &attrs[i]);
 }
 
-static struct workqueue_struct *workqueue;
-
-/*
- * Internal function. Schedule delayed work in the MMC work queue.
- */
-int mmc_schedule_delayed_work(struct delayed_work *work, unsigned long delay)
-{
-	return queue_delayed_work(workqueue, work, delay);
-}
-
-/*
- * Internal function. Flush all scheduled work from the MMC work queue.
- */
-void mmc_flush_scheduled_work(void)
-{
-	flush_workqueue(workqueue);
-}
-
-static int __init mmc_init(void)
-{
-	int ret;
-
-	workqueue = create_singlethread_workqueue("kmmcd");
-	if (!workqueue)
-		return -ENOMEM;
-
-	ret = mmc_register_bus();
-	if (ret == 0) {
-		ret = mmc_register_host_class();
-		if (ret)
-			mmc_unregister_bus();
-	}
-	return ret;
-}
-
-static void __exit mmc_exit(void)
-{
-	mmc_unregister_host_class();
-	mmc_unregister_bus();
-	destroy_workqueue(workqueue);
-}
-
-module_init(mmc_init);
-module_exit(mmc_exit);
