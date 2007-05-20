5c312caa1a75de3933fc4fd0ef1ae93dee2a8da5
Ralf Baechle <ralf@linux-mips.org>
2007-05-20 22:29:01 +0200

    [MIPS] Remove prototype for deleted function qemu_handle_int
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/qemu/q-irq.c
+++ arch/mips/qemu/q-irq.c
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/system.h>
 #include <asm/time.h>
 
-extern asmlinkage void qemu_handle_int(void);
-
 asmlinkage void plat_irq_dispatch(void)
 {
 	unsigned int pending = read_c0_status() & read_c0_cause();
