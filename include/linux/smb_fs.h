3763d734855876bcda9d9b364065bd3134d762e8
Stephen Rothwell <sfr@canb.auug.org.au>
2007-05-22 11:10:19 +1000

    Fix headers check fallout
    commit e8edc6e03a5c8562dc70a6d969f732bdb355a7e7 added an include of
    linux/jiffies.h in linux/smb_fs.h outside the ifdef __KERNEL__.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/smb_fs.h
+++ include/linux/smb_fs.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SMB_FS_H
 #define _LINUX_SMB_FS_H
 
-#include <linux/jiffies.h>
 #include <linux/smb.h>
 
 /*
@@ -31,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>
 #include <linux/vmalloc.h>
 #include <linux/smb_mount.h>
+#include <linux/jiffies.h>
 #include <asm/unaligned.h>
 
 static inline struct smb_sb_info *SMB_SB(struct super_block *sb)
