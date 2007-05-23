58783be5caf07f93cf30e4f9d038090b9faca3d8
Christopher J. PeBenito <cpebenito@tresys.com>
2007-05-23 09:12:07 -0400

    selinux: rename sel_remove_bools() for more general usage.
    sel_remove_bools() will also be used by the object class discovery, rename
    it for more general use.
    
    Signed-off-by: Christopher J. PeBenito <cpebenito@tresys.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -941,9 +941,8 @@ static const struct file_operations sel_commit_bools_ops = {
 	.write          = sel_commit_bools_write,
 };
 
-/* delete booleans - partial revoke() from
- * fs/proc/generic.c proc_kill_inodes */
-static void sel_remove_bools(struct dentry *de)
+/* partial revoke() from fs/proc/generic.c proc_kill_inodes */
+static void sel_remove_entries(struct dentry *de)
 {
 	struct list_head *p, *node;
 	struct super_block *sb = de->d_sb;
@@ -999,7 +998,7 @@ static int sel_make_bools(void)
 	kfree(bool_pending_values);
 	bool_pending_values = NULL;
 
-	sel_remove_bools(dir);
+	sel_remove_entries(dir);
 
 	if (!(page = (char*)get_zeroed_page(GFP_KERNEL)))
 		return -ENOMEM;
@@ -1049,7 +1048,7 @@ out:
 	return ret;
 err:
 	kfree(values);
-	sel_remove_bools(dir);
+	sel_remove_entries(dir);
 	ret = -ENOMEM;
 	goto out;
 }
