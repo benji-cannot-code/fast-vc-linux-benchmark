f4570b97e7cb6fe8bb1fd51997c1fdc60557ac25
David Woodhouse <dwmw2@infradead.org>
2007-05-23 13:57:52 -0700

    Missing 'const' from reiserfs MIN_KEY declaration.
    In stree.c, MIN_KEY is declared const. The extern declaration in dir.c
    doesn't match...
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/reiserfs/dir.c
+++ fs/reiserfs/dir.c
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/buffer_head.h>
 #include <asm/uaccess.h>
 
-extern struct reiserfs_key MIN_KEY;
+extern const struct reiserfs_key MIN_KEY;
 
 static int reiserfs_readdir(struct file *, void *, filldir_t);
 static int reiserfs_dir_fsync(struct file *filp, struct dentry *dentry,
