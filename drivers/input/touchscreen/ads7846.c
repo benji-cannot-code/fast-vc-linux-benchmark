db63910dfd7e158d7251e838d9525782ce6cd579
David Brownell <david-b@pacbell.net>
2007-05-22 23:28:40 -0400

    Input: ads7846 - document that it handles tsc2046 too
    The TSC2046 is an updated version of the ADS7846 ... mention that in
    the Kconfig helptext and driver source.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Acked-by: Kevin Hilman <khilman@mvista.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/touchscreen/ads7846.c
+++ drivers/input/touchscreen/ads7846.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * This code has been heavily tested on a Nokia 770, and lightly
  * tested on other ads7846 devices (OSK/Mistral, Lubbock).
+ * TSC2046 is just newer ads7846 silicon.
  * Support for ads7843 tested on Atmel at91sam926x-EK.
  * Support for ads7845 has only been stubbed in.
  *
