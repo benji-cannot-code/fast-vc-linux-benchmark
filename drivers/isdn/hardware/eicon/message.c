7f06b5614f2b83b11df60d00b90912299296a28b
Andrew Morton <akpm@linux-foundation.org>
2007-05-23 13:58:22 -0700

    drivers/isdn/hardware/eicon/message.c warning fixes
    Squash these:
    
    drivers/isdn/hardware/eicon/message.c: In function 'api_put':
    drivers/isdn/hardware/eicon/message.c:536: warning: cast from pointer to integer of different size
    drivers/isdn/hardware/eicon/message.c: In function 'plci_free_msg_in_queue':
    drivers/isdn/hardware/eicon/message.c:1035: warning: cast to pointer from integer of different size
    drivers/isdn/hardware/eicon/message.c: In function 'data_b3_req':
    drivers/isdn/hardware/eicon/message.c:3121: warning: cast to pointer from integer of different size
    drivers/isdn/hardware/eicon/message.c:3154: warning: cast to pointer from integer of different size
    drivers/isdn/hardware/eicon/message.c: In function 'callback':
    drivers/isdn/hardware/eicon/message.c:4060: warning: cast to pointer from integer of different size
    drivers/isdn/hardware/eicon/message.c: In function 'nl_ind':
    drivers/isdn/hardware/eicon/message.c:7137: warning: cast from pointer to integer of different size
    
    Cc: Karsten Keil <kkeil@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/isdn/hardware/eicon/message.c
+++ drivers/isdn/hardware/eicon/message.c
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-
 /*
  *
   Copyright (c) Eicon Networks, 2002.
@@ -534,7 +533,7 @@ word api_put(APPL   * appl, CAPI_MSG   * msg)
         if (m->header.command == _DATA_B3_R)
         {
 
-          m->info.data_b3_req.Data = (dword)(TransmitBufferSet (appl, m->info.data_b3_req.Data));
+          m->info.data_b3_req.Data = (dword)(long)(TransmitBufferSet (appl, m->info.data_b3_req.Data));
 
         }
 
@@ -1033,7 +1032,7 @@ static void plci_free_msg_in_queue (PLCI   *plci)
       {
 
         TransmitBufferFree (plci->appl,
-          (byte   *)(((CAPI_MSG   *)(&((byte   *)(plci->msg_in_queue))[i]))->info.data_b3_req.Data));
+          (byte *)(long)(((CAPI_MSG *)(&((byte *)(plci->msg_in_queue))[i]))->info.data_b3_req.Data));
 
       }
 
@@ -3119,7 +3118,7 @@ byte data_b3_req(dword Id, word Number, DIVA_CAPI_ADAPTER   * a, PLCI   * plci, 
        && (((byte   *)(parms[0].info)) < ((byte   *)(plci->msg_in_queue)) + sizeof(plci->msg_in_queue)))
       {
 
-        data->P = (byte   *)(*((dword   *)(parms[0].info)));
+        data->P = (byte *)(long)(*((dword *)(parms[0].info)));
 
       }
       else
@@ -3152,7 +3151,7 @@ byte data_b3_req(dword Id, word Number, DIVA_CAPI_ADAPTER   * a, PLCI   * plci, 
        && (((byte   *)(parms[0].info)) < ((byte   *)(plci->msg_in_queue)) + sizeof(plci->msg_in_queue)))
       {
 
-        TransmitBufferFree (appl, (byte   *)(*((dword   *)(parms[0].info))));
+        TransmitBufferFree (appl, (byte *)(long)(*((dword *)(parms[0].info))));
 
       }
     }
@@ -4058,7 +4057,7 @@ capi_callback_suffix:
     {
       if (m->header.command == _DATA_B3_R)
 
-        TransmitBufferFree (appl, (byte   *)(m->info.data_b3_req.Data));
+        TransmitBufferFree (appl, (byte *)(long)(m->info.data_b3_req.Data));
 
       dbug(1,dprintf("Error 0x%04x from msg(0x%04x)", i, m->header.command));
       break;
@@ -7135,7 +7134,7 @@ void nl_ind(PLCI   * plci)
   case N_UDATA:
     if (!(udata_forwarding_table[plci->NL.RBuffer->P[0] >> 5] & (1L << (plci->NL.RBuffer->P[0] & 0x1f))))
     {
-      plci->RData[0].P = plci->internal_ind_buffer + (-((int)(plci->internal_ind_buffer)) & 3);
+      plci->RData[0].P = plci->internal_ind_buffer + (-((int)(long)(plci->internal_ind_buffer)) & 3);
       plci->RData[0].PLength = INTERNAL_IND_BUFFER_SIZE;
       plci->NL.R = plci->RData;
       plci->NL.RNum = 1;
