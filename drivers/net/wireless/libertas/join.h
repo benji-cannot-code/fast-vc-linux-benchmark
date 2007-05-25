93c4d680f130c68f49f24896d86c81da27e4f630
Dan Williams <dcbw@redhat.com>
2007-05-25 17:09:41 -0400

    [PATCH] libertas: make association paths consistent
    The BSS to associate with (in either Infrastructure or IBSS join
    operations) is now stored in _one_ place in the association request (the
    bss member), not two places as before (pattemptedbss and
    curbssparams->bssdescriptor).
    
    Association requests are passed to the necessary association functions
    to (a) give them access to the bss member and (b) ensure that
    association/join/start setup uses settings from the request, not the
    current adapter settings (which may not be valid for the requested
    settings).
    
    Because the 'bss' member of the association request is used now, the
    command return functions from associate and adhoc join/start need access
    to the in-progress association request to update curbssparams when
    everything is done.  The association worker moves the request from
    pending to in-progress for the duration of the association attempt.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/join.h
+++ drivers/net/wireless/libertas/join.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _WLAN_JOIN_H
 
 #include "defs.h"
+#include "dev.h"
 
 struct cmd_ds_command;
 extern int libertas_cmd_80211_authenticate(wlan_private * priv,
@@ -22,7 +23,7 @@ extern int libertas_cmd_80211_ad_hoc_stop(wlan_private * priv,
 				       struct cmd_ds_command *cmd);
 extern int libertas_cmd_80211_ad_hoc_start(wlan_private * priv,
 					struct cmd_ds_command *cmd,
-					void *pssid);
+					void *pdata_buf);
 extern int libertas_cmd_80211_deauthenticate(wlan_private * priv,
 					  struct cmd_ds_command *cmd);
 extern int libertas_cmd_80211_associate(wlan_private * priv,
@@ -44,8 +45,9 @@ struct WLAN_802_11_SSID;
 struct bss_descriptor;
 
 extern int libertas_start_adhoc_network(wlan_private * priv,
-			     struct WLAN_802_11_SSID *adhocssid);
-extern int libertas_join_adhoc_network(wlan_private * priv, struct bss_descriptor *pbssdesc);
+			     struct assoc_request * assoc_req);
+extern int libertas_join_adhoc_network(wlan_private * priv,
+				struct assoc_request * assoc_req);
 extern int libertas_stop_adhoc_network(wlan_private * priv);
 
 extern int libertas_send_deauthentication(wlan_private * priv);
@@ -53,6 +55,6 @@ extern int libertas_send_deauth(wlan_private * priv);
 
 extern int libertas_do_adhocstop_ioctl(wlan_private * priv);
 
-int wlan_associate(wlan_private * priv, struct bss_descriptor * pbssdesc);
+int wlan_associate(wlan_private * priv, struct assoc_request * assoc_req);
 
 #endif
