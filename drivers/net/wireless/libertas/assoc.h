a5468ee8c277cfac8ed120169b8ea39d5aac4e08
Holger Schurig <hs4233@mail.mn-solutions.de>
2007-05-24 23:41:15 -0400

    [PATCH] libertas: rename wlan_association_worker
    Renames wlan_association_worker into libertas_association_worker
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/assoc.h
+++ drivers/net/wireless/libertas/assoc.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "dev.h"
 
-void wlan_association_worker(struct work_struct *work);
+void libertas_association_worker(struct work_struct *work);
 
 struct assoc_request * wlan_get_association_request(wlan_adapter *adapter);
 
