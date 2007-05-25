9a0ea188eccac40cc569a90884b12c0c310a68c4
Holger Schurig <hs4233@mail.mn-solutions.de>
2007-05-25 12:18:36 -0400

    [PATCH] libertas: move contents of fw.h to decl.h
    Also removes some useless "extern" declarations from function declaration.
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/main.c
+++ drivers/net/wireless/libertas/main.c
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "host.h"
 #include "decl.h"
 #include "dev.h"
-#include "fw.h"
 #include "wext.h"
 #include "debugfs.h"
 #include "assoc.h"
