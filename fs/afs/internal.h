21f655a4b415ad817be13826819195849617ccbc
Andrew Morton <akpm@linux-foundation.org>
2007-05-31 00:40:52 -0700

    afs: needs sched.h
    mips:
    
    fs/afs/flock.c: In function `afs_lock_may_be_available':
    fs/afs/flock.c:55: error: dereferencing pointer to incomplete type
    fs/afs/flock.c: In function `afs_lock_work':
    fs/afs/flock.c:84: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:89: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:109: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:135: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:143: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:158: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:161: error: dereferencing pointer to incomplete type
    fs/afs/flock.c:179: error: `TASK_UNINTERRUPTIBLE' undeclared (first use in this function)
    fs/afs/flock.c:179: error: (Each undeclared identifier is reported only once
    fs/afs/flock.c:179: error: for each function it appears in.)
    fs/afs/flock.c:179: error: `TASK_INTERRUPTIBLE' undeclared (first use in this function)
    fs/afs/flock.c:182: error: dereferencing pointer to incomplete type
    
    Cc: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/afs/internal.h
+++ fs/afs/internal.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/rxrpc.h>
 #include <linux/key.h>
 #include <linux/workqueue.h>
+#include <linux/sched.h>
+
 #include "afs.h"
 #include "afs_vl.h"
 
