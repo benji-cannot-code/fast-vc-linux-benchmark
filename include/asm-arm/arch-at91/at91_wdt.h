0133665b5e4da58f46c2ed6b7566683e58e31f19
Andrew Victor <andrew@sanpeople.com>
2007-05-31 10:16:00 +0100

    [ARM] 4421/1: AT91: Value of _KEY fields.
    Use the actual value (0xA5) for the AT91_SHDW_KEY and AT91_WDT_KEY
    register fields instead of a bitmask.
    
    This is consistent with how AT91_RSTC_KEY is defined, and is easier to
    use in code.
    
    Signed-off-by: Andrew Victor <andrew@sanpeople.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-at91/at91_wdt.h
+++ include/asm-arm/arch-at91/at91_wdt.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define AT91_WDT_CR		(AT91_WDT + 0x00)	/* Watchdog Control Register */
 #define		AT91_WDT_WDRSTT		(1    << 0)		/* Restart */
-#define		AT91_WDT_KEY		(0xff << 24)		/* KEY Password */
+#define		AT91_WDT_KEY		(0xa5 << 24)		/* KEY Password */
 
 #define AT91_WDT_MR		(AT91_WDT + 0x04)	/* Watchdog Mode Register */
 #define		AT91_WDT_WDV		(0xfff << 0)		/* Counter Value */
