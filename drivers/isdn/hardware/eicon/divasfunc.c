43d78d587dc13becd5f9f75fefc12c407e0e6600
Sam Ravnborg <sam@ravnborg.org>
2007-06-01 00:47:11 -0700

    isdn: fix section mismatch warnings
    Fix the following section mismatch warnings:
    
    WARNING: drivers/isdn/hardware/eicon/divadidd.o(.init.text+0xc4): Section mismatch: reference to .exit.text: (between 'init_module' and 'diddfunc_init')
    WARNING: drivers/isdn/hardware/eicon/divas.o(.init.text+0xf4): Section mismatch: reference to .exit.text:divasfunc_exit (between 'init_module' and 'divasfunc_init')
    WARNING: drivers/isdn/hardware/eicon/divas.o(.init.text+0x10d): Section mismatch: reference to .exit.text:divasfunc_exit (between 'init_module' and 'divasfunc_init')
    WARNING: drivers/isdn/hardware/eicon/divas.o(.init.text+0x148): Section mismatch: reference to .exit.text:divasfunc_exit (between 'init_module' and 'divasfunc_init')
    
    They all point to situation whare a function marked __init calls a function
    marked __exit - but the __exit section may have been discarded.
    
    Note: This warning is generated by a modified copy of modpost in my
          tree. It will soon hit upstearm.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Karsten Keil <kkeil@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/isdn/hardware/eicon/divasfunc.c
+++ drivers/isdn/hardware/eicon/divasfunc.c
@@ -232,7 +232,7 @@ int DIVA_INIT_FUNCTION divasfunc_init(int dbgmask)
 /*
  * exit
  */
-void DIVA_EXIT_FUNCTION divasfunc_exit(void)
+void divasfunc_exit(void)
 {
 	divasa_xdi_driver_unload();
 	disconnect_didd();
