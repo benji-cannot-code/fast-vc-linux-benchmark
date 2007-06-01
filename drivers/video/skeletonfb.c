7e9f16ecf6857e531353a6810ca1fde46a6f990f
Krzysztof Helt <krzysztof.h1@wp.pl>
2007-06-01 00:46:42 -0700

    skeletonfb: fix of xxxfb_setup ifdef
    This patch fixes wrong ifdef around the xxxfb_setup. It also moves this
    function to remove forward declaration.
    
    Signed-off-by: Krzysztof Helt <krzysztof.h1@wp.pl>
    Signed-off-by: Antonino Daplas <adaplas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/skeletonfb.c
+++ drivers/video/skeletonfb.c
@@ -133,7 +133,6 @@ static struct fb_info info;
 static struct xxx_par __initdata current_par;
 
 int xxxfb_init(void);
-int xxxfb_setup(char*);
 
 /**
  *	xxxfb_open - Optional function. Called when the framebuffer is
@@ -976,6 +975,21 @@ static struct platform_device xxxfb_device = {
 	.name = "xxxfb",
 };
 
+#ifndef MODULE
+    /*
+     *  Setup
+     */
+
+/*
+ * Only necessary if your driver takes special options,
+ * otherwise we fall back on the generic fb_setup().
+ */
+int __init xxxfb_setup(char *options)
+{
+    /* Parse user speficied options (`video=xxxfb:') */
+}
+#endif /* MODULE */
+
 static int __init xxxfb_init(void)
 {
 	int ret;
@@ -1006,21 +1020,6 @@ static void __exit xxxfb_exit(void)
 	driver_unregister(&xxxfb_driver);
 }
 #endif /* CONFIG_PCI */
-
-#ifdef MODULE
-    /*
-     *  Setup
-     */
-
-/* 
- * Only necessary if your driver takes special options,
- * otherwise we fall back on the generic fb_setup().
- */
-int __init xxxfb_setup(char *options)
-{
-    /* Parse user speficied options (`video=xxxfb:') */
-}
-#endif /* MODULE */
 
 /* ------------------------------------------------------------------------- */
 
