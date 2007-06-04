bd99d3fba02f3ea5947032c4b8f5eb63809fcee2
Franck Bui-Huu <fbuihuu@gmail.com>
2007-06-04 17:46:33 +0200

    [MIPS] Make PAGE_OFFSET aware of PHYS_OFFSET
    For platforms that use PHYS_OFFSET and do not use a mapped kernel,
    this patch automatically adds PHYS_OFFSET into PAGE_OFFSET.
    Therefore there are no more needs for them to redefine PAGE_OFFSET.
    
    For mapped kernel, they need to redefine PAGE_OFFSET anyways.
    
    Signed-off-by: Franck Bui-Huu <fbuihuu@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mach-generic/spaces.h
+++ include/asm-mips/mach-generic/spaces.h
@@ -73,7 +73,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This handles the memory map.
  */
 #ifndef PAGE_OFFSET
-#define PAGE_OFFSET		CAC_BASE
+#define PAGE_OFFSET		(CAC_BASE + PHYS_OFFSET)
 #endif
 
 #endif /* __ASM_MACH_GENERIC_SPACES_H */
