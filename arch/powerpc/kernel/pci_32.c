8ba17d7e242e081c03d4772aa8dbec249dcf95c5
York Sun <yorksun@freescale.com>
2007-06-04 11:56:42 -0500

    [POWERPC] Let subordinate transparent bridges be transparent.
    In pcibios_fixup_bus(), bridges that are subordinate
    to transparent bridges were still relocating their
    IORESOURCE_IO and IO_RESOURCE_MEM start and end values.
    
    Fix this by preventing the transparent bridge from
    relocating the start and end values, thus allowing the
    subordinate non-transparent bridge full molestation rights.
    
    Signed-off-by: York Sun <yorksun@freescale.com>
    Signed-off-by: Andy Fleming <afleming@freescale.com>
    Signed-off-by: Jon Loeliger <jdl@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/kernel/pci_32.c
+++ arch/powerpc/kernel/pci_32.c
@@ -1371,7 +1371,7 @@ void __init pcibios_fixup_bus(struct pci_bus *bus)
 		for (i = 0; i < 4; ++i) {
 			if ((res = bus->resource[i]) == NULL)
 				continue;
-			if (!res->flags)
+			if (!res->flags || bus->self->transparent)
 				continue;
 			if (io_offset && (res->flags & IORESOURCE_IO)) {
 				res->start += io_offset;
