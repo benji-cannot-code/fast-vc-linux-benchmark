0d8e7d07d3db786812eecce0aefde8695c32b9fd
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2007-06-04 15:15:56 +1000

    [POWERPC] Less ifdef's in signal.c/signal.h
    This patch moves things around a little bit in the new common signal.c
    and signal.h files to remove the last #ifdef in the middle of the
    common do_signal().
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/signal.c
+++ arch/powerpc/kernel/signal.c
@@ -17,19 +17,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "signal.h"
 
-
-#ifdef CONFIG_PPC64
-static inline int is_32bit_task(void)
-{
-	return test_thread_flag(TIF_32BIT);
-}
-#else
-static inline int is_32bit_task(void)
-{
-	return 1;
-}
-#endif
-
 /*
  * Allocate space for the signal frame
  */
@@ -162,10 +149,8 @@ int do_signal(sigset_t *oldset, struct pt_regs *regs)
 		else
 			ret = handle_signal32(signr, &ka, &info, oldset,
 					regs);
-#ifdef CONFIG_PPC64
 	} else {
 		ret = handle_rt_signal64(signr, &ka, &info, oldset, regs);
-#endif
 	}
 
 	if (ret) {
