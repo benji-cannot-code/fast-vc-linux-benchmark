f76852bc0a7a6e0891e2f7636ff6e8e3dcdc30e9
Markus Rechberger <markus.rechberger@amd.com>
2007-06-04 18:45:44 +0200

    firmware: remove orphaned Email
    Manuel Estrada Sainz passed away on May 9th 2004, his email account got
    deactivated. He was in charge of the firmware_class code, and still got
    CC'ed in recent discussions about it.
    
    Signed-off-by: Markus Rechberger <markus.rechberger@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- Documentation/firmware_class/firmware_sample_driver.c
+++ Documentation/firmware_class/firmware_sample_driver.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * firmware_sample_driver.c -
  *
- * Copyright (c) 2003 Manuel Estrada Sainz <ranty@debian.org>
+ * Copyright (c) 2003 Manuel Estrada Sainz
  *
  * Sample code on how to use request_firmware() from drivers.
  *
--- Documentation/firmware_class/firmware_sample_firmware_class.c
+++ Documentation/firmware_class/firmware_sample_firmware_class.c
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * firmware_sample_firmware_class.c -
  *
- * Copyright (c) 2003 Manuel Estrada Sainz <ranty@debian.org>
+ * Copyright (c) 2003 Manuel Estrada Sainz
  *
  * NOTE: This is just a probe of concept, if you think that your driver would
  * be well served by this mechanism please contact me first.
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/firmware.h>
 
 
-MODULE_AUTHOR("Manuel Estrada Sainz <ranty@debian.org>");
+MODULE_AUTHOR("Manuel Estrada Sainz");
 MODULE_DESCRIPTION("Hackish sample for using firmware class directly");
 MODULE_LICENSE("GPL");
 
