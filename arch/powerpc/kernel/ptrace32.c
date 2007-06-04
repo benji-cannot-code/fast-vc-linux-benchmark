f6d3cb61c30ad5ab5bbcf0a5fd1dd4023f8ca87b
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2007-06-04 15:15:47 +1000

    [POWERPC] ptrace shouldn't touch FP exec mode
    One of the gratuitous difference between 32 and 64-bit ptrace is
    whether you can whack the MSR:FE0 and FE1 bits from ptrace.  This
    patch forbids it unconditionally.  In addition, the 64-bit kernels
    used to return the exception mode in the MSR on reads, but 32-bit
    kernels didn't.  This patch makes it return those bits on both.
    
    Finally, since ptrace-ppc32.h and ptrace-ppc64.h are mostly empty now, and
    since the previous patch made ptrace32.c no longer need the MSR_DEBUGCHANGE
    definition, we just remove those 2 files and move back the remaining bits
    to ptrace.c (they were short lived heh ?).
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/ptrace32.c
+++ arch/powerpc/kernel/ptrace32.c
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/system.h>
 
-#include "ptrace-ppc64.h"
-
 /*
  * does not yet catch signals sent when the child dies.
  * in exit.c or in signal.c.
