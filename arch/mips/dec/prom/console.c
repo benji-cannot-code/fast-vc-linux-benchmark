828431f424f33ec274ad802bad9da16891e7d8ba
Maciej W. Rozycki <macro@linux-mips.org>
2007-06-05 11:45:07 +0100

    [MIPS] DECstation: Optimised early printk()
     This is an optimised implementation of early printk() for the DECstation.  
    After the recent conversion to a MIPS-specific generic routine using a 
    character-by-character output the performance dropped significantly.  
    This change reverts to the previous speed -- even at 9600 bps of the 
    serial console the difference is visible with a naked eye; I presume for a 
    framebuffer it is even worse (it may depend on exactly which one is used 
    though).
    
     Additionally the change includes a fix for a problem that the old 
    implementation had -- the format used would not actually limit the length 
    of the string output.  This new implementation uses a local buffer to deal 
    with it -- even with this additional copying it is much faster than the 
    generic function.
    
     Plus this driver is registered much earlier than the generic one, 
    allowing one to see critical messages, such as one about an incorrect CPU 
    setting used, that are produced beforehand. :-)
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/dec/prom/console.c
+++ arch/mips/dec/prom/console.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  *	DECstation PROM-based early console support.
  *
- *	Copyright (C) 2004  Maciej W. Rozycki
+ *	Copyright (C) 2004, 2007  Maciej W. Rozycki
  *
  *	This program is free software; you can redistribute it and/or
  *	modify it under the terms of the GNU General Public License
@@ -14,15 +14,35 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/string.h>
 
 #include <asm/dec/prom.h>
 
-void prom_putchar(char c)
+static void __init prom_console_write(struct console *con, const char *s,
+				      unsigned int c)
 {
-	char s[2];
+	char buf[81];
+	unsigned int chunk = sizeof(buf) - 1;
 
-	s[0] = c;
-	s[1] = '\0';
+	while (c > 0) {
+		if (chunk > c)
+			chunk = c;
+		memcpy(buf, s, chunk);
+		buf[chunk] = '\0';
+		prom_printf("%s", buf);
+		s += chunk;
+		c -= chunk;
+	}
+}
 
-	prom_printf( s);
+static struct console promcons __initdata = {
+	.name	= "prom",
+	.write	= prom_console_write,
+	.flags	= CON_BOOT | CON_PRINTBUFFER,
+	.index	= -1,
+};
+
+void __init register_prom_console(void)
+{
+	register_console(&promcons);
 }
