d107740cce231da6cd51ef400635451962fe2ae7
Michael Krufky <mkrufky@linuxtv.org>
2007-06-06 15:28:10 -0300

    V4L/DVB (5747): Tea5761: remove duplicated "#include <media/tuner.h>"
    The line, "#include <media/tuner.h>" appears twice.
    This patch removes the second occurance.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tea5761.c
+++ drivers/media/video/tea5761.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev.h>
 #include <linux/delay.h>
 #include <media/tuner.h>
-#include <media/tuner.h>
 
 #define PREFIX "TEA5761 "
 
