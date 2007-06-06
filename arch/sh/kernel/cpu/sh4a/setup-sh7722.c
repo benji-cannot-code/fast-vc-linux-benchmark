c073d65e529e5698ad137b207b5d739469ee5b13
Paul Mundt <lethal@linux-sh.org>
2007-06-06 19:05:11 +0900

    sh: Fix the SH7722 flatmem build.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/sh4a/setup-sh7722.c
+++ arch/sh/kernel/cpu/sh4a/setup-sh7722.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/serial.h>
 #include <linux/mm.h>
+#include <asm/mmzone.h>
 #include <asm/sci.h>
 
 static struct plat_sci_port sci_platform_data[] = {
