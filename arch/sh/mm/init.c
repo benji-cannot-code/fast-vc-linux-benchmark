37ced94d3561111af6ba7757ece34e49b1a9372b
Paul Mundt <lethal@linux-sh.org>
2007-06-06 12:23:06 +0900

    sh: Use asm/sections.h for linker section symbols.
    Kill off a bunch of externs, and use sections.h instead..
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/init.c
+++ arch/sh/mm/init.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mmu_context.h>
 #include <asm/tlb.h>
 #include <asm/cacheflush.h>
+#include <asm/sections.h>
 #include <asm/cache.h>
 
 DEFINE_PER_CPU(struct mmu_gather, mmu_gathers);
@@ -132,11 +133,6 @@ void __set_fixmap(enum fixed_addresses idx, unsigned long phys, pgprot_t prot)
 	set_pte_phys(address, phys, prot);
 }
 #endif	/* CONFIG_MMU */
-
-/* References to section boundaries */
-
-extern char _text, _etext, _edata, __bss_start, _end;
-extern char __init_begin, __init_end;
 
 /*
  * paging_init() sets up the page tables
@@ -252,8 +248,9 @@ void free_initmem(void)
 		free_page(addr);
 		totalram_pages++;
 	}
-	printk("Freeing unused kernel memory: %dk freed\n",
-	       (&__init_end - &__init_begin) >> 10);
+	printk("Freeing unused kernel memory: %ldk freed\n",
+	       ((unsigned long)&__init_end -
+	        (unsigned long)&__init_begin) >> 10);
 }
 
 #ifdef CONFIG_BLK_DEV_INITRD
