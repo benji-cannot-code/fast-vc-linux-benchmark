d094527bb0218306b1ac296caf2111bc268d820c
Eric Miao <eric.y.miao@gmail.com>
2007-06-06 06:45:18 +0100

    [ARM] 4438/1: PXA: remove #ifdef .. #endif from pxa_gpio_demux_handler()
    1. use GPIO_IRQ_mask[] to select those bits of interest, actually
       only those "unmasked" GPIO IRQs with their corresponding bits
       in GPIO_IRQ_mask[] set to "1" should be checked
    
    2. remove #ifdef PXA_LAST_GPIO > 96 .. #endif, GPIO_IRQ_mask[]
       is used to mask out the irrelevant bits, so that even though
       the GEDR3 on PXA25x is reserved, it will be masked, and the
       following code will never run. Another point is that GPIO85-
       GPIO95 bits within GEDR2 will also be masked out on PXA25x
    
    Signed-off-by: eric miao <eric.miao@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/irq.c
+++ arch/arm/mach-pxa/irq.c
@@ -251,7 +251,7 @@ static void pxa_gpio_demux_handler(unsigned int irq, struct irq_desc *desc)
 	do {
 		loop = 0;
 
-		mask = GEDR0 & ~3;
+		mask = GEDR0 & GPIO_IRQ_mask[0] & ~3;
 		if (mask) {
 			GEDR0 = mask;
 			irq = IRQ_GPIO(2);
@@ -267,7 +267,7 @@ static void pxa_gpio_demux_handler(unsigned int irq, struct irq_desc *desc)
 			loop = 1;
 		}
 
-		mask = GEDR1;
+		mask = GEDR1 & GPIO_IRQ_mask[1];
 		if (mask) {
 			GEDR1 = mask;
 			irq = IRQ_GPIO(32);
@@ -282,7 +282,7 @@ static void pxa_gpio_demux_handler(unsigned int irq, struct irq_desc *desc)
 			loop = 1;
 		}
 
-		mask = GEDR2;
+		mask = GEDR2 & GPIO_IRQ_mask[2];
 		if (mask) {
 			GEDR2 = mask;
 			irq = IRQ_GPIO(64);
@@ -297,8 +297,7 @@ static void pxa_gpio_demux_handler(unsigned int irq, struct irq_desc *desc)
 			loop = 1;
 		}
 
-#if PXA_LAST_GPIO >= 96
-		mask = GEDR3;
+		mask = GEDR3 & GPIO_IRQ_mask[3];
 		if (mask) {
 			GEDR3 = mask;
 			irq = IRQ_GPIO(96);
@@ -312,7 +311,6 @@ static void pxa_gpio_demux_handler(unsigned int irq, struct irq_desc *desc)
 			} while (mask);
 			loop = 1;
 		}
-#endif
 	} while (loop);
 }
 
