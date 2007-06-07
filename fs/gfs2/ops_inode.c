7d7aae2b959bad9c4062a134d3e0abf51bb3d74a
Steven Whitehouse <swhiteho@redhat.com>
2007-06-07 11:29:35 +0100

    [GFS2] Fix typo in rename of directories
    A typo caused us to pass a NULL pointer when renaming directories. It
    was accidentally introduced in: [GFS2] Clean up inode number handling
    
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/ops_inode.c
+++ fs/gfs2/ops_inode.c
@@ -750,7 +750,7 @@ static int gfs2_rename(struct inode *odir, struct dentry *odentry,
 		if (error)
 			goto out_end_trans;
 
-		error = gfs2_dir_mvino(ip, &name, nip, DT_DIR);
+		error = gfs2_dir_mvino(ip, &name, ndip, DT_DIR);
 		if (error)
 			goto out_end_trans;
 	} else {
