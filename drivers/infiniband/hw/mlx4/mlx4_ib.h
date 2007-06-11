df2b0c8a11eac43b15d74bf5b96ce52faedb9e5a
Michael S. Tsirkin <mst@dev.mellanox.co.il>
2007-06-11 08:56:10 +0300

    IB/mlx4: Include linux/mutex.h from mlx4_ib.h
    mlx4_ib.h uses struct mutex, so although <linux/mutex.h> seems to be
    pulled in indirectly by one of the headers it includes, the right
    thing is to include <linux/mutex.h> directly.
    
    Signed-off-by: Michael S. Tsirkin <mst@dev.mellanox.co.il>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/mlx4/mlx4_ib.h
+++ drivers/infiniband/hw/mlx4/mlx4_ib.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/compiler.h>
 #include <linux/list.h>
+#include <linux/mutex.h>
 
 #include <rdma/ib_verbs.h>
 #include <rdma/ib_umem.h>
