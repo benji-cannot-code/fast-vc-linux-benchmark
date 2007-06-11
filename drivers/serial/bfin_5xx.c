9a015e4f35cd7a0b7c52f83646ac9e007aa297d8
Mike Frysinger <michael.frysinger@analog.com>
2007-06-11 15:31:30 +0800

    Blackfin serial driver: ignore framing and parity errors
    if we get a break signal, we want to ignore framing and parity errors
    because those will always be set (by nature of the signal)
    
    Signed-off-by: Mike Frysinger <michael.frysinger@analog.com>
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- drivers/serial/bfin_5xx.c
+++ drivers/serial/bfin_5xx.c
@@ -186,6 +186,7 @@ static void bfin_serial_rx_chars(struct bfin_serial_port *uart)
 		uart->port.icount.brk++;
 		if (uart_handle_break(&uart->port))
 			goto ignore_char;
+		status &= ~(PE | FE);
 	}
 	if (status & PE)
 		uart->port.icount.parity++;
@@ -342,6 +343,7 @@ static void bfin_serial_dma_rx_chars(struct bfin_serial_port *uart)
 		uart->port.icount.brk++;
 		if (uart_handle_break(&uart->port))
 			goto dma_ignore_char;
+		status &= ~(PE | FE);
 	}
 	if (status & PE)
 		uart->port.icount.parity++;
