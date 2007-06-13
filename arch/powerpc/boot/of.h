c6a15d02c4e82ea28ca16ee71f50238d4e7b89b9
David Gibson <david@gibson.dropbear.id.au>
2007-06-13 14:52:54 +1000

    [POWERPC] Split low-level OF-related bootloader code into separate files
    Currently, all OF-related code in the bootloader is contained in of.c.
    of.c also provides the platform specific things necessary to boot on
    an OF platform.
    
    However, there are platforms (such as PReP) which can include an OF
    implementation, but are not bootable as pure OF systems.  For use by
    such platforms, this patch splits out the low-level parts of the OF
    code (call_prom() and various wrappers thereof) into a new oflib.c
    file.  In addition, the code related to bootwrapper console output via
    OF are moved to a new ofconsole.c file.  Both these files are included
    in the wrapper.a library where they can be used by both full-OF and
    partial OF platforms.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

---
+++ arch/powerpc/boot/of.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _PPC_BOOT_OF_H_
+#define _PPC_BOOT_OF_H_
+
+typedef void *phandle;
+typedef void *ihandle;
+
+void of_init(void *promptr);
+int of_call_prom(const char *service, int nargs, int nret, ...);
+void *of_claim(unsigned long virt, unsigned long size, unsigned long align);
+void of_exit(void);
+
+/* Console functions */
+void of_console_init(void);
+
+#endif /* _PPC_BOOT_OF_H_ */
