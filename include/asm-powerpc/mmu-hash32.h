b0dddf36a8d7fc3d5b13d36ceb2b3e4fb05625d2
David Gibson <david@gibson.dropbear.id.au>
2007-06-13 14:52:56 +1000

    [POWERPC] Kill typedef-ed structs for hash PTEs and BATs
    Using typedefs to rename structure types if frowned on by CodingStyle.
    However, we do so for the hash PTE structure on both ppc32 (where it's
    called "PTE") and ppc64 (where it's called "hpte_t").  On ppc32 we
    also have such a typedef for the BATs ("BAT").
    
    This removes this unhelpful use of typedefs, in the process
    bringing ppc32 and ppc64 closer together, by using the name "struct
    hash_pte" in both cases.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/mmu-hash32.h
+++ include/asm-powerpc/mmu-hash32.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BPP_RW	0x02		/* Read/write */
 
 #ifndef __ASSEMBLY__
-typedef struct _BAT {
+struct ppc_bat {
 	struct {
 		unsigned long bepi:15;	/* Effective page index (virtual address) */
 		unsigned long :4;	/* Unused */
@@ -47,7 +47,7 @@ typedef struct _BAT {
 		unsigned long :1;	/* Unused */
 		unsigned long pp:2;	/* Page access protections */
 	} batl;			/* Lower register */
-} BAT;
+};
 #endif /* !__ASSEMBLY__ */
 
 /*
@@ -63,7 +63,7 @@ typedef struct _BAT {
 #ifndef __ASSEMBLY__
 
 /* Hardware Page Table Entry */
-typedef struct _PTE {
+struct hash_pte {
 	unsigned long v:1;	/* Entry is valid */
 	unsigned long vsid:24;	/* Virtual segment identifier */
 	unsigned long h:1;	/* Hash algorithm indicator */
@@ -78,7 +78,7 @@ typedef struct _PTE {
 	unsigned long g:1;	/* Guarded */
 	unsigned long  :1;	/* Unused */
 	unsigned long pp:2;	/* Page protection */
-} PTE;
+};
 
 typedef struct {
 	unsigned long id;
