d1f3c32a5204856c9e413999ace7401f2f80a0e6
Thomas Bogendoerfer <tsbogend@alpha.franken.de>
2007-06-13 12:58:53 -0700

    [SCSI] ESP: Don't forget to clear ESP_FLAG_RESETTING.
    esp_reset_cleanup() does everything necessary except clear
    the flag, so we never exit resetting state.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/esp_scsi.c
+++ drivers/scsi/esp_scsi.c
@@ -2034,6 +2034,7 @@ static void esp_reset_cleanup(struct esp *esp)
 			starget_for_each_device(tp->starget, NULL,
 						esp_clear_hold);
 	}
+	esp->flags &= ~ESP_FLAG_RESETTING;
 }
 
 /* Runs under host->lock */
