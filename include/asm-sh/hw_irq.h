da222ea8ecdfdeca8af91c8d831a39be413a68cb
Magnus Damm <damm@igel.co.jp>
2007-06-15 10:41:54 +0900

    sh: rework intc2 code
    The shared intc2 code currently contains cpu-specific #ifdefs.
    This is a tad unclean and it prevents us from using the shared code
    to drive board-specific irqs on the se7780 board.
    
    This patch reworks the intc2 code by moving the base addresses of
    the intc2 registers into struct intc2_desc. This new structure also
    contains the name of the controller in struct irq_chip. The idea
    behind putting struct irq_chip in there is that we can use offsetof()
    to locate the base addresses in the irq_chip callbacks.
    
    One logic change has been made - the original shared intc2 code
    enabled the interrupts by default but with this patch they are all
    disabled by default.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/hw_irq.h
+++ include/asm-sh/hw_irq.h
@@ -6,4 +6,23 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern atomic_t irq_err_count;
 
+struct intc2_data {
+	unsigned short irq;
+	unsigned char ipr_offset, ipr_shift;
+	unsigned char msk_offset, msk_shift;
+	unsigned char priority;
+};
+
+struct intc2_desc {
+	unsigned long prio_base;
+	unsigned long msk_base;
+	unsigned long mskclr_base;
+	struct intc2_data *intc2_data;
+	unsigned int nr_irqs;
+	struct irq_chip chip;
+};
+
+void register_intc2_controller(struct intc2_desc *);
+void init_IRQ_intc2(void);
+
 #endif /* __ASM_SH_HW_IRQ_H */
