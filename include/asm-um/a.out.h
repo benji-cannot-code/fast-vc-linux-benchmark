e8ff331e29c4cfa13ebffa7d0e8726d0e13586f8
Jeff Dike <jdike@addtoit.com>
2007-06-16 10:16:10 -0700

    uml: kill x86_64 STACK_TOP_MAX
    The x86_64 a.out.h got a definition of STACK_TOP_MAX, which interferes with
    the UML version.  So, just undef it like STACK_TOP.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-um/a.out.h
+++ include/asm-um/a.out.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "choose-mode.h"
 
 #undef STACK_TOP
+#undef STACK_TOP_MAX
 
 extern unsigned long stacksizelim;
 
