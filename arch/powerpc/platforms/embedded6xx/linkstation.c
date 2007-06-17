99fedc267aa652258b4fdbdb91572e21fb2a2de1
Arnd Bergmann <arnd@arndb.de>
2007-06-18 01:06:57 +0200

    [POWERPC] fix building without PCI
    Some code looks can be configured to be built without
    PCI support, but does not work properly.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/powerpc/platforms/embedded6xx/linkstation.c
+++ arch/powerpc/platforms/embedded6xx/linkstation.c
@@ -57,6 +57,7 @@ static struct mtd_partition linkstation_physmap_partitions[] = {
 
 static int __init linkstation_add_bridge(struct device_node *dev)
 {
+#ifdef CONFIG_PCI
 	int len;
 	struct pci_controller *hose;
 	const int *bus_range;
@@ -79,7 +80,7 @@ static int __init linkstation_add_bridge(struct device_node *dev)
 	/* Interpret the "ranges" property */
 	/* This also maps the I/O region and sets isa_io/mem_base */
 	pci_process_bridge_OF_ranges(hose, dev, 1);
-
+#endif
 	return 0;
 }
 
