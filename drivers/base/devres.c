bc60315ae0f2a101ab89a9c8edcdf7c38caf83f9
Adrian Bunk <bunk@stusta.de>
2007-06-18 01:42:54 +0200

    Driver core: fix devres_release_all() return value
    Every file should include the headers containing the prototypes for
    it's global functions.
    
    Since the GNU C compiler is now able to detect that the function 
    prototype of devres_release_all() in the header and the actual function 
    disagree regarding the return value, this patch also fixes this bug.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Acked-by: Tejun Heo <htejun@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/devres.c
+++ drivers/base/devres.c
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/device.h>
 #include <linux/module.h>
 
+#include "base.h"
+
 struct devres_node {
 	struct list_head		entry;
 	dr_release_t			release;
