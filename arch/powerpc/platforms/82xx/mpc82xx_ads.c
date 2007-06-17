fc626cc9efb7ad0f56cd922b07d00a3e6def7723
Arnd Bergmann <arnd@arndb.de>
2007-06-18 01:06:55 +0200

    [POWERPC] mpc82xx_ads build fix
    needed for 6xx allyesconfig
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/powerpc/platforms/82xx/mpc82xx_ads.c
+++ arch/powerpc/platforms/82xx/mpc82xx_ads.c
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs_enet_pd.h>
 
 #include <sysdev/fsl_soc.h>
-#include <../sysdev/cpm2_pic.h>
+#include <sysdev/cpm2_pic.h>
 
 #include "pq2ads.h"
 
