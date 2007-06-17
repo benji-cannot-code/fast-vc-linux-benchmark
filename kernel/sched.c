fb012f0157a3ebf5321b4db31c497961d7872874
Ingo Molnar <mingo@elte.hu>
2007-06-17 18:37:45 +0200

    sched: fix SysRq-N (normalize RT tasks)
    Gene Heskett reported the following problem while testing CFS: SysRq-N
    is not always effective in normalizing tasks back to SCHED_OTHER.
    
    The reason for that turns out to be the following bug:
    
     - normalize_rt_tasks() uses for_each_process() to iterate through all
       tasks in the system.  The problem is, this method does not iterate
       through all tasks, it iterates through all thread groups.
    
    The proper mechanism to enumerate over all threads is to use a
    do_each_thread() + while_each_thread() loop.
    
    Reported-by: Gene Heskett <gene.heskett@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -7072,12 +7072,13 @@ EXPORT_SYMBOL(__might_sleep);
 void normalize_rt_tasks(void)
 {
 	struct prio_array *array;
-	struct task_struct *p;
+	struct task_struct *g, *p;
 	unsigned long flags;
 	struct rq *rq;
 
 	read_lock_irq(&tasklist_lock);
-	for_each_process(p) {
+
+	do_each_thread(g, p) {
 		if (!rt_task(p))
 			continue;
 
@@ -7095,7 +7096,8 @@ void normalize_rt_tasks(void)
 
 		__task_rq_unlock(rq);
 		spin_unlock_irqrestore(&p->pi_lock, flags);
-	}
+	} while_each_thread(g, p);
+
 	read_unlock_irq(&tasklist_lock);
 }
 
