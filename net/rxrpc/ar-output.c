39ee8e6f2fcc3c76669c383e86d486ebaaaaae0c
David Howells <dhowells@redhat.com>
2007-06-18 23:30:41 -0700

    [AF_RXRPC]: Return the number of bytes buffered in rxrpc_send_data()
    Return the number of bytes buffered in rxrpc_send_data().
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/rxrpc/ar-output.c
+++ net/rxrpc/ar-output.c
@@ -641,6 +641,7 @@ static int rxrpc_send_data(struct kiocb *iocb,
 			goto efault;
 		sp->remain -= copy;
 		skb->mark += copy;
+		copied += copy;
 
 		len -= copy;
 		segment -= copy;
@@ -710,6 +711,8 @@ static int rxrpc_send_data(struct kiocb *iocb,
 
 	} while (segment > 0);
 
+success:
+	ret = copied;
 out:
 	call->tx_pending = skb;
 	_leave(" = %d", ret);
@@ -726,7 +729,7 @@ call_aborted:
 
 maybe_error:
 	if (copied)
-		ret = copied;
+		goto success;
 	goto out;
 
 efault:
