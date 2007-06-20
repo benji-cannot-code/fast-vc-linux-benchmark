202f6189842ec2f175c05ecd70c7c442adfe1437
MUNEDA Takahiro <muneda.takahiro@jp.fujitsu.com>
2007-06-20 13:00:00 +0900

    [IA64] remove duplicate header include line
    Remove duplicate header include line from arch/ia64/kernel/time.c.
    
    Signed-off-by: MUNEDA Takahiro <muneda.takahiro@jp.fujitsu.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/time.c
+++ arch/ia64/kernel/time.c
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/time.h>
 #include <linux/interrupt.h>
 #include <linux/efi.h>
-#include <linux/profile.h>
 #include <linux/timex.h>
 
 #include <asm/machvec.h>
