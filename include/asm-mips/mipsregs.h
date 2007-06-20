d85b8a4319af9ccbb3a59114840cf1948823b4a6
Ralf Baechle <ralf@linux-mips.org>
2007-06-21 00:22:34 +0100

    [MIPS] Fix scheduling latency issue on 24K, 34K and 74K cores
    The idle loop goes to sleep using the WAIT instruction if !need_resched().
    This has is suffering from from a race condition that if if just after
    need_resched has returned 0 an interrupt might set TIF_NEED_RESCHED but
    we've just completed the test so go to sleep anyway.  This would be
    trivial to fix by just disabling interrupts during that sequence as in:
    
            local_irq_disable();
            if (!need_resched())
                    __asm__("wait");
            local_irq_enable();
    
    but the processor architecture leaves it undefined if a processor calling
    WAIT with interrupts disabled will ever restart its pipeline and indeed
    some processors have made use of the freedom provided by the architecture
    definition.  This has been resolved and the Config7.WII bit indicates that
    the use of WAIT is safe on 24K, 24KE and 34K cores.  It also is safe on
    74K starting revision 2.1.0 so enable the use of WAIT with interrupts
    disabled for 74K based on a c0_prid of at least that.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/mipsregs.h
+++ include/asm-mips/mipsregs.h
@@ -535,6 +535,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MIPS_CONF3_LPA		(_ULCAST_(1) <<  7)
 #define MIPS_CONF3_DSP		(_ULCAST_(1) << 10)
 
+#define MIPS_CONF7_WII		(_ULCAST_(1) << 31)
+
 /*
  * Bits in the MIPS32/64 coprocessor 1 (FPU) revision register.
  */
