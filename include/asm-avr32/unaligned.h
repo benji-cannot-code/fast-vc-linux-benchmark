9180d6cb0b54f91fb79cbd8c96efe6ffce0e5d4e
Haavard Skinnemoen <hskinnemoen@atmel.com>
2007-06-20 15:29:15 +0200

    [AVR32] Remove optimization of unaligned word loads
    If we let unaligned word loads bypass the generic unaligned handling,
    gcc may combine it with a swap.b instruction and turn it into a ldwsp
    instruction, which does not work with unaligned addresses.
    
    Revert the optimization to prevent the RNDIS driver from crashing.
    Hopefully we'll figure something out later (it may be better to do the
    optimization in gcc.)
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>

--- include/asm-avr32/unaligned.h
+++ include/asm-avr32/unaligned.h
@@ -7,31 +7,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * implementation.  The AVR32 AP implementation can handle unaligned
  * words, but halfwords must be halfword-aligned, and doublewords must
  * be word-aligned.
+ *
+ * However, swapped word loads must be word-aligned so we can't
+ * optimize word loads in general.
  */
 
 #include <asm-generic/unaligned.h>
-
-#ifdef CONFIG_CPU_AT32AP7000
-
-/* REVISIT calling memmove() may be smaller for 64-bit values ... */
-
-#undef get_unaligned
-#define get_unaligned(ptr) \
-	___get_unaligned(ptr, sizeof((*ptr)))
-#define ___get_unaligned(ptr, size) \
-	((size == 4) ? *(ptr) : __get_unaligned(ptr, size))
-
-#undef put_unaligned
-#define put_unaligned(val, ptr) \
-	___put_unaligned((__u64)(val), ptr, sizeof((*ptr)))
-#define ___put_unaligned(val, ptr, size)		\
-do {							\
-	if (size == 4)					\
-		*(ptr) = (val);				\
-	else						\
-		__put_unaligned(val, ptr, size);	\
-} while (0)
-
-#endif
 
 #endif /* __ASM_AVR32_UNALIGNED_H */
