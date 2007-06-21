902fc65698e3fe9a76b677f7589f7bc2f6211eb8
Johannes Berg <johannes@sipsolutions.net>
2007-06-22 00:16:14 +0200

    [PATCH] mac80211: regdomain.c needs to include ieee80211_i.h
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/regdomain.c
+++ net/mac80211/regdomain.c
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/netdevice.h>
 #include <net/mac80211.h>
+#include "ieee80211_i.h"
 
 static int ieee80211_regdom = 0x10; /* FCC */
 module_param(ieee80211_regdom, int, 0444);
