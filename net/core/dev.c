9510f63ef2dd4b35e6d2c696a2171c82770ac357
Shannon Nelson <shannon.nelson@intel.com>
2007-06-23 23:09:23 -0700

    [NET]: Re-enable irqs before pushing pending DMA requests
    This moves the local_irq_enable() call in net_rx_action() to before
    calling the CONFIG_NET_DMA's dma_async_memcpy_issue_pending() rather
    than after.  This shortens the irq disabled window and allows for DMA
    drivers that need to do their own irq hold.
    
    Signed-off-by: Shannon Nelson <shannon.nelson@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2010,6 +2010,7 @@ static void net_rx_action(struct softirq_action *h)
 		}
 	}
 out:
+	local_irq_enable();
 #ifdef CONFIG_NET_DMA
 	/*
 	 * There may not be any more sk_buffs coming right now, so push
@@ -2023,7 +2024,6 @@ out:
 		rcu_read_unlock();
 	}
 #endif
-	local_irq_enable();
 	return;
 
 softnet_break:
