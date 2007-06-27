e3140185982eddf635194c7e491892fc1c4ff43e
Michael Ellerman <michael@ellerman.id.au>
2007-06-27 16:02:55 +1000

    [POWERPC] Turn off debugging in arch/powerpc/kernel/pci_64.c
    Commit 3d5134ee8341bffc4f539049abb9e90d469b448d left debugging turned on
    in arch/powerpc/kernel/pci_64.c.  This turns it off again.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/pci_64.c
+++ arch/powerpc/kernel/pci_64.c
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *      2 of the License, or (at your option) any later version.
  */
 
-#define DEBUG
+#undef DEBUG
 
 #include <linux/kernel.h>
 #include <linux/pci.h>
