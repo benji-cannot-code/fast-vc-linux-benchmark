bbcc79662dcaf5b407602b77fea51a38ce577509
Prakash, Sathya <sathya.prakash@lsi.com>
2007-07-02 17:04:10 +0530

    [SCSI] mpt fusion: deregister from transport layer if PCI registration failed
    The mptspi and mptsas drivers are modified to deregister from transport layer
    if registration with PCI driver failed
    
    Signed-off-by: Sathya Prakash <sathya.prakash@lsi.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/message/fusion/mptspi.c
+++ drivers/message/fusion/mptspi.c
@@ -1525,6 +1525,8 @@ static struct pci_driver mptspi_driver = {
 static int __init
 mptspi_init(void)
 {
+	int error;
+
 	show_mptmod_ver(my_NAME, my_VERSION);
 
 	mptspi_transport_template = spi_attach_transport(&mptspi_transport_functions);
@@ -1545,7 +1547,11 @@ mptspi_init(void)
 		  ": Registered for IOC reset notifications\n"));
 	}
 
-	return pci_register_driver(&mptspi_driver);
+	error = pci_register_driver(&mptspi_driver);
+	if (error)
+		spi_release_transport(mptspi_transport_template);
+
+	return error;
 }
 
 /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
