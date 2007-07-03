c0c4a7e1e9feec74ef0d081426a5c29388a6e780
Sergei Shtylyov <sshtylyov@ru.mvista.com>
2007-07-03 22:28:35 +0200

    hpt366: blacklist MAXTOR STM3320620A for UltraDMA/66
    Add the MAXTOR STM3320620A drive into the UltraDMA/66 mode blacklist
    for the HPT36x chips.
    
    Signed-off-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Acked-by: Linas Vepstas <linas@austin.ibm.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/hpt366.c
+++ drivers/ide/pci/hpt366.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/drivers/ide/pci/hpt366.c		Version 1.04	Jun 4, 2007
+ * linux/drivers/ide/pci/hpt366.c		Version 1.05	Jun 26, 2007
  *
  * Copyright (C) 1999-2003		Andre Hedrick <andre@linux-ide.org>
  * Portions Copyright (C) 2001	        Sun Microsystems, Inc.
@@ -183,6 +183,7 @@ static const char *bad_ata66_4[] = {
 	"IC35L040AVER07-0",
 	"IC35L060AVER07-0",
 	"WDC AC310200R",
+	"MAXTOR STM3320620A",
 	NULL
 };
 
