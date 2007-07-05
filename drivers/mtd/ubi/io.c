5b84c5dff96d2a876f43942eb2ba598781df536b
Brijesh Singh <brijesh.s.singh@gmail.com>
2007-07-05 15:07:35 +0530

    UBI: fix signed-unsigned multiplication
    There is signed multiplication assigned to unsigned ei.addr in io.c.
    This causes wrong addresses for big multiplication.This patch solves the
    problem.
    
    Signed-off-by: Brijesh Singh <brijesh.s.singh@gmail.com>
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/io.c
+++ drivers/mtd/ubi/io.c
@@ -299,7 +299,7 @@ retry:
 	memset(&ei, 0, sizeof(struct erase_info));
 
 	ei.mtd      = ubi->mtd;
-	ei.addr     = pnum * ubi->peb_size;
+	ei.addr     = (loff_t)pnum * ubi->peb_size;
 	ei.len      = ubi->peb_size;
 	ei.callback = erase_callback;
 	ei.priv     = (unsigned long)&wq;
