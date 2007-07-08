2c842d438cf090a58ce263a42c221339965d30f8
Patrick McHardy <kaber@trash.net>
2007-07-07 22:36:46 -0700

    [NETFILTER]: nf_conntrack_helper: use hashtable for conntrack helpers
    Eliminate the last global list searched for every new connection.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netfilter/nf_conntrack_helper.h
+++ include/net/netfilter/nf_conntrack_helper.h
@@ -16,8 +16,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct module;
 
 struct nf_conntrack_helper
-{	
-	struct list_head list; 		/* Internal use. */
+{
+	struct hlist_node hnode;	/* Internal use. */
 
 	const char *name;		/* name of the module */
 	struct module *me;		/* pointer to self */
