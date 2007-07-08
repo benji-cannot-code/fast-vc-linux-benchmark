f693f6a44b96793157480c61fc728a7d185e78e9
Yasuyuki Kozakai <yasuyuki.kozakai@toshiba.co.jp>
2007-07-07 22:25:51 -0700

    [NETFILTER]: nf_conntrack: remove old memory allocator of conntrack
    Now memory space for help and NAT are allocated by extension
    infrastructure.
    
    Signed-off-by: Yasuyuki Kozakai <yasuyuki.kozakai@toshiba.co.jp>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netfilter/nf_conntrack.h
+++ include/net/netfilter/nf_conntrack.h
@@ -118,9 +118,6 @@ struct nf_conn
 	/* Unique ID that identifies this conntrack*/
 	unsigned int id;
 
-	/* features - nat, helper, ... used by allocating system */
-	u_int32_t features;
-
 #if defined(CONFIG_NF_CONNTRACK_MARK)
 	u_int32_t mark;
 #endif
@@ -134,9 +131,6 @@ struct nf_conn
 
 	/* Extensions */
 	struct nf_ct_ext *ext;
-
-	/* features dynamically at the end: helper, nat (both optional) */
-	char data[0];
 };
 
 static inline struct nf_conn *
@@ -265,14 +259,6 @@ do {							\
 	__get_cpu_var(nf_conntrack_stat).count++;	\
 	local_bh_enable();				\
 } while (0)
-
-/* no helper, no nat */
-#define	NF_CT_F_BASIC	0
-/* for helper */
-#define	NF_CT_F_HELP	1
-/* for nat. */
-#define	NF_CT_F_NAT	2
-#define NF_CT_F_NUM	4
 
 extern int
 nf_conntrack_register_cache(u_int32_t features, const char *name, size_t size);
