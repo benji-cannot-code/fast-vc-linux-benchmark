15cd06d3fc75b13f193fbe7a99df8e5d8d169efe
Domen Puncer <domen.puncer@telargo.com>
2007-07-09 09:52:03 +0200

    [POWERPC] 52xx: sparse fixes
    sparse caught these static functions / __iomem annotations
    under arch/powerpc/platform/52xx/
    
    Signed-off-by: Domen Puncer <domen.puncer@telargo.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/52xx/efika.c
+++ arch/powerpc/platforms/52xx/efika.c
@@ -84,7 +84,7 @@ static struct pci_ops rtas_pci_ops = {
 };
 
 
-void __init efika_pcisetup(void)
+static void __init efika_pcisetup(void)
 {
 	const int *bus_range;
 	int len;
@@ -145,7 +145,7 @@ void __init efika_pcisetup(void)
 }
 
 #else
-void __init efika_pcisetup(void)
+static void __init efika_pcisetup(void)
 {}
 #endif
 
