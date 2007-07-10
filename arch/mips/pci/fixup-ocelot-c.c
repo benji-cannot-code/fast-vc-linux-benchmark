2c9f66aeb7117163e846aab6382408fe9f9a4d74
Ralf Baechle <ralf@linux-mips.org>
2007-07-10 17:33:00 +0100

    [MIPS] PCI: Make dev pointer argument of pcibios_map_irq const.
    This is to break the code of people who think they are supposed to scribble
    into the pci device structure - it's off limits.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/pci/fixup-ocelot-c.c
+++ arch/mips/pci/fixup-ocelot-c.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-int __init pcibios_map_irq(struct pci_dev *dev, u8 slot, u8 pin)
+int __init pcibios_map_irq(const struct pci_dev *dev, u8 slot, u8 pin)
 {
 	int bus = dev->bus->number;
 
