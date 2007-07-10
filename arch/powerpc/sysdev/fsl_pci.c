8525f8ecd0ab03947b6fc5fd6c5f2e2a4010befb
Roy Zang <tie-fei.zang@freescale.com>
2007-07-10 18:44:34 +0800

    [POWERPC] Create common fsl pci/e files based on 86xx platforms
    Move
    arch/powerpc/platforms/86xx/pci.c -> arch/powerpc/sysdev/fsl_pci.c
    arch/powerpc/sysdev/fsl_pcie.h -> arch/powerpc/sysdev/fsl_pci.h
    as the base to unify 83xx/85xx/86xx pci and pcie.
    
    Add CONFIG_FSL_PCI to build fsl_pci.c for Freescale pci and pcie option.
    The code still works for 86xx platforms.
    
    Signed-off-by: Roy Zang <tie-fei.zang@freescale.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/sysdev/fsl_pci.c
+++ arch/powerpc/sysdev/fsl_pci.c
@@ -25,9 +25,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/prom.h>
 #include <asm/pci-bridge.h>
 #include <sysdev/fsl_soc.h>
-#include <sysdev/fsl_pcie.h>
+#include <sysdev/fsl_pci.h>
 
-#include "mpc86xx.h"
+#include "../platforms/86xx/mpc86xx.h"
 
 #undef DEBUG
 
