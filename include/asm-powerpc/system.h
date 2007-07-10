912dcb113f50128a44c565bdce220cc9a44403ee
Kumar Gala <galak@kernel.crashing.org>
2007-07-09 23:49:09 -0500

    [POWERPC] Move inline asm eieio to using eieio inline function
    Use the eieio function so we can redefine what eieio does rather
    than direct inline asm.  This is part code clean up and partially
    because not all PPCs have eieio (book-e has mbar that maps to eieio).
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- include/asm-powerpc/system.h
+++ include/asm-powerpc/system.h
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef CONFIG_SMP
 #define smp_mb()	mb()
 #define smp_rmb()	rmb()
-#define smp_wmb()	__asm__ __volatile__ ("eieio" : : : "memory")
+#define smp_wmb()	eieio()
 #define smp_read_barrier_depends()	read_barrier_depends()
 #else
 #define smp_mb()	barrier()
