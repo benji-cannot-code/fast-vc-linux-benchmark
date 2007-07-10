326a68d022de0ef4c9a1565f4a27f3a7cf0aa22b
Johannes Berg <johannes@sipsolutions.net>
2007-07-10 19:32:09 +0200

    [PATCH] mac80211: kill antenna select ioctls
    Not used anywhere.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Jiri Benc <jbenc@suse.cz>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/hostapd_ioctl.h
+++ net/mac80211/hostapd_ioctl.h
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * mess shall be deleted completely. */
 enum {
 	PRISM2_PARAM_IEEE_802_1X = 23,
-	PRISM2_PARAM_ANTSEL_TX = 24,
-	PRISM2_PARAM_ANTSEL_RX = 25,
 
 	/* Instant802 additions */
 	PRISM2_PARAM_CTS_PROTECT_ERP_FRAMES = 1001,
