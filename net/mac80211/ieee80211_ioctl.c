c5631a228c7dfa939b9b3899f266ed91558f6983
Johannes Berg <johannes@sipsolutions.net>
2007-07-10 19:32:09 +0200

    [PATCH] mac80211: remove PRISM2_PARAM_DROP_UNENCRYPTED ioctl
    Interestingly, wpa_supplicant doesn't use it, but uses the
    currently unsupported IW_AUTH_DROP_UNENCRYPTED. So I guess
    it doesn't matter anyway.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: Jiri Benc <jbenc@suse.cz>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/ieee80211_ioctl.c
+++ net/mac80211/ieee80211_ioctl.c
@@ -1202,10 +1202,6 @@ static int ieee80211_ioctl_prism2_param(struct net_device *dev,
 		local->cts_protect_erp_frames = value;
 		break;
 
-	case PRISM2_PARAM_DROP_UNENCRYPTED:
-		sdata->drop_unencrypted = value;
-		break;
-
 	case PRISM2_PARAM_PREAMBLE:
 		local->short_preamble = value;
 		break;
@@ -1331,10 +1327,6 @@ static int ieee80211_ioctl_get_prism2_param(struct net_device *dev,
 
 	case PRISM2_PARAM_CTS_PROTECT_ERP_FRAMES:
 		*param = local->cts_protect_erp_frames;
-		break;
-
-	case PRISM2_PARAM_DROP_UNENCRYPTED:
-		*param = sdata->drop_unencrypted;
 		break;
 
 	case PRISM2_PARAM_PREAMBLE:
