7d236548b7c4761081dc0c16ef17cd03ec04a36b
Dave Airlie <airlied@linux.ie>
2007-07-11 12:05:36 +1000

    drm: cleanup list initialisation
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/drm_stub.c
+++ drivers/char/drm/drm_stub.c
@@ -61,6 +61,10 @@ static int drm_fill_in_dev(drm_device_t * dev, struct pci_dev *pdev,
 	int retcode;
 
 	INIT_LIST_HEAD(&dev->filelist);
+	INIT_LIST_HEAD(&dev->ctxlist);
+	INIT_LIST_HEAD(&dev->vmalist);
+	INIT_LIST_HEAD(&dev->maplist);
+
 	spin_lock_init(&dev->count_lock);
 	spin_lock_init(&dev->drw_lock);
 	spin_lock_init(&dev->tasklet_lock);
@@ -81,8 +85,6 @@ static int drm_fill_in_dev(drm_device_t * dev, struct pci_dev *pdev,
 	if (drm_ht_create(&dev->map_hash, 12)) {
 		return -ENOMEM;
 	}
-
-	INIT_LIST_HEAD(&dev->maplist);
 
 	/* the DRM has 6 basic counters */
 	dev->counters = 6;
