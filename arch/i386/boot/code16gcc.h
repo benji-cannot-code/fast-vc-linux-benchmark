45ba24f57068f793292df3bc163c559e6d2dc591
H. Peter Anvin <hpa@zytor.com>
2007-07-11 12:18:38 -0700

    Header file to produce 16-bit code with gcc
    gcc for i386 can be used with the assembly prefix ".code16gcc" to generate
    16-bit (real-mode) code.  This header file provides the assembly prefix.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

---
+++ arch/i386/boot/code16gcc.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+/*
+ * code16gcc.h
+ *
+ * This file is -include'd when compiling 16-bit C code.
+ * Note: this asm() needs to be emitted before gcc omits any code.
+ * Depending on gcc version, this requires -fno-unit-at-a-time or
+ * -fno-toplevel-reorder.
+ *
+ * Hopefully gcc will eventually have a real -m16 option so we can
+ * drop this hack long term.
+ */
+
+#ifndef __ASSEMBLY__
+asm(".code16gcc");
+#endif
