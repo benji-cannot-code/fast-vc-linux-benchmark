be79990e5f91d94267a68736d6283831d6188b6a
Robert P. J. Day <rpjday@mindspring.com>
2007-07-11 17:04:02 -0400

    [MTD] Remove useless references to MIPS_HYDROGEN3 and MIPS_MIRAGE_WHY.
    Signed-off-by: Robert P. J. Day <rpjday@mindspring.com>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- drivers/mtd/maps/alchemy-flash.c
+++ drivers/mtd/maps/alchemy-flash.c
@@ -76,13 +76,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BOARD_FLASH_WIDTH 2 /* 16-bits */
 #endif
 
-#ifdef CONFIG_MIPS_HYDROGEN3
-#define BOARD_MAP_NAME "Hydrogen3 Flash"
-#define BOARD_FLASH_SIZE 0x02000000 /* 32MB */
-#define BOARD_FLASH_WIDTH 4 /* 32-bits */
-#define USE_LOCAL_ACCESSORS /* why? */
-#endif
-
 #ifdef CONFIG_MIPS_BOSPORUS
 #define BOARD_MAP_NAME "Bosporus Flash"
 #define BOARD_FLASH_SIZE 0x01000000 /* 16MB */
@@ -131,13 +124,6 @@ int __init alchemy_mtd_init(void)
 
 	window_addr = 0x20000000 - BOARD_FLASH_SIZE;
 	window_size = BOARD_FLASH_SIZE;
-#ifdef CONFIG_MIPS_MIRAGE_WHY
-	/* Boot ROM flash bank only; no user bank */
-	window_addr = 0x1C000000;
-	window_size = 0x04000000;
-	/* USERFS from 0x1C00 0000 to 0x1FC00000 */
-	alchemy_partitions[0].size = 0x03C00000;
-#endif
 
 	/*
 	 * Static partition definition selection
