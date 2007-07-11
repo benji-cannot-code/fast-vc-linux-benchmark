719f9c75f55c10b2c4ac587397685b25015ea617
Pierre Ossman <drzeus@drzeus.cx>
2007-07-11 20:22:11 +0200

    mmc: update kerneldoc
    Make sure the kerneldoc comments are up to date and relevant.
    
    Signed-off-by: Pierre Ossman <drzeus@drzeus.cx>

--- drivers/mmc/core/sd_ops.c
+++ drivers/mmc/core/sd_ops.c
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *	mmc_wait_for_app_cmd - start an application command and wait for
  			       completion
  *	@host: MMC host to start command
- *	@rca: RCA to send MMC_APP_CMD to
+ *	@card: Card to send MMC_APP_CMD to
  *	@cmd: MMC command to start
  *	@retries: maximum number of retries
  *
