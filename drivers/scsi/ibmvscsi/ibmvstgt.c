3c4b978895d02c6080d9055cc0a3efb45c1054c6
FUJITA Tomonori <tomof@acm.org>
2007-07-11 15:08:24 +0900

    [SCSI] tgt: convert ibmvstgt to use transport tsk_mgmt_response callback
    This converts ibmvstgt to use transport tsk_mgmt_response callback.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Mike Christie <michaelc@cs.wisc.edu>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/ibmvscsi/ibmvstgt.c
+++ drivers/scsi/ibmvscsi/ibmvstgt.c
@@ -819,7 +819,6 @@ static struct scsi_host_template ibmvstgt_sht = {
 	.max_sectors		= DEFAULT_MAX_SECTORS,
 	.transfer_response	= ibmvstgt_cmd_done,
 	.eh_abort_handler	= ibmvstgt_eh_abort_handler,
-	.tsk_mgmt_response	= ibmvstgt_tsk_mgmt_response,
 	.shost_attrs		= ibmvstgt_attrs,
 	.proc_name		= TGT_NAME,
 };
@@ -946,6 +945,7 @@ static int get_system_info(void)
 }
 
 static struct srp_function_template ibmvstgt_transport_functions = {
+	.tsk_mgmt_response = ibmvstgt_tsk_mgmt_response,
 	.it_nexus_response = ibmvstgt_it_nexus_response,
 };
 
