5cd19428f4709babe49eb8e09c821d263f32a6c3
Linus Torvalds <torvalds@woody.linux-foundation.org>
2007-07-12 16:40:08 -0700

    Revert "[BNX2]: Seems to not need net/tcp.h"
    This reverts commit 963bd949b12158d9b5380b718b31c4b33372ed73.  The
    driver _does_ need the networking header files;
    
    	  CC [M]  drivers/net/bnx2.o
    	drivers/net/bnx2.c: In function 'bnx2_start_xmit':
    	drivers/net/bnx2.c:5177: warning: implicit declaration of function 'tcp_optlen'
    	drivers/net/bnx2.c:5181: error: invalid application of 'sizeof' to incomplete type 'struct ipv6hdr'
    	drivers/net/bnx2.c:5202: error: invalid application of 'sizeof' to incomplete type 'struct tcphdr'
    	drivers/net/bnx2.c:5207: warning: implicit declaration of function 'tcp_hdr'
    	drivers/net/bnx2.c:5207: error: invalid type argument of '->'
    	make[2]: *** [drivers/net/bnx2.o] Error 1
    	make[1]: *** [drivers/net] Error 2
    	make: *** [drivers] Error 2
    
    Cc: Ilpo Jävinen <ilpo.jarvinen@helsinki.fi>
    Cc: David Miller <davem@davemloft.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BCM_VLAN 1
 #endif
 #include <net/ip.h>
+#include <net/tcp.h>
 #include <net/checksum.h>
 #include <linux/workqueue.h>
 #include <linux/crc32.h>
