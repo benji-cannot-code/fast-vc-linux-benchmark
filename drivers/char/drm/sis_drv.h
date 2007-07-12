ea7bf15db5655a37a34cc57c56d7635bd79965b5
Dave Airlie <airlied@linux.ie>
2007-07-12 10:21:05 +1000

    drm: de-typedef sman
    Signed-off-by: Dave Airlie <airlied@linux.ie>

--- drivers/char/drm/sis_drv.h
+++ drivers/char/drm/sis_drv.h
@@ -47,6 +47,7 @@ enum sis_family {
 
 #include "drm_sman.h"
 
+
 #define SIS_BASE (dev_priv->mmio)
 #define SIS_READ(reg)         DRM_READ32(SIS_BASE, reg);
 #define SIS_WRITE(reg, val)   DRM_WRITE32(SIS_BASE, reg, val);
@@ -54,7 +55,7 @@ enum sis_family {
 typedef struct drm_sis_private {
 	drm_local_map_t *mmio;
 	unsigned int idle_fault;
-	drm_sman_t sman;
+	struct drm_sman sman;
 	unsigned int chipset;
 	int vram_initialized;
 	int agp_initialized;
