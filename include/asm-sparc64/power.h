53bc92acf86eb1c6db72b5fd781a79e145abcada
David S. Miller <davem@sunset.davemloft.net>
2007-07-11 18:18:04 -0700

    [SPARC64]: Export powerd facilities for external entities.
    Besides the existing usage for power-button interrupts, we'll
    want to make use of this code for domain-services where the
    LDOM manager can send reboot requests to the guest node.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/asm-sparc64/power.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _SPARC64_POWER_H
+#define _SPARC64_POWER_H
+
+extern void wake_up_powerd(void);
+extern int start_powerd(void);
+
+#endif /* !(_SPARC64_POWER_H) */
