0a0583ad8b12ecf210b2d9f27fac1c833540f69b
Steve French <sfrench@us.ibm.com>
2007-07-15 21:58:28 +0000

    [CIFS] Fix build break - inet.h not included when experimental ifdef off
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/netmisc.c
+++ fs/cifs/netmisc.c
@@ -30,9 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fs.h>
 #include <asm/div64.h>
 #include <asm/byteorder.h>
-#ifdef CONFIG_CIFS_EXPERIMENTAL
 #include <linux/inet.h>
-#endif
 #include "cifsfs.h"
 #include "cifspdu.h"
 #include "cifsglob.h"
