06df0a30bd59adab3f0e99bf46ea45eddbd25544
Russell King <rmk@dyn-67.arm.linux.org.uk>
2007-07-15 12:51:41 +0100

    [ARM] rpc: silence two section mismatch warnings
    WARNING: drivers/built-in.o(.text+0x3fd54): Section mismatch: reference to .init.data: (between 'ether3_probe' and 'ether1_setmulticastlist')
    WARNING: drivers/built-in.o(.text+0x40380): Section mismatch: reference to .init.data: (between 'ether1_probe' and 'ether1_interrupt')
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/net/arm/ether3.c
+++ drivers/net/arm/ether3.c
@@ -69,7 +69,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/ecard.h>
 #include <asm/io.h>
 
-static char version[] __initdata = "ether3 ethernet driver (c) 1995-2000 R.M.King v1.17\n";
+static char version[] __devinitdata = "ether3 ethernet driver (c) 1995-2000 R.M.King v1.17\n";
 
 #include "ether3.h"
 
