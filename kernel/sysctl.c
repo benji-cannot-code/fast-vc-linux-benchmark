35a6a66c4961acd0cddd3ffde4a6dda3a7454911
Linus Torvalds <torvalds@woody.linux-foundation.org>
2007-07-16 11:50:38 -0700

    Remove duplicate comments from sysctl.c
    Randy Dunlap noticed that the recent comment clarifications from Andrew
    had somehow gotten duplicated.  Quoth Andrew: "hm, that could have been
    some late-night reject-fixing."
    
    Fix it up.
    
    Cc: From: Andrew Morton <akpm@linux-foundation.org>
    Cc: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1000,10 +1000,6 @@ static ctl_table vm_table[] = {
  * NOTE: do not add new entries to this table unless you have read
  * Documentation/sysctl/ctl_unnumbered.txt
  */
-/*
- * NOTE: do not add new entries to this table unless you have read
- * Documentation/sysctl/ctl_unnumbered.txt
- */
 	{ .ctl_name = 0 }
 };
 
@@ -1144,10 +1140,6 @@ static ctl_table fs_table[] = {
 		.child		= binfmt_misc_table,
 	},
 #endif
-/*
- * NOTE: do not add new entries to this table unless you have read
- * Documentation/sysctl/ctl_unnumbered.txt
- */
 /*
  * NOTE: do not add new entries to this table unless you have read
  * Documentation/sysctl/ctl_unnumbered.txt
