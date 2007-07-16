20e3995ed0e4c05857b80b01a3a9458f4b0ee6b4
Anderson Briglia <briglia.anderson@gmail.com>
2007-07-15 23:38:09 -0700

    vmscan: fix comments related to shrink_list()
    Fix the shrink_list name on some files under mm/ directory.
    
    Signed-off-by: Anderson Briglia <anderson.briglia@indt.org.br>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /*
  * swapper_space is a fiction, retained to simplify the path through
- * vmscan's shrink_list, to make sync_page look nicer, and to allow
+ * vmscan's shrink_page_list, to make sync_page look nicer, and to allow
  * future use of radix_tree tags in the swap cache.
  */
 static const struct address_space_operations swap_aops = {
