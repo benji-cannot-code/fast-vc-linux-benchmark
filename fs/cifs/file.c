2d0083ab034642dbd22d7da46ab056ed10daf240
Steve French <sfrench@us.ibm.com>
2007-07-17 17:34:02 +0000

    [CIFS] More whitespace/formatting fixes (noticed by checkpatch)
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -503,7 +503,8 @@ int cifs_close(struct inode *inode, struct file *file)
 					timeout *= 4;
 				}
 				if (atomic_read(&pSMBFile->wrtPending))
-					cERROR(1,("close with pending writes"));
+					cERROR(1,
+						("close with pending writes"));
 				rc = CIFSSMBClose(xid, pTcon,
 						  pSMBFile->netfid);
 			}
@@ -1286,7 +1287,7 @@ retry:
 						   1);
 				atomic_dec(&open_file->wrtPending);
 				if (rc || bytes_written < bytes_to_write) {
-					cERROR(1,("Write2 ret %d, written = %d",
+					cERROR(1, ("Write2 ret %d, wrote %d",
 						  rc, bytes_written));
 					/* BB what if continued retry is
 					   requested via mount flags? */
--- fs/cifs/netmisc.c
+++ fs/cifs/netmisc.c
@@ -790,7 +790,8 @@ map_smb_to_linux_error(struct smb_hdr *smb)
 		     sizeof (struct smb_to_posix_error); i++) {
 			if (mapping_table_ERRDOS[i].smb_err == 0)
 				break;
-			else if (mapping_table_ERRDOS[i].smb_err == smberrcode) {
+			else if (mapping_table_ERRDOS[i].smb_err ==
+								smberrcode) {
 				rc = mapping_table_ERRDOS[i].posix_code;
 				break;
 			}
@@ -803,7 +804,8 @@ map_smb_to_linux_error(struct smb_hdr *smb)
 		     sizeof (struct smb_to_posix_error); i++) {
 			if (mapping_table_ERRSRV[i].smb_err == 0)
 				break;
-			else if (mapping_table_ERRSRV[i].smb_err == smberrcode) {
+			else if (mapping_table_ERRSRV[i].smb_err ==
+								smberrcode) {
 				rc = mapping_table_ERRSRV[i].posix_code;
 				break;
 			}
