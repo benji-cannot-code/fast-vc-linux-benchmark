aad2276c3d32bf35843ad04be5c1ecadf9734c81
Maciej W. Rozycki <macro@linux-mips.org>
2007-07-17 04:03:50 -0700

    sb1250-duart.c: SB1250 DUART serial support
    This is a driver for the SB1250 DUART, a dual serial port implementation
    included in the Broadcom family of SOCs descending from the SiByte SB1250
    MIPS64 chip multiprocessor.  It is a new implementation replacing the
    old-fashioned driver currently present in the linux-mips.org tree.  It
    supports all the usual features one would expect from a(n asynchronous)
    serial driver, including modem line control (as far as hardware supports it
    -- there is edge detection logic missing from the DCD and RI lines and the
    driver does not implement polling of these lines at the moment), the serial
    console, BREAK transmission and reception, including the magic SysRq.  The
    receive FIFO threshold is not maintained though.
    
    The driver was tested with a SWARM board which uses a BCM1250 SOC (which is
    dual MIPS64 CMP) and has both ports of the single DUART implemented wired
    externally.  Both were tested.  Testing included using the ports as
    terminal lines at 1200bps (which is the ports minimum), 115200bps and a
    couple of random speeds inbetween.  The modem lines were verified to
    operate correctly.  No testing was performed with a use as a network
    interface, like with SLIP or PPP.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Acked-by: Ralf Baechle <ralf@linux-mips.org>
    Cc: Alan Cox <alan@lxorguk.ukuu.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/mips/sibyte/bcm1480/setup.c
+++ arch/mips/sibyte/bcm1480/setup.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 unsigned int sb1_pass;
 unsigned int soc_pass;
 unsigned int soc_type;
+EXPORT_SYMBOL(soc_type);
 unsigned int periph_rev;
 unsigned int zbbus_mhz;
 
--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -143,6 +143,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Micrel KS8695 */
 #define PORT_KS8695	76
 
+/* Broadcom SB1250, etc. SOC */
+#define PORT_SB1250_DUART	77
+
 
 #ifdef __KERNEL__
 
