f825cabe2b632dc6ee173d6480d111eb17654f4f
NeilBrown <neilb@suse.de>
2007-07-17 04:06:13 -0700

    md: change bitmap_unplug and others to void functions
    bitmap_unplug only ever returns 0, so it may as well be void.  Two callers try
    to print a message if it returns non-zero, but that message is already printed
    by bitmap_file_kick.
    
    write_page returns an error which is not consistently checked.  It always
    causes BITMAP_WRITE_ERROR to be set on an error, and that can more
    conveniently be checked.
    
    When the return of write_page is checked, an error causes bitmap_file_kick to
    be called - so move that call into write_page - and protect against recursive
    calls into bitmap_file_kick.
    
    bitmap_update_sb returns an error that is never checked.
    
    So make these 'void' and be consistent about checking the bit.
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -1641,7 +1641,6 @@ static void sync_sbs(mddev_t * mddev, int nospares)
 
 static void md_update_sb(mddev_t * mddev, int force_change)
 {
-	int err;
 	struct list_head *tmp;
 	mdk_rdev_t *rdev;
 	int sync_req;
@@ -1728,7 +1727,7 @@ repeat:
 		"md: updating %s RAID superblock on device (in sync %d)\n",
 		mdname(mddev),mddev->in_sync);
 
-	err = bitmap_update_sb(mddev->bitmap);
+	bitmap_update_sb(mddev->bitmap);
 	ITERATE_RDEV(mddev,rdev,tmp) {
 		char b[BDEVNAME_SIZE];
 		dprintk(KERN_INFO "md: ");
