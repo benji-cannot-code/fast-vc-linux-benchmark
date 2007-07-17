92b42361472c61b62fedfc663e18abedefb120ad
Jean Delvare <khali@linux-fr.org>
2007-07-17 18:29:41 -0300

    V4L/DVB (5884): zr36067: clean up debug function
    Debugging cleanups to the zr36067 driver:
    
    * Use module_param_named() to declare the debug parameter, so we can
      use a single global variable to handle the debug level. This makes
      the driver a bit smaller (by 648 bytes on x86_64), thanks to one
      less level of indirection on every use.
    
    * Change the debug parameter sysfs permissions, so that the debug
      level can be adjusted at runtime, as is done in many other
      media/video drivers.
    
    * The debug level is between 0 and 5, not 0 and 4.
    
    * Move the zr_debug export and dprintk macro definition to a header
      file so that we don't have to define them in each source file.
    
    * Simplify a duplicate test on zr_debug.
    
    Note that zr_debug was subsequently renamed to debug_zr36067 to avoid
    possible conflicts with other Zoran device drivers, on a suggestion
    by Trent Piepho.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Trent Piepho <xyzzy@speakeasy.org>
    Acked-by: Ronald S. Bultje <rbultje@ronald.bitfreak.net>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/zoran_device.c
+++ drivers/media/video/zoran_device.c
@@ -53,20 +53,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "videocodec.h"
 #include "zoran.h"
 #include "zoran_device.h"
+#include "zoran_card.h"
 
 #define IRQ_MASK ( ZR36057_ISR_GIRQ0 | \
 		   ZR36057_ISR_GIRQ1 | \
 		   ZR36057_ISR_JPEGRepIRQ )
 
 extern const struct zoran_format zoran_formats[];
-
-extern int *zr_debug;
-
-#define dprintk(num, format, args...) \
-	do { \
-		if (*zr_debug >= num) \
-			printk(format, ##args); \
-	} while (0)
 
 static int lml33dpath = 0;	/* 1 will use digital path in capture
 				 * mode instead of analog. It can be
@@ -175,7 +168,7 @@ post_office_read (struct zoran *zr,
 static void
 dump_guests (struct zoran *zr)
 {
-	if (*zr_debug > 2) {
+	if (zr36067_debug > 2) {
 		int i, guest[8];
 
 		for (i = 1; i < 8; i++) {	// Don't read jpeg codec here
@@ -1272,7 +1265,7 @@ error_handler (struct zoran *zr,
 		zr->num_errors++;
 
 		/* Report error */
-		if (*zr_debug > 1 && zr->num_errors <= 8) {
+		if (zr36067_debug > 1 && zr->num_errors <= 8) {
 			long frame;
 			frame =
 			    zr->jpg_pend[zr->jpg_dma_tail & BUZ_MASK_FRAME];
@@ -1532,7 +1525,7 @@ zoran_irq (int             irq,
 
 			if (zr->codec_mode == BUZ_MODE_MOTION_DECOMPRESS ||
 			    zr->codec_mode == BUZ_MODE_MOTION_COMPRESS) {
-				if (*zr_debug > 1 &&
+				if (zr36067_debug > 1 &&
 				    (!zr->frame_num || zr->JPEG_error)) {
 					printk(KERN_INFO
 					       "%s: first frame ready: state=0x%08x odd_even=%d field_per_buff=%d delay=%d\n",
@@ -1569,7 +1562,7 @@ zoran_irq (int             irq,
 						    zr->JPEG_missed;
 				}
 
-				if (*zr_debug > 2 && zr->frame_num < 6) {
+				if (zr36067_debug > 2 && zr->frame_num < 6) {
 					int i;
 					printk("%s: seq=%ld stat_com:",
 					       ZR_DEVNAME(zr), zr->jpg_seq_num);
