2810b1622f505b09b2704bb1d92f78daa3bf5260
Roland McGrath <roland@redhat.com>
2007-07-17 04:03:48 -0700

    avoid OPEN_MAX in SCM_MAX_FD
    The OPEN_MAX constant is an arbitrary number with no useful relation to
    anything.  Nothing should be using it.  SCM_MAX_FD is just an arbitrary
    constant and it should be clear that its value is chosen in net/scm.h
    and not actually derived from anything else meaningful in the system.
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Cc: "David S. Miller" <davem@davemloft.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/net/scm.h
+++ include/net/scm.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Well, we should have at least one descriptor open
  * to accept passed FDs 8)
  */
-#define SCM_MAX_FD	(OPEN_MAX-1)
+#define SCM_MAX_FD	255
 
 struct scm_fp_list
 {
