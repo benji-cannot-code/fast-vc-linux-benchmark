4911010c52f2f6b7369f17e96650e46ce60a9e85
Al Viro <viro@ftp.linux.org.uk>
2007-07-17 08:49:35 +0100

    atl1: missing include
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/atl1/atl1_main.c
+++ drivers/net/atl1/atl1_main.c
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/compiler.h>
 #include <linux/delay.h>
 #include <linux/mii.h>
+#include <linux/interrupt.h>
 #include <net/checksum.h>
 
 #include <asm/atomic.h>
