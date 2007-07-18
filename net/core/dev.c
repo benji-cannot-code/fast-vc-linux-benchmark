3aa2f1aea2c68a60992c6d5f30c2db7d6c35163c
Denis Cheng <crquan@gmail.com>
2007-07-18 02:10:54 -0700

    [NET]: move dev_mc_discard from dev_mcast.c to dev.c
    Because this function is only called by unregister_netdevice,
    this moving could make this non-global function static,
    and also remove its declaration in netdevice.h;
    
    Any further, function __dev_addr_discard is also just called by
    dev_mc_discard and dev_unicast_discard, keeping this two functions
    both in one c file could make __dev_addr_discard also static
    and remove its declaration in netdevice.h;
    
    Futhermore, the sequential call to dev_unicast_discard and then
    dev_mc_discard in unregister_netdevice have a similar mechanism that:
    (netif_tx_lock_bh / __dev_addr_discard / netif_tx_unlock_bh),
    they should merged into one to eliminate duplicates in acquiring and
    releasing the dev->_xmit_lock, this would be done in my following patch.
    
    Signed-off-by: Denis Cheng <crquan@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2716,7 +2716,7 @@ int __dev_addr_add(struct dev_addr_list **list, int *count,
 	return 0;
 }
 
-void __dev_addr_discard(struct dev_addr_list **list)
+static void __dev_addr_discard(struct dev_addr_list **list)
 {
 	struct dev_addr_list *tmp;
 
@@ -2783,6 +2783,18 @@ static void dev_unicast_discard(struct net_device *dev)
 	netif_tx_lock_bh(dev);
 	__dev_addr_discard(&dev->uc_list);
 	dev->uc_count = 0;
+	netif_tx_unlock_bh(dev);
+}
+
+/*
+ *	Discard multicast list when a device is downed
+ */
+
+static void dev_mc_discard(struct net_device *dev)
+{
+	netif_tx_lock_bh(dev);
+	__dev_addr_discard(&dev->mc_list);
+	dev->mc_count = 0;
 	netif_tx_unlock_bh(dev);
 }
 
