97f0ad553238a991599cd0b883e6ff7a56d91fb2
Jeremy Fitzhardinge <jeremy@xensource.com>
2007-07-17 18:37:03 -0700

    paravirt: increase IRQ limit
    When running with CONFIG_PARAVIRT, we may want lots of IRQs even if
    there's no IO APIC.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>

--- include/asm-i386/mach-default/irq_vectors_limits.h
+++ include/asm-i386/mach-default/irq_vectors_limits.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_IRQ_VECTORS_LIMITS_H
 #define _ASM_IRQ_VECTORS_LIMITS_H
 
-#ifdef CONFIG_X86_IO_APIC
+#if defined(CONFIG_X86_IO_APIC) || defined(CONFIG_PARAVIRT)
 #define NR_IRQS 224
 # if (224 >= 32 * NR_CPUS)
 # define NR_IRQ_VECTORS NR_IRQS
