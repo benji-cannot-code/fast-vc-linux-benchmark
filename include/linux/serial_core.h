c3b6acd57d991780b577d06fcbfb216b0b0d7867
Maciej W. Rozycki <macro@linux-mips.org>
2007-07-18 00:49:11 -0700

    zs: move to the serial subsystem
    This is a reimplementation of the zs driver for the serial subsystem.  Any
    resemblance to the old driver is purely coincidential.  ;-) I do hope I got
    the handling of modem lines right -- better do not tackle me about the
    issue unless you feel too good...
    
    Any users of the old driver: please note the numbers of the serial lines
    have now been swapped, i.e.  ttyS0 <-> ttyS1 and ttyS2 <-> ttyS3.  It has
    to do with the modem lines mentioned above; basically the port A in a given
    chip has to be initialised before the port B if you want to use the latter
    as the serial console (which is usually the case), as operations on modem
    lines of the serial line associated with the port B access both ports (see
    the comment at the top of the driver for the details of wiring used).
    Please update your scripts.
    
    This is also the reason each SCC now requests an IRQ once only (as seen in
    "/proc/interrupts") -- the handler takes care of both ports at once as the
    line associated with the port B has to take status update interrupts from
    both ports (and yet the line of the port A takes its own for itself too).
    The old driver never got it right...
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -63,8 +63,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* NEC v850.  */
 #define PORT_V850E_UART	40
 
-/* DZ */
-#define PORT_DZ		47
+/* DEC */
+#define PORT_DZ		46
+#define PORT_ZS		47
 
 /* Parisc type numbers. */
 #define PORT_MUX	48
