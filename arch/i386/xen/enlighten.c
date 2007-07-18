0386104e96173a3578541a92d6f12619f961ee9a
Jeremy Fitzhardinge <jeremy@xensource.com>
2007-07-17 18:37:04 -0700

    xen: virtual mmu
    Xen pagetable handling, including the machinery to implement direct
    pagetables.
    
    Xen presents the real CPU's pagetables directly to guests, with no
    added shadowing or other layer of abstraction.  Naturally this means
    the hypervisor must maintain close control over what the guest can put
    into the pagetable.
    
    When the guest modifies the pte/pmd/pgd, it must convert its
    domain-specific notion of a "physical" pfn into a global machine frame
    number (mfn) before inserting the entry into the pagetable.  Xen will
    check to make sure the domain is allowed to create a mapping of the
    given mfn.
    
    Xen also requires that all mappings the guest has of its own active
    pagetable are read-only.  This is relatively easy to implement in
    Linux because all pagetables share the same pte pages for kernel
    mappings, so updating the pte in one pagetable will implicitly update
    the mapping in all pagetables.
    
    Normally a pagetable becomes active when you point to it with cr3 (or
    the Xen equivalent), but when you do so, Xen must check the whole
    pagetable for correctness, which is clearly a performance problem.
    
    Xen solves this with pinning which keeps a pagetable effectively
    active even if its currently unused, which means that all the normal
    update rules are enforced.  This means that it need not revalidate the
    pagetable when loading cr3.
    
    This patch has a first-cut implementation of pinning, but it is more
    fully implemented in a later patch.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>

--- arch/i386/xen/enlighten.c
+++ arch/i386/xen/enlighten.c
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 
 #include "xen-ops.h"
+#include "mmu.h"
 #include "multicalls.h"
 
 EXPORT_SYMBOL_GPL(hypercall_page);
@@ -580,11 +581,9 @@ static __init void xen_pagetable_setup_done(pgd_t *base)
 		 * Should be set_fixmap(), but shared_info is a machine
 		 * address with no corresponding pseudo-phys address.
 		 */
-#if 0
 		set_pte_mfn(fix_to_virt(FIX_PARAVIRT_BOOTMAP),
 			    PFN_DOWN(xen_start_info->shared_info),
 			    PAGE_KERNEL);
-#endif
 
 		HYPERVISOR_shared_info =
 			(struct shared_info *)fix_to_virt(FIX_PARAVIRT_BOOTMAP);
@@ -593,9 +592,7 @@ static __init void xen_pagetable_setup_done(pgd_t *base)
 		HYPERVISOR_shared_info =
 			(struct shared_info *)__va(xen_start_info->shared_info);
 
-#if 0
 	xen_pgd_pin(base);
-#endif
 
 	xen_vcpu_setup(smp_processor_id());
 }
@@ -690,6 +687,31 @@ static const struct paravirt_ops xen_paravirt_ops __initdata = {
 	.alloc_pd_clone = xen_alloc_pd_clone,
 	.release_pd = xen_release_pd,
 	.release_pt = xen_release_pt,
+
+	.set_pte = xen_set_pte,
+	.set_pte_at = xen_set_pte_at,
+	.set_pmd = xen_set_pmd,
+
+	.pte_val = xen_pte_val,
+	.pgd_val = xen_pgd_val,
+
+	.make_pte = xen_make_pte,
+	.make_pgd = xen_make_pgd,
+
+#ifdef CONFIG_X86_PAE
+	.set_pte_atomic = xen_set_pte_atomic,
+	.set_pte_present = xen_set_pte_at,
+	.set_pud = xen_set_pud,
+	.pte_clear = xen_pte_clear,
+	.pmd_clear = xen_pmd_clear,
+
+	.make_pmd = xen_make_pmd,
+	.pmd_val = xen_pmd_val,
+#endif	/* PAE */
+
+	.activate_mm = xen_activate_mm,
+	.dup_mmap = xen_dup_mmap,
+	.exit_mmap = xen_exit_mmap,
 
 	.set_lazy_mode = xen_set_lazy_mode,
 };
