9f78c81262c3450d601102fc72a6afdf923e86da
Henrique de Moraes Holschuh <hmh@hmh.eng.br>
2007-07-18 23:45:47 -0300

    ACPI: thinkpad-acpi: bump up version to 0.15
    Name it thinkpad-acpi version 0.15.
    
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/misc/thinkpad_acpi.c
+++ drivers/misc/thinkpad_acpi.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  02110-1301, USA.
  */
 
-#define IBM_VERSION "0.14"
+#define IBM_VERSION "0.15"
 #define TPACPI_SYSFS_VERSION 0x010000
 
 /*
