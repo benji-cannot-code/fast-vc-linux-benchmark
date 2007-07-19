653f687fe61ab9ca6c1f227a438d8029c8380eda
David S. Miller <davem@sunset.davemloft.net>
2007-07-18 22:03:58 -0700

    [SPARC]: Implement fb_is_primary_device().
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/fb.h
+++ include/asm-sparc64/fb.h
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fb.h>
 #include <linux/fs.h>
 #include <asm/page.h>
+#include <asm/prom.h>
 
 static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
 				unsigned long off)
@@ -13,6 +14,14 @@ static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
 
 static inline int fb_is_primary_device(struct fb_info *info)
 {
+	struct device *dev = info->device;
+	struct device_node *node;
+
+	node = dev->archdata.prom_node;
+	if (node &&
+	    node == of_console_device)
+		return 1;
+
 	return 0;
 }
 
