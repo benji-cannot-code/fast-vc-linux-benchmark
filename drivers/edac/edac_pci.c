c6df166748263c1c365ee43ea935ef5d635ef393
Dave Jiang <djiang@mvista.com>
2007-07-19 01:49:54 -0700

    drivers/edac: mod PCI poll names
    Fixup poll values for MC and PCI.
    Also make mc function names unique to mc.
    
    Signed-off-by: Dave Jiang <djiang@mvista.com>
    Signed-off-by: Douglas Thompson <dougthompson@xmissin.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/edac/edac_pci.c
+++ drivers/edac/edac_pci.c
@@ -227,13 +227,14 @@ static void edac_pci_workq_function(void *ptr)
 
 	if ((pci->op_state == OP_RUNNING_POLL) &&
 		(pci->edac_check != NULL) &&
-		(pci->check_parity_error))
+		(edac_pci_get_check_errors()))
 		pci->edac_check(pci);
 
 	edac_unlock_pci_list();
 
 	/* Reschedule */
-	queue_delayed_work(edac_workqueue, &pci->work, pci->delay);
+	queue_delayed_work(edac_workqueue, &pci->work,
+			msecs_to_jiffies(edac_pci_get_poll_msec()));
 }
 
 /*
@@ -246,15 +247,13 @@ static void edac_pci_workq_setup(struct edac_pci_ctl_info *pci,
 {
 	debugf0("%s()\n", __func__);
 
-	pci->poll_msec = msec;
-	edac_calc_delay(pci);
-
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,20))
 	INIT_DELAYED_WORK(&pci->work, edac_pci_workq_function);
 #else
 	INIT_WORK(&pci->work, edac_pci_workq_function, pci);
 #endif
-	queue_delayed_work(edac_workqueue, &pci->work, pci->delay);
+	queue_delayed_work(edac_workqueue, &pci->work,
+			msecs_to_jiffies(edac_pci_get_poll_msec()));
 }
 
 /*
@@ -390,16 +389,6 @@ struct edac_pci_ctl_info * edac_pci_del_device(struct device *dev)
 	return pci;
 }
 EXPORT_SYMBOL_GPL(edac_pci_del_device);
-
-static inline int edac_pci_get_log_pe(struct edac_pci_ctl_info *pci)
-{
-	return pci->log_parity_error;
-}
-
-static inline int edac_pci_get_panic_on_pe(struct edac_pci_ctl_info *pci)
-{
-	return pci->panic_on_pe;
-}
 
 void edac_pci_generic_check(struct edac_pci_ctl_info *pci)
 {
