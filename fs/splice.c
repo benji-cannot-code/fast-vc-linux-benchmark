b7fe7e04794c9437e322cf6c0fcd78120f33cdd9
Jens Axboe <jens.axboe@oracle.com>
2007-07-20 15:18:12 +0200

    splice: fix bad unlock_page() in error case
    If add_to_page_cache_lru() fails, the page will not be locked. But
    splice jumps to an error path that does a page release and unlock,
    causing a BUG() in unlock_page().
    
    Fix this by adding one more label that just releases the page. This bug
    was actually triggered on EL5 by gurudas pai <gurudas.pai@oracle.com>
    using fio.
    
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/splice.c
+++ fs/splice.c
@@ -595,7 +595,7 @@ find_page:
 		ret = add_to_page_cache_lru(page, mapping, index,
 					    GFP_KERNEL);
 		if (unlikely(ret))
-			goto out;
+			goto out_release;
 	}
 
 	ret = mapping->a_ops->prepare_write(file, page, offset, offset+this_len);
@@ -651,8 +651,9 @@ find_page:
 	 */
 	mark_page_accessed(page);
 out:
-	page_cache_release(page);
 	unlock_page(page);
+out_release:
+	page_cache_release(page);
 out_ret:
 	return ret;
 }
