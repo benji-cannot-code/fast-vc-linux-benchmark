76c35010a03f3e3eab3dd3e20c52f0e42a791120
Adrian Bunk <bunk@stusta.de>
2007-07-20 00:31:44 -0700

    arch/i386/xen/events.c should #include <asm/xen/hypervisor.h>
    Every file should include the headers containing the prototypes for
    its global functions.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Acked-by: Jeremy Fitzhardinge <jeremy@goop.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/i386/xen/events.c
+++ arch/i386/xen/events.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/irq.h>
 #include <asm/sync_bitops.h>
 #include <asm/xen/hypercall.h>
+#include <asm/xen/hypervisor.h>
 
 #include <xen/events.h>
 #include <xen/interface/xen.h>
