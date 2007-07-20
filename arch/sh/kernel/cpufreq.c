664a067dfa30ffd30df74c7259bb1aaabba2e377
Paul Mundt <lethal@linux-sh.org>
2007-07-20 16:03:02 +0900

    sh: cpufreq: Fix up the build for SH-2.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpufreq.c
+++ arch/sh/kernel/cpufreq.c
@@ -24,12 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 #include <linux/smp.h>
 #include <linux/sched.h>	/* set_cpus_allowed() */
-#include <linux/io.h>
 #include <linux/clk.h>
-#include <asm/processor.h>
-#include <asm/watchdog.h>
-#include <asm/freq.h>
-#include <asm/clock.h>
 
 static struct clk *cpuclk;
 
