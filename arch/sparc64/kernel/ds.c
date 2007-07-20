072f83fd479a4b049e738cbf4ab8e0c6b2817aa9
David S. Miller <davem@sunset.davemloft.net>
2007-07-20 02:34:41 -0700

    [SPARC64]: Do not flood log with failed DS messages.
    When booting up a control node it's quite common to
    not be able to register several service types.
    
    And likewise on guests at least one or two are going
    to not be there.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/ds.c
+++ arch/sparc64/kernel/ds.c
@@ -931,8 +931,6 @@ static int ds_handshake(struct ds_info *dp, struct ds_msg_tag *pkt)
 			       dp->id, np->handle);
 			return 0;
 		}
-		printk(KERN_INFO "ds-%lu: Could not register %s service\n",
-		       dp->id, cp->service_id);
 		cp->state = CAP_STATE_UNKNOWN;
 	}
 
