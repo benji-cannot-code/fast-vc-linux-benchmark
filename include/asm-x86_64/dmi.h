8ea585e05fc919f73c198b1d38e435e735538d96
Yinghai Lu <yhlu.kernel@gmail.com>
2007-07-21 17:10:04 +0200

    x86_64: remove extra extern declaring about dmi_ioremap
    Signed-off-by: Yinghai Lu <yinghai.lu@sun.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-x86_64/dmi.h
+++ include/asm-x86_64/dmi.h
@@ -4,15 +4,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/io.h>
 
-extern void *dmi_ioremap(unsigned long addr, unsigned long size);
-extern void dmi_iounmap(void *addr, unsigned long size);
-
 #define DMI_MAX_DATA 2048
 
 extern int dmi_alloc_index;
 extern char dmi_alloc_data[DMI_MAX_DATA];
 
-/* This is so early that there is no good way to allocate dynamic memory. 
+/* This is so early that there is no good way to allocate dynamic memory.
    Allocate data in an BSS array. */
 static inline void *dmi_alloc(unsigned len)
 {
