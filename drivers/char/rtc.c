d224a5fb6afe2f48495025a5dc15269318f5ff6c
Chris Wright <chrisw@sous-sol.org>
2007-07-21 17:10:09 +0200

    x86_64: Untangle asm/hpet.h from asm/timex.h
    When making changes to x86_64 timers, I noticed that touching hpet.h triggered
    an unreasonably large rebuild.  Untangling it from timex.h quiets the extra
    rebuild quite a bit.
    
    Cc: john stultz <johnstul@us.ibm.com>
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/rtc.c
+++ drivers/char/rtc.c
@@ -83,7 +83,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/system.h>
 
-#if defined(__i386__)
+#ifdef CONFIG_X86
 #include <asm/hpet.h>
 #endif
 
--- include/asm-x86_64/timex.h
+++ include/asm-x86_64/timex.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/8253pit.h>
 #include <asm/msr.h>
 #include <asm/vsyscall.h>
-#include <asm/hpet.h>
 #include <asm/system.h>
 #include <asm/processor.h>
 #include <asm/tsc.h>
