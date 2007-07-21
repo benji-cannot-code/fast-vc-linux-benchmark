74a1faa63e6e6a1af6e3b9aa2ffa54c2c989abe9
Rasmus Rohde <rohde@duff.dk>
2007-07-21 15:37:35 -0300

    V4L/DVB (5897): dtt200u: add support for the Miglia TVMini USB DVB-T adapter
    add code for autodetection and firmware download to the Miglia TVMini USB
    DVB-T adapter.  After firmware download, the device re-registers using
    the WT220U_ZL0353_WARM usb id.
    
    Signed-off-by: Rasmus Rohde <rohde@duff.dk>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dvb-usb-ids.h
+++ drivers/media/dvb/dvb-usb/dvb-usb-ids.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define USB_VID_LEADTEK				0x0413
 #define USB_VID_LITEON				0x04ca
 #define USB_VID_MEDION				0x1660
+#define USB_VID_MIGLIA				0x18f3
 #define USB_VID_MSI				0x0db0
 #define USB_VID_OPERA1				0x695c
 #define USB_VID_PINNACLE			0x2304
