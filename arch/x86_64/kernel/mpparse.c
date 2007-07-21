32f4990070b06dc8b63ceea95da7c2dc6746be1c
Jan Beulich <jbeulich@novell.com>
2007-07-21 17:10:23 +0200

    x86_64: remove unused variable maxcpus
    .. and adjust documentation to properly reflect options that are
    x86-64 specific.
    
    Signed-off-by: Jan Beulich <jbeulich@novell.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/x86_64/kernel/mpparse.c
+++ arch/x86_64/kernel/mpparse.c
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Have we found an MP table */
 int smp_found_config;
-unsigned int __initdata maxcpus = NR_CPUS;
 
 /*
  * Various Linux-internal data structures created from the
