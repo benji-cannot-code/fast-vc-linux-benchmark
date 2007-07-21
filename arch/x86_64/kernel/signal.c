01d9e48f2fa90a3262696f8f59fe12d31e98143a
Tim Hockin <thockin@google.com>
2007-07-21 17:10:36 +0200

    x86_64: support poll() on /dev/mcelog
    Background:
     /dev/mcelog is typically polled manually.  This is less than optimal for
     situations where accurate accounting of MCEs is important.  Calling
     poll() on /dev/mcelog does not work.
    
    Description:
     This patch adds support for poll() to /dev/mcelog.  This results in
     immediate wakeup of user apps whenever the poller finds MCEs.  Because
     the exception handler can not take any locks, it can not call the wakeup
     itself.  Instead, it uses a thread_info flag (TIF_MCE_NOTIFY) which is
     caught at the next return from interrupt or exit from idle, calling the
     mce_user_notify() routine.  This patch also disables the "fake panic"
     path of the mce_panic(), because it results in printk()s in the exception
     handler and crashy systems.
    
     This patch also does some small cleanup for essentially unused variables,
     and moves the user notification into the body of the poller, so it is
     only called once per poll, rather than once per CPU.
    
    Result:
     Applications can now poll() on /dev/mcelog.  When an error is logged
     (whether through the poller or through an exception) the applications are
     woken up promptly.  This should not affect any previous behaviors.  If no
     MCEs are being logged, there is no overhead.
    
    Alternatives:
     I considered simply supporting poll() through the poller and not using
     TIF_MCE_NOTIFY at all.  However, the time between an uncorrectable error
     happening and the user application being notified is *the*most* critical
     window for us.  Many uncorrectable errors can be logged to the network if
     given a chance.
    
     I also considered doing the MCE poll directly from the idle notifier, but
     decided that was overkill.
    
    Testing:
     I used an error-injecting DIMM to create lots of correctable DRAM errors
     and verified that my user app is woken up in sync with the polling interval.
     I also used the northbridge to inject uncorrectable ECC errors, and
     verified (printk() to the rescue) that the notify routine is called and the
     user app does wake up.  I built with PREEMPT on and off, and verified
     that my machine survives MCEs.
    
    [wli@holomorphy.com: build fix]
    Signed-off-by: Tim Hockin <thockin@google.com>
    Signed-off-by: William Irwin <bill.irwin@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/x86_64/kernel/signal.c
+++ arch/x86_64/kernel/signal.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/i387.h>
 #include <asm/proto.h>
 #include <asm/ia32_unistd.h>
+#include <asm/mce.h>
 
 /* #define DEBUG_SIG 1 */
 
@@ -472,6 +473,12 @@ do_notify_resume(struct pt_regs *regs, void *unused, __u32 thread_info_flags)
 		regs->eflags |= TF_MASK;
 		clear_thread_flag(TIF_SINGLESTEP);
 	}
+
+#ifdef CONFIG_X86_MCE
+	/* notify userspace of pending MCEs */
+	if (thread_info_flags & _TIF_MCE_NOTIFY)
+		mce_notify_user();
+#endif /* CONFIG_X86_MCE */
 
 	/* deal with pending signal delivery */
 	if (thread_info_flags & (_TIF_SIGPENDING|_TIF_RESTORE_SIGMASK))
--- include/asm-x86_64/mce.h
+++ include/asm-x86_64/mce.h
@@ -106,6 +106,8 @@ extern atomic_t mce_entry;
 
 extern void do_machine_check(struct pt_regs *, long);
 
+extern int mce_notify_user(void);
+
 #endif
 
 #endif
