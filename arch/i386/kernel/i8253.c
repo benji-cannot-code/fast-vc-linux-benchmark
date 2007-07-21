20eb030da13581e81d6d4e14f9ddbf6c88a015b0
Adrian Bunk <bunk@stusta.de>
2007-07-21 17:10:28 +0200

    i386: arch/i386/kernel/i8253.c should #include <asm/timer.h>
    Every file should include the headers containing the prototypes for its
    global functions.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/i386/kernel/i8253.c
+++ arch/i386/kernel/i8253.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/delay.h>
 #include <asm/i8253.h>
 #include <asm/io.h>
+#include <asm/timer.h>
 
 #include "io_ports.h"
 
