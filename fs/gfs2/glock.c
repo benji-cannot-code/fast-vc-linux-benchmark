dd36362808e5c9fa8fa9ad2e686c1f9fd977263e
Jesper Juhl <jesper.juhl@gmail.com>
2007-07-21 17:03:22 +0200

    [GFS2] Clean up duplicate includes in fs/gfs2/
    This patch cleans up duplicate includes in
    	fs/gfs2/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/glock.c
+++ fs/gfs2/glock.c
@@ -26,8 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <linux/seq_file.h>
 #include <linux/debugfs.h>
-#include <linux/module.h>
-#include <linux/kallsyms.h>
 
 #include "gfs2.h"
 #include "incore.h"
