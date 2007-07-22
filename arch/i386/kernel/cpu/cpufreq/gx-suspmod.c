d97fdd44fa1fa32a24781e23f3eff6e53e095c4b
Juergen Beisert <juergen@kreuzholzen.de>
2007-07-22 11:12:38 +0200

    x86: Replace NSC/Cyrix specific chipset access macros by inlined functions.
    Due to index register access ordering problems, when using macros a line
    like this fails (and does nothing):
    
    	setCx86(CX86_CCR2, getCx86(CX86_CCR2) | 0x88);
    
    With inlined functions this line will work as expected.
    
    Note about a side effect: Seems on Geode GX1 based systems the
    "suspend on halt power saving feature" was never enabled due to this
    wrong macro expansion. With inlined functions it will be enabled, but
    this will stop the TSC when the CPU runs into a HLT instruction.
    Kernel output something like this:
    	Clocksource tsc unstable (delta = -472746897 ns)
    
    This is the 3rd version of this patch.
    
     - Adding missed arch/i386/kernel/cpu/mtrr/state.c
    	Thanks to Andres Salomon
     - Adding some big fat comments into the new header file
     	Suggested by Andi Kleen
    
    AK: fixed x86-64 compilation
    
    Signed-off-by: Juergen Beisert <juergen@kreuzholzen.de>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/i386/kernel/cpu/cpufreq/gx-suspmod.c
+++ arch/i386/kernel/cpu/cpufreq/gx-suspmod.c
@@ -80,7 +80,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/smp.h>
 #include <linux/cpufreq.h>
 #include <linux/pci.h>
-#include <asm/processor.h>
+#include <asm/processor-cyrix.h>
 #include <asm/errno.h>
 
 /* PCI config registers, all at F0 */
--- arch/i386/kernel/cpu/mtrr/cyrix.c
+++ arch/i386/kernel/cpu/mtrr/cyrix.c
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mtrr.h>
 #include <asm/msr.h>
 #include <asm/io.h>
+#include <asm/processor-cyrix.h>
 #include "mtrr.h"
 
 int arr3_protected;
