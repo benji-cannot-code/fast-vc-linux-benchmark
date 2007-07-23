4adba4de7579ce6d3c193665bf060e2ed63d416e
Jesper Juhl <jesper.juhl@gmail.com>
2007-07-23 12:14:53 +0200

    [ALSA] Clean up duplicate includes in sound/ppc/
    This patch cleans up duplicate includes in
    	sound/ppc/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/ppc/snd_ps3.c
+++ sound/ppc/snd_ps3.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dmapool.h>
 #include <linux/dma-mapping.h>
 #include <asm/firmware.h>
-#include <linux/io.h>
 #include <asm/dma.h>
 #include <asm/lv1call.h>
 #include <asm/ps3.h>
