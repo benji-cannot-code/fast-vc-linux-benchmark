208e9d87316234a4b12405b5a9d48d4dd287c868
Jesper Juhl <jesper.juhl@gmail.com>
2007-07-23 12:15:42 +0200

    [ALSA] Clean up duplicate includes in sound/core/
    This patch cleans up duplicate includes in
    	sound/core/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/core/rawmidi.c
+++ sound/core/rawmidi.c
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mutex.h>
 #include <linux/moduleparam.h>
 #include <linux/delay.h>
-#include <linux/wait.h>
 #include <sound/rawmidi.h>
 #include <sound/info.h>
 #include <sound/control.h>
