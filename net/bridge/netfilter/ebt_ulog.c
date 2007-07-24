7b69d947d7e5868e0b26e346858a6d696a22e45d
Jesper Juhl <jesper.juhl@gmail.com>
2007-07-24 15:30:30 -0700

    [NETFILTER]: Clean up duplicate includes in net/bridge/
    This patch cleans up duplicate includes in
    	net/bridge/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/netfilter/ebt_ulog.c
+++ net/bridge/netfilter/ebt_ulog.c
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/timer.h>
 #include <linux/netlink.h>
 #include <linux/netdevice.h>
-#include <linux/module.h>
 #include <linux/netfilter_bridge/ebtables.h>
 #include <linux/netfilter_bridge/ebt_ulog.h>
 #include <net/sock.h>
