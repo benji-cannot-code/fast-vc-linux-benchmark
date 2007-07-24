27a43db1b5cd4d7295acb796418127a501c4a57e
Dale Farnsworth <dale@farnsworth.org>
2007-07-24 11:12:24 -0700

    [WATCHDOG] mv64x60_wdt: Add arch/powerpc platform support
    Add support for arch/powerpc, specifically for the prpmc2800 platform.
    
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>

--- drivers/char/watchdog/mv64x60_wdt.c
+++ drivers/char/watchdog/mv64x60_wdt.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/watchdog.h>
 #include <linux/platform_device.h>
 
-#include <asm/mv64x60.h>
+#include <linux/mv643xx.h>
 #include <asm/uaccess.h>
 #include <asm/io.h>
 
