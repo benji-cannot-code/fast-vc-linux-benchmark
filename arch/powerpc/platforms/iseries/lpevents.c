10a048744d6badb9e5f8132a2858c34f7661856b
Stephen Rothwell <sfr@canb.auug.org.au>
2007-07-25 09:29:19 +1000

    [POWERPC] iSeries: Fix section mismatch warning in lpevents
    WARNING: vmlinux.o(.text+0x4f568): Section mismatch: reference to .init.text:.__alloc_bootmem (between '.setup_hvlpevent_queue' and '.process_hvlpevents')
    
    setup_hvlpevent_queue is only called from __init code so make it __init
    as well.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/iseries/lpevents.c
+++ arch/powerpc/platforms/iseries/lpevents.c
@@ -183,7 +183,7 @@ static int set_spread_lpevents(char *str)
 }
 __setup("spread_lpevents=", set_spread_lpevents);
 
-void setup_hvlpevent_queue(void)
+void __init setup_hvlpevent_queue(void)
 {
 	void *eventStack;
 
