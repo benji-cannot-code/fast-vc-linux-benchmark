209316636c4089737e3c54866b3ab8038119d92b
Randy Dunlap <randy.dunlap@oracle.com>
2007-07-26 10:10:35 -0700

    x86 boot code comments typos
    Fix comments typos in new x86 boot code.
    
    Signed-off-by: Randy Dunlap <randy.dunlap@oracle.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- arch/i386/boot/code16gcc.h
+++ arch/i386/boot/code16gcc.h
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * code16gcc.h
  *
  * This file is -include'd when compiling 16-bit C code.
- * Note: this asm() needs to be emitted before gcc omits any code.
+ * Note: this asm() needs to be emitted before gcc emits any code.
  * Depending on gcc version, this requires -fno-unit-at-a-time or
  * -fno-toplevel-reorder.
  *
