feb374fa802dd14be542c1e1472c1c3651d9b836
Ramkrishna Vepa <ram.vepa@neterion.com>
2007-07-25 19:43:12 -0700

    S2io: Increment received packet count correctly
     - Fix to increment the received packet count correctly.
    
    (Resending; Removed HTML sections in the patch)
    
    Signed-off-by: Santosh Rastapur <santosh.rastapur@neterion.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/s2io.c
+++ drivers/net/s2io.c
@@ -85,7 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "s2io.h"
 #include "s2io-regs.h"
 
-#define DRV_VERSION "2.0.24.1"
+#define DRV_VERSION "2.0.25.1"
 
 /* S2io Driver name & version. */
 static char s2io_driver_name[] = "Neterion";
@@ -6735,6 +6735,7 @@ static int rx_osm_handler(struct ring_info *ring_data, struct RxD_t * rxdp)
 	}
 
 	/* Updating statistics */
+	sp->stats.rx_packets++;
 	rxdp->Host_Control = 0;
 	if (sp->rxd_mode == RXD_MODE_1) {
 		int len = RXD_GET_BUFFER0_SIZE_1(rxdp->Control_2);
