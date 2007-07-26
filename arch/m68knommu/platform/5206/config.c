879f187c53ab739cea4de4863b7c1b066fb19f32
Greg Ungerer <gerg@snapgear.com>
2007-07-27 01:09:00 +1000

    m68knommu: remove use of colfire_trap_init
    The switch to using the generic irq framework removed the
    coldfire_trap_init() code, so remove all references to it.
    
    Signed-off-by: Greg Ungerer <gerg@uclinux.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/m68knommu/platform/5206/config.c
+++ arch/m68knommu/platform/5206/config.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 void coldfire_tick(void);
 void coldfire_timer_init(irq_handler_t handler);
 unsigned long coldfire_timer_offset(void);
-void coldfire_trap_init(void);
 void coldfire_reset(void);
 
 /***************************************************************************/
@@ -102,7 +101,6 @@ void config_BSP(char *commandp, int size)
 	mach_sched_init = coldfire_timer_init;
 	mach_tick = coldfire_tick;
 	mach_gettimeoffset = coldfire_timer_offset;
-	mach_trap_init = coldfire_trap_init;
 	mach_reset = coldfire_reset;
 }
 
