d2308a32d9bf374f24818da75482e80e45cf972c
Ben Dooks <ben-linux@fluff.org>
2007-07-26 10:40:59 -0700

    fixup s3c24xx build after arch moves
    Fix the include files moved around during the s3c24xx arch moves.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/spi/spi_s3c24xx.c
+++ drivers/spi/spi_s3c24xx.c
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/hardware.h>
 
 #include <asm/arch/regs-gpio.h>
-#include <asm/arch/regs-spi.h>
+#include <asm/plat-s3c24xx/regs-spi.h>
 #include <asm/arch/spi.h>
 
 struct s3c24xx_spi {
