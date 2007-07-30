58b5fdcaef58dee7aa77ceebb77ba04c04a2ec94
Songmao Tian <tiansm@lemote.com>
2007-07-30 09:06:45 +0800

    [MIPS] Fulong: Remove unneeded header file
    Signed-off-by: Songmao Tian <tiansm@lemote.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/lemote/lm2e/prom.c
+++ arch/mips/lemote/lm2e/prom.c
@@ -16,11 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * option) any later version.
  */
 #include <linux/init.h>
-#include <linux/mm.h>
-#include <linux/sched.h>
 #include <linux/bootmem.h>
-
-#include <asm/addrspace.h>
 #include <asm/bootinfo.h>
 
 extern unsigned long bus_clock;
--- arch/mips/lemote/lm2e/setup.c
+++ arch/mips/lemote/lm2e/setup.c
@@ -29,17 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/bootmem.h>
 #include <linux/init.h>
-#include <linux/io.h>
-#include <linux/ioport.h>
-#include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <linux/kernel.h>
-#include <linux/mc146818rtc.h>
-#include <linux/mm.h>
-#include <linux/module.h>
-#include <linux/pci.h>
-#include <linux/tty.h>
-#include <linux/types.h>
 
 #include <asm/bootinfo.h>
 #include <asm/mc146818-time.h>
