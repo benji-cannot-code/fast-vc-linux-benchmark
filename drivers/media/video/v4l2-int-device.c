01286ac9957dbbe058e5ff3c1cda4481cfe1e9c0
Adrian Bunk <bunk@stusta.de>
2007-07-30 11:43:55 -0300

    V4L/DVB (5940): Export v4l2_int_device_{, un}register
    This patch fixes the following build error:
    
    <--  snip  -->
    
    ...
      MODPOST 2135 modules
    make[2]: *** [__modpost] Error 1
    
    <--  snip  -->
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/v4l2-int-device.c
+++ drivers/media/video/v4l2-int-device.c
@@ -102,6 +102,7 @@ int v4l2_int_device_register(struct v4l2_int_device *d)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(v4l2_int_device_register);
 
 void v4l2_int_device_unregister(struct v4l2_int_device *d)
 {
@@ -115,6 +116,7 @@ void v4l2_int_device_unregister(struct v4l2_int_device *d)
 	}
 	mutex_unlock(&mutex);
 }
+EXPORT_SYMBOL_GPL(v4l2_int_device_unregister);
 
 /* Adapted from search_extable in extable.c. */
 static v4l2_int_ioctl_func *find_ioctl(struct v4l2_int_slave *slave, int cmd,
