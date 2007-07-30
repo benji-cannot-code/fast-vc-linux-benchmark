3ab86f8fe37f766aacbd75b448a32f4c414bb2a7
Alexey Dobriyan <adobriyan@gmail.com>
2007-07-30 23:08:46 +0400

    Fallout from "Remove fs.h from mm.h" patch
    While I was busy compile-testing my patch, ENOSYS sneaked into pm.h
    leading to some compile-breakages mostly on ia64 and some mips configs.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/pm.h
+++ include/linux/pm.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/list.h>
 #include <asm/atomic.h>
+#include <asm/errno.h>
 
 /*
  * Power management requests... these are passed to pm_send_all() and friends.
