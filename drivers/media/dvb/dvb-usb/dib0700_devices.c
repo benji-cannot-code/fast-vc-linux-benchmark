dbd106f88401109b85cb90edfa51d2cfeb013e81
Patrick Boettcher <pb@linuxtv.org>
2007-07-30 17:02:17 -0300

    V4L/DVB (5959): Fix for size of remote keys in DiB0700
    Fix for size of remote control keys in DiB0700.
    
    Signed-off-by: Patrick Boettcher <pb@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/dib0700_devices.c
+++ drivers/media/dvb/dvb-usb/dib0700_devices.c
@@ -915,7 +915,7 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 
 		.rc_interval      = DEFAULT_RC_INTERVAL,
 		.rc_key_map       = dib0700_rc_keys,
-		.rc_key_map_size  = sizeof(dib0700_rc_keys),
+		.rc_key_map_size  = ARRAY_SIZE(dib0700_rc_keys),
 		.rc_query         = dib0700_rc_query
 	}, { DIB0700_DEFAULT_DEVICE_PROPERTIES,
 
@@ -956,7 +956,7 @@ struct dvb_usb_device_properties dib0700_devices[] = {
 
 		.rc_interval      = DEFAULT_RC_INTERVAL,
 		.rc_key_map       = dib0700_rc_keys,
-		.rc_key_map_size  = sizeof(dib0700_rc_keys),
+		.rc_key_map_size  = ARRAY_SIZE(dib0700_rc_keys),
 		.rc_query         = dib0700_rc_query
 
 	}, { DIB0700_DEFAULT_DEVICE_PROPERTIES,
