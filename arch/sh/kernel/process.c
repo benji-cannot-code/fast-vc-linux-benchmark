bf5ad765c5692f1bca0f7d48caefb85bcab87d69
Paul Mundt <lethal@linux-sh.org>
2007-07-31 13:01:43 +0900

    sh: Fix fs.h removal from mm.h regressions.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/process.c
+++ arch/sh/kernel/process.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kdebug.h>
 #include <linux/tick.h>
 #include <linux/reboot.h>
+#include <linux/fs.h>
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
 #include <asm/pgalloc.h>
