ec2768fa0f46634687f59e099f948bcf19ee1d8b
Ralf Baechle <ralf@linux-mips.org>
2007-07-31 00:38:47 -0700

    kbuild: whitelist references from __dbe_table to .init
    This is needed on MIPS where the same mechanism as get_user() is used to
    intercept bus error exceptions for some hardware probes.  Without this
    patch modpost will throw spurious warnings:
    
      LD      vmlinux
      SYSMAP  System.map
      SYSMAP  .tmp_System.map
      MODPOST vmlinux
    WARNING: arch/mips/sgi-ip22/built-in.o(__dbe_table+0x0): Section mismatch: reference to .init.text:
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1122,6 +1122,7 @@ static int initexit_section_ref_ok(const char *name)
 		".machvec",		/* ia64 + powerpc uses these */
 		".machine.desc",
 		".opd",			/* See comment [OPD] */
+		"__dbe_table",
 		".parainstructions",
 		".pdr",
 		".plt",			/* seen on ARCH=um build on x86_64. Harmless */
