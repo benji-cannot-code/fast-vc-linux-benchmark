b58be1f5b9939cd31ad7b359b3bcdd139f3cbc10
Grant Likely <grant.likely@secretlab.ca>
2007-07-31 00:37:38 -0700

    xilinxfb:Remove __initdata from xilinx_fb_fix and xilinx_fb_var
    xilinxfb_drv_probe refers to both tables, but it cannot be initdata.
    
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>
    Cc: Andrei Konovalov <akonovalov@ru.mvista.com>
    Signed-off-by: Antonino Daplas <adaplas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/xilinxfb.c
+++ drivers/video/xilinxfb.c
@@ -80,7 +80,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * Here are the default fb_fix_screeninfo and fb_var_screeninfo structures
  */
-static struct fb_fix_screeninfo xilinx_fb_fix __initdata = {
+static struct fb_fix_screeninfo xilinx_fb_fix = {
 	.id =		"Xilinx",
 	.type =		FB_TYPE_PACKED_PIXELS,
 	.visual =	FB_VISUAL_TRUECOLOR,
@@ -89,7 +89,7 @@ static struct fb_fix_screeninfo xilinx_fb_fix __initdata = {
 	.accel =	FB_ACCEL_NONE
 };
 
-static struct fb_var_screeninfo xilinx_fb_var __initdata = {
+static struct fb_var_screeninfo xilinx_fb_var = {
 	.xres =			XRES,
 	.yres =			YRES,
 	.xres_virtual =		XRES_VIRTUAL,
