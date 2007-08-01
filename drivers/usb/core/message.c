afee9d6cf8d2ec19c7f8e50ce6bb6c59f1a2f2a7
Inaky Perez-Gonzalez <inaky@linux.intel.com>
2007-07-31 20:34:01 -0700

    usb: usb_set_configuration() obeys authorization
    Will refuse to configure a non-authorized device.
    
    Update: simplified if statement--thanks to Ragner Magalhaes for the
    heads up.
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/core/message.c
+++ drivers/usb/core/message.c
@@ -1484,6 +1484,9 @@ static struct usb_interface_assoc_descriptor *find_iad(struct usb_device *dev,
  * channels are available independently; and choosing between open
  * standard device protocols (like CDC) or proprietary ones.
  *
+ * Note that a non-authorized device (dev->authorized == 0) will only
+ * be put in unconfigured mode.
+ *
  * Note that USB has an additional level of device configurability,
  * associated with interfaces.  That configurability is accessed using
  * usb_set_interface().
@@ -1505,7 +1508,7 @@ int usb_set_configuration(struct usb_device *dev, int configuration)
 	struct usb_interface **new_interfaces = NULL;
 	int n, nintf;
 
-	if (configuration == -1)
+	if (dev->authorized == 0 || configuration == -1)
 		configuration = 0;
 	else {
 		for (i = 0; i < dev->descriptor.bNumConfigurations; i++) {
