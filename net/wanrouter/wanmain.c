171d7219c4e52adc463c9008febb5ae487f1b237
Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
2007-08-01 21:50:44 -0700

    [NET]: Removal of duplicated include net/wanrouter/wanmain.c
    Signed-off-by: Michal Piotrowski <michal.k.k.piotrowski@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/wanrouter/wanmain.c
+++ net/wanrouter/wanmain.c
@@ -47,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/capability.h>
 #include <linux/errno.h>	/* return codes */
 #include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/module.h>	/* support for loadable modules */
 #include <linux/slab.h>		/* kmalloc(), kfree() */
 #include <linux/mm.h>
