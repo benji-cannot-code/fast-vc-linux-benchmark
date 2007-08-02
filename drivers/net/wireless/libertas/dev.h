e6c2215f750768c6a9b12addbe933a47f91a7574
Holger Schurig <hs4233@mail.mn-solutions.de>
2007-08-02 13:12:27 -0400

    [PATCH] libertas: remove adapter->scantype
    scantype was initialized with CMD_SCAN_TYPE_ACTIVE, but there is no code
    that would ever change it, so we can use that variable directly.
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/dev.h
+++ drivers/net/wireless/libertas/dev.h
@@ -263,8 +263,6 @@ struct _wlan_adapter {
 	struct list_head network_free_list;
 	struct bss_descriptor *networks;
 
-	u8 scantype;
-
 	u8 adhoccreate;
 
 	/** capability Info used in Association, start, join */
