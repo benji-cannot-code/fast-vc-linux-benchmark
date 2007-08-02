1748a2f4e4c0e08b8a93ba16b1d47e4a5d052326
Holger Schurig <hs4233@mail.mn-solutions.de>
2007-08-02 13:09:34 -0400

    [PATCH] libertas: remove adapter->locallisteninterval
    locallisteninterval was initialized with 0, but there is no code that
    changes it, rendering it rather useless.
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/main.c
+++ drivers/net/wireless/libertas/main.c
@@ -1060,7 +1060,6 @@ static void wlan_init_adapter(wlan_private * priv)
 
 	adapter->psstate = PS_STATE_FULL_POWER;
 	adapter->needtowakeup = 0;
-	adapter->locallisteninterval = 0;	/* default value in firmware will be used */
 
 	adapter->intcounter = 0;
 
