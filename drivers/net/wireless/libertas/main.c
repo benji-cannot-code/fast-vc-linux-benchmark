5cfb19fdce542b33eafe807da31b959357d652c1
Dan Williams <dcbw@redhat.com>
2007-08-03 09:43:03 -0400

    [PATCH] libertas: bump driver version
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/main.c
+++ drivers/net/wireless/libertas/main.c
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "assoc.h"
 #include "join.h"
 
-#define DRIVER_RELEASE_VERSION "322.p1"
+#define DRIVER_RELEASE_VERSION "323.p0"
 const char libertas_driver_version[] = "COMM-USB8388-" DRIVER_RELEASE_VERSION
 #ifdef  DEBUG
     "-dbg"
