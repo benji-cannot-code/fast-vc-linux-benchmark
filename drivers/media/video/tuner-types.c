92f2d0f7005d1ceef689373ca96c640b449a99f5
Trent Piepho <xyzzy@speakeasy.org>
2007-08-03 18:32:38 -0300

    V4L/DVB (5978): tuner: Better tuner radio support
    Add radio support for the Thomson DTT7612 tuner.
    
    This tuner uses a different 1st intermediate frequency than the other radio
    tuners supported (a lot of NTSC radio tuners probably need this change too).
    
    Add a new tuner-simple parameter, radio_if.  It selects the 1st IF used for
    radio reception.  The radio frequency setting code in tuner-simple now uses
    this field, instead of a special case select() block for each tuner with radio
    support.
    
    The tuner parameters for tuners that used a 33.3 MHz RIF now set radio_if to 1
    in tuner-types.c.
    
    The Thomson DTT7612 gets radio_if = 2, also add has_tda9887 = 1 and
    fm_gain_normal = 1.
    
    Add some defines for tda9887 bits that control IF setting in radio mode.
    
    Add a new tda9887 config option, TDA9887_RIF_41_3, that selects a 41.3 MHz
    radio IF.
    
    Fix the way tda9887 radio options work.  The driver was modifying the default
    radio mode config templates based on the TDA9887_XXXX flags.  This means that
    _all_ tuners would get the same settings.  If you had a one tuner than used
    TDA9887_GAIN_NORMAL and one that didn't, both would get the setting.  Now the
    tda9987 driver just checks if tuner mode is radio and then applies the config
    settings directly to the data being sent, just like how all the TV mode
    settings already work.
    
    The PLL setting math is made a little more accurate.
    
    And a grammar error in a printk is fixed.
    
    Signed-off-by:  Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tuner-types.c
+++ drivers/media/video/tuner-types.c
@@ -653,6 +653,7 @@ static struct tuner_params tuner_microtune_4049_fm5_params[] = {
 		.port1_invert_for_secam_lc = 1,
 		.default_pll_gating_18 = 1,
 		.fm_gain_normal=1,
+		.radio_if = 1, /* 33.3 MHz */
 	},
 };
 
@@ -734,6 +735,7 @@ static struct tuner_params tuner_philips_fm1256_ih3_params[] = {
 		.type   = TUNER_PARAM_TYPE_PAL,
 		.ranges = tuner_fm1236_mk3_ntsc_ranges,
 		.count  = ARRAY_SIZE(tuner_fm1236_mk3_ntsc_ranges),
+		.radio_if = 1, /* 33.3 MHz */
 	},
 };
 
@@ -860,6 +862,9 @@ static struct tuner_params tuner_thomson_dtt761x_params[] = {
 		.type   = TUNER_PARAM_TYPE_NTSC,
 		.ranges = tuner_thomson_dtt761x_ntsc_ranges,
 		.count  = ARRAY_SIZE(tuner_thomson_dtt761x_ntsc_ranges),
+		.has_tda9887 = 1,
+		.fm_gain_normal = 1,
+		.radio_if = 2, /* 41.3 MHz */
 	},
 };
 
