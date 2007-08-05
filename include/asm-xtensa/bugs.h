c7b0b626db9eee4938ca55bfdd919c30ab1ca9c6
Chris Zankel <chris@zankel.net>
2007-08-05 10:16:54 -0700

    [XTENSA] Remove extraneous include statement
    We don't need to include processor.h in bug.h.
    
    Signed-off-by: Chris Zankel <chris@zankel.net>

--- include/asm-xtensa/bugs.h
+++ include/asm-xtensa/bugs.h
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _XTENSA_BUGS_H
 #define _XTENSA_BUGS_H
 
-#include <asm/processor.h>
-
-static void __init check_bugs(void)
-{
-}
+static void check_bugs(void) { }
 
 #endif /* _XTENSA_BUGS_H */
