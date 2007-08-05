53bda036b481c066a098c74b1430454270c4d613
Mark Fortescue <mark@mtfhpc.demon.co.uk>
2007-08-04 21:17:18 -0700

    [SPARC32]: Fix build.
    Correct incorrect removal of asm-generic/fcntl.h from asm-sparc/fcntl.h by 
    commit 6ba60d2195cd65d72eaf7ce3903a707c5bf20c7b.
    
    Signed-off-by: Mark Fortescue <mark@mtfhpc.demon.co.uk>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc/fcntl.h
+++ include/asm-sparc/fcntl.h
@@ -33,5 +33,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ARCH_FLOCK_PAD	short __unused;
 #define __ARCH_FLOCK64_PAD	short __unused;
 
+#include <asm-generic/fcntl.h>
 
 #endif
