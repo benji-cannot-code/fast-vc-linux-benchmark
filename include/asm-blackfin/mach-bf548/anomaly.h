79c7c66ab1c0cf94eae3e41900c1ae4ca3508312
Bryan Wu <bryan.wu@analog.com>
2007-08-05 15:31:16 +0800

    Blackfin arch: kill ezkit548 compiling warning
    kill ezkit548 compiling warning:
      - include/asm/cplb.h:42:5: warning: "ANOMALY_05000158" is not defined
    
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- include/asm-blackfin/mach-bf548/anomaly.h
+++ include/asm-blackfin/mach-bf548/anomaly.h
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* Anomalies that don't exist on this proc */
 #define ANOMALY_05000125 (0)
+#define ANOMALY_05000158 (0)
 #define ANOMALY_05000183 (0)
 #define ANOMALY_05000198 (0)
 #define ANOMALY_05000244 (0)
