1b4537e47d06a0d4899e60382fed4341dd216b56
Mariusz Kozlowski <m.kozlowski@tuxland.pl>
2007-08-06 18:05:35 -0300

    V4L/DVB (5982): Dev.c: memset fix
    Looks like memset() is zeroing wrong nr of bytes.
    
    Signed-off-by: Mariusz Kozlowski <m.kozlowski@tuxland.pl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/videodev.c
+++ drivers/media/video/videodev.c
@@ -449,7 +449,7 @@ static int __video_do_ioctl(struct inode *inode, struct file *file,
 	if (cmd == VIDIOCGMBUF) {
 		struct video_mbuf *p=arg;
 
-		memset(p,0,sizeof(p));
+		memset(p, 0, sizeof(*p));
 
 		if (!vfd->vidiocgmbuf)
 			return ret;
