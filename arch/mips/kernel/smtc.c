5048156248b0a2d3f943c54d5b7d1a1f99f5bfb1
Ralf Baechle <ralf@linux-mips.org>
2007-08-06 16:35:23 +0100

    [MIPS] SMTC: Move MIPS_CPU_IPI_IRQ definition into header.
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/kernel/smtc.c
+++ arch/mips/kernel/smtc.c
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * This file should be built into the kernel only if CONFIG_MIPS_MT_SMTC is set.
  */
 
-#define MIPS_CPU_IPI_IRQ	1
-
 #define LOCK_MT_PRA() \
 	local_irq_save(flags); \
 	mtflags = dmt()
