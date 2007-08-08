7b0f76f495927b1ac2c0cef6a33011ed2baf3399
Johannes Berg <johannes@sipsolutions.net>
2007-08-07 18:02:43 -0700

    [NET] net/core/utils: fix sparse warning
    net_msg_warn is not defined because it is in net/sock.h which isn't
    included.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/utils.c
+++ net/core/utils.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/random.h>
 #include <linux/percpu.h>
 #include <linux/init.h>
+#include <net/sock.h>
 
 #include <asm/byteorder.h>
 #include <asm/system.h>
