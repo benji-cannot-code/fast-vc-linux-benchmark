1a7dcbcac840fbf1a0c3e3d8dcce08155c820a91
Alan Stern <stern@rowland.harvard.edu>
2007-08-08 17:16:12 -0400

    USB: remove DEBUG definition from dummy_hcd
    This patch (as958) removes an unneeded and unwanted #define line from
    dummy_hcd.
    
    Signed-off-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/dummy_hcd.c
+++ drivers/usb/gadget/dummy_hcd.c
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * bypassing some hardware (and driver) issues.  UML could help too.
  */
 
-#define DEBUG
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
