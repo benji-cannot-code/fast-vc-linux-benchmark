09183e204778bb3436b67c4baeac7592634daaf1
Nathan Lynch <ntl@pobox.com>
2007-08-10 05:18:44 +1000

    [POWERPC] chrp pci_ops: Use named structure member initializers
    Signed-off-by: Nathan Lynch <ntl@pobox.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/chrp/pci.c
+++ arch/powerpc/platforms/chrp/pci.c
@@ -87,8 +87,8 @@ int gg2_write_config(struct pci_bus *bus, unsigned int devfn, int off,
 
 static struct pci_ops gg2_pci_ops =
 {
-	gg2_read_config,
-	gg2_write_config
+	.read = gg2_read_config,
+	.write = gg2_write_config,
 };
 
 /*
@@ -125,8 +125,8 @@ int rtas_write_config(struct pci_bus *bus, unsigned int devfn, int offset,
 
 static struct pci_ops rtas_pci_ops =
 {
-	rtas_read_config,
-	rtas_write_config
+	.read = rtas_read_config,
+	.write = rtas_write_config,
 };
 
 volatile struct Hydra __iomem *Hydra = NULL;
