478860d7c14d28af16a2ea5640cbcca123b2ebba
David S. Miller <davem@sunset.davemloft.net>
2007-08-08 17:11:39 -0700

    [SPARC64]: Do not assume sun4v chips have load-twin/store-init support.
    Check the cpu type in the OBP device tree before committing to
    using the optimized Niagara memcpy and memset implementation.
    
    If we don't recognize the cpu type, use a completely generic
    version.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/oplib.h
+++ include/asm-sparc64/oplib.h
@@ -1,9 +1,8 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* $Id: oplib.h,v 1.14 2001/12/19 00:29:51 davem Exp $
- * oplib.h:  Describes the interface and available routines in the
+/* oplib.h:  Describes the interface and available routines in the
  *           Linux Prom library.
  *
- * Copyright (C) 1995 David S. Miller (davem@caip.rutgers.edu)
+ * Copyright (C) 1995, 2007 David S. Miller (davem@davemloft.net)
  * Copyright (C) 1996 Jakub Jelinek (jj@sunsite.mff.cuni.cz)
  */
 
@@ -32,8 +31,10 @@ extern int prom_chosen_node;
 extern const char prom_peer_name[];
 extern const char prom_compatible_name[];
 extern const char prom_root_compatible[];
+extern const char prom_cpu_compatible[];
 extern const char prom_finddev_name[];
 extern const char prom_chosen_path[];
+extern const char prom_cpu_path[];
 extern const char prom_getprop_name[];
 extern const char prom_mmu_name[];
 extern const char prom_callmethod_name[];
