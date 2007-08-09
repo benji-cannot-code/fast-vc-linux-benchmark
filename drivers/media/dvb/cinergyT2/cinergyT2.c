2ee69f865be111cfe46b169aefb7ccb43c251ec9
Oleg Nesterov <oleg@tv-sign.ru>
2007-08-09 12:02:30 -0300

    V4L/DVB (5990): cinergyt2_suspend: don't forget to unlock cinergyt2->wq_sem
    Restore unlock of cinergyt2->wq_sem, was deleted by accident.
    
    Signed-off-by: Oleg Nesterov <oleg@tv-sign.ru>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/cinergyT2/cinergyT2.c
+++ drivers/media/dvb/cinergyT2/cinergyT2.c
@@ -1009,6 +1009,8 @@ static int cinergyt2_suspend (struct usb_interface *intf, pm_message_t state)
 	cinergyt2_sleep(cinergyt2, 1);
 	mutex_unlock(&cinergyt2->sem);
 
+	mutex_unlock(&cinergyt2->wq_sem);
+
 	return 0;
 }
 
