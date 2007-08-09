d9bddfce4eeb36ad97cfa6cbb72e8de94ddc7067
Hans Verkuil <hverkuil@xs4all.nl>
2007-08-09 17:13:22 -0300

    V4L/DVB (6003): vp27smpx: correctly attribute the origin of the driver
    Correctly attribute the origin of the driver to Kazuhiko 
    Kawakami.
    
    It took some time to get the S-O-B line from the original 
    tvaudio patch author, but here it is.
    
    Signed-off-by: Kazuhiko Kawakami <kazz-0@mail.goo.ne.jp>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/vp27smpx.c
+++ drivers/media/video/vp27smpx.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 2007 Hans Verkuil <hverkuil@xs4all.nl>
  *
- * Special thanks to Kazz for the i2c data.
+ * Based on a tvaudio patch from Kazuhiko Kawakami <kazz-0@mail.goo.ne.jp>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
