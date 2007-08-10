24b2d0c66fbf8d9b9edc644161d90d20ad187c06
Adrian Bunk <bunk@stusta.de>
2007-08-10 13:51:09 -0700

    drivers/base/power/: make 2 functions static
    suspend_device() and resume_device() can now become static.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/power/suspend.c
+++ drivers/base/power/suspend.c
@@ -55,7 +55,7 @@ suspend_device_dbg(struct device *dev, pm_message_t state, char *info)
  *	@state:	Power state device is entering.
  */
 
-int suspend_device(struct device * dev, pm_message_t state)
+static int suspend_device(struct device * dev, pm_message_t state)
 {
 	int error = 0;
 
