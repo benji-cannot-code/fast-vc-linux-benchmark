5b83a49514ec02bf7b0874d290c582a455ba752e
Felipe Balbi <felipe.lima@indt.org.br>
2007-08-10 09:34:26 -0400

    USB: SisUSB2VGA: Remove if 0'ed code
    Unused code should be removed. We don't need to increase
    the size of the file with dead code inside if 0 statements.
    
    Signed-off-by: Felipe Balbi <felipe.lima@indt.org.br>
    Cc: Thomas Winischhofer <thomas@winischhofer.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/misc/sisusbvga/sisusb.h
+++ drivers/usb/misc/sisusbvga/sisusb.h
@@ -47,9 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* For older kernels, support for text consoles is by default
  * off. To ensable text console support, change the following:
  */
-#if 0
-#define CONFIG_USB_SISUSBVGA_CON
-#endif
+/* #define CONFIG_USB_SISUSBVGA_CON */
 
 /* Version Information */
 
