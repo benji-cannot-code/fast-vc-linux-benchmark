de84770caee250b2b71a52c7d9f5262c4e0fe201
Adrian Bunk <bunk@stusta.de>
2007-08-10 14:50:42 -0700

    [SCSI] scsi_error.c should #include "scsi_transport_api.h"
    Every file should #include the headers containing the prototypes for its
    global functions (in this case for scsi_schedule_eh()).
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/scsi_error.c
+++ drivers/scsi/scsi_error.c
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "scsi_priv.h"
 #include "scsi_logging.h"
+#include "scsi_transport_api.h"
 
 #define SENSE_TIMEOUT		(10*HZ)
 
