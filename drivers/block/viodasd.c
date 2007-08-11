b697ded22101b5537366a70464d6492c3f922576
Jesper Juhl <jesper.juhl@gmail.com>
2007-08-11 22:34:27 +0200

    Clean up duplicate includes in drivers/block/
    This patch cleans up duplicate includes in
    	drivers/block/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Acked-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Cc: Satyam Sharma <satyam.sharma@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- drivers/block/viodasd.c
+++ drivers/block/viodasd.c
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dma-mapping.h>
 #include <linux/completion.h>
 #include <linux/device.h>
-#include <linux/kernel.h>
 
 #include <asm/uaccess.h>
 #include <asm/vio.h>
