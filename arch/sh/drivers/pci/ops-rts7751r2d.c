b4f35e61bbd218e96b9ee67d3aaac64fb34f7239
Magnus Damm <damm@igel.co.jp>
2007-08-12 15:29:16 +0900

    sh: intc - convert board specific r2d code
    This patch converts the board specific interrupt code for r2d to make
    use of intc. While at it we improve the Kconfig to avoid confusion.
    
    - Two sets of interrupt tables exist - one for R2D-1 and one for R2D-PLUS.
    - R2D-1 and R2D-PLUS use the same irq constants.
    - R2D-1 has AX88796 support, R2D-PLUS does not hook up that IRQ.
    - R2D-PLUS has KEY support, R2D-1 does not hook up that IRQ.
    - The number and order of IRQ values are disconnected from register bits.
    - Interrupt sources now start from IRQ 100.
    - The machvec demux function converts from irlm IRQ 0-14 to IRQ 100++.
    
    Tested on R2D-1 and R2D-PLUS boards.
    
    Version 2 adds CONFIG_RTS7751R2D_1 and CONFIG_RTS7751R2D_PLUS together
    with intc structured as __initdata.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/drivers/pci/ops-rts7751r2d.c
+++ arch/sh/drivers/pci/ops-rts7751r2d.c
@@ -20,10 +20,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pci-sh4.h"
 
 static u8 rts7751r2d_irq_tab[] __initdata = {
-	IRQ_PCISLOT1,
-	IRQ_PCISLOT2,
-	IRQ_PCMCIA,
-	IRQ_PCIETH,
+	IRQ_PCI_INTA,
+	IRQ_PCI_INTB,
+	IRQ_PCI_INTC,
+	IRQ_PCI_INTD,
 };
 
 int __init pcibios_map_platform_irq(struct pci_dev *pdev, u8 slot, u8 pin)
