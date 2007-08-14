c2e1f5413e6c26ff69ed1fe3c5ed23badabdf0ec
Patrick McHardy <kaber@trash.net>
2007-08-14 13:14:35 -0700

    [NETFILTER]: nf_conntrack_sip: fix SIP-URI parsing
    The userinfo component of a SIP-URI is optional, continue parsing at the
    beginning of the SIP-URI in case its not found.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/nf_conntrack_sip.c
+++ net/netfilter/nf_conntrack_sip.c
@@ -296,6 +296,7 @@ static int epaddr_len(struct nf_conn *ct, const char *dptr,
 static int skp_epaddr_len(struct nf_conn *ct, const char *dptr,
 			  const char *limit, int *shift)
 {
+	const char *start = dptr;
 	int s = *shift;
 
 	/* Search for @, but stop at the end of the line.
@@ -310,8 +311,10 @@ static int skp_epaddr_len(struct nf_conn *ct, const char *dptr,
 	if (dptr <= limit && *dptr == '@') {
 		dptr++;
 		(*shift)++;
-	} else
+	} else {
+		dptr = start;
 		*shift = s;
+	}
 
 	return epaddr_len(ct, dptr, limit, shift);
 }
