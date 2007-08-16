032dcccd1985f70f43fa7f6e36ba33dee0fc61cf
Magnus Damm <damm@igel.co.jp>
2007-08-17 00:50:44 +0900

    sh: intc - primary priority masking fixes
    This patch contains various intc fixes for problems reported by
    Markus Brunner on the linuxsh-dev mailing list:
    
    http://marc.info/?l=linuxsh-dev&m=118701948224991&w=1
    
    Apart from added comments, the fixes are:
    
    - add intc_set_priority() function prototype to hw_irq.h
    - fix off-by-one error in intc_set_priority()
    - make sure _INTC_WIDTH() is set for primary priority masking
    
    Big thanks to Markus for finding these problems. Version two fixes
    a compile error and an inverted primary check.
    
    Signed-off-by: Magnus Damm <damm@igel.co.jp>
    Acked-by: Markus Brunner <super.firetwister@gmail.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/hw_irq.h
+++ include/asm-sh/hw_irq.h
@@ -91,6 +91,7 @@ struct intc_desc symbol __initdata = {					\
 }
 
 void __init register_intc_controller(struct intc_desc *desc);
+int intc_set_priority(unsigned int irq, unsigned int prio);
 
 void __init plat_irq_setup(void);
 
