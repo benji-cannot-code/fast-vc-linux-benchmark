d502a482285c209bf5afcf0e9718c1c4880d9d55
Hans Verkuil <hverkuil@xs4all.nl>
2007-08-19 07:11:44 -0300

    V4L/DVB (6057): ivtv-fb: remove unused header includes
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/ivtv/ivtv-fb.c
+++ drivers/media/video/ivtv/ivtv-fb.c
@@ -58,12 +58,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 #include "ivtv-driver.h"
-#include "ivtv-queue.h"
 #include "ivtv-udma.h"
-#include "ivtv-irq.h"
-#include "ivtv-fileops.h"
 #include "ivtv-mailbox.h"
-#include "ivtv-cards.h"
 #include <media/ivtv-fb.h>
 
 /* card parameters */
