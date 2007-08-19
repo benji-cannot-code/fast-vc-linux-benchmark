3b0490ce1b960b50316d3998f8d8506a8c4adb0d
Christian Heim <phreak@gentoo.org>
2007-08-19 13:07:59 +0200

    Remove double inclusion of linux/capability.h
    Remove the second inclusion of linux/capability.h, which has been
    introduced with "[PATCH] move capable() to capability.h" (commit
    c59ede7b78db329949d9cdcd7064e22d357560ef)
    
    Signed-off-by: Christian Heim <phreak@gentoo.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/capability.h>
 #include <linux/ctype.h>
 #include <linux/utsname.h>
-#include <linux/capability.h>
 #include <linux/smp_lock.h>
 #include <linux/fs.h>
 #include <linux/init.h>
