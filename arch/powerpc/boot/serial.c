79f8a078739360ee6c100a434582f9f2b204b663
Scott Wood <scottwood@freescale.com>
2007-08-21 03:39:55 +1000

    [POWERPC] bootwrapper: Declare udelay() in ops.h
    Declarations in various users are removed.
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/boot/serial.c
+++ arch/powerpc/boot/serial.c
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "io.h"
 #include "ops.h"
 
-extern void udelay(long delay);
-
 static int serial_open(void)
 {
 	struct serial_console_data *scdp = console_ops.data;
