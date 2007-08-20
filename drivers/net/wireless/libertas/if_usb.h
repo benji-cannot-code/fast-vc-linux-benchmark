d0551727b6c362735056651f327e175c29997580
Dan Williams <dcbw@redhat.com>
2007-08-20 11:43:25 -0400

    [PATCH] libertas: reorganize and simplify init sequence
    This patch moves all firmware load responsibility into the interface-specific
    code and gets rid of the firmware pointer in the generic card structure.  It
    also removes 3 fairly unecessary callbacks: hw_register_dev, hw_unregister_dev,
    and hw_prog_firmware.  It also makes the init sequence from interface
    probe functions more logical, as there are paired add/remove and start/stop
    calls into generic libertas code.
    
    Because the USB driver code uses the same TX URB callback for both firmware
    upload (where the generic libertas structure isn't initialized yet) and for
    normal operation (where it is), some bits of USB code have to deal with
    'priv' being NULL.  All USB firmware upload bits have been changed to not
    require 'priv' at all, but simply the USB card structure.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/if_usb.h
+++ drivers/net/wireless/libertas/if_usb.h
@@ -39,7 +39,7 @@ struct bootcmdrespStr
 
 /* read callback private data */
 struct read_cb_info {
-        wlan_private *priv;
+        struct usb_card_rec *cardp;
         struct sk_buff *skb;
 };
 
@@ -59,6 +59,7 @@ struct usb_card_rec {
 	int bulk_out_size;
 	u8 bulk_out_endpointAddr;
 
+	const struct firmware *fw;
 	u8 CRC_OK;
 	u32 fwseqnum;
 	u32 lastseqnum;
@@ -66,6 +67,7 @@ struct usb_card_rec {
 	u32 fwlastblksent;
 	u8 fwdnldover;
 	u8 fwfinalblk;
+	u8 surprise_removed;
 
 	u32 usb_event_cause;
 	u8 usb_int_cause;
