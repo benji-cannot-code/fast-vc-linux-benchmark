361fc1e4cfa7084c9821e0b0a862ce1be19a908d
Stefan Richter <stefanr@s5r6.in-berlin.de>
2007-08-20 21:40:30 +0200

    firewire: fw-ohci: fix includes
    Add used includes, remove unused includes.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-ohci.c
+++ drivers/firewire/fw-ohci.c
@@ -25,12 +25,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/interrupt.h>
 #include <linux/pci.h>
 #include <linux/delay.h>
-#include <linux/poll.h>
 #include <linux/dma-mapping.h>
+#include <linux/gfp.h>
 #include <linux/mm.h>
+#include <linux/spinlock.h>
 
-#include <asm/uaccess.h>
-#include <asm/semaphore.h>
+#include <asm/page.h>
 #include <asm/system.h>
 
 #include "fw-transaction.h"
