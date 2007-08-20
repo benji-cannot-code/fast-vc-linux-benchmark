9201d62d9bfb3610774f77ab8d7e2762daf28b43
Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
2007-08-19 17:18:13 -0700

    [DCCP] packet_history: Convert dccphtx_tstamp to ktime_t
    Signed-off-by: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ccids/lib/packet_history.h
+++ net/dccp/ccids/lib/packet_history.h
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ktime.h>
 #include <linux/list.h>
 #include <linux/slab.h>
-#include <linux/time.h>
 
 #include "../../dccp.h"
 
@@ -59,7 +58,7 @@ struct dccp_tx_hist_entry {
 	u64		 dccphtx_seqno:48,
 			 dccphtx_sent:1;
 	u32		 dccphtx_rtt;
-	struct timeval	 dccphtx_tstamp;
+	ktime_t		 dccphtx_tstamp;
 };
 
 struct dccp_tx_hist {
