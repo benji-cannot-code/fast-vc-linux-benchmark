eec878902b784033df980ed6bc5e9d83b180ab2c
Hans Verkuil <hverkuil@xs4all.nl>
2007-08-22 08:58:47 -0300

    V4L/DVB (6090): ivtv-fb: correct transparency bit reporting
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/ivtv/ivtv-fb.c
+++ drivers/media/video/ivtv/ivtv-fb.c
@@ -609,9 +609,6 @@ static int _ivtvfb_check_var(struct fb_var_screeninfo *var, struct ivtv *itv)
 		var->blue.length = 8;
 	}
 	else if (var->bits_per_pixel == 16) {
-		var->transp.offset = 0;
-		var->transp.length = 0;
-
 		/* To find out the true mode, check green length */
 		switch (var->green.length) {
 			case 4:
@@ -621,6 +618,8 @@ static int _ivtvfb_check_var(struct fb_var_screeninfo *var, struct ivtv *itv)
 				var->green.length = 4;
 				var->blue.offset = 0;
 				var->blue.length = 4;
+				var->transp.offset = 12;
+				var->transp.length = 1;
 				break;
 			case 5:
 				var->red.offset = 10;
@@ -629,6 +628,8 @@ static int _ivtvfb_check_var(struct fb_var_screeninfo *var, struct ivtv *itv)
 				var->green.length = 5;
 				var->blue.offset = 0;
 				var->blue.length = 5;
+				var->transp.offset = 15;
+				var->transp.length = 1;
 				break;
 			default:
 				var->red.offset = 11;
@@ -637,6 +638,8 @@ static int _ivtvfb_check_var(struct fb_var_screeninfo *var, struct ivtv *itv)
 				var->green.length = 6;
 				var->blue.offset = 0;
 				var->blue.length = 5;
+				var->transp.offset = 0;
+				var->transp.length = 0;
 				break;
 		}
 	}
