51208a42490c1db1050286e9ea532b384a40661b
Geert Uytterhoeven <geert@linux-m68k.org>
2007-08-22 14:01:29 -0700

    m68k: <asm/page.h> needs <linux/compiler.h>
    m68k: <asm/page.h> needs <linux/compiler.h> because of __attribute_const__
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-m68k/page.h
+++ include/asm-m68k/page.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 
+#include <linux/compiler.h>
+
 #include <asm/module.h>
 
 #define get_user_page(vaddr)		__get_free_page(GFP_KERNEL)
