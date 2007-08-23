42a07b5e86a41ed7628f1eb2884f71d316b69251
Anton Vorontsov <avorontsov@ru.mvista.com>
2007-08-23 15:35:53 +0400

    [POWERPC] QE: extern par_io_config_pin and par_io_data_set funcs
    This is needed to configure and control QE pario pins from the kernel.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- include/asm-powerpc/qe.h
+++ include/asm-powerpc/qe.h
@@ -33,6 +33,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern void qe_reset(void);
 extern int par_io_init(struct device_node *np);
 extern int par_io_of_config(struct device_node *np);
+extern int par_io_config_pin(u8 port, u8 pin, int dir, int open_drain,
+			     int assignment, int has_irq);
+extern int par_io_data_set(u8 port, u8 pin, u8 val);
 
 /* QE internal API */
 int qe_issue_cmd(u32 cmd, u32 device, u8 mcn_protocol, u32 cmd_input);
