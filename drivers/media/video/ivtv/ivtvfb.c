84ad272a00a2e5a2434d5b1afb41c41363a65b0e
Hans Verkuil <hverkuil@xs4all.nl>
2007-08-26 06:04:10 -0300

    V4L/DVB (6119): ivtvfb: renamed ivtv-fb to ivtvfb, move header to include/linux
    The convention for framebuffer devices is to call them xxxfb, not xxx-fb.
    Conform to this. Also move the ivtvfb.h header to include/linux: it is a
    public header. The FBIO_WAITFORVSYNC ioctl is now also defined in the
    ivtvfb.h header, no more need to include matroxfb.h for just this ioctl.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/ivtv/ivtvfb.c
+++ drivers/media/video/ivtv/ivtvfb.c
@@ -41,17 +41,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/string.h>
-#include <linux/mm.h>
-#include <linux/tty.h>
 #include <linux/fb.h>
-#include <linux/console.h>
-#include <linux/bitops.h>
-#include <linux/pagemap.h>
-#include <media/ivtv-fb.h>
-
-#include <asm/io.h>
-#include <asm/ioctl.h>
+#include <linux/ivtvfb.h>
 
 #ifdef CONFIG_MTRR
 #include <asm/mtrr.h>
--- include/linux/ivtvfb.h
+++ include/linux/ivtvfb.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
     On Screen Display cx23415 Framebuffer driver
 
-    Copyright (C) 2006  Ian Armstrong <ian@iarmst.demon.co.uk>
+    Copyright (C) 2006, 2007  Ian Armstrong <ian@iarmst.demon.co.uk>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,8 +19,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef _LINUX_IVTV_FB_H
-#define _LINUX_IVTV_FB_H
+#ifndef __LINUX_IVTVFB_H__
+#define __LINUX_IVTVFB_H__
+
+#ifdef __KERNEL__
+#include <linux/compiler.h> /* need __user */
+#else
+#define __user
+#endif
+#include <linux/types.h>
 
 /* Framebuffer external API */
 
@@ -30,7 +37,7 @@ struct ivtvfb_dma_frame {
 	int count;
 };
 
-#define IVTVFB_IOC_DMA_FRAME 	_IOW ('V', BASE_VIDIOC_PRIVATE+0, struct ivtvfb_dma_frame)
+#define IVTVFB_IOC_DMA_FRAME 	_IOW('V', BASE_VIDIOC_PRIVATE+0, struct ivtvfb_dma_frame)
 #define FBIO_WAITFORVSYNC	_IOW('F', 0x20, u_int32_t)
 
 #endif
