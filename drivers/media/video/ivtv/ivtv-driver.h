023ceddc2a6fb30ee5d502d525c65a77be614cf2
Hans Verkuil <hverkuil@xs4all.nl>
2007-08-26 06:13:04 -0300

    V4L/DVB (6123): ivtv: move ivtv.h public header to include/linux
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/ivtv/ivtv-driver.h
+++ drivers/media/video/ivtv/ivtv-driver.h
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <media/tuner.h>
 #include <media/cx2341x.h>
 
-#include <media/ivtv.h>
+#include <linux/ivtv.h>
 
 
 /* Memory layout */
