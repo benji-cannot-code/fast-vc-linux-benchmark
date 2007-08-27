2b38925be7fc08b9be9bb7ad23df2ea0a4b0cb71
Atsushi Nemoto <anemo@mba.ocn.ne.jp>
2007-08-28 00:28:09 +0900

    [MIPS] tx4927: Cleanup unused macros and non-standard IO accessors.
    This patch removes many unused constants, replaces non-standard IO
    accessors with standard ones, and kills terrible tx4927_mips.h file.
    
    Signed-off-by: Atsushi Nemoto <anemo@mba.ocn.ne.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/tx4927/common/tx4927_dbgio.c
+++ arch/mips/tx4927/common/tx4927_dbgio.c
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/mipsregs.h>
 #include <asm/system.h>
-#include <asm/tx4927/tx4927_mips.h>
 
 u8 getDebugChar(void)
 {
