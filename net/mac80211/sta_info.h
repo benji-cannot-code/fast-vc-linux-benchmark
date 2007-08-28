46f5e869c4c2522ece9fd22287eacda864c5b62c
Johannes Berg <johannes@sipsolutions.net>
2007-08-28 17:01:55 -0400

    [MAC80211]: remove fake set_key() call
    Remove adding a fake key with a NONE key algorithm for each
    associated STA. If we have hardware with such TX filtering
    we should probably extend the sta_table_notification()
    callback with the sta information instead; the fact that
    it's treated as a key for some atheros hardware shouldn't
    bother the stack.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Acked-by: Michael Wu <flamingice@sourmilk.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mac80211/sta_info.h
+++ net/mac80211/sta_info.h
@@ -91,10 +91,6 @@ struct sta_info {
 	int channel_use;
 	int channel_use_raw;
 
-	int key_idx_compression; /* key table index for compression and TX
-				  * filtering; used only if sta->key is not
-				  * set */
-
 	int assoc_ap; /* whether this is an AP that we are
 		       * associated with as a client */
 
