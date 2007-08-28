0cbbef1951e3ad8a7ac70be7796c87d7c74129ef
Michael Krufky <mkrufky@linuxtv.org>
2007-08-27 21:59:08 -0300

    V4L/DVB (6133): tuner-simple: convert from tuner sub-driver into dvb_frontend module
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Acked-by: Hans Verkuil <hverkuil@xs4all.nl>
    Acked-by: Mike Isely <isely@pobox.com>
    Acked-by: Steven Toth <stoth@hauppauge.com>
    Acked-by: Patrick Boettcher <pb@linuxtv.org>
    Acked-by: Jarod Wilson <jwilson@redhat.com>
    Acked-by: Trent Piepho <xyzzy@speakeasy.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tuner-driver.h
+++ drivers/media/video/tuner-driver.h
@@ -73,8 +73,6 @@ struct tuner {
 
 /* ------------------------------------------------------------------------ */
 
-extern int default_tuner_init(struct tuner *t);
-
 extern int tda9887_tuner_init(struct tuner *t);
 
 /* ------------------------------------------------------------------------ */
