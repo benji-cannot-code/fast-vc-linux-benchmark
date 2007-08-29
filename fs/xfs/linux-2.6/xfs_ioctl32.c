10c70191336ecdf3fea2f73693b6dc9f43788e08
Christoph Hellwig <hch@infradead.org>
2007-08-29 11:44:37 +1000

    [XFS] kill the v_flag member in struct bhv_vnode
    All flags previously handled at the vnode level are not in the xfs_inode
    where we already have a flags mechanisms and free bits for flags
    previously in the vnode.
    
    SGI-PV: 969608
    SGI-Modid: xfs-linux-melb:xfs-kern:29495a
    
    Signed-off-by: Christoph Hellwig <hch@infradead.org>
    Signed-off-by: David Chinner <dgc@sgi.com>
    Signed-off-by: Tim Shimmin <tes@sgi.com>

--- fs/xfs/linux-2.6/xfs_ioctl32.c
+++ fs/xfs/linux-2.6/xfs_ioctl32.c
@@ -372,7 +372,6 @@ xfs_compat_ioctl(
 	unsigned long	arg)
 {
 	struct inode	*inode = file->f_path.dentry->d_inode;
-	bhv_vnode_t	*vp = vn_from_inode(inode);
 	int		error;
 
 	switch (cmd) {
@@ -460,7 +459,7 @@ xfs_compat_ioctl(
 	}
 
 	error = xfs_ioctl(XFS_I(inode), file, mode, cmd, (void __user *)arg);
-	VMODIFY(vp);
+	xfs_iflags_set(XFS_I(inode), XFS_IMODIFIED);
 
 	return error;
 }
