18dfc0b348bdfcbdea41c1404c670dc297bdc085
Roman Zippel <zippel@linux-m68k.org>
2007-08-30 05:06:17 +0200

    kconfig: oldconfig shall not set symbols if it does not need to
    Avoid setting the value if the symbol doesn't need to be changed or can't
    be changed. Later choices may change the dependencies and thus the
    possible input range.
    
    make oldconfig from a 2.6.22 .config with CONFIG_HOTPLUG_CPU not set
    was in some configurations setting CONFIG_HOTPLUG_CPU=y without asking,
    even when there was no actual requirement for CONFIG_HOTPLUG_CPU.
    This was triggered by SUSPEND_SMP that does a select HOTPLUG_CPU.
    
    Signed-off-by: Roman Zippel <zippel@linux-m68k.org>
    Tested-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -73,7 +73,7 @@ static void check_stdin(void)
 	}
 }
 
-static void conf_askvalue(struct symbol *sym, const char *def)
+static int conf_askvalue(struct symbol *sym, const char *def)
 {
 	enum symbol_type type = sym_get_type(sym);
 	tristate val;
@@ -88,7 +88,7 @@ static void conf_askvalue(struct symbol *sym, const char *def)
 		printf("%s\n", def);
 		line[0] = '\n';
 		line[1] = 0;
-		return;
+		return 0;
 	}
 
 	switch (input_mode) {
@@ -98,23 +98,23 @@ static void conf_askvalue(struct symbol *sym, const char *def)
 	case set_random:
 		if (sym_has_value(sym)) {
 			printf("%s\n", def);
-			return;
+			return 0;
 		}
 		break;
 	case ask_new:
 	case ask_silent:
 		if (sym_has_value(sym)) {
 			printf("%s\n", def);
-			return;
+			return 0;
 		}
 		check_stdin();
 	case ask_all:
 		fflush(stdout);
 		fgets(line, 128, stdin);
-		return;
+		return 1;
 	case set_default:
 		printf("%s\n", def);
-		return;
+		return 1;
 	default:
 		break;
 	}
@@ -124,7 +124,7 @@ static void conf_askvalue(struct symbol *sym, const char *def)
 	case S_HEX:
 	case S_STRING:
 		printf("%s\n", def);
-		return;
+		return 1;
 	default:
 		;
 	}
@@ -175,6 +175,7 @@ static void conf_askvalue(struct symbol *sym, const char *def)
 		break;
 	}
 	printf("%s", line);
+	return 1;
 }
 
 int conf_string(struct menu *menu)
@@ -188,7 +189,8 @@ int conf_string(struct menu *menu)
 		def = sym_get_string_value(sym);
 		if (sym_get_string_value(sym))
 			printf("[%s] ", def);
-		conf_askvalue(sym, def);
+		if (!conf_askvalue(sym, def))
+			return 0;
 		switch (line[0]) {
 		case '\n':
 			break;
@@ -241,7 +243,8 @@ static int conf_sym(struct menu *menu)
 		if (menu_has_help(menu))
 			printf("/?");
 		printf("] ");
-		conf_askvalue(sym, sym_get_string_value(sym));
+		if (!conf_askvalue(sym, sym_get_string_value(sym)))
+			return 0;
 		strip(line);
 
 		switch (line[0]) {
