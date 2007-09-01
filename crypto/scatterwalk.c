9e98814b8acefe48bc40235a4c07360e0a73c33a
Herbert Xu <herbert@gondor.apana.org.au>
2007-09-01 16:52:13 +0800

    [CRYPTO] api: Kill crypto_km_types
    When scatterwalk is built as a module digest.c was broken because it
    requires the crypto_km_types structure which is in scatterwalk.  This
    patch removes the crypto_km_types structure by encoding the logic into
    crypto_kmap_type directly.
    
    In fact, this even saves a few bytes of code (not to mention the data
    structure itself) on i386 which is about the only place where it's
    needed.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/scatterwalk.c
+++ crypto/scatterwalk.c
@@ -24,14 +24,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "internal.h"
 #include "scatterwalk.h"
 
-enum km_type crypto_km_types[] = {
-	KM_USER0,
-	KM_USER1,
-	KM_SOFTIRQ0,
-	KM_SOFTIRQ1,
-};
-EXPORT_SYMBOL_GPL(crypto_km_types);
-
 static inline void memcpy_dir(void *buf, void *sgdata, size_t nbytes, int out)
 {
 	void *src = out ? buf : sgdata;
