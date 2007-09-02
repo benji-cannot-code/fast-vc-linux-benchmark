06cc501239be512797279cbbc1902cbaf073a3b5
Satyam Sharma <satyam@infradead.org>
2007-09-03 01:37:31 +0530

    USB: sisusbvga: Fix bug
    drivers/usb/misc/sisusbvga/sisusb.c: In function sisusb_open
    drivers/usb/misc/sisusbvga/sisusb.c:2444: warning: sisusb is used uninitialized in this function
    
    is a genuine bug (which will cause oops). We cannot use "sisusb" in
    error path for (!interface), because sisusb will itself be derived
    from "interface" later.
    
    Signed-off-by: Satyam Sharma <satyam@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/misc/sisusbvga/sisusb.c
+++ drivers/usb/misc/sisusbvga/sisusb.c
@@ -2441,10 +2441,8 @@ sisusb_open(struct inode *inode, struct file *file)
 	struct usb_interface *interface;
 	int subminor = iminor(inode);
 
-	if (!(interface = usb_find_interface(&sisusb_driver, subminor))) {
-		dev_err(&sisusb->sisusb_dev->dev, "Failed to find interface\n");
+	if (!(interface = usb_find_interface(&sisusb_driver, subminor)))
 		return -ENODEV;
-	}
 
 	if (!(sisusb = usb_get_intfdata(interface)))
 		return -ENODEV;
