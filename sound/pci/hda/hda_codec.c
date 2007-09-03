cdced8b79f71100974a54e3526620cd27f046e92
Maxim Levitsky <maximlevitsky@gmail.com>
2007-09-03 15:26:57 +0200

    [ALSA] hda-intel - fix a race in dynamic power managment
    codec->power_transition is supposed to be true while codec is going
    to be shut off if in the mean time somebody calls snd_hda_power_up,
    hda_power_work will not shut down the codec, but nether will clear
    codec->power_transition, thus it stays on forever. Fix this.
    
    Signed-off-by: Maxim Levitsky <maximlevitsky@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/pci/hda/hda_codec.c
+++ sound/pci/hda/hda_codec.c
@@ -2196,8 +2196,10 @@ static void hda_power_work(struct work_struct *work)
 	struct hda_codec *codec =
 		container_of(work, struct hda_codec, power_work.work);
 
-	if (!codec->power_on || codec->power_count)
+	if (!codec->power_on || codec->power_count) {
+		codec->power_transition = 0;
 		return;
+	}
 
 	hda_call_codec_suspend(codec);
 	if (codec->bus->ops.pm_notify)
