10e1bf5fe5172d62a33c61e84f2ab8873361ca68
Krzysztof Helt <krzysztof.h1@wp.pl>
2007-09-06 15:02:33 +0200

    [ALSA] dbri - Use linux/of.h instead of asm/prom.h
    The linux/of.h header should be used instead of asm/prom.h.
    
    Signed-off-by: Krzysztof Helt <krzysztof.h1@wp.pl>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- sound/sparc/dbri.c
+++ sound/sparc/dbri.c
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sound/control.h>
 #include <sound/initval.h>
 
-#include <asm/prom.h>
+#include <linux/of.h>
 #include <asm/sbus.h>
 #include <asm/atomic.h>
 
