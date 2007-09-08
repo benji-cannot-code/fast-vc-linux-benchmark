f3eb774dbd9c55f49cf95753cfd0bc68ae48cea6
Michael Krufky <mkrufky@linuxtv.org>
2007-09-08 15:17:13 -0300

    V4L/DVB (6196): cx23885: add support for DViCO FusionHDTV 5 Express
    This patch adds digital ATSC / QAM support for the DViCO FusionHDTV5 Express.
    Remote control is supported by ir-kbd-i2c, RTC is supported by rtc-isl1208.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx23885/cx23885-dvb.c
+++ drivers/media/video/cx23885/cx23885-dvb.c
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "s5h1409.h"
 #include "mt2131.h"
+#include "lgdt330x.h"
+#include "dvb-pll.h"
 
 static unsigned int debug = 0;
 
@@ -102,6 +104,12 @@ static struct mt2131_config hauppauge_generic_tunerconfig = {
 	0x61
 };
 
+static struct lgdt330x_config fusionhdtv_5_express = {
+	.demod_address = 0x0e,
+	.demod_chip = LGDT3303,
+	.serial_mpeg = 0x40,
+};
+
 static int dvb_register(struct cx23885_tsport *port)
 {
 	struct cx23885_dev *dev = port->dev;
@@ -130,6 +138,16 @@ static int dvb_register(struct cx23885_tsport *port)
 			dvb_attach(mt2131_attach, port->dvb.frontend,
 				   &dev->i2c_bus[0].i2c_adap,
 				   &hauppauge_generic_tunerconfig, 0);
+		}
+		break;
+	case CX23885_BOARD_DVICO_FUSIONHDTV_5_EXP:
+		port->dvb.frontend = dvb_attach(lgdt330x_attach,
+						&fusionhdtv_5_express,
+						&dev->i2c_bus[0].i2c_adap);
+		if (port->dvb.frontend != NULL) {
+			dvb_attach(dvb_pll_attach, port->dvb.frontend,
+				   0x61, &dev->i2c_bus[0].i2c_adap,
+				   DVB_PLL_LG_TDVS_H06XF);
 		}
 		break;
 	default:
