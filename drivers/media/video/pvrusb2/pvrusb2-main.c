34481a8fbf9cbfbd4b18e03aaa27295e27dc2623
Mike Isely <isely@pobox.com>
2007-09-08 22:32:12 -0300

    V4L/DVB (6211): pvrusb2: Allocate a debug mask bit for reporting video standard things
    It's useful to see specific details for how the pvrusb2 driver is
    figuring out things related to the video standard, independent of
    other initialization activities.  So let's set up a separate debug
    mask bit for this and turn it on.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-main.c
+++ drivers/media/video/pvrusb2/pvrusb2-main.c
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DEFAULT_DEBUG_MASK (PVR2_TRACE_ERROR_LEGS| \
 			    PVR2_TRACE_INFO| \
+			    PVR2_TRACE_STD| \
 			    PVR2_TRACE_TOLERANCE| \
 			    PVR2_TRACE_TRAP| \
 			    0)
