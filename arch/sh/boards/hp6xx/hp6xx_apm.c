cc758c59a79468b7df650a3a0557e3fee34f6a50
Kristoffer Ericson <Kristoffer.Ericson@gmail.com>
2007-09-11 12:43:33 +0900

    sh: hp6xx: APM build fix and new battery values.
    Build fixes for the hp6xx APM code, as well as some adjustments for
    the battery values.
    
    Signed-off-by: Kristoffer Ericson <Kristoffer.Ericson@gmail.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/boards/hp6xx/hp6xx_apm.c
+++ arch/sh/boards/hp6xx/hp6xx_apm.c
@@ -21,9 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define APM_CRITICAL			10
 #define APM_LOW				30
 
-#define HP680_BATTERY_MAX		875
-#define HP680_BATTERY_MIN		600
-#define HP680_BATTERY_AC_ON		900
+#define HP680_BATTERY_MAX		898
+#define HP680_BATTERY_MIN		486
+#define HP680_BATTERY_AC_ON		1023
 
 #define MODNAME "hp6x0_apm"
 
@@ -66,7 +66,7 @@ static void hp6x0_apm_get_power_status(struct apm_power_info *info)
 
 static irqreturn_t hp6x0_apm_interrupt(int irq, void *dev)
 {
-	if (!apm_suspended)
+	if (!APM_DISABLED)
 		apm_queue_event(APM_USER_SUSPEND);
 
 	return IRQ_HANDLED;
@@ -92,7 +92,6 @@ static int __init hp6x0_apm_init(void)
 static void __exit hp6x0_apm_exit(void)
 {
 	free_irq(HP680_BTN_IRQ, 0);
-	apm_get_info = NULL;
 }
 
 module_init(hp6x0_apm_init);
