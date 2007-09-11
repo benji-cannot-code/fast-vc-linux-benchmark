f3284f8b3f2b6b3dd1b5f6b346c68f8eaa736d64
Paul Mundt <lethal@linux-sh.org>
2007-09-11 12:35:51 +0900

    sh: nommu fault handler needs signal.h.
    Trivial build fix.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/mm/fault-nommu.c
+++ arch/sh/mm/fault-nommu.c
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/hardirq.h>
 #include <linux/kprobes.h>
+#include <linux/signal.h>
 #include <asm/system.h>
 #include <asm/ptrace.h>
 #include <asm/kgdb.h>
