467edab8c20d71f94ade5faad72e5bd8ae626644
Bryan Wu <bryan.wu@analog.com>
2007-09-12 17:48:25 +0800

    Blackfin arch: cleanup IO and DMA_IO API function definitions according to other arches
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- arch/blackfin/kernel/bfin_dma_5xx.c
+++ arch/blackfin/kernel/bfin_dma_5xx.c
@@ -584,7 +584,7 @@ void *safe_dma_memcpy(void *dest, const void *src, size_t size)
 }
 EXPORT_SYMBOL(safe_dma_memcpy);
 
-void dma_outsb(void __iomem *addr, const void *buf, unsigned short len)
+void dma_outsb(const void __iomem *addr, const void *buf, unsigned short len)
 {
 	unsigned long flags;
 
@@ -648,7 +648,7 @@ void dma_insb(const void __iomem *addr, void *buf, unsigned short len)
 }
 EXPORT_SYMBOL(dma_insb);
 
-void dma_outsw(void __iomem *addr, const void  *buf, unsigned short len)
+void dma_outsw(const void __iomem *addr, const void  *buf, unsigned short len)
 {
 	unsigned long flags;
 
@@ -712,7 +712,7 @@ void dma_insw(const void __iomem *addr, void *buf, unsigned short len)
 }
 EXPORT_SYMBOL(dma_insw);
 
-void dma_outsl(void __iomem *addr, const void *buf, unsigned short len)
+void dma_outsl(const void __iomem *addr, const void *buf, unsigned short len)
 {
 	unsigned long flags;
 
