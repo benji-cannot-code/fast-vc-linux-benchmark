a4564fd9c62d3622e28029e26b1e4aafee5cb95f
Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
2007-09-13 11:04:04 +0900

    [MIPS] i8295 cleanups.
     o Move i8259 function declarations to include/asm-mips/i8259.h
     o Make i8259.c functions static where possible.
    
    Signed-off-by: Yoichi Yuasa <yoichi_yuasa@tripeaks.co.jp>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/hw_irq.h
+++ include/asm-mips/hw_irq.h
@@ -9,14 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_HW_IRQ_H
 #define __ASM_HW_IRQ_H
 
-#include <linux/profile.h>
 #include <asm/atomic.h>
-
-extern void disable_8259A_irq(unsigned int irq);
-extern void enable_8259A_irq(unsigned int irq);
-extern int i8259A_irq_pending(unsigned int irq);
-extern void make_8259A_irq(unsigned int irq);
-extern void init_8259A(int aeoi);
 
 extern atomic_t irq_err_count;
 
