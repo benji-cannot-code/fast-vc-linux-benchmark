c2c35e282e1d5a586455e40db0807ec167190f8c
Sivakumar Subramani <Sivakumar.Subramani@neterion.com>
2007-09-14 07:43:16 -0400

    S2io: Updating transceiver information in ethtool function
    - Update transceiver information in ethtool function
    
    Signed-off-by: Sreenivasa Honnur <sreenivasa.honnur@neterion.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/s2io.c
+++ drivers/net/s2io.c
@@ -85,7 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "s2io.h"
 #include "s2io-regs.h"
 
-#define DRV_VERSION "2.0.26.4"
+#define DRV_VERSION "2.0.26.5"
 
 /* S2io Driver name & version. */
 static char s2io_driver_name[] = "Neterion";
@@ -4977,7 +4977,9 @@ static int s2io_ethtool_gset(struct net_device *dev, struct ethtool_cmd *info)
 	info->supported = (SUPPORTED_10000baseT_Full | SUPPORTED_FIBRE);
 	info->advertising = (SUPPORTED_10000baseT_Full | SUPPORTED_FIBRE);
 	info->port = PORT_FIBRE;
-	/* info->transceiver?? TODO */
+
+	/* info->transceiver */
+	info->transceiver = XCVR_EXTERNAL;
 
 	if (netif_carrier_ok(sp->dev)) {
 		info->speed = 10000;
