9161dfde4e32d4ffc8dc7de70caac47f0b055e8b
Michael Ellerman <michael@ellerman.id.au>
2007-09-17 16:05:01 +1000

    [POWERPC] Update mpic to use dcr_host_t.base
    Now that dcr_host_t contains the base address, we can use that in the mpic
    code, rather than storing it separately.
    
    Signed-off-by: Michael Ellerman <michael@ellerman.id.au>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/mpic.h
+++ include/asm-powerpc/mpic.h
@@ -225,8 +225,6 @@ struct mpic_reg_bank {
 	u32 __iomem	*base;
 #ifdef CONFIG_PPC_DCR
 	dcr_host_t	dhost;
-	unsigned int	dbase;
-	unsigned int	doff;
 #endif /* CONFIG_PPC_DCR */
 };
 
@@ -289,10 +287,6 @@ struct mpic
 	struct mpic_reg_bank	tmregs;
 	struct mpic_reg_bank	cpuregs[MPIC_MAX_CPUS];
 	struct mpic_reg_bank	isus[MPIC_MAX_ISU];
-
-#ifdef CONFIG_PPC_DCR
-	unsigned int		dcr_base;
-#endif
 
 	/* Protected sources */
 	unsigned long		*protected;
