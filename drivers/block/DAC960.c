d587a0765603c501b652138ad35af01ca10499e8
Andrew Morton <akpm@linux-foundation.org>
2007-09-18 22:46:19 -0700

    Fix "Fix DAC960 driver on machines which don't support 64-bit DMA"
    sparc32:
    
    drivers/block/DAC960.c: In function 'DAC960_V1_EnableMemoryMailboxInterface':
    drivers/block/DAC960.c:1168: error: 'DMA_32BIT_MASK' undeclared (first use in this function)
    drivers/block/DAC960.c:1168: error: (Each undeclared identifier is reported only
    
    Cc: <dac@conglom-o.org>
    Cc: <stable@kernel.org>
    Cc: Alessandro Polverini <alex@nibbles.it>
    Cc: Jeff Garzik <jeff@garzik.org>
    Cc: Matthew Wilcox <matthew@wil.cx>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/block/DAC960.c
+++ drivers/block/DAC960.c
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/genhd.h>
 #include <linux/hdreg.h>
 #include <linux/blkpg.h>
+#include <linux/dma-mapping.h>
 #include <linux/interrupt.h>
 #include <linux/ioport.h>
 #include <linux/mm.h>
