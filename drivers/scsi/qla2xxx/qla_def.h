fca6e2253e8c11ed2efc97d707343e0ad1a7c1d3
Andrew Vasquez <andrew.vasquez@qlogic.com>
2007-09-20 14:07:44 -0700

    [SCSI] qla2xxx: Remove unused member (list) from srb_t structure.
    This change reduces by as much as 16% the memory footprint for
    each allocated sbr_t structure requested from the mempool.
    
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -186,8 +186,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * SCSI Request Block
  */
 typedef struct srb {
-	struct list_head list;
-
 	struct scsi_qla_host *ha;	/* HA the SP is queued on */
 	struct fc_port *fcport;
 
