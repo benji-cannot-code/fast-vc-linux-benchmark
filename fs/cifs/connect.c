255cc505f7e9f4d6613465e2d6ac6337311e7a67
Steve French <sfrench@us.ibm.com>
2007-09-20 15:37:29 +0000

    [CIFS] fix typo in previous commit
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -1751,7 +1751,7 @@ void reset_cifs_unix_caps(int xid, struct cifsTconInfo *tcon,
 					   "mounting with the Unix Extensions\n"
 					   "disabled, if problems are found, "
 					   "by specifying the nounix mount "
-					   "option.");
+					   "option."));
 
 		}
 	}
