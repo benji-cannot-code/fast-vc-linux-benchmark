b3eed83b7591a9fa914166e160ffefc04e68f35e
Paul Mundt <lethal@linux-sh.org>
2007-09-21 11:34:31 +0900

    sh: Wire up URAM node on SH7785.
    Add SH7785 URAM as node 1, follows the SH-X3 change.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/cpu/sh4a/setup-sh7785.c
+++ arch/sh/kernel/cpu/sh4a/setup-sh7785.c
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/init.h>
 #include <linux/serial.h>
 #include <linux/io.h>
+#include <linux/mm.h>
+#include <asm/mmzone.h>
 #include <asm/sci.h>
 
 static struct plat_sci_port sci_platform_data[] = {
@@ -349,4 +351,10 @@ void __init plat_irq_setup_pins(int mode)
 	default:
 		BUG();
 	}
+}
+
+void __init plat_mem_setup(void)
+{
+	/* Register the URAM space as Node 1 */
+	setup_bootmem_node(1, 0x055f0000, 0x05610000);
 }
