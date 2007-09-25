c1a4ab49f8680e153c008880718af2ba7c796069
Matthew Wilcox <matthew@wil.cx>
2007-09-25 12:42:01 -0400

    [SCSI] Fix mistaken uses of ->done
    All these drivers meant to call ->scsi_done() but got confused.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/atari_NCR5380.c
+++ drivers/scsi/atari_NCR5380.c
@@ -2042,7 +2042,7 @@ static void NCR5380_information_transfer(struct Scsi_Host *instance)
 				sink = 1;
 				do_abort(instance);
 				cmd->result = DID_ERROR << 16;
-				cmd->done(cmd);
+				cmd->scsi_done(cmd);
 				return;
 #endif
 			case PHASE_DATAIN:
@@ -2101,7 +2101,7 @@ static void NCR5380_information_transfer(struct Scsi_Host *instance)
 						sink = 1;
 						do_abort(instance);
 						cmd->result = DID_ERROR << 16;
-						cmd->done(cmd);
+						cmd->scsi_done(cmd);
 						/* XXX - need to source or sink data here, as appropriate */
 					} else {
 #ifdef REAL_DMA
