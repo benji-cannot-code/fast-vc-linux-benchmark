671faad33945a021b4a72491841f7717a723ff5f
Steve French <sfrench@us.ibm.com>
2007-09-25 16:17:24 +0000

    [CIFS] move cifs acl code to new file and fix build break
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsacl.h
+++ fs/cifs/cifsacl.h
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *   fs/cifs/cifsacl.h
  *
- *   Copyright (c) International Business Machines  Corp., 2005
+ *   Copyright (c) International Business Machines  Corp., 2007
  *   Author(s): Steve French (sfrench@us.ibm.com)
  *
  *   This library is free software; you can redistribute it and/or modify
