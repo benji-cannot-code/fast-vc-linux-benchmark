ff2873b872d73f1e272188eb661df17e2b398b63
Arnaldo Carvalho de Melo <acme@ghostprotocols.net>
2007-09-26 19:38:55 -0300

    [CCID3]: Remove ifdef surrounding BUG_ON
    As suggested by DaveM.
    
    Signed-off-by: Arnaldo Carvalho de Melo <acme@ghostprotocols.net>

--- net/dccp/ccids/ccid3.h
+++ net/dccp/ccids/ccid3.h
@@ -121,9 +121,7 @@ struct ccid3_hc_tx_sock {
 static inline struct ccid3_hc_tx_sock *ccid3_hc_tx_sk(const struct sock *sk)
 {
     struct ccid3_hc_tx_sock *hctx = ccid_priv(dccp_sk(sk)->dccps_hc_tx_ccid);
-#ifdef CONFIG_IP_DCCP_CCID3_DEBUG
     BUG_ON(hctx == NULL);
-#endif
     return hctx;
 }
 
@@ -174,9 +172,7 @@ struct ccid3_hc_rx_sock {
 static inline struct ccid3_hc_rx_sock *ccid3_hc_rx_sk(const struct sock *sk)
 {
     struct ccid3_hc_rx_sock *hcrx = ccid_priv(dccp_sk(sk)->dccps_hc_rx_ccid);
-#ifdef CONFIG_IP_DCCP_CCID3_DEBUG
     BUG_ON(hcrx == NULL);
-#endif
     return hcrx;
 }
 
