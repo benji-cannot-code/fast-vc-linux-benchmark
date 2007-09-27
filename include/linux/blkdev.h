0baaa4c0f78ee4b4dd9b6f0323da1bbdace1128e
NeilBrown <neilb@suse.de>
2007-09-27 12:46:11 +0200

    Remove flush_dry_bio_endio
    The entire function of flush_dry_bio_endio is to undo the effects
    of bio_endio (when called on a barrier request).  So remove the
    function and the call to bio_endio.
    
    This allows us to remove "bi_size" from "struct request_queue".
    
    Signed-off-by: Neil Brown <neilb@suse.de>
    
    ### Diffstat output
     ./block/ll_rw_blk.c      |   39 ++-------------------------------------
     ./include/linux/blkdev.h |    1 -
     2 files changed, 2 insertions(+), 38 deletions(-)
    
    diff .prev/block/ll_rw_blk.c ./block/ll_rw_blk.c
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -472,7 +472,6 @@ struct request_queue
 	int			orderr, ordcolor;
 	struct request		pre_flush_rq, bar_rq, post_flush_rq;
 	struct request		*orig_bar_rq;
-	unsigned int		bi_size;
 
 	struct mutex		sysfs_lock;
 
