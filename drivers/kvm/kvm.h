8f176f2c0b2ee6ac86106df72593066a585d9131
Izik Eidus <izike@qumranet.com>
2007-09-27 14:11:22 +0200

    KVM: Remove the usage of page->private field by rmap
    When kvm uses user-allocated pages in the future for the guest, we won't
    be able to use page->private for rmap, since page->rmap is reserved for
    the filesystem.  So we move the rmap base pointers to the memory slot.
    
    A side effect of this is that we need to store the gfn of each gpte in
    the shadow pages, since the memory slot is addressed by gfn, instead of
    hfn like struct page.
    
    Signed-off-by: Izik Eidus <izik@qumranet.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/kvm.h
+++ drivers/kvm/kvm.h
@@ -127,6 +127,8 @@ struct kvm_mmu_page {
 	union kvm_mmu_page_role role;
 
 	u64 *spt;
+	/* hold the gfn of each spte inside spt */
+	gfn_t *gfns;
 	unsigned long slot_bitmap; /* One bit set per slot which has memory
 				    * in this shadow page.
 				    */
@@ -160,7 +162,7 @@ struct kvm_mmu {
 	u64 *pae_root;
 };
 
-#define KVM_NR_MEM_OBJS 20
+#define KVM_NR_MEM_OBJS 40
 
 struct kvm_mmu_memory_cache {
 	int nobjs;
@@ -403,6 +405,7 @@ struct kvm_memory_slot {
 	unsigned long npages;
 	unsigned long flags;
 	struct page **phys_mem;
+	unsigned long *rmap;
 	unsigned long *dirty_bitmap;
 };
 
@@ -555,6 +558,7 @@ struct page *gva_to_page(struct kvm_vcpu *vcpu, gva_t gva);
 
 extern hpa_t bad_page_address;
 
+gfn_t unalias_gfn(struct kvm *kvm, gfn_t gfn);
 struct page *gfn_to_page(struct kvm *kvm, gfn_t gfn);
 struct kvm_memory_slot *gfn_to_memslot(struct kvm *kvm, gfn_t gfn);
 void mark_page_dirty(struct kvm *kvm, gfn_t gfn);
