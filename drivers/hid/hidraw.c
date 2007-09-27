d373ecc319ff4c28d2ca3993591ab5e15a009340
Mariusz Kozlowski <m.kozlowski@tuxland.pl>
2007-09-27 11:24:55 +0200

    HID: hidraw_connect() memleak fix
    It looks like hidraw_connect() is leaking memory in case of failure.
    Also it should return -ENOMEM when kzalloc fails.
    
    Signed-off-by: Mariusz Kozlowski <m.kozlowski@tuxland.pl>
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>

--- drivers/hid/hidraw.c
+++ drivers/hid/hidraw.c
@@ -283,7 +283,7 @@ EXPORT_SYMBOL_GPL(hidraw_report_event);
 
 int hidraw_connect(struct hid_device *hid)
 {
-	int minor, result = -EINVAL;
+	int minor, result;
 	struct hidraw *dev;
 
 	/* TODO currently we accept any HID device. This should later
@@ -291,8 +291,11 @@ int hidraw_connect(struct hid_device *hid)
 	 * non-input applications
 	 */
 
-	if (!(dev = kzalloc(sizeof(struct hidraw), GFP_KERNEL)))
-		return -1;
+	dev = kzalloc(sizeof(struct hidraw), GFP_KERNEL);
+	if (!dev)
+		return -ENOMEM;
+
+	result = -EINVAL;
 
 	spin_lock(&minors_lock);
 
@@ -306,8 +309,10 @@ int hidraw_connect(struct hid_device *hid)
 
 	spin_unlock(&minors_lock);
 
-	if (result)
+	if (result) {
+		kfree(dev);
 		goto out;
+	}
 
 	dev->dev = device_create(hidraw_class, NULL, MKDEV(hidraw_major, minor),
 				"%s%d", "hidraw", minor);
@@ -317,6 +322,7 @@ int hidraw_connect(struct hid_device *hid)
 		hidraw_table[minor] = NULL;
 		spin_unlock(&minors_lock);
 		result = PTR_ERR(dev->dev);
+		kfree(dev);
 		goto out;
 	}
 
