8462c13317154e95486b623b45ad4b66b8e202f5
Jeff Garzik <jeff@garzik.org>
2007-09-27 06:25:06 -0400

    drivers/block/umem: move private include away from include/linux
    Move include/linux/umem.h to drivers/block, as umem.c is the only user,
    and its not an exported header.
    
    Move the PCI_{VENDOR,DEVICE}_ID_* constants to include/linux/pci_ids.h.
    
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/block/umem.c
+++ drivers/block/umem.c
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/fcntl.h>        /* O_ACCMODE */
 #include <linux/hdreg.h>  /* HDIO_GETGEO */
 
-#include <linux/umem.h>
+#include "umem.h"
 
 #include <asm/uaccess.h>
 #include <asm/io.h>
