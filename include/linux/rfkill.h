a3e8452130ab5dd787a0e08160094973da3f2b01
Michael Buesch <mb@bu3sch.de>
2007-09-27 14:42:42 +0200

    [PATCH] rfkill: Fix documentation typos
    Signed-off-by: Michael Buesch <mb@bu3sch.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- include/linux/rfkill.h
+++ include/linux/rfkill.h
@@ -30,9 +30,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * enum rfkill_type - type of rfkill switch.
- * RFKILL_TYPE_WLAN: switch is no a Wireless network devices.
- * RFKILL_TYPE_BlUETOOTH: switch is on a bluetooth device.
- * RFKILL_TYPE_UWB: switch is on a Ultra wideband device.
+ * RFKILL_TYPE_WLAN: switch is on a 802.11 wireless network device.
+ * RFKILL_TYPE_BLUETOOTH: switch is on a bluetooth device.
+ * RFKILL_TYPE_UWB: switch is on a ultra wideband device.
  */
 enum rfkill_type {
 	RFKILL_TYPE_WLAN ,
