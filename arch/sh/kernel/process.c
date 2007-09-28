afa749361ab4367cae7c887e391db5f2d2713f42
Paul Mundt <lethal@linux-sh.org>
2007-09-28 15:48:06 +0900

    sh: Follow gUSA preempt changes in __switch_to().
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/process.c
+++ arch/sh/kernel/process.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tick.h>
 #include <linux/reboot.h>
 #include <linux/fs.h>
+#include <linux/preempt.h>
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
 #include <asm/pgalloc.h>
@@ -352,10 +353,9 @@ struct task_struct *__switch_to(struct task_struct *prev,
 
 #ifdef CONFIG_PREEMPT
 	{
-		unsigned long flags;
 		struct pt_regs *regs;
 
-		local_irq_save(flags);
+		preempt_disable();
 		regs = task_pt_regs(prev);
 		if (user_mode(regs) && regs->regs[15] >= 0xc0000000) {
 			int offset = (int)regs->regs[15];
@@ -366,7 +366,7 @@ struct task_struct *__switch_to(struct task_struct *prev,
 				/* Go to rewind point */
 				regs->pc = regs->regs[0] + offset;
 		}
-		local_irq_restore(flags);
+		preempt_enable_no_resched();
 	}
 #endif
 
