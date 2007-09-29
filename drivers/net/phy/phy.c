9b0a82047e39b215ac4e9e008f42c390152ccd0d
Maciej W. Rozycki <macro@linux-mips.org>
2007-09-28 22:42:15 -0700

    PHYLIB: fix an interrupt loop potential when halting
    Ensure the PHY_HALTED state is not entered with the IRQ asserted as it
    could lead to an interrupt loop.
    
    There is a small window in phy_stop(), where the state of the PHY machine
    indicates it has been halted, but its interrupt output might still be
    unmasked.  If an interrupt goes active right at this moment it will loop as
    the phy_interrupt() handler exits immediately with IRQ_NONE if the halted
    state is seen.  It is unsafe to extend the phydev spinlock to cover
    phy_interrupt().  It is safe to swap the order of the actions though as all
    the competing places to unmask the interrupt output of the PHY, which are
    phy_change() and phy_timer() are already covered with the lock as is the
    sequence in question.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Cc: Andy Fleming <afleming@freescale.com>
    Cc: Jeff Garzik <jgarzik@pobox.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -738,8 +738,6 @@ void phy_stop(struct phy_device *phydev)
 	if (PHY_HALTED == phydev->state)
 		goto out_unlock;
 
-	phydev->state = PHY_HALTED;
-
 	if (phydev->irq != PHY_POLL) {
 		/* Disable PHY Interrupts */
 		phy_config_interrupt(phydev, PHY_INTERRUPT_DISABLED);
@@ -747,6 +745,8 @@ void phy_stop(struct phy_device *phydev)
 		/* Clear any pending interrupts */
 		phy_clear_interrupt(phydev);
 	}
+
+	phydev->state = PHY_HALTED;
 
 out_unlock:
 	spin_unlock_bh(&phydev->lock);
