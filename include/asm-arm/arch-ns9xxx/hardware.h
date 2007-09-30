d580de3e94107562e63f25663a59ac2b17a98125
Uwe Kleine-König <ukleinek@informatik.uni-freiburg.de>
2007-09-30 20:35:27 +0100

    [ARM] 4591/1: ns9xxx: simplify REGSET, REGGET and introduce ..._IDX variants
    The semantic of the REGSET macros didn't change, but hopefully
    it's more obvious as it's now.
    
    REGGET is changed to return the unshifted value, analogous to
    REGSET.  REGGETIM behaves as REGGET before.  All callers changed.
    
    ..._IDX is used to work with registers that need a parameter like
    BBU_GCONFb1.
    
    Signed-off-by: Uwe Kleine-König <ukleinek@informatik.uni-freiburg.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-ns9xxx/hardware.h
+++ include/asm-arm/arch-ns9xxx/hardware.h
@@ -28,9 +28,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define io_v2p(x)	((((x) & 0x0f000000) << 4) \
 			 + ((x) & 0x00ffffff))
 
+#define __REGSHIFT(mask)	((mask) & (-(mask)))
+
 #define __REGBIT(bit)		((u32)1 << (bit))
 #define __REGBITS(hbit, lbit)	((((u32)1 << ((hbit) - (lbit) + 1)) - 1) << (lbit))
-#define __REGVAL(mask, value)	(((value) * ((mask) & (-(mask))) & (mask)))
+#define __REGVAL(mask, value)	(((value) * __REGSHIFT(mask)) & (mask))
 
 #ifndef __ASSEMBLY__
 
@@ -40,21 +42,36 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #  define __REGB(x)	(*((volatile u8 *)io_p2v((x))))
 #  define __REGB2(x)	(*((volatile u8 *)io_p2v((x)) + (y)))
 
+#  define __REGSET(var, field, value)					\
+	((var) = (((var) & ~((field) & ~(value))) | (value)))
+
 #  define REGSET(var, reg, field, value)				\
-	((var) = (((var)						\
-		   & ~(reg ## _ ## field & 				\
-		       ~ reg ## _ ## field ## _ ## value))		\
-		  | (reg ## _ ## field ## _ ## value)))
+	__REGSET(var, reg ## _ ## field, reg ## _ ## field ## _ ## value)
+
+#  define REGSET_IDX(var, reg, field, idx, value)			\
+	__REGSET(var, reg ## _ ## field((idx)), reg ## _ ## field ## _ ## value((idx)))
 
 #  define REGSETIM(var, reg, field, value)				\
-	((var) = (((var)						\
-		   & ~(reg ## _ ## field & 				\
-		       ~(__REGVAL(reg ## _ ## field, value))))		\
-		  | (__REGVAL(reg ## _ ## field, value))))
+	__REGSET(var, reg ## _ ## field, __REGVAL(reg ## _ ## field, (value)))
+
+#  define REGSETIM_IDX(var, reg, field, idx, value)			\
+	__REGSET(var, reg ## _ ## field((idx)), __REGVAL(reg ## _ ## field((idx)), (value)))
+
+#  define __REGGET(var, field)						\
+	(((var) & (field)))
 
 #  define REGGET(var, reg, field)					\
-	((var & (reg ## _ ## field)) /					\
-	 ((reg ## _ ## field) & (-(reg ## _ ## field))))
+	 __REGGET(var, reg ## _ ## field)
+
+#  define REGGET_IDX(var, reg, field, idx)				\
+	 __REGGET(var, reg ## _ ## field((idx)))
+
+#  define REGGETIM(var, reg, field)					\
+	 __REGGET(var, reg ## _ ## field) / __REGSHIFT(reg ## _ ## field)
+
+#  define REGGETIM_IDX(var, reg, field, idx)				\
+	 __REGGET(var, reg ## _ ## field((idx))) / 			\
+	 __REGSHIFT(reg ## _ ## field((idx)))
 
 #else
 
