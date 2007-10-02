b3232d402afdb509d856a9c2c0a0e3721d97d181
Anton Vorontsov <avorontsov@ru.mvista.com>
2007-10-02 17:47:40 +0400

    [POWERPC] fsl_soc: fix uninitialized i2c_board_info structure
    i2c_board_info used semi-initialized, causing garbage in the
    info->flags, and that, in turn, causes various symptoms of i2c
    malfunctioning, like PEC mismatches.
    
    Signed-off-by: Anton Vorontsov <avorontsov@ru.mvista.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/sysdev/fsl_soc.c
+++ arch/powerpc/sysdev/fsl_soc.c
@@ -364,7 +364,7 @@ static void __init of_register_i2c_devices(struct device_node *adap_node,
 	struct device_node *node = NULL;
 
 	while ((node = of_get_next_child(adap_node, node))) {
-		struct i2c_board_info info;
+		struct i2c_board_info info = {};
 		const u32 *addr;
 		int len;
 
@@ -381,7 +381,6 @@ static void __init of_register_i2c_devices(struct device_node *adap_node,
 		if (of_find_i2c_driver(node, &info) < 0)
 			continue;
 
-		info.platform_data = NULL;
 		info.addr = *addr;
 
 		i2c_register_board_info(bus_num, &info, 1);
