808ddf879da06a0e15bee124148075fad2cd36f3
Ralph Campbell <ralph.campbell@qlogic.com>
2007-10-05 16:03:21 -0700

    IB/ipath: Remove redundant link state checks
    This patch removes some redundant checks when the SMA changes the link
    state since the same checks are made in the lower level function that
    sets the state.
    
    Signed-off-by: Ralph Campbell <ralph.campbell@qlogic.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/ipath/ipath_mad.c
+++ drivers/infiniband/hw/ipath/ipath_mad.c
@@ -403,7 +403,6 @@ static int recv_subn_set_portinfo(struct ib_smp *smp,
 	struct ib_event event;
 	struct ipath_ibdev *dev;
 	struct ipath_devdata *dd;
-	u32 flags;
 	char clientrereg = 0;
 	u16 lid, smlid;
 	u8 lwe;
@@ -542,7 +541,6 @@ static int recv_subn_set_portinfo(struct ib_smp *smp,
 	 * is down or is being set to down.
 	 */
 	state = pip->linkspeed_portstate & 0xF;
-	flags = dd->ipath_flags;
 	lstate = (pip->portphysstate_linkdown >> 4) & 0xF;
 	if (lstate && !(state == IB_PORT_DOWN || state == IB_PORT_NOP))
 		goto err;
@@ -573,13 +571,9 @@ static int recv_subn_set_portinfo(struct ib_smp *smp,
 		ipath_set_linkstate(dd, lstate);
 		break;
 	case IB_PORT_ARMED:
-		if (!(flags & (IPATH_LINKINIT | IPATH_LINKACTIVE)))
-			break;
 		ipath_set_linkstate(dd, IPATH_IB_LINKARM);
 		break;
 	case IB_PORT_ACTIVE:
-		if (!(flags & IPATH_LINKARMED))
-			break;
 		ipath_set_linkstate(dd, IPATH_IB_LINKACTIVE);
 		break;
 	default:
