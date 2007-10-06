fda41be16bfa26fe074217de637c6178df05368c
WANG Cong <xiyou.wangcong@gmail.com>
2007-10-06 11:17:13 +0800

    [WATCHDOG] Documentation/watchdog/src/watchdog-simple.c: improve this code
    Make some improvements for Documentation/watchdog/src/watchdog-simple.c.
    
    Signed-off-by: WANG Cong <xiyou.wangcong@gmail.com>
    Signed-off-by: Wim Van Sebroeck <wim@iguana.be>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- Documentation/watchdog/src/watchdog-simple.c
+++ Documentation/watchdog/src/watchdog-simple.c
@@ -4,15 +4,25 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, const char *argv[]) {
+int main(void)
+{
 	int fd = open("/dev/watchdog", O_WRONLY);
+	int ret = 0;
 	if (fd == -1) {
 		perror("watchdog");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 	while (1) {
-		write(fd, "\0", 1);
-		fsync(fd);
+		ret = write(fd, "\0", 1);
+		if (ret != 1) {
+			ret = -1;
+			break;
+		}
+		ret = fsync(fd);
+		if (ret)
+			break;
 		sleep(10);
 	}
+	close(fd);
+	return ret;
 }
