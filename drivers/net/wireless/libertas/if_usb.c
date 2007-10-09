23a926b38853ebce571df029188ab7659a6c55af
Holger Schurig <hs4233@mail.mn-solutions.de>
2007-10-09 14:30:10 +0200

    [PATCH] libertas: remove one superfluous include
    This makes scripts/checkincludes.pl happy.
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/if_usb.c
+++ drivers/net/wireless/libertas/if_usb.c
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "defs.h"
 #include "dev.h"
 #include "if_usb.h"
-#include "decl.h"
 
 #define MESSAGE_HEADER_LEN	4
 
