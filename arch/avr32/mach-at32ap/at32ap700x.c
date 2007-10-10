4824e804c5adc0729aa8ef35a86075bc402b72f6
Haavard Skinnemoen <hskinnemoen@atmel.com>
2007-10-10 14:58:29 +0200

    [AVR32] NMI debugging
    Change the NMI handler to use the die notifier chain to signal anyone
    who cares. Add a simple "nmi debugger" which hooks into this chain and
    that may dump registers, task state, etc. when it happens.
    
    Signed-off-by: Haavard Skinnemoen <hskinnemoen@atmel.com>

--- arch/avr32/mach-at32ap/at32ap700x.c
+++ arch/avr32/mach-at32ap/at32ap700x.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/spi/spi.h>
 
 #include <asm/io.h>
+#include <asm/irq.h>
 
 #include <asm/arch/at32ap700x.h>
 #include <asm/arch/board.h>
