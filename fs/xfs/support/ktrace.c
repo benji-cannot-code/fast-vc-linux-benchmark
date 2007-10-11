5f179f67905cb106865451413f3f5dc3a3b6fb2c
Eric Sandeen <sandeen@sandeen.net>
2007-10-11 17:43:32 +1000

    [XFS] ktrace kt_lock is unused, remove it.
    SGI-PV: 970382
    SGI-Modid: xfs-linux-melb:xfs-kern:29747a
    
    Signed-off-by: Eric Sandeen <sandeen@sandeen.net>
    Signed-off-by: Donald Douwsma <donaldd@sgi.com>
    Signed-off-by: Tim Shimmin <tes@sgi.com>

--- fs/xfs/support/ktrace.c
+++ fs/xfs/support/ktrace.c
@@ -91,8 +91,6 @@ ktrace_alloc(int nentries, unsigned int __nocast sleep)
 		return NULL;
 	}
 
-	spinlock_init(&(ktp->kt_lock), "kt_lock");
-
 	ktp->kt_entries  = ktep;
 	ktp->kt_nentries = nentries;
 	ktp->kt_index    = 0;
@@ -114,8 +112,6 @@ ktrace_free(ktrace_t *ktp)
 
 	if (ktp == (ktrace_t *)NULL)
 		return;
-
-	spinlock_destroy(&ktp->kt_lock);
 
 	/*
 	 * Special treatment for the Vnode trace buffer.
