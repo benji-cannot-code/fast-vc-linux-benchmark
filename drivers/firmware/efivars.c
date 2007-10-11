bd94aa79ae1d52bf53c44cbaa57a4f5659ecd8cd
Greg Kroah-Hartman <gregkh@suse.de>
2007-10-11 10:47:49 -0600

    Driver core: rename ktype_edd and ktype_efivar
    This makes it a bit more sane when trying to figure out how to clean up
    the ktype mess.
    
    Based on a larger patch from Kay Sievers
    
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/firmware/efivars.c
+++ drivers/firmware/efivars.c
@@ -403,7 +403,7 @@ static struct attribute *def_attrs[] = {
 	NULL,
 };
 
-static struct kobj_type ktype_efivar = {
+static struct kobj_type efivar_ktype = {
 	.release = efivar_release,
 	.sysfs_ops = &efivar_attr_ops,
 	.default_attrs = def_attrs,
@@ -584,7 +584,7 @@ static struct subsys_attribute *efi_subsys_attrs[] = {
 	NULL,	/* maybe more in the future? */
 };
 
-static decl_subsys(vars, &ktype_efivar, NULL);
+static decl_subsys(vars, &efivar_ktype, NULL);
 static decl_subsys(efi, NULL, NULL);
 
 /*
