07834aebebb3cce375074ef9032786a00f17d3e3
Clemens Ladisch <clemens@ladisch.de>
2007-10-11 14:42:23 +0200

    [ALSA] mpu-401: remove MPU401_INFO_UART_ONLY flag
    Since the last patch made the ENTER_UART command optional, the
    enter_uart option and its corresponding flag have become superfluous.
    The uart_enter option remains for backward compatibility but just prints
    a warning when used.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Jaroslav Kysela <perex@suse.cz>

--- include/sound/mpu401.h
+++ include/sound/mpu401.h
@@ -51,7 +51,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MPU401_INFO_INTEGRATED	(1 << 2)	/* integrated h/w port */
 #define MPU401_INFO_MMIO	(1 << 3)	/* MMIO access */
 #define MPU401_INFO_TX_IRQ	(1 << 4)	/* independent TX irq */
-#define MPU401_INFO_UART_ONLY	(1 << 5)	/* No ENTER_UART cmd needed */
 
 #define MPU401_MODE_BIT_INPUT		0
 #define MPU401_MODE_BIT_OUTPUT		1
