6a54fd33260de1b2d3b34ebcc0fc3bbeafc5e27d
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2007-10-11 23:53:59 +0200

    ide: add missing ide_rate_filter() calls to ->speedproc()-s
    * Fix icside, cris-ide, au1xxx-ide, amd74xx, via82cxxx and pmac host drivers
      to use ide_rate_filter().
    
      This results in the following modes (from user requests) being clipped down:
      - invalid modes (values 0x46-0xFF)  [ for all hosts ]
      - unsupported by a host UDMA modes  [ for hosts which support UDMA]
      - all UDMA modes and MWDMA3-4 modes [ for hosts which don't support UDMA ]
      - invalid modes (values 0x25-0x39)  [ for hosts which don't support UDMA ]
    
    * Host driver specific changes in behavior:
    
      icside:
        - no change
    
      cris-ide
        - clip unsupported UDMA3-6 modes down
        - fix BUG() on trying to set unsupported UDMA3-6 modes
    
      au1xxx-ide/pmac
        - clip unsupported UDMA modes down
    
      amd74xx/via82cxxx
        - clip unsupported UDMA modes down
        - fix random PIO timings being set for unsupported/invalid modes
        - fix unsupported/invalid modes being set on the device
    
    * While at it remove no longer needed checks from pmac.c driver.
    
    Acked-by: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/via82cxxx.c
+++ drivers/ide/pci/via82cxxx.c
@@ -166,6 +166,8 @@ static int via_set_drive(ide_drive_t *drive, u8 speed)
 	struct ide_timing t, p;
 	unsigned int T, UT;
 
+	speed = ide_rate_filter(drive, speed);
+
 	if (speed != XFER_PIO_SLOW)
 		ide_config_drive_speed(drive, speed);
 
