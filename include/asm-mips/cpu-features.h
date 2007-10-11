12e9aa8000dde9aa70b5fb2b0fbaef71340451e8
Ralf Baechle <ralf@linux-mips.org>
2007-10-11 23:46:15 +0100

    [MIPS] Allow hardwiring of the CPU type to a single type for optimization.
    This saves a few k on systems which only ever ship with a single CPU type.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/cpu-features.h
+++ include/asm-mips/cpu-features.h
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_CPU_FEATURES_H
 #define __ASM_CPU_FEATURES_H
 
-
 #include <asm/cpu.h>
 #include <asm/cpu-info.h>
 #include <cpu-feature-overrides.h>
+
+#ifndef current_cpu_type
+#define current_cpu_type()      current_cpu_data.cputype
+#endif
 
 /*
  * SMP assumption: Options of CPU 0 are a superset of all processors.
