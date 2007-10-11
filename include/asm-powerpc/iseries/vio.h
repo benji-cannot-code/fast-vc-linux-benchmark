54913341fc027153592d7a739a53198874b5764a
Stephen Rothwell <sfr@canb.auug.org.au>
2007-10-11 14:57:26 +1000

    [POWERPC] iSeries: Move detection of virtual cdroms
    Now we will only have entries in the device tree for the actual existing
    devices (including their OS/400 properties).  This way viocd.c gets all
    the information about the devices from the device tree.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Acked-by: Jens Axboe <jens.axboe@oracle.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/iseries/vio.h
+++ include/asm-powerpc/iseries/vio.h
@@ -52,6 +52,30 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define VIO_MAX_SUBTYPES 8
 
+struct viocdlpevent {
+	struct HvLpEvent	event;
+	u32			reserved;
+	u16			version;
+	u16			sub_result;
+	u16			disk;
+	u16			flags;
+	u32			token;
+	u64			offset;		/* On open, max number of disks */
+	u64			len;		/* On open, size of the disk */
+	u32			block_size;	/* Only set on open */
+	u32			media_size;	/* Only set on open */
+};
+
+enum viocdsubtype {
+	viocdopen = 0x0001,
+	viocdclose = 0x0002,
+	viocdread = 0x0003,
+	viocdwrite = 0x0004,
+	viocdlockdoor = 0x0005,
+	viocdgetinfo = 0x0006,
+	viocdcheck = 0x0007
+};
+
 /*
  * Each subtype can register a handler to process their events.
  * The handler must have this interface.
