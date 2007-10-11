e55745e457d3354ae1ffb277c1dd47db1a9d3731
Jeff Garzik <jeff@garzik.org>
2007-10-11 16:57:27 -0400

    PCI: Add 'nodomains' boot option, and pci_domains_supported global
    * Introduce pci_domains_supported global, hardcoded to zero if
      !CONFIG_PCI_DOMAINS.
    
    * Introduce 'nodomains' boot option, which clears pci_domains_supported
      on platforms that enable it by default (x86, x86-64, and others when
      they are converted to use this).
    
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>
    Cc: Andi Kleen <ak@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 unsigned int pci_pm_d3_delay = 10;
 
+#ifdef CONFIG_PCI_DOMAINS
+int pci_domains_supported = 1;
+#endif
+
 #define DEFAULT_CARDBUS_IO_SIZE		(256)
 #define DEFAULT_CARDBUS_MEM_SIZE	(64*1024*1024)
 /* pci=cbmemsize=nnM,cbiosize=nn can override this */
@@ -1568,6 +1572,13 @@ int pci_select_bars(struct pci_dev *dev, unsigned long flags)
 	return bars;
 }
 
+static void __devinit pci_no_domains(void)
+{
+#ifdef CONFIG_PCI_DOMAINS
+	pci_domains_supported = 0;
+#endif
+}
+
 static int __devinit pci_init(void)
 {
 	struct pci_dev *dev = NULL;
@@ -1589,6 +1600,8 @@ static int __devinit pci_setup(char *str)
 				pci_no_msi();
 			} else if (!strcmp(str, "noaer")) {
 				pci_no_aer();
+			} else if (!strcmp(str, "nodomains")) {
+				pci_no_domains();
 			} else if (!strncmp(str, "cbiosize=", 9)) {
 				pci_cardbus_io_size = memparse(str + 9, &str);
 			} else if (!strncmp(str, "cbmemsize=", 10)) {
