3f4af76812a83a5bcbfbd8609530d66d13b52bfa
Steve French <sfrench@us.ibm.com>
2007-10-12 18:54:12 +0000

    [CIFS] remove compile warnings when debug disabled
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsacl.c
+++ fs/cifs/cifsacl.c
@@ -132,7 +132,6 @@ int compare_sids(struct cifs_sid *ctsid, struct cifs_sid *cwsid)
 
 static void parse_ace(struct cifs_ace *pace, char *end_of_acl)
 {
-	int i;
 	int num_subauth;
 
 	/* validate that we do not go past end of acl */
@@ -146,6 +145,7 @@ static void parse_ace(struct cifs_ace *pace, char *end_of_acl)
 	num_subauth = cpu_to_le32(pace->num_subauth);
 	if (num_subauth) {
 #ifdef CONFIG_CIFS_DEBUG2
+		int i;
 		cFYI(1, ("ACE revision %d num_subauth %d",
 			pace->revision, pace->num_subauth));
 		for (i = 0; i < num_subauth; ++i) {
@@ -256,7 +256,6 @@ static void parse_dacl(struct cifs_acl *pdacl, char *end_of_acl)
 
 static int parse_sid(struct cifs_sid *psid, char *end_of_acl)
 {
-	int i;
 	int num_subauth;
 
 	/* BB need to add parm so we can store the SID BB */
@@ -270,6 +269,7 @@ static int parse_sid(struct cifs_sid *psid, char *end_of_acl)
 	num_subauth = cpu_to_le32(psid->num_subauth);
 	if (num_subauth) {
 #ifdef CONFIG_CIFS_DEBUG2
+		int i;
 		cFYI(1, ("SID revision %d num_auth %d First subauth 0x%x",
 			psid->revision, psid->num_subauth, psid->sub_auth[0]));
 
