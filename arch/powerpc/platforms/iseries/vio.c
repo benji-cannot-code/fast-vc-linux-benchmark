92bfd38041c248315cab50dd648abf54825d1d3b
Stephen Rothwell <sfr@canb.auug.org.au>
2007-10-12 15:37:32 +1000

    [POWERPC] Fix copyright symbol
    It seems to have been munged by patchwork.
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/iseries/vio.c
+++ arch/powerpc/platforms/iseries/vio.c
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Legacy iSeries specific vio initialisation
  * that needs to be built in (not a module).
  *
- * Â© Copyright 2007 IBM Corporation
+ * © Copyright 2007 IBM Corporation
  *	Author: Stephen Rothwell
  *	Some parts collected from various other files
  *
