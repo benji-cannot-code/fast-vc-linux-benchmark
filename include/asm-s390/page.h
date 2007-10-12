7d1995abd7dc83715f6105c0f054f5c165dd3f65
Sam Ravnborg <sam@ravnborg.org>
2007-10-12 16:11:50 +0200

    [S390] s390: use PAGE_SIZE in vmlinux.lds
    Replace the hardcoded 4096 value with the PAGE_SIZE macro.
    Converted a few decimal numbers to readable hex numbers.
    
    Use of PAGE_SIZE required a small change to page.h
    to allow PAGE_SIZE to be used from assembler/linker scripts.
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/asm-s390/page.h
+++ include/asm-s390/page.h
@@ -10,11 +10,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _S390_PAGE_H
 #define _S390_PAGE_H
 
+#include <linux/const.h>
 #include <asm/types.h>
 
 /* PAGE_SHIFT determines the page size */
 #define PAGE_SHIFT      12
-#define PAGE_SIZE       (1UL << PAGE_SHIFT)
+#define PAGE_SIZE	(_AC(1,UL) << PAGE_SHIFT)
 #define PAGE_MASK       (~(PAGE_SIZE-1))
 #define PAGE_DEFAULT_ACC	0
 #define PAGE_DEFAULT_KEY	(PAGE_DEFAULT_ACC << 4)
