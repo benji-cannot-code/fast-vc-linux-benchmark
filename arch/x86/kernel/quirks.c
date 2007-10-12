93710853dd6449c3c796a604e34ac3eb33a00090
Venki Pallipadi <venkatesh.pallipadi@intel.com>
2007-10-12 23:04:24 +0200

    x86: HPET force enable ICH5 suspend/resume fix
    A bugfix in ich5 hpet force detect which caused resumes to fail.  Thanks to
    Udo A Steinberg for reporting the problem.
    
    Signed-off-by: Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>
    Cc: Andi Kleen <ak@suse.de>
    Cc: john stultz <johnstul@us.ibm.com>
    Cc: Greg KH <greg@kroah.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/quirks.c
+++ arch/x86/kernel/quirks.c
@@ -202,7 +202,6 @@ static void old_ich_force_enable_hpet(struct pci_dev *dev)
 		force_hpet_address = 0xFED00000 | (val << 12);
 		printk(KERN_DEBUG "HPET at base address 0x%lx\n",
 			       force_hpet_address);
-		cached_dev = dev;
 		return;
 	}
 
@@ -224,6 +223,7 @@ static void old_ich_force_enable_hpet(struct pci_dev *dev)
 		force_hpet_address = 0xFED00000 | (val << 12);
 		printk(KERN_DEBUG "Force enabled HPET at base address 0x%lx\n",
 			       force_hpet_address);
+		cached_dev = dev;
 		force_hpet_resume_type = OLD_ICH_FORCE_HPET_RESUME;
 		return;
 	}
