4268cba079fdfaaa1073d2d5bdcdbff6d2b32a58
Cornelia Huck <cornelia.huck@de.ibm.com>
2007-10-12 16:11:21 +0200

    [S390] cio: Introduce ccw_bus_type.shutdown.
    Introduce a shutdown method for the ccw bus that calls the driver
    specific shutdown method in struct ccw_driver.
    Switch zfcp to the new ccw_driver shutdown method.
    
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/asm-s390/ccwdev.h
+++ include/asm-s390/ccwdev.h
@@ -103,6 +103,7 @@ struct ccw_device {
  * @set_online: called when setting device online
  * @set_offline: called when setting device offline
  * @notify: notify driver of device state changes
+ * @shutdown: called at device shutdown
  * @driver: embedded device driver structure
  * @name: device driver name
  */
@@ -114,6 +115,7 @@ struct ccw_driver {
 	int (*set_online) (struct ccw_device *);
 	int (*set_offline) (struct ccw_device *);
 	int (*notify) (struct ccw_device *, int);
+	void (*shutdown) (struct ccw_device *);
 	struct device_driver driver;
 	char *name;
 };
