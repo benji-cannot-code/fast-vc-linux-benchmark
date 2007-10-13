3585b85a17e73cb5cf8c5a8553f0c0edc55e94ef
Geert Uytterhoeven <geert@linux-m68k.org>
2007-10-13 14:31:30 +0200

    b43 wireless needs <linux/io.h>
    b43 wireless needs <linux/io.h>
    
      linux/drivers/net/wireless/b43/pio.h: In function 'b43_pio_write':
      linux/drivers/net/wireless/b43/pio.h:89: error: implicit declaration of function 'mmiowb'
    
      linux/drivers/net/wireless/b43/phy.c: In function 'b43_phy_write':
      linux/drivers/net/wireless/b43/phy.c:301: error: implicit declaration of function 'mmiowb'
    
      linuxdrivers/net/wireless/b43/sysfs.c: In function 'b43_attr_interfmode_store':
      linuxdrivers/net/wireless/b43/sysfs.c:147: error: implicit declaration of function 'mmiowb'
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/net/wireless/b43/sysfs.c
+++ drivers/net/wireless/b43/sysfs.c
@@ -24,12 +24,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 */
 
+#include <linux/capability.h>
+#include <linux/io.h>
+
 #include "b43.h"
 #include "sysfs.h"
 #include "main.h"
 #include "phy.h"
-
-#include <linux/capability.h>
 
 #define GENERIC_FILESIZE	64
 
