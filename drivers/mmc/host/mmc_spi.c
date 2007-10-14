16013bfe6a45d9a7bb7194e049e3ad71ac4ab648
David Brownell <david-b@pacbell.net>
2007-10-14 14:50:25 -0700

    Fix compile while compiling drivers/mmc/host/mmc_spi.o with !BLOCK
    Make sure the mmc_spi driver can build without CONFIG_BLOCK.
    Issue noted by "Avuton Olrich" <avuton@gmail.com> and randconfig.
    
    While that won't be a common configuration, sometimes embedded
    boards use SDIO to interface WLAN or Bluetooth chips (vs some
    parallel interface), and don't provide an MMC/SD socket for use
    with flash memory cards.
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/mmc/host/mmc_spi.c
+++ drivers/mmc/host/mmc_spi.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #include <linux/hrtimer.h>
 #include <linux/delay.h>
-#include <linux/blkdev.h>
+#include <linux/bio.h>
 #include <linux/dma-mapping.h>
 #include <linux/crc7.h>
 #include <linux/crc-itu-t.h>
