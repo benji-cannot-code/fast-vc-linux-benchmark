f205c3a76c6b1e4130c18d930516d4f3fe6138c5
Al Viro <viro@ftp.linux.org.uk>
2007-10-14 19:35:40 +0100

    more trivial signedness fixes in drivers
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/isdn/hisax/hfc_2bds0.c
+++ drivers/isdn/hisax/hfc_2bds0.c
@@ -1020,7 +1020,8 @@ hfc_dbusy_timer(struct IsdnCardState *cs)
 static unsigned int
 *init_send_hfcd(int cnt)
 {
-	int i, *send;
+	int i;
+	unsigned *send;
 
 	if (!(send = kmalloc(cnt * sizeof(unsigned int), GFP_ATOMIC))) {
 		printk(KERN_WARNING
