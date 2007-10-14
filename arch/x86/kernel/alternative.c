49c392aa64a2bb5c67b84c5aba31f6b41ae295f8
Dave Jones <davej@redhat.com>
2007-10-14 22:57:45 +0200

    x86: fix missing include for vsyscall
     > Maybe I just picked a bad time to try, but...
     > 
     > arch/x86/kernel/alternative.c: In function 'apply_alternatives':
     > arch/x86/kernel/alternative.c:191: error: 'VSYSCALL_START' undeclared (first use in this function)
     > arch/x86/kernel/alternative.c:191: error: (Each undeclared identifier is reported only once
     > arch/x86/kernel/alternative.c:191: error: for each function it appears in.)
     > arch/x86/kernel/alternative.c:191: error: 'VSYSCALL_END' undeclared (first use in this function)
     > make[1]: *** [arch/x86/kernel/alternative.o] Error 1
     > make: *** [arch/x86/kernel] Error 2
    
    Try this.
    
    Include missing header for vsyscall.
    
    Signed-off-by: Dave Jones <davej@redhat.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/mce.h>
 #include <asm/nmi.h>
+#include <asm/vsyscall.h>
 
 #define MAX_PATCH_LEN (255-1)
 
