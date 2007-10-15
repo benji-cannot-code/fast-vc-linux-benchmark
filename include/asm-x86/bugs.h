4ce04882b95013da1025b70dbb7ab8247483aee4
Thomas Gleixner <tglx@linutronix.de>
2007-10-15 13:57:47 +0200

    x86: unify some more trivial include/asm-x86/ 32/64 variants
    Scripted unification.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/bugs.h
+++ include/asm-x86/bugs.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifdef CONFIG_X86_32
-# include "bugs_32.h"
-#else
-# include "bugs_64.h"
-#endif
+#ifndef _ASM_X86_BUGS_H
+#define _ASM_X86_BUGS_H
+
+void check_bugs(void);
+
+#endif /* _ASM_X86_BUGS_H */
