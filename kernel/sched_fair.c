d223827ce3fa8671c21516143a864388d873c226
Ingo Molnar <mingo@elte.hu>
2007-10-15 17:00:05 +0200

    sched: simplify check_preempt() methods
    simplify the check_preempt() methods.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Mike Galbraith <efault@gmx.de>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -583,8 +583,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int sleep)
  * Preempt the current task with a newly woken task if needed:
  */
 static void
-__check_preempt_curr_fair(struct cfs_rq *cfs_rq, struct sched_entity *se,
-			  struct sched_entity *curr)
+__check_preempt_curr_fair(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
 	unsigned long ideal_runtime, delta_exec;
 
@@ -664,7 +663,7 @@ static void entity_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 	if (next == curr)
 		return;
 
-	__check_preempt_curr_fair(cfs_rq, next, curr);
+	__check_preempt_curr_fair(cfs_rq, curr);
 }
 
 /**************************************************
@@ -867,7 +866,7 @@ static void check_preempt_curr_fair(struct rq *rq, struct task_struct *p)
 	}
 
 	if (is_same_group(curr, p))
-		__check_preempt_curr_fair(cfs_rq, &p->se, &curr->se);
+		__check_preempt_curr_fair(cfs_rq, &curr->se);
 }
 
 static struct task_struct *pick_next_task_fair(struct rq *rq)
