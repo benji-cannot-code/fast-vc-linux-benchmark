4a4e984845118ed9bc5a98275d4b44ca45f0188d
Thomas Gleixner <tglx@linutronix.de>
2007-10-15 23:28:21 +0200

    x86: unify include/asm/siginfo_32/64.h
    Same file, except for the 64bit PREAMBLE_SIZE define.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/siginfo_64.h
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifndef _X8664_SIGINFO_H
-#define _X8664_SIGINFO_H
-
-#define __ARCH_SI_PREAMBLE_SIZE	(4 * sizeof(int))
-
-#include <asm-generic/siginfo.h>
-
-#endif
