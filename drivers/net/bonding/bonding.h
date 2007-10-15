7aefd0e066112870db3d73a5327a308faec3b4b2
Jay Vosburgh <fubar@us.ibm.com>
2007-10-15 16:44:27 -0700

    bonding: two small fixes for IPoIB support
    	Two small fixes to IPoIB support for bonding:
    
    	1- copy header_ops from slave to bonding for IPoIB slaves
    	2- move release and destroy logic to UNREGISTER from GOING_DOWN
    	   notifier to avoid double release
    
    	Set bonding to version 3.2.1.
    
    Signed-off-by: Moni Shoua <monis at voltaire.com>
    Signed-off-by: Jay Vosburgh <fubar@us.ibm.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/bonding/bonding.h
+++ drivers/net/bonding/bonding.h
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "bond_3ad.h"
 #include "bond_alb.h"
 
-#define DRV_VERSION	"3.2.0"
-#define DRV_RELDATE	"September 13, 2007"
+#define DRV_VERSION	"3.2.1"
+#define DRV_RELDATE	"October 15, 2007"
 #define DRV_NAME	"bonding"
 #define DRV_DESCRIPTION	"Ethernet Channel Bonding Driver"
 
