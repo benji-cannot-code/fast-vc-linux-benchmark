aabe5964bc9753e0eddb7376edf4a108da8b1f5f
Brian Gerst <bgerst@didntduck.org>
2007-10-15 13:57:46 +0200

    x86: trivial header merges
    Merge 32/64-bit headers that simply redirect to asm-generic
    
    [tglx: fixup Kbuild as well]
    
    Signed-off-by: Brian Gerst <bgerst@didntduck.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/errno.h
+++ include/asm-x86/errno.h
@@ -1,14 +1,2 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-#ifdef __KERNEL__
-# ifdef CONFIG_X86_32
-#  include "errno_32.h"
-# else
-#  include "errno_64.h"
-# endif
-#else
-# ifdef __i386__
-#  include "errno_32.h"
-# else
-#  include "errno_64.h"
-# endif
-#endif
+#include <asm-generic/errno.h>
