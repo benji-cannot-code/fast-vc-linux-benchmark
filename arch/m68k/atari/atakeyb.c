2fc5ccdd88e0c1a41dc0d4ad0f75014f4a79c637
Geert Uytterhoeven <geert@linux-m68k.org>
2007-10-15 21:51:10 +0200

    Atari keyboard: incorporate additional review comments
    Atari keyboard: incorporate additional review comments:
      o Kill reference to source file name
      o Return error value from input_register_device() instead of -ENOMEM
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Cc: Michael Schmitz <schmitz@biophys.uni-duesseldorf.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/m68k/atari/atakeyb.c
+++ arch/m68k/atari/atakeyb.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * linux/arch/m68k/atari/atakeyb.c
- *
  * Atari Keyboard driver for 680x0 Linux
  *
  * This file is subject to the terms and conditions of the GNU General Public
