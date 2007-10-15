74d68c551ba08896c13044231b485aec0d154409
Laurent Vivier <Laurent.Vivier@bull.net>
2007-10-15 17:00:19 +0200

    sched: guest CPU accounting: maintain guest state in KVM
    Modify KVM to update guest time accounting.
    
    [ mingo@elte.hu: ported to 2.6.24 KVM. ]
    
    Signed-off-by: Laurent Vivier <Laurent.Vivier@bull.net>
    Acked-by: Avi Kivity <avi@qumranet.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- drivers/kvm/kvm_main.c
+++ drivers/kvm/kvm_main.c
@@ -2047,6 +2047,7 @@ again:
 		kvm_x86_ops->inject_pending_vectors(vcpu, kvm_run);
 
 	vcpu->guest_mode = 1;
+	kvm_guest_enter();
 
 	if (vcpu->requests)
 		if (test_and_clear_bit(KVM_TLB_FLUSH, &vcpu->requests))
@@ -2054,6 +2055,7 @@ again:
 
 	kvm_x86_ops->run(vcpu, kvm_run);
 
+	kvm_guest_exit();
 	vcpu->guest_mode = 0;
 	local_irq_enable();
 
