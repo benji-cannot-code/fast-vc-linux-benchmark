348366f36ac55632ca766550ae1502d520b50eb9
Jeff Dike <jdike@addtoit.com>
2007-10-16 01:27:12 -0700

    uml: clean up tlb flush path
    Tidy the tlb flushing code.
    
    With tt mode gone, there is no reason to have the capability to have
    called directly from do_mmap, do_mprotect, and do_munmap, rather than
    calling a function pointer that it is given.
    
    There was a large amount of data that was passed from function to
    function, being used at the lowest level, without being changed.  This
    stuff is now encapsulated in a structure which is initialized at the
    top layer and passed down.  This simplifies the code, reduces the
    amount of code needed to pass the parameters around, and saves on
    stack space.
    
    A somewhat more subtle change is the meaning of the current operation
    index.  It used to start at -1, being pre-incremented when adding an
    operation.  It now starts at 0, being post-incremented, with
    associated adjustments of +/- 1 on comparisons.
    
    In addition, tlb.h contained a couple of declarations which had no
    users outside of tlb.c, so they could be moved or deleted.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/include/tlb.h
+++ arch/um/include/tlb.h
@@ -9,34 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "um_mmu.h"
 
-struct host_vm_op {
-	enum { NONE, MMAP, MUNMAP, MPROTECT } type;
-	union {
-		struct {
-			unsigned long addr;
-			unsigned long len;
-			unsigned int prot;
-			int fd;
-			__u64 offset;
-		} mmap;
-		struct {
-			unsigned long addr;
-			unsigned long len;
-		} munmap;
-		struct {
-			unsigned long addr;
-			unsigned long len;
-			unsigned int prot;
-		} mprotect;
-	} u;
-};
-
 extern void force_flush_all(void);
-extern void fix_range_common(struct mm_struct *mm, unsigned long start_addr,
-                             unsigned long end_addr, int force,
-			     int (*do_ops)(struct mm_context *,
-					   struct host_vm_op *, int, int,
-					   void **));
 extern int flush_tlb_kernel_range_common(unsigned long start,
 					 unsigned long end);
 
