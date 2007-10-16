8ba64885400c6b9a6016e373abcba7d3654b2417
Nick Piggin <npiggin@suse.de>
2007-10-16 01:24:56 -0700

    mm: debug write deadlocks
    Allow CONFIG_DEBUG_VM to switch off the prefaulting logic, to simulate the
    Makes the race much easier to hit.
    
    This is useful for demonstration and testing purposes, but is removed in a
    subsequent patch.
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1879,6 +1879,7 @@ generic_file_buffered_write(struct kiocb *iocb, const struct iovec *iov,
 		if (maxlen > bytes)
 			maxlen = bytes;
 
+#ifndef CONFIG_DEBUG_VM
 		/*
 		 * Bring in the user page that we will copy from _first_.
 		 * Otherwise there's a nasty deadlock on copying from the
@@ -1886,6 +1887,7 @@ generic_file_buffered_write(struct kiocb *iocb, const struct iovec *iov,
 		 * up-to-date.
 		 */
 		fault_in_pages_readable(buf, maxlen);
+#endif
 
 		page = __grab_cache_page(mapping,index,&cached_page,&lru_pvec);
 		if (!page) {
