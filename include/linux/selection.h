6af2bd11e514aa986614708eff794f85a9186047
Ralf Baechle <ralf@linux-mips.org>
2007-10-16 01:29:04 -0700

    vt: Fix warnings in selection.h
    <linux/selection.h> assumes that struct tty_struct has previously been
    included.  If not, this pile of warnings will result:
    
      CC [M]  drivers/video/console/newport_con.o
    In file included from drivers/video/console/newport_con.c:18:
    include/linux/selection.h:16: warning: 'struct tty_struct' declared inside param
    eter list
    include/linux/selection.h:16: warning: its scope is only this definition or decl
    aration, which is probably not what you want
    include/linux/selection.h:17: warning: 'struct tty_struct' declared inside param
    eter list
    include/linux/selection.h:20: warning: 'struct tty_struct' declared inside param
    eter list
    
    Fixed by adding a forward declaration of struct tty_struct.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Antonino Daplas <adaplas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/selection.h
+++ include/linux/selection.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct tty_struct;
 
 extern struct vc_data *sel_cons;
+struct tty_struct;
 
 extern void clear_selection(void);
 extern int set_selection(const struct tiocl_selection __user *sel, struct tty_struct *tty);
