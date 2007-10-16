31f0901d89282e203abb29ce4fc28973a8392f82
Jesper Juhl <jesper.juhl@gmail.com>
2007-10-16 11:51:29 -0700

    Clean up duplicate includes in arch/i386/xen/
    This patch cleans up duplicate includes in
    	arch/i386/xen/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Signed-off-by: Jeremy Fitzhardinge <jeremy@xensource.com>

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/page-flags.h>
 #include <linux/highmem.h>
-#include <linux/smp.h>
 
 #include <xen/interface/xen.h>
 #include <xen/interface/physdev.h>
