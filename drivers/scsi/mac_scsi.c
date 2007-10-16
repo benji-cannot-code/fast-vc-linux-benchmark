b6fd2b581bb0c2caa273fa4104e71ad68d4a73e3
Boaz Harrosh <bharrosh@panasas.com>
2007-10-16 10:25:01 +0200

    scsi_mac.h: Define AUTOSENSE before include of NCR5380.h
      - Previese patch to NCR5380 broke scsi_mac because
        AUTOSENSE was defined after the inclusion of
        NCR5380.h. Fix it
    
    Signed-off-by: Boaz Harrosh <bharrosh@panasas.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/scsi/mac_scsi.c
+++ drivers/scsi/mac_scsi.c
@@ -54,6 +54,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "scsi.h"
 #include <scsi/scsi_host.h>
 #include "mac_scsi.h"
+
+/* These control the behaviour of the generic 5380 core */
+#define AUTOSENSE
+#define PSEUDO_DMA
+
 #include "NCR5380.h"
 
 #if 0
@@ -571,10 +576,6 @@ static int macscsi_pwrite (struct Scsi_Host *instance,
    return 0;
 }
 
-
-/* These control the behaviour of the generic 5380 core */
-#define AUTOSENSE
-#define PSEUDO_DMA
 
 #include "NCR5380.c"
 
