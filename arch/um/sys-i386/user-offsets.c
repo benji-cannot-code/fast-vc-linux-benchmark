04a09175715f3887db016ad6f54a6ed7dc1add86
Jeff Dike <jdike@addtoit.com>
2007-10-16 01:27:14 -0700

    uml: don't use glibc asm/user.h
    Stop including asm/user.h from libc - it seems to be disappearing from
    distros.  It's replaced with sys/user.h which defines user_fpregs_struct and
    user_fpxregs_struct instead of user_i387_struct and struct user_fxsr_struct on
    i386.
    
    As a bonus, on x86_64, I get to dump some stupid typedefs which were needed in
    order to get asm/user.h to compile.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/sys-i386/user-offsets.c
+++ arch/um/sys-i386/user-offsets.c
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <stddef.h>
 #include <signal.h>
 #include <sys/poll.h>
+#include <sys/user.h>
 #include <sys/mman.h>
 #include <asm/ptrace.h>
-#include <asm/user.h>
 
 #define DEFINE(sym, val) \
 	asm volatile("\n->" #sym " %0 " #val : : "i" (val))
@@ -49,8 +49,8 @@ void foo(void)
 	OFFSET(HOST_SC_FP_ST, _fpstate, _st);
 	OFFSET(HOST_SC_FXSR_ENV, _fpstate, _fxsr_env);
 
-	DEFINE_LONGS(HOST_FP_SIZE, sizeof(struct user_i387_struct));
-	DEFINE_LONGS(HOST_XFP_SIZE, sizeof(struct user_fxsr_struct));
+	DEFINE_LONGS(HOST_FP_SIZE, sizeof(struct user_fpregs_struct));
+	DEFINE_LONGS(HOST_XFP_SIZE, sizeof(struct user_fpxregs_struct));
 
 	DEFINE(HOST_IP, EIP);
 	DEFINE(HOST_SP, UESP);
