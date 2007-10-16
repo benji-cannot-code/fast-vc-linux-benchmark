93d47505681c5d2068c00e50974273a0a8b1dd6c
Sam Ravnborg <sam@ravnborg.org>
2007-10-16 01:26:35 -0700

    alpha: beautify vmlinux.lds
    Introduced a consistent style in vmlinux.lds and it now matches the
    soon-to-be common style for all arch's vmlinux.lds files.
    
    In addition:
    - Replaced hardcoded constant with PAGE_SIZE
    - Fix page.h so PAGE_SIZE can be used from assembler and in lds files
    - Move a few labels inside brackets so linker alignment will not
      make label point ot a too low address
    - Replaced DWARF and STABS sections with definitions from asm-generic
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-alpha/page.h
+++ include/asm-alpha/page.h
@@ -4,11 +4,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef __KERNEL__
 
+#include <linux/const.h>
 #include <asm/pal.h>
 
 /* PAGE_SHIFT determines the page size */
 #define PAGE_SHIFT	13
-#define PAGE_SIZE	(1UL << PAGE_SHIFT)
+#define PAGE_SIZE	(_AC(1,UL) << PAGE_SHIFT)
 #define PAGE_MASK	(~(PAGE_SIZE-1))
 
 #ifndef __ASSEMBLY__
