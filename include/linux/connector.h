0237a4180beedbb3c48bc9a54c8b96911aaa013f
Michal Januszewski <spock@gentoo.org>
2007-10-16 01:28:25 -0700

    uvesafb: add connector entries
    Add connector idx and val constants for v86d and uvesafb.
    
    Signed-off-by: Michal Januszewski <spock@gentoo.org>
    Signed-off-by: Antonino Daplas <adaplas@gmail.com>
    Cc: Evgeniy Polyakov <johnpol@2ka.mipt.ru>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/connector.h
+++ include/linux/connector.h
@@ -37,9 +37,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CN_VAL_CIFS                     0x1
 #define CN_W1_IDX			0x3	/* w1 communication */
 #define CN_W1_VAL			0x1
+#define CN_IDX_V86D			0x4
+#define CN_VAL_V86D_UVESAFB		0x1
 
-
-#define CN_NETLINK_USERS		4
+#define CN_NETLINK_USERS		5
 
 /*
  * Maximum connector's message size.
