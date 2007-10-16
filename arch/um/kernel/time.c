4dddce0eb3ad0630a4d0ee3176337071a947cd2c
Jeff Dike <jdike@addtoit.com>
2007-10-16 01:27:21 -0700

    uml: eliminate hz()
    Eliminate hz() since its only purpose was to provide a kernel-space constant
    to userspace code.  This can be done instead by providing the constant
    directly through kernel_constants.h.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/kernel/time.c
+++ arch/um/kernel/time.c
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kern_util.h"
 #include "os.h"
 
-int hz(void)
-{
-	return HZ;
-}
-
 /*
  * Scheduler clock - returns current time in nanosec units.
  */
