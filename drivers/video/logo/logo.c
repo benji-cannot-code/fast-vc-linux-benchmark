dfc686700a6a78a4b8951fd346046e3458f0a147
Randy Dunlap <rdunlap@xenotime.net>
2007-10-16 01:29:37 -0700

    fbcon: logo: disable logo at boot
    Add logo.nologo kernel boot option to disable the logo in order to provide
    more screen space for kernel messages; especially useful when debugging and
    screen space is more critical.
    
    newport_con driver changes are untested.
    
    [akpm@linux-foundation.org: cleanups, coding-style fixes]
    Signed-off-by: Randy Dunlap <rdunlap@xenotime.net>
    Signed-off-by: Antonino Daplas <adaplas@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -35,6 +35,10 @@ extern const struct linux_logo logo_superh_vga16;
 extern const struct linux_logo logo_superh_clut224;
 extern const struct linux_logo logo_m32r_clut224;
 
+static int nologo;
+module_param(nologo, bool, 0);
+MODULE_PARM_DESC(nologo, "Disables startup logo");
+
 /* logo's are marked __initdata. Use __init_refok to tell
  * modpost that it is intended that this function uses data
  * marked __initdata.
@@ -42,6 +46,9 @@ extern const struct linux_logo logo_m32r_clut224;
 const struct linux_logo * __init_refok fb_find_logo(int depth)
 {
 	const struct linux_logo *logo = NULL;
+
+	if (nologo)
+		return NULL;
 
 	if (depth >= 1) {
 #ifdef CONFIG_LOGO_LINUX_MONO
