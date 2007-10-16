b18106f65960cb778916c1cd9b162fce3221db44
Avi Kivity <avi@qumranet.com>
2007-10-16 17:22:08 +0200

    KVM: Rename KVM_TLB_FLUSH to KVM_REQ_TLB_FLUSH
    We now have a new namespace, KVM_REQ_*, for bits in vcpu->requests.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/kvm.h
+++ drivers/kvm/kvm.h
@@ -68,7 +68,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  * vcpu->requests bit members
  */
-#define KVM_TLB_FLUSH 0
+#define KVM_REQ_TLB_FLUSH          0
 
 /*
  * Address types:
