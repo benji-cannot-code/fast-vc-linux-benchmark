81fef3909e9365714946495de6c18509e1ce232f
Jean Delvare <khali@linux-fr.org>
2007-10-16 01:28:18 -0700

    rtc-pcf8583: Check for i2c adapter functionality
    Not all i2c adapters support I2C-level messaging.  Check that the adapter
    does before probing for a PCF8583 chip, as the driver makes use of
    i2c_transfer and i2c_master_send.
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Alessandro Zummo <a.zummo@towertech.it>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-pcf8583.c
+++ drivers/rtc/rtc-pcf8583.c
@@ -333,6 +333,9 @@ static int pcf8583_probe(struct i2c_adapter *adap, int addr, int kind)
 		}
 	};
 
+	if (!i2c_check_functionality(adap, I2C_FUNC_I2C))
+		return 0;
+
 	pcf = kzalloc(sizeof(*pcf), GFP_KERNEL);
 	if (!pcf)
 		return -ENOMEM;
