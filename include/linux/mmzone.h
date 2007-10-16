59490e3136c04915ba35491cc31c9e56cb0d1376
Mel Gorman <mel@csn.ul.ie>
2007-10-16 01:25:53 -0700

    Group high-order atomic allocations
    In rare cases, the kernel needs to allocate a high-order block of pages
    without sleeping.  For example, this is the case with e1000 cards configured
    to use jumbo frames.  Migrating or reclaiming pages in this situation is not
    an option.
    
    This patch groups these allocations together as much as possible by adding a
    new MIGRATE_TYPE.  The MIGRATE_HIGHATOMIC type are exactly what they sound
    like.  Care is taken that pages of other migrate types do not use the same
    blocks as high-order atomic allocations.
    
    Signed-off-by: Mel Gorman <mel@csn.ul.ie>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -38,11 +38,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MIGRATE_UNMOVABLE     0
 #define MIGRATE_RECLAIMABLE   1
 #define MIGRATE_MOVABLE       2
-#define MIGRATE_TYPES         3
+#define MIGRATE_HIGHATOMIC    3
+#define MIGRATE_TYPES         4
 #else
 #define MIGRATE_UNMOVABLE     0
 #define MIGRATE_UNRECLAIMABLE 0
 #define MIGRATE_MOVABLE       0
+#define MIGRATE_HIGHATOMIC    0
 #define MIGRATE_TYPES         1
 #endif
 
