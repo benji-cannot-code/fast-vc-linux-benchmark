6e2d94daefb16c8f69a90c5ee105e705c4cf6645
Jesper Juhl <jesper.juhl@gmail.com>
2007-10-16 23:26:22 -0700

    Clean up duplicate includes in fs/
    This patch cleans up duplicate includes in
    	fs/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -109,7 +109,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dvb/dmx.h>
 #include <linux/dvb/frontend.h>
 #include <linux/dvb/video.h>
-#include <linux/lp.h>
 
 #ifdef CONFIG_SPARC
 #include <asm/fbio.h>
