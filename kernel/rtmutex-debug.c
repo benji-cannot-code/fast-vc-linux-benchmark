a0030ad906f30f16f1b3da285ac5ee552b98fd4b
Adrian Bunk <bunk@stusta.de>
2007-10-16 23:26:40 -0700

    kernel/rtmutex-debug.c: cleanups
    This patch contains the following cleanups:
    - make the needlessly global variable rt_trace_on static
    - remove the unused global function deadlock_trace_off()
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/rtmutex-debug.c
+++ kernel/rtmutex-debug.c
@@ -83,12 +83,7 @@ do {						\
  * into the tracing code when doing error printk or
  * executing a BUG():
  */
-int rt_trace_on = 1;
-
-void deadlock_trace_off(void)
-{
-	rt_trace_on = 0;
-}
+static int rt_trace_on = 1;
 
 static void printk_task(struct task_struct *p)
 {
