871ca72b81df42e67f8b0c804d7b4705b76efd66
Jesper Juhl <jesper.juhl@gmail.com>
2007-10-16 23:31:15 -0700

    Clean up duplicate includes in Documentation/
    This patch cleans up duplicate includes in
    	Documentation/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Acked-by: Balbir Singh <balbir@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- Documentation/accounting/getdelays.c
+++ Documentation/accounting/getdelays.c
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <signal.h>
 
 #include <linux/genetlink.h>
