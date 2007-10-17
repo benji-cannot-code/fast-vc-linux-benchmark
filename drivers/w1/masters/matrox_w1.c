69d99e299d3b1a2ab679f8a85ec273fd4ead3054
Jesper Juhl <jesper.juhl@gmail.com>
2007-10-16 23:26:22 -0700

    Clean up duplicate includes in drivers/w1/
    This patch cleans up duplicate includes in
    	drivers/w1/
    
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>
    Acked-by: Evgeniy Polyakov <johnpol@2ka.mipt.ru>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/w1/masters/matrox_w1.c
+++ drivers/w1/masters/matrox_w1.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/pci_ids.h>
 #include <linux/pci.h>
-#include <linux/timer.h>
 
 #include "../w1.h"
 #include "../w1_int.h"
