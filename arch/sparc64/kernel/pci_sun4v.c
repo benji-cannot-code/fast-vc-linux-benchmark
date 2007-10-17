9670088ca97032a3329e851697c1516a35bf697d
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2007-10-17 09:22:14 +0200

    SPARC64: fix iommu sg chaining
    Commit 2c941a204070ab32d92d40318a3196a7fb994c00 looks incomplete. The
    helper functions like prepare_sg() need to support sg chaining too.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- arch/sparc64/kernel/pci_sun4v.c
+++ arch/sparc64/kernel/pci_sun4v.c
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/irq.h>
 #include <linux/msi.h>
 #include <linux/log2.h>
-#include <linux/scatterlist.h>
 
 #include <asm/iommu.h>
 #include <asm/irq.h>
