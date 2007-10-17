3bfbdb9785db12dcd29a39b1a12ca0dd52fc3452
Olaf Hering <olaf@aepfle.de>
2007-10-16 23:27:09 -0700

    include linux/types.h in if_fddi.h
    include/linux/if_fddi.h is an exported header.
    It uses __be16. Include linux/types.h to get this prototype.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Cc: "Maciej W. Rozycki" <macro@linux-mips.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/if_fddi.h
+++ include/linux/if_fddi.h
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_IF_FDDI_H
 #define _LINUX_IF_FDDI_H
 
+#include <linux/types.h>
+
 /*
  *  Define max and min legal sizes.  The frame sizes do not include
  *  4 byte FCS/CRC (frame check sequence).
