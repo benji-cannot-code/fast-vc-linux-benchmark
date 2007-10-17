50bcb9dfcc29549517ef06edfac34f679fd9a97e
Adrian Bunk <bunk@kernel.org>
2007-10-17 18:04:38 +0200

    x86: rename .i assembler includes to .h
    .i is an ending used for preprocessed stuff.
    
    This patch therefore renames assembler include files to .h and guards
    the contents with an #ifdef __ASSEMBLY__.
    
    [ tglx: arch/x86 adaptation ]
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-um/frame.h
+++ include/asm-um/frame.h
@@ -2,6 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __UM_FRAME_I
 #define __UM_FRAME_I
 
-#include "asm/arch/frame.i"
+#include "asm/arch/frame.h"
 
 #endif
