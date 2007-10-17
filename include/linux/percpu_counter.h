a9c46c8e797c692395300b6ad4bcea450aa8a55c
Peter Zijlstra <a.p.zijlstra@chello.nl>
2007-10-16 23:25:44 -0700

    lib: percpu_counter_set
    Provide a method to set a percpu counter to a specified value.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/percpu_counter.h
+++ include/linux/percpu_counter.h
@@ -33,6 +33,7 @@ struct percpu_counter {
 
 void percpu_counter_init(struct percpu_counter *fbc, s64 amount);
 void percpu_counter_destroy(struct percpu_counter *fbc);
+void percpu_counter_set(struct percpu_counter *fbc, s64 amount);
 void __percpu_counter_add(struct percpu_counter *fbc, s64 amount, s32 batch);
 s64 percpu_counter_sum(struct percpu_counter *fbc);
 
@@ -74,6 +75,11 @@ static inline void percpu_counter_init(struct percpu_counter *fbc, s64 amount)
 
 static inline void percpu_counter_destroy(struct percpu_counter *fbc)
 {
+}
+
+static inline void percpu_counter_set(struct percpu_counter *fbc, s64 amount)
+{
+	fbc->count = amount;
 }
 
 #define __percpu_counter_add(fbc, amount, batch) \
