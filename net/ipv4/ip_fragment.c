b2dc4e799ddd927e890660d01e4beaf095526767
David Howells <dhowells@redhat.com>
2007-10-17 21:37:22 -0700

    [NET]: Fix uninitialised variable in ip_frag_reasm()
    Fix uninitialised variable in ip_frag_reasm().  err should be set to
    -ENOMEM if the initial call of skb_clone() fails.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/ip_fragment.c
+++ net/ipv4/ip_fragment.c
@@ -487,7 +487,6 @@ static int ip_frag_reasm(struct ipq *qp, struct sk_buff *prev,
 	if (prev) {
 		head = prev->next;
 		fp = skb_clone(head, GFP_ATOMIC);
-
 		if (!fp)
 			goto out_nomem;
 
@@ -513,7 +512,6 @@ static int ip_frag_reasm(struct ipq *qp, struct sk_buff *prev,
 		goto out_oversize;
 
 	/* Head of list must not be cloned. */
-	err = -ENOMEM;
 	if (skb_cloned(head) && pskb_expand_head(head, 0, 0, GFP_ATOMIC))
 		goto out_nomem;
 
@@ -569,6 +567,7 @@ static int ip_frag_reasm(struct ipq *qp, struct sk_buff *prev,
 out_nomem:
 	LIMIT_NETDEBUG(KERN_ERR "IP: queue_glue: no memory for gluing "
 			      "queue %p\n", qp);
+	err = -ENOMEM;
 	goto out_fail;
 out_oversize:
 	if (net_ratelimit())
