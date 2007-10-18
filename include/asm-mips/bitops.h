7dba1478753a87ad5ebcb9d7e293c306617f3235
Nick Piggin <npiggin@suse.de>
2007-10-18 03:06:52 -0700

    mips: fix bitops
    Documentation/atomic_ops.txt defines these primitives must contain a memory
    barrier both before and after their memory operation.  This is consistent with
    the atomic ops implementation on mips.
    
    Signed-off-by: Nick Piggin <npiggin@suse.de>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-mips/bitops.h
+++ include/asm-mips/bitops.h
@@ -241,6 +241,8 @@ static inline int test_and_set_bit(unsigned long nr,
 	unsigned short bit = nr & SZLONG_MASK;
 	unsigned long res;
 
+	smp_llsc_mb();
+
 	if (cpu_has_llsc && R10000_LLSC_WAR) {
 		unsigned long *m = ((unsigned long *) addr) + (nr >> SZLONG_LOG);
 		unsigned long temp;
@@ -308,6 +310,8 @@ static inline int test_and_clear_bit(unsigned long nr,
 {
 	unsigned short bit = nr & SZLONG_MASK;
 	unsigned long res;
+
+	smp_llsc_mb();
 
 	if (cpu_has_llsc && R10000_LLSC_WAR) {
 		unsigned long *m = ((unsigned long *) addr) + (nr >> SZLONG_LOG);
@@ -396,6 +400,8 @@ static inline int test_and_change_bit(unsigned long nr,
 {
 	unsigned short bit = nr & SZLONG_MASK;
 	unsigned long res;
+
+	smp_llsc_mb();
 
 	if (cpu_has_llsc && R10000_LLSC_WAR) {
 		unsigned long *m = ((unsigned long *) addr) + (nr >> SZLONG_LOG);
