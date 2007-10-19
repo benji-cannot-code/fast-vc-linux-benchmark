79e1d7228f06689df313c5b010883709e9749190
Lennert Buytenhek <buytenh@wantstofly.org>
2007-10-19 04:11:03 +0200

    mv643xx_eth: Enable use on Orion platforms
    Allow Orion ARM platforms to use the mv643xx_eth driver.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Acked-by: Tzachi Perelstein <tzachi@marvell.com>
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>

--- drivers/net/mv643xx_eth.c
+++ drivers/net/mv643xx_eth.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * drivers/net/mv643xx_eth.c - Driver for MV643XX ethernet ports
+ * Driver for Marvell Discovery (MV643XX) and Marvell Orion ethernet ports
  * Copyright (C) 2002 Matthew Dharm <mdharm@momenco.com>
  *
  * Based on the 64360 driver from:
