ec0034914c2620e2ebece1c5e0abb8e06c996f21
Cliff Wickman <cpw@sgi.com>
2007-10-18 23:40:46 -0700

    hotplug cpu: migrate a task within its cpuset
    When a cpu is disabled, move_task_off_dead_cpu() is called for tasks that have
    been running on that cpu.
    
    Currently, such a task is migrated:
     1) to any cpu on the same node as the disabled cpu, which is both online
        and among that task's cpus_allowed
     2) to any cpu which is both online and among that task's cpus_allowed
    
    It is typical of a multithreaded application running on a large NUMA system to
    have its tasks confined to a cpuset so as to cluster them near the memory that
    they share.  Furthermore, it is typical to explicitly place such a task on a
    specific cpu in that cpuset.  And in that case the task's cpus_allowed
    includes only a single cpu.
    
    This patch would insert a preference to migrate such a task to some cpu within
    its cpuset (and set its cpus_allowed to its entire cpuset).
    
    With this patch, migrate the task to:
     1) to any cpu on the same node as the disabled cpu, which is both online
        and among that task's cpus_allowed
     2) to any online cpu within the task's cpuset
     3) to any cpu which is both online and among that task's cpus_allowed
    
    In order to do this, move_task_off_dead_cpu() must make a call to
    cpuset_cpus_allowed_locked(), a new subset of cpuset_cpus_allowed(), that will
    not block.  (name change - per Oleg's suggestion)
    
    Calls are made to cpuset_lock() and cpuset_unlock() in migration_call() to set
    the cpuset mutex during the whole migrate_live_tasks() and
    migrate_dead_tasks() procedure.
    
    [akpm@linux-foundation.org: build fix]
    [pj@sgi.com: Fix indentation and spacing]
    Signed-off-by: Cliff Wickman <cpw@sgi.com>
    Cc: Oleg Nesterov <oleg@tv-sign.ru>
    Cc: Christoph Lameter <clameter@sgi.com>
    Cc: Paul Jackson <pj@sgi.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Paul Jackson <pj@sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -22,6 +22,7 @@ extern int cpuset_init_early(void);
 extern int cpuset_init(void);
 extern void cpuset_init_smp(void);
 extern cpumask_t cpuset_cpus_allowed(struct task_struct *p);
+extern cpumask_t cpuset_cpus_allowed_locked(struct task_struct *p);
 extern nodemask_t cpuset_mems_allowed(struct task_struct *p);
 #define cpuset_current_mems_allowed (current->mems_allowed)
 void cpuset_init_current_mems_allowed(void);
@@ -85,6 +86,10 @@ static inline int cpuset_init(void) { return 0; }
 static inline void cpuset_init_smp(void) {}
 
 static inline cpumask_t cpuset_cpus_allowed(struct task_struct *p)
+{
+	return cpu_possible_map;
+}
+static inline cpumask_t cpuset_cpus_allowed_locked(struct task_struct *p)
 {
 	return cpu_possible_map;
 }
