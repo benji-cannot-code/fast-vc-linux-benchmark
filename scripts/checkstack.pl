6beee7c25f5567ca96ff9414b7afbd77d0bb1647
Joern Engel <joern@logfs.org>
2007-10-18 23:39:18 -0700

    fix a trivial typo in scripts/checkstack.pl
    Trivial change in a comment.
    
    Signed-off-by: Joern Engel <joern@logfs.org>
    Signed-off-by: Andre Haupt <andre@finow14.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- scripts/checkstack.pl
+++ scripts/checkstack.pl
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #	AVR32 port by Haavard Skinnemoen <hskinnemoen@atmel.com>
 #
 #	Usage:
-#	objdump -d vmlinux | stackcheck.pl [arch]
+#	objdump -d vmlinux | scripts/checkstack.pl [arch]
 #
 #	TODO :	Port to all architectures (one regex per arch)
 
