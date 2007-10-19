dd1a28476b67398d6485c723f7f9ed3d356c2216
Miklos Szeredi <mszeredi@suse.cz>
2007-10-18 23:39:11 -0700

    put declaration of put_filesystem() in fs.h
    Declarations go into headers.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Cc: Ram Pai <linuxram@us.ibm.com>
    Acked-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/super.c
+++ fs/super.c
@@ -41,10 +41,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 
 
-void get_filesystem(struct file_system_type *fs);
-void put_filesystem(struct file_system_type *fs);
-struct file_system_type *get_fs_type(const char *name);
-
 LIST_HEAD(super_blocks);
 DEFINE_SPINLOCK(sb_lock);
 
