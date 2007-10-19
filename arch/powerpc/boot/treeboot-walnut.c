f1e061853d3dfe2ed3f6ce02fe3d28b2d19995d8
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2007-10-20 00:53:11 +1000

    [POWERPC] 4xx: Enable EMAC for PPC405 Walnut board
    This patch enables the ibm_newemac driver for the Walnut board.  It fixes the
    device tree for the walnut board to order the MAL interrupts correctly and
    adds the local-mac-address property to the EMAC node.  The bootwrapper is also
    updated to extract the MAC address from the OpenBIOS offset where it is stored.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/boot/treeboot-walnut.c
+++ arch/powerpc/boot/treeboot-walnut.c
@@ -110,6 +110,7 @@ static void walnut_flashsel_fixup(void)
 	setprop(sram, "reg", reg_sram, sizeof(reg_sram));
 }
 
+#define WALNUT_OPENBIOS_MAC_OFF 0xfffffe0b
 static void walnut_fixups(void)
 {
 	ibm4xx_fixup_memsize();
@@ -117,6 +118,7 @@ static void walnut_fixups(void)
 	ibm4xx_quiesce_eth((u32 *)0xef600800, NULL);
 	ibm4xx_fixup_ebc_ranges("/plb/ebc");
 	walnut_flashsel_fixup();
+	dt_fixup_mac_addresses((u8 *) WALNUT_OPENBIOS_MAC_OFF);
 }
 
 void platform_init(void)
