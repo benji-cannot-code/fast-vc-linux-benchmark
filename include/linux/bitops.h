c41983f43168a206d67b63eeaee5b83f4ff19cf5
Jiri Slaby <jirislaby@gmail.com>
2007-10-18 23:40:37 -0700

    remove BITS_TO_TYPE macro
    remove BITS_TO_TYPE macro
    
    I realized, that it is actually the same as DIV_ROUND_UP, use it instead.
    
    [akpm@linux-foundation.org: build fix]
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/bitops.h
+++ include/linux/bitops.h
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BIT(nr)			(1UL << (nr))
 #define BIT_MASK(nr)		(1UL << ((nr) % BITS_PER_LONG))
 #define BIT_WORD(nr)		((nr) / BITS_PER_LONG)
-#define BITS_TO_TYPE(nr, t)	(((nr)+(t)-1)/(t))
-#define BITS_TO_LONGS(nr)	BITS_TO_TYPE(nr, BITS_PER_LONG)
+#define BITS_TO_LONGS(nr)	DIV_ROUND_UP(nr, BITS_PER_LONG)
 #define BITS_PER_BYTE		8
 #endif
 
