8cbf9c030c84022344c30bcb52c6ff1e3b59559c
Johann Deneux <johann.deneux@gmail.comx>
2007-10-20 00:47:32 +0200

    changed email
    Changed email address of Johann Deneux (myself)
    Also removed CVS tags in comments (no longer using cvs)
    
    Signed-off-by: Johann Deneux <johann.deneux@gmail.com>
    Signed-off-by: Adrian Bunk <bunk@kernel.org>

--- drivers/hid/usbhid/hid-ff.c
+++ drivers/hid/usbhid/hid-ff.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: hid-ff.c,v 1.2 2002/04/18 22:02:47 jdeneux Exp $
- *
  *  Force feedback support for hid devices.
  *  Not all hid devices use the same protocol. For example, some use PID,
  *  other use their own proprietary procotol.
