1d69f6ea03191c98eaaca8598aec11249a8dd54a
Jose R. Santos <jrs@us.ibm.com>
2007-10-18 23:39:22 -0700

    jbd: config_jbd_debug cannot create /proc entry
    The jbd-debug file used to be located in /proc/sys/fs/jbd-debug, but
    create_proc_entry() does not do lookups on file names that are more that
    one directory deep.  This causes the entry creation to fail and hence, no
    proc file is created.
    
    Instead of fixing this on procfs might as well move the jbd2-debug file to
    debugfs which would be the preferred location for this kind of tunable.
    The new location is now /sys/kernel/debug/jbd/jbd-debug.
    
    [akpm@linux-foundation.org: zillions of cleanups]
    Signed-off-by: Jose R. Santos <jrs@us.ibm.com>
    Acked-by: Jan Kara <jack@suse.cz>
    Cc: <linux-ext4@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/jbd.h
+++ include/linux/jbd.h
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * CONFIG_JBD_DEBUG is on.
  */
 #define JBD_EXPENSIVE_CHECKING
-extern int journal_enable_debug;
+extern u8 journal_enable_debug;
 
 #define jbd_debug(n, f, a...)						\
 	do {								\
