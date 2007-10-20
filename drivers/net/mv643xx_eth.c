7c6f4e2b12f711302ea79cd5c56ddd2329ce14e1
Dale Farnsworth <dale@farnsworth.org>
2007-10-20 08:44:17 -0700

    mv643xx_eth: Remove obsolete checksum offload comment
    We fixed checksum offload a while back.  Remove the note that
    it doesn't work.
    
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>

--- drivers/net/mv643xx_eth.c
+++ drivers/net/mv643xx_eth.c
@@ -59,9 +59,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/delay.h>
 #include <asm/dma-mapping.h>
 
-/* Checksum offload for Tx works for most packets, but
- * fails if previous packet sent did not use hw csum
- */
 #define MV643XX_CHECKSUM_OFFLOAD_TX
 #define MV643XX_NAPI
 #define MV643XX_TX_FAST_REFILL
