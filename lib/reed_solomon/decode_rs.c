49e512966a3b559bf9fb5bb14252b1f3dc9d4fc7
Jörn Engel <joern@logfs.org>
2007-10-20 23:14:42 +0200

    [RSLIB] BUG() when passing illegal parameters to decode_rs8() or decode_rs16()
    Returning -ERANGE should never happen.
    
    Signed-off-by: Jörn Engel <joern@logfs.org>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- lib/reed_solomon/decode_rs.c
+++ lib/reed_solomon/decode_rs.c
@@ -40,8 +40,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 	/* Check length parameter for validity */
 	pad = nn - nroots - len;
-	if (pad < 0 || pad >= nn)
-		return -ERANGE;
+	BUG_ON(pad < 0 || pad >= nn);
 
 	/* Does the caller provide the syndrome ? */
 	if (s != NULL)
