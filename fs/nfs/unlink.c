2a0328d2d8eb2b2eae875b9dd816ad41eb49fd3d
Linus Torvalds <torvalds@woody.linux-foundation.org>
2007-10-19 19:59:18 -0700

    Avoid compile error in fs/nfs/unlink.c
    Erez Zadok reports that certain configurations fail to build due to
    schedule() TASK_[UN]INTERRUPTIBLE not being declared.  Add proper
    include files to fix.
    
    Cc: Erez Zadok <ezk@cs.sunysb.edu>
    Cc: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/nfs/unlink.c
+++ fs/nfs/unlink.c
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sunrpc/sched.h>
 #include <linux/sunrpc/clnt.h>
 #include <linux/nfs_fs.h>
-
+#include <linux/sched.h>
+#include <linux/wait.h>
 
 struct nfs_unlinkdata {
 	struct hlist_node list;
