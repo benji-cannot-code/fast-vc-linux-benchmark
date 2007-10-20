4671a701b51800cf4324352144ded272e9791f3b
Zhang Xiantao <xiantao.zhang@intel.com>
2007-10-20 15:34:38 +0800

    KVM: Portability: Split kvm_vcpu into arch dependent and independent parts (part 1)
    First step to split kvm_vcpu.  Currently, we just use an macro to define
    the common fields in kvm_vcpu for all archs, and all archs need to define
    its own kvm_vcpu struct.
    
    Signed-off-by: Zhang Xiantao <xiantao.zhang@intel.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/lapic.c
+++ drivers/kvm/lapic.c
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include "kvm.h"
+#include "x86.h"
+
 #include <linux/kvm.h>
 #include <linux/mm.h>
 #include <linux/highmem.h>
--- drivers/kvm/x86_emulate.c
+++ drivers/kvm/x86_emulate.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DPRINTF(_f, _a ...) printf(_f , ## _a)
 #else
 #include "kvm.h"
+#include "x86.h"
 #define DPRINTF(x...) do {} while (0)
 #endif
 #include "x86_emulate.h"
