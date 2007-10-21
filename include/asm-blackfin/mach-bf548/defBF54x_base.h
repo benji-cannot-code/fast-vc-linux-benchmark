036c4bc12f94fd031a37c5042678f00008bcc84d
Bryan Wu <bryan.wu@analog.com>
2007-10-21 16:58:49 +0800

    Blackfin arch: Fix bug: missing CHIPID register field definition of BF54x
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- include/asm-blackfin/mach-bf548/defBF54x_base.h
+++ include/asm-blackfin/mach-bf548/defBF54x_base.h
@@ -48,6 +48,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Debug/MP/Emulation Registers (0xFFC00014 - 0xFFC00014) */
 
 #define                           CHIPID  0xffc00014
+/* CHIPID Masks */
+#define                   CHIPID_VERSION  0xF0000000
+#define                    CHIPID_FAMILY  0x0FFFF000
+#define               CHIPID_MANUFACTURE  0x00000FFE
 
 /* System Reset and Interrupt Controller (0xFFC00100 - 0xFFC00104) */
 
