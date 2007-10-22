7ae048085921b5a2415ec69b83a748856af54387
Rusty Russell <rusty@rustcorp.com.au>
2007-10-22 11:03:39 +1000

    Virtio console driver
    This is an hvc-based virtio console driver.  It's suboptimal becuase
    hvc expects to have raw access to interrupts and virtio doesn't assume
    that, so it currently polls.
    
    There are two solutions: expose hvc's "kick" interface, or wean off hvc.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

---
+++ include/linux/virtio_console.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _LINUX_VIRTIO_CONSOLE_H
+#define _LINUX_VIRTIO_CONSOLE_H
+#include <linux/virtio_config.h>
+
+/* The ID for virtio console */
+#define VIRTIO_ID_CONSOLE	3
+
+#ifdef __KERNEL__
+int __init virtio_cons_early_init(int (*put_chars)(u32, const char *, int));
+#endif /* __KERNEL__ */
+
+#endif /* _LINUX_VIRTIO_CONSOLE_H */
