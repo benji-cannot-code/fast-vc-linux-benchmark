da8d22126b2349c07a0c434ef200b97f188610a7
Rusty Russell <rusty@rustcorp.com.au>
2007-10-22 10:56:24 +1000

    lguest_devices belongs in lguest_bus.c: it's not i386-specific.
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/lguest/lguest_bus.c
+++ drivers/lguest/lguest_bus.c
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/paravirt.h>
 
+struct lguest_device_desc *lguest_devices;
+
 static ssize_t type_show(struct device *_dev,
                          struct device_attribute *attr, char *buf)
 {
