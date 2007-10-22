446c2542a9c32b8e4084bb829d24ec25c4b95ff3
Jes Sorensen <jes@sgi.com>
2007-10-22 11:03:28 +1000

    Move i386 part of core.c to x86/core.c.
    Separate i386 architecture specific from core.c and move it to
    x86/core.c and add x86/lguest.h header file to match.
    
    Signed-off-by: Jes Sorensen <jes@sgi.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- arch/x86/lguest/boot.c
+++ arch/x86/lguest/boot.c
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/e820.h>
 #include <asm/mce.h>
 #include <asm/io.h>
+#include <asm/i387.h>
 
 /*G:010 Welcome to the Guest!
  *
