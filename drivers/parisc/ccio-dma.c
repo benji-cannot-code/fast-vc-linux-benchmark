87a8f88381209c616d75ee732467df7ae7e9ffde
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2007-10-23 09:30:28 +0200

    parisc: fix sg_page() fallout
    arch/parisc/kernel/pci-dma.c: In function 'pa11_dma_map_sg':
    arch/parisc/kernel/pci-dma.c:487: error: 'struct scatterlist' has no member named 'page'
    arch/parisc/kernel/pci-dma.c: In function 'pa11_dma_unmap_sg':
    arch/parisc/kernel/pci-dma.c:508: error: 'struct scatterlist' has no member named 'page'
    arch/parisc/kernel/pci-dma.c:508: error: 'struct scatterlist' has no member named 'page'
    arch/parisc/kernel/pci-dma.c: In function 'pa11_dma_sync_sg_for_cpu':
    arch/parisc/kernel/pci-dma.c:535: error: 'struct scatterlist' has no member named 'page'
    arch/parisc/kernel/pci-dma.c:535: error: 'struct scatterlist' has no member named 'page'
    arch/parisc/kernel/pci-dma.c: In function 'pa11_dma_sync_sg_for_device':
    arch/parisc/kernel/pci-dma.c:545: error: 'struct scatterlist' has no member named 'page'
    arch/parisc/kernel/pci-dma.c:545: error: 'struct scatterlist' has no member named 'page'
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- drivers/parisc/ccio-dma.c
+++ drivers/parisc/ccio-dma.c
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/reboot.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/scatterlist.h>
 
 #include <asm/byteorder.h>
 #include <asm/cache.h>		/* for L1_CACHE_BYTES */
