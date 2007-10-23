6ca36d14b35bc6caa05f23e7b66c72874a446d53
Maciej W. Rozycki <macro@linux-mips.org>
2007-10-23 12:43:25 +0100

    [MIPS] R4000/R4400 daddiu erratum workaround
     This complements the generic R4000/R4400 errata workaround code and adds 
    bits for the daddiu problem.  In most places it just modifies handwritten 
    assembly code so that the assembler is allowed to use a temporary register 
    as daddiu may now be treated as a macro that expands to a sequence of li 
    and daddu.  It is the AT register or, where AT is unavailable or used 
    explicitly for another purpose, an explicitly-named register is selected, 
    using the .set at=<reg> feature added recently to gas.  This feature is 
    only used if CONFIG_CPU_DADDI_WORKAROUNDS has been set, so if the 
    workaround remains disabled, the required version of binutils stays 
    unchanged.
    
     Similarly, daddiu instructions put in branch delay slots in noreorder 
    fragments are now taken out of them and the assembler is allowed to 
    reorder them itself as possible (which it does making the whole idea of 
    scheduling them into delay slots manually questionable).
    
     Also in the very few places where such a simple conversion was not 
    possible, a handcoded longer sequence is implemented.
    
     Other than that there are changes to code responsible for building the 
    TLB fault and page clear/copy handlers to avoid daddiu as appropriate.  
    These are only effective if the erratum is verified to be present at the 
    run time.
    
     Finally there is a trivial update to __delay(), because it uses daddiu in 
    a branch delay slot.
    
    Signed-off-by: Maciej W. Rozycki <macro@linux-mips.org>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- include/asm-mips/uaccess.h
+++ include/asm-mips/uaccess.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (C) 1996, 1997, 1998, 1999, 2000, 03, 04 by Ralf Baechle
  * Copyright (C) 1999, 2000 Silicon Graphics, Inc.
+ * Copyright (C) 2007  Maciej W. Rozycki
  */
 #ifndef _ASM_UACCESS_H
 #define _ASM_UACCESS_H
@@ -388,6 +389,12 @@ extern void __put_user_unknown(void);
 	"jal\t" #destination "\n\t"
 #endif
 
+#ifndef CONFIG_CPU_DADDI_WORKAROUNDS
+#define DADDI_SCRATCH "$0"
+#else
+#define DADDI_SCRATCH "$3"
+#endif
+
 extern size_t __copy_user(void *__to, const void *__from, size_t __n);
 
 #define __invoke_copy_to_user(to, from, n)				\
@@ -404,7 +411,7 @@ extern size_t __copy_user(void *__to, const void *__from, size_t __n);
 	: "+r" (__cu_to_r), "+r" (__cu_from_r), "+r" (__cu_len_r)	\
 	:								\
 	: "$8", "$9", "$10", "$11", "$12", "$15", "$24", "$31",		\
-	  "memory");							\
+	  DADDI_SCRATCH, "memory");					\
 	__cu_len_r;							\
 })
 
@@ -513,7 +520,7 @@ extern size_t __copy_user_inatomic(void *__to, const void *__from, size_t __n);
 	: "+r" (__cu_to_r), "+r" (__cu_from_r), "+r" (__cu_len_r)	\
 	:								\
 	: "$8", "$9", "$10", "$11", "$12", "$15", "$24", "$31",		\
-	  "memory");							\
+	  DADDI_SCRATCH, "memory");					\
 	__cu_len_r;							\
 })
 
@@ -536,7 +543,7 @@ extern size_t __copy_user_inatomic(void *__to, const void *__from, size_t __n);
 	: "+r" (__cu_to_r), "+r" (__cu_from_r), "+r" (__cu_len_r)	\
 	:								\
 	: "$8", "$9", "$10", "$11", "$12", "$15", "$24", "$31",		\
-	  "memory");							\
+	  DADDI_SCRATCH, "memory");					\
 	__cu_len_r;							\
 })
 
