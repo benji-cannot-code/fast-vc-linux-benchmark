065c3cd888673fdd4975f1e76596b13ce65f1d32
Eric Van Hensbergen <ericvh@gmail.com>
2007-10-23 13:47:31 -0500

    9p: add virtio transport
    This adds a transport to 9p for communicating between guests and a host
    using a virtio based transport.
    
    Signed-off-by: Eric Van Hensbergen <ericvh@gmail.com>

---
+++ include/linux/virtio_9p.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _LINUX_VIRTIO_9P_H
+#define _LINUX_VIRTIO_9P_H
+#include <linux/virtio_config.h>
+
+/* The ID for virtio console */
+#define VIRTIO_ID_9P	9
+/* Maximum number of virtio channels per partition (1 for now) */
+#define MAX_9P_CHAN	1
+
+#endif /* _LINUX_VIRTIO_9P_H */
