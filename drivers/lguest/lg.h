7212d4164b491cafe604d8a0ce12cd97d5aea117
Rusty Russell <rusty@rustcorp.com.au>
2007-10-23 15:19:49 +1000

    Remove binfmts.h include from lg.h
    It wasn't needed since a very early prototype of lguest.
    
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/lguest/lg.h
+++ drivers/lguest/lg.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/init.h>
 #include <linux/stringify.h>
-#include <linux/binfmts.h>
 #include <linux/futex.h>
 #include <linux/lguest.h>
 #include <linux/lguest_launcher.h>
