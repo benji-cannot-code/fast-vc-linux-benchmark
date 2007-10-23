703c3863e2b235dfb392a4ebec2034465802c25c
Tzachi Perelstein <tzachi@marvell.com>
2007-10-23 15:14:41 -0400

    [ARM] basic support for the Marvell Orion SoC family
    The Marvell Orion is a family of ARM SoCs with a DDR/DDR2 memory
    controller, 10/100/1000 ethernet MAC, and USB 2.0 interfaces,
    and, depending on the specific model, PCI-E interface, PCI-X
    interface, SATA controllers, crypto unit, SPI interface, SDIO
    interface, device bus, NAND controller, DMA engine and/or XOR
    engine.
    
    This contains the basic structure and architecture register definitions.
    
    Signed-off-by: Tzachi Perelstein <tzachi@marvell.com>
    Reviewed-by: Nicolas Pitre <nico@marvell.com>
    Reviewed-by: Lennert Buytenhek <buytenh@marvell.com>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>

---
+++ arch/arm/mach-orion/common.h
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ARCH_ORION_COMMON_H__
+#define __ARCH_ORION_COMMON_H__
+
+/*
+ * Basic Orion init functions used early by machine-setup.
+ */
+void __init orion_map_io(void);
+
+#endif /* __ARCH_ORION_COMMON_H__ */
