453813d392e57abdec28b8a1778c763747cc8da2
Tzachi Perelstein <tzachi@marvell.com>
2007-10-23 15:14:42 -0400

    [ARM] Orion: PCI support
    This patch adds support for PCI and PCI-E controllers in the
    Orion, Orion-NAS and Orion2.
    
    Signed-off-by: Tzachi Perelstein <tzachi@marvell.com>
    Reviewed-by: Nicolas Pitre <nico@marvell.com>
    Reviewed-by: Lennert Buytenhek <buytenh@marvell.com>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-orion/common.h
+++ arch/arm/mach-orion/common.h
@@ -7,4 +7,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 void __init orion_map_io(void);
 
+/*
+ * Shared code used internally by other Orion core functions.
+ * (/mach-orion/pci.c)
+ */
+
+struct pci_sys_data;
+struct pci_bus;
+
+void orion_pcie_id(u32 *dev, u32 *rev);
+u32 orion_pcie_local_bus_nr(void);
+u32 orion_pci_local_bus_nr(void);
+u32 orion_pci_local_dev_nr(void);
+int orion_pci_sys_setup(int nr, struct pci_sys_data *sys);
+struct pci_bus *orion_pci_sys_scan_bus(int nr, struct pci_sys_data *sys);
+int orion_pci_hw_rd_conf(u32 bus, u32 dev, u32 func, u32 where, u32 size, u32 *val);
+int orion_pci_hw_wr_conf(u32 bus, u32 dev, u32 func, u32 where, u32 size, u32 val);
+
 #endif /* __ARCH_ORION_COMMON_H__ */
