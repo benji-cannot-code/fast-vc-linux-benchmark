a358e71183c68961e6e2b1b33416b0b38ec03607
Christian Borntraeger <borntraeger@de.ibm.com>
2007-10-23 12:46:32 +0200

    Fix sctp compile
    sctp fails to compile with
    net/sctp/sm_make_chunk.c: In function 'sctp_pack_cookie':
    net/sctp/sm_make_chunk.c:1516: error: implicit declaration of function 'sg_init_table'
    net/sctp/sm_make_chunk.c:1517: error: implicit declaration of function 'sg_set_page'
    
    use the proper include file.
    
    SCTP maintainers Vlad Yasevich and Sridhar Samudrala  are CCed.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- net/sctp/sm_make_chunk.c
+++ net/sctp/sm_make_chunk.c
@@ -57,7 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ipv6.h>
 #include <linux/net.h>
 #include <linux/inet.h>
-#include <asm/scatterlist.h>
+#include <linux/scatterlist.h>
 #include <linux/crypto.h>
 #include <net/sock.h>
 
