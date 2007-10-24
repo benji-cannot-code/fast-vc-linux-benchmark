03078229e13f0349b4bde8f3b3eeba58b751dda8
Joerg Roedel <joerg.roedel@amd.com>
2007-10-24 12:49:47 +0200

    x86 gart: rename iommu.h to gart.h
    This patch renames the include file asm-x86/iommu.h to asm-x86/gart.h to make
    clear to which IOMMU implementation it belongs. The patch also adds "GART" to
    the Kconfig line.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Acked-by: Muli Ben-Yehuda <muli@il.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/early-quirks.c
+++ arch/x86/kernel/early-quirks.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/apic.h>
 
 #ifdef CONFIG_IOMMU
-#include <asm/iommu.h>
+#include <asm/gart.h>
 #endif
 
 static void __init via_bugs(void)
--- arch/x86/kernel/pci-dma_64.c
+++ arch/x86/kernel/pci-dma_64.c
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/dmar.h>
 #include <asm/io.h>
-#include <asm/iommu.h>
+#include <asm/gart.h>
 #include <asm/calgary.h>
 
 int iommu_merge __read_mostly = 1;
