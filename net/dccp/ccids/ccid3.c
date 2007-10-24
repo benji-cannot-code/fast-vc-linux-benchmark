17dd8e1db07973c4105ba93e392bde46d9636323
Gerrit Renker <gerrit@erg.abdn.ac.uk>
2007-10-24 10:53:01 -0200

    [CCID2/3]: Initialisation assignments of 0 are redundant
    Assigning initial values of `0' is redundant when loading a new CCID structure,
    since in net/dccp/ccid.c the entire CCID structure is zeroed out prior to
    initialisation in ccid_new():
    
        	struct ccid {
        		struct ccid_operations *ccid_ops;
        		char		       ccid_priv[0];
        	};
    
        	// ...
        	if (rx) {
        		memset(ccid + 1, 0, ccid_ops->ccid_hc_rx_obj_size);
        		if (ccid->ccid_ops->ccid_hc_rx_init != NULL &&
        		    ccid->ccid_ops->ccid_hc_rx_init(ccid, sk) != 0)
        			goto out_free_ccid;
        	} else {
        		memset(ccid + 1, 0, ccid_ops->ccid_hc_tx_obj_size);
        		/* analogous to the rx case */
        	}
    
    This patch therefore removes the redundant assignments. Thanks to Arnaldo for
    the inspiration.
    
    Signed-off-by: Gerrit Renker <gerrit@erg.abdn.ac.uk>
    Signed-off-by: Ian McDonald <ian.mcdonald@jandi.co.nz>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>

--- net/dccp/ccids/ccid3.c
+++ net/dccp/ccids/ccid3.c
@@ -605,8 +605,6 @@ static int ccid3_hc_tx_init(struct ccid *ccid, struct sock *sk)
 {
 	struct ccid3_hc_tx_sock *hctx = ccid_priv(ccid);
 
-	hctx->ccid3hctx_s     = 0;
-	hctx->ccid3hctx_rtt   = 0;
 	hctx->ccid3hctx_state = TFRC_SSTATE_NO_SENT;
 	INIT_LIST_HEAD(&hctx->ccid3hctx_hist);
 
@@ -967,8 +965,6 @@ static int ccid3_hc_rx_init(struct ccid *ccid, struct sock *sk)
 	INIT_LIST_HEAD(&hcrx->ccid3hcrx_li_hist);
 	hcrx->ccid3hcrx_tstamp_last_feedback =
 		hcrx->ccid3hcrx_tstamp_last_ack = ktime_get_real();
-	hcrx->ccid3hcrx_s   = 0;
-	hcrx->ccid3hcrx_rtt = 0;
 	return 0;
 }
 
