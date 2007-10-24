f29947b9ff12d437217298871be0d6697ec1f354
Satyam Sharma <satyam@infradead.org>
2007-10-24 18:23:50 +0200

    sched: use show_regs() to improve __schedule_bug() output
    A full register dump along with stack backtrace would make the
    "scheduling while atomic" message more helpful. Use show_regs() instead
    of dump_stack() for this. We already know we're atomic in here (that is
    why this function was called) so show_regs()'s atomicity expectations
    are guaranteed.
    
    Also, modify the output of the "BUG: scheduling while atomic:" header a
    bit to keep task->comm and task->pid together and preempt_count() after
    them.
    
    Signed-off-by: Satyam Sharma <satyam@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pagemap.h>
 
 #include <asm/tlb.h>
+#include <asm/irq_regs.h>
 
 /*
  * Scheduler clock - returns current time in nanosec units.
@@ -3508,12 +3509,19 @@ EXPORT_SYMBOL(sub_preempt_count);
  */
 static noinline void __schedule_bug(struct task_struct *prev)
 {
-	printk(KERN_ERR "BUG: scheduling while atomic: %s/0x%08x/%d\n",
-		prev->comm, preempt_count(), task_pid_nr(prev));
+	struct pt_regs *regs = get_irq_regs();
+
+	printk(KERN_ERR "BUG: scheduling while atomic: %s/%d/0x%08x\n",
+		prev->comm, prev->pid, preempt_count());
+
 	debug_show_held_locks(prev);
 	if (irqs_disabled())
 		print_irqtrace_events(prev);
-	dump_stack();
+
+	if (regs)
+		show_regs(regs);
+	else
+		dump_stack();
 }
 
 /*
