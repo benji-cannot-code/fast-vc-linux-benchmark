807fd69e65d22adae519b2601b2eba37fe8e2824
Jeff Garzik <jgarzik@redhat.com>
2007-10-24 12:58:02 +0200

    x86: lguest build fix
    Fix this error (i386 !SMP build):
    
    arch/x86/lguest/boot.c: In function lguest_init:
    arch/x86/lguest/boot.c:1059: error: pm_power_off undeclared (first use in this function)
    
    by including linux/pm.h.
    
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/lguest/boot.c
+++ arch/x86/lguest/boot.c
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lguest.h>
 #include <linux/lguest_launcher.h>
 #include <linux/virtio_console.h>
+#include <linux/pm.h>
 #include <asm/paravirt.h>
 #include <asm/param.h>
 #include <asm/page.h>
