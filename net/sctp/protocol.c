64ce9aafa815a1803fc30567722855ed9ad4de0c
Vlad Yasevich <vladislav.yasevich@hp.com>
2007-10-24 17:24:26 -0400

    SCTP: Allow ADD_IP to work with AUTH for backward compatibility.
    This patch adds a tunable that will allow ADD_IP to work without
    AUTH for backward compatibility.  The default value is off since
    the default value for ADD_IP is off as well.  People who need
    to use ADD-IP with older implementations take risks of connection
    hijacking and should consider upgrading or turning this tunable on.
    
    Signed-off-by: Vlad Yasevich <vladislav.yasevich@hp.com>

--- net/sctp/protocol.c
+++ net/sctp/protocol.c
@@ -1180,6 +1180,7 @@ SCTP_STATIC __init int sctp_init(void)
 
 	/* Disable ADDIP by default. */
 	sctp_addip_enable = 0;
+	sctp_addip_noauth = 0;
 
 	/* Enable PR-SCTP by default. */
 	sctp_prsctp_enable = 1;
