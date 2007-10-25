822fdc8a5ef011175ee2d0e5efe57d07845c9a5f
Hugh Dickins <hugh@veritas.com>
2007-10-25 09:55:05 +0200

    fix sg_phys to use dma_addr_t
    x86_32 CONFIG_HIGHMEM64G with 5GB RAM hung when booting, after issuing
    some "request_module: runaway loop modprobe binfmt-0000" messages in
    trying to exec /sbin/init.
    
    The binprm buf doesn't see the right ".ELF" header because sg_phys()
    is providing the wrong physical addresses for high pages: a 32-bit
    unsigned long is too small in this case, we need to use dma_addr_t.
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Jens Axboe <jens.axboe@oracle.com>

--- include/linux/scatterlist.h
+++ include/linux/scatterlist.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LINUX_SCATTERLIST_H
 #define _LINUX_SCATTERLIST_H
 
+#include <asm/types.h>
 #include <asm/scatterlist.h>
 #include <linux/mm.h>
 #include <linux/string.h>
@@ -256,7 +257,7 @@ static inline void sg_init_table(struct scatterlist *sgl, unsigned int nents)
  *   on the sg page.
  *
  **/
-static inline unsigned long sg_phys(struct scatterlist *sg)
+static inline dma_addr_t sg_phys(struct scatterlist *sg)
 {
 	return page_to_phys(sg_page(sg)) + sg->offset;
 }
