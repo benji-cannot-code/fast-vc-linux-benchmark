fe7aa78bac4aad42b4652a65678a6352453805a9
Chuck Lever <chuck.lever@oracle.com>
2007-10-26 13:32:56 -0400

    SUNRPC: RPC program information is stored in unsigned integers
    Clean up: When looping over RPC version and procedure numbers, use
    unsigned index variables.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- net/sunrpc/svc.c
+++ net/sunrpc/svc.c
@@ -365,7 +365,7 @@ __svc_create(struct svc_program *prog, unsigned int bufsize, int npools,
 	   void (*shutdown)(struct svc_serv *serv))
 {
 	struct svc_serv	*serv;
-	int vers;
+	unsigned int vers;
 	unsigned int xdrsize;
 	unsigned int i;
 
@@ -764,7 +764,8 @@ svc_register(struct svc_serv *serv, int proto, unsigned short port)
 {
 	struct svc_program	*progp;
 	unsigned long		flags;
-	int			i, error = 0, dummy;
+	unsigned int		i;
+	int			error = 0, dummy;
 
 	if (!port)
 		clear_thread_flag(TIF_SIGPENDING);
