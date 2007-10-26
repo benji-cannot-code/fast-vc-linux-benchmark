239fbce2c2a4f6f6537d13ceb624d6e6a6cb65da
David S. Miller <davem@sunset.davemloft.net>
2007-10-26 00:38:39 -0700

    [IPSEC]: Add missing sg_init_table() calls to ESP.
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/esp4.c
+++ net/ipv4/esp4.c
@@ -111,6 +111,7 @@ static int esp_output(struct xfrm_state *x, struct sk_buff *skb)
 			if (!sg)
 				goto unlock;
 		}
+		sg_init_table(sg, nfrags);
 		skb_to_sgvec(skb, sg, esph->enc_data+esp->conf.ivlen-skb->data, clen);
 		err = crypto_blkcipher_encrypt(&desc, sg, sg, clen);
 		if (unlikely(sg != &esp->sgbuf[0]))
@@ -202,6 +203,7 @@ static int esp_input(struct xfrm_state *x, struct sk_buff *skb)
 		if (!sg)
 			goto out;
 	}
+	sg_init_table(sg, nfrags);
 	skb_to_sgvec(skb, sg, sizeof(*esph) + esp->conf.ivlen, elen);
 	err = crypto_blkcipher_decrypt(&desc, sg, sg, elen);
 	if (unlikely(sg != &esp->sgbuf[0]))
