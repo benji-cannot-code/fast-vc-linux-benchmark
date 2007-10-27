2d9d456203dabe6d5251d83ed71caf430e880f8f
Michael Krufky <mkrufky@linuxtv.org>
2007-10-27 02:00:57 -0300

    V4L/DVB (6468): tda8290: auto-detect tda8290 or tda8295
    Consolidate tda8290_attach() and tda8295_attach() into a single function,
    tda829x_attach(), which will detect chip combinations tda8290 or tda8295 with
    tda8275, tda8275a or tda18271.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tda8290.h
+++ drivers/media/video/tda8290.h
@@ -24,8 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #if defined(CONFIG_TUNER_TDA8290) || (defined(CONFIG_TUNER_TDA8290_MODULE) && defined(MODULE))
 extern int tda8290_probe(struct tuner *t);
 
-extern int tda8290_attach(struct tuner *t);
-extern int tda8295_attach(struct tuner *t);
+extern int tda829x_attach(struct tuner *t);
 #else
 static inline int tda8290_probe(struct tuner *t)
 {
@@ -33,14 +32,7 @@ static inline int tda8290_probe(struct tuner *t)
 	return -EINVAL;
 }
 
-static inline int tda8290_attach(struct tuner *t)
-{
-	printk(KERN_INFO "%s: not probed - driver disabled by Kconfig\n",
-	       __FUNCTION__);
-	return -EINVAL;
-}
-
-static inline int tda8295_attach(struct tuner *t)
+static inline int tda829x_attach(struct tuner *t)
 {
 	printk(KERN_INFO "%s: not probed - driver disabled by Kconfig\n",
 	       __FUNCTION__);
