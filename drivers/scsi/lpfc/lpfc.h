38c95bf68eee4a6029cc490d305d30b942aa678d
James Smart <James.Smart@Emulex.Com>
2007-10-27 13:38:11 -0400

    [SCSI] lpfc 8.2.3 : Temperature handling fix
    Temperature handling fix - return proper error code indicator for applications
    
    Signed-off-by: James Smart <James.Smart@emulex.com>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/lpfc/lpfc.h
+++ drivers/scsi/lpfc/lpfc.h
@@ -387,6 +387,11 @@ struct hbq_s {
 #define LPFC_ELS_HBQ	0
 #define LPFC_EXTRA_HBQ	1
 
+enum hba_temp_state {
+	HBA_NORMAL_TEMP,
+	HBA_OVER_TEMP
+};
+
 struct lpfc_hba {
 	struct lpfc_sli sli;
 	uint32_t sli_rev;		/* SLI2 or SLI3 */
@@ -590,6 +595,7 @@ struct lpfc_hba {
 	 */
 #define QUE_BUFTAG_BIT  (1<<31)
 	uint32_t buffer_tag_count;
+	enum hba_temp_state over_temp_state;
 };
 
 static inline struct Scsi_Host *
