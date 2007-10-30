2960c70b6bbe07e266e2a239b7d10daba906755c
Hans-Christian Egtvedt <hcegtvedt@atmel.com>
2007-10-30 16:33:07 +0100

    [MTD] [NOR] More CFI fixups for Atmel chips
    Convert CFI tables from Atmel cmdset_0001 chips to Intel format and set
    BufWrite timeouts to 0 for Atmel cmdset_0001 and cmdset_0002 chips.
    Some chips may indicate support for buffered writes even though they
    only support dual-word writes.
    
    The CFI fixup must run before fixup_use_write_buffers for this to work.
    
    Signed-off-by: Håvard Skinnemoen <hskinnemoen@atmel.com>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- drivers/mtd/chips/cfi_cmdset_0002.c
+++ drivers/mtd/chips/cfi_cmdset_0002.c
@@ -186,6 +186,10 @@ static void fixup_convert_atmel_pri(struct mtd_info *mtd, void *param)
 		extp->TopBottom = 2;
 	else
 		extp->TopBottom = 3;
+
+	/* burst write mode not supported */
+	cfi->cfiq->BufWriteTimeoutTyp = 0;
+	cfi->cfiq->BufWriteTimeoutMax = 0;
 }
 
 static void fixup_use_secsi(struct mtd_info *mtd, void *param)
@@ -218,6 +222,7 @@ static void fixup_use_atmel_lock(struct mtd_info *mtd, void *param)
 }
 
 static struct cfi_fixup cfi_fixup_table[] = {
+	{ CFI_MFR_ATMEL, CFI_ID_ANY, fixup_convert_atmel_pri, NULL },
 #ifdef AMD_BOOTLOC_BUG
 	{ CFI_MFR_AMD, CFI_ID_ANY, fixup_amd_bootblock, NULL },
 #endif
@@ -230,7 +235,6 @@ static struct cfi_fixup cfi_fixup_table[] = {
 #if !FORCE_WORD_WRITE
 	{ CFI_MFR_ANY, CFI_ID_ANY, fixup_use_write_buffers, NULL, },
 #endif
-	{ CFI_MFR_ATMEL, CFI_ID_ANY, fixup_convert_atmel_pri, NULL },
 	{ 0, 0, NULL, NULL }
 };
 static struct cfi_fixup jedec_fixup_table[] = {
