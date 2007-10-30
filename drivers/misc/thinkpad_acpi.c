2b21c2b4a906a8601355e30cf979c4f55195badb
Henrique de Moraes Holschuh <hmh@hmh.eng.br>
2007-10-30 17:46:23 -0200

    ACPI: thinkpad-acpi: bump up version to 0.17
    The lm-sensors 3.0.0/libsensors4 compatibility changes are reason enough to
    bump up the version string.  Do it.
    
    Signed-off-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/misc/thinkpad_acpi.c
+++ drivers/misc/thinkpad_acpi.c
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  02110-1301, USA.
  */
 
-#define IBM_VERSION "0.16"
+#define IBM_VERSION "0.17"
 #define TPACPI_SYSFS_VERSION 0x020000
 
 /*
