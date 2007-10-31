2a10837250d1adfd10ae0ec5b9262bf9928be996
Richard Purdie <rpurdie@rpsys.net>
2007-10-31 15:00:07 +0100

    leds: Standardise LED naming scheme
    As discussed on LKML some notion of 'function' is needed in
    LED naming. This patch adds this to the documentation and
    standardises existing LED drivers.
    
    Signed-off-by: Richard Purdie <rpurdie@rpsys.net>

--- drivers/leds/leds-clevo-mail.c
+++ drivers/leds/leds-clevo-mail.c
@@ -94,7 +94,7 @@ static void clevo_mail_led_set(struct led_classdev *led_cdev,
 }
 
 static struct led_classdev clevo_mail_led = {
-	.name			= "clevo",
+	.name			= "clevo::mail",
 	.brightness_set		= clevo_mail_led_set,
 };
 
