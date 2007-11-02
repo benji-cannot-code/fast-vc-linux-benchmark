082e1d0def81ca3c09fd0aefdafdb0f330dac7e7
Greg Kroah-Hartman <gregkh@suse.de>
2007-11-02 16:19:59 -0700

    firmware: remove firmware_(un)register()
    These functions are no longer called or needed, so we can remove them.
    
    As I rewrote the whole firmware.c file, add my copyright.
    
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/firmware.c
+++ drivers/base/firmware.c
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * Copyright (c) 2002-3 Patrick Mochel
  * Copyright (c) 2002-3 Open Source Development Labs
+ * Copyright (c) 2007 Greg Kroah-Hartman <gregkh@suse.de>
+ * Copyright (c) 2007 Novell Inc.
  *
  * This file is released under the GPLv2
- *
  */
-
 #include <linux/kobject.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -19,18 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct kset *firmware_kset;
 EXPORT_SYMBOL_GPL(firmware_kset);
 
-int firmware_register(struct kset *s)
-{
-	s->kobj.kset = firmware_kset;
-	s->kobj.ktype = NULL;
-	return subsystem_register(s);
-}
-
-void firmware_unregister(struct kset *s)
-{
-	subsystem_unregister(s);
-}
-
 int __init firmware_init(void)
 {
 	firmware_kset = kset_create_and_add("firmware", NULL, NULL);
@@ -38,6 +26,3 @@ int __init firmware_init(void)
 		return -ENOMEM;
 	return 0;
 }
-
-EXPORT_SYMBOL_GPL(firmware_register);
-EXPORT_SYMBOL_GPL(firmware_unregister);
