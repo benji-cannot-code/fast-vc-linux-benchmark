7917d22b68515cb604f538daf18b67b504eae83b
Ralf Baechle <ralf@linux-mips.org>
2007-11-02 17:26:06 +0000

    Use i8253.c lock for PC speaker on MIPS, too.
    The Jazz machines have to use the PIT timer for dyntick and highresolution
    kernels.  This may break because currently just like i386 used to do MIPS
    uses two separate spinlocks in the actual PIT code and the PC speaker
    code.  So switch to do it the same that x86 currently does PIT locking.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/mips/kernel/i8253.c
+++ arch/mips/kernel/i8253.c
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/time.h>
 
-static DEFINE_SPINLOCK(i8253_lock);
+DEFINE_SPINLOCK(i8253_lock);
 
 /*
  * Initialize the PIT timer.
--- include/asm-mips/i8253.h
+++ include/asm-mips/i8253.h
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_I8253_H
 #define __ASM_I8253_H
 
+#include <linux/spinlock.h>
+
 /* i8253A PIT registers */
 #define PIT_MODE		0x43
 #define PIT_CH0			0x40
 #define PIT_CH2			0x42
+
+extern spinlock_t i8253_lock;
 
 extern void setup_pit_timer(void);
 
