6e62d8b5c5dcad56d99d4286368eed11c234a939
Steve French <sfrench@us.ibm.com>
2007-11-05 21:46:10 +0000

    [CIFS] Fix walking out end of cifs dacl
    Acked-by: Shirish Pargaonkar <shirishp@us.ibm.com>
    Signed-off-by: Steve French <sfrench@us.ibm.com>

--- fs/cifs/cifsacl.h
+++ fs/cifs/cifsacl.h
@@ -36,6 +36,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define UBITSHIFT	6
 #define GBITSHIFT	3
 
+#define ACCESS_ALLOWED	0
+#define ACCESS_DENIED	1
+
 struct cifs_ntsd {
 	__le16 revision; /* revision level */
 	__le16 type;
