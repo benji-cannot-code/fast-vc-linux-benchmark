2fc9bf3e7bcbe505a9d7763a3bc1688279645d3b
Ian Armstrong <ian@iarmst.demon.co.uk>
2007-11-05 14:27:09 -0300

    V4L/DVB (6717): ivtv: Initial merge of video48 yuv handling into the IVTV_IOC_DMA_FRAME framework
    Previously, all yuv data written to /dev/video48 had only basic support with
    no double buffering to avoid display tearing.
    
    With this patch, yuv frames written to video48 are now handled by the existing
    IVTV_IOC_DMA_FRAME framework. As such, the frames are hardware buffered to
    avoid tearing, and honour scaling mode & field order options. Unlike the
    proprietary IVTV_IOC_DMA_FRAME ioctl, all parameters are controlled by the
    V4L2 API.
    
    Due to mpeg & yuv output restrictions being different, their V4L2 output
    controls have been separated. To control the yuv output, the V4L2 calls must
    be done via video48.
    
    If the ivtvfb module is loaded, there will be one side effect to this merge.
    The yuv output window will be constrained to the visible framebuffer area. In
    the event that a virtual framebuffer size is being used, the limit to the
    output size will be the virtual dimensions, but only the portion that falls
    within the currently visible area of the framebuffer will be shown.
    
    Like the IVTV_IOC_DMA_FRAME ioctl, the supplied frames must be padded to 720
    pixels wide. However the height must only be padded up the nearest multiple
    of 32. This would mean an image of 102 lines must be padded to 128. As long
    as the true source image size is given, the padding will not be visible in
    the final output.
    
    Signed-off-by: Ian Armstrong <ian@iarmst.demon.co.uk>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/ivtv/ivtv-yuv.h
+++ drivers/media/video/ivtv/ivtv-yuv.h
@@ -35,8 +35,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern const u32 yuv_offset[IVTV_YUV_BUFFERS];
 
 int ivtv_yuv_filter_check(struct ivtv *itv);
+void ivtv_yuv_setup_stream_frame(struct ivtv *itv);
+int ivtv_yuv_udma_stream_frame(struct ivtv *itv, void *src);
+void ivtv_yuv_frame_complete(struct ivtv *itv);
 int ivtv_yuv_prep_frame(struct ivtv *itv, struct ivtv_dma_frame *args);
 void ivtv_yuv_close(struct ivtv *itv);
-void ivtv_yuv_work_handler (struct ivtv *itv);
+void ivtv_yuv_work_handler(struct ivtv *itv);
 
 #endif
