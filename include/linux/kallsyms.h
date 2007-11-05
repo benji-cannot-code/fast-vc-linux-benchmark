39db36360971886694414195817b3774e1638f1d
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2007-11-05 14:50:55 -0800

    Missing include file in kallsyms.h
    The Build with randconfig fails with following error with the
    2.6.24-rc4-git9
    
    include/linux/kallsyms.h:56: error: `NULL' undeclared (first use in this
    function)
    include/linux/kallsyms.h:56: error: (Each undeclared identifier is
    reported only once
    include/linux/kallsyms.h:56: error: for each function it appears in.)
    make[2]: *** [arch/powerpc/platforms/cell/spu_callbacks.o] Error 1
    make[1]: *** [arch/powerpc/platforms/cell] Error 2
    make: *** [arch/powerpc/platforms] Error 2
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/kallsyms.h
+++ include/linux/kallsyms.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_KALLSYMS_H
 
 #include <linux/errno.h>
+#include <linux/stddef.h>
 
 #define KSYM_NAME_LEN 128
 #define KSYM_SYMBOL_LEN (sizeof("%s+%#lx/%#lx [%s]") + (KSYM_NAME_LEN - 1) + \
