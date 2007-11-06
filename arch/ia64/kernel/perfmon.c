7453c9e19649ca49b5b662a9f744ee024d3f2a6d
Tony Luck <tony.luck@intel.com>
2007-11-06 13:20:43 -0800

    [IA64] Fix perfmon sysctl directory modes
    New sanity checks in sysctl_check_table() complain about a couple
    of mode 0755 that should be 0555 in the perfmon code:
    
    sysctl table check failed: /kernel .1 Writable sysctl directory
    sysctl table check failed: /kernel/perfmon  Writable sysctl directory
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/ia64/kernel/perfmon.c
+++ arch/ia64/kernel/perfmon.c
@@ -559,7 +559,7 @@ static ctl_table pfm_sysctl_dir[] = {
 	{
 		.ctl_name	= CTL_UNNUMBERED,
 		.procname	= "perfmon",
-		.mode		= 0755,
+		.mode		= 0555,
 		.child		= pfm_ctl_table,
 	},
  	{}
@@ -568,7 +568,7 @@ static ctl_table pfm_sysctl_root[] = {
 	{
 		.ctl_name	= CTL_KERN,
 		.procname	= "kernel",
-		.mode		= 0755,
+		.mode		= 0555,
 		.child		= pfm_sysctl_dir,
 	},
  	{}
