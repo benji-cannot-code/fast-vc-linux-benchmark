b7544080e97e54902383e954dcd5b2f3926d3d23
Eric Dumazet <dada1@cosmosbay.com>
2007-11-05 23:39:51 -0800

    [IPV6]: Use the {DEFINE|REF}_PROTO_INUSE infrastructure
    Trivial patch to make "tcpv6,udpv6,udplitev6,rawv6" protocols uses the
    fast "inuse sockets" infrastructure
    
    Each protocol use then a static percpu var, instead of a dynamic one.
    This saves some ram and some cpu cycles
    
    Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/raw.c
+++ net/ipv6/raw.c
@@ -1145,6 +1145,8 @@ static int rawv6_init_sk(struct sock *sk)
 	return(0);
 }
 
+DEFINE_PROTO_INUSE(rawv6)
+
 struct proto rawv6_prot = {
 	.name		   = "RAWv6",
 	.owner		   = THIS_MODULE,
@@ -1167,6 +1169,7 @@ struct proto rawv6_prot = {
 	.compat_setsockopt = compat_rawv6_setsockopt,
 	.compat_getsockopt = compat_rawv6_getsockopt,
 #endif
+	REF_PROTO_INUSE(rawv6)
 };
 
 #ifdef CONFIG_PROC_FS
