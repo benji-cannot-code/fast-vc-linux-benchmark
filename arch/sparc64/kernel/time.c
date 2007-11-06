c4715016e406247d566637736cef26407e51ddac
Lucas Woods <woodzy@gmail.com>
2007-11-05 20:22:27 -0800

    [SPARC]: Remove duplicate includes.
    Signed-off-by: Lucas Woods <woodzy@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc64/kernel/time.c
+++ arch/sparc64/kernel/time.c
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/jiffies.h>
 #include <linux/cpufreq.h>
 #include <linux/percpu.h>
-#include <linux/profile.h>
 #include <linux/miscdevice.h>
 #include <linux/rtc.h>
 #include <linux/kernel_stat.h>
@@ -48,7 +47,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sections.h>
 #include <asm/cpudata.h>
 #include <asm/uaccess.h>
-#include <asm/prom.h>
 #include <asm/irq_regs.h>
 
 DEFINE_SPINLOCK(mostek_lock);
