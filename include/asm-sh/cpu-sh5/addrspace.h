975802cfa9eaef3457512b6bacfb20631999c83c
Paul Mundt <lethal@linux-sh.org>
2007-11-08 18:58:52 +0900

    sh: Add addrspace.h segmentation stub for SH-5.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

---
+++ include/asm-sh/cpu-sh5/addrspace.h
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __ASM_SH_CPU_SH5_ADDRSPACE_H
+#define __ASM_SH_CPU_SH5_ADDRSPACE_H
+
+/* No segmentation.. */
+
+#endif /* __ASM_SH_CPU_SH5_ADDRSPACE_H */
