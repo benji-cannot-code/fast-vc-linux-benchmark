9318ca3259328cce9bdf957d1b9e5a6ae2673827
Bryan Wu <bryan.wu@analog.com>
2007-11-12 23:24:42 +0800

    Blackfin arch: move hard coded pin_req to board file
    Remove some sort of bloaty code, try to get these pin_req arrays built at compile-time
    
     - move this static things to the blackfin board file
     - add pin_req array to struct bfin5xx_spi_master
     - tested on BF537/BF548 with SPI flash
    
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- arch/blackfin/mach-bf527/boards/ezkit.c
+++ arch/blackfin/mach-bf527/boards/ezkit.c
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/bfin5xx_spi.h>
 #include <asm/reboot.h>
 #include <asm/nand.h>
+#include <asm/portmux.h>
 #include <linux/spi/ad7877.h>
 
 /*
@@ -563,6 +564,7 @@ static struct spi_board_info bfin_spi_board_info[] __initdata = {
 static struct bfin5xx_spi_master bfin_spi0_info = {
 	.num_chipselect = 8,
 	.enable_dma = 1,  /* master has the ability to do dma transfer */
+	.pin_req = {P_SPI0_SCK, P_SPI0_MISO, P_SPI0_MOSI, 0},
 };
 
 /* SPI (0) */
