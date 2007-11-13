8498c1ab73d8ea55ad804b2b6789cc7ba4b2e19d
Joe Perches <joe@perches.com>
2007-11-12 20:06:51 -0800

    KVM: Remove ptr comparisons to 0
    Fix sparse warnings "Using plain integer as NULL pointer"
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/kvm_main.c
+++ drivers/kvm/kvm_main.c
@@ -1450,7 +1450,8 @@ int kvm_init(void *opaque, unsigned int vcpu_size,
 
 	/* A kmem cache lets us meet the alignment requirements of fx_save. */
 	kvm_vcpu_cache = kmem_cache_create("kvm_vcpu", vcpu_size,
-					   __alignof__(struct kvm_vcpu), 0, 0);
+					   __alignof__(struct kvm_vcpu),
+					   0, NULL);
 	if (!kvm_vcpu_cache) {
 		r = -ENOMEM;
 		goto out_free_4;
