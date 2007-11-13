7e4e11ad287855f71a6cf210f9253a9d420661d2
Jon Loeliger <jdl@freescale.com>
2007-11-14 04:13:09 +1100

    [POWERPC] Clean out asm/of_{platform, device}.h from sysdev/
    Convert #include of asm/of_{platform, device}.h into
    linux/of_{platform,device}.h for remaining arch/powerpc files.
    
    Signed-off-by: Jon Loeliger <jdl@freescale.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/sysdev/axonram.c
+++ arch/powerpc/sysdev/axonram.c
@@ -43,8 +43,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/types.h>
-#include <asm/of_device.h>
-#include <asm/of_platform.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
+
 #include <asm/page.h>
 #include <asm/prom.h>
 
