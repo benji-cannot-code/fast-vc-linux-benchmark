7bb8c8be5167df783cee1e9f630e88b8321c694b
Len Brown <len.brown@intel.com>
2007-11-14 12:49:13 -0500

    ACPI: video - delete stray run-time printk
    printk("video bus notify\n");
    
    Acked-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/acpi/video.c
+++ drivers/acpi/video.c
@@ -1781,9 +1781,6 @@ static void acpi_video_bus_notify(acpi_handle handle, u32 event, void *data)
 	struct input_dev *input;
 	int keycode;
 
-
-	printk("video bus notify\n");
-
 	if (!video)
 		return;
 
