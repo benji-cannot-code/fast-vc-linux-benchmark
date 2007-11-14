2146fba940bf68e1da17a3a7c6274fe8e6721805
Herbert Xu <herbert@gondor.apana.org.au>
2007-11-13 21:34:34 -0800

    [NET]: Remove unnecessary inclusion of dst.h
    The file net/netevent.h only refers to struct dst_entry * so it
    doesn't need to include dst.h.  I've replaced it with a forward
    declaration.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netevent.h
+++ include/net/netevent.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #ifdef __KERNEL__
 
-#include <net/dst.h>
+struct dst_entry;
 
 struct netevent_redirect {
 	struct dst_entry *old;
