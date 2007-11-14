220d8a9ef594f7fe3567b32e73db809458c09004
Herbert Xu <herbert@gondor.apana.org.au>
2007-11-13 21:39:38 -0800

    [IPSEC]: Move x->outer_mode->output out of locked section
    RO mode is the only one that requires a locked output function.  So
    it's easier to move the lock into that function rather than requiring
    everyone else to run under the lock.
    
    In particular, this allows us to move the size check into the output
    function without causing a potential dead-lock should the ICMP error
    somehow hit the same SA on transmission.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/xfrm6_mode_ro.c
+++ net/ipv6/xfrm6_mode_ro.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/skbuff.h>
+#include <linux/spinlock.h>
 #include <linux/stringify.h>
 #include <linux/time.h>
 #include <net/ipv6.h>
@@ -54,7 +55,9 @@ static int xfrm6_ro_output(struct xfrm_state *x, struct sk_buff *skb)
 	__skb_pull(skb, hdr_len);
 	memmove(ipv6_hdr(skb), iph, hdr_len);
 
+	spin_lock_bh(&x->lock);
 	x->lastused = get_seconds();
+	spin_unlock_bh(&x->lock);
 
 	return 0;
 }
