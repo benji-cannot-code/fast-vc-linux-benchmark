711c7f7ef5777505ae81f6edd5d1eed99eab2114
Jesper Nilsson <jesper.nilsson@axis.com>
2007-11-14 17:00:59 -0800

    cris build fixes: corrected and improved NMI and IRQ handling
    Corrects compile errors and the following:
    
    - Remove oldset parameter from do_signal and do_notify_resume.
    
    - Modified to fit new consolidated IRQ handling code.
    
    - Reverse check order between external nmi and watchdog nmi to avoid false
      watchdog oops in case of a glitch on the nmi pin.
    
    - Return from an pin-generated NMI the same way as for other interrupts.
    
    - Moved blocking of ethernet rx/tx irq from ethernet interrupt handler to
      low-level asm interrupt handlers.  Fixed in the multiple interrupt
      handler also.
    
    - Add space for thread local storage in thread_info struct.
    
    - Add NO_DMA to Kconfig, and include arch specific Kconfig using arch
      independent path. Include subsystem Kconfigs for pcmcia, usb, i2c,
      rtc and pci.
    
    Signed-off-by: Jesper Nilsson <jesper.nilsson@axis.com>
    Acked-by: Mikael Starvik <starvik@axis.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/cris/kernel/ptrace.c
+++ arch/cris/kernel/ptrace.c
@@ -82,13 +82,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* notification of userspace execution resumption
  * - triggered by current->work.notify_resume
  */
-extern int do_signal(int canrestart, sigset_t *oldset, struct pt_regs *regs);
+extern int do_signal(int canrestart, struct pt_regs *regs);
 
 
-void do_notify_resume(int canrestart, sigset_t *oldset, struct pt_regs *regs, 
+void do_notify_resume(int canrestart, struct pt_regs *regs,
 		      __u32 thread_info_flags  )
 {
 	/* deal with pending signal delivery */
 	if (thread_info_flags & _TIF_SIGPENDING)
-		do_signal(canrestart,oldset,regs);
+		do_signal(canrestart,regs);
 }
