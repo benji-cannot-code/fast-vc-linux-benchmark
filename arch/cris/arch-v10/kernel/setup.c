37ff9271e579859f9d498fb41f0fc6b943dc00fd
Jesper Nilsson <jesper.nilsson@axis.com>
2007-11-14 17:01:01 -0800

    cris build fixes: setup.c needs param.h
    Include linux/param.h for HZ.
    
    Signed-off-by: Jesper Nilsson <jesper.nilsson@axis.com>
    Acked-by: Mikael Starvik <starvik@axis.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/cris/arch-v10/kernel/setup.c
+++ arch/cris/arch-v10/kernel/setup.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/proc_fs.h>
 #include <linux/delay.h>
+#include <linux/param.h>
 
 #ifdef CONFIG_PROC_FS
 #define HAS_FPU		0x0001
