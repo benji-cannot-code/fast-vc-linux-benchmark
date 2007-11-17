599110837c38c7f29295216f61ef2e012b6d08c2
Sam Ravnborg <sam@ravnborg.org>
2007-11-17 15:37:31 +0100

    x86: simplify "make ARCH=x86" and fix kconfig all.config
    Simplify "make ARCH=x86" and fix kconfig so we again
    can set 64BIT in all.config.
    
    For a fix the diffstat is nice:
     6 files changed, 3 insertions(+), 36 deletions(-)
    
    The patch reverts these commits:
    0f855aa64b3f63d35a891510cf7db932a435c116
    -> kconfig: add helper to set config symbol from environment variable
    
    2a113281f5cd2febbab21a93c8943f8d3eece4d3
    -> kconfig: use $K64BIT to set 64BIT with all*config targets
    
    Roman Zippel pointed out that kconfig supported string
    compares so the additional complexity introduced by the
    above two patches were not needed.
    
    With this patch we have following behaviour:
    
    # make {allno,allyes,allmod,rand}config [ARCH=...]
    option \ host arch      | 32bit         | 64bit
    =====================================================
    ./.                     | 32bit         | 64bit
    ARCH=x86                | 32bit         | 32bit
    ARCH=i386               | 32bit         | 32bit
    ARCH=x86_64             | 64bit         | 64bit
    
    The general rule are that ARCH= and native architecture
    takes precedence over the configuration.
    So make ARCH=i386 [whatever] will always build a 32-bit
    kernel no matter what the configuration says.
    The configuration will be updated to 32-bit if it was
    configured to 64-bit and the other way around.
    
    This behaviour is consistent with previous behaviour so
    no suprises here.
    
    make ARCH=x86 will per default result in a 32-bit kernel
    but as the only ARCH= value x86 allow the user to select
    between 32-bit and 64-bit using menuconfig. 
    
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Cc: Roman Zippel <zippel@linux-m68k.org>
    Cc: Andreas Herrmann <aherrman@arcor.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>

--- scripts/kconfig/lkc_proto.h
+++ scripts/kconfig/lkc_proto.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /* confdata.c */
 P(conf_parse,void,(const char *name));
-P(conf_set_env_sym,int,(const char *envname, const char *symname, int def));
 P(conf_read,int,(const char *name));
 P(conf_read_simple,int,(const char *name, int));
 P(conf_write,int,(const char *name));
