ed50bf9abd8cf16d3ea48b1b0653346aa9ebfe88
Adrian Bunk <bunk@kernel.org>
2007-11-18 00:41:56 +0800

    Blackfin arch: remove dump_thread()
    The only user is the a.out support.
    
    It was therefore removed prior to the blackfin merge from all
    architectures not supporting a.out.
    
    Currently, Blackfin doesn't suppport a.out.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Bryan Wu <bryan.wu@analog.com>

--- arch/blackfin/kernel/bfin_ksyms.c
+++ arch/blackfin/kernel/bfin_ksyms.c
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 EXPORT_SYMBOL(__ioremap);
 EXPORT_SYMBOL(strcmp);
 EXPORT_SYMBOL(strncmp);
-EXPORT_SYMBOL(dump_thread);
 
 EXPORT_SYMBOL(ip_fast_csum);
 
