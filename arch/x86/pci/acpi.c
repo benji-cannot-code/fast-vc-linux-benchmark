f8b4b6ef6954aef85d0f233c170e1683e79754f1
Yinghai Lu <Yinghai.Lu@Sun.COM>
2007-11-17 16:27:01 +0100

    x86: check boundary in count setup resource
    need to check info->res_num less than PCI_BUS_NUM_RESOURCES, so
    info->bus->resource[info->res_num] = res will not beyond of bus resource
    array when acpi returns too many resource entries.
    
    Signed-off-by: Yinghai Lu <yinghai.lu@sun.com>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Cc: Gary Hade <gary.hade@us.ibm.com>
    Cc: Len Brown <lenb@kernel.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/pci/acpi.c
+++ arch/x86/pci/acpi.c
@@ -78,6 +78,9 @@ count_resource(struct acpi_resource *acpi_res, void *data)
 	struct acpi_resource_address64 addr;
 	acpi_status status;
 
+	if (info->res_num >= PCI_BUS_NUM_RESOURCES)
+		return AE_OK;
+
 	status = resource_to_addr(acpi_res, &addr);
 	if (ACPI_SUCCESS(status))
 		info->res_num++;
@@ -93,6 +96,9 @@ setup_resource(struct acpi_resource *acpi_res, void *data)
 	acpi_status status;
 	unsigned long flags;
 	struct resource *root;
+
+	if (info->res_num >= PCI_BUS_NUM_RESOURCES)
+		return AE_OK;
 
 	status = resource_to_addr(acpi_res, &addr);
 	if (!ACPI_SUCCESS(status))
