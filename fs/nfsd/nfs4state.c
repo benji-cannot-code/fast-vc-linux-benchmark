6265dad613048f29977f66982f5e642840669146
J. Bruce Fields <bfields@citi.umich.edu>
2007-11-19 19:09:50 -0500

    nfsd: eliminate final bogus case from setclientid logic
    We're supposed to generate a different cl_confirm verifier for each new
    client, so these to cl_confirm values should never be the same.
    
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -770,7 +770,7 @@ nfsd4_setclientid(struct svc_rqst *rqstp, struct nfsd4_compound_state *cstate,
 		if (new == NULL)
 			goto out;
 		gen_clid(new);
-	} else if (!same_verf(&conf->cl_confirm, &unconf->cl_confirm)) {
+	} else {
 		/*
 		 * RFC 3530 14.2.33 CASE 3:
 		 * probable client reboot; state will be removed if
@@ -781,11 +781,6 @@ nfsd4_setclientid(struct svc_rqst *rqstp, struct nfsd4_compound_state *cstate,
 		if (new == NULL)
 			goto out;
 		gen_clid(new);
-	} else {
-		/* No cases hit !!! */
-		status = nfserr_inval;
-		goto out;
-
 	}
 	copy_verf(new, &clverifier);
 	new->cl_addr = sin->sin_addr.s_addr;
