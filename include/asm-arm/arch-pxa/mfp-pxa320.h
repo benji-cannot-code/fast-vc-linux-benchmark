884950f81a2538d7c61951369588c9a5abb39790
eric miao <eric.y.miao@gmail.com>
2007-11-20 01:35:51 +0100

    [ARM] 4653/1: pxa: fix a gpio typo in mfp-pxa320.h
    Signed-off-by: bridge wu <bridge.wu@marvell.com>
    Signed-off-by: eric miao <eric.miao@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-pxa/mfp-pxa320.h
+++ include/asm-arm/arch-pxa/mfp-pxa320.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/mfp.h>
 
 /* GPIO */
-#define GPIO46_GPIO		MFP_CFG(GPIO6, AF0)
+#define GPIO46_GPIO		MFP_CFG(GPIO46, AF0)
 #define GPIO49_GPIO		MFP_CFG(GPIO49, AF0)
 #define GPIO50_GPIO		MFP_CFG(GPIO50, AF0)
 #define GPIO51_GPIO		MFP_CFG(GPIO51, AF0)
