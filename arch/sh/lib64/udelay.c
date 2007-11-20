e3febb2b71e2227032b8f5351eed808f58e9bf20
Paul Mundt <lethal@linux-sh.org>
2007-11-20 14:49:36 +0900

    sh: Tidy up lib64 udelay impl.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/lib64/udelay.c
+++ arch/sh/lib64/udelay.c
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <asm/param.h>
 
-extern unsigned long loops_per_jiffy;
-
 /*
  * Use only for very small delays (< 1 msec).
  *
@@ -50,11 +48,10 @@ void __ndelay(unsigned long long nsecs, unsigned long lpj)
 
 void udelay(unsigned long usecs)
 {
-	__udelay(usecs, loops_per_jiffy);
+	__udelay(usecs, cpu_data[raw_smp_processor_id()].loops_per_jiffy);
 }
 
 void ndelay(unsigned long nsecs)
 {
-	__ndelay(nsecs, loops_per_jiffy);
+	__ndelay(nsecs, cpu_data[raw_smp_processor_id()].loops_per_jiffy);
 }
-
