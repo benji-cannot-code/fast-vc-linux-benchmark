f4302a578b4b11cffe280be88035aa3946800f28
Aidan Thornton <makosoft@googlemail.com>
2007-11-20 15:25:08 -0300

    V4L/DVB (6619): Use MTS firmware for the HVR-900
    The HVR-900 requires the MTS version of the xc3028 firmware in order
    to get any sound. The below patch selects this firmware variant on
    HVR-900 cards, as well as splitting the HVR-950 into its own entry
    (since I don't know if it uses the MTS variant and it will have to be
    split off eventually anyway).
    
    Signed-off-by: Aidan Thornton <makosoft@googlemail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define EM2880_BOARD_TERRATEC_PRODIGY_XS	13
 #define EM2820_BOARD_PROLINK_PLAYTV_USB2	14
 #define EM2800_BOARD_VGEAR_POCKETTV             15
+#define EM2880_BOARD_HAUPPAUGE_WINTV_HVR_950	16
 
 #define UNSET -1
 
@@ -178,6 +179,7 @@ struct em28xx_board {
 	unsigned int has_msp34xx:1;
 
 	enum em28xx_decoder decoder;
+	int xc2028_type;
 
 	struct em28xx_input       input[MAX_EM28XX_INPUT];
 };
