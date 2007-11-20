e2b05d09896ce31bc38fcc76c86d04d2800c5448
Chris Pascoe <c.pascoe@itee.uq.edu.au>
2007-11-19 21:57:10 -0300

    V4L/DVB (6649): Add support for the DViCO FusionHDTV Dual Digital 4
    Add support for DViCO's Dual Digital 4 with xc3028 tuner, zl10353 DVB-T
    demodulator and a new-style I2C IR remote control receiver.
    
    This would not have been possible without the work of and advice from
    Mike Krufky, who originally got the Dual Digital 4 and second-gen DVB-T
    NANO devices working with the out-of-tree XC3028 driver.
    
    I converted it to use the in-tree XC3028 driver (after making it suitable
    for our use), and added the IR remote control support based on his advice.
    
    NB: a firmware package is required to use this device.
    
    Signed-off-by: Chris Pascoe <c.pascoe@itee.uq.edu.au>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/cxusb.h
+++ drivers/media/dvb/dvb-usb/cxusb.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb-usb.h"
 
 /* usb commands - some of it are guesses, don't have a reference yet */
+#define CMD_BLUEBIRD_GPIO_RW 0x05
+
 #define CMD_I2C_WRITE     0x08
 #define CMD_I2C_READ      0x09
 
