cd5ad667940269b1890012e0cbcae11584e23d2f
Paul Mundt <lethal@linux-sh.org>
2007-11-21 15:34:02 +0900

    sh: Move PXSEG comments to addrspace.h.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/addrspace.h
+++ include/asm-sh/addrspace.h
@@ -18,6 +18,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* If this CPU supports segmentation, hook up the helpers */
 #ifdef P1SEG
 
+/*
+   [ P0/U0 (virtual) ]		0x00000000     <------ User space
+   [ P1 (fixed)   cached ]	0x80000000     <------ Kernel space
+   [ P2 (fixed)  non-cachable]	0xA0000000     <------ Physical access
+   [ P3 (virtual) cached]	0xC0000000     <------ vmalloced area
+   [ P4 control   ]		0xE0000000
+ */
+
 /* Returns the privileged segment base of a given address  */
 #define PXSEG(a)	(((unsigned long)(a)) & 0xe0000000)
 
