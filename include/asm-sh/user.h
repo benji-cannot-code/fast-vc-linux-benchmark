98202fdaabf1a0961782da591af88667f1a8c283
Paul Mundt <lethal@linux-sh.org>
2007-11-21 17:55:12 +0900

    sh: Fix up user_fpu_struct typo for SH-5.
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/user.h
+++ include/asm-sh/user.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #if defined(__SH5__) || defined(CONFIG_CPU_SH5)
-struct user fpu_struct {
+struct user_fpu_struct {
 	unsigned long fp_regs[32];
 	unsigned int fpscr;
 };
