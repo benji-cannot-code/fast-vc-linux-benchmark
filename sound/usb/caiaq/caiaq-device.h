a296b004ff2d0c50a735d643bbfd8087f0ea7838
Dmitry Torokhov <dtor@mail.ru>
2007-11-21 16:45:23 +0100

    [ALSA] caiaq - misc input handling fixes
     - link input device with its parent so that it placed in proper spot
       in sysfs hierarchy
     - drivers that allow changing their keymaps should use private copy
       of the keymap so that one instance of a device does not affect
       another instance
     - it is preferred for drivers to properly set up input_dev->phys to
       help userspace locate devices
     - drivers should use usb_to_input_id(), or perform endianess conversion,
       themselves, otherwise ID is not correct on big-endian boxes
     - whitespace and formatting cleanup
    Acked-by: Daniel Mack <daniel@caiaq.de>
    
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- sound/usb/caiaq/caiaq-device.h
+++ sound/usb/caiaq/caiaq-device.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define USB_PID_RIGKONTROL2	0x1969
 #define USB_PID_RIGKONTROL3	0x1940
-#define USB_PID_KORECONTROLLER 	0x4711
+#define USB_PID_KORECONTROLLER	0x4711
 #define USB_PID_AK1		0x0815
 #define USB_PID_AUDIO8DJ	0x1978
 
@@ -63,7 +63,7 @@ struct snd_usb_caiaqdev {
 	struct urb **data_urbs_in;
 	struct urb **data_urbs_out;
 	struct snd_usb_caiaq_cb_info *data_cb_info;
-	
+
 	unsigned char ep1_in_buf[EP1_BUFSIZE];
 	unsigned char ep1_out_buf[EP1_BUFSIZE];
 	unsigned char midi_out_buf[EP1_BUFSIZE];
@@ -73,7 +73,7 @@ struct snd_usb_caiaqdev {
 	wait_queue_head_t ep1_wait_queue;
 	wait_queue_head_t prepare_wait_queue;
 	int spec_received, audio_parm_answer;
-	
+
 	char vendor_name[CAIAQ_USB_STR_LEN];
 	char product_name[CAIAQ_USB_STR_LEN];
 	char serial[CAIAQ_USB_STR_LEN];
@@ -94,8 +94,10 @@ struct snd_usb_caiaqdev {
 	/* Linux input */
 #ifdef CONFIG_SND_USB_CAIAQ_INPUT
 	struct input_dev *input_dev;
+	char phys[64];			/* physical device path */
+	unsigned short keycode[10];
 #endif
-	
+
 	/* ALSA */
 	struct snd_pcm *pcm;
 	struct snd_pcm_hardware pcm_info;
