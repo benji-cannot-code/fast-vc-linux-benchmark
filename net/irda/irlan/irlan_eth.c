9decf85758d3c0d39be83a9af8650a27e84c198f
Pavel Emelyanov <xemul@openvz.org>
2007-11-22 19:15:56 +0800

    [IRDA]: Compilation for CONFIG_INET=n case
    Found this occasionally. 
    
    The CONFIG_INET=n is hardly ever set, but if it is the 
    irlan_eth_send_gratuitous_arp() compilation should produce a 
    warning about unused variable in_dev.
    
    Too pedantic? :)
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- net/irda/irlan/irlan_eth.c
+++ net/irda/irlan/irlan_eth.c
@@ -297,6 +297,7 @@ void irlan_eth_flow_indication(void *instance, void *sap, LOCAL_FLOW flow)
  */
 void irlan_eth_send_gratuitous_arp(struct net_device *dev)
 {
+#ifdef CONFIG_INET
 	struct in_device *in_dev;
 
 	/*
@@ -304,7 +305,6 @@ void irlan_eth_send_gratuitous_arp(struct net_device *dev)
 	 * is useful if we have changed access points on the same
 	 * subnet.
 	 */
-#ifdef CONFIG_INET
 	IRDA_DEBUG(4, "IrLAN: Sending gratuitous ARP\n");
 	rcu_read_lock();
 	in_dev = __in_dev_get_rcu(dev);
