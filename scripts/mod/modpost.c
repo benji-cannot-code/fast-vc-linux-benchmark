e6ea8c17e2b4ca66a1b08ef9441430bad04b1f86
Andi Kleen <ak@suse.de>
2007-11-22 03:43:10 +0100

    kbuild: fix a buffer overflow in modpost
    When passing an file name > 1k the stack could be overflowed.
    Not really a security issue, but still better plugged.
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -1657,7 +1657,6 @@ int main(int argc, char **argv)
 {
 	struct module *mod;
 	struct buffer buf = { };
-	char fname[SZ];
 	char *kernel_read = NULL, *module_read = NULL;
 	char *dump_write = NULL;
 	int opt;
@@ -1710,6 +1709,8 @@ int main(int argc, char **argv)
 	err = 0;
 
 	for (mod = modules; mod; mod = mod->next) {
+		char fname[strlen(mod->name) + 10];
+
 		if (mod->skip)
 			continue;
 
