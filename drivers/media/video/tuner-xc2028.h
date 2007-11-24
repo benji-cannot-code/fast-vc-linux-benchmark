9c3358fec58709ede9f1ce4624ef28fd8db7afa1
Mauro Carvalho Chehab <mchehab@infradead.org>
2007-11-24 11:07:12 -0300

    V4L/DVB (6675): Allow selecting the proper SCode table for DTV
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tuner-xc2028.h
+++ drivers/media/video/tuner-xc2028.h
@@ -12,6 +12,20 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define XC2028_DEFAULT_FIRMWARE "xc3028-v27.fw"
 
+enum xc2028_demod_types
+{
+	XC3028_FE_DEFAULT = 0,
+	XC3028_FE_LG60,		/* IF = 6.00 MHz */
+	XC3028_FE_ATI638,	/* IF = 6.38 MHz */
+	XC3028_FE_OREN538,	/* IF = 5.38 MHz */
+	XC3028_FE_OREN36,	/* IF = 3.60 MHz */
+	XC3028_FE_TOYOTA388,	/* IF = 3.88 MHz */
+	XC3028_FE_TOYOTA794,	/* IF = 7.94 MHz */
+	XC3028_FE_DIBCOM52,	/* IF = 5.20 MHz */
+	XC3028_FE_ZARLINK456,	/* IF = 4.56 MHz */
+	XC3028_FE_CHINA,	/* IF = 5.20 MHz */
+};
+
 struct xc2028_ctrl {
 	char			*fname;
 	int			max_len;
@@ -19,6 +33,7 @@ struct xc2028_ctrl {
 	unsigned int		mts   :1;
 	unsigned int		d2633 :1;
 	unsigned int		input1:1;
+	enum xc2028_demod_types demod;
 };
 
 struct xc2028_config {
