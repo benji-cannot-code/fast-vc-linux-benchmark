f85e9d30522ec9f607b24fcf07a24d2c4a86623e
Mike Isely <isely@pobox.com>
2007-11-26 01:53:12 -0300

    V4L/DVB (6692): pvrusb2: Centralize device specific attributes into a single place
    The pvrusb2 driver currently supports two variants of the Hauppauge
    PVR USB2.  However there are other hardware types potentially
    supportable, but the driver at the moment is not structured to make it
    easy to describe these minor variations.  This changeset is the first
    set of changes to make such additional device support possible.
    Device attributes are held in several tables all contained within
    pvrusb2-devattr.c; all other device-specific driver behavior now
    derives from these tables.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-hdw-internal.h
+++ drivers/media/video/pvrusb2/pvrusb2-hdw-internal.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "pvrusb2-hdw.h"
 #include "pvrusb2-io.h"
 #include <media/cx2341x.h>
+#include "pvrusb2-devattr.h"
 
 /* Legal values for PVR2_CID_HSM */
 #define PVR2_CVAL_HSM_FAIL 0
@@ -163,10 +164,6 @@ struct pvr2_decoder_ctrl {
 #define FW1_STATE_RELOAD 3
 #define FW1_STATE_OK 4
 
-/* Known major hardware variants, keyed from device ID */
-#define PVR2_HDW_TYPE_29XXX 0
-#define PVR2_HDW_TYPE_24XXX 1
-
 typedef int (*pvr2_i2c_func)(struct pvr2_hdw *,u8,u8 *,u16,u8 *, u16);
 #define PVR2_I2C_FUNC_CNT 128
 
@@ -180,6 +177,7 @@ struct pvr2_hdw {
 
 	/* Device type, one of PVR2_HDW_TYPE_xxxxx */
 	unsigned int hdw_type;
+	const struct pvr2_device_desc *hdw_desc;
 
 	/* Kernel worker thread handling */
 	struct workqueue_struct *workqueue;
--- drivers/media/video/pvrusb2/pvrusb2-main.c
+++ drivers/media/video/pvrusb2/pvrusb2-main.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/videodev2.h>
 
 #include "pvrusb2-hdw.h"
+#include "pvrusb2-devattr.h"
 #include "pvrusb2-context.h"
 #include "pvrusb2-debug.h"
 #include "pvrusb2-v4l2.h"
