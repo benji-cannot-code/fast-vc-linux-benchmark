f3e763a28a2c1e7e862bb0413078bb54c1b9ade7
Maciej W. Rozycki <macro@ds2.pg.gda.pl>
2007-11-26 20:42:19 +0100

    x86: fix NMI watchdog & 'stopped time' problem
    More than 3 years ago Niclas Gustafsson reported a 'stopped time'
    problem:
    
    > Watching the /proc/interrupts with 10s apart after the "stop".
    >
    > [root@s151 root]# more /proc/interrupts
    >            CPU0
    >   0:   66413955  local-APIC-edge  timer
    [...]
    > LOC:   67355837
    > ERR:          0
    > MIS:          0
    > [root@s151 root]# more /proc/interrupts
    >            CPU0
    >   0:   66413955  local-APIC-edge  timer
    [...]
    > LOC:   67379568
    > ERR:          0
    > MIS:          0
    
    This may be because buggy SMM firmware messes with the 8259A (configured
    for a transparent mode -- yes that rare "local-APIC-edge" mode is tricky
    ;-) ) insanely.
    
    this should resolve:
    
      http://bugzilla.kernel.org/show_bug.cgi?id=2544
      http://bugzilla.kernel.org/show_bug.cgi?id=6296
    
    Patch-dusted-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/nmi_32.c
+++ arch/x86/kernel/nmi_32.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/smp.h>
 #include <asm/nmi.h>
+#include <asm/timer.h>
 
 #include "mach_traps.h"
 
@@ -84,7 +85,7 @@ static int __init check_nmi_watchdog(void)
 
 	prev_nmi_count = kmalloc(NR_CPUS * sizeof(int), GFP_KERNEL);
 	if (!prev_nmi_count)
-		return -1;
+		goto error;
 
 	printk(KERN_INFO "Testing NMI watchdog ... ");
 
@@ -118,7 +119,7 @@ static int __init check_nmi_watchdog(void)
 	if (!atomic_read(&nmi_active)) {
 		kfree(prev_nmi_count);
 		atomic_set(&nmi_active, -1);
-		return -1;
+		goto error;
 	}
 	printk("OK.\n");
 
@@ -129,6 +130,10 @@ static int __init check_nmi_watchdog(void)
 
 	kfree(prev_nmi_count);
 	return 0;
+error:
+	timer_ack = !cpu_has_tsc;
+
+	return -1;
 }
 /* This needs to happen later in boot so counters are working */
 late_initcall(check_nmi_watchdog);
