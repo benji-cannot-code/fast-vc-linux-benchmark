04dcbe3fded59331f3d9c94a2bad48f1d264540c
Julia Lawall <julia@diku.dk>
2007-11-27 00:45:50 -0500

    Input: drop redundant includes of moduleparam.h
    Drop #include <linux/moduleparam.h> in files that also include
    linux/module.h, since module.h includes moduleparam.h already.
    
    Signed-off-by: Julia Lawall <julia@diku.dk>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/mouse/trackpoint.c
+++ drivers/input/mouse/trackpoint.c
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/delay.h>
 #include <linux/serio.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/input.h>
 #include <linux/libps2.h>
 #include <linux/proc_fs.h>
