0a171ae1323486d4baf317fef6265fc5c867d305
Paul Mundt <lethal@linux-sh.org>
2007-11-28 15:56:27 +0900

    sh: CCR1->CCR renaming for SH-2 parts.
    Avoid namespace collision with a CCR1 definition. The general
    SH code always expects CCR anyways, so there's no point in keeping
    the CCR1 naming around.
    
    Fixes up synclink collisions:
    
    drivers/char/pcmcia/synclink_cs.c:283:1: warning: "CCR1" redefined
    In file included from include/asm/cache.h:13,
                     from include/asm/processor_32.h:15,
                     from include/asm/processor.h:60,
                     from include/linux/prefetch.h:14,
                     from include/linux/list.h:8,
                     from include/linux/module.h:9,
                     from drivers/char/pcmcia/synclink_cs.c:38:
    include/asm/cpu/cache.h:21:1: warning: this is the location of the previous definition
    
    Reported-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/cpu-sh2a/cache.h
+++ include/asm-sh/cpu-sh2a/cache.h
@@ -18,11 +18,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SH_CACHE_COMBINED	4
 #define SH_CACHE_ASSOC		8
 
-#define CCR1		0xfffc1000
+#define CCR		0xfffc1000 /* CCR1 */
 #define CCR2		0xfffc1004
-
-/* CCR1 behaves more like the traditional CCR */
-#define CCR		CCR1
 
 /*
  * Most of the SH-2A CCR1 definitions resemble the SH-4 ones. All others not
@@ -42,4 +39,3 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CCR_CACHE_INVALIDATE	(CCR_CACHE_OCI | CCR_CACHE_ICI)
 
 #endif /* __ASM_CPU_SH2A_CACHE_H */
-
