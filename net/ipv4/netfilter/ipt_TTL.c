c67f7a3ee776c78d0fb633522b40a5fb74b1f4c9
Patrick McHardy <kaber@trash.net>
2007-11-30 01:17:11 +1100

    [NETFILTER]: Convert old checksum helper names
    Kill the defines again, convert to the new checksum helper names and
    remove the dependency of NET_ACT_NAT on NETFILTER.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/netfilter/ipt_TTL.c
+++ net/ipv4/netfilter/ipt_TTL.c
@@ -55,8 +55,8 @@ ipt_ttl_target(struct sk_buff *skb,
 	}
 
 	if (new_ttl != iph->ttl) {
-		nf_csum_replace2(&iph->check, htons(iph->ttl << 8),
-					      htons(new_ttl << 8));
+		csum_replace2(&iph->check, htons(iph->ttl << 8),
+					   htons(new_ttl << 8));
 		iph->ttl = new_ttl;
 	}
 
