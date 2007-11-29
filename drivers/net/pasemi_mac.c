3676d2f3833dff95fc222d7c9b7376df4b0a0bfc
Olof Johansson <olof@lixom.net>
2007-11-28 20:56:04 -0600

    pasemi_mac: Move register definitions to include/asm-powerpc
    pasemi_mac: Move register definitions to include/asm-powerpc
    
    Move the common register formats and descriptor layouts from
    drivers/net/pasemi_mac.h to include/asm-poewrpc/pasemi_dma.h
    
    Previously only the ethernet driver was using them, but other drivers
    are coming up that will also use them, so it makes sense to share the
    constants.
    
    Signed-off-by: Olof Johansson <olof@lixom.net>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/pasemi_mac.c
+++ drivers/net/pasemi_mac.c
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/irq.h>
 #include <asm/firmware.h>
+#include <asm/pasemi_dma.h>
 
 #include "pasemi_mac.h"
 
