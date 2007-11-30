bd2104658644dcc890966c31ff1cd61fe932f285
Pekka Enberg <penberg@cs.helsinki.fi>
2007-11-30 09:53:09 +0200

    ipg: remove old contact information
    Remove old comment as up-to-date contact information is in MAINTAINERS.
    
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>
    Cc: Francois Romieu <romieu@fr.zoreil.com>
    Cc: Sorbica Shieh <sorbica@icplus.com.tw>
    Cc: Jesse Huang <jesse@icplus.com.tw>

--- drivers/net/ipg.h
+++ drivers/net/ipg.h
@@ -1,21 +1,9 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- *
- * ipg.h
- *
  * Include file for Gigabit Ethernet device driver for Network
  * Interface Cards (NICs) utilizing the Tamarack Microelectronics
  * Inc. IPG Gigabit or Triple Speed Ethernet Media Access
  * Controller.
- *
- * Craig Rich
- * Sundance Technology, Inc.
- * 1485 Saratoga Avenue
- * Suite 200
- * San Jose, CA 95129
- * 408 873 4117
- * www.sundanceti.com
- * craig_rich@sundanceti.com
  */
 #ifndef __LINUX_IPG_H
 #define __LINUX_IPG_H
