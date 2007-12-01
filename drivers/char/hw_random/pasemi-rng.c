e5b05d326e53050975672073ac06072df4e3ca1a
Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
2007-12-01 12:52:35 +1100

    [HWRNG] pasemi: Add missing wait argument to data_present
    drivers/char/hw_random/pasemi-rng.c: In function `pasemi_rng_data_present':
    drivers/char/hw_random/pasemi-rng.c:53: error: `wait' undeclared (first use in this function)
    drivers/char/hw_random/pasemi-rng.c:53: error: (Each undeclared identifier is reported only once
    drivers/char/hw_random/pasemi-rng.c:53: error: for each function it appears in.)
    drivers/char/hw_random/pasemi-rng.c: At top level:
    drivers/char/hw_random/pasemi-rng.c:93: warning: initialization from incompatible pointer type
    
    Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- drivers/char/hw_random/pasemi-rng.c
+++ drivers/char/hw_random/pasemi-rng.c
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define MODULE_NAME "pasemi_rng"
 
-static int pasemi_rng_data_present(struct hwrng *rng)
+static int pasemi_rng_data_present(struct hwrng *rng, int wait)
 {
 	void __iomem *rng_regs = (void __iomem *)rng->priv;
 	int data, i;
