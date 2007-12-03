a0a9d122bcfca8706881dff27deef6760ec8f2df
Hollis Blanchard <hollisb@us.ibm.com>
2007-12-03 15:30:24 -0600

    KVM: Portability: Move IO device definitions to its own header file
    Signed-off-by: Hollis Blanchard <hollisb@us.ibm.com>
    Acked-by: Carsten Otte <cotte@de.ibm.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/kvm_main.c
+++ drivers/kvm/kvm_main.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "kvm.h"
 #include "x86.h"
 #include "irq.h"
+#include "iodev.h"
 
 #include <linux/kvm.h>
 #include <linux/module.h>
