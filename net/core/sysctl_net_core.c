4d788071e5674f42a8f23edcedd4c1f6d7d71dcd
Pavel Emelyanov <xemul@openvz.org>
2007-12-05 01:36:23 -0800

    [NET]: Remove unneeded ifdefs from sysctl_net_core.c
    This file is already compiled out when the SYSCTL=n, so
    these ifdefs, that enclose the whole file, can be removed.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/sock.h>
 #include <net/xfrm.h>
 
-#ifdef CONFIG_SYSCTL
-
 ctl_table core_table[] = {
 #ifdef CONFIG_NET
 	{
@@ -152,5 +150,3 @@ ctl_table core_table[] = {
 	},
 	{ .ctl_name = 0 }
 };
-
-#endif
