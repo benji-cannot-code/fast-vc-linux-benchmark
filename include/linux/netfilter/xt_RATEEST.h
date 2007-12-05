a39f8861684aa3721fffe75184254126d9401b35
Patrick McHardy <kaber@trash.net>
2007-12-04 23:40:05 -0800

    [NETFILTER]: x_tables: add RATEEST target
    Add new rate estimator target (using gen_estimator). In combination with
    the rateest match (next patch) this can be used for load-based multipath
    routing.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/linux/netfilter/xt_RATEEST.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _XT_RATEEST_TARGET_H
+#define _XT_RATEEST_TARGET_H
+
+struct xt_rateest_target_info {
+	char			name[IFNAMSIZ];
+	int8_t			interval;
+	u_int8_t		ewma_log;
+	struct xt_rateest	*est __attribute__((aligned(8)));
+};
+
+#endif /* _XT_RATEEST_TARGET_H */
