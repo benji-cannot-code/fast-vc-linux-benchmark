cfc87e6980eadc030f480afe2c538262a103decf
Pavel Emelyanov <xemul@openvz.org>
2007-12-05 01:42:49 -0800

    [NET]: Remove the empty net_table
    I have removed all the entries from this table (core_table,
    ipv4_table and tr_table), so now we can safely drop it.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sysctl_net.c
+++ net/sysctl_net.c
@@ -31,10 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/if_tr.h>
 #endif
 
-struct ctl_table net_table[] = {
-	{ 0 },
-};
-
 static struct list_head *
 net_ctl_header_lookup(struct ctl_table_root *root, struct nsproxy *namespaces)
 {
