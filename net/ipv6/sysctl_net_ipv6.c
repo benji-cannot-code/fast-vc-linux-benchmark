0f4020194a5d4559ae786d9658412775b4c6055a
Pavel Emelyanov <xemul@openvz.org>
2007-12-05 01:43:25 -0800

    [IPV6]: Make the ipv6/sysctl_net_ipv6.c compilation cleaner
    Since this file is entirely enclosed with the
    #ifdef CONFIG_SYSCTL/#endif pair, it's OK to move this
    CONFIG_ into a Makefile.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/sysctl_net_ipv6.c
+++ net/ipv6/sysctl_net_ipv6.c
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/addrconf.h>
 #include <net/inet_frag.h>
 
-#ifdef CONFIG_SYSCTL
-
 static ctl_table ipv6_table[] = {
 	{
 		.ctl_name	= NET_IPV6_ROUTE,
@@ -116,8 +114,3 @@ void ipv6_sysctl_unregister(void)
 {
 	unregister_sysctl_table(ipv6_sysctl_header);
 }
-
-#endif /* CONFIG_SYSCTL */
-
-
-
