0e736d8a59c6a8d47578ca1954525831ce7642f7
Dave Olson <dave.olson@qlogic.com>
2007-12-06 00:28:02 -0800

    IB/ipath: Changes for fields moving from devdata to portdata
    This patch moves some arrays that were defined per-device to be
    variables defined in the per context data structure, thus avoiding extra
    kzalloc() calls.
    
    Signed-off-by: Dave Olson <dave.olson@qlogic.com>
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/ipath/ipath_kernel.h
+++ drivers/infiniband/hw/ipath/ipath_kernel.h
@@ -142,6 +142,11 @@ struct ipath_portdata {
 	u32 port_pionowait;
 	/* total number of rcvhdrqfull errors */
 	u32 port_hdrqfull;
+	/*
+	 * Used to suppress multiple instances of same
+	 * port staying stuck at same point.
+	 */
+	u32 port_lastrcvhdrqtail;
 	/* saved total number of rcvhdrqfull errors for poll edge trigger */
 	u32 port_hdrqfull_poll;
 	/* total number of polled urgent packets */
@@ -150,6 +155,7 @@ struct ipath_portdata {
 	u32 port_urgent_poll;
 	/* pid of process using this port */
 	pid_t port_pid;
+	pid_t port_subpid[INFINIPATH_MAX_SUBPORT];
 	/* same size as task_struct .comm[] */
 	char port_comm[16];
 	/* pkeys set by this use of this port */
@@ -320,16 +326,6 @@ struct ipath_devdata {
 	/* count of port 0 hdrqfull errors */
 	u32 ipath_p0_hdrqfull;
 
-	/*
-	 * (*cfgports) used to suppress multiple instances of same
-	 * port staying stuck at same point
-	 */
-	u32 *ipath_lastrcvhdrqtails;
-	/*
-	 * (*cfgports) used to suppress multiple instances of same
-	 * port staying stuck at same point
-	 */
-	u32 *ipath_lastegrheads;
 	/*
 	 * index of last piobuffer we used.  Speeds up searching, by
 	 * starting at this point.  Doesn't matter if multiple cpu's use and
