765b35f626d186aa0bff2d25bec85edab23a1abd
YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>
2007-12-06 16:11:48 -0800

    [IPV6]: Make ndisc_dst_alloc() common for later use.
    For later use, this patch is renaming ndisc_dst_alloc()
    (and related function/structures) to icmp6_dst_alloc()
    (and so on).  This patch also removing unused function-
    pointer argument for it.
    
    Signed-off-by: YOSHIFUJI Hideaki <yoshfuji@linux-ipv6.org>

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -1465,7 +1465,7 @@ void fib6_run_gc(unsigned long dummy)
 	}
 	gc_args.more = 0;
 
-	ndisc_dst_gc(&gc_args.more);
+	icmp6_dst_gc(&gc_args.more);
 	fib6_clean_all(fib6_age, 0, NULL);
 
 	if (gc_args.more)
