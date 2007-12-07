c8b846e33edbca63fad30baf779e6725c746ee9f
Frank Seidel <fseidel@suse.de>
2007-12-07 13:20:42 +0100

    ACPI: Add autoload info to dock driver
    References: https://bugzilla.novell.com/show_bug.cgi?id=302482
    
    Signed-off-by: Thomas Renninger <trenn@novell.com>
    Signed-off-by: Kay Sievers <kasievers@novell.com>
    Signed-off-by: Frank Seidel <fseidel@suse.de>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- include/acpi/acpi_drivers.h
+++ include/acpi/acpi_drivers.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ACPI_BUTTON_HID_SLEEPF		"LNXSLPBN"
 #define ACPI_VIDEO_HID			"LNXVIDEO"
 #define ACPI_BAY_HID			"LNXIOBAY"
+#define ACPI_DOCK_HID			"LNXDOCK"
 
 /* --------------------------------------------------------------------------
                                        PCI
