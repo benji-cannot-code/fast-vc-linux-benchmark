734a6f54cccd48ace01f65982c8f5b0c4b57e793
Gerrit Renker <gerrit@erg.abdn.ac.uk>
2007-12-08 15:08:08 -0200

    [TFRC]: Move comment.
    Moved up the comment "Receiver routines" above the first occurrence of
    RX history routines.
    
    Signed-off-by: Gerrit Renker <gerrit@erg.abdn.ac.uk>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/dccp/ccids/lib/packet_history.c
+++ net/dccp/ccids/lib/packet_history.c
@@ -115,6 +115,11 @@ u32 tfrc_tx_hist_rtt(struct tfrc_tx_hist_entry *head, const u64 seqno,
 EXPORT_SYMBOL_GPL(tfrc_tx_hist_rtt);
 
 
+/*
+ * 	Receiver History Routines
+ */
+static struct kmem_cache *tfrc_rx_hist_slab;
+
 /**
  * tfrc_rx_hist_index - index to reach n-th entry after loss_start
  */
@@ -131,11 +136,6 @@ static inline struct tfrc_rx_hist_entry *
 {
 	return h->ring[tfrc_rx_hist_index(h, h->loss_count)];
 }
-
-/*
- * 	Receiver History Routines
- */
-static struct kmem_cache *tfrc_rx_hist_slab;
 
 void tfrc_rx_hist_add_packet(struct tfrc_rx_hist *h,
 			     const struct sk_buff *skb,
