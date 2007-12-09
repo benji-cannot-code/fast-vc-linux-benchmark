c947faa2e8a465822424715b82640df14bbb6c6b
David Woodhouse <dwmw2@infradead.org>
2007-12-09 16:22:21 -0500

    libertas: stop using ieee80211 for radiotap device
    There seems to be no point in doing it as an ieee80211 device instead of
    a normal netdev, and when we override its ->priv and then call
    free_ieee80211() it has a distressing tendency to crash horribly.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/dev.h
+++ drivers/net/wireless/libertas/dev.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/wireless.h>
 #include <linux/ethtool.h>
 #include <linux/debugfs.h>
-#include <net/ieee80211.h>
 
 #include "defs.h"
 #include "scan.h"
@@ -115,7 +114,6 @@ struct lbs_private {
 	struct net_device_stats stats;
 	struct net_device *mesh_dev; /* Virtual device */
 	struct net_device *rtap_net_dev;
-	struct ieee80211_device *ieee;
 
 	struct iw_statistics wstats;
 	struct lbs_mesh_stats mstats;
