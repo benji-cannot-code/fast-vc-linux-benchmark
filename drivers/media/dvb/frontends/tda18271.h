4503f437f9858c130a665dba1b42939b80de9a0e
Michael Krufky <mkrufky@linuxtv.org>
2007-12-09 22:23:30 -0300

    V4L/DVB (6801): tda18271: pass i2c gate configuration into tda18271_attach()
    If we pass TDA18271_GATE_DIGITAL into tda18271_attach(), it will always try to
    use the digital demodulator's i2c gate.
    
    If we pass TDA18271_GATE_ANALOG into tda18271_attach(), it will always try to
    use the analog demodulator's i2c gate.
    
    If we pass TDA18271_GATE_AUTO into tda18271_attach(), it will try to use the
    analog demodulator's i2c gate when tuning in analog mode, and it will try to
    use the digital demodulator's i2c gate when tuning in digital mode.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda18271.h
+++ drivers/media/dvb/frontends/tda18271.h
@@ -25,13 +25,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include "dvb_frontend.h"
 
+enum tda18271_i2c_gate {
+	TDA18271_GATE_AUTO = 0,
+	TDA18271_GATE_ANALOG,
+	TDA18271_GATE_DIGITAL,
+};
+
 #if defined(CONFIG_DVB_TDA18271) || (defined(CONFIG_DVB_TDA18271_MODULE) && defined(MODULE))
 extern struct dvb_frontend *tda18271_attach(struct dvb_frontend *fe, u8 addr,
-					    struct i2c_adapter *i2c);
+					    struct i2c_adapter *i2c,
+					    enum tda18271_i2c_gate gate);
 #else
 static inline struct dvb_frontend *tda18271_attach(struct dvb_frontend *fe,
 						   u8 addr,
-						   struct i2c_adapter *i2c)
+						   struct i2c_adapter *i2c,
+						   enum tda18271_i2c_gate gate);
 {
 	printk(KERN_WARNING "%s: driver disabled by Kconfig\n", __FUNCTION__);
 	return NULL;
