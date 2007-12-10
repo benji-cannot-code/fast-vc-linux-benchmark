3833e079a71ef57c3e4377415d66d4b40a96b968
Michael Krufky <mkrufky@linuxtv.org>
2007-12-10 16:08:25 -0300

    V4L/DVB (6789): tuner: use char *name instead of 128 byte fixed array for demod info
    Don't waste 128 bytes of memory for a name that might not actually need it.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tuner-driver.h
+++ drivers/media/video/tuner-driver.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern unsigned const int tuner_count;
 
 struct analog_demod_info {
-	char name[128];
+	char *name;
 };
 
 struct analog_tuner_ops {
