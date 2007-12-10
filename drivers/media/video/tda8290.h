3c947527c8e79370075676d6b2c64340a4a8b6cd
Michael Krufky <mkrufky@linuxtv.org>
2007-12-10 11:12:59 -0300

    V4L/DVB (6788): tda8290: fix codingstyle, open brace following struct on the same line
    Fix codingstyle issue discovered after using new checkpatch.pl
    
     ERROR: open brace '{' following struct go on the same line
     396: FILE: linux/drivers/media/video/tda8290.h:24:
    +struct tda829x_config
    +{
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tda8290.h
+++ drivers/media/video/tda8290.h
@@ -21,8 +21,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/i2c.h>
 #include "dvb_frontend.h"
 
-struct tda829x_config
-{
+struct tda829x_config {
 	unsigned int *lna_cfg;
 	int (*tuner_callback) (void *dev, int command, int arg);
 };
