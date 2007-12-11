fd139e133b0438cb7288d853ec6702715724cbb9
David Woodhouse <dwmw2@infradead.org>
2007-12-11 00:07:58 -0500

    libertas: switch to a waitqueue and timer for handling USB firmware load
    No need to busy-wait, even if we did have a 100ms delay in the loop.
    This makes it easier to support the new 'firmware ready' event which is
    in the new firmware, too.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/if_usb.h
+++ drivers/net/wireless/libertas/if_usb.h
@@ -2,6 +2,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _LBS_IF_USB_H
 #define _LBS_IF_USB_H
 
+#include <linux/wait.h>
+#include <linux/timer.h>
+
 /**
   * This file contains definition for USB interface.
   */
@@ -57,6 +60,8 @@ struct usb_card_rec {
 	u8 bulk_out_endpointAddr;
 
 	const struct firmware *fw;
+	struct timer_list fw_timeout;
+	wait_queue_head_t fw_wq;
 	u8 CRC_OK;
 	u32 fwseqnum;
 	u32 lastseqnum;
