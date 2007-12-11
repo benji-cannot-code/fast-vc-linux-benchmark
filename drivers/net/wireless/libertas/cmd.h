38c9aa6335e6461fd3c4389f305f1d02a16db572
Dan Williams <dcbw@redhat.com>
2007-12-11 12:33:30 -0500

    libertas: clean up direct command handling
    Move direct command handling through __lbs_cmd() over to using the
    header as the first member of the command structure, and only define
    the __lbs_cmd() callback in one place rather than 3.  Convert boot2
    version command to new usage.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/cmd.h
+++ drivers/net/wireless/libertas/cmd.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dev.h"
 
 #define lbs_cmd(priv, cmdnr, cmd, callback, callback_arg) \
-	__lbs_cmd(priv, cmdnr, &cmd, sizeof(cmd), callback, callback_arg)
+	__lbs_cmd(priv, cmdnr, (struct cmd_header *) &cmd, sizeof(cmd), \
+			callback, callback_arg)
 
-int __lbs_cmd(struct lbs_private *priv, uint16_t command, void *cmd, int cmd_size, 
-	      int (*callback)(struct lbs_private *, unsigned long, struct cmd_ds_command *),
+int __lbs_cmd(struct lbs_private *priv, uint16_t command,
+	      struct cmd_header *in_cmd, int in_cmd_size, 
+	      int (*callback)(struct lbs_private *, unsigned long, struct cmd_header *),
 	      unsigned long callback_arg);
 
 #endif /* _LBS_CMD_H */
