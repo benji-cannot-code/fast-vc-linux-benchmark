8dd70f6beb461c0c525fe21b54fe5b559ed69ced
David Woodhouse <dwmw2@infradead.org>
2007-12-11 15:28:18 -0500

    libertas: remove casts from lbs_cmd() and lbs_cmd_with_response() macros
    If stupid people like me give it arguments with the wrong type (like a
    pointer to the structure, for example, instead of the structure itself),
    then we should probably notice that at compile time. Otherwise, much
    confusion ensues.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/cmd.h
+++ drivers/net/wireless/libertas/cmd.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dev.h"
 
 #define lbs_cmd(priv, cmdnr, cmd, callback, callback_arg) \
-	__lbs_cmd(priv, cmdnr, (struct cmd_header *) &cmd, sizeof(cmd), \
+	__lbs_cmd(priv, cmdnr, &(cmd).hdr, sizeof(cmd),	  \
 			callback, callback_arg)
 
 #define lbs_cmd_with_response(priv, cmdnr, cmd) \
-	__lbs_cmd(priv, cmdnr, (struct cmd_header *) &cmd, sizeof(cmd), \
+	__lbs_cmd(priv, cmdnr, &(cmd).hdr, sizeof(cmd), \
 		  lbs_cmd_copyback, (unsigned long) &cmd)
  
 int __lbs_cmd(struct lbs_private *priv, uint16_t command,
