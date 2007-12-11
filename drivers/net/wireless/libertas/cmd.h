43a98cd6ad01c70929f616f20648851c33a7c157
Dan Williams <dcbw@redhat.com>
2007-12-11 12:40:35 -0500

    libertas: add simple copyback command callback
    A simple callback which copies the response back into the
    command buffer that was used to send the command to the
    card.  Will allow for direct command processing outside
    the mega-switches in cmd.c and cmdresp.c.
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/cmd.h
+++ drivers/net/wireless/libertas/cmd.h
@@ -11,9 +11,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	__lbs_cmd(priv, cmdnr, (struct cmd_header *) &cmd, sizeof(cmd), \
 			callback, callback_arg)
 
+#define lbs_cmd_with_response(priv, cmdnr, cmd) \
+	__lbs_cmd(priv, cmdnr, (struct cmd_header *) &cmd, sizeof(cmd), \
+		  lbs_cmd_copyback, (unsigned long) &cmd)
+ 
 int __lbs_cmd(struct lbs_private *priv, uint16_t command,
 	      struct cmd_header *in_cmd, int in_cmd_size, 
 	      int (*callback)(struct lbs_private *, unsigned long, struct cmd_header *),
 	      unsigned long callback_arg);
+
+int lbs_cmd_copyback(struct lbs_private *priv, unsigned long extra,
+		     struct cmd_header *resp);
 
 #endif /* _LBS_CMD_H */
