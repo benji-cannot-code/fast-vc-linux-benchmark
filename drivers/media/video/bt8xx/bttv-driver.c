2a3e300a658cc4aa686871ba1bb8bb8015d07ee6
Adrian Bunk <bunk@kernel.org>
2007-12-11 19:23:43 -0300

    V4L/DVB (6797): bt8xx/ section fixes
    This patch fixes the following section mismatch with CONFIG_HOTPLUG=n:
    
    <--  snip  -->
    
    ...
    ...
    
    <--  snip  -->
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/bt8xx/bttv-driver.c
+++ drivers/media/video/bt8xx/bttv-driver.c
@@ -4986,7 +4986,7 @@ static struct pci_driver bttv_pci_driver = {
 #endif
 };
 
-static int bttv_init_module(void)
+static int __init bttv_init_module(void)
 {
 	int ret;
 
@@ -5019,7 +5019,7 @@ static int bttv_init_module(void)
 	return pci_register_driver(&bttv_pci_driver);
 }
 
-static void bttv_cleanup_module(void)
+static void __exit bttv_cleanup_module(void)
 {
 	pci_unregister_driver(&bttv_pci_driver);
 	bus_unregister(&bttv_sub_bus_type);
