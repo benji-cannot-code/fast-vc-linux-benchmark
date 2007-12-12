226433eb3ecab3da42d5a27c11639707be02517e
David Woodhouse <dwmw2@infradead.org>
2007-12-11 20:03:01 -0500

    libertas: disable mesh temporarily while setting eth channel/assoc
    Otherwise the device won't let us change channels.
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/assoc.c
+++ drivers/net/wireless/libertas/assoc.c
@@ -205,6 +205,12 @@ static int assoc_helper_channel(struct lbs_private *priv,
 	if (assoc_req->channel == priv->curbssparams.channel)
 		goto done;
 
+	if (priv->mesh_dev) {
+		/* Disconnect mesh while associating -- otherwise it
+		   won't let us change channels */
+		lbs_mesh_config(priv, 0);
+	}
+
 	lbs_deb_assoc("ASSOC: channel: %d -> %d\n",
 	       priv->curbssparams.channel, assoc_req->channel);
 
@@ -222,7 +228,7 @@ static int assoc_helper_channel(struct lbs_private *priv,
 	if (assoc_req->channel != priv->curbssparams.channel) {
 		lbs_deb_assoc("ASSOC: channel: failed to update channel to %d\n",
 		              assoc_req->channel);
-		goto done;
+		goto restore_mesh;
 	}
 
 	if (   assoc_req->secinfo.wep_enabled
@@ -237,7 +243,11 @@ static int assoc_helper_channel(struct lbs_private *priv,
 	/* Must restart/rejoin adhoc networks after channel change */
 	set_bit(ASSOC_FLAG_SSID, &assoc_req->flags);
 
-done:
+ restore_mesh:
+	if (priv->mesh_dev)
+		lbs_mesh_config(priv, 1);
+
+ done:
 	lbs_deb_leave_args(LBS_DEB_ASSOC, "ret %d", ret);
 	return ret;
 }
