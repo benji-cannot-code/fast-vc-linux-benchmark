ed5f38f949ee2b344c697360d59a7be0a9e44ae2
Michael Chan <mchan@broadcom.com>
2007-12-12 11:20:22 -0800

    [BNX2]: Update version to 1.6.9.
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.c
+++ drivers/net/bnx2.c
@@ -57,8 +57,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_MODULE_NAME		"bnx2"
 #define PFX DRV_MODULE_NAME	": "
-#define DRV_MODULE_VERSION	"1.6.9"
-#define DRV_MODULE_RELDATE	"December 8, 2007"
+#define DRV_MODULE_VERSION	"1.7.0"
+#define DRV_MODULE_RELDATE	"December 11, 2007"
 
 #define RUN_AT(x) (jiffies + (x))
 
