bf2bfa40519e99ec985b97e23102a644d53f8275
Ishizaki Kou <kou.ishizaki@toshiba.co.jp>
2007-12-13 21:13:14 +1100

    [POWERPC] celleb: Split machine definition
    This splits the machine definition for celleb into two definitions,
    one for celleb_beat, and the other for celleb_native.  Though this
    looks complex because of sorting some functions, there are no
    more semantic changes than that for the splitting.
    
    Signed-off-by: Kou Ishizaki <Kou.Ishizaki@toshiba.co.jp>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/celleb/iommu.c
+++ arch/powerpc/platforms/celleb/iommu.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/pci.h>
 #include <linux/of_platform.h>
 
-#include <asm/firmware.h>
+#include <asm/machdep.h>
 
 #include "beat_wrapper.h"
 
@@ -93,7 +93,7 @@ static struct notifier_block celleb_of_bus_notifier = {
 
 static int __init celleb_init_iommu(void)
 {
-	if (!firmware_has_feature(FW_FEATURE_BEAT))
+	if (!machine_is(celleb_beat))
 		return -ENODEV;
 
 	celleb_init_direct_mapping();
