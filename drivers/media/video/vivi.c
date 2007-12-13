b178077e1407bd732191b5f497f567a0894fd859
Mauro Carvalho Chehab <mchehab@infradead.org>
2007-12-13 16:13:37 -0300

    V4L/DVB (6816): Add proper locking for buffer filling
    Avoids de-alocating buffers before finishing to fill a buffer
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/vivi.c
+++ drivers/media/video/vivi.c
@@ -164,6 +164,7 @@ struct vivi_dev {
 	struct list_head           vivi_devlist;
 
 	struct mutex               lock;
+	spinlock_t                 slock;
 
 	int                        users;
 
@@ -389,6 +390,7 @@ static void vivi_thread_tick(struct vivi_dmaqueue  *dma_q)
 
 	int bc;
 
+	spin_lock(&dev->slock);
 	/* Announces videobuf that all went ok */
 	for (bc = 0;; bc++) {
 		if (list_empty(&dma_q->active)) {
@@ -402,6 +404,7 @@ static void vivi_thread_tick(struct vivi_dmaqueue  *dma_q)
 		/* Nobody is waiting something to be done, just return */
 		if (!waitqueue_active(&buf->vb.done)) {
 			mod_timer(&dma_q->timeout, jiffies+BUFFER_TIMEOUT);
+			spin_unlock(&dev->slock);
 			return;
 		}
 
@@ -420,6 +423,7 @@ static void vivi_thread_tick(struct vivi_dmaqueue  *dma_q)
 	if (bc != 1)
 		dprintk(dev, 1, "%s: %d buffers handled (should be 1)\n",
 			__FUNCTION__, bc);
+	spin_unlock(&dev->slock);
 }
 
 static void vivi_sleep(struct vivi_dmaqueue  *dma_q)
@@ -592,6 +596,8 @@ static void vivi_vid_timeout(unsigned long data)
 	struct vivi_dmaqueue *vidq = &dev->vidq;
 	struct vivi_buffer   *buf;
 
+	spin_lock(&dev->slock);
+
 	while (!list_empty(&vidq->active)) {
 		buf = list_entry(vidq->active.next,
 				 struct vivi_buffer, vb.queue);
@@ -600,8 +606,9 @@ static void vivi_vid_timeout(unsigned long data)
 		wake_up(&buf->vb.done);
 		printk(KERN_INFO "vivi/0: [%p/%d] timeout\n", buf, buf->vb.i);
 	}
-
 	restart_video_queue(vidq);
+
+	spin_unlock(&dev->slock);
 }
 
 /* ------------------------------------------------------------------
@@ -1065,7 +1072,7 @@ found:
 			dev->h, dev->m, dev->s, (dev->us + 500) / 1000);
 
 	videobuf_queue_vmalloc_init(&fh->vb_vidq, &vivi_video_qops,
-			NULL, NULL, fh->type, V4L2_FIELD_INTERLACED,
+			NULL, &dev->slock, fh->type, V4L2_FIELD_INTERLACED,
 			sizeof(struct vivi_buffer), fh);
 
 	return 0;
@@ -1225,6 +1232,7 @@ static int __init vivi_init(void)
 
 		/* initialize locks */
 		mutex_init(&dev->lock);
+		spin_lock_init(&dev->slock);
 
 		dev->vidq.timeout.function = vivi_vid_timeout;
 		dev->vidq.timeout.data     = (unsigned long)dev;
