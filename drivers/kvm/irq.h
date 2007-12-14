0edc0548e04206e64b62cb49a4ca7d670cf74b58
Zhang Xiantao <xiantao.zhang@intel.com>
2007-12-14 09:41:22 +0800

    KVM: Portability: Move kvm_vcpu definition back to kvm.h
    This patches moves kvm_vcpu definition to kvm.h, and finally
    kvm.h includes x86.h.
    
    Signed-off-by: Zhang Xiantao <xiantao.zhang@intel.com>
    Acked-by: Carsten Otte <cotte@de.ibm.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- drivers/kvm/irq.h
+++ drivers/kvm/irq.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/hrtimer.h>
 #include <asm/kvm.h>
 #include "iodev.h"
-#include "x86.h"
+#include "kvm.h"
 
 struct kvm;
 struct kvm_vcpu;
