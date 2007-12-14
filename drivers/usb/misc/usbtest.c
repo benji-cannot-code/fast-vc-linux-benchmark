e5ad71e1dcbc3da33846bfd75021fdef6238e8da
Andrew Morton <akpm@linux-foundation.org>
2007-12-13 16:15:34 -0800

    USB: testing driver: don't free a locked mutex
    Dopey thing to do and lockdep will (or should) warn.
    
    Spotted by Daniel Walker.
    
    Cc: Matthias Kaehlcke <matthias.kaehlcke@gmail.com>
    Cc: Daniel Walker <dwalker@mvista.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/misc/usbtest.c
+++ drivers/usb/misc/usbtest.c
@@ -1993,8 +1993,6 @@ static void usbtest_disconnect (struct usb_interface *intf)
 {
 	struct usbtest_dev	*dev = usb_get_intfdata (intf);
 
-	mutex_lock(&dev->lock);
-
 	usb_set_intfdata (intf, NULL);
 	dev_dbg (&intf->dev, "disconnect\n");
 	kfree (dev);
