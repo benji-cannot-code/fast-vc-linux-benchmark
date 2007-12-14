884bef644a0f641e902766d62ad5beb05e38fca8
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2007-12-14 06:36:35 -0600

    [POWERPC] 4xx: Include missing header
    A small error caused a header file to be removed making sequoia support no
    longer compile.  Fix it.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/platforms/44x/sequoia.c
+++ arch/powerpc/platforms/44x/sequoia.c
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uic.h>
 #include <asm/pci-bridge.h>
 
+#include "44x.h"
 
 static struct of_device_id sequoia_of_bus[] = {
 	{ .compatible = "ibm,plb4", },
