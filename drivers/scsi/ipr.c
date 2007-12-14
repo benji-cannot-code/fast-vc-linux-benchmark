dce220927ab79f7624ff7a0936a905e07b23ad82
Denis Cheng <crquan@gmail.com>
2007-12-13 16:14:27 -0800

    [SCSI] ipr: use LIST_HEAD instead of LIST_HEAD_INIT
    Signed-off-by: Denis Cheng <crquan@gmail.com>
    Acked-by: Brian King <brking@linux.vnet.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/ipr.c
+++ drivers/scsi/ipr.c
@@ -85,7 +85,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
  *   Global Data
  */
-static struct list_head ipr_ioa_head = LIST_HEAD_INIT(ipr_ioa_head);
+static LIST_HEAD(ipr_ioa_head);
 static unsigned int ipr_log_level = IPR_DEFAULT_LOG_LEVEL;
 static unsigned int ipr_max_speed = 1;
 static int ipr_testmode = 0;
