1f3f3b54c5f0d91fa073c93a7145cd21c290ea44
David Woodhouse <dwmw2@infradead.org>
2007-12-15 02:38:17 -0500

    libertas: kill pdata_buf member of struct cmd_ctrl_node
    We can use the callback_arg for it; that's the way we're heading anyway...
    
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/cmd.c
+++ drivers/net/wireless/libertas/cmd.c
@@ -1281,7 +1281,6 @@ static void __lbs_cleanup_and_insert_cmd(struct lbs_private *priv,
 	if (!cmdnode)
 		goto out;
 
-	cmdnode->pdata_buf = NULL;
 	cmdnode->callback = NULL;
 	cmdnode->callback_arg = 0;
 
@@ -1802,9 +1801,8 @@ static void lbs_set_cmd_ctrl_node(struct lbs_private *priv,
 	if (!ptempnode)
 		return;
 
-	ptempnode->pdata_buf = pdata_buf;
 	ptempnode->callback = NULL;
-	ptempnode->callback_arg = 0;
+	ptempnode->callback_arg = (unsigned long)pdata_buf;
 
 	lbs_deb_leave(LBS_DEB_HOST);
 }
