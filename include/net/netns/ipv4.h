522b45341ebe55acb508597223ac758b3852dc34
Pavel Emelyanov <xemul@openvz.org>
2007-12-16 13:31:47 -0800

    [IPV4]: Move the devinet pointers on the struct net
    This is the core.
    
    Add all and default pointers on the netns_ipv4 and register
    a new pernet subsys to initialize them.
    
    Also add the ctl_table_header to register the
    net.ipv4.ip_forward ctl.
    
    I don't allocate additional memory for init_net, but use
    global devinets.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __NETNS_IPV4_H__
 #define __NETNS_IPV4_H__
+struct ctl_table_header;
+struct ipv4_devconf;
+
 struct netns_ipv4 {
+	struct ctl_table_header	*forw_hdr;
+	struct ipv4_devconf	*devconf_all;
+	struct ipv4_devconf	*devconf_dflt;
 };
 #endif
