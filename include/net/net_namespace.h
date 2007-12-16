18d426ca18f0f52b795f2dacb20b5a5dbf32c197
Pavel Emelyanov <xemul@openvz.org>
2007-12-16 13:29:36 -0800

    [NETNS]: Add the netns_ipv4 struct
    The ipv4 will store its parameters inside this structure.
    This one is empty now, but it will be eventually filled.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <net/netns/unix.h>
 #include <net/netns/packet.h>
+#include <net/netns/ipv4.h>
 
 struct proc_dir_entry;
 struct net_device;
@@ -47,6 +48,7 @@ struct net {
 
 	struct netns_packet	packet;
 	struct netns_unix	unx;
+	struct netns_ipv4	ipv4;
 };
 
 #ifdef CONFIG_NET
