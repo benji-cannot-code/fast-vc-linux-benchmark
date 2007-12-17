240199b2cc050a6f8d7ef48e990c059bee970d50
Greg Kroah-Hartman <gregkh@suse.de>
2007-12-17 15:54:39 -0400

    Kobject: change drivers/infiniband to use kobject_init_and_add
    Stop using kobject_register, as this way we can control the sending of
    the uevent properly, after everything is properly initialized.
    
    Cc: Roland Dreier <rolandd@cisco.com>
    Cc: Sean Hefty <mshefty@ichips.intel.com>
    Cc: Hal Rosenstock <hal.rosenstock@gmail.com>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -1027,7 +1027,7 @@ struct ib_device {
 
 	struct module               *owner;
 	struct class_device          class_dev;
-	struct kobject               ports_parent;
+	struct kobject               *ports_parent;
 	struct list_head             port_list;
 
 	enum {
