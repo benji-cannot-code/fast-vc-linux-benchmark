d0cd7ee6eef68b686bbf17ec4750f9dece8a43c4
Paul Mundt <lethal@linux-sh.org>
2007-12-17 15:59:38 +0900

    sh: Kill off pgtable.h from scatterlist.h.
    Fixes up the mmc build.
    
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/scatterlist.h
+++ include/asm-sh/scatterlist.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_SH_SCATTERLIST_H
 #define __ASM_SH_SCATTERLIST_H
 
-#include <asm/pgtable.h>
 #include <asm/types.h>
 
 struct scatterlist {
