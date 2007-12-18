584a2cf9cd81bdcdbfd677a8d0bc6d1cd9340b92
Shannon Nelson <shannon.nelson@intel.com>
2007-12-17 16:20:08 -0800

    I/OAT: fix null device in call to dev_err()
    We can't use the device in a dev_err() after a kzalloc failure or after the
    kfree, so simplify it to the pdev that was originally passed in.
    
    Cc: Eric Sesterhenn <snakebyte@gmx.de>
    Signed-off-by: Shannon Nelson <shannon.nelson@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/dma/ioat_dma.c
+++ drivers/dma/ioat_dma.c
@@ -1364,7 +1364,7 @@ err_completion_pool:
 err_dma_pool:
 	kfree(device);
 err_kzalloc:
-	dev_err(&device->pdev->dev,
+	dev_err(&pdev->dev,
 		"Intel(R) I/OAT DMA Engine initialization failed\n");
 	return NULL;
 }
