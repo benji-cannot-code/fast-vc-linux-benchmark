a92bc28f7df245e262936a499eb660ab2304d33b
Ivan Kokshaysky <ink@jurassic.park.msu.ru>
2007-12-17 16:19:57 -0800

    alpha: build fixes
    This fixes some of the alpha-specific build problems, except a) modpost
    warning about COMMON symbol "saved_config" and b) nasty final link
    failure with gcc-4.x, -Os and scsi-disk driver configured built-in
    (due to jump table in .rodata referencing discarded .exit.text).
    
    - build failure with gcc-4.2.x: fix up casts in cia_io* routines to avoid
      warnings ('discards qualifiers from pointer target type'), which are
      failures, thanks to -Werror;
    - modpost warnings: add missing __init qualifier for titan and marvel;
      for non-generic build, move machine vectors from .data to .data.init.refok
      section;
    - unbreak CPU-specific optimization: rearrange cpuflags-y assignments
      so that extended -mcpu value (ev56, pca56, ev67) overrides basic
      one (ev5, ev6) and not vice versa.
    
    Signed-off-by: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/alpha/kernel/machvec_impl.h
+++ arch/alpha/kernel/machvec_impl.h
@@ -135,7 +135,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __initmv __initdata
 #define ALIAS_MV(x)
 #else
-#define __initmv
+#define __initmv __initdata_refok
 
 /* GCC actually has a syntax for defining aliases, but is under some
    delusion that you shouldn't be able to declare it extern somewhere
