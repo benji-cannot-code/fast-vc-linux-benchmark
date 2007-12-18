50abc35be509b0f2f9a8eedde64e64a6fb11cc0c
Joe Perches <joe@perches.com>
2007-12-18 13:14:21 +0100

    [ALSA] include/sound/: Spelling fixes
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- include/sound/soc-dapm.h
+++ include/sound/soc-dapm.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SND_SOC_NOPM	-1
 
 /*
- * SoC dynamic audio power managment
+ * SoC dynamic audio power management
  *
  * We can have upto 4 power domains
  * 	1. Codec domain - VREF, VMID
