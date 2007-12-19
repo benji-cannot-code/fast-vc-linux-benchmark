7b1191970b634c66e3753d34fad5bdd51b9e41e7
Jan Kara <jack@suse.cz>
2007-12-19 15:24:52 +0100

    ocfs2: Use generic_file_llseek
    We should use generic_file_llseek() and not default_llseek() so that
    s_maxbytes gets properly checked when seeking.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>

--- fs/ocfs2/file.c
+++ fs/ocfs2/file.c
@@ -2234,6 +2234,7 @@ const struct inode_operations ocfs2_special_file_iops = {
 };
 
 const struct file_operations ocfs2_fops = {
+	.llseek		= generic_file_llseek,
 	.read		= do_sync_read,
 	.write		= do_sync_write,
 	.mmap		= ocfs2_mmap,
@@ -2252,6 +2253,7 @@ const struct file_operations ocfs2_fops = {
 };
 
 const struct file_operations ocfs2_dops = {
+	.llseek		= generic_file_llseek,
 	.read		= generic_read_dir,
 	.readdir	= ocfs2_readdir,
 	.fsync		= ocfs2_sync_file,
