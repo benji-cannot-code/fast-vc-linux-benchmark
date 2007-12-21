1032c5e944d03d17c9cfdee096eab29d295afa1f
Michael Chan <mchan@broadcom.com>
2007-12-20 19:56:59 -0800

    [BNX2]: Move tx indexes into bnx2_napi struct.
    Tx related fields used in NAPI polling are moved from the main
    bnx2 struct to the bnx2_napi struct.
    
    Signed-off-by: Michael Chan <mchan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2.h
+++ drivers/net/bnx2.h
@@ -6510,6 +6510,9 @@ struct bnx2_napi {
 	struct status_block	*status_blk;
 	u32 			last_status_idx;
 	u32			int_num;
+
+	u16			tx_cons;
+	u16			hw_tx_cons;
 };
 
 struct bnx2 {
@@ -6539,9 +6542,6 @@ struct bnx2 {
 	u16		tx_prod;
 	u32		tx_bidx_addr;
 	u32		tx_bseq_addr;
-
-	u16		tx_cons __attribute__((aligned(L1_CACHE_BYTES)));
-	u16		hw_tx_cons;
 
 	struct bnx2_napi	bnx2_napi;
 
