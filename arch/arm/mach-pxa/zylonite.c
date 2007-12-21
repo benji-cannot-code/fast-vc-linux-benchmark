262df3b3c7ef1302f24c3b17f7ec0871343ac439
Bridge Wu <bridge.wu@marvell.com>
2007-12-21 19:27:08 +0800

    [ARM] pxa: mmc: add 3rd host controller support for pxa310
    This patch is to add the third mmc controller support _only_
    for pxa310.
    
    On zylonite, the third controller support one slot.
    
    Signed-off-by: Bridge Wu <bridge.wu@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/zylonite.c
+++ arch/arm/mach-pxa/zylonite.c
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "generic.h"
 
-#define MAX_SLOTS	2
+#define MAX_SLOTS	3
 struct platform_mmc_slot zylonite_mmc_slot[MAX_SLOTS];
 
 int gpio_backlight;
@@ -243,6 +243,8 @@ static void __init zylonite_init_mmc(void)
 {
 	pxa_set_mci_info(&zylonite_mci_platform_data);
 	pxa3xx_set_mci2_info(&zylonite_mci2_platform_data);
+	if (cpu_is_pxa310())
+		pxa3xx_set_mci3_info(&zylonite_mci_platform_data);
 }
 #else
 static inline void zylonite_init_mmc(void) {}
