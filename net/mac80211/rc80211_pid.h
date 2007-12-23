b1d0c79e81d8163b664075c6dc39a6fc8f2bbea8
Stefano Brivio <stefano.brivio@polimi.it>
2007-12-23 04:39:17 +0100

    rc80211-pid: export human-readable target_pf value to debugfs
    Export the non-shifted target_pf value to debugfs, so that it's human-readable.
    
    Signed-off-by: Stefano Brivio <stefano.brivio@polimi.it>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/mac80211/rc80211_pid.h
+++ net/mac80211/rc80211_pid.h
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * link quality is good, the controller will fail to adjust failed frames
  * percentage to the target. This is intentional.
  */
-#define RC_PID_TARGET_PF (11 << RC_PID_ARITH_SHIFT)
+#define RC_PID_TARGET_PF 11
 
 /* Rate behaviour normalization quantity over time. */
 #define RC_PID_NORM_OFFSET 3
