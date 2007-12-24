a79ffd92c99cf035d5164aa179c55ad583771c82
Herbert Xu <herbert@gondor.apana.org.au>
2007-12-24 08:54:24 +0800

    [CRYPTO] api: Include sched.h for cond_resched in scatterwalk.h
    As Andrew Morton correctly points out, we need to explicitly include
    sched.h as we use the function cond_resched in crypto/scatterwalk.h.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/scatterwalk.h
+++ include/crypto/scatterwalk.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/mm.h>
 #include <linux/scatterlist.h>
+#include <linux/sched.h>
 
 static inline enum km_type crypto_kmap_type(int out)
 {
