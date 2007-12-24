c7d168bb15a075c3e812b07d4c0451222f39f85a
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2007-12-24 10:42:02 -0600

    [POWERPC] 4xx: Use machine_device_initcall for bus probe
    Some machine_xx_initcall macros were recently added that check for the machine
    type before calling the function.  This converts the 4xx platforms to use those
    for bus probing.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/platforms/40x/virtex.c
+++ arch/powerpc/platforms/40x/virtex.c
@@ -18,14 +18,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 static int __init virtex_device_probe(void)
 {
-	if (!machine_is(virtex))
-		return 0;
-
 	of_platform_bus_probe(NULL, NULL, NULL);
 
 	return 0;
 }
-device_initcall(virtex_device_probe);
+machine_device_initcall(virtex, virtex_device_probe);
 
 static int __init virtex_probe(void)
 {
