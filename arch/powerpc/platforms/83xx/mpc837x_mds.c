e46f5e266442f9cfbf1300e940d2d3bbe4c1aa45
Josh Boyer <jwboyer@linux.vnet.ibm.com>
2007-12-24 09:01:11 -0600

    [POWERPC] Remove unneeded variable declarations from mpc837x_mds
    Remove the declarations for isa_io_base and isa_mem_base as they are declared
    in pci-common.c now.
    
    Signed-off-by: Josh Boyer <jwboyer@linux.vnet.ibm.com>

--- arch/powerpc/platforms/83xx/mpc837x_mds.c
+++ arch/powerpc/platforms/83xx/mpc837x_mds.c
@@ -23,11 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "mpc83xx.h"
 
-#ifndef CONFIG_PCI
-unsigned long isa_io_base = 0;
-unsigned long isa_mem_base = 0;
-#endif
-
 /* ************************************************************************
  *
  * Setup the architecture
