37d01354bca476019b55f6ec1b5b2538c722223a
Michael Krufky <mkrufky@linuxtv.org>
2007-12-25 15:10:11 -0300

    V4L/DVB (6927): tda18271: improve printk macros
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda18271-tables.c
+++ drivers/media/dvb/frontends/tda18271-tables.c
@@ -291,7 +291,7 @@ int tda18271_lookup_pll_map(enum tda18271_map_type map_type,
 	}
 
 	if (!map) {
-		dbg_info("%s map is not set!\n", map_name);
+		tda_warn("%s map is not set!\n", map_name);
 		return -EINVAL;
 	}
 
@@ -303,7 +303,7 @@ int tda18271_lookup_pll_map(enum tda18271_map_type map_type,
 	*post_div = map[i].pd;
 	*div      = map[i].d;
 
-	dbg_map("%s: post div = 0x%02x, div = 0x%02x\n",
+	tda_map("%s: post div = 0x%02x, div = 0x%02x\n",
 		map_name, *post_div, *div);
 
 	return 0;
@@ -347,7 +347,7 @@ int tda18271_lookup_map(enum tda18271_map_type map_type, u32 *freq, u8 *val)
 	}
 
 	if (!map) {
-		dbg_info("%s map is not set!\n", map_name);
+		tda_warn("%s map is not set!\n", map_name);
 		return -EINVAL;
 	}
 
@@ -358,7 +358,7 @@ int tda18271_lookup_map(enum tda18271_map_type map_type, u32 *freq, u8 *val)
 	}
 	*val = map[i].val;
 
-	dbg_map("%s: 0x%02x\n", map_name, *val);
+	tda_map("%s: 0x%02x\n", map_name, *val);
 
 	return 0;
 }
