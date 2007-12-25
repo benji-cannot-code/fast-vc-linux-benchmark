d9392605368f851e9b5c7a137436b831ee218228
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2007-12-25 18:13:33 +0200

    UBI: use separate mutex for volumes checking
    Introduce a separate mutex which serializes volumes checking,
    because we cammot really use volumes_mutex - it cases reverse
    locking problems with mtd_tbl_mutex when gluebi is used -
    thanks to lockdep.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -393,6 +393,7 @@ struct ubi_device {
 	void *peb_buf1;
 	void *peb_buf2;
 	struct mutex buf_mutex;
+	struct mutex ckvol_mutex;
 #ifdef CONFIG_MTD_UBI_DEBUG
 	void *dbg_peb_buf;
 	struct mutex dbg_buf_mutex;
