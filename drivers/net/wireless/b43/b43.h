614d58f861da8a1360efa2669d70204ec663ffa2
Michael Buesch <mb@bu3sch.de>
2007-12-26 17:47:10 +0100

    b43: Fix upload of beacon packets to the hardware
    This fixes uploading of the beacon data and writing of the
    TIM and DTIM offsets.
    
    Signed-off-by: Michael Buesch <mb@bu3sch.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/b43.h
+++ drivers/net/wireless/b43/b43.h
@@ -652,6 +652,12 @@ struct b43_wl {
 	u8 nr_devs;
 
 	bool radiotap_enabled;
+
+	/* The beacon we are currently using (AP or IBSS mode).
+	 * This beacon stuff is protected by the irq_lock. */
+	struct sk_buff *current_beacon;
+	bool beacon0_uploaded;
+	bool beacon1_uploaded;
 };
 
 /* Pointers to the firmware data and meta information about it. */
@@ -745,9 +751,6 @@ struct b43_wldev {
 	u16 ktp;		/* Key table pointer */
 	u8 max_nr_keys;
 	struct b43_key key[58];
-
-	/* Cached beacon template while uploading the template. */
-	struct sk_buff *cached_beacon;
 
 	/* Firmware data */
 	struct b43_firmware fw;
