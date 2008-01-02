b8392755e42412239bacecc7d7c761fdf8b71313
Piotr Roszatycki <dexter@debian.org>
2008-01-02 11:16:57 +0100

    USB: add support for 4348:5523 WinChipHead USB->RS 232 adapter
    add support for:
    
      4348:5523 WinChipHead USB->RS 232 adapter with Prolifec PL 2303 chipset
    
    [ mingo@elte.hu: merged it and nursed it upstream ]
    
    Cc: stable <stable@kernel.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/pl2303.h
+++ drivers/usb/serial/pl2303.h
@@ -108,3 +108,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Corega CG-USBRS232R Serial Adapter */
 #define COREGA_VENDOR_ID	0x07aa
 #define COREGA_PRODUCT_ID	0x002a
+
+/* HL HL-340 (ID: 4348:5523) */
+#define HL340_VENDOR_ID		0x4348
+#define HL340_PRODUCT_ID	0x5523
+
