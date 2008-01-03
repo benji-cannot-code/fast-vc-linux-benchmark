5a413115b7fcffeba4281a305b2bd248c99a94d1
Michael Krufky <mkrufky@linuxtv.org>
2008-01-03 01:40:47 -0300

    V4L/DVB (6964): tda18271: document debug level and configuration parameters
    Document debug level module options and
    tda18271_config attach-time parameters.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda18271-fe.c
+++ drivers/media/dvb/frontends/tda18271-fe.c
@@ -25,7 +25,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 int tda18271_debug;
 module_param_named(debug, tda18271_debug, int, 0644);
-MODULE_PARM_DESC(debug, "set debug level (info=1, map=2, reg=4 (or-able))");
+MODULE_PARM_DESC(debug, "set debug level "
+		 "(info=1, map=2, reg=4, adv=8 (or-able))");
 
 /*---------------------------------------------------------------------*/
 
