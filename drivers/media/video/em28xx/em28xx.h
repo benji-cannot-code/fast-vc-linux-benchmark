69345a5d18f754ea83161d871ae92de97ff7b1cf
Mauro Carvalho Chehab <mchehab@infradead.org>
2008-01-05 17:22:01 -0300

    V4L/DVB (6956): Add Radio support for em28xx
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/em28xx/em28xx.h
+++ drivers/media/video/em28xx/em28xx.h
@@ -192,6 +192,7 @@ struct em28xx_board {
 	enum em28xx_decoder decoder;
 
 	struct em28xx_input       input[MAX_EM28XX_INPUT];
+	struct em28xx_input	  radio;
 };
 
 struct em28xx_eeprom {
@@ -308,6 +309,7 @@ struct em28xx {
 	struct list_head inqueue, outqueue;
 	wait_queue_head_t open, wait_frame, wait_stream;
 	struct video_device *vbi_dev;
+	struct video_device *radio_dev;
 
 	unsigned char eedata[256];
 
