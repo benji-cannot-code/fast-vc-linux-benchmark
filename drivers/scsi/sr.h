c2f0ce395af0a8bf47404959418867201b1a24e7
James Bottomley <James.Bottomley@HansenPartnership.com>
2008-01-05 10:39:51 -0600

    [SCSI] sr: update to follow tray status correctly
    Based on an original patch from: David Martin <tasio@tasio.net>
    
    When trying to get the drive status via ioctl CDROM_DRIVE_STATUS, with
    no disk it gives CDS_TRAY_OPEN even if the tray is closed.
    
    ioctl works as expected with ide-cd driver.
    
    Gentoo bug report: http://bugs.gentoo.org/show_bug.cgi?id=196879
    
    Cc: Maarten Bressers <mbres@gentoo.org>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/sr.h
+++ drivers/scsi/sr.h
@@ -21,6 +21,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/genhd.h>
 #include <linux/kref.h>
 
+#define MAX_RETRIES	3
+#define SR_TIMEOUT	(30 * HZ)
+
 struct scsi_device;
 
 /* The CDROM is fairly slow, so we need a little extra time */
