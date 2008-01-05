398dba0e7d875aec2321e2b31d97ee0dd5c69014
Steven Toth <stoth@hauppauge.com>
2008-01-05 17:08:05 -0300

    V4L/DVB (7045): xc5000: Small amount of cleanup and commenting
    xc5000: Small amount of cleanup and commenting, just for clarification.
    
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/xc5000.c
+++ drivers/media/dvb/frontends/xc5000.c
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/module.h>
 #include <linux/moduleparam.h>
-#include <linux/videodev2.h>
+#include <linux/videodev.h>
 #include <linux/delay.h>
 #include <linux/dvb/frontend.h>
 #include <linux/i2c.h>
@@ -213,7 +213,7 @@ static void xc5000_TunerReset(struct dvb_frontend *fe)
 	dprintk(1, "%s()\n", __FUNCTION__);
 
 	if (priv->cfg->tuner_callback) {
-		ret = priv->cfg->tuner_callback(priv->cfg->video_dev,
+		ret = priv->cfg->tuner_callback(priv->cfg->priv,
 						XC5000_TUNER_RESET, 0);
 		if (ret)
 			printk(KERN_ERR "xc5000: reset failed\n");
--- drivers/media/video/cx23885/cx23885-cards.c
+++ drivers/media/video/cx23885/cx23885-cards.c
@@ -248,9 +248,9 @@ static void hauppauge_eeprom(struct cx23885_dev *dev, u8 *eeprom_data)
 /* Tuner callback function for cx23885 boards. Currently only needed
  * for HVR1500Q, which has an xc5000 tuner.
  */
-int cx23885_tuner_callback(void *i2c_bus, int command, int arg)
+int cx23885_tuner_callback(void *priv, int command, int arg)
 {
-	struct cx23885_i2c *bus = i2c_bus;
+	struct cx23885_i2c *bus = priv;
 	struct cx23885_dev *dev = bus->dev;
 
 	switch(dev->board) {
@@ -272,7 +272,7 @@ int cx23885_tuner_callback(void *i2c_bus, int command, int arg)
 
 	return 0; /* Should never be here */
 }
-EXPORT_SYMBOL(cx23885_tuner_callback);
+
 void cx23885_gpio_setup(struct cx23885_dev *dev)
 {
 	switch(dev->board) {
