49c6864a727e3d40284b26525059e7dc8874c1f1
David Brownell <dbrownell@users.sourceforge.net>
2008-01-05 13:21:43 -0800

    USB: at91_udc uses generic GPIO calls; minor cleanup
    Various small at91_udc cleanups:
    
     - Use generic GPIO calls, not older platform-specific ones
     - Use gpio_request()/gpio_free()
     - Use VERBOSE_DEBUG convention, not older VERBOSE
     - Fix sparse complaint about parameter type (changed to gfp_t)
     - Add missing newline to some rarely-seen debug messages
     - Fix some old cleanup bugs on probe() fault paths
    
    Also add a mechanism whereby rm9200 gpios can drive the D+ pullup
    through an inverting transistor, based on a patch from Steve Birtles.
    Most UDC drivers supporting a GPIO based pullup should probably have
    such an option, but testing it requries such a board in hand!
    
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Cc: Steve Birtles <arm_kernel_development@micromark.net.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/asm-arm/arch-at91/board.h
+++ include/asm-arm/arch-at91/board.h
@@ -41,7 +41,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  /* USB Device */
 struct at91_udc_data {
 	u8	vbus_pin;		/* high == host powering us */
-	u8	pullup_pin;		/* high == D+ pulled up */
+	u8	pullup_pin;		/* active == D+ pulled up */
+	u8	pullup_active_low;	/* true == pullup_pin is active low */
 };
 extern void __init at91_add_device_udc(struct at91_udc_data *data);
 
