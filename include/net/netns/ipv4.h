403917759242524cbd3244c2d5d72b87b7093e6a
Eric Dumazet <dada1@cosmosbay.com>
2008-01-05 23:08:49 -0800

    [NETNS]: Should build with CONFIG_SYSCTL=n
    Previous NETNS patches broke CONFIG_SYSCTL=n case
    
    Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -9,7 +9,9 @@ struct ctl_table_header;
 struct ipv4_devconf;
 
 struct netns_ipv4 {
+#ifdef CONFIG_SYSCTL
 	struct ctl_table_header	*forw_hdr;
+#endif
 	struct ipv4_devconf	*devconf_all;
 	struct ipv4_devconf	*devconf_dflt;
 };
