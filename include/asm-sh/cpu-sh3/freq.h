3e72856d47efc8c4e7b9bb4cbd0291981054caeb
Andrew Murray <amurray@mpc-data.co.uk>
2008-01-08 09:56:45 +0900

    sh: sh7712 clock support
    This patch provides specific clock support for the SH7712.
    
    Signed-off-by: Andrew Murray <amurray@mpc-data.co.uk>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- include/asm-sh/cpu-sh3/freq.h
+++ include/asm-sh/cpu-sh3/freq.h
@@ -11,7 +11,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASM_CPU_SH3_FREQ_H
 #define __ASM_CPU_SH3_FREQ_H
 
+#ifdef CONFIG_CPU_SUBTYPE_SH7712
+#define FRQCR			0xA415FF80
+#else
 #define FRQCR			0xffffff80
+#endif
+
 #define MIN_DIVISOR_NR		0
 #define MAX_DIVISOR_NR		4
 
