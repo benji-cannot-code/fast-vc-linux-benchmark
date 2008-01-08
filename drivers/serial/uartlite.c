34b548332af7dccd494bfbc5405636cf38ae1440
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2008-01-09 06:35:05 +1100

    [POWERPC] Xilinx: Update compatible to use values generated by BSP generator.
    Mainly, this involves two changes:
    1) xilinx->xlnx (recognized standard is to use the stock ticker)
    2) In order to have the device tree focus on describing what the
    hardware is as exactly as possible, the compatible strings contain the
    full IP name and IP version.
    
    Signed-off-by: Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
    Acked-by: Peter Korsgaard <jacmet@sunsite.dk>
    Signed-off-by: Grant Likely <grant.likely@secretlab.ca>

--- drivers/serial/uartlite.c
+++ drivers/serial/uartlite.c
@@ -18,10 +18,21 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/tty.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
+#include <linux/init.h>
 #include <asm/io.h>
 #if defined(CONFIG_OF)
+#include <linux/of.h>
 #include <linux/of_device.h>
 #include <linux/of_platform.h>
+
+/* Match table for of_platform binding */
+static struct of_device_id ulite_of_match[] __devinitdata = {
+	{ .compatible = "xlnx,opb-uartlite-1.00.b", },
+	{ .compatible = "xlnx,xps-uartlite-1.00.a", },
+	{}
+};
+MODULE_DEVICE_TABLE(of, ulite_of_match);
+
 #endif
 
 #define ULITE_NAME		"ttyUL"
@@ -276,6 +287,9 @@ static void ulite_release_port(struct uart_port *port)
 
 static int ulite_request_port(struct uart_port *port)
 {
+	pr_debug("ulite console: port=%p; port->mapbase=%x\n",
+		 port, port->mapbase);
+
 	if (!request_mem_region(port->mapbase, ULITE_REGION, "uartlite")) {
 		dev_err(port->dev, "Memory region busy\n");
 		return -EBUSY;
@@ -384,7 +398,7 @@ static inline void __init ulite_console_of_find_device(int id)
 	const unsigned int *of_id;
 	int rc;
 
-	for_each_compatible_node(np, NULL, "xilinx,uartlite") {
+	for_each_matching_node(np, ulite_of_match) {
 		of_id = of_get_property(np, "port-number", NULL);
 		if ((!of_id) || (*of_id != id))
 			continue;
@@ -617,13 +631,6 @@ static int __devexit ulite_of_remove(struct of_device *op)
 {
 	return ulite_release(&op->dev);
 }
-
-/* Match table for of_platform binding */
-static struct of_device_id ulite_of_match[] __devinitdata = {
-	{ .type = "serial", .compatible = "xilinx,uartlite", },
-	{},
-};
-MODULE_DEVICE_TABLE(of, ulite_of_match);
 
 static struct of_platform_driver ulite_of_driver = {
 	.owner = THIS_MODULE,
