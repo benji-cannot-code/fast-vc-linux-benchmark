358776acf036ded79072aab29fdc343f5725f509
Denis V. Lunev <den@openvz.org>
2008-01-10 03:28:55 -0800

    [NETNS]: Provide correct namespace for fibnl netlink socket.
    This patch makes the netlink socket to be per namespace. That allows
    to have each namespace its own socket for routing queries.
    
    Acked-by: Benjamin Thery <benjamin.thery@bull.net>
    Acked-by: Daniel Lezcano <dlezcano@fr.ibm.com>
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -10,6 +10,7 @@ struct ctl_table_header;
 struct ipv4_devconf;
 struct fib_rules_ops;
 struct hlist_head;
+struct sock;
 
 struct netns_ipv4 {
 #ifdef CONFIG_SYSCTL
@@ -21,5 +22,6 @@ struct netns_ipv4 {
 	struct fib_rules_ops	*rules_ops;
 #endif
 	struct hlist_head	*fib_table_hash;
+	struct sock		*fibnl;
 };
 #endif
