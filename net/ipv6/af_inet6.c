13f11e11d0a0b8771f8a116d8aadcc4a6c78a7a6
Daniel Lezcano <dlezcano@fr.ibm.com>
2008-01-10 02:48:33 -0800

    [NETNS][IPV6]: Make a subsystem for af_inet6.
    This patch add a network namespace subsystem for the af_inet6 module.
    It does nothing right now, but one of its purpose is to receive the
    different variables for sysctl in order to initialize them.
    
    When the sysctl variable will be moved to the network namespace
    structure, they will be no longer initialized as global static
    variables, so we must find a place to initialize them. Because the
    sysctl can be disabled, it has no sense to store them in the
    sysctl_net_ipv6 file.
    
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -720,6 +720,21 @@ static void cleanup_ipv6_mibs(void)
 	snmp_mib_free((void **)udplite_stats_in6);
 }
 
+static int inet6_net_init(struct net *net)
+{
+	return 0;
+}
+
+static void inet6_net_exit(struct net *net)
+{
+	return;
+}
+
+static struct pernet_operations inet6_net_ops = {
+	.init = inet6_net_init,
+	.exit = inet6_net_exit,
+};
+
 static int __init inet6_init(void)
 {
 	struct sk_buff *dummy_skb;
@@ -782,6 +797,10 @@ static int __init inet6_init(void)
 	 *	in a host availiable by both INET and INET6 APIs and
 	 *	able to communicate via both network protocols.
 	 */
+
+	err = register_pernet_subsys(&inet6_net_ops);
+	if (err)
+		goto register_pernet_fail;
 
 #ifdef CONFIG_SYSCTL
 	err = ipv6_sysctl_register();
@@ -902,6 +921,8 @@ icmp_fail:
 	ipv6_sysctl_unregister();
 sysctl_fail:
 #endif
+	unregister_pernet_subsys(&inet6_net_ops);
+register_pernet_fail:
 	cleanup_ipv6_mibs();
 out_unregister_sock:
 	sock_unregister(PF_INET6);
@@ -957,6 +978,7 @@ static void __exit inet6_exit(void)
 #ifdef CONFIG_SYSCTL
 	ipv6_sysctl_unregister();
 #endif
+	unregister_pernet_subsys(&inet6_net_ops);
 	cleanup_ipv6_mibs();
 	proto_unregister(&rawv6_prot);
 	proto_unregister(&udplitev6_prot);
