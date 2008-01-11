33ee59e7c79e075123c5bc8f4ca3c426e62777c5
Chuck Lever <chuck.lever@oracle.com>
2008-01-11 17:09:52 -0500

    NFS: Invoke nlmclnt_init during NFS mount processing
    Cache an appropriate nlm_host structure in the NFS client's mount point
    metadata for later use.
    
    Note that there is no need to set NFS_MOUNT_NONLM in the error case -- if
    nfs_start_lockd() returns a non-zero value, its callers ensure that the
    mount request fails outright.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- include/linux/nfs_fs_sb.h
+++ include/linux/nfs_fs_sb.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/atomic.h>
 
 struct nfs_iostats;
+struct nlm_host;
 
 /*
  * The nfs_client identifies our client state to the server.
@@ -81,6 +82,7 @@ struct nfs_server {
 	struct list_head	master_link;	/* link in master servers list */
 	struct rpc_clnt *	client;		/* RPC client handle */
 	struct rpc_clnt *	client_acl;	/* ACL RPC client handle */
+	struct nlm_host		*nlm_host;	/* NLM client handle */
 	struct nfs_iostats *	io_stats;	/* I/O statistics */
 	struct backing_dev_info	backing_dev_info;
 	atomic_long_t		writeback;	/* number of writeback pages */
