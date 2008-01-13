c0a65e8a52b240d0cb5349c1b2b9f15da335505f
Ivo van Doorn <ivdoorn@gmail.com>
2008-01-13 14:16:47 +0100

    mac80211: Initialize vif pointer
    Before calling update_beacon() mac80211 must
    initialize the control.vif pointer so it can
    be used by the driver to determine which
    interface is trying to send the beacon.
    
    v2: ieee80211_beacon_get() should also initialize the
    vif pointer since it can be called by mac80211 internally
    before calling config_interface().
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Acked-by: Johannes Berg <johannes@sipsolutions.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/mac80211/ieee80211_sta.c
+++ net/mac80211/ieee80211_sta.c
@@ -2788,6 +2788,7 @@ static int ieee80211_sta_join_ibss(struct net_device *dev,
 			       "for IBSS beacon\n", dev->name);
 			break;
 		}
+		control.vif = &sdata->vif;
 		control.tx_rate =
 			(sdata->bss_conf.use_short_preamble &&
 			(ratesel.rate->flags & IEEE80211_RATE_PREAMBLE2)) ?
