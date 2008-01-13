4b1010f6b03cb81c27cb1bb2c5c6742f7b0e86d8
Helge Deller <deller@gmx.de>
2008-01-13 23:01:13 +0000

    backlight: Avoid unecessary driver callbacks
    Avoid driver callbacks when the brightness hasn't changed since
    they're not necessary.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Richard Purdie <rpurdie@rpsys.net>

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -95,8 +95,10 @@ static ssize_t backlight_store_power(struct device *dev,
 	mutex_lock(&bd->ops_lock);
 	if (bd->ops) {
 		pr_debug("backlight: set power to %d\n", power);
-		bd->props.power = power;
-		backlight_update_status(bd);
+		if (bd->props.power != power) {
+			bd->props.power = power;
+			backlight_update_status(bd);
+		}
 		rc = count;
 	}
 	mutex_unlock(&bd->ops_lock);
@@ -133,8 +135,10 @@ static ssize_t backlight_store_brightness(struct device *dev,
 		else {
 			pr_debug("backlight: set brightness to %d\n",
 				 brightness);
-			bd->props.brightness = brightness;
-			backlight_update_status(bd);
+			if (bd->props.brightness != brightness) {
+				bd->props.brightness = brightness;
+				backlight_update_status(bd);
+			}
 			rc = count;
 		}
 	}
