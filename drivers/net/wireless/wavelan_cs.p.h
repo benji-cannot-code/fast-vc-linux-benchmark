999e0a55b146d6f79e788847508ced6b96c34d73
Helge Deller <deller@gmx.de>
2008-01-13 15:16:34 +0100

    WAVELAN - compile-time check for struct sizes
    Convert optional struct size checks to non-optional compile-time checks.
    Furthermore BUILD_BUG_ON() which will be optimized away by the compiler.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/wavelan_cs.p.h
+++ drivers/net/wireless/wavelan_cs.p.h
@@ -460,7 +460,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #undef WAVELAN_ROAMING_EXT	/* Enable roaming wireless extensions */
 #undef SET_PSA_CRC		/* Set the CRC in PSA (slower) */
 #define USE_PSA_CONFIG		/* Use info from the PSA */
-#undef STRUCT_CHECK		/* Verify padding of structures */
 #undef EEPROM_IS_PROTECTED	/* Doesn't seem to be necessary */
 #define MULTICAST_AVOID		/* Avoid extra multicast (I'm sceptical) */
 #undef SET_MAC_ADDRESS		/* Experimental */
