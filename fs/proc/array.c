097c6d6730fe668b0f4163a6003757c07d96e429
Oleg Nesterov <oleg@tv-sign.ru>
2008-01-15 00:02:37 +0300

    fix the "remove task_ppid_nr_ns" commit
    Commit 84427eaef1fb91704c7112bdb598c810003b99f3 (remove task_ppid_nr_ns)
    moved the task_tgid_nr_ns(task->real_parent) outside of lock_task_sighand().
    This is wrong, ->real_parent could be freed/reused.
    
    Both ->parent/real_parent point to nothing after __exit_signal() because
    we remove the child from ->children list, and thus the child can't be
    reparented when its parent exits.
    
    rcu_read_lock() protects ->parent/real_parent, but _only_ if we know it was
    valid before we take rcu lock.
    
    Revert this part of the patch.
    
    Signed-off-by: Oleg Nesterov <oleg@tv-sign.ru>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -427,7 +427,6 @@ static int do_task_stat(struct task_struct *task, char *buffer, int whole)
 	cgtime = gtime = cputime_zero;
 
 	rcu_read_lock();
-	ppid = task_tgid_nr_ns(task->real_parent, ns);
 	if (lock_task_sighand(task, &flags)) {
 		struct signal_struct *sig = task->signal;
 
@@ -466,6 +465,7 @@ static int do_task_stat(struct task_struct *task, char *buffer, int whole)
 		}
 
 		sid = task_session_nr_ns(task, ns);
+		ppid = task_tgid_nr_ns(task->real_parent, ns);
 		pgid = task_pgrp_nr_ns(task, ns);
 
 		unlock_task_sighand(task, &flags);
