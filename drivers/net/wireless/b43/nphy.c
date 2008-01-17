dbafb1c2b1410223366f4e9787eb7347a2c44441
John W. Linville <linville@tuxdriver.com>
2008-01-17 16:57:10 -0500

    b43/nphy.c: include headers to avoid build breakage on some platforms
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/b43/nphy.c
+++ drivers/net/wireless/b43/nphy.c
@@ -23,6 +23,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 */
 
+#include <linux/delay.h>
+#include <linux/types.h>
+
 #include "b43.h"
 #include "nphy.h"
 #include "tables_nphy.h"
