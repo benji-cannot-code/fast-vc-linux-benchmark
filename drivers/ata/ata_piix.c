8c683b788d1b50507c3b063036955368b8e9fdfa
Peter Schwenke <peter@bluetoad.com.au>
2008-01-17 23:08:55 +1000

    ata_piix: Add Toshiba Satellite R20 and Tecra M6 to broken suspend list.
    Add Toshiba Satellite R20 and Tecra M6 to broken suspend list.  Matt
    Piermarini reported and provided the M6 patch.  This is from OSDL bug 7780.
    
    Signed-off-by: Peter Schwenke <peter@bluetoad.com.au>
    Cc: Matt Piermarini <mattpiermarini@yahoo.com>
    Acked-by: Tejun Heo <htejun@gmail.com>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/ata/ata_piix.c
+++ drivers/ata/ata_piix.c
@@ -1035,6 +1035,13 @@ static int piix_broken_suspend(void)
 			},
 		},
 		{
+			.ident = "TECRA M6",
+			.matches = {
+				DMI_MATCH(DMI_SYS_VENDOR, "TOSHIBA"),
+				DMI_MATCH(DMI_PRODUCT_NAME, "TECRA M6"),
+			},
+		},
+		{
 			.ident = "TECRA M7",
 			.matches = {
 				DMI_MATCH(DMI_SYS_VENDOR, "TOSHIBA"),
@@ -1046,6 +1053,13 @@ static int piix_broken_suspend(void)
 			.matches = {
 				DMI_MATCH(DMI_SYS_VENDOR, "TOSHIBA"),
 				DMI_MATCH(DMI_PRODUCT_NAME, "TECRA A8"),
+			},
+		},
+		{
+			.ident = "Satellite R20",
+			.matches = {
+				DMI_MATCH(DMI_SYS_VENDOR, "TOSHIBA"),
+				DMI_MATCH(DMI_PRODUCT_NAME, "Satellite R20"),
 			},
 		},
 		{
