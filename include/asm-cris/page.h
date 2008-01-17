5101eb17314dd666bb85b884fcdbb04c72be91d6
Jesper Nilsson <jesper.nilsson@axis.com>
2008-01-17 15:21:11 -0800

    CRIS v10: vmlinux.lds.S: ix kernel oops on boot and use common defines
    - Move alignment to page size of init data outside ifdef for BLK_DEV_INITRD.
      The reservation up to page size of memory after init data was previously
      not done if BLK_DEV_INITRD was undefined.
      This caused a kernel oops when init memory pages were freed after startup,
      data placed in the same page as the last init memory would also be freed
      and reused, with disastrous results.
    
    - Use macros for initcalls and .text sections.
    
    - Replace hardcoded page size constant with PAGE_SIZE define.
    
    - Change include/asm-cris/page.h to use the _AC macro to instead
      of testing __ASSEMBLY__.
    
    Signed-off-by: Jesper Nilsson <jesper.nilsson@axis.com>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Mikael Starvik <mikael.starvik@axis.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-cris/page.h
+++ include/asm-cris/page.h
@@ -5,14 +5,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef __KERNEL__
 
 #include <asm/arch/page.h>
+#include <linux/const.h>
 
 /* PAGE_SHIFT determines the page size */
 #define PAGE_SHIFT	13
-#ifndef __ASSEMBLY__
-#define PAGE_SIZE	(1UL << PAGE_SHIFT)
-#else
-#define PAGE_SIZE	(1 << PAGE_SHIFT)
-#endif
+#define PAGE_SIZE	(_AC(1, UL) << PAGE_SHIFT)
 #define PAGE_MASK	(~(PAGE_SIZE-1))
 
 #define clear_page(page)        memset((void *)(page), 0, PAGE_SIZE)
