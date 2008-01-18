50b0454a3f360680aacd77a060b869219f12a439
Rami Rosen <ramirose@gmail.com>
2008-01-18 05:38:31 -0800

    [BRIDGE]: Remove unused include of a header file in ebtables.c
    In net/bridge/netfilter/ebtables.c,
    - remove unused include of a header file (linux/tty.h) and remove the
      corresponding comment above it.
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/netfilter/ebtables.c
+++ net/bridge/netfilter/ebtables.c
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  2 of the License, or (at your option) any later version.
  */
 
-/* used for print_string */
-#include <linux/tty.h>
 
 #include <linux/kmod.h>
 #include <linux/module.h>
