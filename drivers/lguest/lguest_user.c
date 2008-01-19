301135a9d81fac5ac31292b6bd4bf448f835db0f
Glauber de Oliveira Costa <gcosta@redhat.com>
2008-01-18 23:59:07 -0200

    lguest: Use explicit includes rateher than indirect
    explicitly use ktime.h include
    explicitly use hrtimer.h include
    explicitly use sched.h include
    
    This patch adds headers explicitly to lguest sources file,
    to avoid depending on them being included somewhere else.
    
    Signed-off-by: Glauber de Oliveira Costa <gcosta@redhat.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- drivers/lguest/lguest_user.c
+++ drivers/lguest/lguest_user.c
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/uaccess.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
+#include <linux/sched.h>
 #include "lg.h"
 
 /*L:055 When something happens, the Waker process needs a way to stop the
