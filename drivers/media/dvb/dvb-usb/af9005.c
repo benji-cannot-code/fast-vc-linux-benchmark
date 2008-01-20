4e2b204062101b7c24d75bf624786990600d113c
Luca Olivetti <luca@ventoso.org>
2008-01-20 17:56:43 -0300

    V4L/DVB (7054): ansonic branded dvb-t usb stick support in the af9005 driver
    Marcos Melero (marcosmelero at gmail.com) reported he could make
    his dvb-t usb stick work with the af9005 driver by changing the device
    ids (10b9:6000).
    
    The stick is branded "Ansonic" (one of the brands of a spanish chain of
    supermarkets) with no other identification of the model.
    Since neither Marcos nor me know the OEM for the stick, in the attached
    patch I used Ansonic for the ids/description.
    
    Signed-off-by: Luca Olivetti <luca@ventoso.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/dvb-usb/af9005.c
+++ drivers/media/dvb/dvb-usb/af9005.c
@@ -1027,6 +1027,7 @@ static int af9005_usb_probe(struct usb_interface *intf,
 static struct usb_device_id af9005_usb_table[] = {
 	{USB_DEVICE(USB_VID_AFATECH, USB_PID_AFATECH_AF9005)},
 	{USB_DEVICE(USB_VID_TERRATEC, USB_PID_TERRATEC_CINERGY_T_USB_XE)},
+	{USB_DEVICE(USB_VID_ANSONIC, USB_PID_ANSONIC_DVBT_USB)},
 	{0},
 };
 
@@ -1076,7 +1077,7 @@ static struct dvb_usb_device_properties af9005_properties = {
 	.rc_key_map_size = 0,
 	.rc_query = af9005_rc_query,
 
-	.num_device_descs = 2,
+	.num_device_descs = 3,
 	.devices = {
 		    {.name = "Afatech DVB-T USB1.1 stick",
 		     .cold_ids = {&af9005_usb_table[0], NULL},
@@ -1084,6 +1085,10 @@ static struct dvb_usb_device_properties af9005_properties = {
 		     },
 		    {.name = "TerraTec Cinergy T USB XE",
 		     .cold_ids = {&af9005_usb_table[1], NULL},
+		     .warm_ids = {NULL},
+		     },
+		    {.name = "Ansonic DVB-T USB1.1 stick",
+		     .cold_ids = {&af9005_usb_table[2], NULL},
 		     .warm_ids = {NULL},
 		     },
 		    {NULL},
