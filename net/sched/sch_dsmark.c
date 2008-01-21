6252042f44fd6637845cd6dd9386d58243039f17
David S. Miller <davem@davemloft.net>
2008-01-21 02:21:45 -0800

    [PKT_SCHED] dsmark: Use hweight32() instead of convoluted loop.
    Based upon a patch by Stephen Hemminger and suggestions
    from Patrick McHardy.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/sched/sch_dsmark.c
+++ net/sched/sch_dsmark.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/errno.h>
 #include <linux/skbuff.h>
 #include <linux/rtnetlink.h>
+#include <linux/bitops.h>
 #include <net/pkt_sched.h>
 #include <net/dsfield.h>
 #include <net/inet_ecn.h>
@@ -43,17 +44,6 @@ struct dsmark_qdisc_data {
 	u32			default_index;	/* index range is 0...0xffff */
 	int			set_tc_index;
 };
-
-static inline int dsmark_valid_indices(u16 indices)
-{
-	while (indices != 1) {
-		if (indices & 1)
-			return 0;
-		indices >>= 1;
-	}
-
-	return 1;
-}
 
 static inline int dsmark_valid_index(struct dsmark_qdisc_data *p, u16 index)
 {
@@ -349,7 +339,8 @@ static int dsmark_init(struct Qdisc *sch, struct rtattr *opt)
 		goto errout;
 
 	indices = RTA_GET_U16(tb[TCA_DSMARK_INDICES-1]);
-	if (!indices || !dsmark_valid_indices(indices))
+
+	if (hweight32(indices) != 1)
 		goto errout;
 
 	if (tb[TCA_DSMARK_DEFAULT_INDEX-1])
