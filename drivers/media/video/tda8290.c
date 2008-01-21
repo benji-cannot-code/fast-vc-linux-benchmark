51928643e972ab9b3607ff020c805760507bf661
Michael Krufky <mkrufky@linuxtv.org>
2008-01-21 10:55:37 -0300

    V4L/DVB (7055): make tuner names consistent with standard cases
    Change TDA9887 to tda9887.
    
    TDA8290 shall reflect the actual hardware present in its name,
      ie:
    
    tda8295+18271
    tda8290+8275a
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/tda8290.c
+++ drivers/media/video/tda8290.c
@@ -646,9 +646,6 @@ static int tda8295_probe(struct tuner_i2c_props *i2c_props)
 }
 
 static struct analog_demod_ops tda8290_ops = {
-	.info		= {
-		.name	= "TDA8290",
-	},
 	.set_params     = tda8290_set_params,
 	.has_signal     = tda8290_has_signal,
 	.standby        = tda8290_standby,
@@ -657,9 +654,6 @@ static struct analog_demod_ops tda8290_ops = {
 };
 
 static struct analog_demod_ops tda8295_ops = {
-	.info		= {
-		.name	= "TDA8295",
-	},
 	.set_params     = tda8295_set_params,
 	.has_signal     = tda8295_has_signal,
 	.standby        = tda8295_standby,
@@ -731,6 +725,8 @@ struct dvb_frontend *tda829x_attach(struct dvb_frontend *fe,
 		goto fail;
 	}
 	tuner_info("type set to %s\n", name);
+
+	fe->ops.analog_ops.info.name = name;
 
 	if (priv->ver & TDA8290) {
 		tda8290_init_tuner(fe);
