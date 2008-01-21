12ed5718da0defde987ae2abb1f29209d70eb647
Francisco Alecrim <francisco.alecrim@indt.org.br>
2008-01-21 01:05:23 -0500

    Input: remove duplicated headers in drivers/char/keyboard.c
    drivers/char/keyboard.c: linux/consolemap.h is included more than once.
    
    Signed-off-by: Francisco Alecrim <francisco.alecrim@indt.org.br>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/char/keyboard.c
+++ drivers/char/keyboard.c
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kbd_kern.h>
 #include <linux/kbd_diacr.h>
 #include <linux/vt_kern.h>
-#include <linux/consolemap.h>
 #include <linux/sysrq.h>
 #include <linux/input.h>
 #include <linux/reboot.h>
