c6eee752007c391bcdacea6161e13f6f3ab1d653
Pavel Emelyanov <xemul@openvz.org>
2008-01-22 06:10:13 -0800

    [NETNS][FRAGS]: Make thresholds work in namespaces.
    This is the same as with the timeout variable.
    
    Currently, after exceeding the high threshold _all_
    the fragments are evicted, but it will be fixed in
    later patch.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/inet_frag.h
+++ include/net/inet_frag.h
@@ -8,6 +8,8 @@ struct netns_frags {
 
 	/* sysctls */
 	int			timeout;
+	int			high_thresh;
+	int			low_thresh;
 };
 
 struct inet_frag_queue {
@@ -31,8 +33,6 @@ struct inet_frag_queue {
 #define INETFRAGS_HASHSZ		64
 
 struct inet_frags_ctl {
-	int high_thresh;
-	int low_thresh;
 	int secret_interval;
 };
 
