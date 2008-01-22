3d84d580c6c584f9b09fa012a82d5a301cbb4473
Wade Farnsworth <wfarnsworth@mvista.com>
2008-01-22 13:17:45 -0700

    [POWERPC] 86xx: MPC8641 HPCN - call of_platform_bus_probe()
    Call of_platform_bus_probe() on the MPC8641 HPCN, similar to what is
    done for other platforms.
    
    Signed-off-by: Wade Farnsworth <wfarnsworth@mvista.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/86xx/mpc86xx_hpcn.c
+++ arch/powerpc/platforms/86xx/mpc86xx_hpcn.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kdev_t.h>
 #include <linux/delay.h>
 #include <linux/seq_file.h>
+#include <linux/of_platform.h>
 
 #include <asm/system.h>
 #include <asm/time.h>
@@ -212,6 +213,19 @@ mpc86xx_time_init(void)
 
 	return 0;
 }
+
+static __initdata struct of_device_id of_bus_ids[] = {
+	{ .compatible = "simple-bus", },
+	{},
+};
+
+static int __init declare_of_platform_devices(void)
+{
+	of_platform_bus_probe(NULL, of_bus_ids, NULL);
+
+	return 0;
+}
+machine_device_initcall(mpc86xx_hpcn, declare_of_platform_devices);
 
 define_machine(mpc86xx_hpcn) {
 	.name			= "MPC86xx HPCN",
