fa8f0267dcab125b501db55f4bbdfe5f4c1a64c9
Andrew Victor <linux@maxim.org.za>
2008-01-23 09:27:06 +0100

    [ARM] 4758/1: [AT91] LEDs
    Move the LED initialization code out of the various *_devices.c files,
    and into leds.c.
    Also add support for NEW_LEDs.
    
    Patch from David Brownell.
    
    Signed-off-by: Andrew Victor <linux@maxim.org.za>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-at91/board.h
+++ include/asm-arm/arch-at91/board.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mtd/partitions.h>
 #include <linux/device.h>
 #include <linux/i2c.h>
+#include <linux/leds.h>
 #include <linux/spi/spi.h>
 
  /* USB Device */
@@ -158,9 +159,8 @@ extern void __init at91_add_device_ac97(struct atmel_ac97_data *data);
 extern void __init at91_add_device_isi(void);
 
  /* LEDs */
-extern u8 at91_leds_cpu;
-extern u8 at91_leds_timer;
 extern void __init at91_init_leds(u8 cpu_led, u8 timer_led);
+extern void __init at91_gpio_leds(struct gpio_led *leds, int nr);
 
 /* FIXME: this needs a better location, but gets stuff building again */
 extern int at91_suspend_entering_slow_clock(void);
