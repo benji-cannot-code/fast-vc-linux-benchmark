c0577b6cb85558d6dcdafec5d52b10de2570e2c5
Kumar Gala <galak@kernel.crashing.org>
2008-01-23 06:12:06 -0600

    [POWERPC] Move RapidIO support code from arch/ppc
    Do just enough to move the RapidIO support code for 85xx over from arch/ppc
    into arch/powerpc and make it still build.
    
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/ppc/platforms/85xx/mpc85xx_ads_common.c
+++ arch/ppc/platforms/85xx/mpc85xx_ads_common.c
@@ -43,8 +43,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <mm/mmu_decl.h>
 
-#include <syslib/ppc85xx_rio.h>
-
 #include <platforms/85xx/mpc85xx_ads_common.h>
 
 #ifndef CONFIG_PCI
@@ -191,6 +189,7 @@ mpc85xx_exclude_device(u_char bus, u_char devfn)
 #endif /* CONFIG_PCI */
 
 #ifdef CONFIG_RAPIDIO
+extern void mpc85xx_rio_setup(int law_start, int law_size);
 void platform_rio_init(void)
 {
 	/* 512MB RIO LAW at 0xc0000000 */
