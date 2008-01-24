8f656ad903fff02112a49abe094004c7973aeb57
Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
2008-01-24 19:52:48 +0300

    [MIPS] Malta, Atlas, Sead: remove an extern from .c files
    This patch moves the "extern" declaration for the function
    mips_reboot_setup() from the board setup .c files to the
    header file include/asm-mips/mips-boards/generic.h.
    
    This fixes a warning produced by the checkpatch.pl script.
    
    No functional changes introduced.
    
    This was compile-tested by building the kernel for all
    three boards affected by this change. All builds finished
    successfully.
    
    Signed-off-by: Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/atlas/atlas_setup.c
+++ arch/mips/mips-boards/atlas/atlas_setup.c
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/time.h>
 #include <asm/traps.h>
 
-extern void mips_reboot_setup(void);
-
 #ifdef CONFIG_KGDB
 extern void kgdb_config(void);
 #endif
--- arch/mips/mips-boards/malta/malta_setup.c
+++ arch/mips/mips-boards/malta/malta_setup.c
@@ -36,8 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #endif
 
-extern void mips_reboot_setup(void);
-
 #ifdef CONFIG_KGDB
 extern void kgdb_config(void);
 #endif
