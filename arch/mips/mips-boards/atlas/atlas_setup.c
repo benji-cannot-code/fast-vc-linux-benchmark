cee7142282b5c638556d5aae36f1431106f27610
Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
2008-01-24 19:52:53 +0300

    [MIPS] Malta, Atlas: move an extern function declaration to the header file
    This was compile-tested using default configs for the boards
    affected by this change.
    
    This patch does not introduce any functional changes.
    
    Signed-off-by: Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/atlas/atlas_setup.c
+++ arch/mips/mips-boards/atlas/atlas_setup.c
@@ -35,10 +35,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/time.h>
 #include <asm/traps.h>
 
-#ifdef CONFIG_KGDB
-extern void kgdb_config(void);
-#endif
-
 static void __init serial_init(void);
 
 const char *get_system_type(void)
--- arch/mips/mips-boards/malta/malta_setup.c
+++ arch/mips/mips-boards/malta/malta_setup.c
@@ -36,10 +36,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/console.h>
 #endif
 
-#ifdef CONFIG_KGDB
-extern void kgdb_config(void);
-#endif
-
 struct resource standard_io_resources[] = {
 	{
 		.name = "dma1",
