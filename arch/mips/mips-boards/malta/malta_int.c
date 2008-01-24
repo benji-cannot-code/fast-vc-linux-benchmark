51443df5101156e13cccecede8ef99cbe3334c22
Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
2008-01-24 19:52:49 +0300

    [MIPS] Malta: include <linux/cpu.h> instead of <asm/cpu.h>
    The checkpatch.pl script reported a few warnings about header files.
    This patch fixes these warnings.
    
    Compile-tested using the default Malta config.
    
    No functional changes introduced.
    
    Signed-off-by: Dmitri Vorobiev <dmitri.vorobiev@gmail.com>
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>

--- arch/mips/mips-boards/malta/malta_int.c
+++ arch/mips/mips-boards/malta/malta_int.c
@@ -27,13 +27,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
+#include <linux/io.h>
 #include <linux/kernel_stat.h>
 #include <linux/kernel.h>
 #include <linux/random.h>
 
 #include <asm/i8259.h>
 #include <asm/irq_cpu.h>
-#include <asm/io.h>
 #include <asm/irq_regs.h>
 #include <asm/mips-boards/malta.h>
 #include <asm/mips-boards/maltaint.h>
