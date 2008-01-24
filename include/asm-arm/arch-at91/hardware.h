73e11e711cab829ed8205fdbc1e53a4281a41e6f
Andrew Victor <linux@maxim.org.za>
2008-01-24 15:10:39 +0100

    [ARM] 4764/1: [AT91] AT91CAP9 core support
    Add support for Atmel's AT91CAP9 Customizable Microcontroller family.
      <http://www.atmel.com/products/AT91CAP/Default.asp>
    
    Signed-off-by: Stelian Pop <stelian@popies.net>
    Signed-off-by: Andrew Victor <linux@maxim.org.za>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-at91/hardware.h
+++ include/asm-arm/arch-at91/hardware.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/arch/at91sam9263.h>
 #elif defined(CONFIG_ARCH_AT91SAM9RL)
 #include <asm/arch/at91sam9rl.h>
+#elif defined(CONFIG_ARCH_AT91CAP9)
+#include <asm/arch/at91cap9.h>
 #elif defined(CONFIG_ARCH_AT91X40)
 #include <asm/arch/at91x40.h>
 #else
