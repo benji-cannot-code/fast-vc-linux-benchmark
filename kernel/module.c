c8d7065356f1231798614408a4621c771ccb6715
Arjan van de Ven <arjan@linux.intel.com>
2008-01-25 21:08:33 +0100

    debug: track and print last unloaded module in the oops trace
    Based on a suggestion from Andi:
    
     In various cases, the unload of a module may leave some bad state around
     that causes a kernel crash AFTER a module is unloaded; and it's then hard
     to find which module caused that.
    
    This patch tracks the last unloaded module, and prints this as part of the
    module list in the oops trace.
    
    Right now, only the last 1 module is tracked; I expect that this is enough
    for the vast majority of cases where this information matters; if it turns
    out that tracking more is important, we can always extend it to that.
    
    [ mingo@elte.hu: build fix ]
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/module.c
+++ kernel/module.c
@@ -497,6 +497,8 @@ static struct module_attribute modinfo_##field = {                    \
 MODINFO_ATTR(version);
 MODINFO_ATTR(srcversion);
 
+static char last_unloaded_module[MODULE_NAME_LEN+1];
+
 #ifdef CONFIG_MODULE_UNLOAD
 /* Init the unload section of the module. */
 static void module_unload_init(struct module *mod)
@@ -720,6 +722,8 @@ sys_delete_module(const char __user *name_user, unsigned int flags)
 		mod->exit();
 		mutex_lock(&module_mutex);
 	}
+	/* Store the name of the last unloaded module for diagnostic purposes */
+	sprintf(last_unloaded_module, mod->name);
 	free_module(mod);
 
  out:
@@ -2504,6 +2508,8 @@ void print_modules(void)
 	printk("Modules linked in:");
 	list_for_each_entry(mod, &modules, list)
 		printk(" %s%s", mod->name, module_flags(mod, buf));
+	if (last_unloaded_module[0])
+		printk(" [last unloaded: %s]", last_unloaded_module);
 	printk("\n");
 }
 
