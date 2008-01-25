7bf772f96ceee78c753e77184ed9c235050394e4
Greg Kroah-Hartman <gregkh@suse.de>
2008-01-24 21:59:04 -0800

    Kobject: fix coding style issues in kobject c files
    Clean up the kobject.c and kobject_uevent.c files to follow the
    proper coding style rules.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -239,11 +239,12 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		retval = add_uevent_var(env, "HOME=/");
 		if (retval)
 			goto exit;
-		retval = add_uevent_var(env, "PATH=/sbin:/bin:/usr/sbin:/usr/bin");
+		retval = add_uevent_var(env,
+					"PATH=/sbin:/bin:/usr/sbin:/usr/bin");
 		if (retval)
 			goto exit;
 
-		call_usermodehelper (argv[0], argv, env->envp, UMH_WAIT_EXEC);
+		call_usermodehelper(argv[0], argv, env->envp, UMH_WAIT_EXEC);
 	}
 
 exit:
@@ -251,7 +252,6 @@ exit:
 	kfree(env);
 	return retval;
 }
-
 EXPORT_SYMBOL_GPL(kobject_uevent_env);
 
 /**
@@ -267,7 +267,6 @@ int kobject_uevent(struct kobject *kobj, enum kobject_action action)
 {
 	return kobject_uevent_env(kobj, action, NULL);
 }
-
 EXPORT_SYMBOL_GPL(kobject_uevent);
 
 /**
