153089fde90d18571b4a8294ec34a0b67419301c
Roland Dreier <rolandd@cisco.com>
2008-01-25 14:15:42 -0800

    IB/ipath: Fix some sparse warnings about shadowed symbols
    There are a few places in the ipath driver where a variable is
    re-declared within a block where it is already in scope.  Most of these
    extra declarations can simply be removed, since the variable from the
    outer scope is used in a way so that it does not need to keep its
    variable across the block with the re-declaration.
    
    Signed-off-by: Roland Dreier <rolandd@cisco.com>

--- drivers/infiniband/hw/ipath/ipath_qp.c
+++ drivers/infiniband/hw/ipath/ipath_qp.c
@@ -856,8 +856,6 @@ struct ib_qp *ipath_create_qp(struct ib_pd *ibpd,
 	 * See ipath_mmap() for details.
 	 */
 	if (udata && udata->outlen >= sizeof(__u64)) {
-		int err;
-
 		if (!qp->r_rq.wq) {
 			__u64 offset = 0;
 
