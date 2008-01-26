09519cc0b370b77fb97656936aba1da5433f6dec
Joe Perches <joe@perches.com>
2008-01-26 14:11:10 +0100

    [S390] drivers/s390: Add missing "space"
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- drivers/s390/char/vmlogrdr.c
+++ drivers/s390/char/vmlogrdr.c
@@ -684,7 +684,7 @@ static int vmlogrdr_register_driver(void)
 	/* Register with iucv driver */
 	ret = iucv_register(&vmlogrdr_iucv_handler, 1);
 	if (ret) {
-		printk (KERN_ERR "vmlogrdr: failed to register with"
+		printk (KERN_ERR "vmlogrdr: failed to register with "
 			"iucv driver\n");
 		goto out;
 	}
