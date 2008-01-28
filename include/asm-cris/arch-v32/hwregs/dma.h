a28a80180f2f3b9a06b2eeb69f998790fd8026b0
Jesper Nilsson <jesper.nilsson@axis.com>
2008-01-28 17:42:17 +0100

    CRIS v32: Rename variable used in macro for arch-v32/hwregs/dma.h
    The old name "r" would quite often produce warnings when other
    variables with the same name was shadowed. Rename it __x to
    make it more unlikely to happen.

--- include/asm-cris/arch-v32/hwregs/dma.h
+++ include/asm-cris/arch-v32/hwregs/dma.h
@@ -1,6 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
-/* $Id: dma.h,v 1.7 2005/04/24 18:30:58 starvik Exp $
- *
+/*
  * DMA C definitions and help macros
  *
  */
@@ -99,11 +98,11 @@ typedef struct dma_descr_data {
 
 // give stream command
 #define DMA_WR_CMD( inst, cmd_par ) \
-   do { reg_dma_rw_stream_cmd r = {0}; \
-        do { r = REG_RD( dma, inst, rw_stream_cmd ); } while( r.busy ); \
-        r.cmd = (cmd_par); \
-        REG_WR( dma, inst, rw_stream_cmd, r ); \
-      } while( 0 )
+   do { reg_dma_rw_stream_cmd __x = {0}; \
+	do { __x = REG_RD(dma, inst, rw_stream_cmd); } while (__x.busy); \
+	__x.cmd = (cmd_par); \
+	REG_WR(dma, inst, rw_stream_cmd, __x); \
+   } while (0)
 
 // load: g,c,d:burst
 #define DMA_START_GROUP( inst, group_descr ) \
