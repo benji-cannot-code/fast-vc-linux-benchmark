dccef1ad34a38d8c0a4ddf96b8a18e31ffc71baf
Anthony Liguori <aliguori@us.ibm.com>
2008-01-28 09:59:59 -0600

    virtio: Use PCI revision field to indicate virtio PCI ABI version
    As Avi pointed out, as we continue to massage the virtio PCI ABI, we can make
    things a little more friendly to users by utilizing the PCI revision field to
    indicate which version of the ABI we're using.  This is a hard ABI version
    and incrementing it will cause the guest driver to break.
    
    This is the necessary changes to virtio_pci to support this.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/linux/virtio_pci.h
+++ include/linux/virtio_pci.h
@@ -53,4 +53,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * configuration space */
 #define VIRTIO_PCI_CONFIG		20
 
+/* Virtio ABI version, this must match exactly */
+#define VIRTIO_PCI_ABI_VERSION		0
 #endif
