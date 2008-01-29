8ccbffea9fa88c49743a0976c0407b8b814eaea2
Juergen Beisert <j.beisert@pengutronix.de>
2008-01-29 23:43:49 +0100

    [ARM] 4798/1: pcm027: fix missing header file
    This patch adds a PXA2xx specific header file to control chip setup.
    Without, the PCM027 BSP can't be built.
    
    Signed-off-by: Robert Schwebel <r.schwebel@pengutronix.de>
    Signed-off-by: Juergen Beisert <j.beisert@pengutronix.de>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/pcm027.c
+++ arch/arm/mach-pxa/pcm027.c
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach/arch.h>
 #include <asm/arch/hardware.h>
 #include <asm/arch/pxa-regs.h>
+#include <asm/arch/pxa2xx-regs.h>
 #include <asm/arch/pxa2xx_spi.h>
 #include <asm/arch/pcm027.h>
 #include "generic.h"
