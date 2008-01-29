73cfa4887ad4fb94f0671d5af9878606adb9fc80
Guennadi Liakhovetski <lg@denx.de>
2008-01-29 15:43:13 +0100

    [ARM] 4660/3: at91: allow selecting UART for early kernel messages
    Currently early kernel messages, i.e., those from uncompression, go to the
    debugging UART. And if it is enabled in the platform configuration, but
    not initialized by the bootloader, the machine hangs, waiting for UART
    status change. Besides, having those messages on another UART - typically
    the console UART - may be preferrable. This patch allows selecting the
    UART in kernel configuration.
    
    Signed-off-by: Guennadi Liakhovetski <lg@denx.de>
    Acked-by: Andrew Victor <linux@maxim.org.za>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/linux/atmel_serial.h
+++ include/linux/atmel_serial.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * drivers/serial/atmel_serial.h
+ * include/linux/atmel_serial.h
  *
  * Copyright (C) 2005 Ivan Kokshaysky
  * Copyright (C) SAN People
