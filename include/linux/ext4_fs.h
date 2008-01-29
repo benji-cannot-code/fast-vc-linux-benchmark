e6802a7b9dd2c85a7631019799081572d0bcad59
Takashi Sato <sho@tnes.nec.co.jp>
2008-01-28 23:58:27 -0500

    ext4:  Support large blocksize up to PAGESIZE
    This patch set supports large block size(>4k, <=64k) in ext4,
    just enlarging the block size limit. But it is NOT possible to have 64kB
    blocksize on ext4 without some changes to the directory handling
    code.  The reason is that an empty 64kB directory block would have a
    rec_len == (__u16)2^16 == 0, and this would cause an error to be hit in
    the filesystem.  The proposed solution is treat 64k rec_len
    with a an impossible value like rec_len = 0xffff to handle this.
    
    The Patch-set consists of the following 2 patches.
      [1/2]  ext4: enlarge blocksize
             - Allow blocksize up to pagesize
    
      [2/2]  ext4: fix rec_len overflow
             - prevent rec_len from overflow with 64KB blocksize
    
    Now on 64k page ppc64 box runs with this patch set we could create a 64k
    block size ext4dev, and able to handle empty directory block.
    
    Signed-off-by: Takashi Sato <sho@tnes.nec.co.jp>
    Signed-off-by: Mingming Cao <cmm@us.ibm.com>

--- include/linux/ext4_fs.h
+++ include/linux/ext4_fs.h
@@ -74,8 +74,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Macro-instructions used to manage several block sizes
  */
 #define EXT4_MIN_BLOCK_SIZE		1024
-#define	EXT4_MAX_BLOCK_SIZE		4096
-#define EXT4_MIN_BLOCK_LOG_SIZE		  10
+#define	EXT4_MAX_BLOCK_SIZE		65536
+#define EXT4_MIN_BLOCK_LOG_SIZE		10
 #ifdef __KERNEL__
 # define EXT4_BLOCK_SIZE(s)		((s)->s_blocksize)
 #else
