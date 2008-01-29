e8ce41c5b50f5152a0724af3d9e259dbcbae586f
Adrian Bunk <bunk@kernel.org>
2008-01-28 22:10:58 -0300

    V4L/DVB (7104): stk-sensor.c: make 2 functions static
    This patch makes the following needlessly global functions static:
    - stk_sensor_outb()
    - stk_sensor_inb()
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Acked-by: Jaime Velasco Juan <jsagarribay@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/stk-sensor.c
+++ drivers/media/video/stk-sensor.c
@@ -226,7 +226,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 /* Returns 0 if OK */
-int stk_sensor_outb(struct stk_camera *dev, u8 reg, u8 val)
+static int stk_sensor_outb(struct stk_camera *dev, u8 reg, u8 val)
 {
 	int i = 0;
 	int tmpval = 0;
@@ -251,7 +251,7 @@ int stk_sensor_outb(struct stk_camera *dev, u8 reg, u8 val)
 		return 0;
 }
 
-int stk_sensor_inb(struct stk_camera *dev, u8 reg, u8 *val)
+static int stk_sensor_inb(struct stk_camera *dev, u8 reg, u8 *val)
 {
 	int i = 0;
 	int tmpval = 0;
