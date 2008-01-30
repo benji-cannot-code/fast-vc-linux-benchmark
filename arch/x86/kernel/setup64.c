c768694b4986b52c53fd7f25310002640668485f
Huang, Ying <ying.huang@intel.com>
2008-01-30 13:32:51 +0100

    x86 boot : export boot_params via debugfs for debugging
    This patch export the boot parameters via debugfs for debugging.
    
    The files added are as follow:
    
    boot_params/data    :  binary file for struct boot_params
    boot_params/version :  boot protocol version
    
    This patch is based on 2.6.24-rc5-mm1 and has been tested on i386 and
    x86_64 platform.
    
    This patch is based on the Peter Anvin's proposal.
    
    Signed-off-by: Huang Ying <ying.huang@intel.com>
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/setup64.c
+++ arch/x86/kernel/setup64.c
@@ -25,7 +25,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sections.h>
 #include <asm/setup.h>
 
+#ifndef CONFIG_DEBUG_BOOT_PARAMS
 struct boot_params __initdata boot_params;
+#else
+struct boot_params boot_params;
+#endif
 
 cpumask_t cpu_initialized __cpuinitdata = CPU_MASK_NONE;
 
