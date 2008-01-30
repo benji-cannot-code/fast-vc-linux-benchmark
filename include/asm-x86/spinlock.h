769227ddf96578d7ef1bb73418e5cc30776b1458
Glauber de Oliveira Costa <gcosta@redhat.com>
2008-01-30 13:30:33 +0100

    x86: consolidate spinlock.h
    The cli and sti instructions need to be replaced by paravirt hooks.
    For the i386 architecture, this is already done. The code requirements
    aren't much different from x86_64 POV, so this part is consolidated in
    the common header
    
    Signed-off-by: Glauber de Oliveira Costa <gcosta@redhat.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Acked-by: Jeremy Fitzhardinge <jeremy@xensource.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/spinlock.h
+++ include/asm-x86/spinlock.h
@@ -1,6 +1,20 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef _X86_SPINLOCK_H_
+#define _X86_SPINLOCK_H_
+
+#ifdef CONFIG_PARAVIRT
+#include <asm/paravirt.h>
+#else
+#define CLI_STRING	"cli"
+#define STI_STRING	"sti"
+#define CLI_STI_CLOBBERS
+#define CLI_STI_INPUT_ARGS
+#endif /* CONFIG_PARAVIRT */
+
 #ifdef CONFIG_X86_32
 # include "spinlock_32.h"
 #else
 # include "spinlock_64.h"
+#endif
+
 #endif
