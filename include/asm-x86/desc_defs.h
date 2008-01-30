9a2043394e17ac3a66cd8221318fc52fec9f79f1
Glauber de Oliveira Costa <gcosta@redhat.com>
2008-01-30 13:31:12 +0100

    x86: introduce gate_desc type.
    To account for the differences in gate descriptor in i386 and x86_64
    a gate_desc type is introduced.
    
    Signed-off-by: Glauber de Oliveira Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/desc_defs.h
+++ include/asm-x86/desc_defs.h
@@ -40,7 +40,7 @@ enum {
 };
 
 // 16byte gate
-struct gate_struct {
+struct gate_struct64 {
 	u16 offset_low;
 	u16 segment;
 	unsigned ist : 3, zero0 : 5, type : 5, dpl : 2, p : 1;
@@ -67,6 +67,12 @@ struct ldttss_desc {
 	u32 base3;
 	u32 zero1;
 } __attribute__((packed));
+
+#ifdef CONFIG_X86_64
+typedef struct gate_struct64 gate_desc;
+#else
+typedef struct desc_struct gate_desc;
+#endif
 
 struct desc_ptr {
 	unsigned short size;
