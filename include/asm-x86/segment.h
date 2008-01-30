fe983826f518e11854fb71bdac71a74f95f2874e
Roland McGrath <roland@redhat.com>
2008-01-30 13:33:06 +0100

    x86: early_idt_handler improvements, 64-bit
    It's not too pretty, but I found this made the "PANIC: early exception"
    messages become much more reliably useful: 1. print the vector number,
    2. print the %cs value, 3. handle error-code-pushing vs non-pushing vectors.
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/segment.h
+++ include/asm-x86/segment.h
@@ -196,4 +196,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define GDT_ENTRY_TLS_ENTRIES 3
 #define TLS_SIZE (GDT_ENTRY_TLS_ENTRIES * 8)
 
+#ifdef __KERNEL__
+#ifndef __ASSEMBLY__
+extern const char early_idt_handlers[IDT_ENTRIES][10];
+#endif
+#endif
+
 #endif
