f7fbd3ea876c647cd602b4ee47a30aa8c20ec457
Ingo Molnar <mingo@elte.hu>
2008-01-30 13:32:54 +0100

    x86: migration helpers for KVM
    migration helpers for KVM.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/processor.h
+++ include/asm-x86/processor.h
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/processor-flags.h>
 
+/* migration helpers, for KVM - will be removed in 2.6.25: */
+#include <asm/vm86.h>
+#define Xgt_desc_struct	desc_ptr
+
 /* Forward declaration, a strange C thing */
 struct task_struct;
 struct mm_struct;
