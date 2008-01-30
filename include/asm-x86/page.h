15f2ff53651c7c8a7848910cc8d3ea85c65f4a19
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:34:06 +0100

    x86: move page_is_ram() function
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/page.h
+++ include/asm-x86/page.h
@@ -48,6 +48,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 
 #ifndef __ASSEMBLY__
+
+extern int page_is_ram(unsigned long pagenr);
+
 struct page;
 
 static void inline clear_user_page(void *page, unsigned long vaddr,
