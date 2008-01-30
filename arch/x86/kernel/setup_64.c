087b05121d48960886163af22bcd70eab5765f46
Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
2008-01-30 13:33:18 +0100

    x86: move select_idle_routine() call after detect_ht()
    Move the select_idle_routine() call to after the detect_ht() call at
    identify_cpu() on 64-bit.
    
    This change is for printing the polling idle and HT enabled warning
    message properly.
    
    Signed-off-by: Hiroshi Shimamoto <h-shimamoto@ct.jp.nec.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/setup_64.c
+++ arch/x86/kernel/setup_64.c
@@ -1000,7 +1000,6 @@ void __cpuinit identify_cpu(struct cpuinfo_x86 *c)
 		break;
 	}
 
-	select_idle_routine(c);
 	detect_ht(c);
 
 	/*
@@ -1018,6 +1017,8 @@ void __cpuinit identify_cpu(struct cpuinfo_x86 *c)
 #ifdef CONFIG_X86_MCE
 	mcheck_init(c);
 #endif
+	select_idle_routine(c);
+
 	if (c != &boot_cpu_data)
 		mtrr_ap_init();
 #ifdef CONFIG_NUMA
