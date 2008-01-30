7350e14864ce64c63ad920b6f5a6db9af543a856
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:17 +0100

    x86: move mce related declarations
    Move the mce related declarations where they belong, fix the
    users and remove 32bit dependency in mce.h
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/setup_64.c
+++ arch/x86/kernel/setup_64.c
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/sections.h>
 #include <asm/dmi.h>
 #include <asm/cacheflush.h>
+#include <asm/mce.h>
 
 /*
  * Machine setup..
