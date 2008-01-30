38ae2bc48c4a23d9040c15ff675fbcd643effc56
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:17 +0100

    x86: move debug related declarations to kdebug.h
    Move them and fixup some users.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/head64.c
+++ arch/x86/kernel/head64.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgtable.h>
 #include <asm/tlbflush.h>
 #include <asm/sections.h>
+#include <asm/kdebug.h>
 
 static void __init zap_identity_mappings(void)
 {
--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/proto.h>
 #include <asm/smp.h>
 #include <asm/sections.h>
+#include <asm/kdebug.h>
 
 #ifndef Dprintk
 #define Dprintk(x...)
