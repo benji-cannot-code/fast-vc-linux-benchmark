d4451e2c7b83426e55d20405fd836199fd89a23a
Ingo Molnar <mingo@elte.hu>
2008-01-30 13:34:00 +0100

    x86: unify header part of pageattr_64.c
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/mm/pageattr_64.c
+++ arch/x86/mm/pageattr_64.c
@@ -20,8 +20,9 @@ void clflush_cache_range(void *addr, int size)
 
 #include <asm/processor.h>
 #include <asm/tlbflush.h>
+#include <asm/sections.h>
 #include <asm/uaccess.h>
-#include <asm/io.h>
+#include <asm/pgalloc.h>
 
 pte_t *lookup_address(unsigned long address, int *level)
 {
