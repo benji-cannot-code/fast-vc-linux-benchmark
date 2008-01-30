a7ebe5e5757fc37ecd7677fd590e014d8f624148
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:17 +0100

    x86: move numa related declarations
    More stuff shuffeled to the correct place
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/smp.h>
 #include <asm/sections.h>
 #include <asm/kdebug.h>
+#include <asm/numa.h>
 
 #ifndef Dprintk
 #define Dprintk(x...)
