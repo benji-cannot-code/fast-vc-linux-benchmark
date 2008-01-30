990d704723eccd622ffc08013ade53cb13739d61
Roland McGrath <roland@redhat.com>
2008-01-30 13:30:45 +0100

    x86: desc_empty
    This replaces the desc_empty macro with an inline.  It now handles
    easily any of the four different types used between 32/64 code to
    refer to these 8 bytes.  It's identical in both asm-x86/processor_64.h
    and asm-x86/processor_32.h, so if these files ever get merged this
    function can be in the common code.
    
    This also removes the desc_equal macro because nothing uses it.
    
    Signed-off-by: Roland McGrath <roland@redhat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/processor_64.h
+++ include/asm-x86/processor_64.h
@@ -31,11 +31,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define VIP_MASK	0x00100000	/* virtual interrupt pending */
 #define ID_MASK		0x00200000
 
-#define desc_empty(desc) \
-               (!((desc)->a | (desc)->b))
-
-#define desc_equal(desc1, desc2) \
-               (((desc1)->a == (desc2)->a) && ((desc1)->b == (desc2)->b))
+static inline int desc_empty(const void *ptr)
+{
+	const u32 *desc = ptr;
+	return !(desc[0] | desc[1]);
+}
 
 /*
  * Default implementation of macro that returns current
