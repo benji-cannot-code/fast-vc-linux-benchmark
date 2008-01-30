2d8df007e8cee22907f8377793be354c90765e37
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:37 +0100

    x86: put all kern_addr_valid() incarnations to pgtable.h
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/sparsemem_32.h
+++ include/asm-x86/sparsemem_32.h
@@ -25,8 +25,5 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define MAX_PHYSMEM_BITS	32
 #endif
 
-/* XXX: FIXME -- wli */
-#define kern_addr_valid(kaddr)  (0)
-
 #endif /* CONFIG_SPARSEMEM */
 #endif /* _I386_SPARSEMEM_H */
