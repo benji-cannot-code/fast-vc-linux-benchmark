0a3bbbe55af4f5924f15612755da10790f00cf88
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:38 +0100

    x86: adjust numa 32 namespace
    Use the 64bit numa variable names for numa32 as well.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/topology_32.h
+++ include/asm-x86/topology_32.h
@@ -40,13 +40,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 
 /* Mappings between logical cpu number and node number */
-extern cpumask_t node_2_cpu_mask[];
-extern int cpu_2_node[];
+extern cpumask_t node_to_cpumask_map[];
+extern int cpu_to_node_map[];
 
 /* Returns the number of the node containing CPU 'cpu' */
 static inline int cpu_to_node(int cpu)
 { 
-	return cpu_2_node[cpu];
+	return cpu_to_node_map[cpu];
 }
 
 /* Returns the number of the node containing Node 'node'.  This architecture is flat, 
@@ -56,7 +56,7 @@ static inline int cpu_to_node(int cpu)
 /* Returns a bitmask of CPUs on Node 'node'. */
 static inline cpumask_t node_to_cpumask(int node)
 {
-	return node_2_cpu_mask[node];
+	return node_to_cpumask_map[node];
 }
 
 /* Returns the number of the first CPU on Node 'node'. */
