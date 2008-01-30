f21d676fe27ffc4c840ae04761f1a9009a6ea839
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:18 +0100

    x86: move pmtmr related declarations
    Move more stuff out of proto.h
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic_64.c
+++ arch/x86/kernel/apic_64.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/module.h>
 #include <linux/ioport.h>
 #include <linux/clockchips.h>
+#include <linux/acpi_pmtmr.h>
 
 #include <asm/atomic.h>
 #include <asm/smp.h>
