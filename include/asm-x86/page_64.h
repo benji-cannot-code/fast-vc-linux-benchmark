4e47ee49e15229bec0bb3a8c76eab893706a3d74
Thomas Gleixner <tglx@linutronix.de>
2008-01-30 13:30:17 +0100

    x86: move page related declaration
    end_pfn is in page.h, so end_pfn_map has a place there as well
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/page_64.h
+++ include/asm-x86/page_64.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 
 extern unsigned long end_pfn;
+extern unsigned long end_pfn_map;
 
 void clear_page(void *);
 void copy_page(void *, void *);
