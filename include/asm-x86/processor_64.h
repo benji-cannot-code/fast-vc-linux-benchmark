1fec585c9d3afc521f35def0d22443efced2926d
Glauber de Oliveira Costa <gcosta@redhat.com>
2008-01-30 13:31:38 +0100

    x86: unify mm_segment_t definition
    This patch moves the mm_segment_t structure definition to processor.h
    This makes mmsegment.h file useless, and it is deleted.
    
    Signed-off-by: Glauber de Oliveira Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/processor_64.h
+++ include/asm-x86/processor_64.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/msr.h>
 #include <asm/current.h>
 #include <asm/system.h>
-#include <asm/mmsegment.h>
 #include <linux/personality.h>
 #include <asm/desc_defs.h>
 
--- include/asm-x86/thread_info_64.h
+++ include/asm-x86/thread_info_64.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 struct task_struct;
 struct exec_domain;
-#include <asm/mmsegment.h>
+#include <asm/processor.h>
 
 struct thread_info {
 	struct task_struct	*task;		/* main task structure */
