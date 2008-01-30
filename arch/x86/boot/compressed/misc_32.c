4b469abd1ea77ce877c253fd631300ab434c5575
Ian Campbell <ijc@hellion.org.uk>
2008-01-30 13:33:38 +0100

    x86: unify strings in arch/x86/boot/compressed/misc_??.c
    There seems to be a preference for the 64 bit version so use that on 32 bit and
    drop the stray leading "."
    
    Signed-off-by: Ian Campbell <ijc@hellion.org.uk>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/boot/compressed/misc_32.c
+++ arch/x86/boot/compressed/misc_32.c
@@ -378,8 +378,8 @@ asmlinkage void decompress_kernel(void *rmode, unsigned long end,
 #endif
 
 	makecrc();
-	putstr("Uncompressing Linux... ");
+	putstr("\nDecompressing Linux... ");
 	gunzip();
-	putstr("Ok, booting the kernel.\n");
+	putstr("done.\nBooting the kernel.\n");
 	return;
 }
