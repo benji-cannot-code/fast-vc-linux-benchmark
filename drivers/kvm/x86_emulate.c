virtualopensystems/linux-kvm-arm

KVM: x86 emulator: Make a distinction between repeat prefixes F3 and F2
cmps and scas instructions accept repeat prefixes F3 and F2. So in
order to emulate those prefixed instructions we need to be able to know
if prefixes are REP/REPE/REPZ or REPNE/REPNZ. Currently kvm doesn't make
this distinction. This patch introduces this distinction.

Signed-off-by: Guillaume Thouvenin <guillaume.thouvenin@ext.bull.net>
Signed-off-by: Avi Kivity <avi@qumranet.com>

drivers/kvm/x86_emulate.c
@@ -824,8 +824,10 @@ x86_decode_insn(struct x86_emulate_ctxt *ctxt, struct x86_emulate_ops *ops)
 			c->lock_prefix = 1;
 			break;
 		case 0xf2:	/* REPNE/REPNZ */
+			c->rep_prefix = REPNE_REPNZ;
+			break;
 		case 0xf3:	/* REP/REPE/REPZ */
-			c->rep_prefix = 1;
+			c->rep_prefix = REP_REPE_REPZ;
 			break;
 		default:
 			goto done_prefixes;

benji-cannot-code/fast-vc-linux-benchmark

drivers/kvm/x86_emulate.c
@@ -825,8 +825,10 @@ x86_decode_insn(struct x86_emulate_ctxt *ctxt, struct x86_emulate_ops *ops)
 			c->lock_prefix = 1;
 			break;
 		case 0xf2:	/* REPNE/REPNZ */
+			c->rep_prefix = REPNE_PREFIX;
+			break;
 		case 0xf3:	/* REP/REPE/REPZ */
-			c->rep_prefix = 1;
+			c->rep_prefix = REPE_PREFIX;
 			break;
 		default:
 			goto done_prefixes;