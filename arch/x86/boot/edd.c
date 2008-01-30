b5817cc06ff4c230c08fce7dea1205b11c874b9d
H. Peter Anvin <hpa@zytor.com>
2008-01-30 13:33:03 +0100

    x86 setup: OK -> ok (no need to scream)
    Unnecessary capitals are shouting; no need for it here.
    Thus, change "OK" to "ok" and add a space.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/boot/edd.c
+++ arch/x86/boot/edd.c
@@ -155,7 +155,7 @@ void query_edd(void)
 	 */
 
 	if (!be_quiet)
-		printf("Probing EDD...");
+		printf("Probing EDD... ");
 
 	for (devno = 0x80; devno < 0x80+EDD_MBR_SIG_MAX; devno++) {
 		/*
@@ -175,7 +175,7 @@ void query_edd(void)
 	}
 
 	if (!be_quiet)
-		printf("OK\n");
+		printf("ok\n");
 }
 
 #endif
