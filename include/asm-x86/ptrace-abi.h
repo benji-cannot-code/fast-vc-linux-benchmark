3d0a2a4621eea8c7a7c7d9e3842f511c4066f6b9
Markus Metzger <markus.t.metzger@intel.com>
2008-01-30 13:32:03 +0100

    x86, ptrace: support 32bit-cross-64bit BTS recording
    Support BTS recording of 32bit and 64bit tasks from 32bit or 64bit tasks.
    
    Signed-off-by: Markus Metzger <markus.t.metzger@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/ptrace-abi.h
+++ include/asm-x86/ptrace-abi.h
@@ -86,9 +86,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 struct ptrace_bts_config {
 	/* requested or actual size of BTS buffer in bytes */
-	unsigned long size;
+	unsigned int size;
 	/* bitmask of below flags */
-	unsigned long flags;
+	unsigned int flags;
 };
 
 #define PTRACE_BTS_O_TRACE	0x1 /* branch trace */
