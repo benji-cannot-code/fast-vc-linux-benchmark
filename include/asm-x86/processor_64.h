0d5616cebfd6732da7e4049802819880c89bc465
Glauber de Oliveira Costa <gcosta@redhat.com>
2008-01-30 13:31:27 +0100

    x86: move desc_empty to where they belong
    This patch moves the (duplicated) desc_empty implementation to desc.h,
    where the descriptor things belong.
    
    Signed-off-by: Glauber de Oliveira Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/processor_64.h
+++ include/asm-x86/processor_64.h
@@ -21,12 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/cpumask.h>
 #include <asm/desc_defs.h>
 
-static inline int desc_empty(const void *ptr)
-{
-	const u32 *desc = ptr;
-	return !(desc[0] | desc[1]);
-}
-
 /*
  * Default implementation of macro that returns current
  * instruction pointer ("program counter").
