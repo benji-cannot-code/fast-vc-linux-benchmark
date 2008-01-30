7b0caba1fd4df070f8acc252d27df4414c2d32ca
Ingo Molnar <mingo@elte.hu>
2008-01-30 13:33:24 +0100

    x86: get_cycles() fix
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/tsc.h
+++ include/asm-x86/tsc.h
@@ -28,10 +28,8 @@ static inline cycles_t get_cycles(void)
 	if (!cpu_has_tsc)
 		return 0;
 #endif
-
-#if defined(CONFIG_X86_GENERIC) || defined(CONFIG_X86_TSC)
 	rdtscll(ret);
-#endif
+
 	return ret;
 }
 
