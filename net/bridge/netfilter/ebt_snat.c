2ab30fbc128887306f80d67d7f1dc532ae8e2b94
Jan Engelhardt <jengelh@computergmbh.de>
2008-01-31 04:00:30 -0800

    [NETFILTER]: ebtables: Update modules' descriptions
    Update the MODULES_DESCRIPTION() tags for all Ebtables modules.
    
    Signed-off-by: Jan Engelhardt <jengelh@computergmbh.de>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/bridge/netfilter/ebt_snat.c
+++ net/bridge/netfilter/ebt_snat.c
@@ -89,4 +89,5 @@ static void __exit ebt_snat_fini(void)
 
 module_init(ebt_snat_init);
 module_exit(ebt_snat_fini);
+MODULE_DESCRIPTION("Ebtables: Source MAC address translation");
 MODULE_LICENSE("GPL");
