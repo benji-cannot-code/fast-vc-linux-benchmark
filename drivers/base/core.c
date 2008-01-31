a9178c2f34ee3f19292d9caafb3e230f59310878
Cornelia Huck <cornelia.huck@de.ibm.com>
2008-01-31 10:39:38 +0100

    Driver core: Remove unneeded get_{device,driver}() calls.
    Driver core: Remove unneeded get_{device,driver}() calls.
    
    Code trying to add/remove attributes must hold a reference to
    the device resp. driver anyway, so let's remove those reference
    count games.
    
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Cc: Dave Young <hidave.darkstar@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -424,10 +424,8 @@ struct kset *devices_kset;
 int device_create_file(struct device *dev, struct device_attribute *attr)
 {
 	int error = 0;
-	if (get_device(dev)) {
+	if (dev)
 		error = sysfs_create_file(&dev->kobj, &attr->attr);
-		put_device(dev);
-	}
 	return error;
 }
 
@@ -438,10 +436,8 @@ int device_create_file(struct device *dev, struct device_attribute *attr)
  */
 void device_remove_file(struct device *dev, struct device_attribute *attr)
 {
-	if (get_device(dev)) {
+	if (dev)
 		sysfs_remove_file(&dev->kobj, &attr->attr);
-		put_device(dev);
-	}
 }
 
 /**
