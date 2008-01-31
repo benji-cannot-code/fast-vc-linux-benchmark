efdb63530e27af53667526916af12785fd261cbe
Eric Miao <eric.y.miao@gmail.com>
2008-01-31 00:59:23 -0500

    Input: pxa27x_keypad - add debounce_interval to the keypad platform data
    Currently, only one debounce_interval is introduced for both direct and
    matrix keys. This is true in most cases, although the keypad controller
    supports different debounce for direct/matrix keys.
    
    Some platforms do require this to be tuned, instead of the default
    reset value of 100ms.
    
    Rotary encoder will always use zero debounce time for now to achieve
    certain sensitivity.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- include/asm-arm/arch-pxa/pxa27x_keypad.h
+++ include/asm-arm/arch-pxa/pxa27x_keypad.h
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *
  * 3. rotary can be either interpreted as a relative input event (e.g.
  *    REL_WHEEL/REL_HWHEEL) or specific keys (e.g. UP/DOWN/LEFT/RIGHT)
+ *
+ * 4. matrix key and direct key will use the same debounce_interval by
+ *    default, which should be sufficient in most cases
  */
 struct pxa27x_keypad_platform_data {
 
@@ -44,6 +47,9 @@ struct pxa27x_keypad_platform_data {
 	int		rotary1_rel_code;
 	int		rotary1_up_key;
 	int		rotary1_down_key;
+
+	/* key debounce interval */
+	unsigned int	debounce_interval;
 };
 
 #define KEY(row, col, val)	(((row) << 28) | ((col) << 24) | (val))
