3101803698b6890799c8f2efc19effcd47563a89
Eric Miao <eric.y.miao@gmail.com>
2008-01-31 00:58:00 -0500

    Input: pxa27x_keypad - remove pin configuration from the driver
    The pin configurations will slowly be moved to the board specific code
    at initialization thus to make the driver more generic.
    
    Signed-off-by: Eric Miao <eric.miao@marvell.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- include/asm-arm/arch-pxa/pxa27x_keypad.h
+++ include/asm-arm/arch-pxa/pxa27x_keypad.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct pxa27x_keypad_platform_data {
 	int nr_rows, nr_cols;
 	int keycodes[PXAKBD_MAXROW][PXAKBD_MAXCOL];
-	int gpio_modes[PXAKBD_MAXROW + PXAKBD_MAXCOL];
 
 #ifdef CONFIG_PM
 	u32 reg_kpc;
