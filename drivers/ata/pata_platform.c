457bab45280b6b3fa7ffde01c7a98b745fa6ab05
Jeff Garzik <jeff@garzik.org>
2008-02-01 18:02:30 -0500

    Rename: linux/pata_platform.h to linux/ata_platform.h
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/pata_platform.c
+++ drivers/ata/pata_platform.c
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ata.h>
 #include <linux/libata.h>
 #include <linux/platform_device.h>
-#include <linux/pata_platform.h>
+#include <linux/ata_platform.h>
 
 #define DRV_NAME "pata_platform"
 #define DRV_VERSION "1.2"
