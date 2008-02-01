23ccc64db7acc772a5f9ea5a6041abe34cb29c82
Rami Rosen <ramirose@gmail.com>
2008-01-31 16:42:23 -0800

    [PKTGEN]: Remove an unused definition in pktgen.c.
    - Remove an unused definition (LAT_BUCKETS_MAX) in net/core/pktgen.c.
    - Remove the corresponding comment.
    - The LAT_BUCKETS_MAX seems to have to do with a patch from a long
    time ago which was not applied (Ben Greear), which dealt with latency
    counters.
    
    See, for example : http://oss.sgi.com/archives/netdev/2002-09/msg00184.html
    
    Signed-off-by: Rami Rosen <ramirose@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/pktgen.c
+++ net/core/pktgen.c
@@ -171,8 +171,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define VERSION  "pktgen v2.69: Packet Generator for packet performance testing.\n"
 
-/* The buckets are exponential in 'width' */
-#define LAT_BUCKETS_MAX 32
 #define IP_NAME_SZ 32
 #define MAX_MPLS_LABELS 16 /* This is the max label stack depth */
 #define MPLS_STACK_BOTTOM htonl(0x00000100)
