b0adab92ad53e7308f43d409a7c92716b0efadbc
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-02-01 23:09:36 +0100

    ide: factor out code for tuning devices from ide_probe_port()
    * Factor out code for tuning devices from ide_probe_port()
      to ide_port_tune_devices().
    
    * Move ide_port_tune_devices() call from ide_probe_port()
      to ide_device_add_all().
    
    There should be no functionality changes caused by this patch.
    
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide-probe.c
+++ drivers/ide/ide-probe.c
@@ -795,6 +795,13 @@ static int ide_probe_port(ide_hwif_t *hwif)
 	if (!hwif->present)
 		return -ENODEV;
 
+	return 0;
+}
+
+static void ide_port_tune_devices(ide_hwif_t *hwif)
+{
+	int unit;
+
 	for (unit = 0; unit < MAX_DRIVES; unit++) {
 		ide_drive_t *drive = &hwif->drives[unit];
 
@@ -828,8 +835,6 @@ static int ide_probe_port(ide_hwif_t *hwif)
 		else
 			drive->no_io_32bit = drive->id->dword_io ? 1 : 0;
 	}
-
-	return 0;
 }
 
 #if MAX_HWIFS > 1
@@ -1310,8 +1315,12 @@ int ide_device_add_all(u8 *idx)
 			continue;
 		}
 
-		if (ide_probe_port(hwif) < 0)
+		if (ide_probe_port(hwif) < 0) {
 			ide_hwif_release_regions(hwif);
+			continue;
+		}
+
+		ide_port_tune_devices(hwif);
 	}
 
 	for (i = 0; i < MAX_HWIFS; i++) {
