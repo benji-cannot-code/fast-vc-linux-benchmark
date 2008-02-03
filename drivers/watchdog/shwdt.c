e7c1254d3b6b663d2f06eec04c2588a8404d8c8f
Joe Perches <joe@perches.com>
2008-02-03 17:32:52 +0200

    drivers/watchdog/: Spelling fixes
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Adrian Bunk <bunk@kernel.org>

--- drivers/watchdog/shwdt.c
+++ drivers/watchdog/shwdt.c
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * overflow periods respectively.
  *
  * Also, since we can't really expect userspace to be responsive enough
- * before the overflow happens, we maintain two seperate timers .. One in
+ * before the overflow happens, we maintain two separate timers .. One in
  * the kernel for clearing out WOVF every 2ms or so (again, this depends on
  * HZ == 1000), and another for monitoring userspace writes to the WDT device.
  *
