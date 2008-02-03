4eb4b3db7d4ac75212a3ca88aa7b1ac55587a1fd
Stefan Richter <stefanr@s5r6.in-berlin.de>
2008-02-03 23:12:17 +0100

    firewire: fw-sbp2: sort includes
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-sbp2.c
+++ drivers/firewire/fw-sbp2.c
@@ -29,15 +29,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * and many others.
  */
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/mod_devicetable.h>
+#include <linux/blkdev.h>
 #include <linux/delay.h>
 #include <linux/device.h>
-#include <linux/scatterlist.h>
 #include <linux/dma-mapping.h>
-#include <linux/blkdev.h>
+#include <linux/kernel.h>
+#include <linux/mod_devicetable.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/scatterlist.h>
 #include <linux/string.h>
 #include <linux/stringify.h>
 #include <linux/timer.h>
@@ -49,9 +49,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <scsi/scsi_device.h>
 #include <scsi/scsi_host.h>
 
-#include "fw-transaction.h"
-#include "fw-topology.h"
 #include "fw-device.h"
+#include "fw-topology.h"
+#include "fw-transaction.h"
 
 /*
  * So far only bridges from Oxford Semiconductor are known to support
