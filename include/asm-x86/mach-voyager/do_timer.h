39dc9c3fbbb9bc2f19deda52434da9a9e6867295
Li Zefan <lizf@cn.fujitsu.com>
2008-02-03 16:20:13 +0200

    time: delete comments that refer to noexistent symbols
    Function do_timer_interrupt_hook() don't take argument regs,
    and structure hrtimer_sleeper don't have member cb_pending.
    So delete comments refering to these symbols.
    
    Signed-off-by: Li Zefan <lizf@cn.fujitsu.com>
    Signed-off-by: Adrian Bunk <bunk@kernel.org>

--- include/asm-x86/mach-voyager/do_timer.h
+++ include/asm-x86/mach-voyager/do_timer.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 /**
  * do_timer_interrupt_hook - hook into timer tick
- * @regs:     standard registers from interrupt
  *
  * Call the pit clock event handler. see asm/i8253.h
  **/
