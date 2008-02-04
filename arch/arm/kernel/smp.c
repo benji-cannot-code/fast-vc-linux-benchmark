751dce45392a7b6bebfab44d1aa63c9555b7fb58
Catalin Marinas <catalin.marinas@arm.com>
2008-02-04 17:30:57 +0100

    [ARM] 4814/1: RealView: Add broadcasting clockevents support for ARM11MPCore
    This patch adds dummy local timers for each CPU so that the board clock
    device is used to broadcast events to the other CPUs. The patch also
    adds the declaration for the dummy_timer_setup function (the equivalent
    of local_timer_setup when CONFIG_LOCAL_TIMERS is not set).
    
    Due to the way clockevents work, the dummy timer on the first CPU has to
    be registered before the board timer.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -291,6 +291,11 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	local_irq_enable();
 	local_fiq_enable();
 
+	/*
+	 * Setup local timer for this CPU.
+	 */
+	local_timer_setup(cpu);
+
 	calibrate_delay();
 
 	smp_store_cpu_info(cpu);
@@ -299,11 +304,6 @@ asmlinkage void __cpuinit secondary_start_kernel(void)
 	 * OK, now it's safe to let the boot CPU continue
 	 */
 	cpu_set(cpu, cpu_online_map);
-
-	/*
-	 * Setup local timer for this CPU.
-	 */
-	local_timer_setup(cpu);
 
 	/*
 	 * OK, it's off to the idle thread for us
