e17800ae24391bd4b06f53e0419859cf762bf63c
Catalin Marinas <catalin.marinas@arm.com>
2008-02-04 17:45:03 +0100

    [ARM] 4821/1: RealView: Remove the platform dependencies from localtimer.c
    This patch removes the TWD_BASE macro used to set up and configure the
    local timers on ARM11MPCore. The twd_base_addr and twd_size variables
    are defined in localtimer.c and set from the realview_eb_init function.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-realview/localtimer.c
+++ arch/arm/mach-realview/localtimer.c
@@ -25,9 +25,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/io.h>
 #include <asm/irq.h>
 
-#define TWD_BASE(cpu)	(__io_address(REALVIEW_EB11MP_TWD_BASE) + \
-			 ((cpu) * REALVIEW_EB11MP_TWD_SIZE))
-
 static DEFINE_PER_CPU(struct clock_event_device, local_clockevent);
 
 /*
@@ -41,6 +38,12 @@ void local_timer_interrupt(void)
 }
 
 #ifdef CONFIG_LOCAL_TIMERS
+
+#define TWD_BASE(cpu)	(twd_base_addr + (cpu) * twd_size)
+
+/* set up by the platform code */
+void __iomem *twd_base_addr;
+unsigned int twd_size;
 
 static unsigned long mpcore_timer_rate;
 
