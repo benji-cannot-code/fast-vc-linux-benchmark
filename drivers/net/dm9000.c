2fc0485cc0cb9d7e16acb4949bd47f850db85144
Ben Dooks <ben-linux@fluff.org>
2008-02-05 00:02:20 +0000

    DM9000: Add platform flag for no attached EEPROM
    Allow the platform data to specify to the DM9000 driver
    that there is no posibility of an attached EEPROM on the
    device, so default all reads to 0xff and ignore any
    write operations.
    
    Signed-off-by: Ben Dooks <ben-linux@fluff.org>
    Signed-off-by: Jeff Garzik <jeff@garzik.org>

--- drivers/net/dm9000.c
+++ drivers/net/dm9000.c
@@ -406,6 +406,9 @@ static int dm9000_get_eeprom(struct net_device *dev,
 	if ((len & 1) != 0 || (offset & 1) != 0)
 		return -EINVAL;
 
+	if (dm->flags & DM9000_PLATF_NO_EEPROM)
+		return -ENOENT;
+
 	ee->magic = DM_EEPROM_MAGIC;
 
 	for (i = 0; i < len; i += 2)
@@ -426,6 +429,9 @@ static int dm9000_set_eeprom(struct net_device *dev,
 
 	if ((len & 1) != 0 || (offset & 1) != 0)
 		return -EINVAL;
+
+	if (dm->flags & DM9000_PLATF_NO_EEPROM)
+		return -ENOENT;
 
 	if (ee->magic != DM_EEPROM_MAGIC)
 		return -EINVAL;
@@ -1101,6 +1107,12 @@ dm9000_read_eeprom(board_info_t *db, int offset, u8 *to)
 {
 	unsigned long flags;
 
+	if (db->flags & DM9000_PLATF_NO_EEPROM) {
+		to[0] = 0xff;
+		to[1] = 0xff;
+		return;
+	}
+
 	mutex_lock(&db->addr_lock);
 
 	spin_lock_irqsave(&db->lock, flags);
@@ -1134,6 +1146,9 @@ static void
 dm9000_write_eeprom(board_info_t *db, int offset, u8 *data)
 {
 	unsigned long flags;
+
+	if (db->flags & DM9000_PLATF_NO_EEPROM)
+		return;
 
 	mutex_lock(&db->addr_lock);
 
