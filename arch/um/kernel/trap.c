63cf7987324bb2b4ba2dbaeb4f65a0cfdaf03aaa
Jeff Dike <jdike@addtoit.com>
2008-02-04 22:30:47 -0800

    uml: tidy pgtable.h
    Large pieces of include/asm/pgtable.h were unused cruft.
    
    This uncovered arch/um/kernel/trap.c needing skas.h in order to get
    ptrace_faultinfo.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/kernel/trap.c
+++ arch/um/kernel/trap.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "as-layout.h"
 #include "kern_util.h"
 #include "os.h"
+#include "skas.h"
 #include "sysdep/sigcontext.h"
 
 /*
