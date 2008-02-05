1d292e25831abd1059a4749ed3d38dbcdba7956d
Jeff Dike <jdike@addtoit.com>
2008-02-04 22:30:47 -0800

    UML - Fix build in 2.6.24-rc2-mm1
    The earlier pgtable.h tidying patch made things a bit too tidy.  Add
    back a header which is needed in VMALLOC_START and friend.  Also add
    back a definition of pmd_page_vaddr, which is needed on x86_64.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-um/pgtable.h
+++ include/asm-um/pgtable.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __UM_PGTABLE_H
 
 #include "linux/sched.h"
+#include <asm/fixmap.h>
 
 #define _PAGE_PRESENT	0x001
 #define _PAGE_NEWPAGE	0x002
@@ -309,6 +310,7 @@ static inline pte_t pte_modify(pte_t pte, pgprot_t newprot)
  * this macro returns the index of the entry in the pmd page which would
  * control the given virtual address
  */
+#define pmd_page_vaddr(pmd) ((unsigned long) __va(pmd_val(pmd) & PAGE_MASK))
 #define pmd_index(address) (((address) >> PMD_SHIFT) & (PTRS_PER_PMD-1))
 
 /*
