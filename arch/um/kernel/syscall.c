96307cd08e419f6377db1344c45ec3024d93f6ed
Jeff Dike <jdike@addtoit.com>
2008-02-04 22:31:11 -0800

    uml: get rid of syscall counters
    Get rid of some syscall counters which haven't been useful in ages.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/kernel/syscall.c
+++ arch/um/kernel/syscall.c
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "asm/uaccess.h"
 #include "asm/unistd.h"
 
-/*  Unlocked, I don't care if this is a bit off */
-int nsyscalls = 0;
-
 long sys_fork(void)
 {
 	long ret;
