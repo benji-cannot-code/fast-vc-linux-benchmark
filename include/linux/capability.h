d5a56866d7fa3b2ef41944d765d11ad4a43158f0
Andrew Morgan <andrew@nagrom.org>
2008-02-04 22:29:43 -0800

    Remove unnecessary include from include/linux/capability.h
    KaiGai Kohei observed that this line in the linux header is not needed.
    
    Signed-off-by: Andrew G. Morgan <morgan@kernel.org>
    Cc: KaiGai Kohei <kaigai@kaigai.gr.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/capability.h
+++ include/linux/capability.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _LINUX_CAPABILITY_H
 
 #include <linux/types.h>
-#include <linux/compiler.h>
 
 struct task_struct;
 
