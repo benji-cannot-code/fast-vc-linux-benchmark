baebea01feb371c64fa2875054eb7962fef05adb
Jeff Dike <jdike@addtoit.com>
2008-02-04 22:31:08 -0800

    uml: install panic notifier earlier
    It turns out that if there's a panic early enough, UML will just sit there in
    the LED-blinking loop because the panic notifier hadn't been installed yet.
    
    This patch installs it earlier.
    
    It also fixes the problem which exposed the hang, namely that if you give UML
    a zero-sized initrd, it will ask alloc_bootmem for zero bytes, and that will
    cause the panic.
    
    While I was in initrd.c, I gave it a style makeover.
    
    Prompted by checkpatch, I moved a couple extern declarations of uml_exitcode
    to kern_util.h.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/include/kern_util.h
+++ arch/um/include/kern_util.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sysdep/ptrace.h"
 #include "sysdep/faultinfo.h"
 
+extern int uml_exitcode;
+
 extern int ncpus;
 extern int kmalloc_ok;
 extern int nsyscalls;
