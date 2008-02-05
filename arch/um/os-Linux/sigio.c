6d0437a69e66c259abd1612c26abbbb5ce96f46a
Lucas Woods <woodzy@gmail.com>
2008-02-04 22:30:49 -0800

    arch/um: remove duplicate includes
    Signed-off-by: Lucas Woods <woodzy@gmail.com>
    Cc: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/um/os-Linux/sigio.c
+++ arch/um/os-Linux/sigio.c
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "sigio.h"
 #include "os.h"
 #include "um_malloc.h"
-#include "init.h"
 
 /* Protected by sigio_lock(), also used by sigio_cleanup, which is an
  * exitcall.
