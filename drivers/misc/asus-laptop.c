1a00ac69724072206ed2dd782a1878a281694807
Ralf Baechle <ralf@linux-mips.org>
2008-02-06 01:36:15 -0800

    Remove inclusions of <linux/autoconf.h>
    Nothing should ever include this file.
    
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Acked-by: "Mike Frysinger" <vapier.adi@gmail.com>
    Acked-by: "Bryan Wu" <cooloney.lkml@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/misc/asus-laptop.c
+++ drivers/misc/asus-laptop.c
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  Sam Lin        - GPS support
  */
 
-#include <linux/autoconf.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
