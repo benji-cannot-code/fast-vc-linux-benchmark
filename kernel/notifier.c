796838c43cae7a6581fe1c81fb5d2f4dfdf07edf
Adrian Bunk <bunk@kernel.org>
2008-02-06 01:36:46 -0800

    kernel/notifier.c should #include <linux/reboot.h>
    Every file should include the headers containing the prototypes for its global
    functions (in this case {,un}register_reboot_notifier()).
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/notifier.c
+++ kernel/notifier.c
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/notifier.h>
 #include <linux/rcupdate.h>
 #include <linux/vmalloc.h>
+#include <linux/reboot.h>
 
 /*
  *	Notifier list for kernel code which wants to be called
