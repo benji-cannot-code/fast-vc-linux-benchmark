2df84ecca116d4a9b2fc728811688736865ad1bb
Olaf Hering <olaf@aepfle.de>
2008-02-06 01:40:19 -0800

    jbd.h: hide kernel only code
    Move a few kernel-only things into __KERNEL__.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/jbd.h
+++ include/linux/jbd.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/lockdep.h>
 
 #include <asm/semaphore.h>
-#endif
 
 #define journal_oom_retry 1
 
@@ -85,7 +84,6 @@ static inline void jbd_free(void *ptr, size_t size)
 
 #define JFS_MIN_JOURNAL_BLOCKS 1024
 
-#ifdef __KERNEL__
 
 /**
  * typedef handle_t - The handle_t type represents a single atomic update being performed by some process.
