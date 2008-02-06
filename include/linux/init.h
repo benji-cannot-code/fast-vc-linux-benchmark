1e0b32cfbf42df74e0baa9011053f196ff6863a7
Ingo Molnar <mingo@elte.hu>
2008-02-06 22:39:45 +0100

    generic: add __FINITDATA
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/linux/init.h
+++ include/linux/init.h
@@ -111,6 +111,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __FINIT		.previous
 
 #define __INITDATA	.section	".init.data","aw"
+#define __FINITDATA	.previous
 
 #define __DEVINIT        .section	".devinit.text", "ax"
 #define __DEVINITDATA    .section	".devinit.data", "aw"
