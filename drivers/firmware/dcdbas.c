0aa3498d955485ecde093cd5789910fbcc68b4bc
Matt Domsch <Matt_Domsch@dell.com>
2008-02-06 01:36:24 -0800

    dcdbas: add DMI-based module autloading
    DMI autoload dcdbas on all Dell systems.
    
    This looks for BIOS Vendor or System Vendor == Dell, so this should
    work for systems both Dell-branded and those Dell builds but brands
    for others.  It causes udev to load the dcdbas module at startup,
    which is used by tools called by HAL for wireless control and
    backlight control, among other uses.
    
    Thanks to Kay Sievers for figuring out how to do this with a single alias.
    
    Signed-off-by: Matt Domsch <Matt_Domsch@dell.com>
    Cc: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/firmware/dcdbas.c
+++ drivers/firmware/dcdbas.c
@@ -659,4 +659,5 @@ MODULE_DESCRIPTION(DRIVER_DESCRIPTION " (version " DRIVER_VERSION ")");
 MODULE_VERSION(DRIVER_VERSION);
 MODULE_AUTHOR("Dell Inc.");
 MODULE_LICENSE("GPL");
-
+/* Any System or BIOS claiming to be by Dell */
+MODULE_ALIAS("dmi:*:[bs]vnD[Ee][Ll][Ll]*:*");
