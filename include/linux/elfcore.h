764ec59bb2a578f5b3dcf37f4b70c025286e9b3f
Kirill A. Shutemov <k.shutemov@gmail.com>
2008-02-07 00:15:53 -0800

    Unexport asm/user.h and linux/user.h
    Do not export asm/user.h and linux/user.h during make headers_install.
    
    Signed-off-by: Kirill A. Shutemov <k.shutemov@gmail.com>
    Reviewed-by: David Woodhouse <dwmw2@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Acked-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/elfcore.h
+++ include/linux/elfcore.h
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/signal.h>
 #include <linux/time.h>
+#ifdef __KERNEL__
 #include <linux/user.h>
+#endif
 #include <linux/ptrace.h>
 
 struct elf_siginfo
