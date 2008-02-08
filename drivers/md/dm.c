31198f33efda4c07803ea1153b9f66832548f4f4
Milan Broz <mbroz@redhat.com>
2008-02-08 02:10:27 +0000

    dm: split dm_suspend io_lock hold into two
    Change io_locking to allow processing flush in separate thread.
    
    Because we have DMF_BLOCK_IO already set, any possible
    new ios are queued in dm_requests now.
    
    In the case of interrupting previous wait there can be more
    ios queued (we unlocked io_lock for a while) but this is safe.
    
    Signed-off-by: Milan Broz <mbroz@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1435,9 +1435,11 @@ int dm_suspend(struct mapped_device *md, unsigned suspend_flags)
 
 	if (noflush)
 		__merge_pushback_list(md);
+	up_write(&md->io_lock);
 
 	/* were we interrupted ? */
 	if (pending) {
+		down_write(&md->io_lock);
 		__flush_deferred_io(md);
 		up_write(&md->io_lock);
 
@@ -1445,7 +1447,6 @@ int dm_suspend(struct mapped_device *md, unsigned suspend_flags)
 		r = -EINTR;
 		goto out; /* pushback list is already flushed, so skip flush */
 	}
-	up_write(&md->io_lock);
 
 	dm_table_postsuspend_targets(map);
 
