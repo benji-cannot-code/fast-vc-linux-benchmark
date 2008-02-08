a224c23caa7758d3d1908869f8dc0311ea945a35
Alexey Dobriyan <adobriyan@sw.ru>
2008-02-08 04:18:27 -0800

    proc: simplify function prototypes
    Move code around so as to reduce the number of forward-declarations.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@sw.ru>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/proc/proc_tty.c
+++ fs/proc/proc_tty.c
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/seq_file.h>
 #include <linux/bitops.h>
 
-static int tty_ldiscs_read_proc(char *page, char **start, off_t off,
-				int count, int *eof, void *data);
-
 /*
  * The /proc/tty directory inodes...
  */
