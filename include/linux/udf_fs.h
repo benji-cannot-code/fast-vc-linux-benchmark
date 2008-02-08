85d2f7f8fe32713127c44cd9686f3c4b89c3ead4
Marcin Slusarz <marcin.slusarz@gmail.com>
2008-02-08 04:20:46 -0800

    udf: fix udf_debug macro
    udf_debug should be enclosed with do { } while (0)
    to be safely used in code like below:
    if (something)
    	udf_debug();
    else
    	anything;
    (Otherwise compiler will not compile it with:
    "error: expected expression before 'else'")
    
    Signed-off-by: Marcin Slusarz <marcin.slusarz@gmail.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/udf_fs.h
+++ include/linux/udf_fs.h
@@ -40,11 +40,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifdef UDFFS_DEBUG
 #define udf_debug(f, a...) \
-	{ \
+	do { \
 		printk (KERN_DEBUG "UDF-fs DEBUG %s:%d:%s: ", \
 			__FILE__, __LINE__, __FUNCTION__); \
 		printk (f, ##a); \
-	}
+	} while (0)
 #else
 #define udf_debug(f, a...) /**/
 #endif
