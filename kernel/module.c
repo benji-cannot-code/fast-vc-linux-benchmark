490185942d87e67031d609d915b27a3af0528ce1
Andrew Morton <akpm@linux-foundation.org>
2008-02-08 04:18:43 -0800

    fix "modules: make module_address_lookup() safe"
    Get the constness right, avoid nasty cast.
    
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Kyle McMartin <kyle@mcmartin.ca>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/module.c
+++ kernel/module.c
@@ -2254,7 +2254,7 @@ static const char *get_ksymbol(struct module *mod,
 
 /* For kallsyms to ask for address resolution.  NULL means not found.  Careful
  * not to lock to avoid deadlock on oopses, simply disable preemption. */
-char *module_address_lookup(unsigned long addr,
+const char *module_address_lookup(unsigned long addr,
 			    unsigned long *size,
 			    unsigned long *offset,
 			    char **modname,
@@ -2279,7 +2279,7 @@ char *module_address_lookup(unsigned long addr,
 		ret = namebuf;
 	}
 	preempt_enable();
-	return (char *)ret;
+	return ret;
 }
 
 int lookup_module_symbol_name(unsigned long addr, char *symname)
