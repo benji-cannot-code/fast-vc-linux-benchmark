f12c13135fe3a1b7f7a4202c7622418013a46e38
Jeff Dike <jdike@addtoit.com>
2008-02-08 04:22:07 -0800

    uml: runtime host VMSPLIT detection
    Calculate TASK_SIZE at run-time by figuring out the host's VMSPLIT - this is
    needed on i386 if UML is to run on hosts with varying VMSPLITs without
    recompilation.
    
    TASK_SIZE is now defined in terms of a variable, task_size.  This gets rid of
    an include of pgtable.h from processor.h, which can cause include loops.
    
    On i386, task_size is calculated early in boot by probing the address space in
    a binary search to figure out where the boundary between usable and non-usable
    memory is.  This tries to make sure that a page that is considered to be in
    userspace is, or can be made, read-write.  I'm concerned about a system-global
    VDSO page in kernel memory being hit and considered to be a userspace page.
    
    On x86_64, task_size is just the old value of CONFIG_TOP_ADDR.
    
    A bunch of config variable are gone now.  CONFIG_TOP_ADDR is directly replaced
    by TASK_SIZE.  NEST_LEVEL is gone since the relocation of the stubs makes it
    irrelevant.  All the HOST_VMSPLIT stuff is gone.  All references to these in
    arch/um/Makefile are also gone.
    
    I noticed and fixed a missing extern in os.h when adding os_get_task_size.
    
    Note: This has been revised to fix the 32-bit UML on 64-bit host bug that
    Miklos ran into.
    
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Cc: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-um/fixmap.h
+++ include/asm-um/fixmap.h
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __UM_FIXMAP_H
 #define __UM_FIXMAP_H
 
+#include <asm/processor.h>
 #include <asm/system.h>
 #include <asm/kmap_types.h>
 #include <asm/archparam.h>
@@ -58,7 +59,7 @@ extern void __set_fixmap (enum fixed_addresses idx,
  * at the top of mem..
  */
 
-#define FIXADDR_TOP	(CONFIG_TOP_ADDR - 2 * PAGE_SIZE)
+#define FIXADDR_TOP	(TASK_SIZE - 2 * PAGE_SIZE)
 #define FIXADDR_SIZE	(__end_of_fixed_addresses << PAGE_SHIFT)
 #define FIXADDR_START	(FIXADDR_TOP - FIXADDR_SIZE)
 
--- include/asm-um/processor-generic.h
+++ include/asm-um/processor-generic.h
@@ -12,7 +12,6 @@ struct pt_regs;
 struct task_struct;
 
 #include "asm/ptrace.h"
-#include "asm/pgtable.h"
 #include "registers.h"
 #include "sysdep/archsetjmp.h"
 
@@ -93,7 +92,9 @@ static inline void mm_copy_segments(struct mm_struct *from_mm,
 /*
  * User space process size: 3GB (default).
  */
-#define TASK_SIZE (CONFIG_TOP_ADDR & PGDIR_MASK)
+extern unsigned long task_size;
+
+#define TASK_SIZE (task_size)
 
 #undef STACK_TOP
 #undef STACK_TOP_MAX
