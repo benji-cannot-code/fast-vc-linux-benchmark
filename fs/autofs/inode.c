804c42b9abef70923a3fe1ab4cf8e0263eac0254
Miklos Szeredi <mszeredi@suse.cz>
2008-02-08 04:21:38 -0800

    mount options: fix autofs
    Add a .show_options super operation to autofs.
    
    Use generic_show_options() and save the complete option string in
    autofs_fill_super().
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Acked-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/autofs/inode.c
+++ fs/autofs/inode.c
@@ -55,6 +55,7 @@ out_kill_sb:
 
 static const struct super_operations autofs_sops = {
 	.statfs		= simple_statfs,
+	.show_options	= generic_show_options,
 };
 
 enum {Opt_err, Opt_fd, Opt_uid, Opt_gid, Opt_pgrp, Opt_minproto, Opt_maxproto};
@@ -140,6 +141,8 @@ int autofs_fill_super(struct super_block *s, void *data, int silent)
 	struct autofs_sb_info *sbi;
 	int minproto, maxproto;
 	pid_t pgid;
+
+	save_mount_options(s, data);
 
 	sbi = kzalloc(sizeof(*sbi), GFP_KERNEL);
 	if (!sbi)
