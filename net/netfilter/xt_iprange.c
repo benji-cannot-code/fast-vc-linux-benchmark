d15f978a2e376a2606b7105c20a5280eaa69bf5d
Jan Engelhardt <jengelh@computergmbh.de>
2008-02-07 17:57:11 -0800

    [NETFILTER]: xt_iprange: add missing #include
    Signed-off-by: Jan Engelhardt <jengelh@computergmbh.de>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/netfilter/xt_iprange.c
+++ net/netfilter/xt_iprange.c
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ip.h>
 #include <linux/ipv6.h>
 #include <linux/netfilter/x_tables.h>
+#include <linux/netfilter/xt_iprange.h>
 #include <linux/netfilter_ipv4/ipt_iprange.h>
 
 static bool
