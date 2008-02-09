04e575f3e68fa0c2b68c89d77ed31e71c2e0698b
Martin Schwidefsky <schwidefsky@de.ibm.com>
2008-02-09 18:24:35 +0100

    [S390] 1K/2K page table pages.
    This patch implements 1K/2K page table pages for s390.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/asm-s390/mmu.h
+++ include/asm-s390/mmu.h
@@ -2,7 +2,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __MMU_H
 #define __MMU_H
 
-/* Default "unsigned long" context */
-typedef unsigned long mm_context_t;
+typedef struct {
+	struct list_head crst_list;
+	struct list_head pgtable_list;
+	unsigned long asce_bits;
+	int noexec;
+} mm_context_t;
 
 #endif
