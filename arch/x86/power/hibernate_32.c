08401398a21c69881c3b5e8e8bf220d406f1d249
Rafael J. Wysocki <rjw@sisk.pl>
2008-02-09 23:24:09 +0100

    x86 PM: rename 32-bit files in arch/x86/power
    Rename cpu.c, suspend.c and swsusp.S in arch/x86/power to cpu_32.c,
    hibernate_32.c and hibernate_asm_32.S, respectively, and update the
    purpose and copyright information in these files.
    
    Update the Makefile in arch/x86/power to reflect the above changes.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/power/hibernate_32.c
+++ arch/x86/power/hibernate_32.c
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Suspend support specific for i386 - temporary page tables
+ * Hibernation support specific for i386 - temporary page tables
  *
  * Distribute under GPLv2
  *
