00afabdbd7a67d0fb0713399ee2e1302cb1ebb62
Rafael J. Wysocki <rjw@sisk.pl>
2008-02-09 23:24:09 +0100

    x86 PM: move 64-bit hibernation files to arch/x86/power
    Move arch/x86/kernel/suspend_64.c to arch/x86/power .
    
    Move arch/x86/kernel/suspend_asm_64.S to arch/x86/power
    as hibernate_asm_64.S .
    
    Update purpose and copyright information in
    arch/x86/power/suspend_64.c and
    arch/x86/power/hibernate_asm_64.S .
    
    Update the Makefiles in arch/x86, arch/x86/kernel and
    arch/x86/power to reflect the above changes.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/power/suspend_64.c
+++ arch/x86/power/suspend_64.c
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * Suspend support specific for i386.
+ * Suspend and hibernation support for x86-64
  *
  * Distribute under GPLv2
  *
+ * Copyright (c) 2007 Rafael J. Wysocki <rjw@sisk.pl>
  * Copyright (c) 2002 Pavel Machek <pavel@suse.cz>
  * Copyright (c) 2001 Patrick Mochel <mochel@osdl.org>
  */
@@ -64,7 +65,7 @@ static void __save_processor_state(struct saved_context *ctxt)
 	mtrr_save_fixed_ranges(NULL);
 
 	/*
-	 * control registers 
+	 * control registers
 	 */
 	rdmsrl(MSR_EFER, ctxt->efer);
 	ctxt->cr0 = read_cr0();
