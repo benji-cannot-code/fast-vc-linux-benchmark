d564fae1336d5bb748b38f96362e98af662e12c9
Rafael J. Wysocki <rjw@sisk.pl>
2008-02-09 23:24:09 +0100

    x86 PM: update stale comments
    In some suspend and hibernation files in arch/x86/power there are
    comments referring to arch/x86-64 and arch/i386 .  Update them to
    reflect the current code layout.
    
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/power/hibernate_64.c
+++ arch/x86/power/hibernate_64.c
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* References to section boundaries */
 extern const void __nosave_begin, __nosave_end;
 
-/* Defined in arch/x86_64/kernel/suspend_asm.S */
+/* Defined in hibernate_asm_64.S */
 extern int restore_image(void);
 
 /*
