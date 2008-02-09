e0bb0a8c268b1db73ea7dace2b5570b1d41d8179
Martin Schwidefsky <schwidefsky@de.ibm.com>
2008-02-09 18:24:29 +0100

    [S390] Fix __ffs_word_loop/__ffz_word_loop inlnie assembly.
    The black art of inline assemblies.. The new __ffs_word_loop/
    __ffz_word_loop inline assemblies need an early clobber for the
    two input/output variables.
    
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>

--- include/asm-s390/bitops.h
+++ include/asm-s390/bitops.h
@@ -473,7 +473,7 @@ static inline unsigned long __ffz_word_loop(const unsigned long *addr,
 		"	brct	%1,0b\n"
 		"1:\n"
 #endif
-		: "+a" (bytes), "+d" (size)
+		: "+&a" (bytes), "+&d" (size)
 		: "d" (-1UL), "a" (addr), "m" (*(addrtype *) addr)
 		: "cc" );
 	return bytes;
@@ -508,7 +508,7 @@ static inline unsigned long __ffs_word_loop(const unsigned long *addr,
 		"	brct	%1,0b\n"
 		"1:\n"
 #endif
-		: "+a" (bytes), "+a" (size)
+		: "+&a" (bytes), "+&a" (size)
 		: "d" (0UL), "a" (addr), "m" (*(addrtype *) addr)
 		: "cc" );
 	return bytes;
