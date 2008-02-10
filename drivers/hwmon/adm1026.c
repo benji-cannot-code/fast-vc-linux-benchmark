89fe0bc82b6168a5853f14c647a69c46962e9987
Jean Delvare <khali@linux-fr.org>
2008-02-10 19:01:15 +0100

    hwmon: (adm1026) Properly terminate sysfs groups
    The missing NULL at the end of two sysfs file groups causes a kernel
    crash when calling sysfs_create_group().
    
    Signed-off-by: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Mark M. Hoffman <mhoffman@lightlink.com>

--- drivers/hwmon/adm1026.c
+++ drivers/hwmon/adm1026.c
@@ -1625,6 +1625,7 @@ static struct attribute *adm1026_attributes_temp3[] = {
 	&dev_attr_temp3_crit_enable.attr,
 	&dev_attr_temp3_auto_point1_pwm.attr,
 	&dev_attr_temp3_auto_point2_pwm.attr,
+	NULL
 };
 
 static const struct attribute_group adm1026_group_temp3 = {
@@ -1640,6 +1641,7 @@ static struct attribute *adm1026_attributes_in8_9[] = {
 	&sensor_dev_attr_in9_max.dev_attr.attr,
 	&sensor_dev_attr_in9_min.dev_attr.attr,
 	&sensor_dev_attr_in9_alarm.dev_attr.attr,
+	NULL
 };
 
 static const struct attribute_group adm1026_group_in8_9 = {
