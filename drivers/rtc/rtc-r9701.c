854fb33b38187dc4a24863e1b04641fd6e745aeb
S.Çağlar Onur <caglar@pardus.org.tr>
2008-02-10 05:10:48 +0200

    rtc-r9701.c: silence compiler warning
    Commit 75b6102257874a4ea796af686de2f72cfa0452f9 ("rtc: add support for
    Epson RTC-9701JE V4") introduced the warning
    
      drivers/rtc/rtc-r9701.c: In function `r9701_get_datetime':
      drivers/rtc/rtc-r9701.c:74: warning: unused variable `time'
    
    Signed-off-by: S.Çağlar Onur <caglar@pardus.org.tr>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/rtc/rtc-r9701.c
+++ drivers/rtc/rtc-r9701.c
@@ -72,7 +72,6 @@ static int read_regs(struct device *dev, unsigned char *regs, int no_regs)
 
 static int r9701_get_datetime(struct device *dev, struct rtc_time *dt)
 {
-	unsigned long time;
 	int ret;
 	unsigned char buf[] = { RSECCNT, RMINCNT, RHRCNT,
 				RDAYCNT, RMONCNT, RYRCNT };
