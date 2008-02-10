0d9a9dbbb7999aef65c4548410ae022c2ee5d995
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-02-11 00:32:13 +0100

    ide: remove stale version number
    On Thursday 03 January 2008, Robert Hancock wrote:
    
    [...]
    
    > How about getting rid of this stupid thing in drivers/ide/ide.c:
    > 
    > #define       REVISION        "Revision: 7.00alpha2"
    > 
    > which is used in:
    > 
    > printk(KERN_INFO "Uniform Multi-Platform E-IDE driver " REVISION "\n");
    > 
    > It's been 7.00alpha2 for god knows how long, so clearly this version 
    > number is not useful..
    
    Cc: Robert Hancock <hancockr@shaw.ca>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/ide.c
+++ drivers/ide/ide.c
@@ -45,8 +45,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  *  inspiration from lots of linux users, esp.  hamish@zot.apana.org.au
  */
 
-#define	REVISION	"Revision: 7.00alpha2"
-
 #define _IDE_C			/* Tell ide.h it's really us */
 
 #include <linux/module.h>
@@ -1619,7 +1617,7 @@ static int __init ide_init(void)
 {
 	int ret;
 
-	printk(KERN_INFO "Uniform Multi-Platform E-IDE driver " REVISION "\n");
+	printk(KERN_INFO "Uniform Multi-Platform E-IDE driver\n");
 	system_bus_speed = ide_system_bus_speed();
 
 	printk(KERN_INFO "ide: Assuming %dMHz system bus speed "
