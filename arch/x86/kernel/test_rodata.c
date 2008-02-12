06bd65055fd248664d45e7a23ff3f9806ad4b2d4
Harvey Harrison <harvey.harrison@gmail.com>
2008-02-12 12:12:01 -0800

    x86: include proper prototypes for rodata_test
    extern should not appear in C files.  Also, the definitions
    do not match the prototype currently, not sure what way you
    want to go with this, I've switched the prototype to return
    int, but I can see going to the void return as well.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/test_rodata.c
+++ arch/x86/kernel/test_rodata.c
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * of the License.
  */
 #include <linux/module.h>
+#include <asm/cacheflush.h>
 #include <asm/sections.h>
-extern int rodata_test_data;
 
 int rodata_test(void)
 {
