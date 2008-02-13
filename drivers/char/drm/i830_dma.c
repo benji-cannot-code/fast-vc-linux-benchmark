f684f8aa6004173e5aa0b7f25a58a7cd8fef59ed
Harvey Harrison <harvey.harrison@gmail.com>
2008-02-13 15:03:17 -0800

    Final removal of FASTCALL()/fastcall
    All users are gone, remove definitions and comments referring
    to them.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Acked-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/drm/i830_dma.c
+++ drivers/char/drm/i830_dma.c
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "i830_drm.h"
 #include "i830_drv.h"
 #include <linux/interrupt.h>	/* For task queue support */
-#include <linux/pagemap.h>	/* For FASTCALL on unlock_page() */
+#include <linux/pagemap.h>
 #include <linux/delay.h>
 #include <asm/uaccess.h>
 
