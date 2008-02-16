d02be81e8035bcb61241edbd8b2415a8858e828d
Artem Bityutskiy <Artem.Bityutskiy@nokia.com>
2008-02-16 15:42:52 +0200

    UBI: make ubi-header.h local
    The new trend in linux is not to store headers which define
    on-media format in the include/ directory, but instead, store
    them locally. This is because these headers "do not define any
    kernel<->userspace interface".
    
    Do so for UBI as well.
    
    Signed-off-by: Artem Bityutskiy <Artem.Bityutskiy@nokia.com>

--- drivers/mtd/ubi/ubi.h
+++ drivers/mtd/ubi/ubi.h
@@ -38,10 +38,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/string.h>
 #include <linux/vmalloc.h>
 #include <linux/mtd/mtd.h>
-
-#include <mtd/ubi-header.h>
 #include <linux/mtd/ubi.h>
 
+#include "ubi-media.h"
 #include "scan.h"
 #include "debug.h"
 
