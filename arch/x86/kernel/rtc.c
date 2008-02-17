51ef966aebe46c27f721a9ac40c7fc0e1659ddcc
David P. Reed <dpreed@reed.com>
2008-02-17 16:56:39 -0500

    x86: fix cmos read and write to not use inb_p and outb_p
    fix code to access CMOS rtc registers so that it does not use inb_p and
    outb_p routines, which are deprecated.  Extensive research on all known
    CMOS RTC chipset timing shows that there is no need for a delay in
    accessing the registers of these chips even on old machines. These chipa
    are never on an expansion bus, but have always been "motherboard"
    resources, either in the processor chipset or explicitly on the
    motherboard, and they are not part of the ISA/LPC or PCI buses, so
    delays should not be based on bus timing. The reason to fix it:
    
     1) port 80 writes often hang some laptops that use ENE EC chipsets,
        esp. those designed and manufactured by Quanta for HP;
    
     2) RTC accesses are timing sensitive, and extra microseconds may matter;
    
     3) the new "io_delay" function is calibrated by expansion bus timing needs,
        thus is not appropriate for access to CMOS rtc registers.
    
    Signed-off-by: David P. Reed <dpreed@reed.com>
    Acked-by: Alan Cox <alan@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/rtc.c
+++ arch/x86/kernel/rtc.c
@@ -147,8 +147,8 @@ unsigned char rtc_cmos_read(unsigned char addr)
 	unsigned char val;
 
 	lock_cmos_prefix(addr);
-	outb_p(addr, RTC_PORT(0));
-	val = inb_p(RTC_PORT(1));
+	outb(addr, RTC_PORT(0));
+	val = inb(RTC_PORT(1));
 	lock_cmos_suffix(addr);
 	return val;
 }
@@ -157,8 +157,8 @@ EXPORT_SYMBOL(rtc_cmos_read);
 void rtc_cmos_write(unsigned char val, unsigned char addr)
 {
 	lock_cmos_prefix(addr);
-	outb_p(addr, RTC_PORT(0));
-	outb_p(val, RTC_PORT(1));
+	outb(addr, RTC_PORT(0));
+	outb(val, RTC_PORT(1));
 	lock_cmos_suffix(addr);
 }
 EXPORT_SYMBOL(rtc_cmos_write);
