e0b18de243ec9080d470b0b47363be5141ccdcd3
Ivo van Doorn <ivdoorn@gmail.com>
2008-02-17 17:34:26 +0100

    rt2x00: Rename dscape -> mac80211
    The dscape stack was renamed to mac80211 a long time ago,
    we are long overdue with fixing all comments to reflect this.
    
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/rt2x00/rt2500usb.h
+++ drivers/net/wireless/rt2x00/rt2500usb.h
@@ -788,8 +788,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define RXD_W3_EIV			FIELD32(0xffffffff)
 
 /*
- * Macro's for converting txpower from EEPROM to dscape value
- * and from dscape value to register value.
+ * Macro's for converting txpower from EEPROM to mac80211 value
+ * and from mac80211 value to register value.
  */
 #define MIN_TXPOWER	0
 #define MAX_TXPOWER	31
