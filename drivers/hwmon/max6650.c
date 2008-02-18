eda8631660e4eee8284449cba5969bea8a556279
Mark M. Hoffman <mhoffman@lightlink.com>
2008-02-17 22:28:03 -0500

    hwmon: normal_i2c arrays should be const
    Signed-off-by: Mark M. Hoffman <mhoffman@lightlink.com>

--- drivers/hwmon/max6650.c
+++ drivers/hwmon/max6650.c
@@ -45,7 +45,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * Addresses to scan. There are four disjoint possibilities, by pin config.
  */
 
-static unsigned short normal_i2c[] = {0x1b, 0x1f, 0x48, 0x4b, I2C_CLIENT_END};
+static const unsigned short normal_i2c[] = {0x1b, 0x1f, 0x48, 0x4b,
+						I2C_CLIENT_END};
 
 /*
  * Insmod parameters
