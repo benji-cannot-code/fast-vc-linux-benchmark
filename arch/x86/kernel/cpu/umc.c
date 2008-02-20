d084bd15c13286ead39d980d62b2ffc8144677c4
Paolo Ciarrocchi <paolo.ciarrocchi@gmail.com>
2008-02-21 00:18:34 +0100

    x86: coding style fixes to arch/x86/kernel/cpu/umc.c
    Before:
       total: 3 errors, 1 warnings, 23 lines checked
    After:
       total: 0 errors, 0 warnings, 25 lines checked
    
    No code changed:
    
    arch/x86/kernel/cpu/umc.o:
    
       text	   data	    bss	    dec	    hex	filename
         24	    616	      0	    640	    280	umc.o.before
         24	    616	      0	    640	    280	umc.o.after
    
    md5:
       e8daa3eaed0963a0cdd2e83c2e1f9823  umc.o.before.asm
       e8daa3eaed0963a0cdd2e83c2e1f9823  umc.o.after.asm
    
    Signed-off-by: Paolo Ciarrocchi <paolo.ciarrocchi@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/umc.c
+++ arch/x86/kernel/cpu/umc.c
@@ -4,17 +4,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/processor.h>
 #include "cpu.h"
 
-/* UMC chips appear to be only either 386 or 486, so no special init takes place.
+/*
+ * UMC chips appear to be only either 386 or 486,
+ * so no special init takes place.
  */
 
 static struct cpu_dev umc_cpu_dev __cpuinitdata = {
 	.c_vendor	= "UMC",
-	.c_ident 	= { "UMC UMC UMC" },
+	.c_ident	= { "UMC UMC UMC" },
 	.c_models = {
 		{ .vendor = X86_VENDOR_UMC, .family = 4, .model_names =
-		  { 
-			  [1] = "U5D", 
-			  [2] = "U5S", 
+		  {
+			  [1] = "U5D",
+			  [2] = "U5S",
 		  }
 		},
 	},
