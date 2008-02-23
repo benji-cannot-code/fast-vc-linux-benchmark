83b65939af47cf49eaeb3a020ae4a076c5244729
Srinivasa Ds <srinivasa@in.ibm.com>
2008-02-23 15:24:04 -0800

    kprobes: refuse kprobe insertion on add/sub_preempt_counter()
    Kprobes makes use of preempt_disable(),preempt_enable_noresched() and these
    functions inturn call add/sub_preempt_count().  So we need to refuse user from
    inserting probe in to these functions.
    
    This patch disallows user from probing add/sub_preempt_count().
    
    Signed-off-by: Srinivasa DS <srinivasa@in.ibm.com>
    Acked-by: Ananth N Mavinakayanahalli <ananth@in.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/sched.c
+++ kernel/sched.c
@@ -3767,7 +3767,7 @@ void scheduler_tick(void)
 
 #if defined(CONFIG_PREEMPT) && defined(CONFIG_DEBUG_PREEMPT)
 
-void add_preempt_count(int val)
+void __kprobes add_preempt_count(int val)
 {
 	/*
 	 * Underflow?
@@ -3783,7 +3783,7 @@ void add_preempt_count(int val)
 }
 EXPORT_SYMBOL(add_preempt_count);
 
-void sub_preempt_count(int val)
+void __kprobes sub_preempt_count(int val)
 {
 	/*
 	 * Underflow?
