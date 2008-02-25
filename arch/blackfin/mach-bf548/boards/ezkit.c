b3de12a6df63396ecef77448aae91160536d88b8
Mike Frysinger <vapier.adi@gmail.com>
2008-02-25 11:38:11 +0800

    [Blackfin] arch: fix bug - set right partition size in the board files
     - set default u-boot partition size to 256k
     - modify the offset with the size change
     - use mtd defines (append for offset and full for size)
       where applicable rather than churning constants when we dont have to
    
    Signed-off-by: Grace Pan <grace.pan@analog.com>
    Signed-off-by: Mike Frysinger <vapier.adi@gmail.com>
    Signed-off-by: Bryan Wu <cooloney@kernel.org>

--- arch/blackfin/mach-bf548/boards/ezkit.c
+++ arch/blackfin/mach-bf548/boards/ezkit.c
@@ -286,8 +286,8 @@ static struct mtd_partition partition_info[] = {
 	},
 	{
 		.name = "File System",
-		.offset = 4 * SIZE_1M,
-		.size = (256 - 4) * SIZE_1M,
+		.offset = MTDPART_OFS_APPEND,
+		.size = MTDPART_SIZ_FULL,
 	},
 };
 
@@ -334,7 +334,7 @@ static struct platform_device bf54x_sdh_device = {
 static struct mtd_partition ezkit_partitions[] = {
 	{
 		.name       = "Bootloader",
-		.size       = 0x20000,
+		.size       = 0x40000,
 		.offset     = 0,
 	}, {
 		.name       = "Kernel",
@@ -382,8 +382,8 @@ static struct mtd_partition bfin_spi_flash_partitions[] = {
 		.mask_flags = MTD_CAP_ROM
 	}, {
 		.name = "linux kernel",
-		.size = 0x1c0000,
-		.offset = 0x40000
+		.size = MTDPART_SIZ_FULL,
+		.offset = MTDPART_OFS_APPEND,
 	}
 };
 
