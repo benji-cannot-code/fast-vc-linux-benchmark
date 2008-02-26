98923f798aa3f52972c94ce043f3c61fc1d3c9df
Matthew Wilcox <matthew@wil.cx>
2008-02-26 10:00:17 -0500

    include: Remove unnecessary inclusions of asm/semaphore.h
    None of these files use any of the functionality promised by
    asm/semaphore.h.  It's possible that they (or some user of them) rely
    on it dragging in some unrelated header file, but I can't build all
    these files, so we'll have to fix any build failures as they come up.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>

--- include/linux/jbd2.h
+++ include/linux/jbd2.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/bit_spinlock.h>
 #include <linux/mutex.h>
 #include <linux/timer.h>
-
-#include <asm/semaphore.h>
 #endif
 
 #define journal_oom_retry 1
