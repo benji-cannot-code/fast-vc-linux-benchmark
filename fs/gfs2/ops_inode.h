5f1782d8f856e3f05f58907d9c6dd8ab64e2565b
Denis Cheng <crquan@gmail.com>
2008-02-26 15:25:03 +0800

    [GFS2] remove gfs2_dev_iops
    struct inode_operations gfs2_dev_iops is always the same as gfs2_file_iops,
    since Jan 2006, when GFS2 merged into mainstream kernel.
    
    So one of them could be removed.
    
    Signed-off-by: Denis Cheng <crquan@gmail.com>
    Signed-off-by: Steven Whitehouse <swhiteho@redhat.com>

--- fs/gfs2/ops_inode.h
+++ fs/gfs2/ops_inode.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 extern const struct inode_operations gfs2_file_iops;
 extern const struct inode_operations gfs2_dir_iops;
 extern const struct inode_operations gfs2_symlink_iops;
-extern const struct inode_operations gfs2_dev_iops;
 extern const struct file_operations gfs2_file_fops;
 extern const struct file_operations gfs2_dir_fops;
 extern const struct file_operations gfs2_file_fops_nolock;
