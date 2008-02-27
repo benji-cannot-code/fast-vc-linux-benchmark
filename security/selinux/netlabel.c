12656bedf58e05550ce94afb9071a52078e757eb
Adrian Bunk <bunk@kernel.org>
2008-02-27 23:20:42 +0200

    selinux: selinux/netlabel.c should #include "netlabel.h"
    Every file should include the headers containing the externs for its
    global code.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Acked-by: Paul Moore <paul.moore@hp.com>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/netlabel.c
+++ security/selinux/netlabel.c
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "objsec.h"
 #include "security.h"
+#include "netlabel.h"
 
 /**
  * selinux_netlbl_sidlookup_cached - Cache a SID lookup
