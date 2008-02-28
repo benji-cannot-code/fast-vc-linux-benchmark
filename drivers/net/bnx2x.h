4748131738e30dcbffcc8ff7c92ff436a7c95d9e
Eliezer Tamir <eliezert@broadcom.com>
2008-02-28 11:49:42 -0800

    [BNX2X]: Spelling fixes
    Signed-off-by: Eliezer Tamir <eliezert@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x.h
+++ drivers/net/bnx2x.h
@@ -518,7 +518,7 @@ struct bnx2x {
 	 */
 	u8      		stat_pending;
 
-	/* End of fileds used in the performance code paths */
+	/* End of fields used in the performance code paths */
 
 	int     		panic;
 	int     		msglevel;
