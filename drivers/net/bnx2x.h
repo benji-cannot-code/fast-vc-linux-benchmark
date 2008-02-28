9ebef9775cecbe19c3b098f8c82657f70990afcf
Eliezer Tamir <eliezert@broadcom.com>
2008-02-28 11:53:13 -0800

    [BNX2X]: Correct RX filtering and MC configuration
    The configuration of RX filtering needed the following corrections:
    
    Drop flags need to be set per Rx queue.
    
    Have to tell the microcode to collect drop stats, and properly wait
    for them to complete when going down.
    
    Sometimes we failed to detect proper completion due to a logical error
    in the wait loop.
    
    Signed-off-by: Eliezer Tamir <eliezert@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/net/bnx2x.h
+++ drivers/net/bnx2x.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* bnx2x.h: Broadcom Everest network driver.
  *
- * Copyright (c) 2007 Broadcom Corporation
+ * Copyright (c) 2007-2008 Broadcom Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -432,8 +432,6 @@ struct bnx2x_fastpath {
 #define BNX2X_FP_STATE_OPEN     	0xa0000
 #define BNX2X_FP_STATE_HALTING  	0xb0000
 #define BNX2X_FP_STATE_HALTED   	0xc0000
-#define BNX2X_FP_STATE_DELETED  	0xd0000
-#define BNX2X_FP_STATE_CLOSE_IRQ	0xe0000
 
 	int     		index;
 
@@ -514,7 +512,6 @@ struct bnx2x {
 	struct eth_spe  	*spq;
 	dma_addr_t      	spq_mapping;
 	u16     		spq_prod_idx;
-	u16     		dsb_sp_prod_idx;
 	struct eth_spe  	*spq_prod_bd;
 	struct eth_spe  	*spq_last_bd;
 	u16     		*dsb_sp_prod;
