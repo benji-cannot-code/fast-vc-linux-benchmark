f8ceb2538ba59ac8b789e167e3cdbd5039ec4106
Yinghai Lu <Yinghai.Lu@Sun.COM>
2008-02-28 23:56:50 -0800

    x86 pci: remove checking type for mmconfig probe
    doesn't need to check if it is type1 or type2, we can use raw_pci_ops
    directly.
    
    also make pci_direct_conf1 static again.
    
    anyway is there system with type 2 and mmconf support?
    
    Signed-off-by: Yinghai Lu <yinghai.lu@sun.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/pci/init.c
+++ arch/x86/pci/init.c
@@ -7,14 +7,13 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
    in the right sequence from here. */
 static __init int pci_access_init(void)
 {
-	int type __maybe_unused = 0;
-
 #ifdef CONFIG_PCI_DIRECT
+	int type = 0;
+
 	type = pci_direct_probe();
 #endif
-	pci_mmcfg_early_init(type);
-	if (raw_pci_ops)
-		return 0;
+	pci_mmcfg_early_init();
+
 #ifdef CONFIG_PCI_BIOS
 	pci_pcbios_init();
 #endif
@@ -27,7 +26,7 @@ static __init int pci_access_init(void)
 #ifdef CONFIG_PCI_DIRECT
 	pci_direct_init(type);
 #endif
-	if (!raw_pci_ops)
+	if (!raw_pci_ops && !raw_pci_ext_ops)
 		printk(KERN_ERR
 		"PCI: Fatal: No config space access function found\n");
 
