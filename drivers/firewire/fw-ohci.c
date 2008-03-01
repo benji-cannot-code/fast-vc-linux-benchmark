91b4bef60b895f85921c0ae59be18e68f47eb609
Stefan Richter <stefanr@s5r6.in-berlin.de>
2008-03-01 12:34:50 +0100

    firewire: fw-ohci: switch on bus power after resume on PPC PMac
    The platform feature calls in the suspend method switched off cable
    power, but the calls in the resume method did not switch it back on.
    
    Add the necessary feature call to .resume.  Also add the corresponding
    call to .suspend to make .suspend's behavior explicitly the same on all
    PMacs.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>

--- drivers/firewire/fw-ohci.c
+++ drivers/firewire/fw-ohci.c
@@ -2252,8 +2252,10 @@ static int pci_suspend(struct pci_dev *pdev, pm_message_t state)
 	if (machine_is(powermac)) {
 		struct device_node *ofn = pci_device_to_OF_node(pdev);
 
-		if (ofn)
+		if (ofn) {
 			pmac_call_feature(PMAC_FTR_1394_ENABLE, ofn, 0, 0);
+			pmac_call_feature(PMAC_FTR_1394_CABLE_POWER, ofn, 0, 0);
+		}
 	}
 #endif /* CONFIG_PPC_PMAC */
 
@@ -2270,8 +2272,10 @@ static int pci_resume(struct pci_dev *pdev)
 	if (machine_is(powermac)) {
 		struct device_node *ofn = pci_device_to_OF_node(pdev);
 
-		if (ofn)
+		if (ofn) {
+			pmac_call_feature(PMAC_FTR_1394_CABLE_POWER, ofn, 0, 1);
 			pmac_call_feature(PMAC_FTR_1394_ENABLE, ofn, 0, 1);
+		}
 	}
 #endif /* CONFIG_PPC_PMAC */
 
