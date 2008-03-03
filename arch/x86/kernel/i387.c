b436b2d60e83f351e4e2a11657a730ecebce2954
Suresh Siddha <suresh.b.siddha@intel.com>
2008-03-03 13:01:08 -0800

    x86, i387: fix ptrace leakage using init_fpu()
    This bug got introduced by the recent i387 merge:
    
      commit 4421011120b2304e5c248ae4165a2704588aedf1
      Author: Roland McGrath <roland@redhat.com>
      Date:   Wed Jan 30 13:31:50 2008 +0100
    
          x86: x86 i387 user_regset
    
    Current usage of unlazy_fpu() in ptrace specific routines is wrong.
    unlazy_fpu() will not init fpu if the task never used math. So the
    ptrace calls can expose the parent tasks FPU data in some cases.
    
    Replace it with the init_fpu() which will init the math state, if the
    task never used math before.
    
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Acked-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/i387.c
+++ arch/x86/kernel/i387.c
@@ -133,7 +133,7 @@ int xfpregs_get(struct task_struct *target, const struct user_regset *regset,
 	if (!cpu_has_fxsr)
 		return -ENODEV;
 
-	unlazy_fpu(target);
+	init_fpu(target);
 
 	return user_regset_copyout(&pos, &count, &kbuf, &ubuf,
 				   &target->thread.i387.fxsave, 0, -1);
@@ -148,7 +148,7 @@ int xfpregs_set(struct task_struct *target, const struct user_regset *regset,
 	if (!cpu_has_fxsr)
 		return -ENODEV;
 
-	unlazy_fpu(target);
+	init_fpu(target);
 	set_stopped_child_used_math(target);
 
 	ret = user_regset_copyin(&pos, &count, &kbuf, &ubuf,
@@ -308,7 +308,7 @@ int fpregs_get(struct task_struct *target, const struct user_regset *regset,
 	if (!HAVE_HWFP)
 		return fpregs_soft_get(target, regset, pos, count, kbuf, ubuf);
 
-	unlazy_fpu(target);
+	init_fpu(target);
 
 	if (!cpu_has_fxsr)
 		return user_regset_copyout(&pos, &count, &kbuf, &ubuf,
@@ -333,7 +333,7 @@ int fpregs_set(struct task_struct *target, const struct user_regset *regset,
 	if (!HAVE_HWFP)
 		return fpregs_soft_set(target, regset, pos, count, kbuf, ubuf);
 
-	unlazy_fpu(target);
+	init_fpu(target);
 	set_stopped_child_used_math(target);
 
 	if (!cpu_has_fxsr)
