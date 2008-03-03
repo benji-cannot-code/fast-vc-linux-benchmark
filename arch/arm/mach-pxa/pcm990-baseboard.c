419e52279ff595fd3303848f2d30b73f52b8f607
eric miao <eric.miao@marvell.com>
2008-03-03 09:44:25 +0800

    [ARM] pxa: separate GPIOs and their mode definitions to pxa2xx-gpio.h
    two reasons:
    1. GPIO namings and their mode definitions are conceptually not part
       of the PXA register definitions
    
    2. this is actually a temporary move in the transition of PXA2xx to
       use MFP-alike APIs (as what PXA3xx is now doing), so that legacy
       code will still work and new code can be added in step by step
    
    Signed-off-by: eric miao <eric.miao@marvell.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-pxa/pcm990-baseboard.c
+++ arch/arm/mach-pxa/pcm990-baseboard.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/ide.h>
 #include <asm/mach/map.h>
 #include <asm/arch/pxa-regs.h>
+#include <asm/arch/pxa2xx-gpio.h>
 #include <asm/arch/mmc.h>
 #include <asm/arch/ohci.h>
 #include <asm/arch/pcm990_baseboard.h>
