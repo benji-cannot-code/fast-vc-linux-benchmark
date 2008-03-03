ff13fd9416654d2a555a05ab6c9092277baee3a9
Glauber Costa <gcosta@redhat.com>
2008-03-03 14:12:58 -0300

    x86: move equal types to common file
    move definitions that are now equal in type from
    smpboot_{32,64}.c to smpboot.c
    
    cpu_callin_map is put temporarily in smp_64.h (already
    exists in smp_32.h), and will soon be merged.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/smp_64.h
+++ include/asm-x86/smp_64.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/thread_info.h>
 
 extern cpumask_t cpu_initialized;
+extern cpumask_t cpu_callin_map;
 
 extern int smp_call_function_mask(cpumask_t mask, void (*func)(void *),
 				  void *info, int wait);
