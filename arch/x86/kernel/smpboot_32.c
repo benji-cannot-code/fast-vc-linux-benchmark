36c9d0d58ac6d67952c51028d85b6ac461d939bd
Glauber Costa <gcosta@redhat.com>
2008-03-03 14:13:05 -0300

    x86: use remove_from_maps in cpu_disable
    it is already used in x86_64. In i386, it only
    removes from cpu_online_map
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/smpboot_32.c
+++ arch/x86/kernel/smpboot_32.c
@@ -1042,6 +1042,11 @@ void __init native_smp_prepare_boot_cpu(void)
 }
 
 #ifdef CONFIG_HOTPLUG_CPU
+static void __ref remove_cpu_from_maps(int cpu)
+{
+	cpu_clear(cpu, cpu_online_map);
+}
+
 int __cpu_disable(void)
 {
 	cpumask_t map = cpu_online_map;
@@ -1067,7 +1072,7 @@ int __cpu_disable(void)
 
 	remove_siblinginfo(cpu);
 
-	cpu_clear(cpu, map);
+	remove_cpu_from_maps(cpu);
 	fixup_irqs(map);
 	/* It's now safe to remove this processor from the online map */
 	cpu_clear(cpu, cpu_online_map);
