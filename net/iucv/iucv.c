9fb27e59e576f514da116751efc90ea5bcea8595
Heiko Carstens <heiko.carstens@de.ibm.com>
2008-03-03 12:12:33 -0800

    iucv: fix build error on !SMP
    Since a5fbb6d1064be885d2a6b82f625186753cf74848
    "KVM: fix !SMP build error" smp_call_function isn't a define anymore
    that folds into nothing but a define that calls up_smp_call_function
    with all parameters. Hence we cannot #ifdef out the unused code
    anymore...
    This seems to be the preferred method, so do this for s390 as well.
    
    net/iucv/iucv.c: In function 'iucv_cleanup_queue':
    net/iucv/iucv.c:657: error: '__iucv_cleanup_queue' undeclared
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
    Signed-off-by: Ursula Braun <braunu@de.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/iucv/iucv.c
+++ net/iucv/iucv.c
@@ -622,7 +622,6 @@ static int iucv_sever_pathid(u16 pathid, u8 userdata[16])
 	return iucv_call_b2f0(IUCV_SEVER, parm);
 }
 
-#ifdef CONFIG_SMP
 /**
  * __iucv_cleanup_queue
  * @dummy: unused dummy argument
@@ -633,7 +632,6 @@ static int iucv_sever_pathid(u16 pathid, u8 userdata[16])
 static void __iucv_cleanup_queue(void *dummy)
 {
 }
-#endif
 
 /**
  * iucv_cleanup_queue
