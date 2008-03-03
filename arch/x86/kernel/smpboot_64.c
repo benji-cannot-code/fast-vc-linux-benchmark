63e874039282632fb01232c4040a1ce4fdf0b5c4
Glauber Costa <gcosta@redhat.com>
2008-03-03 14:12:39 -0300

    x86: unify smp_cpus_done
    definition is moved to common header. x86_64 version is now called
    native_smp_cpus_done
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/smpboot_64.c
+++ arch/x86/kernel/smpboot_64.c
@@ -988,7 +988,7 @@ int __cpuinit native_cpu_up(unsigned int cpu)
 /*
  * Finish the SMP boot.
  */
-void __init smp_cpus_done(unsigned int max_cpus)
+void __init native_smp_cpus_done(unsigned int max_cpus)
 {
 	smp_cleanup_boot();
 	setup_ioapic_dest();
