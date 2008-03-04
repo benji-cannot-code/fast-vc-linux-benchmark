00419ba0f174c7e5645b6b720970fde716f7a683
Daniel Lezcano <dlezcano@fr.ibm.com>
2008-03-03 23:27:06 -0800

    [NETNS][IPV6] ip6_fib - fib6_clean_all handle several network namespaces
    The function fib6_clean_all takes the network namespace as
    parameter. That allows to flush the routes related to a specific
    network namespace.
    
    Signed-off-by: Daniel Lezcano <dlezcano@fr.ibm.com>
    Signed-off-by: Benjamin Thery <benjamin.thery@bull.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -2432,6 +2432,7 @@ static int addrconf_ifdown(struct net_device *dev, int how)
 {
 	struct inet6_dev *idev;
 	struct inet6_ifaddr *ifa, **bifa;
+	struct net *net = dev->nd_net;
 	int i;
 
 	ASSERT_RTNL();
@@ -2439,7 +2440,7 @@ static int addrconf_ifdown(struct net_device *dev, int how)
 	if (dev == init_net.loopback_dev && how == 1)
 		how = 0;
 
-	rt6_ifdown(dev);
+	rt6_ifdown(net, dev);
 	neigh_ifdown(&nd_tbl, dev);
 
 	idev = __in6_dev_get(dev);
