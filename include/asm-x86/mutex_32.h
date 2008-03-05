0d7d2819191c1f7c17789121e1e660ecbad521d1
Joe Perches <joe@perches.com>
2008-03-04 16:46:27 -0800

    x86: include/asm-x86/mutex_32.h - use angle brackets for include
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/mutex_32.h
+++ include/asm-x86/mutex_32.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _ASM_MUTEX_H
 #define _ASM_MUTEX_H
 
-#include "asm/alternative.h"
+#include <asm/alternative.h>
 
 /**
  *  __mutex_fastpath_lock - try to take the lock by moving the count
