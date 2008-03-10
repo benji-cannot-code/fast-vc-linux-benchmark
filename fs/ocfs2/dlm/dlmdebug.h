955ce9f4e420be0e3496cf093869c03e47bdf62a
Sunil Mushran <sunil.mushran@oracle.com>
2008-03-10 15:16:24 -0700

    ocfs2/dlm: Dumps the lockres' into a debugfs file
    This patch dumps all the lockres' alongwith all the locks into
    a debugfs file. Useful for debugging.
    
    Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
    Signed-off-by: Joel Becker <joel.becker@oracle.com>
    Signed-off-by: Mark Fasheh <mfasheh@suse.com>

--- fs/ocfs2/dlm/dlmdebug.h
+++ fs/ocfs2/dlm/dlmdebug.h
@@ -31,11 +31,19 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct dlm_debug_ctxt {
 	struct kref debug_refcnt;
 	struct dentry *debug_state_dentry;
+	struct dentry *debug_lockres_dentry;
 };
 
 struct debug_buffer {
 	int len;
 	char *buf;
+};
+
+struct debug_lockres {
+	int dl_len;
+	char *dl_buf;
+	struct dlm_ctxt *dl_ctxt;
+	struct dlm_lock_resource *dl_res;
 };
 
 int dlm_debug_init(struct dlm_ctxt *dlm);
