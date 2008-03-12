79fb336c6f6d960b78fa61706fe2b71ff1bb94f3
Trond Myklebust <Trond.Myklebust@netapp.com>
2008-03-12 12:24:49 -0400

    SUNRPC: Add a generic RPC credential
    Add an rpc credential that is not tied to any particular auth mechanism,
    but that can be cached by NFS, and later used to look up a cred for
    whichever auth mechanism that turns out to be valid when the RPC call is
    being made.
    
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- net/sunrpc/auth.c
+++ net/sunrpc/auth.c
@@ -567,6 +567,7 @@ static struct shrinker rpc_cred_shrinker = {
 void __init rpcauth_init_module(void)
 {
 	rpc_init_authunix();
+	rpc_init_generic_auth();
 	register_shrinker(&rpc_cred_shrinker);
 }
 
