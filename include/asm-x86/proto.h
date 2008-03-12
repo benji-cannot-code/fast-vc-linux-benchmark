9b5367bac4974e216f19507cea086dbbd7548173
Andi Kleen <andi@firstfloor.org>
2008-03-12 03:53:28 +0100

    x86: account overlapped mappings in max_pfn_mapped
    When end_pfn is not aligned to 2MB (or 1GB) then the kernel might
    map more memory than end_pfn. Account this in max_pfn_mapped.
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Cc: andreas.herrmann3@amd.com
    Cc: mingo@elte.hu
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/proto.h
+++ include/asm-x86/proto.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern void early_idt_handler(void);
 
-extern void init_memory_mapping(unsigned long start, unsigned long end);
-
 extern void system_call(void);
 extern void syscall_init(void);
 
