9c090fb278fb3611263e43e3a9c90c62c99b787f
Reynes Philippe <tremyfr@yahoo.fr>
2008-03-17 16:19:05 -0700

    sched: sched.c needs tick.h
    kernel/sched.c:506: erreur: implicit declaration of function tick_get_tick_sched
    kernel/sched.c:506: erreur: invalid type argument of ->
    kernel/sched.c:506: erreur: NOHZ_MODE_INACTIVE undeclared (first use in this function)
    kernel/sched.c:506: erreur: (Each undeclared identifier is reported only once
    kernel/sched.c:506: erreur: for each function it appears in.)
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- kernel/sched.c
+++ kernel/sched.c
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/unistd.h>
 #include <linux/pagemap.h>
 #include <linux/hrtimer.h>
+#include <linux/tick.h>
 
 #include <asm/tlb.h>
 #include <asm/irq_regs.h>
