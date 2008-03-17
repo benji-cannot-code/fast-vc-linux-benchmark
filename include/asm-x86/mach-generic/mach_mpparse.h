bd58352d5be016ee45782edecf2f8a95f2cb0853
Alexey Starikovskiy <astarikovskiy@suse.de>
2008-03-17 22:08:42 +0300

    x86: remove mpc_oem_pci_bus()
    Signed-off-by: Alexey Starikovskiy <astarikovskiy@suse.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/mach-generic/mach_mpparse.h
+++ include/asm-x86/mach-generic/mach_mpparse.h
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/genapic.h>
 
 #define mpc_oem_bus_info (genapic->mpc_oem_bus_info)
-#define mpc_oem_pci_bus (genapic->mpc_oem_pci_bus)
 
 int mps_oem_check(struct mp_config_table *mpc, char *oem, char *productid); 
 int acpi_madt_oem_check(char *oem_id, char *oem_table_id); 
