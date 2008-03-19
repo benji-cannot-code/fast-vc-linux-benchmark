12f81f471eaaf265b5d023126807a6a5badd5e60
Glauber de Oliveira Costa <gcosta@redhat.com>
2008-03-19 14:25:17 -0300

    x86: move assignment of CPU_PREPARE before do_boot_cpu
    Done to match x86_64
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/smpboot_32.c
+++ arch/x86/kernel/smpboot_32.c
@@ -959,6 +959,7 @@ int __cpuinit native_cpu_up(unsigned int cpu)
 		return -EINVAL;
 	}
 
+	per_cpu(cpu_state, cpu) = CPU_UP_PREPARE;
 #ifdef CONFIG_HOTPLUG_CPU
 
 	/*
@@ -977,7 +978,6 @@ int __cpuinit native_cpu_up(unsigned int cpu)
 		return -EIO;
 	}
 
-	per_cpu(cpu_state, cpu) = CPU_UP_PREPARE;
 	/* Unleash the CPU! */
 	cpu_set(cpu, smp_commenced_mask);
 
