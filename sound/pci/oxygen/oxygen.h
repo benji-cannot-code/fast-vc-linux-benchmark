b7fc498e02c1154867808d4be47190aeed510af0
Clemens Ladisch <clemens@ladisch.de>
2008-03-19 08:19:41 +0100

    [ALSA] oxygen: change model-specific PCM device configuration
    When specifying which PCM devices to use, model drivers now use flags
    that also specify the routing between PCM devices and DMA channels
    instead of just DMA channel bits.  This simplifies some code that checks
    for these flags.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/oxygen/oxygen.h
+++ sound/pci/oxygen/oxygen.h
@@ -17,6 +17,16 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define PCM_AC97	5
 #define PCM_COUNT	6
 
+/* model-specific configuration of outputs/inputs */
+#define PLAYBACK_0_TO_I2S	0x001
+#define PLAYBACK_1_TO_SPDIF	0x004
+#define PLAYBACK_2_TO_AC97_1	0x008
+#define CAPTURE_0_FROM_I2S_1	0x010
+#define CAPTURE_0_FROM_I2S_2	0x020
+#define CAPTURE_1_FROM_SPDIF	0x080
+#define CAPTURE_2_FROM_I2S_2	0x100
+#define CAPTURE_2_FROM_AC97_1	0x200
+
 enum {
 	CONTROL_SPDIF_PCM,
 	CONTROL_SPDIF_INPUT_BITS,
@@ -92,8 +102,8 @@ struct oxygen_model {
 				 unsigned int reg, int mute);
 	void (*gpio_changed)(struct oxygen *chip);
 	size_t model_data_size;
+	unsigned int pcm_dev_cfg;
 	u8 dac_channels;
-	u8 used_channels;
 	u8 function_flags;
 	u16 dac_i2s_format;
 	u16 adc_i2s_format;
