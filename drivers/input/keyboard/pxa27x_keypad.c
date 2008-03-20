bc8364032f20bc8e7ca437108f9df5ef6b141f85
Samuel Ortiz <sameo@openedhand.com>
2008-03-20 09:48:14 -0400

    Input: pxa27x - fix keypad KPC macros
    We want to mask (key_number - 1), not key_number. The current
    implementation works fine for all values but the maximum one,
    i.e. 8.
    
    Signed-off-by: Samuel Ortiz <sameo@openedhand.com>
    Acked-by: Eric Miao <eric.miao@marvell.com>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/input/keyboard/pxa27x_keypad.c
+++ drivers/input/keyboard/pxa27x_keypad.c
@@ -51,9 +51,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KPKDI           0x0048
 
 /* bit definitions */
-#define KPC_MKRN(n)	((((n) & 0x7) - 1) << 26) /* matrix key row number */
-#define KPC_MKCN(n)	((((n) & 0x7) - 1) << 23) /* matrix key column number */
-#define KPC_DKN(n)	((((n) & 0x7) - 1) << 6)  /* direct key number */
+#define KPC_MKRN(n)	((((n) - 1) & 0x7) << 26) /* matrix key row number */
+#define KPC_MKCN(n)	((((n) - 1) & 0x7) << 23) /* matrix key column number */
+#define KPC_DKN(n)	((((n) - 1) & 0x7) << 6)  /* direct key number */
 
 #define KPC_AS          (0x1 << 30)  /* Automatic Scan bit */
 #define KPC_ASACT       (0x1 << 29)  /* Automatic Scan on Activity */
