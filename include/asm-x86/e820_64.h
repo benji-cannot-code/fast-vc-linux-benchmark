d8a98151b3522fcf25c53e76dc6a1ae5c21d0864
Yinghai Lu <yhlu.kernel.send@gmail.com>
2008-03-20 23:58:33 -0700

    x86: early memtest to find bad ram
    do simple memtest after init_memory_mapping
    
    use find_e820_area_size to find all ram range that is not reserved.
    
    and do some simple bits test to find some bad ram.
    
    if find some bad ram, use reserve_early to exclude that range.
    
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/e820_64.h
+++ include/asm-x86/e820_64.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef __ASSEMBLY__
 extern unsigned long find_e820_area(unsigned long start, unsigned long end, 
 				    unsigned long size, unsigned long align);
+extern unsigned long find_e820_area_size(unsigned long start,
+					 unsigned long *sizep,
+					 unsigned long align);
 extern void add_memory_region(unsigned long start, unsigned long size, 
 			      int type);
 extern void update_memory_range(u64 start, u64 size, unsigned old_type,
