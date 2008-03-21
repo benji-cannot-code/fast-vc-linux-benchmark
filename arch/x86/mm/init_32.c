38da989d1cf3f974cac52e3333b66a2abcce4d94
Thomas Gleixner <tglx@linutronix.de>
2008-03-21 21:27:10 +0100

    x86: replace the now useless max_pfn_mapped define
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_32.c
+++ arch/x86/mm/init_32.c
@@ -52,7 +52,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 unsigned int __VMALLOC_RESERVE = 128 << 20;
 
-unsigned long end_pfn_map;
+unsigned long max_pfn_mapped;
 
 DEFINE_PER_CPU(struct mmu_gather, mmu_gathers);
 unsigned long highstart_pfn, highend_pfn;
@@ -197,7 +197,7 @@ static void __init kernel_physical_mapping_init(pgd_t *pgd_base)
 				set_pmd(pmd, pfn_pmd(pfn, prot));
 
 				pfn += PTRS_PER_PTE;
-				end_pfn_map = pfn;
+				max_pfn_mapped = pfn;
 				continue;
 			}
 			pte = one_page_table_init(pmd);
@@ -212,7 +212,7 @@ static void __init kernel_physical_mapping_init(pgd_t *pgd_base)
 
 				set_pte(pte, pfn_pte(pfn, prot));
 			}
-			end_pfn_map = pfn;
+			max_pfn_mapped = pfn;
 		}
 	}
 }
