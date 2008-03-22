f223b207eb3566fb1942daa1ec627dfd3d29bf15
Andi Kleen <andi@firstfloor.org>
2008-03-22 10:59:28 +0100

    x86: re-add rdmsrl_safe
    RDMSR for 64bit values with exception handling.
    
    Makes it easier to deal with 64bit valued MSRs. The old 64bit code
    base had that too as checking_rdmsrl(), but it got dropped somehow.
    
    Signed-off-by: Andi Kleen <andi@firstfloor.org>
    Cc: andreas.herrmann3@amd.com
    Cc: mingo@elte.hu
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/paravirt.h
+++ include/asm-x86/paravirt.h
@@ -694,6 +694,13 @@ do {						\
 	_err;					\
 })
 
+static inline int rdmsrl_safe(unsigned msr, unsigned long long *p)
+{
+	int err;
+
+	*p = paravirt_read_msr(msr, &err);
+	return err;
+}
 
 static inline u64 paravirt_read_tsc(void)
 {
