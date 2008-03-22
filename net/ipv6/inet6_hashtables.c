c0afed96ede8059ad881f55d78e729e1834f82e9
Pavel Emelyanov <xemul@openvz.org>
2008-03-22 16:50:58 -0700

    [SOCK]: Add udp_hash member to struct proto.
    Inspired by the commit ab1e0a13 ([SOCK] proto: Add hashinfo member to 
    struct proto) from Arnaldo, I made similar thing for UDP/-Lite IPv4 
    and -v6 protocols.
    
    The result is not that exciting, but it removes some levels of
    indirection in udpxxx_get_port and saves some space in code and text.
    
    The first step is to union existing hashinfo and new udp_hash on the
    struct proto and give a name to this union, since future initialization 
    of tcpxxx_prot, dccp_vx_protinfo and udpxxx_protinfo will cause gcc 
    warning about inability to initialize anonymous member this way.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/inet6_hashtables.c
+++ net/ipv6/inet6_hashtables.c
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 void __inet6_hash(struct sock *sk)
 {
-	struct inet_hashinfo *hashinfo = sk->sk_prot->hashinfo;
+	struct inet_hashinfo *hashinfo = sk->sk_prot->h.hashinfo;
 	struct hlist_head *list;
 	rwlock_t *lock;
 
