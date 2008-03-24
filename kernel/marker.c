ce5352d0f9cc618f2da4a5e399c41f8af09c048d
Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
2008-03-24 12:29:47 -0700

    markers: update preempt_disable. call_rcu, rcu_barrier comments
    Add comments requested by Andrew.
    
    Updated comments about synchronize_sched().  Since we use call_rcu and
    rcu_barrier now, these comments were out of sync with the code.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    Cc: Christoph Hellwig <hch@infradead.org>
    Cc: Mike Mason <mmlnx@us.ibm.com>
    Cc: Dipankar Sarma <dipankar@in.ibm.com>
    Cc: David Smith <dsmith@redhat.com>
    Cc: "Paul E. McKenney" <paulmck@us.ibm.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Adrian Bunk <adrian.bunk@movial.fi>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/marker.c
+++ kernel/marker.c
@@ -105,9 +105,9 @@ void marker_probe_cb(const struct marker *mdata, void *call_private,
 	char ptype;
 
 	/*
-	 * disabling preemption to make sure the teardown of the callbacks can
-	 * be done correctly when they are in modules and they insure RCU read
-	 * coherency.
+	 * preempt_disable does two things : disabling preemption to make sure
+	 * the teardown of the callbacks can be done correctly when they are in
+	 * modules and they insure RCU read coherency.
 	 */
 	preempt_disable();
 	ptype = ACCESS_ONCE(mdata->ptype);
@@ -552,9 +552,9 @@ static int set_marker(struct marker_entry **entry, struct marker *elem,
 
 /*
  * Disable a marker and its probe callback.
- * Note: only after a synchronize_sched() issued after setting elem->call to the
- * empty function insures that the original callback is not used anymore. This
- * insured by preemption disabling around the call site.
+ * Note: only waiting an RCU period after setting elem->call to the empty
+ * function insures that the original callback is not used anymore. This insured
+ * by preempt_disable around the call site.
  */
 static void disable_marker(struct marker *elem)
 {
@@ -566,8 +566,8 @@ static void disable_marker(struct marker *elem)
 	elem->ptype = 0;	/* single probe */
 	/*
 	 * Leave the private data and id there, because removal is racy and
-	 * should be done only after a synchronize_sched(). These are never used
-	 * until the next initialization anyway.
+	 * should be done only after an RCU period. These are never used until
+	 * the next initialization anyway.
 	 */
 }
 
@@ -602,9 +602,6 @@ void marker_update_probe_range(struct marker *begin,
 
 /*
  * Update probes, removing the faulty probes.
- * Issues a synchronize_sched() when no reference to the module passed
- * as parameter is found in the probes so the probe module can be
- * safely unloaded from now on.
  *
  * Internal callback only changed before the first probe is connected to it.
  * Single probe private data can only be changed on 0 -> 1 and 2 -> 1
