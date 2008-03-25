c66bdabafb13d405b3a9d359b2366c25ce04fe96
Glauber Costa <gcosta@redhat.com>
2008-03-25 18:36:24 -0300

    x86: move dma_unmap_sg to common header
    i386 gets an empty function.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/pci-base_32.c
+++ arch/x86/kernel/pci-base_32.c
@@ -35,6 +35,7 @@ static const struct dma_mapping_ops pci32_dma_ops = {
 	.map_single = pci32_map_single,
 	.unmap_single = NULL,
 	.map_sg = pci32_dma_map_sg,
+	.unmap_sg = NULL,
 };
 
 const struct dma_mapping_ops *dma_ops = &pci32_dma_ops;
