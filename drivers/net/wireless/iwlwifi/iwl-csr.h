4ceecffc16c387b71a59d23cc59395bf2527d805
Mohamed Abbas <mabbas@linux.intel.com>
2008-03-25 16:33:36 -0700

    iwlwifi: Add led support
    This patch add LEDS support to 3965 and 4965 drivers. It is based on
    led trigger and class. For our drivers we needed to avoid two things.
    1- We receive led trigger on/off on each Rx\Tx frame. In our driver
        we can not call led command like that. In this driver once driver
        receive a start of traffic it call the led command to start blinking
        then we count all bytes of Tx and Rx frame, after two second we count the
        blink rate of last two second then id blink rate changed we call the led
         commands
    2- Since we can call led command very often, we make sure we call the
        led command after we receive the statistics notification so
        we don't need to wake up the ucode id it is in sleep state.
        This patch was tested with 4965 and 3945.
    
    Signed-off-by: Mohamed Abbas <mabbas@linux.intel.com>
    Signed-off-by: Ian Schram<ischram@telenet.be>
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/iwlwifi/iwl-csr.h
+++ drivers/net/wireless/iwlwifi/iwl-csr.h
@@ -94,6 +94,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define CSR_UCODE_DRV_GP1_CLR   (CSR_BASE+0x05c)
 #define CSR_UCODE_DRV_GP2       (CSR_BASE+0x060)
 #define CSR_GIO_CHICKEN_BITS    (CSR_BASE+0x100)
+#define CSR_LED_REG             (CSR_BASE+0x094)
 
 /* Analog phase-lock-loop configuration (3945 only)
  * Set bit 24. */
@@ -214,6 +215,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* GI Chicken Bits */
 #define CSR_GIO_CHICKEN_BITS_REG_BIT_L1A_NO_L0S_RX  (0x00800000)
 #define CSR_GIO_CHICKEN_BITS_REG_BIT_DIS_L0S_EXIT_TIMER  (0x20000000)
+
+/* LED */
+#define CSR_LED_BSM_CTRL_MSK (0xFFFFFFDF)
+#define CSR_LED_REG_TRUN_ON (0x78)
+#define CSR_LED_REG_TRUN_OFF (0x38)
 
 /*=== HBUS (Host-side Bus) ===*/
 #define HBUS_BASE	(0x400)
--- drivers/net/wireless/iwlwifi/iwl4965-base.c
+++ drivers/net/wireless/iwlwifi/iwl4965-base.c
@@ -2239,8 +2239,9 @@ static void iwl4965_build_tx_cmd_basic(struct iwl_priv *priv,
 			cmd->cmd.tx.timeout.pm_frame_timeout = cpu_to_le16(3);
 		else
 			cmd->cmd.tx.timeout.pm_frame_timeout = cpu_to_le16(2);
-	} else
+	} else {
 		cmd->cmd.tx.timeout.pm_frame_timeout = 0;
+	}
 
 	cmd->cmd.tx.driver_txop = 0;
 	cmd->cmd.tx.tx_flags = tx_flags;
@@ -5713,6 +5714,8 @@ static void iwl4965_alive_start(struct iwl_priv *priv)
 	IWL_DEBUG_INFO("ALIVE processing complete.\n");
 	wake_up_interruptible(&priv->wait_command_queue);
 
+	iwl_leds_register(priv);
+
 	if (priv->error_recovering)
 		iwl4965_error_recovery(priv);
 
@@ -5736,6 +5739,8 @@ static void __iwl4965_down(struct iwl_priv *priv)
 
 	if (!exit_pending)
 		set_bit(STATUS_EXIT_PENDING, &priv->status);
+
+	iwl_leds_unregister(priv);
 
 	iwlcore_clear_stations_table(priv);
 
