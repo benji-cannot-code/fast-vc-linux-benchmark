0618ab28929e76c2395d58cfc6c780a67af4da12
Paul Mundt <lethal@linux-sh.org>
2008-03-26 19:02:47 +0900

    sh: Fix occasional FPU register corruption under preempt.
    Presently with preempt enabled there's the possibility to be preempted
    after the TIF_USEDFPU test and the register save, leading to bogus
    state post-__switch_to(). Use an explicit preempt_disable()/enable()
    pair around unlazy_fpu()/clear_fpu() to avoid this. Follows the x86
    change.
    
    Reported-by: Takuo Koguchi <takuo.koguchi.sw@hitachi.com>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/process_32.c
+++ arch/sh/kernel/process_32.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgalloc.h>
 #include <asm/system.h>
 #include <asm/ubc.h>
+#include <asm/fpu.h>
 
 static int hlt_counter;
 int ubc_usercnt = 0;
