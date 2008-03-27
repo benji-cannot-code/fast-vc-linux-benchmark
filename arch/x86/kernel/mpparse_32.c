6d0b1dc5046ef5b5beb797bee5cee05fc1ff886a
Alexey Starikovskiy <astarikovskiy@suse.de>
2008-03-27 23:54:57 +0300

    x86: move disabled_cpus to smpboot.c
    Signed-off-by: Alexey Starikovskiy <astarikovskiy@suse.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/mpparse_32.c
+++ arch/x86/kernel/mpparse_32.c
@@ -70,9 +70,8 @@ unsigned int boot_cpu_physical_apicid = -1U;
 /* Make it easy to share the UP and SMP code: */
 #ifndef CONFIG_X86_SMP
 unsigned int num_processors;
-#endif
-
 unsigned disabled_cpus __cpuinitdata;
+#endif
 
 /* Make it easy to share the UP and SMP code: */
 #ifndef CONFIG_X86_SMP
