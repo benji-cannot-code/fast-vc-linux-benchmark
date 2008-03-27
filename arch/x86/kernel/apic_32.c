eaca02228ed9c1fc3e666c5b621e572ab270be86
Alexey Starikovskiy <astarikovskiy@suse.de>
2008-03-27 23:54:38 +0300

    x86: move mp_lapic_addr to apic_32.c
    Signed-off-by: Alexey Starikovskiy <astarikovskiy@suse.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/apic_32.c
+++ arch/x86/kernel/apic_32.c
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 # error SPURIOUS_APIC_VECTOR definition error
 #endif
 
+unsigned long mp_lapic_addr;
+
 /*
  * Knob to control our willingness to enable the local APIC.
  *
