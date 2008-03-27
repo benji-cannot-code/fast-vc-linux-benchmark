24cd78f6057cf2f23f17a82692b07f848172a259
Lennert Buytenhek <buytenh@wantstofly.org>
2008-03-27 14:51:39 -0400

    sata_mv: mbus decode window support
    Make it possible to pass mbus_dram_target_info to the sata_mv
    driver via the platform data, make the sata_mv driver program
    the window registers based on this data if it is passed in, and
    make the Orion platform setup code use this method instead of
    programming the SATA mbus window registers by hand.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Reviewed-by: Tzachi Perelstein <tzachi@marvell.com>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Nicolas Pitre <nico@marvell.com>

--- arch/arm/mach-orion/common.c
+++ arch/arm/mach-orion/common.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mbus.h>
 #include <linux/mv643xx_eth.h>
 #include <linux/mv643xx_i2c.h>
+#include <linux/ata_platform.h>
 #include <asm/page.h>
 #include <asm/setup.h>
 #include <asm/timex.h>
@@ -290,6 +291,7 @@ static struct platform_device orion_sata = {
 
 void __init orion_sata_init(struct mv_sata_platform_data *sata_data)
 {
+	sata_data->dram = &orion_mbus_dram_info;
 	orion_sata.dev.platform_data = sata_data;
 	platform_device_register(&orion_sata);
 }
@@ -343,8 +345,6 @@ void __init orion_init(void)
 	 */
 	orion_setup_cpu_wins();
 	orion_setup_eth_wins();
-	if (dev == MV88F5182_DEV_ID)
-		orion_setup_sata_wins();
 
 	/*
 	 * REgister devices
