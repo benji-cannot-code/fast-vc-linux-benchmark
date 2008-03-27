51142ecf3b31275b7710e373cec1663e0b8daf38
Takashi Yamamoto <TakashiA.Yamamoto@jp.sony.com>
2008-03-27 11:37:38 +1100

    [POWERPC] PS3: Add ps3_get_speid routine
    Add a new routine ps3_get_speid() which returns the logical
    SPE ID.  This ID is needed for profiling support.
    
    Signed-off-by: Takashi Yamamoto <TakashiA.Yamamoto@jp.sony.com>
    Signed-off-by: Geoff Levand <geoffrey.levand@am.sony.com>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/platforms/ps3/spu.c
+++ arch/powerpc/platforms/ps3/spu.c
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/spu.h>
 #include <asm/spu_priv1.h>
 #include <asm/lv1call.h>
+#include <asm/ps3.h>
 
 #include "../cell/spufs/spufs.h"
 #include "platform.h"
@@ -140,6 +141,12 @@ static void _dump_areas(unsigned int spe_id, unsigned long priv2,
 	pr_debug("%s:%d: ls:      %lxh\n", func, line, ls);
 	pr_debug("%s:%d: shadow:  %lxh\n", func, line, shadow);
 }
+
+inline u64 ps3_get_spe_id(void *arg)
+{
+	return spu_pdata(arg)->spe_id;
+}
+EXPORT_SYMBOL_GPL(ps3_get_spe_id);
 
 static unsigned long get_vas_id(void)
 {
