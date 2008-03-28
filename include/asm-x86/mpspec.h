a8b76bb2d9ece55fcdd5c239ab99ebe573ac18b0
Ingo Molnar <mingo@elte.hu>
2008-03-28 11:59:57 +0100

    x86: mpparse, move generic processor info to apic_32.c fix
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/mpspec.h
+++ include/asm-x86/mpspec.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _AM_X86_MPSPEC_H
 #define _AM_X86_MPSPEC_H
 
+#include <linux/init.h>
+
 #include <asm/mpspec_def.h>
 
 #ifdef CONFIG_X86_32
