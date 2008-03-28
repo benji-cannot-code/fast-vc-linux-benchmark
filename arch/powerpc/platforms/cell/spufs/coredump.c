b2fa466edd0782e0ff71b62c1a78b9d6559f613d
Gerhard Stenzel <stenzel@de.ibm.com>
2008-03-28 14:18:30 +1100

    [POWERPC] spufs: fix incorrect file descriptors in SPU coredump note names
    At present, ppu-gdb can't trace spu infomation with coredump generated
    by the kernel. While the core dumps notes have correct contents, they
    have the wrong names, as the file descriptors used to generate the note
    names are off-by-one. An application that opens a SPE context as fd 3,
    the current core dump code will generate notes like:
      SPU/4/mem
      SPU/4/regs
    etc.
    
    This confuses GDB, which knows it is looking for SPE context 3 (from
    parsing the spu_context_run system call arguments), and cannot find
    any notes that match context 3.
    
    This change corrects the file descriptor counting, to only increment
    the fd until after we've written the note name.
    
    Signed-off-by: Gerhard Stenzel <stenzel@de.ibm.com>
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- arch/powerpc/platforms/cell/spufs/coredump.c
+++ arch/powerpc/platforms/cell/spufs/coredump.c
@@ -134,8 +134,6 @@ static struct spu_context *coredump_next_context(int *fd)
 		if (ctx->flags & SPU_CREATE_NOSCHED)
 			continue;
 
-		/* start searching the next fd next time we're called */
-		(*fd)++;
 		break;
 	}
 
@@ -158,6 +156,9 @@ int spufs_coredump_extra_notes_size(void)
 			break;
 
 		size += rc;
+
+		/* start searching the next fd next time */
+		fd++;
 	}
 
 	return size;
@@ -240,6 +241,9 @@ int spufs_coredump_extra_notes_write(struct file *file, loff_t *foffset)
 		}
 
 		spu_release_saved(ctx);
+
+		/* start searching the next fd next time */
+		fd++;
 	}
 
 	return 0;
