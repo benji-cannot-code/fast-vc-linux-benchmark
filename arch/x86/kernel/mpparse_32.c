50268721ffd844d00ae0982181fab0a53b9d2dc1
Ingo Molnar <mingo@elte.hu>
2008-03-28 11:57:55 +0100

    x86: mpparse, move disabled cpus to smpboot.c, fix
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/mpparse_32.c
+++ arch/x86/kernel/mpparse_32.c
@@ -197,7 +197,9 @@ static void __cpuinit MP_processor_info(struct mpc_config_processor *m)
 	int apicid;
 
 	if (!(m->mpc_cpuflag & CPU_ENABLED)) {
+#ifdef CONFIG_X86_SMP
 		disabled_cpus++;
+#endif
 		return;
 	}
 
@@ -902,7 +904,9 @@ void __cpuinit mp_register_lapic (u8 id, u8 enabled)
 	}
 
 	if (!enabled) {
+#ifdef CONFIG_X86_SMP
 		++disabled_cpus;
+#endif
 		return;
 	}
 
