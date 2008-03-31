ba83b1114ef9f89b725c92a8f645b3ec4d0cff26
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2008-03-31 10:03:41 +0900

    [SCSI] bsg: remove minor in struct bsg_device
    minor in struct bsg_device is used as identifier to find the
    corresponding struct bsg_device_class. However, request_queuse can be
    used as identifier for that and the minor in struct bsg_device is
    unnecessary.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- block/bsg.c
+++ block/bsg.c
@@ -38,7 +38,6 @@ struct bsg_device {
 	struct list_head done_list;
 	struct hlist_node dev_list;
 	atomic_t ref_count;
-	int minor;
 	int queued_cmds;
 	int done_cmds;
 	wait_queue_head_t wq_done;
@@ -759,9 +758,8 @@ static struct bsg_device *bsg_add_device(struct inode *inode,
 	bsg_set_block(bd, file);
 
 	atomic_set(&bd->ref_count, 1);
-	bd->minor = iminor(inode);
 	mutex_lock(&bsg_mutex);
-	hlist_add_head(&bd->dev_list, bsg_dev_idx_hash(bd->minor));
+	hlist_add_head(&bd->dev_list, bsg_dev_idx_hash(iminor(inode)));
 
 	strncpy(bd->name, rq->bsg_dev.class_dev->class_id, sizeof(bd->name) - 1);
 	dprintk("bound to <%s>, max queue %d\n",
@@ -771,7 +769,7 @@ static struct bsg_device *bsg_add_device(struct inode *inode,
 	return bd;
 }
 
-static struct bsg_device *__bsg_get_device(int minor)
+static struct bsg_device *__bsg_get_device(int minor, struct request_queue *q)
 {
 	struct bsg_device *bd;
 	struct hlist_node *entry;
@@ -779,7 +777,7 @@ static struct bsg_device *__bsg_get_device(int minor)
 	mutex_lock(&bsg_mutex);
 
 	hlist_for_each_entry(bd, entry, bsg_dev_idx_hash(minor), dev_list) {
-		if (bd->minor == minor) {
+		if (bd->queue == q) {
 			atomic_inc(&bd->ref_count);
 			goto found;
 		}
@@ -807,7 +805,7 @@ static struct bsg_device *bsg_get_device(struct inode *inode, struct file *file)
 	if (!bcd)
 		return ERR_PTR(-ENODEV);
 
-	bd = __bsg_get_device(iminor(inode));
+	bd = __bsg_get_device(iminor(inode), bcd->queue);
 	if (bd)
 		return bd;
 
