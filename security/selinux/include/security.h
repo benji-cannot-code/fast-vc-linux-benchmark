e7a671e6d785718c94e017839d5deadea888e42d
Eric Paris <eparis@redhat.com>
2008-04-01 13:24:09 -0400

    SELinux: turn mount options strings into defines
    Convert the strings used for mount options into #defines rather than
    retyping the string throughout the SELinux code.
    
    Signed-off-by: Eric Paris <eparis@redhat.com>
    Acked-by: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: James Morris <jmorris@namei.org>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -42,6 +42,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ROOTCONTEXT_MNT	0x04
 #define DEFCONTEXT_MNT	0x08
 
+#define CONTEXT_STR	"context="
+#define FSCONTEXT_STR	"fscontext="
+#define ROOTCONTEXT_STR	"rootcontext="
+#define DEFCONTEXT_STR	"defcontext="
+
 struct netlbl_lsm_secattr;
 
 extern int selinux_enabled;
