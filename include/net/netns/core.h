6016c7249c2969e75d7a83dd7e758acf9baf9b2a
Pavel Emelyanov <xemul@openvz.org>
2008-03-31 19:42:16 -0700

    [SOCK][NETNS]: Add the percpu prot_inuse counter in the struct net.
    Such an accounting would cost us two more dereferences to get the
    percpu variable from the struct net, so I make sock_prot_inuse_get
    and _add calls work differently depending on CONFIG_NET_NS - without
    it old optimized routines are used.
    
    The per-cpu counter for init_net is prepared in core_initcall, so
    that even af_inet, that starts as fs_initcall, will already have the
    init_net prepared.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/netns/core.h
+++ include/net/netns/core.h
@@ -3,12 +3,15 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __NETNS_CORE_H__
 
 struct ctl_table_header;
+struct prot_inuse;
 
 struct netns_core {
 	/* core sysctls */
 	struct ctl_table_header	*sysctl_hdr;
 
 	int	sysctl_somaxconn;
+
+	struct prot_inuse	*inuse;
 };
 
 #endif
