3428e3305911c441c8282f2cf5bda7cb732bd824
Holger Schurig <hs4233@mail.mn-solutions.de>
2008-04-02 16:27:42 +0200

    libertas: move association code from scan.c into assoc.c
    Besides code moving, I did the following changes:
    
    * made some functions static
    * removed some unneeded #include's
    * made patch checkpatch.pl clean
    
    Signed-off-by: Holger Schurig <hs4233@mail.mn-solutions.de>
    Acked-by: Dan Williams <dcbw@redhat.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/libertas/main.c
+++ drivers/net/wireless/libertas/main.c
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dev.h"
 #include "wext.h"
 #include "debugfs.h"
+#include "scan.h"
 #include "assoc.h"
 #include "cmd.h"
 
