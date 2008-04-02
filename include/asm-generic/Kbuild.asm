f65c082ebfbd0bcb5bb88baebaa0c83b20103786
Christian Borntraeger <borntraeger@de.ibm.com>
2008-04-02 13:04:40 -0700

    kvm: provide kvm.h for all architecture: fixes headers_install
    Currently include/linux/kvm.h is not considered by make headers_install,
    because Kbuild cannot handle " unifdef-$(CONFIG_FOO) += foo.h.  This problem
    was introduced by
    
    commit fb56dbb31c4738a3918db81fd24da732ce3b4ae6
    Author: Avi Kivity <avi@qumranet.com>
    Date:   Sun Dec 2 10:50:06 2007 +0200
    
        KVM: Export include/linux/kvm.h only if $ARCH actually supports KVM
    
        Currently, make headers_check barfs due to <asm/kvm.h>, which <linux/kvm.h>
        includes, not existing.  Rather than add a zillion <asm/kvm.h>s, export kvm.
        only if the arch actually supports it.
    
        Signed-off-by: Avi Kivity <avi@qumranet.com>
    
    which makes this an 2.6.25 regression.
    
    One way of solving the issue is to enhance Kbuild, but Avi and David conviced
    me, that changing headers_install is not the way to go.  This patch changes
    the definition for linux/kvm.h to unifdef-y.
    
    If  unifdef-y is used for linux/kvm.h "make headers_check" will fail on all
    architectures without asm/kvm.h.  Therefore, this patch also provides
    asm/kvm.h on all architectures.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Acked-by: Avi Kivity <avi@qumranet.com>
    Cc: Sam Ravnborg <sam@ravnborg.org
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: <linux-arch@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-generic/Kbuild.asm
+++ include/asm-generic/Kbuild.asm
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+header-y  += kvm.h
+
 ifeq ($(wildcard include/asm-$(SRCARCH)/a.out.h),include/asm-$(SRCARCH)/a.out.h)
 unifdef-y += a.out.h
 endif
