ef5fd643dad1c717c7fcef2f24b2b2f50c5a12c3
Guennadi Liakhovetski <g.liakhovetski@pengutronix.de>
2008-04-04 13:46:34 -0300

    V4L/DVB (7501): soc-camera: use a spinlock for videobuffer queue
    All drivers should provide a spinlock to be used in videobuf operations.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@pengutronix.de>
    Reviewed-by: Brandon Philips <bphilips@suse.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- include/media/soc_camera.h
+++ include/media/soc_camera.h
@@ -49,6 +49,7 @@ struct soc_camera_device {
 struct soc_camera_file {
 	struct soc_camera_device *icd;
 	struct videobuf_queue vb_vidq;
+	spinlock_t *lock;
 };
 
 struct soc_camera_host {
@@ -74,6 +75,8 @@ struct soc_camera_host_ops {
 	int (*try_bus_param)(struct soc_camera_device *, __u32);
 	int (*set_bus_param)(struct soc_camera_device *, __u32);
 	unsigned int (*poll)(struct file *, poll_table *);
+	spinlock_t* (*spinlock_alloc)(struct soc_camera_file *);
+	void (*spinlock_free)(spinlock_t *);
 };
 
 struct soc_camera_link {
