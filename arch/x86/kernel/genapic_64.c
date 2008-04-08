e7a148bce1bbb4d2a6e4d6be4a8f16338a719ffb
Ingo Molnar <mingo@elte.hu>
2008-04-08 12:20:50 +0200

    x86: support for new UV apic, prepare
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/genapic_64.c
+++ arch/x86/kernel/genapic_64.c
@@ -26,9 +26,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 /* which logical CPU number maps to which CPU (physical APIC ID) */
+#ifdef CONFIG_SMP
 u16 x86_cpu_to_apicid_init[NR_CPUS] __initdata
 					= { [0 ... NR_CPUS-1] = BAD_APICID };
 void *x86_cpu_to_apicid_early_ptr;
+#endif
 DEFINE_PER_CPU(u16, x86_cpu_to_apicid) = BAD_APICID;
 EXPORT_PER_CPU_SYMBOL(x86_cpu_to_apicid);
 
