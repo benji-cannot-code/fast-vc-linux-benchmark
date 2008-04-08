ce19d7116df84c9033eff2814f793456a01d263a
Glauber Costa <gcosta@redhat.com>
2008-04-08 13:20:43 -0300

    x86: introduce pci-dma.c
    This patch introduces pci-dma.c, a common file for pci dma
    between i386 and x86_64. As a start, dma_set_mask() is the same
    between architectures, and is placed there.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

---
+++ arch/x86/kernel/pci-dma.c
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#include <linux/dma-mapping.h>
+
+int dma_set_mask(struct device *dev, u64 mask)
+{
+	if (!dev->dma_mask || !dma_supported(dev, mask))
+		return -EIO;
+
+	*dev->dma_mask = mask;
+
+	return 0;
+}
+EXPORT_SYMBOL(dma_set_mask);
+
+
