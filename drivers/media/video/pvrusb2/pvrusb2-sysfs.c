57e112b11fb725845c8499f1489a5ce18a23edd3
Mike Isely <isely@pobox.com>
2008-04-09 05:44:57 -0300

    V4L/DVB (7715): pvrusb2: Clean out all use of __FUNCTION__
    Apparently the kernel developers no longer consider it proper
    etiquette to use __FUNCTION__; everyone must instead use __func__
    (even though it breaks with older compilers).  And worse still, actual
    effort is being expended to sweep this change throughout the kernel
    source tree.  Don't these people have better things to do?  So...
    Completely clean out all use of __FUNCTION__ from the pvrusb2 driver
    (it was just in the sysfs interface).  I'm not going to use __func__
    either.  So there.
    
    Signed-off-by: Mike Isely <isely@pobox.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/pvrusb2/pvrusb2-sysfs.c
+++ drivers/media/video/pvrusb2/pvrusb2-sysfs.c
@@ -609,8 +609,9 @@ static void pvr2_sysfs_add_control(struct pvr2_sysfs *sfp,int ctl_id)
 
 	ret = sysfs_create_group(&sfp->class_dev->kobj,&cip->grp);
 	if (ret) {
-		printk(KERN_WARNING "%s: sysfs_create_group error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "sysfs_create_group error: %d",
+			   ret);
 		return;
 	}
 	cip->created_ok = !0;
@@ -641,15 +642,17 @@ static void pvr2_sysfs_add_debugifc(struct pvr2_sysfs *sfp)
 	sfp->debugifc = dip;
 	ret = device_create_file(sfp->class_dev,&dip->attr_debugcmd);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		dip->debugcmd_created_ok = !0;
 	}
 	ret = device_create_file(sfp->class_dev,&dip->attr_debuginfo);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		dip->debuginfo_created_ok = !0;
 	}
@@ -852,8 +855,8 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	class_dev->driver_data = sfp;
 	ret = device_register(class_dev);
 	if (ret) {
-		printk(KERN_ERR "%s: device_register failed\n",
-		       __FUNCTION__);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_register failed");
 		kfree(class_dev);
 		return;
 	}
@@ -865,8 +868,9 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	ret = device_create_file(sfp->class_dev,
 				       &sfp->attr_v4l_minor_number);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		sfp->v4l_minor_number_created_ok = !0;
 	}
@@ -878,8 +882,9 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	ret = device_create_file(sfp->class_dev,
 				       &sfp->attr_v4l_radio_minor_number);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		sfp->v4l_radio_minor_number_created_ok = !0;
 	}
@@ -890,8 +895,9 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	sfp->attr_unit_number.store = NULL;
 	ret = device_create_file(sfp->class_dev,&sfp->attr_unit_number);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		sfp->unit_number_created_ok = !0;
 	}
@@ -903,8 +909,9 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	ret = device_create_file(sfp->class_dev,
 				       &sfp->attr_bus_info);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		sfp->bus_info_created_ok = !0;
 	}
@@ -916,8 +923,9 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	ret = device_create_file(sfp->class_dev,
 				 &sfp->attr_hdw_name);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		sfp->hdw_name_created_ok = !0;
 	}
@@ -929,8 +937,9 @@ static void class_dev_create(struct pvr2_sysfs *sfp,
 	ret = device_create_file(sfp->class_dev,
 				 &sfp->attr_hdw_desc);
 	if (ret < 0) {
-		printk(KERN_WARNING "%s: device_create_file error: %d\n",
-		       __FUNCTION__, ret);
+		pvr2_trace(PVR2_TRACE_ERROR_LEGS,
+			   "device_create_file error: %d",
+			   ret);
 	} else {
 		sfp->hdw_desc_created_ok = !0;
 	}
