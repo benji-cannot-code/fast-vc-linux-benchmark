5cccfabbd7dd98b746f60b6c52d80a2dc94c7671
Philipp Zabel <philipp.zabel@gmail.com>
2008-04-09 19:11:52 +0100

    [ARM] 4943/2: magician: fix magician.h GPIO header includes
    PXA GPIO definitions were split from pxa-regs.h into pxa2xx-gpio.h.
    
    Signed-off-by: Philipp Zabel <philipp.zabel@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- include/asm-arm/arch-pxa/magician.h
+++ include/asm-arm/arch-pxa/magician.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef _MAGICIAN_H_
 #define _MAGICIAN_H_
 
-#include <asm/arch/pxa-regs.h>
+#include <asm/arch/pxa2xx-gpio.h>
 
 /*
  * PXA GPIOs
