d7c70953c1068aad65242c4005c47a8050fa93b5
Janne Grunau <janne-dvb@grunau.be>
2008-04-09 19:13:13 -0300

    V4L/DVB (7538): Adds selectable adapter numbers as per module option
    The adapter_nr module options can be used to allocate static adapter
    numbers on a driver level. It avoids problems with changing DVB apapter
    numbers after warm/cold boot or device unplugging and repluging.
    
    Each driver holds DVB_MAX_ADAPTER long array of the preferred order of
    adapter numbers.
    
    options dvb-usb-dib0700 adapter_nr=7,6,5,4,3,2,1,0 would result in a
    reversed allocation of adapter numbers.
    
    With adapter_nr=2,5 it tries first to get adapter number 2 and 5. If
    both are already in use it will allocate the lowest free adapter number.
    
    Signed-off-by: Janne Grunau <janne-dvb@grunau.be>
    Acked-by: Hermann Pitton <hermann.pitton@arcor.de>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/ttusb-budget/dvb-ttusb-budget.c
+++ drivers/media/dvb/ttusb-budget/dvb-ttusb-budget.c
@@ -57,9 +57,10 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 */
 
 static int debug;
-
 module_param(debug, int, 0644);
 MODULE_PARM_DESC(debug, "Turn on/off debugging (default:off).");
+
+DVB_DEFINE_MOD_OPT_ADAPTER_NR(adapter_nr);
 
 #define dprintk(x...) do { if (debug) printk(KERN_DEBUG x); } while (0)
 
@@ -1670,7 +1671,10 @@ static int ttusb_probe(struct usb_interface *intf, const struct usb_device_id *i
 
 	mutex_unlock(&ttusb->semi2c);
 
-	if ((result = dvb_register_adapter(&ttusb->adapter, "Technotrend/Hauppauge Nova-USB", THIS_MODULE, &udev->dev)) < 0) {
+	result = dvb_register_adapter(&ttusb->adapter,
+				      "Technotrend/Hauppauge Nova-USB",
+				      THIS_MODULE, &udev->dev, adapter_nr);
+	if (result < 0) {
 		ttusb_free_iso_urbs(ttusb);
 		kfree(ttusb);
 		return result;
