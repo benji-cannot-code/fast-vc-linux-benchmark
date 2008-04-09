3e707e8edeaf82ea212cc464e3b21b97aec8800f
Joerg Roedel <joerg.roedel@amd.com>
2008-04-09 14:15:30 +0200

    KVM: SVM: add intercept for machine check exception
    To properly forward a MCE occured while the guest is running to the host, we
    have to intercept this exception and call the host handler by hand. This is
    implemented by this patch.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- include/asm-x86/kvm_host.h
+++ include/asm-x86/kvm_host.h
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SS_VECTOR 12
 #define GP_VECTOR 13
 #define PF_VECTOR 14
+#define MC_VECTOR 18
 
 #define SELECTOR_TI_MASK (1 << 2)
 #define SELECTOR_RPL_MASK 0x03
