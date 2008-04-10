7e213c9d65d39942a6940afe3f91525a414ab026
Stephen Rothwell <sfr@canb.auug.org.au>
2008-04-10 16:44:30 +1000

    [POWERPC] iSeries: Localise and constify some iSeries data
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- include/asm-powerpc/iseries/alpaca.h
+++ include/asm-powerpc/iseries/alpaca.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 struct alpaca {
 	struct lppaca *lppaca_ptr;	/* Pointer to LpPaca for PLIC */
-	void *reg_save_ptr;		/* Pointer to LpRegSave for PLIC */
+	const void *reg_save_ptr;	/* Pointer to LpRegSave for PLIC */
 };
 
 #endif /* _ASM_POWERPC_ISERIES_ALPACA_H */
