4517e247b97c0e637080790fff78fc3a7ab3e3df
Hartmut Hackmann <hartmut.hackmann@t-online.de>
2008-04-09 23:07:11 -0300

    V4L/DVB (7654): tda10086: make the xtal frequency a configuration option
    Some DVB-S boards, i.e. with the SD1878 tuner, use a 4 MHz reference frequency.
    This reqires a different setup of the clock PLL.
    This patch adds an enum to the tda10086_config struct and sets the proper
    values for the boards.
    
    This patch also fixes the DVB-S section of the MD7134_BRIDGE_2
    
    Signed-off-by: Hartmut Hackmann <hartmut.hackmann@t-online.de>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda10086.h
+++ drivers/media/dvb/frontends/tda10086.h
@@ -27,6 +27,11 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/dvb/frontend.h>
 #include <linux/firmware.h>
 
+enum tda10086_xtal {
+	TDA10086_XTAL_16M,
+	TDA10086_XTAL_4M
+};
+
 struct tda10086_config
 {
 	/* the demodulator's i2c address */
@@ -37,6 +42,9 @@ struct tda10086_config
 
 	/* do we need the diseqc signal with carrier? */
 	u8 diseqc_tone;
+
+	/* frequency of the reference xtal */
+	enum tda10086_xtal xtal_freq;
 };
 
 #if defined(CONFIG_DVB_TDA10086) || (defined(CONFIG_DVB_TDA10086_MODULE) && defined(MODULE))
