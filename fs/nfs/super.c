0d3cff1f5fc1588cf9d82f9d1cd9c756dada230d
Jeff Layton <jlayton@redhat.com>
2008-04-11 11:50:58 -0400

    NFS: remove duplicate flags assignment from nfs_validate_mount_data
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Signed-off-by: Trond Myklebust <Trond.Myklebust@netapp.com>

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -1276,7 +1276,6 @@ static int nfs_validate_mount_data(void *options,
 		args->flags		= data->flags;
 		args->rsize		= data->rsize;
 		args->wsize		= data->wsize;
-		args->flags		= data->flags;
 		args->timeo		= data->timeo;
 		args->retrans		= data->retrans;
 		args->acregmin		= data->acregmin;
