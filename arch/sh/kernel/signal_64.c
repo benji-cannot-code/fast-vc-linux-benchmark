ee5630f7bec1027094e0d6faeb4df7d7092d4ec4
Adrian Bunk <adrian.bunk@movial.fi>
2008-04-13 21:15:38 +0300

    sh64: add missing #include <asm/fpu.h>'s
    This patch fixes some compile errors due to missing save_fpu()
    prototypes on sh64 caused by
    commit 9bbafce2eec190ef7e44b0eb1095ba17ce6ad3af
    (sh: Fix occasional FPU register corruption under preempt).
    
    Signed-off-by: Adrian Bunk <adrian.bunk@movial.fi>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>

--- arch/sh/kernel/signal_64.c
+++ arch/sh/kernel/signal_64.c
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/uaccess.h>
 #include <asm/pgtable.h>
 #include <asm/cacheflush.h>
+#include <asm/fpu.h>
 
 #define REG_RET 9
 #define REG_ARG1 2
