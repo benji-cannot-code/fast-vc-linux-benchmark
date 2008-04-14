b92f20f1afb6bf2cba99d125a8bc5b011e6979f4
Pavel Emelyanov <xemul@openvz.org>
2008-04-13 22:28:42 -0700

    [NETNS]: Add an empty netns_dccp structure on struct net.
    According to the overall struct net design, it will be
    filled with DCCP-related members.
    
    Signed-off-by: Pavel Emelyanov <xemul@openvz.org>
    Acked-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/net_namespace.h
+++ include/net/net_namespace.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/netns/packet.h>
 #include <net/netns/ipv4.h>
 #include <net/netns/ipv6.h>
+#include <net/netns/dccp.h>
 #include <net/netns/x_tables.h>
 
 struct proc_dir_entry;
@@ -54,6 +55,9 @@ struct net {
 	struct netns_ipv4	ipv4;
 #if defined(CONFIG_IPV6) || defined(CONFIG_IPV6_MODULE)
 	struct netns_ipv6	ipv6;
+#endif
+#if defined(CONFIG_IP_DCCP) || defined(CONFIG_IP_DCCP_MODULE)
+	struct netns_dccp	dccp;
 #endif
 #ifdef CONFIG_NETFILTER
 	struct netns_xt		xt;
---
+++ include/net/netns/dccp.h
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
+#ifndef __NETNS_DCCP_H__
+#define __NETNS_DCCP_H__
+
+struct netns_dccp {
+};
+
+#endif
