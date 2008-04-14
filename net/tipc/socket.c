7c74e225f7944cb3205e1734fdb4cee03cca9897
Allan Stephens <allan.stephens@windriver.com>
2008-04-13 21:35:11 -0700

    [TIPC]: Improve socket time conversions
    This patch modifies TIPC's socket code to use standard kernel
    routines to handle time conversions between jiffies and ms.
    This ensures proper operation even when HZ isn't 1000.
    
    Acknowledgements to Eric Sesterhenn <snakebyte@gmx.de> for
    identifying this issue and proposing a solution.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/tipc/socket.c
+++ net/tipc/socket.c
@@ -59,7 +59,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SS_LISTENING	-1	/* socket is listening */
 #define SS_READY	-2	/* socket is connectionless */
 
-#define OVERLOAD_LIMIT_BASE    5000
+#define OVERLOAD_LIMIT_BASE	5000
+#define CONN_TIMEOUT_DEFAULT	8000	/* default connect timeout = 8s */
 
 struct tipc_sock {
 	struct sock sk;
@@ -171,7 +172,7 @@ static int tipc_create(struct net *net, struct socket *sock, int protocol)
 	}
 
 	sock_init_data(sock, sk);
-	sk->sk_rcvtimeo = 8 * HZ;   /* default connect timeout = 8s */
+	sk->sk_rcvtimeo = msecs_to_jiffies(CONN_TIMEOUT_DEFAULT);
 
 	tsock = tipc_sk(sk);
 	port = tipc_get_port(ref);
@@ -1530,7 +1531,7 @@ static int setsockopt(struct socket *sock,
 		res = tipc_set_portunreturnable(tsock->p->ref, value);
 		break;
 	case TIPC_CONN_TIMEOUT:
-		sock->sk->sk_rcvtimeo = (value * HZ / 1000);
+		sock->sk->sk_rcvtimeo = msecs_to_jiffies(value);
 		break;
 	default:
 		res = -EINVAL;
@@ -1583,7 +1584,7 @@ static int getsockopt(struct socket *sock,
 		res = tipc_portunreturnable(tsock->p->ref, &value);
 		break;
 	case TIPC_CONN_TIMEOUT:
-		value = (sock->sk->sk_rcvtimeo * 1000) / HZ;
+		value = jiffies_to_msecs(sock->sk->sk_rcvtimeo);
 		break;
 	default:
 		res = -EINVAL;
