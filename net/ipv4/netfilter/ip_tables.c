5f1bf02d397ae57c39b74e2dae3a1d6b24844498
Patrick McHardy <kaber@trash.net>
2008-04-14 11:15:45 +0200

    [NETFILTER]: {ip,ip6,arp}_tables: return EAGAIN for invalid SO_GET_ENTRIES size
    Rule dumping is performed in two steps: first userspace gets the
    ruleset size using getsockopt(SO_GET_INFO) and allocates memory,
    then it calls getsockopt(SO_GET_ENTRIES) to actually dump the
    ruleset. When another process changes the ruleset in between the
    sizes from the first getsockopt call doesn't match anymore and
    the kernel aborts. Unfortunately it returns EAGAIN, as for multiple
    other possible errors, so userspace can't distinguish this case
    from real errors.
    
    Return EAGAIN so userspace can retry the operation.
    
    Fixes (with current iptables SVN version) netfilter bugzilla #104.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -1181,7 +1181,7 @@ get_entries(struct net *net, struct ipt_get_entries __user *uptr, int *len)
 		else {
 			duprintf("get_entries: I've got %u not %u!\n",
 				 private->size, get.size);
-			ret = -EINVAL;
+			ret = -EAGAIN;
 		}
 		module_put(t->me);
 		xt_table_unlock(t);
@@ -1940,7 +1940,7 @@ compat_get_entries(struct net *net, struct compat_ipt_get_entries __user *uptr,
 		} else if (!ret) {
 			duprintf("compat_get_entries: I've got %u not %u!\n",
 				 private->size, get.size);
-			ret = -EINVAL;
+			ret = -EAGAIN;
 		}
 		xt_compat_flush_offsets(AF_INET);
 		module_put(t->me);
