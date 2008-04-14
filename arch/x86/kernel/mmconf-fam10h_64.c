7eb2f848b7bed97b3bd9b8b77f64836f084d54ea
Yinghai Lu <yhlu.kernel@gmail.com>
2008-04-14 16:08:25 -0700

    x86: add pci=check_enable_amd_mmconf and dmi check
    so will disable that feature by default, and only enable that via
    pci=check_enable_amd_mmconf or for system match with dmi table.
    
    Signed-off-by: Yinghai Lu <yhlu.kernel@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/kernel/mmconf-fam10h_64.c
+++ arch/x86/kernel/mmconf-fam10h_64.c
@@ -7,11 +7,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/mm.h>
 #include <linux/string.h>
 #include <linux/pci.h>
+#include <linux/dmi.h>
 #include <asm/pci-direct.h>
 #include <linux/sort.h>
 #include <asm/io.h>
 #include <asm/msr.h>
 #include <asm/acpi.h>
+
+#include "../pci/pci.h"
 
 struct pci_hostbridge_probe {
 	u32 bus;
@@ -177,6 +180,9 @@ void __cpuinit fam10h_check_enable_mmcfg(void)
 	u64 val;
 	u32 address;
 
+	if (!(pci_probe & PCI_CHECK_ENABLE_AMD_MMCONF))
+		return;
+
 	address = MSR_FAM10H_MMIO_CONF_BASE;
 	rdmsrl(address, val);
 
@@ -213,4 +219,26 @@ void __cpuinit fam10h_check_enable_mmcfg(void)
 	val |= fam10h_pci_mmconf_base | (8 << FAM10H_MMIO_CONF_BUSRANGE_SHIFT) |
 	       FAM10H_MMIO_CONF_ENABLE;
 	wrmsrl(address, val);
+}
+
+static int __devinit set_check_enable_amd_mmconf(const struct dmi_system_id *d)
+{
+        pci_probe |= PCI_CHECK_ENABLE_AMD_MMCONF;
+        return 0;
+}
+
+static struct dmi_system_id __devinitdata mmconf_dmi_table[] = {
+        {
+                .callback = set_check_enable_amd_mmconf,
+                .ident = "Sun Microsystems Machine",
+                .matches = {
+                        DMI_MATCH(DMI_SYS_VENDOR, "Sun Microsystems"),
+                },
+        },
+	{}
+};
+
+void __init check_enable_amd_mmconf_dmi(void)
+{
+	dmi_check_system(mmconf_dmi_table);
 }
