41f46b9c5ee057c66e7995e46c71a06dce39cc3c
Andrew Victor <linux@maxim.org.za>
2008-04-15 21:16:38 +0100

    [ARM] 4982/1: [AT91] Drop old-style UART initialization (Part 1)
    All the SAM9 boards supported by mainline and the AT91 patches have
    been converted to the new-style UART initialization.  Therefore drop
    support for the old at91_init_serial() interface for SAM9.
    
    at91_uarts[] array can also be marked as __initdata.
    
    The warning that no serial-console is defined moved from
    at91_set_serial_console() to at91_add_device_serial() since the whole
    point is the board-specific file is not calling
    at91_set_serial_console().
    
    Signed-off-by: Andrew Victor <linux@maxim.org.za>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-at91/at91cap9_devices.c
+++ arch/arm/mach-at91/at91cap9_devices.c
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/dma-mapping.h>
 #include <linux/platform_device.h>
-#include <linux/mtd/physmap.h>
+#include <linux/i2c-gpio.h>
 
 #include <video/atmel_lcdc.h>
 
@@ -349,6 +349,7 @@ void __init at91_add_device_nand(struct at91_nand_data *data)
 #else
 void __init at91_add_device_nand(struct at91_nand_data *data) {}
 #endif
+
 
 /* --------------------------------------------------------------------
  *  TWI (i2c)
@@ -1042,7 +1043,7 @@ static inline void configure_usart2_pins(unsigned pins)
 		at91_set_B_periph(AT91_PIN_PD6, 0);	/* CTS2 */
 }
 
-static struct platform_device *at91_uarts[ATMEL_MAX_UART];	/* the UARTs to use */
+static struct platform_device *__initdata at91_uarts[ATMEL_MAX_UART];	/* the UARTs to use */
 struct platform_device *atmel_default_console_device;	/* the serial console device */
 
 void __init at91_register_uart(unsigned id, unsigned portnr, unsigned pins)
@@ -1083,8 +1084,6 @@ void __init at91_set_serial_console(unsigned portnr)
 {
 	if (portnr < ATMEL_MAX_UART)
 		atmel_default_console_device = at91_uarts[portnr];
-	if (!atmel_default_console_device)
-		printk(KERN_INFO "AT91: No default serial console defined.\n");
 }
 
 void __init at91_add_device_serial(void)
@@ -1095,6 +1094,9 @@ void __init at91_add_device_serial(void)
 		if (at91_uarts[i])
 			platform_device_register(at91_uarts[i]);
 	}
+
+	if (!atmel_default_console_device)
+		printk(KERN_INFO "AT91: No default serial console defined.\n");
 }
 #else
 void __init at91_register_uart(unsigned id, unsigned portnr, unsigned pins) {}
