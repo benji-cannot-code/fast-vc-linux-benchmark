da4cfb00c5a715d0a04bc5b2d1bcb9f74eb8891e
Karl Dahlke <eklhad@comcast.net>
2008-04-15 01:30:32 -0400

    Input: put ledstate in the keyboard notifier
    Led state should be part of the key event, like shiftstate, and not
    grabbed asynchronously after the fact.
    
    [samuel.thibault@ens-lyon.org: various fixes]
    
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Dmitry Torokhov <dtor@mail.ru>

--- drivers/char/keyboard.c
+++ drivers/char/keyboard.c
@@ -1238,6 +1238,7 @@ static void kbd_keycode(unsigned int keycode, int down, int hw_raw)
 	}
 
 	param.shift = shift_final = (shift_state | kbd->slockstate) ^ kbd->lockstate;
+	param.ledstate = kbd->ledflagstate;
 	key_map = key_maps[shift_final];
 
 	if (atomic_notifier_call_chain(&keyboard_notifier_list, KBD_KEYCODE, &param) == NOTIFY_STOP || !key_map) {
@@ -1286,6 +1287,7 @@ static void kbd_keycode(unsigned int keycode, int down, int hw_raw)
 
 	(*k_handler[type])(vc, keysym & 0xff, !down);
 
+	param.ledstate = kbd->ledflagstate;
 	atomic_notifier_call_chain(&keyboard_notifier_list, KBD_POST_KEYSYM, &param);
 
 	if (type != KT_SLOCK)
