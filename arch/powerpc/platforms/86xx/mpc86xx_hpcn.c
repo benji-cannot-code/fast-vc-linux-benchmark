7cd38028abbbaf98449d78c8d0ecd382bde14ef8
Paul Gortmaker <paul.gortmaker@windriver.com>
2008-04-16 13:53:06 -0400

    [POWERPC] 86xx: mark functions static, other minor cleanups
    Cleanups as suggested by Stephen Rothwell and Dale Farnsworth, which
    incudes marking a bunch of functions static and add a vendor prefix to
    the compat node check for uniqueness.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>
    Signed-off-by: Kumar Gala <galak@kernel.crashing.org>

--- arch/powerpc/platforms/86xx/mpc86xx_hpcn.c
+++ arch/powerpc/platforms/86xx/mpc86xx_hpcn.c
@@ -56,7 +56,7 @@ static void mpc86xx_8259_cascade(unsigned int irq, struct irq_desc *desc)
 }
 #endif	/* CONFIG_PCI */
 
-void __init
+static void __init
 mpc86xx_hpcn_init_irq(void)
 {
 	struct mpic *mpic1;
@@ -163,7 +163,7 @@ mpc86xx_hpcn_setup_arch(void)
 }
 
 
-void
+static void
 mpc86xx_hpcn_show_cpuinfo(struct seq_file *m)
 {
 	struct device_node *root;
@@ -191,13 +191,13 @@ static int __init mpc86xx_hpcn_probe(void)
 {
 	unsigned long root = of_get_flat_dt_root();
 
-	if (of_flat_dt_is_compatible(root, "mpc86xx"))
+	if (of_flat_dt_is_compatible(root, "fsl,mpc8641hpcn"))
 		return 1;	/* Looks good */
 
 	return 0;
 }
 
-long __init
+static long __init
 mpc86xx_time_init(void)
 {
 	unsigned int temp;
