e944ce79190c997a02d36e96b8f94ad5c9c00b92
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2008-04-17 14:35:01 +1000

    [POWERPC] irqtrace support for 64-bit powerpc
    This adds the low level irq tracing hooks to the powerpc architecture
    needed to enable full lockdep functionality.
    
    This is partly based on Johannes Berg's initial version.  I removed
    the asm trampoline that isn't needed (thus improving performance) and
    modified all sorts of bits and pieces, reworking most of the assembly,
    etc...
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/kernel/setup_64.c
+++ arch/powerpc/kernel/setup_64.c
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/serial_8250.h>
 #include <linux/bootmem.h>
 #include <linux/pci.h>
+#include <linux/lockdep.h>
 #include <linux/lmb.h>
 #include <asm/io.h>
 #include <asm/kdump.h>
@@ -178,6 +179,9 @@ void __init early_setup(unsigned long dt_ptr)
 
 	/* Enable early debugging if any specified (see udbg.h) */
 	udbg_early_init();
+
+	/* Initialize lockdep early or else spinlocks will blow */
+	lockdep_init();
 
  	DBG(" -> early_setup(), dt_ptr: 0x%lx\n", dt_ptr);
 
