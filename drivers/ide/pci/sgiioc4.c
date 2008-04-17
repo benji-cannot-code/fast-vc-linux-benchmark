a1a143ce59b73b5b3fc8c6e553e211b6c9a841b3
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-04-18 00:46:23 +0200

    ide: remove dead/obsolete ->busproc method
    ->busproc method is used by HDIO_SET_BUSSTATE ioctl but it has no chance
    of working as intended (in 2.4.x days) because to issue an ioctl there
    is a device node needed and:
    
    - for BUSSTATE_TRISTATE+OFF it is too late (devices are already gone)
    
    - for BUSSTATE_TRISTATE+ON it is too early (devices are not registered yet)
    
    Just remove ->busproc method for now (it was only implemented by hpt366,
    siimage and tc86c001 host drivers).
    
    Cc: Sergei Shtylyov <sshtylyov@ru.mvista.com>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/sgiioc4.c
+++ drivers/ide/pci/sgiioc4.c
@@ -563,7 +563,6 @@ ide_init_sgiioc4(ide_hwif_t * hwif)
 						clear interrupts */
 	hwif->maskproc = &sgiioc4_maskproc;	/* Mask on/off NIEN register */
 	hwif->quirkproc = NULL;
-	hwif->busproc = NULL;
 
 	hwif->INB = &sgiioc4_INB;
 
