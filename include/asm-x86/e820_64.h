3b2415e6cc2ea0476617556371f576a1f1bdba5e
Yinghai Lu <Yinghai.Lu@Sun.COM>
2008-04-17 17:40:45 +0200

    x86: clean up find_e820_area(), 64-bit
    Change size to unsigned long, becase caller and user all used unsigned long.
    Also make bad_addr take an alignment parameter.
    
    Signed-off-by: Yinghai Lu <yinghai.lu@sun.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/e820_64.h
+++ include/asm-x86/e820_64.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #ifndef __ASSEMBLY__
 extern unsigned long find_e820_area(unsigned long start, unsigned long end, 
-				    unsigned size, unsigned long align);
+				    unsigned long size, unsigned long align);
 extern void add_memory_region(unsigned long start, unsigned long size, 
 			      int type);
 extern void update_memory_range(u64 start, u64 size, unsigned old_type,
