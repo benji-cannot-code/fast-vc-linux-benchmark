d6c9dd781c2d4cd742a42ecb9814bdb4cd2ab9c9
Bahadir Balban <bahadir.balban@arm.com>
2008-04-18 22:43:13 +0100

    RealView: Base support for the PB11MPCore platform
    This patch adds the base files for the PB11MPCore platform support.
    
    Signed-off-by: Bahadir Balban <bahadir.balban@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- include/asm-arm/arch-realview/irqs.h
+++ include/asm-arm/arch-realview/irqs.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define __ASM_ARCH_IRQS_H
 
 #include <asm/arch/board-eb.h>
+#include <asm/arch/board-pb11mp.h>
 
 #define IRQ_LOCALTIMER		29
 #define IRQ_LOCALWDOG		30
