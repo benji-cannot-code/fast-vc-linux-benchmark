10cb54cd556072b0531035f70688140e973cfad2
Kay Sievers <kay.sievers@vrfy.org>
2008-04-18 13:44:26 -0700

    [MTD] [MAPS] fix platform driver hotplug/coldplug
    Since 43cc71eed1250755986da4c0f9898f9a635cb3bf, the platform modalias is
    prefixed with "platform:".  Add MODULE_ALIAS() to the hotpluggable MTD mapping
    platform drivers, to re-enable auto loading.
    
    NOTE oddness with physmap ...  it's a legacy driver in some configs, which
    means it can't always support hotplugging.  (Not that most of these mapping
    drivers would often be used as modules...)
    
    [dbrownell@users.sourceforge.net: bugfix, more drivers, registration fixes]
    Signed-off-by: Kay Sievers <kay.sievers@vrfy.org>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- drivers/mtd/maps/omap_nor.c
+++ drivers/mtd/maps/omap_nor.c
@@ -157,6 +157,7 @@ static struct platform_driver omapflash_driver = {
 	.remove	= __devexit_p(omapflash_remove),
 	.driver = {
 		.name	= "omapflash",
+		.owner	= THIS_MODULE,
 	},
 };
 
@@ -175,4 +176,4 @@ module_exit(omapflash_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("MTD NOR map driver for TI OMAP boards");
-
+MODULE_ALIAS("platform:omapflash");
