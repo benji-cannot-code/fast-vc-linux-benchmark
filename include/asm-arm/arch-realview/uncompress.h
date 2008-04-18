cf4ebc787a18b35b7003269f52cef710829aaebe
Catalin Marinas <catalin.marinas@arm.com>
2008-04-18 22:43:13 +0100

    RealView: Add uncompressing support to PB11MPCore
    This patch adds the UART address detection in uncompress.h for the
    PB11MPCore platform.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- include/asm-arm/arch-realview/uncompress.h
+++ include/asm-arm/arch-realview/uncompress.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/mach-types.h>
 
 #include <asm/arch/board-eb.h>
+#include <asm/arch/board-pb11mp.h>
 
 #define AMBA_UART_DR(base)	(*(volatile unsigned char *)((base) + 0x00))
 #define AMBA_UART_LCRH(base)	(*(volatile unsigned char *)((base) + 0x2c))
@@ -35,6 +36,8 @@ static inline unsigned long get_uart_base(void)
 {
 	if (machine_is_realview_eb())
 		return REALVIEW_EB_UART0_BASE;
+	else if (machine_is_realview_pb11mp())
+		return REALVIEW_PB11MP_UART0_BASE;
 	else
 		return 0;
 }
