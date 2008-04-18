c9da6fd8eff795a730317d9fd318c8900689fd75
Bahadir Balban <bahadir.balban@arm.com>
2008-04-18 22:43:15 +0100

    RealView: Base support for the PB1176 platform
    This patch adds the base files for the PB1176 platform support.
    
    Signed-off-by: Bahadir Balban <bahadir.balban@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- include/asm-arm/arch-realview/irqs.h
+++ include/asm-arm/arch-realview/irqs.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <asm/arch/board-eb.h>
 #include <asm/arch/board-pb11mp.h>
+#include <asm/arch/board-pb1176.h>
 
 #define IRQ_LOCALTIMER		29
 #define IRQ_LOCALWDOG		30
