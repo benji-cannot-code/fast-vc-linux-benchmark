a6e3ad4813ece54ce17b35be37dcd5ce4007a9d5
Matthew Wilcox <matthew@wil.cx>
2008-04-18 22:15:50 -0400

    net: Remove unnecessary inclusions of asm/semaphore.h
    None of these files use any of the functionality promised by
    asm/semaphore.h.  It's possible that they rely on it dragging in some
    unrelated header file, but I can't build all these files, so we'll have
    fix any build failures as they come up.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>

--- net/dccp/proto.c
+++ net/dccp/proto.c
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <net/xfrm.h>
 
 #include <asm/ioctls.h>
-#include <asm/semaphore.h>
 #include <linux/spinlock.h>
 #include <linux/timer.h>
 #include <linux/delay.h>
