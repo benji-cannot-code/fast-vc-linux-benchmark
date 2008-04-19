b9f5bfbcd6102daea745ee2da9472560db78a314
Akinobu Mita <akinobu.mita@gmail.com>
2008-04-19 23:55:17 +0900

    x86: use cpumask function for present, possible, and online cpus
    cpu_online(), cpu_present(), for_each_possible_cpu(), num_possible_cpus()
    
    Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/genapic_64.c
+++ arch/x86/kernel/genapic_64.c
@@ -52,7 +52,7 @@ void __init setup_apic_routing(void)
 	else
 #endif
 
-	if (cpus_weight(cpu_possible_map) <= 8)
+	if (num_possible_cpus() <= 8)
 		genapic = &apic_flat;
 	else
 		genapic = &apic_physflat;
