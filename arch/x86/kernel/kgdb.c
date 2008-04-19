797b7f45d23a3a98ee8b920fe66223ca702c139d
Ingo Molnar <mingo@elte.hu>
2008-04-19 19:19:54 +0200

    x86: KGDB build fix
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/kgdb.c
+++ arch/x86/kernel/kgdb.c
@@ -47,11 +47,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/apicdef.h>
 #include <asm/system.h>
 
-#ifdef CONFIG_X86_32
-# include <mach_ipi.h>
-#else
-# include <asm/mach_apic.h>
-#endif
+#include <mach_ipi.h>
 
 /*
  * Put the error code here just in case the user cares:
