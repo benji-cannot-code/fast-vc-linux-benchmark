eccc79c726d71b127951eb9794c02b45b3169c55
Pavel Machek <pavel@suse.cz>
2008-04-21 22:15:06 +0000

    trivial: small cleanups
    These are small cleanups all over the tree.
    
    Trivial style and comment changes to
      fs/select.c, kernel/signal.c, kernel/stop_machine.c & mm/pdflush.c
    
    Signed-off-by: Pavel Machek <pavel@suse.cz>
    Signed-off-by: Jesper Juhl <jesper.juhl@gmail.com>

--- kernel/stop_machine.c
+++ kernel/stop_machine.c
@@ -136,8 +136,7 @@ static void restart_machine(void)
 	preempt_enable_no_resched();
 }
 
-struct stop_machine_data
-{
+struct stop_machine_data {
 	int (*fn)(void *);
 	void *data;
 	struct completion done;
