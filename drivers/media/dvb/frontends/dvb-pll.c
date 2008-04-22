f7f718133f0e6f55faf61d6e7c946e5ebe4ee621
Michael Krufky <mkrufky@linuxtv.org>
2008-04-22 14:46:20 -0300

    V4L/DVB (7425): dvb-pll: remove dead code
    remove #if 0'd support for:
    
    Philips TUV1236D
    Philips TD1316
    Philips FMD1216ME
    LG TDVS-H06xF
    Philips FCV1236D
    Thomson FE6600
    Microtune 4042 FI5
    Thomson dtt761x
    
    Support for these tuners has been moved into the 'tuner-simple' module.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/dvb-pll.c
+++ drivers/media/dvb/frontends/dvb-pll.c
@@ -81,7 +81,6 @@ struct dvb_pll_desc {
 /* ----------------------------------------------------------- */
 /* descriptions                                                */
 
-
 static struct dvb_pll_desc dvb_pll_thomson_dtt7579 = {
 	.name  = "Thomson dtt7579",
 	.min   = 177000000,
@@ -96,7 +95,6 @@ static struct dvb_pll_desc dvb_pll_thomson_dtt7579 = {
 		{  999999999, 166667, 0xf4, 0x08 },
 	},
 };
-
 
 static void thomson_dtt759x_bw(struct dvb_frontend *fe, u8 *buf,
 			       const struct dvb_frontend_parameters *params)
@@ -137,7 +135,6 @@ static struct dvb_pll_desc dvb_pll_lg_z201 = {
 		{  999999999, 166667, 0xfc, 0x04 },
 	},
 };
-
 
 static struct dvb_pll_desc dvb_pll_unknown_1 = {
 	.name  = "unknown 1", /* used by dntv live dvb-t */
@@ -247,7 +244,6 @@ static struct dvb_pll_desc dvb_pll_tua6034 = {
 	},
 };
 
-
 /* ALPS TDED4
  * used in Nebula-Cards and USB boxes
  */
@@ -290,7 +286,6 @@ static struct dvb_pll_desc dvb_pll_tdhu2 = {
 	}
 };
 
-
 /* Samsung TBMV30111IN / TBMV30712IN1
  * used in Air2PC ATSC - 2nd generation (nxt2002)
  */
@@ -327,8 +322,6 @@ static struct dvb_pll_desc dvb_pll_philips_sd1878_tda8261 = {
 	},
 };
 
-
-
 static void opera1_bw(struct dvb_frontend *fe, u8 *buf,
 		      const struct dvb_frontend_parameters *params)
 {
@@ -354,7 +347,6 @@ static struct dvb_pll_desc dvb_pll_opera1 = {
 		{ 2250000, 500, 0xe5, 0xc4 },
 	}
 };
-
 
 /* ----------------------------------------------------------- */
 
