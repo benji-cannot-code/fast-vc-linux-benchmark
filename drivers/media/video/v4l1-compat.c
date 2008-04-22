903d6630703d44eb0ca22d21473c03c67d5968b5
Marcin Slusarz <marcin.slusarz@gmail.com>
2008-04-22 14:45:57 -0300

    V4L/DVB (7365): reduce stack usage of v4l1_compat_sync
    poll_one allocated on stack struct poll_wqueues which is pretty big
    structure (>500 bytes on x86_64). v4l1_compat_sync invokes poll_one
    in a loop, so allocate struct poll_wqueues in v4l1_compat_sync (with
    kmalloc) and pass it to poll_one.
    
    Signed-off-by: Marcin Slusarz <marcin.slusarz@gmail.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/v4l1-compat.c
+++ drivers/media/video/v4l1-compat.c
@@ -200,14 +200,13 @@ pixelformat_to_palette(unsigned int pixelformat)
 
 /* ----------------------------------------------------------------- */
 
-static int poll_one(struct file *file)
+static int poll_one(struct file *file, struct poll_wqueues *pwq)
 {
 	int retval = 1;
 	poll_table *table;
-	struct poll_wqueues pwq; /*TODO: allocate dynamically*/
 
-	poll_initwait(&pwq);
-	table = &pwq.pt;
+	poll_initwait(pwq);
+	table = &pwq->pt;
 	for (;;) {
 		int mask;
 		set_current_state(TASK_INTERRUPTIBLE);
@@ -222,7 +221,7 @@ static int poll_one(struct file *file)
 		schedule();
 	}
 	set_current_state(TASK_RUNNING);
-	poll_freewait(&pwq);
+	poll_freewait(pwq);
 	return retval;
 }
 
@@ -1069,6 +1068,7 @@ static noinline int v4l1_compat_sync(
 	int err;
 	enum v4l2_buf_type captype = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	struct v4l2_buffer buf;
+	struct poll_wqueues *pwq;
 
 	memset(&buf, 0, sizeof(buf));
 	buf.index = *i;
@@ -1092,10 +1092,11 @@ static noinline int v4l1_compat_sync(
 		goto done;
 	}
 
+	pwq = kmalloc(sizeof(*pwq), GFP_KERNEL);
 	/*  Loop as long as the buffer is queued, but not done  */
 	while ((buf.flags & (V4L2_BUF_FLAG_QUEUED | V4L2_BUF_FLAG_DONE))
 						== V4L2_BUF_FLAG_QUEUED) {
-		err = poll_one(file);
+		err = poll_one(file, pwq);
 		if (err < 0 ||	/* error or sleep was interrupted  */
 		    err == 0)	/* timeout? Shouldn't occur.  */
 			break;
@@ -1103,6 +1104,7 @@ static noinline int v4l1_compat_sync(
 		if (err < 0)
 			dprintk("VIDIOCSYNC / VIDIOC_QUERYBUF: %d\n", err);
 	}
+	kfree(pwq);
 	if (!(buf.flags & V4L2_BUF_FLAG_DONE)) /* not done */
 		goto done;
 	do {
