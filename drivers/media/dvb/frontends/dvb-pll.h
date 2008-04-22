a659b44cf62363d3323b32d3bb229add96e3859f
Michael Krufky <mkrufky@linuxtv.org>
2008-04-22 14:46:19 -0300

    V4L/DVB (7423): dvb-pll: remove support for Philips TD1316
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/dvb-pll.h
+++ drivers/media/dvb/frontends/dvb-pll.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_TDHU2                  16
 #define DVB_PLL_SAMSUNG_TBMV           17
 #define DVB_PLL_PHILIPS_SD1878_TDA8261 18
-#define DVB_PLL_PHILIPS_TD1316         19
 #define DVB_PLL_OPERA1                 21
 
 /**
