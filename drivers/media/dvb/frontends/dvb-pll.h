62ac4060fca1f68f0b2bd4a47fcf6aa7eeb0ae4e
Michael Krufky <mkrufky@linuxtv.org>
2008-04-22 14:46:20 -0300

    V4L/DVB (7424): dvb-pll: remove support for Philips TUV1236D
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/dvb-pll.h
+++ drivers/media/dvb/frontends/dvb-pll.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define DVB_PLL_TUA6034                10
 #define DVB_PLL_TDA665X                12
 #define DVB_PLL_TDED4                  14
-#define DVB_PLL_TUV1236D               15
 #define DVB_PLL_TDHU2                  16
 #define DVB_PLL_SAMSUNG_TBMV           17
 #define DVB_PLL_PHILIPS_SD1878_TDA8261 18
