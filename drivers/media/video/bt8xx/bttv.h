feb239790cd2ab278b942f8c44cd66c40c63926d
Mauro Lacy <mauro@lacy.com.ar>
2008-04-22 14:45:58 -0300

    V4L/DVB (7368): bttv: added support for Kozumi KTV-01C card
    Signed-off-by: Mauro Lacy <mauro@lacy.com.ar>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/bt8xx/bttv.h
+++ drivers/media/video/bt8xx/bttv.h
@@ -175,6 +175,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define BTTV_BOARD_DVICO_FUSIONHDTV_2	   0x94
 #define BTTV_BOARD_TYPHOON_TVTUNERPCI	   0x95
 #define BTTV_BOARD_GEOVISION_GV600	   0x96
+#define BTTV_BOARD_KOZUMI_KTV_01C          0x97
 
 
 /* more card-specific defines */
