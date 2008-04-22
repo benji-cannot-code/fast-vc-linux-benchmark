96cac51ae31c84c21434fcd67ec5346a93ec10c2
Michael Krufky <mkrufky@linuxtv.org>
2008-04-22 14:46:06 -0300

    V4L/DVB (7385): tda18271: store agc_mode configuration independently of std_bits
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda18271.h
+++ drivers/media/dvb/frontends/tda18271.h
@@ -27,7 +27,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct tda18271_std_map_item {
 	u16 if_freq;
-	u8 std_bits;
+
+	/* EP3[4:3] */
+	unsigned int agc_mode:2;
+	/* EP3[2:0] */
+	unsigned int std:3;
+	/* EP4[7] */
 	unsigned int fm_rfn:1;
 };
 
