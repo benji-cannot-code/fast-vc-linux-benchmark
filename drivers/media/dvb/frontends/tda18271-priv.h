190e1710292fd79cf410278105a597062d67a85a
Michael Krufky <mkrufky@linuxtv.org>
2008-04-22 14:41:54 -0300

    V4L/DVB (7136): tda18271: use hybrid_tuner_request_state to manage tuner instances
    Convert tda18271 to use the new hybrid_tuner_request_state and
    hybrid_tuner_release_state macros to manage state sharing between
    hybrid tuner instances.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda18271-priv.h
+++ drivers/media/dvb/frontends/tda18271-priv.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/mutex.h>
+#include "tuner-i2c.h"
 #include "tda18271.h"
 
 #define R_ID     0x00	/* ID byte                */
@@ -99,17 +100,15 @@ enum tda18271_ver {
 };
 
 struct tda18271_priv {
-	u8 i2c_addr;
-	struct i2c_adapter *i2c_adap;
 	unsigned char tda18271_regs[TDA18271_NUM_REGS];
 
-	struct list_head tda18271_list;
+	struct list_head	hybrid_tuner_instance_list;
+	struct tuner_i2c_props	i2c_props;
 
 	enum tda18271_mode mode;
 	enum tda18271_i2c_gate gate;
 	enum tda18271_ver id;
 
-	unsigned int count;
 	unsigned int tm_rfcal;
 	unsigned int cal_initialized:1;
 
