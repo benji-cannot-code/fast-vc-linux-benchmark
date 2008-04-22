6e4c0b226c2754a2332443c918883d6f1809b583
Michael Krufky <mkrufky@linuxtv.org>
2008-04-22 14:46:06 -0300

    V4L/DVB (7383): tda18271: add attach-time parameter to limit i2c transfer size
    Add attach-time parameter, "small_i2c" to limit i2c transfer size to write
    at most 16 registers at a time during initialization.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/dvb/frontends/tda18271-fe.c
+++ drivers/media/dvb/frontends/tda18271-fe.c
@@ -1082,6 +1082,9 @@ struct dvb_frontend *tda18271_attach(struct dvb_frontend *fe, u8 addr,
 
 		fe->tuner_priv = priv;
 
+		if (cfg)
+			priv->small_i2c = cfg->small_i2c;
+
 		if (tda18271_get_id(fe) < 0)
 			goto fail;
 
