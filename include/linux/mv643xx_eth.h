458608d77f4814d6f8cb10b71edc0f0469d4bf45
Lennert Buytenhek <buytenh@wantstofly.org>
2008-04-24 01:27:32 +0200

    mv643xx_eth: configurable t_clk
    Make t_clk configurable via platform device data (with the current
    hardcoded value, 133 MHz, being the default), as it varies across
    different chip families.
    
    Signed-off-by: Lennert Buytenhek <buytenh@marvell.com>
    Acked-by: Nicolas Pitre <nico@marvell.com>
    Signed-off-by: Dale Farnsworth <dale@farnsworth.org>

--- include/linux/mv643xx_eth.h
+++ include/linux/mv643xx_eth.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 struct mv643xx_eth_shared_platform_data {
 	struct mbus_dram_target_info	*dram;
+	unsigned int	t_clk;
 };
 
 struct mv643xx_eth_platform_data {
