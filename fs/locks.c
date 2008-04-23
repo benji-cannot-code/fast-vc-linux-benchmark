b9f8bc480a21d298468311fb39112c5df3120c02
David M. Richter <richterd@citi.umich.edu>
2008-04-23 16:28:59 -0400

    leases: fix a return-value mixup
    Fixes a return-value mixup from 85c59580b30c82aa771aa33b37217a6b6851bc14
    "locks: Fix potential OOPS in generic_setlease()", in which -ENOMEM replaced
    what had been intended to stay -EAGAIN in the variable "error".
    
    Signed-off-by: David M. Richter <richterd@citi.umich.edu>
    Signed-off-by: J. Bruce Fields <bfields@citi.umich.edu>

--- fs/locks.c
+++ fs/locks.c
@@ -1405,6 +1405,7 @@ int generic_setlease(struct file *filp, long arg, struct file_lock **flp)
 			rdlease_count++;
 	}
 
+	error = -EAGAIN;
 	if ((arg == F_RDLCK && (wrlease_count > 0)) ||
 	    (arg == F_WRLCK && ((rdlease_count + wrlease_count) > 0)))
 		goto out;
