ae05c01f7920c5e9bdb86b8476bd9cd346ad2a5d
Cornelia Huck <cornelia.huck@de.ibm.com>
2008-04-24 10:15:28 +0200

    netiucv: Fix missing driver attributes.
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Frank Blaschka <frank.blaschka@de.ibm.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/s390/net/netiucv.c
+++ drivers/s390/net/netiucv.c
@@ -2146,6 +2146,7 @@ static int __init netiucv_init(void)
 	if (rc)
 		goto out_dbf;
 	IUCV_DBF_TEXT(trace, 3, __func__);
+	netiucv_driver.groups = netiucv_drv_attr_groups;
 	rc = driver_register(&netiucv_driver);
 	if (rc) {
 		PRINT_ERR("NETIUCV: failed to register driver.\n");
