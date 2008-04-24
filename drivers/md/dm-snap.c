735e963f9496f0723e633857edf1010b41cb591b
Milan Broz <mbroz@redhat.com>
2008-04-24 21:42:36 +0100

    dm snapshot: reduce default memory allocation
    Limit the amount of memory allocated per snapshot on systems
    with a large page size.  (The larger default chunk size on
    these systems compensates for the smaller number of pages reserved.)
    
    Signed-off-by: Milan Broz <mbroz@redhat.com>
    Signed-off-by: Alasdair G Kergon <agk@redhat.com>

--- drivers/md/dm-snap.c
+++ drivers/md/dm-snap.c
@@ -37,9 +37,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define SNAPSHOT_COPY_PRIORITY 2
 
 /*
- * Each snapshot reserves this many pages for io
+ * Reserve 1MB for each snapshot initially (with minimum of 1 page).
  */
-#define SNAPSHOT_PAGES 256
+#define SNAPSHOT_PAGES (((1UL << 20) >> PAGE_SHIFT) ? : 1)
 
 static struct workqueue_struct *ksnapd;
 static void flush_queued_bios(struct work_struct *work);
