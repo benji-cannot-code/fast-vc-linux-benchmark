b240ae12dfd9448db88957d97d9e1685ee167603
Tom Tucker <tom@opengridcomputing.com>
2008-04-24 21:30:47 -0500

    svc: Remove unused header files from svc_xprt.c
    This cosmetic patch removes unused header files that svc_xprt.c
    inherited from svcsock.c
    
    Signed-off-by: Tom Tucker <tom@opengridcomputing.com>

--- net/sunrpc/svc_xprt.c
+++ net/sunrpc/svc_xprt.c
@@ -7,30 +7,9 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include <linux/sched.h>
 #include <linux/errno.h>
-#include <linux/fcntl.h>
-#include <linux/net.h>
-#include <linux/in.h>
-#include <linux/inet.h>
-#include <linux/udp.h>
-#include <linux/tcp.h>
-#include <linux/unistd.h>
-#include <linux/slab.h>
-#include <linux/netdevice.h>
-#include <linux/skbuff.h>
-#include <linux/file.h>
 #include <linux/freezer.h>
 #include <linux/kthread.h>
 #include <net/sock.h>
-#include <net/checksum.h>
-#include <net/ip.h>
-#include <net/ipv6.h>
-#include <net/tcp_states.h>
-#include <linux/uaccess.h>
-#include <asm/ioctls.h>
-
-#include <linux/sunrpc/types.h>
-#include <linux/sunrpc/clnt.h>
-#include <linux/sunrpc/xdr.h>
 #include <linux/sunrpc/stats.h>
 #include <linux/sunrpc/svc_xprt.h>
 
