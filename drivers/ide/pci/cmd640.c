5481019e8b2ec4828dbabfffb5e35f89c5fbd995
Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>
2008-04-27 15:38:24 +0200

    ide: remove obsoleted "idex=" kernel parameters
    * Remove obsoleted "idex=" kernel parameters.
    
    * Make probe_* and cmd640_vlb variables static.
    
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/cmd640.c
+++ drivers/ide/pci/cmd640.c
@@ -112,10 +112,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #define DRV_NAME "cmd640"
 
-/*
- * This flag is set in ide.c by the parameter:  ide0=cmd640_vlb
- */
-int cmd640_vlb;
+static int cmd640_vlb;
 
 /*
  * CMD640 specific registers definition.
