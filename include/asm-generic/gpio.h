789d41e4381c44a272096848ad73e81c2ac2f761
Guennadi Liakhovetski <g.liakhovetski@pengutronix.de>
2008-04-28 02:14:44 -0700

    gpiolib: better rmmod infrastructure
    As long as one or more GPIOs on a gpio chip are used its driver should not be
    unloaded.  The existing mechanism (gpiochip_remove failure) doesn't address
    that, since rmmod can no longer be made to fail by having the cleanup code
    report errors.  Module usecounts are the solution.
    
    Assuming standard "initialize struct to zero" policies, this change won't
    affect SOC platform drivers.  However, drivers for external chips (on I2C and
    SPI busses) should be updated if they can be built as modules.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@pengutronix.de>
    [ gpio_ensure_requested() needs to update module usecounts too ]
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-generic/gpio.h
+++ include/asm-generic/gpio.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 struct seq_file;
+struct module;
 
 /**
  * struct gpio_chip - abstract a GPIO controller
@@ -49,6 +50,7 @@ struct seq_file;
  */
 struct gpio_chip {
 	char			*label;
+	struct module		*owner;
 
 	int			(*direction_input)(struct gpio_chip *chip,
 						unsigned offset);
