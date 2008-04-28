d15908b83962af79038be72910edc9951c8112bf
Jan Kara <jack@suse.cz>
2008-04-28 02:16:14 -0700

    ext3: fix mount messages when quota disabled
    When quota is disabled, we should not print 'journaled quota not supported'
    when user tried to mount non-journaled quota.  Also fix typo in the message.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/ext3/super.c
+++ fs/ext3/super.c
@@ -1098,6 +1098,9 @@ clear_qf_name:
 		case Opt_quota:
 		case Opt_usrquota:
 		case Opt_grpquota:
+			printk(KERN_ERR
+				"EXT3-fs: quota options not supported.\n");
+			break;
 		case Opt_usrjquota:
 		case Opt_grpjquota:
 		case Opt_offusrjquota:
@@ -1105,7 +1108,7 @@ clear_qf_name:
 		case Opt_jqfmt_vfsold:
 		case Opt_jqfmt_vfsv0:
 			printk(KERN_ERR
-				"EXT3-fs: journalled quota options not "
+				"EXT3-fs: journaled quota options not "
 				"supported.\n");
 			break;
 		case Opt_noquota:
