785a204c381fd552cfd636d081a772c13ebb262a
Harvey Harrison <harvey.harrison@gmail.com>
2008-04-28 02:16:21 -0700

    reiserfs: fix sparse warning in journal.c
    fs/reiserfs/journal.c:4319:2: warning: returning void-valued expression
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Cc: Chris Mason <chris.mason@oracle.com>
    Cc: Jeff Mahoney <jeffm@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/reiserfs/journal.c
+++ fs/reiserfs/journal.c
@@ -4317,5 +4317,5 @@ static void __reiserfs_journal_abort_soft(struct super_block *sb, int errno)
 
 void reiserfs_journal_abort(struct super_block *sb, int errno)
 {
-	return __reiserfs_journal_abort_soft(sb, errno);
+	__reiserfs_journal_abort_soft(sb, errno);
 }
