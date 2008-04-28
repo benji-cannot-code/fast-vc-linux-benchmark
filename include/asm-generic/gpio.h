7b0d9aefef34b5d2bab6f39d57aea00d40dce0b4
Guennadi Liakhovetski <g.liakhovetski@pengutronix.de>
2008-04-28 02:14:46 -0700

    gpio: define gpio_is_valid()
    Introduce a gpio_is_valid() predicate; use it in gpiolib.
    
    Signed-off-by: Guennadi Liakhovetski <g.liakhovetski@pengutronix.de>
        [ use inline function; follow the gpio_* naming convention;
          work without gpiolib; all programming interfaces need docs ]
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-generic/gpio.h
+++ include/asm-generic/gpio.h
@@ -17,6 +17,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define ARCH_NR_GPIOS		256
 #endif
 
+static inline int gpio_is_valid(int number)
+{
+	/* only some non-negative numbers are valid */
+	return ((unsigned)number) < ARCH_NR_GPIOS;
+}
+
 struct seq_file;
 struct module;
 
@@ -99,6 +105,12 @@ extern int __gpio_cansleep(unsigned gpio);
 
 
 #else
+
+static inline int gpio_is_valid(int number)
+{
+	/* only non-negative numbers are valid */
+	return number >= 0;
+}
 
 /* platforms that don't directly support access to GPIOs through I2C, SPI,
  * or other blocking infrastructure can use these wrappers.
