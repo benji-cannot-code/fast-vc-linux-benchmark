ee15e3fc817613dfefff2684d13ca6de14116723
Christoph Lameter <clameter@sgi.com>
2008-04-28 18:52:40 -0700

    x86: thread_info: merge thread_info allocation
    Make them similar so that both use THREAD_ORDER and THREAD_FLAGS and have a
    THREAD_SIZE definition that is setup in asm/page_xx.h
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/asm-x86/page_32.h
+++ include/asm-x86/page_32.h
@@ -14,6 +14,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 #define __PAGE_OFFSET		_AC(CONFIG_PAGE_OFFSET, UL)
 
+#ifdef CONFIG_4KSTACKS
+#define THREAD_ORDER	0
+#else
+#define THREAD_ORDER	1
+#endif
+#define THREAD_SIZE 	(PAGE_SIZE << THREAD_ORDER)
+
+
 #ifdef CONFIG_X86_PAE
 #define __PHYSICAL_MASK_SHIFT	36
 #define __VIRTUAL_MASK_SHIFT	32
