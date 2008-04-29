12f543faa216f7f12da10005edf5156c36196c4d
David S. Miller <davem@davemloft.net>
2008-04-29 02:38:50 -0700

    sparc: Add kgdb support.
    Current limitations:
    
    1) On SMP single stepping has some fundamental issues,
       shared with other sw single-step architectures such
       as mips and arm.
    
    2) On 32-bit sparc we don't support SMP kgdb yet.  That
       requires some reworking of the IPI mechanisms and
       infrastructure on that platform.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/ttable.h
+++ include/asm-sparc64/ttable.h
@@ -176,6 +176,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define KPROBES_TRAP(lvl) TRAP_ARG(bad_trap, lvl)
 #endif
 
+#ifdef CONFIG_KGDB
+#define KGDB_TRAP(lvl) TRAP_IRQ(kgdb_trap, lvl)
+#else
+#define KGDB_TRAP(lvl) TRAP_ARG(bad_trap, lvl)
+#endif
+
 #define SUN4V_ITSB_MISS					\
 	ldxa	[%g0] ASI_SCRATCHPAD, %g2;		\
 	ldx	[%g2 + HV_FAULT_I_ADDR_OFFSET], %g4;	\
