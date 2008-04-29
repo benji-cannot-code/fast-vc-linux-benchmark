76dfa7c1f9f1ca7f0433ec8192fad98dd5a79aa1
Joe Perches <joe@perches.com>
2008-04-29 08:11:12 -0400

    ext4: remove duplicate include of ext4_fs_i.h header file
    include/linux/ext4_fs_i.h is included in include/linux/ext_fs.h twice
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Mingming Cao <cmm@us.ibm.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- include/linux/ext4_fs.h
+++ include/linux/ext4_fs.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/types.h>
 #include <linux/blkdev.h>
 #include <linux/magic.h>
-
 #include <linux/ext4_fs_i.h>
 
 /*
@@ -177,7 +176,6 @@ struct ext4_group_desc
 #define EXT4_BG_INODE_ZEROED	0x0004 /* On-disk itable initialized to zero */
 
 #ifdef __KERNEL__
-#include <linux/ext4_fs_i.h>
 #include <linux/ext4_fs_sb.h>
 #endif
 /*
