5daca7b821f336a6d71c8b96e2a6224df264a124
Adrian Bunk <bunk@kernel.org>
2008-04-29 00:59:54 -0700

    make cgroup_enable_task_cg_lists() static
    Make the needlessly global cgroup_enable_task_cg_lists() static.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Paul Menage <menage@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -1716,7 +1716,7 @@ static void cgroup_advance_iter(struct cgroup *cgrp,
  * The tasklist_lock is not held here, as do_each_thread() and
  * while_each_thread() are protected by RCU.
  */
-void cgroup_enable_task_cg_lists(void)
+static void cgroup_enable_task_cg_lists(void)
 {
 	struct task_struct *p, *g;
 	write_lock(&css_set_lock);
