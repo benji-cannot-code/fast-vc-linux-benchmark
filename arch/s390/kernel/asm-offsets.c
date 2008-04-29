d48cbcf22eb17552dcea47554b5995c2157bfb05
Christoph Lameter <clameter@sgi.com>
2008-04-29 01:04:09 -0700

    s390: use kbuild.h instead of defining macros in asm-offsets.c
    s390 has a strange marker in DEFINE.  Undefine the DEFINE from kbuild.h and
    define it the way s390 wants it to preserve things as they were.
    
    May be good if the arch maintainer could go over this and check if this
    workaround is really necessary.
    
    Signed-off-by: Christoph Lameter <clameter@sgi.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- arch/s390/kernel/asm-offsets.c
+++ arch/s390/kernel/asm-offsets.c
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  */
 
 #include <linux/sched.h>
+#include <linux/kbuild.h>
 
 /* Use marker if you need to separate the values later */
-
+#undef DEFINE
 #define DEFINE(sym, val, marker) \
 	asm volatile("\n->" #sym " %0 " #val " " #marker : : "i" (val))
-
-#define BLANK() asm volatile("\n->" : : )
 
 int main(void)
 {
