f38c0f12b11ace92e28bb83c7d36c865700e34e2
Geoff Levand <geoffrey.levand@am.sony.com>
2008-05-01 08:24:58 +1000

    [POWERPC] Fix slb.c compile warnings
    Arrange for a syntax check to always be done on the powerpc/mm/slb.c
    DBG() macro by defining it to pr_debug() for non-debug builds.
    
    Also, fix these related compile warnings:
    
      slb.c:273: warning: format '%04x' expects type 'unsigned int', but argument 2 has type 'long unsigned int
      slb.c:274: warning: format '%04x' expects type 'unsigned int', but argument 2 has type 'long unsigned int'
    
    Signed-off-by: Geoff Levand <geoffrey.levand@am.sony.com>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Paul Mackerras <paulus@samba.org>

--- arch/powerpc/mm/slb.c
+++ arch/powerpc/mm/slb.c
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifdef DEBUG
 #define DBG(fmt...) udbg_printf(fmt)
 #else
-#define DBG(fmt...)
+#define DBG pr_debug
 #endif
 
 extern void slb_allocate_realmode(unsigned long ea);
@@ -280,8 +280,8 @@ void slb_initialize(void)
 		patch_slb_encoding(slb_compare_rr_to_size,
 				   mmu_slb_size);
 
-		DBG("SLB: linear  LLP = %04x\n", linear_llp);
-		DBG("SLB: io      LLP = %04x\n", io_llp);
+		DBG("SLB: linear  LLP = %04lx\n", linear_llp);
+		DBG("SLB: io      LLP = %04lx\n", io_llp);
 	}
 
 	get_paca()->stab_rr = SLB_NUM_BOLTED;
