0831e106dd01cbef1c1ddb1b8c291f12dfe2c4e5
Dan Williams <dan.j.williams@intel.com>
2008-04-30 00:52:31 -0700

    md: remove a stray command from a copy and paste error in resync_start_store
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -2461,7 +2461,6 @@ resync_start_show(mddev_t *mddev, char *page)
 static ssize_t
 resync_start_store(mddev_t *mddev, const char *buf, size_t len)
 {
-	/* can only set chunk_size if array is not yet active */
 	char *e;
 	unsigned long long n = simple_strtoull(buf, &e, 10);
 
