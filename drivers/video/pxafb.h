83a1cf99477600e04e61bb26108cc057f055ddeb
Eric Miao <eric.miao@marvell.com>
2008-04-30 00:52:25 -0700

    pxafb: move parallel LCD timing setup into dedicate function
    the new_regs stuff has been removed, and all the setup (modification to those
    fbi->reg_*) is protected with IRQ disabled
    
       * disable IRQ is too heavy here, provided that no IRQ context will
         touch the fbi->reg_* and the only possible contending place is
         in the CPUFREQ_POSTCHANGE (task context), a mutex will be better,
         leave this for future improvement
    
    Signed-off-by: eric miao <eric.miao@marvell.com>
    Cc: "Antonino A. Daplas" <adaplas@pol.net>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/pxafb.h
+++ drivers/video/pxafb.h
@@ -22,14 +22,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
  * for more details.
  */
 
-/* Shadows for LCD controller registers */
-struct pxafb_lcd_reg {
-	unsigned int lccr0;
-	unsigned int lccr1;
-	unsigned int lccr2;
-	unsigned int lccr3;
-};
-
 /* PXA LCD DMA descriptor */
 struct pxafb_dma_descriptor {
 	unsigned int fdadr;
