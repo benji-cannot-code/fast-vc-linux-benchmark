29a6da6937adbd566ca98e37e42747072bb782be
Jiri Slaby <jirislaby@gmail.com>
2008-04-30 00:53:43 -0700

    Char: moxa, ioctl cleanup
    - allow stats only for sys_admin
    - move TCSBRK* processing to .break_ctl tty op
    - let TIOCGSOFTCAR and TIOCSSOFTCAR be processed by ldisc
    - remove MOXA_GET_MAJOR, MOXA_GET_CUMAJOR
    - fix jiffies subtraction by time_after
    - move moxa ioctl numbers into the header; still not exported to userspace,
      needs _IOC and 32/64 compat cleanup anyways
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>
    Tested-by: Oyvind Aabling <Oyvind.Aabling@uni-c.dk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/char/moxa.h
+++ drivers/char/moxa.h
@@ -2,6 +2,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #ifndef MOXA_H_FILE
 #define MOXA_H_FILE
 
+#define	MOXA		0x400
+#define MOXA_GET_IQUEUE 	(MOXA + 1)	/* get input buffered count */
+#define MOXA_GET_OQUEUE 	(MOXA + 2)	/* get output buffered count */
+#define MOXA_GETDATACOUNT       (MOXA + 23)
+#define MOXA_GET_IOQUEUE	(MOXA + 27)
+#define MOXA_FLUSH_QUEUE	(MOXA + 28)
+#define MOXA_GETMSTATUS         (MOXA + 65)
+
 /*
  *    System Configuration
  */
