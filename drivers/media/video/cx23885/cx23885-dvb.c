1aa6c9d4d99434784a6efbd43646013064a31747
Michael Krufky <mkrufky@linuxtv.org>
2008-05-02 16:20:10 -0300

    V4L/DVB (7829): cx23885: remove remaining references to dvb-pll
    The cx23885 driver used to use dvb-pll for LG-TDVS-H064F support on the
    FusionHDTV5 Express.  This has since been converted to use tuner-simple
    instead, once digital tuning support was added to tuner-simple.
    
    Since cx23885 no longer uses dvb-pll, remove the #include "dvb-pll.h",
    and the DVB_PLL Kconfig selection.
    
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/video/cx23885/cx23885-dvb.c
+++ drivers/media/video/cx23885/cx23885-dvb.c
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "lgdt330x.h"
 #include "xc5000.h"
 #include "tda10048.h"
-#include "dvb-pll.h"
 #include "tuner-xc2028.h"
 #include "tuner-simple.h"
 #include "dib7000p.h"
