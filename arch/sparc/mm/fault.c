7ef3299a979d238073856f019081e0ed46094e2a
David S. Miller <davem@davemloft.net>
2008-05-01 21:28:59 -0700

    sparc32: Kill totally unused memory information tables.
    The code in arch/sparc/prom/memory.c computes three tables, the list
    of total memory, the list of available memory (total minus what
    firmware is using), and the list of firmware taken memory.
    
    Only the available memory list is even used.
    
    Therefore, kill those unused tables and make prom_meminfo() return
    just the available memory list.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/sparc/mm/fault.c
+++ arch/sparc/mm/fault.c
@@ -56,7 +56,7 @@ int prom_probe_memory (void)
 	register int i;
 
 	i = 0;
-	mlist= *prom_meminfo()->v0_available;
+	mlist= prom_meminfo();
 	bytes = tally = mlist->num_bytes;
 	base_paddr = (unsigned long) mlist->start_adr;
   
