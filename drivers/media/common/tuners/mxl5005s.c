693234385b315306f4f38e8b421b16be6faf914a
Steven Toth <stoth@hauppauge.com>
2008-05-03 14:28:43 -0300

    V4L/DVB(7877): mxl5005s: Ensure debug is off
    Ensure debug is off
    
    Signed-off-by: Steven Toth <stoth@hauppauge.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>

--- drivers/media/common/tuners/mxl5005s.c
+++ drivers/media/common/tuners/mxl5005s.c
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include "dvb_frontend.h"
 #include "mxl5005s.h"
 
-static int debug = 2;
+static int debug;
 
 #define dprintk(level, arg...) do {    \
 	if (level <= debug)            \
