adccaadb64f2d6ef6a8e158234c23673ff31cd12
David S. Miller <davem@davemloft.net>
2008-05-06 00:00:16 -0700

    Revert "atm: Do not free already unregistered net device."
    This reverts commit 65e4113684e50cee75357ce10dc9026b0929e4e9.
    
    Unlike the other cases Pavel fixed, this case did not
    setup a netdev->destructor of free_netdev, therefore this
    change was not correct.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/atm/br2684.c
+++ net/atm/br2684.c
@@ -351,6 +351,7 @@ static void br2684_push(struct atm_vcc *atmvcc, struct sk_buff *skb)
 			list_del(&brdev->br2684_devs);
 			write_unlock_irq(&devs_lock);
 			unregister_netdev(net_dev);
+			free_netdev(net_dev);
 		}
 		return;
 	}
@@ -771,6 +772,7 @@ static void __exit br2684_exit(void)
 
 		list_del(&brdev->br2684_devs);
 		unregister_netdev(net_dev);
+		free_netdev(net_dev);
 	}
 }
 
