98bc0b30495e40fcf41c89ed9152eb25103a020d
WANG Cong <xiyou.wangcong@gmail.com>
2008-05-06 12:45:35 +0800

    [Patch] fs/binfmt_elf.c: fix a wrong free
    In kmalloc failing path, we shouldn't free pointers in 'info',
    because the struct 'info' is uninitilized when kmalloc is called.
    
    And when kmalloc returns NULL, it's needless to kfree it.
    
    Signed-off-by: WANG Cong <wangcong@zeuux.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Reviewed-by: Pekka Enberg <penberg@cs.helsinki.fi>
    
    --
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -1901,7 +1901,7 @@ static int elf_core_dump(long signr, struct pt_regs *regs, struct file *file, un
 	/* alloc memory for large data structures: too large to be on stack */
 	elf = kmalloc(sizeof(*elf), GFP_KERNEL);
 	if (!elf)
-		goto cleanup;
+		goto out;
 	
 	segs = current->mm->map_count;
 #ifdef ELF_CORE_EXTRA_PHDRS
@@ -2035,8 +2035,9 @@ end_coredump:
 	set_fs(fs);
 
 cleanup:
-	kfree(elf);
 	free_note_info(&info);
+	kfree(elf);
+out:
 	return has_dumped;
 }
 
