23d6b6207de7d8e0ee7c708753a3548c99ceaf25
Brice Goglin <brice@myri.com>
2008-05-09 02:19:08 +0200

    myri10ge: trivial formatting fix
    Add some blank lines to uniformize the code and match
    the upstream code.
    
    Signed-off-by: Brice Goglin <brice@myri.com>
    Signed-off-by: Andrew Gallatin <gallatin@myri.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/net/myri10ge/myri10ge.c
+++ drivers/net/myri10ge/myri10ge.c
@@ -1329,7 +1329,7 @@ myri10ge_get_settings(struct net_device *netdev, struct ethtool_cmd *cmd)
 	ptr = mgp->product_code_string;
 	if (ptr == NULL) {
 		printk(KERN_ERR "myri10ge: %s: Missing product code\n",
-			netdev->name);
+		       netdev->name);
 		return 0;
 	}
 	for (i = 0; i < 3; i++, ptr++) {
@@ -1363,6 +1363,7 @@ static int
 myri10ge_get_coalesce(struct net_device *netdev, struct ethtool_coalesce *coal)
 {
 	struct myri10ge_priv *mgp = netdev_priv(netdev);
+
 	coal->rx_coalesce_usecs = mgp->intr_coal_delay;
 	return 0;
 }
@@ -1422,6 +1423,7 @@ myri10ge_get_ringparam(struct net_device *netdev,
 static u32 myri10ge_get_rx_csum(struct net_device *netdev)
 {
 	struct myri10ge_priv *mgp = netdev_priv(netdev);
+
 	if (mgp->csum_flag)
 		return 1;
 	else
@@ -1431,6 +1433,7 @@ static u32 myri10ge_get_rx_csum(struct net_device *netdev)
 static int myri10ge_set_rx_csum(struct net_device *netdev, u32 csum_enabled)
 {
 	struct myri10ge_priv *mgp = netdev_priv(netdev);
+
 	if (csum_enabled)
 		mgp->csum_flag = MXGEFW_FLAGS_CKSUM;
 	else
