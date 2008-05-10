4f1cc97dbfdd7535369f004f9f6aa50f98ad49ec
James Bottomley <James.Bottomley@HansenPartnership.com>
2008-05-10 08:33:58 -0500

    [SCSI] aha152x: fix init suspiciously returned 1, it should follow 0/-E convention
    Reported-by: Frank de Jong <frapex@xs4all.nl>
    > [1.] One line summary of the problem:
    > linux-2.6.25.3, aha152x'->init suspiciously returned 1, it should
    > follow 0/-E convention. The module / driver works okay. Unloading the
    > module is impossible.
    
    The driver is apparently returning 0 on failure and 1 on success.
    That's a bit unfortunate.  Fix it by altering to -ENODEV and 0.
    
    Cc: Stable Tree <stable@kernel.org>
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>

--- drivers/scsi/aha152x.c
+++ drivers/scsi/aha152x.c
@@ -3831,7 +3831,7 @@ static int __init aha152x_init(void)
 			iounmap(p);
 		}
 		if (!ok && setup_count == 0)
-			return 0;
+			return -ENODEV;
 
 		printk(KERN_INFO "aha152x: BIOS test: passed, ");
 #else
@@ -3910,7 +3910,7 @@ static int __init aha152x_init(void)
 #endif
 	}
 
-	return 1;
+	return 0;
 }
 
 static void __exit aha152x_exit(void)
