fc89d5fa42802c57bd18a33bb9b8e8c629ab6254
Thomas Gleixner <tglx@linutronix.de>
2008-05-12 15:43:36 +0200

    x86: init64.c include initrd.h
    free_initrd_mem needs a prototype, which is in linux/initrd.h
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/swap.h>
 #include <linux/smp.h>
 #include <linux/init.h>
+#include <linux/initrd.h>
 #include <linux/pagemap.h>
 #include <linux/bootmem.h>
 #include <linux/proc_fs.h>
