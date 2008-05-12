db8de9085de66f3b9ee57d4df694756d8798a0c5
Thomas Gleixner <tglx@linutronix.de>
2008-05-12 15:43:35 +0200

    x86: k8topology add missing header
    k8_scan_nodes is global and needs a prototype. Add the header file
    which contains it.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/k8topology_64.c
+++ arch/x86/mm/k8topology_64.c
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/numa.h>
 #include <asm/mpspec.h>
 #include <asm/apic.h>
+#include <asm/k8.h>
 
 static __init int find_northbridge(void)
 {
