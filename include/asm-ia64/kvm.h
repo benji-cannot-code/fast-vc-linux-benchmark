8aedee8ced5b30ac0e054e95f88fbec501a354e0
Xiantao Zhang <xiantao.zhang@intel.com>
2008-05-14 19:44:57 +0800

    KVM: ia64: Set KVM_IOAPIC_NUM_PINS to 48
    Guest's firmware needs an iosapic with 48 pins for ia64 guests.  Needed to
    get networking going.
    
    Signed-off-by: Xiantao Zhang <xiantao.zhang@intel.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- include/asm-ia64/kvm.h
+++ include/asm-ia64/kvm.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* Architectural interrupt line count. */
 #define KVM_NR_INTERRUPTS 256
 
-#define KVM_IOAPIC_NUM_PINS  24
+#define KVM_IOAPIC_NUM_PINS  48
 
 struct kvm_ioapic_state {
 	__u64 base_address;
