7949d25e986c782567e6e51f7b8e555882c4d225
Andrew Morton <akpm@linux-foundation.org>
2008-05-14 16:05:59 -0700

    net/irda/irnet/irnet_irda.c needs unaligned.h
    net/irda/irnet/irnet_irda.c: In function 'irnet_discovery_indication':
    net/irda/irnet/irnet_irda.c:1676: error: implicit declaration of function 'get_unaligned'
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- net/irda/irnet/irnet_irda.c
+++ net/irda/irnet/irnet_irda.c
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 #include "irnet_irda.h"		/* Private header */
 #include <linux/seq_file.h>
+#include <asm/unaligned.h>
 
 /*
  * PPP disconnect work: we need to make sure we're in
