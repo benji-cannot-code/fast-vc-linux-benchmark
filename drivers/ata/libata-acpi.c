5e37b40332d116aaed2afc32bb8f0589587a720f
Jeff Garzik <jeff@garzik.org>
2008-05-19 17:56:10 -0400

    drivers/ata: trim trailing whitespace
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/libata-acpi.c
+++ drivers/ata/libata-acpi.c
@@ -119,7 +119,7 @@ static void ata_acpi_associate_ide_port(struct ata_port *ap)
 		ap->pflags |= ATA_PFLAG_INIT_GTM_VALID;
 }
 
-static void ata_acpi_handle_hotplug(struct ata_port *ap, struct ata_device 
+static void ata_acpi_handle_hotplug(struct ata_port *ap, struct ata_device
 				    *dev, u32 event)
 {
 	char event_string[12];
@@ -131,7 +131,7 @@ static void ata_acpi_handle_hotplug(struct ata_port *ap, struct ata_device
 	acpi_handle handle, tmphandle;
 	unsigned long sta;
 	acpi_status status;
-	
+
 	if (!ap)
 		ap = dev->link->ap;
 	ehi = &ap->link.eh_info;
@@ -168,10 +168,10 @@ static void ata_acpi_handle_hotplug(struct ata_port *ap, struct ata_device
 			else {
 				struct ata_link *tlink;
 				struct ata_device *tdev;
-				
+
 				ata_port_for_each_link(tlink, ap) {
 					ata_link_for_each_dev(tdev, tlink) {
-						tdev->flags |= 
+						tdev->flags |=
 							ATA_DFLAG_DETACH;
 					}
 				}
