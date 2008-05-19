d046f2f44e741315fadc26c35fdb5451c0cd9c53
Adrian Bunk <bunk@kernel.org>
2008-05-20 01:02:40 +0300

    pcmcia: remove CVS keywords
    This patch removes CVS keywords that weren't updated for a long time
    from comments.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

--- drivers/pcmcia/hd64465_ss.c
+++ drivers/pcmcia/hd64465_ss.c
@@ -1,7 +1,5 @@
 FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /*
- * $Id: hd64465_ss.c,v 1.7 2003/07/06 14:42:50 lethal Exp $
- *
  * Device driver for the PCMCIA controller module of the
  * Hitachi HD64465 handheld companion chip.
  *
