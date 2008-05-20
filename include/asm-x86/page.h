5ece547de48bbf72c6eaa01310eb63dc759b0b16
Jeremy Fitzhardinge <jeremy@goop.org>
2008-05-20 08:26:18 +0100

    x86: fix warning on 32-bit non-PAE
    Fix the warning:
    
    include2/asm/pgtable.h: In function `pte_modify':
    include2/asm/pgtable.h:290: warning: left shift count >= width of type
    
    On 32-bit PAE the virtual and physical addresses are both 32-bits,
    so it ends up evaluating 1<<32.  Do the shift as a 64-bit shift then
    cast to the appropriate size.  This should all be done at compile time,
    and so have no effect on generated code.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Tested-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-x86/page.h
+++ include/asm-x86/page.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 /* to align the pointer to the (next) page boundary */
 #define PAGE_ALIGN(addr)	(((addr)+PAGE_SIZE-1)&PAGE_MASK)
 
-#define __PHYSICAL_MASK		((((phys_addr_t)1) << __PHYSICAL_MASK_SHIFT) - 1)
+#define __PHYSICAL_MASK		((phys_addr_t)(1ULL << __PHYSICAL_MASK_SHIFT) - 1)
 #define __VIRTUAL_MASK		((1UL << __VIRTUAL_MASK_SHIFT) - 1)
 
 #ifndef __ASSEMBLY__
