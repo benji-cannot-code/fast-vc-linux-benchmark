f49a8c877cba933fb97d8e55c65a9712b4d75d25
Jeremy Fitzhardinge <jeremy@goop.org>
2008-05-20 08:26:22 +0100

    x86: clarify use of _PAGE_CHG_MASK
    _PAGE_CHG_MASK is defined as the set of bits not updated by
    pte_modify(); specifically, the pfn itself, and the Accessed and Dirty
    bits (which are updated by hardware).
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Tested-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/asm-x86/pgtable.h
+++ include/asm-x86/pgtable.h
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _KERNPG_TABLE	(_PAGE_PRESENT | _PAGE_RW | _PAGE_ACCESSED |	\
 			 _PAGE_DIRTY)
 
+/* Set of bits not changed in pte_modify */
 #define _PAGE_CHG_MASK	(PTE_MASK | _PAGE_PCD | _PAGE_PWT |		\
 			 _PAGE_ACCESSED | _PAGE_DIRTY)
 
