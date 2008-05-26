f4518e2b7a400993b6679c8ead5f498630bcf76b
Jeremy Fitzhardinge <jeremy@goop.org>
2008-05-26 23:31:25 +0100

    xen-console: add save/restore
    Add code to:
    
     1. Deal with the console page being canonicalized.  During save, the
        console's mfn in the start_info structure is canonicalized to a pfn.
        In order to deal with that, we always use a copy of the pfn and
        indirect off that all the time.  However, we fall back to using the
        mfn if the pfn hasn't been initialized yet.
    
     2. Restore the console event channel, and rebind it to the existing irq.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/xen/hvc-console.h
+++ include/xen/hvc-console.h
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern struct console xenboot_console;
 
+void xen_console_resume(void);
+
 void xen_raw_console_write(const char *str);
 void xen_raw_printk(const char *fmt, ...);
 
