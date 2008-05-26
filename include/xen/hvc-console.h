a997372883ecc05eee5a50afcf903ad45ff113ca
Jeremy Fitzhardinge <jeremy@goop.org>
2008-05-26 23:30:59 +0100

    xen: add raw console write functions for debug
    Add a couple of functions which can write directly to the Xen console
    for debugging.  This output ends up on the host's dom0 console
    (assuming it allows the domain to write there).
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/xen/hvc-console.h
+++ include/xen/hvc-console.h
@@ -4,4 +4,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 
 extern struct console xenboot_console;
 
+void xen_raw_console_write(const char *str);
+void xen_raw_printk(const char *fmt, ...);
+
 #endif	/* XEN_HVC_CONSOLE_H */
