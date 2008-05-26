ea63f7cda555477ba7d6920e5f494933dda4ebbb
Jeremy Fitzhardinge <jeremy@goop.org>
2008-05-26 23:31:12 +0100

    xen: make earlyprintk=xen work again
    For some perverse reason, if you call add_preferred_console() it prevents
    setup_early_printk() from successfully enabling the boot console -
    unless you make it a preferred console too...
    
    Also, make xenboot console output distinct from normal console output,
    since it gets repeated when the console handover happens, and the
    duplicated output is confusing without disambiguation.
    
    Signed-off-by: Jeremy Fitzhardinge <jeremy.fitzhardinge@citrix.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Markus Armbruster <armbru@redhat.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>

--- arch/x86/xen/enlighten.c
+++ arch/x86/xen/enlighten.c
@@ -1258,6 +1258,7 @@ asmlinkage void __init xen_start_kernel(void)
 	boot_params.hdr.ramdisk_size = xen_start_info->mod_len;
 
 	if (!is_initial_xendomain()) {
+		add_preferred_console("xenboot", 0, NULL);
 		add_preferred_console("tty", 0, NULL);
 		add_preferred_console("hvc", 0, NULL);
 	}
