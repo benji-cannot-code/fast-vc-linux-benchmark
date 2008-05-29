7a15927b275ab8c5e3f1dab6ddea4dab9a7e1e9f
Christian Borntraeger <borntraeger@de.ibm.com>
2008-05-29 11:10:01 +0200

    virtio_blk: fix endianess annotations
    Since commit 72e61eb40b55dd57031ec5971e810649f82b0259 (virtio: change config
    to guest endian) config space is no longer fixed endian.
    
    Lets change the virtio_blk_config variables.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

--- include/linux/virtio_blk.h
+++ include/linux/virtio_blk.h
@@ -16,14 +16,14 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct virtio_blk_config
 {
 	/* The capacity (in 512-byte sectors). */
-	__le64 capacity;
+	__u64 capacity;
 	/* The maximum segment size (if VIRTIO_BLK_F_SIZE_MAX) */
-	__le32 size_max;
+	__u32 size_max;
 	/* The maximum number of segments (if VIRTIO_BLK_F_SEG_MAX) */
-	__le32 seg_max;
+	__u32 seg_max;
 	/* geometry the device (if VIRTIO_BLK_F_GEOMETRY) */
 	struct virtio_blk_geometry {
-		__le16 cylinders;
+		__u16 cylinders;
 		__u8 heads;
 		__u8 sectors;
 	} geometry;
