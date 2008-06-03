6ed68671b8f147cb09cf5572b615ad96267170d3
Ingo Molnar <mingo@elte.hu>
2008-06-03 09:28:52 +0200

    x86 mpparse: build fix
    fix this build bug:
    
     drivers/acpi/pci_irq.c: In function 'acpi_pci_irq_enable':
     drivers/acpi/pci_irq.c:574: error: implicit declaration of function 'mp_config_acpi_gsi'
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/acpi.h
+++ include/asm-x86/acpi.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/numa.h>
 #include <asm/processor.h>
 #include <asm/mmu.h>
+#include <asm/mpspec.h>
 
 #define COMPILER_DEPENDENT_INT64   long long
 #define COMPILER_DEPENDENT_UINT64  unsigned long long
