6542798d673a745366b578a11da89f918b4d6b36
Glauber Costa <gcosta@redhat.com>
2008-06-04 15:35:03 -0300

    x86: move cpu_exit_clear to process_32.c
    Take it out of smpboot.c, and move it to process_32.c, closer
    to its only user.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/numa_32.h
+++ include/asm-x86/numa_32.h
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #define _ASM_X86_32_NUMA_H 1
 
 extern int pxm_to_nid(int pxm);
+extern void numa_remove_cpu(int cpu);
 
 #ifdef CONFIG_NUMA
 extern void __init remap_numa_kva(void);
