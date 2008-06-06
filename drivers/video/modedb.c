750df21aa2906511c2fdb9e321bc04c12b04a4fa
Geoff Levand <geoffrey.levand@am.sony.com>
2008-06-05 22:46:44 -0700

    fbdev: export symbol fb_mode_option
    Frame buffer and mode setting drivers can be built as modules,
    so fb_mode_option needs to be exported to support these.
    
    Prevents this error:
    
      ERROR: "fb_mode_option" [drivers/ps3/ps3av_mod.ko] undefined!
    
    Signed-off-by: Geoff Levand <geoffrey.levand@am.sony.com>
    Acked-by: Geert Uytterhoeven <Geert.Uytterhoeven@sonycom.com>
    Cc: Krzysztof Helt <krzysztof.h1@poczta.fm>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- drivers/video/modedb.c
+++ drivers/video/modedb.c
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #endif
 
 const char *fb_mode_option;
+EXPORT_SYMBOL_GPL(fb_mode_option);
 
     /*
      *  Standard video mode definitions (taken from XFree86)
