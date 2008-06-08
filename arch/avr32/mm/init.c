ef5e70222127e61caf2d582b8012735390f2caa8
Haavard Skinnemoen <haavard.skinnemoen@atmel.com>
2008-06-08 23:28:45 +0200

    avr32: export empty_zero_page
    Fixes one of two ext4 build problems:
    ERROR: "empty_zero_page" [fs/ext4/ext4dev.ko] undefined!
    
    Signed-off-by: Haavard Skinnemoen <haavard.skinnemoen@atmel.com>

--- arch/avr32/mm/init.c
+++ arch/avr32/mm/init.c
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <linux/swap.h>
 #include <linux/init.h>
 #include <linux/mmzone.h>
+#include <linux/module.h>
 #include <linux/bootmem.h>
 #include <linux/pagemap.h>
 #include <linux/nodemask.h>
@@ -29,6 +30,7 @@ DEFINE_PER_CPU(struct mmu_gather, mmu_gathers);
 pgd_t swapper_pg_dir[PTRS_PER_PGD];
 
 struct page *empty_zero_page;
+EXPORT_SYMBOL(empty_zero_page);
 
 /*
  * Cache of MMU context last used.
