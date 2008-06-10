263c83ae86217db76f2929d7cee2cd1026257ee9
Andreas Herrmann <andreas.herrmann3@amd.com>
2008-06-10 16:05:18 +0200

    x86: PAT: fix ambiguous paranoia check in pat_init()
    Starting with commit 8d4a4300854f3971502e81dacd930704cb88f606 (x86:
    cleanup PAT cpu validation) the PAT CPU feature flag is not cleared
    anymore. Now the error message
    
      "PAT enabled, but CPU feature cleared"
    
    in pat_init() is misleading.
    
    Furthermore the current code does not check for existence of the PAT
    CPU feature flag if a CPU is whitelisted in validate_pat_support.
    
    This patch clears pat_wc_enabled if boot CPU has no PAT feature flag
    and adapts the paranoia check.
    
    Signed-off-by: Andreas Herrmann <andreas.herrmann3@amd.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/cpu/addon_cpuid_features.c
+++ arch/x86/kernel/cpu/addon_cpuid_features.c
@@ -54,6 +54,9 @@ void __cpuinit init_scattered_cpuid_features(struct cpuinfo_x86 *c)
 #ifdef CONFIG_X86_PAT
 void __cpuinit validate_pat_support(struct cpuinfo_x86 *c)
 {
+	if (!cpu_has_pat)
+		pat_disable("PAT not supported by CPU.");
+
 	switch (c->x86_vendor) {
 	case X86_VENDOR_INTEL:
 		if (c->x86 == 0xF || (c->x86 == 6 && c->x86_model >= 15))
@@ -65,8 +68,6 @@ void __cpuinit validate_pat_support(struct cpuinfo_x86 *c)
 		return;
 	}
 
-	pat_disable(cpu_has_pat ?
-		    "PAT disabled. Not yet verified on this CPU type." :
-		    "PAT not supported by CPU.");
+	pat_disable("PAT disabled. Not yet verified on this CPU type.");
 }
 #endif
