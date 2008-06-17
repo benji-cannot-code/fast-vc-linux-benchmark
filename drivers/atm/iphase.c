620793ef2e8be1099b1e56c1b696ac93a9fb12e2
Jorge Boncompte [DTI2] <jorge@dti2.net>
2008-06-16 17:16:04 -0700

    atm: [iphase] set drvdata before enabling interrupts
    Signed-off-by: Jorge Boncompte [DTI2] <jorge@dti2.net>
    Signed-off-by: Chas Williams <chas@cmf.nrl.navy.mil>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- drivers/atm/iphase.c
+++ drivers/atm/iphase.c
@@ -3199,6 +3199,8 @@ static int __devinit ia_init_one(struct pci_dev *pdev,
 	IF_INIT(printk("dev_id = 0x%x iadev->LineRate = %d \n", (u32)dev,
 		iadev->LineRate);)
 
+	pci_set_drvdata(pdev, dev);
+
 	ia_dev[iadev_count] = iadev;
 	_ia_dev[iadev_count] = dev;
 	iadev_count++;
@@ -3219,8 +3221,6 @@ static int __devinit ia_init_one(struct pci_dev *pdev,
 
 	iadev->next_board = ia_boards;  
 	ia_boards = dev;  
-
-	pci_set_drvdata(pdev, dev);
 
 	return 0;
 
