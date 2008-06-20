b4f87773f1495706930f4e25c569c76e2475fcdb
Ingo Molnar <mingo@elte.hu>
2008-06-20 15:34:46 +0200

    x86: unify __set_fixmap, fix
    fix build failure:
    
     arch/x86/mm/pgtable.c:280: warning: ‘enum fixed_addresses’ declared inside parameter list
     arch/x86/mm/pgtable.c:280: warning: its scope is only this definition or declaration, which is probably not what you want
     arch/x86/mm/pgtable.c:280: error: parameter 1 (‘idx’) has incomplete type
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/mm/pgtable.c
+++ arch/x86/mm/pgtable.c
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/pgalloc.h>
 #include <asm/pgtable.h>
 #include <asm/tlb.h>
+#include <asm/fixmap.h>
 
 pte_t *pte_alloc_one_kernel(struct mm_struct *mm, unsigned long address)
 {
