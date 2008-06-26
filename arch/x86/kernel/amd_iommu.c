e8dc2759885713abe6c42db900b0eec14d029459
Joerg Roedel <joerg.roedel@amd.com>
2008-06-26 21:28:06 +0200

    x86, AMD IOMMU: add amd_iommu.h to export functions to the generic x86 dma code
    This patch adds the amd_iommu.h file which will be included in the generic
    code.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Cc: iommu@lists.linux-foundation.org
    Cc: bhavna.sarathy@amd.com
    Cc: Sebastian.Biemueller@amd.com
    Cc: robert.richter@amd.com
    Cc: joro@8bytes.org
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/amd_iommu.c
+++ arch/x86/kernel/amd_iommu.c
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 #include <asm/proto.h>
 #include <asm/gart.h>
 #include <asm/amd_iommu_types.h>
+#include <asm/amd_iommu.h>
 
 #define CMD_SET_TYPE(cmd, t) ((cmd)->data[1] |= ((t) << 28))
 
