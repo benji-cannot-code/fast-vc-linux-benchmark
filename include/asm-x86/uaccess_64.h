6821ddeb68dd6647b5d60ad57fd49af39265209b
Glauber Costa <gcosta@redhat.com>
2008-06-30 17:34:39 -0300

    x86: introduce likely in macro.
    Put the likely hint in access_ok. Just for
    bisectability.
    
    Signed-off-by: Glauber Costa <gcosta@redhat.com>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- include/asm-x86/uaccess_64.h
+++ include/asm-x86/uaccess_64.h
@@ -49,7 +49,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 	flag;								\
 })
 
-#define access_ok(type, addr, size) (__range_not_ok(addr, size) == 0)
+#define access_ok(type, addr, size) (likely(__range_not_ok(addr, size) == 0))
 
 /*
  * The exception table consists of pairs of addresses: the first is the
