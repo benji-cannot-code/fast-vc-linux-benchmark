cb476f6f5923507803f18470d6de200a101257ed
Harvey Harrison <harvey.harrison@gmail.com>
2008-07-04 19:48:58 +0800

    crypto: rmd - sparse annotations
    Similar to the rmd128.c annotations, significantly cuts down on the
    noise.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- crypto/rmd160.c
+++ crypto/rmd160.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct rmd160_ctx {
 	u64 byte_count;
 	u32 state[5];
-	u32 buffer[16];
+	__le32 buffer[16];
 };
 
 #define K1  RMD_K1
@@ -53,7 +53,7 @@ struct rmd160_ctx {
 	(c) = rol32((c), 10); \
 }
 
-static void rmd160_transform(u32 *state, u32 const *in)
+static void rmd160_transform(u32 *state, const __le32 *in)
 {
 	u32 aa, bb, cc, dd, ee, aaa, bbb, ccc, ddd, eee;
 
@@ -314,8 +314,8 @@ static void rmd160_final(struct crypto_tfm *tfm, u8 *out)
 {
 	struct rmd160_ctx *rctx = crypto_tfm_ctx(tfm);
 	u32 i, index, padlen;
-	u64 bits;
-	u32 *dst = (u32 *)out;
+	__le64 bits;
+	__le32 *dst = (__le32 *)out;
 	static const u8 padding[64] = { 0x80, };
 
 	bits = cpu_to_le64(rctx->byte_count << 3);
--- crypto/rmd256.c
+++ crypto/rmd256.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:linux-main-100k-v1-e0bd41dc-8e12-4f1b-978d-a3645ae59da0
 struct rmd256_ctx {
 	u64 byte_count;
 	u32 state[8];
-	u32 buffer[16];
+	__le32 buffer[16];
 };
 
 #define K1  RMD_K1
@@ -49,7 +49,7 @@ struct rmd256_ctx {
 	(a) = rol32((a), (s)); \
 }
 
-static void rmd256_transform(u32 *state, u32 const *in)
+static void rmd256_transform(u32 *state, const __le32 *in)
 {
 	u32 aa, bb, cc, dd, aaa, bbb, ccc, ddd, tmp;
 
@@ -289,8 +289,8 @@ static void rmd256_final(struct crypto_tfm *tfm, u8 *out)
 {
 	struct rmd256_ctx *rctx = crypto_tfm_ctx(tfm);
 	u32 i, index, padlen;
-	u64 bits;
-	u32 *dst = (u32 *)out;
+	__le64 bits;
+	__le32 *dst = (__le32 *)out;
 	static const u8 padding[64] = { 0x80, };
 
 	bits = cpu_to_le64(rctx->byte_count << 3);
