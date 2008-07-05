382638cf536382f718a63bc7e42bf864b177f780
Cyrill Gorcunov <gorcunov@gmail.com>
2008-07-05 15:53:39 +0400

    x86: e820 memmap - add checking for NULL early param
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>
    Cc: Cyrill Gorcunov <gorcunov@gmail.com>
    Cc: akpm@linux-foundation.org
    Cc: andi@firstfloor.org
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/e820.c
+++ arch/x86/kernel/e820.c
@@ -1199,6 +1199,9 @@ static int __init parse_memmap_opt(char *p)
 	char *oldp;
 	u64 start_at, mem_size;
 
+	if (!p)
+		return -EINVAL;
+
 	if (!strcmp(p, "exactmap")) {
 #ifdef CONFIG_CRASH_DUMP
 		/*
